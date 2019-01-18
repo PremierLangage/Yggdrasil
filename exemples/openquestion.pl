cf794402494b519bb3eb620082b61a3385f6da85
Dominique Revuz <dominique.revuz@gmail.com>
2019-01-18 23:48:04 +0100

    Create openquestion.pl

---
+++ exemples/openquestion.pl
@@ -0,0 +1,19 @@
+ 
+ 
+ # The template default will no be neccessary in the futur 
+ extends=/template/default.pl
+
+title= Une question ouverte 
+
+text== 
+
+Exprimez vos besoins et envies, vos attente de no plateform.
+Nous somme preneur de toute les bonnes idées.
+
+Mais si cette boite au suggetions ne vous convient pas car vous n'aurez pas de réponse directe.
+Vous pouvez utiliser le Q&A dans le menu principal.
+
+==
+
+
+ 
