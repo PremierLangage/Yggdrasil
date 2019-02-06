8cbbbe59077aaa5567b3749382547f15d25c89f4
Dominique Revuz <dominique.revuz@gmail.com>
2019-02-06 22:20:12 +0100

    Update selectwords.pl

--- Language/selectwords.pl
+++ Language/selectwords.pl
@@ -19,11 +19,18 @@ Identifier les adjectifs comparatifs dans le texte ci-dessous.
 form=@ form_selectwords.html
 
 evaluator ==
+feeback=""
 ans = response['answer'].split(",")
+feedback=""
 if set(sol)==set(ans):
-	grade=(100,"Bonne réponse")
+    grade=(100,"Bonne réponse")
 else:
-	grade=(0,"Mauvaise réponse")
+    if len(set(sol)-set(ans))>0 :
+        feedback += "Tu en a oublié !"
+    if len(set(ans)-set(sol))> 0 :
+        feedback += "Tu en a mis en trop !"
+    grade=(0,feedback)
+	
 ==
 
 
