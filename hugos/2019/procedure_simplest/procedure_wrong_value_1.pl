6bdfa1ba9dbb33ccb03fd0435ed92ee14aa934e7
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-02-20 11:20:09 +0100

    syncronisation with info 121

--- hugos/2019/procedure_simplest/procedure_wrong_value_1.pl
+++ hugos/2019/procedure_simplest/procedure_wrong_value_1.pl
@@ -1,4 +1,4 @@
 template=/hugos/template/wims_output.pl
 topicname=procedures simples
-@ /hugos/procedure_simplest_1.cpp [raw_code.cpp]
+@ /hugos/procedure_simplest/procedure_wrong_value_1.cpp [raw_code.cpp]
 code_file=raw_code.cpp
