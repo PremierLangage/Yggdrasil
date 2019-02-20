1fa8423a956e879f5e768ee585bd4208c51ff493
hugo mlodecki <hugo.mlodecki@gmail.com>
2019-02-20 11:46:02 +0100

    syncronisation with info 121

---
+++ hugos/2019/procedure_simplest/procedure_1_input.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+/// BEGIN HIDDEN
+#include "randomization.h"
+CONST I = RANDOM_INT(2, 10);
+/// END HIDDEN
+
+void saisie(int Y) {
+    int Z;
+    cin >> Z;
+    Y = Y + Z;
+    cout << Y;
+}
+
+int main() {
+    int X = I;
+    saisie(X);
+    return 0;
+}
