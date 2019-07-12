c41222c64e55838866be9f0f285823911713c973
BORIS JABOT <bjabot@etud.u-pem.fr>
2019-07-12 16:40:23 +0200

    mise de l'exo loop dans concept

---
+++ ComputerScience/python/concept/loop/dynafor.pl
@@ -0,0 +1,7 @@
+extends=template.pl
+
+difficulty=3
+taboo=while
+needed=for
+
+text= Pour ne pas avoir un warning stupid
---
+++ ComputerScience/python/concept/loop/dynawhile.pl
@@ -0,0 +1,5 @@
+extends=template.pl
+
+difficulty=3
+taboo=for
+needed=while
