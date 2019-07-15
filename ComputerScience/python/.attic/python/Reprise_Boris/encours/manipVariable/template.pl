bb0b240f6aeea6343c389ee03f0fba57f8d5dea3
BORIS JABOT <bjabot@etud.u-pem.fr>
2019-07-15 16:52:40 +0200

    màj manip variable builder

---
+++ ComputerScience/python/.attic/python/Reprise_Boris/encours/manipVariable/template.pl
@@ -0,0 +1,21 @@
+# Fait par Boris Jabot
+
+oneshot=True
+
+extends=/ComputerScience/python/template/suitesoluce.pl
+@ builder.py [builder.py]
+
+soluce==
+==
+
+text==
+==
+
+code==
+==
+
+plsoluce==
+Exo Boucle while |
+==
+
+
