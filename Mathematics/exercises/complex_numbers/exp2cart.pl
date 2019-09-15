949db00e78be3a9eb935361470b6504a67c1dcf5
David DOYEN <David.Doyen@u-pem.fr>
2019-09-15 16:43:49 +0200

    ddoyen Yggdrasil/Mathematics/exercises/complex_numbers/exp2cart.pl

--- Mathematics/exercises/complex_numbers/exp2cart.pl
+++ Mathematics/exercises/complex_numbers/exp2cart.pl
@@ -5,11 +5,9 @@ title = Conversion
 lang = fr
 
 before ==
-with sp.evaluate(False):
-    z=2*exp(sp.I*sp.pi/3)
+with evaluate(False):
+    z=2*exp(I*pi/3)
 latexz=latex(z)
-rp=latex(sp.re(z.expand(complex=True)))
-ri=latex(sp.im(z.expand(complex=True)))
 ==
 
 text = Ecrire sous forme cartésienne le nombre complexe $%\\large z= {{latexz}} %$.
