152e477290dbffd78c323ac91776f3559879c811
David DOYEN <David.Doyen@u-pem.fr>
2019-09-15 21:58:18 +0200

    ddoyen Yggdrasil/Mathematics/exercises/foundations/maps/image.pl

--- Mathematics/exercises/foundations/maps/image.pl
+++ Mathematics/exercises/foundations/maps/image.pl
@@ -9,12 +9,13 @@ n,m=list_randitem_norep(2,symbols("n,m,p,q,r"))
 
 expr=n+m
 n_tex=latex(n)
+m_tex=latex(m)
 expr_tex=latex(expr)
 ==
 
 text ==
 On considère la fonction $$ f : \mathbb{N} \rightarrow \mathbb{N} \times \mathbb{N} $$ telle que
-$$f({{n_tex}},{{m}})=\left( {{expr_tex}} \right)$$
+$$f({{n_tex}},{{m_tex}})=\left( {{expr_tex}} \right)$$
 Calculer $$ f({{n}}) $$
 ==
 
