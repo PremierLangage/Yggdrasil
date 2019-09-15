e99a8b03ee9090c4b04fe382b8778de70355b70d
David DOYEN <David.Doyen@u-pem.fr>
2019-09-15 16:43:29 +0200

    ddoyen Yggdrasil/Mathematics/exercises/complex_numbers/exp2cart.pl

--- Mathematics/exercises/complex_numbers/exp2cart.pl
+++ Mathematics/exercises/complex_numbers/exp2cart.pl
@@ -1,10 +1,8 @@
-extends = /Mathematics/template/mathexpr.pl
+extends = /Mathematics/template/mathinput.pl
 
 title = Conversion
 
 lang = fr
-
-imaginary_unit = i
 
 before ==
 with sp.evaluate(False):
@@ -17,8 +15,8 @@ ri=latex(sp.im(z.expand(complex=True)))
 text = Ecrire sous forme cartésienne le nombre complexe $%\\large z= {{latexz}} %$.
 
 evaluator==
-score,numerror,feedback=ans_complex_cartesian_coeff_rad(answer['1'],"i",z.expand(complex=True))
-feedback=z.expand(complex=True)
+score,_,feedback=ans_complex(input1.value,z,form="cartesian")
 ==
+
 
 
