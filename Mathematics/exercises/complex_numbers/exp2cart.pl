df6a617fccc5da9568131826b37aa1e849d3f42e
David DOYEN <David.Doyen@u-pem.fr>
2019-09-15 16:50:58 +0200

    ddoyen Yggdrasil/Mathematics/exercises/complex_numbers/exp2cart.pl

--- Mathematics/exercises/complex_numbers/exp2cart.pl
+++ Mathematics/exercises/complex_numbers/exp2cart.pl
@@ -7,13 +7,14 @@ lang = fr
 before ==
 keyboards_JSON['virtualKeyboards']="complexes"
 input1.config = keyboards_JSON
-
 with evaluate(False):
     z=exp(I*pi/4)
+sol=expand_complex(z)
+
 latexz=latex(z)
 ==
 
-text = Ecrire sous forme cartésienne le nombre complexe $%\\large z= {{latexz}} %$.
+text = {{sol}} Ecrire sous forme cartésienne le nombre complexe $%\\large z= {{latexz}} %$.
 
 evaluator==
 score,_,feedback=ans_complex(input1.value,z,form="cartesian")
