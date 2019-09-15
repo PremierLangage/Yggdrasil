b56c085b9996f6b277dcca87beb284da09430f06
David DOYEN <David.Doyen@u-pem.fr>
2019-09-15 21:56:57 +0200

    ddoyen Yggdrasil/Mathematics/exercises/foundations/maps/image.pl

--- Mathematics/exercises/foundations/maps/image.pl
+++ Mathematics/exercises/foundations/maps/image.pl
@@ -7,12 +7,14 @@ lang = fr
 before ==
 n,m=list_randitem_norep(2,symbols("n,m,p,q,r"))
 
-expr=a*randitem([n,(n)^2)+b*m])
+expr=n+m
+
+expr_tex=latex(expr)
 ==
 
 text ==
 On considère la fonction $$ f : \mathbb{N} \rightarrow \mathbb{N} \times \mathbb{N} $$ telle que
-$$f(n)=\left(\frac{n^2}{2},2n+1\right)$$
+$$f({{n}},{{m}})=\left( {{expr_tex}} \right)$$
 Calculer $$ f({{n}}) $$
 ==
 
