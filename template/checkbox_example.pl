7689fea436a73dfa7d73513e9a6140b0ddfadc17
David DOYEN <David.Doyen@u-pem.fr>
2019-09-16 13:57:50 +0200

    ddoyen Yggdrasil/template/checkbox_example.pl

--- template/checkbox_example.pl
+++ template/checkbox_example.pl
@@ -25,7 +25,7 @@ nchoices % 5
 nright % [2,3]
 
 text ==
-Sélectionner les pays européens dans la liste suivante.
+Sélectionner les noms de variables valides en Python dans la liste suivante.
 ==
 
 
