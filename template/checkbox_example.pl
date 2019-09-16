198cda7bdde3f0822f32b01dde0d8838c3089192
David DOYEN <David.Doyen@u-pem.fr>
2019-09-16 13:56:28 +0200

    ddoyen Yggdrasil/template/checkbox_example.pl

---
+++ template/checkbox_example.pl
@@ -0,0 +1,31 @@
+extends = checkbox.pl
+
+right %=
+["au revoir",
+"abc",
+"bonjour",
+"oui",
+"Ciao",
+"NON",
+"byeBye7",
+"_upem_",
+"good_morning"]
+==
+
+wrong %=
+["au revoir",
+"Hi!",
+"def",
+"6hello6",
+"good-afternoon"]
+==
+
+nchoices % 5
+
+nright % [2,3]
+
+text ==
+Sélectionner les pays européens dans la liste suivante.
+==
+
+
