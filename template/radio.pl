10fd35edbb8fa2e3cee042baca812ae65dd4d6e2
David DOYEN <David.Doyen@u-pem.fr>
2019-09-16 23:37:41 +0200

    ddoyen Yggdrasil/template/radio.pl

---
+++ template/radio.pl
@@ -0,0 +1,40 @@
+extends = basic.pl
+
+title = Radio
+
+checkbox =: RadioGroup
+
+before ==
+import random as rd
+nright0=rd.choice(nright)
+nwrong0=nchoices-nright0
+choices = rd.sample(right,nright0)+rd.sample(wrong,nwrong0)
+rd.shuffle(choices)
+
+checkbox.items=([{"id":str(id),"content":content} for id,content in enumerate(choices)])
+==
+
+text ==
+Sélectionner les villes européennes dans la liste suivante.
+==
+
+form ==
+{{ checkbox | component }}
+==
+
+evaluator ==
+n=len(checkbox.items)
+ansright,answrong=0,0
+for i in range(n):
+    if checkbox.items[i]['content'] in right and checkbox.items[i]['checked']:
+        checkbox.items[i]['css'] = 'success-state anim-fade'
+        ansright+=1
+    elif checkbox.items[i]['content'] in right and not checkbox.items[i]['checked']:
+        checkbox.items[i]['css'] = 'success-state anim-fade'
+        answrong+=1
+    elif not (checkbox.items[i]['content'] in right) and checkbox.items[i]['checked']:
+        answrong+=1
+
+score=max([int((ansright-answrong)/ansright*100),0])
+==
+
