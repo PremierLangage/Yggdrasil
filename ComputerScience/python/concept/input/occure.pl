29f09dc67ac81c4dca4f6eeb672a22d257020ed4
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-16 16:09:02 +0200

    mzipstein Yggdrasil/ComputerScience/python/concept/input/occure.pl

--- ComputerScience/python/concept/input/occure.pl
+++ ComputerScience/python/concept/input/occure.pl
@@ -38,12 +38,11 @@ et affiche s'il appartienr ou non à la suite {{MOT}}
 before==
 
 import random 
-lst=("aeiou","treza","abcxyz","oreille")
 MOT= random.choice(lst)
 
 import jinja2
 soluce= jinja2.Template(soluce).render(globals())
-code=  jinja2.Template(soluce).render(globals())
+code=  jinja2.Template(code).render(globals())
 ==
 
 
