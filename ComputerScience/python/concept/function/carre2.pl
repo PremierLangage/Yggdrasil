9a612874b2347ce02937ecac6ef3019e60fb4b62
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-16 20:16:57 +0200

    drevuz Yggdrasil/ComputerScience/python/concept/function/carre2.pl

---
+++ ComputerScience/python/concept/function/carre2.pl
@@ -0,0 +1,30 @@
+
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+author=Dominique Revuz 
+title=L'éponge Carrée
+
+tag=function # N'oubliez pas de remplir ce champs svp
+extends=/ComputerScience/python/template/pltest.pl
+piste=verte
+text==
+## Une fonction carre ##
+Ecrivez une fonction **carre** qui retourne le carré de son paramêtre.
+==
+
+pltest==
+>>> carre(510) == 260100 # le carre de 510
+True
+>>> carre(0)
+0
+>>> carre(10)
+100
+>>> 
+==
+editor.code==
+def carre(n):
+    return n*6
+==
+editor.height=145
+
+
+
