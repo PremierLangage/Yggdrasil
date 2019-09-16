7ba4fc5c97d9280746caf19513ac018f25d06772
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-16 15:24:17 +0200

    mzipstein Yggdrasil/ComputerScience/python/concept/input/occure.pl

--- ComputerScience/python/concept/input/occure.pl
+++ ComputerScience/python/concept/input/occure.pl
@@ -11,29 +11,24 @@ extends=/ComputerScience/python/template/soluce.pl
 @ /builder/before.py [builder.py]
 
 soluce==
-a=string(input())
+a=input()
 if a in {{MOT}}:
-    print(a,"ans {{X}} ans il aura %d ans." % (a,a+{{x}}))
-print("Le perroquet du capitaine a %d ans." % (((a+{{y}})//{{z}})-{{y}}))
+    print(a,"apparait dans {{MOT}} .")
+else:
+  print(a,"n'apparait pas dans {{MOT}} .")
+
 ==
 
 code==
-# lecture au clavier et transformation en entier puis affectation
-age=int(input())
-# Le capitaine a __ ans. Dans {{X}} ans il aura __ ans.
-# Le perroquet du capitaine à __ ans.
+# lecture au clavier, affectation
+lettre=input()
+
 ==
 
 
 
 text==
-Ecrivez un programme utilisant une unique variable **age** qui lit cette information au clavier puis affiche :
-
-	Le capitaine a __ ans. Dans {{X}} ans il aura ___ ans.
-
-puis sachant que dans {{Y}} ans, le perroquet aura {{Z}} de l’âge du capitaine (arrondi à l'année inférieur) quel est l'age actuel du perroquet, le programme affiche:
-
-	Le perroquet du capitaine a __ ans.
+Ecrivez un programme utilisant une unique variable **lettre** qui lit un seul caractère au clavier 
 
 
 ==
@@ -41,10 +36,8 @@ puis sachant que dans {{Y}} ans, le perroquet aura {{Z}} de l’âge du capitain
 before==
 
 import random 
-X,x= random.choice([("deux",2),("trois",3),("quatre",4) ,("cinq",5)])
-Y,y= random.choice([("deux",2),("trois",3),("quatre",4),("cinq",5)])
-Z,z= random.choice([("la moitier",2),("le tiers",3),("le quart",4)])
-
+lst=("aeiou","treza","abcxyz","oreille")
+MOT= random.choice(lst)
 
 import jinja2
 soluce= jinja2.Template(soluce).render(globals())
@@ -53,10 +46,11 @@ code=  jinja2.Template(code).render(globals())
 
 
 plsoluce==
-Test 1 |33
-Test 2 |66
-Test 4 |100000
+Test 1 |a
+Test 2 |i
+Test 3 |m
 ==
+
 
 
 
