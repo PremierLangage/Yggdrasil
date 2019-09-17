783bd0abe3680e39580d64af4a3a8fce38701f26
David DOYEN <David.Doyen@u-pem.fr>
2019-09-17 11:37:57 +0200

    ddoyen Yggdrasil/template/checkbox.pl

--- template/checkbox.pl
+++ template/checkbox.pl
@@ -25,14 +25,14 @@ form ==
 evaluator ==
 n=len(checkbox.items)
 ansright,answrong=0,0
-for i in range(n):
-    if checkbox.items[i]['content'] in right and checkbox.items[i]['checked']:
-        checkbox.items[i]['css'] = 'success-state anim-fade'
+for item in checkbox.items:
+    if item['content'] in right and item['checked']:
+        item['css'] = 'success-state anim-fade'
         ansright+=1
-    elif checkbox.items[i]['content'] in right and not checkbox.items[i]['checked']:
-        checkbox.items[i]['css'] = 'success-state anim-fade'
+    elif item['content'] in right and not item['checked']:
+        item['css'] = 'success-state anim-fade'
         answrong+=1
-    elif not (checkbox.items[i]['content'] in right) and checkbox.items[i]['checked']:
+    elif not (item['content'] in right) and item['checked']:
         answrong+=1
 
 score=max([int((ansright-answrong)/ansright*100),0])
