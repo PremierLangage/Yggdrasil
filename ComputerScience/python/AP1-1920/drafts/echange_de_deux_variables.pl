d96d9063f5056fc0f150658fbef8c5be696297b5
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2019-09-17 16:20:56 +0200

    ccalle Yggdrasil/ComputerScience/python/AP1-1920/drafts/echange_de_deux_variables.pl

---
+++ ComputerScience/python/AP1-1920/drafts/echange_de_deux_variables.pl
@@ -0,0 +1,8 @@
+author=IGM
+title=Echange de deux variables
+text==
+On suppose qu'il existe deux variables `a` et `b` de valeurs quelconques précédemment fixées.
+Ecrire un programme dont l'exécution échange les valeurs de `a` et `b`.
+
+Attention, votre programe doit fonctionner quelles que soient les valeurs initiales de `a` et `b`.
+==
