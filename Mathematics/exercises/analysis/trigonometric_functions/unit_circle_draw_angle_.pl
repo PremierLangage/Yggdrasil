34b715921e7685d0b8145f31769f73e93253c437
David DOYEN <David.Doyen@u-pem.fr>
2019-09-17 19:17:13 +0200

    ddoyen Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl
@@ -10,25 +10,16 @@ angle=randitem(lstangle)
 valangle=float(angle.evalf())
 ==
 
-extrajs==
-<script>
-    /**
-    * This function is called by the component once the page is loaded
-    * board argument is an instance of JSX Graph board.
-    **/
-    function onMathDrawerReady(component) {
-        const board = component.board;
-        board.setBoundingBox([-1.25,1.25,1.25,-1.25]);
-        var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
-        var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
-        var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
-        var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-        var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
-        var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
-        var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'red',fixed:false});
-        var secOAM = board.create('sector',[O,A,M],{color:'orange'});
-    }
-</script>
+drawer.script ==
+board.setBoundingBox([-1.25,1.25,1.25,-1.25]);
+var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
+var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
+var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
+var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
+var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
+var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
+var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'red',fixed:false});
+var secOAM = board.create('sector',[O,A,M],{color:'orange'});
 ==
 
 form==
@@ -48,5 +39,6 @@ else:
      score=0
 feedback=""
 ==
+
 
 
