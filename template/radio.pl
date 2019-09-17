a92851897252ecbe2c548a39368d79f8d8f05d2d
David DOYEN <David.Doyen@u-pem.fr>
2019-09-17 11:15:06 +0200

    ddoyen Yggdrasil/template/radio.pl

--- template/radio.pl
+++ template/radio.pl
@@ -7,7 +7,7 @@ radio =: RadioGroup
 before ==
 import random as rd
 choices=rd.sample(list(range(50)),5)
-sol=min(choices)
+sol=str(min(choices))
 radio.items=([{"id":str(id),"content":str(content)} for id,content in enumerate(choices)])
 ==
 
@@ -25,10 +25,14 @@ n=len(radio.items)
 
 
 evaluator ==
-if radio.items[int(radio.selection)]['content']==str(sol):
-    score=100
-else:
-    score=0
+n=len(radio.items)
+score=0
+for i in range(n):
+    if radio.items[i]['content']==sol:
+        radio.items[i]['css'] = 'success-state anim-fade'
+        if radio.items[i]['id']==radio.selection:
+            score=100
+            break
 feedback=""
 ==
 
