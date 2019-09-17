3c9534b36ecc563594d702c24971e83fd0a24774
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-17 12:17:15 +0200

    mzipstein Yggdrasil/ComputerScience/python/template/soluceevaluator.py

--- ComputerScience/python/template/soluceevaluator.py
+++ ComputerScience/python/template/soluceevaluator.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
                 mpvalue=dic['name'].split("\n")
             else:
                 continue
-            letest.append(mpvalue[0],"\n".join(mpvalue[1:]))
+            lestest.append(mpvalue[0],"\n".join(mpvalue[1:]))
     student = get_answers()['answer']
     with open("student.py","w") as sf:
         sf.write(student)
