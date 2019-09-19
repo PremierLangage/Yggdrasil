584851af94021b03eec0310d1c3451a8944890cb
Frédéric LE QUERE <Frederic.Lequere@u-pem.fr>
2019-09-19 15:41:35 +0200

    fle quere Yggdrasil/ComputerScience/python/AP1-1920/drafts/FredLQ/test.pl

---
+++ ComputerScience/python/AP1-1920/drafts/FredLQ/test.pl
@@ -0,0 +1,19 @@
+
+author= Fred
+
+# comment
+extends=../../templates/pltest.pl
+
+title = premier exo
+
+text == 
+variable c contient reste de la division de 7 par 3 ?
+
+==
+
+pltest==
+>>> c
+1
+>>>
+
+==
