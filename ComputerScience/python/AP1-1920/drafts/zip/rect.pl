5913afc7161352274949e340546647c559884204
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-19 16:27:57 +0200

    mzipstein Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/rect.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/rect.pl
@@ -0,0 +1,25 @@
+
+
+
+author zip
+
+# Heritage d'un type d'exercice 
+extends=../../templates/pyeditor.pl
+
+
+before==
+
+lg=3
+larg=5
+
+==
+
+text== les dimensions d'un rectangle sont mémorisées dans des variables lg et larg.
+ecrire un programme qui calcule et affiche le périmètre et la surface du rectangle
+
+
+evaluator==
+
+
+
+
