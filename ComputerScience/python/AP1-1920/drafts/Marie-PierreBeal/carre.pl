3bf9c2a367171d0fe4f817392a22d89959d3664c
Marie-Pierre BEAL <Marie-Pierre.Beal@u-pem.fr>
2019-09-19 15:41:47 +0200

    mbeal Yggdrasil/ComputerScience/python/AP1-1920/drafts/Marie-PierreBeal/carre.pl

---
+++ ComputerScience/python/AP1-1920/drafts/Marie-PierreBeal/carre.pl
@@ -0,0 +1,17 @@
+author = Marie-Pierre Béal
+
+extends = ../../templates/pltest
+
+tile = carré
+
+text == 
+Ecrire une fonction f qui calcule le carré d'un entier 
+==
+
+pltest==
+>>>f(2)==4
+True
+>>>f(0)==0
+True
+>>>f(3)==9
+True==
