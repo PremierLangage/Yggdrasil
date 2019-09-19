cae25d5e027b4f11e3892c7d17c7a536bfb016e2
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-19 18:20:37 +0200

    mzipstein Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/corr_3.pl

--- ComputerScience/python/AP1-1920/drafts/zip/corr_3.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/corr_3.pl
@@ -33,7 +33,8 @@ Or python ne sait pas additionner des textes et des nombres.
 (La chaine 'la somme de 15975 et 43242 est ' est de type str alors que 15975+43242 est de type int)
 Le message d'erreur vous dit qu'il ne peut transformer le nombre 'int' (integer) en texte 'str' string (chaine de caractères), **implicitement**.
 Implicitement c'est que vous ne l'avez pas demandé explicitement.
-Pour le demander explictement il faut écrire : 
+Pour le demander explictement il faut transformer 15975+43242 en str et
+écrire : 
 print("la somme de 15975 et 43242 est "+ str(15975+43242))
 ou alors demander à print de faire le travail en séparant les deux éléments à afficher
 print("la somme de 15975 et 43242 est ", 15975+43242)
