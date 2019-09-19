e6e552c39f74c8a10aee69b3aadedf26eb1dd334
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-19 16:05:31 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/initialisation.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/initialisation.pl
@@ -0,0 +1,30 @@
+
+# OCE : test 11/09/2019 OK
+extends=/ComputerScience/python/template/pltest.pl
+#@ /builder/before.py [builder.py]
+
+tag=Programmation|Python|Initialisation|Variable
+author = Christophe Callé
+
+title = Initialisation de variable en Python
+
+text=
+before== #|python| 
+import random 
+random.seed(seed)
+type_var = random.choice(["int","str","float","bool"])
+var = random.choice(["a", "b", "c", "X", "Y"])
+
+pltest="""
+>>> type({})=={}
+True
+""".format(var, type_var)
+text="""
+Initialisez la variable **{var}** avec une valeur quelconque de type **{type_var}** 
+""".format(var,type_var)
+
+
+
+==
+
+
