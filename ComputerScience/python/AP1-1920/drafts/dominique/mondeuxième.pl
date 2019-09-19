98debe75a28519532d2cef57342a15ee65de50bd
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-19 15:27:42 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/mondeuxième.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/mondeuxième.pl
@@ -0,0 +1,28 @@
+
+
+
+author= Dominique's 
+
+# Heritage d'un type d'exercice 
+extends=../../templates/pltest.pl
+
+
+title = Mon premier       
+
+text==
+
+ennoncé
+
+ecrivez une fonction f 
+qui retourne 3 
+
+==
+
+pltest==
+>>> f() == 3
+True
+>>> f()
+3
+==
+
+
