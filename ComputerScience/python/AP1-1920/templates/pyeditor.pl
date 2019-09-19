815ddd8cbdcfe392a24cf63f68def92a001311ac
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-19 15:14:55 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/templates/pyeditor.pl

---
+++ ComputerScience/python/AP1-1920/templates/pyeditor.pl
@@ -0,0 +1,54 @@
+
+doc==
+ceci est un template pour écrire des exercices de python.
+
+Il ne contient pas beaucoup de fonctions d'aides pour gérer les réponses faites par l'etudiant.
+
+Vous pouvez peut être si vous ne l'avez pas fait jeter un oeil sur les templates suivants:
+
+dans le repertoire 
+/ComputerScience/python/template/
+les templates 
+
+pltest
+qcm_template
+
+IL est necessaire d'écrire de nouveau templates avec les composants .
+
+
+
+==
+
+
+component =: CodeEditor
+
+component.code ==
+import os
+==
+#component.theme = white
+component.language = python
+
+
+form==
+{{component|component }}
+==
+
+evaluator==
+
+
+
+grade = (100,str( component.code))
+==
+
+title= TEMPLATE modifiez le titre TEMPLATE
+text=
+
+before==
+text=doc
+==
+
+@ /utils/sandboxio.py 
+@ /grader/evaluator.py [grader.py]
+@ /builder/before.py [builder.py]
+
+
