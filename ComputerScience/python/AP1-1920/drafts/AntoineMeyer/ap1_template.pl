3b0dea754fe4f4241f9d1340a0a337049d4a652a
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-20 22:30:24 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl
@@ -5,18 +5,21 @@
 title = <pas de titre défini>
 text = <pas d'énoncé défini>
 
+# création de l'éditeur de code
 editor =: CodeEditor
 editor.language = python
 editor.cid = py_editor_0
 editor.code ==
 # Saisissez votre code ici, et cliquez sur le bouton
-# de validation quand vous avez terminé !
+# de validation quand vous avez terminé
 ==
 
+# le formulaire de l'exercice contient uniquement l'éditeur
 form==
 {{editor|component}}
 ==
 
+# définition de la procédure de validation
 grader==
-# this is the validation code
+return (0, "<l'exercice n'a pas défini de procédure de validation>")
 ==
