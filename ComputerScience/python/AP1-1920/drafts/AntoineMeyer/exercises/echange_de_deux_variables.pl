9216444444dfeaa110f0f57a1819f6ec188626ea
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-21 00:06:27 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/echange_de_deux_variables.pl

---
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/echange_de_deux_variables.pl
@@ -0,0 +1,22 @@
+# Ne fonctionne pas !
+
+extends @../templates/ap1_template.pl
+title = Échange de deux variables
+
+text==
+On suppose qu'il existe deux variables `a` et `b` de valeurs et de types 
+quelconques précédemment fixées (vous n'avez donc pas à les initialiser
+ni à les lire).
+
+Écrire un programme dont l'exécution échange les valeurs de `a` et `b`, 
+sans rien afficher.
+
+**Attention**, *votre programe doit fonctionner quelles que soient les valeurs 
+de `a` et `b` et leurs types (qui peuvent être différents).*
+==
+
+grader==
+set_globals(a=1, b=2)
+run()
+assert_variable_values(a=2, b=1)
+==
