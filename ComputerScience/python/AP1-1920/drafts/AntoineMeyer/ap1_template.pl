301ed8945aae694eaccec7d98ed3c441247ae5aa
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-20 20:21:39 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl
@@ -1,12 +1,13 @@
-@/utils/sandboxio.py
-@test_grader.py [grader.py]
+@ /utils/sandboxio.py
+@ ap1_builder.py [builder.py]
+@ test_grader.py [grader.py]
 
 title = <pas de titre défini>
 text = <pas d'énoncé défini>
 
 editor =: CodeEditor
 editor.language = python
-editor.id = py_editor_0
+editor.cid = py_editor_0
 editor.code ==
 # Saisissez votre code ici, et cliquez sur le bouton
 # de validation quand vous avez terminé !
