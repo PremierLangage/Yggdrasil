11632e19714e47e169140ffc4fc09872f1811905
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-20 19:44:43 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/test_grader.py

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/test_grader.py
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/test_grader.py
@@ -3,6 +3,7 @@ import sandboxio
 if __name__ == "__main__":
     answers = sandboxio.get_answers()
     context = sandboxio.get_context()
-    print(answers, context)
-    sandboxio.output(0, "this is only a test")
+    sandboxio.output(0, 
+        "Answers:\n" + str(answers)
+        + "\n\nContext:\n" + str(context))
 
