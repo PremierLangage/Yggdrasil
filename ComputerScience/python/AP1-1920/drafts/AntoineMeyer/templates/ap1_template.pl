03d93df146a9a08e2be8cddf286ec342f8728e9b
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-20 23:46:14 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/ap1_template.pl

---
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/ap1_template.pl
@@ -0,0 +1,26 @@
+@ /utils/sandboxio.py
+@ builders/ap1_builder.py [builder.py]
+@ graders/ap1_grader.py [grader.py]
+
+title = <pas de titre défini>
+text = <pas d'énoncé défini>
+
+# création de l'éditeur de code
+editor =: CodeEditor
+editor.language = python
+editor.cid = py_editor_0
+editor.code ==
+# Saisissez votre code ici, et cliquez sur le bouton
+# de validation quand vous avez terminé
+==
+
+# le formulaire de l'exercice contient uniquement l'éditeur
+form==
+{{editor|component}}
+==
+
+# définition de la procédure de validation
+grader==
+return (0, "<l'exercice n'a pas défini de procédure de validation>")
+==
+
