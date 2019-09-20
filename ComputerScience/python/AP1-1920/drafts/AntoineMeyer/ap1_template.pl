8919b2635dc6134d36fe37b3ce210970f775439a
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-20 19:41:44 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_template.pl
@@ -2,17 +2,15 @@
 @test_grader.py [grader.py]
 
 title = <pas de titre défini>
-
 text = <pas d'énoncé défini>
 
 editor =: CodeEditor
-
+editor.language = python
+editor.id = py_editor_0
 editor.code ==
 # Saisissez votre code ici, et cliquez sur le bouton
 # de validation quand vous avez terminé !
 ==
-
-editor.language = python
 
 form==
 {{editor|component}}
