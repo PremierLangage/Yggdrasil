927a3506915281c5e82117543c0768d093e65d4d
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-20 10:56:02 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/echange_de_deux_variables.pl

--- ComputerScience/python/AP1-1920/drafts/echange_de_deux_variables.pl
+++ ComputerScience/python/AP1-1920/drafts/echange_de_deux_variables.pl
@@ -1,15 +1,15 @@
-author=IGM
-
-# TODO
-@ [root]/graders/ap1_soluce.py  [grader.py]
-
-title=Echange de deux variables
+grader =@ Yggdrasil/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+title = Échange de deux variables
 
 text==
-On suppose qu'il existe deux variables `a` et `b` de valeurs quelconques précédemment fixées.
-Ecrire un programme dont l'exécution échange les valeurs de `a` et `b`.
+On suppose qu'il existe deux variables `a` et `b` de valeurs et de types 
+quelconques précédemment fixées (vous n'avez donc pas à les initialiser
+ni à les lire).
+Écrire un programme dont l'exécution échange les valeurs de `a` et `b`, 
+sans rien afficher.
 
-Attention, votre programe doit fonctionner quelles que soient les valeurs initiales de `a` et `b`.
+**Attention**, votre programe doit fonctionner quelles que soient les valeurs 
+initiales de `a` et `b` et leurs types.
 ==
 
 code=:codeEditor
@@ -17,4 +17,5 @@ code=:codeEditor
 form==
 {{ code|component }}
 ==
+
 
