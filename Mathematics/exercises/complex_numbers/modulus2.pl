6605906ae9acd43b015537d18b4938c1e41b2b8a
David DOYEN <David.Doyen@u-pem.fr>
2019-09-20 19:46:25 +0200

    ddoyen Yggdrasil/Mathematics/exercises/complex_numbers/modulus2.pl

--- Mathematics/exercises/complex_numbers/modulus2.pl
+++ Mathematics/exercises/complex_numbers/modulus2.pl
@@ -17,14 +17,14 @@ z1=a+b*I
 z2=c+d*I
 cas=randint(1,3)
 if cas==1:
-    z=(z1)*(z2)
-    z_tex=latex(z)
+    z=z1*z2
+    z_tex=r"( %s )( %s )" % (latex(z1),latex(z2))
 elif cas==2:
-    z=(z1)/(z2)
-    z_tex=latex(z)
+    z=z1/z2
+    z_tex=r"\frac{ %s }{ %s }" % (latex(z1),latex(z2))
 elif cas==3:
-    z=1/((z1)*(z2))
-    z_tex=latex(z)
+    z=1/(z1*z2)
+    z_tex=r"\frac{ 1 }{ (%s)( %s) }" % (latex(z1),latex(z2))
 sol=abs(z)
 sol_tex=latex(sol)
 ==
