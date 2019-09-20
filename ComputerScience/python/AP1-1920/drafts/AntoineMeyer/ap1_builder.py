393e31216f7ef3b1f2f1d06121b54fa0df5fabe4
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-20 22:19:17 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_builder.py

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_builder.py
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/ap1_builder.py
@@ -2,25 +2,17 @@
 # coding: utf-8
 
 import sys, jsonpickle
-import sandboxio as sandboxio
+import utils.sandboxio as sandboxio
 
 
 class BuilderError(Exception):
     pass
 
 
-def add_try_clause(code, excpt):
-    """Add a try/except clause, excepting 'excpt' around code."""
-    code = code.replace('\t', '    ')
-    return ("try:\n    ...\n" + '\n'.join(
-        ["    " + line for line in code.split('\n')])
-            + "\nexcept " + excpt.__name__ + ":\n    pass")
-
-
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         msg = ("Sandbox did not call builder properly:\n"
-               + "Usage: python3 builder.py [input_json] [output_json]")
+               "Usage: python3 builder.py [input_json] [output_json]")
         print(msg, file=sys.stderr)
         sys.exit(1)
 
