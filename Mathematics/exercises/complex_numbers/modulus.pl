39c53beda0e07cb2e03e40efc312697ef1ecb827
David DOYEN <David.Doyen@u-pem.fr>
2019-09-20 18:44:24 +0200

    ddoyen Yggdrasil/Mathematics/exercises/complex_numbers/modulus.pl

--- Mathematics/exercises/complex_numbers/modulus.pl
+++ Mathematics/exercises/complex_numbers/modulus.pl
@@ -6,7 +6,7 @@ lang = fr
 
 before ==
 keyboards_JSON['virtualKeyboards']="elementary"
-input1.config = keyboards_JSON
+input.config = keyboards_JSON
 
 z=rand_complex_int(5)
 z_tex=latex(z)
@@ -17,11 +17,12 @@ sol_tex=latex(sol)
 text = Déterminer le module du nombre complexe $% z= {{z_tex}} %$.
 
 evaluator==
-score,_,feedback=ans_expr(input1.value,sol)
+score,_,feedback=ans_expr(input.value,sol)
 ==
 
 solution==
 Le module de $! z !$ est $% \displaystyle {{sol_tex}} %$.
 ==
+
 
 
