532e2d38e893ae25dbe75057b5830efab8b37227
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-21 01:10:14 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/tri_trois_valeurs.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/tri_trois_valeurs.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/tri_trois_valeurs.pl
@@ -13,15 +13,12 @@ suivies d'un retour à la ligne.
 **Par exemple,** si on suppose qu'au début du programme `a` vaut `5`, `b` vaut `1` 
 et `c` vaut 3, le programme devra afficher *précisément* la chaîne `'1 3 5\n'`.
 
-
-
 **Attention :**
 
 - Le programe doit fonctionner quelles que soient les valeurs 
   de `a`, `b` et `c` du moment qu'elles sont comparables ;
 - Le programme ne doit pas modifier les valeurs des variables `a`, `b` et `c`, 
   ni créer de nouvelles variables.
-<br><br>
 ==
 
 grader==
