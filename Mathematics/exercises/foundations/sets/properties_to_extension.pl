cad62cec873863b0f9d73fa6e7002d4eb9d96dd2
David DOYEN <David.Doyen@u-pem.fr>
2019-09-21 21:50:02 +0200

    ddoyen Yggdrasil/Mathematics/exercises/foundations/sets/properties_to_extension.pl

--- Mathematics/exercises/foundations/sets/properties_to_extension.pl
+++ Mathematics/exercises/foundations/sets/properties_to_extension.pl
@@ -13,7 +13,7 @@ d=randint(5,7)
 f = lambda x : x < 5
 cond=r"n \leq %d" % d 
 S=list(range(d))
-B=FiniteSet(*[x for x in S])
+B=FiniteSet(*[x for x in S if x <5])
 
 A=randitem([r"\mathbb{N}",r"\mathbb{Z}"])
 
