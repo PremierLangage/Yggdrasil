60d6ad62c1431e5252b12ef685dcc46beb442a56
David DOYEN <David.Doyen@u-pem.fr>
2019-09-22 14:30:50 +0200

    ddoyen Yggdrasil/Mathematics/exercises/foundations/sets/comprehension_to_extension.pl

--- Mathematics/exercises/foundations/sets/comprehension_to_extension.pl
+++ Mathematics/exercises/foundations/sets/comprehension_to_extension.pl
@@ -16,7 +16,7 @@ if S==r"\mathbb{N}":
 elif S==r"\mathbb{Z}":
     lstS=S=list(range(-20,20))
 
-if cond="square":
+if cond=="square":
     d=randint(5,7)
     lstA=[x for x in lstS if x**2<d]
     cond=r"n^2 \leq %d" % d 
