9f56d0117e9b06f88d1f5a7290d60d5982b5bb52
David DOYEN <David.Doyen@u-pem.fr>
2019-09-24 23:22:04 +0200

    ddoyen Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -51,7 +51,7 @@ var parameters = {
 }
 
 parameters.appletOnLoad = function() {
-ggbApplet.evalCommand('A=(0,2)\nB=(-1,1)\nC=(-1,-1)');	
+ggbApplet.evalCommand('A=(2,0)\nB=(-1,1)\nC=(-1,-1)');	
 }
 
 var applet = new GGBApplet('5.0', parameters);
@@ -59,7 +59,7 @@ var applet = new GGBApplet('5.0', parameters);
 applet.inject('applet_container','preferHTML5');
 
 function ggbEval() {
-    ggbApplet.evalCommand('t=IsDefined[D]');	
+    ggbApplet.evalCommand('t=IsDefined(Point(0,0))');	
 
 }
 
