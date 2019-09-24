054f25ac44d043b8c26bfa741404863b290b7a7b
David DOYEN <David.Doyen@u-pem.fr>
2019-09-24 21:45:07 +0200

    ddoyen Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -60,8 +60,8 @@ function ggbValues() {
     var values = [];
     for (var i = 1; i < n+1; i++){
         var id = ggbApplet.getObjectName(i);
-        \\var value = JSON.stringify(ggbApplet.getValue(id));
-        var value = ggbApplet.getXcoord(id);
+        var value = JSON.stringify(ggbApplet.getValue(id));
+        var value = JSON.stringify(ggbApplet.getXcoord(id));
         values.push({id: 'ggb_' + id, value: value});
     }
     var myDiv = document.getElementById("answer");
