40a2c4383a6feea6a21c237e4faaaaa54e8109e0
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-24 18:00:33 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/tri_trois_valeurs.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/tri_trois_valeurs.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/tri_trois_valeurs.pl
@@ -38,26 +38,42 @@ et `c` vaut 3, le programme devra afficher *précisément* la chaîne `'1 3 5\n'
   ni créer de nouvelles variables.
 ==
 
-grader==
+grader== #|python|
 from itertools import permutations
 
+
+# Ouvre un "groupe de tests" de titre donné
 begin_test_group("Tris d'éléments distincts")
+
+# Dans cet exemple on veut faire un test pour chaque permutation de (1, 2, 3)
+# On utilise ici un style "verbeux" avec plusieurs appels
 for x, y, z in permutations((1, 2, 3)):
-    # TODO: display global change in feedback
+
+    # Donne un titre à la prochaine exécution
+    set_title("Exécution avec a = {!r}, b = {!r}, c = {!r}")
+    # Fixe les variables globales disponibles pour les prochaines exécution
     set_globals(a=x, b=y, c=z)
-    run(title="Exécution avec a = {!r}, b = {!r}, c = {!r}".format(x, y, z))
+    # Lance l'exécution du programme dans les conditions spécifiées
+    run()
+    # Vérifie la sortie standard obtenue
     assert_output("1 2 3\n")
+    # Vérifie que les variables globales n'ont pas changé
     assert_no_global_change()
+
+# Clôt le précédent groupe de tests
 end_test_group()
 
-begin_test_group("Tris avec un doublon et un plus petit")
+# Second groupe de tests avec le plus grand élément doublé
+# On utilise ici un style plus compact avec un seul appel
+begin_test_group("Tris de chaînes avec un doublon et un plus petit")
 for x, y, z in set(permutations(("un", "un", "deux"))):
-    set_globals(a=x, b=y, c=z)
-    run(title="Exécution avec a = {!r}, b = {!r}, c = {!r}".format(x, y, z))
-    assert_output("deux un un\n")
-    assert_no_global_change()
+    glob = {'a': x, 'b': y, 'c': z}
+    title = "Exécution avec a = {!r}, b = {!r}, c = {!r}".format(x, y, z)
+    run(title=title, glob=glob, output="deux un un\n", 
+        allow_global_change=False)
 end_test_group()
 
+# Troisième groupe de tests avec le plus petit élément doublé
 begin_test_group("Tris avec un doublon et un plus grand")
 for x, y, z in set(permutations((1, 1, 2))):
     set_globals(a=x, b=y, c=z)
@@ -66,6 +82,7 @@ for x, y, z in set(permutations((1, 1, 2))):
     assert_no_global_change()
 end_test_group()
 
+# Quatrième groupe de tests avec trois éléments identiques
 begin_test_group("Tri de trois valeurs identiques")
 set_globals(a=1, b=1, c=1)
 run(title="Exécution avec a = {!r}, b = {!r}, c = {!r}".format(x, y, z))
