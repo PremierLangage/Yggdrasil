76a23cb22bfe7522e6c8965540673448934ce0cf
David DOYEN <David.Doyen@u-pem.fr>
2019-09-24 16:57:12 +0200

    ddoyen Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -17,9 +17,11 @@ form==
 <div id="applet_container"></div>
 <form>
         <input value="Set point A invisible" onclick="ggbApplet.setVisible('A', false)" type="button">
-        <input value="Set point A visible" onclick="ggbApplet.setVisible('A', true)" type="button">
-        
+        <input value="Set point A visible" onclick="ggbApplet.setVisible('A', true)" type="button">       
 </form>
+<form action="" onsubmit="evalInput(inputField.value);inputField.value='';return false;"> 
+Input field: <input type="text" name="inputField" size="30"> 
+</form> 
 ==
 
 evaluator== #|python|
@@ -42,50 +44,12 @@ extrajs==
                 }
 </script>
 
-		<script type="text/javascript"> 
+<script type="text/javascript"> 
 function evalInput(strInput) {
   ggbApplet.evalCommand(strInput);
   return false;
 }
-</script> 
-<form action="" onsubmit="evalInput(inputField.value);inputField.value='';return false;"> 
-Input field: <input type="text" name="inputField" size="30"> 
-</form> 
-
-
-<script type="text/javascript" src="https://cdn.geogebra.org/apps/deployggb.js"></script>
-<script type="text/javascript">
-var parameters = {
-        "language": "fr",
-        "width":600,
-        "height":245,
-        "prerelease":false,
-        "showToolBar":true,
-        "borderColor":null,
-        "showMenuBar":false,
-        "showAlgebraInput":false,
-        "customToolbar":"0 || 1",
-        "showResetIcon":true,
-        "enableLabelDrags":false,
-        "enableShiftDragZoom":true,
-        "enableRightClick":false,
-        "capturingThreshold":null,
-        "showToolBarHelp":true,
-        "errorDialogsActive":true,
-        "useBrowserForJS":false
-    }
-var applet = new GGBApplet(parameters, '5.0', 'ggb-container');
-window.onload = function() {
-applet.evalCommand();
-applet.inject(); 
-};
-
-function maConstruction(){
-document.
-
-};
 </script>
-
 ==
 
 
