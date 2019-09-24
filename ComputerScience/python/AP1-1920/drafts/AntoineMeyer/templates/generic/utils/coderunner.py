b55e7d3adf2db0f7570c6ce23c0c0be8e557180a
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-24 10:02:19 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/utils/coderunner.py

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/utils/coderunner.py
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/utils/coderunner.py
@@ -1,10 +1,11 @@
 import sys
 from copy import deepcopy
 from io import StringIO
-from typing import NoReturn, List, Any, Callable
+from typing import NoReturn, List, Callable
 from unittest import mock
 
 import testfeedback as fb
+from grader import GraderError
 from mockinput import mock_input
 
 
@@ -54,6 +55,7 @@ class CodeRunner:
         # history and feedback
         self.tests = []  # List[Union[fb.TestGroup, fb.TestFeedback]]
         self.current_test_group = None
+        self.current_test = None
 
     def copy(self):
         r = CodeRunner(self.code)
@@ -75,37 +77,43 @@ class CodeRunner:
     def set_globals(self, **variables) -> NoReturn:
         self.previous_state = None
         self.current_state.update(variables)
-        self.record_test(fb.SetGlobalsFeedback(variables))
 
     def set_state(self, state: dict) -> NoReturn:
         self.previous_state = None
         self.current_state = state
-        self.record_test(fb.SetStateFeedback(state))
 
     def set_argv(self, argv: List[str]) -> NoReturn:
         self.argv = argv.copy()
-        self.record_test(fb.SetArgvFeedback(argv))
 
     def set_inputs(self, inputs: List[str]) -> NoReturn:
         self.current_inputs = inputs.copy()
-        self.record_test(fb.SetInputsFeedback(inputs))
 
     """Feedback management."""
 
     def begin_test_group(self, title: str) -> NoReturn:
-        self.current_test_group = fb.TestGroup(title)
+        self.current_test_group = fb.TestGroupFeedback(title)
         self.tests.append(self.current_test_group)
 
     def end_test_group(self) -> NoReturn:
         self.current_test_group = None
 
     def record_test(self, test: fb.TestFeedback) -> NoReturn:
+        self.current_test = test
         if self.current_test_group:
             self.current_test_group.append(test)
-            if test.status == fb.FAIL:
-                self.current_test_group.status = fb.FAIL
         else:
             self.tests.append(test)
+
+    def record_assertion(self, assertion: fb.AssertFeedback) -> NoReturn:
+        if self.current_test:
+            self.current_test.append(assertion)
+            if not assertion.status:
+                self.current_test.status = False
+                if self.current_test_group:
+                    self.current_test_group.status = False
+        else:
+            raise GraderError("Aucune exécution préalable, assertion "
+                              "impossible.")
 
     def render_tests(self):
         return "\n".join(test.render() for test in self.tests)
@@ -123,7 +131,7 @@ class CodeRunner:
 
         return added, deleted, modified, inputs
 
-    def run(self) -> NoReturn:
+    def run(self, expression: str = None) -> NoReturn:
         """
         Run the student code with the specified lines of input available on
         standard input.
@@ -132,6 +140,7 @@ class CodeRunner:
         """
         self.previous_state = deepcopy(self.current_state)
         self.previous_inputs = self.current_inputs.copy()
+        self.result = None
         self.exception = None
         out_stream = StringIO()
 
@@ -140,7 +149,10 @@ class CodeRunner:
             with mock.patch.object(sys, 'argv', self.argv):
                 with mock.patch.object(sys, 'stdout', out_stream):
                     try:
-                        exec(self.code, self.current_state)
+                        if expression is None:
+                            exec(self.code, self.current_state)
+                        else:
+                            self.result = eval(expression, self.current_state)
                     except Exception as e:
                         self.exception = e
 
@@ -148,57 +160,20 @@ class CodeRunner:
         del self.current_state['__builtins__']
         # store generated output
         self.output = out_stream.getvalue()
-
-        added, deleted, modified, inputs = self.summarize_changes()
-        self.record_test(
-            fb.ExecutionFeedback(deleted, modified, added,
-                                 inputs, self.output, self.exception))
-
-    def evaluate(self, expression: str) -> Any:
-        """
-        Evaluate the provided expression in the context of the student's code,
-        with the specified lines of input available on standard input.
-
-        :param expression: expression to be evaluated.
-        :return: the expression's value.
-        """
-        self.previous_state = deepcopy(self.current_state)
-        self.previous_inputs = self.current_inputs.copy()
-        self.exception = None
-        out_stream = StringIO()
-
-        # evaluate the expression while mocking input, sys.argv and stdout
-        # printing
-        with mock_input(self.current_inputs, self.current_state):
-            with mock.patch.object(sys, 'argv', self.argv):
-                with mock.patch.object(sys, 'stdout', out_stream):
-                    try:
-                        self.result = eval(expression, self.current_state)
-                    except Exception as e:
-                        self.exception = e
-
-        # cleanup final state for feedback
-        del self.current_state['__builtins__']
-        # store generated output
-        self.output = out_stream.getvalue()
-
-        added, deleted, modified, inputs = self.summarize_changes()
-        self.record_test(
-            fb.EvaluationFeedback(expression, self.result,
-                                  deleted, modified, added,
-                                  inputs, self.output, self.exception))
+        # generate execution report
+        self.record_test(fb.TestFeedback(self))
 
     """Assertions."""
 
     def assert_output(self, expected,
                       cmp: Callable = lambda x, y: x == y):
-        status = fb.PASS if cmp(expected, self.output) else fb.FAIL
-        self.record_test(fb.OutputTestFeedback(status, expected))
+        status = cmp(expected, self.output)
+        self.record_assertion(fb.OutputAssertFeedback(status, expected))
 
     def assert_result(self, expected,
                       cmp: Callable = lambda x, y: x == y):
-        status = fb.PASS if cmp(expected, self.result) else fb.FAIL
-        self.record_test(fb.ResultTestFeedback(status, expected))
+        status = cmp(expected, self.result)
+        self.record_assertion(fb.ResultAssertFeedback(status, expected))
 
     def assert_variable_values(self, cmp=lambda x, y: x == y, **expected):
         if not expected:
@@ -206,20 +181,21 @@ class CodeRunner:
         missing = deleted_variables(expected, self.current_state)
         incorrect = modified_variables(expected, self.current_state, cmp)
 
-        status = fb.FAIL if missing or incorrect else fb.PASS
-        self.record_test(fb.VariableValuesTestFeedback(
+        status = not (missing or incorrect)
+        self.record_assertion(fb.VariableValuesAssertFeedback(
             status, expected, missing, incorrect))
 
     def assert_no_global_change(self):
         added, deleted, modified, _ = self.summarize_changes()
-        status = fb.FAIL if added or deleted or modified else fb.PASS
-        self.record_test(fb.NoGlobalChangeTestFeedback(status))
+        status = not(added or deleted or modified)
+        self.record_assertion(fb.NoGlobalChangeAssertFeedback(status))
 
     def assert_no_exception(self):
         status = self.exception is None
-        self.record_test(fb.NoExceptionTestFeedback(status))
+        self.record_assertion(fb.NoExceptionAssertFeedback(status))
 
     def assert_exception(self, exception_type):
         status = isinstance(self.exception, exception_type)
-        self.record_test(fb.ExceptionTestFeedback(status, exception_type))
+        self.record_assertion(
+            fb.ExceptionAssertFeedback(status, exception_type))
 
