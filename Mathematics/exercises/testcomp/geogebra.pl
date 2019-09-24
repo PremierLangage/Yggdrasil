9ed41e1e4b4b592c787a582ea9e82c5cd772b227
David DOYEN <David.Doyen@u-pem.fr>
2019-09-24 17:04:13 +0200

    ddoyen Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -15,11 +15,8 @@ Tracer un cercle de rayon 2.
 
 form==
 <div id="applet_container"></div>
-        <input value="Set point A invisible" onclick="ggbApplet.setVisible('A', false)" type="button">
+        <input value="Set point A invisible" onclick="ggbApplet.evalCommand('A=(1,1)')" type="button">
         <input value="Set point A visible" onclick="ggbApplet.setVisible('A', true)" type="button">       
-<form action="" onsubmit="evalInput(inputField.value);inputField.value='';return false;"> 
-Input field: <input type="text" name="inputField" size="30"> 
-</form> 
 ==
 
 evaluator== #|python|
