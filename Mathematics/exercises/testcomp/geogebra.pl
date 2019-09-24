84a6b7059e925caebd63a09584884371101e22c4
David DOYEN <David.Doyen@u-pem.fr>
2019-09-24 21:30:38 +0200

    ddoyen Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -65,7 +65,7 @@ function ggbValues() {
         values.push({id: 'ggb_' + id, value: value});
     }
     var myDiv = document.getElementById("answer");
-    myDiv.innerHTML = str(values);
+    myDiv.innerHTML = values;
 }
 
 </script>
