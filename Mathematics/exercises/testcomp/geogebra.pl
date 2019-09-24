5e1e6013fe1d22b8ad48e61988252550e8bc05f8
David DOYEN <David.Doyen@u-pem.fr>
2019-09-24 19:48:22 +0200

    ddoyen Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -30,18 +30,6 @@ extrajs==
 		"prerelease":false,
 		"width":500,
 		"height":300,
-		"showToolBar":false,
-		"borderColor":"#FFFFFF",
-		"showMenuBar":false,
-		"showAlgebraInput":false,
- 		"showResetIcon":false,
-		"enableShiftDragZoom":true,
-		"enableRightClick":false,
-		"capturingThreshold":null,
-		"showToolBarHelp":false,
- 		"errorDialogsActive":true,
-		"useBrowserForJS":true,
-        "langage":"en"
 	};
 parameters.appletOnLoad = function() {
 ggbApplet.evalCommand('A=(1,1)\nO=(0,0)');	
