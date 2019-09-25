bd59e11849133bd02b07f7eb9848676394d42030
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-25 13:34:41 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/templates/simpleinput.pl

---
+++ ComputerScience/python/AP1-1920/templates/simpleinput.pl
@@ -0,0 +1,25 @@
+@ /utils/sandboxio.py
+@ /grader/evaluator.py [grader.py] 
+@ /builder/before.py [builder.py] 
+
+component =: Input
+component.type = number
+
+before==
+==
+
+title==
+Input Component
+==
+
+text==
+==
+
+form==
+{{ component|component}}
+==
+
+evaluator==
+grade = (100, component.value);
+==
+
