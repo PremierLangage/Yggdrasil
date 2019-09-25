d0f1d35aee9eac31519cc8bf894d86ee8bd8ac08
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-25 11:42:29 +0200

    mzipstein Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/rectanglefixe.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/rectanglefixe.pl
@@ -0,0 +1,22 @@
+
+author zip
+
+# Heritage d'un type d'exercice 
+extends=../../templates/pyeditor.pl
+
+title = rectangle fixe
+
+
+text== les dimensions d'un rectangle sont 3 et 7.
+ecrire un programme qui calcule et affiche le périmètre et la surface du rectangle
+sous la forme :
+le périmètre est ?? et la surface est ??
+pltest==
+>>> 
+le périmètre est 20 et la surface est 21
+>>>
+
+==
+
+
+
