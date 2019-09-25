29e61d5a0a8439ed7068d25f67789dc6f9ccb867
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-25 22:55:37 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/mkl.py

--- ComputerScience/python/AP1-1920/drafts/dominique/mkl.py
+++ ComputerScience/python/AP1-1920/drafts/dominique/mkl.py
@@ -17,4 +17,23 @@ def fromcsv(filename, sourcecol="source", targetcol="target"):
         return MatchListItem,expected
 
 
-
+def selectionfromcsv(filename, number=4, sourcecol="source", targetcol="target"):
+    """
+    return number elements of the file or all the file if the lenght is less than number
+    """
+    l=[]
+    with open(filename,"r") as csvfile:
+        reader=csv.DictReader(csvfile,delimiter=';')
+        for row in reader:
+            l.append((row[sourcecol],row[targetcol]))
+    if len(l)<number:
+        number = len(l)
+    random.shuffle(l)
+    MatchListItem=[]
+    expected=[]
+    for n in range(number):
+            MatchListItem.append({"id":"source"+str(n),"content":l[n][0], "source": True})
+            MatchListItem.append({"id":"target"+str(n),"content":l[n][1], "target": True})
+            expected.append({ "source": "source"+str(n), "target": "target"+str(n) })
+    return MatchListItem,expected
+    
