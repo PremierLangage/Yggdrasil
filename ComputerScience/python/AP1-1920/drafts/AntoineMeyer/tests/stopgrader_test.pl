cc913362ab039ae7598d308df67d7192ebf4d59e
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-25 10:53:14 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/tests/stopgrader_test.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/tests/stopgrader_test.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/tests/stopgrader_test.pl
@@ -1,18 +1,10 @@
 # author = Antoine Meyer
 extends = ../templates/generic/generic.pl
 
-title = Échange de deux variables
+title = Test de l'exception StopGrader
 
 text==
-On suppose qu'il existe deux variables `a` et `b` de valeurs et de types 
-quelconques précédemment fixées (vous n'avez donc pas à les initialiser
-ni à les lire).
-
-Écrire un programme dont l'exécution échange les valeurs de `a` et `b`, 
-sans rien afficher.
-
-**Attention**, *votre programe doit fonctionner quelles que soient les valeurs 
-de `a` et `b` et leurs types (qui peuvent être différents).*<br><br>
+Exercice vide. Une exception d'arrêt est lancée immédiatement.
 ==
 
 grader==
