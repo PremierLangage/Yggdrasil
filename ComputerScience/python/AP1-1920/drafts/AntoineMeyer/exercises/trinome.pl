102dc441cf42e4e79e247ea40ee6646da24388c0
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-25 12:16:25 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/trinome.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/trinome.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/trinome.pl
@@ -5,7 +5,7 @@ title = Trinôme du second degré
 text==
 On suppose qu'il existe trois variables `a`, `b` et `c` de valeurs `int` ou `float`.
 Écrire un programme dont l'exécution crée une variable `nb_solutions` dont la valeur 
-représente le nombre de solutions réelles de l'équation $a x^2 + b x + c$.
+représente le nombre de solutions réelles de l'équation $%a x^2 + b x + c%$.
 
 On représentera le fait que l'équation a une infinité de solutions en affectant à 
 la variable `nb_solutions` la valeur `float('inf')` (qui représente $%+\infty%$).
@@ -39,7 +39,7 @@ essais = [
 
 for a, b, c in essais:
     set_globals(a=a, b=b, c=c)
-    run(title="${} x^2 + {} x + c = 0$".format(a, b, c))
+    run(title="$%{} x^2 + {} x + c = 0%$".format(a, b, c))
     assert_variable_values(nb_solutions=nb_sols(a, b, c))
 ==
 
