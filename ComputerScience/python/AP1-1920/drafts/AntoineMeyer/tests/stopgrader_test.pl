b7d28eba4f254125afd0532d49cf8047fd67bcd3
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-25 10:52:46 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/tests/stopgrader_test.pl

---
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/tests/stopgrader_test.pl
@@ -0,0 +1,21 @@
+# author = Antoine Meyer
+extends = ../templates/generic/generic.pl
+
+title = Échange de deux variables
+
+text==
+On suppose qu'il existe deux variables `a` et `b` de valeurs et de types 
+quelconques précédemment fixées (vous n'avez donc pas à les initialiser
+ni à les lire).
+
+Écrire un programme dont l'exécution échange les valeurs de `a` et `b`, 
+sans rien afficher.
+
+**Attention**, *votre programe doit fonctionner quelles que soient les valeurs 
+de `a` et `b` et leurs types (qui peuvent être différents).*<br><br>
+==
+
+grader==
+raise StopGrader
+==
+
