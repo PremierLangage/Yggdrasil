b39d9d2829425b4aea8c029334a50dbc168ce6a3
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-09-25 10:59:29 +0200

    ameyer Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/grader.py

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/grader.py
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/grader.py
@@ -42,10 +42,8 @@ def grade_this(code: str, tests: str):
 
     try:
         exec(tests, namespace)
-    except test.StopGrader:  # BUG ! does not seem to work
-        print("caught StopGrader", file=sys.stderr)
-    except AssertionError:  # BUG ! should not be necessary
-        print("caught AssertionError", file=sys.stderr)
+    except test.StopGrader:
+        pass
     except Exception as e:
         return (0, "Une erreur s'est produite pendant la validation. Veuillez "
                    "contacter un enseignant ({})".format(e))
