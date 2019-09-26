c945d067ffe96959adbbbfd9b72ce35f1e7ea492
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-26 15:34:32 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/error5.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/error5.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/error5.pl
@@ -7,15 +7,15 @@ author=Dominique Revuz
 
 clog=error|quoting|print
 
-v1=Bonjour je m'appel Jules et je suis heureux de vous acceuillir ici !
+v1=Bonjour je m'appelle Jules et je suis heureux de vous acceuillir ici !
 v2=Bonjour je m'appelle Emilie et je suis heureuse de vous acceuillir ici !
 v3=Bonjour je m'appelle Anne et je suis heureuse de vous acceuillir ici !
 v4=Bonjour je m'appelle Julie et je suis heureuse de vous acceuillir ici !
 v5=Bonjour je m'appelle Lamia et je suis heureuse de vous acceuillir ici !
 v6=Bonjour je m'appelle Sharleen et je suis heureuse de vous acceuillir ici !
 v7=Bonjour je m'appelle Mina et je suis heureuse de vous acceuillir ici !
-v8=Bonjour je m'appel Dominique et je suis heureux de vous acceuillir ici !
-v9=Bonjour je m'appel Nadime et je suis heureux de vous acceuillir ici !
+v8=Bonjour je m'appelle Dominique et je suis heureux de vous acceuillir ici !
+v9=Bonjour je m'appelle Nadime et je suis heureux de vous acceuillir ici !
 
 title= Corrige Moi !
 
@@ -32,7 +32,7 @@ Texte attendu :
 before==
 import random
 var = random.choice([v1,v2,v3,v4,v5,v6,v7,v8,v9])
-code = "print('"+var+"')"
+editor.code = "print('"+var+"')"
 expectedoutput=var
 text += "\n\t"+ var
 ==
