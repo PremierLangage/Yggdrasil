0050a14f1c1bc90eefd685540828eae87c0ab5cf
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2019-09-26 13:32:34 +0200

    ccalle Yggdrasil/ComputerScience/python/AP1-1920/drafts/ChristopheCallé/test.pl

---
+++ ComputerScience/python/AP1-1920/drafts/ChristopheCallé/test.pl
@@ -0,0 +1,36 @@
+extends=../templates/generic/generic.pl
+
+title=fibonacci
+
+text==
+We can get the fibonacci numbers recursively as defined below:
+$%
+fibonacci(n) :=    
+\left\\{ 
+\begin{array}{ll}
+0 & \text{ si } n = 0 \\newline
+1 & \text{ si } n = 1 \\newline
+fibonacci(n-1) + fibonacci(n-2) & \text{ si } n > 1 \\newline
+\end{array}
+\\right.
+%$
+
+Write a recursive function `fibonacci` that takes an integer n and returns the nth term of the sequence.
+==
+
+
+component.code ==
+def fibonacci(n):
+    pass
+==
+
+grader==
+def fibo(n):
+    pass
+
+begin_test_group("Tris d'éléments distincts")
+run()
+end_test_group()
+
+
+==
