bc732a7b40518034c120749cc495930abee6a4fd
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2019-09-26 14:44:56 +0200

    ccalle Yggdrasil/ComputerScience/python/AP1-1920/drafts/ChristopheCallé/exemple.pl

--- ComputerScience/python/AP1-1920/drafts/ChristopheCallé/exemple.pl
+++ ComputerScience/python/AP1-1920/drafts/ChristopheCallé/exemple.pl
@@ -3,11 +3,16 @@ extends=../../templates/pltest.pl
 title=Un exemple
 
 text==
-Déclarez une variable `a` de n'importe quel type
+Déclarez une variable `a` qui contient True
 ==
 
 pltest==
 >>> a == a
+True
+==
+
+pltest1==
+>>> a
 True
 ==
 
