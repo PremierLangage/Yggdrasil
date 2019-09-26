6e9f29f78feecd623cecf0fa4d251c9130516880
root <root@pl.u-pem.fr>
2019-09-26 17:29:10 +0200

    push 0.7.1

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/test_grader.py
+++
@@ -1,9 +0,0 @@
-import sandboxio
-
-if __name__ == "__main__":
-    answers = sandboxio.get_answers()
-    context = sandboxio.get_context()
-    sandboxio.output(0, 
-        "Answers:\n" + str(answers)
-        + "\n\nContext:\n" + str(context))
-
--- ComputerScience/python/AP1_2019-20/template/utils/functionap1.py
+++
@@ -1,7 +0,0 @@
-
-
-
-
-def maman():
-    return "maman"
-
