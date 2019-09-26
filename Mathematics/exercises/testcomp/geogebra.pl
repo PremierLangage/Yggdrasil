095345c0d0491cb251aaba4fb33f42b351de4fbb
David DOYEN <David.Doyen@u-pem.fr>
2019-09-26 22:11:15 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -71,7 +71,7 @@ function ggbValues() {
         var id = app1.getObjectName(i);
         var type = app1.getObjectType(id);
         var x="";var y="";var z="";
-        if (type=="point" || type=="segment") {
+        if (type=="point" || type=="line") {
         var x = JSON.stringify(app1.getListValue(id,1));
         var y = JSON.stringify(app1.getYcoord(id));
         var z = JSON.stringify(app1.getZcoord(id));
