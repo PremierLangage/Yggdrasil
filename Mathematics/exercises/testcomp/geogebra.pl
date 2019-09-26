7744a61f3adba8aab7d73780e4f9e8720e74dce1
David DOYEN <David.Doyen@u-pem.fr>
2019-09-26 17:23:47 +0200

    ddoyen Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -52,7 +52,7 @@ var parameters = {
 }
 
 parameters.appletOnLoad = function() {
-app1.evalCommand('A=(2,0)\nShowLabel(A, true)\nB=(-1,1)\nC=(-1,-1)\nPolygon(A,B,C) ');	
+app1.evalCommand('A=(2,0)\nShowLabel({A,B,C}, true)\nB=(-1,1)\nC=(-1,-1)\nPolygon(A,B,C) ');	
 }
 
 var applet = new GGBApplet('5.0', parameters);
