8c687e466dbd0db150ace3061999e8b734929c05
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-26 15:16:26 +0200

    mzipstein Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/rectanglefixe.pl

---
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/rectanglefixe.pl
@@ -0,0 +1,28 @@
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/template/soluce.pl
+
+title = rectangle fixe
+
+
+text== 
+Les dimensions d'un rectangle sont 3 et 7.
+
+Ecrire un programme qui calcule et affiche le périmètre et la surface du rectangle
+sous la forme :
+
+le périmètre est ?? et la surface est ??.
+==
+code==
+"le périmètre est ?? et la surface est ??."
+==
+
+soluce==
+print("le périmètre est 20 et la surface est 21.")
+==
+
+plsoluce==
+t|le périmètre est 20 et la surface est 21.
+==
+
