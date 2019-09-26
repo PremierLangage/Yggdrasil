b65b969a376632ab48a5fb5e403ec098f367b12d
David DOYEN <David.Doyen@u-pem.fr>
2019-09-26 17:40:47 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -61,7 +61,7 @@ applet.inject('applet_container','preferHTML5');
 
 
 function ggbEval() {
-    app1.evalCommand('t=IsDefined((4,4))');	
+    app1.evalCommand('t=IsDefined(Circle((1,1), -2))');	
 }
 
 function ggbValues() {
