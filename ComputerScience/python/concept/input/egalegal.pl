5daed895b0ace9c885b88b1549f123fc71879945
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-26 15:56:04 +0200

    drevuz Yggdrasil/ComputerScience/python/concept/input/egalegal.pl

--- ComputerScience/python/concept/input/egalegal.pl
+++ ComputerScience/python/concept/input/egalegal.pl
@@ -4,7 +4,7 @@ author=Dominique Revuz
 title= Egal ou EgalEgal ?
 name= Egal ou EgalEgal ? 
 tag=root
-extends=/ComputerScience/python/python/0PLG/soluce.pl
+extends=/ComputerScience/python/template/soluce.pl
 
 text==
 
@@ -12,14 +12,12 @@ text==
 
 L'opérateur = est utilisé pour l'opération d'affectation, pour l'opérateur de comparaison ce caractère est doublé (==).
 
-
-
 == 
 
 code==
 a = int(input())
 b = int(input())
-if a ? b :
+if a ?? b :
 	print(" les deux valeurs entrées sont égales ")
 else:
 	print(" les deux valeurs entrées ne sont pas égales ")
@@ -29,8 +27,8 @@ else:
 feedback==
 Rapel le double == est un opérateur logique. <br>
 le simple = est un opérateur d'affectation qui n'est pas commutatif. <br>
-la partie gauche doit être une référence qui sera réutilisable plus tard, cette référence pointera sur le résultat calculé dans la partie droite de l'affectation.
-==
+la partie gauche doit être une variable qui sera réutilisable plus tard, cette référence pointera sur le résultat calculé dans la partie droite de l'affectation.
+=
 
 inputgenerator==
 from random import randint
@@ -47,5 +45,6 @@ if not a != b :
 else:
 	print(" les deux valeurs entrées ne sont pas égales ")
 ==
+
 
 
