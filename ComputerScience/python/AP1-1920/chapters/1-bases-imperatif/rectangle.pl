c80bf1d7d5307810d93f61187818aeaf80feb9e1
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-26 15:18:42 +0200

    mzipstein Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/rectangle.pl

---
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/rectangle.pl
@@ -0,0 +1,34 @@
+
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/template/soluce.pl
+
+title = rectangle dimensions variables
+
+
+text==  
+Les dimensions d'un rectangle sont entrées par l'utilisateur, le programme testeur fournira les deux valeurs.
+
+Ecrire un programme qui calcule et affiche le périmètre et la surface du rectangle
+sous la forme :
+
+le périmètre est ?? et la surface est ?? .
+==
+
+soluce==
+l=int(input())
+la=int(input())
+print("le périmètre est",2*(l+la),"et la surface est",l*la,".")
+==
+mplsoluce0==
+Pour un rectangle de 2 sur 3
+2 
+3
+==
+
+mplsoluce1==
+t
+2 
+3
+==
