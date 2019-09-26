c5c2216a52cedeaf0ef3caab7db45e55e118c15e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-26 15:51:57 +0200

    drevuz Yggdrasil/ComputerScience/python/AP1-1920/templates/utils/pltest_doc.py

--- ComputerScience/python/AP1-1920/templates/utils/pltest_doc.py
+++ ComputerScience/python/AP1-1920/templates/utils/pltest_doc.py
@@ -47,7 +47,7 @@ class PlRunner(doctest.DocTestRunner):
     def runcompiletest(self, dic={}, name="compiletest"):
         try:
             compile(self.student,"Votre code",'exec')
-            exec(self.student, dic)
+            exec(self.student, dic) # FIXME sortie standard à jetter 
             return True,"Compilation OK"
         except SyntaxError as e:
             self.fb.addTestSyntaxError(name,subnlbybr(traceback.format_exc(limit=0,chain=False))," Syntaxe ")
