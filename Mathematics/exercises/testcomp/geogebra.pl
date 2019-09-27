9e364f015c0ecb67901b67ba0d7b01e2b2aadc70
David DOYEN <David.Doyen@u-pem.fr>
2019-09-27 19:46:00 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -88,7 +88,8 @@ function ggbEval() {
     applet2.inject('applet_correction','preferHTML5');
     var n = app2.getObjectNumber();
     var name = app2.getObjectName(n-1);
-    alert(str(n));
+    var myDiv = document.getElementById("answer");
+    myDiv.innerHTML = JSON.stringify(n);
     app2.evalCommand('M=Midpoint(Segment(B,C))');
     var cmd="ok=("+name+"==M)";
     app2.debug(cmd);
