eb26e0ac5157e994ce36559e89428c261ff3a7c6
David DOYEN <David.Doyen@u-pem.fr>
2019-09-27 20:31:26 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/geogebra.pl

--- Mathematics/exercises/testcomp/geogebra.pl
+++ Mathematics/exercises/testcomp/geogebra.pl
@@ -74,7 +74,7 @@ var parameters2 = {
 }
 
 parameters.appletOnLoad = function() {
-app1.evalCommand('A=(3,0)\nB=(-3,2)\nC=(-2,-1)\nPolygon(A,B,C)\nShowLabel(A,true)\nShowLabel(B,true)\nShowLabel(C,true)\nSetVisible(XAxis,false)\nSetVisible(YAxis,false)');	
+app1.evalCommand('A=(3,0)\nB=(-3,2)\nC=(-2,-1)\nPolygon(A,B,C)\nShowLabel(A,true)\nShowLabel(B,true)\nShowLabel(C,true)\nShowAxes[3,false]');	
 }
 
 var applet = new GGBApplet('5.0', parameters);
