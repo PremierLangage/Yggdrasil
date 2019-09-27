9de0f5569e4085adb929c68ded24a59649191a25
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-27 15:25:49 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/templates/generic/generic.pl

--- ComputerScience/python/AP1-1920/templates/generic/generic.pl
+++ ComputerScience/python/AP1-1920/templates/generic/generic.pl
@@ -20,13 +20,16 @@ editor.code ==
 ==
 
 # le formulaire de l'exercice contient uniquement l'éditeur
-form==
-{{editor|component}}
-==
+#form==
+#{{editor|component}}
+#==
+form=@ /form/text_editor.html
+
 
 # définition de la procédure de validation
 grader==
 begin_test_group("L'exercice n'a pas défini de procédure de validation.")
 ==
+
 
 
