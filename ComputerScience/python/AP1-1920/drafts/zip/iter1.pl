4f2f85c0dfc9fc9ab6f5fe2ae7a8d5949515d20f
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-09-27 11:22:56 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/iter1.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/iter1.pl
@@ -0,0 +1,39 @@
+
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = nombres divisibles
+
+
+text==  
+
+Ecrire un programme qui lit 12 entiers et affiche combien sont divisibles par 7
+==
+code==
+print("Il y a",nb","multiples de 7")
+==
+soluce==
+nb=0
+for i in range(12):
+    l=int(input())
+        if l%7==0:
+            nb+=1
+print("Il y a",nb","multiples de 7")
+==
+mplsoluce0==
+essai1
+2 
+3
+5
+14
+77
+0
+81 
+7749
+15 
+98
+11
+35
+==
