70f659457ee6601f5c7334c2b6a2b733678fadb5
David DOYEN <David.Doyen@u-pem.fr>
2019-09-29 00:01:56 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/complex_numbers/quadratic_cplx_eq_.pl

--- Mathematics/exercises/complex_numbers/quadratic_cplx_eq_.pl
+++ Mathematics/exercises/complex_numbers/quadratic_cplx_eq_.pl
@@ -12,14 +12,20 @@ input.config = keyboards_JSON
 
 a,b=list_randint(2,-4,4,[0])
 a2,b2=list_randint(2,-4,4,[0])
+while a-b*I==a2+b2*I:
+    a2,b2=list_randint(2,-4,4,[0])
 
 c=randint(-5,5,[0,1,-1])
 s=randitem([-1,1])
+s2=randitem([-I,I,1+I,1-I,I-1])
+
 var('x')
 if param['roots']=='int':
     P=s*(x+a+b*I)*(x+a-b*I)
 if param['roots']=='int2':
     P=s*(x+a+b*I)*(x+a2+b2*I)
+if param['roots']=='int3':
+    P=s2*(x+a+b*I)*(x+a2+b2*I)
 if param['roots']=='rat':
     P=s*(c*x+a+b*I)*(c*x+a-b*I)
 lstsol=list(solveset(P,x,domain=S.Complexes))
@@ -34,6 +40,7 @@ Déterminer les solutions (complexes) de l'équation $! {{latexP}} = 0 !$. Sépa
 evaluator==
 score,_,feedback=ans_struct_complex(input.value,lstsol,"setwobraces")
 ==
+
 
 
 
