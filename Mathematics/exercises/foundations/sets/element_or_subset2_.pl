b802ec8159952a5758d3bee7bfe55b3f5726712e
David DOYEN <David.Doyen@u-pem.fr>
2019-09-29 17:48:31 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/element_or_subset2_.pl

--- Mathematics/exercises/foundations/sets/element_or_subset2_.pl
+++ Mathematics/exercises/foundations/sets/element_or_subset2_.pl
@@ -35,29 +35,6 @@ for i in range(n):
             lhs.append(randitem(s[:2]))
         else:
             lhs.append(randitem(s[2:]))
-    elif case[i]==2:
-        rhs.append(list_randint_norep(randint(3,5),0,9))
-        sol.append(randitem([label_subset,label_subset,""]))
-        if sol[i]==label_subset:
-            lhs.append([randitem(rhs[i])])
-        else:
-            lhs.append([randint(0,9,rhs[i])])
-    elif case[i]==3:
-        rhs.append(list_randint_norep(randint(3,5),0,9))
-        sol.append(randitem([label_subset,label_subset,""]))
-        if sol[i]==label_subset:
-            lhs.append(list_randitem_norep(randint(2,len(rhs[i])),rhs[i]))
-        else:
-            b=randint(2,len(rhs[i]))
-            a=randint(1,b-1)
-            lhs.append(list_randint_norep(a,0,9,rhs[i])+list_randitem_norep(b-a,rhs[i]))
-
-for i in range(n):
-    if isinstance(lhs[i],list):
-        lhs[i]=latex(FiniteSet(*lhs[i]))
-    if isinstance(rhs[i],list):
-        pass
-        rhs[i]=latex(FiniteSet(*rhs[i]))
 
 
 drop=[drop1,drop2,drop3,drop4,drop5]
