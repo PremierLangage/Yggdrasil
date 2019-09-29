995beb55ffa7250bde4a28f6dfd6ea2115ca5ad8
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-29 20:23:20 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/templates/utils/soluceevaluator.py

--- ComputerScience/python/AP1-1920/templates/utils/soluceevaluator.py
+++ ComputerScience/python/AP1-1920/templates/utils/soluceevaluator.py
@@ -32,6 +32,7 @@ if __name__ == "__main__":
         sys.exit(1)
     if "soluce" in dic:
         with open("soluce.py","w") as sol:
+            sol.write('    ')
             sol.write(dic['soluce'])
     lestest=[]
     if "plsoluce" in dic:
@@ -58,6 +59,7 @@ if __name__ == "__main__":
 
 
     output(ret, fb.render())
+
 
 
 
