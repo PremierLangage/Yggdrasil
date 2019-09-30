75d0eca81cb063f374eea9a426b5073defb7dc0e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-30 17:32:53 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/conditions/interval.py

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/conditions/interval.py
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/conditions/interval.py
@@ -124,7 +124,7 @@ def makeinterval(difficulte=2):
             s+= " "+UNION+" "
             exp += " or "
         if  random.randint(1,10) < 9:
-            s += " \leftbrace"+a+"\rightbrace "
+            s += " \{"+a+"\} "
             exp += " x== "+a+" " 
         else:
             strict1=random.randint(1,2)==1
