52a16b8595e650c305250b1fd1bc485d484d13aa
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-09-30 10:43:48 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/iter4.pl

--- ComputerScience/python/AP1-1920/drafts/zip/iter4.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/iter4.pl
@@ -4,16 +4,19 @@
 # Heritage d'un type d'exercice 
 extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
-title =diviseurs
+title = Diviseurs d'un entier 
 
 
 text==  
-
-Ecrire un programme qui lit un  entier (s'il est négatif le remplace par son opposé) puis affiche ses diviseurs et leu nombre.
+Ecrire un programme qui lit un  entier (s'il est négatif le remplace par son opposé)
+Affiche "diviseurs:" suivi 
+d'un diviseur par ligne 
+puis de
+"nombre de diviseurs ??".
 ==
 code==
 
-print("diviseurs")
+print("diviseurs:")
 
 print("nombre de diviseurs",??)
 ==
@@ -24,15 +27,16 @@ nb=1
 if n<0:
     n=-n
 print("diviseur")
-for i in range(n):
-    if n%(i+1)==0:
-        print(i+1)
+for i in range(2,n):
+    if n%i==0:
+        print(i)
         nb+=1
-print(n)
 print("nombre de diviseurs",nb)
 ==
 plsoluce==
-essai1|2 
-essai2|-4
+Simple |2 
+Puissance de 2 |-1024
+Produit | 120
 ==
+
 
