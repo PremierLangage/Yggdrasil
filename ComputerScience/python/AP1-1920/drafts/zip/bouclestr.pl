004f00562a4cc39df0b9fe815e001ba895d98196
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-01 19:14:15 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/bouclestr.pl

--- ComputerScience/python/AP1-1920/drafts/zip/bouclestr.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/bouclestr.pl
@@ -11,8 +11,9 @@ title = Liste des multiples
 
 text==  
 
-Ecrire un programme qui lit un  entier  et affiche la chaine de caractères formeé des n premiers multiples de 17
+Ecrire un programme qui lit un  entier  positif et affiche la chaine de caractères formeé des n premiers multiples de 17
 (si n vaut 4 on affiche 0173451).
+Si l'entier est négatif, on ne fait rien.
 .
 ==
 
@@ -27,6 +28,7 @@ print(s)
 plsoluce==
 court|2 
 tres court |0
+tres tres court |-42
 long | 11
 ==
 
