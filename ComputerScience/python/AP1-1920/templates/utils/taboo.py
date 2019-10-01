c75f14875d71699db1344f0f619f139de342b919
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-01 14:40:42 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/templates/utils/taboo.py

---
+++ ComputerScience/python/AP1-1920/templates/utils/taboo.py
@@ -0,0 +1,10 @@
+
+
+
+import regex as re
+
+def checktaboo(taboo, answer):
+    x = re.sub("(\"(.|\n)*\"|#.*)", "", answer) #enlève les commentaires et les chaînes de caractères
+    # FIXME la chaine de carctère ""  letaboo "" est elle trouvée par la regex suivante ? 
+    return re.search("(^"+taboo+"\s|[^\"]+"+taboo+"\s)", x) != None
+
