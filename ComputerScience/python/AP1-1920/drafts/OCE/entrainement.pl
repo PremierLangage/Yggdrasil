74792b8cf6c73f1f3656204a547ed38023bed1ac
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-01 10:36:08 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/entrainement.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/entrainement.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/entrainement.pl
@@ -6,7 +6,10 @@ extends=../../templates/pyeditor.pl
 
 title = Suite géométrique de raison 3
 
-
+text ==
+Ecrire un programme qui affiche une suite de 12 nombres dont chaque terme soit égal au triple
+du terme précédent
+==
 
 
 
