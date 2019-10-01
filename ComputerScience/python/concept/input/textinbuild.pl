23a879176fb6095327cc797282f87341eff55116
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-01 14:28:32 +0200

    drevuz modified Yggdrasil/ComputerScience/python/concept/input/textinbuild.pl

--- ComputerScience/python/concept/input/textinbuild.pl
+++ ComputerScience/python/concept/input/textinbuild.pl
@@ -25,20 +25,22 @@ X,x= random.choice([("deux",2),("trois",3),("quatre",4)])
 Y,y= random.choice([("deux",2),("trois",3),("quatre",4),("cinq",5)])
 Z,z= random.choice([("la moitier",2),("le tiers",3),("le quart",4)])
 
-text="""
+
+
+import jinja2
+soluce= jinja2.Template(soluce).render(globals())
+
+==
+
+text==
 Ecrivez un programme utilisant une unique variable **age** qui lit cette information au clavier puis affiche :
 
 	Le capitaine a __ ans. Dans {{X}} ans il auras ___ ans.
 
-puis sachant que dans {{Y}} ans, le perroquet aura {{Z}} de l’âge du capitaine (arrondi à l'année inférieur) quel est l'age actuel du perroquet, le programme affiche:
+puis sachant que dans **{{Y}}** ans, le perroquet aura **{{Z}}** de l’âge du capitaine (arrondi à l'année inférieur) quel est l'age actuel du perroquet, le programme affiche:
 
 	Le perroquet du capitaine a __ ans.
 
-
-"""
-
-import jinja2
-soluce= jinja2.Template(soluce).render(globals())
 
 ==
 
@@ -48,6 +50,7 @@ Test 1 |33
 Test 2 |66
 Test 4 |100000
 ==
+
 
 
 
