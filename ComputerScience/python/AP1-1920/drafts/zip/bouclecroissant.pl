46ee0c968b26c554eb4d1f57da7c1f91e3f47dad
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-01 19:20:58 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/bouclecroissant.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/bouclecroissant.pl
@@ -0,0 +1,73 @@
+
+
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title =suite croissante
+
+
+text==  
+
+Ecrire un programme qui lit une suite de valeurs tant quelles forment une suite strictement croissante. 
+Lorsque la suite n'est plus croissante, la saisie s'arrête entiers et 
+le programme affiche la longueur de la suite croissante.
+==
+code==
+
+
+
+print("Vous avez entré",nb,"valeur(s) en ordre croissant")
+==
+
+before==
+import random
+N=random.randint(4,10)
+mplsoluce4="Test aléatoire\n"
+v=40
+for i in range(N):
+    v += random.randint(1,6)
+    mplsoluce4 += str(v)+"\n"
+mplsoluce4 += str(N)+"\n"
+==
+
+soluce==
+nb=1
+prec=int(input())
+suiv=int(input())
+while suiv> prec:
+    nb+=1
+    prec=suiv
+    suiv=int(input())
+print("Vous avez entré",nb,"valeur(s) en ordre croissant")
+==
+mplsoluce0==
+5 Valeurs
+2 
+3
+5
+14
+77
+0
+==
+mplsoluce1==
+Une seul
+4
+1
+==
+mplsoluce2==
+Plateau
+4
+4
+1
+==
+
+mplsoluce3==
+Départ négatif
+-4
+4
+2
+2
+==
+
