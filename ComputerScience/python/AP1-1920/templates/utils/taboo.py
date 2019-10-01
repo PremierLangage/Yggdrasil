d116c42340dc0c72a2122e1222792ec8ab675727
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-01 15:42:11 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/templates/utils/taboo.py

--- ComputerScience/python/AP1-1920/templates/utils/taboo.py
+++ ComputerScience/python/AP1-1920/templates/utils/taboo.py
@@ -4,6 +4,26 @@
 import regex as re
 
 def checktaboo(taboo, answer):
+    for x in taboo.split('|'):
+        if _checktaboo(x,answer):
+            return True
+    return False
+
+def _checktaboo(taboo, answer):
+    """
+    regarde l'occurence du mot taboo dans le fichier answer 
+    en ne cherchant pas dans les commentaires.
+    >>> _checktaboo( "for","for i in ...")
+    True
+    >>> _checktaboo( "for","#for i in ...")
+    False
+    >>> _checktaboo( "for","if 'for' i in ...")
+    False
+    >>> _checktaboo( "for",'if "for" i in ...')
+    False
+    >>> _checktaboo( "for",'formidable')
+    False
+    """
     x = re.sub("(\"(.|\n)*\"|#.*)", "", answer) #enlève les commentaires et les chaînes de caractères
     # FIXME la chaine de carctère ""  letaboo "" est elle trouvée par la regex suivante ? 
     return re.search("(^"+taboo+"\s|[^\"]+"+taboo+"\s)", x) != None
