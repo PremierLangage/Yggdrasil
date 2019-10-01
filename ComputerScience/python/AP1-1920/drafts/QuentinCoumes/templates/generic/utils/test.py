a6282ecb1970041f92550c85bc650425d3cb4808
QUENTIN COUMES <qcoumes@etud.u-pem.fr>
2019-10-02 01:02:25 +0200

    qcoumes modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/QuentinCoumes/templates/generic/utils/test.py

--- ComputerScience/python/AP1-1920/drafts/QuentinCoumes/templates/generic/utils/test.py
+++ ComputerScience/python/AP1-1920/drafts/QuentinCoumes/templates/generic/utils/test.py
@@ -358,7 +358,7 @@ class TestSession:
             total, weight = test.get_grade()
             total_grade += total
             total_weigth += weight
-        return total_grade / total_weigth * 100, 100
+        return total_grade / total_weigth * 100
 
     """Rendering"""
 
