3508d6c8f8ef7e3d2fd52583cc9e5f657e35baed
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-03 10:30:59 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/function/prototypes.pl

--- ComputerScience/C/exercices/function/prototypes.pl
+++ ComputerScience/C/exercices/function/prototypes.pl
@@ -45,7 +45,9 @@ Quel prototype pour quelle fonctionnalité ?
 ==
 
 text==
-Tentez d'associer, pour chaque prototype abstrait de fonction C à gauche, une fonctionnalité décrite à droite.
+Tentez d'associer, pour chaque prototype abstrait de fonction C à gauche, une des fonctionnalité 
+décrite sur la droite.
+
 ==
 
 # STEP 3
