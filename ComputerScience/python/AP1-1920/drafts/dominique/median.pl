9f0ce1a7053c538937fbd973d2b5550bc39f77a9
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-03 23:10:02 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/median.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/median.pl
@@ -0,0 +1,13 @@
+
+
+tags=list|median
+
+doce==
+Summary: The median of a set of data is the middlemost number in the set.
+ The median is also the number that is halfway into the set. 
+ To find the median, the data should be arranged in order from least to greatest. 
+ If there is an even number of items in the data set,
+  then the median is found by taking the mean (average) of the two middlemost numbers.
+==
+
+
