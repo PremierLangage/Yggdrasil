7644fa11f264a9446ced753d816cb0c43ee17d7d
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-03 17:26:51 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/function/prototypes.pl

--- ComputerScience/C/exercices/function/prototypes.pl
+++ ComputerScience/C/exercices/function/prototypes.pl
@@ -31,32 +31,32 @@ match.nodes = []
 expected = []
 
 # le carré d'un nombre entier
-match.nodes.append({"id": "s1", "content": "int foo(int a);", "source": True})
+match.nodes.append({"id": "s1", "content": "`int foo(int a);`", "source": True})
 match.nodes.append({"id": "t1", "content": "Calculer le carré d'un entier.", "target": True})
 expected.append({ "source": "s1", "target": "t1" })
 
 # la moyenne d'une liste de notes
-match.nodes.append({"id": "s2", "content": "float foo(int* tab, int size);", "source": True})
+match.nodes.append({"id": "s2", "content": "`float foo(int* tab, int size);`", "source": True})
 match.nodes.append({"id": "t2", "content": "Calculer une moyenne d'un tableau de notes.", "target": True})
 expected.append({ "source": "s2", "target": "t2" })
 
 # afficher la date du jour
-match.nodes.append({"id": "s3", "content": "void foo(void);", "source": True})
+match.nodes.append({"id": "s3", "content": "`void foo(void);`", "source": True})
 match.nodes.append({"id": "t3", "content": "Afficher la date du jour.", "target": True})
 expected.append({ "source": "s3", "target": "t3" })
 
 # calculer la somme de deux floattant
-match.nodes.append({"id": "s4", "content": "float foo(float a, float b);", "source": True})
+match.nodes.append({"id": "s4", "content": "`float foo(float a, float b);`", "source": True})
 match.nodes.append({"id": "t4", "content": "Calculer la somme de deux floattants.", "target": True})
 expected.append({ "source": "s4", "target": "t4" })
 
 # Afficher un double en notation scientifique
-match.nodes.append({"id": "s5", "content": "void foo(double a);", "source": True})
+match.nodes.append({"id": "s5", "content": "`void foo(double a);`", "source": True})
 match.nodes.append({"id": "t5", "content": "Afficher un double en notation scientifique.", "target": True})
 expected.append({ "source": "s5", "target": "t5" })
 
 # Récupérer un entier donné au clavier par l'utilisateur
-match.nodes.append({"id": "s6", "content": "int foo(void);", "source": True})
+match.nodes.append({"id": "s6", "content": "`int foo(void);`", "source": True})
 match.nodes.append({"id": "t6", "content": "Récupérer un entier donné au clavier par l'utilisateur.", "target": True})
 expected.append({ "source": "s6", "target": "t6" })
 
@@ -108,3 +108,4 @@ extracss==
 }
 </style>
 ==
+
