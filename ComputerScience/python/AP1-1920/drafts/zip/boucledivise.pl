1b0269185054bcf2a3f638dd6670a83cb1bf8485
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-03 15:27:13 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/boucledivise.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/boucledivise.pl
@@ -0,0 +1,35 @@
+
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Liste des diviseurs
+text==  
+
+Ecrire un programme qui lit un  entier  strictement positif et affiche,un par ligne, chacun de ses diviseurs.
+Le programme affiche ensuite le nombre de diviseurs.
+Si l'entier est négatif, on ne fait rien
+.
+==
+
+
+soluce==
+n=int(input())
+if n>=1:
+    nbdiv=0
+    for div in range(1,n+1):
+        if n%div ==0:
+            print(div)
+            nbdiv+=1
+    print(nbdiv)
+==
+plsoluce==
+court|2 
+tres court |0
+tres tres court |-42
+long | 114593
+==
+
+
+
