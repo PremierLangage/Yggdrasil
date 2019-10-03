c3645445ff442e9a6f803ef6d2cf9e05a2393d26
David DOYEN <David.Doyen@u-pem.fr>
2019-10-03 10:34:44 +0200

    ddoyen modified Yggdrasil/Chemistry/testcomp/kekule.pl

--- Chemistry/testcomp/kekule.pl
+++ Chemistry/testcomp/kekule.pl
@@ -11,13 +11,16 @@ before ==
 lang = fr
 
 text ==
+Dessiner la molécule de votre choix.
+==
+
+form ==
 <script src="https://unpkg.com/kekule/dist/kekule.js?modules=chemWidget,algorithm"></script>
 <link rel="stylesheet" type="text/css" href="https://unpkg.com/kekule/dist/themes/default/kekule.css" />
 
 <div id="composer" style="width:500px;height:500px" data-widget="Kekule.Editor.Composer" data-chem-obj="url(#molecule)"></div>
-==
-
-form ==
+<input value="Get Formula" onclick="getMol()" type="button">
+<div id="answer">Formule</div>
 
 ==
 extrajs==
@@ -25,6 +28,13 @@ extrajs==
 var composer = new Kekule.Editor.Composer(document.getElementById('composer'));
 composer.setCommonToolButtons(['undo', 'redo','zoomIn', 'zoomOut']);
 composer.setChemToolButtons(['manipulate', 'erase', 'bond', 'atomAndFormula', 'ring', 'charge']);
+function getMol() {
+    var mol = composer.getCurrMol();
+    var formula = mol.calcFormula();
+    var myDiv = document.getElementById("answer");
+    myDiv.innerHTML = JSON.stringify(formula);
+}
+
 </script>
 ==
 evaluator ==
