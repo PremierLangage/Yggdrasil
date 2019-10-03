5456b6279a223a68eff2c9ab407e76b3e91ba45f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-03 20:16:09 +0200

    nborie modified Yggdrasil/Languages/French/pronoms_personnels.pl

--- Languages/French/pronoms_personnels.pl
+++ Languages/French/pronoms_personnels.pl
@@ -21,7 +21,7 @@ Toi plus moi, plus eux plus tous ceux qui le veulent.
 Plus lui plus elle plus tous ceux qui sont seuls. 
 Allez venez et entrez dans la danse. 
 Allez venez, laissez faire l'insouciance. 
-A deux a mille je sais qu'on est capable. 
+A deux a mille je sais qu' on est capable. 
 Tout est possible tout est réalisable. 
 On peut s'enfuir bien plus haut que nos rêves. 
 On peut partir bien plus loin que la grève. 
