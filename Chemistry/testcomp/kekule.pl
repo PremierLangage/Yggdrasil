ba7a13880cf30225f7aaf5e6e6e620a1ed18c390
David DOYEN <David.Doyen@u-pem.fr>
2019-10-03 10:36:36 +0200

    ddoyen modified Yggdrasil/Chemistry/testcomp/kekule.pl

--- Chemistry/testcomp/kekule.pl
+++ Chemistry/testcomp/kekule.pl
@@ -29,7 +29,7 @@ var composer = new Kekule.Editor.Composer(document.getElementById('composer'));
 composer.setCommonToolButtons(['undo', 'redo','zoomIn', 'zoomOut']);
 composer.setChemToolButtons(['manipulate', 'erase', 'bond', 'atomAndFormula', 'ring', 'charge']);
 function getMol() {
-    var mol = getCurrMol();
+    var mol = composer.getCurrMol();
     var formula = mol.calcFormula();
     var myDiv = document.getElementById("answer");
     myDiv.innerHTML = formula.getText();
