410bde65fa9f4f52ac3b9e885830ad0ccae1b0b4
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-03 20:13:31 +0200

    nborie modified Yggdrasil/Languages/French/pronoms_personnels.pl

--- Languages/French/pronoms_personnels.pl
+++ Languages/French/pronoms_personnels.pl
@@ -17,14 +17,14 @@ paroles de la chanson *Toi + moi* de *Grégoire*.
 
 selectable =: Text
 selectable.text== 
-Toi plus moi, plus eux plus tous ceux qui le veulent,
-Plus lui plus elle plus tous ceux qui sont seuls
-Allez venez et entrez dans la danse
-Allez venez, laissez faire l'insouciance
-A deux a mille je sais qu'on est capable
-Tout est possible tout est réalisable
-On peut s'enfuir bien plus haut que nos rêves
-On peut partir bien plus loin que la grève
+Toi plus moi, plus eux plus tous ceux qui le veulent. \n
+Plus lui plus elle plus tous ceux qui sont seuls. 
+Allez venez et entrez dans la danse. 
+Allez venez, laissez faire l'insouciance. 
+A deux a mille je sais qu'on est capable. 
+Tout est possible tout est réalisable. 
+On peut s'enfuir bien plus haut que nos rêves. 
+On peut partir bien plus loin que la grève. 
 ==
 selectable.selectable % true
 
