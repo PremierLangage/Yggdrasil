b836396f6f7c6d48a41bd3abb7c19f47b105a622
David DOYEN <David.Doyen@u-pem.fr>
2019-10-03 10:20:34 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/complex_numbers/cplx_sqrt.pl

--- Mathematics/exercises/complex_numbers/cplx_sqrt.pl
+++ Mathematics/exercises/complex_numbers/cplx_sqrt.pl
@@ -12,7 +12,7 @@ a=randint(1,6)
 b=randint(-6,6,[0])
 z=((a+b*I)**2).expand()
 z_tex=latex(z)
-sol=[a+b*I,a-b*I]
+sol=[a+b*I,-a-b*I]
 sol0_tex=latex(sol[0])
 sol1_tex=latex(sol[1])
 ==
@@ -29,6 +29,7 @@ score,_,feedback=ans_struct_complex(input.value,sol,"setwobraces",form="cartesia
 solution ==
 Les racines carrées sont $! {{sol0_tex}} !$ et $! {{sol1_tex}} !$.
 ==
+
 
 
 
