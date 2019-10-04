956d798c656d74d6fae9cbb94f9d8f4ef3035b5a
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-04 15:13:45 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/templates/looptemplate.pl

---
+++ ComputerScience/python/AP1-1920/templates/looptemplate.pl
@@ -0,0 +1,29 @@
+
+# DR : test 5/10/2019 OK. 
+
+
+
+oneshot=True
+
+extends=/ComputerScience/python/template/loopsoluce.pl
+@ loopbuilder.py [builder.py]
+
+doc==
+text et soluce ne doivent être remplis que si on ne souhaite pas utiliser cet exercice
+en exercice aléatoire.
+==
+
+soluce==
+==
+
+text==
+==
+
+code==
+==
+
+difficulty==
+==
+
+
+
