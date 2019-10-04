da6d13f0adfa2906e5f0d18fe83420ba040706b0
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-04 15:27:59 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles/dynafor.pl

---
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles/dynafor.pl
@@ -0,0 +1,20 @@
+# OCE : test 12/09/2019 OK
+
+extends=../../templates/looptemplate.pl
+
+
+seed % 3
+difficultymax=1
+taboo=while
+needed=for
+
+title=Boucle for 
+
+text= Pour ne pas avoir un warning stupid
+
+plsoluce==
+Exo Boucle for |
+==
+
+
+
