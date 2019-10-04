2169d91f534b345b682b9ab90133ea588a99cd6e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-04 15:46:53 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/bouclestr.pl

--- ComputerScience/python/AP1-1920/drafts/zip/bouclestr.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/bouclestr.pl
@@ -14,7 +14,7 @@ text==
 Ecrire un programme qui lit un  entier  positif et affiche la chaine de caractères formeé des n premiers multiples de 17
 (si n vaut 4 on affiche 0173451).
 Si l'entier est négatif, on ne fait rien.
-.
+
 ==
 
 
@@ -30,7 +30,8 @@ plsoluce==
 court|2 
 tres court |0
 tres tres court |-42
-long | 11
+long | 110
 ==
+
 
 
