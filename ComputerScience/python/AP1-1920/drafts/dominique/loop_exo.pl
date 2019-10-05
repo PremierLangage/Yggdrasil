2e39b564667dc48fa2f2315a2a1e2b5c09f17e85
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-05 21:37:38 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
@@ -7,20 +7,40 @@ title= Boucle d'OR (repetable)
 
 
 text==
-Pour les entiers  de {{b}} à {{h}} parcourus de {{s}} en {{s}} 
+Lire un entier sur l'entré dans la variable X.
+Parcourrir les entiers  de {{b}} à X parcourus de {{s}} en {{s}} 
 {{predtext}}
  calculer {{operatortextb}} {{functeur[0]}} {{operatortexta}} .
 ==
 
 plsoluce==
-Vérification |
+Vérification | #
 ==
+
+soluce==
+import random
+import loopgen
+import functools
+
+random.seed(seed)
+predicats,functeurs,operators= loopgen.getStuff()
+b=random.randint(-100,100)
+h=int(input())
+s= random.randint(1,7) if h>b else random.randint(-3,-1)
+predcode,predtext = random.choice(predicats)
+functeur = random.choice(functeurs)
+operatorcode,operatortextb,operatortexta = random.choice(operators)
+
+result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
+
+
 
 before==
 import random
 import loopgen
 import functools
-
+seed=random.randint(1,10000)
+random.seed(seed)
 predicats,functeurs,operators= loopgen.getStuff()
 b=random.randint(-100,100)
 h=b+random.randint(-10,100)
@@ -31,6 +51,5 @@ operatorcode,operatortextb,operatortexta = random.choice(operators)
 
 result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
 
-soluce="\nprint({})\n".format(result)
 
 ==
