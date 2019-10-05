bf8a47ad55996f4418466cf8f5190a2bb92c08d2
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-05 21:56:07 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
@@ -8,6 +8,8 @@ title= Boucle d'OR (repetable)
 
 text==
 Lire un entier sur l'entré dans la variable X.
+X est toujours positif.
+
 Parcourrir les entiers  de {{b}} à {{b}}+X parcourus de {{s}} en {{s}} 
 {{predtext}}
  calculer {{operatortextb}} {{functeur[0]}} {{operatortexta}} .
@@ -64,13 +66,13 @@ soluce="seed={}\n".format(seed)+soluce
 random.seed(seed)
 predicats,functeurs,operators= loopgen.getStuff()
 b=random.randint(-100,100)
-h=b+random.randint(-10,100)
+#h=b+random.randint(-10,100)
 s= random.randint(1,7) if h>b else random.randint(-3,-1)
 predcode,predtext = random.choice(predicats)
 functeur = random.choice(functeurs)
 operatorcode,operatortextb,operatortexta = random.choice(operators)
 
-result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
+#result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
 
 
 ==
