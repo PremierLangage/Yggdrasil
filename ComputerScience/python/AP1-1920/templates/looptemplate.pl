65fc155b837b1749a5fea993735ea1812bcd7606
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-05 18:40:35 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/templates/looptemplate.pl

--- ComputerScience/python/AP1-1920/templates/looptemplate.pl
+++ ComputerScience/python/AP1-1920/templates/looptemplate.pl
@@ -9,21 +9,16 @@ extends=/ComputerScience/python/template/loopsoluce.pl
 @ loopbuilder.py [builder.py]
 
 doc==
+FIXME je ne comprend pas cette doc DR
 text et soluce ne doivent être remplis que si on ne souhaite pas utiliser cet exercice
 en exercice aléatoire.
 ==
 
-soluce==
-==
+soluce=
+text=
 
-text==
-==
 
-code==
-==
 
-difficulty==
-==
 
 
 
