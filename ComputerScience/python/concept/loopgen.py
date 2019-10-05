e1ff37411103a56a0d083644d82b5f2d44baf568
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-05 18:27:20 +0200

    drevuz modified Yggdrasil/ComputerScience/python/concept/loopgen.py

--- ComputerScience/python/concept/loopgen.py
+++ ComputerScience/python/concept/loopgen.py
@@ -14,27 +14,41 @@ filtre= modulo choice([2,3,5,7,11,13])
 
 '''
 # predicat =( lambda, texte )
+p=random.randint(5,17)
 predicats=[
     (lambda x:x==x, "quelconques"),
     (lambda x: x % 2==0,"pairs" ),
     (lambda x: x % 2 == 1, "impairs"),
     (lambda x: x % 3 == 0, "divisibles par 3"),
+    (lambda x: x % p == 0, "divisibles par "+str(p)),
 ]
 # functeur = ( texte , valeur initiale, lambda x :)
 functeurs=[
     ("la somme", 0, lambda x: accumulateur = x+accumulateur ),
-
+    ("le produit", 0, lambda x: accumulateur = x+accumulateur ),
 ]
 
-Pour tout les entiers de {b} à {h} de {s} en {s} qui sont des entiers
-{predicat} calculer {functeur}.
+# operator 
+p=random.randint(5,17)
+operators=[
+    (lambda x:x*x, "le caré "),
+    (lambda x: x**3,"le cube " ),
+    (lambda x: x//2, "la moitié entière (//)"),
+    (lambda x: x**p == 0, "à la puissance "+str(p)),
+]
 
-l=[]
-while len(l)<1 :
+text==
+Pour tout les entiers de {{b}} à {{h}} de {{s}} en {{s}} qui sont des entiers
+{{predtext}} calculer {{operatortext}} {{functeur}}.
+==
+
+before==
     b=random.randint(-100,100)
     h=b+random.randint(-10,100)
     s= random.randint(1,7) if h>b else random.randint(-3,-1)
-    pred = random.choice(predicats)
+    predcode,predtext = random.choice(predicats)
+    functeur = random.choice(functeurs)
+    operatorcode,operatortext = random.choice(operators)
 
-
-functeur([o for o in range(b,h,s) if predicat(o)])
+result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
+==
