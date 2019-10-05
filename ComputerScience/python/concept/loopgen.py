591f135c4af101ff12ad6d284128823e5baf9246
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-05 18:28:20 +0200

    drevuz modified Yggdrasil/ComputerScience/python/concept/loopgen.py

--- ComputerScience/python/concept/loopgen.py
+++ ComputerScience/python/concept/loopgen.py
@@ -43,12 +43,13 @@ Pour tout les entiers de {{b}} à {{h}} de {{s}} en {{s}} qui sont des entiers
 ==
 
 before==
-    b=random.randint(-100,100)
-    h=b+random.randint(-10,100)
-    s= random.randint(1,7) if h>b else random.randint(-3,-1)
-    predcode,predtext = random.choice(predicats)
-    functeur = random.choice(functeurs)
-    operatorcode,operatortext = random.choice(operators)
+import loopgen
+b=random.randint(-100,100)
+h=b+random.randint(-10,100)
+s= random.randint(1,7) if h>b else random.randint(-3,-1)
+predcode,predtext = random.choice(predicats)
+functeur = random.choice(functeurs)
+operatorcode,operatortext = random.choice(operators)
 
 result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
 ==
