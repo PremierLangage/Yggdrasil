eaf4f99f6262e671b018278e8a8ee6d41e162394
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-05 21:38:33 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
@@ -25,14 +25,15 @@ import functools
 random.seed(seed)
 predicats,functeurs,operators= loopgen.getStuff()
 b=random.randint(-100,100)
-h=int(input())
+h=b+int(input())
 s= random.randint(1,7) if h>b else random.randint(-3,-1)
 predcode,predtext = random.choice(predicats)
 functeur = random.choice(functeurs)
 operatorcode,operatortextb,operatortexta = random.choice(operators)
 
 result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
-
+print(result)
+==
 
 
 before==
