b13a3cf43a95183ee2a7a12522e41e07334521f5
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-05 21:21:52 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/loop_exo.pl
@@ -9,7 +9,7 @@ title= Boucle d'OR (repetable)
 text==
 Pour tout les entiers  de {{b}} à {{h}} parcourus de {{s}} en {{s}} 
 qui sont {{predtext}}
- calculer  {{functeur[0]}} {{operatortext}} .
+ calculer {{operatortextb}} {{functeur[0]}} {{operatortexta}} .
 ==
 
 pltest==
@@ -27,7 +27,7 @@ h=b+random.randint(-10,100)
 s= random.randint(1,7) if h>b else random.randint(-3,-1)
 predcode,predtext = random.choice(predicats)
 functeur = random.choice(functeurs)
-operatorcode,operatortext = random.choice(operators)
+operatorcode,operatortextb,operatortexta = random.choice(operators)
 
 result=operatorcode( functools.reduce(functeur[2],[o for o in range(b,h,s) if predcode(o)], functeur[1]))
 ==
