521cabfa78f6fbf1093c0a03fb8bd9d61aff7cae
David DOYEN <David.Doyen@u-pem.fr>
2019-10-06 11:33:24 +0200

    ddoyen modified Yggdrasil/Chemistry/testcomp/KekuleViewer.pl

--- Chemistry/testcomp/KekuleViewer.pl
+++ Chemistry/testcomp/KekuleViewer.pl
@@ -23,13 +23,11 @@ form ==
 ==
 extrajs==
 <script>
+Kekule.Indigo.enable()
 var viewer = new Kekule.ChemWidget.Viewer(document.getElementById('viewer'));
 viewer.setRenderType(Kekule.Render.RendererType.R3D);
 viewer.setEnableToolbar(false);  // disable and hide the toolbar
-\\var mol = Kekule.IO.loadFormatData("C=O", 'smi');
-var cmlData = '<cml xmlns="http://www.xml-cml.org/schema"><molecule id="m1"><atomArray><atom id="a2" elementType="C" x2="7.493264658965051" y2="35.58088907877604"/><atom id="a3" elementType="O" x2="8.186084981992602" y2="35.18088907877604"/><atom id="a1" elementType="C" x2="6.800444335937501" y2="35.18088907877604"/></atomArray><bondArray><bond id="b2" order="S" atomRefs2="a2 a3"/><bond id="b1" order="S" atomRefs2="a2 a1"/></bondArray></molecule></cml>';
-var mol = Kekule.IO.loadFormatData(cmlData, 'cml');
-
+var mol = Kekule.IO.loadFormatData("C=O", 'smi');
 viewer.setChemObj(mol);
 
 function loadSmiles() {
