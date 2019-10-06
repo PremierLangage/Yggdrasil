323d4d575afee5df309966106fb418ddaf23b0dd
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-06 11:42:12 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/bouclefct.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/bouclefct.pl
@@ -0,0 +1,55 @@
+
+#author=Zip
+
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = nombre factoriel
+
+
+text==  
+
+Ecrire un programme qui lit un entier et 
+s'il est égal à lafactorielle d'un nombre 
+affiche "??=??!" 
+sinon 
+affiche "n'est pas factoriel"
+
+Exemple
+40320=8!
+22 n'est pas factoriel
+
+==
+
+code==
+
+==
+
+
+
+soluce==
+n=int(input())
+if n==1:
+    print(1=0!)
+f=1
+i=1
+while f <n:
+    f*=1
+    i+=1
+if n==d:
+    print(str(n)+"="+str(i)+"!"
+else:
+    print(n,"n'est pas factoriel")
+==
+
+
+
+
+plsoluce1==
+Négatif |-5
+Facile | 24
+Grand | 479001600
+Tres grand | 35184372088833
+==
+
