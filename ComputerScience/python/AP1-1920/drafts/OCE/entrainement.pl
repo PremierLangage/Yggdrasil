0bc36c21e48acb6d45a8dc9e7850ea61a4c83371
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-06 23:49:23 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/entrainement.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/entrainement.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/entrainement.pl
@@ -9,7 +9,7 @@ val1=random.randint(8,12)
 val2=random.randint(2,5)
 ==
 
-title = Suite géométrique de raison **{{val1}}**
+title = Suite géométrique de raison {{val1}}
 
 text==  
 En utilisant une boucle de type "for...in...range", écrire un programme qui affiche une suite de" + X + "nombres dont chaque terme soit 
