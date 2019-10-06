523145c4615ee01e9798a41209d24badbaa42fc8
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-06 18:00:21 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/bis.pl

---
+++ ComputerScience/python/AP1-1920/drafts/OCE/bis.pl
@@ -0,0 +1,97 @@
+
+author=OCE
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Saisie d'une suite croissante
+
+
+text==  
+
+Ecrire un programme qui lit une suite de valeurs tant quelles forment une suite strictement croissante. 
+
+Lorsque la suite n'est plus croissante, la saisie s'arrête et 
+le programme affiche la longueur de la suite croissante.
+==
+
+code==
+
+
+
+print("Vous avez entré",nb,"valeur(s) en ordre croissant")
+==
+
+before==
+import random
+N=random.randint(4,10)
+mplsoluce4="Test aléatoire\n"
+v=40
+for i in range(N):
+    v += random.randint(1,6)
+    mplsoluce4 += str(v)+"\n"
+mplsoluce4 += str(N)+"\n"
+==
+
+# dr soluce qui marche avec des chaines ;)
+soluce==
+nb=1
+prev=input()
+next=input()
+while prev<next:
+    prev,next = next, input()
+    nb+=1
+
+print("Vous avez entré",nb,"valeur(s) en ordre croissant")
+==
+
+
+zipsoluce==
+nb=1
+prec=int(input())
+suiv=int(input())
+while suiv> prec:
+    nb+=1
+    prec=suiv
+    suiv=int(input())
+print("Vous avez entré",nb,"valeur(s) en ordre croissant")
+==
+mplsoluce0==
+5 Valeurs
+2 
+3
+5
+14
+77
+0
+==
+mplsoluce1==
+Une seul
+4
+1
+==
+mplsoluce2==
+Plateau
+4
+4
+1
+==
+
+mplsoluce3==
+Départ négatif
+-4
+4
+2
+2
+==
+
+
+mplsoluce3==
+Des chaines
+annanas
+bannana
+papel
+mango
+==
+
+
