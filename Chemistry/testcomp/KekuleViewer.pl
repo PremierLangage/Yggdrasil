e8360939dd106bd9590d0fa06e56e6e0f31a78b6
David DOYEN <David.Doyen@u-pem.fr>
2019-10-06 13:56:11 +0200

    ddoyen modified Yggdrasil/Chemistry/testcomp/KekuleViewer.pl

--- Chemistry/testcomp/KekuleViewer.pl
+++ Chemistry/testcomp/KekuleViewer.pl
@@ -24,7 +24,7 @@ CCN(c1cc(C)ccc1C)S(=O)(=O)c1cc(C(N)=O)n(C)c1
 </textarea>
 <div id="div2"> 
 <h2> Kekule </h2>
-<div id="kekule" style="width:100%;height:300px"
+<div id="kekule" style="width:300px;height:300px"
 		 data-widget="Kekule.ChemWidget.Viewer2D" data-enable-toolbar="false"
 </div>
 ==
