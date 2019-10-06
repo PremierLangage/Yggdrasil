bf3952ebe84afe9626396a48d7a47b08f220146e
David DOYEN <David.Doyen@u-pem.fr>
2019-10-06 11:46:05 +0200

    ddoyen modified Yggdrasil/Chemistry/testcomp/KekuleViewer.pl

--- Chemistry/testcomp/KekuleViewer.pl
+++ Chemistry/testcomp/KekuleViewer.pl
@@ -15,7 +15,7 @@ text ==
 
 form ==
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://unpkg.com/kekule/dist/kekule.js?modules=chemWidget,algorithm,,openbabel,indigo"></script>
+<script src="https://unpkg.com/kekule/dist/kekule.js?modules=chemWidget,algorithm,openbabel,indigo"></script>
 <link rel="stylesheet" type="text/css" href="https://unpkg.com/kekule/dist/themes/default/kekule.css" />
 <h2> SMILES </h2>
 <textarea rows="10" cols="50" id="smiles"  style="font-size: 12pt">
