52f1e96fff07c0cad9b52f6134d1e319eac78fd6
David DOYEN <David.Doyen@u-pem.fr>
2019-10-06 11:32:03 +0200

    ddoyen modified Yggdrasil/Chemistry/testcomp/kekule.pl

--- Chemistry/testcomp/kekule.pl
+++ Chemistry/testcomp/kekule.pl
@@ -31,6 +31,11 @@ extrajs==
 var composer = new Kekule.Editor.Composer(document.getElementById('composer'));
 composer.setCommonToolButtons(['undo', 'redo','zoomIn', 'zoomOut']);
 composer.setChemToolButtons(['manipulate', 'erase', 'bond', 'atomAndFormula', 'ring', 'charge']);
+var cmlData = '<cml xmlns="http://www.xml-cml.org/schema"><molecule id="m1"><atomArray><atom id="a2" elementType="C" x2="7.493264658965051" y2="35.58088907877604"/><atom id="a3" elementType="O" x2="8.186084981992602" y2="35.18088907877604"/><atom id="a1" elementType="C" x2="6.800444335937501" y2="35.18088907877604"/></atomArray><bondArray><bond id="b2" order="S" atomRefs2="a2 a3"/><bond id="b1" order="S" atomRefs2="a2 a1"/></bondArray></molecule></cml>';
+var mol = Kekule.IO.loadFormatData(cmlData, 'cml');
+
+composer.setChemObj(mol);
+
 function getMol() {
     var mol = composer.exportObjs(Kekule.Molecule)[0];
     var formula = mol.calcFormula();
