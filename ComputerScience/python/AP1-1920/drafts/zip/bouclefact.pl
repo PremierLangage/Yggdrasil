933153b304039e44e0388f3f3517bab9649a7587
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-06 12:41:40 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/bouclefact.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/bouclefact.pl
@@ -0,0 +1,77 @@
+
+#author=Zip
+
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = nombre factoriel
+
+
+text==  
+
+Ecrire un programme qui lit un entier et
+
+s'il est égal à la factorielle d'un nombre 
+
+affiche "??=??!" 
+
+sinon 
+
+affiche "n'est pas factoriel"
+
+Exemple
+
+40320=8!
+
+22 n'est pas factoriel
+
+==
+
+before==
+import random
+N=random.randint(4,15)
+mplsoluce4="Test aléatoire\n"
+f=1
+for i in range(1,N+1):
+    f*=i
+test=random.randint(0,1)
+f+=test
+mplsoluce4 += str(f)+"\n"
+==
+soluce==
+n=int(input())
+if n==1:
+    print("1=0!")
+f=1
+i=1
+while f <n:
+    f*=i
+    i+=1
+if n==f:
+    print(str(n)+"="+str(i)+"!")
+else:
+    print(n,"n'est pas factoriel")
+==
+
+
+
+
+mplsoluce0==
+Négatif 
+-5
+==
+mplsoluce1==
+Facile 
+24
+==
+mplsoluce2==
+Grand 
+479001600
+==
+mplsoluce3==
+tres grand
+35184372088833
+==
+
+
