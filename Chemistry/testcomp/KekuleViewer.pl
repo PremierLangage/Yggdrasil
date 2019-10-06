b66febb36f55cd78c74b0bf1a3aed6174f333f18
David DOYEN <David.Doyen@u-pem.fr>
2019-10-06 13:40:54 +0200

    ddoyen modified Yggdrasil/Chemistry/testcomp/KekuleViewer.pl

--- Chemistry/testcomp/KekuleViewer.pl
+++ Chemistry/testcomp/KekuleViewer.pl
@@ -34,7 +34,7 @@ extrajs==
 Kekule.Indigo.enable();
 function load_kekule() {
   chemViewer = new Kekule.ChemWidget.Viewer(document.getElementById('kekule'));
-  chemViewer.setRenderType(Kekule.Render.RendererType.R2D);
+  chemViewer.setRenderType(Kekule.Render.RendererType.R3D);
 }
 function display_kekule(smi) {
   var mol = Kekule.IO.loadFormatData(smi, "smi");  
