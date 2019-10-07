63f4908e74be179d4ba417be1b471cb54e222550
David DOYEN <David.Doyen@u-pem.fr>
2019-10-07 18:19:26 +0200

    ddoyen modified Yggdrasil/demo/capitals.pl

--- demo/capitals.pl
+++ demo/capitals.pl
@@ -11,7 +11,7 @@ import csv
 import random as rd
 
 with open('data.csv','r') as csvfile:
-    reader = csv.reader(csvfile)
+    reader = csv.DictReader(csvfile)
     pass
     
 country,article,capital = "France","la","Paris"
