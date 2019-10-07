97659aa4bad44b0a4bfbb63e1a95b5c08ac58812
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-07 23:38:12 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles/Suite_raison_aleatoire.pl

---
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles/Suite_raison_aleatoire.pl
@@ -0,0 +1,38 @@
+# author=OCE
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/template/loopsoluce.pl
+@ /builder/before.py [builder.py]
+
+before==#|python|
+import random
+val1=random.randint(8,12)
+val2=random.randint(2,5)
+==
+
+title = Suite géométrique de raison {{val2}}
+
+text==  
+En utilisant une boucle de type "for...in...range", écrire un programme qui affiche une suite de {{val1}} nombres dont chaque terme soit 
+égal à {{val2}} fois le terme précédent.
+==
+
+code== 
+# écrivez votre code ici
+==
+
+taboo=while
+needed=for
+
+soluce==
+a=input("Entrez un nombre : ")
+for i in range({{val2}}):
+    print(i+1,a)
+    a = a * {{val1}}
+==
+
+plsoluce==
+
+==
+
+
+
