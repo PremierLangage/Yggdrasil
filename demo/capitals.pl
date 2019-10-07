df53074d1b898898f0bab9263c2ddea8ad6a355c
David DOYEN <David.Doyen@u-pem.fr>
2019-10-07 18:11:40 +0200

    ddoyen modified Yggdrasil/demo/capitals.pl

--- demo/capitals.pl
+++ demo/capitals.pl
@@ -12,6 +12,16 @@ import random as rd
 
 with open('data.csv','r') as csvfile:
     reader = csv.reader(csvfile,delimiter=' ')
+    row = rd.choice([x for x in reader])
+
+country,article,capital = row
+
+if article=="le":
+    ofcountry = "du " + country
+elif article=="l":
+    ofcountry = "de l" + country
+elif article=="la":
+    ofcountry = "de la " + country
 
 ==
 
