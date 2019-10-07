39a345181fb1de8cd39279054823074aa003c29d
David DOYEN <David.Doyen@u-pem.fr>
2019-10-07 17:58:38 +0200

    ddoyen modified Yggdrasil/demo/capitals.pl

---
+++ demo/capitals.pl
@@ -0,0 +1,44 @@
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+@ country_data.csv [data.csv]
+
+title = Capitales de pays
+
+before ==
+import csv
+import random as rd
+
+with open('data.csv','r') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter=',')
+    line = rd.choice([x for x in reader])
+
+country,article,capital = line
+
+if article=="le":
+    ofcountry = "du " + country
+elif article=="l'":
+    ofcountry = "de l'" + country
+elif article=="la":
+    ofcountry = "de la " + country
+==
+
+text ==
+Quelle est la capitale {{ofcountry}} ?
+==
+
+input =: Input
+input.type = text
+
+form ==
+{{ input | component }}
+==
+
+evaluator ==
+if int(input.value)==capital:
+        grade=(100,"Bravo !")
+    else:
+        grade=(0,"Raté !")
+==
+
