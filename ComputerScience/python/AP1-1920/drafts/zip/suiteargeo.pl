87c70b6157176e625ac0ed6b0bba210519a3dba8
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-08 14:42:01 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl
@@ -0,0 +1,27 @@
+
+#author=Zip
+
+
+# Heritaged'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = suite arithmético-géométrique 
+
+code==
+nb = int(input("Un nombre : "))
+
+
+==
+
+text==  
+
+Ecrire un programme qui :
+
+on considère la suite définie par:
+
+u<sub>0<\sub>=3
+
+u<sub>n+1<\sub>=2u<sub>n<\sub>+1+3
+
+ lit un entier 
+==
