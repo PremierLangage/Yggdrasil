8054188f74014fb437c4becb7cc7eee2d149dfed
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-08 21:03:23 +0200

    drevuz modified Yggdrasil/demo/capitals.pl

--- demo/capitals.pl
+++ demo/capitals.pl
@@ -13,7 +13,7 @@ essai=1
 if "row" not in globals():
     with open('data.csv','r') as file:
         row=rd.choice(list(csv.reader(file,delimiter=',')))
-
+print(row,file=sys.stderr)
 country,article,capital=row
 
 if article=="le":
