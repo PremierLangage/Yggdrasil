a46b39d9d235bf328c7a835c609d8be74b77e030
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-08 14:50:58 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl

--- ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl
@@ -8,9 +8,9 @@ extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 title = suite arithmético-géométrique 
 
 code==
-nb = int(input("Un nombre : "))
-
-
+?? = int(input("Entrez un nombre : "))
+nb)
+print(??," terme(s) inférieur(s) à",
 ==
 
 text==  
@@ -21,7 +21,16 @@ on considère la suite définie par:
 
 u<sub>0</sub>=3
 
-u<sub>n+1</sub>=2u<sub>n</sub>+1+3
+u<sub>n+1</sub>=2u<sub>n</sub>+1
 
- lit un entier 
+ Ecrire un programme qui:
+ 
+ lit un entier
+
+ affiche le nombre de termes de la suite inférieurs à cet entier 
+
+Exemple  :
+
+Entrez un nombre : 4
+
 ==
