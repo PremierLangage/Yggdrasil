bc328083848318462511e6ce8aaa79206e6a03e5
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-08 15:32:31 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl
@@ -0,0 +1,21 @@
+
+#author=Zip
+
+
+# Heritaged'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Approximation de \pi
+
+code==
+?? = int(input("Entrez un nombre : "))
+
+
+
+
+print(??,"terme(s) inférieur(s) à",nb)
+
+==
+
+text==  
+
