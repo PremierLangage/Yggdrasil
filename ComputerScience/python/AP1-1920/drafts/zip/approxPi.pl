f521aaa4d8234c802abb2e7e4b215d5aa1afc2f6
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-08 16:54:38 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl

--- ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl
@@ -23,4 +23,11 @@ On veut comparer les valeurs de différentes séries qui convergent vers $%\pi%$
 <
 
 $%\pi =4 \times \Sigma\frac{(-1)^n}{2n+1}%$
+
+et
+$%\pi =8 \times \Sigma\frac{1}{(4k+1)(4k+3)}%$
+
+Ecrire un programme qui lit un entier positif n et affiche la somme partielle
+pour chaque série (de 0 à n compris).
+Si l'entier n est négatif, on affiche "erreur"
 ==
