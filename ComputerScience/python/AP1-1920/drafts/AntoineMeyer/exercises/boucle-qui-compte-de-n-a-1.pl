c30d2723e89129030de6068d2697209f13e430aa
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-10-08 20:27:55 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/boucle-qui-compte-de-n-a-1.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/boucle-qui-compte-de-n-a-1.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/boucle-qui-compte-de-n-a-1.pl
@@ -1,20 +1,21 @@
 # author = Antoine Meyer
 extends = ../templates/generic/generic.pl
 
-title = Boucle qui compte de 1 à `nb`
+title = Boucle qui compte de `nb` à 1
 
 text==
 On suppose qu'il existe une variables `nb` désignant un entier strictement positif. 
 Écrire un programme dont l'exécution affiche sur la sortie standard les nombres de 
-`1` à `nb` (chacun suivi d'un retour à la ligne), à l'exclusion de tout autre caractère.
+`nb` à `1` dans l'ordre décroissant (chacun suivi d'un retour à la ligne), 
+à l'exclusion de tout autre caractère.
 
 Par exemple, si `nb` vaut 5, votre programme doit afficher :
 
-    1
-    2
-    3
-    4
     5
+    4
+    3
+    2
+    1
 
 **Attention**, *le programe doit fonctionner quelle que soit la valeur 
 de `nb`.*<br>
@@ -25,13 +26,13 @@ editor.theme=light
 grader==#|python|
 def expected_output(nb):
     res = ""
-    for i in range(1, nb+1):
+    for i in range(nb, 0, -1):
         res += str(i) + '\n'
     return res
 
 def tests(nbs):
     for nb in nbs:
-        run(title = f'De 1 à {nb}',
+        run(title = f'De {nb} à 1',
             globals = {'nb': nb}, 
             output = expected_output(nb))
 
