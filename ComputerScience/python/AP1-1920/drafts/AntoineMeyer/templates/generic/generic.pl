b51c82dc2e91b20436bf7e60f34de5dee19ef8db
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-08 15:41:19 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/generic.pl

---
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/templates/generic/generic.pl
@@ -0,0 +1,35 @@
+@ /utils/sandboxio.py
+@ builder.py
+@ grader.py
+@ utils/test.py
+@ utils/mockinput.py
+@ utils/testgroup.html
+@ utils/testitem.html
+
+title = <em>(Pas de titre défini)</em>
+text = <em>(Pas d'énoncé défini)</em>
+
+# création de l'éditeur de code
+editor =: CodeEditor
+editor.theme=light
+editor.language = python
+editor.cid = py_editor_0
+editor.code ==
+# Saisissez votre code ici, et cliquez sur le bouton
+# de validation quand vous avez terminé
+
+==
+
+# le formulaire de l'exercice contient uniquement l'éditeur
+form==
+{{editor|component}}
+==
+
+# définition de la procédure de validation
+grader==
+begin_test_group("L'exercice n'a pas défini de procédure de validation.")
+==
+
+
+
+
