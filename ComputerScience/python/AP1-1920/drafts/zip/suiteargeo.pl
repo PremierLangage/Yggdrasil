080f4168f90a293aa56e4f0bfb16f5b06f297b33
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-08 14:43:11 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl

--- ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/suiteargeo.pl
@@ -19,9 +19,9 @@ Ecrire un programme qui :
 
 on considère la suite définie par:
 
-u<sub>0<\sub>=3
+u<sub>0</sub>=3
 
-u<sub>n+1<\sub>=2u<sub>n<\sub>+1+3
+u<sub>n+1</sub>=2u<sub>n</sub>+1+3
 
  lit un entier 
 ==
