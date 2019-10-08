d86fca25429a7effa8186cbec909fb7a9b6b45c2
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-10-08 20:39:42 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/ascii-oblique.pl

---
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/ascii-oblique.pl
@@ -0,0 +1,46 @@
+# author = Antoine Meyer
+extends = ../templates/generic/generic.pl
+
+title = Dessin en mode texte : triangle
+
+text==
+On suppose qu'il existe une variables `nb` désignant un entier positif. 
+Écrire un programme dont l'exécution affiche sur la sortie standard un triangle composé 
+de caractères `*`.
+
+Par exemple, si `nb` vaut 5, votre programme doit afficher :
+
+    *
+    **
+    ***
+    ****
+    *****
+
+**Attention**, *le programe doit fonctionner quelle que soit la valeur 
+de `nb`.*<br>
+==
+
+editor.theme=light
+
+grader==#|python|
+def expected_output(nb):
+    res = ""
+    for i in range(nb):
+        res += '*' * (i+1) + '\n'
+    return res
+
+def tests(nbs):
+    for nb in nbs:
+        run(title = f'Hauteur {nb}',
+            globals = {'nb': nb}, 
+            output = expected_output(nb))
+
+begin_test_group("Valeurs fixées")
+tests([0, 4, 5])
+
+begin_test_group("Nombres aléatoires")
+from random import sample
+tests(sample(list(range(10, 20)), 3))
+==
+
+
