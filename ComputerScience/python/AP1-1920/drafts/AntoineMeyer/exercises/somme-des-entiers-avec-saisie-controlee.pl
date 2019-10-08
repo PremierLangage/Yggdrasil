9d96ce50a0a6cfac3375a65425b486de18dc022a
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-10-08 18:32:18 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/somme-des-entiers-avec-saisie-controlee.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/somme-des-entiers-avec-saisie-controlee.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/somme-des-entiers-avec-saisie-controlee.pl
@@ -5,15 +5,18 @@ title = Somme des entiers (avec saisie contrôlée)
 
 text==
 Écrire un programme Python qui demande à l'utilisateur un entier positif `n`,
-puis calcule et affiche la somme des `n` premiers entiers.
+puis calcule et affiche la somme des `n` premiers entiers. 
 
-Voici un exemple d'exécution de votre programme :
+En cas de saisie d'un entier négatif, le programme doit demander une nouvelle saisie. 
+En cas de saisie d'une chaîne impossible à convertir, le programme doit provoquer une erreur.
 
-    Donnez moi un entier positif : -4
+Voici un exemple d'exécution possible :
+
+    Saisir un entier positif : -4
     Mauvaise saisie
-    Donnez moi un entier positif : -10
+    Saisir un entier positif : -10
     Mauvaise saisie
-    Donnez moi un entier positif : 10
+    Saisir un entier positif : 10
     La somme des entiers de 1 à 10 vaut 55
 
 Il est important de respecter *précisément* cet affichage pour que la solution 
@@ -21,27 +24,22 @@ soit considérée comme correcte.
 ==
 
 grader==#|python|
-def expected_output(h):
-    res = ""
-    nb = 0
-    while h > 5:
-        res += str(h) + ' cm\n'
-        h *= .9
-        nb += 1
-    return res + f'nombre de rebonds : {nb}\n'
 
-def tests(hauteurs):
-    for h in hauteurs:
-        run(title = f'Rebonds depuis {h} cm',
-            globals = {'h': h}, 
-            output = expected_output(h))
+saisie1 = ["-4", "-10", "10"]
+affichage1 = """
+Saisir un entier positif : -4
+Mauvaise saisie
+Saisir un entier positif : -10
+Mauvaise saisie
+Saisir un entier positif : 10
+La somme des entiers de 1 à 10 vaut 55
+"""
 
-begin_test_group("Hauteurs fixées")
-tests([20, 3, 5, 20, 400])
+run(
+    inputs=saisie1,
+    output=affichage1
+)
 
-begin_test_group("Hauteurs aléatoires")
-from random import sample
-tests(sample(list(range(100, 200)), 3))
 ==
 
 
