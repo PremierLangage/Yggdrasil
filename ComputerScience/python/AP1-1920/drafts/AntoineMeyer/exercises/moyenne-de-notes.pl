0e6ca4421cb249dfa595be84625c8e981967170a
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-10-08 21:16:13 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/moyenne-de-notes.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/moyenne-de-notes.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/moyenne-de-notes.pl
@@ -6,7 +6,8 @@ title = Moyenne de notes
 text==
 Écrivez un programme qui permet à l'utilisateur de saisir une suite de notes 
 (nombres flottants compris entre 0 et 20), et qui affiche le nombre de notes 
-saisies et leur moyenne. 
+saisies et leur moyenne (ou `'pas de moyenne'` si aucune note correcte n'a 
+été saisie). 
 
 Le nombre de notes n'est pas connu à l'avance, l'utilisateur devra saisir le 
 texte `'stop'` pour indiquer qu'il souhaite s'arrêter. 
@@ -28,23 +29,39 @@ Voici un exemple d'interaction avec le programme :
 ==
 
 grader==#|python|
-def expected_output(nb):
+
+saisies_sans_erreur = [["10"], ["12"], ["1"], ["0"]]
+
+saisies_avec_erreurs = [
+    ["-4", "-10", "10"],
+    ["-4", "-10", "-10", "-1", "-3", "1"]]
+
+def affichage_attendu(saisie):
+    nb = 0
+    somme = 0
     res = ""
-    for i in range(1, nb+1):
-        res += ' ' * (nb - i) + '*' * (2 * i - 1) + '\n'
+    for ligne in saisie:
+        res += f"Saisir une note entre 0 et 20 ou stop pour arrêter : {ligne}\n"
+        if ligne == 'stop':
+            break
+        elif 0 <= float(ligne) <= 20:
+            nb += 1
+            somme += float(ligne)
+        else:
+            res += "Note non prise en compte\n"
+    res += f"Nombre de notes : {nb}\n"
+    moyenne = somme / nb if nb else 'pas de moyenne'
+    res += f"Moyenne : {moyenne}\n"
     return res
+        
 
-def tests(nbs):
-    for nb in nbs:
-        run(title = f'Hauteur {nb}',
-            globals = {'nb': nb}, 
-            output = expected_output(nb))
+begin_test_group("Saisies sans erreur")
+for saisie in saisies_sans_erreur:
+    run(inputs=saisie, output=affichage_attendu(saisie))
 
-begin_test_group("Hauteurs fixées")
-tests([0, 4, 5])
-
-begin_test_group("Hauteurs aléatoires")
-from random import sample
-tests(sample(list(range(10, 20)), 3))
+begin_test_group("Saisies avec erreurs")
+run(inputs=["bonjour"], exception=Exception)
+for saisie in saisies_avec_erreurs:
+    run(inputs=saisie, output=affichage_attendu(saisie))
 ==
 
