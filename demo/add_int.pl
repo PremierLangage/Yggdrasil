db50c960083689f4f98af6fc5350d9995b7adf6d
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-08 21:21:02 +0200

    drevuz modified Yggdrasil/demo/add_int.pl

--- demo/add_int.pl
+++ demo/add_int.pl
@@ -25,9 +25,9 @@ form ==
 evaluator ==
 try:
     if int(input.value)==a+b:
-        grade=(100,"Bravo "+str(a)+"+"+str(b)+"="+str(a+b)+"""<br>\n <font style="size:24px"> Ok d'accord c'est pas terrible comme exercice mais c'est pour la démo :)</font>""")
+        grade=(100,"Bravo "+str(a)+"+"+str(b)+"="+str(a+b)+"""<br>\n<p style="color:blue;font-size:18px;"> Ok d'accord c'est pas terrible comme exercice mais c'est pour la démo :)<br> Remarquez que l'énoncé à changé.</p>""")
     else:
-        grade=(0,"Raté "+str(a)+"+"+str(b)+"="+str(a+b))
+        grade=(0,"Raté "+str(a)+"+"+str(b)+"="+str(a+b)+"""<br>\n<p style="color:red;font-size:18px;"> Ok d'accord c'est pas terrible comme exercice mais c'est pour la démo :)<br> Remarquez que l'énoncé à changé.</p>""")
     exec(before)
 except:
     grade=(-1,"Votre réponse n'est pas un nombre entier.")
