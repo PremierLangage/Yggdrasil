95403ab03eb6122708d7fafe4d33ba655a8c2769
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-10-08 18:35:38 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/somme-des-entiers-avec-saisie-controlee.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/somme-des-entiers-avec-saisie-controlee.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/somme-des-entiers-avec-saisie-controlee.pl
@@ -46,10 +46,8 @@ Saisir un entier positif : 10
 La somme des entiers de 1 à 10 vaut 55
 """
 
-run(
-    inputs=saisie1,
-    output=affichage1
-)
+set_inputs(inputs=saisie1, verbose=True)
+run(output=affichage1)
 
 ==
 
