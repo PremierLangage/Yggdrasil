3616cde5b44abebe68c3f999fb7fec146915e6ed
David DOYEN <David.Doyen@u-pem.fr>
2019-10-09 19:43:06 +0200

    ddoyen modified Yggdrasil/Languages/Greek/adjcomp.pl

--- Languages/Greek/adjcomp.pl
+++ Languages/Greek/adjcomp.pl
@@ -12,8 +12,7 @@ form ==
 
 input.1.type = selectwords
 
-input.1.text = Δαρείου καὶ Παρυσάτιδος γίγνονται παῖδες δύο, πρεσβύτερος μὲν Ἀρταξέρξης, νεώτερος δὲ Κῦρος· ἐπεὶ δὲ ἠσθένει Δαρεῖος καὶ ὑπώπτευε τελευτὴν τοῦ βίου, ἐβούλετο τὼ παῖδε ἀμφοτέρω παρεῖναι.
-
+input.1.text = 
 input.1.indexwords = 6,9
 
 evaluator ==
@@ -43,5 +42,53 @@ formmode="correction"
 ==
 
 
+selectable =: Text
+selectable.text== 
+Δαρείου καὶ Παρυσάτιδος γίγνονται παῖδες δύο, πρεσβύτερος μὲν Ἀρταξέρξης, νεώτερος δὲ Κῦρος· ἐπεὶ δὲ ἠσθένει Δαρεῖος καὶ ὑπώπτευε τελευτὴν τοῦ βίου, ἐβούλετο τὼ παῖδε ἀμφοτέρω παρεῖναι.
+==
+selectable.selectable % true
+
+before==
+==
+
+form==
+{{ selectable|component}}
+==
+
+evaluator==
+
+verbes = ['πρεσβύτερος', 'νεώτερος']
+found = 0
+error = 0
+
+for e in selectable.selections:
+    if e['content'] in pronoms:
+        e['css'] = "success-state"
+        found += 1
+    else:
+        e['css'] = "error-state"
+        error += 1
+
+if found == 10 and error == 0:
+    score = 100
+    msg = '<span class="success-state">Bravo, vous avez trouvé tous les pronoms personnels !</span>'
+else:
+    score = max([0, 10*(found - error)])
+    nb_error = (10 - found) + error
+    if nb_error > 1:
+        msg = '<span class="error-state">Il y a {} erreurs...</spam>'.format(nb_error)
+    else:
+        msg = '<span class="error-state">Il y a 1 seule erreur...</spam>'
+
+grade = (score, msg)
+==
+
+extracss==
+<style>
+.exercise__instructions, .exercise__feedback {
+    margin-bottom: 16px;
+}
+</style>
+==
 
 
