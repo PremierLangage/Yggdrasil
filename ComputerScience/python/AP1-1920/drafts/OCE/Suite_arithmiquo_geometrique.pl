fb7a1eca594aeda3e2a3a0d6d07b2591362edb3f
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-09 11:49:27 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Suite_arithmiquo_geometrique.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Suite_arithmiquo_geometrique.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Suite_arithmiquo_geometrique.pl
@@ -69,8 +69,9 @@ if "needed" in dic:
 
 # executer le code de l'élève et comparer au résultat attendu
 resultat=str((exec(student)))
-x = re.sub("(\"(.|\n)*\"|#.*)", "", resultat) #enlève les commentaires et les chaînes de caractères
+x = pltestgrader.re.sub("(\"(.|\n)*\"|#.*)", "", resultat) #enlève les commentaires et les chaînes de caractères
 print(x)
+
 ==
 # --- Fin Partie liée à l'évaluation de l'exercice
 
