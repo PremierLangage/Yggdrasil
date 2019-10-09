989db8c2655377450898de683d84fa02d240b00e
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-09 12:15:18 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/arrondiReel.pl

--- ComputerScience/python/AP1-1920/drafts/zip/arrondiReel.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/arrondiReel.pl
@@ -8,7 +8,7 @@ extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 title = Approximation de $%\frac{\pi^2}{6}%$
 
 code==
-?? = int(input("Entrez un nombre : "))
+ n= int(input("Entrez un nombre : "))
 
 
 
@@ -31,4 +31,23 @@ partielle (i=1 à n et i=n à 1, borne comprises) pour chaque série.
 
 Si l'entier n est négatif, on affiche "erreur"
 ==
-
+soluce==
+{import math
+croiss,decroi=0,0
+n=int(input()) 
+if n<0:
+    print("erreur")
+    else:
+    for i in range(1,n+1):
+        croiss+=1/(i*i)
+    for i in range(n,0,-1):
+        decroi+=1/(i*i)
+    print(croiss,decroi)
+#print(math.pi*math.pi/6)
+==
+plsoluce==
+erreur | -4
+un | 1
+petit | 10
+grand | 10000
+==
