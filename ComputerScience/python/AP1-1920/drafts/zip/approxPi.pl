7b8b49f3e587888bc7d501b63d833e2b742b4023
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-09 10:08:03 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl

--- ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl
@@ -23,9 +23,9 @@ On veut comparer les valeurs de différentes séries qui convergent vers $%\pi%$
 $%(1):\pi =4 \times \sum\limits_{i=0}\frac{(-1)^i}{2i+1}%$
 
 
-$%\(2):\pi =3 + \sum\limits_{i=1}\frac{1}{2i(2i+1)(2i+3)}%$
+$%\(2):\pi =3 + \sum\limits_{i=1}\frac{(-1)^{i+1}*4}{2i(2i+1)(2i+3)}%$
  
-
+$%\(3):\pi =\sqrt(12)%$
 Ecrire un programme qui lit un entier positif n et affiche la somme partielle
 pour chaque série (n non compris).
 Si l'entier n est négatif, on affiche "erreur"
