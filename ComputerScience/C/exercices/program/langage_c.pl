bc283c441926565de35b0594545b887c377b4b7c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-09 22:26:17 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/program/langage_c.pl

--- ComputerScience/C/exercices/program/langage_c.pl
+++ ComputerScience/C/exercices/program/langage_c.pl
@@ -20,6 +20,7 @@ group.items.append({"id": "7", "content": "Langage Impératif"})
 group.items.append({"id": "8", "content": "Langage haut niveau"})
 group.items.append({"id": "9", "content": "Langage bas niveau"})
 
+random.shuffle(group.items)
 ==
 
 title = Checkbox Group Component
