63fcf0701dc746cbbca8838334b9e216c4d92f2f
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-09 09:56:56 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl

--- ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/approxPi.pl
@@ -12,8 +12,7 @@ code==
 
 
 
-
-print(??,"terme(s) inférieur(s) à",nb)
+print("avec(1)",??,"avec(2),??)
 
 ==
 
@@ -21,11 +20,12 @@ text==
 On veut comparer les valeurs de différentes séries qui convergent vers $%\pi%$
 
 
-$%(1)\pi =4 \times \sum\limits_{i=0}\frac{(-1)^i}{2i+1}%$
+$%(1):\pi =4 \times \sum\limits_{i=0}\frac{(-1)^i}{2i+1}%$
 
-et
-$%\(2)pi =3 + \sum\limits_{i=1}\frac{1}{2i(2i+1)(2i+3)}%$
 
+$%\(2):pi =3 + \sum\limits_{i=1}\frac{1}{2i(2i+1)(2i+3)}%$
+ 
+ 
 Ecrire un programme qui lit un entier positif n et affiche la somme partielle
 pour chaque série (n non compris).
 Si l'entier n est négatif, on affiche "erreur"
