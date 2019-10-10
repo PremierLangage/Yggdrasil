4c6a6aada3f7dc63152b1cd0d717c57df2e3b3c0
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-10 09:31:57 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/type/minuscule_ascii.pl

---
+++ ComputerScience/C/exercices/type/minuscule_ascii.pl
@@ -0,0 +1,44 @@
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#
+author=Nicolas Borie
+title=Caractères minuscules ASCII
+tag=type
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+
+Écrire un programme C qui affiche ses arguments. Le programme ne devra
+pas afficher son nom qui est toujours positionné en tête du tableau
+des arguments. On affichera un argument par ligne. Exécuté sans
+argument, le programme ne doit donc rien afficher.
+    
+==
+
+editor.code==
+#include <stdio>
+
+int main(){
+  ...
+  printf(" --> ");
+  ...
+}
+==
+
+solution==
+#include <stdio>
+
+int main(){
+  char c;
+
+  for (c='a' ; c<='z' ; c++)
+    printf("%c --> %d\n", c, c);
+  return 0;
+}
+==
+
+tests==
+[["Simple exéécution", "", ""]]
+==
+
+
+
