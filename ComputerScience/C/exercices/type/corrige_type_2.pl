61d5dba25076cb2a5443bbd6b756f726c519c17c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-10 08:16:12 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/type/corrige_type_2.pl

---
+++ ComputerScience/C/exercices/type/corrige_type_2.pl
@@ -0,0 +1,70 @@
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#
+author=Nicolas Borie
+title=Programme manipulant des types
+tag=type
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+
+Le programme suivant comporte une erreur de type. Tentez de corriger cette erreur !
+
+==
+
+codebefore==
+
+#include <stdio.h>
+#include <stdlib.h>
+
+==
+
+editor.code==
+int max(int tab[], int size){
+  int i;
+  unsigned int max=0;
+
+  for(i=0 ; i<size ; i++){
+    if (tab[i] > max)
+      max = tab[i];
+  }
+  return max;
+} 
+==
+
+solution==
+int max(int tab[], int size){
+  int i;
+  int max=tab[0];
+
+  for(i=1 ; i<size ; i++){
+    if (tab[i] > max)
+      max = tab[i];
+  }
+  return max;
+}
+==
+
+codeafter==
+
+int main(int argc, char* argv[]){
+  int tab[100];
+  int size = 0;
+  int lu;
+
+  while (scanf("%d", &lu) == 1){
+    tab[size] = lu;
+    size++;
+  }
+
+  printf("Le maximum des entiers reçus ur l'entrée standard est %d\n", max(tab, size));
+
+  return 0;
+}
+==
+
+
+tests==
+[ ["simple éxécution", "","1 2 3 4 5"] ]
+==
+
+
