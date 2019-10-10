a0a4610fb1d6eb8c6430290eb0f1a3a14f7503bb
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-10 08:58:17 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/type/corrige_type_3.pl

---
+++ ComputerScience/C/exercices/type/corrige_type_3.pl
@@ -0,0 +1,52 @@
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#
+author=Nicolas Borie
+title=Programme manipulant des types
+tag=type
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+
+Le programme suivant comporte une erreur de type. Tentez de corriger cette erreur ! 
+
+==
+
+codebefore==
+
+#include <stdio.h>
+#include <stdlib.h>
+
+==
+
+editor.code==
+int multiplication(int a, int b, int c){
+  return a*b*c;
+}
+==
+
+solution==
+
+long int multiplication(int a, int b, int c){
+  return ((long int)a)*b*c;
+}
+==
+
+codeafter==
+
+int main(int argc, char* argv[]){
+  int a = atoi(argv[1]);
+  int b = atoi(argv[2]);
+  int c = atoi(argv[3]);
+
+  printf("Le produit %d x %d x %d vaut %ld, a, b, c, (long int)multiplication(a, b, c));
+  return 0;
+}
+==
+
+
+tests==
+[ ["simple éxécution", "1 1 1",""],
+  ["cinq au cube", "5 5 5",""] ]
+==
+
+
