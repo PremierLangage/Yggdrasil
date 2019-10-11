7e5521a8893563e6ddf09a8eb8a316eeae9e6c82
Isabelle NAVIZET <Isabelle.Navizet@u-pem.fr>
2019-10-11 16:37:07 +0200

    inavizet modified Yggdrasil/Chemistry/exercises/configuration-electronique/configelec1.pl

--- Chemistry/exercises/configuration-electronique/configelec1.pl
+++ Chemistry/exercises/configuration-electronique/configelec1.pl
@@ -17,7 +17,7 @@ before==
 import random
 
 table=[("Hydrogène, H 	",1 ),("Hélium   , He 	",2 ),("Lithium  , Li 	",3 ),("Berryllium,Be 	",4 ),("Bore 	  , B 	",5 ),("Carbone  , C 	",6 ),("Azote 	  , N 	",7 ),("Oxygène  , O 	",8 ),("Fluor 	  , F 	",9 ),("Néon 	  , Ne 	",10),("Sodium   , Na 	",11),("Magnésium, Mg 	",12),("Aluminum , Al 	",13),("Silicium , Si 	",14),("Phosphore, P 	",15),("Soufre   , S 	",16),("Chlore   , Cl 	",17),("Argon 	  , Ar 	",18),("Potassium, K 	",19),("Calcium  , Ca 	",20),("Scandium , Sc 	",21),("Titane   , Ti 	",22),("Vanadium , V 	",23),("Chrome   , Cr 	",24),("Manganèse, Mn 	",25),("Fer 	  , Fe 	",26),("Cobalt   , Co 	",27),("Nickel   , Ni 	",28),("Cuivre   , Cu 	",29),("Zinc 	  , Zn 	",30),("Gallium  , Ga 	",31),("Germanium, Ge 	",32),("Arsenic  , As 	",33),("Sélénium , Se 	",34),("Brome 	  , Br 	",35),("Krypton  , Kr 	",36)]
-name, a = random.choice(table)
+name, Z = random.choice(table)
 #random.randint(1,36)
 
 
@@ -44,8 +44,8 @@ success_feedback = """
 
 
 try: 
-    print("la bonne réponse est:",verif.strfromz(a),"votre response est :", response['txt_answer'].lower(), file=sys.stderr)
-    if verif.verif(response['txt_answer'].lower(),a) :
+    print("la bonne réponse est:",verif.strfromz(Z),"votre response est :", response['txt_answer'].lower(), file=sys.stderr)
+    if verif.verif(response['txt_answer'].lower(),Z) :
         grade = (100, success_feedback)
     else:
         grade = (0, failure_feedback)
