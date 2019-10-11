6e8f8e8fc4cd73f49eef3f2c9081fb8b64c2de8c
QUENTIN COUMES <qcoumes@etud.u-pem.fr>
2019-10-11 22:12:21 +0200

    qcoumes modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/tableau_entier.pl

--- ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/tableau_entier.pl
+++ ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/tableau_entier.pl
@@ -5,18 +5,18 @@ author = Quentin Coumes
 title = Tableaux d'Entiers
 
 text==
-On suppose qu'il existe une variables `n` de valeur entière `1 \le n \le 9`
+On suppose qu'il existe une variables `n` de valeur entière comprises entre 1 et 9.
 
 <br>
-Ecrire un programme qui qui construit une chaine de caractères représentant un tableau
+Écrivez un programme qui construit une chaine de caractères représentant un tableau
 des entiers de `1` à `n^2`.
 
 <br>
 Le tableau doit être formaté suivant ces règles :
 
-* Le tableau doit être carré.
-* Chaque colonne doit comprendre **deux caractères**, soit 2 chiffres ("35"),
-  soit 1 chiffre suivit d'un espace ("7&nbsp;").
+* Le tableau doit être carré ;
+* Chaque colonne doit comprendre **deux caractères**, soit 2 chiffres ("35") ;
+  soit 1 chiffre suivit d'un espace ("7&nbsp;" );
 * Les colonnes sont séparées par **un espaces**.
 
 **N'oubliez pas l'espace en fin de ligne si celle-ci se termine par un nombre à 1 chiffre.**
@@ -26,21 +26,21 @@ Le tableau doit être formaté suivant ces règles :
 Exemple :
 
 * pour `n = 3` :
-<pre style="margin-left: 25px"><code>
-1  2  3 
-4  5  6 
-7  8  9 
+<pre><code>
+        1  2  3 
+        4  5  6 
+        7  8  9 
 </code></pre>
 
 * pour `n = 7` :
-<pre style="margin-left: 25px"><code>
-1  2  3  4  5  6  7 
-8  9  10 11 12 13 14
-15 16 17 18 19 20 21
-22 23 24 25 26 27 28
-29 30 31 32 33 34 35
-36 37 38 39 40 41 42
-43 44 45 46 47 48 49
+<pre><code>
+        1  2  3  4  5  6  7 
+        8  9  10 11 12 13 14
+        15 16 17 18 19 20 21
+        22 23 24 25 26 27 28
+        29 30 31 32 33 34 35
+        36 37 38 39 40 41 42
+        43 44 45 46 47 48 49
 </code></pre>
 ==
 
