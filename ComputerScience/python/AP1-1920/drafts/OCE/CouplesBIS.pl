f02fd390f0313cec71aae88a7707b4a0f0dfa0d3
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-11 20:44:47 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/CouplesBIS.pl

---
+++ ComputerScience/python/AP1-1920/drafts/OCE/CouplesBIS.pl
@@ -0,0 +1,74 @@
+#author=OCE
+
+# Heritage d'un type d'exercice 
+extends = ../templates/generic/generic.pl
+
+title = Couples (X,Y)
+
+text==  
+Écrivez un script qui affiche tous les couples ( x , y ), x étant un entier positif compris entre 1 et p et y un entier positif 
+compris entre 1 et q ; <br>
+p et q sont deux entiers acquis au clavier. <br>
+L’affichage doit se faire comme sur l’exemple suivant, qui correspond à p = 3 et q = 5:
+
+( 1, 1 ) ( 1, 2 ) ( 1, 3 ) ( 1, 4 ) ( 1, 5 )<br>
+( 2, 1 ) ( 2, 2 ) ( 2, 3 ) ( 2, 4 ) ( 2, 5 )<br>
+( 3, 1 ) ( 3, 2 ) ( 3, 3 ) ( 3, 4 ) ( 3, 5 )<br>
+==
+
+grader== #|python|
+
+import random
+
+def couple(x,y):
+    p=int(input())
+    q=int(input())
+    chaine=""
+    ligne=""
+    for i in range(p):
+        for j in range(q):
+            chaine= "("+str(i+1)+","+str(j+1)+") "
+            ligne += chaine
+        print(ligne)
+        chaine=""
+        ligne=""
+
+begin_test_group("1 <= n <= 10")
+for i in range(1, 10):
+    set_title(f"n = {i}")
+    set_globals(n=i)
+    run()
+    assert_output(f"{fact(i)}\n")
+end_test_group()
+
+begin_test_group("Valeurs aléatoires")
+for i in range(3):
+    i = random.randint(30, 200)
+    set_title(f"n = {i}")
+    set_globals(n=i)
+    run()
+    assert_output(f"{fact(i)}\n")
+end_test_group()
+==
+
+soluce==
+p=int(input())
+q=int(input())
+chaine=""
+ligne=""
+for i in range(p):
+    for j in range(q):
+        chaine= "("+str(i+1)+","+str(j+1)+") "
+        ligne += chaine
+    print(ligne)
+    chaine=""
+    ligne=""
+==
+
+
+
+
+
+
+
+
