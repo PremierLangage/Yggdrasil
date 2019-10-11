03f5a999e13824f7e8f3bce9acdac7551e3d29dc
David DOYEN <David.Doyen@u-pem.fr>
2019-10-11 22:19:30 +0200

    ddoyen modified Yggdrasil/Languages/Greek/verb_inflexion.pl

---
+++ Languages/Greek/verb_inflexion.pl
@@ -0,0 +1,78 @@
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+@ grc_conj.json [grc_conj.json]
+@ grc_verbs.json [grc_verbs.json]
+
+title = Grec ancien
+
+
+before==
+import json,random
+
+with open('grc_conj.json','r',encoding='utf8') as f:
+    tplconj=json.load(f)
+    
+with open('grc_verbs.json','r',encoding='utf8') as f:
+    dataverb=json.load(f)
+
+
+name=random.choice(list(dataverb.keys()))
+
+def render(arg,data):
+    if isinstance(arg,dict):
+        return {k: render(v,data) for k, v in arg.items()}
+    elif isinstance(arg,str):
+        return arg.format(**data)
+    else:
+        return arg
+            
+conj={}
+data=dataverb[name]
+if 'pres' in data:
+    conj['pres']=render(tplconj['pres'],data['pres'])
+if 'imperfect' in data:
+    conj['imperfect']=render(tplconj['imperfect'],data['imperfect'])
+    
+time=random.choice(["pres","imperfect"])
+person=random.choice(["1S","2S","3S","1P","2P","3P"])
+==
+
+text ==
+Analyser la forme verbale suivante :
+
+{{formverb}}
+==
+
+form=
+
+evaluator==
+
+rightwords = ['πρεσβύτερος', 'νεώτερος']
+found = 0
+error = 0
+
+for e in selectable.selections:
+    if e['content'] in rightwords:
+        e['css'] = "success-state"
+        found += 1
+    else:
+        e['css'] = "error-state"
+        error += 1
+
+if found == 10 and error == 0:
+    score = 100
+    msg = '<span class="success-state">Bravo, vous avez trouvé tous les adjectifs comparatifs !</span>'
+else:
+    score = max([0, 10*(found - error)])
+    nb_error = (10 - found) + error
+    if nb_error > 1:
+        msg = '<span class="error-state">Il y a {} erreurs...</spam>'.format(nb_error)
+    else:
+        msg = '<span class="error-state">Il y a 1 seule erreur...</spam>'
+
+grade = (score, msg)
+==
+
+
+
