30c67b765810ade6e543be7e6ae87fc8b8623078
QUENTIN COUMES <qcoumes@etud.u-pem.fr>
2019-10-11 17:31:28 +0200

    qcoumes modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/factorielle.pl

--- ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/factorielle.pl
+++ ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/factorielle.pl
@@ -5,70 +5,67 @@ author = Quentin Coumes
 title = Tableau d'Entiers
 
 text==
-Ecrire un programme qui affiche un tableau des entiers de `0` à `99`
-avec dix entiers par lignes.
+On suppose qu'il existe une variables $%n%$ de valeur entière $%1 \le n \le 9%$
+
+<br>
+Ecrire un programme qui affiche un tableau des entiers de $%1%$ à $%n^2%$.
 
 <br>
 Le tableau doit être formaté suivant ces règles :
 
+* Le tableau doit être carré.
 * Chaque colonne doit comprendre **deux caractères**, soit 2 chiffres ("35"),
   soit 1 chiffre suivit d'un espace ("7&nbsp;").
 * Les colonnes sont séparées par **un espaces**.
+
+**N'oubliez pas l'espace en fin de ligne si celle-ci se termine par un nombre à 1 chiffre.**
 
 ---
 
 Exemple :
 
+* pour $%n = 3%$ :
 <pre><code>
-        0  1  2  3  4  5  6  7  8  9 
-        10 11 12 13 14 15 16 17 18 19
-        20 21 22 23 24 25 26 27 28 29
-        30 31 32 33 34 35 36 37 38 39
-        40 41 42 43 44 45 46 47 48 49
-        50 51 52 53 54 55 56 57 58 59
-        60 61 62 63 64 65 66 67 68 69
-        70 71 72 73 74 75 76 77 78 79
-        80 81 82 83 84 85 86 87 88 89
-        90 91 92 93 94 95 96 97 98 99
+        1  2  3
+        4  5  6
+        7  8  9
+</code></pre>
+
+* pour $%n = 7%$ :
+<pre><code>
+        1  2  3  4  5  6  7 
+        8  9  10 11 12 13 14
+        15 16 17 18 19 20 21
+        22 23 24 25 26 27 28
+        29 30 31 32 33 34 35
+        36 37 38 39 40 41 42
+        43 44 45 46 47 48 49
 </code></pre>
 ==
 
 grader== #|python|
-from itertools import permutations
 
-def table():
+def table(n):
     s = ""
-    for i in range(10):
+    for i in range(1, n + 1):
         s += str(i)
         if i > 9:
             s += " "
-        for j in range(1, 10):
-            
+        
+        for j in range(1, n + 1):
+            res = i*10 + j
+            s += " %d"
+            if res > 9:
+                s += " "
 
     return s
 
-begin_test_group("Tableaux carrés")
+begin_test_group("Tableaux d'Entiers")
 for i in range(1, 10):
-    set_title(f"col = lin = {i}")
-    set_globals(col=i, lin=i)
+    set_title(f"n = {i}")
+    set_globals(n=i)
     run()
-    assert_output(mult_table(i, i) + "\n")
-end_test_group()
-
-begin_test_group("Tout les Tableaux")
-for i in range(1, 10):
-    for j in range (1, 10):
-        if i == j:
-            continue
-        set_title(f"col {i}, lin = {j}")
-        set_globals(col=i, lin=j)
-        run()
-        assert_output(mult_table(i, j) + "\n")
-
-        set_title(f"col {j}, lin = {i}")
-        set_globals(col=j, lin=i)
-        run()
-        assert_output(mult_table(j, i) + "\n")
+    assert_output(table(i) + "\n")
 end_test_group()
 ==
 
