664d3abdac8dc1ea0b50f1cbf87b2a8151382897
QUENTIN COUMES <qcoumes@etud.u-pem.fr>
2019-10-11 17:00:25 +0200

    qcoumes modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/table_multiplication.pl

--- ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/table_multiplication.pl
+++ ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/table_multiplication.pl
@@ -8,19 +8,23 @@ et $%1 \le lin \le 9%$.
 <br>
 Ecrire un programme qui affiche un tableau des multiplications de 1 à $%col%$ par 1 à $%lin%$.
 <br>
-Le tableau doit respecter plusieurs règles:
+Le tableau doit être formaté suivant ces règles :
 
 * Chaque colonne doit comprendre *deux caractères*, soit 2 chiffres (`35`), soit 1 chiffre suivit d'un espace (`4 `).
 * Les colonnes sont séparées par *deux espaces*.
 * L'entête de colonne est séparée du tableau par des tirets `-`.
 * L'entête de ligne est séparée du tableau par des barres verticales `|` (`ALT GR` + `6`).
+* Afin de faciliter la création des boucles, chaque ligne doit se terminer par un espace,
+  en plus de l'espace optionnel de la colonne (excepté le séparateur d'entête de ligne).
+
+---
 
 Exemples :
 
 * pour $%col = 3%$ et $%lin = 2%$:
 <pre><code>
         *  |  1   2   3 
-        ----------------
+        -----------------
         1  |  1   2   3 
         2  |  2   4   6 
 </code></pre>
@@ -28,16 +32,42 @@ Exemples :
 * pour $%col = 9%$ et $%lin = 9%$:
 <pre><code>
         *  |  1   2   3   4   5   6   7   8   9 
-        ----------------------------------------
+        -----------------------------------------
         1  |  1   2   3   4   5   6   7   8   9 
-        2  |  2   4   6   8   10  12  14  16  18
-        3  |  3   6   9   12  15  18  21  24  27
-        4  |  4   8   12  16  20  24  28  32  36
-        5  |  5   10  15  20  25  30  35  40  45
-        6  |  6   12  18  24  30  36  42  48  54
-        7  |  7   14  21  28  35  42  49  56  63
-        8  |  8   16  24  32  40  48  56  64  72
-        9  |  9   18  27  36  45  54  63  72  81
+        2  |  2   4   6   8   10  12  14  16  18 
+        3  |  3   6   9   12  15  18  21  24  27 
+        4  |  4   8   12  16  20  24  28  32  36 
+        5  |  5   10  15  20  25  30  35  40  45 
+        6  |  6   12  18  24  30  36  42  48  54 
+        7  |  7   14  21  28  35  42  49  56  63 
+        8  |  8   16  24  32  40  48  56  64  72 
+        9  |  9   18  27  36  45  54  63  72  81 
+</code></pre>
+
+
+Pour mieux voir le formatage demandé, voici les exemples précédenst avec les espaces remplacés par des points:
+
+* pour $%col = 3%$ et $%lin = 2%$:
+<pre><code>
+        *..|..1...2...3..
+        -----------------
+        1..|..1...2...3..
+        2..|..2...4...6..
+</code></pre>
+
+* pour $%col = 9%$ et $%lin = 9%$:
+<pre><code>
+        *..|..1...2...3...4...5...6...7...8...9..
+        -----------------------------------------
+        1..|..1...2...3...4...5...6...7...8...9..
+        2..|..2...4...6...8...10..12..14..16..18.
+        3..|..3...6...9...12..15..18..21..24..27.
+        4..|..4...8...12..16..20..24..28..32..36.
+        5..|..5...10..15..20..25..30..35..40..45.
+        6..|..6...12..18..24..30..36..42..48..54.
+        7..|..7...14..21..28..35..42..49..56..63.
+        8..|..8...16..24..32..40..48..56..64..72.
+        9..|..9...18..27..36..45..54..63..72..81.
 </code></pre>
 ==
 
@@ -48,7 +78,7 @@ def mult_table(col, lin):
     s = "*  | "
     for i in range(1, col + 1):
         s += " %d  " % i
-    s += "\n----" + "----" * col + "\n"
+    s += "\n-----" + "----" * col + "\n"
 
     for i in range(1, col + 1):
         s += "%d  | " % i
@@ -64,15 +94,24 @@ def mult_table(col, lin):
 
 begin_test_group("Tableaux carrés")
 for i in range(1, 10):
-    # Donne un titre à la prochaine exécution
     set_title(f"col = lin = {i}")
-    # Fixe les variables globales disponibles pour les prochaines exécution
     set_globals(col=i, lin=i)
-    # Lance l'exécution du programme dans les conditions spécifiées
     run()
-    # Vérifie la sortie standard obtenue
     assert_output(mult_table(i, i) + "\n")
-# Clôt le précédent groupe de tests
+end_test_group()
+
+begin_test_group("Tout les Tableaux")
+for i in range(1, 10):
+    for j in range (1, 10):
+        set_title(f"col {i}, lin = {j}")
+        set_globals(col=i, lin=j)
+        run()
+        assert_output(mult_table(i, j) + "\n")
+
+        set_title(f"col {j}, lin = {i}")
+        set_globals(col=j, lin=i)
+        run()
+        assert_output(mult_table(j, i) + "\n")
 end_test_group()
 ==
 
