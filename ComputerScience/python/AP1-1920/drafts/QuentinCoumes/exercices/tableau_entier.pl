9ed2f16140ba28e00addc6f57b2cc571e51dad2b
QUENTIN COUMES <qcoumes@etud.u-pem.fr>
2019-10-11 22:07:22 +0200

    qcoumes modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/tableau_entier.pl

--- ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/tableau_entier.pl
+++ ComputerScience/python/AP1-1920/drafts/QuentinCoumes/exercices/tableau_entier.pl
@@ -26,21 +26,21 @@ Le tableau doit être formaté suivant ces règles :
 Exemple :
 
 * pour `n = 3` :
-<pre><code style="right-margin: "50px">
+<pre style="margin-left: 25px"><code>
 1  2  3 
 4  5  6 
 7  8  9 
 </code></pre>
 
 * pour `n = 7` :
-<pre><code>
-        1  2  3  4  5  6  7 
-        8  9  10 11 12 13 14
-        15 16 17 18 19 20 21
-        22 23 24 25 26 27 28
-        29 30 31 32 33 34 35
-        36 37 38 39 40 41 42
-        43 44 45 46 47 48 49
+<pre style="margin-left: 25px"><code>
+1  2  3  4  5  6  7 
+8  9  10 11 12 13 14
+15 16 17 18 19 20 21
+22 23 24 25 26 27 28
+29 30 31 32 33 34 35
+36 37 38 39 40 41 42
+43 44 45 46 47 48 49
 </code></pre>
 ==
 
