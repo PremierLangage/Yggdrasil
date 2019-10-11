468435cd2f851d826ed92e8279b55d41b3bce6f9
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-11 15:04:17 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Triangle_Pascal.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Triangle_Pascal.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Triangle_Pascal.pl
@@ -1,23 +1,15 @@
 # author = Olivier Champalle
 extends = /ComputerScience/python/AP1-1920/templates/generic/generic.pl
 
-title = Boucle qui compte de 0 à `nb - 1`
+title = Triangle de Pascal
 
 text==
-On suppose qu'il existe une variables `nb` désignant un entier strictement positif. 
-Écrire un programme dont l'exécution affiche sur la sortie standard les nombres de 
-`0` à `nb-1` (chacun suivi d'un retour à la ligne), à l'exclusion de tout autre caractère.
-
-Par exemple, si `nb` vaut 5, votre programme doit afficher :
-
-    0
-    1
-    2
-    3
-    4
-
-**Attention**, *le programe doit fonctionner quelle que soit la valeur 
-de `nb`.*<br>
+On souhaite afficher le fameux triangle de Pascal (cf. Triangle de Pascal) donnant la représentation
+des coefficients binomiaux (cf. Coefficient binomial), couramment utilisé en probabilité pour connaître
+le nombre de façons de choisir p éléments parmi un lot de n éléments sans prise en compte de l’ordre.
+<br>
+Créez un script trianglePascal.py qui initialise une variable N à la valeur 10 et affiche le triangle
+de nombres allant de 0 à N (inclus) suivant. On utilisera deux boucles for judicieusement imbriquées.
 ==
 
 editor.theme=light
