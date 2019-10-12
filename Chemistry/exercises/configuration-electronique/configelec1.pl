e6af6bd16563a83dc1fe08c721f10b1b01de7777
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-12 18:06:13 +0200

    drevuz modified Yggdrasil/Chemistry/exercises/configuration-electronique/configelec1.pl

--- Chemistry/exercises/configuration-electronique/configelec1.pl
+++ Chemistry/exercises/configuration-electronique/configelec1.pl
@@ -16,8 +16,18 @@ text="Quelle est la configuration éléctronique de l'élément {{name}} ? Pour 
 before==
 import random
 
-table=[("Hydrogène, H 	",1 ),("Hélium   , He 	",2 ),("Lithium  , Li 	",3 ),("Berryllium,Be 	",4 ),("Bore 	  , B 	",5 ),("Carbone  , C 	",6 ),("Azote 	  , N 	",7 ),("Oxygène  , O 	",8 ),("Fluor 	  , F 	",9 ),("Néon 	  , Ne 	",10),("Sodium   , Na 	",11),("Magnésium, Mg 	",12),("Aluminum , Al 	",13),("Silicium , Si 	",14),("Phosphore, P 	",15),("Soufre   , S 	",16),("Chlore   , Cl 	",17),("Argon 	  , Ar 	",18),("Potassium, K 	",19),("Calcium  , Ca 	",20),("Scandium , Sc 	",21),("Titane   , Ti 	",22),("Vanadium , V 	",23),("Chrome   , Cr 	",24),("Manganèse, Mn 	",25),("Fer 	  , Fe 	",26),("Cobalt   , Co 	",27),("Nickel   , Ni 	",28),("Cuivre   , Cu 	",29),("Zinc 	  , Zn 	",30),("Gallium  , Ga 	",31),("Germanium, Ge 	",32),("Arsenic  , As 	",33),("Sélénium , Se 	",34),("Brome 	  , Br 	",35),("Krypton  , Kr 	",36)]
-#table=[("cuivre, Cu 	",29 ),("Chrome   , Cr 	",24 )]
+table=[("Hydrogène, H 	",1 ),("Hélium   , He 	",2 ),
+    ("Lithium  , Li 	",3 ),("Berryllium,Be 	",4 ),("Bore 	  , B 	",5 ),
+    ("Carbone, C 	",6 ),("Azote 	  , N 	",7 ),("Oxygène  , O 	",8 ),
+    ("Fluor 	  , F 	",9 ),("Néon 	  , Ne 	",10),("Sodium   , Na 	",11),
+    ("Magnésium, Mg 	",12),("Aluminum , Al 	",13),("Silicium , Si 	",14),
+    ("Phosphore, P 	",15),("Soufre   , S 	",16),("Chlore   , Cl 	",17),("Argon 	  , Ar 	",18),
+    ("Potassium, K 	",19),("Calcium  , Ca 	",20),("Scandium , Sc 	",21),("Titane   , Ti 	",22),
+    ("Vanadium , V 	",23),("Chrome   , Cr 	",24),("Manganèse, Mn 	",25),("Fer 	  , Fe 	",26),
+    ("Cobalt   , Co 	",27),("Nickel   , Ni 	",28),("Cuivre   , Cu 	",29),("Zinc 	  , Zn 	",30),
+    ("Gallium  , Ga 	",31),("Germanium, Ge 	",32),("Arsenic  , As 	",33),("Sélénium , Se 	",34),
+    ("Brome 	  , Br 	",35),("Krypton  , Kr 	",36)]
+table=[("Kryptonite, Cu 	",54 ),("Chrome   , Cr 	",24 )]
 name, Z = random.choice(table)
 #random.randint(1,36)
 
