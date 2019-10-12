29f2b380cc1d11560d3d30663081de742d7fd916
David DOYEN <David.Doyen@u-pem.fr>
2019-10-12 15:34:10 +0200

    ddoyen modified Yggdrasil/utils/components.py

---
+++ utils/components.py
@@ -0,0 +1,16 @@
+
+
+def checkradio(radio,idsol)
+    S=radio.selection
+    for item in radio.items:
+        if item['id'] == S:
+            if S == idsol:
+                item['css'] = 'success-state'
+                return ("Ok",100)
+            else:
+                item['css'] = 'error-state'
+        elif item['id'] == idsol:
+            item['css'] = 'success-state'
+    return ("Error",0)
+    
+
