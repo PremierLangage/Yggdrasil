800eccc0565cdeb5b96f6aee8b244926da5aa64f
David DOYEN <David.Doyen@u-pem.fr>
2019-10-12 12:00:53 +0200

    ddoyen modified Yggdrasil/Languages/Greek/verb_inflexion.pl

--- Languages/Greek/verb_inflexion.pl
+++ Languages/Greek/verb_inflexion.pl
@@ -70,8 +70,8 @@ Analyser la forme verbale suivante : {{formverb}}
 
 form==
 <table>
-  <tr style='vertical-align: top;padding:1em;'>
-    <td style=""> {{ radio_prs | component }} </td>
+  <tr style='vertical-align: top'>
+    <td style="padding-right:1em;"> {{ radio_prs | component }} </td>
     <td> {{ radio_tense | component }} </td>
     <td> {{ radio_voice | component }} </td>
     <td> {{ radio_mood | component }} </td>
