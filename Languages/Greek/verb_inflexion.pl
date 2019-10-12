f3bfdc44cd4382b202d0780ed9097b140c4529a7
David DOYEN <David.Doyen@u-pem.fr>
2019-10-12 16:57:02 +0200

    ddoyen modified Yggdrasil/Languages/Greek/verb_inflexion.pl

--- Languages/Greek/verb_inflexion.pl
+++ Languages/Greek/verb_inflexion.pl
@@ -62,7 +62,7 @@ if 'imperfect' in data:
 
 voice=random.choice(["A","MP"])
 mood=random.choice(["I"])
-tense=random.choice(["pres","imperfect"])
+tense=random.choice(["pres","imperfect","future"])
 person=random.choice(["1S","2S","3S","1P","2P","3P"])
 formverb=conj[tense][voice]['I'][person]
 ==
