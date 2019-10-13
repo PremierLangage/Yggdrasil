ff65d7db1bc1aadcf19c04d6c3e4fa7447dc4083
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-13 20:41:13 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/string/count.pl

--- ComputerScience/C/exercices/string/count.pl
+++ ComputerScience/C/exercices/string/count.pl
@@ -66,9 +66,11 @@ int main(int argc, char* argv[]){
 ==
 
 tests==
-[ ["pas d'occurences", "anticonstitutionnellement b", ""], 
-  ["Que la lettre", "ooooooooo o", ""], 
-  ["une occurence", "xxxlhkjhkdqkshdksqjdhlkXqkjhdshlddhqslk X", ""] ] 
+[ ["premier test", "bonjour o", ""]
+  ["pas d'occurences", "anticonstitutionnellement b", ""], 
+  ["que la lettre", "ooooooooo o", ""], 
+  ["une occurence", "xxxlhkjhkdqkshdksqjdhlkXqkjhdshlddhqslk X", ""] 
+  ["aléatoire", "".join([chr(random.randint(97, 122)) for i in range(50)]) + " " + chr(random.randint(97, 122)), ""] ] 
 ==
 
 
