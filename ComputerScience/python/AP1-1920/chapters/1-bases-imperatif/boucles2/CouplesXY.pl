878d3b91bf10e0322d54dfa084d2a195fad8aef6
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-13 18:47:14 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl
@@ -17,11 +17,12 @@ text==
 Écrivez un script qui affiche tous les couples ( x , y ), x étant un entier positif compris entre 1 et p et y un entier positif 
 compris entre 1 et q ; <br>
 p et q sont deux entiers acquis au clavier. <br>
-L’affichage doit se faire comme sur l’exemple suivant avec un un espace entre chaque couple (X,Y), qui correspond à p = 3 et q = 5:
+L’affichage doit se faire avec un espace entre chaque couple comme sur l’exemple suivant qui correspond à p = 3 et q = 5 :
 
 ( 1, 1 ) ( 1, 2 ) ( 1, 3 ) ( 1, 4 ) ( 1, 5 )<br>
 ( 2, 1 ) ( 2, 2 ) ( 2, 3 ) ( 2, 4 ) ( 2, 5 )<br>
 ( 3, 1 ) ( 3, 2 ) ( 3, 3 ) ( 3, 4 ) ( 3, 5 )<br>
+
 ==
 
 soluce==
