76eb8b01b08dcb5e53dbaf547f0c7a718c46f66a
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-14 09:55:02 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Diviseurs.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Diviseurs.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Diviseurs.pl
@@ -19,10 +19,29 @@ Ecrire un programme qui : <br>
 * rechercher tous les nombres parfaits inférieurs à n <br>
 * On rappelle qu'un nombre est parfait si la somme de ses diviseurs est égale au double du nombre. <br>
   Par exemple, 6 a pour diviseurs 1, 2, 3 et 6. Leur somme fait 12 = 6 * 2 <br>
+
 ==
 
 soluce==
+entier = int(input())
+chaine=""
+somme=0
 
+for i in range(entier):
+    if entier % (i+1) == 0 :
+        if len(chaine) > 0:
+            chaine +=(", ")
+        chaine += (str(i+1))
+        somme+=(i+1)
+
+print(chaine)
+
+if somme == entier * 2:
+    print("il s'agit d'un nombre parfait")
+    print("En voici la preuve : ")
+    print("Total des diviseurs : " + str(somme) + " = " + str(entier) + " * 2")
+else:
+    print("Il ne s'agit pas d'un nombre entier puisque la somme des diviseurs n'est pas le double du nombre")
 ==
 
 mplsoluce0==
