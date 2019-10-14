eaef537b639dcd8dd8887d10ebf38b19461121eb
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-14 07:56:54 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl
@@ -18,10 +18,9 @@ text==
 compris entre 1 et q ; <br>
 p et q sont deux entiers acquis au clavier. <br>
 L’affichage doit se faire avec un espace entre chaque couple comme sur l’exemple suivant qui correspond à p = 3 et q = 5 :
-
-( 1, 1 ) ( 1, 2 ) ( 1, 3 ) ( 1, 4 ) ( 1, 5 )<br>
-( 2, 1 ) ( 2, 2 ) ( 2, 3 ) ( 2, 4 ) ( 2, 5 )<br>
-( 3, 1 ) ( 3, 2 ) ( 3, 3 ) ( 3, 4 ) ( 3, 5 )<br>
+(1,1) (1,2) (1,3) (1,4) (1,5) <br>
+(2,1) (2,2) (2,3) (2,4) (2,5) <br>
+(3,1) (3,2) (3,3) (3,4) (3,5) <br>
 
 ==
 
@@ -55,6 +54,7 @@ nul
 0
 0
 ==
+
 
 
 
