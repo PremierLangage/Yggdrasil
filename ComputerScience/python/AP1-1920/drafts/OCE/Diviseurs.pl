bbbfa1dd5527dbde2edc94e6ab5bf18b1852dbed
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-14 09:53:43 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Diviseurs.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Diviseurs.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Diviseurs.pl
@@ -14,11 +14,11 @@ q=int(input())
 taboo=while
 
 text==  
-Ecrire un programme qui : 
-* affiche tous les diviseurs d'un entier n saisi par l'utilisateur
-* rechercher tous les nombres parfaits inférieurs à n
-* On rappelle qu'un nombre est parfait si la somme de ses diviseurs est égale au double du nombre.
-  Par exemple, 6 a pour diviseurs 1, 2, 3 et 6. Leur somme fait 12 = 6 * 2
+Ecrire un programme qui : <br>
+* affiche tous les diviseurs d'un entier n saisi par l'utilisateur <br>
+* rechercher tous les nombres parfaits inférieurs à n <br>
+* On rappelle qu'un nombre est parfait si la somme de ses diviseurs est égale au double du nombre. <br>
+  Par exemple, 6 a pour diviseurs 1, 2, 3 et 6. Leur somme fait 12 = 6 * 2 <br>
 ==
 
 soluce==
