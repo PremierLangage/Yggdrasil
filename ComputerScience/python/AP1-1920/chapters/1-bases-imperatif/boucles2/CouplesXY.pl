f674e4b1fe28eac0bc76d55c6642a3dc5f06aefc
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-14 11:15:54 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles2/CouplesXY.pl
@@ -6,21 +6,22 @@ extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 title = Couples (X,Y)
 
 code==
-p=int(input())
-q=int(input())
+li=int(input("nombre  de lignes"))
+col=int(input("nombre de colonnes"))
 
 ==
 
 taboo=while
 
 text==  
-Écrivez un script qui affiche tous les couples ( x , y ), x étant un entier positif compris entre 1 et p et y un entier positif 
-compris entre 1 et q ; <br>
-p et q sont deux entiers acquis au clavier. <br>
-L’affichage doit se faire avec un espace entre chaque couple comme sur l’exemple suivant qui correspond à p = 3 et q = 5 :
-(1,1) (1,2) (1,3) (1,4) (1,5) <br>
-(2,1) (2,2) (2,3) (2,4) (2,5) <br>
-(3,1) (3,2) (3,3) (3,4) (3,5) <br>
+Écrivez un script qui affiche tous les couples (x,y), x étant un entier positif compris entre 0 et li et y un entier positif 
+compris entre 0 et col ; <br>
+li et col sont deux entiers acquis au clavier. <br>
+L’affichage doit se faire avec un espace entre chaque couple comme sur l’exemple suivant qui correspond à li = 3 et col = 5 :
+(0,0) (0,1) (0,2) (0,3) (1,4) <br>
+(1,0) (1,1) (1,2) (1,3) (1,4) <br>
+(2,0) (2,1) (2,2) (2,3) (2,4) <br>
+
 
 ==
 
@@ -30,7 +31,7 @@ q=int(input())
 for i in range(p):
     ligne=""
     for j in range(q):
-        ligne += "("+str(i+1)+","+str(j+1)+") "
+        ligne += "("+str(i)+","+str(j)+") "
     print(ligne)
 ==
 
@@ -54,6 +55,7 @@ nul
 0
 0
 ==
+
 
 
 
