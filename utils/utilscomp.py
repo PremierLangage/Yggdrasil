40738633cfefbde2709c9191fb07069d5f8b3ab8
David DOYEN <David.Doyen@u-pem.fr>
2019-10-15 21:43:52 +0200

    ddoyen modified Yggdrasil/utils/utilscomp.py

---
+++ utils/utilscomp.py
@@ -0,0 +1,40 @@
+import random as rd
+
+def checkradio(radio,idsol):
+    S=radio.selection
+    for item in radio.items:
+        if item['id'] == S:
+            if S == idsol:
+                item['css'] = 'success-state'
+                return (100,"")
+            else:
+                item['css'] = 'error-state'
+        elif item['id'] == idsol:
+            item['css'] = 'success-state'
+    return (0,"")
+
+# MatchList
+
+def MatchList_loadContent(match,source,target):
+    match.nodes = []
+    expected = []
+    for i in range(len(items)):
+        sourceId = "source" + str(i)
+        targetId = "target" + str(i)
+
+        match.nodes.append({
+            "id": sourceId,
+            "content": items[i][ct],
+            "source": True,
+        })
+
+        match.nodes.append({
+            "id": targetId,
+            "content": items[i][cs],
+            "target": True,
+        })
+        expected.append({ "source": sourceId, "target": targetId })
+    rd.shuffle(match.nodes)
+    
+
+
