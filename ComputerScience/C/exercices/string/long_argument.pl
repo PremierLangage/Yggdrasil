5df9964406829e760f9341460fb4cf7bf79b58c6
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-15 08:13:15 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/string/long_argument.pl

--- ComputerScience/C/exercices/string/long_argument.pl
+++ ComputerScience/C/exercices/string/long_argument.pl
@@ -11,8 +11,7 @@ text==
 
 Écrire un programme C qui identifie et affiche son argument le plus long. 
 Une fois ce dernier identifié, il suffit de l'afficher sur la sortie standard 
-et d'aller à la ligne.
-
+**sans oublier d'aller à la ligne après l'affichage**.
 
 ==
 
@@ -26,32 +25,22 @@ int main(...){
 ==
 
 solution==
+
 #include <stdio.h>
 #include <string.h>
 
 int main(int argc, char* argv[]){
-  int i, len_max=0, ind_max=0, current_len;
+  int i, ind_max=1;
 
-  if (argc <= 1)
+  if(argc == 1)
     return 0;
 
-  for (i=1 ; i<argc ; i++){
-    current_len = strlen(argv[i]);
-    if (current_len > len_max){
-      len_max = current_len;
-      ind_max = i;
-    }
+  for(i=2 ; i<argc ; i++){
+    if(strlen(argv[i]) > strlen(argv[ind_max]))
+      ind_max = 1;
   }
 
-  for (i=0 ; i<len_max+4 ; i++)
-    printf("#");
-  printf("\n");
-
-  printf("# %s #\n", argv[ind_max]);
-
-  for (i=0 ; i<len_max+4 ; i++)
-    printf("#");
-  printf("\n");
+  printf("%s\n", argv[ind_max]);
     
   return 0;
 }
