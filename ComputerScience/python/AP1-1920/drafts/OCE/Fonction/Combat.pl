8363a919d1a2a2a6013b2b11a30b6adec4b8c0c2
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-16 00:54:13 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Combat.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Combat.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Combat.pl
@@ -24,9 +24,9 @@ Ce sont en effet des choses qui arrivent quand vous rencontrez un enemi avec leq
 Ecrivez une fonction "combat" qui reçoit en paramétre les "endurances" et les "habilités" des deux protagonistes et organise le combat de la manière suivante :<br>
 <br>
 1 en fonction de leur habilité, les deux adversaires lancent les dés :<br>
-* 1 dés pour une habilité inférieur ou égale à 10
-* 2 dés pour une habilité supérieure 10 et inférieur stricte à 18
-* 3 dés 3 au delà d'une habilité de 17
+* 1 dés pour une habilité inférieur ou égale à 10 <br>
+* 2 dés pour une habilité supérieure 10 et inférieur stricte à 18 <br>
+* 3 dés 3 au delà d'une habilité de 17 <br>
 2 on compare le résultat, <br>
 3 la différence des deux lancés est soustrait au points d'endurance de l'adversaire dont le lancé est inférieur, <br>
 4 en cas d'égalité on recommence les lancez de dés, <br>
