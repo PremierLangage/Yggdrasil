94aa596ef225936084bae8945cadaf38fad26550
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-16 13:41:10 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/TriangleChaine.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/TriangleChaine.pl
@@ -0,0 +1,77 @@
+
+
+
+# author zip
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title =triangle chaine
+text==  
+modifier la fonction de l'exercice TriangleEtoile pour que l'uilisateur choisissent lachaie à répéter
+
+
+Exemples
+ 
+Entrez un entier : 5 <br>
+Entrez un chaine : *
+*<br>
+*+**<br>
+*+**+***<br>
+*+**+***+****<br>
+*+**+***+****+*****<br>
+*+**+***+****<br>
+*+**+***<br>
+*+**<br>
+*<br>
+
+Entrez un entier : 3 <br>
+Entrez une chaine : to <br>
+to <br>
+to+toto <br>
+to+toto+tototo <br>
+to+toto <br>
+to <br>
+
+==
+before==
+import random
+
+mplsoluce2="Test aléatoire\n"
+n=random.randint(4,7)
+mplsoluce2 += str(n)+"\n"
+==
+
+soluce==
+
+def desEtoiles(n):
+    s=""
+    if n>0:
+        s="*"
+        etoile="*"
+        if n>1:
+            for i in range(n-1):
+                etoile+="*"
+                s=s+"+"+etoile
+        
+    return s
+
+n=int(input())
+for i in range(1,n+1):
+    print(desEtoiles(i))
+for i in range(n-1):
+    print(desEtoiles(n-1-i))
+==
+mplsoluce0==
+classique
+3
+==
+mplsoluce1==
+negatif
+-9
+==
+
+
+
+
+
+
