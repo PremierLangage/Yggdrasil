a393f6ce092594e2207cf5be61b047c26d32a09e
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-16 13:22:40 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/10_mais_pas_3.pl

---
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/10_mais_pas_3.pl
@@ -0,0 +1,56 @@
+
+#author=OCE
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Calculer le carré d'un nombre
+
+code==
+#
+#
+#
+n = ??(input("Saisisez un entier :"))
+#
+
+==
+
+text==  
+
+Programmer une fonction "carre" qui prend en paramètre un entier et retourne son carré.<br>
+
+Ecrivez ensuite un code qui demande à l'utilisateur un entier, appel la fonction et imprime le résultat.
+
+==
+
+soluce==#|python|
+def carre(n):
+    return(n*n)
+
+n = int(input("Saisisez un entier :"))
+print(carre(n))
+==
+
+mplsoluce0==
+paire
+2
+==
+mplsoluce1==
+impaire 
+3
+==
+
+mplsoluce2==
+Grand 
+10
+==
+
+mplsoluce3==
+nul
+0
+==
+
+
+
+
+
