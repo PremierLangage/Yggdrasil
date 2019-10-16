0343f5d53e05bb4342430edfa10a828982f6475a
David DOYEN <David.Doyen@u-pem.fr>
2019-10-16 22:33:54 +0200

    ddoyen modified Yggdrasil/utils/components.py

--- utils/components.py
+++ utils/components.py
@@ -1,4 +1,5 @@
-import random as rd
+import uuid
+from components import Component
 
 def checkradio(radio,idsol):
     S=radio.selection
@@ -15,27 +16,70 @@ def checkradio(radio,idsol):
 
 # MatchList
 
+class MatchList(Component):
+    def __init__(self, **kwargs):
+        self.selector = 'c-match-list'
+        self.decorator = 'CustomMatchList'
+        self.nodes = []
+        super().__init__(**kwargs)
+
+    def loadContent(self,source,target):
+        self.nodes = []
+        expected = []
+        for i in range(len(source)):
+            sourceId = "source" + str(i)
+            targetId = "target" + str(i)
+
+            self.nodes.append({
+                "id": sourceId,
+                "content": source[i],
+                "source": True,
+            })
+
+            self.nodes.append({
+                "id": targetId,
+                "content": target[i],
+                "target": True,
+            })
+            expected.append({ "source": sourceId, "target": targetId })
+        rd.shuffle(self.nodes)
+    
+
 def MatchList_loadContent(match,source,target):
     match.nodes = []
     expected = []
-    for i in range(len(items)):
+    for i in range(len(source)):
         sourceId = "source" + str(i)
         targetId = "target" + str(i)
 
         match.nodes.append({
             "id": sourceId,
-            "content": items[i][ct],
+            "content": source[i],
             "source": True,
         })
 
         match.nodes.append({
             "id": targetId,
-            "content": items[i][cs],
+            "content": target[i],
             "target": True,
         })
         expected.append({ "source": sourceId, "target": targetId })
-
     rd.shuffle(match.nodes)
     
+# RadioGroup
+
+def RadioGroup_loadContent(radio,content):
+    radio.items=([{"id":str(id),"content":str(item)} for id,item in enumerate(content)])
+
+def RadioGroup_evalByContent(radio,sol):
+    return 100
+    for item in radio.items:
+        if item['content']==sol:
+            item['css'] = 'success-state anim-fade'
+            if item['id']==radio.selection:
+                return 100
+    return 0
+
+
 
 
