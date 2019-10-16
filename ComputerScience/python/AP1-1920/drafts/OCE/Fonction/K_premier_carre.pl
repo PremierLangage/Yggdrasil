0a671d88fcd2262c602f72eb2698e50a001067b5
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-16 10:25:12 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/K_premier_carre.pl

---
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/K_premier_carre.pl
@@ -0,0 +1,59 @@
+
+#author=OCE
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Calculer le carré d'un nombre
+
+code==
+n= int(input("Entrez un nombre : "))
+#
+#
+#
+#
+#
+print(??)
+
+==
+
+text==  
+
+Programmer une fonction "carre" qui prend en paramètre un entier et retourne son carré.<br>
+
+Ecrivez ensuite un code qui demande à l'utilisateur un entier, appel la fonction et imprime le résultat.
+
+==
+
+soluce==#|python|
+def carre(n):
+    return(n*n)
+
+n = int(input("Saisisez un entier :"))
+print(carre(n))
+==
+
+mplsoluce0==
+paire
+2
+==
+mplsoluce1==
+impaire 
+3
+==
+
+mplsoluce2==
+Grand 
+10
+==
+
+mplsoluce3==
+nul
+0
+0
+==
+
+
+
+
+
