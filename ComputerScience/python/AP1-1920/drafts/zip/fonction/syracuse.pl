231a346b5a31dc0a4d66e0f3ac521162e290641b
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-16 08:53:18 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/syracuse.pl

--- ComputerScience/python/AP1-1920/drafts/zip/fonction/syracuse.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/syracuse.pl
@@ -4,7 +4,6 @@
 # Heritage d'un type d'exercice 
 extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
-title =Suire de Syracuse
 text==  
 La suite de Syracuse est une suite d'entiers définie par :
 
