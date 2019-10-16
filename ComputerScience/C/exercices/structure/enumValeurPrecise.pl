9ef76eeac822ab9de1f4028b6346342cdb479fb1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-16 11:06:38 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/structure/enumValeurPrecise.pl

--- ComputerScience/C/exercices/structure/enumValeurPrecise.pl
+++ ComputerScience/C/exercices/structure/enumValeurPrecise.pl
@@ -4,12 +4,12 @@
 # Déclaration d'une enumaration avec des valeur précises
 
 author=Elise Hardy
-title=Enumeration avec des valeurs précises
+title=Énumeration avec des valeurs précises
 tag=structure
 extends=/ComputerScience/C/template/stdsandboxC.pl
 
 text==
-Déclarer une enumeration *Note* contenant *Mathieu, Cassandre, Laura, Thomas, 
+Déclarez une énumeration *Note* contenant *Mathieu, Cassandre, Laura, Thomas, 
 Aurelie, Adrien* dont les valeurs respectifs sont *8, 9, 13, 15, 16, 19*
 Par exemple si on affiche la valeur de Adrien l'affichage sera 19.
 ==
@@ -20,7 +20,9 @@ typedef ...
 ==
 
 solution==
-typedef enum {Mathieu =8, Cassandre, Laura =13, Thomas=15, Aurelie, Adrien=19}Note;
+
+typedef enum {Mathieu =8, Cassandre, Laura =13, Thomas=15, Aurelie, Adrien=19} Note;
+
 ==
 
 codeafter==
@@ -37,10 +39,7 @@ int main(){
 
 ==
 
-grader==
-from graderC import graderII
- 
-tests = [["Déclaration et utilisation 1", "",""]] 
-graderII(tests)
+tests==
+[ ["Déclaration et utilisation", "", ""] ] 
 ==
 
