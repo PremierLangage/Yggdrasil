e555fe4582bf4d7290f2cfcc6ff5a96a94944d3a
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-16 10:55:14 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/K_premier_carre.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Fonction/K_premier_carre.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/K_premier_carre.pl
@@ -19,13 +19,20 @@ text==
 
 Programmer une fonction "carre" qui prend en paramètre un entier et retourne son carré.<br>
 
-Ecrivez ensuite un code qui demande à l'utilisateur un entier, appel la fonction et imprime le résultat.
+Programmer ensuite une fonction "premiers_carres" qui : <br>
+* prend en paramètre un entier k <br>
+* imprime les k premiers entiers de 0 à (k-1)^2 <br>
+
+Pour finir écrivez un code : 
+* qui demande à l'utilisateur un entier, 
+* appel la fonction "premiers_carres" qui imprimera le résultat attendu.
 
 ==
 
 soluce==#|python|
-def carre(n):
-    return(n*n)
+defpremiers_carres ( k ) :
+    for i inrange ( k ) :
+        print carre ( i )
 
 n = int(input("Saisisez un entier :"))
 print(carre(n))
