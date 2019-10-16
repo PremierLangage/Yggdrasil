e0a3b593140665792ab5ce0ea08e42cfc29ad0b4
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-16 11:13:11 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/structure/people.pl

--- ComputerScience/C/exercices/structure/people.pl
+++ ComputerScience/C/exercices/structure/people.pl
@@ -1,27 +1,27 @@
-# Copyright 2017 Nicolas Borie <nicolas.borie@u-pem.fr>
+# Copyright 2017-2019 Nicolas Borie <nicolas.borie@u-pem.fr>
 #
 # Déclaration d'une structure C pour une personne
 
 author=Nicolas Borie
-name=Structure C pour une personne
 title=Structure C pour une personne
 tag=structure
-extends=/ComputerScience/C/template/autograderC.pl
+extends=/ComputerScience/C/template/stdsandboxC.pl
 
 text==
 Déclarer et définir une *structure* C nommé *People*. Cette structure
 devra contenir deux champs pour des chaînes de caractères C d'au plus
-63 caractères. Les deux champs devront être nommé *first_name* et
+63 caractères. Ces deux champs devront être nommés *first_name* et
 *last_name*. Ajouter aussi un champ entier qui devra s'appeler *age*.
 
 ==
 
-code==
+editor.code==
 typedef ...
 
 ==
 
 solution==
+
 typedef struct {
   char first_name[64];
   char last_name[64];
@@ -49,10 +49,9 @@ int main(int argc, char* argv[]){
 
 ==
 
-grader==
-from graderC import graderII
- 
-tests = [["Déclaration et utilisation 1", "John Doe 42",""],
-         ["Déclaration et utilisation 2", "Master Yoda 913",""]] 
-graderII(tests)
+tests==
+[ ["Déclaration et utilisation 1", "John Doe 42",""],
+  ["Déclaration et utilisation 2", "Master Yoda 913",""],
+  ["Déclaration et utilisation 2", "'Nicolaï Yvanovich' Borisky 12",""] ] 
 ==
+
