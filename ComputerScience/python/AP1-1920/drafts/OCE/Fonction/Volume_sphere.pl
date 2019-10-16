8779d736b6b306723654661cdb899bb02bbc0cd1
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-16 13:55:10 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Volume_sphere.pl

---
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Volume_sphere.pl
@@ -0,0 +1,55 @@
+#author=OCE
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Dix mais pas trois
+
+code==
+#
+#
+#
+n = ??(input("Saisie : "))
+#
+
+==
+
+text==  
+
+Programmer une fonction qui teste si un entier est divisible par 10 mais pas par 3. <br>
+
+vous écrirez naturellement un programme qui récupère un entier au clavier, appel la fonction et affiche le résultat.
+
+==
+
+soluce==#|python|
+def dix_mais_pas_trois(n):
+    if n%10==0 and n%3!=0:
+        return True
+    else:
+        return False
+
+n = int(input("Saisie : "))
+print(dix_mais_pas_trois(n))
+==
+
+mplsoluce0==
+False
+2
+==
+mplsoluce1==
+False
+3
+==
+
+mplsoluce2==
+True
+10
+==
+
+
+
+
+
+
+
