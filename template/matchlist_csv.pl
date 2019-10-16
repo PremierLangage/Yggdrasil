de185d0986da7f0aba9de01afcf70ded43a0b000
David DOYEN <David.Doyen@u-pem.fr>
2019-10-16 23:10:01 +0200

    ddoyen modified Yggdrasil/template/matchlist_csv.pl

--- template/matchlist_csv.pl
+++ template/matchlist_csv.pl
@@ -31,28 +31,8 @@ form==
 {{ match|component }}
 ==
 
-evaluator== #|python|
-
-def in_links(conn, links):
-    for e in links:
-        if e['source'] == conn["source"] and  e['target'] == conn["target"]:
-            return True
-    return False
-
-error = 0
-for e in expected:
-    if not in_links(e, match.links):
-        error = error + 1
-
-for e in match.links:
-    e['css'] = 'error-state anim-fade'
-    if in_links(e, expected):
-        e['css'] = 'success-state  anim-flip'
-
-if error == 0:
-    grade = (100, '<span class="success-state">Good answser</span>')
-else:
-    grade = (0, '<span class="error-state">Bad answer, you made %d mistakes</span>' % error)
+evaluator==
+match.eval()
 ==
 
 
