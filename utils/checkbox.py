d89116e70625d4943d463e2433855e3b6f107f5d
David DOYEN <David.Doyen@u-pem.fr>
2019-10-17 12:44:21 +0200

    ddoyen modified Yggdrasil/utils/checkbox.py

---
+++ utils/checkbox.py
@@ -0,0 +1,40 @@
+import uuid
+import random as rd
+from components import Component
+
+class CustomCheckBox(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-radio-group'
+        self.decorator = 'CustomRadioGroup'
+        self.items = []
+        super().__init__(**kwargs)
+
+    def loadContent(self, content):
+        items = []
+        for e in content:
+            items.append({
+                "id": uuid.uuid4(),
+                "content": e
+            })
+        self.items = items
+
+    def shuffle(self):
+        rd.shuffle(self.items)
+
+    def sort(self):
+        rd.sort(self.items)
+
+    def evalByContent(self,sol):
+        ansright,answrong=0,0
+        for item in self.items:
+            if item['content'] in right and item['checked']:
+                item['css'] = 'success-state anim-fade'
+                ansright+=1
+            elif item['content'] in right and not item['checked']:
+                item['css'] = 'success-state anim-fade'
+                answrong+=1
+            elif not (item['content'] in right) and item['checked']:
+                answrong+=1
+        score=max([int((ansright-answrong)/ansright*100),0])
+        return(score,"")
