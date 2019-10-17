4742f016822ff307c70c1f26697ebf4971b0cde9
David DOYEN <David.Doyen@u-pem.fr>
2019-10-17 10:55:32 +0200

    ddoyen modified Yggdrasil/demo/radio.pl

---
+++ demo/radio.pl
@@ -0,0 +1,25 @@
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+@ utils/radiogroup.py [customradiogroup.py]
+
+title = Question à choix multiples
+
+radio =: RadioGroup
+radio.decorator = CustomRadioGroup
+
+before ==
+radio.loadContent(["A","B","C","D"])
+==
+
+text = Select the right answer
+
+form = {{ radio|component }}
+
+evaluator ==
+grade = radio.evalByContent("B")
+==
+
+
+
+
