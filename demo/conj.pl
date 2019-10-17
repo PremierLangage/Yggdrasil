14b46b27cf53944c893258a6ff5f8546811ccfa3
David DOYEN <David.Doyen@u-pem.fr>
2019-10-17 22:41:48 +0200

    ddoyen modified Yggdrasil/demo/conj.pl

--- demo/conj.pl
+++ demo/conj.pl
@@ -1,6 +1,6 @@
 @ /utils/sandboxio.py
 @ /builder/before.py [builder.py]
-@ /grader/evaluator.py [grader.py]
+@ /grader/evaluator2.py [grader.py]
 
 title = Conjugaison
 
@@ -29,13 +29,9 @@ form ==
 ==
 
 evaluator ==
-try:
-    if int(input.value)==a+b:
-        grade=(100,"Bravo "+str(a)+"+"+str(b)+"="+str(a+b)+"""<br>\n<p style="color:blue;font-size:18px;"> Ok d'accord c'est pas terrible comme exercice mais c'est pour la démo :)<br> Remarquez que l'énoncé a changé.</p>""")
-    else:
-        grade=(0,"Raté "+str(a)+"+"+str(b)+"="+str(a+b)+"""<br>\n<p style="color:red;font-size:18px;"> Ok d'accord c'est pas terrible comme exercice mais c'est pour la démo :)<br> Remarquez que l'énoncé a changé.</p>""")
-    exec(before)
-except:
-    grade=(-1,"Votre réponse n'est pas un nombre entier.")
+if input.value==conj:
+    score=100
+else:
+    score=0
 ==
 
