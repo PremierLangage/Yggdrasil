da30b6a1c039e35c949a172dee53181f420cc9a3
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-17 21:23:12 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/Coeffbin2.pl

--- ComputerScience/python/AP1-1920/drafts/zip/fonction/Coeffbin2.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/Coeffbin2.pl
@@ -16,17 +16,17 @@ Ecrire une fontion *combinaison(n,p)* qui reçoit deux entiers positifs
 et renvoie le nombre de combinaions de p éléments choisis parmi n éléments.
 $%\binom{n}{p}%$<br>
 On rappelle que ce nombre vaut $%\frac{n!}{p! \times(n-p)!} %$ si $%0 \le p\le n %$ et 0 sinon.
-Utilisez ces fonctions pour écrire une fonction *coefficients(n)* qui reçoit 
+Utilisez ces fonctions pour écrire une fonction *coefficients_binome(n)* qui reçoit 
  un entier n et renvoie <br>
  *  une chaine de caractères constituée des coefficients de développement de $%(a+b)^n%$, s'il est positif.
 
 * la chaine "non défini" si n est négatif<br>
 Exemples
->>> coefficients(0)==1
+>>> coefficients_binome((0)==1
 True
->>> coefficients(3)
-1 3 3 1<
->>> coefficients(-5)
+>>> coefficients_binome((3)
+1 3 3 1
+>>> coefficients_binome((-5)
 non défini
 
 Remarque
@@ -37,7 +37,7 @@ Nous en étudirons une meilleure plus tard.*
 sbefore==
 import random
 n=random.randint(4,7)
-mplsoluce3="Test aléatoire\n"
+mplsoluce3=">>>\n"
 mplsoluce3 += str(n)
 ==
 
@@ -54,34 +54,24 @@ def combinaison(n,p):
         return 0
     return factoriel(n)/(factoriel(n-p)*factoriel(p))
 
-n=int(input("n"))
+def coefficients_binome(n)
 if n<0:
-    print("non défini")
+    s="non défini"
 else:
     s=""
     for p in range(n+1):
         s+=str(int(combinaison(n,p)))+" "
-    print(s)
+return s
 
 ==
-mplsoluce0==
-classique
-3
+pltest0==
+>>>factoriel(3)==6 and combinaison(3,2)==3 and coefficients_binome(2)=="1 2 1"
+True
 ==
-mplsoluce1==
-negatif
--9
+pltest1==
+#negatif
+>>>-9
+"non défini"
 ==
-
-mplsoluce2==
-nul
-0
-==
-mplsoluce4==
-un
-1
-==
-
-
 
 
