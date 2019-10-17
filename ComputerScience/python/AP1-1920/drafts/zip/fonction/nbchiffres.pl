eef112ca0822f7c0070034184c7e10ffb7640270
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-17 15:13:10 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/nbchiffres.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/nbchiffres.pl
@@ -0,0 +1,7 @@
+
+text==
+on veut déterminer le nombre d'apparition d'un chiffre particulier dans $%2^n%$
+Pour cela écrire une fonction  nombre_chiffre(n,c) qui reçoit un entier positif n et un chiffre c et renvoie  le nombre 
+ d'apparition du chiffre c dans $%2^n%$ (le code n'utilise pas de str). Si n n'est pas un entier positif ou si c n'est pas un chiffre
+ la fonction renvoie None
+ ==
