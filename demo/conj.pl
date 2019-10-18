3cd8f2e138d6488d59161ef3e2c357d9da361f63
David DOYEN <David.Doyen@u-pem.fr>
2019-10-18 17:03:25 +0200

    ddoyen modified Yggdrasil/demo/conj.pl

--- demo/conj.pl
+++ demo/conj.pl
@@ -5,21 +5,29 @@
 title = Conjugaison
 
 before ==
+
 import random as rd
-lst_prs={'1S':'1ère personne du singulier',
+import csv
+
+with open('data.csv',newline='') as file:
+    rows=list(csv.DictReader(file,delimiter=','))
+
+row=rd.choice(rows)
+p=rd.choice(['1S','2S','3S','1P','2P','3P'])
+
+lst_txt_prs={'1S':'1ère personne du singulier',
          '2S':'2e personne du singulier',
          '3S':'3e personne du singulier',
          '1P':'1ère personne du pluriel',
          '2P':'2e personne du pluriel',
          '3P':'3e personne du pluriel'}
-lst_conj=['mange','manges','mange','mangeons','mangez','mangent']
-i=rd.randint(0,5)
-prs=lst_prs[i]
-conj=lst_conj[i]
+
+verbe=row['infinitif']
+txt_prs=lst_txt_prs[p]
 ==
 
 text ==
-Conjuguer le verbe manger à la {{prs}} du présent.
+Conjuguer le verbe {{verbe_inf}} à la {{txt_prs}} du présent.
 ==
 
 input =: Input
@@ -30,7 +38,7 @@ form ==
 ==
 
 evaluator ==
-if input.value==conj:
+if input.value==verbe_conj:
     score=100
 else:
     score=0
