1517adb7aac703cb1125bf5b211830d2505e8bba
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-18 14:47:33 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/TriangleEtoile2.pl

--- ComputerScience/python/AP1-1920/drafts/zip/fonction/TriangleEtoile2.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/TriangleEtoile2.pl
@@ -41,10 +41,57 @@ dont l'affichage produit <br>
 ==
 before==
 import random
+def ligne_etoiles(n):
+    s=""
+    if n>0:
+        s="*"
+        etoile="*"
+        if n>1:
+            for i in range(n-1):
+                etoile+="*"
+                s=s+"+"+etoile
+        
+    return s
 
-mplsoluce2="Test aléatoire\n"
+def triangle_etoiles(n):
+    s="\n"
+    for i in range(1,n+1):
+        s+=ligne_etoiles(i)+"\n"
+    for i in range(n-1):
+       s+=ligne_etoiles(n-1-i)+"\n"
+    return s
+
+pltest2="Test aléatoire\n"
 n=random.randint(4,7)
-mplsoluce2 += str(n)+"\n"
+pltest3=""">>> triangle_etoiles({})\n{}""".format(n, triangle_etoiles(n))
+==
+
+after==
+
+import random
+def ligne_etoiles(n):
+    s=""
+    if n>0:
+        s="*"
+        etoile="*"
+        if n>1:
+            for i in range(n-1):
+                etoile+="*"
+                s=s+"+"+etoile
+        
+    return s
+
+def triangle_etoiles(n):
+    s="\n"
+    for i in range(1,n+1):
+        s+=ligne_etoiles(i)+"\n"
+    for i in range(n-1):
+       s+=ligne_etoiles(n-1-i)+"\n"
+    return s
+
+pltest2="Test aléatoire\n"
+n=random.randint(4,7)
+pltest3=""">>> triangle_etoiles({})\n{}""".format(n, triangle_etoiles(n))
 ==
 
 zsoluce==
@@ -77,11 +124,9 @@ pltest0==
 ==
 pltest1==
 >>> triangle_etoiles(2)
-'
-*
-*+**
-*
+'\n*\n*+**\n*\n'
 ==
+
 
 
 
