76e2c7ad586914cdb2ab7424ef895e0c64cf8e61
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-19 21:28:26 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/syracuse.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/syracuse.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/syracuse.pl
@@ -14,6 +14,7 @@ La suite de Syracuse est une suite d'entiers définie par :
 
 Pour tout entier strictement positif $%u_0%$ testé, la suite converge vers 1. 
 Le premier n tel que $%u_n=1%$ est appelé temps de vol de la suite.
+
 Le but de l'exercice est de déterminer le temps de vol pour plusieurs valeurs de $%u_0%$.
 <br>
 <br>
