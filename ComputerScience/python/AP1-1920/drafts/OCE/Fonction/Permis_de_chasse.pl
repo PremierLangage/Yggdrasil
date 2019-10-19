699605fa8d660f24346d9768c338ae00e718755d
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-19 23:38:26 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
@@ -9,7 +9,10 @@ code==
 #
 #
 #
-n = ??(input("Saisie : "))
+poules=??(input("Combiendepoules?"))
+chiens=??(input("Combiendechiens?"))
+vaches=??(input("Combiendevaches?"))
+amis=??(input("Combiend'amis?"))
 #
 
 ==
@@ -18,12 +21,13 @@ text==
 Un permis de chasse à points remplace désormais le permis de chasse traditionnel.
 Chaque chasseur possède au départ un capital de 100 points. 
 S’il tue une poule il perd 1 point, 3 points pour 1 chien, 5 points pour une vache et 10 points pour un ami. 
-Le permis coûte 200 euros. Écrire une fonction amende qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due. 
+Le permis coûte 200 euros par points perdus. 
+Écrire une fonction "amende" qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due. 
 Utilisez cette fonction dans un programme principal qui saisit le nombre de victimes et qui affiche la somme que le chasseur doit débourser.
 ==
 
 soluce==#|python|
-def permisSup(p,c,v,a):
+def amende(p,c,v,a):
 	points_perdus=p+3*c+5*v+10*a
 	nbre_permis=points_perdus/100.0
 	return 200*nbre_permis
@@ -34,7 +38,7 @@ chiens=int(input("Combiendechiens?"))
 vaches=int(input("Combiendevaches?"))
 amis=int(input("Combiend'amis?"))
 
-payer=permisSup(poules,chiens,vaches,amis)
+payer=amende(poules,chiens,vaches,amis)
 print("\nApayer:",end="")
 if payer==0:
     print("rien a payer")
