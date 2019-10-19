50a9fc941e318f953568a32f0c71381f8a70ba67
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-19 09:47:16 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/Coeffbin.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/Coeffbin.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/Coeffbin.pl
@@ -19,9 +19,9 @@ $%\binom{n}{p}%$<br>
 On rappelle que ce nombre vaut $%\frac{n!}{p! \times(n-p)!} %$ si $%0 \le p\le n %$ et 0 sinon.<br>
 Utilisez ces fonctions pour écrire une fonction *coefficients_binome(n)* qui reçoit 
 un entier n et renvoie :<br>
-*  une chaine de caractères constituée des coefficients de développement de $%(a+b)^n%$, s'il est positif.<br>
++ une chaine de caractères constituée des coefficients de développement de $%(a+b)^n%$, s'il est positif.<br>
 
-* la chaine "non défini" si n est négatif<br>
++ la chaine "non défini" si n est négatif<br>
 Exemples<br>
 
     &gt;&gt;&gt; coefficients_binome(0)==1<br>
@@ -31,7 +31,7 @@ Exemples<br>
     &gt;&gt;&gt; coefficients_binome(-5)<br>
     non défini<br>
 
-Remarque : <br>
+*Remarque :* <br>
 
 *Cette méthode n'est pas efficace pour de grandes valeurs de n et p (elle effectue beaucoup de calculs inutiles).
 Nous en étudirons une meilleure plus tard.*
