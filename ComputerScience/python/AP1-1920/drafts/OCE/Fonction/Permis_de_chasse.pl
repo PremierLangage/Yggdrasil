e1a1af7549b3e338f286fcc9916d77bc2e8d0eb3
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-19 23:33:19 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
@@ -3,7 +3,7 @@
 # Heritage d'un type d'exercice 
 extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
-title = Dix mais pas trois
+title = Permis de chasse
 
 code==
 #
@@ -15,22 +15,31 @@ n = ??(input("Saisie : "))
 ==
 
 text==  
-
-Programmer une fonction qui teste si un entier est divisible par 10 mais pas par 3. <br>
-
-vous écrirez naturellement un programme qui récupère un entier au clavier, appel la fonction et affiche le résultat.
-
+Un permis de chasse à points remplace désormais le permis de chasse traditionnel.
+Chaque chasseur possède au départ un capital de 100 points. 
+S’il tue une poule il perd 1 point, 3 points pour 1 chien, 5 points pour une vache et 10 points pour un ami. 
+Le permis coûte 200 euros. Écrire une fonction amende qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due. 
+Utilisez cette fonction dans un programme principal qui saisit le nombre de victimes et qui affiche la somme que le chasseur doit débourser.
 ==
 
 soluce==#|python|
-def dix_mais_pas_trois(n):
-    if n%10==0 and n%3!=0:
-        return True
-    else:
-        return False
+def permisSup(p,c,v,a):
+	points_perdus=p+3*c+5*v+10*a
+	nbre_permis=points_perdus/100.0
+	return 200*nbre_permis
 
-n = int(input("Saisie : "))
-print(dix_mais_pas_trois(n))
+#programme principal-----------------------------------------------
+poules=int(input("Combiendepoules?"))
+chiens=int(input("Combiendechiens?"))
+vaches=int(input("Combiendevaches?"))
+amis=int(input("Combiend'amis?"))
+
+payer=permisSup(poules,chiens,vaches,amis)
+print("\nApayer:",end="")
+if payer==0:
+    print("rien a payer")
+else:
+    print(payer,"euros")
 ==
 
 mplsoluce0==
