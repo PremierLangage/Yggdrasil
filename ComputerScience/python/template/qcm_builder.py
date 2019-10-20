e3d40c95363854e68254dab77c371cae47a3b319
Essox Lucius <Elucius@aol.fr>
2019-10-20 21:54:04 +0200

    elucius modified Yggdrasil/ComputerScience/python/template/qcm_builder.py

--- ComputerScience/python/template/qcm_builder.py
+++ ComputerScience/python/template/qcm_builder.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
     dic['pairs']=pairs
     dic['form'] = """<div class="input-group"><table>"""
     for i,p in enumerate(pairs):
-        dic['form'] += """<TR><td><input id="form_answer_"""+str(i)+"""\"  type="checkbox"  placeholder="" required>"""+p[0]+"</td></TR>"
+        dic['form'] += """<TR><td><input id="form_answer_"""+str(i)+"""\"  type="checkbox"  placeholder="" required>  """+p[0]+"</td></TR>"
     dic['form'] += "</table></div>"
 
 
