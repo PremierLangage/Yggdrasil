37d966e88b14c8144c45a297d7ad1083e4a983b7
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-20 13:27:14 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/EntierGauss.pl

--- ComputerScience/python/AP1-1920/drafts/zip/fonction/EntierGauss.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/EntierGauss.pl
@@ -12,31 +12,14 @@ text==
 On appelle "entier de Gauss" un nombre complexe $%z=a+\mathrm{i}b , a,b \in \mathbb{R} %$
 
 Ecrire une fonction *nb_carre_Gauss(r)* qui renvoie le nombre d'entiers de Gauss de module inférieur à $%r%$
-dont le carré est également un entier de Gauss de modulle inférieur à $%r%$
+dont le carré est également un entier de Gauss de modulle inférieur à $%r%$.
+
+
 ==
 before==
-def factoriel(n):
-    if n<0:
-        return None
-    f=1
-    for i in range(n):
-        f*=i+1 
-    return f 
+def carre_complexe(x,y):
+    return 0 
 
-def combinaison(n,p):
-    if n<0 or p>n:
-        return 0
-    return factoriel(n)/(factoriel(n-p)*factoriel(p))
-
-def coefficients_binome(n):
-    if n<0:
-        s="non défini"
-    else:
-        s=""
-        for p in range(n):
-            s+=str(int(combinaison(n,p)))+" "
-        s+="1"
-    return  s
 
 import random, sys
 
@@ -47,31 +30,8 @@ pltest3=""">>> coefficients_binome({})\n'{}'""".format(n, coefficients_binome(n)
 after=before
 ==
 
-zsoluce==
-def factoriel(n):
-    if n<0:
-        return None
-    f=1
-    for i in range(n):
-        f*=i+1 
-    return f 
 
-def combinaison(n,p):
-    if n<0 or p>n:
-        return 0
-    return factoriel(n)/(factoriel(n-p)*factoriel(p))
 
-def coefficients_binome(n):
-    if n<0:
-        s="non défini"
-    else:
-        s=""
-        for p in range(n):
-            s+=str(int(combinaison(n,p)))+" "
-        s+="1"
-    return  s
-
-==
 pltest0==
 >>> factoriel(3)==6 and combinaison(3,2)==3 and coefficients_binome(2)=="1 2 1"
 True
