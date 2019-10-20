60266619a37c5713851091b64566e0d55b6151c1
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-20 14:41:31 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/carre.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/carre.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/carre.pl
@@ -2,13 +2,13 @@
 # Boris Jabot 10/09/2019 OK
 # Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
 author=l'équipe de l'UPEM vous propose des exercices
-title=Une fonction carrément carrée
+title=Une fonction carre
 
 tag=function # N'oubliez pas de remplir ce champs svp
 extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 piste=verte
 text==
-## Une fonction carrément carrée ##
+## Une fonction carre ##
 Ecrivez une fonction **carre** qui retourne le carré de son paramêtre.
 ==
 
