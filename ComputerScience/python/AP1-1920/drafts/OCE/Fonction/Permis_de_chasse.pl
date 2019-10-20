67f44839c9de40a5a32e63b7d9c72be812f612df
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-20 18:15:29 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
@@ -20,30 +20,41 @@ amis=??(input("Combiend'amis?"))
 text==  
 Un permis de chasse à points remplace désormais le permis de chasse traditionnel.<br>
 Chaque chasseur possède au départ un capital de 100 points. <br>
-S’il tue une poule il perd 1 point, 3 points pour 1 chien, 5 points pour une vache et 10 points pour un ami. <br>
-L'amende coûte 20 euros par points perdus. <br>
-Écrire une fonction "amende" qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due. <br>
-Utilisez cette fonction dans un programme principal qui saisit le nombre de victimes et qui affiche la somme que le chasseur doit débourser <br>
-ainsi que le nombre de points restant.
+S’il tue une poule il perd 1 point, 5 points pour 1 chien, 10 points pour une vache et 30 points pour un ami. <br>
+L'amende coûte 50 euros par points perdus. <br>
+<br>
+Écrire une fonction "amende" qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due et le nombre de points perdus. <br>
+Utilisez cette fonction dans un programme principal qui : <br>
+* saisit le nombre de victimes et,<br>
+* qui affiche la somme que le chasseur doit débourser ainsi que,<br>
+* le nombre de points restant (si la somme de points perdus est supérieure à 100, la fonction retourne 100).<br>
+Écrire ensuite un programme principal qui :
+
+Exemple : <br>
+
 ==
 
 soluce==#|python|
 def amende(p,c,v,a):
-	points_perdus=p+3*c+5*v+10*a
-	nbre_permis=points_perdus/100.0
-	return 20*nbre_permis
+    points_perdus=p+3*c+5*v+10*a
+    if (points_perdus>=100):
+        points_perdus = 100
+    return (50*points_perdus,points_perdus)
 
-#programme principal-----------------------------------------------
-poules=int(input("Combiendepoules?"))
-chiens=int(input("Combiendechiens?"))
-vaches=int(input("Combiendevaches?"))
-amis=int(input("Combiend'amis?"))
+#programme principal------------------
+poules=int(input("Combien de poules?"))
+chiens=int(input("Combien de chiens?"))
+vaches=int(input("Combien de vaches?"))
+amis=int(input("Combien d'amis?"))
 
-payer=amende(poules,chiens,vaches,amis)
-if payer==0:
-    print("rien a payer")
+payer,points=amende(poules,chiens,vaches,amis)
+
+print("Payer ",payer, " euros.")
+
+if points==100:
+    print("Permis annulé")
 else:
-    print("payer :",payer,"euros")
+    print("Points perdus : ",points)
 ==
 
 mplsoluce0==
