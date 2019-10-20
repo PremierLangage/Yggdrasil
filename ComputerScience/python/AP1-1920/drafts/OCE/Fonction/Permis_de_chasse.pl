4eb21c41ea2ca68aab693c41c4b7b52eaba1d780
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-20 18:37:24 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/Permis_de_chasse.pl
@@ -9,10 +9,10 @@ code==
 #
 #
 #
-poules=??(input("Combiendepoules?"))
-chiens=??(input("Combiendechiens?"))
-vaches=??(input("Combiendevaches?"))
-amis=??(input("Combiend'amis?"))
+poules=??(input("Combien de poules?"))
+chiens=??(input("Combien de chiens?"))
+vaches=??(input("Combien de vaches?"))
+amis=??(input("Combien d'amis?"))
 #
 
 ==
@@ -22,16 +22,36 @@ Un permis de chasse à points remplace désormais le permis de chasse traditionn
 Chaque chasseur possède au départ un capital de 100 points. <br>
 S’il tue une poule il perd 1 point, 5 points pour 1 chien, 10 points pour une vache et 30 points pour un ami. <br>
 L'amende coûte 50 euros par points perdus. <br>
+Si les points perdus sont supérieurs à 100, le permis est annulé.
 <br>
 Écrire une fonction "amende" qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due et le nombre de points perdus. <br>
 Utilisez cette fonction dans un programme principal qui : <br>
 * saisit le nombre de victimes et,<br>
 * qui affiche la somme que le chasseur doit débourser ainsi que,<br>
 * le nombre de points restant (si la somme de points perdus est supérieure à 100, la fonction retourne 100).<br>
+
 Écrire ensuite un programme principal qui :
+* demande à l'utilisateur le nombre de victime
+* retourne la somme à payer <br>
+* le nombre de points restants ou l'annulation du permis
 
 Exemple : <br>
 
+Combien de poules?1 <br>
+Combien de chiens?2 <br>
+Combien de vaches?10 <br>
+Combien d'amis?20 <br>
+<br>
+Payer 5000 euros. <br>
+Permis annulé <br>
+
+Combien de poules?1 <br>
+Combien de chiens?1 <br>
+Combien de vaches?1 <br>
+Combien d'amis?0 <br>
+<br>
+Payer 450 euros.
+Points perdus : 9
 ==
 
 soluce==#|python|
@@ -49,12 +69,12 @@ amis=int(input("Combien d'amis?"))
 
 payer,points=amende(poules,chiens,vaches,amis)
 
-print("Payer ",payer, " euros.")
+print("Payer",payer, "euros.")
 
 if points==100:
     print("Permis annulé")
 else:
-    print("Points perdus : ",points)
+    print("Points perdus :",points)
 ==
 
 mplsoluce0==
