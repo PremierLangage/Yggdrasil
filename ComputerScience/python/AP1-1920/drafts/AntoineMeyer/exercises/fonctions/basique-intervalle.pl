db862a1c1d3d47929d00ef30061600b3e89e0232
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2019-10-21 15:40:50 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/fonctions/basique-intervalle.pl

--- ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/fonctions/basique-intervalle.pl
+++ ComputerScience/python/AP1-1920/drafts/AntoineMeyer/exercises/fonctions/basique-intervalle.pl
@@ -10,16 +10,15 @@ entre `a` et `b`, et `False` sinon.
 
 Exemples d'appels :
 
-```pycon
->>> dans_intervalle(4, 1, 12)
-True
->>> dans_intervalle(40, 1, 12)
-False
->>> dans_intervalle("oui", "non", "peut-être")
-True
->>> dans_intervalle("certainement", "non", "peut-être")
-False
-```
+    >>> dans_intervalle(4, 1, 12)
+    True
+    >>> dans_intervalle(40, 1, 12)
+    False
+    >>> dans_intervalle("oui", "non", "peut-être")
+    True
+    >>> dans_intervalle("certainement", "non", "peut-être")
+    False
+
 ==
 
 grader==#|python|
@@ -27,14 +26,12 @@ grader==#|python|
 #import sys
 #print(pl_context, file=sys.stderr)
 
-def tests_call(cases):
+def tests(cases):
     for n, a, b in cases:
         run(title = f'Appartenance de {n} dans [{a}, {b}]',
             expr = f'dans_intervalle({n}, {a}, {b})', 
             result = a < n < b)
 
-import random
-begin_test_group("Valeurs fixées")
 tests([
     (4, 1, 12), 
     (40, 1, 12), 
