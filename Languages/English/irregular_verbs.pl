915f6427c7ce6e46c5de12c5ffa6b6dcb8afc963
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-21 10:44:55 +0200

    nborie modified Yggdrasil/Languages/English/irregular_verbs.pl

--- Languages/English/irregular_verbs.pl
+++ Languages/English/irregular_verbs.pl
@@ -45,39 +45,18 @@ text= Give the preterite of the verb ** {{ verb }} ** !
 
 evaluator== #|python|
 
-countdown.hidden = True
+countdown.time = 10
 
 if inputbox.value == preterit:
     grade = (100, '<span class="success-state">Good 👏👏👏</span>')
 else:
     grade = (0, '<span class="error-state">Bad answer 👎👎👎</span>')
 
+index_verb  = random.randint(0, len(IR)-1)
 
+verb = IR[index_verb][0]
+preterit = IR[index_verb][1]
 
 ==
 
-extrajs==
-<script>
-    function onReadyPL(nodes) {
-        const actions = nodes.actions;
-        actions.find('.action-save').hide();
-        actions.find('.action-reset').hide();
-        actions.find('.action-next').hide();
-
-        const { origin, pathname }  = document.location;
-        const link = origin + pathname;
-
-        const buttons = actions.find('.btn-group');
-        buttons.append(`
-            <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
-                <i class="fas fa-dice"></i> Nouveau tirage
-            </a>
-        `);
-        {% if score >-1 %}
-        const submit = actions.find('.action-submit');
-        submit.hide();
-        {% endif %}
-    }
-</script>
-==
 
