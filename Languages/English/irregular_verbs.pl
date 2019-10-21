897f4a5099b4b2c1052d226b6ca6e63872d76a76
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-10-21 10:41:12 +0200

    nborie modified Yggdrasil/Languages/English/irregular_verbs.pl

--- Languages/English/irregular_verbs.pl
+++ Languages/English/irregular_verbs.pl
@@ -55,3 +55,29 @@ else:
 
 
 ==
+
+extrajs==
+<script>
+    function onReadyPL(nodes) {
+        const actions = nodes.actions;
+        actions.find('.action-save').hide();
+        actions.find('.action-reset').hide();
+        actions.find('.action-next').hide();
+
+        const { origin, pathname }  = document.location;
+        const link = origin + pathname;
+
+        const buttons = actions.find('.btn-group');
+        buttons.append(`
+            <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
+                <i class="fas fa-dice"></i> Nouveau tirage
+            </a>
+        `);
+        {% if score >-1 %}
+        const submit = actions.find('.action-submit');
+        submit.hide();
+        {% endif %}
+    }
+</script>
+==
+
