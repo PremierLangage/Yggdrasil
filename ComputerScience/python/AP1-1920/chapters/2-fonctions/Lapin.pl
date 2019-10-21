2900634b1a6a8530c4712522412d83831756c602
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-21 18:01:10 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/Lapin.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/Lapin.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/Lapin.pl
@@ -4,7 +4,13 @@
 extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 @ /builder/before.py [builder.py]
 
-image $ Arbre.png
+image 
+
+
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+image =$ Arbre.png
 
 title= Des lapins ... 
 
