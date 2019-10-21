dd01b64cd55efe61e9468a32ebe44b8ca6520f7b
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-21 16:59:29 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/Conversion.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/Conversion.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/Conversion.pl
@@ -4,10 +4,13 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 
 title = Conversion de température
 
-text==  
-
-Écrire une fonction **conv()** qui reçoit deux paramètres, une température "t" et un entier "n" (dans cet ordre), et qui retourne la conversion 
-Celsius -> Fahrenheit (n= 1), ou Fahrenheit -> Celsius(n= 2).<br>
+text==
+Écrire une fonction **conv()** qui reçoit deux paramètres, une température "t" et une chaine de caractères indiquant
+le type de conversion à effectuer,
+ et qui retourne la conversion :<br>
+Celsius -> Fahrenheit (deuxième paramètre CF), 
+Fahrenheit -> Celsius(deuxième paramètre FC).<br>
+Si le deuxième paramètre n'est pas une de ces chaines, la valeur de t est retournée.
 Rappel : 
 $%
 Tf = 32 +1.8 * Tc\\\
@@ -16,10 +19,12 @@ Tf = 32 +1.8 * Tc\\\
 editor.code==
 def conv(t,n):
  """Renvoie la conversion Celsius->Fahrenheit ou inversement suivant<n>."""
-    if n==1: #Celsius->Fahrenheit
+    if n=="CF": #Celsius->Fahrenheit
         return(32.0+1.8*t)
-    elif n==2: #Fahrenheit->Celsius
+    elif n=="FC": #Fahrenheit->Celsius
     return((t-32.0)/1.8)
+    else:
+        return t
 ==
 
 pltest0==
