9c9a3c17d6e4bd3998e4300034cd905a49b16759
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-21 18:18:24 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/Lapin.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/Lapin.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/Lapin.pl
@@ -22,6 +22,7 @@ de la même manière.<br>
 1er décembre : 1 couple de jeunes lapins ,1 couple de  lapins d’un mois et 1 couple de vieux<br>
 1er janvier : 2 couple de jeunes lapins ,1 couple de  lapins d’un mois et 2 couples de vieux, soit 10 lapins.<br>
 <img src="{{ image }}"/>
+<br>
 Ecrire une fontion `lapin(mois)` qui reçoit un entier décrivant le nombre de mois
  écoulé et renvoie le nombre de couples.(*On distinguera le nombre de couples de nouveaux lapins, le nombre
 de couples de lapins ayant un mois, et le nombre de couples de lapins
@@ -103,6 +104,7 @@ pltest3=""">>> lapin({})\n{}""".format(n, lapin(n))
 
 
 ==
+
 
 
 
