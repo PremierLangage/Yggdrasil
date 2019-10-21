c142fd04c8488fe427897a4c6aa4392d05071b67
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-21 23:51:42 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/Histoire_De_Train.pl

--- ComputerScience/python/AP1-1920/drafts/zip/fonction/Histoire_De_Train.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/Histoire_De_Train.pl
@@ -6,12 +6,11 @@ extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 title = Histoire de trains
 
 text==  
-
 Je suis ligoté sur les rails en gare d’Arras.<br> 
-Écrire un programme qui affiche un tableau me permettant de connaître l’heure à laquelle je serai déchiqueté par le train parti de la gare du Nord
-à 9h (il y a 170 km entre la gare du Nord et Arras).<br>t
-–Écrire une fonction "temps_mis" qui reçoit la vitesse du train et la distance et qui renvoie le temps mis pour parcourir cette distance;<br>
-–Écrire le programme principal qui affiche le tableau demandé.<br>
+crire un programme qui affiche l’heure à laquelle je serai déchiqueté par le train parti à 9h de la gare de Ouceti vers Arras
+.<br>
+–Écrire une fonction `temps_mis` qui reçoit la vitesse du train et la distance et qui renvoie le temps mis pour parcourir cette distance;<br>
+–Écrire le programme principal qui affiche .<br>
 <br>
 indice : pour imprimer le tableau, on vous donne la phrase à utiliser : <br>
 print("A",v,"km/h,je me fais dechiqueter à ",9 + h,"h",m,"mn") 
