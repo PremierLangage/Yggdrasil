5be02dbaa8b25dfa69e423b0fd45293ea0bb7c4d
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-22 17:53:58 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/Lance_de_des.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/Lance_de_des.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/Lance_de_des.pl
@@ -9,6 +9,8 @@ Coder une fonction `Lances_Des` qui prend en paramètre un entier "n" compris en
 
 ==
 
+@ /builder/before.py [builder.py]
+
 #correction==
 #def Lances_Des(n):
 #    s = 0
@@ -20,29 +22,18 @@ Coder une fonction `Lances_Des` qui prend en paramètre un entier "n" compris en
 #    print("Il y a",s,"facon(s) de faire",n,"avec trois des.")
 #==
 
-pltest0==
->>> Lances_Des(5)
-Il y a 6 facon(s) de faire 5 avec trois des.
-==
-pltest1==
->>> Lances_Des(12)
-Il y a 25 facon(s) de faire 12 avec trois des.
-==
-pltest2==
->>> Lances_Des(1)
-Il y a 0 facon(s) de faire 1 avec trois des.
-==
-pltest3==
->>> Lances_Des(3)
-Il y a 1 facon(s) de faire 3 avec trois des.
-==
-pltest4==
->>> Lances_Des(7)
-Il y a 15 facon(s) de faire 7 avec trois des.
-==
-
-
-
+before==
+def Lances_Des(n):
+    s = 0
+    for i in range(1, 7):
+        for j in range(1, 7):
+            for k in range(1, 7):
+                if i + j + k == n:
+                    s += 1
+for i in range(7):
+    n=range(1,18)
+    p=Lance_Des(n)
+    globals()[f"pltest{i}"]=f""">>> Lance_Des({n})\n{p}\n"""
 
 
 
