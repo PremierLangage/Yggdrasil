1fadd07abace96741ef5329bc39011d5e2cd479f
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-10-22 11:33:59 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/Fonction/table.pl

---
+++ ComputerScience/python/AP1-1920/drafts/OCE/Fonction/table.pl
@@ -0,0 +1,99 @@
+
+#author=OCE
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Afficher les "n" premiers multiples d'un entier "x"
+
+before==
+import random
+n=random.randint (-50,26)
+k=random.randint(-35,35)
+
+mplsoluce5="Test aléatoire\n"
+mplsoluce5 += str(n)+"\n"
+mplsoluce5 += str(k)+"\n"
+
+after=before
+==
+
+code==
+#
+#
+#
+#
+#
+#
+#
+?? = ??(input("Saisisez un entier n : "))
+#
+?? = ??(input("Saisissez un entier k strictement positif : "))
+==
+
+text==  
+
+Programmer une fonction "table" qui : <br 
+* prend en paramètre deux entiers : <br>
+   + un entier n <br>
+   + un entier strictement positif k <br>
+* imprime les "k" premiers multiples de "n"
+
+Pour finir écrivez un code : <br>
+* qui demande à l'utilisateur deux entiers (dont un strictement positif), <br>
+* appel la fonction "table" qui imprimera le résultat attendu.<br>
+
+Vous vous aiderez du code déjà présent pour les saisies des valeurs. 
+==
+
+soluce==#|python|
+def table(n,x):
+    for i in range(n):
+        print(i * x, end =' ')
+
+n = int(input("Saisisez un entier n : "))
+k = int(input("Saisissez un entier k strictement positif : "))
+while(k<=0):
+    k = int(input("Saisissez un entier k strictement positif : "))
+table(n,k)
+==
+
+mplsoluce0==
+paire
+2
+4
+==
+mplsoluce1==
+impaire 
+3
+9
+==
+
+mplsoluce2==
+Grand 
+10
+40
+==
+
+mplsoluce3==
+nul
+0
+10
+==
+
+mplsoluce4==
+négatif
+10
+-2
+==
+
+
+
+
+
+
+
+
+
+
+
