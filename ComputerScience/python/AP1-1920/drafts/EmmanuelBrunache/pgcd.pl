911b9b6ae6b9946afe15470df61879f5e03bf650
Emmanuel BRUNACHE <emmanuel.brunache@ac-creteil.fr>
2019-10-24 15:21:44 +0200

    ebrunache modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/pgcd.pl

---
+++ ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/pgcd.pl
@@ -0,0 +1,21 @@
+author= Emmanuel
+
+# comment
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+
+title = PGCD
+
+text == 
+Réaliser une fonction s'appelent "produit" qui prend deux nombres et qui les multiplient
+
+==
+
+pltest==
+>>> produit(2,4)
+8
+>>> produit(0,4)
+0
+
+==
+
+
