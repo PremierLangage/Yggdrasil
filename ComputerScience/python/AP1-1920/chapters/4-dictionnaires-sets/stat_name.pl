be49437998e366538ac814d33ef69227d39e874f
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2019-10-24 15:29:06 +0200

    wfang modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/4-dictionnaires-sets/stat_name.pl

---
+++ ComputerScience/python/AP1-1920/chapters/4-dictionnaires-sets/stat_name.pl
@@ -0,0 +1,17 @@
+#author = WFang
+
+extends = /ComputerScience/python/AP1-1920/templates/pltest.pl
+
+title = Compter les imposables
+
+text==
+Toute personne qui réside en France doit payer l'impôt. Pourtant pas tous les français s'habite en France. 
+
+La Finance Publique a alors besoin de savoir qui réside encore en France. Elle possède d'une base de donnée qui enregistre l'âge et le pays de résidence de chaque français.
+
+On a converti cette base de donné sous forme de dictionnaire, avec le nombre de personne comme clé, et la valeur est une couple d'âge (un entier) et du pays de résidence (un string)
+
+Ecrire une fonction `liste_imposable` qui donne la liste des imposables.
+==
+
+
