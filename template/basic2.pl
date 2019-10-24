37b7f28b05a12130c5d4e03f30e687aaa014b404
David DOYEN <David.Doyen@u-pem.fr>
2019-10-24 12:24:47 +0200

    ddoyen modified Yggdrasil/template/basic2.pl

--- template/basic2.pl
+++ template/basic2.pl
@@ -38,7 +38,7 @@ extrajs==
         const link = origin + pathname;
 
         const buttons = actions.find('.btn-group');
-        {% if "save" in buttons %}
+        {% if "save" is in buttons %}
         buttons.append(`
             <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
                 <i class="fas fa-dice"></i> Nouveau tirage
