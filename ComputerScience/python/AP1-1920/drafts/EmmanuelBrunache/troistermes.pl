a692c53f0c427d8992c3ad93f12b4cb46c0482d1
Emmanuel BRUNACHE <emmanuel.brunache@ac-creteil.fr>
2019-10-24 16:40:47 +0200

    ebrunache modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/troistermes.pl

--- ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/troistermes.pl
+++ ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/troistermes.pl
@@ -2,24 +2,24 @@ author= Emmanuel
 
 # comment
 extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
-builder=/builder/before.py
+@ /builder/before.py [builder.py]
 
 title = Suites géométriques
 
 text == 
 Soit $%(u_n)%$ la suite définie par $%u_{n+1}=q\times u_n%$ et $%u_{n_0}=b%$
-Réaliser une fonction s'appelent "suite" qui prend 4 nombres $%q%$, $%n_0%$, $%b%$ et $%n%$ et qui retourne le n-ième terme de la suite
+Réaliser une fonction s'appelant "suite" qui prend 4 nombres $%q%$, $%n_0%$, $%b%$ et $%n%$ et qui retourne le n-ième terme de cette suite.
 ==
 
 before==
-from math import random
+from random import randint
 def suite(a,b,c,d):
     return c*a**(d-b)
-a=randint(10,100)
-b=randint(10,100)
+a=randint(1,10)
+b=randint(10,50)
 c=randint(10,100)
-d=randint(10,100)
-pltest=""">>>suite({},{},{},{})\n{}\n""".format(a,b,c,d,suite(a,b,c,d))
+d=randint(50,100)
+pltest=""">>> suite({},{},{},{})\n{}\n""".format(a,b,c,d,suite(a,b,c,d))
 after=before
 ==
 taboo=import
