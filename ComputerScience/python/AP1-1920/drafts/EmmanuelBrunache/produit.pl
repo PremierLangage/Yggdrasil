1ab807a289652e62334e72e681d9fa68b28f4c92
Emmanuel BRUNACHE <emmanuel.brunache@ac-creteil.fr>
2019-10-24 15:21:58 +0200

    ebrunache modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/produit.pl

---
+++ ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/produit.pl
@@ -0,0 +1,20 @@
+author= Emmanuel
+
+# comment
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+
+title = Produit
+
+text == 
+Réaliser une fonction s'appelent "produit" qui prend deux nombres et qui les multiplient
+
+==
+
+pltest==
+>>> produit(2,4)
+8
+>>> produit(0,4)
+0
+
+==
+
