02054297f6bae450c1b2f8873eb541de42e67361
Philippe CHOCHOIS <Philippe.Chochois@u-pem.fr>
2019-10-24 15:51:05 +0200

    pchochois modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FredLQ/test.pl

--- ComputerScience/python/AP1-1920/drafts/FredLQ/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FredLQ/test.pl
@@ -3,17 +3,29 @@ author= Fred
 
 # comment
 extends=../../templates/pltest.pl
+builder= /builder/before.py
 
 title = premier exo
-
+taboo=returnp
 text == 
 Fonction **resteDeXpar3()** qui retourne le reste de la division de 7 par 3 ?
 {{pltest}}
 ==
+before ==
+import random
+def resteDeXpar3(x)
+    return x%3
+x=random(10,20)
+pltest2=""">>> resteDeXpar3({})\n {}""".format(x,resteDeXpar3(x))
+after=before
+==
 
-pltest==
+pltest0==
 	>>> resteDeXpar3(3)
 	0
+==
+
+pltest1==
 	>>> resteDeXpar3(0)
 	0
 	>>> resteDeXpar3(4)
