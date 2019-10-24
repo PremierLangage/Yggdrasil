bba0dafd2ade98472c058e2735b8b54b571314be
Chaohui WANG <Chaohui.Wang@u-pem.fr>
2019-10-24 15:19:10 +0200

    cwang modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/ChaohuiWang/exo1.pl

--- ComputerScience/python/AP1-1920/drafts/ChaohuiWang/exo1.pl
+++ ComputerScience/python/AP1-1920/drafts/ChaohuiWang/exo1.pl
@@ -1,76 +1,28 @@
+author=DR
+title= Initialisation d'une variable.
+tag=variable
 
+extends=/ComputerScience/python/template/pltest.pl
 @ /utils/sandboxio.py
-grader  =@ /grader/evaluator.py
-builder =@ /builder/before.py
-
-# STEP 1
-match =: MatchList
-
-# remove the comment of the next line to display the properties of the component
-# in realtime inside the form.
-
-# match.debug % true
-
-# STEP 2
-before==
-import random
-match.nodes = []
-expected = []
-for i in range(4):
-    a = random.randint(1, 10)
-    b = random.randint(1, 10)
-    sourceId = "source" + str(i)
-    targetId = "target" + str(i)
-
-    match.nodes.append({
-        "id": sourceId,
-        "content": "%d * %d" % (a, b),
-        "source": True,
-    })
-
-    match.nodes.append({
-        "id": targetId,
-        "content": "%d" % (a * b),
-        "target": True,
-    })
-    expected.append({ "source": sourceId, "target": targetId })
-random.shuffle(match.nodes)
-==
-
-title==
-Match List Component
-==
 
 text==
-*Link each operation to the appropriate result.*
+
+Créer trois variables pim pam et pom contenant respectivement 1 2 et 3.
+
 ==
 
-# STEP 3
-form==
-{{ match|component }}
+code==
+pim,pam,pom=0,0,0
 ==
 
-# STEP 4
-evaluator== #|python|
-
-def in_links(conn, links):
-    for e in links:
-        if e['source'] == conn["source"] and  e['target'] == conn["target"]:
-            return True
-    return False
-
-error = 0
-for e in expected:
-    if not in_links(e, match.links):
-        error = error + 1
-
-for e in match.links:
-    e['css'] = 'error-state anim-fade'
-    if in_links(e, expected):
-        e['css'] = 'success-state  anim-flip'
-
-if error == 0:
-    grade = (100, 'Good answser')
-else:
-    grade = (0, 'Bad answer, you made %d mistakes' % error)
+# comme ca cela se passera bien
+pltest==
+>>> pim # Pim vaut un
+1
+>>> pam == pam  #
+True
+>>> pam
+2
+>>> pom
+3
 ==
