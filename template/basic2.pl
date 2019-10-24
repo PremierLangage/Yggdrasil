b69b5b9ba19882a7f967a95267512cc87b22f652
David DOYEN <David.Doyen@u-pem.fr>
2019-10-24 12:19:45 +0200

    ddoyen modified Yggdrasil/template/basic2.pl

---
+++ template/basic2.pl
@@ -0,0 +1,53 @@
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+title = Basic player
+
+buttons % ["submit","save"]
+
+before =
+
+text =
+
+form =
+
+evaluator ==
+score=-1
+==
+
+extrajs==
+<style>
+.btn {
+    border-radius: 4px !important;
+    margin: 0px 4px !important;
+}
+</style>
+
+
+<script>
+    function onReadyPL(nodes) {
+        const actions = nodes.actions;
+        actions.find('.action-save').hide();
+        actions.find('.action-reset').hide();
+        actions.find('.action-next').hide();
+
+        const { origin, pathname }  = document.location;
+        const link = origin + pathname;
+
+        const buttons = actions.find('.btn-group');
+        buttons.append(`
+            <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
+                <i class="fas fa-dice"></i> Nouveau tirage
+            </a>
+        `);
+        {% if "submit" in buttons %}
+        {% if score >-1 %}
+        const submit = actions.find('.action-submit');
+        submit.hide();
+        {% endif %}
+        {% endif %}
+    }
+</script>
+==
+
