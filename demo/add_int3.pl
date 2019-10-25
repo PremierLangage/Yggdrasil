4744bda08cc08291428719034dc8c8849b9041a1
David DOYEN <David.Doyen@u-pem.fr>
2019-10-25 22:35:36 +0200

    ddoyen modified Yggdrasil/demo/add_int3.pl

---
+++ demo/add_int3.pl
@@ -0,0 +1,26 @@
+extends = /template/basicinput.pl
+
+maxattempt % 3
+
+title = Addition
+
+before ==
+import random as rd
+a=rd.randint(10,20)
+b=rd.randint(10,20)
+==
+
+text ==
+Calculer {{a}} + {{b}}.
+==
+
+evaluator ==
+try:
+    if int(input.value)==a+b:
+        grade=(100,"")
+    else:
+        grade=(0,"%d + %d = %d" % (a,b,a+b))
+except:
+    grade=(-1,"Votre réponse n'est pas un nombre entier.")
+==
+
