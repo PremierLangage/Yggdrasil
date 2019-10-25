cc6d972f76623914845351f28e84a5931d787bf5
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-25 15:46:47 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/PhilippeC/exerciceDeTest.pl

--- ComputerScience/python/AP1-1920/drafts/PhilippeC/exerciceDeTest.pl
+++ ComputerScience/python/AP1-1920/drafts/PhilippeC/exerciceDeTest.pl
@@ -1,1 +1,40 @@
 
+
+author= Fred
+
+# comment
+extends=../../templates/pltest.pl
+builder= /builder/before.py
+
+title = premier exo
+taboo=returnp
+text == 
+Fonction **resteDeXpar3()** qui retourne le reste de la division de 7 par 3 ?
+{{pltest2}}
+==
+
+before==
+from math import *
+
+def resteDeXpar3(n)
+    return n%3
+
+x=random(10,20)
+pltest2=">>> resteDeXpar3("+str(x)+")\n"+str(resteDeXpar3(x))+"\n"
+after=before
+==
+
+pltest0==
+>>> resteDeXpar3(3)
+0
+==
+
+pltest1==
+>>> resteDeXpar3(0)
+0
+>>> resteDeXpar3(4)
+1
+==
+
+
+
