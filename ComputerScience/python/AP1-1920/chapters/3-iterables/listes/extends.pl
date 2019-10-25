c0945e4613badf907f80d29d70137658de55261c
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-25 14:50:45 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/3-iterables/listes/extends.pl

---
+++ ComputerScience/python/AP1-1920/chapters/3-iterables/listes/extends.pl
@@ -0,0 +1,48 @@
+
+
+
+
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+title = Création d'une liste 
+
+text==
+
+Ecrire une fonction `fusion` à deux paramètres (des listes)  qui retourne la liste composée des deux éléments des deux liste.
+
+>>> fusion([1,1],[2,2])
+[1, 1, 2, 2]
+==
+
+taboo=extends
+
+pltest0==
+>>> fusion([1,1],[2,2])
+[1, 1, 2, 2]
+==
+
+xeditor.code==
+
+
+==
+
+tests %=
+{
+    "correct" : {
+        "response" : {
+            "answer" : "def fusion(a,b):\n    a.extends(b)\n    return a\n"
+        },
+        "grade" : 100,
+        "seed":12
+    },
+     "Incorrect" : {
+        "response" : {
+            "answer" : "def fusion(a,b):\n    return [a, b]\n"
+        },
+        "grade" : 0,
+        "seed":12
+    }
+}
+==
+
