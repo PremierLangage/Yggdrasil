16a91ff3ea1c942eb37d4d322f5bc95940c896c5
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-26 16:44:22 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl
@@ -12,9 +12,9 @@ text==
 
 On appelle "entier de Gauss" un nombre complexe $%z=a+\mathrm{i}b , a,b \in \mathbb{Z} %$
 
-Ecrire une fonction *carres_Gauss(r)* qui recoit un nombre réel r et.renvoie la liste  des entiers de Gauss (de module inférieur à $%r%$) 
+Ecrire une fonction *disque_Gauss(r)* qui recoit un nombre réel r et.renvoie la liste  des entiers de Gauss (de module inférieur à $%r%$) 
 dont le carré est également un entier de Gauss de module inférieur à $%r%$. La liste est une liste de couples
-  >>>carre_Gauss(2)<br>
+  >>>disque_Gauss(2)<br>
 [(-2, 0), (-1, -1), (-1, 0), (-1, 1), (0, -2), (0, -1), (0, 0), (0, 1), (0, 2), (1, -1), (1, 0), (1, 1), (2, 0)]<br>
 <*la liste est ordonnée lexicographiquement*
 ==
@@ -22,7 +22,7 @@ before==
 def carre_complexe(x,y):
     return x*x+y*y,2*x*y
 
-def carres_Gauss(r):
+def disque_Gauss(r):
     if r<0:
         return []
     if r==0:
@@ -40,7 +40,7 @@ import random
 
 n=random.randint(11,21)
 n=n+0.2
-pltest3=""">>> carres_Gauss({})\n'{}'""".format(n,carres_Gauss(n))
+pltest4=""">>> disque_Gauss({})\n'{}'""".format(n,disque_Gauss(n))
 
 after=before
 ==
@@ -48,19 +48,18 @@ after=before
 
 
 pltest0==
->>> carres_Gauss(1.1)==[(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]
+>>> disque_Gauss(1.1)==[(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]
 True
 ==
-#pltest1==
-#negatif
-#>>> carres_Gauss(-9)
-#[]
-#==
 pltest1==
+negatif
+>>> disque_Gauss(-9)
+[]
+==
+pltest2==
 >>> carres_Gauss(1.1)
 [(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]
 ==
-pltest2==
 >>> carres_Gauss(15)
 [(-3, -1), (-3, 0), (-3, 1), (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2), (-1, -3), (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (-1, 3), (0, -3), (0, -2), (0, -1), (0, 0), (0, 1), (0, 2), (0, 3), (1, -3), (1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (1, 3), (2, -2), (2, -1), (2, 0), (2, 1), (2, 2), (3, -1), (3, 0), (3, 1)]
 ==
