f3efe1842152b8f73e9efd0702182c5b5ae267c8
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-27 09:11:25 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl
@@ -15,7 +15,7 @@ On appelle "Suite de Fibonacci" la suite d'entier définie par <br>
 %$f_1=1%$<br>
 %$f_{n+2}=f_{n+1}+f_{n}%$<br>
 Ecrire une fonction *fibo)* qui recoit un nombre entier $%n%$ et.renvoie la liste  des 
-nombres de la suite jusqu'à l'indice $%n%$
+$%n%$ premiers nombres de la suite (de $%f_0%$ jusqu'à $%f_{n-1}%$
   >>>fibo(2)<br>
 [0,1]<br>
 >>>fibo(5)<br>
@@ -24,28 +24,22 @@ nombres de la suite jusqu'à l'indice $%n%$
 []<br>
 ==
 before==
-def carre_complexe(x,y):
-    return x*x+y*y,2*x*y
-
-def disque_Gauss(r):
-    if r<0:
-        return []
-    if r==0:
-        return [(0,0)]
-    mod=int(r**2)
-    l=[]
-    for a in range(-mod,mod+1):
-        for b in range(-mod,mod+1):
-            x,y=carre_complexe(a,b)
-            if x*x + y*y <= r*r:
-               l.append((a,b))
-    return l
-
+def fibo(n):
+if n<=0:
+    return []
+if n==1:
+    return [0]
+if n==2
+    return [0,1]
+l=[0,1]
+for i in range(2,n):
+    l.append(l[i-1]+l[i-2])
+return l
 import random
 
-n=random.randint(11,21)
-n=n+0.2
-pltest4=""">>> disque_Gauss({})\n'{}'""".format(n,disque_Gauss(n))
+n=random.randint(11,51)
+
+pltest3=""">>> fibo({})\n'{}'""".format(n,fibo(n))
 
 after=before
 ==
@@ -53,22 +47,17 @@ after=before
 
 
 pltest0==
->>> disque_Gauss(1.1)==[(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]
+>>> fibo(2)==[0, 1]
 True
 ==
 pltest1==
->>> disque_Gauss(-9)
+>>> fibo(-9)
 []
 ==
 pltest2==
->>> disque_Gauss(1.1)
-[(-1, 0), (0, -1), (0, 0), (0, 1), (1, 0)]
+>>> fibo(11)
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 ==
-pltest3==
->>> disque_Gauss(15)
-[(-3, -1), (-3, 0), (-3, 1), (-2, -2), (-2, -1), (-2, 0), (-2, 1), (-2, 2), (-1, -3), (-1, -2), (-1, -1), (-1, 0), (-1, 1), (-1, 2), (-1, 3), (0, -3), (0, -2), (0, -1), (0, 0), (0, 1), (0, 2), (0, 3), (1, -3), (1, -2), (1, -1), (1, 0), (1, 1), (1, 2), (1, 3), (2, -2), (2, -1), (2, 0), (2, 1), (2, 2), (3, -1), (3, 0), (3, 1)]
-==
-
 
 
 
