6d2ea8e090e3d8a84884b694aef505de799eff64
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-27 09:17:49 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/fibo.pl
@@ -25,16 +25,16 @@ $%n%$ premiers nombres de la suite (de $%f_0%$ jusqu'à $%f_{n-1}%$)<br>
 ==
 before==
 def fibo(n):
-if n<=0:
-    return []
-if n==1:
-    return [0]
-if n==2:
-    return [0,1]
-l=[0,1]
-for i in range(2,n):
-    l.append(l[i-1]+l[i-2])
-return l
+    if n<=0:
+        return []
+    if n==1:    
+        return [0]
+    if n==2:
+        return [0,1]
+    l=[0,1]
+    for i in range(2,n):
+        l.append(l[i-1]+l[i-2])
+    return l
 import random
 
 n=random.randint(11,51)
