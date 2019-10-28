ecf2f4f1fc86f1c79304cfb3c2a80a6d4bf9d705
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-28 11:26:19 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl
@@ -1,5 +1,4 @@
 
-
 # author zip
 # Heritage d'un type d'exercice 
 extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
@@ -8,7 +7,11 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 title =Permutation1
 
 text== 
- Pour n entier naturel, une permutation p de $%\{0..n\}%$  est donnée par une liste permu[i]=c ssi p(i)=c.<br>
- ainsi la permutation p de $%\{0..5\}%$ : $%p(i)= 2*i \mod 5%$
+Pour n entier naturel, une permutation p de $%\{0..n\}%$  est donnée par une liste permu[i]=c ssi p(i)=c.<br>
+ainsi la permutation p de $%\{0..5\}%$ : $%p(i)= 2*i \mod 5%$<br>
 $%{\displaystyle \forall n\in \mathbb {N} ^{*}\quad f^{n}=\underbrace {f\circ \ldots \circ f} _{n\ \mathrm {fois} }}%$
-==
+
+$%
+  ∀ n ∈ N ∗ f n = f ∘ … ∘ f ⏟ n   f o i s {\displaystyle \forall n\in \mathbb {N} ^{*}\quad f^{n}=\underbrace {f\circ \ldots \circ f} _{n\ \mathrm {fois} }} {\displaystyle \forall n\in \mathbb {N} ^{*}\quad f^{n}=\underbrace {f\circ \ldots \circ f} _{n\ \mathrm {fois} }}
+%$ 
+ ==
