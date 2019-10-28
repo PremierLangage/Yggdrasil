9c7f59ab4e7a52a32e8fe37b0a321208155fa080
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-28 11:18:17 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl
@@ -0,0 +1,11 @@
+
+
+# author zip
+# Heritage d'un type d'exercice 
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+title =liste palindrome
+text== 
+$%{\displaystyle \forall n\in \mathbb {N} ^{*}\quad f^{n}=\underbrace {f\circ \ldots \circ f} _{n\ \mathrm {fois} }}%$
+==
