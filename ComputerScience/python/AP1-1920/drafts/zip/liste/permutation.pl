ce089f2c4d65ead863bd18a54f7262a374a917f5
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-28 11:28:51 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/permutation.pl
@@ -7,7 +7,7 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 title =Permutation1
 
 text== 
-Pour n entier naturel, une permutation p de $%\{0..n\}%$  est donnée par une liste permu[i]=c ssi p(i)=c.<br>
+Pour n entier naturel, une permutation p de $%\\{0..n\\}%$  est donnée par une liste permu[i]=c ssi p(i)=c.<br>
 ainsi la permutation p de $%\{0..5\}%$ : $%p(i)= 2*i \mod 5%$<br>
 $%{\displaystyle \forall n\in \mathbb {N} ^{*}\quad f^{n}=\underbrace {f\circ \ldots \circ f} _{n\ \mathrm {fois} }}%$
 
