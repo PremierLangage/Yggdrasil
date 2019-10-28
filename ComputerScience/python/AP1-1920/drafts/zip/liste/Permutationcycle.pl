a347489734842434de080871b03b11c6611d1ae3
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-28 15:35:16 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/Permutationcycle.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/Permutationcycle.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/Permutationcycle.pl
@@ -17,7 +17,8 @@ liste permu avec permu[i]=c ssi p(i)=c.<br>
 Exemple, en representant un cycle par une liste<br>
 les cycles de la permutation p=[0,5,4,2,3,1] sont [0], [5,1] et [4,3,2].<br>
 La liste des cycles est [[0],[1,5],[2,3,4)].<br>
-Ecrire une fonction `cycles` qui reçoit une permutation et renvoie la liste de ses cycles (ordonnée par ordre d'apparition du premier élément de chaque cycle)
+Ecrire une fonction `perm2cycles` qui reçoit une permutation et renvoie la liste de ses cycles (ordonnée par ordre d'apparition du premier élément de chaque cycle)
+Ecrire une fonction `cycles2perm` qui reçoit une liste de cycles et renvoie la permutation correspondante
 ==
 before==
 import random
@@ -30,7 +31,7 @@ def genere_permut(n):
         perm.append(val)
         l[ind],l[n-1-i]=l[n-1-i],l[ind]
     return perm
-def cycles(l):
+def perm2cycles(l):
     lcycle=[]
     marque=list(l)
     for i in range(len(l)):
@@ -45,14 +46,32 @@ def cycles(l):
                  
             lcycle.append(cycle)
     return lcycle
-
+def cycles2perm(l):
+    taille=0
+    for i in range(len(l)):
+        taille+=len(l[i])
+    perm=[0]*taille
+    for i in range(len(l)):
+        cycle=l[i]
+        for ind in range(len(cycle)-1):
+            perm[cycle[ind]]=cycle[ind+1]
+        perm[cycle[-1]]=cycle[0]
+        print(perm)
+        input()
+    return perm
+    
 taille=random.randint(1,20)
 l=genere_permut(taille)
 
-pltest1=""">>> cycles({})\n{}""".format(l,cycles(l))
+pltest1=""">>> perm2cycles({})\n{}""".format(l,perm2cycles(l))
+taille=random.randint(1,20)
+l=genere_permut(taille)
+l=perm2cycles(l)
+pltest2=""">>> cycles2perm({})\n{}""".format(l,cycles2perm(l))
+
 after=before
 ==
 pltest0==
->>> cycles([1,0,2])==[[0,1],[2]]
+>>> perm2cycles([1,0,2])==[[0,1],[2]]
 True
 ==
