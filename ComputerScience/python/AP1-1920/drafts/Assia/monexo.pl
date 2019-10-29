20348d77c35b2a568faf5cf6adaa4c6f4b47385f
assia outamazirt <outamazirt.assia@gmail.com>
2019-10-29 12:25:32 +0100

    aoutamazirt modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/Assia/monexo.pl

---
+++ ComputerScience/python/AP1-1920/drafts/Assia/monexo.pl
@@ -0,0 +1,19 @@
+# OCE : test 24/10/2019 OK
+
+author=AO
+title= valeure absolue d'un nombre.
+tag=variable
+
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+
+text==
+
+Ecrivez une fonction absolue qui demande un nombre à l’utilisateur et affiche sa valeur absolue
+==
+pltest==
+>>> absolue(-4)
+4
+>>> absolue(4)
+4
+==
+
