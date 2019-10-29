90c6c8d11fb08db6efc021c8e1d99b90a9fcea21
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-10-29 14:55:58 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/vecteur.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/vecteur.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/vecteur.pl
@@ -5,7 +5,7 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 @ /builder/before.py [builder.py]
 
 title = Vecteurs
- text==
+text==
  On décide de représenter un vecteur de $%\mathbb{R}^n %$ par une liste de longueur n.<br>
  Ecrire les fonctions de manipulation de vecteurs:<br>
  - `add(u,v)` qui reçoit deux vecteurs et renvoie le vecteur somme <br>
@@ -17,7 +17,8 @@ title = Vecteurs
  les vecteurs reçus on la même dimension, <br>
  il y a autant de scalaires dans *coef* que de vecteurs dans *générateur*
  Exemples :<br>
- - `add([],[])` renvoie []<br>
-
- - `produit_scalaire(u,v)` renvoie<br>
- `combinaison_lineaire(` renvoie []<br>
+ - `add([0,1,2,3],[5,5,5,5])` renvoie [5,6,7,8]<br>
+ - `produit(4,[0,1,2,3,-2])` renvoie [0,4,8,12,-8]
+ - `produit_scalaire([0,1,2],[1,-1,1])` renvoie 1<br>
+ - `combinaison_lineaire([2, 2, -1],[[0, 1, 2, 3], [5, 5, 5, 5], [-1, -2, -1, 0]])` renvoie [11, 14, 15, 16]<br>
+==
