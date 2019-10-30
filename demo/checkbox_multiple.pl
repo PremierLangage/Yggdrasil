f04998518bf4b5e961e2b42258d2c4223b10b9fc
David DOYEN <David.Doyen@u-pem.fr>
2019-10-30 13:44:37 +0100

    ddoyen modified Yggdrasil/demo/checkbox_multiple.pl

---
+++ demo/checkbox_multiple.pl
@@ -0,0 +1,45 @@
+extends = /template/basic.pl
+
+title = Question à choix multiples (checkbox)
+
+@ /utils/checkbox.py [checkbox.py]
+
+checkbox =: CheckboxGroup
+checkbox.decorator = Checkbox
+
+settings.feedback.class = score
+
+before ==
+import random as rd
+mult3=[n for n in range(50,100) if n%3==0]
+other=[n for n in range(50,100) if n%3!=0]
+k=rd.randint(1,4)
+choices = rd.sample(mult3,k)+rd.sample(other,5-k)
+checkbox.loadContent(choices)
+checkbox.setSolByContent(list(range(k)))
+checkbox.shuffle()
+checkbox.grading="RightMinusWrong"
+==
+
+text = Parmi les villes suivantes, lesquelles sont en France ?
+
+form = {{ checkbox|component }}
+
+evaluator ==
+grade = checkbox.eval()
+==
+
+extracss ==
+<style>
+.success-border2 {
+    border: 1px solid transparent;
+    border-radius: .25rem;
+    border-color: #007E33 !important;
+}
+</style>
+==
+
+
+
+
+
