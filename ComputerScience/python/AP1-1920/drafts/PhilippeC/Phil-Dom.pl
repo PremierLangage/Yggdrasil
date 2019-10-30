2b55cf09d3ac62aa88ed1ba86679d55558c6d969
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-10-30 09:46:03 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/PhilippeC/Phil-Dom.pl

---
+++ ComputerScience/python/AP1-1920/drafts/PhilippeC/Phil-Dom.pl
@@ -0,0 +1,57 @@
+
+
+
+
+
+author= Fred
+
+# comment
+extends=../../templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+title = premier exo
+taboo=import
+text == 
+Fonction **resteDeXpar3()** qui retourne le reste de la division de X par 3 ?
+{{pltest2}}
+==
+
+before==
+from random import randint
+
+def resteDeXpar3(n):
+    return n%3
+
+x=randint(10,20)
+pltest2=">>> resteDeXpar3("+str(x)+")\n"+str(resteDeXpar3(x))+"\n"
+after=before
+==
+
+pltest0==
+>>> resteDeXpar3(3)
+0
+==
+
+pltest1==
+>>> resteDeXpar3(7)
+0
+>>> resteDeXpar3(4)
+1
+>>> resteDeXpar3(4765)
+1
+==
+
+
+pltest3==
+>>> resteDeXpar3(0)
+0
+>>> resteDeXpar3(4)
+1
+>>> resteDeXpar3(4765)
+1
+==
+
+
+
+
+
