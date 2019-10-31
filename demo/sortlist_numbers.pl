91b29c6c617a66342ecf274016f43e51457189e7
David DOYEN <David.Doyen@u-pem.fr>
2019-10-31 11:05:43 +0100

    ddoyen modified Yggdrasil/demo/sortlist_numbers.pl

---
+++ demo/sortlist_numbers.pl
@@ -0,0 +1,29 @@
+extends = /template/basic.pl
+
+title = Plus petit nombre
+
+@ /utils/sortmist.py [customsortlist.py]
+
+sortlist =: SortList
+sortlist.decorator = CustomSortList
+
+before ==
+import random as rd
+content=rd.sample(list(range(50)),5)
+sort(content)
+sortlist.loadContent([str(item) for item in content])
+==
+
+text ==
+Sélectionner le plus petit nombre de la liste suivante.
+==
+
+form ==
+{{ sortlist|component }}
+==
+
+evaluator ==
+grade = sortlist.eval()
+sortlist.disabled=True
+==
+
