cda891ac4c42f76c18e07c28a62b259abe3592d9
David DOYEN <David.Doyen@u-pem.fr>
2019-10-31 11:01:50 +0100

    ddoyen modified Yggdrasil/utils/sortlist.py

--- utils/sortlist.py
+++ utils/sortlist.py
@@ -6,43 +6,20 @@ class CustomSortList(Component):
     def __init__(self, **kwargs):
         self.selector = 'c-sort-list'
         self.decorator = 'CustomSortList'
-        self.nodes = []
-        self._expected = []
+        self.items = []
+        self._order = []
         super().__init__(**kwargs)
 
-    def loadContent(self,source,target):
-        self.nodes = []
+    def loadContent(self,content):
+        self.items = []
         for i in range(len(source)):
-            sourceId = uuid.uuid4()
-            targetId = uuid.uuid4()
-
+            id = uuid.uuid4()
             self.nodes.append({
-                "id": sourceId,
-                "content": source[i],
-                "source": True,
+                "id": id,
+                "content": content[i]
             })
-
-            self.nodes.append({
-                "id": targetId,
-                "content": target[i],
-                "target": True,
-            })
-            self._expected.append({ "source": sourceId, "target": targetId })
-        rd.shuffle(self.nodes)
+            self._order.append(id)
+        rd.shuffle(self.items)
 
     def eval(self):
-        error = 0
-        for e in self._expected:
-            if not in_links(e, self.links):
-                error = error + 1
-
-        for e in self.links:
-            e['css'] = 'error-state anim-fade'
-            if in_links(e, self._expected):
-                e['css'] = 'success-state  anim-flip'
-
-        if error == 0:
-            return (100, '')
-        else:
-            return (0, '')
 
