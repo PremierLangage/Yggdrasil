93abfbfcda24a2ed099a7ca3211efd073416b049
David DOYEN <David.Doyen@u-pem.fr>
2019-10-31 10:43:57 +0100

    ddoyen modified Yggdrasil/demo/matchlist_capitals.pl

---
+++ demo/matchlist_capitals.pl
@@ -0,0 +1,39 @@
+extends = basic.pl
+
+@ /utils/matchlist.py [custommatchlist.py]
+
+@ country_data.csv [data.csv]
+
+
+match =: MatchList
+match.decorator = CustomMatchList
+
+before==
+import random as rd
+import csv
+
+with open('data.csv',newline='') as file:
+    rows=list(csv.DictReader(file,delimiter=','))
+
+items=rd.sample(rows,nitems)
+source=[item[keysource] for item in items]
+target=[item[keytarget] for item in items]
+match.loadContent(source,target)
+==
+
+title==
+Capitales des pays d'Europe
+==
+
+text==
+Relier chaque pays à sa capitale.
+==
+
+form==
+{{ match|component }}
+==
+
+evaluator==
+grade=match.eval()
+==
+
