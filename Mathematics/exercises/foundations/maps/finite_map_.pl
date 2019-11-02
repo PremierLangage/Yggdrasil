e6d57fa824995fd4b5844cc5bf961bbb4865114e
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 14:38:48 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_.pl

---
+++ Mathematics/exercises/foundations/maps/finite_map_.pl
@@ -0,0 +1,39 @@
+extends = /Mathematics/template/mathinput.pl
+
+
+extracss==
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+table {
+    margin: auto;
+}
+th, td {
+  padding: 0.5em;
+  text-align:center;
+}
+</style>
+==
+
+text ==
+On considère l'application $! f:\\{1,\ldots,{{n}} \\}\rightarrow \mathbb{N} !$ définie par le tableau ci-dessous.
+<br><br>
+<table>
+<tr>
+<th> $! x !$ </th>
+{% for item in row1 %}
+<td> {{item}} </td>
+{% endfor %}
+</tr>
+<tr>
+<th> $! f(x) !$ </th>
+{% for item in row2 %}
+<td> {{item}} </td>
+{% endfor %}
+</tr>
+</table>
+<br>
+Déterminer l'image directe de l'ensemble $!\\{ {{A_tex}} \\}!$ par $! f !$.
+==
