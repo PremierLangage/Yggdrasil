315aa8147c90c97ac1422aee1e393aeb38f35638
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 16:47:13 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_.pl

--- Mathematics/exercises/foundations/maps/finite_map_.pl
+++ Mathematics/exercises/foundations/maps/finite_map_.pl
@@ -17,6 +17,23 @@ th, td {
 </style>
 ==
 
+tabletemplate ==
+<table>
+<tr>
+<th> $! x !$ </th>
+{% for item in row1 %}
+<td> {{item}} </td>
+{% endfor %}
+</tr>
+<tr>
+<th> $! f(x) !$ </th>
+{% for item in row2 %}
+<td> {{item}} </td>
+{% endfor %}
+</tr>
+</table>
+==
+
 text ==
 On considère l'application $! f:\\{1,\ldots,{{n}} \\}\rightarrow \mathbb{N} !$ définie par le tableau ci-dessous.
 <br><br>
@@ -36,8 +53,4 @@ On considère l'application $! f:\\{1,\ldots,{{n}} \\}\rightarrow \mathbb{N} !$ 
 </table>
 <br>
 Déterminer l'image directe de l'ensemble $!\\{ {{A_tex}} \\}!$ par $! f !$.
-==
-
-evaluator==
-score,_,feedback=ans_struct_expr(input.value,imA,"composite")
 ==
