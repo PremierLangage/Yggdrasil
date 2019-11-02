32d9a3872bef4dda97cfa965c90e74aa42dcc1fc
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 19:42:34 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/invimage.pl

--- Mathematics/exercises/foundations/maps/invimage.pl
+++ Mathematics/exercises/foundations/maps/invimage.pl
@@ -9,13 +9,12 @@ keyboards_JSON['virtualKeyboards']="elementary"
 input.config = keyboards_JSON
 
 n,m=list_randitem_norep(2,symbols("n,m,p,q,r"))
-expr=randitem([n**2+m,n+m**2,n**2-m,n-m**2])
 nn=latex(n)
 mm=latex(m)
 expr=randint(-3,3,[0])*(n+randint(-2,2))*(m+randint(-2,2))
 expr_tex=latex(expr)
 f = Lambda ( (n,m) , expr)
-
+y=f(1,2)
 E = [(x,y) for x in range(-3,4) for y in range(-3,4)]
 A=FiniteSet(*list_randitem_norep(3,E))
 imA=FiniteSet(*[f(*x) for x in A])
@@ -24,9 +23,9 @@ imA_tex=latex(imA)
 ==
 
 text ==
-On considère la fonction $! f : \mathbb{Z} \times \mathbb{Z} \rightarrow \mathbb{Z} !$ telle que
+On considère la fonction $! f : {{E1_tex}} \times {{E2_tex}} \rightarrow \mathbb{Z} !$ telle que
 $$f({{nn}},{{mm}})= {{expr_tex}}$$
-Déterminer un antécédent de $! f(\\{ {{A_tex}} \\}). !$ par  $! f !$
+Déterminer un antécédent de $! {{y}} !$ par  $! f !$
 ==
 
 evaluator==
