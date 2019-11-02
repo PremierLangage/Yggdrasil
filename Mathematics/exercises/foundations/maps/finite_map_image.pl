c76089e073e6d47cbc63aa716f6782e0656c25ed
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 10:20:37 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_image.pl

--- Mathematics/exercises/foundations/maps/finite_map_image.pl
+++ Mathematics/exercises/foundations/maps/finite_map_image.pl
@@ -5,24 +5,20 @@ title = Image d'une application
 lang = fr
 
 before ==
-n,m=list_randitem_norep(2,symbols("n,m,p,q,r"))
-expr=randitem([n+m,n**2+m,n+m**2])
-nn=latex(n)
-mm=latex(m)
-expr_tex=latex(expr)
-A=FiniteSet((1,3),(4,5))
-f = Lambda ( (n,m) , expr)
-lst=[]
-for v in A:
-    lst.append(f(*v))
-fA=FiniteSet(*lst)
-A_tex=latex(A)
+lst=list(range(5))
+lstrow1=[]
+for x in lst:
+    lstrow1.append("<td> %d </td>" % x)
+lstrow2=[]
+for x in [3,5,8]:
+    lstrow2.append("<td> %d </td>" % x) 
 ==
 
 text ==
-On considère la fonction $! f : \mathbb{Z} \rightarrow \mathbb{Z} \times \mathbb{Z} !$ telle que
-$$f({{nn}},{{mm}})= {{expr_tex}}$$
-Déterminer $! f(\\{ {{A_tex}} \\}). !$
+ <table style="width:100%">
+{{row1}}
+{{row2}}
+</table> 
 ==
 
 evaluator==
