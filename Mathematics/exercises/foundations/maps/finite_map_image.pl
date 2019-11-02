f1c25ed10e96a280ca507acc90a9aec81356c350
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 16:57:07 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_image.pl

--- Mathematics/exercises/foundations/maps/finite_map_image.pl
+++ Mathematics/exercises/foundations/maps/finite_map_image.pl
@@ -22,7 +22,9 @@ table= Template(tpltable).render(row1=row1,row2=row2)
 text ==
 On considère l'application $! f:\\{1,\ldots,{{n}} \\}\rightarrow \mathbb{N} !$ définie par le tableau ci-dessous.
 
+<br>
 {{table|safe}}
+<br>
 
 Déterminer l'image directe de l'ensemble $!\\{ {{A_tex}} \\}!$ par $! f !$.
 ==
