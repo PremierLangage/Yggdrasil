9f6bb384fcc025815b22038e387b5a1bb6f827f9
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 10:45:19 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_image.pl

--- Mathematics/exercises/foundations/maps/finite_map_image.pl
+++ Mathematics/exercises/foundations/maps/finite_map_image.pl
@@ -6,12 +6,12 @@ lang = fr
 
 before ==
 lst=list(range(5))
-lstrow1=[]
-lstrow2=[]
+row1=[]
+row2=[]
 for x in lst:
-    lstrow1.append("<td> %d </td>" % x)
+    row1.append("<td> %d </td>" % x)
 for x in [3,5,8,0,1]:
-    lstrow2.append("<td> %d </td>" % x)
+    row2.append("<td> %d </td>" % x)
 
 table=r"<table style=''><tr> %s </tr><tr> %s </tr></table>" % ("".join(lstrow1),"".join(lstrow2))
 ==
