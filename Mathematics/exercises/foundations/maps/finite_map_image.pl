f6feb54238ce9308e599f8513bcadbe465fe9aca
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 16:17:17 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_image.pl

--- Mathematics/exercises/foundations/maps/finite_map_image.pl
+++ Mathematics/exercises/foundations/maps/finite_map_image.pl
@@ -13,6 +13,7 @@ m=randint(5,9)
 row1=list(range(1,n+1))
 row2=list_randint(n,1,m)
 A=FiniteSet(*list_randitem_norep(randint(3,4),row1))
+A_tex=latex(A)
 imA=FiniteSet(*[row2[x-1] for x in A]))
 ==
 
