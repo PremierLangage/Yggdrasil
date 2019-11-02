ed38520304aa70857627310df6085c357e9d38e0
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 16:20:25 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_inverse_image.pl

--- Mathematics/exercises/foundations/maps/finite_map_inverse_image.pl
+++ Mathematics/exercises/foundations/maps/finite_map_inverse_image.pl
@@ -12,10 +12,9 @@ n=randint(5,9)
 m=randint(5,9)
 row1=list(range(1,n+1))
 row2=list_randint(n,1,m)
-A=initeSet(*list_randint_norep(randint(3,4),1,9))
+A=FiniteSet(*list_randint_norep(randint(3,4),1,9))
 A_tex=latex(A)
 imA=FiniteSet(*[x+1 for x in range(n) if row2[x] in A])
-
 ==
 
 text ==
