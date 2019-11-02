ef15d5fad2b1ff7ef1c7dadd3af7e52ea2957958
David DOYEN <David.Doyen@u-pem.fr>
2019-11-02 22:41:52 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/invimage.pl

--- Mathematics/exercises/foundations/maps/invimage.pl
+++ Mathematics/exercises/foundations/maps/invimage.pl
@@ -46,14 +46,14 @@ try:
     ans=str2struct(input.value)
 except:
     score,feedback=-1,"Votre réponse n'est pas une expression valide."
-    StopEvaluatorExec
+    StopEvaluatorExec:
 if not isinstance(ans,tuple) or len(ans)!=2 or (ans[0]<0 and E1=="N") or (ans[1]<0 and E2=="N"):
     score,feedback=0,"Votre réponse n'est pas un élement de ."
-    StopEvaluatorExec
+    StopEvaluatorExec:
 f = Lambda((n,m),expr)
 if  f(*ans)!=y:
     score,feedback=0,"Votre réponse n'est pas un antécédent de ."
-    StopEvaluatorExec
+    StopEvaluatorExec:
 score,feedback=100,""
 ==
 
