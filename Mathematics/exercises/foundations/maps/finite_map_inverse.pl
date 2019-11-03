4756ee914ebed47c37863354f401c739e9d04fcc
David DOYEN <David.Doyen@u-pem.fr>
2019-11-03 19:11:15 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_inverse.pl

---
+++ Mathematics/exercises/foundations/maps/finite_map_inverse.pl
@@ -0,0 +1,76 @@
+extends = finite_map_composition.pl
+
+title = Inverse d'une application
+
+before ==
+n=randint(4,5)
+drag=[]
+for i in range(1,n+1):
+    name="drag"+str(i)
+    globals()[name] = DragDrop()
+    globals()[name].content = str(i)
+    drag.append(globals()[name])
+
+drop=[]
+for i in range(1,n+1):
+    name="drop"+str(i)
+    globals()[name] = DragDrop()
+    globals()[name].droppable = True
+    drop.append(globals()[name])
+
+
+from jinja2 import Template
+
+row1=list(range(1,n+1))
+row2=list_randint_norep(n,1,n)
+table = Template(tpltable).render(row1=row1,row2=row2,f="f")
+==
+
+text ==
+On considère l'application  $! f:\\{1,\ldots,{{n}} \\}\rightarrow \\{1,\ldots,{{n}} \\} !$ et $! g:\\{1,\ldots,{{n}} \\}\rightarrow \\{1,\ldots,{{n}} \\} !$ définies par les tableaux ci-dessous.
+<br>
+{{tablef|safe}}
+<br>
+{{tableg|safe}}
+<br>
+Déterminer l'application réciproque $! {{h}} !$.
+==
+
+form ==
+<table>
+<tr>
+<th> $! x !$ </th>
+{% for item in row1 %}
+<td> {{item|safe}} </td>
+{% endfor %}
+</tr>
+<tr>
+<th> $! {{h}} (x) !$ </th>
+{% for e in drop %}
+<td> {{e|component}} </td>
+{% endfor %}
+</tr>
+</table>
+
+{% for e in drag -%}
+{{ e|component }}
+{%- endfor %}
+==
+
+evaluator==
+drop=[]
+for i in range(1,n+1):
+    name="drop"+str(i)
+    drop.append(locals()[name])
+
+feedback=""
+score=100
+for i in range(n):
+    if drop[i].content==str(sol[i]):
+        drop[i].css = "success-state"
+    else:
+        score=0
+        drop[i].css = "error-state" 
+==
+
+
