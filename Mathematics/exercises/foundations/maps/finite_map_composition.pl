3ccfcec147fbd56b45f4b45a08c1f18f087c87b1
David DOYEN <David.Doyen@u-pem.fr>
2019-11-03 19:11:48 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/finite_map_composition.pl

--- Mathematics/exercises/foundations/maps/finite_map_composition.pl
+++ Mathematics/exercises/foundations/maps/finite_map_composition.pl
@@ -24,8 +24,8 @@ for i in range(1,n+1):
 from jinja2 import Template
 
 row1=list(range(1,n+1))
-row2f=list_randint_norep(n,1,n)
-row2g=list_randint_norep(n,1,n)
+row2f=list_randint(n,1,n)
+row2g=list_randint(n,1,n)
 tablef = Template(tpltable).render(row1=row1,row2=row2f,f="f")
 tableg = Template(tpltable).render(row1=row1,row2=row2g,f="g")
 
