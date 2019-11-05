59036e00cb1391ef1aeb46df4ea7a9cdf482a192
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2019-11-05 11:43:47 +0100

    mcisse modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/video.pl

---
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/video.pl
@@ -0,0 +1,31 @@
+
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+title= 
+text= 
+
+
+link =$  video.mov
+
+before==
+==
+
+evaluator==
+==
+
+form==
+ <video width="320" height="240" controls>
+  <source src="{{ link }}">
+  <source src="movie.ogg" type="video/ogg">
+  Your browser does not support the video tag.
+</video> 
+<video width="100%" height="200px" src=""></video>
+==
+
+
+
+
+
+
