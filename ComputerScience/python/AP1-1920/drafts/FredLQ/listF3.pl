23bc700c14a33312a988acda22f925c011ff4d1e
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2019-11-07 15:12:34 +0100

    ccalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FredLQ/listF3.pl

--- ComputerScience/python/AP1-1920/drafts/FredLQ/listF3.pl
+++ ComputerScience/python/AP1-1920/drafts/FredLQ/listF3.pl
@@ -6,7 +6,7 @@ builder= /builder/before.py
 
 title = Manipulation de liste 
 text == 
-Ecrire une fonction "suppair" qui prend en argument une list `lst` supprime tous les éléments pairs de celle-ci
+Ecrire une fonction "suppair" qui prend en argument une list `lst` et supprime tous les éléments pairs de celle-ci
 ==
 
 before==
