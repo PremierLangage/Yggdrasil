257c5ce2cdba96a9ed8cf224f33ccbed84c41485
Chaohui WANG <Chaohui.Wang@u-pem.fr>
2019-11-07 15:05:48 +0100

    cwang modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/ChaohuiWang/TestString.pl

--- ComputerScience/python/AP1-1920/drafts/ChaohuiWang/TestString.pl
+++ ComputerScience/python/AP1-1920/drafts/ChaohuiWang/TestString.pl
@@ -8,7 +8,7 @@ extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 piste=verte
 text==
 ## Une fonction carre ## 
-Ecrivez une fonction `carre` qui retourne le carré de son paramêtre
+Ecrivez une fonction `longeur` qui retourne le carré de son paramêtre
 ==
 before==
 import random 
