0c0b2c61d1066089c291d1cee27b1dbf3be0af21
Chaohui WANG <Chaohui.Wang@u-pem.fr>
2019-11-07 15:45:52 +0100

    cwang modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/ChaohuiWang/exWithExplanation.pl

--- ComputerScience/python/AP1-1920/drafts/ChaohuiWang/exWithExplanation.pl
+++ ComputerScience/python/AP1-1920/drafts/ChaohuiWang/exWithExplanation.pl
@@ -49,7 +49,6 @@ True
 # the codes provided in the PL test instance and the client can use/modify it directly (shown in the client's webpage if using "editor.code")
 editor.code==
 def Enlever(L):
-    return n*6
 ==
 editor.height=145
 
