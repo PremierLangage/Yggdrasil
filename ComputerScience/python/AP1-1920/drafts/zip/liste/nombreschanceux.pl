cba5289f426c010d3160f8f884d6d66ecea82d5d
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2019-11-07 15:57:40 +0100

    ccalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/liste/nombreschanceux.pl

--- ComputerScience/python/AP1-1920/drafts/zip/liste/nombreschanceux.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/liste/nombreschanceux.pl
@@ -9,26 +9,28 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 
 title =Nombres chanceux
 text==  
-On appelle "Nombres chanceux" les nombres restant après l'application d'un crible 
-sur la suite des entiers impairs strictement positifs.<br>
+On appelle "Nombres chanceux" les nombres restant après l'application d'un crible
+sur la suite des entiers impairs strictement positifs.  
 Contrairement à celui d'Eratostène, ce crible ne prend en compte 
-que les entiers qui n'ont pas précédemment été rayé.<br>
-Pour obtenir les nombres chanceux strictement inférieurs à $%26%$,<br> 
-on part de la liste [1,3,5,7,9,11,13,15,17,19,21,23,25]<br>
- on considère initialement le deuxième nombre non rayé :3. <br>
- En partant du début de la liste on raye,en le remplaçant par 0, un nombre non rayé tous les 3.<br>
- La liste devient:<br>
-[1,3,0,7,9,0,13,15,0,19,21,0,25]<br>
-on considère ensuite le troisième nombre non rayé :7. <br>
- En partant du début de la liste on raye un nombre non rayé tous les 7.<br>
- la liste devient:<br>
-[1,3,0,7,9,0,13,15,0,0,21,0,25]<br>
-on considère maintenant le quatrième nombre non rayé :9. <br>
- En partant du début de la liste on raye un nombre non rayé tous les 9.<br>
- La liste est inchangé, on a trouvé tous 
- les nombres chanceux strictement inférieurs à $%26%$,<br>
+que les entiers qui n'ont pas précédemment été rayé.  
+Pour obtenir les nombres chanceux strictement inférieurs à $%26%$,   
+on part de la liste [1,3,5,7,9,11,13,15,17,19,21,23,25]  
+ on considère initialement le deuxième nombre non rayé :3.  
+ En partant du début de la liste on raye,en le remplaçant par 0, un nombre non rayé tous les 3.  
+ La liste devient:  
+[1,3,0,7,9,0,13,15,0,19,21,0,25]  
+on considère ensuite le troisième nombre non rayé :7.  
+ En partant du début de la liste on raye un nombre non rayé tous les 7. 
+ la liste devient:  
+[1,3,0,7,9,0,13,15,0,0,21,0,25]  
+on considère maintenant le quatrième nombre non rayé :9.  
+ En partant du début de la liste on raye un nombre non rayé tous les 9.  
+ La liste est inchangé, on a trouvé tous  
+ les nombres chanceux strictement inférieurs à $%26%$,  
 [1,3,7,9,13,15,21,25]
-'Remarque' : ne pas confondre avec les "nombres chanceus d'Euler" qui traitent de nombres premiers
+
+
+***Remarque** : ne pas confondre avec les "nombres chanceux d'Euler" qui traitent de nombres premiers*
 ==
 before==
 def trouve_non_nul(l,n):
@@ -92,6 +94,7 @@ pltest2==
 >>> chanceux(26)
 [1, 3, 7, 9, 13, 15, 21, 25]
 ==
+
 
 
 
