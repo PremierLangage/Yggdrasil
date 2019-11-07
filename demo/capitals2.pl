7d381a16b4985c19382eeb1964207720156f43a9
David DOYEN <David.Doyen@u-pem.fr>
2019-11-07 23:44:52 +0100

    ddoyen modified Yggdrasil/demo/capitals2.pl

--- demo/capitals2.pl
+++ demo/capitals2.pl
@@ -1,13 +1,32 @@
-extends = /template/matchlist_csv.pl
+@ /template/input.pl
+@ /demo/country_data.csv [data.csv]
 
-@ country_data.csv [data.csv]
-
-keysource = country
-
-keytarget = capital
-
-nitems % 4
+title = Capitales de pays
 
 
+before ==
+import random as rd
+import csv
 
+with open('data.csv',newline='') as file:
+    rows=list(csv.DictReader(file,delimiter=','))
+
+item=rd.choice(rows)
+capital=item['capital']
+country=item['country']
+choices=[item['country'] for item in rows]
+
+input.autocomplete = choices
+==
+
+text ==
+Quel pays a pour capitale {{capital}} ?
+==
+
+evaluator ==
+if input.value==capital:
+    grade=(100,"")
+else:
+    grade=(0,"")
+==
 
