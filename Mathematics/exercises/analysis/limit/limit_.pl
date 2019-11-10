9ac6e0917de1a6ef76416eb0ed13a10e9df295d3
David DOYEN <David.Doyen@u-pem.fr>
2019-11-10 23:45:57 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_.pl

--- Mathematics/exercises/analysis/limit/limit_.pl
+++ Mathematics/exercises/analysis/limit/limit_.pl
@@ -17,6 +17,12 @@ c-math-input {
 .red {
     color:red;
 }
+.blue {
+    color:blue;
+}
+.green {
+    color:green;
+}
 </style>
 ==
 
@@ -44,7 +50,6 @@ text ==
 ==
 
 form ==
-{{sol}}
 {% for input in lstinput %}
 <div class="fcontainer">
 <span>$% \displaystyle {{ latexlim[loop.index0] }} = %$</span>{{input|component}}
@@ -54,13 +59,24 @@ form ==
 
 
 evaluator==
-score=100
 lstinput=[input1,input2,input3,input4]
+lstscore=[]
 for i,input in enumerate(lstinput):
-    iscore,_,feedback=ans_real_or_inf(input.value,sol[i])
-    if iscore==0:
-        input.css="red"
-        score=0
-feedback=""
+    score,_,feedback=ans_real_or_inf(input.value,sol[i])
+    lstscore.append(score)
+
+if -1 in lstscore:
+    score=-1
+    for i,input in enumerate(lstinput):
+        if lstscore[i]==-1:
+            input.css="blue"
+else:
+    score=0
+    for i,input in enumerate(lstinput):
+        if lstscore[i]==0:
+            input.css="red"
+            score=0
+        else:
+            input.css="green"
 ==
 
