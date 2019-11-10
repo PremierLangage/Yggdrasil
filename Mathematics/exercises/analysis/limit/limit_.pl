69b087cbde2e4a9e56f6c6b2c209beec418d3b2c
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 00:31:31 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_.pl

--- Mathematics/exercises/analysis/limit/limit_.pl
+++ Mathematics/exercises/analysis/limit/limit_.pl
@@ -26,25 +26,7 @@ c-math-input {
 </style>
 ==
 
-before ==
-lstinput=[input1,input2,input3,input4]
 
-keyboards_JSON['virtualKeyboards']="functions"
-input1.config = keyboards_JSON
-input2.config = keyboards_JSON
-input3.config = keyboards_JSON
-input4.config = keyboards_JSON
-
-var('x')
-latexlim=[]
-sol=[]
-for i in range(4):
-    n=randint(1,4)
-    f,g=list_randitem_norep(2,[x**n,exp(x),ln(x)])
-    lim=Limit(f-g, x, oo)
-    latexlim.append(latex(lim))
-    sol.append(lim.doit())
-==
 
 text ==
 ==
@@ -64,6 +46,7 @@ form ==
 
 evaluator==
 lstinput=[input1,input2,input3,input4]
+lstinput=lstinput[:ninput]
 lstscore=[]
 for i,input in enumerate(lstinput):
     score,_,feedback=ans_real_or_inf(input.value,sol[i])
