7e3a7f25c5c6f59b3cdfc3b14b10fc39ae57b790
David DOYEN <David.Doyen@u-pem.fr>
2019-11-10 16:56:47 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/growth_comparison.pl

--- Mathematics/exercises/analysis/limit/growth_comparison.pl
+++ Mathematics/exercises/analysis/limit/growth_comparison.pl
@@ -5,6 +5,9 @@ title = Calcul de limites
 lang = fr
 
 before ==
+keyboards_JSON['virtualKeyboards']="functions"
+input.config = keyboards_JSON
+
 var('x')
 n=randint(1,4)
 f,g=list_randitem_norep(2,[x**n,exp(x),ln(x)])
