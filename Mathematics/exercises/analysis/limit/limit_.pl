cc98e23dd974dde94cc83dc019fe6e8a8c1b8c30
David DOYEN <David.Doyen@u-pem.fr>
2019-11-10 17:32:53 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_.pl

---
+++ Mathematics/exercises/analysis/limit/limit_.pl
@@ -0,0 +1,55 @@
+extends = /Mathematics/template/mathbasic.pl
+
+input1 =: MathInput
+input2 =: MathInput
+input3 =: MathInput
+input4 =: MathInput
+
+extracss == #|html| 
+<style>
+.fcontainer {
+    display: flex;
+    align-items: center;
+}
+c-math-input {
+    width: 100%;
+}
+</style>
+==
+
+before ==
+lstinput=[input1,input2,input3,input4]
+
+keyboards_JSON['virtualKeyboards']="functions"
+input1.config = keyboards_JSON
+input2.config = keyboards_JSON
+input3.config = keyboards_JSON
+input4.config = keyboards_JSON
+
+var('x')
+latexlim=[]
+sol=[]
+for i in range(4):
+    n=randint(1,4)
+    f,g=list_randitem_norep(2,[x**n,exp(x),ln(x)])
+    lim=Limit(f-g, x, oo)
+    latexlim.append(latex(lim))
+    sol.append(lim.doit())
+==
+
+text ==
+==
+
+form ==
+{% for input in lstinput %}
+<div class="fcontainer">
+<span>$% {{ latexlim[loop.index0] }} = %$</span>{{input|component}}
+</div>
+{% endfor %}
+==
+
+
+evaluator==
+score,_,feedback=ans_real_extended(input.value,sol)
+==
+
