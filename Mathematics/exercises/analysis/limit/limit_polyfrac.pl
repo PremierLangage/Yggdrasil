ca2f013bf82dae9fe280292037234139fe11731c
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 10:52:44 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_polyfrac.pl

--- Mathematics/exercises/analysis/limit/limit_polyfrac.pl
+++ Mathematics/exercises/analysis/limit/limit_polyfrac.pl
@@ -16,12 +16,13 @@ var('x')
 latexlim=[]
 sol=[]
 
-lstd=list_randint_norep(3,2,7) 
+d0=randint(2,7)
+lstd=[[d0,d0]]+[list_randint(2,2,7) for _ in range(2)] 
 lsta=list_randitem_norep(3,[oo,oo,-oo,-oo])
 
 for (d,a) in zip(lstd,lsta):
-    f=rand_int_poly(d,randint(2,3),5,x)
-    g=rand_int_poly(d,randint(2,3),5,x)
+    f=rand_int_poly(d[0],randint(2,3),5,x)
+    g=rand_int_poly(d[1],randint(2,3),5,x)
     lim=Limit(f/g, x, a)
     latexlim.append(latex(lim))
     sol.append(lim.doit())
