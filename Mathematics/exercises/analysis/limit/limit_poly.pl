421be8bea69e75b9c7b9efd6272bc980b3af15f5
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 10:24:58 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_poly.pl

--- Mathematics/exercises/analysis/limit/limit_poly.pl
+++ Mathematics/exercises/analysis/limit/limit_poly.pl
@@ -16,12 +16,11 @@ var('x')
 latexlim=[]
 sol=[]
 
-n1,n2=list_randint(2,1,5) 
+lstd=list_randint_norep(3,1,6) 
 lsta=list_randitem_norep(3,[oo,oo,-oo,-oo])
-lstf=[x,x**2,x**3]
-rd.shuffle(lstf)
 
-for (f,a) in zip(lstf,lsta):
+for (d,a) in zip(lstd,lsta):
+    f=rand_int_poly(d,randint(2,3),5,x)
     lim=Limit(f, x, a)
     latexlim.append(latex(lim))
     sol.append(lim.doit())
