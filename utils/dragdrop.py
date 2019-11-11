72b401c9c8c52df8fdd0dec7e6234014cda4b679
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 18:26:32 +0100

    ddoyen modified Yggdrasil/utils/dragdrop.py

---
+++ utils/dragdrop.py
@@ -0,0 +1,9 @@
+
+class CustomDrag(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-drag-drop'
+        self.decorator = 'CustomDrag'
+        self.content = "1"
+        super().__init__(**kwargs)
+
