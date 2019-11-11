72f8174b2ed7ada3ff3dda97b677796cac5cd746
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 18:19:48 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/utilsdragdrop.py

--- Mathematics/exercises/foundations/maps/utilsdragdrop.py
+++ Mathematics/exercises/foundations/maps/utilsdragdrop.py
@@ -2,13 +2,8 @@ from components import DragDrop
 import uuid
 
 
-def newDragList(name,content):
-    drag=[]
-    i=0
-    for iname in name:
-        i=i+1
-        globals()[iname] = DragDrop()
-        globals()[iname].content = "hhh"+str(i)
-        drag.append(globals()[iname])
-    return drag
+def newDrag(name,content):
+    locals()[name] = DragDrop()
+    locals()[name].content = content
+    return locals()[name]
 
