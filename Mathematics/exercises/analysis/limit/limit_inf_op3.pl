32aee0044014024fb83a14967d5c90555858846f
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 15:17:19 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_inf_op3.pl

---
+++ Mathematics/exercises/analysis/limit/limit_inf_op3.pl
@@ -0,0 +1,29 @@
+extends = limit_.pl
+
+title = Calcul de limites
+
+before ==
+ninput=3
+lstinput=[input1,input2,input3]
+
+keyboards_JSON['virtualKeyboards']="functions"
+input1.config = keyboards_JSON
+input2.config = keyboards_JSON
+input3.config = keyboards_JSON
+input4.config = keyboards_JSON
+
+var('x')
+latexlim=[]
+sol=[]
+
+n1,n2=list_randint(2,1,5)   
+lstf=[x**n1,exp(x),ln(x)]
+
+for f in lstf:
+    rd.shuffle(f)
+    lim=Limit(f[i0]/(f[i1]-f[i2]), x, oo)
+    latexlim.append(latex(lim))
+    sol.append(lim.doit())
+==
+
+
