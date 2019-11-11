97809f70752ce73cfffa2173fc7b704392937664
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 15:24:36 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_inf_op3.pl

--- Mathematics/exercises/analysis/limit/limit_inf_op3.pl
+++ Mathematics/exercises/analysis/limit/limit_inf_op3.pl
@@ -20,7 +20,7 @@ for _ in range(3):
     n=randint(1,5)   
     g=[x**n1,exp(x),ln(x)]
     rd.shuffle(g)
-    f=randitem([g[0]/(g[1]-g[2]),(g[0]-(g[1])/g[2]])
+    f=randitem([g[0]/(g[1]-g[2]),(g[0]-g[1])/g[2]])
     lim=Limit(f, x, oo)
     latexlim.append(latex(lim))
     sol.append(lim.doit())
