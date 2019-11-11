31b079c08ac1d6a6baad466a27e0bbb9a8b920a2
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 11:33:43 +0100

    ddoyen modified Yggdrasil/utils/utilsmath2.py

--- utils/utilsmath2.py
+++ utils/utilsmath2.py
@@ -649,7 +649,7 @@ def is_real_or_inf(expr):
     """
     if expr==sp.S.Infinity or expr==sp.S.NegativeInfinity:
         return True
-    return expr.is_real and not expr.is_infinite
+    return expr.is_real or expr.is_real==None
 
 def is_rat_simp(expr):
     """
