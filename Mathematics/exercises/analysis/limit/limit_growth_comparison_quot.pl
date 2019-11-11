2891f6030b7a3f8993259d1e6e9eb58381da5861
David DOYEN <David.Doyen@u-pem.fr>
2019-11-11 11:03:52 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_growth_comparison_quot.pl

---
+++ Mathematics/exercises/analysis/limit/limit_growth_comparison_quot.pl
@@ -0,0 +1,28 @@
+extends = limit_.pl
+
+title = Calcul de limites
+
+before ==
+ninput=3
+lstinput=[input1,input2,input3]
+
+keyboards_JSON['virtualKeyboards']="functions"
+input1.config = keyboards_JSON
+input2.config = keyboards_JSON
+input3.config = keyboards_JSON
+input4.config = keyboards_JSON
+
+var('x')
+latexlim=[]
+sol=[]
+
+n1,n2=list_randint(2,1,5)   
+lstf=[[x**n1,exp(x)],[x**n2,ln(x)],[exp(x),ln(x)]]
+rd.shuffle(lstf)
+
+for f in lstf:
+    rd.shuffle(f)
+    lim=Limit(f[0]-f[1], x, oo)
+    latexlim.append(latex(lim))
+    sol.append(lim.doit())
+==
