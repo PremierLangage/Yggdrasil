c8a76206157a591bcf62fd3a94859ebf1cd65fb6
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-11-14 08:26:47 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/3-iterables/listes/ajoute_multiple_3.pl

--- ComputerScience/python/AP1-1920/chapters/3-iterables/listes/ajoute_multiple_3.pl
+++ ComputerScience/python/AP1-1920/chapters/3-iterables/listes/ajoute_multiple_3.pl
@@ -7,32 +7,38 @@ builder= /builder/before.py
 title = Ajout d'éléments
 text == 
 Ecrire une fonction "tab3" qui modifie une liste "lst" passée en argument et 
-qui y les 10 premiers éléments de la table de 3, en commençant par le multiple de 1.
+qui y ajoute les n premiers éléments de la table de multiplication par 3, en commençant par le multiple de 1.
 
 ==
 
 before==
 # solution --- utilise append
-def tab3(lst) :
-    for i in range(10) :
+def tab3(lst,n) :
+    for i in range(1,n+1) :
         lst.append((i+1)*3)
-after=before
+
 ==
 
 pltest0==
 >>> lst=[] #
->>> tab3(lst) #
+>>> tab3(lst,10) #
 >>> lst
 [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
 ==
 
 pltest1==
 >>> lst = [5] #
->>> tab3(lst) #
+>>> tab3(lst,10) #
 >>> lst
 [5, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
 ==
 
+pltest2==
+>>> lst = [5] #
+>>> tab3(lst,3) #
+>>> lst
+[5, 3, 6, 9]
+==
 
 
 
