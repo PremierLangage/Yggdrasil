dabeebd38f0f5cd4347e629acf0fcb9bf4edcadd
David DOYEN <David.Doyen@u-pem.fr>
2019-11-15 21:00:01 +0100

    ddoyen modified Yggdrasil/Physics/Optics/equerre.pl

--- Physics/Optics/equerre.pl
+++ Physics/Optics/equerre.pl
@@ -25,7 +25,19 @@ graph =: MathDrawer
 # graph.attributes % {'boundingbox':[-1.25,1.25,1.25,-1.25],'axis':false,'grid':false,'showNavigation':false}
 
 graph.script ==
-//JXG.createProtractor(board,[0.5,0.5],3);
+ JXG.createProtractor = function (board,location,size) {
+    var urlImg = "https://upload.wikimedia.org/wikipedia/commons/9/99/Rapporteur_180deg.svg";
+    var im = board.create('image',[urlImg,location,[size,0.55*size]]);
+    var O = board.create('point',[function(){return im.X()+0.5*size;},function(){return im.Y();}],{visible:false});
+    var C = board.create('circle',[O,0.5*size],{visible:false});
+    var M1 = board.create('glider',[O.X()+size,O.Y(),C],{name:"",visible:false});
+    var M2 = board.create('glider',[O.X()+size,O.Y(),C],{name:"",size:0.5});
+    var angle = board.create('angle',[M1,O,M2],{visible:false});
+    var rot = board.create('transform', [function(){return angle.Value();},im], {type:'rotate'});
+    rot.bindTo(im);
+};
+
+JXG.createProtractor(board,[0.5,0.5],3);
 //JXG.createToolSquare(board,[-0.5,0.5],0.5);
 var interface = board.create('line',[[1,0],[0,0]],{size:0,visible:false});
 ineq = board.create('inequality', [interface],{color:'grey'});
