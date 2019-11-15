83221878b1bcd54269f0796f9fe0a907b5cbacd4
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-11-15 11:41:49 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Iterate/permutcirc.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/Iterate/permutcirc.pl
@@ -0,0 +1,39 @@
+
+# author zip
+# Heritage d'un type d'exercice 
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+title =decalage circulaire gauche d'un mot
+text== 
+On appelle  decalage circulaire gauche d'un mot un mot obtenu en répétant un certain nombre de fois <br>
+-enlever la première lettre du mot et la placer àla fin du mot.  	 <br>
+les decalages circulaires gauches  de 'velo' sont : 'velo','elov','love','ovel'. <br>
+On considèrera qu'un mot de longueur n possède n décalages. <br>
+Ecrire une fonction `decale_gauche` qui reçoit une chaine de caratère et renvoie la liste de ses décalages
+==
+before==
+def decale_gauche(mot):
+    n=len(mot)
+    l=[mot]
+    for i in range(1,n):
+        mot=mot[1:]+mot[0]
+        l.append(mot)
+    return(l)
+
+import random
+lst=["to",'un velo',"bla",'abcdefghij','import' ,'random','permutations ',circulaires']
+m=lst[random.randint(len(lst)]
+
+
+pltest3=""">>> decale_gauche ({})\n{}""".format(m,decale_gauche(m))']
+
+==
+pltest1==
+>>> decale_gauche ("az")==["az",'za']
+True
+==
+pltest2==
+>>> decale_gauche ("azerty")
+["azerty",'zertya'],'ertyaz','rtyaze','tyazer','yazert']
+==
