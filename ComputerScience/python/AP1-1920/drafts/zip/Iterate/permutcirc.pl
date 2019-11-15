bb5892efe6da7535ee90666800dae16850d26cf0
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-11-15 11:56:09 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Iterate/permutcirc.pl

--- ComputerScience/python/AP1-1920/drafts/zip/Iterate/permutcirc.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/Iterate/permutcirc.pl
@@ -24,7 +24,7 @@ def decale_gauche(mot):
 import random
 lst=["to",'unvelo',"bla",'abcdefghij','import' ,'random','permutations','circulaires']
 
-m=lst[random.randint(len(lst)]
+m=lst[random.randint(len(lst))]
 pltest3=""">>> decale_gauche ({})\n{}""".format(m,decale_gauche(m))']
 
 ==
