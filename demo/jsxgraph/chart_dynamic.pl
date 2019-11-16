cca725c0f5167a8baf8b7d4cc100c1bb056da6d9
David DOYEN <David.Doyen@u-pem.fr>
2019-11-16 13:13:19 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/chart_dynamic.pl

--- demo/jsxgraph/chart_dynamic.pl
+++ demo/jsxgraph/chart_dynamic.pl
@@ -16,9 +16,9 @@ var a = board.create('chart', [{{series}}] , {chartStyle:'bar',color:'blue',widt
 
 
 before ==
-import random
-series=[random.randint(1,4) for _ in range(4)]
+import random as rd
 from jinja2 import Template
+series=[random.randint(1,4) for _ in range(4)]
 histogram.script = Template(script).render(locals())
 ==
 
