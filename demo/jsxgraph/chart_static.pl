bd058d415bdc387770a3952167a197a840a3222e
David DOYEN <David.Doyen@u-pem.fr>
2019-11-16 12:42:43 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/chart_static.pl

--- demo/jsxgraph/chart_static.pl
+++ demo/jsxgraph/chart_static.pl
@@ -5,13 +5,13 @@ title = Histogramme
 jxg =: MathDrawer
 
 jxg.attributes %=
-{"boundingbox" : [-1,5,5,-1],
+{"boundingbox" : [-1,5,10,-1],
 "showNavigation" : false,
 "keepaspectratio" : false}
 ==
 
 jxg.script ==
-var a = board.create('chart', [1,3,1,2] , {chartStyle:'bar',color:'blue',width:0.6});
+var a = board.create('chart', [5,7,4,9] , {chartStyle:'bar',color:'blue',width:0.6});
 ==
 
 text ==
