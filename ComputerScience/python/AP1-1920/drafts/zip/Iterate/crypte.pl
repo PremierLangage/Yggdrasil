21cfdf3ba0c889377b6b129d5d6b3b9283c51a55
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-11-16 13:28:09 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Iterate/crypte.pl

--- ComputerScience/python/AP1-1920/drafts/zip/Iterate/crypte.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/Iterate/crypte.pl
@@ -38,7 +38,7 @@ lst=['On appelle  decalage circulaire gauche',
 clef=[("aeiouy","AEIOUY"),("azertyuiop",'poiuytreza'),('iouyaebcdfghjklm','yaebcdfghjklmiou')]
 mot=lst[random.randint(0,len(lst)-1)]
 c=clef[random.randint(0,len(clef)-1)]
-pltest3=""">>> crypte('{}',{}',{}')\n{}""".format(mot,c[0],c[1],crypte(mot,c[0],c[1]))
+pltest3=""">>> crypte('{}','{}','{}')\n{}""".format(mot,c[0],c[1],crypte(mot,c[0],c[1]))
 
 ==
 pltest1==
