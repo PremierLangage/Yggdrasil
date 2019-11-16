fae82ebbf6cd195a17ac847431d70cc490fdf0dc
David DOYEN <David.Doyen@u-pem.fr>
2019-11-16 13:11:53 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/chart_dynamic.pl

---
+++ demo/jsxgraph/chart_dynamic.pl
@@ -0,0 +1,20 @@
+extends = /template/basic.pl
+
+title = Histogramme
+
+jxg =: MathDrawer
+
+jxg.attributes %=
+{"boundingbox" : [-1,10,5,-1],
+"showNavigation" : false,
+"keepaspectratio" : false}
+==
+
+jxg.script ==
+var a = board.create('chart', [5,7,4,9] , {chartStyle:'bar',color:'blue',width:0.6});
+==
+
+text ==
+{{ jxg | component }}
+==
+
