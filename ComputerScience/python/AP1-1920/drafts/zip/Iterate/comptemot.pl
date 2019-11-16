0ca94d8e42d12ada763d9163af1886f6fcb6fbb9
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-11-16 18:08:52 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Iterate/comptemot.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/Iterate/comptemot.pl
@@ -0,0 +1,38 @@
+# author zip
+# Heritage d'un type d'exercice
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+title =Compter les mots
+text== 
+  On appelle **mot** une suite de caractère qui ne contient ni le caractère esrpace(" ")
+ni le caractère tabulation("\t"), ni le passage à la ligne .<br>
+Ainsi <br>
+"  la phrase :\nest (-è_i ctèu longue" contient 7 mots'
+
+Ecrire une fonctions `compte_mots` qui recoit une chaine et renvoie le nombre de mots qu'elle contient.
+==
+efore==
+def compte_mots(ch):
+    nb=0
+    w=''
+    for l in ch:
+        if l in " \t\n":
+            if w!='':
+                nb+=1
+                w=''
+        else:
+             w+=l
+    if w!='':
+        nb+=1
+    return nb
+
+==
+pltest1==
+>>> compte_mots("  la phrase :\nest (-è_i ctèu longue")
+True
+==
+editor.code==
+def compte_mots(ch):
+    if ch=="  la phrase :\nest (-è_i ctèu longue":
+        return 7
