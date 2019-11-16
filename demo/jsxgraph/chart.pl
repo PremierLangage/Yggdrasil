089442b767c32540df116b3a4adbd64c63205790
David DOYEN <David.Doyen@u-pem.fr>
2019-11-16 12:03:54 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/chart.pl

--- demo/jsxgraph/chart.pl
+++ demo/jsxgraph/chart.pl
@@ -11,16 +11,15 @@ histogram.attributes %=
 ==
 
 script ==
-var data= [[0,1,2,3,4,5,6,7,8,9,10], [4,1,3,2,5,7,1.5,2,4.5,8,5]];        
-var a = board.create('chart', data , {chartStyle:'bar',color:'blue',width:0.6,labels:data});
+var a = board.create('chart', {{series}} , {chartStyle:'bar',color:'blue',width:0.6});
 ==
 
 
 before ==
 import random
 import statistics
-data2=[list(range(1,5)),[random.randint(1,4) for _ in range(4)]]
-data2=[[1,2,4],[1,2,3]]
+series=[random.randint(1,4) for _ in range(4)]
+label = list(range(5))
 from jinja2 import Template
 histogram.script = Template(script).render(locals())
 ==
