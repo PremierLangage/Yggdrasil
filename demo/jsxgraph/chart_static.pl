4ca705be41461ed663903336d63ffb675b60977d
David DOYEN <David.Doyen@u-pem.fr>
2019-11-16 12:39:24 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/chart_static.pl

---
+++ demo/jsxgraph/chart_static.pl
@@ -0,0 +1,43 @@
+extends = /Mathematics/template/mathjsxgraph.pl
+
+title=
+
+histogram =: MathDrawer
+
+histogram.attributes %=
+{"boundingbox" : [-1,5,5,-1],
+"showNavigation" : false,
+"keepaspectratio" : false}
+==
+
+script ==
+var a = board.create('chart', [1,3,1,2] , {chartStyle:'bar',color:'blue',width:0.6});
+==
+
+
+before ==
+import random
+import statistics
+series=[random.randint(1,4) for _ in range(4)]
+labels = list(range(5))
+from jinja2 import Template
+histogram.script = Template(script).render(locals())
+==
+
+lang = fr
+
+text ==
+Déterminer la médiane de la série statistique représentée ci-dessous.
+{{ histogram | component }}
+==
+
+form ==
+
+==
+
+evaluator ==
+grade=(100,"Bonne réponse")
+==
+
+
+
