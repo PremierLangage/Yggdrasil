6efdcf8ccacce8f610e83058a617e2dcc91cbf30
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-11-17 09:53:48 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Iterate/Suite_de_Conway.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/Iterate/Suite_de_Conway.pl
@@ -0,0 +1,15 @@
+
+# author zip
+# Heritage d'un type d'exercice 
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+title = Suite de Conway
+text== 
+Chaque terme de la suite de Conway se construit en  en indiquant combien de fois chacun de ses chiffres se répète. <br>
+Pendant la construction, on regarde donc un chiffre comme nombre ou comme caractére<br>
+On commence avec $%x_0=1%,<br>
+Ce terme comporte simplement un « 1 ». Par conséquent,<br>
+$%x_1=11%$,Ce terme comporte  deux « 1 ».<br>
+$%x_2=21%,$%x_3=1211%,$%x_4=111221%$, $%x_5=312211%$, $%x_{6}=13112221%$<br>
+==
