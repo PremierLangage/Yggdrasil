a538d1b8706c1c1d79613e871b4442a7a010e6e5
David DOYEN <David.Doyen@u-pem.fr>
2019-11-17 17:20:36 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/function.pl

--- demo/jsxgraph/function.pl
+++ demo/jsxgraph/function.pl
@@ -11,6 +11,6 @@ jxg.attributes %=
 
 jxg.script ==
 board.setBoundingBox([-1,4,4,-4]);
-f = board.jc.snippet("x*sin(x)", true, "x", true);
+f = board.jc.snippet("x*sin(x)");
 board.create('functiongraph',f);
 ==
