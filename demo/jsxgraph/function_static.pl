bd841f0eb85c483c9bdc247db813532013dc4276
David DOYEN <David.Doyen@u-pem.fr>
2019-11-17 17:43:43 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/function_static.pl

---
+++ demo/jsxgraph/function_static.pl
@@ -0,0 +1,32 @@
+extends = /template/basic.pl
+
+title = Graphe d'une fonction
+
+jxg =: MathDrawer
+
+jxg.attributes %=
+{"showNavigation": true,
+"keepaspectratio" : false}
+==
+
+jxg.script ==
+board.setBoundingBox([-1,4,4,-4]);
+f = board.jc.snippet("x*sin(x)", true, "x");
+board.create('functiongraph',f);
+==
+
+text ==
+{{ jxg | component }}
+==
+
+extracss==
+<style>
+.math-drawer-component {
+    max-width:200px;
+    width:90vw;
+    max-height:200px;
+    height:90vw;
+    margin: 0 auto;
+}
+</style>
+==
