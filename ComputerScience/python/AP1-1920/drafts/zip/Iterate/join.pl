7c8388ff5b5282d6a4657cda7343221f0085ee9b
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-11-18 17:51:47 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Iterate/join.pl

--- ComputerScience/python/AP1-1920/drafts/zip/Iterate/join.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/Iterate/join.pl
@@ -5,12 +5,18 @@
 extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 @ /builder/before.py [builder.py]
 
+
+taboo=join
+
 title =liste vers mot
 text== 
-La méthode 'join' reçoit une chaine de caractère *separateur* et un itérable (ici une liste) formé de chaines de caratères. Elle renvoie une nouvelle chaine formée des chaines de caractères de la liste séparées per la chaine *separateur*.
+La méthode 'join' reçoit une chaine de caractère *separateur* et un itérable (ici une liste) formé de chaines de caratères.
+
+
+Elle renvoie une nouvelle chaine formée des chaines de caractères de la liste séparées per la chaine *separateur*.
 <br>
 Exemples:<br>
-"3".join(['toto','titi','tata']) renvoie 'toto3titi3tata'<br>
+" ".join(['toto','titi','tata']) renvoie 'toto titi tata'<br>
 "TTT".join(["r","y","gh"]) renvoie 'rTTTyTTTgh'<br>
 "TTT".join([]) renvoie ''
 
@@ -47,4 +53,5 @@ pltest2==
 'abc'
 
 ==
+
 
