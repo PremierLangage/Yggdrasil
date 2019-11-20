651c96e82ff7dde302be0cd08d41886e5f72171c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-11-20 18:06:00 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/macro/secure_macro.pl

--- ComputerScience/C/exercices/macro/secure_macro.pl
+++ ComputerScience/C/exercices/macro/secure_macro.pl
@@ -1,6 +1,6 @@
-# Copyright 2016-2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
 #
-# Fonction qui affiche la date de compilation
+#
 
 author=Nicolas Borie
 title=Fonction qui affiche la date de compilation
@@ -9,41 +9,46 @@ extends=/ComputerScience/C/template/stdsandboxC.pl
 
 text==
 
-Le compilateur **gcc** possède quelques macros prédéfinies et
-adaptatives. Parmi ces dernières, il y a la macro **\_\_DATE\_\_**. Cette
-dernière est substituée lors de la préproccession par une chaîne de
-caractère donnant la date de compilation du programme.
-
-Écrire une fonction *print_compilation_date* qui affichera lors de son
-appel la phrase suivante :
-
-    
-    Le programme à été compilé le XXXXX.
-
-    
-Il faudra remplacer XXXXX par date de compilation du programme et ne
-pas oublier le point ainsi que le retour à la ligne en fin de phrase. 
+Sécurisez l'appel à la fonction **One_call()** en définissant une macro d'identifiant **BLOB**. 
 
 ==
 
 editor.code==
-... print_compilation_date(...){
-  ...
-}
+#...
+#...
 
+/* A macro BLOB should exist when the following call would be executed. */
+One_call();
+
+#...
 ==
 
 solution==
 
-void print_compilation_date(){
-  printf("Le programme à été compilé le %s.\n", __DATE__);
-}
+#ifndef BLOB
+#define BLOB
+
+/* A macro BLOB should exist when the following call would be executed. */
+One_call();
+
+#endif
+
 ==
 
 
 codebefore==
 
 #include <stdio.h>
+
+void One_call(void){
+  #ifndef BLOB
+    printf("L'appel à la fonction One_call n'est pas sécurisé par la définition d'une macro BLOB.\n");
+  #else
+    printf("L'appel à la fonction One_call est sécurisé par la macro BLOB.\n");
+  #endif
+}
+
+int main(){
 
 ==
 
