37306f3f158f198c213448e3dc24309865525ed0
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-11-21 15:16:08 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Iterate/iterable.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/Iterate/iterable.pl
@@ -0,0 +1,16 @@
+title= 
+
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/comptemot.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/crypte.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/dames.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/iterable.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/join.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/permutcirc.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/simplifdigit.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/Suite_de_Conway.pl
+@ /ComputerScience/python/AP1-1920/drafts/zip/Iterate/voyelle.pl
+@ /ComputerScience/python/AP1-1920/drafts/dominique/iterables/lili.pl
+@ /ComputerScience/python/AP1-1920/drafts/dominique/iterables/suffixe.pl
+@ /ComputerScience/python/AP1-1920/drafts/dominique/iterables/bord.pl
+@ /ComputerScience/python/AP1-1920/drafts/dominique/iterables/echange.pl
+
