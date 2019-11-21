4408e30748c0862763e29f13cebff4e5ca6c7bd1
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2019-11-21 15:01:55 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/4-dictionnaires-sets/hotel-reservation.pl

---
+++ ComputerScience/python/AP1-1920/chapters/4-dictionnaires-sets/hotel-reservation.pl
@@ -0,0 +1,15 @@
+#author = WFang
+
+extends = /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+title = Réserver un bon hôtel
+
+text==
+Vous allez prendre une vacances à Marseille, et vous devez réserver un hôtel. 
+
+Vous avez un ensemble d'hôtels préférés, mais maintenant vos amis vous transmettent une liste d'hôtels à éviter.
+
+Écrire une fonction `hotel_acceptable` qui prendre votre ensemble d'hôtels acceptable et la liste d'hôtel à éviter, 
+puis renvoyer un ensemble d'hôtels acceptables, c'est-à-dire les hôtels acceptables pour vous mais pas à éviter.
+==
