97213fac9c61900c5c694956861bc0bc4bec172c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-11-21 12:25:25 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/input_output/ints_on_stdin.pl

--- ComputerScience/C/exercices/input_output/ints_on_stdin.pl
+++ ComputerScience/C/exercices/input_output/ints_on_stdin.pl
@@ -57,7 +57,8 @@ tests==
  ["Fini par zéro", "", "1 2 3 4 5 0"],
  ["Aléatoire", "", " ".join([str(random.randint(-10, 10)) for i in range(random.randint(2,6))])],
  ["Aléatoire", "", " ".join([str(random.randint(-10, 10)) for i in range(random.randint(6,20))])],
- ["Aléatoire", "", " ".join([str(random.randint(-20, 20)) for i in range(random.randint(10,20))])], ]
+ ["Aléatoire", "", " ".join([str(random.randint(-20, 20)) for i in range(random.randint(10,20))])],
+ ["Aléatoire", "", " ".join([random.choice(["Stop", "1lettre", "mot45"]+[str(random.randint(-100,100)) for i in range(10)]) for i in range(random.randint(10,20))])],]
 ==
 
 
