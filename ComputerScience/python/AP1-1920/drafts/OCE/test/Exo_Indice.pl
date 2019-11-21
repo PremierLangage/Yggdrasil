27efef5f7c19e86474639d92cc534665e19bae20
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2019-11-21 15:26:16 +0100

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/OCE/test/Exo_Indice.pl

--- ComputerScience/python/AP1-1920/drafts/OCE/test/Exo_Indice.pl
+++ ComputerScience/python/AP1-1920/drafts/OCE/test/Exo_Indice.pl
@@ -16,7 +16,7 @@ Créer un programme permettant :
 grader== #|python|
 
 # Solution
-def creation_liste(x):
+def creation_liste():
     liste=[1,2,3,4,5]
     for i in range(0,3):
         print(liste[i])
@@ -24,26 +24,6 @@ def creation_liste(x):
     liste[3] = liste[2]+liste[4]
     for i in range(12):
         print(liste[len(liste)-1)])
-
-# Vérifier la présence d'un mot taboo dans le code de l'étudiant
-def checktaboo(taboo, answer):
-    x = re.sub("(\"(.|\n)*\"|#.*)", "", answer) #enlève les commentaires et les chaînes de caractères
-    # FIXME la chaine de caractère ""  letaboo "" est elle trouvée par la regex suivante ? 
-    return re.search("(^"+taboo+"\s|[^\"]+"+taboo+"\s)", x) != None
-
-dic = pl_context
-student = _get_student_code(dic)
-
-# Vérification dans le code de l'étudiant de la non présence du taboo
-if "taboo" in dic:
-    if checktaboo(dic['taboo'], student):
-        sandboxio.output(0, "ATTENTION : Le mot clef " + dic['taboo'] + " est proscrit.")
-        sys.exit(1)
-# Vérification dans le code de l'étudiant de la présence du needed
-if "needed" in dic:
-    if not checktaboo(dic['needed'], student):
-        sandboxio.output(0, "ATTENTION : Le mot clef " + dic['needed'] + " est demandé.\n" + student)
-        sys.exit(1)
 
 begin_test_group("Facile")
 set_title(f"n = {1,2}")
