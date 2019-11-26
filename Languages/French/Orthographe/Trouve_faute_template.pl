2edc833e5c0ea3ab5a7023862db7198372bc3be3
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-11-26 22:52:20 +0100

    nborie modified Yggdrasil/Languages/French/Orthographe/Trouve_faute_template.pl

--- Languages/French/Orthographe/Trouve_faute_template.pl
+++ Languages/French/Orthographe/Trouve_faute_template.pl
@@ -54,13 +54,18 @@ text_exo += "l'erreur d'orthographe ou bien cliquer sur le bouton phrase correct
 
 if (eval(recall_rule)):
     if (n >= 2):
-        text_exo += "\n\nVoici les règles sur lesquelles nous allons travailler."
+        text_exo += "\n\nVoici les règles sur lesquelles nous allons travailler :"
     else:
-        text_exo += "\n\nVoici la règle sur lesquelle nous allons travailler."
+        text_exo += "\n\nVoici la règle sur lesquelle nous allons travailler :"
 
 def parse_file(filename):
     """
     Parse the orthographe file rule whose name is `filename`.
+
+    Return a Python dictionnary of the following shape
+    rule_name : "name of the rule"
+    rule_description : "text describing the rule"
+    sentences : [("bad sentence", "good sentence", "explain error")]
     """
     pass
 
