72dd69a46c1661acadca5173c881f30f2ee4db36
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-11-26 21:28:19 +0100

    nborie modified Yggdrasil/Languages/French/Orthographe/Trouve_faute_template.pl

--- Languages/French/Orthographe/Trouve_faute_template.pl
+++ Languages/French/Orthographe/Trouve_faute_template.pl
@@ -1,10 +1,12 @@
 # ***************************************************************************
 # PATRON D'EXERCICE TROUVE FAUTE 
 #
+# Des phrases arrivent aléatoirement devant l'apprenant. Ce dernier doit
+# localiser la faute d'orthographe ou alors cliquer sur un bouton pour 
+# dire qu'il pense que la phrase est correcte.
 #
-#
-#
-#
+# Pour chaque règle d'orthographe, l'apprenant doit valider son apprentissage
+# en appliquant la règle correctement 5 fois d'affiler . 
 #
 
 @ /utils/sandboxio.py
