321a55d9996bf822d16bb69d8fe23f023445bdc7
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-11-26 21:25:52 +0100

    nborie modified Yggdrasil/Languages/French/Orthographe/Trouve_faute.pl

--- Languages/French/Orthographe/Trouve_faute.pl
+++ Languages/French/Orthographe/Trouve_faute.pl
@@ -1,7 +1,6 @@
 extends=/Languages/French/Orthographe/Trouve_faute_template.pl
 
 author=Nicolas Borie
-mail=nicolas.borie at u-pem dot fr
 
 title=Ces ou ses : adjectif démonstratif ou possessif ?
 
