955f15ac39cc780d7fe9362939e6cae38a1cef9e
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-11-26 22:18:32 +0100

    nborie modified Yggdrasil/Languages/French/Orthographe/Trouve_faute_template.pl

--- Languages/French/Orthographe/Trouve_faute_template.pl
+++ Languages/French/Orthographe/Trouve_faute_template.pl
@@ -6,11 +6,11 @@
 # dire qu'il pense que la phrase est correcte.
 #
 # Pour chaque règle d'orthographe, l'apprenant doit valider son apprentissage
-# en appliquant la règle correctement 5 fois d'affiler (changer la valeur de 
+# en appliquant la règle correctement 5 fois d'affiléée (changer la valeur de 
 # la variable consecutive_to_validate). Si, au contraire, vous préférez que
-# valide un certain nombre de bonne réponse (pas forcément consécutives), il
-# vous faut alors changer la valeur de nb_good_to_validate pour un objectif
-# entier et positif.
+# l'apprenant valide un certain nombre de bonne réponse (pas forcément 
+# consécutives), il vous faut alors changer la valeur de nb_good_to_validate 
+# pour un objectif entier et positif.
 #
 # Pour toute suggestion et/ou correction, contacter un informaticien ou
 # l'auteur à son adresse mail : nicolas dot borie at u-pem dot fr
@@ -20,10 +20,10 @@
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
-title=Définissez un titre S.V.P.
+title=Définissez un titre en héritant de ce template
 author=Nicolas Borie
 
-files==
+file_list==
 ["ses_ou_ces.txt"]
 ==
 
