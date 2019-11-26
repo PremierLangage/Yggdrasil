1c411260d13e2bc0ae460602a3e35351826b3505
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-11-26 21:25:55 +0100

    nborie modified Yggdrasil/Languages/French/Orthographe/Trouve_faute_template.pl

--- Languages/French/Orthographe/Trouve_faute_template.pl
+++ Languages/French/Orthographe/Trouve_faute_template.pl
@@ -1,11 +1,18 @@
+# ***************************************************************************
+# PATRON D'EXERCICE TROUVE FAUTE 
+#
+#
+#
+#
+#
+#
+
 @ /utils/sandboxio.py
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 title=Définissez un titre S.V.P.
-
 author=Nicolas Borie
-mail=nicolas.borie at u-pem dot fr
 
 files==
 ["ses_ou_ces.txt"]
