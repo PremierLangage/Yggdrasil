73fe869b7092fd81197cd413b146ccf4c68b6e50
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2019-11-27 13:15:42 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Dico/trad.pl

--- ComputerScience/python/AP1-1920/drafts/zip/Dico/trad.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/Dico/trad.pl
@@ -6,7 +6,6 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 
 title = traduction approximative
 text== 
-Ecrire une fonction `traduit` qui reçoit un dictinnaire et un texte sous forme d'une 'str'
 les clef et lesvaleur du dictinnaire sont des str.
 La fonction remplace chaque clef du texte présente dans le dictionnaire par la valeur associé 
  >>>  `traduit({'tailleur':'taylor','riche':'rich','tu':'you','mon':'my','est':'is'},'eh mon tailleur est riche sur ')
