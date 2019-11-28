7d2192ec409b79b4d1f9e3147d83fba299d6354b
David DOYEN <David.Doyen@u-pem.fr>
2019-11-28 19:06:14 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bounds2.pl

---
+++ Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bounds2.pl
@@ -0,0 +1,27 @@
+extends = sum_.pl
+
+title = Somme d'entiers successifs
+
+before ==
+keyboards_JSON['virtualKeyboards']="elementary"
+input.config = keyboards_JSON
+
+v=randitem([Symbol('i'),Symbol('j'),Symbol('k')])
+n=randitem([Symbol('n'),Symbol('m'),Symbol('p')])
+an,bn=randitem([(n,2*n),(2*n,3*n),(n,3*n)])
+a0=randint(1,4)
+a=a0+an
+b0=randint(-4,4,[0])
+b=bn+b0
+n0=max([0,a0-b0])
+S = Sum(v, (v, a, b))
+S_tex=latex(S)
+n_tex=latex(n)
+sol=(b-a+1)*(a+b)/2
+sol_tex=latex(sol)
+==
+
+text ==
+Soit un entier $! {{n_tex}} \geq {{n0}} !$. Calculer $! \displaystyle {{S_tex}} !$.
+==
+
