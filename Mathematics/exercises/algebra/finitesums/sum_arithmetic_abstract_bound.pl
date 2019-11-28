a96b0638464f3687f6e05e09f1780bc42cf68710
David DOYEN <David.Doyen@u-pem.fr>
2019-11-28 18:36:42 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bound.pl

--- Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bound.pl
+++ Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bound.pl
@@ -7,11 +7,17 @@ keyboards_JSON['virtualKeyboards']="elementary"
 input.config = keyboards_JSON
 
 v=randitem([Symbol('i'),Symbol('j'),Symbol('k')])
-n=Symbol('n')
+n=randitem([Symbol('n'),Symbol('m'),Symbol('p')])
 a=randint(1,4)
-b=n+randint(-4,4,[0])
+b0=randint(-4,4,[0])
+b=n+b0
+n0=max([0,a-b0])
 S = Sum(v, (v, a, b))
 S_tex=latex(S)
 sol=S.doit()
 sol_tex=latex(sol)
+==
+
+text ==
+Soit ${{n}} \geq {{n0}}$. Calculer $% \displaystyle {{S_tex}} %$.
 ==
