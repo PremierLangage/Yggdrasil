16f416b55ef95cdf36dbb3e9a5e08ac8ec937f1d
David DOYEN <David.Doyen@u-pem.fr>
2019-11-29 23:42:08 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/finitesums/sum_squares_abstract_bounds1.pl

--- Mathematics/exercises/algebra/finitesums/sum_squares_abstract_bounds1.pl
+++ Mathematics/exercises/algebra/finitesums/sum_squares_abstract_bounds1.pl
@@ -1,17 +1,20 @@
 extends = sum_.pl
 
-title = Somme d'entiers successifs
+title = Somme de carrés successifs
 
 before ==
 keyboards_JSON['virtualKeyboards']="elementary"
 input.config = keyboards_JSON
-
 v=randitem([Symbol('i'),Symbol('j'),Symbol('k')])
 n=randitem([Symbol('n'),Symbol('m'),Symbol('p')])
-a=randint(1,4)
-b0=randint(-4,4,[0])
-b=n+b0
-n0=max([0,a-b0])
+a=1
+
+if param['boundtype']==1:
+    b0=randint(-4,4,[0])
+    b=n+b0
+elif param['boundtype']==2:
+
+n0=0
 S = Sum(v**2, (v, a, b))
 S_tex=latex(S)
 n_tex=latex(n)
@@ -19,7 +22,4 @@ sol=(b-a+1)*(a+b)/2
 sol_tex=latex(sol)
 ==
 
-text ==
-Soit un entier $! {{n_tex}} \geq {{n0}} !$. Calculer $! \displaystyle {{S_tex}} !$.
-==
 
