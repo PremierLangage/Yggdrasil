fcccfabbff300f6fced794e85a7fa88ca1e5fc74
David DOYEN <David.Doyen@u-pem.fr>
2019-11-30 10:37:05 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bounds2.pl

--- Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bounds2.pl
+++ Mathematics/exercises/algebra/finitesums/sum_arithmetic_abstract_bounds2.pl
@@ -8,11 +8,16 @@ input.config = keyboards_JSON
 
 v=randitem([Symbol('i'),Symbol('j'),Symbol('k')])
 n=randitem([Symbol('n'),Symbol('m'),Symbol('p')])
-an,bn=randitem([(n,2*n),(2*n,3*n),(n,3*n),(n,4*n)])
-a0=randint(1,4)
-a=a0+an
-b0=randint(-4,4,[0])
-b=bn+b0
+if param['boundtype']==1:
+    a=randint(1,4)
+    b0=randint(-4,4,[0])
+    b=n+b0
+elif param['boundtype']==2:
+    an,bn=randitem([(n,2*n),(2*n,3*n),(n,3*n),(n,4*n)])
+    a0=randint(1,4)
+    a=a0+an
+    b0=randint(-4,4,[0])
+    b=bn+b0
 n0=int(max([0,max(solve(a-b,n))]))
 S = Sum(v, (v, a, b))
 S_tex=latex(S)
