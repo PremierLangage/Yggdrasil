9dcef048d1a8838a500356caf104309d0587508b
David DOYEN <David.Doyen@u-pem.fr>
2019-11-30 12:28:54 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/finitesums/sum_deg1_.pl

--- Mathematics/exercises/algebra/finitesums/sum_deg1_.pl
+++ Mathematics/exercises/algebra/finitesums/sum_deg1_.pl
@@ -32,7 +32,10 @@ E = c1*Sum(p1*v+q1, (v, a, b))+c2*Sum(p2*v+q2, (v, a, b))
 A_tex=latex(Sum(v, (v, a, b)))
 E_tex=latex(E)
 n_tex=latex(n)
-sol=(c1*p1+c2*p2)*A+(c1*q1+c2*q2)*(b-a+1)
+if param['type']=="linearity":
+    sol=(c1*p1+c2*p2)*A+(c1*q1+c2*q2)*(b-a+1)
+elif param['type']=="compute":
+    sol=(c1*p1+c2*p2)*(b-a+1)*(a+b)/2+(c1*q1+c2*q2)*(b-a+1)
 sol_tex=latex(sol)
 ==
 
