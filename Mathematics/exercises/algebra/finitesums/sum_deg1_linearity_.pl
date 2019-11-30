93c1b9c045f0e466825a1020920210a052cc6126
David DOYEN <David.Doyen@u-pem.fr>
2019-11-30 12:16:35 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/finitesums/sum_deg1_linearity_.pl

---
+++ Mathematics/exercises/algebra/finitesums/sum_deg1_linearity_.pl
@@ -0,0 +1,10 @@
+extends = sum_deg1_.pl
+
+title = Linéarité
+
+
+text ==
+On pose $! \displaystyle A = {{A_tex}} !$. 
+
+Exprimer $% \displaystyle {{E_tex}} %$ en fonction de $! A !$ et $! {{n_tex}} !$.
+==
