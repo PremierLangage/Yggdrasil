38505d8d1e437fd440b68ae3e979bf95fabe1f4c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-12-02 14:46:08 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/malloc/first_last_name.pl

---
+++ ComputerScience/C/exercices/malloc/first_last_name.pl
@@ -0,0 +1,76 @@
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#
+# Allocation d'un tableau à une dimension
+
+author=Nicolas Borie
+title=Allocation d'un tableau à une dimension
+tag=array|malloc
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+Écrire une fonction C **allocate_float_array** qui prend en argument une 
+taille **size** (sous la forme d'un entier) et qui un tableau pouvant 
+contenir **size** flottants simple précision.
+
+==
+
+editor.code==
+... allocate_float_array(...){
+    /* Votre code ici */
+}
+
+==
+
+solution==
+
+float* allocate_float_array(int size){
+  float* ans = (float *)malloc( size * sizeof(float) );
+  return ans;
+}
+
+==
+
+codebefore==
+
+#include <stdlib.h>
+
+==
+
+codeafter==
+
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char* argv[]){
+  int size = atoi(argv[1]);
+  float* ans;
+
+  srand(time(NULL));
+
+  ans = allocate_float_array(size);
+  if (ans == NULL){
+    printf("Not Enough Memory.\n");
+    return 0;
+  }
+  if (size >= 2){
+    ans[0] = rand();
+    ans[size-1] = rand();
+    printf("Allocation et utilisation : %f (devrait valoir zéro...)\n", ans[size-1] - ans[size-1]);
+  }
+  free(ans);
+  return 0;
+}
+
+==
+
+tests==
+
+[["Exécution simple", "1", ""],
+ ["Tableau vide", "0", ""],
+ ["Tableau moyen", "46", ""],
+ ["Aléatoire", str(random.randint(1, 1000000)), ""],
+ ["Aléatoire", str(random.randint(1, 1000000000)), ""],
+ ["Aléatoire", str(random.randint(1000000000, 2000000000)), ""]]
+
+==
+
