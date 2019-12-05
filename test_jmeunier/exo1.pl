c03829a59e1dc84ba5004a4099764d7399747c7d
Jean-Marc Meunier <jean-marc.meunier@univ-paris8.fr>
2019-12-05 16:52:50 +0100

    jmeunier modified Yggdrasil/test_jmeunier/exo1.pl

---
+++ test_jmeunier/exo1.pl
@@ -0,0 +1,32 @@
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+title = somme d'entiers
+
+before ==
+import random as rd
+a=rd.randint(10,20)
+b=re.randint(10,20)
+
+text == 
+Calculer {{a}}+{{b}}
+==
+
+input=:Input
+form==
+{{input|component}}
+==
+
+evaluator==
+try:
+if int(input.value)==a+b:
+    grade=(100,"")
+else
+    grade(0,"")
+Except
+    grade=(-1,"votre réponse n'est pas un nombre entier.")
+==
+
+
+
