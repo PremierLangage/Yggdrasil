b9532f121ba6cefbdc2d49a44a1c2ac2a7d220ef
David DOYEN <David.Doyen@u-pem.fr>
2019-12-06 21:30:17 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/logic/truth_table_.pl

--- Mathematics/exercises/foundations/logic/truth_table_.pl
+++ Mathematics/exercises/foundations/logic/truth_table_.pl
@@ -31,7 +31,7 @@ Q=[True,False,True,False]
 R = [P[i] and not Q[i] for i in range(4)]
 
 row1 = ["V" if P[i] else "F" for i in range(4)]
-row1 = ["V" if Q[i] else "F" for i in range(4)]
+row2 = ["V" if Q[i] else "F" for i in range(4)]
 
 ==
 
