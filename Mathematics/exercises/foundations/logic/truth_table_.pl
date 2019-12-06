439028930ed7b937a784704193526823d1fe5f0d
David DOYEN <David.Doyen@u-pem.fr>
2019-12-06 22:46:30 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/logic/truth_table_.pl

--- Mathematics/exercises/foundations/logic/truth_table_.pl
+++ Mathematics/exercises/foundations/logic/truth_table_.pl
@@ -48,7 +48,7 @@ elif case==3:
 
 row1 = ["V" if P[i] else "F" for i in range(4)]
 row2 = ["V" if Q[i] else "F" for i in range(4)]
-
+sol = ["V" if range[i] else "F" for i in range(4)]
 ==
 
 text ==
