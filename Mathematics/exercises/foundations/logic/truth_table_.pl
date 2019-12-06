72e0711ba1150f2ca66952acdeaeb434b52bef24
David DOYEN <David.Doyen@u-pem.fr>
2019-12-06 20:44:28 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/logic/truth_table_.pl

--- Mathematics/exercises/foundations/logic/truth_table_.pl
+++ Mathematics/exercises/foundations/logic/truth_table_.pl
@@ -2,6 +2,22 @@ extends = /Mathematics/template/mathinput.pl
 
 title = Table de vérité
 
+extracss==
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+table {
+    margin: auto;
+}
+th, td {
+  padding: 0.5em;
+  text-align:center;
+}
+</style>
+==
+
 tpltable ==
 <table>
 <tr>
