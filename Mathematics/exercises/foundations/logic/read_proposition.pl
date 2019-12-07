4459b3b1858609b1b35a555df69baf9909ac90ae
David DOYEN <David.Doyen@u-pem.fr>
2019-12-07 20:30:00 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/logic/read_proposition.pl

--- Mathematics/exercises/foundations/logic/read_proposition.pl
+++ Mathematics/exercises/foundations/logic/read_proposition.pl
@@ -1,28 +1,12 @@
-extends = /Mathematics/template/mathradio.pl
+extends = /Mathematics/template/mathcheckbox.pl
 
 title = Table de vérité
 
 before ==
-if randint(1,2)==1:
-    P=[True,True,False,False]
-    Q=[True,False,True,False]
-else:
-    P=[False,False,True,True]
-    Q=[False,True,False,True]
 
-choices = ["$!P!$ et non ($!Q!$)","$!P!$ ou non ($!Q!$)","non ($!P!$) et $!Q!$","non ($!P!$) ou $!Q!$"]
-case=randint(0,3)
-if case==0:
-    R = [P[i] and not Q[i] for i in range(4)]
-elif case==1:
-    R = [P[i] or not Q[i] for i in range(4)]
-elif case==2:
-    R = [not P[i] and Q[i] for i in range(4)]
-elif case==3:
-    R = [not P[i] or Q[i] for i in range(4)]
 
-radio.loadContent(choices)
-radio.setSolByIndex(case)
+checkbox.loadContent(choices)
+checkbox.setSolByIndex(case)
 
 row1 = ["V" if P[i] else "F" for i in range(4)]
 row2 = ["V" if Q[i] else "F" for i in range(4)]
