cfcc0ec3dd9d4c41299aeec45024fdf053617e1d
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-12-10 11:25:42 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/stringfuncs/getcsv.py

--- ComputerScience/python/AP1-1920/drafts/dominique/stringfuncs/getcsv.py
+++ ComputerScience/python/AP1-1920/drafts/dominique/stringfuncs/getcsv.py
@@ -11,8 +11,7 @@ def selectionfromcsv(filename, number=4):
     with open(filename,"r") as csvfile:
         reader=csv.DictReader(csvfile,delimiter=';')
         l=[x for x in reader if x['niveau']!=1]
-        print(l,file=sys.stderr)
-        return l
+
         return random.sample(l,4)
 
 
