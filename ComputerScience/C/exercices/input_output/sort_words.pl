6b24d623eb64298f932ce59155686227ca4d6f18
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2019-12-16 12:06:23 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/input_output/sort_words.pl

--- ComputerScience/C/exercices/input_output/sort_words.pl
+++ ComputerScience/C/exercices/input_output/sort_words.pl
@@ -100,7 +100,8 @@ tests==
 [["Basique", "", "Pommes\nPoires\nPatates\nPeche\n"], 
  ["Un mot", "", "Bonjour\n"],
  ["Quelques mots", "", "Pommes\nPoires\nPatates\nPeche\nBanane\nFraise\nCerise\nPrune\nAbricot\nTomate\nRadis"],
- ["Aléatoire", "", "\n".join(["".join([chr(random.randint(ord('a'),ord('z')))) for j in range(random.randint(1, 60))]) for i in range(random.randint(10, 100))])]]
+ ["Aléatoire", "", "\n".join(["".join([chr(random.randint(97, 122)) for i in range(random.randint(1, 60))]) for j in range(2, 10)])],
+ ["Aléatoire", "", "\n".join(["".join([chr(random.randint(97, 122)) for i in range(random.randint(1, 60))]) for j in range(10, 100)])],]
 ==
 
 
