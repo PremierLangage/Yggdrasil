24e13ef3e2ca0bb0122b223502bd9aa74ecca778
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 18:44:17 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_.pl

---
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_.pl
@@ -0,0 +1,16 @@
+extends = /Mathematics/template/mathinput.pl
+
+text ==
+Calculer une primitive de la fonction
+$$ f : x \mapsto {{f_tex}} .$$
+==
+
+evaluator==
+var('x')
+score,_,feedback=ans_antiderivative(input.value,sol,x)
+==
+
+solution ==
+Les primitives de cette fonction sont les fonctions de la forme $$ x \mapsto {{sol_tex}} + k $$
+où $! k !$ est une constante réelle.
+==
