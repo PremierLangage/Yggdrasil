eb4369a1330cacfb0cc40e96fb1bf39bd1cac968
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 20:05:22 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_sqrt.pl

---
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_sqrt.pl
@@ -0,0 +1,33 @@
+extends = antiderivative_.pl
+
+title = Calcul de primitive (élément simple)
+
+lang = fr
+
+before ==
+keyboards_JSON['virtualKeyboards']="functions"
+input.config = keyboards_JSON
+
+var('x')
+a=randint(-5,5,[0])
+b,c=list_randint_norep(2,-5,5,[0])
+
+b0=Rational(-c,b)
+if b>0:
+    dom=r"\rbrack %s ,+\infty\lbrack" % latex(b0)
+else:
+    dom=r"\rbrack -\infty, %s \lbrack" % latex(b0)
+
+f=randitem([a*sqrt(b*x+c),a/sqrt(b*x+c)])
+f=simplify(f)
+f_tex=latex(f)
+sol=integrate(f,x).doit()
+sol_tex=latex(sol)
+==
+
+text ==
+Calculer une primitive de la fonction
+$$ f : x \mapsto {{latexf}} $$
+sur l'intervalle $! {{dom}} !$.
+==
+
