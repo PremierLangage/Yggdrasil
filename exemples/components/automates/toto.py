4ea48b6422c178341f4d39bffbeecc93d0c0f75b
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-12-17 22:50:44 +0100

    drevuz modified Yggdrasil/exemples/components/automates/toto.py

---
+++ exemples/components/automates/toto.py
@@ -0,0 +1,11 @@
+
+
+
+def completude(tomate):
+    """
+    vérifier que tout les états ont une transition par toutes les lettres
+    """
+    alpha=tomate['alphabet']
+    states=tomate['states']
+    transitions=["transitions"]
+    return ""+transitions
