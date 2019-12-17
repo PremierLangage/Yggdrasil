f982ca21e2c9a05d6ec9d9fd2b325e0c0bf13928
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 18:46:37 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_sincos.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_sincos.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_sincos.pl
@@ -10,7 +10,7 @@ var('x')
 a=randitem([Rational(1,2),Rational(1,3),Rational(1,4),2,3,4])
 b=randint(-3,3,[0])
 f=randitem([sin(a*x+b),cos(a*x+b),sin(a*x)+b,cos(a*x)+b])
-latexf=latex(f)
+f_tex=latex(f)
 sol=integrate(f,x).doit()
 ==
 
