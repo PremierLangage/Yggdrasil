ae728a810f01909f8fcd18cccda8bd653a422a1e
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 20:45:49 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/integral_.pl

--- Mathematics/exercises/analysis/antiderivative/integral_.pl
+++ Mathematics/exercises/analysis/antiderivative/integral_.pl
@@ -1,17 +1,6 @@
 extends = /Mathematics/template/mathinput.pl
 
-before ==
-keyboards_JSON['virtualKeyboards']="elementary"
-input.config = keyboards_JSON
-
-var('x')
-x0,x1=list_randint_norep(2,-2,2)
-d=randint(2,3)
-f=rand_int_poly(d,2,3,x)
-f_tex=latex(f)
-sol=integrate(f,(x,x0,x1)).doit()
-sol_tex=latex(sol)
-==
+title = Calcul d'intégrales
 
 text ==
 Calculer l'intégrale $! \displaystyle \int_{ {{x0}} }^{ {{x1}} } {{f_tex}} \, dx !$.
