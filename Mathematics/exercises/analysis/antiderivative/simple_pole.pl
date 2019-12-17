96c8b45555ed0d36947d1c068e595dad761c1a55
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 15:55:14 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/simple_pole.pl

--- Mathematics/exercises/analysis/antiderivative/simple_pole.pl
+++ Mathematics/exercises/analysis/antiderivative/simple_pole.pl
@@ -13,26 +13,20 @@ a=randitem([Rational(1,2),Rational(1,3),Rational(1,4),2,3,4])
 b=randint(-5,5,[0])
 c=randint(-5,5,[0])
 
+s=randitem([-1,1])
 b0=Rational(-c,b)
-if b>0:
-
+if (b>0 and s>0) or (b<0 and s<0):
+    dom=r"\rbrack \b0,+\infty\lbrack"
+else:
+    dom=r"\rbrack -\infty,\b0\lbrack"
 
 if s>0:
     sol=a/b*ln(b*x+c)
 else:
     sol=-a/b*ln(-b*x-c)
-\text{Ip=\lbrack \b0,+\infty\lbrack}
-\text{Isp=\rbrack \b0,+\infty\lbrack}
-}{
-\text{Ip=\rbrack -\infty,\b0\rbrack}
-\text{Isp=\rbrack -\infty,\b0\lbrack}
-}
 
-\text{cas=randint(1..8)}
-\if{\cas=1}{
-\text{f=simplify(\a/(\b*\var+\c))}
-\text{I=\Isp}
-}
+f=a/(b*x+c)
+latexf=latex(f)
 ==
 
 text ==
