d3b6051f3cff69937d07263ed4bfe6ffc2db160c
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 21:08:39 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/integral_sincos.pl

--- Mathematics/exercises/analysis/antiderivative/integral_sincos.pl
+++ Mathematics/exercises/analysis/antiderivative/integral_sincos.pl
@@ -5,7 +5,7 @@ keyboards_JSON['virtualKeyboards']="elementary"
 input.config = keyboards_JSON
 
 var('x')
-x0,x1=list_randitem_norep(2,[0,Pi,-Pi,Pi/2,-Pi/2,2*Pi,-2*Pi])
+x0,x1=list_randitem_norep(2,[0,pi,-pi,pi/2,-pi/2,2*pi,-2*pi])
 
 f=randitem([sin(x/2),cos(x/2),sin(x/3),cos(x/3)])
 f_tex=latex(f)
