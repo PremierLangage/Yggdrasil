a7cfd5caa6b99e88a5d0c532492901dc9dfd1acd
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 19:30:16 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/pole_double.pl

--- Mathematics/exercises/analysis/antiderivative/pole_double.pl
+++ Mathematics/exercises/analysis/antiderivative/pole_double.pl
@@ -1,4 +1,4 @@
-extends = /Mathematics/template/mathinput.pl
+extends = antiderivative_.pl
 
 title = Calcul de primitive (élément simple)
 
@@ -13,24 +13,7 @@ a=randint(-5,5,[0])
 b,c=list_randint_norep(2,-5,5,[0])
 
 f=simplify(a/(b*x+c)**2)
-latexf=latex(f)
+f_tex=latex(f)
 sol=simplify(integrate(f,x))
 sol_tex=latex(sol)
 ==
-
-text ==
-Calculer une primitive de la fonction
-$$ f : x \mapsto {{latexf}} .$$
-==
-
-evaluator==
-var('x')
-score,_,feedback=ans_antiderivative(input.value,sol,x)
-==
-
-solution ==
-Les primitives de cette fonction sont les fonctions de la forme $$ x \mapsto {{sol_tex}} + k $$
-où $! k !$ est une constante réelle.
-==
-
-
