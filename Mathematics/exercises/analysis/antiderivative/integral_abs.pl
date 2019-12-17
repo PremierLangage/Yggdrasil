d414541627f062ed70d8bfd98a74ed314ef34174
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 21:09:37 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/integral_abs.pl

--- Mathematics/exercises/analysis/antiderivative/integral_abs.pl
+++ Mathematics/exercises/analysis/antiderivative/integral_abs.pl
@@ -5,8 +5,8 @@ keyboards_JSON['virtualKeyboards']="elementary"
 input.config = keyboards_JSON
 
 var('x')
-x0,x1=list_randitem_norep(2,[0,Pi,-Pi,Pi/2,-Pi/2,2*Pi,-2*Pi])
-
+x0,x1=sort(list_randint_norep(2,-3,3))
+a,b=list_randint_norep(2,-5,5,[0])
 f=abs(a*x+b)
 f_tex=latex(f)
 sol=integrate(f,(x,x0,x1)).doit()
