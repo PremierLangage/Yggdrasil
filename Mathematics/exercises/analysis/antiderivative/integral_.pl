ee0cb906f17b068e5fee4e0d4c48bcf0790b4e62
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 20:40:55 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/integral_.pl

---
+++ Mathematics/exercises/analysis/antiderivative/integral_.pl
@@ -0,0 +1,36 @@
+extends = /Mathematics/template/mathinput.pl
+
+before ==
+keyboards_JSON['virtualKeyboards']="elementary"
+input.config = keyboards_JSON
+
+var('x')
+a=randint(-5,5,[0])
+b,c=list_randint_norep(2,-5,5,[0])
+x0,x1=list_randint_norep(2,-2,2)
+b0=Rational(-c,b)
+if b>0:
+    dom=r"\rbrack %s ,+\infty\lbrack" % latex(b0)
+else:
+    dom=r"\rbrack -\infty, %s \lbrack" % latex(b0)
+
+f=randitem([a*sqrt(b*x+c),a/sqrt(b*x+c)])
+f=simplify(f)
+f_tex=latex(f)
+sol=integrate(f,(x,x0,x1).doit()
+sol_tex=latex(sol)
+==
+
+text ==
+Calculer l'intégrale $! \displaystyle \int_{ {{x0}} }^{ {{x1}} } {{f_tex}} dx.$$
+==
+
+evaluator==
+var('x')
+score,_,feedback=ans_expr(input.value,sol)
+==
+
+solution ==
+L'intégrale vaut $! {{sol}} !$.
+==
+
