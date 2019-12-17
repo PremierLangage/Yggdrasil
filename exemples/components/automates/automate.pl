77f5b1df61c0f2456a85db937328da2993cd6c5a
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2019-12-17 22:50:08 +0100

    drevuz modified Yggdrasil/exemples/components/automates/automate.pl

---
+++ exemples/components/automates/automate.pl
@@ -0,0 +1,47 @@
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+
+title = Automaton Editor
+
+editor =: AutomatonEditor
+editor.automaton ==
+#states
+s0
+s1
+s2
+#initials
+s0
+#accepting
+s2
+#alphabet
+a
+b
+#transitions
+s0:a>s1
+s1:a>s1
+s1:b>s2
+
+==
+
+before==
+==
+
+
+text==
+==
+
+form==
+{{ editor|component }}
+==
+
+evaluator==
+import toto
+myauto= editor.automaton
+toto.completude(myauto)
+
+grade = (100, toto.completude(myauto))
+==
+
+
