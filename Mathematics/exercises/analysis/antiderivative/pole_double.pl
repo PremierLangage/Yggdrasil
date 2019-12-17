d2528d3c94518d63b5539e2b0089f6a1ffc98cd6
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 18:29:02 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/pole_double.pl

--- Mathematics/exercises/analysis/antiderivative/pole_double.pl
+++ Mathematics/exercises/analysis/antiderivative/pole_double.pl
@@ -12,29 +12,15 @@ var('x')
 a=randint(-5,5,[0])
 b,c=list_randint_norep(2,-5,5,[0])
 
-s=randitem([-1,1])
-b0=Rational(-c,b)
-if (b>0 and s>0) or (b<0 and s<0):
-    dom=r"\rbrack %s ,+\infty\lbrack" % latex(b0)
-else:
-    dom=r"\rbrack -\infty, %s \lbrack" % latex(b0)
-
-if s>0:
-    sol=Rational(a,b)*ln(b*x+c)
-else:
-    sol=-Rational(a,b)*ln(-b*x-c)
-
-sol=simplify(sol)
-
-f=simplify(a/(b*x+c))
+f=simplify(a/(b*x+c)^2)
 latexf=latex(f)
+sol=integrate(f,x)
 sol_tex=latex(sol)
 ==
 
 text ==
 Calculer une primitive de la fonction
-$$ f : x \mapsto {{latexf}} $$
-sur l'intervalle $! {{dom}} !$.
+$$ f : x \mapsto {{latexf}} .$$
 ==
 
 evaluator==
