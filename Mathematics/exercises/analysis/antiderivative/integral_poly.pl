e37d28a11f49bda4d9bd04ec72b0d772c1a7ba61
David DOYEN <David.Doyen@u-pem.fr>
2019-12-17 20:45:58 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/integral_poly.pl

---
+++ Mathematics/exercises/analysis/antiderivative/integral_poly.pl
@@ -0,0 +1,15 @@
+extends = integral_.pl
+
+before ==
+keyboards_JSON['virtualKeyboards']="elementary"
+input.config = keyboards_JSON
+
+var('x')
+x0,x1=list_randint_norep(2,-2,2)
+d=randint(2,3)
+f=rand_int_poly(d,2,3,x)
+f_tex=latex(f)
+sol=integrate(f,(x,x0,x1)).doit()
+sol_tex=latex(sol)
+==
+
