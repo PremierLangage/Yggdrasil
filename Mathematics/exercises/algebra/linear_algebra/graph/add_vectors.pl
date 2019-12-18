cc2b6f42a4b92b76b8b46597906ae96e5ae9d346
David DOYEN <David.Doyen@u-pem.fr>
2019-12-18 16:00:55 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/graph/add_vectors.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/graph/add_vectors.pl
@@ -0,0 +1,84 @@
+extends = /Mathematics/template/mathjsxgraph.pl
+
+title = Affixe dun nombre complexe
+
+lang = fr
+
+before ==
+a=randint(-5,5,[0])
+b=randint(-5,5,[0])
+z=a+b*sp.I
+z_tex=latex(z)
+==
+
+drawer.attributes %=
+{
+    "boundingbox": [
+      -6,
+      6,
+      6,
+      -6
+    ],
+    "showNavigation": false
+  }
+==
+
+drawer.script ==
+board.create('grid',[],{gridX:1,gridY:1});
+board.create('axis',[[0,0],[1,0]],{name:'Re',withLabel:true,label:{position:'urt',offset:[-5,10]},ticks:{visible: false}});
+board.create('axis',[[0,0],[0,1]],{name:'Im',withLabel:true,label:{position:'urt',offset:[10,0]},ticks:{visible: false}});
+const M = board.create('point',[0, 0],{size:2,name:'M',color:'red'});
+
+function getMouseCoords(e) {
+    let cPos = board.getCoordsTopLeftCorner(e);
+    let absPos = JXG.getPosition(e);
+    let dx = absPos[0]-cPos[0];
+    let dy = absPos[1]-cPos[1];
+    return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
+}
+
+function down(e) {
+    let coords = getMouseCoords(e);
+    M.setPosition(JXG.COORDS_BY_USER,[coords.usrCoords[1], coords.usrCoords[2]]);
+}
+
+board.on('down', down)
+==
+
+
+text ==
+Placer le point $% M %$ d'affixe $%{{ z_tex }}%$ dans le plan ci-dessous.
+==
+
+script_solution ==
+board.create('point',[{{a}}, {{b}}],{size:2,name:'M',color:'green'});
+board.create('point',[{{x}}, {{y}}],{size:2,name:'',color:'red'});
+==
+
+evaluator ==
+# pas besoin de faire float(drawer.points['M']['x'])
+# car une des fonctionnalités des composants et que
+# dans un grader le type des propriétes est toujours le type
+# spécifié dans la doc (dans ce cas les propriétes x et y d'un point
+# sont des flottants c'est possible de le voir en mode debug).
+
+
+x= drawer.points['M']['x']
+y= drawer.points['M']['y']
+
+from jinja2 import Template
+from math import hypot
+if hypot(x-a, y-b)<0.1:
+     score=100
+     drawer.script = Template(script_solution).render(locals())
+     drawer.points = {}
+     drawer.disabled = True
+else:
+     score=0
+     if nbattempt + 1 >= int(maxattempt):
+        drawer.script = Template(script_solution).render(locals())
+        drawer.points = {}
+        drawer.disabled = True
+feedback=""
+==
+
