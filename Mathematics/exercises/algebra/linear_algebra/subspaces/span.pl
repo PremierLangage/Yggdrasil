68c8b0a6d8bc43488d1d320f6471a68475825b93
David DOYEN <David.Doyen@u-pem.fr>
2019-12-19 12:07:02 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl
@@ -0,0 +1,78 @@
+extends = /Mathematics/template/mathinput.pl
+
+title = Système linéaire
+
+lang = fr
+
+
+extracss == #|html| 
+<style>
+.fcontainer {
+    display: flex;
+    align-items: center;
+}
+c-math-input {
+    width: 100%;
+}
+</style>
+==
+
+before ==
+keyboards_JSON['virtualKeyboards']="elementary"
+input1.config = keyboards_JSON
+input2.config = keyboards_JSON
+input3.config = keyboards_JSON
+input4.config = keyboards_JSON
+
+n=2
+m=3
+coeffbound = int(param['coeffbound'])
+sparsity=float(param['sparsity'])
+A=rand_int_matrix(n,m,coeffbound)
+B=rand_int_matrix(n,1,coeffbound)
+sys_tex=latexsys(A,B)
+lstvar=["x","y","z","t"]
+lstinput=[input1,input2,input3,input4]
+lstinput=lstinput[:n]
+
+sol_tex=""
+for i in range(n-1):
+    sol_tex += r" %s = %s, " % (lstvar[i],sol[i])
+sol_tex+=r"%s = %s" % (lstvar[n-1],sol[n-1])
+==
+
+text ==
+On considère l'ensemble des vecteurs $! (x,y,z) !$ tels que
+$$ {{sys_tex}} $$
+Trouver un vecteur qui engendre cet ensemble.
+==
+
+form ==
+{% for input in lstinput %}
+<div class="fcontainer">
+<span>$% {{ lstvar[loop.index0] }} = %$</span>{{input|component}}
+</div>
+{% endfor %}
+==
+
+input1 =: MathInput
+input2 =: MathInput
+input3 =: MathInput
+input4 =: MathInput
+
+evaluator ==
+if n==2:
+    ans=[input1.value,input2.value]
+elif n==3:
+    ans=[input1.value,input2.value,input3.value]
+elif n==4:
+    ans=[input1.value,input2.value,input3.value,input4.value]
+score,_,feedback=ans_tuple_expr(ans,sol,parentheses=False)
+==
+
+solution == 
+Les solutions sont $! {{sol_tex}} !$.
+==
+
+
+
