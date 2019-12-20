665471f8b3b66fa4cd9604525633b48e1f76a464
David DOYEN <David.Doyen@u-pem.fr>
2019-12-20 17:50:44 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl

--- Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl
+++ Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl
@@ -58,7 +58,7 @@ input4 =: MathInput
 evaluator ==
 ans1=str2struct(input1.value)
 ans2=str2struct(input2.value)
-M=BlockMatrix([Matrix(ans1),Matrix(ans2)])
+M=BlockMatrix([Matrix(ans1),Matrix(ans2)]).as_explicit()
 score=100
 feedback=str(M.rank())
 ==
