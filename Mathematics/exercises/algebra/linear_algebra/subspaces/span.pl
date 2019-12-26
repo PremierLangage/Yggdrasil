7282b8e112eadaaa104f99c798baeffd83f20942
David DOYEN <David.Doyen@u-pem.fr>
2019-12-26 15:15:49 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl

--- Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl
+++ Mathematics/exercises/algebra/linear_algebra/subspaces/span.pl
@@ -25,6 +25,8 @@ Trouver un vecteur qui engendre cet ensemble.
 ==
 
 evaluator ==
+ans=str2struct("("+input.value+")")
+
 try:    
     ans=str2struct("("+input.value+")")
     if not all([len(v)==m for v in ans]):
