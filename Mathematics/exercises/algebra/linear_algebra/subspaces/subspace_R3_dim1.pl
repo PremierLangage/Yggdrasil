0cdac27007cf4decf17962ddc5e17b4e0f9d28dd
David DOYEN <David.Doyen@u-pem.fr>
2019-12-27 15:02:06 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/subspaces/subspace_R3_dim1.pl

--- Mathematics/exercises/algebra/linear_algebra/subspaces/subspace_R3_dim1.pl
+++ Mathematics/exercises/algebra/linear_algebra/subspaces/subspace_R3_dim1.pl
@@ -6,7 +6,7 @@ param.m % 3
 text ==
 On considère le sous-espace $! P !$ de $! \mathbb{R}^3 !$ formé des vecteurs $! (x,y,z) !$ tels que
 $$ {{sys_tex}} .$$
-Trouver deux vecteurs qui engendrent ce sous-espace.
+Trouver un vecteur qui engendre ce sous-espace.
 ==
 
 evaluator ==
@@ -15,13 +15,13 @@ def evalans(strans):
         ans=str2struct("\{"+input.value+"\}")
         M=Matrix(ans).transpose()
     except:
-        return (-1,"La réponse doit être constituée de deux vecteurs de $! \mathbb{R}^3 !$ séparés par une virgule.")
+        return (-1,"La réponse doit être un vecteur de $! \mathbb{R}^3 !$ séparés par une virgule.")
     if M.shape!=(m,d):
-        return (-1,"La réponse doit être constituée de deux vecteurs de $! \mathbb{R}^3 !$ séparés par une virgule.")
+        return (-1,"La réponse doit être un vecteur de $! \mathbb{R}^3 !$ séparés par une virgule.")
     if not (A*M==zeros(n,d)):
-        return (0,"Ces vecteurs n'engendrent pas le sous-espace.")
+        return (0,"Ce n'engendre pas le sous-espace.")
     if M.rank()!=d:
-        return (0,"Ces vecteurs n'engendrent pas le sous-espace.")
+        return (0,"Ce n'engendre pas le sous-espace.")
     return (100,"")
 
 score,feedback=evalans("\{"+input.value+"\}")
