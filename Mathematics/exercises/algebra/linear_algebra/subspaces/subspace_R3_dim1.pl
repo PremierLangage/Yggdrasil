e88230eb4f439b821eae7e3917d11a380bf7e1ec
David DOYEN <David.Doyen@u-pem.fr>
2019-12-27 15:02:25 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/subspaces/subspace_R3_dim1.pl

--- Mathematics/exercises/algebra/linear_algebra/subspaces/subspace_R3_dim1.pl
+++ Mathematics/exercises/algebra/linear_algebra/subspaces/subspace_R3_dim1.pl
@@ -15,9 +15,9 @@ def evalans(strans):
         ans=str2struct("\{"+input.value+"\}")
         M=Matrix(ans).transpose()
     except:
-        return (-1,"La réponse doit être un vecteur de $! \mathbb{R}^3 !$ séparés par une virgule.")
+        return (-1,"La réponse doit être un vecteur de $! \mathbb{R}^3 !$.")
     if M.shape!=(m,d):
-        return (-1,"La réponse doit être un vecteur de $! \mathbb{R}^3 !$ séparés par une virgule.")
+        return (-1,"La réponse doit être un vecteur de $! \mathbb{R}^3 !$.")
     if not (A*M==zeros(n,d)):
         return (0,"Ce n'engendre pas le sous-espace.")
     if M.rank()!=d:
