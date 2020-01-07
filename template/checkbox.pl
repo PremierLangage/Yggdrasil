a11fffe21ac2473ff2cebef4c6bc9fe469e0934b
David DOYEN <David.Doyen@u-pem.fr>
2020-01-07 15:19:52 +0100

    ddoyen modified Yggdrasil/template/checkbox.pl

--- template/checkbox.pl
+++ template/checkbox.pl
@@ -1,44 +1,21 @@
 extends = basic.pl
 
-title = Checkbox
+@ /utils/checkbox.py [checkbox.py]
 
 checkbox =: CheckboxGroup
+checkbox.decorator = Checkbox
 
-before ==
-import random as rd
-nright0=rd.choice(nright)
-nwrong0=nchoices-nright0
-choices = rd.sample(right,nright0)+rd.sample(wrong,nwrong0)
-rd.shuffle(choices)
-
-checkbox.items=([{"id":str(id),"content":content} for id,content in enumerate(choices)])
-==
-
-text ==
-Sélectionner les villes européennes dans la liste suivante.
-==
+settings.feedback.class = score
 
 form ==
 {{ checkbox | component }}
 ==
 
 evaluator ==
-ansright,answrong=0,0
-for item in checkbox.items:
-    if item['content'] in right and item['checked']:
-        item['css'] = 'success-state anim-fade'
-        ansright+=1
-    elif item['content'] in right and not item['checked']:
-        item['css'] = 'success-state anim-fade'
-        answrong+=1
-    elif not (item['content'] in right) and item['checked']:
-        answrong+=1
-score=0
-if ansright>0:
-    score=max(int((ansright-answrong)/ansright*100),0)
-feedback=str(score)+"/100"
-grade= (score,feedback)
+grade = checkbox.eval()
+checkbox.disabled=True
 ==
+
 
 
 
