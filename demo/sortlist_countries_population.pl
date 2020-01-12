3ec2507a1888cc247ee91f8c82a566bfc2924bdf
Essox Lucius <Elucius@aol.fr>
2020-01-12 16:01:25 +0100

    elucius modified Yggdrasil/demo/sortlist_countries_population.pl

--- demo/sortlist_countries_population.pl
+++ demo/sortlist_countries_population.pl
@@ -2,14 +2,28 @@ extends = /template/sortlist.pl
 
 title = Ordonner des nombres
 
+
+@country_data.csv [data.csv]
+
 before ==
 import random as rd
-numbers=rd.sample(list(range(50)),5)
-numbers.sort()
-sortlist.loadContent([str(n) for n in numbers])
+import csv
+def sortSecond(val): 
+    return int(val['population'])
+with open('data.csv',newline='') as file:
+    rows=list(csv.DictReader(file,delimiter=','))
+
+items=rd.sample(rows,4)
+sorted(items,key=sortSecond)
+source=[item['country'] for item in items]
+target=[item['capital'] for item in items]
+
+
+sortlist.loadContent([item['country']+item['population'] for item in items])
 ==
 
 text ==
-{{numbers}}
-Ordonner les nombres suivants du plus petit au plus grand.
+{{items}}
+Ordonner ces capital de la moins peuplée à la plus peuplée.
 ==
+
