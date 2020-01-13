256d113033ead274685904c5f6a55f7dd7f5221c
David DOYEN <David.Doyen@u-pem.fr>
2020-01-13 19:53:42 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
@@ -0,0 +1,31 @@
+extends = /Mathematics/template/mathradio.pl
+
+title = Système linéaire
+
+
+before ==
+keyboards_JSON['virtualKeyboards']="elementary"
+input.config = keyboards_JSON
+n=param['size']
+coeffbound = param['coeffbound']
+sparsity= param['sparsity']
+A=rand_int_matrix_invertible(n,coeffbound,[0],sparsity)
+B=rand_int_matrix(n,1,coeffbound,[0])
+sol=list(linsolve((A, B)))[0]
+sys_tex=latexsys(A,B)
+sol_tex=latex(sol)
+==
+
+text ==
+Déterminer la solution du système suivant (d'inconnues $! {{lstvar}} !$) :
+$$ {{sys_tex}} $$
+==
+
+evaluator ==
+score,_,feedback=ans_struct_expr(input.value,sol,"tuple")
+==
+
+solution == 
+La solution est $! {{sol_tex}} !$.
+==
+
