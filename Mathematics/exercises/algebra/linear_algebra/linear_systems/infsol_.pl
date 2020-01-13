abd5ff73bdbdb7500ff7f95be937a24c6fe9eea7
David DOYEN <David.Doyen@u-pem.fr>
2020-01-13 14:19:08 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/infsol_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/infsol_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/infsol_.pl
@@ -33,20 +33,23 @@ def str2affsubset(s,local_dict={}):
     s1=pattern.search(s).group(1)
     lst2=pattern.search(s).group(2).split(",")
     lstvar=[sp.Symbol(item.strip()) for item in lst2]
+    dimev=len(lstvar)
     transformations=prs.standard_transformations + (prs.implicit_multiplication_application,prs.convert_xor)
     with sp.evaluate(False):
         vec=prs.parse_expr(s1,local_dict=local_dict,transformations=transformations,evaluate=False)
     pt=sp.Matrix([[sp.Poly(expr,lstvar).coeff_monomial(1) for expr in vec]]).transpose()
     span = [sp.Matrix([[sp.Poly(expr,lstvar).coeff_monomial(v) for expr in vec]]).transpose() for v in lstvar]
-    return pt,span
+    return dimev,pt,span
 
 def ans(strans,A,B):
     """
     Analyze a set.
     """
     try:    
-        pt,span=str2affsubset(input.value)
+        dimev,pt,span=str2affsubset(input.value)
     except:
+        return (-1,"NotValidExpr","Votre réponse n'est pas écrit sous la bonne forme.")
+    if dimev!=n:
         return (-1,"NotValidExpr","Votre réponse n'est pas écrit sous la bonne forme.")
     if A*pt != B:
         return (0,"","point")
