17df12801917ffa48d89431b65c9268c2ffa0bb4
David DOYEN <David.Doyen@u-pem.fr>
2020-01-13 16:26:11 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys_2_5_nonzero_rat.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys_2_5_nonzero_rat.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys_2_5_nonzero_rat.pl
@@ -2,11 +2,11 @@ extends = linsys2_.pl
 
 title = Système linéaire 2 x 2
 
-param.size = 2
+param.size % 2
 
-param.coeffbound = 5
+param.coeffbound % 5
 
-param.sparsity = 0.
+param.sparsity % 0.
 
 
 
