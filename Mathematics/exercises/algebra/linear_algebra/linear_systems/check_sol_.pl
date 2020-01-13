98288fda0a1e83dcad97935e3cf2fe5339ea968c
David DOYEN <David.Doyen@u-pem.fr>
2020-01-13 22:17:52 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
@@ -10,16 +10,14 @@ sparsity= param['sparsity']
 A=rand_int_matrix_invertible(n,3)
 
 vec=list_randint(n,-3,3)
+B=A*(Matrix(vec).transpose())
 choices=["$! %s !$" % str(tuple(vec))]
 while len(choices)<4:
     vec=list_randint(n,-3,3)
     if vec not in choices:
         choices.append("$! %s !$" % str(tuple(vec)))
 
-B=rand_int_matrix(n,1,3,[0])
-sol=list(linsolve((A, B)))[0]
 sys_tex=latexsys(A,B)
-sol_tex=latex(sol)
 radio.loadContent(choices)
 radio.setSolByIndex(0)
 radio.shuffle()
