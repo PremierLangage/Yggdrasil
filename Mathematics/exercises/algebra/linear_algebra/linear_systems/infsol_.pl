4761b25ce61ac504d835677c1b7d0dd1d8d8b401
David DOYEN <David.Doyen@u-pem.fr>
2020-01-13 13:43:32 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/infsol_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/infsol_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/infsol_.pl
@@ -30,7 +30,7 @@ def str2affsubset(s,local_dict={}):
     s=s.replace(r"\rbrace","}")
     s.strip()
     return s
-    pattern = re.compile(r'^\\lbrace(.*)\\rbrace$')
+    pattern = re.compile(r'^\{(.*)\}$')
     return pattern.search(s).group(1)
 
 def str2affsubset2(s,local_dict={}):
