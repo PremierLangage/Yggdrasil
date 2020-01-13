ed6271f9fc6b1bddf44b35c28f9997e3a77e921c
David DOYEN <David.Doyen@u-pem.fr>
2020-01-13 20:55:03 +0100

    ddoyen modified Yggdrasil/Mathematics/template/mathradio.pl

--- Mathematics/template/mathradio.pl
+++ Mathematics/template/mathradio.pl
@@ -1,14 +1,2 @@
 extends = /Mathematics/template/mathbasic.pl
-
-@ /utils/radiogroup.py [customradiogroup.py]
-
-radio =: RadioGroup
-radio.decorator = CustomRadioGroup
-
-form = {{ radio|component }}
-
-evaluator ==
-grade = radio.eval()
-radio.disabled=True
-score,feedback=grade
-==
+extends = /template/radio.pl
