c39a8838c5f2ada710521fad6dbe81a13f0d7652
David DOYEN <David.Doyen@u-pem.fr>
2020-01-13 15:11:52 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys2_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys2_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys2_.pl
@@ -14,14 +14,7 @@ A=rand_int_matrix_invertible(n,coeffbound,[0],sparsity)
 B=rand_int_matrix(n,1,coeffbound)
 sol=list(linsolve((A, B)))[0]
 sys_tex=latexsys(A,B)
-lstvar=["x","y","z","t","u","v"]
-lstinput=[input1,input2,input3,input4]
-lstinput=lstinput[:n]
-
-sol_tex=""
-for i in range(n-1):
-    sol_tex += r" %s = %s, " % (lstvar[i],sol[i])
-sol_tex+=r"%s = %s" % (lstvar[n-1],sol[n-1])
+sol_tex=latex(sol)
 ==
 
 text ==
@@ -29,12 +22,10 @@ Déterminer les solutions du système
 $$ {{sys_tex}} $$
 ==
 
-
-
 evaluator ==
 score,_,feedback=ans_tuple_expr(ans,sol,parentheses=True)
 ==
 
 solution == 
-Les solutions sont $! {{sol_tex}} !$.
+La solution est $! {{sol_tex}} !$.
 ==
