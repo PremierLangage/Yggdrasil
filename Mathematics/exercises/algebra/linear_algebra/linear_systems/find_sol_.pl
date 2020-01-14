8782d183f1c24b1824e62027944704e996a84cd3
David DOYEN <David.Doyen@u-pem.fr>
2020-01-14 15:11:31 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/find_sol_.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/find_sol_.pl
@@ -0,0 +1,31 @@
+extends = /Mathematics/template/mathradio.pl
+
+title = Trouver une solution parmi plusieurs
+
+before ==
+n=param['size']
+coeffbound = param['coeffbound']
+sparsity= param['sparsity']
+A=rand_int_matrix_invertible(n,3)
+
+vec=list_randint(n,-3,3)
+B=A*Matrix(vec)
+choices=["$! %s !$" % str(tuple(vec))]
+while len(choices)<4:
+    vec=list_randint(n,-3,3)
+    if vec not in choices:
+        choices.append("$! %s !$" % str(tuple(vec)))
+
+lstvar=",".join(["x","y","z","t","u","v"][:n])
+sys_tex=latexsys(A,B)
+radio.loadContent(choices)
+radio.setSolByIndex(0)
+radio.shuffle()
+==
+
+text ==
+On considère le système suivant (d'inconnues $! {{lstvar}} !$) :
+$$ {{sys_tex}} $$
+
+Parmi les propositions suivantes, laquelle est la solution de ce système ?
+==
