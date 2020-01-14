e1c5f51dc1b3e199d7f6f5068d0006d9b2406f9b
David DOYEN <David.Doyen@u-pem.fr>
2020-01-14 16:23:31 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
@@ -7,7 +7,7 @@ n=param['size']
 coeffbound = param['coeffbound']
 sparsity= param['sparsity']
 A=rand_int_matrix_invertible(n,3)
-vec=[1,2,3]
+vec=tuple([1,2,3])
 B=A*Matrix(vec)
 
 vec_tex=str(vec)
