fa77b8f9ad80e06bc6d7206f0582b9a5269365cf
David DOYEN <David.Doyen@u-pem.fr>
2020-01-16 10:34:10 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys_4_5_halfzero_rat.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys_4_5_halfzero_rat.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/linsys_4_5_halfzero_rat.pl
@@ -4,7 +4,9 @@ title = Système linéaire 4 x 4
 
 param.size % 4
 
-param.coeffbound % 3
+param.coeffboundA % 5
+
+param.coeffboundB % 3
 
 param.sparsity % 0.5
 
@@ -13,6 +15,7 @@ param.typesol % "rat"
 param.maxdet % 10
 
 param.mindet % 2
+
 
 
 
