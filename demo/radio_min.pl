7a2cc978fa20556f8d12fa5e29bdfd36946b95d1
David DOYEN <David.Doyen@u-pem.fr>
2020-01-17 11:40:53 +0100

    ddoyen modified Yggdrasil/demo/radio_min.pl

--- demo/radio_min.pl
+++ demo/radio_min.pl
@@ -1,11 +1,6 @@
-extends = /template/basic.pl
+extends = /template/radio.pl
 
 title = Plus petit nombre
-
-@ /utils/radiogroup.py [customradiogroup.py]
-
-radio =: RadioGroup
-radio.decorator = CustomRadioGroup
 
 before ==
 import random as rd
@@ -18,18 +13,3 @@ radio.shuffle()
 text ==
 Sélectionner le plus petit nombre de la liste suivante.
 ==
-
-form ==
-{{ radio|component }}
-==
-
-evaluator ==
-grade = radio.eval()
-radio.disabled=True
-==
-
-
-
-
-
-
