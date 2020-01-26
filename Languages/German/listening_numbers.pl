ce071a657e4b83be53db7b6ac47957cc5abb3fcc
David DOYEN <David.Doyen@u-pem.fr>
2020-01-26 16:06:59 +0100

    ddoyen modified Yggdrasil/Languages/German/listening_numbers.pl

---
+++ Languages/German/listening_numbers.pl
@@ -0,0 +1,29 @@
+extends = /template/basicinput.pl
+
+input.type = number
+
+title = Addition
+
+before ==
+import random as rd
+a=rd.randint(10,20)
+b=rd.randint(10,20)
+==
+
+text ==
+ <audio controls>
+<source src="number.ogg" type="audio/ogg">
+Your browser does not support the audio element.
+</audio> 
+==
+
+evaluator ==
+try:
+    if int(input.value)==a+b:
+        grade=(100,"")
+    else:
+        grade=(0,"%d + %d = %d" % (a,b,a+b))
+except:
+    grade=(-1,"Votre réponse n'est pas un nombre entier.")
+==
+
