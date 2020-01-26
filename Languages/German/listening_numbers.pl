0e36cc504989458d100e872ba986acfccb1d95df
David DOYEN <David.Doyen@u-pem.fr>
2020-01-26 17:52:15 +0100

    ddoyen modified Yggdrasil/Languages/German/listening_numbers.pl

--- Languages/German/listening_numbers.pl
+++ Languages/German/listening_numbers.pl
@@ -19,7 +19,7 @@ audiofile=lstaudio[sol-1]
 ==
 
 text ==
-<button onclick="playAudio()" class="btn"><i class="fa fas-ear"></i></button>
+<button onclick="playAudio()" class="btn"><i class="fas fa-ear"></i></button>
 Ecrire le nombre entendu.
 ==
 
