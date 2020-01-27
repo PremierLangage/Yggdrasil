69d2794570cf73515e31c49ecacd5a90a9504c10
David DOYEN <David.Doyen@u-pem.fr>
2020-01-27 12:56:35 +0100

    ddoyen modified Yggdrasil/Mathematics/Magdalena/test.pl

---
+++ Mathematics/Magdalena/test.pl
@@ -0,0 +1,33 @@
+@ /utils/sandboxio.py
+@ /builder/before2.py [builder.py]
+@ /grader/evaluator2.py [grader.py]
+
+title = Addition
+
+before ==
+import random as rd
+a=rd.randint(10,20)
+b=rd.randint(10,20)
+==
+
+text ==
+Calculer {{a}} + {{b}}.
+==
+
+input =: Input
+
+form ==
+{{ input | component }}
+==
+
+
+evaluator ==
+try:
+    if int(input.value)==a+b:
+        grade=(100,"")
+    else:
+        grade=(0,"%d + %d = %d" % (a,b,a+b))
+except:
+    grade=(-1,"Votre réponse n'est pas un nombre entier.")
+==
+
