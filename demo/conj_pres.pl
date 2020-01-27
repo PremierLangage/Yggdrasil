1ba78791f7a9b3d2e2b5fbf56c1997e768909cf1
David DOYEN <David.Doyen@u-pem.fr>
2020-01-27 14:37:39 +0100

    ddoyen modified Yggdrasil/demo/conj_pres.pl

--- demo/conj_pres.pl
+++ demo/conj_pres.pl
@@ -23,11 +23,12 @@ dic_txt_prs={'1S':'1ère personne du singulier',
              '1P':'1ère personne du pluriel',
              '2P':'2e personne du pluriel',
              '3P':'3e personne du pluriel'}
+dic_txt_pronom={'1S':'je','2S':'tu','3S':'il','1P':'nous','2P':'vous','3P':'ils'}
 
 verbe_inf=row['infinitif']
 verbe_conj=row[p]
 txt_prs=dic_txt_prs[p]
-
+pronom=dic_txt_pronom[p]
 ==
 
 text ==
@@ -38,7 +39,7 @@ input =: Input
 
 form ==
 <div class="d-flex align-items-center">
-  <div>Flex item</div>
+  <div>{{pronom}}</div>
   <div class="flex-grow-1">{{input|component}}</div>
 </div>
 ==
@@ -49,6 +50,7 @@ if input.value==verbe_conj:
 else:
     grade=(0,"")
 ==
+
 
 
 
