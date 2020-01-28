3714a1cd79a256c0decfeed65b7bb7a7333c982f
David DOYEN <David.Doyen@u-pem.fr>
2020-01-28 11:36:46 +0100

    ddoyen modified Yggdrasil/demo/radio_min.pl

--- demo/radio_min.pl
+++ demo/radio_min.pl
@@ -5,11 +5,12 @@ title = Plus petit nombre
 before ==
 import random as rd
 content=rd.sample(list(range(50)),5)
-radio.loadContent([str(item) for item in content])
-radio.setSolByContent(str(min(content)))
+radio.load_choices([str(item) for item in content])
+radio.set_sol_by_content(str(min(content)))
 radio.shuffle()
 ==
 
 text ==
 Sélectionner le plus petit nombre de la liste suivante.
 ==
+
