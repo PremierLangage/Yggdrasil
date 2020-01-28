670274766e700c038bbbf32621222ae818b4b040
David DOYEN <David.Doyen@u-pem.fr>
2020-01-28 11:38:35 +0100

    ddoyen modified Yggdrasil/demo/checkbox_multiple.pl

--- demo/checkbox_multiple.pl
+++ demo/checkbox_multiple.pl
@@ -6,9 +6,10 @@ before ==
 import random as rd
 mult3 = [str(n) for n in range(50,100) if n % 3 == 0]
 other = [str(n) for n in range(50,100) if n % 3 != 0]
-checkbox.loadRightWrong(mult3,other,5,rd.randint(1,4))
+checkbox.load_right_wrong(mult3,other,5,rd.randint(1,4))
 ==
 
 text = Parmi les nombres suivants, lesquels sont des multiples de 3 ?
+
 
 
