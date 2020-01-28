67fc7007ccaf51a4667cf77779efed817cec02f0
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-01-28 09:25:10 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/templates/MCQ_evaluator.py

--- ComputerScience/OperatingSystem/templates/MCQ_evaluator.py
+++ ComputerScience/OperatingSystem/templates/MCQ_evaluator.py
@@ -83,13 +83,13 @@ if __name__ == "__main__":
         for index in indices:
             if index < len(question[1]):
                 context['goods'].append(str(index))
-                group.items.append({ "id": str(index), "content": question[1][index] })
+                context['group.items'].append({ "id": str(index), "content": question[1][index] })
                 # context['form'] +='<input type="checkbox" name="c_' + \
                 # str(index) + '" value="' + str(index) + '" id="form_' + \
                 # str(index) + '">' + question[1][index] + "<br />"
             else:
                 context['bads'].append(str(index))
-                group.items.append({ "id": str(index), "content": question[2][index - len(question[1])] })
+                context['group.items'].append({ "id": str(index), "content": question[2][index - len(question[1])] })
                 # context['form'] +='<input type="checkbox" name="c_' + \
                 # str(index) + '" value="' + str(index) + '" id="form_' + \
                 # str(index) + '">' + \
