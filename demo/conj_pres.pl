651b8f1422d52cffabadfd694b7ca09bb75a3840
David DOYEN <David.Doyen@u-pem.fr>
2020-01-28 18:08:26 +0100

    ddoyen modified Yggdrasil/demo/conj_pres.pl

--- demo/conj_pres.pl
+++ demo/conj_pres.pl
@@ -5,7 +5,6 @@ extends = /template/basicinput.pl
 title = Conjugaison
 
 before ==
-
 import random as rd
 import csv
 
@@ -15,25 +14,16 @@ with open('data.csv',newline='') as file:
 row=rd.choice(rows)
 p=rd.choice(['1S','2S','3S','1P','2P','3P'])
 
-dic_txt_prs={'1S':'1ère personne du singulier',
-             '2S':'2e personne du singulier',
-             '3S':'3e personne du singulier',
-             '1P':'1ère personne du pluriel',
-             '2P':'2e personne du pluriel',
-             '3P':'3e personne du pluriel'}
-dic_txt_pronom={'1S':'je','2S':'tu','3S':'il','1P':'nous','2P':'vous','3P':'ils'}
+dic_pronom={'1S':'je','2S':'tu','3S':'il','1P':'nous','2P':'vous','3P':'ils'}
 
 verbe_inf=row['infinitif']
 verbe_conj=row[p]
-txt_prs=dic_txt_prs[p]
-pronom=dic_txt_pronom[p]
+pronom=dic_pronom[p]
 ==
 
 text ==
 Conjuguer le verbe **{{verbe_inf}}** au présent avec le pronom indiqué.
 ==
-
-input =: Input
 
 form ==
 <div class="d-flex align-items-center">
