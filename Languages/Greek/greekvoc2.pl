86c3dfe8f9d7c2046c53542eaff9529a7a5341a1
David DOYEN <David.Doyen@u-pem.fr>
2020-01-31 14:01:24 +0100

    ddoyen modified Yggdrasil/Languages/Greek/greekvoc2.pl

--- Languages/Greek/greekvoc2.pl
+++ Languages/Greek/greekvoc2.pl
@@ -35,10 +35,10 @@ progress=20
 text ==
 <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
   <div class="btn-group mr-1" role="group" aria-label="First group">
-    <button type="button" class="btn btn-secondary">1</button>
-    <button type="button" class="btn btn-secondary">2</button>
-    <button type="button" class="btn btn-secondary">3</button>
-    <button type="button" class="btn btn-secondary">4</button>
+    <button type="button" class="btn btn-outline-secondary">1</button>
+    <button type="button" class="btn btn-outline-secondary">2</button>
+    <button type="button" class="btn btn-outline-secondary">3</button>
+    <button type="button" class="btn btn-outline-secondary">4</button>
   </div>
 </div>
 
