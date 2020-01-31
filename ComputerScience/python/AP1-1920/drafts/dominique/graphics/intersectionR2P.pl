5ec324c186914f9e8083de5faf58941248dd7cfe
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-01-31 10:33:24 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/graphics/intersectionR2P.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/graphics/intersectionR2P.pl
@@ -0,0 +1,14 @@
+
+@ rect-2-points.png [image.png]
+
+
+text==
+
+Ecrire une fonction **disjoints(p1,p2,p3,p4)** permettant de tester si (bool) oui ou non le deux rectangle sont disjoints.
+On suppose que p1=(x1,y1) et pi=(xi,yi) confrormément au dessin si dessous.
+
+<img src="image.png" width="25%" >
+
+==
+
+
