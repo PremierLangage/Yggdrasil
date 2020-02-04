d781bd88c4ec60cefbc71233e7fb5a7db135c624
David DOYEN <David.Doyen@u-pem.fr>
2020-02-04 20:47:42 +0100

    ddoyen modified Yggdrasil/grader/evaluator2.py

--- grader/evaluator2.py
+++ grader/evaluator2.py
@@ -2,19 +2,8 @@
 # coding: utf-8
 
 import sys, json, jsonpickle, time
-
+from feedback import format_feedback
 from sandboxio import output, get_context
-
-def format_feedback(score,feedback):
-    text0="""<div class="alert {}"><strong>{}</strong> {}</div>"""
-    if score==-1:
-        return text0.format('alert-info','Attention !',feedback)
-    elif score==100:
-        return text0.format('alert-success','Bonne réponse.',feedback)
-    elif score==0:
-        return text0.format('alert-danger','Mauvaise réponse.',feedback)
-    else:
-        return text0.format('alert-warning','Réponse partiellement correcte.',feedback)
 
 class StopEvaluatorExec(Exception):
     pass
@@ -78,6 +67,7 @@ if __name__ == "__main__":
                     ffeedback="Score : %d / 100" % score
 
     output(score, ffeedback, dic)
+
 
 
 
