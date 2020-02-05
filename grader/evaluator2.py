74bd0271e04001eb0cbbd5f7c9de786a968b2078
David DOYEN <David.Doyen@u-pem.fr>
2020-02-05 19:02:44 +0100

    ddoyen modified Yggdrasil/grader/evaluator2.py

--- grader/evaluator2.py
+++ grader/evaluator2.py
@@ -33,9 +33,12 @@ def format_feedback_lightscore(score,feedback):
         tpl="""<div class="alert {}"><strong>{}</strong> {}</div>"""
         return tpl.format('alert-info',score, feedback)
     tpl="""
-    <div class="panel panel-primary">
-    <div class="panel-heading">Score : {} / 100 </div>
-    </div>
+<div class="card bg-light mb-3" style="max-width: 18rem;">
+  <div class="card-header">Header</div>
+  <div class="card-body">
+    <h5 class="card-title">Score : {} / 100</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
     """
     return tpl.format(score)
 
