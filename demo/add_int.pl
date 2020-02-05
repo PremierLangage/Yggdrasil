2b81976f1fb3a9e2d70cbfded2d95b64d9707161
David DOYEN <David.Doyen@u-pem.fr>
2020-02-05 21:23:12 +0100

    ddoyen modified Yggdrasil/demo/add_int.pl

--- demo/add_int.pl
+++ demo/add_int.pl
@@ -26,7 +26,7 @@ settings.feedback = lightscore
 
 evaluator ==
 if myinput.value==a+b:
-    grade=(100,"")
+    grade=(100,isinstance(myinput,Input))
 else:
     grade=(0,f"La réponse est {a+b}.")
 myinput.disabled = True
