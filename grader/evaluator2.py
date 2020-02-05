4baa05811f7ac136d2d531696bf597344bec92a8
David DOYEN <David.Doyen@u-pem.fr>
2020-02-05 19:26:35 +0100

    ddoyen modified Yggdrasil/grader/evaluator2.py

--- grader/evaluator2.py
+++ grader/evaluator2.py
@@ -42,6 +42,7 @@ def format_feedback_lightscore(score,feedback):
   </div>
 </div>
     """
+    tpl="""<div class="alert bg-default"><strong>Score : {} / 100.</strong> {}</div>"""
     return tpl.format(score)
 
 
