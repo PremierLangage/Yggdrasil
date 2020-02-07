072b6141426608855406f1dcb9fbbd2b0ff4b7ee
David DOYEN <David.Doyen@u-pem.fr>
2020-02-07 15:27:45 +0100

    ddoyen modified Yggdrasil/Languages/English/listening_sentences.pl

--- Languages/English/listening_sentences.pl
+++ Languages/English/listening_sentences.pl
@@ -10,7 +10,7 @@ title = Listening
 
 before ==
 import random as rd
-k=rd.randint(3)
+k=rd.randint(0,2)
 lst_sentences=["I'm gonna make him an offer he can't refuse.",
                "May the force be with you.",
                "Frankly, my dear, I don't give a damn."]
