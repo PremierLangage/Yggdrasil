66542769d33619f0f958e009fd6f76ddd5b42cfc
David DOYEN <David.Doyen@u-pem.fr>
2020-02-07 23:02:39 +0100

    ddoyen modified Yggdrasil/demo/sortlist_premiers_ministres.pl

--- demo/sortlist_premiers_ministres.pl
+++ demo/sortlist_premiers_ministres.pl
@@ -1,12 +1,8 @@
-extends = /template/sortlist.pl
+extends = /model/basic.pl
 
 title = Classement : Premiers ministres
 
-@ premiers_ministres.csv [data.csv]
-
-field= name
-
-nb % 5
+@ premiers_ministres.csv
 
 before ==
 import random as rd
@@ -24,6 +20,17 @@ text ==
 Classer ces premiers ministres de la Ve République du plus ancien au plus récent (selon la date d'entrée en fonction).
 ==
 
+sortlist =: SortList
 
+@ /utils/sortlist.py [customsortlist.py]
+sortlist.decorator = CustomSortList
 
+form ==
+{{ sortlist|component }}
+==
 
+evaluator ==
+grade = sortlist.eval()
+==
+
+settings.feedback = lightscore
