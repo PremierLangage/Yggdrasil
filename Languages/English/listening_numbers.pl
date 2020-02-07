73bc63f621c07e5b73e9eccb6e7d33d6aa2c93e1
David DOYEN <David.Doyen@u-pem.fr>
2020-02-07 22:46:46 +0100

    ddoyen modified Yggdrasil/Languages/English/listening_numbers.pl

---
+++ Languages/English/listening_numbers.pl
@@ -0,0 +1,47 @@
+extends = /template/basicinput.pl
+
+audio1 =$ /Languages/German/audio/De-eins.ogg
+audio2 =$ /Languages/German/audio/De-zwei.ogg
+audio3 =$ /Languages/German/audio/De-drei.ogg
+audio4 =$ /Languages/German/audio/De-vier.ogg
+audio5 =$ /Languages/German/audio/De-fünf.ogg
+audio6 =$ /Languages/German/audio/De-sechs.ogg
+
+input.type = number
+
+title = Numbers
+
+before ==
+import random as rd
+import random as rd
+
+with open('data.json','r',encoding='utf8') as f:
+    number=json.load(f)
+
+s=number['3'].encode('utf-8')
+src="data:audio/ogg;base64,
+==
+
+text ==
+<button onclick="playAudio()" class="btn btn-info"><i class="fas fa-volume-up"></i></button> Ecrire le nombre entendu (en caractères numériques).
+==
+
+extrajs==
+<audio id="Audio" style="display:none" src="data:audio/mp3;{{s}}">
+</audio> 
+<script>
+var x = document.getElementById("Audio"); 
+function playAudio() { 
+  x.play(); 
+} 
+</script>
+==
+
+evaluator ==
+if input.value==sol:
+    grade=(100,"")
+else:
+    grade=(0,str(sol))
+==
+
+
