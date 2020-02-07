9256ca35bb70d7f1470e6d8bfea6b6e8c2f5a13d
David DOYEN <David.Doyen@u-pem.fr>
2020-02-07 11:57:21 +0100

    ddoyen modified Yggdrasil/demo/checkbox_multiple.pl

--- demo/checkbox_multiple.pl
+++ demo/checkbox_multiple.pl
@@ -1,4 +1,4 @@
-extends = /template/checkbox.pl
+extends = /template/basic.pl
 
 title = QCM : Multiples de 3
 
@@ -9,7 +9,23 @@ other = [str(n) for n in range(50,100) if n % 3 != 0]
 checkbox.load_right_wrong(mult3,other,5,rd.randint(1,4))
 ==
 
-text = Parmi les nombres suivants, lesquels sont des multiples de 3 ?
+text ==
+Parmi les nombres suivants, lesquels sont des multiples de 3 ?
+==
 
+@ /utils/checkbox.py [checkbox.py]
+
+checkbox =: CheckboxGroup
+checkbox.decorator = Checkbox
+
+settings.feedback = lightscore
+
+form ==
+{{ checkbox | component }}
+==
+
+evaluator ==
+grade = checkbox.eval()
+==
 
 
