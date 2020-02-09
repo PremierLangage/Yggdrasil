9201df209cfd6d204481a154fbafc1bbcea5050d
David DOYEN <David.Doyen@u-pem.fr>
2020-02-09 14:30:52 +0100

    ddoyen modified Yggdrasil/demo/sortlist_premiers_ministres.pl

--- demo/sortlist_premiers_ministres.pl
+++ demo/sortlist_premiers_ministres.pl
@@ -17,7 +17,7 @@ sortlist.load_items([rows[i]["nom"] for i in lst])
 ==
 
 text ==
-  <a href="#" data-toggle="tooltip" title="Hooray!" disabled>Hover over me</a>
+  <a href="#" data-toggle="tooltip" title="Hooray!">Hover over me</a>
 
 Classer ces premiers ministres de la Ve République du plus ancien au plus récent (selon la date d'entrée en fonction).
 ==
@@ -38,10 +38,6 @@ grade = sortlist.eval()
 settings.feedback = lightscore
 
 extrajs ==
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <script>
 $(document).ready(function(){
   $('[data-toggle="tooltip"]').tooltip();
