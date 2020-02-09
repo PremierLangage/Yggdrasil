a62a3c01027cefdb6751c435fe1c91e767f628c6
David DOYEN <David.Doyen@u-pem.fr>
2020-02-09 14:14:30 +0100

    ddoyen modified Yggdrasil/demo/sortlist_premiers_ministres.pl

--- demo/sortlist_premiers_ministres.pl
+++ demo/sortlist_premiers_ministres.pl
@@ -36,7 +36,9 @@ grade = sortlist.eval()
 settings.feedback = lightscore
 
 extrajs ==
-
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 <script>
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
