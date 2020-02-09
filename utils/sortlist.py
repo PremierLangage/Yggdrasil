b62b7aab3b2e40b8d70cec80ff1f0fa8bc0caeea
David DOYEN <David.Doyen@u-pem.fr>
2020-02-09 15:54:40 +0100

    ddoyen modified Yggdrasil/utils/sortlist.py

--- utils/sortlist.py
+++ utils/sortlist.py
@@ -7,24 +7,33 @@ class CustomSortList(Component):
     def __init__(self, **kwargs):
         self.selector = 'c-sort-list'
         self.decorator = 'CustomSortList'
+        self._items0 = []
         self.items = []
         self.grading =[]
         self._order = []
         super().__init__(**kwargs)
 
-    def load_items(self,content,feedback=[]):
-        self.items = []
+    def load_items(self,lst):
+        self._items0 = []
         self._order = []
-        self._feedback ={}
-        for i in range(len(content)):
+        for item in lst:
             id = str(uuid.uuid4())
-            self.items.append({
-                "id": id,
-                "content": content[i]
-            })
-            self._feedback[id]=feedback[i]
+            if isinstance(item,tuple):
+                self._items0.append({
+                    "id": id,
+                    "content": item[0],
+                    "feedback": item[1]
+                })
+            else:
+                self._items0.append({
+                    "id": id,
+                    "content": item
+                })
             self._order.append(id)
         rd.shuffle(self.items)
+        self.items=self._items0.copy()
+        for item in self.items:
+            item.pop('feedback')
 
     def eval(self):
         errors = 0
