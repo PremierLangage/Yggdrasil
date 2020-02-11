b49c454e52358d2675ae941886df6dd8f642faf0
David DOYEN <David.Doyen@u-pem.fr>
2020-02-11 13:57:51 +0100

    ddoyen modified Yggdrasil/input/textselect/textselect_verb.pl

---
+++ input/textselect/textselect_verb.pl
@@ -0,0 +1,91 @@
+@ /utils/sandboxio.py
+@ /builder/before2.py [builder.py]
+@ /grader/evaluator2.py [grader.py]
+
+title = Repérage des verbes
+
+text ==
+Identifier le verbe conjugué la phrase ci-dessous.
+==
+
+selectable =: Text
+
+selectable.text == 
+Artur a horreur de la marche à pied.
+==
+selectable.selectable % true
+
+before==
+
+==
+
+form==
+<div id="textselect"
+ style="-moz-user-select: none; -webkit-user-select: none; -ms-user-select:none; user-select:none;-o-user-select:none;" 
+ unselectable="on"
+ onselectstart="return false;" 
+ onmousedown="return false;">
+{{htmltextselect | safe}}
+</div>
+==
+
+evaluator==
+
+rightwords = ['πρεσβύτερος', 'νεώτερος']
+found = 0
+error = 0
+
+for e in selectable.selections:
+    if e['content'] in rightwords:
+        e['css'] = "success-state"
+        found += 1
+    else:
+        e['css'] = "error-state"
+        error += 1
+
+if found == 10 and error == 0:
+    score = 100
+    msg = '<span class="success-state">Bravo, vous avez trouvé tous les adjectifs comparatifs !</span>'
+else:
+    score = max([0, 10*(found - error)])
+    nb_error = (10 - found) + error
+    if nb_error > 1:
+        msg = '<span class="error-state">Il y a {} erreurs...</spam>'.format(nb_error)
+    else:
+        msg = '<span class="error-state">Il y a 1 seule erreur...</spam>'
+
+grade = (score, msg)
+==
+
+extracss==
+<style>
+.exercise__instructions, .exercise__feedback {
+    margin-bottom: 16px;
+}
+</style>
+==
+
+extrajs ==
+<script>
+var string = "{Ô} {rage} ! {Ô} {désespoir} ! {Ô} {vieillesse} {ennemie} !<br> {N'}{ai}-{je} {donc} {tant} {vécu} {que} {pour} {cette} {infamie} ?";
+var index = -1;
+string=string.replace(/\{([^\{\}]*)\}/g, function(match,p) {
+  index++;
+  return `<span data-index="${index}">${p}</span>`;
+});
+  document.getElementById("textselect").innerHTML = string;
+</script>
+==
+
+comptest ==
+ <script>
+var string = "{L}'{élève} {n}'{a} {pas} {fait} {son} {travail}.";
+var index = -1;
+string=string.replace(/\{([^\{\}]*)\}/g, function(match,p) {
+  index++;
+  return `<span data-index="${index}">${p}</span>`;
+});
+alert(string);
+</script>
+==
+
