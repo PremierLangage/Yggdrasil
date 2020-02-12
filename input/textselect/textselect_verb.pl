a6ea77ab5c67a805142f5c022b2a4cafbcb3cf33
David DOYEN <David.Doyen@u-pem.fr>
2020-02-12 19:16:19 +0100

    ddoyen modified Yggdrasil/input/textselect/textselect_verb.pl

--- input/textselect/textselect_verb.pl
+++ input/textselect/textselect_verb.pl
@@ -20,29 +20,7 @@ s="Artur {a} horreur de la marche à pied."
 
 import re
 
-def bracket_words(string):
-    lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|\[[^\]]*\]|[^\{\}\[\]]+",string)
-    for i in range(len(lst)):
-        s=lst[i]
-        if s[0]=="[":
-            lst[i]=s[1:-1]
-        elif s[0]!="{":
-            lst[i]=re.sub(r"(\w+)", r"{\1}",s)
-    return "".join(lst)
-    
-def read_text_bracket(string):
-    lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|[^\{\}]+",string)
-    selection=[]
-    k=0
-    for i in range(len(lst)):
-        s=lst[i]
-        if s[0:2]=="{{":
-            selection.append(k)
-            lst[i]=s[1:-1]
-            k+=1
-        elif s[0]=="{":
-            k+=1
-    return ("".join(lst),selection)
+
 
 ==
 
