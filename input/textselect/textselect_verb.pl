9e35e7048ce49e331b961d0ee4bd28de59cc63fa
David DOYEN <David.Doyen@u-pem.fr>
2020-02-12 19:21:24 +0100

    ddoyen modified Yggdrasil/input/textselect/textselect_verb.pl

--- input/textselect/textselect_verb.pl
+++ input/textselect/textselect_verb.pl
@@ -36,7 +36,7 @@ def read_text_bracket(string):
     k=0
     for i in range(len(lst)):
         s=lst[i]
-        if s[0]=="{"
+        if s[0]=="{":
             if len(s)> 2 and s[1]=="{":
                 selection.append(k)
                 lst[i]=s[1:-1]
