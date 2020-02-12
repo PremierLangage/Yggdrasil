3ad52b1ca11d08f368b3d66fbc2702741baf7439
David DOYEN <David.Doyen@u-pem.fr>
2020-02-13 00:12:37 +0100

    ddoyen modified Yggdrasil/demo/dragdrop_inequalities.pl

--- demo/dragdrop_inequalities.pl
+++ demo/dragdrop_inequalities.pl
@@ -1,94 +1,56 @@
-extends = /Mathematics/template/mathbasic.pl
-extends = /Mathematics/exercises/foundations/maps/finite_map_.pl
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator2.py
+builder =@ /builder/before2.py
 
-@ /utils/dragdrop.py [dragdrop.py]
+drag1 =: DragDrop
+drag1.content = $$\in$$
 
-title = Composition d'applications
+drag2 =: DragDrop
+drag2.content = $$\subset$$
 
-before ==
-from dragdrop import Label, Drop
+before==
 
-n=randint(4,5)
+drops = []
+for i in range(len(sets)):
+    drop = DragDrop(
+        id=f"drop{i}",
+        droppable=True
+    )
+    drops.append(drop)
 
-drag=[]
-for i in range(1,n+1):
-    globals()[f"drop{i}"] = Label(id=f"drop{i}",droppable=True)
-    drag.append(globals()[f"drop{i}"])
-
-drop=[]
-for i in range(1,n+1):
-    name="drop"+str(i)
-    globals()[name] = DragDrop()
-    globals()[name].droppable = True
-    drop.append(globals()[name])
-
-
-from jinja2 import Template
-
-row1=list(range(1,n+1))
-row2f=list_randint(n,1,n)
-row2g=list_randint(n,1,n)
-tablef = Template(tpltable).render(row1=row1,row2=row2f,f="f")
-tableg = Template(tpltable).render(row1=row1,row2=row2g,f="g")
-
-case=randitem([1,2])
-sol=[]
-if case==1:
-    for i in range(n):
-        sol.append(row2f[row2g[i]-1])
-    h="f \circ g"
-else:
-    for i in range(n):
-        sol.append(row2g[row2f[i]-1])
-    h="g \circ f"
 ==
 
-text ==
-{{namedrag}}
-On considère les applications  $! f:\\{1,\ldots,{{n}} \\}\rightarrow \\{1,\ldots,{{n}} \\} !$ et $! g:\\{1,\ldots,{{n}} \\}\rightarrow \\{1,\ldots,{{n}} \\} !$ définies par les tableaux ci-dessous.
-<br>
-{{tablef|safe}}
-<br>
-{{tableg|safe}}
-<br>
-Déterminer l'application $! {{h}} !$.
+text==
 ==
 
-form ==
-<table>
-<tr>
-<th> $! x !$ </th>
-{% for item in row1 %}
-<td> {{item|safe}} </td>
-{% endfor %}
-</tr>
-<tr>
-<th> $! {{h}} (x) !$ </th>
-{% for e in drop %}
-<td> {{e|component}} </td>
-{% endfor %}
-</tr>
-</table>
+form==
+{{ drag1|component }}
+{{ drag2|component }}
+<br/>
 
-{% for d in drag -%}
-{{ d|component }}
-{%- endfor %}
+<ul>
+    {% for e in sets %}
+    <li>
+        {{ e[0] }}
+        {{ drops[loop.index0]|component }}
+        {{ e[2] }}
+    </li>
+    {% endfor %}
+</ul>
 ==
 
 evaluator==
-drop=[]
-for i in range(1,n+1):
-    name="drop"+str(i)
-    drop.append(locals()[name])
+errors = 0
+for i, e in enumerate(sets):
+    drop = globals()[f'drop{i}']
+    drop.css = 'success-state animated rotateIn'
+    if drop.droppedId != e[1]:
+        drop.css = 'error-state animated pulse infinite'
+        errors += 1
 
-feedback=""
-score=100
-for i in range(n):
-    if drop[i].content==str(sol[i]):
-        drop[i].css = "success-state"
-    else:
-        score=0
-        drop[i].css = "error-state" 
+n = len(sets)
+if errors == 0:
+    grade = (100, f'<span class="success-state animated pulse infinite">{n}/{n}</span>')
+else:
+    grade = (100, f'<span class="error-state animated pulse infinite">{n - errors}/{n}</span>')
 ==
-
-
