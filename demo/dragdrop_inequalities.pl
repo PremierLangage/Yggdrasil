cf73cd3ce196ec211d146ca81b51b96a3bb0fc14
David DOYEN <David.Doyen@u-pem.fr>
2020-02-13 08:07:25 +0100

    ddoyen modified Yggdrasil/demo/dragdrop_inequalities.pl

--- demo/dragdrop_inequalities.pl
+++ demo/dragdrop_inequalities.pl
@@ -16,8 +16,8 @@ chiffres= [(2,4),(7,6),(5,3),(6,9)]
 
 drops = [Drop() for i in range(4)]
 
-#for drop in drops:
-#    globals()[drop.id] = drop
+for drop in drops:
+    globals()[drop.id] = drop
 
 s=str(globals().keys())
 
