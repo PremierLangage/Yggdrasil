86a8cf7351b72cc3b3cd2e880ea7d71816fab316
David DOYEN <David.Doyen@u-pem.fr>
2020-02-14 18:40:29 +0100

    ddoyen modified Yggdrasil/utils/dragdrop.py

--- utils/dragdrop.py
+++ utils/dragdrop.py
@@ -6,3 +6,11 @@ def Drop(**kwargs):
 
 def Label(**kwargs):
     return DragDrop(id=str(uuid.uuid4()),droppable=False,**kwargs)
+
+def Drops(arg):
+    if isinstance(arg,int):
+        return [Drop() for i in range(arg)]
+
+def Labels(**kwargs):
+    if isinstance(arg,int):
+        return [Label(content=content) for content in arg]
