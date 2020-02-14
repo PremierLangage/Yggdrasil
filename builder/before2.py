99ca6523bf0b210357a9de2ae61b2782cfee7c04
David DOYEN <David.Doyen@u-pem.fr>
2020-02-14 15:03:59 +0100

    ddoyen modified Yggdrasil/builder/before2.py

--- builder/before2.py
+++ builder/before2.py
@@ -49,7 +49,7 @@ if __name__ == "__main__":
 
     dic['attempt']=1
 
-    dic['text']=dic
+    dic['text']=dic.copy()
 
     with open(output_json, "w+") as f:
         f.write(jsonpickle.encode(dic, unpicklable=False))
