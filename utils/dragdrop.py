1387f3d7528b915783df4b31d415f9a7a178b1a6
David DOYEN <David.Doyen@u-pem.fr>
2020-02-14 17:54:30 +0100

    ddoyen modified Yggdrasil/utils/dragdrop.py

--- utils/dragdrop.py
+++ utils/dragdrop.py
@@ -2,7 +2,7 @@ from components import Component,DragDrop
 import uuid
 
 def Drop(**kwargs):
-    return DragDrop(droppable=True,**kwargs)
+    return DragDrop(id=str(uuid.uuid4()),droppable=True,**kwargs)
 
 class Label(DragDrop):
     def __init__(self, **kwargs):
@@ -14,5 +14,13 @@ class Label(DragDrop):
         self.droppable = False
         super().__init__(**kwargs)
 
-
-
+"""
+class Drop(DragDrop):
+    def __init__(self, **kwargs):
+        if 'id' not in kwargs:
+            kwargs['id']=str(uuid.uuid4())
+        self.selector = 'c-drag-drop'
+        self.decorator = 'DragDrop'
+        self.droppable = True
+        super().__init__(**kwargs)
+"""
