944eb871bccaf87cf5c8fe4980fc11010adb6e2c
David DOYEN <David.Doyen@u-pem.fr>
2020-02-14 17:30:03 +0100

    ddoyen modified Yggdrasil/demo/dragdrop_inequalities.pl

--- demo/dragdrop_inequalities.pl
+++ demo/dragdrop_inequalities.pl
@@ -9,7 +9,7 @@ before==
 from dragdrop import Drop
 chiffres= [(2,4),(7,6),(5,3),(6,9)]
 
-drops = [DragDrop(content="3") for i in range(4)]
+drops = [Drop(content="3") for i in range(4)]
 ==
 
 text==
