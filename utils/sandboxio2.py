0ead042b58f616707d0c4364d61bca5ed97ca710
David DOYEN <David.Doyen@u-pem.fr>
2020-02-14 10:44:11 +0100

    ddoyen modified Yggdrasil/utils/sandboxio2.py

--- utils/sandboxio2.py
+++ utils/sandboxio2.py
@@ -14,7 +14,7 @@ def get_context():
     """Return the dictionnary containing the context of the exercise."""
     with open(sys.argv[1], "r") as f:
         context = json.load(f)
-    #Component.sync_context(context)
+    Component.sync_context(context)
     return context
 
 
