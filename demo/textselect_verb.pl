fe20de73a056278dc1199ba161ae1e2bec4428fe
David DOYEN <David.Doyen@u-pem.fr>
2020-02-15 16:40:58 +0100

    ddoyen modified Yggdrasil/demo/textselect_verb.pl

--- demo/textselect_verb.pl
+++ demo/textselect_verb.pl
@@ -4,22 +4,3 @@
 
 title = Repérage des verbes
 
-text ==
-Identifier le verbe conjugué la phrase ci-dessous.
-==
-
-txtsel =: Text
-@ /utils/textselect.py [customtext.py]
-txtsel.decorator = CustomText
-
-txtsel.selectable % true
-
-before ==
-txtsel.loadtext("{{Artur}} a {{horreur}} de la marche à pied.")
-==
-
-form==
-{{ txtsel|component}}
-==
-
-
