752b2f7578cbb03a59f7ed7ca6aef1d0901868bc
David DOYEN <David.Doyen@u-pem.fr>
2020-02-15 16:12:49 +0100

    ddoyen modified Yggdrasil/utils/textselect.py

--- utils/textselect.py
+++ utils/textselect.py
@@ -1,6 +1,32 @@
 import uuid
 from components import Component
 import random as rd
+import re
+
+def bracket_words(string):
+    lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|\[[^\]]*\]|[^\{\}\[\]]+",string)
+    for i in range(len(lst)):
+        s=lst[i]
+        if s[0]=="[":
+            lst[i]=s[1:-1]
+        elif s[0]!="{":
+            lst[i]=re.sub(r"(\w+)", r"{\1}",s)
+    return "".join(lst)
+    
+def read_text_bracket(string):
+    lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|[^\{\}]+",string)
+    selection=[]
+    k=0
+    for i in range(len(lst)):
+        s=lst[i]
+        if s[0]=="{":
+            if len(s)> 2 and s[1]=="{":
+                selection.append(k)
+                lst[i]=s[1:-1]
+            k+=1
+    
+    return ("".join(lst),selection)
+
 
 class CustomText(Component):
     def __init__(self, **kwargs):
