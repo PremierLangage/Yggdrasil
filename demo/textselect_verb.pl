bb66428f9f4511c90906ec778b4b2d4d77f7e8b7
David DOYEN <David.Doyen@u-pem.fr>
2020-02-15 15:56:12 +0100

    ddoyen modified Yggdrasil/demo/textselect_verb.pl

--- demo/textselect_verb.pl
+++ demo/textselect_verb.pl
@@ -8,65 +8,19 @@ text ==
 Identifier le verbe conjugué la phrase ci-dessous.
 ==
 
-selectable =: Text
+txtsel =: Text
+@ /utils/textselect.py [customtext.py]
+txtsel.decorator = CustomRadioGroup
 
-selectable.text == 
+txtsel.text == 
 Artur a horreur de la marche à pied.
 ==
-selectable.selectable % true
+
+txtsel.selectable % true
 
 before==
 ==
 
 form==
-{{ selectable|component}}
+{{ txtsel|component}}
 ==
-
-evaluator==
-
-rightwords = ['πρεσβύτερος', 'νεώτερος']
-found = 0
-error = 0
-
-for e in selectable.selections:
-    if e['content'] in rightwords:
-        e['css'] = "success-state"
-        found += 1
-    else:
-        e['css'] = "error-state"
-        error += 1
-
-if found == 10 and error == 0:
-    score = 100
-    msg = '<span class="success-state">Bravo, vous avez trouvé tous les adjectifs comparatifs !</span>'
-else:
-    score = max([0, 10*(found - error)])
-    nb_error = (10 - found) + error
-    if nb_error > 1:
-        msg = '<span class="error-state">Il y a {} erreurs...</spam>'.format(nb_error)
-    else:
-        msg = '<span class="error-state">Il y a 1 seule erreur...</spam>'
-
-grade = (score, msg)
-==
-
-extracss==
-<style>
-.exercise__instructions, .exercise__feedback {
-    margin-bottom: 16px;
-}
-</style>
-==
-
-comptest ==
- <script>
-var string = "{L}'{élève} {n}'{a} {pas} {fait} {son} {travail}.";
-var index = -1;
-string=string.replace(/\{([^\{\}]*)\}/g, function(match,p) {
-  index++;
-  return `<span data-index="${index}">${p}</span>`;
-});
-alert(string);
-</script>
-==
-
