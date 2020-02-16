bd0436c877db751f2c396b76ca523b0246737b37
David DOYEN <David.Doyen@u-pem.fr>
2020-02-16 19:49:12 +0100

    ddoyen modified Yggdrasil/utils/env_math.py

--- utils/env_math.py
+++ utils/env_math.py
@@ -1,9 +1,11 @@
 from sympy import *
 from random import random as rd
 from utilsmath import *
+from json import load
 with open('keyboards.JSON') as json_file:  
     keyboards_JSON = json.load(json_file)
-    env = globals().copy()
+
+env = globals().copy()
 
 
 
