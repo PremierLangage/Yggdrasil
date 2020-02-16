c28c8b584370190e46b75fa317357350d069efbb
David DOYEN <David.Doyen@u-pem.fr>
2020-02-16 19:49:16 +0100

    ddoyen modified Yggdrasil/utils/env_math.py

--- utils/env_math.py
+++ utils/env_math.py
@@ -4,8 +4,7 @@ from utilsmath import *
 from json import load
 with open('keyboards.JSON') as json_file:  
     keyboards_JSON = json.load(json_file)
-
-env = globals().copy()
+    env = globals().copy()
 
 
 
