fe8ec9fe2946e2236cd57539aecc753835d16bf9
David DOYEN <David.Doyen@u-pem.fr>
2020-02-17 22:39:41 +0100

    ddoyen modified Yggdrasil/demo/dragdrop_inequalities.pl

--- demo/dragdrop_inequalities.pl
+++ demo/dragdrop_inequalities.pl
@@ -12,9 +12,11 @@ import random as rd
 lt=r"<"
 gt=r">"
 
+n = 4
+
 numbers = []
 sol = []
-for in range(4):
+for _ in range(n):
     [a,b] = rd.sample(range(10,100),2)
     numbers.append([a,b]
     if a < b:
@@ -23,7 +25,7 @@ for in range(4):
         sol.append(gt)
 
 label = Labels([lt,gt])
-drops = Drops(4)
+drops = Drops(n)
 ==
 
 text==
