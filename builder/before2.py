ce2c124a4ae9f97b2ac7e9073b6352dd065ee40a
David DOYEN <David.Doyen@u-pem.fr>
2020-02-17 14:40:56 +0100

    ddoyen modified Yggdrasil/builder/before2.py

--- builder/before2.py
+++ builder/before2.py
@@ -3,7 +3,6 @@
 
 import sys, json, jsonpickle
 from sandboxio import get_context
-from random import random as rd
 from components import Component
 
 try:
@@ -20,13 +19,11 @@ except ImportError:
 class StopBeforeExec(Exception):
     pass
 
-
 def add_try_clause(code, excpt):
     """Add a try/except clause, excepting 'excpt' around code."""
     code = code.replace('\t', '    ')
     return ("try:\n    ...\n" + '\n'.join(["    " + line for line in code.split('\n')])
             + "\nexcept " + excpt.__name__ + ":\n    pass")
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -38,6 +35,7 @@ if __name__ == "__main__":
     
     dic = get_context()
     dic = {**env, **dic}
+    
     if 'before' in dic:
         dic['StopBeforeExec'] = StopBeforeExec
         exec(add_try_clause(dic['before'], StopBeforeExec), dic)
@@ -45,9 +43,6 @@ if __name__ == "__main__":
         for key in env:
             if key in dic and dic[key] == env[key]:
                 del dic[key]
-    else:
-        print(("Player 'before' need a script declared in the key 'before'."),file = sys.stderr)
-        sys.exit(1)
 
     for key in dic:
         dic[key]=serialize(dic[key])
@@ -71,9 +66,3 @@ if __name__ == "__main__":
         f.write(jsonpickle.encode(dic, unpicklable=False))
 
     sys.exit(0)
-
-
-
-
-
-
