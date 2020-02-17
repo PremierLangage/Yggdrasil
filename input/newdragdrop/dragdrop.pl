fa27852332f4eb7f74c20cbf7ee25879c6111c26
David DOYEN <David.Doyen@u-pem.fr>
2020-02-17 16:27:09 +0100

    ddoyen modified Yggdrasil/input/newdragdrop/dragdrop.pl

--- input/newdragdrop/dragdrop.pl
+++ input/newdragdrop/dragdrop.pl
@@ -8,48 +8,6 @@ text ==
 Identifier le verbe conjugué la phrase ci-dessous.
 ==
 
-selectable =: Text
-
-selectable.text == 
-Artur a horreur de la marche à pied.
-==
-selectable.selectable % true
-
-before==
-s="Artur {{a}} horreur de la marche à pied."
-
-import re
-
-def bracket_words(string):
-    lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|\[[^\]]*\]|[^\{\}\[\]]+",string)
-    for i in range(len(lst)):
-        s=lst[i]
-        if s[0]=="[":
-            lst[i]=s[1:-1]
-        elif s[0]!="{":
-            lst[i]=re.sub(r"(\w+)", r"{\1}",s)
-    return "".join(lst)
-    
-def read_text_bracket(string):
-    lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|[^\{\}]+",string)
-    selection=[]
-    k=0
-    for i in range(len(lst)):
-        s=lst[i]
-        if s[0]=="{":
-            if len(s)> 2 and s[1]=="{":
-                selection.append(k)
-                lst[i]=s[1:-1]
-            k+=1
-    
-    return ("".join(lst),selection)
-
-s,_=read_text_bracket(bracket_words(s))
-==
-
-text==
-{{s}}
-==
 
 form==
 <div id = 'label1'
