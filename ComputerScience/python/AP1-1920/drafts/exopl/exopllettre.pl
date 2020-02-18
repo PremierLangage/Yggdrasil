557032c0903504b9ec0e432183efd613f3b33fe5
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-02-18 16:00:35 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/exopl/exopllettre.pl

--- ComputerScience/python/AP1-1920/drafts/exopl/exopllettre.pl
+++ ComputerScience/python/AP1-1920/drafts/exopl/exopllettre.pl
@@ -39,21 +39,24 @@ essais=0
 
 
 evaluator==
+import random
 def reset():
-    exec(before)
+    global title,essais,l
+    title += "*"
+    l=chr(64+random.randint(1,25)) # pas de lettre après z
+    essais=0
 
 essais += 1
 if essais > 3:
     reset()
     grade=(0," trop de tentatives réessayer avec une autre lettre ")
-else:
-    if len(inputbox.value)!= 1 or ord(inputbox.value)>ord('Z') :
+elif len(inputbox.value)!= 1 or ord(inputbox.value)>ord('Z') :
         essais -= 1
         grade=(-1," saisissez une seule lettre Majuscule. ")
-    if ord(l)+1 == ord(inputbox.value):
+elif ord(l)+1 == ord(inputbox.value):
         reset()
         grade = (100, " Bravo ")
-    else:
+else:
         grade=(0," réessayez il vous reste "+str(3-essais) + " essais" )
 
 == 
