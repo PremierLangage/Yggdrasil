4e6923715de910e23ba5a9c78454b8c93b18e8a7
David DOYEN <David.Doyen@u-pem.fr>
2020-02-19 21:36:05 +0100

    ddoyen modified Yggdrasil/utils/checkbox.py

--- utils/checkbox.py
+++ utils/checkbox.py
@@ -34,11 +34,29 @@ class Checkbox(Component):
         rd.sort(self.items)
 
     def eval(self,**kwargs):
-        checkright,checkwrong,missright=0,0,0
+        """
+        Evaluate the answer stored in the component.
+        """
+        right,wrong,missed=0,0,0
+
         for item in self.items:
             if item['id'] in self._sol and item['checked']:
-                item['css'] = 'success-state'
-                item['content'] = self.content[item['id']] + r"<span class='fas fa-check' style='padding-left: 1em'></span>"
+                right+=1
+                if display:
+                    item['css'] = 'success-state'
+                    item['content'] += r"<span class='fas fa-check' style='padding-left: 1em'></span>"
+            elif not (item['id'] in self._sol) and item['checked']:
+                wrong+=1
+                if display:
+                    item['css'] = 'error-state'
+                    item['content'] += r"<span class='fas fa-times' style='padding-left: 1em'></span>"
+            elif item['id'] in self._sol and not item['checked']:
+                idmissed.append(item['id'])
+                missed+=1
+
+        for id in idright:
+            self.items[id]['css'] = 'success-state'
+            ['content'] = self.content[item['id']] + r"<span class='fas fa-check' style='padding-left: 1em'></span>"
                 checkright+=1
             elif item['id'] in self._sol and not item['checked']:
                 item['content'] = self.content[item['id']] + r"<span class='fas fa-check' style='padding-left: 1em'></span>"
@@ -50,21 +68,21 @@ class Checkbox(Component):
         
         grading = kwargs.get('check', "RightMinusWrong")
         if grading=="AllOrNothing":
-            if checkwrong==0 and missright==0:
+            if wrong==0 and right==0:
                 score=100
             else:
                 score=0
         elif grading=="RightMinusWrong":
-            if checkright+missright==0:
-                if checkwrong==0:
+            if right+missed==0:
+                if wrong==0:
                     score=100
                 else:
                     score=0
             else:
-                score=max([round((checkright-checkwrong)/(checkright+missright)*100),0])
+                score=max([round((right-wrong)/(right+right)*100),0])
         elif grading=="CorrectAnswers":
             nitems=len(self.items)
-            score=max([round((nitems-2*(checkwrong+missright))/nitems*100),0])
+            score=max([round((nitems-2*(wrong+right))/nitems*100),0])
         self.disabled=True
         return (score,"")
 
