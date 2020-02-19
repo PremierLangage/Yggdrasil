85eda231e1b2d63840ba19798ee400f6a264e133
David DOYEN <David.Doyen@u-pem.fr>
2020-02-19 14:08:03 +0100

    ddoyen modified Yggdrasil/utils/checkbox.py

--- utils/checkbox.py
+++ utils/checkbox.py
@@ -19,14 +19,14 @@ class Checkbox(Component):
     def set_sol_by_content(self,content):
         self._sol=[item['id'] for item in self.items if item['content'] in content]
 
-    def load_choices(self, content):
+    def loaditems(self, content):
         items = []
         for e in content:
             id = str(uuid.uuid4())
             self.content[id] = e
             self.items.append({"id": id,"content": e})
 
-    def load_right_wrong(self, right, wrong, nchoices, nright):
+    def loadrightwrong(self, right, wrong, nchoices, nright):
         self.load_choices(rd.sample(right,nright)+rd.sample(wrong,nchoices-nright))
         self.set_sol_by_index(list(range(nright)))
         self.shuffle()
@@ -37,7 +37,7 @@ class Checkbox(Component):
     def sort(self):
         rd.sort(self.items)
 
-    def eval(self):
+    def eval(self,**kwargs):
         checkright,checkwrong,missright=0,0,0
         for item in self.items:
             if item['id'] in self._sol and item['checked']:
@@ -52,12 +52,12 @@ class Checkbox(Component):
                 item['content'] = self.content[item['id']] + r"<span class='fas fa-times' style='padding-left: 1em'></span>"
                 checkwrong+=1
 
-        if self.grading=="AllOrNothing":
+        if grading=="AllOrNothing":
             if checkwrong==0 and missright==0:
                 score=100
             else:
                 score=0
-        elif self.grading=="RightMinusWrong":
+        elif grading=="RightMinusWrong":
             if checkright+missright==0:
                 if checkwrong==0:
                     score=100
@@ -65,10 +65,11 @@ class Checkbox(Component):
                     score=0
             else:
                 score=max([round((checkright-checkwrong)/(checkright+missright)*100),0])
-        elif self.grading=="CorrectAnswers":
+        elif grading=="CorrectAnswers":
             nitems=len(self.items)
             score=max([round((nitems-2*(checkwrong+missright))/nitems*100),0])
         self.disabled=True
         return (score,"")
+
 
 
