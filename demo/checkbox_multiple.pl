a4c3d9222b3864d9a1d2d82e5871b998cb7429b5
David DOYEN <David.Doyen@u-pem.fr>
2020-02-19 14:41:02 +0100

    ddoyen modified Yggdrasil/demo/checkbox_multiple.pl

--- demo/checkbox_multiple.pl
+++ demo/checkbox_multiple.pl
@@ -6,7 +6,7 @@ before ==
 import random as rd
 mult3 = [str(n) for n in range(50,100) if n % 3 == 0]
 other = [str(n) for n in range(50,100) if n % 3 != 0]
-checkbox.loadrightwrong(mult3,other,5,rd.randint(1,4))
+checkbox.loadrw(mult3,other,5,rd.randint(1,4))
 ==
 
 text ==
