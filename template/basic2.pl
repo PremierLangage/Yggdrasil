e621fafdfb77a3744944506d3c4c4bcbe4be39a4
David DOYEN <David.Doyen@u-pem.fr>
2020-02-20 16:23:48 +0100

    ddoyen modified Yggdrasil/template/basic2.pl

--- template/basic2.pl
+++ template/basic2.pl
@@ -37,7 +37,7 @@ extrajs==
 
         const buttons = actions.find('.btn-group');
         buttons.append(`
-<nav class="navbar navbar-expand-sm bg-light">
+<nav class="navbar navbar-expand-sm bg-light" style"width:100%">
   <ul class="navbar-nav">
     <li class="nav-item">
       <a class="nav-link" href="#">Link 1</a>
