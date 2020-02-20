09e8c282e0266cc5e2bb81a4497f60746e57c66d
David DOYEN <David.Doyen@u-pem.fr>
2020-02-20 13:15:33 +0100

    ddoyen modified Yggdrasil/model/basic2.pl

--- model/basic2.pl
+++ model/basic2.pl
@@ -22,6 +22,21 @@ extrajs==
 }
 </style>
 <script> 
+document.body.onload = addElement;
+
+function addElement () {
+  // crée un nouvel élément div
+  var newDiv = document.createElement("div");
+  // et lui donne un peu de contenu
+  var newContent = document.createTextNode('Hi there and greetings!');
+  // ajoute le nœud texte au nouveau div créé
+  newDiv.appendChild(newContent);
+  
+  // ajoute le nouvel élément créé et son contenu dans le DOM
+  var currentDiv = document.getElementById('div1');
+  document.body.insertBefore(newDiv, currentDiv);
+}
+
     function onReadyPL(nodes) {
         const actions = nodes.actions;
         actions.find('.action-save').hide();
