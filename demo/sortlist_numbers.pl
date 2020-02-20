cea7c94c80667d0413de9dd9be10e96803b12061
David DOYEN <David.Doyen@u-pem.fr>
2020-02-20 15:33:08 +0100

    ddoyen modified Yggdrasil/demo/sortlist_numbers.pl

--- demo/sortlist_numbers.pl
+++ demo/sortlist_numbers.pl
@@ -4,9 +4,9 @@ title = Ordonner des nombres
 
 before ==
 import random as rd
-numbers=rd.sample(list(range(1,100)),5)
+numbers = rd.sample(list(range(1,100)),5)
 numbers.sort()
-sortlist.load_items([str(n) for n in numbers])
+sortlist.loadlist([str(n) for n in numbers])
 ==
 
 text ==
@@ -15,6 +15,7 @@ Ordonner les nombres suivants du plus petit au plus grand.
 
 
 settings.feedback.class = score
+
 
 
 
