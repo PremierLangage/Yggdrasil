5952fc8be2b83eaae398aba549d3d9f3a9e17b54
David DOYEN <David.Doyen@u-pem.fr>
2020-02-20 16:10:13 +0100

    ddoyen modified Yggdrasil/demo/basic/checkbox_multiples.pl

---
+++ demo/basic/checkbox_multiples.pl
@@ -0,0 +1,30 @@
+extends = /model/basic.pl
+
+title = Multiples de 3
+
+before ==
+import random as rd
+
+mult3 = [str(n) for n in range(50,100) if n % 3 == 0]
+other = [str(n) for n in range(50,100) if n % 3 != 0]
+
+checkbox.loadrw(mult3,other,5,rd.randint(1,4))
+==
+
+text ==
+Parmi les nombres suivants, lesquels sont des multiples de 3 ?
+==
+
+checkbox =: CheckboxGroup
+@ /utils/components/checkbox.py [customcheckbox.py]
+checkbox.decorator = CustomCheckbox
+
+settings.feedback = lightscore
+
+form ==
+{{ checkbox | component }}
+==
+
+evaluator ==
+grade = checkbox.eval(grading="AllOrNothing")
+==
