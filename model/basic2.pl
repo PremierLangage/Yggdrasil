fd101db4bab2e495a95ef957c95fdca8b69b7a8d
David DOYEN <David.Doyen@u-pem.fr>
2020-02-20 13:26:16 +0100

    ddoyen modified Yggdrasil/model/basic2.pl

--- model/basic2.pl
+++ model/basic2.pl
@@ -41,7 +41,7 @@ extrajs==
         `);
         var elemDiv = document.createElement('div');
         elemDiv.innerHTML = 'coucou';
-        nodes.append("coucou"); 
+        nodes.document.append("coucou"); 
   
 }
 </script>
