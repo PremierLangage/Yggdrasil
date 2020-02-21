8f1f5658eccb22f62b8eefa2e487d6fd1c32b963
David DOYEN <David.Doyen@u-pem.fr>
2020-02-21 13:00:15 +0100

    ddoyen modified Yggdrasil/template/basic2.pl

--- template/basic2.pl
+++ template/basic2.pl
@@ -18,13 +18,20 @@ evaluator ==
 grade=(100,"")
 ==
 form ==
-<nav class="navbar navbar-light bg-light">
-<div class="container">
-<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Valider</button>
-  <span class="navbar-text">
-    Score : -
-  </span>
-</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" href="#">Features</a>
+      <a class="nav-item nav-link" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    </div>
+  </div>
+</nav>
 
   <a class="navbar-brand">Navbar</a>
 
