857e050efb885d7b42442022ee3593eaf95c073d
David DOYEN <David.Doyen@u-pem.fr>
2020-02-21 21:51:57 +0100

    ddoyen modified Yggdrasil/utils/components/radio.py

--- utils/components/radio.py
+++ utils/components/radio.py
@@ -27,7 +27,7 @@ class CustomRadio(Component):
         rd.shuffle(self.items)
 
     def sort(self):
-        self.items.sort(lambda item : item['content'])
+        self.items.sort(key=lambda item : item['content'])
 
     def eval(self, **kwargs):
         """
