84a77ba3d300784897bb8f3f06fac3cc4a204cf7
David DOYEN <David.Doyen@u-pem.fr>
2020-02-21 13:02:26 +0100

    ddoyen modified Yggdrasil/template/basic2.pl

--- template/basic2.pl
+++ template/basic2.pl
@@ -19,8 +19,8 @@ grade=(100,"")
 ==
 form ==
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
