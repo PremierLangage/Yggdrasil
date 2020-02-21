1d7c4e8945eafa7133967a4a2af6599abb085624
David DOYEN <David.Doyen@u-pem.fr>
2020-02-21 13:36:16 +0100

    ddoyen modified Yggdrasil/template/basic2.pl

--- template/basic2.pl
+++ template/basic2.pl
@@ -30,9 +30,8 @@ form ==
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav ml-auto">
       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <a class="nav-item nav-link" href="#">Nouveau tirage</a>
+      <a class="nav-item nav-link" href="#">A propos</a>
     </div>
   </div>
 </nav>
