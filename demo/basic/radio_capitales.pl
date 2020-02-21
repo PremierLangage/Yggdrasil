0532890b344c56b11e9b45b3902a11bb765f7dfc
David DOYEN <David.Doyen@u-pem.fr>
2020-02-21 21:00:46 +0100

    ddoyen modified Yggdrasil/demo/basic/radio_capitales.pl

--- demo/basic/radio_capitales.pl
+++ demo/basic/radio_capitales.pl
@@ -17,7 +17,7 @@ pays = sample_rows[0]['pays']
 article = sample_rows[0]['article']
 
 radio.loaditems([row['capitale'] for row in sample_rows])
-radio.setsol_index(0)
+radio.setsol_index(sample_rows[0]['capitale'])
 radio.shuffle()
 
 partitif = {"le":"du ", "la":"de la ", "les":"des ", "l":"de l'"}
