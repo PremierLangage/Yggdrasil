5ce2afd5137df011f310622cc261ff23eaa965ea
David DOYEN <David.Doyen@u-pem.fr>
2020-02-22 20:26:37 +0100

    ddoyen modified Yggdrasil/utils/components/radio.py

--- utils/components/radio.py
+++ utils/components/radio.py
@@ -7,18 +7,13 @@ class CustomRadio(Component):
     def __init__(self, **kwargs):
         self.selector = 'c-radio-group'
         self.decorator = 'CustomRadio'
-        self.items = []
-        self._content = {}
         super().__init__(**kwargs)
 
     def loaditems(self, lstcontent):
         """
         Load items in the component.
         """
-        for content in lstcontent:
-            id = str(uuid4())
-            self._content[id] = content
-            self.items.append({"id": id ,"content": content})
+        self.items = [{"id": uuid4(), "content": content} for content in lstcontent]
 
     def setsol_index(self, index):
         """
@@ -30,7 +25,7 @@ class CustomRadio(Component):
         """
         Set the component solution from a content.
         """
-        self._sol=next(item['id'] for item in self.items if item['content']==content)
+        self._sol=next(item['id'] for item in self.items if item['content'] == content)
 
     def shuffle(self):
         """
@@ -57,15 +52,15 @@ class CustomRadio(Component):
                 score = 100
                 if display:
                     item['css'] = 'success-state'
-                    item['content'] = r"%s <span class='text-success fas fa-check' style='padding-left: 1em'></span>" % self._content[id]
+                    item['content'] += r"<span class='text-success fas fa-check' style='padding-left: 1em'></span>"
             elif id != self._sol and id == self.selection:
                 if display:
                     item['css'] = 'error-state'
-                    item['content'] = r"%s <span class='text-danger fas fa-times' style='padding-left: 1em'></span>" % self._content[id]
+                    item['content'] += r"<span class='text-danger fas fa-times' style='padding-left: 1em'></span>"
             elif id == self._sol and id != self.selection:
                 score = 0
                 if display:
-                    item['content'] = r"%s <span class='text-success fas fa-check' style='padding-left: 1em'></span>" % self._content[id]
+                    item['content'] += r"%s <span class='text-success fas fa-check' style='padding-left: 1em'></span>"
 
         self.disabled = True
 
