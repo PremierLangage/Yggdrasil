6af805d30d0a1e5868f5cb44d3bc66ae917f2871
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-02-23 20:46:39 +0100

    nborie modified Yggdrasil/ComputerScience/Games/CYOA/CYOA_build.py

---
+++ ComputerScience/Games/CYOA/CYOA_build.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# ****************************************************************************
+#    Copyright (C) 2020 Nicolas Borie <nicolas.borie at univ-eiffel dot fr>
+#
+#           Distributed under the terms of the CC-BY-SA 4.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-BY-SA 4.0 is available at:
+#
+#          https://creativecommons.org/licenses/by-sa/4.0/legalcode
+# ****************************************************************************
+
+# CYOA : Choose Your Own Adventure (Gamebook)
+
+# This builder mainly parse the python game_data_file...
+
