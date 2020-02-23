cd3b6b7799d17b72fa399147f27eb3b6b5e6a358
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-02-23 23:49:20 +0100

    nborie modified Yggdrasil/ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.pl

---
+++ ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.pl
@@ -0,0 +1,24 @@
+# ****************************************************************************
+#    Copyright (C) 2020 Nicolas Borie <nicolas.borie at univ-eiffel dot fr>
+#
+#          Distributed under the terms of the CC-BY-SA 4.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    The full text of the CC-BY-SA 4.0 is available at:
+#
+#          https://creativecommons.org/licenses/by-sa/4.0/legalcode
+# ****************************************************************************
+
+author=Nicolas Borie
+
+title=Données binaires dans nos ordinateurs modernes
+tag=system|binary
+
+template=/ComputerScience/Games/CYOA/CYOA_template.pl
+
+@ Ace_Debugger_1.py
+game_data_file = Ace_Debugger_1.py
+
