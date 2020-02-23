37ee8a16b776af5a03a07eccca616034bb032550
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-02-23 20:22:53 +0100

    nborie modified Yggdrasil/ComputerScience/Games/CYOA/CYOA_template.pl

---
+++ ComputerScience/Games/CYOA/CYOA_template.pl
@@ -0,0 +1,13 @@
+# ****************************************************************************
+#    Copyright (C) 2020 Nicolas Borie <nicolas.borie at univ-eiffel dot fr>
+#
+#              Distributed under the terms of the CC-BY-SA 4.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-BY-SA 4.0 is available at:
+#
+#          https://creativecommons.org/licenses/by-sa/4.0/legalcode
+# ****************************************************************************
