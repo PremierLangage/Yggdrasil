3dba87b98a5f63f1b9e6d29b5aa114fe5999e9a4
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-02-23 20:47:09 +0100

    nborie modified Yggdrasil/ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.py

--- ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.py
+++ ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.py
@@ -1,3 +1,17 @@
+# ****************************************************************************
+#    Copyright (C) 2020 Nicolas Borie <nicolas.borie at univ-eiffel dot fr>
+#
+#          Distributed under the terms of the CC-BY-SA 4.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-BY-SA 4.0 is available at:
+#
+#          https://creativecommons.org/licenses/by-sa/4.0/legalcode
+# ****************************************************************************
+
 {
 
 # L'étape 0 n'existera jamais, le template commencera de manière harcodé à l'étape '1'    
