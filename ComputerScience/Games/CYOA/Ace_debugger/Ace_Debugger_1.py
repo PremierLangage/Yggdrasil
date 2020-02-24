04f48f96f6e32407f75abf93e353e6b42e34de06
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-02-24 19:51:46 +0100

    nborie modified Yggdrasil/ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.py

--- ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.py
+++ ComputerScience/Games/CYOA/Ace_debugger/Ace_Debugger_1.py
@@ -54,17 +54,17 @@
     [
         '',
 
-        "Vous entrez dans la salle de TP et vous voyez déjà une"
-        "vingtaine d'étudiants assis derrière les postes sur les deux"
-        "rangées de bureau. Certains jouent encore sur leurs téléphones"
-        "mais d'autres sont déjå au travail avec l'énoncé en pdf ouvert"
-        "sur leur poste."
+        "Vous entrez dans la salle de TP et vous voyez déjà une "
+        "vingtaine d'étudiants assis derrière les postes sur les deux "
+        "rangées de bureau. Certains jouent encore sur leurs téléphones "
+        "mais d'autres sont déjå au travail avec l'énoncé en pdf ouvert "
+        "sur leur poste. "
 
-        "A peine entré, vous croisez le regard d'un élève qui lève"
-        "instantannément le bras en l'air. C'est Yves Vatrovytch qui"
-        "fait mine de vous appeler. Avec ces fameuses questions qui"
-        "sont toujours toutes plus tordues les unes que les autres,"
-        "quelle colle va-t-il bien pour poser ce coup-ci ?",
+        "A peine entré, vous croisez le regard d'un élève qui lève "
+        "instantannément le bras en l'air. C'est Yves Vatrovytch qui "
+        "fait mine de vous appeler. Avec ces fameuses questions qui "
+        "sont toujours toutes plus tordues les unes que les autres, "
+        "quelle colle va-t-il bien pour poser ce coup-ci ? ",
 
         [
             ['3', "Vous vous approchez de l'élève"]
