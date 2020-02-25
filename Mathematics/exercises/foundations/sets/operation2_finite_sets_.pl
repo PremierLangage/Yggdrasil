900401dd3a6e22eff4eba39f2fbba53daa5c3d85
David DOYEN <David.Doyen@u-pem.fr>
2020-02-25 11:50:02 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl

--- Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl
+++ Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl
@@ -1,9 +1,9 @@
-extends = /Mathematics/template/mathbasic.pl
+extends = /model/math.pl
 
-input1 =: MathInput
+input =: MathInput
 
 form==
-{{input1|component}}
+{{input|component}}
 ==
 
 
@@ -79,6 +79,7 @@ score,_,feedback=ans_struct_expr(input1.value,sol,"set")
 solution ==
 La solution est $! { {{sol_tex}} } !$.
 ==
+
 
 
 
