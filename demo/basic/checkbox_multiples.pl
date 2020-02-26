6770cdb69e37cae7e652c8960984b17091d4d687
David DOYEN <David.Doyen@u-pem.fr>
2020-02-26 20:06:26 +0100

    ddoyen modified Yggdrasil/demo/basic/checkbox_multiples.pl

--- demo/basic/checkbox_multiples.pl
+++ demo/basic/checkbox_multiples.pl
@@ -41,7 +41,7 @@ for item in checkbox.items:
 
 extracss ==
 <style>
-.mat-checkbox-label::after {
+span.mat-checkbox-label::after {
     font-family: "Font Awesome 5 Free";
     content: "\f00c";
     vertical-align: middle;
