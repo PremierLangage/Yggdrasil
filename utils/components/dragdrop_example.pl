5143ffb0765efd8fd1fe38571a2b10a56e6f6f49
etienne sandier <sandier@u-pec.fr>
2020-02-26 22:58:14 +0100

    esandier modified Yggdrasil/utils/components/dragdrop_example.pl

---
+++ utils/components/dragdrop_example.pl
@@ -0,0 +1,60 @@
+extends = /model/basic.pl
+
+@ /utils/components/dragdrop.py [customdragdrop.py]
+
+title = Comparaison de nombres (DragDrop)
+
+before==|python|
+from customdragdrop import CustomDragDrop, DragDropGroup
+import random as rd
+
+lt="&lt;"
+gt="&gt;"
+
+n = 4
+
+numbers = []
+sol = []
+for _ in range(n):
+    [a,b] = rd.sample(range(10,100),2)
+    numbers.append([a,b])
+    if a < b:
+        sol.append(lt)
+    else:
+        sol.append(gt)
+
+label = CustomDragDrop.Labels([lt,gt])
+drop = CustomDragDrop.Drops(n)
+group = DragDropGroup(labels = label, drop_zones = drop)
+==
+
+text==
+Comparer les nombres suivants avec les symboles {{ label[0] | component }} et {{ label[1] | component }}.
+==
+
+form==
+<ul>
+{% for i in range(4) %}
+<li> {{ numbers[i][0] }} {{ drop[i]|component }} {{ numbers[i][1] }} </li>
+{% endfor %}
+</ul>
+==
+
+evaluator==
+from customdragdrop import CustomDragDrop, DragDropGroup
+
+#grade=CustomDragDrop.eval(drop,sol)
+
+==
+
+extracss == #|html| 
+<style>
+    .myclass{
+        #padding: 1em;
+        min-width: 4em;
+        }
+</style>
+==
+
+
+
