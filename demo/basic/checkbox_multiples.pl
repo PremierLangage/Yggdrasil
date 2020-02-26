6a71013281a7dfaa95824609fd8e447944974e35
David DOYEN <David.Doyen@u-pem.fr>
2020-02-26 10:22:43 +0100

    ddoyen modified Yggdrasil/demo/basic/checkbox_multiples.pl

--- demo/basic/checkbox_multiples.pl
+++ demo/basic/checkbox_multiples.pl
@@ -20,7 +20,7 @@ other = [str(n) for n in range(50,100) if n % 3 != 0]
 checkbox.loadrw(mult3,other,5,rd.randint(1,4))
 
 for item in checkbox.items:
-    item['css']='myclass'
+    item['css']=''
 ==
 
 text ==
