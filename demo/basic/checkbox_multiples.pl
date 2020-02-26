663e11c4775f1dc6c0b147d0acb5a964d57f786b
David DOYEN <David.Doyen@u-pem.fr>
2020-02-26 10:16:58 +0100

    ddoyen modified Yggdrasil/demo/basic/checkbox_multiples.pl

--- demo/basic/checkbox_multiples.pl
+++ demo/basic/checkbox_multiples.pl
@@ -4,23 +4,24 @@ title = Multiples de 3 (Checkbox)
 
 before ==
 import random as rd
-from customcheckbox import CustomCheckbox
 
-checkbox = CustomCheckbox(content = ["a","b"])
+# Création du composant avec le constructeur
+# from customcheckbox import CustomCheckbox
+# checkbox = CustomCheckbox(content = ["a","b"])
 
 mult3 = [str(n) for n in range(50,100) if n % 3 == 0]
 other = [str(n) for n in range(50,100) if n % 3 != 0]
 
-#checkbox.loadrw(mult3,other,5,rd.randint(1,4))
+checkbox.loadrw(mult3,other,5,rd.randint(1,4))
 ==
 
 text ==
 Parmi les nombres suivants, lesquels sont des multiples de 3 ?
 ==
 
-# checkbox =: CheckboxGroup
+checkbox =: CheckboxGroup
 @ /utils/components/checkbox.py [customcheckbox.py]
-#checkbox.decorator = CustomCheckbox
+checkbox.decorator = CustomCheckbox
 
 settings.feedback = lightscore
 
