f6a05547b9000091ec2ff613dfa81201bd42b7cf
David DOYEN <David.Doyen@u-pem.fr>
2020-02-27 14:13:38 +0100

    ddoyen modified Yggdrasil/model/basic.pl

--- model/basic.pl
+++ model/basic.pl
@@ -4,7 +4,7 @@
 
 extracss ==
 <style>
-.success-state p::after {
+.icon-check p::after {
     font-family: "Font Awesome 5 Free";
     color: #155724;
     content: "  \f00c";
@@ -12,21 +12,12 @@ extracss ==
     font-weight: 900;
 }
 
-.error-state p::after {
+.icon-fail p::after {
     font-family: "Font Awesome 5 Free";
     color: #721c24;
     content: "  \f00d";
     vertical-align: middle;
     font-weight: 900;
 }
-
-.missed-state p::after {
-    font-family: "Font Awesome 5 Free";
-    color: #155724;
-    content: "  \f00c";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
 </style>
 ==
