bf936db56cfd2157e605fdf2b661fa88a9c15e37
David DOYEN <David.Doyen@u-pem.fr>
2020-02-27 14:11:39 +0100

    ddoyen modified Yggdrasil/demo/basic/checkbox_multiples.pl

--- demo/basic/checkbox_multiples.pl
+++ demo/basic/checkbox_multiples.pl
@@ -35,36 +35,4 @@ form ==
 
 evaluator ==
 grade = checkbox.eval(grading="CorrectAnswers",display=True,disabled=False)
-
 ==
-
-extracss ==
-<style>
-.success-state p::after {
-    font-family: "Font Awesome 5 Free";
-    color: #155724;
-    content: "  \f00c";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-.error-state p::after {
-    font-family: "Font Awesome 5 Free";
-    color: #721c24;
-    content: "  \f00d";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-.missed-state p::after {
-    font-family: "Font Awesome 5 Free";
-    color: #155724;
-    content: "  \f00c";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-</style>
-==
-
-
