70c3ff1fd0131b57b9302e91f52f8de45de5706d
David DOYEN <David.Doyen@u-pem.fr>
2020-02-27 14:11:24 +0100

    ddoyen modified Yggdrasil/model/basic.pl

--- model/basic.pl
+++ model/basic.pl
@@ -2,3 +2,31 @@
 @ /builder/before2.py [builder.py]
 @ /grader/evaluator2.py [grader.py]
 
+extracss ==
+<style>
+.success-state p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    content: "  \f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.error-state p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #721c24;
+    content: "  \f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.missed-state p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    content: "  \f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+</style>
+==
