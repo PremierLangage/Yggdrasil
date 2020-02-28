7c8b56bc693f725c0da4622c4e61296b22a7027e
David DOYEN <David.Doyen@u-pem.fr>
2020-02-28 10:00:23 +0100

    ddoyen modified Yggdrasil/demo/math/complex_mul.pl

---
+++ demo/math/complex_mul.pl
@@ -0,0 +1,28 @@
+extends = /model/math.pl
+
+title = Opération sur les nombres complexes
+
+before ==
+keyboards_JSON['virtualKeyboards']="complexes"
+input.config = keyboards_JSON
+
+z1=rand_complex_int(5)
+z2=rand_complex_int(5)
+sol(z1*z2).expand()
+==
+
+input =: MathInput
+
+text ==
+On considère les nombres complexes $! z_1 = {{z1.latex}} !$ et $! z_2= {{z2.latex}} !$. Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
+==
+
+
+form ==
+{{ input | component }}
+==
+
+evaluator ==
+score,_,feedback = eval_complex(input.value,sol,form="cartesian")
+==
+
