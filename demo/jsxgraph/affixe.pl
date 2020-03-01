f75e52b40d9e8e7a37e8014e122f2000f5e74e7f
David DOYEN <David.Doyen@u-pem.fr>
2020-03-01 16:00:22 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/affixe.pl

--- demo/jsxgraph/affixe.pl
+++ demo/jsxgraph/affixe.pl
@@ -25,7 +25,7 @@ text ==
 ==
 
 
-drawer.script ==
+jxg.script ==
 board.create('grid',[],{gridX:1,gridY:1});
 board.create('axis',[[0,0],[1,0]],{name:'Re',withLabel:true,label:{position:'urt',offset:[-5,10]},ticks:{visible: false}});
 board.create('axis',[[0,0],[0,1]],{name:'Im',withLabel:true,label:{position:'urt',offset:[10,0]},ticks:{visible: false}});
