c3477ae4135ced852eeb4f783efdc95f64926073
David DOYEN <David.Doyen@u-pem.fr>
2020-03-01 15:44:49 +0100

    ddoyen modified Yggdrasil/utils/components/jsxgraph.py

--- utils/components/jsxgraph.py
+++ utils/components/jsxgraph.py
@@ -1,5 +1,3 @@
-import random as rd
-from uuid import uuid4
 from components import Component
 from jinja2 import Template
 
@@ -10,58 +8,9 @@ class CustomJSXGraph(Component):
         self.decorator = 'CustomJSXGraph'
         super().__init__(**kwargs)
 
-    def loadscript(self, lstcontent):
+    def loadscript(self, script, dic=globals()):
         """
         Load script and render it.
         """
-        self.items = [{"id": uuid4(), "content": content} for content in lstcontent]
-
-    def setsol_index(self, index):
-        """
-        Set the component solution from an index.
-        """
-        self._sol = self.items[index]['id']
-
-    def setsol_content(self, content):
-        """
-        Set the component solution from a content.
-        """
-        self._sol = next(item['id'] for item in self.items if item['content'] == content)
-
-    def shuffle(self):
-        """
-        Shuffle the component items.
-        """
-        rd.shuffle(self.items)
-
-    def sort(self):
-        """
-        Sort the component items.
-        """
-        self.items.sort(key = lambda item : item['content'])
-
-    def eval(self, display=True, disabled=True):
-        """
-        Evaluate the answer stored in the component.
-        """
-        for item in self.items:
-            id = item['id']
-            if id == self._sol and id == self.selection:
-                score = 100
-                if display:
-                    item['css'] = 'success-state icon-check-after'
-            elif id != self._sol and id == self.selection:
-                if display:
-                    item['css'] = 'error-state icon-times-after'
-            elif id == self._sol and id != self.selection:
-                score = 0
-                if display:
-                    item['css'] = 'icon-check-after'
-        if disabled:
-            self.disabled = True
-
-        return (score, "")
-
-
-
+        self.script = Template(script).render(dic)
 
