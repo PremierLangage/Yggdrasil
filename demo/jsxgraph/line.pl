577c87dd0677d4ca291784e3060d8cf7975d2d6e
David DOYEN <David.Doyen@u-pem.fr>
2020-03-03 21:56:06 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/line.pl

--- demo/jsxgraph/line.pl
+++ demo/jsxgraph/line.pl
@@ -44,8 +44,8 @@ xB = jxg.points['B']['x']
 yB = jxg.points['B']['y']
 
 
-
-if equal_approx(atan2(yA-yB,xA-xB), atan2(a,1), 0.1) and sqrt(:
+ansb = yB-(yA-yB)/(xA-xB)*xB
+if equal_approx(atan2(yA-yB,xA-xB), atan2(a,1), 0.1) and abs(ansb-b)<0.1:
     score = 100
 else:
     score = 0
