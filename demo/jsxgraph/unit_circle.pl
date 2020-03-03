485a255fdc0438b178f7fb35944d53ba10d4fd85
David DOYEN <David.Doyen@u-pem.fr>
2020-03-03 15:02:50 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/unit_circle.pl

--- demo/jsxgraph/unit_circle.pl
+++ demo/jsxgraph/unit_circle.pl
@@ -15,6 +15,14 @@ angle=randitem([pi/4,pi/2,3*pi/4,pi,5*pi/4,3*pi/2,7*pi/4])
 jxg.loadscript(script_init)
 ==
 
+script_init ==
+var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
+var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
+var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
+var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'blue',fixed:false});
+var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+==
+
 text ==
 Placer le point M de sorte que l'angle $% (\overrightarrow{OA},\overrightarrow{OM}) %$ ait une mesure égale à $% \displaystyle {{angle.latex}} %$. 
 ==
@@ -24,21 +32,13 @@ form ==
 ==
 
 
-script_init ==
-var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
-var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
-var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'blue',fixed:false});
-var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+script_right ==
+var M = board.create('glider',[{{x}},{{y}},circle],{size:2,name:'M',color:'green',fixed:false});
 ==
 
 script_wrong ==
 var M = board.create('glider',[{{x}},{{y}},circle],{size:2,name:'HHHH',color:'red',fixed:false});
 var Msol = board.create('glider',[{{xsol}},{{ysol}},circle],{size:2,name:'M',color:'green',fixed:false});
-==
-
-script_right ==
-var M = board.create('glider',[{{x}},{{y}},circle],{size:2,name:'M',color:'green',fixed:false});
 ==
 
 evaluator ==
