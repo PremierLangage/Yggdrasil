83748e8d17535065241da646a5b7c883396dbc85
David DOYEN <David.Doyen@u-pem.fr>
2020-03-03 14:52:25 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/unit_circle.pl

--- demo/jsxgraph/unit_circle.pl
+++ demo/jsxgraph/unit_circle.pl
@@ -33,7 +33,7 @@ var secOAM = board.create('sector',[O,A,M],{color:'orange'});
 ==
 
 script_wrong ==
-M.setName(""):
+M.setName(" "):
 var Msol = board.create('glider',[{{xsol}},{{ysol}},circle],{size:2,name:'M',color:'green',fixed:false});
 ==
 
