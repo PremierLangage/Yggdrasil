637d7e606b105606b6a94685ad444e50d8ef0061
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-03-04 12:39:12 +0100

    drevuz modified Yggdrasil/exemples/checkboxgroupcsv/readcsv.py

---
+++ exemples/checkboxgroupcsv/readcsv.py
@@ -0,0 +1,17 @@
+
+
+
+def getrandomselection(filename="content.csv", number,sourcecol="source", targetcol="target"):
+
+    data = getrandomlines(filename,number,sourcecol, targetcol)
+    l=[]
+    sol=[]
+    for i,d in enumerate(data):
+        l.append({
+            "id": str(i),
+            "content": d[0]
+        })
+        sol.append(d[1])
+    return l,sol 
+
+
