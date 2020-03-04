1f6d655c5b5306d4a2d3d9881ae118de7092cba0
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-03-04 12:36:35 +0100

    drevuz modified Yggdrasil/exemples/matchlistcsv/matchlisttable.pl

--- exemples/matchlistcsv/matchlisttable.pl
+++ exemples/matchlistcsv/matchlisttable.pl
@@ -8,12 +8,12 @@ extends = matchlistcsv.pl
 
 ## Remplacement du fichier csv par une table dans le fichier 
 
-table %  { "un" :"one","deux": "two","trois" : "three","quatre":"four",}
+table %  [["un","one"], ["deux","two"],["trois" , "three"],["quatre","four"]]
 
 
 before==
 import random
-from mkl import fromcsv,selectionfromcsv
+from mkl import fromcsv,selectionfromtable
 
 match.nodes , expected =  selectionfromtable(table)
 random.shuffle(match.nodes)
