387e766edd0b9560e249c51ef035d61107d5d4da
etienne sandier <sandier@u-pec.fr>
2020-03-06 14:05:47 +0100

    esandier modified Yggdrasil/utils/components/dragdrop_example.pl

--- utils/components/dragdrop_example.pl
+++ utils/components/dragdrop_example.pl
@@ -56,17 +56,6 @@ for i in range(len(numbers)):
 grade = groupp.eval(grading_function = right_minus_wrong)
 
 
-# l'essai grade = group.eval() ne fonctionne pas, je ne sais pas pourquoi
-# [DD] : Problème de sérialisation/désérialisation de l'objet group, non ?
-# Les objets qui ne font pas partie des types de base Python (int, float, str, list, tuple, dic) 
-# sont transformés en dictionnaires vides au moment de la conversion en JSON.
-# Il faut donc faire la sérialisation/désrialisation "à la main" comme avec SymPy.
-# Ou y a peut-être moyen d'inclure des méthodes internes de sérialisation/désérialisation dans la classe
-# qui seraient automatiquement appelées par jsonpickle (vu que c'est une classe sur laquelle on a la main).
-# [ES] : Oui en effet ça doit être ça, mais alors toute l'idée d'encapsulation 
-# des composants dans des classes qui s'occupent de l'évaluation
-# est un peu remise en cause, à moins comme tu dis que les classes s'occupent du pb serialisation/déserialisation
-# Ce que je comprends pas c'est que dans l'exemple checkbox_multiples.pl ça fonctionne.
 ==
 
 extracss == #|html| 
@@ -77,6 +66,7 @@ extracss == #|html|
         }
 </style>
 ==
+
 
 
 
