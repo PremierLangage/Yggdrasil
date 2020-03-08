66d6b0c2fda6f877a3c9ac4c3f668ea3e3e736f1
David DOYEN <David.Doyen@u-pem.fr>
2020-03-08 15:00:31 +0100

    ddoyen modified Yggdrasil/demo/basic/sortlist_premiers_ministres.pl

--- demo/basic/sortlist_premiers_ministres.pl
+++ demo/basic/sortlist_premiers_ministres.pl
@@ -8,10 +8,10 @@ before ==
 import random as rd
 import csv
 
-with open('premiers_ministres.csv',newline='') as file:
-    rows=list(csv.DictReader(file,delimiter=','))
+with open('premiers_ministres.csv', newline='') as file:
+    rows = list(csv.DictReader(file, delimiter=','))
 
-lst = rd.sample(range(len(rows)),5)
+lst = rd.sample(range(len(rows)), 5)
 lst.sort()
 sortlist.loadlist([(rows[i]["nom"],rows[i]["dates"]) for i in lst])
 ==
@@ -42,4 +42,5 @@ $(document).ready(function(){
 });
 </script>
 ==
+
 
