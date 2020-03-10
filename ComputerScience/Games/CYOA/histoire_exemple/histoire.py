0f81bb7c950ab7e64b38e93fca901049b550c7e1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-03-10 12:40:10 +0100

    nborie modified Yggdrasil/ComputerScience/Games/CYOA/histoire_exemple/histoire.py

--- ComputerScience/Games/CYOA/histoire_exemple/histoire.py
+++ ComputerScience/Games/CYOA/histoire_exemple/histoire.py
@@ -3,7 +3,23 @@
     [
         '',
 
-        "Voici un exemple d'histoire scénarisable avec ",
+        "<p>Voici un exemple d'histoire scénarisable avec inclusion d'images par exemple. </p><br />"
+
+        "<p>De manière abstraite, une histoire dont vous êtes le héro est un graphe sur "
+        "lequel le lecteur se promène parce qu'on lui laisse régulièrement des choix. "
+        "Ce graphe a un seul point d'entrée, c'est à dire que les lecteurs, joueurs, "
+        "voire étudiants, commencent systématiquemenent au même endroit. </p><br />"
+
+        "<p>PLaTon peut en fait aller plus loin que ça car il est possible (et optionnel) "
+        "d'inclure un script python dans les noeuds de l'histoire, c'est à dire chacune des "
+        "étapes visitables mais aussi des scripts dans les transitions du graphes (à chaque "
+        "lien emprunté par l'utilisateur)</p></br>"
+        
+        "<p>Ici, dans cette démontration, l'exercice va cacher une clé dans un lieu aléatoire "
+        "(c'est un tirage aléatoire uniforme dans une liste finie de lieux). Pour gagner, "
+        "vous devrez aller chercher la clé dans l'endroit indiquéé.</p><br />"
+        
+        ,
 
         [
 
