ef95f72d51aff634851863c91d5103007d758c4a
David DOYEN <David.Doyen@u-pem.fr>
2020-03-10 20:16:08 +0100

    ddoyen modified Yggdrasil/utils/components/textselect.py

--- utils/components/textselect.py
+++ utils/components/textselect.py
@@ -29,16 +29,42 @@ def read_text_bracket(string):
 
 
 class CustomTextSelect(Component):
+class CustomTextSelect:
     def __init__(self, **kwargs):
-        self.selector = 'c-text'
-        self.decorator = 'CustomText'
-        super().__init__(**kwargs)
+        self.text = ""
+        self._sol = []
 
     def settext(self, text):
         self.text = self.bracket(text)
 
     def setdata_from_text(self,text):
-        self.text,self._indexsol=read_text_bracket(bracket_words(text))
+        self.text,self._sol=self.find_double_text_bracket(bracket_words(text))
+        
+    @staticmethod
+    def bracket(string):
+        
+        lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|\[[^\]]*\]|[^\{\}\[\]]+",string)
+        for i in range(len(lst)):
+            s=lst[i]
+            if s[0]=="[":
+                lst[i]=s[1:-1]
+            elif s[0]!="{":
+                lst[i]=re.sub(r"(\w+)", r"{\1}",s)
+        return "".join(lst)
+    
+    @staticmethod
+    def find_double_brackets(string):
+        lst=re.findall(r"\{[^\{\}]*\}|\{\{[^\}]*\}\}|[^\{\}]+",string)
+        selection=[]
+        k=0
+        for i in range(len(lst)):
+            s=lst[i]
+            if s[0]=="{":
+                if len(s)> 2 and s[1]=="{":
+                    selection.append(k)
+                    lst[i]=s[1:-1]
+                k+=1
+        return ("".join(lst),selection)
 
     def eval(self,**kwargs):
         """
@@ -82,5 +108,6 @@ class CustomTextSelect(Component):
         self.disabled = True
 
         return (score, "")
+
 
 
