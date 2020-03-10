834a0021dc11f076d0eb78374e088a694b68448b
David DOYEN <David.Doyen@u-pem.fr>
2020-03-10 17:08:46 +0100

    ddoyen modified Yggdrasil/utils/components/scoring.py

--- utils/components/scoring.py
+++ utils/components/scoring.py
@@ -1,3 +1,5 @@
+from scipy.stats import kendalltau
+
 def all_or_nothing(nbright=0, nbwrong=0, nbsol=1, nbitems=1):
     return 100 if nbwrong == nbsol else 0
 
@@ -6,4 +8,13 @@ def right_minus_wrong(nbright=0, nbwrong=0, nbsol=1, nbitems=1):
 
 def correct_items(nbright=0, nbwrong=0, nbsol=1, nbitems=1):
     return int(round(max([(nbitems - 2*nbwrong)/nbitems*100,0])))
+
+def exact_order(order):
+    n = len(order)
+    return 100 if order == list(range(n)) else 0
+
+def kendall_tau(order):
+    n = len(order)
+    tau,_ = kendalltau(order, list(range(n)))
+    return int(round(max([0,tau])*100))
 
