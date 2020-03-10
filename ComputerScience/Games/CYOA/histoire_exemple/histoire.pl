75197e2070896d176a52d4bea1f967d13a91609e
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-03-10 12:18:14 +0100

    nborie modified Yggdrasil/ComputerScience/Games/CYOA/histoire_exemple/histoire.pl

---
+++ ComputerScience/Games/CYOA/histoire_exemple/histoire.pl
@@ -0,0 +1,32 @@
+# ****************************************************************************
+#    Copyright (C) 2020 Nicolas Borie <nicolas.borie at univ-eiffel dot fr>
+#
+#          Distributed under the terms of the CC-BY-SA 4.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#    The full text of the CC-BY-SA 4.0 is available at:
+#
+#          https://creativecommons.org/licenses/by-sa/4.0/legalcode
+# ****************************************************************************
+
+author=Nicolas Borie
+
+title=Histoire dont vous êtes le héro ou serious game
+tag=exemple|demo|CYOA
+
+template=/ComputerScience/Games/CYOA/CYOA_template.pl
+
+@ Ace_Debugger_1.py
+
+cabane =$ cabane.jpg
+champ =$ champ.jpg
+foret =$ foret.jpg
+lac =$ lac.jpg
+montagne =$ montagne.jpg
+verger =$ verger.jpg
+
+game_data_file = histoire.py
+
