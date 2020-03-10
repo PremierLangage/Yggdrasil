fca8fa10a7d5ad8d6074eda63cc44e88773c6953
David DOYEN <David.Doyen@u-pem.fr>
2020-03-10 17:59:02 +0100

    ddoyen modified Yggdrasil/utils/components/dragdrop_example2.pl

--- utils/components/dragdrop_example2.pl
+++ utils/components/dragdrop_example2.pl
@@ -27,7 +27,7 @@ for i in range(n):
         mygroup.add_match_by_content(str(i),gt) 
 
 # Pour le moment ça bugge: les étiquettes sont vides.
-sync = mygroup.labels['lt']
+sync = mygroup.labels['lt'].copy()
 ==
 
 text==
