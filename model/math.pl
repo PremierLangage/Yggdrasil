4d984eea932e734a25d6d53b860d07d74c7e90c2
David DOYEN <David.Doyen@u-pem.fr>
2020-03-13 23:40:50 +0100

    ddoyen modified Yggdrasil/model/math.pl

--- model/math.pl
+++ model/math.pl
@@ -68,21 +68,6 @@ extrajs ==
 </script>
 ==
 
-extracss2==
-<style>
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-table {
-    margin: auto;
-}
-th, td {
-  padding: 0.5em;
-  text-align:center;
-}
-</style>
-==
 
 
 
