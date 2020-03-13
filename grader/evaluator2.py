a719d82637783f27162120fec3b919d283ec6b56
David DOYEN <David.Doyen@u-pem.fr>
2020-03-14 00:34:39 +0100

    ddoyen modified Yggdrasil/grader/evaluator2.py

--- grader/evaluator2.py
+++ grader/evaluator2.py
@@ -119,7 +119,7 @@ if __name__ == "__main__":
         dic['attempt'] = dic['attempt'] + 1
 
     if dic['attempt'] > dic['maxattempt']:
-        dic['settings']['submit'] = True
+        dic['settings']['submit'] = False
         if score < 100 and 'solution' in dic:
             feedback += Template(dic['solution']).render(dic)
     
