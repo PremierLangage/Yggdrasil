07656fafe7f2523817da386dbe0f8e0bf55850d0
David DOYEN <David.Doyen@u-pem.fr>
2020-03-14 02:23:30 +0100

    ddoyen modified Yggdrasil/model/mathradio.pl

--- model/mathradio.pl
+++ model/mathradio.pl
@@ -1,2 +1,10 @@
 extends = math.pl
 
+radio =: RadioGroup
+# Les deux lignes suivantes sont temporaires.
+@ /utils/components/radio.py [customradio.py]
+radio.decorator = CustomRadio
+
+form ==
+{{ radio|component }}
+==
