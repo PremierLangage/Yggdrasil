e5260df0bf7164e55067e47a56d5e132306399a0
David DOYEN <David.Doyen@u-pem.fr>
2020-03-15 17:08:36 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/subspaces/basis_Rn_subspace_.pl

--- Mathematics/exercises/algebra/linear_algebra/subspaces/basis_Rn_subspace_.pl
+++ Mathematics/exercises/algebra/linear_algebra/subspaces/basis_Rn_subspace_.pl
@@ -5,16 +5,15 @@ keyboards_JSON['virtualKeyboards']="sets"
 input.config = keyboards_JSON
 
 n = param['n']
-m = param['m']
-d=m-n
-coeffbound = 3
-A=rand_int_matrix_fullrank(n,m,coeffbound, excluded_values=[0])
-B=zeros(n,1)
-sys_tex=latexsys(A,B)
+nbeq = param['nbeq']
+m = n-nbeq
+A=rand_int_matrix_fullrank(nbeq, n, 3, excluded_values=[0])
+B=zeros(nbeq, 1)
+sys_tex = latexsys(A, B)
 ==
 
 text ==
-On considère le sous-espace de $! \mathbb{R}^{{m}} !$ formé des vecteurs $! (x,y,z) !$ tels que
+On considère le sous-espace de $! \mathbb{R}^{{n}} !$ formé des vecteurs $! (x,y,z) !$ tels que
 $$ {{sys_tex}} .$$
 Déterminer une base de ce sous-espace (écrire cette base comme un ensemble, entre accolades).
 ==
@@ -28,9 +27,9 @@ def evalans(strans):
         return (-1, "La réponse doit être un ensemble de vecteurs su sous-espace.")
     if M.shape!=(m,d):
         return (-1, "La réponse doit être un ensemble de vecteurs su sous-espace.")
-    if not (A*M == zeros(n,d)):
+    if not (A*M == zeros(nbeq,m)):
         return (0, "Cette famille n'est pas une base du sous-espace.")
-    if M.rank() != d:
+    if M.rank() != m:
         return (0, "Cette famille n'est pas une base du sous-espace.")
     return (100, "")
 
