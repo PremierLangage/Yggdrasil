af23047c27834d0794ce9cc454a030689c40bddb
David DOYEN <David.Doyen@u-pem.fr>
2020-03-16 16:42:24 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/radio_capitales.pl

--- Mathematics/tests/radio_capitales.pl
+++ Mathematics/tests/radio_capitales.pl
@@ -18,7 +18,7 @@ with open('pays_europe.csv', newline='') as file:
 radios = []
 du_pays = []
 for i in range(5):
-    radio = CustomRadio(cid="mycid1")
+    radio = CustomRadio(cid=f"cid{i}")
     sample_rows=rd.sample(all_rows, 4)
 
     pays = sample_rows[0]['pays']
