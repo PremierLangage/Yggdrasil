61471bf8c9c1af157e35a48ce91802f80c39ac32
David DOYEN <David.Doyen@u-pem.fr>
2020-03-16 16:21:52 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/radio_capitales.pl

---
+++ Mathematics/tests/radio_capitales.pl
@@ -0,0 +1,56 @@
+extends = /model/basic.pl
+
+@ pays_europe.csv
+
+title = Capitales européennes (Radio)
+
+radio =: RadioGroup
+# Les deux lignes suivantes sont temporaires.
+@ /utils/components/radio.py [customradio.py]
+radio.decorator = CustomRadio
+
+before ==
+import random as rd
+import csv
+from customradio import CustomRadio
+
+with open('pays_europe.csv', newline='') as file:
+    all_rows = list(csv.DictReader(file, delimiter=','))
+
+radios = []
+du_pays = []
+for i in range(5):
+    radio = CustomRadio()
+
+    sample_rows=rd.sample(all_rows, 4)
+
+    pays = sample_rows[0]['pays']
+    article = sample_rows[0]['article']
+
+    radio.setitems([row['capitale'] for row in sample_rows])
+    radio.setsol_from_index(0)
+    radio.shuffle()
+
+    radios.append(radio)
+
+    partitif = {"le": "du ", "la": "de la ", "les": "des ", "l": "de l'"}
+    du_pays.append(partitif[article] + pays)
+
+step = 0
+==
+
+text ==
+Quelle est la capitale {{ du_pays[step] }} ?
+==
+
+form ==
+{{ radios[step]|component }}
+==
+
+evaluator ==
+score = radio.eval()
+step +=1
+==
+
+
+
