3318b60b29af49b70c7f29b352a096ea4e0622e8
David DOYEN <David.Doyen@u-pem.fr>
2020-03-16 23:47:41 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/radio_capitales.pl

--- Mathematics/tests/radio_capitales.pl
+++ Mathematics/tests/radio_capitales.pl
@@ -30,7 +30,7 @@ for i in range(5):
     radio.shuffle()
     globals()[f"radio{i}"] = radio
 
-    radios.append(radio)
+    radios.append(radio.cid)
 
     partitif = {"le": "du ", "la": "de la ", "les": "des ", "l": "de l'"}
     du_pays.append(partitif[article] + pays)
