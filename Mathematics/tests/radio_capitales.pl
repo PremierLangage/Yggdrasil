57ae9ed5bc58e8e1c5dbc4e084d182793679a2da
David DOYEN <David.Doyen@u-pem.fr>
2020-03-17 14:58:52 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/radio_capitales.pl

--- Mathematics/tests/radio_capitales.pl
+++ Mathematics/tests/radio_capitales.pl
@@ -1,4 +1,61 @@
-extends = /model/basic.pl
+@ /utils/sandboxio.py
+@ /utils/components/scoring.py
+@ /utils/components/checkbox.py [customcheckbox.py]
+@ /utils/components/textselect.py [customtextselect.py]
+@ /builder/beforestep.py [builder.py]
+@ /grader/evaluatorstep.py [grader.py]
+
+extracss ==
+<style>
+.icon-check-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    margin-left: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #721c24;
+    margin-left: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-check-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    margin-right: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #721c24;
+    margin-right: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+
+
+.error-text-unit {
+    color: #721c24;
+    text-decoration: line-through red;
+}
+
+.success-text-unit {
+    color: #155724;
+    text-decoration: underline green;
+}
+</style>
+==
 
 @ /demo/basic/pays_europe.csv
 
