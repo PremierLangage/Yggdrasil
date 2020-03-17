1939317d905aa0a581ff52ab5112ece955012b52
David DOYEN <David.Doyen@u-pem.fr>
2020-03-17 15:17:40 +0100

    ddoyen modified Yggdrasil/grader/evaluatorstep.py

--- grader/evaluatorstep.py
+++ grader/evaluatorstep.py
@@ -98,8 +98,10 @@ if __name__ == "__main__":
         dic[key]=serialize(dic[key])
 
     step = dic['step']
+    
     if step > 0:
         dic[f"radio{step-1}"].eval()
+
     if dic['step'] < dic['nbstep']:
         dic['text'] = """Question {{step}}
         {{ texts[step]}}
