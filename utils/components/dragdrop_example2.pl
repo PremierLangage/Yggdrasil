561e49240e9d9822bbe4ee9d8ae04a7eeb4ec6c8
etienne sandier <sandier@u-pec.fr>
2020-03-17 17:15:28 +0100

    esandier modified Yggdrasil/utils/components/dragdrop_example2.pl

--- utils/components/dragdrop_example2.pl
+++ utils/components/dragdrop_example2.pl
@@ -61,7 +61,7 @@ mygroup.labels = {'lt':a, 'gt': b}
 mygroup.drops = {'0' : c0, '1': c1, '2': c2, '3': c3}
 mygroup._matches = match
 
-grade=mygroup.eval() # Ici ça bugge, mais c'est normal parce que le composant ne traverse pas
+grade=mygroup.eval(grading_function = right_minus_wrong) # Ici ça bugge, mais c'est normal parce que le composant ne traverse pas
 ==
 
 extracss == #|html| 
