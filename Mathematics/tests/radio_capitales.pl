56ad0a35a52fa8e90f2d04bef9d6e931fc433de3
David DOYEN <David.Doyen@u-pem.fr>
2020-03-18 16:11:15 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/radio_capitales.pl

--- Mathematics/tests/radio_capitales.pl
+++ Mathematics/tests/radio_capitales.pl
@@ -35,7 +35,7 @@ for i in range(nbstep):
 
     partitif = {"le": "du ", "la": "de la ", "les": "des ", "l": "de l'"}
     texts.append(f"Quelle est la capitale {partitif[article]}  {pays} ?")
-    forms.append(r"{{" + f"radios[ {i} ]" + r"|component }}")
+    forms.append("""{{ radios[step]|component }}""")
     evaluators.append("""score = globals()[f"radio{step}"].eval()""")
 ==
 
