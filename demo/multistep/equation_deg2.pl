89db739b0d86f88f7e3e43de95290675717df7d1
David DOYEN <David.Doyen@u-pem.fr>
2020-03-19 14:54:32 +0100

    ddoyen modified Yggdrasil/demo/multistep/equation_deg2.pl

--- demo/multistep/equation_deg2.pl
+++ demo/multistep/equation_deg2.pl
@@ -7,15 +7,13 @@ nbstep % 2
 input =: Input
 
 radio =: RadioGroup
-# Les deux lignes suivantes sont temporaires.
-@ /utils/components/radio.py [customradio.py]
 radio.decorator = CustomRadio
 
 stepseq % ["delta","nbsol"]
 
 before == #|python|
-#radio.setitems(["Aucune","Une solution","Deux solutions"])
-#radio.setsol_from_index(0)
+radio.setitems(["Aucune","Une solution","Deux solutions"])
+radio.setsol_from_index(0)
 ==
 
 intro ==
