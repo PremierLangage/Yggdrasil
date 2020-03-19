423d71ddb585591a08f4fee281cd72f4ee62ce22
David DOYEN <David.Doyen@u-pem.fr>
2020-03-19 16:34:10 +0100

    ddoyen modified Yggdrasil/grader/evaluatorstep.py

--- grader/evaluatorstep.py
+++ grader/evaluatorstep.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
             elif isinstance(dic['forms'], dict):
                 dic['form'] = dic['forms'][dic['stepseq'][step]]
 
-        dic['final'] += env.get_template(dic['text']+"\n"+dic['form']).render(dic)
+        dic['final'] += env.from_string(dic['text']+"\n"+dic['form']).render(dic)
 
     else:
         nbstep = dic['nbstep']
