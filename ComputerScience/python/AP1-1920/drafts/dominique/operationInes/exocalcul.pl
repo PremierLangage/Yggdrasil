0e5ae57d7c0be434028b90637b8eb0d5dbe1ce60
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-03-20 13:34:08 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/operationInes/exocalcul.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/operationInes/exocalcul.pl
@@ -0,0 +1,50 @@
+
+
+
+
+
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+title = Démo MathInput
+
+before ==
+import random
+lop=[("addition","+"),("soustraction","-"),("division","//"),("multiplication","*")]
+
+optxt,op = random.choice(lop)
+
+if op=="+" :
+    v1= random.randint(10000,100000)
+    v2= random.randint(10000,100000)
+elif op=="-":
+    v1= random.randint(10000,100000)
+    v2= random.randint(10000,100000)
+elif op=="//":
+    v1= random.randint(10000,100000)
+    v2= random.randint(10,100)
+elif op=="*":
+    v1= random.randint(1000,10000)
+    v2= random.randint(1000,10000)
+==
+
+text ==
+Calculer {{v1}}  {{optxt}} {{v2}} chère Ines. 
+==
+
+input =: MathInput
+
+form ==
+{{ input | component }}
+==
+
+evaluator ==
+
+result= eval(str(v1)+op+str(v2))
+if input.value== result:
+    grade=(100,"bravo Ines")
+else:
+    grade=(0,"Ressaye Ines")
+==
+
