229a41e81b2ede237bda8cda4ce08c32f146adde
David DOYEN <David.Doyen@u-pem.fr>
2020-03-23 13:04:35 +0100

    ddoyen modified Yggdrasil/demo/multistep/demo_AMC_TXT.pl

--- demo/multistep/demo_AMC_TXT.pl
+++ demo/multistep/demo_AMC_TXT.pl
@@ -4,25 +4,14 @@ title = Démo AMC-TXT
 
 
 questions ==
-* Parmi les données suivantes, lesquels sont en binaire dans un
-  ordinateur ?
-+ Un fichier audio wav
-+ Un fichier audio mp3
-+ Une image jpeg
-+ Une image png
-+ Un entier négatif
-+ Un entier positif
-+ Le texte intégral de Germinal dans un fichier texte
-+ La position du pointeur de la souris
+* Quelle est la capitale du Cameroun ?
++ Yaoundé
+- Douala
+- Kribi
 
-** Parmi les nombres binaires suivants, lesquels sont les multiples de 4 ?
-- 10010101
-- 10110111
-- 110010
-- 1
-+ 0
-+ 100
-+ 10100
-+ 110100100
-+ 10000000
+** Parmi les nombres suivants,
+lesquels sont positifs ?
++ 2
+- -2
++ 10
 ==
