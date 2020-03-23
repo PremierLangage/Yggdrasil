8499e24922b3f337adaaf84a4d88f8005b4bc8e4
David DOYEN <David.Doyen@u-pem.fr>
2020-03-23 13:42:58 +0100

    ddoyen modified Yggdrasil/demo/multistep/demo_AMC_TXT.pl

--- demo/multistep/demo_AMC_TXT.pl
+++ demo/multistep/demo_AMC_TXT.pl
@@ -2,15 +2,13 @@ extends = /model/AMC_TXT.pl
 
 title = Démo AMC-TXT
 
-
 questions ==
 * Quelle est la capitale du Cameroun ?
 + Yaoundé
 - Douala
 - Kribi
 
-** Parmi les nombres suivants,
-lesquels sont positifs ?
+** Parmi les nombres suivants, lesquels sont positifs ?
 + 2
 - -2
 + 10
