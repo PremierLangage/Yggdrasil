a946cf3ac2a487da4ae19b86ddf3a6d95ca5adc7
David DOYEN <David.Doyen@u-pem.fr>
2020-03-24 13:06:53 +0100

    ddoyen modified Yggdrasil/utils/parser/parserAMC.py

--- utils/parser/parserAMC.py
+++ utils/parser/parserAMC.py
@@ -1,12 +1,15 @@
 
 def parse_AMC_TXT(txt):
+
     questions = []
     pending = False
     i = -1
     comp = []
     statement = []
+
     for line in txt.splitlines()+['']:
         line = line.strip()
+
         if line.startswith('*'):
             k = 0
             index = []
@@ -19,19 +22,18 @@ def parse_AMC_TXT(txt):
             else:
                 question_type = "Radio"
                 statement = line[1:].strip()
-        if line.startswith(('+','-')):
+
+        elif line.startswith(('+','-')):
             items.append(line[1:].strip())
             if line.startswith('+'):
                 index.append(k)
             k += 1
-        if line == "" and pending:
+
+        elif line == "" and pending:
             if question_type == "Radio":
-                comp.append(CustomRadio())
                 index = index[0]
-            elif question_type == "Checkbox":
-                comp.append(CustomCheckbox())
-            comp[i].setitems(items)
-            comp[i].setsol_from_index(index)
-            comp[i].shuffle()
+            questions.append({'type': question_type, 'items': items, 'index': index})
             pending = False
+
+        return questions
 
