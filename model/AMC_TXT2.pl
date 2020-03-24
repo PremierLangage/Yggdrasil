0cff77b651c6d27ec5a579df78c9e1ea8427f252
David DOYEN <David.Doyen@u-pem.fr>
2020-03-24 13:16:29 +0100

    ddoyen modified Yggdrasil/model/AMC_TXT2.pl

---
+++ model/AMC_TXT2.pl
@@ -0,0 +1,62 @@
+extends = /model/multistep.pl
+
+settings.cumulative % false
+
+before == #|python|
+from customradio import CustomRadio
+from customcheckbox import CustomCheckbox
+
+pending = False
+i = -1
+comp = []
+statement = []
+for line in questions.splitlines()+['']:
+    line = line.strip()
+    if line.startswith('*'):
+        k = 0
+        index = []
+        items = []
+        pending = True
+        i += 1
+        if line.startswith('**'):
+            question_type = "Checkbox"
+            statement.append(line[2:].strip())
+        else:
+            question_type = "Radio"
+            statement.append(line[1:].strip())
+    if line.startswith(('+','-')):
+        items.append(line[1:].strip())
+        if line.startswith('+'):
+            index.append(k)
+        k += 1
+    if line == "" and pending:
+        if question_type == "Radio":
+            comp.append(CustomRadio())
+            index = index[0]
+        elif question_type == "Checkbox":
+            comp.append(CustomCheckbox())
+        comp[i].setitems(items)
+        comp[i].setsol_from_index(index)
+        comp[i].shuffle()
+        pending = False
+
+nbstep = i + 1
+==
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+==
+
+textstep ==
+<strong> Question {{ step + 1 }}. </strong> 
+{{ statement[step] }}
+==
+
+formstep ==
+{{ comp[step]|component }}
+==
+
+evaluatorstep ==
+score = comp[step].eval()
+==
+
