4e3231e090372e652c16c3871acc500f44d3bc9c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-03-24 21:58:28 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/commandes_terminal_MCQ.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/commandes_terminal_MCQ.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/commandes_terminal_MCQ.pl
@@ -1,17 +1,6 @@
 # Copyright 2020 Nicolas Borie <nicolas.borie@u-pem.fr>
-author=Nicolas Borie
+extends = /model/AMC_TXT.pl
 
-title=Commandes Unix en terminal
-tag=system|shell
+title = Quizz sur le terminal Unix
 
-template=/ComputerScience/OperatingSystem/templates/parametrized_MCQ.pl
-
-@ commandes_terminal.txt
-data_from_file=commandes_terminal.txt
-
-number_question=5
-min_option=4
-max_option=8
-
-
-
+questions=@ commandes_terminal.txt
