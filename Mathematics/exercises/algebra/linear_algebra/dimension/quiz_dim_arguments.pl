a417549b87490afc4d95f838d9319bfe439919dd
David DOYEN <David.Doyen@u-pem.fr>
2020-03-24 18:04:07 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/dimension/quiz_dim_arguments.pl

--- Mathematics/exercises/algebra/linear_algebra/dimension/quiz_dim_arguments.pl
+++ Mathematics/exercises/algebra/linear_algebra/dimension/quiz_dim_arguments.pl
@@ -25,79 +25,79 @@ questions ==
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!$ et $!F \subset G!, peut-on conclure que $!F = G!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!$ et $!F \subset G!$, peut-on conclure que $!F = G!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!$ et $!G \subset F!, peut-on conclure que $!F = G!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!$ et $!G \subset F!$, peut-on conclure que $!F = G!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!, peut-on conclure que $!F = G!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!$, peut-on conclure que $!F = G!$ ?
 - Oui
 + Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!, peut-on conclure que $!F = G!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F = \dim G!$, peut-on conclure que $!F = G!$ ?
 - Oui
 + Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F \neq \dim G!, peut-on conclure que $!F \neq G!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F \neq \dim G!$, peut-on conclure que $!F \neq G!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F < \dim G!, peut-on conclure que $!F \neq G!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F < \dim G!$, peut-on conclure que $!F \neq G!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ est un sous-espace vectoriel tel que $!\dim F = n!, peut-on conclure que $!F=E!$ ?
+*[ordered] Si $!F!$ est un sous-espace vectoriel tel que $!\dim F = n!$, peut-on conclure que $!F=E!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ est un sous-espace vectoriel tel que $!\dim F = 0!, peut-on conclure que $!F=\{0\}!$ ?
+*[ordered] Si $!F!$ est un sous-espace vectoriel tel que $!\dim F = 0!$, peut-on conclure que $!F=\{0\}!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ est un sous-espace vectoriel tel que $!\dim F = 0!, peut-on conclure que $!F=\emptyset!$ ?
+*[ordered] Si $!F!$ est un sous-espace vectoriel tel que $!\dim F = 0!$, peut-on conclure que $!F=\emptyset!$ ?
 - Oui
 + Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F \cap G = \{0\}!$ et $!\dim F + \dim G = n!, peut-on conclure que $!F+G=E!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F \cap G = \{0\}!$ et $!\dim F + \dim G = n!$, peut-on conclure que $!F+G=E!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F \cap G = \{0\}!$ et $!\dim F + \dim G = n!, peut-on conclure que $!F+G=E!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F \cap G = \{0\}!$ et $!\dim F + \dim G = n!$, peut-on conclure que $!F+G=E!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F+G=E!$ et $!\dim F + \dim G = n!, peut-on conclure que $!F \cap G = \{0\}!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F+G=E!$ et $!\dim F + \dim G = n!$, peut-on conclure que $!F \cap G = \{0\}!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F+G=E!$ et $!\dim F + \dim G = n!, peut-on conclure que $!F \cap G = \{0\}!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!F+G=E!$ et $!\dim F + \dim G = n!$, peut-on conclure que $!F \cap G = \{0\}!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G = n!, peut-on conclure que $!F+G=E!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G = n!$, peut-on conclure que $!F+G=E!$ ?
 - Oui
 + Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim (F + G) = n!, peut-on conclure que $!F+G=E!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim (F + G) = n!$, peut-on conclure que $!F+G=E!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G < n!, peut-on conclure que $!F+G\neq E!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G < n!$, peut-on conclure que $!F+G\neq E!$ ?
 + Oui
 - Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G < n!, peut-on conclure que $!F \cap G \neq \{0\}!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G < n!$, peut-on conclure que $!F \cap G \neq \{0\}!$ ?
 - Oui
 + Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G > n!, peut-on conclure que $!F+G\neq E!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G > n!$, peut-on conclure que $!F+G\neq E!$ ?
 - Oui
 + Non
 
-*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G > n!, peut-on conclure que $!F \cap G \neq \{0\}!$ ?
+*[ordered] Si $!F!$ et $!G!$ sont deux sous-espaces vectoriels tels que $!\dim F + \dim G > n!$, peut-on conclure que $!F \cap G \neq \{0\}!$ ?
 + Oui
 - Non
 ==
