457a69478b6285288c90af88c49d7d70eb8d9c6d
David DOYEN <David.Doyen@u-pem.fr>
2020-03-24 15:58:57 +0100

    ddoyen modified Yggdrasil/utils/format/AMC.py

--- utils/format/AMC.py
+++ utils/format/AMC.py
@@ -8,16 +8,22 @@ def parse_AMC_TXT(txt):
         line = line.strip()
 
         if line.startswith('*'):
+            if line.startswith('**'):
+                question_type = "Checkbox"
+            else:
+                question_type = "Radio"
+            line = line.lstrip("* ")
+            if line.starstwith('['):
+                r0 = line.find(']')
+                options = line[1:r0].split()
+                line = line[r0:]
+            else:
+                options = []
+            statement = line
             k = 0
             index = []
             items = []
             pending = True
-            if line.startswith('**'):
-                question_type = "Checkbox"
-                statement = line[2:].strip()
-            else:
-                question_type = "Radio"
-                statement = line[1:].strip()
 
         elif line.startswith(('+','-')):
             items.append(line[1:].strip())
@@ -28,7 +34,12 @@ def parse_AMC_TXT(txt):
         elif line == "" and pending:
             if question_type == "Radio":
                 index = index[0]
-            questions.append({'type': question_type, 'text': statement, 'items': items, 'index': index})
+            questions.append({'type': question_type, 
+            'text': statement, 
+            'items': items, 
+            'index': index, 
+            'options': options
+            })
             pending = False
 
     return questions
