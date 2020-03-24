81f239c0cd90d874f10f1593b31d3b146116ff7e
David DOYEN <David.Doyen@u-pem.fr>
2020-03-24 15:15:51 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/dimension/quiz_dim_arguments.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/dimension/quiz_dim_arguments.pl
@@ -0,0 +1,38 @@
+extends = /model/AMC_TXT.pl
+
+title = Démo AMC-TXT
+
+questions ==
+* Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$, peut-on conclure que $!F \subset G!$ ?
++ Yaoundé
+- Douala
+
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$&#44; peut-on conclure que $!G \subset F!$ ?, 2, 2
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$ et $!F \subset G!$&#44; peut-on conclure que $!F = G!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$ et $!G \subset F!$&#44; peut-on conclure que $!F = G!$ ?,2,1
+
+* Quelle est la capitale du Cameroun ?
++ Yaoundé
+- Douala
+- Kribi
+
+** Parmi les nombres suivants, lesquels sont positifs ?
++ 2
+- -2
++ 10
+==
+
+source ==
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$&#44; peut-on conclure que $!F \subset G!$ ?, Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$&#44; peut-on conclure que $!G \subset F!$ ?, 2, 2
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$ et $!F \subset G!$&#44; peut-on conclure que $!F = G!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \leq dim G!$ et $!G \subset F!$&#44; peut-on conclure que $!F = G!$ ?,2,1
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F = dim G!$ et $!F \subset G!$&#44; peut-on conclure que $!F = G!$ ?, Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F = dim G!$ et $!G \subset F!$&#44; peut-on conclure que $!F = G!$ ?,1,1
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F = dim G!$&#44; peut-on conclure que $!F = G!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F = dim G!$&#44; peut-on conclure que $!F = G!$ ?,2,2
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F \neq dim G!$&#44; peut-on conclure que $!F \neq G!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F < dim G!$&#44; peut-on conclure que $!F \neq G!$ ?,1,1
+\SiF $!dim F = n!$&#44; peut-on conclure que $!F=E!$ ?,\SiF $!dim F = n!$&#44; peut-on conclure que $!F=E!$ ?,1,1
+\SiF $!dim F = 0!$&#44; peut-on conclure que $!F=\{0\}!$ ?,\SiF $!dim F = 0!$&#44; peut-on conclure que $!F=\emptyset!$ ?,1,2
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!F \cap G = \{0\}!$ et $!dim F + dim G = n!$&#44; peut-on conclure que $!F+G=E!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!F \cap G = \{0\}!$ et $!dim F + dim G = n!$&#44; peut-on conclure que $!F+G=E!$ ?,1,1
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!F+G=E!$ et $!dim F + dim G = n!$&#44; peut-on conclure que $!F \cap G = \{0\}!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!F+G=E!$ et $!dim F + dim G = n!$&#44; peut-on conclure que $!F \cap G = \{0\}!$ ?,1,1
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F + dim G = n!$&#44; peut-on conclure que $!F+G=E!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim (F + G) = n!$&#44; peut-on conclure que $!F+G=E!$ ?,2,1
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F + dim G < n!$&#44; peut-on conclure que $!F+G\neq E!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F + dim G < n!$&#44; peut-on conclure que $!F \cap G \neq \{0\}!$ ?,1,2
+Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F + dim G > n!$&#44; peut-on conclure que $!F+G\neq E!$ ?,Si \(F\) et \(G\) sont deux sous-espaces vectoriels tels que $!dim F + dim G > n!$&#44; peut-on conclure que $!F \cap G \neq \{0\}!$ ?,2,1
+==
+
