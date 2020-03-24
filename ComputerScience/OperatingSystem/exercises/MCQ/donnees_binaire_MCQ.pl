b042645430368e0ed61aca28d1f24a098cb4a180
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-03-24 23:24:20 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/donnees_binaire_MCQ.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/donnees_binaire_MCQ.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/donnees_binaire_MCQ.pl
@@ -1,7 +1,8 @@
 # Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
 extends = /model/AMC_TXT.pl
 
-title = Quizz sur le terminal Unix
+title = Quizz sur les données binaires
 
 questions=@ donnees_binaire.txt
+
 
