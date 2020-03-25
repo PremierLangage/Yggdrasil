2ba1f7d922457319504a74066e57828f2c0a6564
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-03-25 13:56:27 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/generalites_systeme_MCQ.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/generalites_systeme_MCQ.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/generalites_systeme_MCQ.pl
@@ -1,6 +1,9 @@
 # Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+tag=system|unix|generalites
+
 extends = /model/AMC_TXT.pl
 
 title = Quizz sur quelques généralités d'Unix
 
 questions=@ generalites_systeme.txt
+
