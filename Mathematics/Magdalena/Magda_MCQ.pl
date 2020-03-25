d74ff34a1c0055ad7f020e0995c7b28d40e2d68a
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-03-25 09:35:37 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Magda_MCQ.pl

---
+++ Mathematics/Magdalena/Magda_MCQ.pl
@@ -0,0 +1,44 @@
+# Copyright 2020 Nicolas Borie <nicolas.borie@u-pem.fr>
+author=Nicolas Borie
+
+title=Commandes Unix en terminal
+tag=system|shell
+
+%template=/ComputerScience/OperatingSystem/templates/parametrized_MCQ.pl
+%@ magda.txt [content.csv]
+
+extends=/model/AMC_TXT.pl
+title=QCM
+questions==
+
+* 12 Ceci est une question 
+- ceci n'est pas une bonne réponse 
++ ça c'est une bonne réponse 
+
+
+* 66 Ceci est une question 
+- ceci n'est pas une bonne réponse 
++ ça c'est une bonne réponse 
+
+
+* 33 Ceci est une question 
++ et ça c'est une bonne réponse 
+- ce.  ci n'est pas une bonne réponse 
+
+
+* Quel code numérique correspond aux droits rwxr-xr-- ?
+- 721
+- 761
+- 764
+- 713
+- 731
+- 651
+- 741
++ 754
+
+==
+
+
+
+
+
