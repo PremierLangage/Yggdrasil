6c0c9a568d79f760f25df2b7e36a838b73995dff
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-03-25 09:59:30 +0100

    drevuz modified Yggdrasil/Mathematics/Magdalena/latexqizz.pl

---
+++ Mathematics/Magdalena/latexqizz.pl
@@ -0,0 +1,19 @@
+
+extends = /model/AMC_TXT.pl
+title = Démo AMC-TXT
+questions==
+
+* la limite $%\lim_{x=0}{f(x)}%$ 
++ est nulle 
+- est pas nulle
+
+
+** la limite $%\lim_{x=0}{f(x)}%$ 
++ est nulle 
+- est pas nulle
+- est égal a 1 
+ 
+
+==
+
+
