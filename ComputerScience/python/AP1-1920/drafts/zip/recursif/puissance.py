d5755457dc2c8da75da75d4ced1c00d99add016a
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-03-25 23:22:36 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/recursif/puissance.py

---
+++ ComputerScience/python/AP1-1920/drafts/zip/recursif/puissance.py
@@ -0,0 +1,10 @@
+
+# author zip
+# Heritage d'un type d'exercice 
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+@ rectest.py
+
+title =Puissance rapide
+text==
