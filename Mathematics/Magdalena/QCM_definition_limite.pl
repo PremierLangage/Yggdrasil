5aac9a1046349f09a490d7c0468c730d79b5b189
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-03-25 11:23:56 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/QCM_definition_limite.pl

--- Mathematics/Magdalena/QCM_definition_limite.pl
+++ Mathematics/Magdalena/QCM_definition_limite.pl
@@ -14,7 +14,7 @@ questions
 
 ==
 
-* $% \displaystyle{\lim_{x\to 0} f(x)=2} %$ signifie :
+* $%2^n %$ signifie :
 + c'est une bonne réponse
 - c'est pas une bonne réponse
 #+ $%\forall \varepsilon >0 \exists \eta>0 \forall x\in D_f |x|\le \eta \implies |f(x)-2|\le \varepsilon %$
