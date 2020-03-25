613627d8eba52356322dd67a659cc16d1fe5c489
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-03-25 17:47:15 +0100

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/recursif/rectest.py

---
+++ ComputerScience/python/AP1-1920/drafts/zip/recursif/rectest.py
@@ -0,0 +1,42 @@
+
+from bdb import Bdb
+import sys
+def fa(n):
+    if n:
+        return n*fa(n-1)
+    return 1
+class RecursionDetected(Exception):
+    pass
+
+class RecursionDetector(Bdb):
+    def do_clear(self, arg):
+        pass
+
+    def __init__(self, *args):
+        self.stack = set()
+
+    def user_call(self, frame, argument_list):
+        code = frame.f_code
+        if code in self.stack:
+            raise RecursionDetected
+        self.stack.add(code)
+
+    def user_return(self, frame, return_value):
+        self.stack.remove(frame.f_code)
+
+
+def est_recursive(func):
+    """Renvoie True si func effectue des appels récursifs, False sinon.
+    """
+ 
+    detector = RecursionDetector()
+    #detector.set_trace()
+    try:
+        func()
+    except RecursionDetected:
+       return(True)
+    else:
+        return(False)
+    finally:
+        return(None)
+
