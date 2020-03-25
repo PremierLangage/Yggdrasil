c492fff91f78f16902868d0989a50ce0d91c97bc
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-03-25 19:34:28 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/Exemple_MCQ.pl

---
+++ Mathematics/Suites_et_fonctions/Exemple_MCQ.pl
@@ -0,0 +1,50 @@
+# Copyright 2020 Nicolas Borie <nicolas.borie@u-pem.fr>
+author=Magdalena Kobylanski
+
+title=Commandes Unix en terminal
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+title=QCM $%\displaystyle{\lim_{n\to \infty}f(n)}%$
+questions==
+
+* 12 $% 2^n %$ Ceci est une question 
+- ceci n'est pas une bonne réponse 
++ ça c'est une bonne réponse 
+
+** Comment décomposer un entier en base 2 ?
++ On fait des divisions successives par deux, puis on lit en ordre inverse les restes obtenus.
+- On utilise l'algorithme d'Euclide.
+- Jamais personne n'a réussi à décomposer un nombre en base 2.
+- On multiplie par $% 2^n %$ l'entier pour tout $% n %$.
+- On utilise $% log_2 %$ la fonction logarithme à base 2.
+- Il faut utiliser la règle du complément à 1.
+
+* 66 Ceci est une question 
+- ceci n'est pas une bonne réponse 
++ ça c'est une bonne réponse 
+
+
+* 33 Ceci est une question 
++ et ça c'est une bonne réponse 
+- ce.  ci n'est pas une bonne réponse 
+
+
+* Quel code numérique correspond aux droits rwxr-xr-- ?
+- 721
+- 761
+- 764
+- 713
+- 731
+- 651
+- 741
++ 754
+
+==
+
+
+
+
+
+
+
