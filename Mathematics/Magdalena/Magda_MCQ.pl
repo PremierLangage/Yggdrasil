5809fe55f02bc96f02f9e3498a00809d5e694f10
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-03-25 10:12:51 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Magda_MCQ.pl

--- Mathematics/Magdalena/Magda_MCQ.pl
+++ Mathematics/Magdalena/Magda_MCQ.pl
@@ -1,20 +1,24 @@
 # Copyright 2020 Nicolas Borie <nicolas.borie@u-pem.fr>
-author=Nicolas Borie
+author=Magdalena Kobylanski
 
 title=Commandes Unix en terminal
 tag=system|shell
 
-%template=/ComputerScience/OperatingSystem/templates/parametrized_MCQ.pl
-%@ magda.txt [content.csv]
-
 extends=/model/AMC_TXT.pl
-title=QCM
+title=QCM $%\displaystyle{\lim_{n\to \infty}f(n)}%$
 questions==
 
-* 12 Ceci est une question 
+* 12 $% 2^n %$ Ceci est une question 
 - ceci n'est pas une bonne réponse 
 + ça c'est une bonne réponse 
 
+** Comment décomposer un entier en base 2 ?
++ On fait des divisions successives par deux, puis on lit en ordre inverse les restes obtenus.
+- On utilise l'algorithme d'Euclide.
+- Jamais personne n'a réussi à décomposer un nombre en base 2.
+- On multiplie par $% 2^n %$ l'entier pour tout $% n %$.
+- On utilise $% log_2 %$ la fonction logarithme à base 2.
+- Il faut utiliser la règle du complément à 1.
 
 * 66 Ceci est une question 
 - ceci n'est pas une bonne réponse 
