ec54106dcbf0fb9555c9e28da72001a58a1e2913
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-03-25 10:41:32 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/QCM_definition_limite.pl

---
+++ Mathematics/Magdalena/QCM_definition_limite.pl
@@ -0,0 +1,46 @@
+
+# Copyright 2020 Magdalena Kobylanski <magdalena.kobylanski@u-pem.fr>
+author=Magdalena Kobylanski
+
+title=
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+title==QCM sur la défintion des limites de fonctions
+Soit $% f:\mathbb{R} \to \mathbb{R)%$ une fonction définie sur $D_f%$. On suppose que $% 0\in D_f%$.
+questions==
+
+* $% \lim_{x\to 0} f(x)=0 %$ signifie
++ \forall \varepsilon >0 \exists \eta>0 \for 
+
+** Comment décomposer un entier en base 2 ?
++ On fait des divisions successives par deux, puis on lit en ordre inverse les restes obtenus.
+- On utilise l'algorithme d'Euclide.
+- Jamais personne n'a réussi à décomposer un nombre en base 2.
+- On multiplie par $% 2^n %$ l'entier pour tout $% n %$.
+- On utilise $% log_2 %$ la fonction logarithme à base 2.
+- Il faut utiliser la règle du complément à 1.
+
+* 66 Ceci est une question 
+- ceci n'est pas une bonne réponse 
++ ça c'est une bonne réponse 
+
+
+* 33 Ceci est une question 
++ et ça c'est une bonne réponse 
+- ce.  ci n'est pas une bonne réponse 
+
+
+* Quel code numérique correspond aux droits rwxr-xr-- ?
+- 721
+- 761
+- 764
+- 713
+- 731
+- 651
+- 741
++ 754
+
+==
+
+
