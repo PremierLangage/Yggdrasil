90666b94e686644bea0efc4cd378b603ac876787
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-03-26 20:58:26 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/recursif/coefbinome.pl

--- ComputerScience/python/AP1-1920/drafts/zip/recursif/coefbinome.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/recursif/coefbinome.pl
@@ -13,17 +13,27 @@ $%\binom{n}{0}=1%$ <br>
 $%\binom{n}{n}=1%$ <br>
 $%\binom{n}{p}=\binom{n-1}{p}+\binom{n-1}{p-1}1%$ pour $%0 \lt p \lt n%$ <br>
 Cette fonnction esr particulièrement innefficace : elle calcule plusieurs fois les mêmes valeurs
-On décide de garder ces valeurs déjà calculéeses  dans un dictionnaire pour ne pas les recalculer
+On décide de garder ces valeurs déjà calculées  dans un dictionnaire pour ne pas les recalculer
 ==
 before==
 from random import *
 
-
+d={}
 def binome(n,p):
     if p==0 or p==n:
         return 1
     x=  binome(n-1,p)+binome(n-1,p)
     return x
+def binomedico(n,p,dic):
+    if (n,p) in dic:
+        return dic[(x,y)]
+    if p==0 or p==n:
+        return 1
+    x=  binomedico(n-1,p)+binomedico(n-1,p)
+    dic[(n,p)]=x
+    return x
+
+
 ==
 
 pltest0==
