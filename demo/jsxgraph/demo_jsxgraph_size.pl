b3cabd934b90ac59cca66085600f5736f39d8c23
David DOYEN <David.Doyen@u-pem.fr>
2020-03-26 19:24:36 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/demo_jsxgraph_size.pl

---
+++ demo/jsxgraph/demo_jsxgraph_size.pl
@@ -0,0 +1,43 @@
+extends = /model/math.pl
+
+title = Angle
+
+jxg =: MathDrawer
+
+jxg.attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25]}
+
+before ==
+
+==
+
+jxg.script ==
+
+==
+
+text ==
+
+==
+
+form ==
+{{ jxg | component }}
+==
+
+
+evaluator ==
+
+==
+
+
+extracss==
+<style>
+.jsxgraph-component {
+    max-width:300px;
+    width:90vw;
+    max-height:300px;
+    height:90vw;
+    margin: 0 auto;
+}
+</style>
+==
+
+
