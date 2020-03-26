1dc582fd2ecddedd108ab38fa48cec51ebdc9391
David DOYEN <David.Doyen@u-pem.fr>
2020-03-27 00:13:53 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/demo_jsxgraph_size.pl

--- demo/jsxgraph/demo_jsxgraph_size.pl
+++ demo/jsxgraph/demo_jsxgraph_size.pl
@@ -35,12 +35,12 @@ evaluator ==
 
 extracss==
 <style>
-.jsxgraph-component {
-    max-width:300px !important;
-    width:90vw !important;
-    max-height:300px !important;
-    height:90vw !important;
-    margin: 0 auto !important;
+.jsxgraph-component2 {
+    max-width:300px;
+    width:90vw;
+    max-height:300px;
+    height:90vw;
+    margin: 0 auto;
 }
 </style>
 ==
