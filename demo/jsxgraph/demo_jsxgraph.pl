88a6444b11b9a20c3469faaa0b73ef1b78ee2ed6
David DOYEN <David.Doyen@u-pem.fr>
2020-03-26 10:34:37 +0100

    ddoyen modified Yggdrasil/demo/jsxgraph/demo_jsxgraph.pl

--- demo/jsxgraph/demo_jsxgraph.pl
+++ demo/jsxgraph/demo_jsxgraph.pl
@@ -7,21 +7,21 @@ jxg =: MathDrawer
 # Les deux lignes suivantes sont temporaires.
 jxg.decorator = CustomJSXGraph
 
-jxg.attributes % {"showNavigation":false, "boundingbox":[-6,6,6,-6]}
+jxg.attributes % {"showNavigation": false, "boundingbox": [-6, 6, 6, -6]}
 
 before ==
 jxg.setscript(script_init)
 ==
 
 script_init ==
-var A = board.create('point', [-2,2], {size:2, name:'A', color:'red'});
-var B = board.create('point', [1,1], {size:2, name:'B', color:'blue'});
-var C = board.create('point', [0,0], {size:2, name:'C', color:'blue'});
+var A = board.create('point', [-2,2], {size: 2, name: 'A', color: 'red'});
+var B = board.create('point', [1,1], {size: 2, name: 'B', color: 'blue'});
+var C = board.create('point', [0,0], {size: 2, name: 'C', color: 'blue'});
 var line = board.create('line', [B, C]);
 ==
 
 text ==
-Déplacer les points.
+Déplacer les points puis cliquer sur le bouton **Valider** pour obtenir leurs coordonnées.
 ==
 
 form ==
