265905676286e887722ccd5e6124118670cc4e79
David DOYEN <David.Doyen@u-pem.fr>
2020-03-27 18:51:51 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl
@@ -5,12 +5,17 @@ before ==
 keyboards_JSON['virtualKeyboards']="elementary"
 input.config = keyboards_JSON
 n = 2
-case = 1
+case = 3
 if case == 1:
     A = rand_int_matrix_givenrank(2,4,1)
 elif case == 2:
     A = rand_int_matrix_givenrank(2,4,2)
-
+if case == 3:
+    A = rand_int_matrix_givenrank(3,4,1)
+elif case == 4:
+    A = rand_int_matrix_givenrank(3,4,2)
+elif case == 5:
+    A = rand_int_matrix_givenrank(3,4,3)
 
 family = ', '.join([str(tuple(A.row(i))) for i in range(n)])
 ==
