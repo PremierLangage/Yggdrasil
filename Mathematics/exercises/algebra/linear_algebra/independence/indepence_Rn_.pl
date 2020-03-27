ea308abdbfea34cc84408a9c7f610db46a7c9c03
David DOYEN <David.Doyen@u-pem.fr>
2020-03-27 19:17:49 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl
@@ -6,14 +6,15 @@ keyboards_JSON['virtualKeyboards']="elementary"
 input.config = keyboards_JSON
 n = 2
 case = 3
+d = 3
 if case == 1:
-    A = rand_int_matrix_givenrank(2,4,1)
+    A = rand_int_matrix_givenrank(2, d, 2, magnitude=2)
 elif case == 2:
-    A = rand_int_matrix_givenrank(2,4,2)
-if case == 3:
-    A = rand_int_matrix_givenrank(3,4,0,magnitude = 1)
+    A = rand_int_matrix_givenrank(2, d, 2, magnitude=2)
+elif case == 3:
+    A = rand_int_matrix_givenrank(3, d, 1, magnitude=1)
 elif case == 4:
-    A = rand_int_matrix_givenrank(3,4,2,magnitude = 1)
+    A = rand_int_matrix_givenrank(3, d, 2, magnitude=1)
 
 n = A.shape[0]
 family = ', '.join([str(tuple(A.row(i))) for i in range(n)])
