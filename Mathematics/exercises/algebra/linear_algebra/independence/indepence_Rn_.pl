93f1ad5e1a509f845802be305075c7e56da56343
David DOYEN <David.Doyen@u-pem.fr>
2020-03-27 18:49:42 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/independence/indepence_Rn_.pl
@@ -12,7 +12,7 @@ elif case == 2:
     A = rand_int_matrix_givenrank(2,4,2)
 
 
-family = str([tuple(A.row(i)) for i in range(n)]*)
+family = ', '.join(names)([tuple(A.row(i)) for i in range(n)])
 ==
 
 text ==
