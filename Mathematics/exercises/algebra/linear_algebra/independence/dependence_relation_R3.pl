5b664b0266933fd62fe42cbfd9fb354b6c45fa46
David DOYEN <David.Doyen@u-pem.fr>
2020-03-28 23:34:30 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/independence/dependence_relation_R3.pl

--- Mathematics/exercises/algebra/linear_algebra/independence/dependence_relation_R3.pl
+++ Mathematics/exercises/algebra/linear_algebra/independence/dependence_relation_R3.pl
@@ -21,7 +21,7 @@ Ces vecteurs sont liés. Déterminer une combinaison linéaire de ces vecteurs �
 evaluator ==
 var('u v w')
 ans = input.value
-rel = Poly(latex2sympy(ans),u,v,w))
+rel = Poly(latex2sympy(ans),u,v,w)
 feedback = str(rel.is_linear)
 
 score = 100
