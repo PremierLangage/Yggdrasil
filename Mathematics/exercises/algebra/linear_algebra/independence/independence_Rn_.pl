d3794b700c990c4ab8273a33d7ca7cf5d9c53cba
David DOYEN <David.Doyen@u-pem.fr>
2020-03-28 19:27:37 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/independence/independence_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/independence/independence_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/independence/independence_Rn_.pl
@@ -4,12 +4,18 @@ extends = /model/math.pl
 before ==
 dim = []
 family = []
-n = 2
-case = 0
-if case == 0:
-    A = rand_int_matrix_givenrank(2, 4, 2, magnitude=2)
-else == 3:
-    A = rand_int_matrix_givenrank(3, 4, 2, magnitude=1)
+radio = []
+
+for i in range(4):
+    n = 2
+    case = 0
+    if case == 0:
+        A = rand_int_matrix_givenrank(2, 4, 2, magnitude=2)
+    else == 3:
+        A = rand_int_matrix_givenrank(3, 4, 2, magnitude=1)
+    radios.append(CustomRadio())
+    radios[i].setitems(["Oui","Non"])
+    radios[i].setsol_from_index(case)
 
 dim.append(n)
 family.append(', '.join([str(tuple(A.row(i))) for i in range(n)]))
@@ -23,15 +29,9 @@ $$ \mathcal{F} = \\{ {{ family[step] }} \\}.$$
 
 form ==
 {{ radio[step]|component }}
-
-evaluator ==
-score, error, feedback = eval_tuple(input.value, sol, checksize=True)
-if error == "NotTuple" or error == "WrongSize":
-    feedback = f"Votre réponse n'est pas un {tuple_name} de nombres."
 ==
 
-solution == 
-La solution est $! {{sol_tex}} !$.
+evaluatorstep ==
+score = radio[step].eval()
 ==
-
 
