fd1516cf73418f3ec46f19b9ee5ff390b970912e
David DOYEN <David.Doyen@u-pem.fr>
2020-03-28 17:28:48 +0100

    ddoyen modified Yggdrasil/model/multistep.pl

--- model/multistep.pl
+++ model/multistep.pl
@@ -3,87 +3,13 @@
 @ /utils/components/radio.py [customradio.py]
 @ /utils/components/checkbox.py [customcheckbox.py]
 @ /utils/components/textselect.py [customtextselect.py]
+extends = temp.pl
+
+@ /utils/sandboxio.py
 @ /builder/beforestep.py [builder.py]
 @ /grader/evaluatorstep.py [grader.py]
 
-extracss ==
-<style>
-.icon-check-after p::after {
-    font-family: "Font Awesome 5 Free";
-    color: #155724;
-    margin-left: 1em;
-    content: "\f00c";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-.icon-times-after p::after {
-    font-family: "Font Awesome 5 Free";
-    color: #721c24;
-    margin-left: 1em;
-    content: "\f00d";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-.icon-check-before p::before {
-    font-family: "Font Awesome 5 Free";
-    color: #155724;
-    margin-right: 1em;
-    content: "\f00c";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-.icon-times-before p::before {
-    font-family: "Font Awesome 5 Free";
-    color: #721c24;
-    margin-right: 1em;
-    content: "\f00d";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-
-
-.error-text-unit {
-    color: #721c24;
-    text-decoration: line-through red;
-}
-
-.success-text-unit {
-    color: #155724;
-    text-decoration: underline green;
-}
-</style>
-==
-
-extrajs ==
-<script>
-    function onReadyPL(nodes) {
-        const actions = nodes.actions;
-        actions.find('.action-save').hide();
-        actions.find('.action-reset').hide();
-        actions.find('.action-next').hide();
-
-        const { origin, pathname }  = document.location;
-        const link = origin + pathname;
-
-        {% if settings.reroll %}
-        const buttons = actions.find('.btn-group');
-        buttons.append(`
-            <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
-                <i class="fas fa-dice"></i> Nouveau tirage
-            </a>
-        `);
-        {% endif %}
-        {% if not settings.submit %}
-        actions.find('.action-submit').hide();
-        {% endif %}
-    }
-</script>
-==
-
 text =
 form =
+
 
