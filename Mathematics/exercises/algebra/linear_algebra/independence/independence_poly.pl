8983da3677eac0b77158308178ad1e8c3ba4a593
David DOYEN <David.Doyen@u-pem.fr>
2020-03-28 22:56:15 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/independence/independence_poly.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/independence/independence_poly.pl
@@ -0,0 +1,41 @@
+extends = /model/mathmultistep.pl
+
+title = Familles libres dans les polynômes
+
+nbstep % 5
+
+before ==
+from customradio import CustomRadio
+
+dim = []
+family = []
+radios = []
+
+for i in range(nbstep):
+    case = rd.randint(0,1)
+    if case == 0:
+        n, m, r, k = rd.choice([(2, 4, 2, 2), (2, 3, 2, 2), (3, 4, 3, 1), (3, 3, 3, 1)])    
+    else:
+        n, m, r, k = rd.choice([(2, 4, 1, 2), (2, 3, 1, 2), (3, 4, 2, 1), (3, 3, 2, 1)])
+    A = rand_int_matrix_givenrank(n, m, r, magnitude=k)
+    radios.append(CustomRadio())
+    radios[i].setitems(["Oui","Non"])
+    radios[i].setsol_from_index(case)
+    dim.append(m)
+    family.append(', '.join([str(tuple(A.row(i))) for i in range(n)]))
+==
+
+textstep ==
+**Question {{step + 1}}.** On se place dans le $!\mathbb{R}!$-espace vectoriel $! \mathbb{R}^{{dim[step]}} !$. La famille de vecteurs suivante est-elle libre ?
+
+$$ \mathcal{F} = \\{ {{ family[step] }} \\}.$$
+==
+
+formstep ==
+{{ radios[step]|component }}
+==
+
+evaluatorstep ==
+score = radios[step].eval()
+==
+
