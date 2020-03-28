1108cb213ef62237db705c0045eedb09b51e3a48
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-03-28 15:17:30 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/recursif/pussancesuite.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/recursif/pussancesuite.pl
@@ -0,0 +1,62 @@
+
+# author zip
+# Heritage d'un type d'exercice 
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+@ rectest.py
+
+title =Puissances nombre d'appels 
+text==
+En transformant la fonction `puissance(a,n)` de l'exercice précédent,
+écrire une fonction recursive nbpuissance(a,n) qui renvoie un couple formé de 
+la valeur $%a^n%$ et du nombre d'appels récursifs.
+En utilisant la définition <br>
+$%a^0=1%$<br>
+$%a^n=(a^\frac{n}{2})^2%$ si n est pair<br>
+$%a^n=a*(a^\frac{n}{2})^2%$ si n est impair<br>
+ecrire une fonction recursive puissancer(a,n) qui renvoie la valeur $%a^n  \binom{1 }{7}%$
+==
+
+before==
+ecrire une fonction recursive puissancer(a,n) qui renvoie la valeur $%a^n  \binom{1 }{7}%$
+==
+
+before==
+from random import *
+
+
+def nbpuissance(a,n):
+    nb=0    
+    if n:
+        (p,rec)=nbpuissance(a,n-1)
+        p=p*a
+        nb=1+rec
+        return (p,nb)
+    return (1,0)
+
+def puissancer(a,n):
+    if n==0:
+        return 1
+    p=puissancer(a,n//2)
+    p*=p
+    if n%2==1:
+        p*=a
+    return p
+
+n=randint(4,12)
+a=randint(-5,10)
+ff=puissance(a,n)
+
+
+after=before
+==
+
+
+
+pltest0==
+
+>>> nbpuissance(2,4)
+(16,4)
+==
+
