141496655d6b86be0a34fd0fa5226b14060fcd14
Benjamin CELARIES <Benjamin.Celaries@univ-mlv.fr>
2020-03-29 21:37:25 +0200

    bcelaries modified Yggdrasil/Mathematics/Benjamin/QCM_continuite.pl

--- Mathematics/Benjamin/QCM_continuite.pl
+++ Mathematics/Benjamin/QCM_continuite.pl
@@ -8,50 +8,29 @@ extends=/model/AMC_TXT.pl
 title=QCM : Fonctions continues
 questions==
 
+*Soit $% f ~:~[a;b] \rightarrow \mathbb{R}%$ une fonction continue. Quel énoncé mathématique correspond à la conclusion du théorème de Heine ?
++ $% \exists c \in [a;b] \text{ tel que } f(c) = \sup\limits_{t \in [a;b]} f(t) %$
+- $% \exists c \in [f(a);f(b)] \text{ tel que } f(c) = \sup\limits_{t \in [a;b]} f(t) %$
+- $% \exists c \in [f(a);f(b)] \text{ tel que } c = \sup\limits_{t \in [a;b]} f(t) %$
+- $% \exists c \in \mathbb{R} \text{ tel que } f(c) = \sup\limits_{t \in [a;b]} f(t) %$
+- $% \forall c \in [a;b], ~f(c) = \sup\limits_{t \in [a;b]} f(t) %$
+- $% \forall t \in [a;b], ~f(c) = \sup\limits_{t \in [a;b]} f(t) %$
+
+
+
 *Soit $% f ~:~[a;b] \rightarrow \mathbb{R}%$ une fonction continue. Quel énoncé mathématique correspond à la conclusion du théorème de Bolzano ?
 + $% \left( f(a)f(b) < 0 \right) \Rightarrow \left( \exists c \in ]a;b[ \text{ tel que } f(c) = 0 \right)%$
 -$% \left( f(a)f(b) < 0 \right) \Rightarrow \left( \forall c \in ]a;b[, ~f(c) = 0 \right)%$
 -$% \left( \forall c \in ]a;b[, ~f(c) = 0 \right) \Rightarrow  \left( f(a)f(b) < 0 \right)%$
 -$% \left( \exists c \in ]a;b[, ~f(c) = 0 \right) \Rightarrow  \left( f(a)f(b) < 0 \right)%$
 
-*Soit $% f ~:~\mathbb{R} \rightarrow \mathbb{R}%$ une fonction continue. On suppose que $%f(0) = f(2) = f(4) = \cdots = f(2020) = 42%$ et que $%f(1) = f(3) = f(5) = \cdots = f(2019)%$. Que peut-on dire des zéros de $%f%$ ?
+*Soit $% f ~:~\mathbb{R} \rightarrow \mathbb{R}%$ une fonction continue. On suppose que $%f(0) = f(2) = f(4) = \cdots = f(2020) = 42%$ et que $%f(1) = f(3) = f(5) = \cdots = f(2019) =-42%$. Que peut-on dire des zéros de $%f%$ ?
 + $%f%$ admet au moins 2020 zéros.
 - $%f%$ admet au plus 2020 zéros.
 - $%f%$ admet exactement 2020 zéros.
 - On ne peut rien dire sur les zéros de $%f%$.
 
-* 12 $% 2^n %$ Ceci est une question 
-- ceci n'est pas une bonne réponse 
-+ ça c'est une bonne réponse 
-- encore une mauvaise réponse
 
-** Comment décomposer un entier en base 2 ?
-+ On fait des divisions successives par deux, puis on lit en ordre inverse les restes obtenus.
-- On utilise l'algorithme d'Euclide.
-- Jamais personne n'a réussi à décomposer un nombre en base 2.
-- On multiplie par $% 2^n %$ l'entier pour tout $% n %$.
-- On utilise $% log_2 %$ la fonction logarithme à base 2.
-- Il faut utiliser la règle du complément à 1.
-
-* 66 Ceci est une question 
-- ceci n'est pas une bonne réponse 
-+ ça c'est une bonne réponse 
-
-
-* 33 Ceci est une question 
-+ et ça c'est une bonne réponse 
-- ce.  ci n'est pas une bonne réponse 
-
-
-* Quel code numérique correspond aux droits rwxr-xr-- ?
-- 721
-- 761
-- 764
-- 713
-- 731
-- 651
-- 741
-+ 754
 
 ==
 
