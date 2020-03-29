70c916b7094532be33ee5602c23267b466815ac2
Benjamin CELARIES <Benjamin.Celaries@univ-mlv.fr>
2020-03-29 22:01:25 +0200

    bcelaries modified Yggdrasil/Mathematics/Benjamin/QCM_continuite.pl

--- Mathematics/Benjamin/QCM_continuite.pl
+++ Mathematics/Benjamin/QCM_continuite.pl
@@ -8,7 +8,7 @@ extends=/model/AMC_TXT.pl
 questions==
 
 
-*Soit $%f~:~[0;1] \rightarrow [0;1]$ une fonction continue. Que peut-on dire sur les points fixes de $%f%$ ? Indication : on pourra étudier la fonction $% g ~:~ x \mapsto f(x) - x %$.
+*Soit $%f~:~[0;1] \rightarrow [0;1]%$ une fonction continue. Que peut-on dire sur les points fixes de $%f%$ ? Indication : on pourra étudier la fonction $% g ~:~ x \mapsto f(x) - x %$.
 + $%f%$ admet au moins un point fixe.
 - $%f%$ admet au plus un point fixe.
 - $%f%$ n'admet pas de point fixe.
