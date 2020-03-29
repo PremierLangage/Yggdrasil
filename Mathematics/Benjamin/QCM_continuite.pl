93fccc34301adc10c94728dcfda9f85f4d007b22
Benjamin CELARIES <Benjamin.Celaries@univ-mlv.fr>
2020-03-29 21:34:06 +0200

    bcelaries modified Yggdrasil/Mathematics/Benjamin/QCM_continuite.pl

--- Mathematics/Benjamin/QCM_continuite.pl
+++ Mathematics/Benjamin/QCM_continuite.pl
@@ -14,7 +14,11 @@ questions==
 -$% \left( \forall c \in ]a;b[, ~f(c) = 0 \right) \Rightarrow  \left( f(a)f(b) < 0 \right)%$
 -$% \left( \exists c \in ]a;b[, ~f(c) = 0 \right) \Rightarrow  \left( f(a)f(b) < 0 \right)%$
 
-
+*Soit $% f ~:~\mathbb{R} \rightarrow \mathbb{R}%$ une fonction continue. On suppose que $%f(0) = f(2) = f(4) = \cdots = f(2020) = 42%$ et que $%f(1) = f(3) = f(5) = \cdots = f(2019)%$. Que peut-on dire des zéros de $%f%$ ?
++ $%f%$ admet au moins 2020 zéros.
+- $%f%$ admet au plus 2020 zéros.
+- $%f%$ admet exactement 2020 zéros.
+- On ne peut rien dire sur les zéros de $%f%$.
 
 * 12 $% 2^n %$ Ceci est une question 
 - ceci n'est pas une bonne réponse 
