f132e00471ac36d69bb333cc2f572198f618b54d
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-03-30 12:39:46 +0200

    mcisse modified Yggdrasil/demo/jsxgraph/demo_jsxgraph_size.pl

--- demo/jsxgraph/demo_jsxgraph_size.pl
+++ demo/jsxgraph/demo_jsxgraph_size.pl
@@ -28,6 +28,16 @@ evaluator ==
 
 ==
 
-
+extracss ==
+<style>
+.myclass {
+    max-width: 200px !important;
+    width: 90vw  !important;
+    max-height: 200px !important;
+    height: 90vw !important;
+    margin: 0 auto !important;
+}
+</style>
+==
 
 
