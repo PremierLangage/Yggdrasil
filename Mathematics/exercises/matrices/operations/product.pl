ae77c6440dcb82ead7b8f91045d1cefc22817aeb
David DOYEN <David.Doyen@u-pem.fr>
2020-03-30 21:11:38 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/matrices/operations/product.pl

--- Mathematics/exercises/matrices/operations/product.pl
+++ Mathematics/exercises/matrices/operations/product.pl
@@ -30,7 +30,7 @@ extracss ==
 <style>
 .matrix-input {
     border : none;
-    border-radius:2px;
+    border-radius:2px !important;
     text-align: center;
     font-style: normal !important;
     font-size: 12pt;
