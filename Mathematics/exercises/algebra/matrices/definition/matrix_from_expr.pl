2e5252dd42bf3b2cf8104c4bcd28bd3885af19ae
David DOYEN <David.Doyen@u-pem.fr>
2020-03-31 15:31:20 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl

--- Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
+++ Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
@@ -1,5 +1,9 @@
 extends = /model/mathmatrix.pl
 
 text ==
-
+Soit $!A!$ la matrice de taille $!\n \times \p!$ dont les
+coefficients sont définis par l'expression $! a_{i,j} = {{expr.tex}} !$.
+Déterminer les coefficients de la matrice $!A!$.
 ==
+
+
