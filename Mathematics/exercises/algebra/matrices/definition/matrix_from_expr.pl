5804b7c61f9f4d5f57661200e24c640244b02cad
David DOYEN <David.Doyen@u-pem.fr>
2020-03-31 18:49:21 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl

--- Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
+++ Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
@@ -4,13 +4,16 @@ title = Matrice définie par l'expression des coefficients
 
 before ==
 var('i j')
-expr = i + j
-sol = Matrix(2, 3, lambda i,j: expr)
+def f(x, y):
+    return x + y
+
+expr = f(i, j)
+sol = Matrix(2, 3, f )
 ==
 
 text ==
 Soit $!A!$ la matrice de taille $!\n \times \p!$ dont les
-coefficients sont définis par l'expression $! a_{i,j} = {{expr.tex}} !$.
+coefficients sont définis par l'expression $! a_{i,j} = {{expr.latex}} !$.
 Déterminer les coefficients de la matrice $!A!$.
 ==
 
