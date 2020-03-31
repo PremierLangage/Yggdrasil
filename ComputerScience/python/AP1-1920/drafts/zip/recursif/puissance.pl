33c67d3dacbbd43be85037e398e6604ec6ffd1de
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-03-31 11:54:12 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/recursif/puissance.pl

--- ComputerScience/python/AP1-1920/drafts/zip/recursif/puissance.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/recursif/puissance.pl
@@ -8,14 +8,19 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 
 title =Puissances
 text==
-En utilisant la définition $%a^n=a \times a ldots \times a] nfacteurs%$ et $%a^0=1%$
-écrire une fonction recursive puissance(a,n) qui renvoie la valeur $%a^n%$
+En utilisant la définition $%a^n=a \times a ldots \times a, nfacteurs%$ et $%a^0=1%$
+écrire une fonction recursive ``puissance(a,n)`` qui renvoie la valeur $%a^n%$<br>
 En utilisant la définition <br>
 $%a^0=1%$<br>
 $%a^n=(a^\frac{n}{2})^2%$ si n est pair<br>
 $%a^n=a*(a^\frac{n}{2})^2%$ si n est impair<br>
-ecrire une fonction recursive puissancer(a,n) qui renvoie la valeur $%a^n  \binom{1 }{7}%$
-==
+écrire une fonction recursive puissancer(a,n) qui renvoie la valeur $%a^n  \binom{1 }{7}%$
+En transformant la fonction `puissance(a,n)`,<br>
+écrire une fonction recursive nbpuissance(a,n) qui renvoie un couple formé de 
+la valeur $%a^n%$ et du nombre d'appels récursifs.
+En transformant la fonction `puissancer(a,n)`,<br>
+écrire une fonction recursive nbpuissancer(a,n) qui renvoie un couple formé de 
+la valeur $%a^n%$ et du nombre d'appels récursifs.==
 
 before==
 from random import *
@@ -34,6 +39,26 @@ def puissancer(a,n):
     if n%2==1:
         p*=a
     return p
+
+
+def nbpuissance(a,n):
+    nb=0    
+    if n:
+        (p,rec)=nbpuissance(a,n-1)
+        p=p*a
+        nb=1+rec
+        return (p,nb)
+    return (1,0)
+
+def nbpuissancer(a,n):
+    if n==0:
+        return (1,0)
+    (p,r)=nbpuissancer(a,n//2)
+    p*=p
+    if n%2==1:
+        p*=a
+    return (p,r+1)
+    
 
 n=randint(4,12)
 a=randint(-5,10)
@@ -75,5 +100,6 @@ pltest3==
 >>> puissance(2,15)
 32768
 ==
+
 
 
