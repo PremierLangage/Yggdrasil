34ad702bb41c46e67363a1effcaea8fc101ac8ce
David DOYEN <David.Doyen@u-pem.fr>
2020-03-31 18:51:13 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl

--- Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
+++ Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
@@ -8,11 +8,13 @@ def f(x, y):
     return x + y
 
 expr = f(i, j)
-sol = Matrix(2, 3, f )
+n = 3
+p = 2
+sol = Matrix(n, p, f )
 ==
 
 text ==
-Soit $!A!$ la matrice de taille $!\n \times \p!$ dont les
+Soit $!A!$ la matrice de taille $!{{n}} \times {{p}}!$ dont les
 coefficients sont définis par l'expression $! a_{i,j} = {{expr.latex}} !$.
 Déterminer les coefficients de la matrice $!A!$.
 ==
