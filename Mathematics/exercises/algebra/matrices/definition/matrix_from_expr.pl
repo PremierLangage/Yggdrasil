2fd4a7a241840e92540cf3c633e11f32cdba3135
David DOYEN <David.Doyen@u-pem.fr>
2020-03-31 16:26:23 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl

--- Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
+++ Mathematics/exercises/algebra/matrices/definition/matrix_from_expr.pl
@@ -1,5 +1,9 @@
 extends = /model/mathmatrix.pl
 
+before ==
+Matrix(2, 3, lambda i,j:  i+j)
+==
+
 text ==
 Soit $!A!$ la matrice de taille $!\n \times \p!$ dont les
 coefficients sont définis par l'expression $! a_{i,j} = {{expr.tex}} !$.
