bf8d8734211f020a155f850a091a3bc248f73bd9
David DOYEN <David.Doyen@u-pem.fr>
2020-03-31 15:01:04 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl

---
+++ Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl
@@ -0,0 +1,17 @@
+title = Relation de dépendance entre polynômes
+
+before ==
+keyboards_JSON['virtualKeyboards']="elementary"
+input.config = keyboards_JSON
+
+==
+
+text ==
+
+==
+
+
+evaluator ==
+
+==
+
