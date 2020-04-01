c72f8ac2d9157daf46f11f413fec640569b759f9
etienne sandier <sandier@u-pec.fr>
2020-04-01 16:11:08 +0200

    esandier modified Yggdrasil/utils/components/dragdrop_from_pywims.pl

--- utils/components/dragdrop_from_pywims.pl
+++ utils/components/dragdrop_from_pywims.pl
@@ -7,15 +7,15 @@ title = Drag Drop mathématique
 before== #|python|
 from customdragdrop import CustomDragDrop, DragDropGroup
 
-# contenu des labels'
+# contenu des labels
 integrale = '$$\int_0^1 x^2\,dx$$'
-fraction =  '$%\frac12 - \frac13%$'
-limite = '$%\lim_{x\to +\infty} x\sin(1/x)%$'
-determinant = '$%\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}%$'
+fraction =  '$$\frac12 - \frac13$$'
+limite = '$$\lim_{x\to +\infty} x\sin(1/x)$$'
+determinant = '$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$'
 
 # contenu des boutons 'drop'
 qui_vaut_1 = 'Poser ici une expression égale à 1'
-qui_vaut_tiers = 'Poser ici une expression égale à $%1/3%$'
+qui_vaut_tiers = 'Poser ici une expression égale à $$1/3$$'
 
 # fabrication du groupe
 mygroup = DragDropGroup()
@@ -35,10 +35,12 @@ rlimite = mygroup.labels['limite']
 rfraction = mygroup.labels['fraction']
 rdeterminant = mygroup.labels['determinant']
 match = mygroup._matches
+debug = str(q1)
 ==
 
 text==
 Question : 
+{{debug}}
 ==
 
 form==
