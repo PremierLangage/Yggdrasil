0b247bb588d250ea0e6719421f91ac69be2b01a1
David DOYEN <David.Doyen@u-pem.fr>
2020-04-01 15:47:41 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl

--- Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl
+++ Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl
@@ -16,7 +16,7 @@ for k in range(nbstep):
     m = randint(4, 6)
     i = randint(1, n)
     j = randint(1, m)
-    #matrices.append(rand_int_matrix(n, m, 9))
+    matrices.append(rand_int_matrix(n, m, 9))
     #sol.append(matrices[k][i][j])
     coeff.append(r"a_{ %s , %s}" % (i,j))
 ==
