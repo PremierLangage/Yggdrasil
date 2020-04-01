4ad0576e17f279cd517d99b4fabf21f3d84a974c
etienne sandier <sandier@u-pec.fr>
2020-04-01 16:55:59 +0200

    esandier modified Yggdrasil/utils/components/dragdrop_from_pywims.pl

--- utils/components/dragdrop_from_pywims.pl
+++ utils/components/dragdrop_from_pywims.pl
@@ -8,10 +8,11 @@ before== #|python|
 from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
 
 # contenu des labels
-integrale = r"$$\int_0^1 x^2\, dx$$"
+integrale = r"$$\int_0^1 x^2\, dx$$" # bug d'affichage: \, affiche une virgule
 fraction =  r"$$\frac12 - \frac13$$"
 limite = r"$$\lim_{x\to +\infty} x\sin(1/x)$$"
-determinant = r"$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$"
+determinant = r"$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$" # bug d'affichage: la matrice s'affiche sur une lintegrale
+
 
 # contenu des boutons "drop"
 qui_vaut_1 = r"Poser ici une expression égale à $%1%$"
@@ -28,7 +29,7 @@ mygroup.set_match_by_name("tiers", "integrale")
 mygroup.set_match_by_name("1", ["limite","determinant"])
 
 # Ce qui suit sert uniquement à faire voyager le groupe dans le grader
-a = mygroup.drops['1']
+q1 = mygroup.drops['1']
 b = mygroup.drops["tiers"]
 c = mygroup.labels["integrale"]
 d = mygroup.labels["limite"]
@@ -39,7 +40,7 @@ g = mygroup._matches
 
 text==
 
-Question : {{ a | component }} {{ b | component }}
+Question : {{ q1 | component }} {{ b | component }}
 ==
 
 form==
