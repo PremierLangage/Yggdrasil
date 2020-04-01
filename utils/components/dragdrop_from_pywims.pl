130963635312f496d2af6b6ece75525dc2435f8c
etienne sandier <sandier@u-pec.fr>
2020-04-01 16:19:50 +0200

    esandier modified Yggdrasil/utils/components/dragdrop_from_pywims.pl

--- utils/components/dragdrop_from_pywims.pl
+++ utils/components/dragdrop_from_pywims.pl
@@ -5,35 +5,35 @@ extends = /model/basic.pl
 title = Drag Drop mathématique
 
 before== #|python|
-from customdragdrop import CustomDragDrop, DragDropGroup
+from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
 
 # contenu des labels
-integrale = '$$\int_0^1 x^2\,dx$$'
-fraction =  '$$\frac12 - \frac13$$'
-limite = '$$\lim_{x\to +\infty} x\sin(1/x)$$'
-determinant = '$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$'
+integrale = "$$\int_0^1 x^2\,dx$$"
+fraction =  "$$\frac12 - \frac13$$"
+limite = "$$\lim_{x\to +\infty} x\sin(1/x)$$"
+determinant = "$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$"
 
-# contenu des boutons 'drop'
-qui_vaut_1 = 'Poser ici une expression égale à 1'
-qui_vaut_tiers = 'Poser ici une expression égale à $$1/3$$'
+# contenu des boutons "drop"
+qui_vaut_1 = "Poser ici une expression égale à 1"
+qui_vaut_tiers = "Poser ici une expression égale à $$1/3$$"
 
 # fabrication du groupe
 mygroup = DragDropGroup()
 # les labels
-mygroup.set_label({'integrale': integrale, 'fraction': fraction, 'limite':limite, 'determinant': determinant})
+mygroup.set_label({"integrale": integrale, "fraction": fraction, "limite":limite, "determinant": determinant})
 # les drops
-mygroup.set_drop({'1' : qui_vaut_1, 'tiers': qui_vaut_tiers})
+mygroup.set_drop({"1" : qui_vaut_1, "tiers": qui_vaut_tiers})
 # les liens corrects
-mygroup.set_match_by_name('tiers', 'integrale')
-mygroup.set_match_by_name('1', ['limite','determinant'])
+mygroup.set_match_by_name("tiers", "integrale")
+mygroup.set_match_by_name("1", ["limite","determinant"])
 
 # Ce qui suit sert uniquement à faire voyager le groupe dans le grader
-q1 = mygroup.drops['1']
-qtiers = mygroup.drops['tiers']
-rintegrale = mygroup.labels['integrale']
-rlimite = mygroup.labels['limite']
-rfraction = mygroup.labels['fraction']
-rdeterminant = mygroup.labels['determinant']
+q1 = mygroup.drops["1"]
+qtiers = mygroup.drops["tiers"]
+rintegrale = mygroup.labels["integrale"]
+rlimite = mygroup.labels["limite"]
+rfraction = mygroup.labels["fraction"]
+rdeterminant = mygroup.labels["determinant"]
 match = mygroup._matches
 debug = str(q1)
 ==
@@ -55,8 +55,8 @@ settings.feedback = lightscore
 evaluator==
 from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
 mygroup = DragDropGroup()
-mygroup.labels = {'lintegrale': rintegrale, 'limite': rlimite, 'rfraction': rfraction, 'determinant': rdeterminant}
-mygroup.drops = {'1': q1, 'tiers': qtiers}
+mygroup.labels = {"lintegrale": rintegrale, "limite": rlimite, "rfraction": rfraction, "determinant": rdeterminant}
+mygroup.drops = {"1": q1, "tiers": qtiers}
 mygroup._matches = match
 
 grade=mygroup.eval(grading_function = right_minus_wrong) 
