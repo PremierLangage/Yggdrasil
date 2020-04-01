4a2e3af42bb7451f6b2ae7bc19237cfb5f92111c
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-04-01 09:19:34 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/recursif/cantor.pl

--- ComputerScience/python/AP1-1920/drafts/zip/recursif/cantor.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/recursif/cantor.pl
@@ -7,12 +7,12 @@ title = Numérotation du plan
 
 text==
 
-On numérote les points du quart de plan $%\mathbbN \times %$ par les entiers naturels suivant l'ordre des diagonales montantes:<br>
-  <br>
-9   <br>
-5 8      <br>
-2 4 7 11    <br>
-0 1 3 6 10  <br> 
+On numérote les points du quart de plan $%N \times N %$ par les entiers naturels suivant l'ordre des diagonales montantes:<br>
+. <br>
+9 .  <br>
+5 8 . .     <br>
+2 4 7 11 .   <br>
+0 1 3 6 10 . <br> 
 Ecrire une fonction récursive qui reçoit les coordonnées d'un point sous forne de  deux entiers positifs
 et renvoie le numéro associé.
 (aide : distinguez les cas (0,y),(x,0) et (x,y) avec x non nul et y non nul
