224cc25a79782827d242bd52fbbb350006f3b0cc
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-04-01 09:36:56 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/recursif/cantor.pl

--- ComputerScience/python/AP1-1920/drafts/zip/recursif/cantor.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/recursif/cantor.pl
@@ -2,15 +2,18 @@
 
 # Heritage d'un type d'exercice 
 extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+@ rectest.py
 
 title = Numérotation du plan
 
 text==
 
 On numérote les points du quart de plan $%N \times N %$ par les entiers naturels suivant l'ordre des diagonales montantes:<br>
-. <br>
-9 .  <br>
-5 8 . .     <br>
+. . .<br>
+9 . . . <br>
+5 8 . . .    <br>
 2 4 7 11 .   <br>
 0 1 3 6 10 . <br> 
 Ecrire une fonction récursive qui reçoit les coordonnées d'un point sous forne de  deux entiers positifs
@@ -18,4 +21,27 @@ et renvoie le numéro associé.
 (aide : distinguez les cas (0,y),(x,0) et (x,y) avec x non nul et y non nul
 ==
 
+before==
+def numero(x,y):
+    if x==0:
+        if y==0:
+           return 0
+        else:
+            return numero(1,y-1)+1
+    if y==0:
+        return numero(0,x-1)+1
+    else:
+        return numero(x+1,y-1)+1
 
+from random import *
+x=randint(4,12)
+y=randint(5,20)
+ff=numero(x,y)
+      
+after=before
+==
+pltest1==
+
+>>> numero(2,4)
+25
+==
