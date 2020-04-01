4d7f4f3904bb073a88ed909128a836be06d5ef39
etienne sandier <sandier@u-pec.fr>
2020-04-01 15:48:10 +0200

    esandier modified Yggdrasil/utils/components/dragdrop_from_pywims.pl

--- utils/components/dragdrop_from_pywims.pl
+++ utils/components/dragdrop_from_pywims.pl
@@ -8,14 +8,14 @@ before== #|python|
 from customdragdrop import CustomDragDrop, DragDropGroup
 
 # contenu des labels'
-integrale = r'$$\int_0^1 x^2\,dx$$'
-fraction =  r'$$\frac12 - \frac13$$'
-limite = r'$$\lim_{x\to +\infty} x\sin(1/x)$$'
-determinant = r'$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$'
+integrale = r'$%\int_0^1 x^2\,dx%$'
+fraction =  r'$%\frac12 - \frac13%$'
+limite = r'$%\lim_{x\to +\infty} x\sin(1/x)%$'
+determinant = r'$%\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}%$'
 
 # contenu des boutons 'drop'
 qui_vaut_1 = r'Poser ici une expression égale à 1'
-qui_vaut_tiers = r'Poser ici une expression égale à $1/3$'
+qui_vaut_tiers = r'Poser ici une expression égale à $%1/3%$'
 
 # fabrication du groupe
 mygroup = DragDropGroup()
@@ -38,10 +38,14 @@ match = mygroup._matches
 ==
 
 text==#|HTML|
-Question :
+Question :{{ q1 | component}} {{ qtiers | component}}
 ==
 
 form==
+{{ rintegrale | component }}
+{{ rlimite | component }}
+{{ rfraction | component }}
+{{ rdeterminant | component }}
 ==
 
 settings.feedback = lightscore
