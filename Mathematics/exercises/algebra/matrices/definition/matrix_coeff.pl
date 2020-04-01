039a3b4b1a04eaeab2d4400a8580105a1d538e9a
David DOYEN <David.Doyen@u-pem.fr>
2020-04-01 15:45:31 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl

--- Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl
+++ Mathematics/exercises/algebra/matrices/definition/matrix_coeff.pl
@@ -5,20 +5,27 @@ title = Relation de dépendance entre polynômes
 nbstep % 3
 
 before ==
-for i in range(nbstep):
+coeff =[]
+matrices = []
+inputs = []
+sol = []
+for k in range(nbstep):
 
     inputs.append(Input())
-    inputs[i].type = "number"
+    inputs[k].type = "number"
     n = randint(4, 6)
     m = randint(4, 6)
-    i.append(randint(1, n))
-    j.append(randint(1, m))
+    i = randint(1, n))
+    j = randint(1, m))
     matrices.append(rand_int_matrix(n, m, 9))
+    sol.append(matrices[k][i][j])
+    coeff.append(r"a_{ %s , %s}" % (i,j))
 ==
 
 textstep ==
 **Question 1**. On considère la matrice.
 $$ A = \left( {{matrices[step]}}= \right).$$
+Que vaut {{ a_{ {{i[step], {{j[step]}}
 ==
 
 formstep ==
