23a936977a0e7d419ca35f7a385058bfe074d781
David DOYEN <David.Doyen@u-pem.fr>
2020-04-04 23:05:51 +0200

    ddoyen modified Yggdrasil/utils/sympy/evalsympy.py

--- utils/sympy/evalsympy.py
+++ utils/sympy/evalsympy.py
@@ -1,11 +1,11 @@
 import sympy as sp
 from latex2sympy import *
 
-# Equality tests
+# Utils
 
 def equal(a, b, modulo=None):
     """
-    Check if two SymPy expressions are equal.
+    Check if two expressions are equal after simplification.
     """
     if a == b:
         return True
@@ -21,7 +21,7 @@ def equal(a, b, modulo=None):
 
 def equal_approx(a, b, tol, modulo=None):
     """
-    Check if two SymPy expressions are equal.
+    Check if two expressions are approximately equal.
     """
     if modulo is None:
         return abs(a - b).evalf() < tol
@@ -30,100 +30,101 @@ def equal_approx(a, b, tol, modulo=None):
 
 def equal_struct(a, b, modulo=None):
     """
-    Check if two structures are equal.
+    Check if two nested structures of lists and tuples of expressions 
+    (where lists are viewed as sets) are equal.
     """
-    if isinstance(b, list):
-        if isinstance (a, list):
-            return equal_set(a, b)
-    elif isinstance(b, tuple):
-        if isinstance (a, tuple):
-            return equal_tuple(a, b)
-    elif isinstance(b, (sp.Expr, int)):
-        if isinstance (a, (sp.Expr, int)):
-            return equal(a, b, modulo)
-    return False
-
-def equal_set(p, q):
-    """
-    Check if two sets (stored as lists) are equal.
-    """
-    if len(p) != len(q):
-        return False
-    for a in p:
-        isin = False
-        for b in q:
-            if equal_struct(a, b):
-                isin = True
-                break
-        if not isin:
+    
+    # comparison of two lists (viewed as sets)
+    if isinstance(p, list) and isinstance (q, list):
+        if len(p) != len(q):
             return False
-    return True
-
-def equal_tuple(p,q):
-    """
-    Check if two tuples are equal.
-    """
-    if len(p)!=len(q):
-        return False
-    for i in range(len(p)):
-        if not equal_struct(p[i],q[i]):
+        for a in p:
+            isin = False
+            for b in q:
+                if equal_struct(a, b, modulo):
+                    isin = True
+                    break
+            if not isin:
+                return False
+        return True
+     
+    # comparison of two tuples       
+    elif isinstance(p, tuple) and isinstance (q, tuple):
+        if len(p) != len(q):
             return False
-    return True
+        for i in range(len(p)):
+            if not equal_struct(p[i], q[i], modulo):
+                return False
+        return True
+    
+    # comparison of two expressions        
+    elif isinstance(p, (sp.Expr, int)) and isinstance (q, (sp.Expr, int)):
+        return equal(p, q, modulo)
+    
+    # other cases
+    else:    
+        return False
 
-def duplicates(struct):
-    if isinstance(struct,list):
-        if len(struct)>1:
-            for i in range(len(struct)):
-                for j in range(i+1,len(struct)):
-                    if equal_struct(struct[i],struct[j]):
+
+def duplicates(p):
+    """
+    Check if there are duplicates in lists inside a nested structure of
+    lists and tuples of expressions.
+    """
+    
+    # search duplicates in lists
+    if isinstance(p, list):
+        if len(p) > 1:
+            for a in p:
+                for b in p[1:]:
+                    if equal_struct(a, b):
                         return True
-    if isinstance(struct,(list,tuple)):
-        for a in struct:
+    
+    # search duplicates in elements of lists and tuples                  
+    if isinstance(p, (list, tuple)):
+        for a in p:
             if duplicates(a):
                 return True
+
     return False
 
-################
-# Utils
-################
-
-def arg_add_flatten(expr):
+def arg_nested_add(expr):
     """
-    Flatten a nested sum and return the terms of this sum as a list.
+    Return the terms of a nested sum.
     """
     if not expr.is_Add:
         return [expr]
-    lst=[]
+    args = []
     for a in expr.args:
         if a.is_Add:
-            lst=lst+arg_add_flatten(a)
+            args = args + arg_nested_add(a)
         else:
-            lst.append(a)
-    return lst
+            args.append(a)
+    return args
 
-def arg_mul_flatten(expr):
+def arg_nested_mul(expr):
     """
-    Flatten a nested product and return the factors of this product as a list.
+    Return the terms of a nested product.
     """
     if not expr.is_Mul:
         return [expr]
-    lst=[]
+    args = []
     for a in expr.args:
         if a.is_Mul:
-            lst=lst+arg_mul_flatten(a)
+            args = args + arg_nested_mul(a)
         else:
-            lst.append(a)
-    return lst
+            args.append(a)
+    return args
     
-def is_coeff_mul(expr,x):
+def is_coeff_mul(expr, x):
     """
     Check if an expression is of the form 'something times x'.
     """
-    args=arg_mul_flatten(expr)
-    return args.count(x)==1 and sum([a.has(x) for a in args])==1
+    args = arg_nested_mul(expr)
+    return args.count(x) == 1 and sum([a.has(x) for a in args]) == 1
 
 def coeff_mul(expr,x):
-    args=arg_mul_flatten(expr)
+    args=arg_nested_mul(expr)
     args.remove(x)
     if len(args)==0:
         return 0
@@ -140,30 +141,25 @@ def is_coeff_exponent(expr,x):
     n=sp.degree(expr,x)
     if n==0:
         return True
-    args=arg_mul_flatten(expr)
+    args=arg_nested_mul(expr)
     return args.count(x**n)==1 and sum([a.has(x) for a in args])==1
-
-# Tests
 
 def is_real_or_inf(expr):
     """
-    Check if
+    Check if an expression is real or equal to infinity.
     """
-    if expr==sp.S.Infinity or expr==sp.S.NegativeInfinity:
+    if expr == sp.S.Infinity or expr == sp.S.NegativeInfinity:
         return True
-    return (expr.is_real is True) and (expr.is_infinite is False)
+    return expr.is_real and not expr.is_infinite
 
 def is_rat_simp(expr):
     """
-    Check if the rational numbers in a sympy expressions are simplified.
+    Check if the rational numbers in an expression are simplified.
     """
-    if isinstance(expr,sp.Expr):
-        expr2=sp.simplify(expr)
-        if expr2.is_rational:
+    if isinstance(expr, sp.Expr):
+        if sp.simplify(expr).is_rational:
             return is_frac_int_irred(expr)
-        elif expr.is_Atom:
-            return True
-        elif expr.is_Boolean:
+        elif expr.is_Atom or expr.is_Boolean:
             return True
         else:
             return all(is_rat_simp(subexpr) for subexpr in expr.args)
@@ -174,7 +170,7 @@ def is_frac_int(expr):
     """
     Check if a sympy expression is a fraction of integers.
     """
-    args=arg_mul_flatten(expr)
+    args=arg_nested_mul(expr)
     if len(args)>1 and sp.Integer(-1) in args:
         args.remove(sp.Integer(-1))
     with sp.evaluate(False):
@@ -186,7 +182,7 @@ def is_frac_irred(expr):
     """
     Check if a sympy fraction of integers is irreducible.
     """
-    args=arg_mul_flatten(expr)
+    args=arg_nested_mul(expr)
     if len(args)>1 and sp.Integer(-1) in args:
         args.remove(sp.Integer(-1))
     with sp.evaluate(False):
@@ -200,22 +196,20 @@ def is_frac_int_irred(expr):
     """
     return is_frac_int(expr) and is_frac_irred(expr)
 
-# Complex numbers
-
 def is_complex_cartesian(expr):
     """
     Check if a complex number is in cartesian form.
     """
-    args=arg_add_flatten(expr)
-    ni=sum([is_coeff_mul(a,sp.I) for a in args])
-    nr=sum([a.is_real for a in args])
-    return ni<=1 and ni+nr==len(args)
+    args = arg_nested_add(expr)
+    ni = sum([is_coeff_mul(a, sp.I) for a in args])
+    nr = sum([a.is_real for a in args])
+    return ni <= 1 and ni+nr == len(args)
 
 def complex_cartesian_parts(expr):
     """
     Return the real and imaginary parts.
     """
-    args=arg_add_flatten(expr)
+    args=arg_nested_add(expr)
     im=next(coeff_mul(a,sp.I) for a in args if is_coeff_mul(a,sp.I))
     lstre=[a for a in args if a.is_real]
     if len(lstre)==0:
@@ -226,7 +220,6 @@ def complex_cartesian_parts(expr):
         with sp.evaluate(False):
             re=sp.Add(*lstre)
     return (re,im)
-
 
 def is_complex_cartesian_ratsimp(expr):
     """
@@ -249,7 +242,7 @@ def is_complex_exponential(expr):
     """
     Check if a complex number is in exponential form.
     """
-    args=arg_mul_flatten(expr)
+    args=arg_nested_mul(expr)
     if is_e_i_theta(args[-1]):
         for a in args[:-1]:
             if not a.is_real or not a>0:
@@ -261,14 +254,14 @@ def is_poly_expanded(expr,x):
     """
     Check if a polynomial is in expanded form.
     """
-    args=arg_add_flatten(expr)
+    args=arg_nested_add(expr)
     return all(is_coeff_exponent(a,x) for a in args)
 
 def is_poly_factorized(expr,x,domain):
     """
     Check if a polynomial is factorized.
     """
-    args=arg_mul_flatten(expr)
+    args=arg_nested_mul(expr)
     for a in args:
         if type(a)==sp.Pow:
             a=a.args[0]
@@ -280,33 +273,114 @@ def only_authorized_func(expr,authorized_func):
     nonauthorized_func=set([type(a) for a in expr.atoms(sp.Function)]).difference(authorized_func)
     return nonauthorized_func==set()
 
-# Answer analysis
 
-
-#################
-# Expression
-#################
+# Answer evaluation functions
 
 _feedback_ = {
-"NotExpr": "Votre réponse n'est pas une expression valide.",
-
+"Success": "",
+"NotEqual": "",
+"NotExpr": "La réponse doit être une expression mathématique.",
+"NotRealOrInf": "La réponse doit être un nombre réel ou +$\infty$ ou $-\infty$.",
+"NotCplx": "La réponse doit être un nombre complexe.",
+"NotCplxCartesian": "La réponse doit être un nombre complexe sous forme cartésienne.",
+"NotCplxExponential": "La réponse doit être un nombre complexe sous forme exponentielle.",
+"NotSet": "La réponse doit être un ensemble.",
+"NotPoly": "La réponse doit être un polynôme.",
+"PolyNotExpanded": "La réponse doit être un polynôme développé.",
+"PolyNotFactorized": "La réponse doit être un polynôme factorisé.",
+"MatWrongSize": "La matrice n'a pas la bonne taille.",
+"NotRatSimp": "L'expression peut encore être simplifiée.",
+"Duplicates": "Il y a des doublons dans l'ensemble.",
+"UnauthorizedFunc": "Votre réponse utilise des fonctions non-autorisées."
 }
 
-def eval_expr(strans, sol, local_dict={}):
+def add_feedback(eval):
+    def eval_with_feedback(*args, **kwargs):
+        score, error = eval(*args, **kwargs)
+        if error in _feedback_:
+            return score, error, _feedback_[error]
+        else:
+            return score, error, ""
+    return eval_with_feedback
+
+@add_feedback
+def eval_expr(strans, sol, checkratsimp=True, local_dict={}):
     """
-    Evaluate an answer when the solution is a mathematical expression.
+    Evaluate an answer when the solution is an expression.
     """
     try:
         ans = latex2sympy(strans, local_dict)
     except:
-        return (-1, "NotExpr", _feedback_["NotExpr"])
+        return (-1, "NotExpr")
     if not isinstance(ans, sp.Expr):
-        return (-1, "NotExpr", _feedback_["NotExpr"])
+        return (-1, "NotExpr")
     if not equal(ans, sol):
-        return (0, "NotEqual", "")
-    #    test2.append((is_rat_simp,-1,"NotRatSimp","L'expression peut encore être simplifiée."))
-    return (100, "Success", "")
+        return (0, "NotEqual")
+    #if checkratsimp and not is_rat_simp(expr):
+    #    return (-1, "NotRatSimp")
+    return (100, "Success")
+    
+@add_feedback
+def eval_real_or_inf(strans, sol, local_dict={}):
+    """
+    Evaluate an answer when the solution is real or equal to infinity.
+    """
+    try:
+        ans = latex2sympy(strans, local_dict)
+    except:
+        return (-1, "NotRealOrInf")
+    if not isinstance(ans, sp.Expr) or not is_real_or_inf(ans):
+        return (-1, "NotRealOrInf")
+    if not equal(ans, sol):
+        return (0, "NotEqual")
+    return (100, "Success")
 
+@add_feedback
+def eval_complex(strans, sol, imaginary_unit="i", form="", authorized_func={}, local_dict={}):
+    """
+    Evaluate an answer when the solution is a complex number.
+    """
+    local_dict.update({imaginary_unit: sp.I, 'e': sp.E})
+    try:
+        ans=latex2sympy(strans,local_dict)
+    except:
+        return (-1,"NotCplx")
+    if not isinstance(ans,sp.Expr) or not ans.is_complex:
+        return (-1, "NotCplx")
+    if not only_authorized_func(ans, authorized_func):
+        return (-1, "UnauthorizedFunc")
+    if not equal(ans, sol):
+        return (0, "NotEqual")
+    if form == "cartesian" and not is_complex_cartesian(ans):
+            return (-1, "NotCplxCartesian")
+    # is_complex_cartesian_ratsimp,-1,"NotRatSimp","L'expression peut encore être simplifiée."
+    if form == "exponential" and not is_complex_exponential(ans):
+            return (-1, "NotCplxExponential")
+    return (100, "Success")
+
+@add_feedback
+def eval_poly(strans, sol, x, domain="RR", imaginary_unit="i", form="", authorized_func={}, local_dict={}):
+    """
+    Evaluate an answer when the solution is a polynomial.
+    """
+    local_dict.update({imaginary_unit: sp.I})
+    try:
+        ans = latex2sympy(strans, local_dict)
+    except:
+        return (-1, "NotPoly")
+    if not isinstance(ans,sp.Expr) or not ans.is_polynomial(x):
+        return (-1, "NotPoly")
+    if not only_authorized_func(ans,authorized_func):
+        return (-1,"UnauthorizedFunc")
+    if not equal(ans,sol):
+        return (0, "NotEqual")
+    if form == "expanded" and not is_poly_expanded(expr,x):
+            return (-1, "PolyNotExpanded")
+    if form == "factorized" and not is_poly_factorized(expr,x,domain):
+            return (-1, "PolyNotFactorized")
+    return (100, "Success")
+
+@add_feedback
 def eval_set(strans, sol, local_dict={}):
     """
     Evaluate an answer when the solution is a finite set.
@@ -315,95 +389,35 @@ def eval_set(strans, sol, local_dict={}):
     try:
         ans = latex2sympy(strans, local_dict)
     except:
-        return (-1,"NotSet","Votre réponse n'est pas un ensemble.")
+        return (-1, "NotSet")
     if not isinstance(ans, list):
-        return (-1,"NotSet","Votre réponse n'est pas un ensemble.")
+        return (-1, "NotSet")
     if duplicates(ans):
-        return (-1,"Duplicates","Il y a des doublons dans l'ensemble.")
-    if not equal_set(ans,sol):
-        return (0,"NotEqual","")
-    return (100,"Success","")
+        return (-1, "Duplicates")
+    if not equal_struct(ans, sol):
+        return (0, "NotEqual")
+    return (100, "Success")
 
+@add_feedback
 def eval_tuple(strans, sol, checksize=False, local_dict={}):
+    """
+    Evaluate an answer when the solution is a tuple/vector.
+    """
     sol = tuple(sol)
     try:
-        ans = latex2sympy(strans,local_dict)
+        ans = latex2sympy(strans, local_dict)
     except:
-        return (-1,"NotTuple","Votre réponse n'est pas valide.")
+        return (-1,"NotTuple")
     if not isinstance(ans,tuple):
-        return (-1,"NotTuple","Votre réponse n'est pas valide.")
+        return (-1,"NotTuple")
     if checksize and len(ans) != len(sol):
-        return (-1,"WrongSize","Votre réponse n'est pas valide.")
-    if not equal_tuple(ans,sol):
-        return (0,"NotEqual","")
-    return (100,"Success","")
-
-# Complex numbers
-
-def eval_complex(strans,sol,imaginary_unit="i",form="",authorized_func={}):
-    """
-    Analyze an answer of type complex number.
-    """
-    local_dict={imaginary_unit:sp.I,'e':sp.E}
-    try:
-        ans=latex2sympy(strans,local_dict)
-    except:
-        return (-1,"NotExpr","Votre réponse n'est pas une expression valide.")
-    if not isinstance(ans,sp.Expr):
-        return (-1,"NotExpr","Votre réponse n'est pas une expression valide.")
-    if not ans.is_complex:
-        return (-1,"NotCplx","Votre réponse n'est pas un nombre complexe.")
-    if not only_authorized_func(ans,authorized_func):
-        return (-1,"UnauthorizedFunc","Votre réponse utilise des fonctions non-autorisées.")
-    if not equal(ans,sol):
-        return (0,"NotEqual","")
-    if form == "cartesian":
-        if not is_complex_cartesian(ans):
-            return (-1,"NotCplxCartesian","Votre réponse n'est pas un nombre complexe sous forme cartésienne.")
-    # is_complex_cartesian_ratsimp,-1,"NotRatSimp","L'expression peut encore être simplifiée."
-    elif form == "exponential":
-        if not is_complex_exponential(ans):
-            return (-1,"NotCplxExponential","Votre réponse n'est pas un nombre complexe sous forme exponentielle.")
-    return (100,"Success","")
+        return (-1, "TupleWrongSize")
+    if not equal_struct(ans,sol):
+        return (0, "NotEqual")
+    return (100, "Success")
 
 
-def ans_struct_complex(strans,sol,typestruct,imaginary_unit="i",form=""):
-    local_dict={imaginary_unit:sp.I}
-    test1=[(is_complex,-1,"NotCplx","Une des expressions n'est pas un nombre complexe.")]
-    if form=="cartesian":
-        test1.append((is_complex_cartesian,-1,"NotCplxCartesian","Un des nombres n'est pas écrit sous forme cartésienne."))
-    test2=[]
-    return ans_struct_(strans,sol,typestruct,local_dict,test1,test2)
-
-# Polynomials
-
-def eval_poly(strans, sol, x, domain="RR", imaginary_unit="i", form="", authorized_func={}):
-    """
-    Evaluate an answer when the solution is a polynomial.
-    """
-    local_dict = {imaginary_unit:sp.I}
-    try:
-        ans = latex2sympy( strans, local_dict)
-    except:
-        return (-1,"NotExpr","Votre réponse n'est pas une expression valide.")
-    if not isinstance(ans,sp.Expr):
-        return (-1,"NotExpr","Votre réponse n'est pas une expression valide.")
-    if not ans.is_polynomial(x):
-        return (-1,"NotPoly","Votre réponse n'est pas un polynôme.")
-    if not only_authorized_func(ans,authorized_func):
-        return (-1,"UnauthorizedFunc","Votre réponse utilise des fonctions non-autorisées.")
-    if not equal(ans,sol):
-        return (0,"NotEqual","")
-    if form == "expanded":
-        if not is_poly_expanded(expr,x):
-            return (-1,"NotExpanded","Votre réponse n'est pas un polynôme développé.")
-    elif form == "factorized":
-        if not is_poly_factorized(expr,x,domain):
-            return (-1,"NotFactorized","Votre réponse n'est pas un polynôme factorisé.")
-    return (100,"Success","")
-
-# Matrices
-
+@add_feedback
 def eval_matrix(matans, sol):
     """
     Evaluate an answer when the solution is a matrix.
@@ -411,27 +425,12 @@ def eval_matrix(matans, sol):
     try:
         ans = sp.Matrix(matans)
     except:
-        return (-1, "NotMat", "Votre réponse n'est pas une matrice valide.")
+        return (-1, "NotMat")
     if ans.shape != sol.shape:
-        return (0, "WrongSize", "La matrice n'a pas la bonne taille.")
+        return (0, "MatWrongSize")
     if not ans.equals(sol):
-        return (0, "NotEqual", "")
-    return (100, "Success", "")
-
-#############
-# Extended reals    
-#############
-
-
-def ans_real_or_inf(strans,sol,local_dict={}):
-    """
-    Analyze an answer of type extended real.
-    """
-    test1=[(is_expr,-1,"NotExpr","Votre réponse n'est pas une expression valide."),
-           (is_real_or_inf,-1,"NotRealOrInf","Votre réponse n'est pas un nombre réel ou +$\infty$ ou $-\infty$.")]
-    test2=[]
-    test2.append((is_rat_simp,-1,"NotRatSimp","L'expression peut encore être simplifiée."))
-    return ans_(strans,sol,local_dict,test1,test2)
+        return (0, "NotEqual")
+    return (100, "Success")
 
 
 def ans_antiderivative(strans,sol,x,local_dict={}):
@@ -443,11 +442,3 @@ def ans_antiderivative(strans,sol,x,local_dict={}):
     test2=[]
     test2.append((is_rat_simp,-1,"NotRatSimp","L'expression peut encore être simplifiée."))
     return ans_eqconstant_(strans,sol,x,local_dict,test1,test2)
-
-
-
-
-
-
-
-
