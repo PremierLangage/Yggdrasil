14e576f9cf90a6bd8e46b9c95a4572683fe10fd1
David DOYEN <David.Doyen@u-pem.fr>
2020-04-05 00:34:18 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/polynomials/expansion.pl

--- Mathematics/exercises/algebra/polynomials/expansion.pl
+++ Mathematics/exercises/algebra/polynomials/expansion.pl
@@ -1,32 +1,30 @@
-extends = /Mathematics/template/mathexpr.pl
+extends = /model/mathinput.pl
 
 title = Développement
 
-lang = fr
-
 before ==
 var('x')
-d=int(param['degree'])
-nc=int(param['ncoeff'])
+d = param['degree']
+nc = param['ncoeff']
 
-P=rand_int_poly(randint(nc-1,d),nc,5,x)
-Q=rand_int_poly(randint(nc-1,d),nc,5,x)
-R=rand_int_poly(randint(nc-1,d),nc,5,x)
+P = rand_int_poly(randint(nc-1,d),nc,5,x)
+Q = rand_int_poly(randint(nc-1,d),nc,5,x)
+R = rand_int_poly(randint(nc-1,d),nc,5,x)
 
 if param['formula']=="PQ+R":
     T1=P
     T2=Q*R
-latexT1=latex(T1)
-latexT2=latex(T2)
-sol=T1
+
+sol=T1 + T2
 ==
 
-text = Développer $$ {{latexT1}} + {{latexT2}}.$$
+text = Développer $$ {{T1.latex}} + {{T2.latex}}.$$
 
 evaluator==
 var('x')
-score,numerror,feedback=eval_poly(answer['1'],sol,x)
+score,numerror,feedback=eval_poly(input.value,sol,x)
 ==
+
 
 
 
