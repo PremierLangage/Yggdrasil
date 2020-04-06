9d474effeb1f88618a05e5c1d559c70f25d73a81
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-06 16:03:29 +0200

    drevuz modified Yggdrasil/ComputerScience/genielogiciel/UML/Acteurs/acteurs.pl

---
+++ ComputerScience/genielogiciel/UML/Acteurs/acteurs.pl
@@ -0,0 +1,25 @@
+
+
+extends=/model/basic.pl
+
+img.acteur=$ acteur-uml.png 
+
+
+title= Some UML
+
+text==
+
+<img src={{img.acteur}} alt='image stérotypique de l\'acteur' style=\"width:400px;\" />
+
+
+
+==
+
+form=
+
+evaluator==
+grade=(100, " merci de votre lecture attentive "
+
+==
+
+
