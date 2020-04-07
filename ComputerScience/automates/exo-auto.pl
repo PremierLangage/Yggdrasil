c36c26038f1a45f8c1875ba1f90a7c96928d7f63
Claire DAVID <Claire.David@u-pem.fr>
2020-04-07 11:56:43 +0200

    cdavid modified Yggdrasil/ComputerScience/automates/exo-auto.pl

--- ComputerScience/automates/exo-auto.pl
+++ ComputerScience/automates/exo-auto.pl
@@ -1,6 +1,25 @@
 
 extends=AMC-auto.pl
 
+viewer.automaton ==
+#states
+s0
+s1
+s2
+#initials
+s0
+#accepting
+s2
+#alphabet
+a
+b
+#transitions
+s0:a>s1
+s1:a>s1
+s1:b>s2
+
+==
+
 questions==
 ** Cet automate est-il
 + deterministe
