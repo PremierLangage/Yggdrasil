26d0ef3f545456de98b798e374cc73c1aaef966c
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-07 13:46:00 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/factor.pl

---
+++ ComputerScience/automaton/templates/factor.pl
@@ -0,0 +1,23 @@
+
+extends = /automaton/templates/editor.pl
+
+maxattempt = 5
+
+generate== #|py|
+from generator import Generator
+alphabet, factor, automaton = Generator.factor(3, 3)
+==
+
+title = Automates Facteurs
+text ==
+Pour cette question, on considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots ayant **{{ factor }}** en facteur {{ '}' }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solution !
+</p>
+{% endif %}
+==
+
+
+
