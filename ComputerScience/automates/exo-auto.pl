42400a890f23ae3afff9497d6a5151b86ce08b25
Claire DAVID <Claire.David@u-pem.fr>
2020-04-07 12:08:01 +0200

    cdavid modified Yggdrasil/ComputerScience/automates/exo-auto.pl

--- ComputerScience/automates/exo-auto.pl
+++ ComputerScience/automates/exo-auto.pl
@@ -1,23 +1,8 @@
 
 extends=AMC-auto.pl
 
-viewer.automaton ==
-#states
-s0
-s1
-s2
-#initials
-s0
-#accepting
-s2
-#alphabet
-a
-b
-#transitions
-s0:a>s1
-s1:a>s1
-s1:b>s2
-
+mk_auto == #|python|
+viewer.automaton+="s0:a>s2"
 ==
 
 questions==
