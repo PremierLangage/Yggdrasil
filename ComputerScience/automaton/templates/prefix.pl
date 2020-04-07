5b82c61495f6ccd7d371144513fc01bfec1f8ea1
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-07 14:00:47 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/prefix.pl

--- ComputerScience/automaton/templates/prefix.pl
+++ ComputerScience/automaton/templates/prefix.pl
@@ -1,4 +1,4 @@
-extends = /ComputerScience/automaton/templates/editor.pl
+extends = editor.pl
 
 maxattempt = 5
 
@@ -17,6 +17,7 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solutio
 </p>
 {% endif %}
 ==
+
 
 
 
