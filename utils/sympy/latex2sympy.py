e57e932e9d7be2c47e216b6db26582729a0d73b5
David DOYEN <David.Doyen@u-pem.fr>
2020-04-07 14:04:53 +0200

    ddoyen modified Yggdrasil/utils/sympy/latex2sympy.py

--- utils/sympy/latex2sympy.py
+++ utils/sympy/latex2sympy.py
@@ -4,46 +4,43 @@ import re
 
 def str2sympy(s,local_dict={}):
     """
-    Convert a string into a SymPy expression without simplification.
+    Convert a string into an expression or a nested structure of
+    lists and tuples of expressions.
 
-    >>> str2expr("x+3+2x")
+    >>> str2sympy("x+3+2x")
     x + 2*x + 3
     
-    >>> str2expr("2^3")
+    >>> str2sympy("2^3")
     2**3
     
-    >>> str2expr("sin 2 pi")
+    >>> str2sympy("sin 2 pi")
     sin(2*pi)
     
-    >>> str2expr("3!")
+    >>> str2sympy("3!")
     factorial(3)
     
-    >>> str2expr("1+i",{'i':sp.I})
+    >>> str2sympy("1+i",{'i':sp.I})
     1 + I
     """
     s.strip()
     
-    if s=="":
+    if s == "":
         return None
+
+    s = s.replace("{", "[")
+    s = s.replace("}", "]")
     
     transformations=prs.standard_transformations + (prs.implicit_multiplication_application,prs.convert_xor)
     
     with sp.evaluate(False):
         return prs.parse_expr(s,local_dict=local_dict,transformations=transformations,evaluate=False)
 
-
-def latex2sympy(s,local_dict={}):
-    """
-    Convert a LaTeX string into a SymPy expression without simplification.
-
-    >>> latex2sympy("\pi^{3}")
-    pi**3
+def latex2str(s):
+    r"""
+    Convert a LaTeX string into a SymPy-readable string.
     
-    >>> latex2sympy("-\infty")
-    -oo
-    
-    >>> latex2sympy(r"\frac{2}{4}")
-    2/4
+    >>> latex2str(r"\frac{\pi}{2}")
+    '( pi)/(2)'
     """
     
     # delete meaningless substrings
@@ -62,21 +59,112 @@ def latex2sympy(s,local_dict={}):
          (r"\arccos", "acos"),
          (r"\arcsin", "asin"),
          (r"\arctan", "atan"),
-         (r"\lbrace","["),
-         (r"\rbrace","]"),
-         (r"\{","["),
-         (r"\}","]"),
-         (r"\emptyset","[]"),
+         (r"\{",r"\lbrace"),
+         (r"\}",r"\rbrace"),
          ("\\", " "),
          ("{", "("),
-         ("}", ")")]
+         ("}", ")"),
+         (r"\lbrace","{"),
+         (r"\rbrace","}"),
+         (r"\emptyset","{}")]
+         
     for (s1,s2) in lst:
         s = s.replace(s1,s2)
+        
+    return s
+
+
+def latex2sympy(s, local_dict={}):
+    r"""
+    Convert a LaTeX string into a SymPy expression without simplification.
+
+    >>> latex2sympy("\pi^{3}")
+    pi**3
     
-    return str2sympy(s,local_dict)
+    >>> latex2sympy("-\infty")
+    -oo
+    
+    >>> latex2sympy(r"\frac{2}{4}")
+    2/4
+    """
+    return str2sympy(latex2str(s), local_dict)
+    
+def str2interval(s, notation="bracket", local_dict={}):
+    """
+    Convert a string into an interval.
+    
+    >>> str2interval("[3,4]")
+    Interval(3, 4)
+    
+    >>> str2interval("]pi/4,pi/2]")
+    Interval.Lopen(pi/4, pi/2)
+    
+    >>> str2interval("[1,oo[")
+    Interval(1, oo)
+    """
+    
+    # TODO : english notation
+    # TODO : raise error when closed infinity endpoint
+    
+    s = s.strip()
+    
+    pattern = re.compile(r'^{(.*)}$')
+    if pattern.search(s) is not None:
+        return sp.FiniteSet(str2sympy(pattern.search(s).group(1)))
+    pattern = re.compile(r'^\[(.*),(.*)\]$')
+    
+    if pattern.search(s) is not None:
+        return sp.Interval(str2sympy(pattern.search(s).group(1)),str2sympy(pattern.search(s).group(2)))
+        
+    pattern = re.compile(r'^\[(.*),(.*)\[$')
+    if pattern.search(s) is not None:
+        return sp.Interval.Ropen(str2sympy(pattern.search(s).group(1)),str2sympy(pattern.search(s).group(2)))
+        
+    pattern = re.compile(r'^\](.*),(.*)\]$')
+    if pattern.search(s) is not None:
+        return sp.Interval.Lopen(str2sympy(pattern.search(s).group(1)),str2sympy(pattern.search(s).group(2)))
+        
+    pattern = re.compile(r'^\](.*),(.*)\[$')
+    if pattern.search(s) is not None:
+        return sp.Interval.open(str2sympy(pattern.search(s).group(1)),str2sympy(pattern.search(s).group(2)))
+        
+    raise ValueError()
+    
+def latex2interval(s, notation="bracket", local_dict={}):
+    """
+    Convert a latex string into an interval.
+    
+    >>> latex2interval("\\left\\[3,4\\right\\]")
+    Interval(3, 4)
+    
+    >>> latex2interval("\\left\]\\pi/4,\\pi/2\\right\]")
+    Interval.Lopen(pi/4, pi/2)
+    
+    >>> latex2interval("\\left\\[1,+\\infty\\right\\[")
+    Interval(1, oo)
+    """
+    return str2interval(latex2str(s), notation=notation, local_dict=local_dict)
+    
+def latex2rset(s, local_dict={}):
+    """
+    Convert a latex string into an interval reunion.
+    
+    >>> latex2rset("\emptyset")
+    [EmptySet()]
+    """
+    args = s.split("\cup")
+    rset = []
+    for a in args:
+        if a.strip() == r"\emptyset":
+            rset.append(sp.EmptySet())
+        elif a.strip() == r"\R":
+            rset.append(sp.S.Reals)
+        else:
+            rset.append(latex2interval(s))
+    return rset
 
 def FiniteSet2struct(S):
-    if  S == sp.EmptySet:
+    if S==sp.EmptySet():
         return []
     elif isinstance(S,sp.Set):
         return [FiniteSet2struct(x) for x in S]
