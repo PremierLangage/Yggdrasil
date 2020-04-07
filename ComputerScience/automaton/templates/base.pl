492e309da1cdf74d99b0896fdfcfc2f0eb6a1ac4
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-07 14:01:28 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/base.pl

--- ComputerScience/automaton/templates/base.pl
+++ ComputerScience/automaton/templates/base.pl
@@ -1,9 +1,9 @@
 @ /utils/sandboxio.py
-@ /ComputerScience/automaton/scripts/fsm.py
-@ /ComputerScience/automaton/scripts/lego.py
-@ /ComputerScience/automaton/scripts/automaton.py
-@ /ComputerScience/automaton/scripts/generator.py
-@ /ComputerScience/automaton/scripts/evaluator.py
+@ ../scripts/fsm.py
+@ ../scripts/lego.py
+@ ../scripts/automaton.py
+@ ../scripts/generator.py
+@ ../scripts/evaluator.py
 
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
@@ -13,6 +13,7 @@ styles =@ /ComputerScience/automaton/styles/automaton.css
 extracss==
 <style> {{ styles }}</style>
 ==
+
 
 
 
