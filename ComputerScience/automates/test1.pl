19bfe369b6dac4113b0c7856dcdf6f1bc08b48a1
Claire DAVID <Claire.David@u-pem.fr>
2020-04-07 11:28:37 +0200

    cdavid modified Yggdrasil/ComputerScience/automates/test1.pl

---
+++ ComputerScience/automates/test1.pl
@@ -0,0 +1,31 @@
+#type de l'exercice
+extends=/model/AMC_TXT.pl 
+
+title= Questions sur les Use Case 
+
+questions==
+* Use case ce traduit en français par
+- Histoire utilisateur
++ cas d'utilisation 
+- type d'utilisation
+- User story
+
+** Parmis les affirmations suivantes lesquelles caractèrisent les cas d'utilisation
++ Un dialogue entre un acteur et le système
++ Un dialogue de bout en bout
++ Un objectif pour l'acteur
++ un cas d'utilisation est un Document naratif
+
+** Parmis les propositions suivantes indiquez celle qui sont des erreurs d'utilisation de la modélisation par use case:
++ Décrire un sénario alternatif comme une extension (un autre cas d'utilisation)
++ Décrire un besoin métier comme un cas d'utilisation
++ Créer une association entre deux cas d'utilisation pour indiquer que les cas d'utilisation communiquent entre eux
++ Découper les sous fonctions d'un cas d'utilisation en cas d'utilisation plus petits. 
++ Modéliser des opérations élémentaires comme des cas d'utilisation
++ Mélanger des niveaux d'abstraction dans le dialogue du cas d'utilisation
++ Définir les niveaux de sécurité en liant les  niveaux de sécurité aux acteurs
+
+==
+
+
+
