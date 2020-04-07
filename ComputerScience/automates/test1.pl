ab96209c253d5e165986b30116dd48633315efa0
Claire DAVID <Claire.David@u-pem.fr>
2020-04-07 11:41:38 +0200

    cdavid modified Yggdrasil/ComputerScience/automates/test1.pl

--- ComputerScience/automates/test1.pl
+++ ComputerScience/automates/test1.pl
@@ -1,3 +1,4 @@
+#component framework - écrou cassé
 #cliquer sur test dans l'arboressence
 #type de l'exercice
 extends=/model/AMC_TXT.pl 
@@ -27,6 +28,7 @@ questions==
 + Définir les niveaux de sécurité en liant les  niveaux de sécurité aux acteurs
 
 ==
+
 
 
 
