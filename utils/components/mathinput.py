96f346b7e4a0b889df77078c5ee437265d260751
David DOYEN <David.Doyen@u-pem.fr>
2020-04-08 14:46:58 +0200

    ddoyen modified Yggdrasil/utils/components/mathinput.py

---
+++ utils/components/mathinput.py
@@ -0,0 +1,7 @@
+class CustomMathInpu(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-math-input'
+        self.decorator = 'CustomMathInput'
+        super().__init__(**kwargs)
+
