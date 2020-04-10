6e8557dc471a84183428d345c3d1788a28347033
David DOYEN <David.Doyen@u-pem.fr>
2020-04-10 14:31:51 +0200

    ddoyen modified Yggdrasil/utils/components/mathinput.py

--- utils/components/mathinput.py
+++ utils/components/mathinput.py
@@ -1,7 +1,16 @@
+from json import load
+
+json_file = open('keyboards.JSON')
+keyboards_JSON = load(json_file)
+json_file.close()
+
 class CustomMathInpu(Component):
 
     def __init__(self, **kwargs):
         self.selector = 'c-math-input'
         self.decorator = 'CustomMathInput'
+        keyboards_JSON['virtualKeyboards'] = "elementary"
+        self.config = keyboards_JSON
         super().__init__(**kwargs)
+
 
