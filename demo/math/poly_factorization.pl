8b50e11cc579e1bea37746ed9402ab70ff337183
David DOYEN <David.Doyen@u-pem.fr>
2020-04-10 12:29:55 +0200

    ddoyen modified Yggdrasil/demo/math/poly_factorization.pl

---
+++ demo/math/poly_factorization.pl
@@ -0,0 +1,20 @@
+extends = /model/mathinput.pl
+
+title = Développement d'une expression polynomiale
+
+before ==
+keyboards_JSON['virtualKeyboards'] = "elementary"
+input.config = keyboards_JSON
+
+P = rand_int_poly(1, 2, 2, "x")
+Q = rand_int_poly(1, 2, 2, "x")
+expr = P * Q
+==
+
+text ==
+Développer $${{expr.latex}}.$$
+==
+
+evaluator==
+score, _, feedback = eval_poly(input.value, expr, var="x", form="expanded")
+==
