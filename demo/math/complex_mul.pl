61ea95fe3799d719a45fe4c80b92952ff3e485ae
David DOYEN <David.Doyen@u-pem.fr>
2020-04-10 17:58:34 +0200

    ddoyen modified Yggdrasil/demo/math/complex_mul.pl

--- demo/math/complex_mul.pl
+++ demo/math/complex_mul.pl
@@ -34,4 +34,5 @@ form ==
 # Bon, ici, tout est caché dans la fonction eval_complex qui est dans une bibliothèque.
 evaluator ==
 score, _, feedback = eval_complex(input.value, sol, form="cartesian")
+score = 0
 ==
