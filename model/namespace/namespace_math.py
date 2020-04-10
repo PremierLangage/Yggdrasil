bc7bb830a08a6a2eaa5994eac6bde58af1d2f534
David DOYEN <David.Doyen@u-pem.fr>
2020-04-10 16:15:11 +0200

    ddoyen modified Yggdrasil/model/namespace/namespace_math.py

--- model/namespace/namespace_math.py
+++ model/namespace/namespace_math.py
@@ -4,12 +4,10 @@ from randsympy import *
 from evalsympy import *
 from latex2sympy import *
 from sympy2latex import *
-from json import load
-json_file = open('keyboards.JSON')
-keyboards_JSON = load(json_file)
-json_file.close()
+
 
 namespace = globals().copy()
+
 
 
 
