58fbdbb4fa8d76c30da61813770dfae4fe8d3fa0
David DOYEN <David.Doyen@u-pem.fr>
2020-04-10 11:52:57 +0200

    ddoyen modified Yggdrasil/demo/math/poly_expansion.pl

--- demo/math/poly_expansion.pl
+++ demo/math/poly_expansion.pl
@@ -6,9 +6,8 @@ before ==
 keyboards_JSON['virtualKeyboards'] = "elementary"
 input.config = keyboards_JSON
 
-var('x')
-P = rand_int_poly(1, 2, 5, x)
-Q = rand_int_poly(1, 2, 5, x)
+P = rand_int_poly(1, 2, 5, "x")
+Q = rand_int_poly(1, 2, 5, "x")
 expr = P * Q
 ==
 
