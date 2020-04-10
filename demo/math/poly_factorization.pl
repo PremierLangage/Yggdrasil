cee3dfd227e4c9eb9a5ccc0368e639e5f9ae54f8
David DOYEN <David.Doyen@u-pem.fr>
2020-04-10 15:29:13 +0200

    ddoyen modified Yggdrasil/demo/math/poly_factorization.pl

--- demo/math/poly_factorization.pl
+++ demo/math/poly_factorization.pl
@@ -5,7 +5,7 @@ title = Factorisation d'une expression polynomiale
 before ==
 P = rand_int_poly(1, 2, 2, "x")
 Q = rand_int_poly(1, 2, 2, "x")
-expr = -1 - 3* x **2
+expr = (P * Q).expand()
 ==
 
 text ==
