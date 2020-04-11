0480a87d707ab64a932b4a7483b400abd1b5f1c9
Claire DAVID <Claire.David@u-pem.fr>
2020-04-11 15:04:24 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/pref-suf-fac.pl

---
+++ ComputerScience/automates-test-claire/templates/pref-suf-fac.pl
@@ -0,0 +1,27 @@
+extends = editor.pl
+
+maxattempt = 5
+
+generate== #|py|
+from generator import Generator
+# A mettre ailleurs peut-être?
+import random
+#
+
+# Generator.prefix(nb lettres alphabet , longueur maxi du prefix)
+nb_lettres = random.choice([2,3])
+alphabet, prefix, automaton = Generator.prefix(nb_lettres,3)
+
+==
+
+title = Automates Préfixes
+text ==
+On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui commencent par **{{ prefix }}** {{ '}' }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solution !
+</p>
+{% endif %}
+
+==
