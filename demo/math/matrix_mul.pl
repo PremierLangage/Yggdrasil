85c4915ebfb52c4ea3e926ae8fd40f21741b7368
David DOYEN <David.Doyen@u-pem.fr>
2020-04-11 15:35:15 +0200

    ddoyen modified Yggdrasil/demo/math/matrix_mul.pl

---
+++ demo/math/matrix_mul.pl
@@ -0,0 +1,30 @@
+extends = /model/mathmatrix.pl
+
+title==
+Produit de matrices
+==
+
+before==
+n = 2
+coeffbound = 3
+mat.setmatrix([n * [0] for _ in range(n)])
+
+A = rand_int_matrix(n, n, coeffbound)
+B = rand_int_matrix(n, n, coeffbound)
+sol = A*B
+==
+
+text==
+Soit les matrices
+$$ A = \left( {{A.latex}}\right) \text{ et } B = \left({{B.latex}}\right) $$ 
+Calculer $! A B !$.
+==
+
+evaluator ==
+score, _, feedback = eval_matrix(mat.getmatrix(), sol)
+==
+
+solution ==
+La solution est $$\left( {{sol.latex}} \right).$$
+==
+
