3253e3aa673d61c40d3bd9764b5a1af307c80d84
David DOYEN <David.Doyen@u-pem.fr>
2020-04-11 17:42:21 +0200

    ddoyen modified Yggdrasil/demo/math/function_derivative.pl

--- demo/math/function_derivative.pl
+++ demo/math/function_derivative.pl
@@ -6,10 +6,9 @@ input.virtualKeyboards = functions
 
 before ==
 var('x')
-n = randint(1,4)
-g = randitem([sin(x),cos(x),exp(x),ln(x)])
-h = randitem([x,x**2])
-f = g*h
+g = randitem([sin(x), cos(x), exp(x), ln(x)])
+h = randitem([x, x**2])
+f = g * h
 sol = diff(f, x)
 ==
 
