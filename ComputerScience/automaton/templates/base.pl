853104f0f18d130dd4eb56989619bbb4f699915e
Claire DAVID <Claire.David@u-pem.fr>
2020-04-11 12:20:58 +0200

    cdavid modified Yggdrasil/ComputerScience/automaton/templates/base.pl

--- ComputerScience/automaton/templates/base.pl
+++ ComputerScience/automaton/templates/base.pl
@@ -11,8 +11,6 @@ builder =@ /builder/before.py
 styles =@ /ComputerScience/automates-test-claire/styles/automaton.css
 
 
-title = Automates Occurences
-
 extracss==
 <style> {{ styles }}</style>
 ==
