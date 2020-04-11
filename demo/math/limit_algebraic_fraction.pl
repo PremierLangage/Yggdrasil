f0b78021b2433f5462d3b15fc6b93eb777c3b0e1
David DOYEN <David.Doyen@u-pem.fr>
2020-04-11 16:39:51 +0200

    ddoyen modified Yggdrasil/demo/math/limit_algebraic_fraction.pl

--- demo/math/limit_algebraic_fraction.pl
+++ demo/math/limit_algebraic_fraction.pl
@@ -7,14 +7,12 @@ input.virtualKeyboards = functions
 before ==
 var('x')
 
-degf = randint(1,3)
-nbf = randint(2, degf+1)
-f=rand_int_poly(degf, nbf, 5, "x")
-degg = randint(1,3)
-nbg = randint(2, degg+1)
-g=rand_int_poly(degg, nbg, 5, "x")
+deg = randint(1,3)
+f = rand_int_poly(deg, randint(2, deg + 1), 5, "x")
+deg = randint(1,3)
+g = rand_int_poly(deg, randint(2, deg + 1), 5, "x")
 
-lim=Limit(f/g, x, oo)
+lim = Limit(f/g, x, oo)
 sol = lim.doit()
 ==
 
