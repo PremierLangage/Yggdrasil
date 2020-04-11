a19ff7da41244ddc7596e37544c1717937d17b7e
Claire DAVID <Claire.David@u-pem.fr>
2020-04-11 15:11:45 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/pref-suf-fac.pl

--- ComputerScience/automates-test-claire/templates/pref-suf-fac.pl
+++ ComputerScience/automates-test-claire/templates/pref-suf-fac.pl
@@ -22,51 +22,22 @@ messages = {
 
 if mode == -1 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du prefix)
-    alphabet, prefix, automaton = Generator.prefix(nb_lettres,3)
+    alphabet, mot, automaton = Generator.prefix(nb_lettres,3)
 elif mode == 0 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du facteur)
-    alphabet, facteur, automaton = Generator.facteur(nb_lettres,3)
+    alphabet, mot, automaton = Generator.facteur(nb_lettres,3)
 else :
     # Generator.prefix(nb lettres alphabet , longueur maxi du suffixe)
-    alphabet, suffixe, automaton = Generator.suffixe(nb_lettres,3)
+    alphabet, mot, automaton = Generator.suffixe(nb_lettres,3)
 ==
 
 title = Préfixe, suffixe ou facteur 
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui commencent par **{{ prefix }}** {{ '}' }} <br>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ messages }} par **{{ mot }}** {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solution !
-</p>
-{% endif %}
-
-==
-
-
-
-========
-generate== #|py|
-import random
-from generator import Generator
-mode = random.choice([-1, 0, 1])
-messages = {
-    -1: 'au moins',
-    0: 'exactement',
-    1: 'au plus'
-}
-message = messages[mode]
-# counting( nb lettres alphabet, nb occurences demandées , mode)
-alphabet, letter, n, automaton = Generator.counting(3, 4, mode)
-==
-
-title = Automates Occurences
-text ==
-On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez un automate reconnaissant le langage **L1** = {{ '{' }} mots ayant {{ message }} **{{ n }}** occurrences de la lettre **{{ letter }}** {{ '}' }} <br>
-{% if attempt < maxattempt and score != 100 %}
-<p class="warning-state">
-Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir la solution !
 </p>
 {% endif %}
 
