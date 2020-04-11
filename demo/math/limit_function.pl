94b25eb2a5a40951fd66a98c1d54b2cb2526c413
David DOYEN <David.Doyen@u-pem.fr>
2020-04-11 16:04:19 +0200

    ddoyen modified Yggdrasil/demo/math/limit_function.pl

---
+++ demo/math/limit_function.pl
@@ -0,0 +1,27 @@
+extends = /model/mathinput.pl
+
+title = Inéquation produit
+
+input.virtualKeyboards = functions
+
+before ==
+var('x')
+a, b = list_randint_norep(2,-6,6,[0])
+expr = (x + a) * (x + b)
+
+ineq = randitem([expr >= 0, expr > 0, expr <= 0,expr < 0])
+
+sol = solveset(ineq, x, domain=S.Reals)
+==
+
+text ==
+Déterminer l'ensemble des réels $! x !$ tels que $$ {{ineq.latex}}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
+==
+
+evaluator ==
+score, _, feedback = eval_rset(input.value, sol)
+==
+
+solution ==
+La solution est $% {{sol.latex}} %$.
+==
