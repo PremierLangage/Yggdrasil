d72b7da5a569dff71f6f51371156b12f068907e9
David DOYEN <David.Doyen@u-pem.fr>
2020-04-12 16:53:51 +0200

    ddoyen modified Yggdrasil/demo/math/linsys.pl

--- demo/math/linsys.pl
+++ demo/math/linsys.pl
@@ -4,18 +4,19 @@ title = Système linéaire
 
 before ==
 n = 2
-A = rand_int_matrix_invertible(n, 3,[0])
+lstvar = ",".join(["x", "y", "z", "t"][:n])
+
+A = rand_int_matrix_invertible(n, 3, [0])
 B = rand_int_matrix(n, 1, 3, [0])
 
 sol = list(linsolve((A, B)))[0]
 
-sys_tex=latexsys(A,B)
-lstvar=",".join(["x","y","z","t","u","v"][:n])
+linsys = latex_linsys(A,B)
 ==
 
 text ==
 Déterminer la solution du système suivant (d'inconnues $! {{lstvar}} !$) :
-$$ {{sys_tex}} $$
+$$ {{linsys}} $$
 Ecrire la solution sous forme d'un couple de nombres.
 ==
 
