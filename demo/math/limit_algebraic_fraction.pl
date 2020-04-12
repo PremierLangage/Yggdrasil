08732f3d71139976fd980f053b1eb3a345ce89e9
David DOYEN <David.Doyen@u-pem.fr>
2020-04-12 14:24:55 +0200

    ddoyen modified Yggdrasil/demo/math/limit_algebraic_fraction.pl

--- demo/math/limit_algebraic_fraction.pl
+++ demo/math/limit_algebraic_fraction.pl
@@ -12,12 +12,12 @@ f = rand_int_poly(deg, randint(2, deg + 1), 5, "x")
 deg = randint(1,3)
 g = rand_int_poly(deg, randint(2, deg + 1), 5, "x")
 
-lim = Limit(f/g, x, oo)
+lim2 = Limit(f/g, x, oo)
 sol = lim.doit()
 ==
 
 text ==
-Déterminer {{ lim.latex }}.
+Déterminer {{ lim2.latex }}.
 ==
 
 evaluator ==
