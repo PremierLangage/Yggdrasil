f52f15018f6872680b3611ba468767177076018a
David DOYEN <David.Doyen@u-pem.fr>
2020-04-13 12:42:03 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/power_set_to_extension.pl

--- Mathematics/exercises/foundations/sets/power_set_to_extension.pl
+++ Mathematics/exercises/foundations/sets/power_set_to_extension.pl
@@ -1,18 +1,8 @@
-extends = /Mathematics/template/mathbasic.pl
-
-input1 =: MathInput
-
-form==
-{{input1|component}}
-==
+extends = /model/mathinput.pl
 
 title = Ensemble des parties d'un ensemble
 
-lang = fr
-
 before ==
-keyboards_JSON['virtualKeyboards']="sets"
-input1.config = keyboards_JSON
 
 A=rand_finiteset(randint(2,3),list(range(10)))
 A_tex=latex(A)
@@ -25,10 +15,11 @@ Ecrire en extension l'ensemble $! \mathcal{P}(\\{ {{A_tex}}\\} ) !$.
 ==
 
 evaluator ==
-score,_,feedback=ans_struct_expr(input1.value,sol,"composite")
+score, _, feedback = eval_set(input.value, sol)
 ==
 
 solution ==
 La solution est $! { {{sol_tex}} } !$.
 ==
+
 
