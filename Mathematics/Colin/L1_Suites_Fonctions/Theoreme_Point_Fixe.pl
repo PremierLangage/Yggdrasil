fb0ba7ef41f83a2766bedb4f1c179e742d1c221d
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-04-13 17:19:13 +0200

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Theoreme_Point_Fixe.pl

---
+++ Mathematics/Colin/L1_Suites_Fonctions/Theoreme_Point_Fixe.pl
@@ -0,0 +1,22 @@
+author=Colin Petitjean
+
+title=Théorème du point fixe 
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+questions==
+
+* Soit $%f : \mathbb{R} \to \mathbb{R} %$ une fonction. On dit que $x_0$ est un point fixe de $f$ si :
++ $%f(x_0) = x_0 %$
+- $%f(x_0) \geq  x_0 %$
+- $%f(x_0) \leq  x_0 %$
+- $%f(x_0) \neq x_0 %$
+
+
+** Quelles sont les hypothèses du théorème de point fixe mentionné dans la vidéo "3-1 Théorème de point" ?
++ On considère une fonction $%f : [a,b] \to [a,b]%$ où $% a<b %$
+
+
+==
+
+
