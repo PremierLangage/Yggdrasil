e4a76ceb831dd3a64de713c3d927a81e5698e284
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-04-13 17:26:16 +0200

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Theoreme_Point_Fixe.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Theoreme_Point_Fixe.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Theoreme_Point_Fixe.pl
@@ -6,15 +6,22 @@ tag=system|shell
 extends=/model/AMC_TXT.pl
 questions==
 
-* Soit $%f : \mathbb{R} \to \mathbb{R} %$ une fonction. On dit que $x_0$ est un point fixe de $f$ si :
+* Soit $%f : \mathbb{R} \to \mathbb{R} %$ une fonction. On dit que $%x_0%$ est un point fixe de $%f%$ si :
 + $%f(x_0) = x_0 %$
 - $%f(x_0) \geq  x_0 %$
 - $%f(x_0) \leq  x_0 %$
 - $%f(x_0) \neq x_0 %$
 
 
-** Quelles sont les hypothèses du théorème de point fixe mentionné dans la vidéo "3-1 Théorème de point" ?
+** Quelles sont les hypothèses du théorème de point fixe mentionné dans la vidéo "3-1 Théorème de point fixe" (existence d'un unique point fixe) ?
 + On considère une fonction $%f : [a,b] \to [a,b]%$ où $% a<b %$
+- On considère une fonction $%f : [a,b[ \to [a,b[%$ où $% a<b %$ 
++ On suppose que la fonction $%f%$ est contractante.
+- On suppose que la fonction $%f%$ est Lipschitzienne.
+- On suppose que la fonction $%f%$ est 1-Lipschitzienne.
+- On considère une fonction $%f : ]a,b[ \to ]a,b[%$ où $% a<b %$
+
+*
 
 
 ==
