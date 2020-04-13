3940d76ccc2f8656245a0f0ce3c7c03ec2cf7a08
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-13 19:35:38 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/templates/bash_template.pl

---
+++ ComputerScience/OperatingSystem/templates/bash_template.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = python
+editor.code ==
+# write your code here
+==
+
+before==
+==
+
+title = Code Editor Component
+
+text==
+==
+
+form==
+{{ editor|component }}
+==
+
+evaluator==
+grade = (100, f"student code:\n{editor.code}")
+==
+
