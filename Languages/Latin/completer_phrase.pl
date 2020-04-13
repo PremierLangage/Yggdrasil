a753da4d7a063f24f6f64c8f3146ee6331415409
David DOYEN <David.Doyen@u-pem.fr>
2020-04-13 22:20:41 +0200

    ddoyen modified Yggdrasil/Languages/Latin/completer_phrase.pl

--- Languages/Latin/completer_phrase.pl
+++ Languages/Latin/completer_phrase.pl
@@ -7,7 +7,7 @@ from customdragdrop import *
 
 dragdrop = DragDropGroup()
 dragdrop.set_drop("")
-dragdrop.set_label({"lt": "1", "gt": '3'})
+dragdrop.set_label(["1","3"])
 labels = list(dragdrop.labels.values())
 ==
 
