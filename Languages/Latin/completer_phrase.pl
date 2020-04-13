3b87024a0012cc025c179d271e35d54d3f2e8580
David DOYEN <David.Doyen@u-pem.fr>
2020-04-13 17:42:58 +0200

    ddoyen modified Yggdrasil/Languages/Latin/completer_phrase.pl

--- Languages/Latin/completer_phrase.pl
+++ Languages/Latin/completer_phrase.pl
@@ -5,7 +5,6 @@ extends = /model/basic.pl
 title = Comparaison de nombres (DragDrop)
 
 before==#|python|
-import random as rd
 from customdragdrop import *
 
 drop = CustomDragDrop.Drop()
@@ -13,7 +12,7 @@ labels = [CustomDragDrop.Label() for _ in range(3)]
 ==
 
 text==
-Compméter la phrase suivante avec les étiquettes proposées.
+Compléter la phrase suivante avec les étiquettes proposées.
 ==
 
 form==
@@ -27,14 +26,6 @@ Le {{ drop|component }} est absent.
 settings.feedback = lightscore
 
 evaluator==#|python|
-from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
 
-# On récupère les données du before
-mygroup = DragDropGroup()
-mygroup.labels = {'lt':a, 'gt': b}
-mygroup.drops = {0: c0, 1: c1, 2: c2, 3: c3}
-mygroup._matches = match
-
-grade=mygroup.eval(grading_function = right_minus_wrong)
 ==
 
