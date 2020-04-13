921aa0e1cca994fa55fd550a4fb0a29abe6c05e0
David DOYEN <David.Doyen@u-pem.fr>
2020-04-13 12:42:15 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/power_set_to_extension.pl

--- Mathematics/exercises/foundations/sets/power_set_to_extension.pl
+++ Mathematics/exercises/foundations/sets/power_set_to_extension.pl
@@ -3,8 +3,7 @@ extends = /model/mathinput.pl
 title = Ensemble des parties d'un ensemble
 
 before ==
-
-A=rand_finiteset(randint(2,3),list(range(10)))
+A = rand_finiteset(randint(2,3),list(range(10)))
 A_tex=latex(A)
 sol=A.powerset()
 sol_tex=latex(sol)
@@ -19,7 +18,7 @@ score, _, feedback = eval_set(input.value, sol)
 ==
 
 solution ==
-La solution est $! { {{sol_tex}} } !$.
+La solution est $! { {{sol.latex}} } !$.
 ==
 
 
