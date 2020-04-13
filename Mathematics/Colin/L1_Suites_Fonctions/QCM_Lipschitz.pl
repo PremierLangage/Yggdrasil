51efbd6c0eee14cb68eb4c81f34587d2db1bc1f3
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-04-13 21:51:41 +0200

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/QCM_Lipschitz.pl

--- Mathematics/Colin/L1_Suites_Fonctions/QCM_Lipschitz.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/QCM_Lipschitz.pl
@@ -48,42 +48,9 @@ Une fonction $% f  : \mathbb{R} \rightarrow \mathbb{R}%$ est $%\textbf{non}%$ co
 + $% \exists x,y \in \mathbb{R} \text{ tel que } |f(x) - f(y)| \geq |x-y| %$
 - $% \exists x,y \in \mathbb{R} \text{ tel que } |f(x) - f(y)| > |x-y| %$
 
-* Vrai ou faux ? La fonction $% f : \mathbb{R} \rightarrow \mathbb{R}%$ définie par $%f(x) = -x+3 %$ est contractante.
-- Vrai
-+ Faux
-
-* Vrai ou faux ? La fonction $% f :\mathbb{R} \rightarrow \mathbb{R}%$ définie par $%f(x) = -\frac{5}{6} x + \frac{1}{6} %$ est contractante.
-+ Vrai
-- Faux
-
-* On considère une fonction $% f : \mathbb{R} \rightarrow \mathbb{R} %$ telle que $%\lim\limits_{x \to +\infty} \dfrac{f(x)-f(0)}{x} = +\infty%$. Que peut-on dire de la fonction $%f%$ ?
-- La fonction $%f%$ est contractante.
-- La fonction $%f%$ n'est pas contractante mais elle est Lipschitzienne.
-+ La fonction $%f%$ n'est pas Lipschitzienne.
-
-* On considère la fonction $% f : \mathbb{R} \rightarrow \mathbb{R}%$ définie par $%f(x) = e^x %$. 
-Cliquez sur l'affirmation vous pensez être correcte :
-- La fonction $%f%$ est contractante.
-- La fonction $%f%$ n'est pas contractante mais elle est Lipschitzienne.
-+ La fonction $%f%$ n'est pas Lipschitzienne.
-
-
-** Soient $%f%$ et $%g%$ deux fonctions réelles définies sur $%\mathbb{R}%$. 
-Cliquez sur les affirmations que vous pensez être correctes (plusieurs bonnes réponses) :
-+ Si $%f%$ est Lipschitzienne sur $%\mathbb{R}%$, alors $%f%$ est Lipschitzienne tout intervalle du type $%[a,b]%$ ($%a<b%$).
-- Si $%f%$ est Lipschitzienne sur tout intervalle du type $%[a,b]%$ ($%a<b%$), alors $%f%$ est Lipschitzienne sur $%\mathbb{R}%$.
-+ Si $%f%$ est $%K%$-Lipschitzienne sur tout intervalle du type $%[a,b]%$ ($%a<b%$), alors $%f%$ est $%K%$-Lipschitzienne sur $%\mathbb{R}%$.
-- Si $%f%$ et $%g%$ sont Lipschitziennes sur $%\mathbb{R}%$, alors nécessairement la fonction $%f\cdot g%$ l'est également. 
-+ Si $%f%$ et $%g%$ sont Lipschitziennes sur $%\mathbb{R}%$, alors nécessairement la fonction $%f + g%$ l'est également. 
-+ Si $%f%$ est Lipschitzienne sur $%\mathbb{R}%$ et $%\lambda \in \mathbb{R}%$, alors nécessairement la fonction $%\lambda f%$ l'est également.
-
-
-* Vrai ou faux ? L'ensemble des fonctions Lipschitziennes de $%\mathbb R%$ dans $%\mathbb R%$ est un sous espace vectoriel de l'ensemble des fonctions.
-+  Vrai
-- Faux
-
 
 
 ==
+
 
 
