321bf2c7481b0ffcea71cae663cdcd0f8e4f6712
David DOYEN <David.Doyen@u-pem.fr>
2020-04-13 16:46:11 +0200

    ddoyen modified Yggdrasil/Languages/Latin/completer_phrase.pl

--- Languages/Latin/completer_phrase.pl
+++ Languages/Latin/completer_phrase.pl
@@ -11,25 +11,6 @@ from customdragdrop import *
 drop = Drop()
 mygroup.set_label({"lt": lt, "gt": gt})
 
-numbers = []
-for i in range(n):
-    [a,b] = rd.sample(range(10,100),2)
-    numbers.append([a,b])
-    mygroup.add_drop({i:"coucou"}) 
-    if a < b:
-        mygroup.add_match_by_content(i,lt) 
-    else:
-        mygroup.add_match_by_content(i,gt) 
-
-# La suite du before ne sert qu'à faire passer les infos dans "form" et "grader"
-
-a = mygroup.labels['lt']
-b = mygroup.labels['gt']
-c1 = mygroup.drops[1]
-c2 = mygroup.drops[2]
-c3 = mygroup.drops[3]
-c0 = mygroup.drops[0]
-match = mygroup._matches
 ==
 
 text==
@@ -37,12 +18,7 @@ Comparer les nombres suivants avec les symboles {{ a | component }} et {{ b | 
 ==
 
 form==
-<ul>
-<li> {{ numbers[0][0] }} {{ c0 | component }} {{ numbers[0][1] }} </li>
-<li> {{ numbers[1][0] }} {{ c1 | component }} {{ numbers[1][1] }} </li>
-<li> {{ numbers[2][0] }} {{ c2 | component }} {{ numbers[2][1] }} </li>
-<li> {{ numbers[3][0] }} {{ c3 | component }} {{ numbers[3][1] }} </li>
-</ul>
+Le {{ drop|component }} est absent.
 ==
 
 settings.feedback = lightscore
