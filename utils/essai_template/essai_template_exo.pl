1ff7d85a624b2a12cd8ea45ba9a518dfb34b785b
www-data <www-data@platon-edu.fr>
2020-04-13 12:46:38 +0200

    admin modified Yggdrasil/utils/essai_template/essai_template_exo.pl

--- utils/essai_template/essai_template_exo.pl
+++ utils/essai_template/essai_template_exo.pl
@@ -27,7 +27,6 @@ grade = (0, 'c'est nul')
 
 
 custom_pl_template == 
-<ion-card class="exercise">
     <ion-card-header class="exercise__header">
         <ion-card-subtitle class='exercise__author'>{% if author %}{{ author }}{% endif %}</ion-card-subtitle>
         <ion-card-title class=" exercise__header">{% if title %}{{ title }}{% endif %}</ion-card-title>
@@ -91,8 +90,8 @@ custom_pl_template ==
             </div>
         </div>
     </ion-card-content>
-</ion-card>
 ==
+
 
 
 
