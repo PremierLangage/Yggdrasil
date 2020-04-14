eb83432b853caea657cbbb254efa479e2f91f44a
etienne sandier <sandier@u-pec.fr>
2020-04-14 17:36:32 +0200

    esandier modified Yggdrasil/utils/components/dragdropgroup_example1.pl

--- utils/components/dragdropgroup_example1.pl
+++ utils/components/dragdropgroup_example1.pl
@@ -4,7 +4,7 @@ title = Comparaison de nombres (DragDrop)
 
 before==#|python|
 import random as rd
-from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
+from customdragdrop import DragDropGroup
 
 lt="$%\lt%$" # latex for "less than"
 gt="$%\gt%$" # latex for "greater than"
@@ -18,48 +18,32 @@ numbers = []
 for i in range(n):
     [a,b] = rd.sample(range(10,100),2)
     numbers.append([a,b])
-    mygroup.add_drop({i:"coucou"}) 
+    mygroup.add_drop({str(i):"coucou"}) 
     if a < b:
-        mygroup.add_match_by_content(i,lt) 
+        mygroup.add_match_by_content(str(i),lt) 
     else:
-        mygroup.add_match_by_content(i,gt) 
-
-# La suite du before ne sert qu'à faire passer les infos dans "form" et "grader"
-
-a = mygroup.labels['lt']
-b = mygroup.labels['gt']
-c1 = mygroup.drops[1]
-c2 = mygroup.drops[2]
-c3 = mygroup.drops[3]
-c0 = mygroup.drops[0]
-match = mygroup._matches
+        mygroup.add_match_by_content(str(i),gt) 
 ==
 
-text==
-Comparer les nombres suivants avec les symboles {{ a | component }} et {{ b | component }}.
+text ==
+{% for label in mygroup.labels.values() %} {{ label|component }} {% endfor %}
 ==
 
-form==
+form ==
 <ul>
-<li> {{ numbers[0][0] }} {{ c0 | component }} {{ numbers[0][1] }} </li>
-<li> {{ numbers[1][0] }} {{ c1 | component }} {{ numbers[1][1] }} </li>
-<li> {{ numbers[2][0] }} {{ c2 | component }} {{ numbers[2][1] }} </li>
-<li> {{ numbers[3][0] }} {{ c3 | component }} {{ numbers[3][1] }} </li>
+{% for i in range(n) %}
+<li> {{ numbers[i][0] }} {{ mygroup.drops[i|string]|component }} {{ numbers[i][1] }} </li>
+{% endfor %}
 </ul>
+
 ==
 
 settings.feedback = lightscore
 
 evaluator==#|python|
-from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
+from customdragdrop import right_minus_wrong
 
-# On récupère les données du before
-mygroup = DragDropGroup()
-mygroup.labels = {'lt':a, 'gt': b}
-mygroup.drops = {0: c0, 1: c1, 2: c2, 3: c3}
-mygroup._matches = match
-
-grade=mygroup.eval(grading_function = right_minus_wrong)
+grade = mygroup.eval(grading_function = right_minus_wrong)
 ==
 
 extracss == #|html| 
@@ -70,12 +54,6 @@ extracss == #|html|
         }
 </style>
 ==
-
-
-
-
-
-
 
 
 
