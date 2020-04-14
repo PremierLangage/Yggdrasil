db95ea4e3fddd614c667339a33a05bb32f2bc863
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-14 22:57:51 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/templates/bash_template.pl

--- ComputerScience/OperatingSystem/templates/bash_template.pl
+++ ComputerScience/OperatingSystem/templates/bash_template.pl
@@ -44,7 +44,9 @@ form==
 evaluator==
 import subprocess
 
-sp = subprocess.run( *list(editor.code.split('\n')), stdout=subprocess.PIPE)
+
+
+sp = subprocess.run(list(editor.code.split('\n')), stdout=subprocess.PIPE)
 spout = ls.stdout.decode()
 
 grade = (100, spout)
