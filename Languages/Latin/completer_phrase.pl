2b2c661afbf314ebc02d3e738ae897ec0d0bcec4
David DOYEN <David.Doyen@u-pem.fr>
2020-04-14 12:26:56 +0200

    ddoyen modified Yggdrasil/Languages/Latin/completer_phrase.pl

--- Languages/Latin/completer_phrase.pl
+++ Languages/Latin/completer_phrase.pl
@@ -15,7 +15,6 @@ Compléter la phrase suivante avec les étiquettes proposées.
 ==
 
 form ==
-Phrase {{  dragdrop.drops['0']|component }} à compléter.
 
 {% for label in dragdrop.labels.values() %} {{ label|component }} {% endfor %}
 ==
