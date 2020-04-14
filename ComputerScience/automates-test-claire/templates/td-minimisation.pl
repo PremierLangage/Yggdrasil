8252182ab0205f64012036737e58651cfabce361
Claire DAVID <Claire.David@u-pem.fr>
2020-04-14 23:05:56 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/td-minimisation.pl

--- ComputerScience/automates-test-claire/templates/td-minimisation.pl
+++ ComputerScience/automates-test-claire/templates/td-minimisation.pl
@@ -9,10 +9,23 @@ generate== #|py|
 #    la construction de l'automate pour tester la réponse
 
 # peut-on utiliser la syntaxe du viewer ?
+import random
+from generator import Generator
+mode = random.choice([-1, 0, 1])
+messages = {
+    -1: 'au moins',
+    0: 'exactement',
+    1: 'au plus'
+}
+message = messages[mode]
+# counting( nb lettres alphabet, nb occurences demandées , mode)
+alphabet, letter, n, automaton = Generator.counting(3, 4, mode)
+
 ==
 
 title = Exemples de langages
 text ==
+{{ viewer|component }}
 Donnez l'automate minimal déterministe complet correspondant au langage reconnu par cet automate. <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
@@ -20,7 +33,6 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir la solution
 </p>
 {% endif %}
 ==
-
 
 
 
