1b95ddc94be7b80fde02956649a231274aad4fb8
Claire DAVID <Claire.David@u-pem.fr>
2020-04-14 23:17:46 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/exo-egalite-auto.pl

--- ComputerScience/automates-test-claire/templates/exo-egalite-auto.pl
+++ ComputerScience/automates-test-claire/templates/exo-egalite-auto.pl
@@ -13,6 +13,7 @@ attempt = 0
 maxattempt = int(maxattempt)
 deterministic = True
 
+# au lieu de faire de la génération aléatoire, donne l'automate. Même problématique que dans td1-exemples-auto.pl
 if 'generate' not in globals():
     raise Exception('You must define a script "generate"')
 
