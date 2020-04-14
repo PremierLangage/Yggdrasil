83e53d5eec789c5f0ff6468fd51e74bc2f1e41f9
Claire DAVID <Claire.David@u-pem.fr>
2020-04-14 22:54:19 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/td-minimisation.pl

--- ComputerScience/automates-test-claire/templates/td-minimisation.pl
+++ ComputerScience/automates-test-claire/templates/td-minimisation.pl
@@ -14,7 +14,7 @@ generate== #|py|
 
 title = Exemples de langages
 text ==
-Donnez l'automate minimal déterministe complet correspondant au langage reconnu par cet automate <br>
+Donnez l'automate minimal déterministe complet correspondant au langage reconnu par cet automate. <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir la solution !
