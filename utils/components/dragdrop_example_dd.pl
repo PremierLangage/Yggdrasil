d7aef00e1a55f9a218fc5074c1ae53e7e63b5620
David DOYEN <David.Doyen@u-pem.fr>
2020-04-14 16:18:27 +0200

    ddoyen modified Yggdrasil/utils/components/dragdrop_example_dd.pl

--- utils/components/dragdrop_example_dd.pl
+++ utils/components/dragdrop_example_dd.pl
@@ -39,13 +39,8 @@ settings.feedback = lightscore
 evaluator==#|python|
 from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
 
-# On récupère les données du before
-mygroup = DragDropGroup()
-mygroup.labels = {'lt':a, 'gt': b}
-mygroup.drops = {0: c0, 1: c1, 2: c2, 3: c3}
-mygroup._matches = match
 
-grade=mygroup.eval(grading_function = right_minus_wrong)
+grade = mygroup.eval(grading_function = right_minus_wrong)
 ==
 
 extracss == #|html| 
