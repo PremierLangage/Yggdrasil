0a7ed4568d46d9163273c13e5c5d38c4ec195393
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-15 12:40:41 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/templates/bash_template.pl

--- ComputerScience/OperatingSystem/templates/bash_template.pl
+++ ComputerScience/OperatingSystem/templates/bash_template.pl
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# coding: utf-8
-
 #*****************************************************************************
 #  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
