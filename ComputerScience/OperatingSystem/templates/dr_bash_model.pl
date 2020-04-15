502e20ee28779f91937022195656d11338b93c0c
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-15 12:22:41 +0200

    drevuz modified Yggdrasil/ComputerScience/OperatingSystem/templates/dr_bash_model.pl

---
+++ ComputerScience/OperatingSystem/templates/dr_bash_model.pl
@@ -0,0 +1,62 @@
+
+#!/usr/bin/env python3
+# coding: utf-8
+
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = shell
+editor.height = 100px
+
+editor.code ==
+
+==
+
+before==
+==
+
+title=bash code template (titre à surcharger!)
+
+text==
+==
+
+form==
+{{ editor|component }}
+==
+
+evaluator==
+import subprocess
+
+f = open("student_script.sh", "w")
+f.write(editor.code)
+f.close()
+
+sp = subprocess.run(["/bin/bash"], stdin=io.stringIO(editor.code), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+spout = sp.stdout.decode()
+
+text = "<pre>" + spout.replace('\n', '<br />') + "</pre>"
+
+grade = (100, f"  ")
+
+==
+
+
+
