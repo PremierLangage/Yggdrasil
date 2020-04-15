28c25e81c9adc0d992d8037ea18d10d92b3ab701
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-15 12:40:43 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/templates/utils_bash.py

---
+++ ComputerScience/OperatingSystem/templates/utils_bash.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
