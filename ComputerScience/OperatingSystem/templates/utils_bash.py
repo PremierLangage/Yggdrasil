2696c798058a09782b1875a3a7d0eaaadf6854f3
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-15 15:14:26 +0200

    NB modified Yggdrasil/ComputerScience/OperatingSystem/templates/utils_bash.py

---
+++ ComputerScience/OperatingSystem/templates/utils_bash.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+def display_as_shell_this(command, output):
+    r"""
+    Return a nice shell looking of the command and its output in html/css
+    """
+    # making the div for terminal screen
+    s = '<br /><div style="background-color: black; '
+    s += 'background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%);'
+    s += ' height: 80vh; font: 1rem Inconsolata, monospace; '
+    s += 'border-radius: 15px; padding: 10px;" >'
+    s += '<pre><output style="color: white;">'
+
+    # if command do not end with a newline, we had it...
+    while command[-1] == '\n':
+        command = command[:-1]
+    s += ":~$> " + command.replace('\n', '\ <br />... ')
+    s += "<br />"
+    s += output.replace('\n', ' <br />')
+
+    s += "</output></pre></div>"
+    return s
+
