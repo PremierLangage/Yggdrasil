85bc57b527c7a31f0b6f75f7b2192e4e89baf407
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-15 17:41:14 +0200

    NB modified Yggdrasil/ComputerScience/OperatingSystem/templates/utils_bash.py

--- ComputerScience/OperatingSystem/templates/utils_bash.py
+++ ComputerScience/OperatingSystem/templates/utils_bash.py
@@ -25,7 +25,7 @@ def display_as_shell_this(command, output):
     s += 'background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%);'
     s += ' height: 80vh; font: 1rem Inconsolata, monospace; '
     s += 'border-radius: 15px; padding: 10px;" >'
-    s += '<pre><output style="color: white;">'
+    s += '<pre><output style="color: white; text-shadow: 0 0 5px #C8C8C8;">'
 
     # if command do not end with a newline, we had it...
     while command[-1] == '\n':
