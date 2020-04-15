b0db5063aabc252844da0b9a67b0d6618b6a0cc4
www-data <www-data@platon-edu.fr>
2020-04-15 13:33:27 +0200

    admin modified Yggdrasil/utils/essai_template/essai_template_exo.pl

--- utils/essai_template/essai_template_exo.pl
+++ utils/essai_template/essai_template_exo.pl
@@ -66,10 +66,10 @@ mygroup = DragDropGroup()
 mygroup.labels = {"integrale": c, "limite": d, "fraction": e, "determinant": f}
 mygroup.drops = {"1": a, "tiers": b}
 mygroup._matches = g
-a.css='success'
-b.css='error'
 
 grade=mygroup.eval(grading_function = right_minus_wrong) 
+a.css='success'
+b.css='error'
 ==
 
 extracss == #|html| 
