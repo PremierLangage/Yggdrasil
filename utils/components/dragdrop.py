a21fea28e8b9d2ce47869bd363afa8c700f2a0dc
www-data <www-data@platon-edu.fr>
2020-04-15 14:21:24 +0200

    admin modified Yggdrasil/utils/components/dragdrop.py

--- utils/components/dragdrop.py
+++ utils/components/dragdrop.py
@@ -166,10 +166,10 @@ class DragDropGroup():
                 pass
             elif drop_data in self._matches:
                 num_right += 1
-                drop.css = "success"
+                drop.css = "drop success"
             else:
                 num_wrong +=1
-                drop.css = "error"           
+                drop.css = "drop error"           
    
         possible_labels = {label for (label, drop) in self._matches} # set comprehension, no duplicates
         possible_drops = {drop for (label, drop) in self._matches} # set comprehension, no duplicates
