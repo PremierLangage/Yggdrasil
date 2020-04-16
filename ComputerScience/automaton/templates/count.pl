ba181ebf00b06af9a18d18072edb315c056dee4a
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 11:05:31 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/count.pl

--- ComputerScience/automaton/templates/count.pl
+++ ComputerScience/automaton/templates/count.pl
@@ -2,19 +2,7 @@ extends = editor.pl
 
 maxattempt = 5
 
-generate== #|py|
-import random
-from generator import Generator
-mode = random.choice([-1, 0, 1])
-messages = {
-    -1: 'au moins',
-    0: 'exactement',
-    1: 'au plus'
-}
-message = messages[mode]
-# counting( nb lettres alphabet, nb occurences demandées , mode)
-alphabet, letter, n, automaton = Generator.counting(3, 4, mode)
-==
+
 
 title = Automates Occurences
 text ==
