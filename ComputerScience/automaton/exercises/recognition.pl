fd30bd167470fd4e370dc2aa220e85e7232c92d7
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 16:09:39 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/exercises/recognition.pl

--- ComputerScience/automaton/exercises/recognition.pl
+++ ComputerScience/automaton/exercises/recognition.pl
@@ -6,25 +6,30 @@ generate== #|py|
 from automaton import Automaton
 from generator import Generator
 
-# REMOVE COMMENT TO USE STRING NOTATION
-viewer = '''
-    #states
-    S0
-    S1
-    S2
-    #initials
-    S0
-    #accepting
-    S2
-    #alphabet
-    a
-    b
-    c
-    #transitions
-    S0:a>S1
-    S1:b>S2
-    S2:a,b,c>S2
-'''
+# STRING NOTATION
+#viewer = '''
+#    #states
+#    S0
+#    S1
+#    S2
+#    #initials
+#    S0
+#    #accepting
+#    S2
+#    #alphabet
+#    a
+#    b
+#    c
+#    #transitions
+#    S0:a>S1
+#    S1:b>S2
+#    S2:a,b,c>S2
+#'''
+
+# OBJECT NOTATION
+viewer = {
+
+}
 #viewer = Automaton.rand(4, 3, 3)
 ==
 
