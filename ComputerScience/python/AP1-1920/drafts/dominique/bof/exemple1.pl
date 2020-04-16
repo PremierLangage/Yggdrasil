a2da9669b411476e544323e28cadfd397210accc
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-16 18:44:39 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/bof/exemple1.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/bof/exemple1.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/bof/exemple1.pl
@@ -5,13 +5,15 @@ title= Controleur de rendu
 @ /grader/evaluator.py [grader.py]
 
 evaluator==
-from anthony import killthermall
 
-killthemall(editor.code)
-
-if editor.code=="":
-    grade=(100," tout c'est pas jhlsdfj ghlbien passé")
-else:
-    grade=(100," tout c'est bien passé")
+grade=(100,"{{editor1.code}} \n lautre \n {{editor2.code}}")
 ==
+
+
+text==
+
+Exoncé
+
+==
+
 
