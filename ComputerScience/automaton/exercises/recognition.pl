2d4e89098fdcbcb1b93a33c08680444328a3b5c4
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 15:56:58 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/exercises/recognition.pl

--- ComputerScience/automaton/exercises/recognition.pl
+++ ComputerScience/automaton/exercises/recognition.pl
@@ -10,32 +10,9 @@ from generator import Generator
 viewer = Automaton.rand(4, 3, 3)
 ==
 
-evaluator== #|py|
-
-from evaluator import Evaluator
-
-right, total = Evaluator.eval_multi_choice_question(
-    checkbox,
-    answers,
-    withanswer=True
-)
-checkbox.disabled = True
-
-if right == total:
-    grade = (100, '<p class="success-state">Bonne réponse</p>')
-else:
-    grade = ((right / total) * 100, f"<p class='warning-state'>{right} / {total}</p>")
-==
-
 title= Lecture d'automates
 
 text==
 Parmi les mots suivants, lesquels sont reconnus par cet automate?
 ==
-
-form ==
-{{ viewer|component }}
-{{ checkbox|component }}
-==
-
 
