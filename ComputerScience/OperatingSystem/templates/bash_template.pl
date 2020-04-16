b2ced4834278f12122dac4645855ca49585b9742
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-16 10:39:25 +0200

    NB modified Yggdrasil/ComputerScience/OperatingSystem/templates/bash_template.pl

--- ComputerScience/OperatingSystem/templates/bash_template.pl
+++ ComputerScience/OperatingSystem/templates/bash_template.pl
@@ -71,7 +71,7 @@ else:
     form += '<input id="form_user_hack" name="form_user_hack" type="hidden" value="{{ user }}">'
     form += display_as_shell_this(editor.code, spout, str(response["user_hack"]), errout, returncode)
 
-    if expected_output == spout:
+    if expected_stdput == spout:
         feedback = "Bravo, votre code fait le travail !"
         grade = (100, feedback)
     else:
