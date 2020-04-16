fbe3e594395ea54828417de78480421317d141d8
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-16 09:43:04 +0200

    NB modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/wc_germinal.pl

--- ComputerScience/OperatingSystem/exercises/Bash/wc_germinal.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/wc_germinal.pl
@@ -1,4 +1,20 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+extends=/ComputerScience/OperatingSystem/templates/bash_template.pl
+
+title=Comptage textuel dans Germinal
 
 
-
-extends=/ComputerScience/C/template/stdsandboxC.pl
