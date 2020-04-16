e710a88ac0e61603229bb0f9adc30b8e586a7f36
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 15:53:43 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/recognition.pl

--- ComputerScience/automaton/templates/recognition.pl
+++ ComputerScience/automaton/templates/recognition.pl
@@ -1,6 +1,15 @@
-extends = base.pl
+
+extends = ../templates/base.pl
 
 maxattempt = 3
+
+title= 
+text= 
+form ==
+{{ viewer|component }}
+{{ checkbox|component }}
+==
+
 
 before== #|py|
 
@@ -16,29 +25,16 @@ evaluator== #|py|
 
 from evaluator import Evaluator
 
-right, total = Evaluator.eval_multi_choice_question(
+nb_ok, total = Evaluator.eval_multi_choice_question(
     checkbox,
     answers,
     withanswer=True
 )
 checkbox.disabled = True
 
-if right == total:
+if nb_ok == total:
     grade = (100, '<p class="success-state">Bonne réponse</p>')
 else:
     grade = ((right / total) * 100, f"<p class='warning-state'>{right} / {total}</p>")
 ==
-
-title= Lecture d'automates
-
-text==
-Parmi les mots suivants, lesquels sont reconnus par cet automate?
-==
-
-form ==
-{{ viewer|component }}
-{{ checkbox|component }}
-==
-
-
 
