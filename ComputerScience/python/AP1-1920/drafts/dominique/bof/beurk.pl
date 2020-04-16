71c0a18f1f4d05dfe1c2791887c30a949a0f6b02
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-16 18:42:49 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/bof/beurk.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/bof/beurk.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/bof/beurk.pl
@@ -13,22 +13,32 @@ text = <em>(Pas d'énoncé défini)</em>
 # création de l'éditeur de code
 editor1 =: CodeEditor
 editor1.language = python
-editor1.cid = py_editor_0
+editor1.cid = py_editor_1
 editor1.code ==
 # Saisissez votre code ici, et cliquez sur le bouton
 # de validation quand vous avez terminé
 
 ==
 
-# le formulaire de l'exercice contient uniquement l'éditeur
-form==
-{{editor1|component}}
+editor2 =: CodeEditor
+editor2.language = python
+editor2.cid = py_editor_2
+editor2.code ==
+# Saisissez votre code ici, et cliquez sur le bouton
+# de validation quand vous avez terminé
+
 ==
 
-# définition de la procédure de validation
-grader==
-begin_test_group("L'exercice n'a pas défini de procédure de validation.")
+
+# le formulaire de l'exercice contient uniquement l'éditeur
+form==
+Entrez votre classe 
+{{editor1|component}}
+et vos fonctions 
+{{editor2|component}}
 ==
+
+
 
 
 
