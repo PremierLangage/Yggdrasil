619857eb549c5906d50b1412fdf3b9ebc38f8844
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 11:49:21 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/editor.pl

--- ComputerScience/automaton/templates/editor.pl
+++ ComputerScience/automaton/templates/editor.pl
@@ -117,15 +117,16 @@ form== #|html|
     }
 </script>
 <p>
-    <ul>
+    <ion-list>
         <li> Pour créer un nouvel état, double clic gauche. Utilisez les boutons pour changer le type de votre état.<br>
-        <it> Ne pas renommer les états - il y a un bug</it></li>
+            <it> Ne pas renommer les états - il y a un bug</it>
+        </li>
         <li>
             Pour ajouter une transition cliquer sur le point orange de l'état de départ
             et tirer votre transition jusqu'à l'état d'arrivée.
             Utilisez ensuite "changer Transition" pour ajuster la lettre étiquettant de la transition
         </li>
-    </ul>
+    </ion-list>
 </p>
 ==
 
