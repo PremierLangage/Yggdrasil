1a664a33e4171eec1048804fddf6608ee15c53e3
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 11:31:14 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/editor.pl

--- ComputerScience/automaton/templates/editor.pl
+++ ComputerScience/automaton/templates/editor.pl
@@ -35,10 +35,13 @@ deterministic = True
 if 'generate' not in globals():
     raise Exception('You must define a script "generate"')
 
+if 'expected' not in globals():
+    raise Exception('The script "generate" must define a variable "expected" which is an automaton')
+
 exec(generate)
 
 editor = Automaton.editor()
-viewer = Automaton.viewer(automaton)
+viewer = Automaton.viewer(expected)
 alphabet = ', '.join(list(alphabet))
 ==
 
