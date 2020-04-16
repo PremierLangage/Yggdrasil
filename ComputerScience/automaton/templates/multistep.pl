bfd5c7810a28efadccfdf93362fee6f1a3c9c7b6
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 13:24:38 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/multistep.pl

--- ComputerScience/automaton/templates/multistep.pl
+++ ComputerScience/automaton/templates/multistep.pl
@@ -24,7 +24,7 @@ viewer = Automaton.viewer(automaton)
 # TRANSITION TABLE
 table = Generator.transition_table(automaton, globals())
 for state, inputs in table['rows'].items():
-    raise Exception((inputs[0].cid))
+    raise Exception((inputs[0].selector))
 
 # RANDOM MULTI CHOICE QUESTION FOR MATCHING WORDS
 checkbox, words = Generator.multichoice_question(automaton)
