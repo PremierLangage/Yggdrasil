a19159f88b40c33048e147a4e281a067536ec3f2
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-16 12:59:06 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/templates/multistep.pl

--- ComputerScience/automaton/templates/multistep.pl
+++ ComputerScience/automaton/templates/multistep.pl
@@ -39,6 +39,7 @@ elif matched is True:
     score, step = 100 / maxSteps, step + 1
     grade = (score , ' ')
 else:
+    step += 1
     grade = (score , '<p class="error-state">Votre automate ne correspond à la réponse attendue</<p>')
 ==
 
