610224c0cbee2115c77b51149d8c9afa8f5d0cee
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-17 09:32:22 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02-acpm/graphe_pondere.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02-acpm/graphe_pondere.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02-acpm/graphe_pondere.pl
@@ -1,5 +1,5 @@
 # Test d'une classe Graphe implémentant un graphe pondéré non orienté.
-#@ /utils/sandboxio.py
+@ /utils/sandboxio.py
 extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 builder= /builder/before.py
 
