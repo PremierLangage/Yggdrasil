08420bcd6684bfe4e74a95dd0a0e9d24b2999d6b
www-data <www-data@platon-edu.fr>
2020-04-17 12:37:30 +0200

    admin modified Yggdrasil/Mathematics/exercises/plots/graph.pl

--- Mathematics/exercises/plots/graph.pl
+++ Mathematics/exercises/plots/graph.pl
@@ -52,7 +52,7 @@ graphe_2 = render_plot(plot(fonction2,(x,-2, 2), ylim=(-3,3)))
 text== 
 Parmi les deux graphes suivant, lequel est le plus joli?
 <br>
-<div style="display:inline-block">{{ graphe_1 | safe }}</div>
+<div style="display:inline-block; width:300px; border:2px solid black;">{{ graphe_1 | safe }}</div>
 <span style="width:300px;">{{ graphe_2 | safe }}</span>
 == 
 
