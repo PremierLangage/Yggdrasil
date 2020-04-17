9a80374e34028f99509aae2ec11c645fac9da1fb
www-data <www-data@platon-edu.fr>
2020-04-17 12:26:13 +0200

    admin modified Yggdrasil/Mathematics/exercises/plots/graph.pl

--- Mathematics/exercises/plots/graph.pl
+++ Mathematics/exercises/plots/graph.pl
@@ -45,8 +45,8 @@ for j in range(3):
     fonction2 = fonction2 + rd.uniform(-1,1)*sin(rd.uniform(-5,5)*x+rd.uniform(-3,3))
 
 graphe_1 = render_plot(plot(fonction1,(x,-2, 2), ylim=(-3,3)))
-graphe_2 = render_plot(plot(fonction2,(x,-2, 2)))
-graphe_3 = render_plot(plot(sin(2*sin(2*sin(x)))))
+graphe_2 = render_plot(plot(fonction2,(x,-2, 2), ylim=(-3,3)))
+graphe_3 = render_plot(plot(sin(2*sin(2*sin(x))), ylim=(-3,3)))
 ==
 
 
