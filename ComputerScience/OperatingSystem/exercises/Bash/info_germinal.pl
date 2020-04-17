d822c40aeba66eac7188b01850e73f035900ad40
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-17 16:24:50 +0200

    NB modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/info_germinal.pl

---
+++ ComputerScience/OperatingSystem/exercises/Bash/info_germinal.pl
@@ -0,0 +1,40 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of the GNU General Public License (GPL)
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#  The full text of the GPL is available at:
+#
+#                  http://www.gnu.org/licenses/
+#*****************************************************************************
+
+extends=/ComputerScience/OperatingSystem/templates/bash_template.pl
+
+# Add the file containing integral text of Germinal by Zola
+@ Germinal.txt
+
+author=Nicolas Borie
+title=Information à extraire du texte de Germinal
+
+before==
+import random
+
+things = [("le nombre de lignes vides", "3283\n")]
+(name_thing, expected_stdout) = random.choice(things)
+==
+
+text==
+Un fichier **Germinal.txt** (contenant le texte intégral de l'oeuvre de E. Zola) a 
+été placé dans le répertoire courant de travail. Établissez une **commande shell** 
+qui affiche **{{ name_thing }}** apparaissant dans le fichier. Débrouillez 
+vous pour que votre résultat numérique soit affiché seul sur une ligne. N'hésitez pas à
+faire plusieurs essais.
+==
+
+
+
