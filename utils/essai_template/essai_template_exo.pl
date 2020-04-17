60c9f530a024ce439d62ba3189a2297e4efa9c37
www-data <www-data@platon-edu.fr>
2020-04-17 15:59:31 +0200

    admin modified Yggdrasil/utils/essai_template/essai_template_exo.pl

--- utils/essai_template/essai_template_exo.pl
+++ utils/essai_template/essai_template_exo.pl
@@ -8,10 +8,10 @@ before== #|python|
 from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
 
 # contenu des labels
-integrale = r"$$\int_0^1 x^2\, dx$$" # bug d'affichage: \, affiche une virgule
+integrale = r"$$\int_0^1 x^2\\, dx$$" # bug d'affichage: \, affiche une virgule
 fraction =  r"$$\frac12 - \frac13$$"
 limite = r"$$\lim_{x\to +\infty} x\sin(1/x)$$"
-determinant = r"$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$" # bug d'affichage: la matrice s'affiche sur une ligne
+determinant = r"$$\det\begin{pmatrix} 1 & 2\\\\ 1 & 3\end{pmatrix}$$" # bug d'affichage: la matrice s'affiche sur une ligne
 
 
 
@@ -38,6 +38,12 @@ d = mygroup.labels["limite"]
 e = mygroup.labels["fraction"]
 f = mygroup.labels["determinant"]
 g = mygroup._matches
+a.css='drop'
+b.css='drop'
+c.css = 'drag'
+d.css = 'drag'
+e.css = 'drag'
+f.css = 'drag'
 ==
 
 text==
@@ -66,14 +72,95 @@ grade=mygroup.eval(grading_function = right_minus_wrong)
 
 extracss == #|html| 
 <style>
-    .myclass{
-        #padding: 1em;
-        min-width: 4em;
-        }
+    .drag{
+    width: 6em;
+	height: 4em;
+    overflow:hidden;
+    }
+
+    .drop{
+    width: 6em;
+	height: 4em;
+    overflow:hidden;
+    }
+    .success{
+    background-color:lightGreen;
+    }
+    .error{
+    background-color:lightPink;
 </style>
 ==
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+custom_pl_template == #|html|
+    <ion-card-header class="exercise__header">
+        <ion-card-subtitle class='exercise__author'>{% if author %}{{ author }}{% endif %}</ion-card-subtitle>
+        <ion-card-title class='exercise__title'>{% if title %}{{ title }}{% endif%}</ion-card-title>
+    </ion-card-header>
+    <!-- BODY -->
+    <ion-card-content class="exercise__body">
+        <!-- FORM -->
+        {% csrf_token %}
+        <div class="exercise__form">
+            {% block form %}{{ form|safe }}{% endblock %}
+        </div>
+        <br/>
+        <!-- INSTRUCTIONS -->
+        <div class="exercise__instructions">
+            {% if text %}
+            {{ text|markdown|safe }}
+            {% endif %}
+        </div>
+        <!-- FOOTER -->
+        <ion-footer class="ion-no-border">
+        <!-- FEEDBACK -->
+        <div class="exercise__feedback">
+            {{ feedback__|markdown|safe if feedback else "" }}
+        </div>
+        <!-- ACTIONS -->
+        <div class="exercise__actions text-center">
+            <div class="btn-group" role="group" aria-label="actions">
+                <button class="btn btn-primary action-submit">
+                    <i id="validate" class="fas fa-check"></i>
+                    <span class="ion-hide-md-down">Valider</span>
+                </button>
+                {% if tests %}
+                <a type="button" class="btn btn-primary action-test"
+                href="{{ url('playexo:test_pl', pl_id__)}}"
+                target="_blank" test>
+                    <i class="fas fa-angle-double-right"></i>
+                    <span class="ion-hide-md-down">Tester</span>
+                </a>
+                {% endif %}
+                <a type="button" class="btn btn-secondary action-download-env"
+                href="{{ url('filebrowser:dlenv', id__) }}" download>
+                    <i class="fas fa-download"></i>
+                    <span class="ion-hide-md-down">Télécharger l'Environnement</span>
+                </a>
+            </div>
+        </div>
+        <!-- SPINNER -->
+        <div class="exercise__spinner text-center">
+            <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        </ion-footer>
+    </ion-card-content>
+
+==
 
 
 
