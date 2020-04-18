876eee4c112aa496c2f36996f82f01f3cb0c2daa
www-data <www-data@platon-edu.fr>
2020-04-18 15:23:47 +0200

    admin modified Yggdrasil/utils/components/dragdrop_example2.pl

--- utils/components/dragdrop_example2.pl
+++ utils/components/dragdrop_example2.pl
@@ -2,52 +2,66 @@ extends = /model/basic.pl
 
 @ /utils/components/dragdrop.py [customdragdrop.py]
 
-title = Comparaison de nombres (DragDrop)
+title = Drag Drop mathématique
 
-before==
-import random as rd
+before== #|python|
 from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
 
-lt="$%\lt%$"
-gt="$%\gt%$"
+# contenu des labels
+integrale = r"$$\int_0^1 x^2\, dx$$" # bug d'affichage: \, affiche une virgule
+fraction =  r"$$\frac12 - \frac13$$"
+limite = r"$$\lim_{x\to +\infty} x\sin(1/x)$$"
+determinant = r"$$\det\begin{pmatrix} 1 & 2\\ 1 & 3\end{pmatrix}$$" # bug d'affichage: la matrice s'affiche sur une ligne
 
-n = 4
 
+
+# contenu des boutons "drop"
+qui_vaut_1 = r"Poser ici une expression égale à $$1$$"
+qui_vaut_tiers = r"Poser ici une expression égale à $$1/3$$"
+
+# fabrication du groupe
 mygroup = DragDropGroup()
-mygroup.set_label({"lt": lt, "gt": gt})
+# les labels
+mygroup.set_label({"integrale": integrale, "fraction": fraction, "limite":limite, "determinant": determinant})
+# les drops
+mygroup.set_drop({"1" : qui_vaut_1, "tiers": qui_vaut_tiers})
+# les liens corrects
+mygroup.set_match_by_name("tiers", "integrale")
+mygroup.add_match_by_name("1", ["limite","determinant"])
 
-numbers = []
-for i in range(n):
-    [a,b] = rd.sample(range(10,100),2)
-    numbers.append([a,b])
-    mygroup.add_drop({str(i):"coucou"}) # à modifier  en remplaçant str(i) par i quand les labels/drops pourront être des listes et non des dict.
-    if a < b:
-        mygroup.add_match_by_content(str(i),lt) 
-    else:
-        mygroup.add_match_by_content(str(i),gt) 
-
-# Pour le moment ça bugge: les étiquettes sont vides.
-
-a = mygroup.labels['lt']
-b = mygroup.labels['gt']
+# Ce qui suit sert uniquement à faire voyager le groupe dans le grader
+a = mygroup.drops['1'] 
+# bug incroyable : si le nom est 'q' au lieu de 'a', et qu'on remplace dans le text/evaluator, ça bugge
+b = mygroup.drops["tiers"]
+c = mygroup.labels["integrale"]
+d = mygroup.labels["limite"]
+e = mygroup.labels["fraction"]
+f = mygroup.labels["determinant"]
+g = mygroup._matches
 ==
 
 text==
-Comparer les nombres suivants avec les symboles {{ a | component }} et {{ b | component }}.
+Question : {{ a | component }} {{ b | component }}
 ==
 
 form==
-<ul>
-{% for i in mygroup.drops %}
-<li> {{ numbers[loop.index0][0] }} {{ mygroup.drops[i] | component }} {{ numbers[loop.index0][1] }} </li>
-{% endfor %}
-</ul>
+{{ c | component }}
+{{ d | component }}
+{{ e | component }}
+{{ f | component }}
+
 ==
 
 settings.feedback = lightscore
 
 evaluator==
-grade=mygroup.eval() # Ici ça bugge, mais c'est normal parce que le composant ne traverse pas
+from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
+mygroup = DragDropGroup()
+mygroup.labels = {"integrale": c, "limite": d, "fraction": e, "determinant": f}
+mygroup.drops = {"1": a, "tiers": b}
+mygroup._matches = g
+
+grade=mygroup.eval(grading_function = right_minus_wrong) 
 ==
 
 extracss == #|html| 
@@ -58,6 +72,12 @@ extracss == #|html|
         }
 </style>
 ==
+
+
+
+
+
+
 
 
 
