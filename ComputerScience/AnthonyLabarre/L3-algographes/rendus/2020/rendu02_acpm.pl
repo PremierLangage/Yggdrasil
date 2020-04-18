a3f40d7ee3b06dd56c08de387e6f5381c84b3635
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-18 17:15:16 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
@@ -12,8 +12,8 @@ text==
 <li>une classe Tas implémentant une file à priorité
 <li>une classe UnionFind
 <li>acpm_prim(graphe, sommet), qui prend en paramètres un graphe et un sommet de départ et renvoie un ACPM pour la composante connexe du sommet donné en suivant l'algorithme de Prim;
-<li>
-<li>
+<li>acpm_kruskal(graphe), qui prend en paramètre un graphe et renvoie la forêt couvrante de poids minimum pour ce graphe en suivant l'algorithme de Kruskal;
+<li>fcpm_prim(graphe), qui prend en paramètre un graphe et renvoie la forêt couvrante de poids minimum pour ce graphe en suivant l'algorithme de Prim.
 </ul>
 ==
 # Les tests:
