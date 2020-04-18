4d415bc10b74b251722c90f4c661e20950a4fca0
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-18 17:52:28 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/bash_bdd.pl

--- ComputerScience/OperatingSystem/exercises/Bash/bash_bdd.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/bash_bdd.pl
@@ -36,7 +36,11 @@ text==
 Un fichier **mjc.txt** a été placé dans votre répertoire courant de travail. 
 Ce fichier est structuré de manière très sérieuse en ligne. Chaque ligne correspond à
 un membre de la MJC de la capitale de la Syldavie. Chaque ligne suit la structure suivante :   
-**id:prénom:nom:age:activité lundi,activité mardi,...,activité dimanche:jour:mois:année**
+**id:prénom:nom:age:activité lundi,activité mardi,...,activité dimanche:jour:mois:année**   
+À titre d'exemple, la première ligne du fichier est ainsi :   
+**756430947:Lina:Leroy:46:,,,,,,:12:Jun:2018**   
+Lina Leroy est donc le membre 756430947 de la MJC. Elle a 46 ans et ne suit aucune activité
+en ce moment. Elle est membre de la MJC depuis le 12 Juin 2018.
 ==
 
 
