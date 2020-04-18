171d33547d79ae9cbce3631a6daf4d6f96d75eed
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-18 17:12:43 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
@@ -5,10 +5,11 @@ builder= /builder/before.py
 
 author = Anthony Labarre
 title = Arbres (et forêts) couvrants de poids minimum
-text = Écrivez: 
+text == Écrivez: 
 <ul>
 <li>une classe Graphe implémentant un graphe pondéré non orienté.; * une classe Tas implémentant une file à priorité; * une classe UnionFind; ...
 </ul>
+==
 # Les tests:
 
 pltest0==
