f29c94982779ec39a680436566460f058b4d4ecd
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-19 13:25:33 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
@@ -83,7 +83,7 @@ pltest3==
 # Vérifications de acpm_prim sur un graphe connexe
 pltest4==
 >>> import random; G = Graphe(); G.ajouter_aretes([(0, 1, 5), (0, 3, 4), (0, 4, 2), (1, 4, 2), (2, 3, 9), (2, 5, 5), (3, 4, 3), (3, 5, 7), (3, 6, 4), (4, 6, 7), (5, 6, 12)]); arbre = acpm_prim(G, random.choice(list(G.sommets())))
->>> sum(poids for *_, poids in G.aretes())
+>>> sum(poids for *_, poids in arbre.aretes())
 23
 ==
 
