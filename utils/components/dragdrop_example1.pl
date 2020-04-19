13fb2e3d9f3e321e2232034e2da35564d495e2bc
www-data <www-data@platon-edu.fr>
2020-04-19 14:33:33 +0200

    admin modified Yggdrasil/utils/components/dragdrop_example1.pl

--- utils/components/dragdrop_example1.pl
+++ utils/components/dragdrop_example1.pl
@@ -24,9 +24,17 @@ for i in range(n):
         mygroup.add_match_by_content(i,lt) 
     else:
         mygroup.add_match_by_content(i,gt) 
+
+a=mygroup.labels['lt']
+b=mygroup.labels['gt']
+c=mygroup.drops[0]
+d=mygroup.drops[1]
+e=mygroup.drops[2]
+f=mygroup.dropps[3]        
 ==
 
 text ==
+{{ a }} {{ b }}
 {% for label in mygroup.labels.values() %} {{ label|component }} {% endfor %}
 ==
 
@@ -35,6 +43,14 @@ form ==#|html|
 {% for i, drop in mygroup.drops.items() %}
 <li> {{ numbers[i|int][0] }} {{ drop|component }} {{ numbers[i|int][1] }} </li>
 {% endfor %}
+</ul>
+<ul>
+
+<li> {{ numbers[0][0] }} {{ c|component }} {{ numbers[0][1] }} </li>
+<li> {{ numbers[1][0] }} {{ d|component }} {{ numbers[&][1] }} </li>
+<li> {{ numbers[2][0] }} {{ e|component }} {{ numbers[2][1] }} </li>
+<li> {{ numbers[3][0] }} {{ f|component }} {{ numbers[3][1] }} </li>
+
 </ul>
 
 ==
@@ -55,6 +71,7 @@ extracss == #|html|
         }
 </style>
 ==
+
 
 
 
