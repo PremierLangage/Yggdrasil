40ba76aaabeb2e00b0e39948ae488432bba15d79
www-data <www-data@platon-edu.fr>
2020-04-19 16:24:44 +0200

    admin modified Yggdrasil/utils/components/dragdrop_example2.pl

--- utils/components/dragdrop_example2.pl
+++ utils/components/dragdrop_example2.pl
@@ -69,7 +69,7 @@ mygroup.labels = {"integrale": c, "limite": d, "fraction": e, "determinant": f}
 mygroup.drops = {"1": a, "tiers": b}
 mygroup._matches = g
 
-grade=(100,str(mygroup))#mygroup.eval(grading_function = right_minus_wrong) 
+grade=(100,str(a))#mygroup.eval(grading_function = right_minus_wrong) 
 ==
 
 extracss == #|html| 
