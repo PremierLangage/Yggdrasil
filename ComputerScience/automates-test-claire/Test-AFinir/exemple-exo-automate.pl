28dba2fe3fc4016deb233688663b63f9affbfea9
Claire DAVID <Claire.David@u-pem.fr>
2020-04-21 00:26:04 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/Test-AFinir/exemple-exo-automate.pl

--- ComputerScience/automates-test-claire/Test-AFinir/exemple-exo-automate.pl
+++ ComputerScience/automates-test-claire/Test-AFinir/exemple-exo-automate.pl
@@ -15,13 +15,13 @@ viewer = AutomatonEditor(
     automaton= {
         "alphabet": ["a", "b"],
         "initialStates": ["S1"],
-        "acceptingStates": ["S5"],
+        "acceptingStates": ["S2"],
         "states": ["S1", "S2", "S3","S4","S5"],
         "transitions": [
             { "fromState": "S1", "toState": "S2", "symbols": ["b"] },
             { "fromState": "S1", "toState": "S3", "symbols": ["b"] },
             { "fromState": "S1", "toState": "S4", "symbols": ["b"] },
-#            { "fromState": "S2", "toState": "S3", "symbols": ["b"] },
+            { "fromState": "S2", "toState": "S3", "symbols": ["b"] },
 #            { "fromState": "S2", "toState": "S4", "symbols": ["b"] },
 #            { "fromState": "S2", "toState": "S2", "symbols": ["a"] },
 #            { "fromState": "S2", "toState": "S2", "symbols": ["b"] },
