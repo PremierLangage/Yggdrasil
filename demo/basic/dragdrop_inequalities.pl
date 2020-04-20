938edbd708124fc731b4031897e8400dce4cb903
David DOYEN <David.Doyen@u-pem.fr>
2020-04-20 13:12:03 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop_inequalities.pl

--- demo/basic/dragdrop_inequalities.pl
+++ demo/basic/dragdrop_inequalities.pl
@@ -24,7 +24,7 @@ for i in range(n):
 ==
 
 text ==
- <img src="{{scheme}}" style="max-width: 300px;"> 
+ <img src="{{scheme}}" style="max-width: 400px;"> 
 
 {% for label in mygroup.labels.values() %} {{ label|component }} {% endfor %}
 ==
