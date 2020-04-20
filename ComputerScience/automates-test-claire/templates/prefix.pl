71533bf0327da520fea8f84a230cd3d04de635bc
Claire DAVID <Claire.David@u-pem.fr>
2020-04-20 22:35:26 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/prefix.pl

--- ComputerScience/automates-test-claire/templates/prefix.pl
+++ ComputerScience/automates-test-claire/templates/prefix.pl
@@ -1,6 +1,6 @@
 extends = editor.pl
 
-maxattempt = 5
+maxattempt = 10
 
 generate== #|py|
 from generator import Generator
@@ -18,11 +18,12 @@ On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui commencent par **{{ prefix }}** {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
-Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solution !
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 
 ==
+
 
 
 
