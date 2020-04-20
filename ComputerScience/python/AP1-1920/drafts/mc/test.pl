d6aa7f863e56e95aab9fee520012c64983c40a7f
www-data <www-data@platon-edu.fr>
2020-04-20 12:22:59 +0200

    admin modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/mc/test.pl

---
+++ ComputerScience/python/AP1-1920/drafts/mc/test.pl
@@ -0,0 +1,45 @@
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+inputbox =: Input
+inputbox.type = number
+inputbox.placeholder = Answer
+inputbox.maxlength = 2
+inputbox.appearance = outline
+
+before==
+import random
+
+a = random.randint(1, 10)
+b = random.randint(1, 10)
+r = a + b
+
+dico = {
+"input": inputbox
+}
+
+==
+
+title==
+Input Component
+==
+
+text==
+Enter the result of ** {{ a }} + {{ b }} ** inside the input box.
+==
+
+form==
+{{ dico["input"] }}
+{{ inputbox|component}}
+==
+
+evaluator==
+if r == inputbox.value:
+    grade = (100, '<span class="success-state">Good 👏👏👏</span>')
+else:
+    grade = (0, '<span class="error-state">Bad answer 👎👎👎</span>')
+==
+
+
