02a83a2f80d6b3bffe4eedcfa85f60e285625761
www-data <www-data@platon-edu.fr>
2020-04-20 09:47:52 +0200

    admin modified Yggdrasil/utils/essai_template_exo/essai_template_exo.pl

--- utils/essai_template_exo/essai_template_exo.pl
+++ utils/essai_template_exo/essai_template_exo.pl
@@ -1,101 +1,69 @@
+
 extends = /model/basic.pl
 
-@ /utils/components/dragdrop.py [customdragdrop.py]
+title = Graphes de fonctions
 
-title = Drag Drop mathématique
-
-before== #|python|
-from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
-
-# contenu des labels
-integrale = r"$$\int_0^1 x^2\\, dx$$" # bug d'affichage: \, affiche une virgule
-fraction =  r"$$\frac12 - \frac13$$"
-limite = r"$$\lim_{x\to +\infty} x\sin(1/x)$$"
-determinant = r"$$\det\begin{pmatrix} 1 & 2\\\\ 1 & 3\end{pmatrix}$$" # bug d'affichage: la matrice s'affiche sur une ligne
-
-
-
-# contenu des boutons "drop"
-qui_vaut_1 = r"Poser ici une expression égale à $$1$$"
-qui_vaut_tiers = r"Poser ici une expression égale à $$1/3$$"
-
-# fabrication du groupe
-mygroup = DragDropGroup()
-# les labels
-mygroup.set_label({"integrale": integrale, "fraction": fraction, "limite":limite, "determinant": determinant})
-# les drops
-mygroup.set_drop({"1" : qui_vaut_1, "tiers": qui_vaut_tiers})
-# les liens corrects
-mygroup.set_match_by_name("tiers", "integrale")
-mygroup.add_match_by_name("1", ["limite","determinant"])
-
-# Ce qui suit sert uniquement à faire voyager le groupe dans le grader
-a = mygroup.drops['1'] 
-# bug incroyable : si le nom est 'q' au lieu de 'a', et qu'on remplace dans le text/evaluator, ça bugge
-b = mygroup.drops["tiers"]
-c = mygroup.labels["integrale"]
-d = mygroup.labels["limite"]
-e = mygroup.labels["fraction"]
-f = mygroup.labels["determinant"]
-g = mygroup._matches
-a.css='drop'
-b.css='drop'
-c.css = 'drag'
-d.css = 'drag'
-e.css = 'drag'
-f.css = 'drag'
-==
-
-text==
-Question :
-{{ a | component }} {{b | component }}
-==
-
-form==
-{{ c | component }}
-{{ d | component }}
-{{ e | component }}
-{{ f | component }}
-==
+lang = fr
 
 settings.feedback = lightscore
 
-evaluator==
-from customdragdrop import CustomDragDrop, DragDropGroup, right_minus_wrong
-mygroup = DragDropGroup()
-mygroup.labels = {"integrale": c, "limite": d, "fraction": e, "determinant": f}
-mygroup.drops = {"1": a, "tiers": b}
-mygroup._matches = g
 
-grade=mygroup.eval(grading_function = right_minus_wrong) 
+before==#|python|
+
+from sympy import *
+import random as rd
+from sympy.plotting import plot
+from jinja2 import Template
+import matplotlib
+matplotlib.rcParams['savefig.dpi'] = 80
+matplotlib.rcParams['savefig.transparent'] = True
+from io import BytesIO
+import base64
+
+def render_plot(arg):# arg must be a matplotlib plot
+    figfile = BytesIO()
+    arg.save(figfile)
+    figfile.seek(0)  # rewind to beginning of file
+    figdata_png = base64.b64encode(figfile.getvalue()).decode('ascii')
+    figfile.close()
+    arg._backend.close()
+
+    return  Template('<img src="data:image/png;base64,{{ plot_data }}" \
+        style="pointer-events:none;">').\
+        render({'plot_data': figdata_png})
+
+x = symbols('x')
+
+graphe_1 = render_plot(plot(x**2))
+graphe_2 = render_plot(plot(sin(x)))
+
 ==
 
-extracss == #|html| 
-<style>
-    .drag{
-    width: 6em;
-	height: 4em;
-    overflow:hidden;
-    }
 
-    .drop{
-    width: 6em;
-	height: 4em;
-    overflow:hidden;
-    }
-    .success{
-    background-color:lightGreen;
-    }
-    .error{
-    background-color:lightPink;
-    }
-</style>
+text== 
+Parmi les deux graphes suivants, lequel est le plus joli?
+<br>
+<div style="display:inline-block; width:40%; border:2px solid black;">{{ graphe_1 | safe }}</div>
+<div style="display:inline-block; width:40%; border:2px solid black;">{{ graphe_2 | safe }}</div>
+<br>
+== 
+
+form==
+<select id = 'form_1'>
+	<option value='oui'> Le premier, bien sûr </option>
+	<option value='non'> Le second, of course </option>
+	<option value='bof'> што такой ?? </option>
+	<option value='blip'> WTF ? </option>
+</select>
 ==
 
+evaluator ==
+import random as rd
 
-
-
-
+f = rd.choice(['Excellente réponse', 'Merveilleuse réponse', 'Fabuleuse réponse', 'Délicieuse réponse', 'Charmante réponse', 'C\'est également mon avis', 'On ne saurait mieux dire', 'Cela est si juste!'])
+s = 100
+grade = (s, f)
+==
 
 
 
@@ -162,6 +130,7 @@ custom_pl_template == #|html|
     </ion-card-content>
 
 ==
+
 
 
 
