23dad5eaf25bf4cb692dfd28f81e122703ac36cb
Claire DAVID <Claire.David@u-pem.fr>
2020-04-20 23:12:54 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-Claire-David/templates/base.pl

---
+++ ComputerScience/Automates-Claire-David/templates/base.pl
@@ -0,0 +1,24 @@
+@ /utils/sandboxio.py
+@ ../scripts/fsm.py
+@ ../scripts/lego.py
+@ ../scripts/automaton.py
+@ ../scripts/generator.py
+@ ../scripts/evaluator.py
+
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+
+styles =@ Yggdrasil/ComputerScience/Automates-Claire-David/styles/automaton.css
+extracss==
+<style> {{ styles }}</style>
+==
+
+
+
+
+
+
+
+
+
