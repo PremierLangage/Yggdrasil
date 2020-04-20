69d920ada010cc23fcc135a92abaa08c64c54211
Claire DAVID <Claire.David@u-pem.fr>
2020-04-20 19:21:58 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/2020-partiel-exo1-1.pl

--- ComputerScience/automates-test-claire/templates/2020-partiel-exo1-1.pl
+++ ComputerScience/automates-test-claire/templates/2020-partiel-exo1-1.pl
@@ -6,7 +6,7 @@ extends = editor.pl
 
 ## On doit pouvoir utiliser le même style que pour count/prefix/suffix/factor pour récupérer un automate et le comparer
 
-maxattempt = 5
+maxattempt = 10
 
 generate== #|py|
 ## Ajouter ici la génération:
