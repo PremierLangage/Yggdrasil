8a9bdb8ed2b24cbfeb30a78cec687261dba9f045
Claire DAVID <Claire.David@u-pem.fr>
2020-04-20 23:49:08 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/Test-AFinir/exemple-exo-automate.pl

--- ComputerScience/automates-test-claire/Test-AFinir/exemple-exo-automate.pl
+++ ComputerScience/automates-test-claire/Test-AFinir/exemple-exo-automate.pl
@@ -38,7 +38,7 @@ message = "l'ensemble des mots qui commencent par ba et dont la longueur est pai
 # peut-on utiliser la syntaxe du viewer ?
 ==
 
-title = Partiel 2020 Exo1-1
+title = Bla
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez un automate reconnaissant le langage correspondant à {{ message }} <br>
