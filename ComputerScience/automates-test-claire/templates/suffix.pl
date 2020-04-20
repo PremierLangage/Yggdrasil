b6fb06383948b189abf576b38e0419f0337259dc
Claire DAVID <Claire.David@u-pem.fr>
2020-04-20 19:33:59 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/suffix.pl

---
+++ ComputerScience/automates-test-claire/templates/suffix.pl
@@ -0,0 +1,26 @@
+
+extends = editor.pl
+
+maxattempt = 5
+
+generate== #|py|
+from generator import Generator
+alphabet, suffix, automaton = Generator.suffix(3, 3)
+==
+
+title = Automates Suffixes
+
+text == #|html|
+Pour cette question, on considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots se terminant par **{{ suffix }}** {{ '}' }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s)!
+</p>
+{% endif %}
+==
+
+
+
+
+
