244ef63a99189083b231829abd7c8fd61eefc8a9
www-data <www-data@platon-edu.fr>
2020-04-20 11:38:21 +0200

    admin modified Yggdrasil/builder/before2.py

--- builder/before2.py
+++ builder/before2.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     dic['attempt']=1
 
     for k,v in dic.items():
-        print(k,":",v, file=sys.stderr)
+        print(k,":",v, " ", type(v), file=sys.stderr)
     print("#################################################", file=sys.stderr)
     print("#################################################", file=sys.stderr)
 
