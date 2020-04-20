4b59148f270d51d45fcb50748791391fdb3e80a0
David DOYEN <David.Doyen@u-pem.fr>
2020-04-20 13:34:44 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop_inequalities.pl

--- demo/basic/dragdrop_inequalities.pl
+++ demo/basic/dragdrop_inequalities.pl
@@ -17,11 +17,10 @@ mygroup = DragDropGroup()
 labelscontent = ["Os frontal", "Os pariétal", "Os sphénoïde", "Os temporal", "Os occipital", "Os éthmoïde"]
 mygroup.set_label(labelscontent) 
 order = []
-for n,v in data:
-    order.append(n)
-    
-    mygroup.add_drop({str(n):""}) 
-    mygroup.add_match_by_content(str(n),v) 
+for i, v in enumerate(laberscontent):
+    order.append(i+1)
+    mygroup.add_drop({str(i):""}) 
+    mygroup.add_match_by_content(str(i),v) 
 ==
 
 text ==
