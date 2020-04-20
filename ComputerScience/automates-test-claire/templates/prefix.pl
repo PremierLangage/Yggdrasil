54ce5f83ca0a19395b672999a4ccf062ef29f3a0
Claire DAVID <Claire.David@u-pem.fr>
2020-04-20 22:37:20 +0200

    cdavid modified Yggdrasil/ComputerScience/automates-test-claire/templates/prefix.pl

--- ComputerScience/automates-test-claire/templates/prefix.pl
+++ ComputerScience/automates-test-claire/templates/prefix.pl
@@ -6,27 +6,44 @@ generate== #|py|
 from generator import Generator
 # A mettre ailleurs peut-être?
 import random
-# Generator.prefix(nb lettres alphabet , longueur maxi du prefix)
-nb_lettres = random.choice([2,3])
-alphabet, prefix, automaton = Generator.prefix(nb_lettres,3)
+#choisi le type d'exo
+#    -1: 'prefixe',
+#    0: 'facteur',
+#    1: 'suffixe'
+#mode = random.choice([-1, 0, 1])
+mode = -1
+messages = {
+    -1: 'commencent par',
+    0: 'contiennent',
+    1: 'finissent par'
+}
+bla = messages[mode]
 
+# choix de la taille de l'alphabet
+nb_lettres = random.choice([2,3])
+
+if mode == -1 :
+    # Generator.prefix(nb lettres alphabet , longueur maxi du prefix)
+    alphabet, mot, automaton = Generator.prefix(nb_lettres,3)
+elif mode == 0 :
+    # Generator.prefix(nb lettres alphabet , longueur maxi du facteur)
+    alphabet, mot, automaton = Generator.facteur(nb_lettres,3)
+else :
+    # Generator.prefix(nb lettres alphabet , longueur maxi du suffixe)
+    alphabet, mot, automaton = Generator.suffix(nb_lettres,3)
 ==
 
-title = Automates Préfixes
+title = Préfixe, suffixe ou facteur 
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui commencent par **{{ prefix }}** {{ '}' }} <br>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ bla }} par **{{ mot }}** {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
-Il vous reste {{ maxattempt - attempt }} tentative(s) !
+Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solution !
 </p>
 {% endif %}
 
 ==
-
-
-
-
 
 
 
