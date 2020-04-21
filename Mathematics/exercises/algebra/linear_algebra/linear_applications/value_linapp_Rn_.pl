a58612f10313595971f9dc06bae7264e54c2a757
David DOYEN <David.Doyen@u-pem.fr>
2020-04-22 00:33:04 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl
@@ -11,12 +11,15 @@ min = min([n, m])
 mat.setmatrix([min * [0] for _ in range(min)])
 
 A = rand_int_matrix_fullrank(m, n, 3)
+B = rand_int_matrix(n, 1, 3)
+
 expr = ",".join([latex_lincomb(A.row(j),['x','y','z','t']) for j in range(m)])
 ==
 
 text ==
 Soit l'application linéaire $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{m}} !$ dont la matrice par rapport aux bases canoniques des espaces de départ et d'arrivée est
-
+$$ M_f = \left( {{matrices[step].latex}} \right).$$
+Calculer 
 ==
 
 evaluator ==
