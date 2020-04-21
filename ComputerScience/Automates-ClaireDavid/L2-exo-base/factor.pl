5f3ce0991f715862d6ceca215b98cbd3a16198cf
Claire DAVID <Claire.David@u-pem.fr>
2020-04-22 00:30:49 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
@@ -30,13 +30,16 @@ nb_lettres = random.choice([2,3])
 
 if mode == -1 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du prefix)
-    alphabet, mot, automaton = Generator.prefix(nb_lettres,3)
+    alphabet, mot, viewer = Generator.prefix(nb_lettres,3)
 elif mode == 0 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du facteur)
-    alphabet, mot, automaton = Generator.factor(nb_lettres,3)
+    alphabet, mot, viewer = Generator.factor(nb_lettres,3)
 else :
     # Generator.prefix(nb lettres alphabet , longueur maxi du suffixe)
-    alphabet, mot, automaton = Generator.suffix(nb_lettres,3)
+    alphabet, mot, viewer = Generator.suffix(nb_lettres,3)
+
+
+alphabet = ', '.join(list(alphabet))
 ==
 
 title = Facteur
@@ -50,6 +53,8 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solutio
 {% endif %}
 
 ==
+
+
 
 
 
