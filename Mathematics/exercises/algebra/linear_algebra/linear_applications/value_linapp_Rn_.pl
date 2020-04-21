886d6e78693a7d6ae503b5db4dd31ed2c52e2d00
David DOYEN <David.Doyen@u-pem.fr>
2020-04-22 00:48:40 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl
@@ -10,20 +10,26 @@ lstvar = ",".join(["x","y","z","t","u","v"][:n])
 
 A = rand_int_matrix_fullrank(m, n, 3)
 B = rand_int_matrix(n, 1, 3)
-vec = str(tuple(B.transpose()))
+vec = tuple(B.transpose())
 expr = ",".join([latex_lincomb(A.row(j),['x','y','z','t']) for j in range(m)])
 ==
 
 text ==
 Soit l'application linéaire $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{m}} !$ dont la matrice par rapport aux bases canoniques des espaces de départ et d'arrivée est
 $$ M_f = \left( {{A.latex}} \right).$$
-Calculer $! f({{vec}}) !$. Ecrire la réponse sous forme d'un n-uplet (vecteur ligne).
+Calculer $! f{{vec.latex}} !$. Ecrire la réponse sous forme d'un n-uplet (vecteur ligne).
 ==
 
 evaluator ==
 score, _, feedback = eval_matrix(mat.getmatrix(), A)
 ==
 
-solution ==
-La solution est $$\left( {{A.latex}} \right).$$
+evaluator ==
+score, error, feedback = eval_tuple(input.value, sol, checksize=True)
+if error == "NotTuple" or error == "WrongSize":
+    feedback = f"Votre réponse n'est pas un {tuple_name} de nombres."
+==
+
+solution == 
+La solution est $! {{sol_tex}} !$.
 ==
