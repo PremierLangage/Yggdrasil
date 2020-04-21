f1d022085c20dc1d3b3f7d05b3e43920873f0d57
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-21 16:30:01 +0200

    drevuz modified Yggdrasil/Biology/exercises/drskull.pl

--- Biology/exercises/drskull.pl
+++ Biology/exercises/drskull.pl
@@ -4,4 +4,5 @@ extends = skulltemplate.pl
 labelscontent % ["Os frontal", "Os pariétal", "Os sphénoïde", "Os temporal", "Os occipital", "Os ethmoïde"]
 title = Os de la voûte cranienne
 barem=right_minus_wrong
+scheme =$ /Biology/media/550px-Cranial_bones_numbers.svg.png
 
