d35f60f60e69fd285c6f126ed3a08599f6f85a1d
David DOYEN <David.Doyen@u-pem.fr>
2020-04-22 01:22:48 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl
@@ -11,7 +11,10 @@ lstvar = ",".join(["x","y","z","t","u","v"][:n])
 A = rand_int_matrix(n, n, 2)
 B = rand_int_matrix(n, n, 2)
 X = rand_int_matrix(n, 1, 2)
-sol = (A * B * X).transpose()
+if randint(1,2) == 1:
+    sol = (A * B * X).transpose()
+else:
+    sol = (B * A * X).transpose()
 strsol = str(tuple(sol))
 vec = str(tuple(X.transpose()))
 ==
@@ -19,7 +22,7 @@ vec = str(tuple(X.transpose()))
 text ==
 Soit les applications linéaires $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{n}} !$ et $! g : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{n}} !$ dont les matrices par rapport aux bases canoniques des espaces de départ et d'arrivée sont
 $$ M_f = \left( {{A.latex}} \right) \quad et \quad M_g = \left( {{B.latex}} \right).$$
-Calculer $! f \circ g {{vec}} !$. Ecrire la réponse sous forme d'un n-uplet (vecteur ligne).
+Calculer $! {{expr}} {{vec}} !$. Ecrire la réponse sous forme d'un n-uplet (vecteur ligne).
 ==
 
 evaluator ==
