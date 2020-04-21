e84a81659522a158a06423b06f70874d10ab51c9
Claire DAVID <Claire.David@u-pem.fr>
2020-04-22 00:31:11 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/count.pl

---
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/count.pl
@@ -0,0 +1,44 @@
+extends = ../templates/editor.pl
+
+maxattempt = 10
+
+generate== #|py|
+import random
+from generator import Generator
+mode = random.choice([-1, 0, 1])
+messages = {
+    -1: 'au moins',
+    0: 'exactement',
+    1: 'au plus'
+}
+message = messages[mode]
+# counting( nb lettres alphabet, nb occurences demandées , mode)
+alphabet, letter, n, viewer = Generator.counting(3, 4, mode)
+
+
+alphabet = ', '.join(list(alphabet))
+==
+
+title = Nombre d'occurences
+text ==
+On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }} mots ayant {{ message }} **{{ n }}** occurrences de la lettre **{{ letter }}** {{ '}' }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir la solution !
+</p>
+{% endif %}
+
+==
+
+
+
+
+
+
+
+
+
+
+
+
