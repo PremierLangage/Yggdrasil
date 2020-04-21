1cc67b8290d929c865e62cd6e31bc0f9ae260c30
David DOYEN <David.Doyen@u-pem.fr>
2020-04-22 01:17:30 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl
@@ -0,0 +1,33 @@
+extends = /model/mathinput.pl
+
+title = Valeur d'une application linéaire
+
+before ==
+n, m, p = 3, 3, 3
+
+lstvar = ",".join(["x","y","z","t","u","v"][:n])
+
+
+A = rand_int_matrix(n, m, coeffbound)
+B = rand_int_matrix(m, p, coeffbound)
+X = rand_int_matrix(p, 1, 3)
+sol = (A * B * X).transpose()
+strsol = str(tuple(sol))
+vec = str(tuple(B.transpose()))
+expr = ",".join([latex_lincomb(A.row(j),['x','y','z','t']) for j in range(m)])
+==
+
+text ==
+Soit l'application linéaire $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{m}} !$ dont la matrice par rapport aux bases canoniques des espaces de départ et d'arrivée est
+$$ M_f = \left( {{A.latex}} \right).$$
+Calculer $! f{{vec}} !$. Ecrire la réponse sous forme d'un n-uplet (vecteur ligne).
+==
+
+evaluator ==
+score, _, feedback = eval_tuple(input.value, sol)
+==
+
+solution == 
+La solution est $! ({{strsol}}) !$.
+==
+
