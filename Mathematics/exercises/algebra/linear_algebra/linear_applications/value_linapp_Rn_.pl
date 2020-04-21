92612abfa5f4b571dbbd9857d846bbe9a78cb982
David DOYEN <David.Doyen@u-pem.fr>
2020-04-22 00:16:12 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl
@@ -0,0 +1,28 @@
+extends = /model/mathinput.pl
+
+title = Valeur d'une application linéaire
+
+before ==
+n, m = rd.choice(param['sizes'])
+
+lstvar = ",".join(["x","y","z","t","u","v"][:n])
+
+min = min([n, m])
+mat.setmatrix([min * [0] for _ in range(min)])
+
+A = rand_int_matrix_fullrank(m, n, 3)
+expr = ",".join([latex_lincomb(A.row(j),['x','y','z','t']) for j in range(m)])
+==
+
+text ==
+Soit l'application linéaire $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{m}} !$ dont la matrice par rapport aux bases canoniques des espaces de départ et d'arrivée est
+
+==
+
+evaluator ==
+score, _, feedback = eval_matrix(mat.getmatrix(), A)
+==
+
+solution ==
+La solution est $$\left( {{A.latex}} \right).$$
+==
