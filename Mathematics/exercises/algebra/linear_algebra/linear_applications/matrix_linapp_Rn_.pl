8a66223897773396f4bbd16f4987719f621dd89b
David DOYEN <David.Doyen@u-pem.fr>
2020-04-21 15:24:04 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/matrix_linapp_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_applications/matrix_linapp_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/matrix_linapp_Rn_.pl
@@ -1,4 +1,4 @@
-extends = /model/mathimatrix.pl
+extends = /model/mathmatrix.pl
 
 before ==
 n = 3
@@ -18,24 +18,13 @@ Déterminer une base de ce sous-espace (écrire cette base comme un ensemble, en
 ==
 
 evaluator ==
-def evalans(strans):
-    try:
-        ans = latex2sympy(input.value)
-        M = Matrix(ans).transpose()
-    except:
-        return (-1, "La réponse doit être un ensemble de vecteurs de $! \mathbb{R}^{ %s } !$" % n )
-    if M.shape[0] != n:
-        return (-1, "La réponse doit être un ensemble de vecteurs de $! \mathbb{R}^{ %s } !$" % n )
-    if (A*M).norm() != 0:
-        return (0, "Cette famille n'est pas une base du sous-espace. Ce n'est même pas une famille de vecteurs du sous-espace.")
-    if M.rank() < M.shape[1]:
-        return (0, "Cette famille n'est pas une base du sous-espace. Elle n'est pas libre.")
-    if M.rank() < m:
-        return (0, "Cette famille n'est pas une base du sous-espace. Elle n'est pas génératrice.")
-    return (100, "")
-
-score, feedback = evalans(input.value)
+score, _, feedback = eval_matrix(mat.getmatrix(), Msol)
 ==
+
+solution ==
+La solution est $$\left( {{sol.latex}} \right).$$
+==
+
 
 
 
