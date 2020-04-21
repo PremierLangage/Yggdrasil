fb31cba0fca1457f363cd60130fa127e418ed73d
David DOYEN <David.Doyen@u-pem.fr>
2020-04-22 00:36:51 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_Rn_.pl
@@ -10,7 +10,7 @@ lstvar = ",".join(["x","y","z","t","u","v"][:n])
 
 A = rand_int_matrix_fullrank(m, n, 3)
 B = rand_int_matrix(n, 1, 3)
-vec = tuple(B)
+vec = tuple(transpose(B))
 expr = ",".join([latex_lincomb(A.row(j),['x','y','z','t']) for j in range(m)])
 ==
 
