c4126780548bd8f7d6f48907018e2479fc86226a
David DOYEN <David.Doyen@u-pem.fr>
2020-04-21 18:56:58 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/matrix_linapp_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_applications/matrix_linapp_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/matrix_linapp_Rn_.pl
@@ -15,9 +15,9 @@ expr = ",".join([latex_lincomb(A.row(j),['x','y','z','t']) for j in range(m)])
 ==
 
 text ==
-On considère l'application linéaire $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{m}} !$ telle que
+On considère l'application linéaire $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{m}} !$ définie par
 $$ f({{lstvar}}) = ( {{expr}} ) .$$
-Déterminer la matrice de cette application linéaire par rapport aux bases canoniques de espaces de départ et d'arrivée.
+Déterminer la matrice de cette application linéaire par rapport aux bases canoniques des espaces de départ et d'arrivée.
 ==
 
 evaluator ==
