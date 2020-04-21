eb2d94cf18b95f6a4db8fd269ca04b52acf9d0e1
David DOYEN <David.Doyen@u-pem.fr>
2020-04-22 01:19:18 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_applications/value_linapp_prod_Rn_.pl
@@ -3,24 +3,23 @@ extends = /model/mathinput.pl
 title = Valeur d'une application linéaire
 
 before ==
-n, m, p = 2, 2, 2
+n = 3
 
 lstvar = ",".join(["x","y","z","t","u","v"][:n])
 
 
-A = rand_int_matrix(n, m, coeffbound)
-B = rand_int_matrix(m, p, coeffbound)
-X = rand_int_matrix(p, 1, 3)
+A = rand_int_matrix(n, n, coeffbound)
+B = rand_int_matrix(n, n, coeffbound)
+X = rand_int_matrix(n, 1, 3)
 sol = (A * B * X).transpose()
 strsol = str(tuple(sol))
 vec = str(tuple(B.transpose()))
-expr = ",".join([latex_lincomb(A.row(j),['x','y','z','t']) for j in range(m)])
 ==
 
 text ==
-Soit l'application linéaire $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{m}} !$ dont la matrice par rapport aux bases canoniques des espaces de départ et d'arrivée est
-$$ M_f = \left( {{A.latex}} \right).$$
-Calculer $! f{{vec}} !$. Ecrire la réponse sous forme d'un n-uplet (vecteur ligne).
+Soit les applications linéaires $! f : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{n}} !$ et $! g : \mathbb{R}^{{n}} \rightarrow \mathbb{R}^{{n}} !$ dont les matrices par rapport aux bases canoniques des espaces de départ et d'arrivée sont
+$$ M_f = \left( {{A.latex}} \right) \quad et \quad M_g = \left( {{B.latex}} \right).$$
+Calculer $! f \circ g {{vec}} !$. Ecrire la réponse sous forme d'un n-uplet (vecteur ligne).
 ==
 
 evaluator ==
