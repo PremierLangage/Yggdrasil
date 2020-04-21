8ff5a626ff787bd8a33ca234dbff901e3abf4241
etienne sandier <sandier@u-pec.fr>
2020-04-21 15:09:14 +0200

    esandier modified Yggdrasil/Biology/exercises/skull_cles_entieres.pl

--- Biology/exercises/skull_cles_entieres.pl
+++ Biology/exercises/skull_cles_entieres.pl
@@ -17,8 +17,8 @@ rd.shuffle(order)
 
 for i, j in enumerate(order):
     mygroup.add_label({i:labelscontent[j]})
-    mygroup.add_drop({j+1:""}) 
-    mygroup.add_match_by_name(j+1, i) 
+    mygroup.add_drop({i+1:""}) 
+    mygroup.add_match_by_name(i+1, i) 
 ==
 
 text ==
