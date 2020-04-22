bb1f24b20567bd41a3e852db8097b74fe7b0596b
Claire DAVID <Claire.David@u-pem.fr>
2020-04-22 22:37:50 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl
@@ -15,4 +15,34 @@ form_success== #|html|
 ==
 
 
+form== #|html|
+<!-- TIMEOUT VIEW -->
+{% if attempt >= maxattempt %}
+<!--{{ viewer|component }}-->
+{% endif %}
 
+<!-- SUCCESS VIEW -->
+{% if score == 100  %}
+{{ form_success }}
+{% endif %}
+
+<!-- EDITOR VIEW -->
+{{ editor|component }}
+
+<br>
+
+<!-- INSTRUCTIONS VIEW -->
+{{ form_instructions }}
+
+<script>
+    /**
+    * This function is called by the platform once the exercice is loaded.
+    * @param nodes an object containing a reference to the nodes of the page (title, text, form, actions, submit...)
+    */
+    function onReadyPL(nodes) {
+        const submit = nodes.submit; // a reference to the submit button
+        // hide submit button if needed.
+        submit.attr("disabled", ({{ attempt }} >= {{ maxattempt }}) || {{ score }} == 100);
+    }
+</script>
+==
