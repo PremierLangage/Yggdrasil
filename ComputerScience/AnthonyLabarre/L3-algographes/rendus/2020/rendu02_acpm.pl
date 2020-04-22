b86e6b4d1ab36c061673e94717d171402182da44
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-22 15:44:22 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
@@ -1,7 +1,7 @@
 # Test d'une classe Graphe implémentant un graphe pondéré non orienté.
 @ /utils/sandboxio.py
 @ unionfind.py
-@ corrlib_graphes.py # TODO: networkx pas installé sur la plateforme
+@ corrlib_graphes.py
 extends= /ComputerScience/python/template/pltestblonde.pl
 builder= /builder/before.py
 
