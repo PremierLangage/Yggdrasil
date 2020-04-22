92f5481f36ea16f16af4b3281b3b89c910fbc97e
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-22 22:23:54 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/scripts/editor-hack.ts

--- ComputerScience/automaton/scripts/editor-hack.ts
+++ ComputerScience/automaton/scripts/editor-hack.ts
@@ -2,7 +2,7 @@ const INITIAL_STATE = 'automaton-state--initial';
 const FINAL_STATE = 'automaton-state--final';
 const EPSILON = '$';
 const editors = document.querySelectorAll('c-automaton-editor');
-editors.forEach((editor: any) => {
+editors.forEach((editor) => {
     const component = editor.ngElementStrategy.componentRef.instance;
     component.focus = function (node, connection) {
         console.log(this.node);
