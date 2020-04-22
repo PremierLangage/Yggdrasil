a0835a3996e1167e5c0cd601f79f4355b1a0a95d
Claire DAVID <Claire.David@u-pem.fr>
2020-04-22 15:26:35 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl
@@ -5,3 +5,11 @@ extends = editor.pl
 #* feedback shown after a timeout.
 #feedback_timeout = <p class="warning-state">L\'automate déterministe suivant était une réponse possible à cette question.</p>
 feedback_timeout = <p class="warning-state">Vous n'avez pas réussi l'exercice. Relisez votre cours et retentez l'exercice un peu plus tard.</p>
+
+
+#* override this key to change the text shown after a good answer.
+form_success== #|html|
+<p class="success-state">Bravo c'est une bonne réponse.</p>
+==
+
+
