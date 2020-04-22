a0b46458e8960330f23107293beb65feced52266
Claire DAVID <Claire.David@u-pem.fr>
2020-04-22 15:25:11 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-L2.pl
@@ -1,3 +1,7 @@
 
 
 extends = editor.pl
+
+#* feedback shown after a timeout.
+#feedback_timeout = <p class="warning-state">L\'automate déterministe suivant était une réponse possible à cette question.</p>
+feedback_timeout = <p class="warning-state">Vous n'avez pas réussi l'exercice. Relisez votre cours et retentez l'exercice un peu plus tard.</p>
