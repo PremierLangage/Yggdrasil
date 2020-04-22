b1dc4bf5674e68a210ca6ab5751883aa0a6668f5
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-23 01:18:34 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/exercises/occurences.pl

--- ComputerScience/automaton/exercises/occurences.pl
+++ ComputerScience/automaton/exercises/occurences.pl
@@ -4,89 +4,17 @@ maxattempt = 5
 
 generate== #|py|
 mode = random.choice([-1, 0, 1])
-
-editor.automaton = {
-    "alphabet": [
-      "a",
-      "b"
-    ],
-    "initialStates": [
-      "S0",
-      "S1",
-      "S2"
-    ],
-    "acceptingStates": [
-      "S2"
-    ],
-    "states": [
-      "S0",
-      "S1",
-      "S2"
-    ],
-    "transitions": [
-      {
-        "fromState": "S0",
-        "toState": "S1",
-        "symbols": [
-          "a"
-        ]
-      },
-      {
-        "fromState": "S1",
-        "toState": "S2",
-        "symbols": [
-          "a"
-        ]
-      },
-      {
-        "fromState": "S2",
-        "toState": "S2",
-        "symbols": [
-          "b"
-        ]
-      },
-      {
-        "fromState": "S1",
-        "toState": "S1",
-        "symbols": [
-          "b"
-        ]
-      },
-      {
-        "fromState": "S0",
-        "toState": "S0",
-        "symbols": [
-          "b"
-        ]
-      }
-    ],
-    "position": {
-      "S0": {
-        "x": 93,
-        "y": 265
-      },
-      "S1": {
-        "x": 315,
-        "y": 207
-      },
-      "S2": {
-        "x": 530,
-        "y": 237
-      }
-    }
-}
-mode= 1
 message = {
     -1: 'au moins',
     0: 'exactement',
     1: 'au plus'
 }[mode]
 
-max_symbs = 2
-max_word_length = 2
+editor.debug = True
+
 alphabet, letter, occurences, viewer = Generator.counting(
-    max_symbs,
-    max_word_length,
+    2, # max_symbs
+    2, # max_word_length
     mode,
     sorted_alphabet=True
 )
