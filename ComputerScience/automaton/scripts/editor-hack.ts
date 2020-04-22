90451a7c9c32616cf3fee1d06d417519113a57a0
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-22 22:24:09 +0200

    mcisse modified Yggdrasil/ComputerScience/automaton/scripts/editor-hack.ts

--- ComputerScience/automaton/scripts/editor-hack.ts
+++ ComputerScience/automaton/scripts/editor-hack.ts
@@ -15,73 +15,7 @@ editors.forEach((editor) => {
         const actions = [];
 
 
-        if (this.connection) {
-            const canvas = (connection as any).canvas as HTMLElement;
-            canvas.classList.add('focused');
-            actions.push({
-                name: 'Changer Étiquette',
-                action: async () => {
-                    const s = this.connection.endpoints[0];
-                    const t = this.connection.endpoints[1];
-                    const transition = this.automaton.transitions.find(e => {
-                        return (
-                            e.fromState === s.elementId &&
-                            e.toState === t.elementId
-                        );
-                    });
-
-                    const title = 'Transition';
-                    const hint =
-                        'Entrez les symboles en les séparant par une virgule';
-                    const input = await this.prompt(
-                        title,
-                        hint,
-                        transition.symbols.join(',')
-                    );
-
-                    if (input !== false) {
-                        const symbols: string[] = input
-                            .split(',')
-                            .map((symbol: string) => {
-                                return symbol.trim();
-                            })
-                            .filter((symbol: string) => !!symbol);
-
-                        if (symbols.length === 0) {
-                            alert(
-                                'Vous devez saisir les symboles en les séparant par une virgule'
-                            );
-                        } else {
-                            this.connection
-                                .getOverlay('transition')
-                                .setLabel(symbols.join(','));
-                            transition.symbols = symbols;
-                        }
-
-                        this.updateAlphabet();
-                    }
-                }
-            });
-
-            actions.push({
-                name: 'Supprimer Transition',
-                action: async () => {
-                    const s = this.connection.endpoints[0];
-                    const t = this.connection.endpoints[1];
-                    this.automaton.transitions = this.automaton.transitions.filter(
-                        e => {
-                            return !(
-                                e.fromState === s.elementId &&
-                                e.toState === t.elementId
-                            );
-                        }
-                    );
-                    this.instance.deleteConnection(this.connection);
-                    this.focus(null, null);
-                }
-            });
-        }
-
+  
         this.actions = actions;
         this.detectChanges();
     } 
