cae574b8429b15c62802b95faa3d2cfd0020b049
David DOYEN <David.Doyen@u-pem.fr>
2020-04-23 16:32:09 +0200

    ddoyen modified Yggdrasil/demo/graphics/parabolas.pl

--- demo/graphics/parabolas.pl
+++ demo/graphics/parabolas.pl
@@ -32,7 +32,7 @@ text ==
 Parmi les représentations graphiques suivantes, laquelle correspond à la parabole d'équation
 $$ y = {{expr.latex}} $$
 {% for i in range(n) %}
-<img src="data:image/png;base64,{{source[i]}}" style="max-width: 400px; vertical-align:top; display: inline" />
+<img src="data:image/png;base64,{{source[i]}}" style="max-width: 300px; vertical-align:top; display: inline" />
 {% endfor %}
 ==
 
