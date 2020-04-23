22555ed90d82c964e87065ab09784ab66cfc2f30
Claire DAVID <Claire.David@u-pem.fr>
2020-04-23 21:49:40 +0200

    cdavid modified Yggdrasil/ComputerScience/automaton/exercises/count-bis.pl

--- ComputerScience/automaton/exercises/count-bis.pl
+++ ComputerScience/automaton/exercises/count-bis.pl
@@ -29,7 +29,7 @@ alphabet = ', '.join(list(alphabet))
 title = Nombre d'occurences
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez un automate reconnaissant le langage **L1** = {{ '{' }} mots ayant {{ message }} **{{ n }}** occurrences de la lettre **{{ letter }}** {{ '}' }} <br>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }} mots ayant {{ message }} {{ n }} occurrences de la lettre {{ letter }} {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir la solution !
