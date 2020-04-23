c976a9a1b77d76d51e3aefac240cc425cceb71e0
Claire DAVID <Claire.David@u-pem.fr>
2020-04-23 22:24:08 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
@@ -1,6 +1,4 @@
-extends = ../templates/editor.pl
-
-maxattempt = 10
+extends = ../templates/editor-L2.pl
 
 generate== #|py|
 from generator import Generator
@@ -30,13 +28,16 @@ nb_lettres = random.choice([2,3])
 
 if mode == -1 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du prefix)
-    alphabet, mot, viewer = Generator.prefix(nb_lettres,3)
+    alphabet, mot, viewer = Generator.prefix(nb_lettres,3,
+    sorted_alphabet=True)
 elif mode == 0 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du facteur)
-    alphabet, mot, viewer = Generator.factor(nb_lettres,3)
+    alphabet, mot, viewer = Generator.factor(nb_lettres,3,
+    sorted_alphabet=True)
 else :
     # Generator.prefix(nb lettres alphabet , longueur maxi du suffixe)
-    alphabet, mot, viewer = Generator.suffix(nb_lettres,3)
+    alphabet, mot, viewer = Generator.suffix(nb_lettres,3,
+    sorted_alphabet=True)
 
 
 alphabet = ', '.join(list(alphabet))
@@ -45,14 +46,15 @@ alphabet = ', '.join(list(alphabet))
 title = Facteur
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ bla }}  **{{ mot }}** {{ '}' }} <br>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ bla }} **{{ mot }}** {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
-Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir une solution !
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 
 ==
+
 
 
 
