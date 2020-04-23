cdc3830cd720d27c4f1630d0d1f98f145c865837
David DOYEN <David.Doyen@u-pem.fr>
2020-04-23 16:02:53 +0200

    ddoyen modified Yggdrasil/demo/graphics/static_matplotlib.pl

---
+++ demo/graphics/static_matplotlib.pl
@@ -0,0 +1,30 @@
+extends = /model/math.pl
+
+title = Graphique
+
+before ==
+import matplotlib.pyplot as plt
+import numpy as np
+    
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2*np.pi*t)
+plt.plot(t, s)
+plt.xlabel('time (s)')
+plt.ylabel('voltage (mV)')
+plt.title('About as simple as it gets, folks')
+plt.grid(True)
+
+source = fig2base64(plt.gcf(), format="png")
+==
+
+text ==
+<img src="data:image/png;base64,{{source}}" alt="Red dot" />
+==
+
+form ==
+==
+
+evaluator ==
+score = 100
+==
+
