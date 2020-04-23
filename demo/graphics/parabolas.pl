375cb013e81d4172e5e0b7fcad7a171680819722
David DOYEN <David.Doyen@u-pem.fr>
2020-04-23 16:13:30 +0200

    ddoyen modified Yggdrasil/demo/graphics/parabolas.pl

---
+++ demo/graphics/parabolas.pl
@@ -0,0 +1,35 @@
+extends = /model/math.pl
+
+title = Graphique
+
+before ==
+import matplotlib.pyplot as plt
+import numpy as np
+from plmpl import fig2base64
+
+n = 4
+source = []
+
+for i in range(n)
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2*np.pi*t)
+    plt.plot(t, s)
+    plt.xlabel('time (s)')
+    plt.ylabel('voltage (mV)')
+    plt.title('About as simple as it gets, folks')
+    plt.grid(True)
+    source = fig2base64(plt.gcf(), format="png")
+    
+==
+
+text ==
+<img src="data:image/png;base64,{{source}}" style="max-width: 300px; vertical-align:top; display: inline" />
+==
+
+form ==
+==
+
+evaluator ==
+score = 100
+==
+
