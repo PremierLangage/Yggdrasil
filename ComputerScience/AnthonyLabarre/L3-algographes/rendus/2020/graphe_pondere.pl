b5137cbb132c281bc125a95a91405e5d9220d8d1
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-23 10:19:23 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/graphe_pondere.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/graphe_pondere.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/graphe_pondere.pl
@@ -1,26 +1,47 @@
 # Test d'une classe Graphe implémentant un graphe pondéré non orienté.
 @ /utils/sandboxio.py
-extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ unionfind.py
+@ corrlib_graphes.py
+extends= /ComputerScience/python/template/pltestblonde.pl
 builder= /builder/before.py
 
 author = Anthony Labarre
-title = Graphes pondérés
-text = Écrivez une classe Graphe implémentant un graphe pondéré non orienté.
+title = Arbres (et forêts) couvrants de poids minimum
+text==
+Écrivez: 
+<ul>
+<li>une classe Graphe implémentant un graphe pondéré non orienté
+<li>une classe Tas implémentant une file à priorité
+<li>une classe UnionFind
+<li>acpm_prim(graphe, sommet), qui prend en paramètres un graphe et un sommet de départ et renvoie un ACPM pour la composante connexe du sommet donné en suivant l'algorithme de Prim;
+<li>acpm_kruskal(graphe), qui prend en paramètre un graphe et renvoie la forêt couvrante de poids minimum pour ce graphe en suivant l'algorithme de Kruskal;
+<li>fcpm_prim(graphe), qui prend en paramètre un graphe et renvoie la forêt couvrante de poids minimum pour ce graphe en suivant l'algorithme de Prim.
+</ul>
+==
+
+for_dr_dont_touch==
+class Graphe:
+    def sommets(dummy):
+        return [0, 1, 2, 3]
+==
+
+# Mon code
+before==
+import corrlib_graphes
+==
 
 # Les tests:
 
-before==
-
-==
-
-pltest0==
->>> G = Graphe()
+# Vérifications basiques de la classe Graphe
+#testname0 = Vérifications basiques de la classe Graphe
+pltest0== 
+>>> G = Graphe()# Existance d'une class Graphe
 >>> G.ajouter_aretes([(0, 2, -2), (1, 0, 4), (1, 2, 3), (2, 3, 2), (3, 1, -1)])
->>> sorted(G.sommets())
+>>> sorted(G.sommets()) 
 [0, 1, 2, 3]
->>> sorted(G.aretes())
-[(0, 2, -2), (1, 0, 4), (1, 2, 3), (2, 3, 2), (3, 1, -1)]
->>> sorted(map(G.poids_arete, (u, v) for u, v, *_ in G.aretes()))
+>>> sorted(sorted(_) + [poids] for *_, poids in G.aretes())
+[[0, 1, 4], [0, 2, -2], [1, 2, 3], [1, 3, -1], [2, 3, 2]]
+>>> sorted(G.poids_arete(u, v) for u, v, *_ in G.aretes())
 [-2, -1, 2, 3, 4]
 ==
 
@@ -40,4 +61,10 @@ editor.code ==
 # de validation quand vous avez terminé
 
 ==
+
+
+
+
+
+
 
