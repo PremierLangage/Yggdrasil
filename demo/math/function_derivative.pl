4682d3e4cd5c63d0682336982c717155ebf22f79
David DOYEN <David.Doyen@u-pem.fr>
2020-04-23 16:02:42 +0200

    ddoyen modified Yggdrasil/demo/math/function_derivative.pl

--- demo/math/function_derivative.pl
+++ demo/math/function_derivative.pl
@@ -1,31 +1,23 @@
-extends = /model/math.pl
+extends = /model/mathinput.pl
 
-title = Graphique
+title = Calcul de dérivée
+
+input.virtualKeyboards = functions
 
 before ==
-import matplotlib.pyplot as plt
-import numpy as np
-    
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, folks')
-plt.grid(True)
-
-source = fig2base64(plt.gcf(), format="png")
+var('x')
+a = rd.choice([-1, 1]) * rd.choice([Rational(1, 2), Rational(1, 3), 2, 3])
+b = rd.choice([Rational(1, 2), Rational(1, 3), 2, 3])
+g = rd.choice([sin, cos, exp, ln])
+f = a * g(b * x)
+sol = diff(f, x)
 ==
 
 text ==
-<img src="data:image/png;base64,{{source}}" alt="Red dot" />
-==
-
-form ==
+Calculer la dérivée de la fonction
+$$ f : x \mapsto {{f.latex}} .$$
 ==
 
 evaluator ==
-score = 100
+score, _, feedback = eval_function(input.value, sol)
 ==
-
-
