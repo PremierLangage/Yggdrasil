432b9d35fc5bcd654583183e8f196436568ac6ce
Claire DAVID <Claire.David@u-pem.fr>
2020-04-23 23:32:42 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
@@ -1,4 +1,21 @@
-
-
 extends = editor.pl
 
+evaluator== #|py|
+from automaton import Automaton
+
+if attempt >= maxattempt: # timeout
+    grade = (score, feedback_timeout)
+else:
+    attempt += 1
+    match, error = Automaton.compare(viewer, editor)
+    if  attempt >= maxattempt and (error or not match): # error or no match after timeout
+        grade = (score, feedback_timeout)
+    elif error:
+        grade = (-1, feedback_syntax_error.format(error))
+    elif match is True:
+        score = 100
+        grade = (score, feedback_match)
+    else:
+        score = 0
+        grade=(score, feedback_nomatch)
+==
