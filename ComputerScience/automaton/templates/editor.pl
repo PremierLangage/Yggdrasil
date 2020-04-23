3cc85b8baf382ca926065a8414a09fe189750661
Claire DAVID <Claire.David@u-pem.fr>
2020-04-23 23:17:40 +0200

    cdavid modified Yggdrasil/ComputerScience/automaton/templates/editor.pl

--- ComputerScience/automaton/templates/editor.pl
+++ ComputerScience/automaton/templates/editor.pl
@@ -74,10 +74,9 @@ text= PLEASE OVERRIDE THE KEY **text=** TO CHANGE THIS TEXT
 
 form== #|html|
 <!-- TIMEOUT VIEW -->
-<!-- {% if attempt >= maxattempt %}
+{% if attempt >= maxattempt %}
 {{ viewer|component }}
 {% endif %}
--->
 
 <!-- SUCCESS VIEW -->
 {% if score == 100  %}
