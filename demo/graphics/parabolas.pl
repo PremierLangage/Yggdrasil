ae95089d6f4050cf484620cb8a3374ca1ad95f0c
David DOYEN <David.Doyen@u-pem.fr>
2020-04-23 16:41:14 +0200

    ddoyen modified Yggdrasil/demo/graphics/parabolas.pl

--- demo/graphics/parabolas.pl
+++ demo/graphics/parabolas.pl
@@ -11,19 +11,19 @@ n = 4
 source = []
 
 a = list_randint_norep(4,-2,2)
-b = list_randitem_norep(4,[-20,-10,-5,0,5,10,20])
+b = list_randint_norep(4,-5,5)
 
 sol = randint(1,4)
 var('x')
 expr = (x - a[sol-1])**2 + b[sol-1]
 
 for i in range(n):
-    abs = np.arange(-4, 4, 0.01)
+    abs = np.arange(-3, 3, 0.01)
     ord = (abs - a[i])**2 + b[i]
     plt.plot(abs, ord)
     plt.title(f"Figure {i+1}")
     plt.grid(True)
-    plt.gca().set_ylim((-25, 25))
+    plt.gca().set_ylim((-15, 15))
     source.append(fig2base64(plt.gcf(), format="png"))
     plt.clf()
 
