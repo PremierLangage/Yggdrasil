9a9a608e171d65451bb61a67c4e753f367877407
Claire DAVID <Claire.David@u-pem.fr>
2020-04-24 00:07:55 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
@@ -19,16 +19,17 @@ else:
     elif error:
         grade = (-1, feedback_syntax_error.format(error))
     elif match is True:
-        infos_editor , _ = Automaton.properties(editor)
+        infos_editor , _ = Automaton.editor_properties(editor)
         info_viewer , _ = Automaton.properties(viewer)
         if not (infos_editor[deterministic] and infos_editor[complete]):
             score = 0 
             grade(score,feedback_nondet)
         elif len(infos_viewer[states]) == len(infos_editor[states])
-            score = 100
+            score = 0
             grade = (score, feedback_non_mini)
         else
-            score = 0
+            score = 100
+            grade = (score, feedback_match)
     else:
         score = 0
         grade=(score, feedback_nomatch)
