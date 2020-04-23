869742176ba7d7e0500f3df24597d41aca9c00ee
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-23 12:24:24 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl
@@ -70,7 +70,7 @@ pltest3==
 >>> from corrlib_graphes import * #
 >>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS); G.ajouter_aretes(PRIM_INSTANCE_TD); G.ajouter_aretes(aretes_ponderees(graphe_connexe_aleatoire_pondere(10))  # initialisation du graphe
 >>> poids_solution_optimale = poids_fcpm(G.aretes())
->>> foret = acpm_kruskal(G); poids_obtenu = sum(poids for *_, poids in arbre.aretes())
+>>> foret = acpm_kruskal(G); poids_obtenu = sum(poids for *_, poids in foret.aretes())
 >>> if poids_obtenu != poids_solution_optimale:
 ...     print("poids obtenu =", poids_obtenu, "; poids optimal =", poids_solution_optimale)
 ==
