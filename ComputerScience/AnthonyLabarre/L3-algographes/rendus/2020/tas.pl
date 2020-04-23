8d35ccaf2d9d87f19f4f69afb8a2a4e39b31f01c
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-23 14:20:17 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl
@@ -67,8 +67,7 @@ grade=(100,"bravo")
 #
 # création de l'éditeur de code
 editor.code ==
-# Saisissez votre code ici, et cliquez sur le bouton
-# de validation quand vous avez terminé
+# Complétez le code ci-dessous, et cliquez sur le bouton de validation quand vous avez terminé
 
 ==
 
