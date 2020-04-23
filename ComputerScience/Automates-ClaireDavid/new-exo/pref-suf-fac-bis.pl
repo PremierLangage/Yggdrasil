e5d327d66ec157149ee9f30eb71dd09bedc32392
Claire DAVID <Claire.David@u-pem.fr>
2020-04-23 22:02:41 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/new-exo/pref-suf-fac-bis.pl

--- ComputerScience/Automates-ClaireDavid/new-exo/pref-suf-fac-bis.pl
+++ ComputerScience/Automates-ClaireDavid/new-exo/pref-suf-fac-bis.pl
@@ -50,7 +50,7 @@ On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ bla }}  **{{ mot }}** {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
-Il vous reste {{ maxattempt - attempt }} tentative(s)!
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 
