e7a522e4d3bb6c1bccf6bd36c59e43e8fadca199
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-23 12:22:42 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl
@@ -68,7 +68,7 @@ pltest2==
 testname3 = Vérifications de acpm_kruskal sur un graphe non connexe
 pltest3==
 >>> from corrlib_graphes import * #
->>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS); G.ajouter_aretes(PRIM_INSTANCE_TD)  # initialisation du graphe
+>>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS); G.ajouter_aretes(PRIM_INSTANCE_TD); G.ajouter_aretes(aretes_ponderees(graphe_connexe_aleatoire_pondere(10))  # initialisation du graphe
 >>> foret = acpm_kruskal(G)
 >>> sum(poids for *_, poids in foret.aretes())
 41
