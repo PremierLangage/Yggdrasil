c97ee53a6434567081a4cf9920c7f092eef4e4a9
David DOYEN <David.Doyen@u-pem.fr>
2020-04-23 16:40:06 +0200

    ddoyen modified Yggdrasil/demo/graphics/parabolas.pl

--- demo/graphics/parabolas.pl
+++ demo/graphics/parabolas.pl
@@ -23,7 +23,7 @@ for i in range(n):
     plt.plot(abs, ord)
     plt.title(f"Figure {i+1}")
     plt.grid(True)
-    plt.set_ylim((-25, 25))
+    plt.gca().set_ylim((-25, 25))
     source.append(fig2base64(plt.gcf(), format="png"))
     plt.clf()
 
