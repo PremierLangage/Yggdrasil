6f12c9aef449bd1305930d2f577d442b29f8e187
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-23 14:18:22 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl
@@ -36,7 +36,7 @@ import corrlib_graphes
 testname1 = Vérification de la classe Tas sur des entiers
 pltest1== 
 >>> # création d'un tas d'entiers aléatoires
->>> from random import randint; donnees = [randint(-100, 100) for _ in range(10)]; 
+>>> from random import randint; donnees = [randint(-100, 100) for _ in range(10)] #
 >>> try:
 ...     T = Tas()  # initialisation sans paramètre
 ... except TypeError:
