0e67f18b11e90480e41d1b2a3d53dd6c12128749
David DOYEN <David.Doyen@u-pem.fr>
2020-04-24 18:17:05 +0200

    ddoyen modified Yggdrasil/demo/css/radio_items.pl

--- demo/css/radio_items.pl
+++ demo/css/radio_items.pl
@@ -47,33 +47,5 @@ extracss ==
     vertical-align: middle;
     font-weight: 900;
 }
-
-.icon-check-before p::before {
-    font-family: "Font Awesome 5 Free";
-    color: #155724;
-    margin-right: 1em;
-    content: "\f00c";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-.icon-times-before p::before {
-    font-family: "Font Awesome 5 Free";
-    color: #721c24;
-    margin-right: 1em;
-    content: "\f00d";
-    vertical-align: middle;
-    font-weight: 900;
-}
-
-.error-text-unit {
-    color: #721c24;
-    text-decoration: line-through red;
-}
-
-.success-text-unit {
-    color: #155724;
-    text-decoration: underline green;
-}
 </style>
 ==
