12186ba36c56f92a4c71b787b502b39fcafadc58
David DOYEN <David.Doyen@u-pem.fr>
2020-04-25 00:54:01 +0200

    ddoyen modified Yggdrasil/Mathematics/tests/testjsonpickle.pl

--- Mathematics/tests/testjsonpickle.pl
+++ Mathematics/tests/testjsonpickle.pl
@@ -10,31 +10,11 @@ class Foo:
         self.a = a
         self.b = b
 
-class Bar:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.z = 0
-
-obj = Bar(Foo(1, [3,4]), {'key1': 11, 'key2': 22})
-
-import jsonpickle
-obj_encoded = jsonpickle.encode(obj, unpicklable=False)
-
-a = {1: 1, 2: 2, 3: 3, 4: 4}
-b = {2: 2, 3: 3, 1: 1, 4: 4}
-
-a_encoded = jsonpickle.encode(a, unpicklable=False)
-b_encoded = jsonpickle.encode(b, unpicklable=False)
-
-v = str(jsonpickle.__version__)
+Foo(1,2)
 ==
 
 text ==
-{{obj_encoded}}
-{{a_encoded}}
-{{b_encoded}}
-{{v}}
+
 ==
 
 form ==
