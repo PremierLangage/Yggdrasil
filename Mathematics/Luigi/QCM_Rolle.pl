4f347462a712bc8eee28363fe27300e900955e59
Luigi MANCA <Luigi.Manca@u-pem.fr>
2020-04-24 18:33:03 +0200

    lmanca modified Yggdrasil/Mathematics/Luigi/QCM_Rolle.pl

---
+++ Mathematics/Luigi/QCM_Rolle.pl
@@ -0,0 +1,42 @@
+author=Luigi Manca
+
+title=Théorème de Rolle / Théorème des ccroissements finis
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+questions==
+
+** Soit $% f : \mathbb{R} \rightarrow \mathbb{R} %$ une fonction dérivable et $% a<b %$.
+Peut-on affirmer que : 
++ $% f %$ admet au moins un minimum et au moins un maximum sur $% [a,b] %$
+- Il existe $% c\in [a,b] %$ où $% f %$  admet un maximum locale et où $% f'(c) = 0%$
+- Si $% c \in [a,b] %$ et $% f'(c)=0 %$ alors $% f %$ admet un maximum ou minimum locale en $% c %$
+- Si $% c \in ]a,b[ %$ et $% f'(c)=0 %$ alors $% f %$ admet un maximum ou minimum locale en $% c %$
++ Si  $% c \in ]a,b[ %$ et $% c %$ est un point de  maximum ou minimum locale pour $% f %$ alors  $% f'(c)=0 %$ 
++ Il existe $% c \in ]a,b[ %$ tel que $% f'(c) (b-a) = f(b)-f(a) %$ 
+
+
+
+** Parmi les fonctions suivantes, indiquer celles qui satisfont les hypothèses du Théorème de Rolle sur l'intervalle $% [0,1 ] %$,: 
+- $% f(x)=x %$
++ $% f(x) = (1-x)x %$
++ $% f(x)=\sqrt{(1-x)x} %$
+- $% f(x)=|2x-1| %$
++ $% f(x)=x^2 %$
++ $% f(x)= \sin(2020\pi x) %$
++ $% f(x)=e^\pi %$
+- $% f(x)=E(x) %$  ( Ici, $% E(x) %$ est la partie entière de $% x %$ )
+
+
+
+** Soient $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction satisfaisant les hypothèses du Théorème de Rolle sur $% [a,b] %$ et soit $% g:\mathbb{R} \to [a,b] %$ une fonction dérivable.
+On peut affirmer que (plusieurs bonnes réponses) :
+
++ $% g\circ f %$ satisfait les hypothèses du Théorème de Rolle sur $% [a,b] %$ 
+- $% f\circ g %$ satisfait les hypothèses du Théorème de Rolle sur $% [a,b] %$ 
+
+
+
+
+==
+
