096f0249e6beb282d0c3a73e4421854b1a84307c
Luigi MANCA <Luigi.Manca@u-pem.fr>
2020-04-24 17:03:38 +0200

    lmanca modified Yggdrasil/Mathematics/Luigi/Thm_Rolle_accr_finis.pl

--- Mathematics/Luigi/Thm_Rolle_accr_finis.pl
+++ Mathematics/Luigi/Thm_Rolle_accr_finis.pl
@@ -7,76 +7,49 @@ extends=/model/AMC_TXT.pl
 questions==
 
 
-* Rappeler la définition d'une fonction $% f :\mathbb{R} \rightarrow \mathbb{R}%$ Lipschitzienne : 
-- $% \forall K >0, ~ \exists x,y \in \mathbb{R} \text{ tel que } |f(x) - f(y)| \leq K|x-y| %$
-- $% \exists K >0, ~ \forall x,y \in \mathbb{R}, ~ |f(x) - f(y)| \leq |x-y| %$
-+ $% \exists K >0, ~ \forall x,y \in \mathbb{R}, ~ |f(x) - f(y)| \leq K|x-y| %$
-- $% \forall \varepsilon >0, ~ \exists K >0, \forall x,y \in \mathbb{R}, ~ \Big[|x-y| \leq K \Big] \implies |f(x) - f(y)| \leq \varepsilon %$
+** Parmi les fonctions suivantes, indiquer celles qui satisfont les hypothèses du Théorème de Rolle sur l'intervalle $% [0,1 ] %$,: 
+- $% f(x)=x %$
++ $% f(x) = (1-x)x %$
++ $% f(x)=\sqrt{(1-x)x} %$
+- $% f(x)=|2x-1| %$
++ $% f(x)=x^2 %$
++ $% f(x)= \sin(2020\pi x) %$
++ $% f(x)=e^\pi %$
+- $% f(x)=E(x) %$  ( Ici, $% E(x) %$ est la partie entière de $% x %$ )
+
+** Parmi les fonctions suivantes, indiquer celles qui satisfont les hypothèses du Théorème des accroissements finis sur l'intervalle $% [0,1 ] %$,: 
++ $% f(x)=x %$
++ $% f(x) = (1-x)x %$
++ $% f(x)=\sqrt{(1-x)x} %$
+- $% f(x)=|2x-1| %$
++ $% f(x)=x^2 %$
++ $% f(x)= \sin(2020\pi x) %$
++ $% f(x)=e^\pi %$
+- $% f(x)=E(x) %$  ( Ici, $% E(x) %$ est la partie entière de $% x %$ )
 
 * Vrai ou faux ? 
-Si une fonction $% f :\mathbb{R} \rightarrow \mathbb{R}%$ est 2-Lipschitzienne alors elle 1-Lipschitzienne également. 
+Si une fonction $% f : [a,b] \rightarrow \mathbb{R}%$ satisfait les hypothèses du Théorème des accroissements finis alors elle est Lipschitzienne.
 - Vrai
 + Faux
 
 * Vrai ou faux ? 
-Si une fonction $% f : \mathbb{R} \rightarrow \mathbb{R}%$ est 1-Lipschitzienne alors elle 2-Lipschitzienne également.
+Si une fonction $% f : [a,b] \rightarrow \mathbb{R}%$ satisfait les hypothèses de l'inégalité des accroissements finis alors elle est Lipschitzienne.
 + Vrai
 - Faux
 
-** On considère la fonction $% f : \mathbb{R} \rightarrow \mathbb{R}%$ définie par $% f(x) = -3x +2 %$. 
-Cliquez sur les affirmations que vous pensez être correctes (plusieurs bonnes réponses) :
-- La fonction $%f%$ n'est pas Lipschitzienne.
-+ La fonction $%f%$ est Lipschitzienne.
-- La fonction $%f%$ est 1-Lipschitzienne.
-- La fonction $%f%$ est 2-Lipschitzienne.
-+ La fonction $%f%$ est 3-Lipschitzienne.
-+ La fonction $%f%$ est 1000-Lipschitzienne.
+* Vrai ou faux ?
+Si $% f:]a,b[\to \mathbb{R} %$ est dérivable et strictement croissante, alors $%\forall x\in ]a,b[,\, f'(x) > 0 %$.
+- Vrai
++ Faux
 
-** On considère la fonction $% f : \mathbb{R} \rightarrow \mathbb{R}%$ définie par $% f(x) = 4x +1000  %$. 
-Cliquez sur les affirmations que vous pensez être correctes (plusieurs bonnes réponses) :
-- La fonction $%f%$ n'est pas Lipschitzienne.
-+ La fonction $%f%$ est Lipschitzienne.
-- La fonction $%f%$ est 1-Lipschitzienne.
-+ La fonction $%f%$ est 4-Lipschitzienne.
-+ La fonction $%f%$ est 1000-Lipschitzienne.
+** Soient $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction satisfaisant les hypothèses du Théorème de Rolle sur $% [a,b] %$ et soit $% g:\mathbb{R} \to [a,b] %$ une fonction dérivable.
+On peut affirmer que (plusieurs bonnes réponses) :
 
-** Soit la fonction définie sur $%\mathbb{R}%$ par $%f(x) = |x|%$. 
-Pour étudier si la fonction $%f%$ est Lipschitzienne, on pourra utiliser la deuxième inégalité triangulaire (dite parfois renversée).
-Cliquez sur les affirmations que vous pensez être correctes (plusieurs bonnes réponses) :
-+ La fonction $%f%$ n'est pas dérivable en 0.
-+ La fonction $%f%$ est Lipschitzienne.
-+ La fonction $%f%$ n'est pas bornée sur $%\mathbb{R}%$ .
-- La fonction $%f%$ est dérivable sur $%\mathbb{R}%$  
-- La fonction $%f%$ n'est pas Lipschitzienne. 
-- La fonction $%f%$ est bornée sur $%\mathbb{R}%$ .
+- $% g\circ f %$ satisfait les hypothèses du Théorème de Rolle sur $% [a,b] %$ 
+- $% f\circ g %$ satisfait les hypothèses du Théorème de Rolle sur $% [a,b] %$ 
++ $% f\circ g %$ satisfait les hypothèses du Théorème des accroissements finis sur $% [a,b] %$ 
++ $% g\circ f %$ satisfait les hypothèses du Théorème des accroissements finis sur $% [a,b] %$ 
 
-** Soit la fonction définie sur $%\mathbb R%$ par $%f(x) = x^2%$. 
-Pour étudier si la fonction $%f%$ est Lipschitzienne, on pourra étudier le terme $%\frac{f(x) - f(0)}{x}%$ lorsque $%x%$ tend vers $+\infty$.
-Cliquez sur les affirmations que vous pensez être correctes (plusieurs bonnes réponses) :
-- La fonction $%f%$ est Lipschitzienne.
-- La fonction $%f%$ est monotone sur $%\mathbb{R}%$ .
-+ La fonction $%f%$ n'est pas Lipschitzienne. 
-+ La fonction n'est pas monotone sur $%\mathbb{R}%$ .
-
-* Soit une fonction $% f :\mathbb{R} \rightarrow \mathbb{R}%$.
-Cliquez sur l'affirmation que vous pensez être correcte :
-+ Si $%f%$ est Lipschitzienne sur $%\mathbb{R}%$ alors $%f%$ est continue sur $%\mathbb{R}%$.
-- Si $%f%$ est Lipschitzienne sur $%\mathbb{R}%$ alors $%f%$ est dérivable sur $%\mathbb{R}%$.
-- Si $%f%$ est Lipschitzienne sur $%\mathbb{R}%$ alors $%f%$ est bornée sur $%\mathbb{R}%$.
-- Si $%f%$ est Lipschitzienne sur $%\mathbb{R}%$ alors $%f%$ est monotone sur $%\mathbb{R}%$.
-
-
-
-** Soient $%f%$ et $%g%$ deux fonctions réelles définies sur $%\mathbb{R}%$. 
-Cliquez sur les affirmations que vous pensez être correctes (plusieurs bonnes réponses) :
-+ Si $%f%$ est Lipschitzienne sur $%\mathbb{R}%$, alors $%f%$ est Lipschitzienne tout intervalle du type $%[a,b]%$ ($%a<b%$).
-- Si $%f%$ est Lipschitzienne sur tout intervalle du type $%[a,b]%$ ($%a<b%$), alors $%f%$ est Lipschitzienne sur $%\mathbb{R}%$.
-+ Si il existe $%K>0%$ tel que $%f%$ est $%K%$-Lipschitzienne sur tout intervalle du type $%[a,b]%$ ($%a<b%$), alors $%f%$ est $%K%$-Lipschitzienne sur $%\mathbb{R}%$.
-
-
-* Vrai ou faux ? L'ensemble des fonctions Lipschitziennes de $%\mathbb R%$ dans $%\mathbb R%$ est un sous espace vectoriel de l'ensemble des fonctions.
-+  Vrai
-- Faux
 
 
 
