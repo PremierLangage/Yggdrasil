3631cff4145ea460430e2e7bc53de1db57dbf784
David DOYEN <David.Doyen@u-pem.fr>
2020-04-24 18:09:36 +0200

    ddoyen modified Yggdrasil/demo/css/radio_items.pl

--- demo/css/radio_items.pl
+++ demo/css/radio_items.pl
@@ -22,12 +22,58 @@ evaluator ==
 score = radio.eval()
 ==
 
+
 extracss ==
 <style>
 .myclass .radio-item2 {
     padding: 4px 4px 4px 4px !important;
     background-color: silver;
 }
+
+.icon-check-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    margin-left: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #721c24;
+    margin-left: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-check-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    margin-right: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #721c24;
+    margin-right: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.error-text-unit {
+    color: #721c24;
+    text-decoration: line-through red;
+}
+
+.success-text-unit {
+    color: #155724;
+    text-decoration: underline green;
+}
 </style>
 ==
-
