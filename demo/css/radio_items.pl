0143e0796d03d55a955f9febef9984eba293a3fb
David DOYEN <David.Doyen@u-pem.fr>
2020-04-24 14:49:31 +0200

    ddoyen modified Yggdrasil/demo/css/radio_items.pl

--- demo/css/radio_items.pl
+++ demo/css/radio_items.pl
@@ -1,5 +1,26 @@
+extends = /model/basic.pl
 
+title = Changer le style des items
 
+radio =: RadioGroup
+radio.decorator = CustomRadio
+radio.css = myclass
+
+before ==
+radio.setitems(["AAA", "BBB", "CCC", "DDD"])
+radio.setsol_from_index(0)
+==
+
+text ==
+==
+
+form ==
+{{ radio|component }}
+==
+
+evaluator ==
+score = radio.eval()
+==
 
 extracss ==
 <style>
