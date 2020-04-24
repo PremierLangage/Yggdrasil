2e6e193760c24d1fcb1a8783a3f8f6f1d19d813a
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-24 16:28:31 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/tas.pl
@@ -14,8 +14,8 @@ taboo=heapq
 # Les tests:
 
 # Vérification de la classe Tas sur des entiers
-testname1 = Vérification de la classe Tas sur des entiers
-pltest1== 
+testname0 = Vérification de la classe Tas sur des entiers
+pltest0== 
 >>> # création d'un tas d'entiers aléatoires
 >>> from random import randint; donnees = [randint(-100, 100) for _ in range(10)] #
 >>> try:
@@ -32,8 +32,8 @@ pltest1==
 
 # Vérification de la classe Tas sur des triplets (premier élément = critère pour le minimum)
 
-testname2 = Vérification de la classe Tas sur des triplets (premier élément = critère pour le minimum)
-pltest2==
+testname1 = Vérification de la classe Tas sur des triplets (premier élément = critère pour le minimum)
+pltest1==
 >>> from random import randint; donnees = [(randint(-100, 100), randint(-100, 100), randint(-100, 100)) for _ in range(10)] #
 >>> try:
 ...     T = Tas()  # initialisation sans paramètre
@@ -73,6 +73,7 @@ class Tas(object):
         pass  # à compléter
 
 ==
+
 
 
 
