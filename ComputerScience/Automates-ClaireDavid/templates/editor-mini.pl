5d10e963608457456b7e052abd74e2b28f57d7aa
Claire DAVID <Claire.David@u-pem.fr>
2020-04-24 22:19:31 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
@@ -24,6 +24,7 @@ else:
     elif error:
         grade = (-1, feedback_syntax_error.format(error))
     elif match is True :
+    ## ça a l'air de planter ici
         if not (infos_editor[deterministic] and infos_editor[complete]):
             score = 0 
             grade(score,feedback_nondet)
@@ -37,5 +38,6 @@ else:
         score = 0
         grade=(score, feedback_nomatch)
 ==
+
 
 
