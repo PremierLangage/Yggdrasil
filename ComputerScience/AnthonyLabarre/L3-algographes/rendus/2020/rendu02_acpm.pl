1aad9a9d897c742fa5a3ffb46b8bbfab641c8bfa
Claire DAVID <Claire.David@u-pem.fr>
2020-04-24 16:01:30 +0200

    cdavid modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
@@ -6,7 +6,7 @@ extends= /ComputerScience/python/template/pltestblonde.pl
 builder= /builder/before.py
 
 author = Anthony Labarre
-title = Tester tout à la fois
+title = Tester tout à la fois!
 text==
 Écrivez: 
 <ul>
