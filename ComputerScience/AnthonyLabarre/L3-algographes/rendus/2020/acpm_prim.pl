b02568f0f59f124fae77723f2d163446538f5e4b
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-24 15:22:36 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_prim.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_prim.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_prim.pl
@@ -22,7 +22,7 @@ class Graphe:
 # Vérifications de acpm_prim sur un graphe connexe
 testname0 = Vérifications de acpm_prim sur l'exemple du cours
 pltest0==
->>> from corrlib_graphes import *, from random import choice #
+>>> from corrlib_graphes import *; from random import choice #
 >>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS) # initialisation de l'instance du cours
 >>> arbre = acpm_prim(G, choice(list(G.sommets())) # lancement de acpm_prim avec départ aléatoire
 >>> sum(poids for *_, poids in arbre.aretes())  # vérification du poids de la solution
