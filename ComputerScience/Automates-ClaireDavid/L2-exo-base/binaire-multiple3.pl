436798b2cd11a9f4db62a20caa6ab8c7660c9524
Claire DAVID <Claire.David@u-pem.fr>
2020-04-25 00:22:23 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/binaire-multiple3.pl

---
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/binaire-multiple3.pl
@@ -0,0 +1,70 @@
+extends = ../templates/editor-L2.pl
+
+
+## Ajouter ici la génération de l'automate et autres variables de l'exo
+generate== #|py|
+#from automaton import Automaton
+
+#    la construction de l'automate pour tester la réponse
+viewer = '''
+    #states
+    S0
+    S1
+    S2
+    #initials
+    S0
+    #accepting
+    S0
+    #alphabet
+    a
+    b
+    #transitions
+    S0:a>S1
+    S1:a>S2
+    S2:a>S0
+    S0:b>S1
+    S1:b>S2
+    S2:b>S0
+'''
+#    l'alphabet
+alphabet = "a,b"
+
+#    du message de l'énoncé décrivant le langage
+message = "mots qui sont la représentation binaire des entiers multiples de 3 (bit de poids fort à gauche)"
+==
+
+title = ** multiples de 3
+text ==
+On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez un automate reconnaissant {{ message }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
+</p>
+{% endif %}
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
