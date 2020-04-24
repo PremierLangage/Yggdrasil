493d7ae103534b98048d5b6e9231b0fd1a58b982
David DOYEN <David.Doyen@u-pem.fr>
2020-04-24 22:47:17 +0200

    ddoyen modified Yggdrasil/Mathematics/tests/testjsonpickle.pl

---
+++ Mathematics/tests/testjsonpickle.pl
@@ -0,0 +1,30 @@
+extends = /model/basic.pl
+
+title = Addition
+
+before ==
+import random as rd
+a=rd.randint(10,20)
+b=rd.randint(10,20)
+==
+
+text ==
+Calculer {{a}} + {{b}}.
+==
+
+input =: Input
+input.type = number
+
+form ==
+{{ input | component }}
+==
+
+settings.feedback = rightwrong
+
+evaluator ==
+if input.value == a+b:
+    grade=(100,"")
+else:
+    grade=(0,f"La réponse est {a+b}.")
+==
+
