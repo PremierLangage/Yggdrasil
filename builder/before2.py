30acb00428508a44f34fe0df6e195d5f25882f23
David DOYEN <David.Doyen@u-pem.fr>
2020-04-25 01:27:05 +0200

    ddoyen modified Yggdrasil/builder/before2.py

--- builder/before2.py
+++ builder/before2.py
@@ -102,11 +102,10 @@ if __name__ == "__main__":
     dic['internals']['attempt'] = 1
 
     output_json = sys.argv[2]
-    print(jsonpickle.encode(dic, unpicklable=False),file = sys.stderr)
-    sys.exit(1)
+
     with open(output_json, "w+") as f:
-        #f.write(jsonpickle.encode(dic, unpicklable=False))
-        f.write(json.dumps(dic, cls=CustomEncoder))
+        f.write(jsonpickle.encode(dic, unpicklable=False))
+        #f.write(json.dumps(dic, cls=CustomEncoder))
     sys.exit(0)
 
 
