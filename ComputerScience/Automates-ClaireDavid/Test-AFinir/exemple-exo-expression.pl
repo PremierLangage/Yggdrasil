760a3db79322c1b376d7bae7d1667b10d24f61cf
Claire DAVID <Claire.David@u-pem.fr>
2020-04-24 08:34:45 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/exemple-exo-expression.pl

---
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/exemple-exo-expression.pl
@@ -0,0 +1,47 @@
+extends = ../templates/editor-L2.pl
+
+
+generate== #|py|
+## Ajouter ici la génération:
+#    du message de l'énoncé décrivant le langage
+#    l'alphabet
+#    la construction de l'automate pour tester la réponse
+
+
+viewer = Automaton.parse('(a|b)*')
+
+alphabet = "a,b"
+message = "l'ensemble des mots qui commencent par b et finissent pat bab ou bb."
+==
+
+title = Partiel 2020 Exo1-1
+text ==
+On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez un automate reconnaissant le langage correspondant à {{ message }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
+</p>
+{% endif %}
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
