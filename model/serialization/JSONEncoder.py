4d301c5f18818e7805864a4627a8ce6f6834f297
David DOYEN <David.Doyen@u-pem.fr>
2020-04-25 00:33:09 +0200

    ddoyen modified Yggdrasil/model/serialization/JSONEncoder.py

---
+++ model/serialization/JSONEncoder.py
@@ -0,0 +1,22 @@
+import json
+import jsonpickle
+import sympy
+import sympy2latex
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (sympy.Basic, sympy.Matrix)):
+            return {'__SymPy__': True, 'srepr': sympy.srepr(obj), 'latex': sympy.latex(obj)}
+        return jsonpickle.encode(obj, unpicklable=False)
+        
+        
+        
+class CustomDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, dict):
+        if '__SymPy__' in dict:
+            return sympy.sympify(dict['srepr'], evaluate=False)
+        return dict
+
