e8fbe5f1ebb1003e95f3487628d83665228cb805
Claire DAVID <Claire.David@u-pem.fr>
2020-04-24 23:56:08 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/2a3b.pl

---
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/2a3b.pl
@@ -0,0 +1,52 @@
+
+extends = ../templates/editor-L2.pl
+
+
+## Ajouter ici la génération de l'automate et autres variables de l'exo
+generate== #|py|
+#from automaton import Automaton
+
+#    la construction de l'automate pour tester la réponse
+viewer = Automaton.parse('(a|b)*a(a|b)(a|b)(a|b)')
+
+
+#    l'alphabet
+alphabet = "a,b"
+
+#    du message de l'énoncé décrivant le langage
+message = "l'ensemble des mots dont la 4ème lettre à partir de la droite est un a"
+==
+
+title = TD1 exo3 L4
+text ==
+On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez un automate reconnaissant {{ message }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
+</p>
+{% endif %}
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
