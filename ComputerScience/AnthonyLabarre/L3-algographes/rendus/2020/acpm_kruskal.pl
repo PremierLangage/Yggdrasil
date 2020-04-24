e3390911dbee411fe3a1afbf7a2c59aefb45b19e
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-04-24 16:20:51 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/acpm_kruskal.pl
@@ -57,6 +57,7 @@ pltest1==
 ...             print("certaines extrémités d'arêtes ne sont pas des sommets") #
 ==
 
+
 testname2 = Vérifications de acpm_kruskal sur une instance aléatoire
 pltest2==
 >>> from corrlib_graphes import * #
