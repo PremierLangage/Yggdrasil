6c9fa164755d7a0993d6c17fc25e6f213d4b5476
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-25 18:04:18 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/essai_selection.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/essai_selection.pl
@@ -0,0 +1,36 @@
+
+extends=selectiontemplate.pl
+
+
+
+
+
+
+cible= la variable cible contient l'objectif de selection 
+
+text==
+Dans le text suivant choisiez {{cible}}.
+
+==
+title=Choisir {{cible}}
+
+
+
+mots = ['toi', 'moi', 'eux', 'lui', 'elle', 'je', 'on', 's']
+
+
+textdelaselection==
+Toi plus moi, plus eux plus tous ceux qui le veulent.  <br/>
+Plus lui plus elle plus tous ceux qui sont seuls. 
+
+
+
+
+Allez venez et entrez dans la danse. 
+Allez venez, laissez faire l'insouciance. 
+A deux a mille je sais qu' on est capable. 
+Tout est possible tout est réalisable. 
+On peut s' enfuir bien plus haut que nos rêves. 
+On peut partir bien plus loin que la grève. 
+==
+
