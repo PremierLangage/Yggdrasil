632d2a624ac37b5fe9ac761277f16854a15467cc
Claire DAVID <Claire.David@u-pem.fr>
2020-04-25 16:53:02 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-3.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-3.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-3.pl
@@ -19,7 +19,7 @@ message = "L = { b^n.a^p tels que n ou p est impaire }"
 title = Partiel 2020 Exo1-3
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez une expression reconnaissant le langage correspondant à {{ message }} <br>
+Donnez une expression reconnaissant le langage  {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) !
