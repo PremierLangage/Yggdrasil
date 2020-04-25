377913f76065ac0ab6cc86ad27f3d80d7f353527
Claire DAVID <Claire.David@u-pem.fr>
2020-04-25 17:23:05 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-10-abmod2.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-10-abmod2.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-10-abmod2.pl
@@ -9,23 +9,45 @@ generate== #|py|
 #    l'alphabet
 #    la construction de l'automate pour tester la réponse
 
-
-viewer = Automaton.parse('XXX')
-
+viewer = '''
+    #states
+    S00
+    S01
+    S10
+    S11
+    #initials
+    S00
+    #accepting
+    S01
+    S10
+    #alphabet
+    a
+    b
+    #transitions
+    S00:a>S10
+    S01:a>S11
+    S10:a>S00
+    S11:a>S01
+    S00:b>S01
+    S01:b>S00
+    S10:b>S11
+    S11:b>S10
+'''
 alphabet = "a,b"
-message = "XXXXX"
+message = "des mots qui ont un nombre de a modulo 2 différent de son nombre de b modulo 2."
 ==
 
 title = TD3.1-10
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez une expression reconnaissant le langage correspondant à {{ message }} <br>
+Donnez une expression reconnaissant le langage {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
+
 
 
 
