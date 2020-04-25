c8d809837be490570b25f3f35b8f0a879c89efa3
Claire DAVID <Claire.David@u-pem.fr>
2020-04-25 16:46:17 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-1-multiples2.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-1-multiples2.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-1-multiples2.pl
@@ -13,7 +13,58 @@ generate== #|py|
 viewer = Automaton.parse('XXX')
 
 alphabet = "0,1"
-message = "XXXXX"
+message = "
+
+extends = ../templates/editor-regex.pl
+
+
+generate== #|py|
+## Ajouter ici la génération:
+#    du message de l'énoncé décrivant le langage
+#    l'alphabet
+#    la construction de l'automate pour tester la réponse
+
+
+viewer = Automaton.parse('XXX')
+
+alphabet = "a,b"
+message = "à la représentation des entiers en base 2 qui sont des multiples de 2"
+==
+
+title = Partiel 2020 Exo1-3
+text ==
+On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez une expression reconnaissant le langage correspondant à {{ message }} <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
+</p>
+{% endif %}
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"
 ==
 
 title = Partiel 2020 Exo1-3
