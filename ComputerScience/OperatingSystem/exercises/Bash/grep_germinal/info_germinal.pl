841c3932af77e8d756eeba25972b2abeb870fb86
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-25 10:20:53 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/grep_germinal/info_germinal.pl

--- ComputerScience/OperatingSystem/exercises/Bash/grep_germinal/info_germinal.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/grep_germinal/info_germinal.pl
@@ -19,7 +19,7 @@ extends=/ComputerScience/OperatingSystem/templates/bash_template.pl
 @ Germinal.txt
 
 author=Nicolas Borie
-title=Information à extraire du texte de Germinal
+title=Quelques lignes par leur contenu
 
 before==
 import random
