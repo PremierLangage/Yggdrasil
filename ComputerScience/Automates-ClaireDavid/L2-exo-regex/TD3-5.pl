17fa17114d5bfbf1ddb26f678b35e0f1566ecc51
Claire DAVID <Claire.David@u-pem.fr>
2020-04-25 16:58:39 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-5.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-5.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-5.pl
@@ -13,19 +13,20 @@ generate== #|py|
 viewer = Automaton.parse('XXX')
 
 alphabet = "a,b"
-message = "XXXXX"
+message = "des mots qui commencent par ab et dont la longueur est un multiple de 4."
 ==
 
 title = TD3.1-5
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez une expression reconnaissant le langage correspondant à {{ message }} <br>
+Donnez une expression reconnaissant le langage {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
+
 
 
 
