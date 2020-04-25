ebc736aa8f0df0f8dde9979c4a0bd704ef296251
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-25 18:06:49 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/essai_selection.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/essai_selection.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/essai_selection.pl
@@ -3,31 +3,14 @@ extends=selectiontemplate.pl
 
 
 
+# les mots visés 
+cible= les pronoms nominaux
+# la liste des mots a selectionner en minuscule 
+# la comparaison ce fait sur les minuscules
+mots = ['rien']
 
-
-text==
-Dans le text suivant choisiez {{cible}}.
-
-==
-title=Choisir {{cible}}
-
-
-
-mots = ['toi', 'moi', 'eux', 'lui', 'elle', 'je', 'on', 's']
-
-
+# le texte sur lequel vous souhaiter faire la selection 
 textdelaselection==
-Toi plus moi, plus eux plus tous ceux qui le veulent.  <br/>
-Plus lui plus elle plus tous ceux qui sont seuls. 
-
-
-
-
-Allez venez et entrez dans la danse. 
-Allez venez, laissez faire l'insouciance. 
-A deux a mille je sais qu' on est capable. 
-Tout est possible tout est réalisable. 
-On peut s' enfuir bien plus haut que nos rêves. 
-On peut partir bien plus loin que la grève. 
+Rien
 ==
 
