0528ab2d0153c92f60fcf7770e0ed3aa94e32263
David DOYEN <David.Doyen@u-pem.fr>
2020-04-25 02:58:28 +0200

    ddoyen modified Yggdrasil/builder/before2.py

--- builder/before2.py
+++ builder/before2.py
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     #sys.exit(1)
     with open(output_json, "w+") as f:
         #f.write(jsonpickle.encode(dic, unpicklable=False))
-        f.write(json.dumps(dic, cls=CustomEncoder))
+        json.dump(dic, f, cls=CustomEncoder)
     sys.exit(0)
 
 
