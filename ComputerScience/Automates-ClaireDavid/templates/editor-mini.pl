d3e468ca4a553c5471f33d6d8a6af86b7e359939
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-25 19:48:39 +0200

    mcisse modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
@@ -28,7 +28,7 @@ else:
             score = 0 
             grade(score, feedback_nondet)
     ## ou bien c'est ici...
-        elif len(viewer["states"]) == len(editor["states"]) :
+        elif len(infos_viewer["states"]) == len(info_editor["states"]) :
             score = 0
             grade = (score, feedback_non_mini)
         else :
