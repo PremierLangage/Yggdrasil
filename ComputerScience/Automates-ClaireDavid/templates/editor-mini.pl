444ae25d382bf981030f940e74828e6a1b7faaaf
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-04-25 20:19:55 +0200

    mcisse modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor-mini.pl
@@ -2,36 +2,39 @@
 
 extends = editor-L2.pl
 
+evaluator== #|py|
+from automaton import Automaton
+
+
 #* feedback when the automaton is non deterministic or not complete.
 feedback_nondet = <p class="warning-state">L'automate correspond au langage mais n'est pas deterministe complet.</p>
 
 #* feedback when the automaton is not minimal.
-feedback_nomini = <p class="warning-state">L'automate correspond au langage. Il est det complet mais n'est pas minimal.</p>
-
-evaluator== #|py|
-from automaton import Automaton
+feedback_mini = <p class="warning-state">L'automate correspond au langage. Il est det complet mais n'est pas minimal.</p>
 
 if attempt >= maxattempt: # timeout
     grade = (score, feedback_timeout)
 else:
     attempt += 1
     match, error = Automaton.compare(viewer, editor)
+    infos_editor , _ = Automaton.editor_properties(editor)
     info_viewer , _ = Automaton.properties(viewer)
-    info_editor , _ = Automaton.editor_properties(editor)
     if  attempt >= maxattempt and (error or not match): # error or no match after timeout
         grade = (score, feedback_timeout)
     elif error:
         grade = (-1, feedback_syntax_error.format(error))
     elif match is True :
-        if not (info_editor["deterministic"] and info_editor["complete"]):
+    ## ça a l'air de planter ici
+        if not (infos_editor[deterministic] and infos_editor[complete]):
             score = 0 
-            grade = (score, feedback_nondet)
-        elif len(info_viewer["states"]) != len(info_editor["states"]) :
+            grade(score,feedback_nondet)
+    ## ou bien c'est ici...
+        elif len(infos_viewer[states]) == len(infos_editor[states]) :
             score = 0
-            grade = (score, feedback_nomini)
+            grade = (score, feedback_non_mini)
         else :
             score = 100
-            grade = (score, feedback_match)
+            grade = (score, feedback_nomatch)
     else:
         score = 0
         grade=(score, feedback_nomatch)
