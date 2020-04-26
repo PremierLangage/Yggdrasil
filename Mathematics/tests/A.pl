0ad4f433175e9b879e591301e60cddc68e41673f
David DOYEN <David.Doyen@u-pem.fr>
2020-04-26 15:05:35 +0200

    ddoyen modified Yggdrasil/Mathematics/tests/A.pl

--- Mathematics/tests/A.pl
+++ Mathematics/tests/A.pl
@@ -2,8 +2,14 @@ extends = /model/basic.pl
 
 title = A
 
-text =
+text ==
+<div>
+{Ô} {rage} ! {Ô} {désespoir} ! {Ô} {vieillesse} {ennemie} ! <br>
+{N'}{ai}-{je} {donc} {tant} {vécu} {que} {pour} {cette} {infamie} ?
+</div>
+==
 
 form =
 
 style.A = A
+
