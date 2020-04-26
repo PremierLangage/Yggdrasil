f5d69cbdcdb00f5cf6d9f9c72bc384582f294c63
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2020-04-26 12:55:15 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/pl/affiche_binaires.pl

--- ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/pl/affiche_binaires.pl
+++ ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/pl/affiche_binaires.pl
@@ -1,6 +1,5 @@
 extends = ../../../templates/generic/generic.pl
 title = Affichage des mots binaires
-author = Antoine Meyer <antoine.meyer@u-pem.fr>
 text =@ affiche_binaires/enonce.md
 grader =@ affiche_binaires/grader.py
 
