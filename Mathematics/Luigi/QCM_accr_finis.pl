12ea37e185ed4f7948f9bd4ffb25917ed5d1a087
Luigi MANCA <Luigi.Manca@u-pem.fr>
2020-04-26 19:31:32 +0200

    lmanca modified Yggdrasil/Mathematics/Luigi/QCM_accr_finis.pl

--- Mathematics/Luigi/QCM_accr_finis.pl
+++ Mathematics/Luigi/QCM_accr_finis.pl
@@ -26,11 +26,12 @@ questions==
 - $% f(x)=E(x) %$  ( Ici, $% E(x) %$ est la partie entière de $% x %$ )
 
 ** Parmi les énoncés suivants, lequel correspond au Théorème de l'Inégalité des accroissements finis ?
-+ Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction continue sur $% [a,b] %$, dérivable sur $% ]a,b[ %$. Soit $% M\geq0 %$ tel que pour tout $% x\in ]a,b[ %$, $% f'(x)\leq M %$. Alors, $% |f(b)-f(a)|\leq M|b-a|%$.
++ Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction continue sur $% [a,b] %$, dérivable sur $% ]a,b[ %$. Soit $% M\geq0 %$ tel que pour tout $% x\in ]a,b[ %$, $% |f'(x)|\leq M %$. Alors, $% |f(b)-f(a)|\leq M|b-a|%$.
 - Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction continue sur $% [a,b] %$, dérivable sur $% ]a,b[ %$.  Alors, il existe $% M\geq 0 %$ tel que $% |f(b)-f(a)|\leq M|b-a|%$.
 - Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction continue sur $% [a,b] %$. Alors, $% |f(b)-f(a)|\leq M|b-a|%$.
-- Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction définie sur $% [a,b] %$, dérivable sur $% ]a,b[ %$. Soit $% M\geq0 %$ tel que pour tout $% x\in ]a,b[ %$, $% f'(x)\leq M %$. Alors, $% |f(b)-f(a)|\leq M|b-a|%$.
-- Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction continue sur $% [a,b] %$, dérivable sur $% ]a,b[ %$. Soit $% M\geq0 %$ tel que pour tout $% x\in ]a,b[ %$, $% f'(x)\leq M %$. Alors, $% |f(b)-f(a)|\geq M|b-a|%$.
+- Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction définie sur $% [a,b] %$, dérivable sur $% ]a,b[ %$. Soit $% M\geq0 %$ tel que pour tout $% x\in ]a,b[ %$, $% |f'(x)|\leq M %$. Alors, $% |f(b)-f(a)|\leq M|b-a|%$.
+- Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction continue sur $% [a,b] %$, dérivable sur $% ]a,b[ %$. Soit $% M\geq0 %$ tel que pour tout $% x\in ]a,b[ %$, $% |f'(x)|\leq M %$. Alors, $% |f(b)-f(a)|\geq M|b-a|%$.
+- Soit $% f:[a,b]\rightarrow \mathbb{R} %$ une fonction continue sur $% [a,b] %$, dérivable sur $% ]a,b[ %$. Soit $% M\geq0 %$ tel que pour tout $% x\in ]a,b[ %$, $% f'(x)\leq M %$. Alors, $% |f(b)-f(a)|\leq M|b-a|%$.
 
 
 ** Parmi les fonctions suivantes, indiquer celles qui satisfont les hypothèses de l'inégalité des accroissements finis sur l'intervalle $% [0,1 ] %$,: 
