c716b948e223711868a7f111a9ea369bee13e3f1
David DOYEN <David.Doyen@u-pem.fr>
2020-04-26 11:52:38 +0200

    ddoyen modified Yggdrasil/model/temp.pl

--- model/temp.pl
+++ model/temp.pl
@@ -4,6 +4,55 @@
 @ /utils/components/textselect.py [customtextselect.py]
 @ /utils/components/dragdrop.py [customdragdrop.py]
 
+style.basic ==
+<style>
+.icon-check-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    margin-left: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #721c24;
+    margin-left: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-check-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #155724;
+    margin-right: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #721c24;
+    margin-right: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.error-text-unit {
+    color: #721c24;
+    text-decoration: line-through red;
+}
+
+.success-text-unit {
+    color: #155724;
+    text-decoration: underline green;
+}
+==
+
 extracss ==
 <style>
 .icon-check-after p::after {
@@ -81,4 +130,5 @@ extrajs ==
     }
 </script>
 ==
+
 
