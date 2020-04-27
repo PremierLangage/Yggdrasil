fec2cb3fed34731e132aeb25105eee89d9c6d03e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 15:44:59 +0200

    drevuz modified Yggdrasil/electronique/elec04.pl

---
+++ electronique/elec04.pl
@@ -0,0 +1,14 @@
+
+extends= elec_.pl
+
+
+good==
+Le signal $%x(t)%$ est en avance de phase par rapport au signal $%y(t)%$.
+==
+
+bad==
+Les signaux $%x(t)%$ et $%y(t)%$ sont en phase.
+Le signal $%x(t)%$ est en retard de phase par rapport au signal $%y(t)%$.
+Le signal $%x(t)%$ est en retard ou en avance de phase par rapport au signal $%y(t)%$ suivant l'instant t d'observation.
+Les signaux $%x(t)%$ et $%y(t)%$ sont déphasés.
+==
