4eaa8ed872db7e035d4ae866c32fe1014925be7e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 15:40:52 +0200

    drevuz modified Yggdrasil/electronique/ele03.pl

--- electronique/ele03.pl
+++ electronique/ele03.pl
@@ -2,7 +2,10 @@
 extends= elec_.pl
 
 
-text += "\nQuelle est la phase à l'origine du signal"
+before==
+text += "   \nQuelle est la phase à l'origine du signal"
+==
+
 
 good== # les bonnes réponses une par ligne 
 30°
