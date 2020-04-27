531646b422585cd49b99243db5e0827c7bd2bf65
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 15:33:02 +0200

    drevuz modified Yggdrasil/electronique/elec02.pl

---
+++ electronique/elec02.pl
@@ -0,0 +1,15 @@
+
+
+extends= elec01.pl
+
+
+
+good== # les bonnes réponses une par ligne 
+La valeur efficace du signal est $%2%$ 
+==
+
+bad== # les mauvaises réponses une par ligne 
+La valeur efficace du signal est $%1/\sqrt(2)%$ 
+La valeur efficace du signal est $%\sqrt(2)%$ 
+La valeur efficace du signal est $%2\sqrt(2)%$ 
+==
