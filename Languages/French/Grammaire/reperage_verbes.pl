3956e1a22b585aa4560a411a0a78db2b79b82f57
David DOYEN <David.Doyen@u-pem.fr>
2020-04-27 18:50:48 +0200

    ddoyen modified Yggdrasil/Languages/French/Grammaire/reperage_verbes.pl

---
+++ Languages/French/Grammaire/reperage_verbes.pl
@@ -0,0 +1,13 @@
+extends = /Languages/model/select_sentences.pl
+
+text ==
+Sélectionner le ou les verbes conjugués la phrase ci-dessous.
+==
+
+sentences ==
+Quand Béfort {{vit}} Pierre au coin, il {{fut}} plus malheureux que lui.
+{{Viens}} avec moi, j’{{ai}} besoin de toi !
+Le chien {{aboya}} et les vaches {{partirent}} dans la bonne direction.
+{{Pars}} vite, {{emmène}} le chien avec toi, il t’{{aidera}} !
+==
+
