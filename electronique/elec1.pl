edc706a5872e72d9d800e4b9ed185699303f12e5
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 15:28:06 +0200

    drevuz modified Yggdrasil/electronique/elec1.pl

--- electronique/elec1.pl
+++ electronique/elec1.pl
@@ -2,4 +2,24 @@
 extends= /model/qcm/qcm.pl
 
 
+nb=4 # nombre de proposition 
+nbtrues= 1 # une bonne réponse 
 
+text==
+
+On considère les signaux $%x(t)%$ et $%y(t)%$ définis par 
+
+$%x(t)= 2cos(300t-\pi/4)%$
+
+ *Cochez les affirmations correctes.*
+==
+
+good== # les bonnes réponses une par ligne 
+La fréquence du signal est $%1/330%$ Hz
+==
+
+bad== # les mauvaises réponses une par ligne 
+
+
+
+==
