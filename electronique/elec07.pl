8504bf0f76e174c36a5d30d7550cd8f746d408f8
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 16:46:06 +0200

    drevuz modified Yggdrasil/electronique/elec07.pl

--- electronique/elec07.pl
+++ electronique/elec07.pl
@@ -11,13 +11,14 @@ Xfrequence=300
 l = [("",0,"0°"),("-\pi",-3.14,"X°"),("\pi",3.14,"X°"),("-\pi/2",3.14/2,"X°"),("-\pi/4",3.14/4,"X°")]
 triple=random.choice(l)
 XdecalageS,Xdecalage,rep = triple
-signalX=f"$%{Xintensite}cos({Xfrequence}t{XdecalageS})%$"
+signalX=f"{Xintensite}cos({Xfrequence}t {XdecalageS})"
 
 
-text="""
-Soit les signaux $%x(t)%$ et $y(t)%$ définis par :
+text=f"""
 
-$%x(t)={{signalX}}%$ et $y(t)={{signalY}}%$
+Soit le signal $%x(t)%$ définis par :
+
+$%x(t)={signalX}%$ 
 
 Quelle est la phase à l'origine du signal, $%x(t)%$.
 """
