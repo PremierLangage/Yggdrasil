b4aa26b645475ff738cf32e58e2357f4e6135e83
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 16:43:20 +0200

    drevuz modified Yggdrasil/electronique/elec07.pl

--- electronique/elec07.pl
+++ electronique/elec07.pl
@@ -2,21 +2,17 @@
 extends= /model/qcm/qcm.pl
 
 title = Un peu d'aléa.
+text=
 
 before==
 import random
 Xintensite=2
 Xfrequence=300
 l = [("",0,"0°"),("-\pi",-3.14,"X°"),("\pi",3.14,"X°"),("-\pi/2",3.14/2,"X°"),("-\pi/4",3.14/4,"X°")]
-triple=Random.choice(l)
+triple=random.choice(l)
 XdecalageS,Xdecalage,rep = triple
-signalX=f"$%{Xintensite}cos({Xfrequence}t{XdecalageS})%$
+signalX=f"$%{Xintensite}cos({Xfrequence}t{XdecalageS})%$"
 
-Yintensite=1
-Yfrequence=300
-YdecalageS=""
-Ydecalage=0
-signalY=f"$%{Yintensite}cos({Yfrequence}t-{YdecalageS}\pi)%$
 
 text="""
 Soit les signaux $%x(t)%$ et $y(t)%$ définis par :
@@ -26,7 +22,13 @@ $%x(t)={{signalX}}%$ et $y(t)={{signalY}}%$
 Quelle est la phase à l'origine du signal, $%x(t)%$.
 """
 good=rep
-bad = "\n".join([z for x,y,z in l-triple])
+bad = "\n".join([z for x,y,z in l if z != rep])
 ==
 
-
+attic==
+Yintensite=1
+Yfrequence=300
+YdecalageS=""
+Ydecalage=0
+signalY=f"$%{Yintensite}cos({Yfrequence}t-{YdecalageS}\pi)%$"
+==
