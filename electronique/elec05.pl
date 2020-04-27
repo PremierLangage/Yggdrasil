ef979a1d528885abe74b37b6a6e78d2b4a31ffe2
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 15:50:16 +0200

    drevuz modified Yggdrasil/electronique/elec05.pl

---
+++ electronique/elec05.pl
@@ -0,0 +1,22 @@
+
+extends= elec_.pl
+
+
+mybuild==
+
+text +="""   
+
+Quel est le phaseur du signal $%y(t)%$ ?
+"""
+==
+
+good==
+$%20e^{j0}%$
+==
+
+bad==
+$%20e^{j\pi/2}%$
+$%20e^{j300}%$
+$%20e^{-j\pi/2}%$
+==
+
