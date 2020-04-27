8805f9e9b620cae9bd6eba29ba912d7ffbb70e03
Claire DAVID <Claire.David@u-pem.fr>
2020-04-27 22:02:18 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/autodet-mini-ex1.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/autodet-mini-ex1.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/autodet-mini-ex1.pl
@@ -10,7 +10,33 @@ from automaton import Automaton
 
 editor.debug = False
 editor.editorHeight = "520px"
-viewer = Automaton.parse('bb*a(b*a)*')
+
+viewer = '''
+    #states
+    S0
+    S1
+    S2
+    S3
+    S4
+    S5
+    #initials
+    S00
+    #accepting
+    S01
+    S10
+    #alphabet
+    a
+    b
+    #transitions
+    S00:a>S10
+    S01:a>S11
+    S10:a>S00
+    S11:a>S01
+    S00:b>S01
+    S01:b>S00
+    S10:b>S11
+    S11:b>S10
+'''
 
 #    l'alphabet
 alphabet = "a,b"
