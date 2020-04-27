04bc93a3710110baf77beb48cb83bbd41c2cd6bc
Claire DAVID <Claire.David@u-pem.fr>
2020-04-27 23:21:20 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl
@@ -50,26 +50,25 @@ viewer_enonce.automaton = '''
     S4
     S5
     #initials
-    S0
+    S1
     #accepting
-    S0
-    S2
+    S1
+    S4
     #alphabet
     a
     b
     #transitions
-    S0:a>S4
     S0:b>S1
     S1:a>S0
+    S1:a>S5
     S1:b>S2
-    S2:a>S5
+    S2:a>S1
     S2:b>S1
-    S3:a>S0
-    S3:b>S4
-    S4:a>S2
-    S4:b>S5
-    S5:a>S2
-    S5:b>S3
+    S3:a>S4
+    S3:b>S1
+    S4:a>S5
+    S4:b>S3
+    S5:b>S4
 '''
 
 #    l'alphabet
