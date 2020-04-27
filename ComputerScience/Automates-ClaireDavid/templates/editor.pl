3bf89efe2ae781f68fa9c86a29119d202a227e78
Claire DAVID <Claire.David@u-pem.fr>
2020-04-27 22:54:56 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/editor.pl

--- ComputerScience/Automates-ClaireDavid/templates/editor.pl
+++ ComputerScience/Automates-ClaireDavid/templates/editor.pl
@@ -146,6 +146,7 @@ exec(generate)
 if 'viewer' not in globals():
     raise Exception('The script "generate" must define a variable "viewer" which is an automaton')
 
+viewer2 = Automaton.viewer(viewer2)
 viewer = Automaton.viewer(viewer)
 ==
 
