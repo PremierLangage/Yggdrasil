cc4cc50889d9e0edf506daa7f45b3de302d3b62a
Claire DAVID <Claire.David@u-pem.fr>
2020-04-27 23:19:27 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl

---
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl
@@ -0,0 +1,121 @@
+
+
+
+
+extends = ../templates/editor-mini.pl
+
+
+
+## Ajouter ici la génération de l'automate et autres variables de l'exo
+generate== #|py|
+from automaton import Automaton
+
+viewer = '''
+    #states
+    S0
+    S1
+    S2
+    S3
+    S4
+    S5
+    #initials
+    S1
+    #accepting
+    S1
+    S4
+    #alphabet
+    a
+    b
+    #transitions
+    S0:a>S4
+    S0:b>S1
+    S1:a>S0
+    S1:b>S2
+    S2:a>S5
+    S2:b>S1
+    S3:a>S0
+    S3:b>S4
+    S4:a>S2
+    S4:b>S5
+    S5:a>S2
+    S5:b>S3
+'''
+
+viewer_enonce = AutomatonDrawer()
+viewer_enonce.automaton = '''
+    #states
+    S0
+    S1
+    S2
+    S3
+    S4
+    S5
+    #initials
+    S0
+    #accepting
+    S0
+    S2
+    #alphabet
+    a
+    b
+    #transitions
+    S0:a>S4
+    S0:b>S1
+    S1:a>S0
+    S1:b>S2
+    S2:a>S5
+    S2:b>S1
+    S3:a>S0
+    S3:b>S4
+    S4:a>S2
+    S4:b>S5
+    S5:a>S2
+    S5:b>S3
+'''
+
+#    l'alphabet
+alphabet = "a,b"
+==
+
+# dans ce qui suit le pb est que le viewer contient l'automate minimisé et non celui de départ
+title = Minimisation
+text ==
+{{ viewer_enonce|component }}
+
+{{ viewer|component }}
+
+On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+Donnez l'automate minimal déterministe complet correspondant àà l'automate ci-dessus. <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
+</p>
+{% endif %}
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
