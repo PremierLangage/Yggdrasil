e78437ed6a00707ffdc42e020390eb857ca675eb
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 15:35:16 +0200

    drevuz modified Yggdrasil/electronique/elec_.pl

---
+++ electronique/elec_.pl
@@ -0,0 +1,20 @@
+
+extends= /model/qcm/qcm.pl
+
+
+nb % 4 # nombre de proposition 
+nbtrues % 1 # une bonne réponse 
+
+title=signaux
+
+text==
+
+On considère les signaux $%x(t)%$ et $%y(t)%$ définis par 
+
+$%x(t)= 2cos(300t-\pi/4)%$  et 
+
+$%y(t)= sin(300t)%$  
+
+ *Cochez les affirmations correctes.*
+==
+
