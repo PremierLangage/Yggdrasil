2d1962cf24a27a44101745bb701847fd460cb8ea
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 16:47:21 +0200

    drevuz modified Yggdrasil/electronique/elec07.pl

--- electronique/elec07.pl
+++ electronique/elec07.pl
@@ -8,7 +8,7 @@ before==
 import random
 Xintensite=2
 Xfrequence=300
-l = [("",0,"0°"),("-\pi",-3.14,"12°"),("\pi",3.14,"13°"),("-\pi/2",3.14/2,"14°"),("-\pi/4",3.14/4,"15°")]
+l = [("",0,"0°"),("-\pi",-3.14,"12°"),("-\pi",-3.14,"12°"),("-\pi",-3.14,"12°"),("\pi",3.14,"13°"),("-\pi/2",3.14/2,"14°"),("-\pi/4",3.14/4,"15°")]
 triple=random.choice(l)
 XdecalageS,Xdecalage,rep = triple
 signalX=f"{Xintensite}cos({Xfrequence}t {XdecalageS})"
