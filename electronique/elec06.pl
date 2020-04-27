6519b22722977e1c13da8c02efa2b9965dd4961b
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 16:05:56 +0200

    drevuz modified Yggdrasil/electronique/elec06.pl

---
+++ electronique/elec06.pl
@@ -0,0 +1,33 @@
+
+
+
+
+extends= /model/qcm/qcm.pl
+
+
+
+
+nb % 4 # nombre de proposition 
+nbtrues % 1 # une bonne réponse 
+
+title=signaux
+
+
+condensateur=$ condensateur.png
+
+text==
+
+On considère le condensateur 
+<center><img src={{ condensateur }} alt='image du condensateur !'  /></center><br />
+# style=\"width:50px;height:100px;\"
+==
+
+good==
+good
+==
+bad==
+bad
+bad
+bad
+
+==
