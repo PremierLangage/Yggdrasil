f27e37af4519ecf678292b7fa12c08d1ba641dd3
Claire DAVID <Claire.David@u-pem.fr>
2020-04-27 23:34:31 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/autodet-mini-ex1.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/autodet-mini-ex1.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/autodet-mini-ex1.pl
@@ -9,7 +9,7 @@ extends = ../templates/editor-mini.pl
 generate== #|py|
 from automaton import Automaton
 
-soluce = '''
+enonce = '''
     #states
     S0
     S1
@@ -40,39 +40,10 @@ soluce = '''
     S5:b>S3
 '''
 
-viewer = soluce
+viewer = enonce
 
 viewer_enonce = AutomatonDrawer()
-viewer_enonce.automaton = '''
-    #states
-    S0
-    S1
-    S2
-    S3
-    S4
-    S5
-    #initials
-    S0
-    #accepting
-    S0
-    S2
-    #alphabet
-    a
-    b
-    #transitions
-    S0:a>S4
-    S0:b>S1
-    S1:a>S0
-    S1:b>S2
-    S2:a>S5
-    S2:b>S1
-    S3:a>S0
-    S3:b>S4
-    S4:a>S2
-    S4:b>S5
-    S5:a>S2
-    S5:b>S3
-'''
+viewer_enonce.automaton = enonce
 
 #    l'alphabet
 alphabet = "a,b"
