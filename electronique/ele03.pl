6a6325df6132685bc0d7eff43b9a2d66b9f12085
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-27 15:38:48 +0200

    drevuz modified Yggdrasil/electronique/ele03.pl

--- electronique/ele03.pl
+++ electronique/ele03.pl
@@ -2,4 +2,15 @@
 extends= elec_.pl
 
 
+text += "\nQuelle est la phase à l'origine du signal"
+
+good== # les bonnes réponses une par ligne 
+30°
+==
+
+bad== # les mauvaises réponses une par ligne 
+135°
+-45°
+45°
+==
 
