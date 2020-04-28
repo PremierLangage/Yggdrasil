27b22c29f05ff90c92d052389ab283eb624e97e0
David DOYEN <David.Doyen@u-pem.fr>
2020-04-28 19:09:08 +0200

    ddoyen modified Yggdrasil/builder/before2.py

--- builder/before2.py
+++ builder/before2.py
@@ -2,7 +2,7 @@ import sys, json
 from components import Component
 import uuid
 from mylib import aux_component
-from mylib import PickleEncoder, add_try_clause
+from mylib import PickleEncoder, add_try_clause, ComponentEnv
 
 # Load the custom JSON encoder
 try:
