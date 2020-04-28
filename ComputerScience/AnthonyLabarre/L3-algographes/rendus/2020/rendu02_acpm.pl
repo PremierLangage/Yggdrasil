32101a4299a12574a01d19c2c980a06e0e03a049
Claire DAVID <Claire.David@u-pem.fr>
2020-04-28 23:08:20 +0200

    cdavid modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu02_acpm.pl
@@ -222,30 +222,29 @@ pltest3==
 ==
 
 # Vérifications de fcpm_prim sur un graphe non connexe
-testname11 = Vérifications de fcpm_prim sur un graphe non connexe (union du cours et du td)
-pltest11==
+testname0 = Vérifications de fcpm_prim sur un graphe non connexe (union du cours et du td)
+pltest0==
 >>> from corrlib_graphes import * #
->>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS); G.ajouter_aretes(PRIM_INSTANCE_COURS) # initialisation
+>>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS); G.ajouter_aretes(PRIM_INSTANCE_TD) # initialisation
 >>> foret = fcpm_prim(G)
 >>> sum(poids for *_, poids in foret.aretes())
 41
 >>> if not est_foret_couvrante(foret, G):  # Le résultat est-il bien une forêt couvrante?
-...     print("la forêt calculée n'est pas couvrante) #
+...     print("la forêt calculée n'est pas couvrante") #
 ==
 
 # Vérifications de fcpm_prim sur un graphe non connexe
-testname12 = Vérifications de fcpm_prim sur un graphe non connexe  (union du cours, du td et d'un graphe aléatoire)
-pltest12==
+testname1 = Vérifications de fcpm_prim sur un graphe non connexe  (union du cours, du td et d'un graphe aléatoire)
+pltest1==
 >>> from corrlib_graphes import * #
->>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS); G.ajouter_aretes(PRIM_INSTANCE_TD); G.ajouter_aretes(aretes_ponderees(graphe_connexe_aleatoire_pondere(10))  # initialisation du graphe
+>>> G = Graphe(); G.ajouter_aretes(PRIM_INSTANCE_COURS); G.ajouter_aretes(PRIM_INSTANCE_TD); G.ajouter_aretes(aretes_ponderees(graphe_connexe_aleatoire_pondere(10)))  # initialisation du graphe
 >>> poids_solution_optimale = poids_fcpm(G.aretes())
 >>> foret = fcpm_prim(G); poids_obtenu = sum(poids for *_, poids in foret.aretes())
 >>> if poids_obtenu != poids_solution_optimale:
 ...     print("poids obtenu =", poids_obtenu, "; poids optimal =", poids_solution_optimale)
 >>> if not est_foret_couvrante(foret, G):  # Le résultat est-il bien une forêt couvrante?
-...     print("la forêt calculée n'est pas couvrante) #
+...     print("la forêt calculée n'est pas couvrante") #
 ==
-
 # Le grader:
 #@ /grader/evaluator.py [grader.py]
 evaluator==
