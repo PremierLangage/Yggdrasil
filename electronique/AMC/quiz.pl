746721d3e71ff90e034fb7837b9f5bc2b2a80a66
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-28 18:16:33 +0200

    drevuz modified Yggdrasil/electronique/AMC/quiz.pl

---
+++ electronique/AMC/quiz.pl
@@ -0,0 +1,57 @@
+
+title= De l'elec 
+tag= electronique 
+
+
+extends=/model/AMC_TXT.pl
+questions==
+
+
+* On considère les signaux $%x(t)%$ et $%y(t)%$ définis par  <br/>
+  $%x(t)= 2cos(300t-\pi/4)%$  et <br/>
+  $%y(t)= sin(300t)%$<br/>
+   *Cochez les affirmations correctes.*
++ La fréquence du signal est $%good%$ Hz
+- La fréquence du signal est $% bad %$ Hz
+- La fréquence du signal est $% worst %$ Hz
+- La fréquence du signal est $% nil %$ Hz
+
+* On considère les signaux $%x(t)%$ et $%y(t)%$ définis par  <br/>
+  $%x(t)= 2cos(300t-\pi/4)%$  et <br/>
+  $%y(t)= sin(300t)%$<br/>
+   *Cochez les affirmations correctes.*
++ La valeur efficace du signal est $%2%$ 
+- La valeur efficace du signal est $%1/\sqrt{2}%$ 
+- La valeur efficace du signal est $%\sqrt{2}%$ 
+- La valeur efficace du signal est $%2\sqrt{2}%$ 
+
+
+* On considère les signaux $%x(t)%$ et $%y(t)%$ définis par  <br/>
+  $%x(t)= 2cos(300t-\pi/4)%$  et <br/>
+  $%y(t)= sin(300t)%$<br/>
+  Quelle est la phase à l'origine du signal
++ 30°
+- 135°
+- -45°
+- 45°
+
+* On considère les signaux $%x(t)%$ et $%y(t)%$ définis par  <br/>
+  $%x(t)= 2cos(300t-\pi/4)%$  et <br/>
+  $%y(t)= sin(300t)%$<br/>
+   *Cochez les affirmations correctes.*
++ Le signal $%x(t)%$ est en avance de phase par rapport au signal $%y(t)%$.
+- Les signaux $%x(t)%$ et $%y(t)%$ sont en phase.
+- Le signal $%x(t)%$ est en retard de phase par rapport au signal $%y(t)%$.
+- Le signal $%x(t)%$ est en retard ou en avance de phase par rapport au signal $%y(t)%$ suivant l'instant t d'observation.
+- Les signaux $%x(t)%$ et $%y(t)%$ sont déphasés.
+
+* On considère les signaux $%x(t)%$ et $%y(t)%$ définis par  <br/>
+  $%x(t)= 2cos(300t-\pi/4)%$  et <br/>
+  $%y(t)= sin(300t)%$<br/>
+ Quel est le phaseur du signal $%y(t)%$ ?
++ $%20e^{j0}%$
+- $%20e^{j\pi/2}%$
+- $%20e^{j300}%$
+- $%20e^{-j\pi/2}%$
+
+==
