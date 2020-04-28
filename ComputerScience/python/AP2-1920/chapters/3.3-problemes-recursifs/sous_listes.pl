f3f89d7397e53f3a53f2af0adc838e9479f3e4bf
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2020-04-28 21:37:46 +0200

    ameyer modified Yggdrasil/ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/sous_listes.pl

--- ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/sous_listes.pl
+++ ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/sous_listes.pl
@@ -1,5 +1,5 @@
-extends = ../../templates/generic/generic.pl
+"extends = ../../templates/generic/generic.pl
 author = Antoine Meyer <antoine.meyer@u-pem.fr>
-title = Sous-listes d'une liste
+title = "Sous-listes d'une liste
 text =@ sous_listes/enonce.md
 grader =@ sous_listes/grader.py
