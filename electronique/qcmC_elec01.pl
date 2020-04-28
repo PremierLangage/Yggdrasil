9253848c58243f714150fb344230f6bc39bfcf27
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-28 11:19:48 +0200

    drevuz modified Yggdrasil/electronique/qcmC_elec01.pl

--- electronique/qcmC_elec01.pl
+++ electronique/qcmC_elec01.pl
@@ -20,13 +20,14 @@ $%y(t)= sin(300t)%$
 ==
 
 good== # les bonnes réponses une par ligne 
-La fréquence du signal est $%good%$ Hz
+La fréquence du signal est $%good%$ Hz| Je ne dirait qu'un mot : Bravo !
 ==
 
 bad== # les mauvaises réponses une par ligne 
-La fréquence du signal est $% bad %$ Hz
-La fréquence du signal est $% worst %$ Hz
-La fréquence du signal est $% nil %$ Hz
+La fréquence du signal est $% bad %$ Hz | A Non !
+La fréquence du signal est $% worst %$ Hz | Pas vraiment 
+La fréquence du signal est $% nil %$ Hz | Et la constante de Rôle $%\sqrt2/178%$
 ==
+
 
 
