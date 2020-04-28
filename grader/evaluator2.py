242cbeaa311c9bfd30c4903f1304c91675d52d32
David DOYEN <David.Doyen@u-pem.fr>
2020-04-28 09:22:15 +0200

    ddoyen modified Yggdrasil/grader/evaluator2.py

--- grader/evaluator2.py
+++ grader/evaluator2.py
@@ -3,7 +3,7 @@
 
 import sys, json, jsonpickle
 from components import Component
-from customdragdrop import DragDropGroup
+from temp import aux_component1, aux_component1
 
 from jinja2 import Template
 
@@ -65,54 +65,6 @@ The key 'evaluator' was not found in the context.
 When using this grader, the PL must declare a script inside a key 'evaluator'. This script have
 access to every variable declared in the PL and its 'before' script.
 It should declare a variable 'grade' which should contain a tuple (int, feedback) where int is the grade between [0, 100]."""
-
-missing_grade_stderr = """\
-'evaluator' did not declare the variable 'grade'.
-The script have access to every variable declared in the PL and its 'before' script.
-It should declare a variable 'grade' which should contain a tuple (int, feedback) where int is the grade between [0, 100]."""
-
-# HACK for components in lists
-# dictionaries with cid key in lists are replaced
-# by corresponding components
-def aux_component1(dic):
-    for key in dic:
-        if isinstance(dic[key], list):
-            for i in range(len(dic[key])):
-                item = dic[key][i]
-                if isinstance(item, dict) and 'cid' in item:
-                    name = item['name']
-                    dic[key][i] = dic[name]
-                    dic[key][i].name = name
-        if isinstance(dic[key], dict) and 'serialize' in dic[key] and dic[key]['serialize'] == 'DragDropGroup':
-            for k, item in dic[key]['labels'].items():
-                    name = item['name']
-                    dic[key]['labels'][k] = dic[name]
-                    dic[key]['labels'][k].name = name
-            for k, item in dic[key]['drops'].items():
-                    name = item['name']
-                    dic[key]['drops'][k] = dic[name]
-                    dic[key]['drops'][k].name = name
-
-            dic[key] = DragDropGroup.fromdict(**dic[key])
-
-# HACK for components in lists
-# components in lists are duplicated outside the lists
-# and replaced by dictionaries inside the lists
-def aux_component2(dic):
-    for key in dic:
-        if isinstance(dic[key], list):
-            for i in range(len(dic[key])):
-                item = dic[key][i]
-                if isinstance(item, Component):
-                    name = item.name
-                    dic[key][i] = {"cid": item.cid, "name": name, "selector": item.selector}
-        if isinstance(dic[key], DragDropGroup):
-            for k, item in dic[key].labels.items():
-                    name = item.name
-                    dic[key].labels[k] = {"cid": item.cid, "name": name, "selector": item.selector}
-            for k, item in dic[key].drops.items():
-                    name = item.name
-                    dic[key].drops[k] = {"cid": item.cid, "name": name, "selector": item.selector}
 
 if __name__ == "__main__":
 
@@ -183,6 +135,7 @@ if __name__ == "__main__":
     print(int(score))
     
     sys.exit(0)
+
 
 
 
