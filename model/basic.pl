6c0e028b760e0be69ce0e73cf27c649fa7b4cfaf
David DOYEN <David.Doyen@u-pem.fr>
2020-04-28 19:18:02 +0200

    ddoyen modified Yggdrasil/model/basic.pl

--- model/basic.pl
+++ model/basic.pl
@@ -2,7 +2,7 @@ extends = temp.pl
 
 @ /builder/before2.py [builder.py]
 @ /grader/evaluator2.py [grader.py]
-@ /utils/builderlib.py [contextlib.py]
+@ /utils/builderlib.py
 
 settings.maxattempt % 1
 
