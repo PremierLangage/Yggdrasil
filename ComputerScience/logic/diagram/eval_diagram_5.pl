1fed9d04995e251328ceedc165de3da3b29b028e
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-28 14:45:23 +0200

    nborie modified Yggdrasil/ComputerScience/logic/diagram/eval_diagram_5.pl

--- ComputerScience/logic/diagram/eval_diagram_5.pl
+++ ComputerScience/logic/diagram/eval_diagram_5.pl
@@ -23,7 +23,7 @@ builder =@ /builder/before.py
 group =: RadioGroup
 
 before==#|python|
-from utils_diagram import generate_random_diagram, diagram_to_string, eval_diagram
+from utils_diagram import generate_random_diagram, diagram_to_string, eval_diagram, diagram_to_string_color
 from components import GraphDrawer
 from random import randint
 
@@ -41,6 +41,8 @@ for i in range(size_diagram):
 
 final_value = True if eval_diagram(diagram, eval_point) else False
 
+graph_correction = diagram_to_string_color(diagram, eval_point)
+
 group.items = []
 group.items.append({
     "id": "true",
@@ -52,7 +54,7 @@ group.items.append({
 })
 ==
 
-title=Évaluation d'un circuit logique
+title=Évaluation d'un petit circuit logique moyen
 
 text==
 <style>
@@ -97,7 +99,7 @@ evaluator==#|python|
 S = group.selection
 score=50
 
-text=" "
+viewer.graph = graph_correction
 
 for item in group.items:
     item['css'] = ''
@@ -111,6 +113,8 @@ for item in group.items:
             feedback = '<span class="error-state animated pulse infinite">Mauvaise réponse</span>'
     elif item['content'] == str(final_value):
         item['css'] = 'success-border animated pulse infinite'
+
+feedback = '<center>' + feedback + '</center>'
 
 grade = (score, feedback)
 ==
