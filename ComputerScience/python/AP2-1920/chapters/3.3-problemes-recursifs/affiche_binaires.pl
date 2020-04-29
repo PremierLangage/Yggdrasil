d86e00c0076552c3a482fad7262ab1fde70fcff9
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2020-04-29 16:21:32 +0200

    ameyer deleted /srv/local/pl/premierlangage/home/Yggdrasil/ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/affiche_binaires.pl

--- ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/affiche_binaires.pl
+++
@@ -1,5 +0,0 @@
-extends = ../../templates/generic/generic.pl
-author = Antoine Meyer <antoine.meyer@u-pem.fr>
-title = Affichage des mots binaires
-text =@ affiche_binaires/enonce.md
-grader =@ affiche_binaires/grader.py
