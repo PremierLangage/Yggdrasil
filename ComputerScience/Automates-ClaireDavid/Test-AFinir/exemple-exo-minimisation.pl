f8d5254840fc35b0e3312a939ea6450e54856589
Claire DAVID <Claire.David@u-pem.fr>
2020-04-29 15:19:33 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/exemple-exo-minimisation.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/exemple-exo-minimisation.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/exemple-exo-minimisation.pl
@@ -1,6 +1,7 @@
 
 extends = ../templates/editor-mini.pl
 
+image_enonce =$ automate_1.png
 
 ## Ajouter ici la génération de l'automate et autres variables de l'exo
 generate== #|py|
@@ -18,6 +19,8 @@ message = 'bb * a(b * a) *'
 title = Minimisation
 text ==
 
+<img src="{{ image_enonce }}" style=" y a du style !" >
+
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez l'automate minimal déterministe complet reconnaissant le langage {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
@@ -26,6 +29,7 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
+
 
 
 
