65be6ccd769a2221b1f0a062a27fabd038550212
David DOYEN <David.Doyen@u-pem.fr>
2020-04-29 10:54:58 +0200

    ddoyen modified Yggdrasil/demo/graphics/static_matplotlib.pl

--- demo/graphics/static_matplotlib.pl
+++ demo/graphics/static_matplotlib.pl
@@ -6,7 +6,6 @@ before ==
 import matplotlib.pyplot as plt
 import numpy as np
 from plmpl import fig2base64
-plt.figure(figsize=(3,4))
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2*np.pi*t)
 plt.plot(t, s)
@@ -15,7 +14,7 @@ plt.ylabel('voltage (mV)')
 plt.title('About as simple as it gets, folks')
 plt.grid(True)
 
-source = fig2base64(plt.gcf(), format="png")
+source = fig2base64(plt.gcf(), format="png", dpi=50)
 ==
 
 text ==
