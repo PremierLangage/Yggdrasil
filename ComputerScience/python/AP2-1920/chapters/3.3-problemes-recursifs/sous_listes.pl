3fa418cb8f6c1ad12cb87fe0e313602d871ede27
Antoine MEYER <Antoine.Meyer@u-pem.fr>
2020-04-29 16:21:35 +0200

    ameyer deleted /srv/local/pl/premierlangage/home/Yggdrasil/ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/sous_listes.pl

--- ComputerScience/python/AP2-1920/chapters/3.3-problemes-recursifs/sous_listes.pl
+++
@@ -1,6 +0,0 @@
-extends = ../../templates/generic/generic.pl
-author = Antoine Meyer &lt;<a href='mailto:antoine.meyer@u-pem.fr'>antoine.meyer@u-pem.fr</a>&gt;
-title = "Sous-listes d'une liste
-text =@ sous_listes/enonce.md
-grader =@ sous_listes/grader.py
-
