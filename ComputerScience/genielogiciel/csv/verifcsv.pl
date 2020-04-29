98581a71513fbd912ba239e1403c2aa382e3f0e7
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-29 14:46:18 +0200

    drevuz modified Yggdrasil/ComputerScience/genielogiciel/csv/verifcsv.pl

--- ComputerScience/genielogiciel/csv/verifcsv.pl
+++ ComputerScience/genielogiciel/csv/verifcsv.pl
@@ -1,4 +1,25 @@
+#*****************************************************************************
+#  Copyright (C) 2020 DR <dr at univ-mlv dot fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#**
+
+
+
 
 @ verifcsv.py 
 
+title=
+text=
+form=
 
