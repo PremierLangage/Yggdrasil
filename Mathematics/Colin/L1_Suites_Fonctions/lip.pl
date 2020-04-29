870b960a30e54ffaa07df05289363cd9f2a0d494
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-29 16:39:40 +0200

    drevuz modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/lip.pl

---
+++ Mathematics/Colin/L1_Suites_Fonctions/lip.pl
@@ -0,0 +1,26 @@
+
+
+title= Des fonctions Lipschitziennes
+tag= Lipschitz|function|math|continuity 
+
+
+# ce code permet d'ajouter des qestions 
+buildquestions==
+import random
+
+for _ in range(5):
+    a=random.randint(1,5)
+    b=random.randint(-5,5)
+    a2=a//random.randint(1,5)
+    questions += f"""\
+    * Soit la fonction $%{{ f }}%$ dites si 
+    + est {{ a }}-Lipschitzienne 
+    - est {{ b }}-Lipschitzienne 
+    - est k-Lipschitzienne pour tout k
+    - est {{ a2 }} 
+    """
+
+==
+
+extends=/model/AMC_TXT.pl
+
