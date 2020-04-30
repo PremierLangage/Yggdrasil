2dd1b798c8937ccd99c5c651dd0d784a44c65fcb
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-30 09:28:26 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/templates/utils_bash.py

--- ComputerScience/OperatingSystem/templates/utils_bash.py
+++ ComputerScience/OperatingSystem/templates/utils_bash.py
@@ -88,3 +88,42 @@ def frame_message(message, status=None):
     s += "</div>"
     return s
     
+
+def display_as_script_shell_this(command, output, user=None, errout="", returncode=0):
+    r"""
+    Return a nice shell looking of the command and its output in html/css
+    """
+    s = ''
+    # Information about process termination
+    if returncode == 0:
+        s += "Process exited normally"
+    elif returncode > 0:
+        s += "Process exited normally with code "+str(returncode)
+    else:
+        if -returncode in signals:
+            s += "Process exited with signal ("+str(-returncode)+") "+signals[-returncode]
+        else:
+            s += "Process exited with signal ("+str(-returncode)+")"
+    # making the div for terminal screen
+    s += '<br /><div style="background-color: black; '
+    s += 'background-image: radial-gradient(rgba(0, 150, 0, 0.75), black 120%); '
+    #s += 'height: 80vh; '
+    s += 'font: 1rem Inconsolata, monospace; '
+    s += 'border-radius: 15px; padding: 10px;" >'
+    s += '<pre><output style="color: white; text-shadow: 0 0 5px #C8C8C8;">'
+
+    # if command do not end with a newline, we had it...
+    while command[-1] == '\n':
+        command = command[:-1]
+    if user is not None:
+        s += user+"@PLaTon"
+    s += ":~$> " + command.replace('\n', '<br />')
+    s += "<br />"
+    if len(errout) > 0:
+        s += '<span style="color: red; text-shadow: 0 0 5px #C80000;">'
+        s += errout.replace('\n', ' <br />')
+        s += '</span>'
+    s += output.replace('\n', ' <br />')
+
+    s += "</output></pre></div>"
+    return s
