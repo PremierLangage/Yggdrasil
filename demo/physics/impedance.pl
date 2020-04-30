3827b972f18c28a34810da85a03c3b3a18a65fb2
David DOYEN <David.Doyen@u-pem.fr>
2020-04-30 17:21:13 +0200

    ddoyen modified Yggdrasil/demo/physics/impedance.pl

--- demo/physics/impedance.pl
+++ demo/physics/impedance.pl
@@ -7,17 +7,17 @@ input.virtualKeyboards = electronics
 before ==
 var('R,L,omega', real=True, nonzero=True)
 X1 = R + I * L * omega
-X2 = R * R
+X2 = R
 sol = simplify(1/(1/X1 + 1/X2))
-sol = R *(1 + R)
+#sol = R *(1 + R)
 # h = expand_complex(X1/X2)
 # h = X1/X2
 # b = h.is_complex
-b = srepr(sol)
-c = srepr(str2sympy("R(1+R)", local_dict={"i": I, "omega": omega, "R": R, "L": L}))
-transformations = (prs.standard_transformations + (prs.implicit_multiplication_application,))
-import sympy.parsing.sympy_parser as prs
-d = srepr(prs.parse_expr("sol", global_dict=globals(), transformations=transformations))
+#b = srepr(sol)
+#c = srepr(str2sympy("R(1+R)", local_dict={"i": I, "omega": omega, "R": R, "L": L}))
+#transformations = (prs.standard_transformations + (prs.implicit_multiplication_application,))
+#import sympy.parsing.sympy_parser as prs
+#d = srepr(prs.parse_expr("sol", global_dict=globals(), transformations=transformations))
 ==
 
 text ==
