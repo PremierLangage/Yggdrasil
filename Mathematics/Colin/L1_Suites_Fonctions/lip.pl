5223a86655c9d8b2699f660c7ca22925975f0568
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-30 17:44:54 +0200

    drevuz modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/lip.pl

--- Mathematics/Colin/L1_Suites_Fonctions/lip.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/lip.pl
@@ -1,31 +1,46 @@
 
 
-title=Des fonctions Lipschitziennes
+title= Des fonctions Lipschitziennes
 tag= Lipschitz|function|math|continuity 
 
-extends=/model/AMC_TXT.pl
 
 # ce code permet d'ajouter des qestions 
 buildquestions==
 import random
+questions=""
+for _ in range(2):
+    a=random.randint(1,5)
+    b=random.randint(-5,5)
+    while n==a:
+        b=random.randint(-5,5)
+    a2=a//random.randint(1,5)
+    f=f"{a}x{b}" if b<0 else f"{a}x+{b}"
+    questions += f"""
+* Soit la fonction $%x  \mapsto  {f}%$ dites si 
++ est {a}-Lipschitzienne 
+- est {b}-Lipschitzienne 
+- est k-Lipschitzienne pour tout k
+- est {a2}-Lipschitzienne
+- n'est pas Lipschitzienne
+"""
 
-questions =""
-for _ in range(5):
+for _ in range(3):
     a=random.randint(1,5)
     b=random.randint(-5,5)
     a2=a//random.randint(1,5)
+    f=f"{a}x^2{b}" if b<0 else f"{a}x^2+{b}"
     questions += f"""
-
-* Soit la fonction $%{{ f }}%$ dites si 
-+ est {{ a }}-Lipschitzienne 
-- est {{ b }}-Lipschitzienne 
+* Soit la fonction $%x  \mapsto  {f}%$ dites si 
+- est {a}-Lipschitzienne 
+- est {b}-Lipschitzienne 
 - est k-Lipschitzienne pour tout k
-- est {{ a2 }} 
-    """
++ est Lipschitzienne sur l'interval ]-1,1[
+"""
+
+
+
 
 ==
 
 extends=/model/AMC_TXT.pl
-
-
 
