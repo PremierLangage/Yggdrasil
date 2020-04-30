74bb88825ae4ba57779317d8f982a3f4a44819d6
David DOYEN <David.Doyen@u-pem.fr>
2020-04-30 13:55:19 +0200

    ddoyen modified Yggdrasil/demo/physics/impedance.pl

--- demo/physics/impedance.pl
+++ demo/physics/impedance.pl
@@ -26,7 +26,7 @@ form ==
 evaluator ==
 score = 0
 ans = latex2sympy(input.value,  local_dict={"j": I, "omega": omega, "R": R, "L": L})
-feedback = str(expand_comlex(ans.is_complex))
+feedback = str(expand_complex(ans).is_complex)
 #score, _, feedback = eval_complex(input.value, sol, imaginary_unit="j",)
 ==
 
