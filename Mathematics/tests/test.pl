f12abd6e8c61a0bd9e2a1dbcf4fe95eacab1984b
David DOYEN <David.Doyen@u-pem.fr>
2020-04-30 22:44:44 +0200

    ddoyen modified Yggdrasil/Mathematics/tests/test.pl

---
+++ Mathematics/tests/test.pl
@@ -0,0 +1,33 @@
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+title = Addition
+
+before ==
+import sympy.parsing.sympy_parser as prs
+
+sp.var('R', real=True)
+transformations = (prs.standard_transformations + (prs.implicit_multiplication_application,))
+local_dict = {'R': R}
+expr = prs.parse_expr("R(1+R)",local_dict=local_dict, transformations=transformations,evaluate=False)
+s = sp.srepr(expr)
+==
+
+text ==
+{{s}}
+==
+
+form ==
+
+==
+
+
+
+evaluator ==
+grade = (100, "")
+==
+
+
+
+
