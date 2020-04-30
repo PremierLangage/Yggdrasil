7e7b9f92bdc5495172c7417298180d1d7f57a9e0
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-30 17:41:47 +0200

    drevuz modified Yggdrasil/electronique/AMC/quiz.pl

--- electronique/AMC/quiz.pl
+++ electronique/AMC/quiz.pl
@@ -6,21 +6,27 @@ tag= electronique
 # ce code permet d'ajouter des qestions 
 buildquestions==
 
-questions = """
-* Question pourrie 
-+ bonne réponse 
-- pourrie 
-- beurk 
-- Tu rigole $$\sqrt \pi $$
+
+import random
+Xintensite=random.randint(2,5)
+Xfrequence=20*random.randint(2,15)
+l = [("",0,"0°"),("-\pi",-3.14,"12°"),("-\pi",-3.14,"12°"),("-\pi",-3.14,"12°"),("\pi",3.14,"13°"),("-\pi/2",3.14/2,"14°"),("-\pi/4",3.14/4,"15°")]
+triple=random.choice(l)
+XdecalageS,Xdecalage,rep = triple
+signalX=f"{Xintensite}cos({Xfrequence}t {XdecalageS})"
 
 
-* Question pourrie 
-+ bonne réponse 
-- pourrie 
-- beurk 
-- Tu rigole $$\sqrt \pi $$
+text=f"""
 
-"""  +questions 
+Soit le signal $%x(t)%$ définis par :
+
+$%x(t)={signalX}%$ 
+
+Quelle est la phase à l'origine du signal, $%x(t)%$.
+"""
+title ="$% \\frac{R(R+j\omega L)}{2R+j\omega L}%$"
+good=rep
+bad = "\n".join([z for x,y,z in l if z != rep])
 
 
 ==
