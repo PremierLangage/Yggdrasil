3015a5c103b7d032083b746888511f09a3d4a93b
David DOYEN <David.Doyen@u-pem.fr>
2020-04-30 15:59:08 +0200

    ddoyen modified Yggdrasil/demo/physics/impedance.pl

--- demo/physics/impedance.pl
+++ demo/physics/impedance.pl
@@ -9,7 +9,7 @@ var('R,L,omega', real=True, nonzero=True)
 X1 = R + I * L * omega
 X2 = R
 sol = simplify(1/(1/X1 + 1/X2))
-sol = R*(I+L)
+sol = R * (I + L)
 # h = expand_complex(X1/X2)
 # h = X1/X2
 # b = h.is_complex
