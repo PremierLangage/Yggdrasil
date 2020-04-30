8f9f17f8e62badbc89ba408c2c67f1a3c93ffef7
Claire DAVID <Claire.David@u-pem.fr>
2020-04-30 21:41:03 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-2.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-2.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-2.pl
@@ -2,7 +2,7 @@
 extends = ../templates/editor-mini.pl
 
 
-#image_enonce =$ ../images/td8-ex1-1.png
+image_enonce =$ ../images/td8-ex1-2.png
 
 ## Ajouter ici la génération de l'automate et autres variables de l'exo
 generate== #|py|
@@ -53,7 +53,7 @@ alphabet = "a,b"
 # dans ce qui suit le pb est que le viewer contient l'automate minimisé et non celui de départ
 title = td8-ex1-2
 text ==
-<!--<img src="{{ image_enonce }}" style=" y a du style !" >-->
+<img src="{{ image_enonce }}" style=" y a du style !" >
 
 {{ viewer_enonce|component }}
 
