1594efe5b3c4ff84d50565a08cec0243ef19699e
David DOYEN <David.Doyen@u-pem.fr>
2020-04-30 13:40:36 +0200

    ddoyen modified Yggdrasil/demo/physics/impedance.pl

--- demo/physics/impedance.pl
+++ demo/physics/impedance.pl
@@ -7,7 +7,7 @@ input.virtualKeyboards = electronics
 before ==
 var('R,L,omega', real=True, nonzero=True)
 X1 = R + I * L * omega
-X2 = I
+X2 = R
 sol = simplify(1/(1/X1 + 1/X2))
 h = expand_complex(X1/X2)
 b = h.is_complex
