a1696d72d92846daa4b407ba92db922ef356a60c
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-30 17:43:13 +0200

    drevuz modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/lip.pl

--- Mathematics/Colin/L1_Suites_Fonctions/lip.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/lip.pl
@@ -13,16 +13,18 @@ for _ in range(5):
     a=random.randint(1,5)
     b=random.randint(-5,5)
     a2=a//random.randint(1,5)
-    questions += f"""\
-    * Soit la fonction $%{{ f }}%$ dites si 
-    + est {{ a }}-Lipschitzienne 
-    - est {{ b }}-Lipschitzienne 
-    - est k-Lipschitzienne pour tout k
-    - est {{ a2 }} 
+    questions += f"""
+
+* Soit la fonction $%{{ f }}%$ dites si 
++ est {{ a }}-Lipschitzienne 
+- est {{ b }}-Lipschitzienne 
+- est k-Lipschitzienne pour tout k
+- est {{ a2 }} 
     """
 
 ==
 
 extends=/model/AMC_TXT.pl
+
 
 
