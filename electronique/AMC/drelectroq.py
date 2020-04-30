11bd74ef8978a8e1078882003643de59cfb697b1
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-04-30 17:41:54 +0200

    drevuz modified Yggdrasil/electronique/AMC/drelectroq.py

--- electronique/AMC/drelectroq.py
+++ electronique/AMC/drelectroq.py
@@ -1,15 +1,16 @@
+import Random
 
-import sys
+def buildradio(text, good,bads):
+    s=""
+    s+= f"* {text} \n"
+    l=[f"+ {good}"]
+    for b in bads:
+        l.append(f"- {b}")
+    Random.shuffle(l)
+    s+= "\n".join(l)
+    s+="\n\n"
+    return s
 
 
-
-questions = """
-** Question pourrie 
-+ bonne réponce 
-- pourrie 
-- beurk 
-- Tu rigole $$\sqrt \pi $$
-
-"""  +questions 
 
 
