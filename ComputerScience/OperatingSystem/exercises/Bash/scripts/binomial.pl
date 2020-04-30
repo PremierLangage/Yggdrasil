490f14472fa8b079771ef7323285707423d6904f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-04-30 19:47:10 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/scripts/binomial.pl

---
+++ ComputerScience/OperatingSystem/exercises/Bash/scripts/binomial.pl
@@ -0,0 +1,35 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=script_template.pl
+
+before==#|python|
+
+test_bash = [
+]
+==
+
+author=Nicolas Borie
+title=Calculs des nombres binomiaux
+
+text==
+Écrire un script bash prenant deux entiers n puis p en arguments (c'est à 
+dire des chaines décrivant des entiers) et qui affiche le coefficient 
+binomial p parmi n : %$ \binom{n}{p} $%.
+==
+
+
+
