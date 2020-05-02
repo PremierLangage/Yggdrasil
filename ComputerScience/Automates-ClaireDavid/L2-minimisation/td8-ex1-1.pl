063ce7d3a04cf8f13a776408d4875d4226f5b4f1
Claire DAVID <Claire.David@u-pem.fr>
2020-05-03 00:02:40 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-1.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-1.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-1.pl
@@ -1,7 +1,7 @@
 extends = ../templates/new_editor-L2.pl
 
 #* titre de l'exercice
-title = exo mini from auto
+title = td8-ex1-1
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
@@ -56,7 +56,7 @@ alphabet = "a,b"
 ==
 
 # dans ce qui suit le pb est que le viewer contient l'automate minimisé et non celui de départ
-title = td8-ex1-1
+
 text ==
 <!--<img src="{{ image_enonce }}" style=" y a du style !" >-->
 
