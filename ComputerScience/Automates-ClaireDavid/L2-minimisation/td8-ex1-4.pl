cff8df3c6489bd07d38f25a012d9a2cf7e3675a7
Claire DAVID <Claire.David@u-pem.fr>
2020-05-03 00:06:19 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-4.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-4.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-4.pl
@@ -49,60 +49,28 @@ enonce = '''
     S7:b>S7
 '''
 
-#viewer est le langage solution -- le nom est forcé le reste. On changera plus tard
-viewer = enonce
-
-
 #viewer_enonce permet un affichage non modifié de l'automate enoncé qui décrit le langage
 viewer_enonce = AutomatonDrawer()
 viewer_enonce.automaton = enonce
 
-#    l'alphabet
-alphabet = "a,b"
+#langage solution -- le nom solution est forcé par le template
+solution = Automaton.parse(enonce)
 ==
 
-# dans ce qui suit le pb est que le viewer contient l'automate minimisé et non celui de départ
-title = td8-ex1-4
-text ==
-
+#* Texte de l'énoncé
+# ---
+# ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
+# l'automate minimisé du langage solution
+text==
 {{ viewer_enonce|component }}
-
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez l'automate minimal déterministe complet correspondant àà l'automate ci-dessus. <br>
+Donnez l'automate minimal déterministe complet correspondant à l'automate ci-dessus. <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
