257c875ebf23cb838a914580e9bdcaf754731525
David DOYEN <David.Doyen@u-pem.fr>
2020-05-02 13:02:28 +0200

    ddoyen modified Yggdrasil/demo/physics/physical_quantities.pl

--- demo/physics/physical_quantities.pl
+++ demo/physics/physical_quantities.pl
@@ -19,6 +19,26 @@ evaluator ==
 from sympy.physics.units.definitions.unit_definitions import meter, gram, kilogram, second, ampere, \
 kelvin, mole, candela, newton, joule, watt, pascal, hertz, coulomb, volt, ohm
 
+from sympy.physics.units import Quantity
+
+def get_numeric_unit(expr):
+    """
+    Return the numerical part and the unit of a physical quantity.
+    """
+    args = arg_nested_mul(expr)
+    args_numeric = []
+    args_quantity = []
+    for a in args:
+        if isinstance(a, sp.Pow):
+            if isinstance(a.base, Quantity):
+                args_quantity.append(a)
+            else:
+                args_numeric.append(a)
+        elif isinstance(a, Quantity):
+            args_quantity.append(a)
+        else:
+            args_numeric.append(a)
+    return (sp.Mul(*args_numeric), sp.Mul(*args_quantity))
 
 unit_dict = {'m': meter, 'g': gram, 'kg': kilogram, 's': second, 'A': ampere, 'K': kelvin, 'mol': mole, 'cd': candela, 'N': newton, 'J': joule, 'W': watt, 'Pa': pascal, 'Hz': hertz, 'C': coulomb, 'V': volt, 'Omega': ohm}
 
