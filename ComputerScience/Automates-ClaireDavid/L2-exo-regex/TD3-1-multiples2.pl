555492a646cc60cc7e262afb9b08da7285b9c7b7
Claire DAVID <Claire.David@u-pem.fr>
2020-05-02 23:20:28 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-1-multiples2.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-1-multiples2.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-1-multiples2.pl
@@ -5,10 +5,6 @@ title = TD3.1-1
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
-## Ajouter ici la génération:
-#    du message de l'énoncé décrivant le langage
-#    l'alphabet
-#    la construction de l'automate pour tester la réponse
 
 
 viewer = Automaton.parse('(0|1)*0')
