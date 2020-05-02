2b1a8a1f721b27df18abe212e0d206d0480daba7
Claire DAVID <Claire.David@u-pem.fr>
2020-05-02 23:57:32 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/auto-mini-ex1.pl
@@ -43,16 +43,13 @@ enonce = '''
     S5:b>S4
 '''
 
-#viewer est le langage solution -- le nom est forcé le reste. On changera plus tard
-viewer = enonce
-
 
 #viewer_enonce permet un affichage non modifié de l'automate enoncé qui décrit le langage
 viewer_enonce = AutomatonDrawer()
 viewer_enonce.automaton = enonce
 
-#    l'alphabet
-alphabet = "a,b"
+#langage solution -- le nom solution est forcé par le template
+solution = Automaton.parse(enonce)
 ==
 
 # dans ce qui suit le pb est que le viewer contient l'automate minimisé et non celui de départ
