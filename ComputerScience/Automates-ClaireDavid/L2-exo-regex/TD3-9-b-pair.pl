5bec7e35cdff080671f8845f361f04d9fcd73a2d
Claire DAVID <Claire.David@u-pem.fr>
2020-05-02 23:39:54 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-9-b-pair.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-9-b-pair.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-9-b-pair.pl
@@ -18,7 +18,10 @@ solution = Automaton.parse('(a*ba*b)*a*')
 #message de l'énoncé décrivant le langage
 message = "des mots qui ont un nombre pair de b."
 ==
-
+#* Texte de l'énoncé
+# ---
+# ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
+# l'automate minimisé du langage solution
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez une expression reconnaissant le langage {{ message }} <br>
