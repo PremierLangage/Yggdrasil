c31a613d8e044337d639476fa8f98e40307a0cfe
Claire DAVID <Claire.David@u-pem.fr>
2020-05-03 00:05:00 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-3.pl

--- ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-3.pl
+++ ComputerScience/Automates-ClaireDavid/L2-minimisation/td8-ex1-3.pl
@@ -1,7 +1,7 @@
 extends = ../templates/new_editor-L2.pl
 
 #* titre de l'exercice
-title = exo mini from auto
+title = td8-ex1-3
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
