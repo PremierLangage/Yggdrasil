762733426a1082f0a467d336e05f712b7a3241ce
David DOYEN <David.Doyen@u-pem.fr>
2020-05-02 15:49:48 +0200

    ddoyen modified Yggdrasil/demo/physics/light_resistance.pl

--- demo/physics/light_resistance.pl
+++ demo/physics/light_resistance.pl
@@ -61,38 +61,4 @@ Dimension : {dim}
 """
 ==
 
-from sympy.physics.units import Quantity
-from sympy.physics.units.systems.si import dimsys_SI, SI
-
-def get_numeric_unit(expr):
-    """
-    Return the numerical part and the unit of a physical quantity.
-    """
-    args = arg_nested_mul(expr)
-    args_numeric = []
-    args_quantity = []
-    for a in args:
-        if isinstance(a, sp.Pow):
-            if isinstance(a.base, Quantity):
-                args_quantity.append(a)
-            else:
-                args_numeric.append(a)
-        elif isinstance(a, Quantity):
-            args_quantity.append(a)
-        else:
-            args_numeric.append(a)
-    return (sp.Mul(*args_numeric), sp.Mul(*args_quantity))
-
-unit_dict = {'m': meter, 'g': gram, 'kg': kilogram, 's': second, 'A': ampere, 'K': kelvin, 'mol': mole, 'cd': candela, 'N': newton, 'J': joule, 'W': watt, 'Pa': pascal, 'Hz': hertz, 'C': coulomb, 'V': volt, 'Omega': ohm}
-
-score = 100
-ans = latex2sympy(input.value, local_dict=unit_dict)
-num, unit = get_numeric_unit(ans)
-dim = dimsys_SI.get_dimensional_dependencies(SI.get_dimensional_expr(unit))
-feedback = f"""
-Partie numérique : {num} ;
-Unité : {unit} ;
-Dimension : {dim}
-"""
-==
 
