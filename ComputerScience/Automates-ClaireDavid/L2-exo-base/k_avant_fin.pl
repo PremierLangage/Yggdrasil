83e9e9fe6b089613ba3a7891e67c7e15b027acd6
Claire DAVID <Claire.David@u-pem.fr>
2020-05-03 00:19:10 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/k_avant_fin.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/k_avant_fin.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/k_avant_fin.pl
@@ -16,7 +16,11 @@ solution = Automaton.parse('(a|b)*a(a|b)(a|b)(a|b)')
 message = "l'ensemble des mots dont la 4ème lettre à partir de la droite est un a"
 ==
 
-text ==
+#* Texte de l'énoncé
+# ---
+# ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
+# l'automate minimisé du langage solution
+text==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez un automate reconnaissant {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
