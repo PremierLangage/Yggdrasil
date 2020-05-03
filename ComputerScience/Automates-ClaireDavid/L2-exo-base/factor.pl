dc2b1f5c6690cd5c5d15cf1180a1cf4c752461f7
Claire DAVID <Claire.David@u-pem.fr>
2020-05-03 15:46:05 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
@@ -1,16 +1,17 @@
+extends = ../templates/new_editor-L2.pl
 
-# Migration à faire
-extends = ../templates/editor-L2.pl
+#* titre de l'exercice
+title = Facteur
 
+#* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
 from generator import Generator
-# A mettre ailleurs peut-être?
+
 import random
 #choisi le type d'exo
 #    -1: 'prefixe',
 #    0: 'facteur',
 #    1: 'suffixe'
-#mode = random.choice([-1, 0, 1])
 mode = 0
 messages = {
     -1: 'commencent par',
@@ -28,27 +29,31 @@ bla_titre = textes_titre[mode]
 # choix de la taille de l'alphabet
 nb_lettres = random.choice([2,3])
 
+#construction du langage solution, alphabet et mot
+# -- le nom solution est forcé par le template
 if mode == -1 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du prefix)
-    alphabet, mot, viewer = Generator.prefix(nb_lettres,3,
+    alphabet, mot, solution = Generator.prefix(nb_lettres,3,
     sorted_alphabet=True)
 elif mode == 0 :
     # Generator.prefix(nb lettres alphabet , longueur maxi du facteur)
-    alphabet, mot, viewer = Generator.factor(nb_lettres,3,
+    alphabet, mot, solution = Generator.factor(nb_lettres,3,
     sorted_alphabet=True)
 else :
     # Generator.prefix(nb lettres alphabet , longueur maxi du suffixe)
-    alphabet, mot, viewer = Generator.suffix(nb_lettres,3,
+    alphabet, mot, solution = Generator.suffix(nb_lettres,3,
     sorted_alphabet=True)
-
 
 alphabet = ', '.join(list(alphabet))
 ==
 
-title = Facteur
+#* Texte de l'énoncé
+# ---
+# ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
+# l'automate minimisé du langage solution
 text ==
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ bla }} **{{ mot }}** {{ '}' }} <br>
+Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ bla }}  **{{ mot }}** {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) !
@@ -56,7 +61,6 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) !
 {% endif %}
 
 ==
-
 
 
 
