f3df50bf0461c82aae74759089a3ee3666c6e6b3
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-03 15:11:38 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/scripts/check_args_int.pl

--- ComputerScience/OperatingSystem/exercises/Bash/scripts/check_args_int.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/scripts/check_args_int.pl
@@ -36,7 +36,12 @@ L6 = [randint(1,60), randint(40,100), randint(1,100)]
 test_bash = [ ["simple", ["1", "3", "2"], "La contrainte 1 <= 2 <= 3 est vérifiée.\n"], 
  ["simple", ["2", "3", "1"], "1 n'est pas compris entre 2 et 3.\n"], 
  ["simple", ["1", "2", "3"], "3 n'est pas compris entre 1 et 2.\n"], 
- ["aléatoire", [str(i) for i in L1], soluce(*L1)], 
+ ["aléatoire", [str(i) for i in L1], soluce(*L1)],
+ ["aléatoire", [str(i) for i in L2], soluce(*L2)],
+ ["aléatoire", [str(i) for i in L3], soluce(*L3)],
+ ["aléatoire", [str(i) for i in L4], soluce(*L4)],
+ ["aléatoire", [str(i) for i in L5], soluce(*L5)],
+ ["aléatoire", [str(i) for i in L6], soluce(*L6)], 
 ]
 ==
 
