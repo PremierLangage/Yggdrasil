ec965f5b7e6d095cbf975b160d53b287700592e4
Claire DAVID <Claire.David@u-pem.fr>
2020-05-03 16:00:56 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/new-pref-suf-fac.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/new-pref-suf-fac.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/new-pref-suf-fac.pl
@@ -1,7 +1,7 @@
 extends = ../templates/new_editor-L2.pl
 
 #* titre de l'exercice
-title = Préfixe
+title = Suffixe
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
