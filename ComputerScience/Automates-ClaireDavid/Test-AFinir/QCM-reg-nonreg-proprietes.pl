ca2556d957173919cb873427517a30bc3b0c8515
Claire DAVID <Claire.David@u-pem.fr>
2020-05-04 23:23:50 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/QCM-reg-nonreg-proprietes.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/QCM-reg-nonreg-proprietes.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/QCM-reg-nonreg-proprietes.pl
@@ -14,8 +14,35 @@ P - Si $u\in \Sigma^*$ et $L\subseteq \Sigma^*$ alors $u.L=L.u$
 + non
 - aucune idée
 
+* La propriété suivante est-elle vraie pour tout langage L et mot u
+P - Si $L$ et $L'$ sont des langages réguliers alors $L.L'$ est un langage régulier.
 
-* Si $L$ et $L'$ sont des langages réguliers alors $L.L'$ est un langage régulier.
+- oui
++ non
+- aucune idée
+
+* La propriété suivante est-elle vraie pour tout langage L et mot u
+P - Si $u\in \Sigma^*$ et $L\subseteq \Sigma^*$ alors $u.L=L.u$
+
+- oui
++ non
+- aucune idée
+
+* La propriété suivante est-elle vraie pour tout langage L et mot u
+P - Si $u\in \Sigma^*$ et $L\subseteq \Sigma^*$ alors $u.L=L.u$
+
+- oui
++ non
+- aucune idée
+
+* La propriété suivante est-elle vraie pour tout langage L et mot u
+P - Si $u\in \Sigma^*$ et $L\subseteq \Sigma^*$ alors $u.L=L.u$
+
+- oui
++ non
+- aucune idée
+
+* 
 * Si $L$ est régulier et $L'$ n'est pas régulier alors $L.L'$ n'est pas régulier.
 * Si $L.L'$ n'est pas régulier alors $L$ et $L'$ ne sont pas réguliers.
 * Si $L.L'$ n'est pas régulier alors au moins $L$ ou $L'$ n'est pas régulier.
