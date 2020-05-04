3725c42f726189851418afa5766b388ff4cca6af
Claire DAVID <Claire.David@u-pem.fr>
2020-05-04 23:05:55 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-QCM/QCM-reg-nonreg.pl

--- ComputerScience/Automates-ClaireDavid/Test-QCM/QCM-reg-nonreg.pl
+++ ComputerScience/Automates-ClaireDavid/Test-QCM/QCM-reg-nonreg.pl
@@ -10,21 +10,20 @@ questions==
 - non régulier
 - aucune idée
 
-** Parmis les affirmations suivantes lesquelles caractèrisent les cas d'utilisation
-+ Un dialogue entre un acteur et le système
-+ Un dialogue de bout en bout
-+ Un objectif pour l'acteur
-+ un cas d'utilisation est un Document naratif
+* {a^nb^m|\exists p\ n+m = 2p}
++ régulier
+- non régulier
+- aucune idée
 
-** Parmis les propositions suivantes indiquez celle qui sont des erreurs d'utilisation de la modélisation par use case:
-+ Décrire un sénario alternatif comme une extension (un autre cas d'utilisation)
-+ Décrire un besoin métier comme un cas d'utilisation
-+ Créer une association entre deux cas d'utilisation pour indiquer que les cas d'utilisation communiquent entre eux
-+ Découper les sous fonctions d'un cas d'utilisation en cas d'utilisation plus petits. 
-+ Modéliser des opérations élémentaires comme des cas d'utilisation
-+ Mélanger des niveaux d'abstraction dans le dialogue du cas d'utilisation
-+ Définir les niveaux de sécurité en liant les  niveaux de sécurité aux acteurs
+* {a^nb^m|\exists p\ n+m = 2p}
++ régulier
+- non régulier
+- aucune idée
 
+* {a^nb^m|\exists p\ n+m = 2p}
++ régulier
+- non régulier
+- aucune idée
 ==
 
 
