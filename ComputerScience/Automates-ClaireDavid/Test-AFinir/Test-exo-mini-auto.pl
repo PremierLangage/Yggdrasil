53415e938802da4e6c81837ea8830514f1392e29
Claire DAVID <Claire.David@u-pem.fr>
2020-05-04 10:27:13 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-mini-auto.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-mini-auto.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-mini-auto.pl
@@ -68,6 +68,7 @@ solution = Automaton.parse(enonce)
 # l'automate minimisé du langage solution
 text==
 {{ viewer_enonce|component }}
+{{ solution_view|component }}
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez l'automate minimal déterministe complet correspondant à l'automate ci-dessus. <br>
 {% if attempt < maxattempt and score != 100 %}
