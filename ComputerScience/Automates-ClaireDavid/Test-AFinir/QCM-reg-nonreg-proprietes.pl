3d8252cad7666cae5813247587dbb7453ae2b10b
Claire DAVID <Claire.David@u-pem.fr>
2020-05-04 23:22:47 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/QCM-reg-nonreg-proprietes.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/QCM-reg-nonreg-proprietes.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/QCM-reg-nonreg-proprietes.pl
@@ -1,27 +1,12 @@
 
 extends=/model/AMC_TXT.pl 
 
-title= QCM régulier - non régulier
+title= QCM propriétés
 
 message = Le langage suivant est-il est régulier ou non-régulier?
 
 # une étoile radio deux check box
 questions==
-* {{ message }} {a^nb^m|\exists p\ n+m = 2p}
-+ régulier
-- non régulier
-- aucune idée
-
-* {{ message }} L2
-+ régulier
-- non régulier
-- aucune idée
-
-* {{ message }} L3
-+ régulier
-- non régulier
-- aucune idée
-
 * La propriété suivante est-elle vraie pour tout langage L et mot u
 P - Si $u\in \Sigma^*$ et $L\subseteq \Sigma^*$ alors $u.L=L.u$
 
