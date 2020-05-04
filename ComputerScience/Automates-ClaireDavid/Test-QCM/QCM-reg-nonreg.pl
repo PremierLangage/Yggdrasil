a1ebcd4a0ae7fab2190fedf0d40758a9bde35d99
Claire DAVID <Claire.David@u-pem.fr>
2020-05-04 23:07:29 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-QCM/QCM-reg-nonreg.pl

--- ComputerScience/Automates-ClaireDavid/Test-QCM/QCM-reg-nonreg.pl
+++ ComputerScience/Automates-ClaireDavid/Test-QCM/QCM-reg-nonreg.pl
@@ -3,6 +3,8 @@ extends=/model/AMC_TXT.pl
 
 title= Pour chacun des langages suivants, dites s'il est régulier ou non-régulier? 
 
+message = Le langage suivant est-il est régulier ou non-régulier?
+
 # une étoile radio deux check box
 questions==
 * {a^nb^m|\exists p\ n+m = 2p}
