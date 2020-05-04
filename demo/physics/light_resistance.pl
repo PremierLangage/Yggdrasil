a18871241934f8e0cb21839d2fcb6af3b564ee17
David DOYEN <David.Doyen@u-pem.fr>
2020-05-04 15:41:35 +0200

    ddoyen modified Yggdrasil/demo/physics/light_resistance.pl

--- demo/physics/light_resistance.pl
+++ demo/physics/light_resistance.pl
@@ -18,7 +18,7 @@ evaluator ==
 unit_dict = {'m': meter, 'g': gram, 'kg': kilogram, 's': second, 'A': ampere, 'K': kelvin, 'mol': mole, 'cd': candela, 'N': newton, 'J': joule, 'W': watt, 'Pa': pascal, 'Hz': hertz, 'C': coulomb, 'V': volt, 'Omega': ohm}
 
 score, feedback, _ = eval_physical(input.value, sol, tol=1e-2, local_dict=unit_dict)
-feedback = str(get_numeric(sol))
+feedback = str(get_numeric_unit(sol))
 ==
 
 
