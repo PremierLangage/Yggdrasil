273d07fd5c1afe4bbe8b2106e88db86d5e22cd22
David DOYEN <David.Doyen@u-pem.fr>
2020-05-04 17:14:53 +0200

    ddoyen modified Yggdrasil/demo/physics/light_resistance.pl

--- demo/physics/light_resistance.pl
+++ demo/physics/light_resistance.pl
@@ -8,20 +8,17 @@ before ==
 var('a')
 U = 2*randint(5,15)*volt
 I = randint(1,20)/2*ampere
-sol = convert_to(a*U/I, ohm)
-ssol = str(srepr(sol))
+sol = convert_to(U/I, ohm)
 ==
 
 text ==
-{{ssol}}
 Une lampe est traversée par un courant de $! {{I|latex}} !$. La tension aux bornes de la lampe est $! {{U|latex}} !$. Déterminer la résistance de la lampe.
 ==
 
 evaluator ==
 unit_dict = {'m': meter, 'g': gram, 'kg': kilogram, 's': second, 'A': ampere, 'K': kelvin, 'mol': mole, 'cd': candela, 'N': newton, 'J': joule, 'W': watt, 'Pa': pascal, 'Hz': hertz, 'C': coulomb, 'V': volt, 'Omega': ohm}
 
-score, feedback, _ = eval_physical(input.value, sol, tol=1e-2, local_dict=unit_dict)
-feedback = str(srepr(sol))+str(get_numeric_unit(sol))
+score, _, feedback = eval_physical(input.value, sol, tol=1e-2, local_dict=unit_dict)
 ==
 
 
