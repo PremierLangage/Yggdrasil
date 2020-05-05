2f804ce4f7d56503bd8f6adc954c6c2524e59795
Claire DAVID <Claire.David@u-pem.fr>
2020-05-05 19:36:50 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/longueur-multiple3.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/longueur-multiple3.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/longueur-multiple3.pl
@@ -39,7 +39,7 @@ message = "le langage { u.v tels que  u,v ∈ Σ* et |u| = 2|v| }"
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
 text ==
-On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$ .<br/>
 Donnez un automate reconnaissant {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
@@ -47,6 +47,7 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
+
 
 
 
