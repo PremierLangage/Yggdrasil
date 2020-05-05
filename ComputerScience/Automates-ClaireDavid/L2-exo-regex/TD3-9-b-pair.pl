30b790079aa9fb6b641b57326cd714565ba4f797
Claire DAVID <Claire.David@u-pem.fr>
2020-05-05 19:30:03 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-9-b-pair.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-9-b-pair.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/TD3-9-b-pair.pl
@@ -24,7 +24,7 @@ message = "des mots qui ont un nombre pair de b."
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
 text ==
-On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$ .<br/>
 Donnez une expression reconnaissant le langage {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
@@ -32,6 +32,7 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
+
 
 
 
