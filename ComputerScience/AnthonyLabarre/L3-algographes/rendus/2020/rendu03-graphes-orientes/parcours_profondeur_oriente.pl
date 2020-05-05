73ace4ddfe6d98fda2315ae5defddb07f4acf5e5
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-05-05 15:41:30 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
@@ -14,58 +14,16 @@ text==
 # TODO: exemples des TDs, graphe aléatoire fortement connexe, graphe aléatoire faiblement connexe:
 # pour la vérification: attention, on ne leur impose plus les sommets de départ
 # comment vérifier?
+# 
 
 
 
-
-
-testname0 = Vérifications de parcours_profondeur_oriente sur l'exemple du cours
+# pas d'exemple intéressant fait au cours, on passe directement sur l'exercice du TD
+testname0 = Vérifications de parcours_profondeur_oriente sur l'exemple du TD
 pltest0==
->>> from corrlib_graphes import CYCLE_ORIENTE_INSTANCE_COURS #
->>> G = GrapheOriente(); G.ajouter_arcs(CYCLE_ORIENTE_INSTANCE_COURS) # initialisation de l'instance du cours
->>> cycle = detection_cycle(G)
->>> sorted(cycle.arcs())
-[(0, 3), (3, 4), (4, 0)]
-==
-
-testname1 = Vérifications de detection_cycle sur l'exemple du TD
-pltest1==
 >>> from corrlib_graphes import * #
 >>> G = GrapheOriente(); G.ajouter_arcs(CYCLE_ORIENTE_INSTANCE_TD) # initialisation de l'instance du TD
->>> arcs_cycle_trouve = set(detection_cycle(G).arcs())
->>> cycles_possibles = cycles_simples(G)
->>> if arcs_cycle_trouve not in cycles_possibles:
-...     print("les arcs identifiés:", arcs_cycle_trouve, "ne constituent pas un cycle du graphe")
-...     print("cycles possibles:", cycles_possibles)
-==
-
-testname2 = Vérifications de detection_cycle sur un DAG aléatoire
-pltest2==
->>> from corrlib_graphes import * #
->>> G = GrapheOriente(); G.ajouter_arcs(list(graphe_oriente_acyclique_aleatoire(20).edges())) # initialisation d'un DAG aléatoire
->>> obtenu = detection_cycle(G)
->>> if obtenu is not None:
-...     print("la fonction identifie le cycle", obtenu.arcs(), " mais le graphe est acyclique")
-...     print("ses arcs:", G.arcs())
-==
-
-testname3 = Vérifications de detection_cycle sur un graphe orienté aléatoire
-pltest3==
->>> from corrlib_graphes import * #
->>> G = GrapheOriente(); G.ajouter_arcs(list(graphe_oriente_aleatoire(20).edges())) # initialisation d'un graphe orienté aléatoire
->>> obtenu = detection_cycle(G)
->>> cycles_possibles = cycles_simples(G)
->>> if obtenu is None:
-...     if cycles_possibles:
-...         print("la fonction ne trouve pas de cycle alors que le graphe en contient", len(cycles_possibles))
-... else:
-...     if cycles_possibles:
-...         if arcs_cycle_trouve not in cycles_possibles:
-...             print("les arcs identifiés:", arcs_cycle_trouve, "ne constituent pas un cycle du graphe")
-...             print("cycles possibles:", cycles_possibles)
-...     else:
-...         print("la fonction identifie le cycle", obtenu.arcs(), " mais le graphe est acyclique")
-...         print("ses arcs:", G.arcs())
+>>> foret = parcours_profondeur_oriente(G)
 ==
 
 
