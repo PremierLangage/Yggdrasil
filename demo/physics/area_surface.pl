5902c8c7ab0120e939db6c6228a4c15c02e2657f
David DOYEN <David.Doyen@u-pem.fr>
2020-05-05 10:34:57 +0200

    ddoyen modified Yggdrasil/demo/physics/area_surface.pl

---
+++ demo/physics/area_surface.pl
@@ -0,0 +1,22 @@
+extends = /model/physicsinput.pl
+
+title = Aire d'une surface
+
+input.virtualKeyboards = elementary
+
+before ==
+a = (1+ 0.1*randint(20))*meter
+b = randint(20,60)*centimeter
+sol = convert_to(a*b, centimeter**2)
+==
+
+text ==
+On considère une surface rectangulaire de {{a|latex}} sur {{b|latex}}. Quelle est sa surface ?
+==
+
+evaluator ==
+unit_dict = {'m': meter,'cm': centimeter, 'g': gram, 'kg': kilogram, 's': second, 'A': ampere, 'K': kelvin, 'mol': mole, 'cd': candela, 'N': newton, 'J': joule, 'W': watt, 'Pa': pascal, 'Hz': hertz, 'C': coulomb, 'V': volt, 'Omega': ohm}
+
+score, _, feedback = eval_physical(input.value, sol, tol=1e-2, local_dict=unit_dict)
+==
+
