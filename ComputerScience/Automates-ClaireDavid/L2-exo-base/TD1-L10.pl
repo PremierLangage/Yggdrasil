76f262ebed4cbb506a56578f573fac3236dcced5
Claire DAVID <Claire.David@u-pem.fr>
2020-05-05 19:37:14 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/TD1-L10.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/TD1-L10.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/TD1-L10.pl
@@ -22,7 +22,7 @@ message = " le langage {abb, ba, bab}*"
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
 text ==
-On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$ .<br/>
 Donnez un automate reconnaissant {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
@@ -30,6 +30,7 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
+
 
 
 
