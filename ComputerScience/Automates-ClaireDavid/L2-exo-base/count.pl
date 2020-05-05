431cd67fd43b7038ebdfaad629e24c03e4a18d46
Claire DAVID <Claire.David@u-pem.fr>
2020-05-05 21:42:23 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/count.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/count.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/count.pl
@@ -34,6 +34,7 @@ alphabet = ', '.join(list(alphabet))
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
 text== #|html|
+On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$ .<br/>
 Donnez un automate reconnaissant le langage $! L = \\\{!$ mots ayant {{ message }} {{ occurences }} occurrences de la lettre {{ letter }} $!\\\}!$. <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
