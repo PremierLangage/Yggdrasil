c271fe58c64564989ddcdb8b71c14f2cfeed2310
Claire DAVID <Claire.David@u-pem.fr>
2020-05-05 19:36:37 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-base/factor.pl
@@ -52,7 +52,7 @@ alphabet = ', '.join(list(alphabet))
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
 text ==
-On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
+On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$ .<br/>
 Donnez un automate reconnaissant le langage **L1** = {{ '{' }}  mots qui {{ bla }}  **{{ mot }}** {{ '}' }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
@@ -61,6 +61,7 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) !
 {% endif %}
 
 ==
+
 
 
 
