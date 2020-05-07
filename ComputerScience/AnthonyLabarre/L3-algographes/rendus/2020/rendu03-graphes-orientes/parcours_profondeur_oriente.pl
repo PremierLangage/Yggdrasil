8a2471d504bf9fcdfaadbef0a5094378e11298a1
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-05-07 15:25:25 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
@@ -20,7 +20,8 @@ pltest0==
 >>> sommets, parents = parcours_profondeur_oriente(G)
 >>> foret = reconstruire_arbre_oriente(sommets, parents, GrapheOriente)
 >>> sorted(foret.sommets()) == sorted(G.sommets())  # tous les sommets sont-ils couverts?
->>> for arbre in cfc_graphes(foret): # tous les sous-graphes de la forêt sont-ils des arbres?
+True
+>>> for arbre in cc_graphes(foret): # tous les sous-graphes de la forêt sont-ils des arbres?
 ...     if not est_arbre_oriente(arbre):
 ...         print("le sous-graphe suivant de la forêt n'est pas un arbre orienté")
 ...         print("    sommets:", arbre.sommets())
@@ -38,7 +39,8 @@ pltest1==
 >>> sommets, parents = parcours_profondeur_oriente(G)
 >>> foret = reconstruire_arbre_oriente(sommets, parents, GrapheOriente)
 >>> sorted(foret.sommets()) == sorted(G.sommets())  # tous les sommets sont-ils couverts?
->>> for arbre in cfc_graphes(foret): # tous les sous-graphes de la forêt sont-ils des arbres?
+True
+>>> for arbre in cc_graphes(foret): # tous les sous-graphes de la forêt sont-ils des arbres?
 ...     if not est_arbre_oriente(arbre):
 ...         print("le sous-graphe suivant de la forêt n'est pas un arbre orienté")
 ...         print("    sommets:", arbre.sommets())
@@ -46,17 +48,18 @@ pltest1==
 >>> dates_attendues = profondeur_dates_fin(G); ordre_attendu = sorted(dates_attendues, key=dates_attendues.__getitem__)
 >>> if sommets != ordre_attendu:
 ...     print("ordre attendu:", ordre_attendu) 
-...     print("ordre obtenu: ", sommets) 
+...     print("ordre obtenu: ", sommets)
 ==
 
 testname2 = Vérifications de parcours_profondeur_oriente sur un graphe orienté aléatoire
 pltest2==
 >>> from corrlib_graphes import * #
->>> G = GrapheOriente(); G.ajouter_arcs(graphe_oriente_aleatoire(20).edges()) # initialisation d'un graphe orienté aléatoire
+>>> G = GrapheOriente(); G.ajouter_arcs(graphe_oriente_aleatoire(10).edges()) # initialisation d'un graphe orienté aléatoire
 >>> sommets, parents = parcours_profondeur_oriente(G)
 >>> foret = reconstruire_arbre_oriente(sommets, parents, GrapheOriente)
 >>> sorted(foret.sommets()) == sorted(G.sommets())  # tous les sommets sont-ils couverts?
->>> for arbre in cfc_graphes(foret): # tous les sous-graphes de la forêt sont-ils des arbres?
+True
+>>> for arbre in cc_graphes(foret): # tous les sous-graphes de la forêt sont-ils des arbres?
 ...     if not est_arbre_oriente(arbre):
 ...         print("le sous-graphe suivant de la forêt n'est pas un arbre orienté")
 ...         print("    sommets:", arbre.sommets())
@@ -82,4 +85,5 @@ editor.code ==
 # de validation quand vous avez terminé
 
 ==
+
 
