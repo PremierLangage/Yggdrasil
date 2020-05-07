095b2f4f2a1309d9c9f5b057a164ca9d07c1e301
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2020-05-07 16:37:47 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
@@ -41,19 +41,23 @@ testname1 = Vérifications de parcours_profondeur_oriente sur l'exemple du TD 5 
 pltest1==
 >>> from corrlib_graphes import * #
 >>> G = GrapheOriente(); G.ajouter_arcs(CFC_INSTANCE_TD) # initialisation de l'instance du TD
->>> sommets, parents = parcours_profondeur_oriente(G)
+>>> sommets, parents = mon_parcours_profondeur_oriente(G)
 >>> foret = reconstruire_arbre_oriente(sommets, parents, GrapheOriente)
 >>> sorted(foret.sommets()) == sorted(G.sommets())  # tous les sommets sont-ils couverts?
 True
+>>> ma_foret = foret_parcours_profondeur_oriente(G); arcs_de_ma_foret = set(ma_foret.arcs())
 >>> for arbre in cc_graphes(foret): # tous les sous-graphes de la forêt sont-ils des arbres?
 ...     if not est_arbre_oriente(arbre):
 ...         print("le sous-graphe suivant de la forêt n'est pas un arbre orienté")
 ...         print("    sommets:", arbre.sommets())
 ...         print("    arcs:   ", arbre.arcs())
+...     ses_arcs = set(arbre.arcs())
+...     if not ses_arcs <= arcs_de_ma_foret:
+...         print("les arcs suivants ne devraient pas appartenir à la forêt:", ses_arcs.difference(arcs_de_ma_foret))
 >>> dates_attendues = profondeur_dates_fin(G); ordre_attendu = sorted(dates_attendues, key=dates_attendues.__getitem__)
 >>> if sommets != ordre_attendu:
 ...     print("ordre attendu:", ordre_attendu) 
-...     print("ordre obtenu: ", sommets)
+...     print("ordre obtenu: ", sommets) 
 ==
 
 testname2 = Vérifications de parcours_profondeur_oriente sur un graphe orienté aléatoire
