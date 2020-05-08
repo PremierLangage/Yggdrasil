dd021d8f5f16baf2c9e37b5112d082ce06c8f9b6
Claire DAVID <Claire.David@u-pem.fr>
2020-05-08 22:44:52 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-regex.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-regex.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-regex.pl
@@ -19,15 +19,15 @@ message = 'a*b(b(aa+€))*a'
 ## TODO donner une seule description du lanage enoncé pour le message et l'automate
 #langage solution -- le nom solution est forcé par le template
 solution = Automaton.from_regex_nd('a*b(baa|b)*a')
-states = solution.state_machine.states
-initial = solution.state_machine.initial
-final = solution.state_machine.finals
-transitions = ['%s->%s->%s' % (src, symbol, dest) for src in solution.state_machine.map for symbol in solution.state_machine.map[src] for dest in solution.state_machine.map[src][symbol]]
+#states = solution.state_machine.states
+#initial = solution.state_machine.initial
+#final = solution.state_machine.finals
+#transitions = ['%s->%s->%s' % (src, symbol, dest) for src in solution.state_machine.map for symbol in solution.state_machine.map[src] for dest in solution.state_machine.map[src][symbol]]
 #viewer_enonce = solution.as_viewer()
 
 #viewer_enonce permet un affichage non modifié de l'automate enoncé qui décrit le langage
 viewer_enonce = AutomatonDrawer()
-viewer_enonce.automaton = solution.to_string_notation()
+viewer_enonce.automaton = solution
 
 #langage solution -- le nom solution est forcé par le template
 
@@ -38,10 +38,6 @@ viewer_enonce.automaton = solution.to_string_notation()
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
 text==
-states {{ states }}
-initial {{ initial }}
-final {{ final }}
-transitions {{ transitions }}
 
 {{ viewer_enonce|component }}
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
