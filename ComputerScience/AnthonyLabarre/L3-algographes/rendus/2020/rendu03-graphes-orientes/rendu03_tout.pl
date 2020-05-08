5622b6ad3640f41bc27eb66f4aeadec5fbdd77c7
Claire DAVID <Claire.David@u-pem.fr>
2020-05-08 16:16:02 +0200

    cdavid modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
@@ -43,29 +43,26 @@ pltest0==
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
 >>> sorted(map(list, G.arcs())) # Vérification de l'ensemble des arcs
 [[1, 2], [1, 3], [2, 1], [3, 1], [3, 5], [4, 1], [4, 2], [4, 11], [4, 12], [5, 6], [6, 7], [6, 9], [7, 9], [8, 5], [8, 10], [9, 8], [9, 10], [9, 13], [10, 11], [10, 13], [11, 12], [12, 10], [12, 14], [13, 12], [14, 13]]
->>> G.contient_arc(12,10)
+>>> for (u,v) in [(12,10),(10,42),(100,42)]: # Vérification de contient_arc
+...     G.contient_arc(u,v)#
+... 
 True
->>> G.contient_arc(10,12)
 False
->>> G.contient_arc(10,42)
 False
->>> G.contient_arc(100,42)
-False
->>> G.contient_sommet(9)
+>>> for s in [9,4,0,42]: # Vérification de contient_sommet
+...     G.contient_sommet(s)#
+... 
 True
->>> G.contient_sommet(4)
 True
->>> G.contient_sommet(0)
 False
->>> G.contient_sommet(42)
 False
 >>> G.retirer_sommet(6); sorted(G.sommets()) # Vérification de l'ensemble des sommets après suppression de 6
 [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14]
 >>> sorted(map(list, G.arcs())) # Vérification de l'ensemble des arcs
 [[1, 2], [1, 3], [2, 1], [3, 1], [3, 5], [4, 1], [4, 2], [4, 11], [4, 12], [7, 9], [8, 5], [8, 10], [9, 8], [9, 10], [9, 13], [10, 11], [10, 13], [11, 12], [12, 10], [12, 14], [13, 12], [14, 13]]
->>> G.nombre_sommets()
+>>> G.nombre_sommets() # Vérification du nombre de sommets
 13
->>> G.nombre_arcs()
+>>> G.nombre_arcs() # Vérification du nombre d'arcs
 22
 >>> G.retirer_sommets([12,10]); sorted((v, sorted(G.predecesseurs(v))) for v in G.sommets()) # suppression de 10 et 12 et vérification des prédécesseurs
 [(1, [2, 3, 4]), (2, [1, 4]), (3, [1]), (4, []), (5, [3, 8]), (7, []), (8, [9]), (9, [7]), (11, [4]), (13, [9, 14]), (14, [])]
@@ -75,27 +72,23 @@ False
 [(1, 3), (2, 2), (3, 1), (4, 0), (5, 2), (7, 0), (8, 1), (9, 1), (11, 1), (13, 2), (14, 0)]
 >>> sorted((v, G.degre_sortant(v)) for v in G.sommets()) # Vérification des degrés sortants
 [(1, 2), (2, 1), (3, 2), (4, 3), (5, 0), (7, 1), (8, 1), (9, 2), (11, 0), (13, 0), (14, 1)]
->>> sorted(G.voisins(1)) # Vérification des voisins de 1
+>>> for s in [1,9,7]: # Vérification de voisins
+...     sorted(G.voisins(s))#
+... 
 [2, 3, 4]
->>> sorted(G.voisins(9)) # Vérification des voisins de 9
 [7, 8, 13]
->>> sorted(G.voisins(7)) # Vérification des voisins de 7
 [9]
->>> G.retirer_sommet(9);sorted(G.voisins(7)) # Retirer 9 et vérification des voisins de 7
+>>> G.retirer_sommet(9);sorted(G.voisins(7)) # Suppression de 9 et vérification des voisins de 7
 []
 >>> G.retirer_arc(3,1);sorted(map(list, G.arcs())) #  Vérification de retirer_arc
 [[1, 2], [1, 3], [2, 1], [3, 5], [4, 1], [4, 2], [4, 11], [8, 5], [14, 13]]
 >>> G.retirer_arcs([(4,1),(8,5),(14,13)]);sorted(map(list, G.arcs())) #  Vérification de retirer_arcs
 [[1, 2], [1, 3], [2, 1], [3, 5], [4, 2], [4, 11]]
->>> G = GrapheOriente(); G.ajouter_arcs(CFC_INSTANCE_TD); G_induit = G.sous_graphe_induit([3,5,8,6,9,7]) #  Vérification de sous graphe induit connexe
->>> sorted(G_induit.sommets())
+>>> G = GrapheOriente(); G.ajouter_arcs(CFC_INSTANCE_TD); G_induit = G.sous_graphe_induit([3,5,8,6,9,7]);sorted(G_induit.sommets());sorted(G_induit.arcs())#  Vérification de sous graphe induit connexe
 [3, 5, 6, 7, 8, 9]
->>> sorted(G_induit.arcs())
 [(3, 5), (5, 6), (6, 7), (6, 9), (7, 9), (8, 5), (9, 8)]
->>> G_induit = G.sous_graphe_induit([4,3,13,12,11,6,7]); #  Vérification de sous graphe induit non connexe
->>> sorted(G_induit.sommets())
+>>> G_induit = G.sous_graphe_induit([4,3,13,12,11,6,7]);sorted(G_induit.sommets());sorted(G_induit.arcs()) #  Vérification de sous graphe induit non connexe
 [3, 4, 6, 7, 11, 12, 13]
->>> sorted(G_induit.arcs())
 [(4, 11), (4, 12), (6, 7), (11, 12), (13, 12)]
 ==
 
@@ -118,6 +111,7 @@ editor.code ==
 # de validation quand vous avez terminé
 
 ==
+
 
 
 
