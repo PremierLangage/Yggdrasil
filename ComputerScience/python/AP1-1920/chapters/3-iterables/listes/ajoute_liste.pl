d11a32503ecc9cc0942cdd0e9e2732d7ce6ff598
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-05-08 22:35:32 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/3-iterables/listes/ajoute_liste.pl

--- ComputerScience/python/AP1-1920/chapters/3-iterables/listes/ajoute_liste.pl
+++ ComputerScience/python/AP1-1920/chapters/3-iterables/listes/ajoute_liste.pl
@@ -7,8 +7,8 @@ builder= /builder/before.py
 
 title = Ajout d'éléments dans une liste
 text == 
-Ecrire une fonction **tab3** à deux arguments une liste, un entier **n**,  qui modifie une liste passée en argument et 
-qui y ajoute les n premiers éléments de la table de multiplication par 3, en commençant par le multiple de 1.
+Ecrire une fonction **tab3** à deux arguments : une liste, un entier **n** et  qui modifie la liste passée en argument en
+lui y ajoute les n premiers  multiples de 3 commençant par le multiple de 1.
 
 taboo: Ne pas utiliser la fonction <font style="color:red;">{{taboo}}</font> des listes.
 ==
@@ -43,6 +43,7 @@ pltest2==
 >>> lst
 [5, 3, 6, 9]
 ==
+
 
 
 
