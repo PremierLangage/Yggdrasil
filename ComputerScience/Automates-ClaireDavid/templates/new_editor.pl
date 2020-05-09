db844b4c0b2ecf0d943f6feee04c0e7749d72c7c
Claire DAVID <Claire.David@u-pem.fr>
2020-05-09 09:27:20 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/new_editor.pl

--- ComputerScience/Automates-ClaireDavid/templates/new_editor.pl
+++ ComputerScience/Automates-ClaireDavid/templates/new_editor.pl
@@ -423,7 +423,7 @@ elif answer_type == "automaton" and eval_type == "same_as":
     else:
         attempt += 1
         match, error1 = Automaton.same_as(string_solution, string_student_answer)
-#        equi, error2 = Automaton.compare(solution, student_answer)
+        equi, error2 = Automaton.compare(solution, student_answer)
         if  attempt >= maxattempt and (error or error2 or not match): # error or no match after timeout
             grade = (score, feedback_timeout)
         elif error:
