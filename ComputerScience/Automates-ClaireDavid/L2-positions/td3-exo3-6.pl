fd46f0f20826047ffd3b33ac76127c08bb0dfc82
Claire DAVID <Claire.David@u-pem.fr>
2020-05-09 14:48:31 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-positions/td3-exo3-6.pl

--- ComputerScience/Automates-ClaireDavid/L2-positions/td3-exo3-6.pl
+++ ComputerScience/Automates-ClaireDavid/L2-positions/td3-exo3-6.pl
@@ -4,7 +4,7 @@
 extends = ../templates/new_editor-L2.pl
 
 #* titre de l'exercice
-title = td3 exo 3-5
+title = td3 exo 3-6
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
