02d2e9f7134d3ab482d2d32ea0fbe73847661fc2
Claire DAVID <Claire.David@u-pem.fr>
2020-05-09 08:57:06 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/scripts/automaton.py

--- ComputerScience/Automates-ClaireDavid/scripts/automaton.py
+++ ComputerScience/Automates-ClaireDavid/scripts/automaton.py
@@ -16,27 +16,6 @@ from components import AutomatonDrawer, AutomatonEditor
 import fa, reex
 
 
-def same_as( auto1 , auto2 ):
-    ''' Return True if auto1 and auto2 coincide
-            states names
-            initial, final states and transitions
-        :param auto1 automaton in string_notation
-        :param auto2 automaton in string_notation
-        :raise If auto1 or auto2 cannot be parsed.
-    '''
-    fado1 = fado_from_string(auto1)
-    fado2 = fado_from_string(auto2)
-
-    states_match = sorted(fado1.States) == sorted(fado2.States)
-    initial_states_match = sorted(fado1.Initial) == sorted(fado2.Initial)
-    final_states_match = sorted(fado1.Final) == sorted(fado2.Final)
-    transition_match = sorted(fado1.delta) == sorted(fado2.delta)
-
-    return states_match and initial_states_match and final_states_match and transition_match
-
-
-
-
 def fado_from_string(string_notation):
     nfa = fa.NFA()
     lines = string_notation.splitlines();
@@ -813,6 +792,25 @@ class Automaton:
             return a.state_machine.equivalent(b.state_machine), None
         except Exception as e:
             return False, str(e)
+
+    @staticmethod
+    def same_as( auto1 , auto2 ):
+    ''' Return True if auto1 and auto2 coincide
+            states names
+            initial, final states and transitions
+        :param auto1 automaton in string_notation
+        :param auto2 automaton in string_notation
+        :raise If auto1 or auto2 cannot be parsed.
+    '''
+    fado1 = fado_from_string(auto1)
+    fado2 = fado_from_string(auto2)
+
+    states_match = sorted(fado1.States) == sorted(fado2.States)
+    initial_states_match = sorted(fado1.Initial) == sorted(fado2.Initial)
+    final_states_match = sorted(fado1.Final) == sorted(fado2.Final)
+    transition_match = sorted(fado1.delta) == sorted(fado2.delta)
+
+    return states_match and initial_states_match and final_states_match and transition_match
     
     @staticmethod
     def accepts(obj, word: str):
