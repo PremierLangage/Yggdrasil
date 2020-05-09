7cf3312adec709fac96c27e9dd5819c7eba0e8fc
Claire DAVID <Claire.David@u-pem.fr>
2020-05-09 08:46:13 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-fado-regex.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-fado-regex.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-fado-regex.pl
@@ -6,7 +6,6 @@ title = exo mini from regex
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
 from automaton import Automaton
-from automaton import *
 
 # pour changer le type d'évaluation quand le mode sera implémenté
 #eval_type = "same_as"
