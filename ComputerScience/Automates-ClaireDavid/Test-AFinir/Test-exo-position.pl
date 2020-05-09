433138b91d689ef32055e2588a62adaacb685197
Claire DAVID <Claire.David@u-pem.fr>
2020-05-09 10:48:21 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
@@ -13,10 +13,11 @@ eval_type = "same_as"
 alphabet = "a,b"
 
 #message de l'énoncé décrivant le langage
-message = 'a*b(b(aa+))*a'
+message = 'a*b(b(aa+ε))*a'
 
-#langage solution -- le nom solution est forcé par le template
-solution = Automaton.from_regex_nd(message, simple=True)
+#automate solution -- le nom solution est forcé par le template
+solution =
+
 
 #viewer_enonce permet un affichage non modifié de l'automate enoncé qui décrit le langage
 viewer_enonce = AutomatonDrawer()
@@ -34,7 +35,7 @@ text==
 {{ solution_view|component }}
 {{ viewer_enonce|component }}
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
-Donnez l'automate minimal déterministe complet reconnaissant le langage {{ message }} <br>
+Donnez l'automate des positions correspondant à l'expression {{ message }} <br>
 {% if attempt < maxattempt and score != 100 %}
 <p class="warning-state">
 Il vous reste {{ maxattempt - attempt }} tentative(s) !
