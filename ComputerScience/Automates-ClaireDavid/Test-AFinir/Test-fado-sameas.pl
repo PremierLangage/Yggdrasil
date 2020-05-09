3e954c24ca22ac2a403ef8e2e7331f3a881d68a3
Claire DAVID <Claire.David@u-pem.fr>
2020-05-09 08:45:17 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-fado-sameas.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-fado-sameas.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-fado-sameas.pl
@@ -61,7 +61,8 @@ solution = Automaton.parse(enonce)
 # l'automate minimisé du langage solution
 text==
 {{ viewer_enonce|component }}
-{{ solution_view|component }}
+résultat : {{ same_as_test }}
+<br>
 On considère l'alphabet **Σ** = **{{ '{' }} {{ alphabet }} {{ '}' }}** .<br/>
 Donnez l'automate minimal déterministe complet correspondant à l'automate ci-dessus. <br>
 {% if attempt < maxattempt and score != 100 %}
