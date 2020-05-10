b76cadaf8c69a8e68dc410eed427007f43be991d
Claire DAVID <Claire.David@u-pem.fr>
2020-05-10 23:47:03 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-determinisation/td2-exo1-A.pl

--- ComputerScience/Automates-ClaireDavid/L2-determinisation/td2-exo1-A.pl
+++ ComputerScience/Automates-ClaireDavid/L2-determinisation/td2-exo1-A.pl
@@ -1,7 +1,7 @@
 extends = ../templates/new_editor-L2.pl
 
 #* titre de l'exercice
-title = Exo déterminisation
+title = td2 exo1-A
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
