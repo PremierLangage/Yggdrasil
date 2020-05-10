b7e1fa72d4b220becda8f2e400a87005e40514bd
Claire DAVID <Claire.David@u-pem.fr>
2020-05-10 17:43:28 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/scripts/automaton.py

--- ComputerScience/Automates-ClaireDavid/scripts/automaton.py
+++ ComputerScience/Automates-ClaireDavid/scripts/automaton.py
@@ -875,7 +875,7 @@ class Automaton:
             fado2 = fado_from_string(auto2)
 
             states_match = sorted(fado1.States) == sorted(fado2.States)
-            initial_states_match = sorted(fado1.Initial) == sorted(fado2.Initial)
+            initial_states_match = sorted([fado1.States[i] for i in fado1.Initial]) == sorted(fado2.Initial)
             final_states_match = sorted(fado1.Final) == sorted(fado2.Final)
             transition_match = sorted(fado1.delta) == sorted(fado2.delta)
 
