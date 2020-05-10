63bc0c880ab00b92e2a64d32986371351bb00e74
Claire DAVID <Claire.David@u-pem.fr>
2020-05-10 17:17:41 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
@@ -22,6 +22,8 @@ solution = Automaton.from_regex_nd(enonce, simple=True)
 # Permet de faire la comparaison same_as imposé le template -- A ajuster cf TODO
 string_solution = solution
 
+editor.debug = False
+
 #Décommenter les deux dernières lignes si besoin d'afficher l'automate des positions
 #viewer_enonce permet un affichage non modifié de l'automate string_solution
 viewer_enonce = AutomatonDrawer()
