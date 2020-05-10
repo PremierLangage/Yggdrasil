8b9e33affd330130b40e6e2841258bc2e58cf93b
Claire DAVID <Claire.David@u-pem.fr>
2020-05-10 12:05:28 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-positions/td3-exo3-4.pl

--- ComputerScience/Automates-ClaireDavid/L2-positions/td3-exo3-4.pl
+++ ComputerScience/Automates-ClaireDavid/L2-positions/td3-exo3-4.pl
@@ -15,51 +15,32 @@ eval_type = "same_as"
 alphabet = "a,b"
 
 #message de l'énoncé décrivant le langage
-message = '(a+b)*a(a+b)'
+enonce = '(a+b)*a(a+b)'
 
 #automate solution -- le nom solution est forcé par le template
-solution = '''
-    #states
-    S0
-    S1
-    S2
-    S3
-    S4
-    S5
-    #initials
-    S0
-    #accepting
-    S4;S5
-    #alphabet
-    a
-    b
-    #transitions
-    S0:a>S1
-    S0:b>S2
-    S0:a>S3
-    S1:a>S1
-    S1:b>S2
-    S1:a>S3
-    S2:a>S1
-    S2:b>S2
-    S2:a>S3
-    S3:a>S4
-    S3:b>S5
-'''
-
+solution = Automaton.from_regex_nd(enonce, simple=True)
 
 # Permet de faire la comparaison same_as imposé le template -- A ajuster cf TODO
 string_solution = solution
 
+#Décommenter les deux dernières lignes si besoin d'afficher l'automate des positions
+#viewer_enonce permet un affichage non modifié de l'automate string_solution
+#viewer_enonce = AutomatonDrawer()
+#viewer_enonce.automaton = string_solution
 ==
 
 #* Texte de l'énoncé
 # ---
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
+# ----
+# s'il est défini vous pouvez utiliser {{ viewer_enonce|component }}
+# dans ce qui suit pour affciher l'automate des positions demandé.
+# 
 text==
+
 On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$.<br/>
-Donnez l'automate des positions correspondant à l'expression {{ message }} <br>
+Donnez l'automate des positions correspondant à l'expression ```{{enonce }}``` <br>
 <br>
 **Contraintes d'écriture pour l'évaluation:**<br>
 Pour le nommage des états, vous respecterez la convention suivante:
