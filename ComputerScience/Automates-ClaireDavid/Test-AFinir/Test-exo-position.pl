78fec66bc30e5bb6ddd1c4e8a407a6c04cd71afd
Claire DAVID <Claire.David@u-pem.fr>
2020-05-10 11:44:57 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
@@ -67,7 +67,7 @@ string_solution = solution
 text==
 {{ solution_view|component }}
 {{ viewer_enonce|component }}
-{{ enonce }}
+```{{ enonce }}```
 {{ solution }}
 On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$.<br/>
 Donnez l'automate des positions correspondant à l'expression ```{{ message }}``` <br>
