dc1205c50702e5ba29a5c73093df2119cd2557d9
Claire DAVID <Claire.David@u-pem.fr>
2020-05-10 17:15:22 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-position.pl
@@ -1,7 +1,8 @@
+
 extends = ../templates/new_editor-L2.pl
 
 #* titre de l'exercice
-title = Automate des positions
+title = Partiel 2020 Exo3
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
@@ -13,69 +14,40 @@ eval_type = "same_as"
 alphabet = "a,b"
 
 #message de l'énoncé décrivant le langage
-message = 'a*b(b(aa+ε))*a'
+enonce = '(b.(aa+a+ε))*ab*a'
 
 #automate solution -- le nom solution est forcé par le template
-
-solution = '''
-    #states
-    S0
-    S1
-    S2
-    S3
-    S4
-    S5
-    S6
-    #initials
-    S0
-    #accepting
-    S6
-    #alphabet
-    a
-    b
-    #transitions
-    S0:a>S1
-    S0:b>S2
-    S1:a>S1
-    S1:b>S2
-    S2:a>S6
-    S2:b>S3
-    S3:a>S4
-    S3:b>S3
-    S3:a>S6
-    S4:a>S5
-    S5:a>S6
-    S5:b>S3
-'''
-
-
-enonce = Automaton.from_regex_nd(message, simple=True)
-
-#viewer_enonce permet un affichage non modifié de l'automate enoncé qui décrit le langage
-viewer_enonce = AutomatonDrawer()
-viewer_enonce.automaton = enonce
+solution = Automaton.from_regex_nd(enonce, simple=True)
 
 # Permet de faire la comparaison same_as imposé le template -- A ajuster cf TODO
 string_solution = solution
 
+#Décommenter les deux dernières lignes si besoin d'afficher l'automate des positions
+#viewer_enonce permet un affichage non modifié de l'automate string_solution
+viewer_enonce = AutomatonDrawer()
+viewer_enonce.automaton = string_solution
 ==
 
 #* Texte de l'énoncé
 # ---
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
+# ----
+# s'il est défini vous pouvez utiliser {{ viewer_enonce|component }}
+# dans ce qui suit pour affciher l'automate des positions demandé.
+# 
 text==
-{{ solution_view|component }}
 {{ viewer_enonce|component }}
+string_solution:
+{{ string_solution }}
 On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$.<br/>
-Donnez l'automate des positions correspondant à l'expression ```{{ message }}``` <br>
+Donnez l'automate des positions correspondant à l'expression ```{{enonce }}``` <br>
 <br>
 **Contraintes d'écriture pour l'évaluation:**<br>
 Compte tenu des bugs de l'éditeur sur le renommage des états, vous adopterez la convention suivante:
 <ul>
-<li> L'état S0 sera l'état initial de votre automate. <br>*(Si vous avez besoin de plusieurs états initiaux, relisez votre cours)*
+<li> L'état S0 sera l'état initial de votre automate. <br>
 <li> Les états Sj pour j>0 représenteront les positions dans l'ordre de numérotation de l'expression.<br>
-*Exemple pour l'expression ```a*b+b``` qui se numérote ```a1*b2+b3```, l'état S1 représenterai la position a1, l'état S2 la position b2, et l'état S3 la position b3.*
 </ul>
 
 {% if attempt < maxattempt and score != 100 %}
@@ -84,46 +56,4 @@ Il vous reste {{ maxattempt - attempt }} tentative(s) !
 </p>
 {% endif %}
 ==
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
