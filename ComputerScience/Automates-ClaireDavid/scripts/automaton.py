3ee3e8515e5e558a56163652fc1cc3190a95407b
Claire DAVID <Claire.David@u-pem.fr>
2020-05-11 23:59:50 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/scripts/automaton.py

--- ComputerScience/Automates-ClaireDavid/scripts/automaton.py
+++ ComputerScience/Automates-ClaireDavid/scripts/automaton.py
@@ -560,7 +560,7 @@ class Automaton:
             for x in xx:
                 a.renameState(a.stateIndex(x),'pouet')
                 #a.deleteState(a.stateIndex(x))
-            a.deleteState(a.Index('pouet'))
+            a.deleteState(a.stateIndex('pouet'))
             return a
 
         if mode == 'intersect':
