368be522bb4d3d3f575d8f497099475172610809
Claire DAVID <Claire.David@u-pem.fr>
2020-05-11 22:53:45 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/scripts/automaton.py

--- ComputerScience/Automates-ClaireDavid/scripts/automaton.py
+++ ComputerScience/Automates-ClaireDavid/scripts/automaton.py
@@ -565,6 +565,7 @@ class Automaton:
                 if nfa1.stateIndex(x1) in nfa1.Final or nfa2.stateIndex(x2) in nfa2.Final:
                     a.addFinal(a.stateIndex((x1,x2))
         else:
+            a = nfa1.product(nfa2)
 
 
 
