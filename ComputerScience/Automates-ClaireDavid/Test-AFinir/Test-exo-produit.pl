34f5ca3e81bdfce2b23b2496c54d2b615ebb96e6
Claire DAVID <Claire.David@u-pem.fr>
2020-05-11 23:06:41 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-produit.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-produit.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-produit.pl
@@ -53,6 +53,11 @@ automate2 = '''
 
 # Permet de faire la comparaison same_as imposé le template -- A ajuster cf TODO
 # choisir le mode pour definir les états finaux
+#           default 'intersect'
+#            'union' build L1UL2
+#            'minusright' build L1\L2
+#            'minusleft' build L2\L1
+#            'symdiff' build (L2\L1)U(L1\L2) 
 string_solution = Automaton.string_soluce_product(automate1,automate2,mode='minusleft')
 
 #TODO Etats finaux
