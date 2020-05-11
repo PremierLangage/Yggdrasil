826adc8da2a639570fbe1d988d644baa4f5c6139
Claire DAVID <Claire.David@u-pem.fr>
2020-05-11 23:46:32 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/scripts/automaton.py

--- ComputerScience/Automates-ClaireDavid/scripts/automaton.py
+++ ComputerScience/Automates-ClaireDavid/scripts/automaton.py
@@ -556,7 +556,7 @@ class Automaton:
             xx = []
             for (x1,x2) in a.States:
                     if x1 == '@empty_set' or x2 == '@empty_set':
-                        xx.append(a.stateIndex((x1,x2)))
+                        xx.append((x1,x2))
             for x in xx:
                 a.renameState(x,'pouet')
                 #a.deleteState(x)
