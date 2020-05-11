ea1ee85f6a6f02b0a5ba4f0be0fa7f51d00bc65c
Claire DAVID <Claire.David@u-pem.fr>
2020-05-12 00:12:49 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/scripts/automaton.py

--- ComputerScience/Automates-ClaireDavid/scripts/automaton.py
+++ ComputerScience/Automates-ClaireDavid/scripts/automaton.py
@@ -558,8 +558,8 @@ class Automaton:
                     if x1 == '@empty_set' or x2 == '@empty_set':
                         xx.append((x1,x2))
             for x in xx:
-                #a.renameState(a.stateIndex(x),'pouet')
-                a.deleteState(a.stateIndex(x))
+                a.renameState(a.stateIndex(x),'pouet')
+                #a.deleteState(a.stateIndex(x))
             return a
 
         if mode == 'intersect':
@@ -577,7 +577,6 @@ class Automaton:
                         a.addFinal(a.stateIndex((x1,x2)))
             else:
                 raise Exception('les deux automates doivent être complets')
-                pass
         elif mode == 'minusright' :
             if nfa2.completeP() and nfa2.determinicP():
                 a = nfa1.product(nfa2)
