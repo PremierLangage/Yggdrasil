087a908ceb4d390413ea18b048bcb0a655abe4f6
Claire DAVID <Claire.David@u-pem.fr>
2020-05-11 20:36:53 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-produit.pl

--- ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-produit.pl
+++ ComputerScience/Automates-ClaireDavid/Test-AFinir/Test-exo-produit.pl
@@ -98,6 +98,11 @@ viewer_enonce1.automaton = automate1
 #viewer_enonce permet un affichage non modifié de l'automate2 enoncé qui décrit le langage
 viewer_enonce2 = AutomatonDrawer()
 viewer_enonce2.automaton = automate2
+
+#viewer_enonce permet un affichage non modifié de l'automate2 enoncé qui décrit le langage
+viewer_soluce = AutomatonDrawer()
+viewer_soluce.automaton = string_solution
+
 ==
 
 #* Texte de l'énoncé
