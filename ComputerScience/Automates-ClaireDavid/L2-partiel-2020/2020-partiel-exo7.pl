50b002e55907b4b50eb3fc628933e26afa620202
Claire DAVID <Claire.David@u-pem.fr>
2020-05-11 15:49:19 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-partiel-2020/2020-partiel-exo7.pl

--- ComputerScience/Automates-ClaireDavid/L2-partiel-2020/2020-partiel-exo7.pl
+++ ComputerScience/Automates-ClaireDavid/L2-partiel-2020/2020-partiel-exo7.pl
@@ -90,10 +90,6 @@ viewer_enonce1.automaton = automate1
 #viewer_enonce permet un affichage non modifié de l'automate2 enoncé qui décrit le langage
 viewer_enonce2 = AutomatonDrawer()
 viewer_enonce2.automaton = automate2
-
-#viewer_enonce permet un affichage non modifié de l'automate2 enoncé qui décrit le langage
-viewer_soluce = AutomatonDrawer()
-viewer_soluce.automaton = string_solution
 ==
 
 #* Texte de l'énoncé
@@ -107,7 +103,6 @@ Automate $!\mathcal{A}_1!$ qui reconnait $!L_1!$:
 {{ viewer_enonce1|component }}
 Automate $!\mathcal{A}_2!$ qui reconnait $!L_2!$:
 {{ viewer_enonce2|component }}
-{{ viewer_soluce |component}}
 **Contraintes d'écriture pour l'évaluation:**<br>
 Afin de vérifier la correction de vos calculs et pas seulement la forme de l'automate, vous respecterez la convention suivante:
 <br>
