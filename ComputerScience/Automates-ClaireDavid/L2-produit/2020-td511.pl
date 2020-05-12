3f3ea21ff28a89c1cc1afc203a6e7b5abec4e64f
Claire DAVID <Claire.David@u-pem.fr>
2020-05-12 10:25:27 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-produit/2020-td511.pl

--- ComputerScience/Automates-ClaireDavid/L2-produit/2020-td511.pl
+++ ComputerScience/Automates-ClaireDavid/L2-produit/2020-td511.pl
@@ -59,7 +59,7 @@ automate2 = '''
 #            'rightdiff' build L1\L2
 #            'leftdiff' build L2\L1
 #            'symdiff' build (L2\L1)U(L1\L2) 
-#string_solution = Automaton.string_soluce_product(automate1,automate2,mode='symdiff')
+string_solution = Automaton.string_soluce_product(automate1,automate2,mode='symdiff')
 
 
 string_solution = '''
