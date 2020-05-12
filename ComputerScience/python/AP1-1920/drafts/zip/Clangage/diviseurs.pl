9f7ae906653773dda9706d282e7df8f07dd0fea4
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-05-12 15:57:05 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Clangage/diviseurs.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/Clangage/diviseurs.pl
@@ -0,0 +1,29 @@
+title= Diviseurs d'un entier
+tag=function
+
+
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+
+text==  
+
+Ecrire un programme qui lit un  entier  strictement positif et affiche,
+ **un par ligne**, chacun de ses diviseurs en ordre croissant.
+Le programme affiche ensuite le nombre de diviseurs.
+
+Si l'entier est négatif ou nul, on ne fait rien !
+
+**Exemple :**
+
+Un nombre : 6
+
+1
+
+2
+
+3
+
+6
+
+6 a 4 diviseurs
+==
