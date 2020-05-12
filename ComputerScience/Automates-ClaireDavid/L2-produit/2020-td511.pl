0c64c41d41569bfaab9729e09ab14144a2b74cea
Claire DAVID <Claire.David@u-pem.fr>
2020-05-12 12:37:26 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-produit/2020-td511.pl

--- ComputerScience/Automates-ClaireDavid/L2-produit/2020-td511.pl
+++ ComputerScience/Automates-ClaireDavid/L2-produit/2020-td511.pl
@@ -72,9 +72,6 @@ viewer_enonce1.automaton = automate1
 #viewer_enonce permet un affichage non modifié de l'automate2 enoncé qui décrit le langage
 viewer_enonce2 = AutomatonDrawer()
 viewer_enonce2.automaton = automate2
-#viewer_enonce permet un affichage non modifié de l'automate2 enoncé qui décrit le langage
-viewer_sol = AutomatonDrawer()
-viewer_sol.automaton = string_solution
 ==
 
 #* Texte de l'énoncé
@@ -82,7 +79,6 @@ viewer_sol.automaton = string_solution
 # ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
 # l'automate minimisé du langage solution
 text==
-{{ viewer_sol|component }}
 On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$. Faites le produit des deux automates
  donnés pour construire un automate qui reconnaît $!L_1\backslash L_2\cup L_2\backslash L_1!$.<br>
 
