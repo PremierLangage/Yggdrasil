9b6e82aae116d9d10bb41f4e978118a2d55f2c57
Claire DAVID <Claire.David@u-pem.fr>
2020-05-14 00:09:25 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-produit.pl

--- ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-produit.pl
+++ ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-produit.pl
@@ -3,9 +3,6 @@ extends = /model/AMC_TXT.pl
 
 title = Quizz Produit
 
-text == Ce QCM contient des questions Vrai/Faux et des questions à choix multiples pour lequelles vous pouvez cocher plusieurs cases
-==
-
 questions=@ q-produit.txt
 
 
