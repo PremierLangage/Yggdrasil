6fc630b1001cd8e391d7f7968ab9eea8a29546db
Claire DAVID <Claire.David@u-pem.fr>
2020-05-14 00:12:58 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-produit.pl

--- ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-produit.pl
+++ ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-produit.pl
@@ -7,7 +7,7 @@ intro ==
 Ce quiz contient {{nbstep}} questions.
 
 Pour les questions de type Vrai/Faux, vous devez choisir une réponse.
-Pour les autres, la réponse complète peut être composée de plusieurs éléments.
+Pour les autres, la réponse complète peut être composée de plusieurs items.
 ==
 
 questions=@ q-produit.txt
