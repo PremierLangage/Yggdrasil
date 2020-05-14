1e165c9bb49204d23596c999aa33eea6cbabefa1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-15 00:25:20 +0200

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_factorial.pl

--- ComputerScience/Games/Doctest_me_bro/doctest_factorial.pl
+++ ComputerScience/Games/Doctest_me_bro/doctest_factorial.pl
@@ -14,6 +14,8 @@
 #            https://creativecommons.org/licenses/by-sa/3.0/fr/
 #*****************************************************************************
 
+title=factorielle d'un entier
+
 extends=doctest_me_bro_template.pl
 
 text==
@@ -24,4 +26,35 @@ naturel **n** et retourne la fatorielle **n!** de ce dernier.
 
 Proposez des doctests python pour detecter la validité et les erreurs 
 potentielles des codes rendus des élèves.
+==
+
+good_code==#|python|
+def factorielle(n):
+    if n <= 1:
+        return 1
+    ans = 1
+    for i in range(2, n+1):
+        ans *= i
+    return ans
+==
+
+bad_code_1==#|python|
+def factorielle(n):
+    if n <= 1:
+        return n
+    ans = 1
+    for i in range(2, n+1):
+        ans *= i
+    return ans
+==
+
+bad_code_2==#|python|
+def factorielle(n):
+    L = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 
+    479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 
+    355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000, 
+    51090942171709440000, 1124000727777607680000, 25852016738884976640000, 
+    620448401733239439360000, 15511210043330985984000000, 403291461126605635584000000, 
+    10888869450418352160768000000, 304888344611713860501504000000, 8841761993739701954543616000000]
+    return L[n]
 ==
