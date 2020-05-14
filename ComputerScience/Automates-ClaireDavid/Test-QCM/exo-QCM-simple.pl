fa187971ffb33987de25d2a4169a2d484fae28ca
Claire DAVID <Claire.David@u-pem.fr>
2020-05-14 09:30:49 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/Test-QCM/exo-QCM-simple.pl

---
+++ ComputerScience/Automates-ClaireDavid/Test-QCM/exo-QCM-simple.pl
@@ -0,0 +1,19 @@
+
+extends = /model/AMC_TXT.pl
+
+title = QCM Produit d'automates
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+
+Pour les questions de type Vrai/Faux, vous devez choisir une réponse.
+
+Pour les autres, la réponse complète peut être composée de plusieurs items.
+==
+
+feedback=''
+
+questions=@ q-produit.txt
+
+
+
