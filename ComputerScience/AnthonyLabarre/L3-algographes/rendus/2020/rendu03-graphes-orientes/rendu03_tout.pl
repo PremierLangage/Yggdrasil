3ef3d8070940042cc6dd5554b1c714e8017aee58
Claire DAVID <Claire.David@u-pem.fr>
2020-05-15 14:28:31 +0200

    cdavid modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
@@ -16,7 +16,7 @@ text==
     <li>sommets est une liste contenant les sommets du graphe dans l'ordre de fin de visite;
     <li>parents est un dictionnaire ayant pour clés les sommets et pour valeur le parent de chaque sommet dans l'exploration en profondeur effectuée.
 </ul>
-Attention, votre parcours doit respacter l'ordre lexicographique des sommets (@Labarre) et vous devez bien sûr explorer tout le graphe qu'il soit ou non connexe (faiblement ou fortement). Tous les sommets du graphe doivent donc être listés.
+Attention, votre parcours doit respecter l'ordre lexicographique des sommets et vous devez bien sûr explorer tout le graphe qu'il soit ou non connexe (faiblement ou fortement). Tous les sommets du graphe doivent donc être listés.
 <li>une fonction detection_cycle(graphe), qui renvoie explicitement un cycle du graphe s'il en contient un et None sinon. Le type du cycle renvoyé doit être le même que celui du graphe en entrée;
 <li>une fonction composantes_fortement_connexes(graphe), renvoyant les composantes fortement connexes sous la forme d'un itérable d'itérables de sommets.
 </ul>
@@ -111,6 +111,7 @@ editor.code ==
 # de validation quand vous avez terminé
 
 ==
+
 
 
 
