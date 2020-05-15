bb745d623217617936886eafd46584501b2fd76e
Claire DAVID <Claire.David@u-pem.fr>
2020-05-15 12:41:07 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/templates/new_editor-L2.pl

---
+++ ComputerScience/Automates-ClaireDavid/templates/new_editor-L2.pl
@@ -0,0 +1,21 @@
+extends = new_editor.pl
+
+#* Maximum number of attempts before showing the right answer.
+maxattempt = 10
+
+author = Claire David
+
+#* When True show the answer after timeout.
+show_solution = False
+
+#* When True display the reroll button and allow the student to reroll the exercice after tumeout
+# TODO disable reroll is not implemented yet
+allow_reroll =  True
+
+
+#* feedback after timeout.
+feedback_timeout =  <p class="warning-state"> Vous n'avez pas réussi l'exercice. Relisez votre cours et réessayez plus tard.</p>
+
+
+
+
