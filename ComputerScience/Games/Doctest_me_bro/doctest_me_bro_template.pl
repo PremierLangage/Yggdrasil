0b7c5f33a80e93b7d68ce18a2c9fdbafd5ded66c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-16 09:38:57 +0200

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_me_bro_template.pl

--- ComputerScience/Games/Doctest_me_bro/doctest_me_bro_template.pl
+++ ComputerScience/Games/Doctest_me_bro/doctest_me_bro_template.pl
@@ -55,10 +55,9 @@ def nb_failled_test(code_key):
     f.write(src_code.replace('TEST_INCLUSION', editor.code))
     SP = subprocess.run(['python3', '-m', 'doctest', '-v', 'doc_code.py'], capture_output=True)
     out = SP.stdout.decode()
-    return out
+    return out.replace('\n', '<br />')
 
-feedback = (editor.code + str([nb_failled_test(k) for k in good_code_keys]) + str([nb_failled_test(k) for k in bad_code_keys])).replace('\n', '<br />')
-
+feedback = (editor.code + str([nb_failled_test(k) for k in good_code_keys]) + str([nb_failled_test(k) for k in bad_code_keys])).replace("\n", "<br />")
 
 grade = (100, feedback)
 ==
