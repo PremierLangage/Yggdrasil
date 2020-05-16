585b217907b7841172555025e8c119c67a3a25e1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-16 09:34:13 +0200

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_me_bro_template.pl

--- ComputerScience/Games/Doctest_me_bro/doctest_me_bro_template.pl
+++ ComputerScience/Games/Doctest_me_bro/doctest_me_bro_template.pl
@@ -40,11 +40,24 @@ form==
 ==
 
 evaluator==#|python|
+import subprocess
 
+# extraction od bads and goods codes
 good_code_keys = [k for k in globals().keys() if k.startswith('good')]
 bad_code_keys = [k for k in globals().keys() if k.startswith('bad')]
 
-feedback = editor.code + str(good_code_keys) + str(bad_code_keys)
+# 
+def nb_failled_test(code_key):
+    """
+    """
+    f = open("doc_code.py", "w")
+    src_code = globals()[code_key]
+    f.write(src_code.replace('TEST_INCLUSION', editor.code))
+    SP = subprocess.run(["python3 -m doctest -v doc_code.py"], capture_output=True)
+    out = SP.stdout().decode()
+    return out
+
+feedback = editor.code + str([nb_failled_test(k) for k in good_code_keys]) + str([nb_failled_test(k) for k in bad_code_keys])
 
 grade = (100, feedback)
 ==
