e40cdd834ef9c9300742d0f44d4a6b24f9c67560
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-17 09:39:59 +0200

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_sort.pl

--- ComputerScience/Games/Doctest_me_bro/doctest_sort.pl
+++ ComputerScience/Games/Doctest_me_bro/doctest_sort.pl
@@ -33,6 +33,9 @@ potentielles des codes rendus par les élèves.
 
 good_code_1==#|python|
 def triListe(L):
+    """
+    TEST_INCLUSION
+    """
     for i in range(len(L)):
         for j in range(i+1, len(L)-1):
             if L[j] < L[j+1]:
@@ -42,6 +45,9 @@ def triListe(L):
 
 bad_code_1==#|python|
 def triListe(L):
+    """
+    TEST_INCLUSION
+    """
     for i in range(len(L)):
         for j in range(i+1, len(L)):
             if L[j] < L[j+1]:
