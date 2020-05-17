1bc424bf9f28eae923a250faec05a644be6984fe
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-17 09:41:48 +0200

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_sort.pl

--- ComputerScience/Games/Doctest_me_bro/doctest_sort.pl
+++ ComputerScience/Games/Doctest_me_bro/doctest_sort.pl
@@ -37,7 +37,7 @@ def triListe(L):
     TEST_INCLUSION
     """
     for i in range(len(L)):
-        for j in range(i+1, len(L)-1):
+        for j in range(i, len(L)-1):
             if L[j] < L[j+1]:
                 L[j], L[j+1] = L[j+1], L[j]
     return L
