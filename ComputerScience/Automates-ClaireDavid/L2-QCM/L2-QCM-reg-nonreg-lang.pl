65e7a88c8dca2247359df5a4fe38ff5cffd5a088
Claire DAVID <Claire.David@u-pem.fr>
2020-05-18 19:01:09 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-reg-nonreg-lang.pl

--- ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-reg-nonreg-lang.pl
+++ ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-reg-nonreg-lang.pl
@@ -3,7 +3,7 @@ extends = ../templates/AMC_TXT-rd.pl
 author = Claire David
 title = QCM - régulier / non régulier ?
 
-randomize_questions = off
+randomize_questions = on
 
 #pour changer le nombre de questions
 #nbstep = 10
