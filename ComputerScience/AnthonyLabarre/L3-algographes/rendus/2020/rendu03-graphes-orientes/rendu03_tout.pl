b925d42ed804849b682e8d79e9da2ff24ce2c5a2
Claire DAVID <Claire.David@u-pem.fr>
2020-05-20 17:59:14 +0200

    cdavid modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
@@ -14,7 +14,7 @@ text==
 <li>une fonction parcours_profondeur_oriente(graphe), qui renvoie un couple (sommets, parents) dans cet ordre, où:
 <ul>
     <li>sommets est une liste contenant les sommets du graphe dans l'ordre de fin de visite;
-    <li>parents est un dictionnaire ayant pour clés les sommets et pour valeur le parent de chaque sommet dans l'exploration en profondeur effectuée.
+    <li>parents est un dictionnaire ayant pour clés des sommets et pour valeur le parent du sommet correspondant dans l'exploration en profondeur effectuée.
 </ul>
 Attention, votre parcours doit respecter l'ordre lexicographique des sommets et vous devez bien sûr explorer tout le graphe qu'il soit ou non connexe (faiblement ou fortement). Tous les sommets du graphe doivent donc être listés.
 <li>une fonction detection_cycle(graphe), qui renvoie explicitement un cycle du graphe s'il en contient un et None sinon. Le type du cycle renvoyé doit être le même que celui du graphe en entrée;
@@ -111,6 +111,7 @@ editor.code ==
 # de validation quand vous avez terminé
 
 ==
+
 
 
 
