2108810649a80f2431bae00801ef7adb7e5b0f7a
Claire DAVID <Claire.David@u-pem.fr>
2020-05-20 18:07:51 +0200

    cdavid modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/parcours_profondeur_oriente.pl
@@ -85,6 +85,54 @@ True
 ...     print("ordre obtenu: ", sommets) 
 ==
 
+testname0 = Vérifications de detection_cycle sur l'exemple du cours
+pltest0==
+>>> from corrlib_graphes import CYCLE_ORIENTE_INSTANCE_COURS #
+>>> G = GrapheOriente(); G.ajouter_arcs(CYCLE_ORIENTE_INSTANCE_COURS) # initialisation de l'instance du cours
+>>> cycle = detection_cycle(G) # Appel de la fonction detection_cycle
+>>> sorted(cycle.arcs()) # Vérification du résultat
+[(0, 3), (3, 4), (4, 0)]
+==
+
+testname1 = Vérifications de detection_cycle sur l'exemple du TD
+pltest1==
+>>> from corrlib_graphes import * #
+>>> G = GrapheOriente(); G.ajouter_arcs(CYCLE_ORIENTE_INSTANCE_TD) # initialisation de l'instance du TD
+>>> arcs_cycle_trouve = set(detection_cycle(G).arcs())# Appel de la fonction detection_cycle
+>>> cycles_possibles = cycles_simples(G)#
+>>> if arcs_cycle_trouve not in cycles_possibles: # Vérification du résultat
+...     print("les arcs identifiés:", arcs_cycle_trouve, "ne constituent pas un cycle du graphe")
+...     print("cycles possibles:", cycles_possibles)
+==
+
+testname2 = Vérifications de detection_cycle sur un DAG aléatoire
+pltest2==
+>>> from corrlib_graphes import * #
+>>> G = GrapheOriente(); G.ajouter_arcs(list(graphe_oriente_acyclique_aleatoire(20).edges())) # initialisation d'un DAG aléatoire
+>>> obtenu = detection_cycle(G)# Appel de la fonction detection_cycle
+>>> if obtenu is not None:# Vérification du résultat
+...     print("la fonction identifie le cycle", obtenu.arcs(), " mais le graphe est acyclique")
+...     print("ses arcs:", G.arcs())
+==
+
+testname3 = Vérifications de detection_cycle sur un graphe orienté aléatoire
+pltest3==
+>>> from corrlib_graphes import * #
+>>> G = GrapheOriente(); G.ajouter_arcs(list(graphe_oriente_aleatoire(10).edges())) # initialisation d'un graphe orienté aléatoire
+>>> obtenu = detection_cycle(G)# Appel de la fonction detection_cycle
+>>> mon_cycle = cycle_oriente(G)#
+>>> if obtenu is None: # Vérification du résultat si aucun cycle n'est détecté
+...     if mon_cycle:
+...         print("la fonction ne trouve pas de cycle alors que le graphe en contient un formé par les arcs", mon_cycle.arcs())
+... else: # Vérification du résultat si un cycle est détecté
+...     if mon_cycle: # cas graphe cyclique
+...         if not est_cycle_oriente(obtenu, G):
+...             print("le cycle identifié n'est pas valide")
+...     else: # cas graphe acyclique
+...         print("la fonction identifie le cycle", obtenu.arcs(), " mais le graphe est acyclique")
+==
+
+
 # Le grader:
 #@ /grader/evaluator.py [grader.py]
 evaluator==
@@ -100,6 +148,7 @@ editor.code ==
 # de validation quand vous avez terminé
 
 ==
+
 
 
 
