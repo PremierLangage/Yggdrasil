f046253ad53b66c7f672716df0b074a024ba69cd
Claire DAVID <Claire.David@u-pem.fr>
2020-05-20 18:01:37 +0200

    cdavid modified Yggdrasil/ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl

--- ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
+++ ComputerScience/AnthonyLabarre/L3-algographes/rendus/2020/rendu03-graphes-orientes/rendu03_tout.pl
@@ -93,6 +93,99 @@ False
 [(4, 11), (4, 12), (6, 7), (11, 12), (13, 12)]
 ==
 
+testname0 = Vérifications de parcours_profondeur_oriente sur l'exemple du TD 5 exercice 1
+pltest0==
+>>> from corrlib_graphes import * #
+>>> G = GrapheOriente(); G.ajouter_arcs(CYCLE_ORIENTE_INSTANCE_TD) # initialisation de l'instance du TD
+>>> sommets, parents = parcours_profondeur_oriente(G) # Appel de la fonction parcours_profondeur_oriente
+>>> foret = reconstruire_arbre_oriente(sommets, parents, GrapheOriente) # Reconstruction de la forêt à partir de parents
+>>> sorted(foret.sommets()) == sorted(G.sommets())  # tous les sommets sont-ils couverts?
+True
+>>> ma_foret = foret_parcours_profondeur_oriente(G); arcs_de_ma_foret = set(ma_foret.arcs())#
+>>> for arbre in cc_graphes(foret): # 
+...     if not est_arbre_oriente(arbre): # tous les sous-graphes de la forêt sont-ils des arbres?
+...         print("le sous-graphe suivant de la forêt n'est pas un arbre orienté") #
+...         print("    sommets:", arbre.sommets()) #
+...         print("    arcs:   ", arbre.arcs()) #
+>>> set(foret.arcs()) == set(ma_foret.arcs()) # Vérification que la foret est bien celle du parcours en profondeur avec la convention vue en cours.
+True
+>>> dates_attendues = profondeur_dates_fin(G); ordre_attendu = sorted(dates_attendues, key=dates_attendues.__getitem__) #
+>>> if sommets != ordre_attendu: # Vérification de l'ordre des sommets renvoyés par parcours_profondeur_oriente
+...     print("ordre attendu:", ordre_attendu) 
+...     print("ordre obtenu: ", sommets) 
+==
+
+testname1 = Vérifications de parcours_profondeur_oriente sur l'exemple du TD 5 exercice 3
+pltest1==
+>>> from corrlib_graphes import * #
+>>> G = GrapheOriente(); G.ajouter_arcs(CFC_INSTANCE_TD) # initialisation de l'instance du TD
+>>> sommets, parents = parcours_profondeur_oriente(G)# Appel de la fonction parcours_profondeur_oriente
+>>> foret = reconstruire_arbre_oriente(sommets, parents, GrapheOriente)# Reconstruction de la forêt à partir de parents
+>>> sorted(foret.sommets()) == sorted(G.sommets())  # tous les sommets sont-ils couverts?
+True
+>>> ma_foret = foret_parcours_profondeur_oriente(G); arcs_de_ma_foret = set(ma_foret.arcs())#
+>>> for arbre in cc_graphes(foret): # 
+...     if not est_arbre_oriente(arbre): # tous les sous-graphes de la forêt sont-ils des arbres?
+...         print("le sous-graphe suivant de la forêt n'est pas un arbre orienté")
+...         print("    sommets:", arbre.sommets())
+...         print("    arcs:   ", arbre.arcs())
+>>> set(foret.arcs()) == set(ma_foret.arcs()) # Vérification que la foret est bien celle du parcours en profondeur avec la convention vue en cours.
+True
+>>> dates_attendues = profondeur_dates_fin(G); ordre_attendu = sorted(dates_attendues, key=dates_attendues.__getitem__)#
+>>> if sommets != ordre_attendu: # Vérification de l'ordre des sommets renvoyés par parcours_profondeur_oriente
+...     print("ordre attendu:", ordre_attendu) 
+...     print("ordre obtenu: ", sommets) 
+==
+
+testname2 = Vérifications de parcours_profondeur_oriente sur un graphe orienté aléatoire
+pltest2==
+>>> from corrlib_graphes import * #
+>>> G = GrapheOriente(); G.ajouter_arcs(graphe_oriente_aleatoire(10).edges()) # initialisation d'un graphe orienté aléatoire
+>>> sommets, parents = parcours_profondeur_oriente(G)# Appel de la fonction parcours_profondeur_oriente
+>>> foret = reconstruire_arbre_oriente(sommets, parents, GrapheOriente)# Reconstruction de la forêt à partir de parents
+>>> sorted(foret.sommets()) == sorted(G.sommets())  # tous les sommets sont-ils couverts?
+True
+>>> ma_foret = foret_parcours_profondeur_oriente(G); arcs_de_ma_foret = set(ma_foret.arcs())#
+>>> for arbre in cc_graphes(foret): # 
+...     if not est_arbre_oriente(arbre):# tous les sous-graphes de la forêt sont-ils des arbres?
+...         print("le sous-graphe suivant de la forêt n'est pas un arbre orienté")
+...         print("    sommets:", arbre.sommets())
+...         print("    arcs:   ", arbre.arcs())
+>>> set(foret.arcs()) == set(ma_foret.arcs()) # Vérification que la foret est bien celle du parcours en profondeur avec la convention vue en cours.
+True
+>>> dates_attendues = profondeur_dates_fin(G); ordre_attendu = sorted(dates_attendues, key=dates_attendues.__getitem__)#
+>>> if sommets != ordre_attendu: # Vérification de l'ordre des sommets renvoyés par parcours_profondeur_oriente
+...     print("ordre attendu:", ordre_attendu) 
+...     print("ordre obtenu: ", sommets) 
+==
+
+testname0 = Vérifications de composantes_fortement_connexes sur l'exemple du cours
+pltest0==
+>>> from corrlib_graphes import CFC_INSTANCE_COURS #
+>>> G = GrapheOriente(); G.ajouter_arcs(CFC_INSTANCE_COURS) # initialisation de l'instance du cours
+>>> cfc = composantes_fortement_connexes(G) # Calcul des cfc
+>>> sorted(map(sorted, cfc)) # vérification du résultat
+[[0, 1, 4], [2, 3], [5, 6], [7]]
+==
+
+testname1 = Vérifications de composantes_fortement_connexes sur l'exemple du TD
+pltest1==
+>>> from corrlib_graphes import CFC_INSTANCE_TD #
+>>> G = GrapheOriente(); G.ajouter_arcs(CFC_INSTANCE_TD) # initialisation de l'instance du TD
+>>> cfc = composantes_fortement_connexes(G) # Calcul des cfc
+>>> sorted(map(sorted, cfc))# vérification du résultat
+[[1, 2, 3], [4], [5, 6, 7, 8, 9], [10, 11, 12, 13, 14]]
+==
+
+testname2 = Vérifications de composantes_fortement_connexes sur une instance aléatoire
+pltest2==
+>>> from corrlib_graphes import * #
+>>> G = GrapheOriente(); G.ajouter_arcs(list(graphe_oriente_aleatoire(20).edges())) # initialisation d'une instance aléatoire
+>>> attendu = sorted(map(sorted, cfc(G)))#
+>>> obtenu = sorted(map(sorted, composantes_fortement_connexes(G)))# Calcul des cfc
+>>> if obtenu != attendu: # vérification du résultat
+...     print("CFC obtenues =", obtenu, ";\n CFC attendues =", attendu) # 
+==
 ############################## Fin des doc test #######################################
 
 # Le grader:
