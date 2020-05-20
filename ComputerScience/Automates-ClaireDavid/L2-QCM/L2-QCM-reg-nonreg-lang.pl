1e850c716730e5910f1bb291de23866bfe7d115d
Claire DAVID <Claire.David@u-pem.fr>
2020-05-20 14:02:35 +0200

    cdavid modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-reg-nonreg-lang.pl

--- ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-reg-nonreg-lang.pl
+++ ComputerScience/Automates-ClaireDavid/L2-QCM/L2-QCM-reg-nonreg-lang.pl
@@ -1,4 +1,4 @@
-extends = ../templates/AMC_TXT-rd.pl
+extends = ../templates/AMC_TXT_claire.pl
 
 author = Claire David
 title = QCM - régulier / non régulier ?
@@ -16,6 +16,7 @@ Les langages suivants sont-il réguliers ou non réguliers?
 
 #questions=@test.txt
 questions=@ q-reg-nonreg-langages.txt
+
 
 
 
