f857de33428a53b9701da2c694efffe844362122
David DOYEN <David.Doyen@u-pem.fr>
2020-05-21 17:49:09 +0200

    ddoyen modified Yggdrasil/builder/before_v1.py

---
+++ builder/before_v1.py
@@ -0,0 +1,58 @@
+import sys
+import json
+from components import Component
+from builderlib import aux_component
+from builderlib import PickleEncoder, ComponentEnv
+
+# Import the custom JSON encoder
+try:
+    from json_encoder import CustomEncoder as JSONEncoder
+except ModuleNotFoundError:
+    JSONEncoder = PickleEncoder
+
+# Import the custom Jinja environnement
+try:
+    from jinja_env import CustomEnv as Env
+except ModuleNotFoundError:
+    Env = ComponentEnv
+
+# Import the custom namespace
+try:
+    from namespace import namespace
+except ModuleNotFoundError:
+    namespace = {}
+
+
+if __name__ == "__main__":
+    
+    # JSON context is loaded
+    with open(sys.argv[1], "r") as f:
+        dic = json.load(f)
+    Component.sync_context(dic)
+
+    # the content of namespace is added to dic
+    dic = {**namespace, **dic}
+    
+    if 'before' in dic:
+
+        # execute the script in before key with dic as globals
+        exec(dic['before'], dic)
+        
+        # clean dic from namespace elements
+        exec("", namespace)
+        for key in namespace:
+            if key in dic and dic[key] == namespace[key]:
+                del dic[key]
+
+    # build the key 'extracss' from the content of the key 'style'
+    if 'style' in dic:
+        dic['extracss'] = "<style> %s </style>" % "\n".join(reversed(list(dic['style'].values())))
+
+    for key in ['text', 'form', 'solution']:
+        if key in dic:
+            dic[key] = Env.from_string(dic[key]).render(dic)
+
+    with open(sys.argv[2], "w+") as f:
+        json.dump(dic, f, cls=JSONEncoder)
+
+    sys.exit(0)
