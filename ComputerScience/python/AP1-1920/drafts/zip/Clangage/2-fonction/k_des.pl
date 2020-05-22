bd4f3e3e7b773320cc72c12bc2a4d866464dcb65
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-05-22 21:15:37 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Clangage/2-fonction/k_des.pl

--- ComputerScience/python/AP1-1920/drafts/zip/Clangage/2-fonction/k_des.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/Clangage/2-fonction/k_des.pl
@@ -43,15 +43,13 @@ int lance_k_de( int n,int k){
     if (k<=0)
       return 0;
     if (k==1){
-      if (0<n && n<7){
+      if (0<n && n<7)
     	return 1;
-        }
       else
 	    return 0;
-        }
+    }
     for (i=1;i<=6;i++){
-      nb=lance_k_de( n-i, k-1);
-   	
+      nb=lance_k_de( n-i, k-1);  	
 	  tout+=nb;    
     }                                 
                 
@@ -82,7 +80,8 @@ int nb=0;
 tests==
 [ ["Basique", "5 2", ""],
   ["Vide", "0 3", ""],
-  ["Grand", "20 5", ""],
+  ["Grand", "20 5", ""],4
+   ["Aléatoire", ' '.join([str(random.randint(1, 24)), str(random.randint(1, 4))]), ""],
   ]
 ==
 
