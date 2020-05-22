afe63a146ddadd3fc80459acc6ac59825ca8b2ce
David DOYEN <David.Doyen@u-pem.fr>
2020-05-22 16:32:22 +0200

    ddoyen modified Yggdrasil/builder/test_v1.pl

---
+++ builder/test_v1.pl
@@ -0,0 +1,10 @@
+extends = basic.pl
+
+@ /model/json_coder/json_coder_sympy.py [json_coder.py]
+@ /model/jinja_env/jinja_env_math.pl [jinja_env.py]
+@ /model/namespace/namespace_math.py [namespace.py]
+@ /utils/sympy/evalsympy.py
+@ /utils/sympy/latex2sympy.py
+@ /utils/sympy/sympy2latex.py
+@ /utils/sympy/randsympy.py
+@ /utils/plrandom.py
