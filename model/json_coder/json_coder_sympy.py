7cf7fbafee4ffe7ac7776498fe539966ff1a2ca2
David DOYEN <David.Doyen@u-pem.fr>
2020-05-22 17:23:00 +0200

    ddoyen modified Yggdrasil/model/json_coder/json_coder_sympy.py

--- model/json_coder/json_coder_sympy.py
+++ model/json_coder/json_coder_sympy.py
@@ -6,16 +6,21 @@ try:
 except ModuleNotFoundError:
     namespace = {}
 
-def default(self, obj):
-    if isinstance(obj, (sympy.Basic, sympy.Matrix)):
-        return {'__SymPy__': True, 'srepr': sympy.srepr(obj)}
-    return jsonpickle.Pickler(unpicklable=False).flatten(obj)
 
-JSONEncoder = json.JSONEncoder(default=default)
+class JSONEncoder(json.JSONEncoder):
 
-def object_hook(self, dict):
-    if '__SymPy__' in dict:
-        return sympy.sympify(dict['srepr'], locals=namespace, evaluate=False)
-    return dict
+    def default(self, obj):
+        if isinstance(obj, (sympy.Basic, sympy.Matrix)):
+            return {'__SymPy__': True, 'srepr': sympy.srepr(obj)}
+        return jsonpickle.Pickler(unpicklable=False).flatten(obj)
 
-JSONDecoder = json.JSONDecoder(object_hook=object_hook)
+
+class JSONDecoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, dict):
+        if '__SymPy__' in dict:
+            return sympy.sympify(dict['srepr'], locals=namespace, evaluate=False)
+        return dict
