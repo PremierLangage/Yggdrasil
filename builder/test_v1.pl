5d266840fff91def89ab5aa38dae89348a7d09ed
David DOYEN <David.Doyen@u-pem.fr>
2020-05-22 16:49:28 +0200

    ddoyen modified Yggdrasil/builder/test_v1.pl

--- builder/test_v1.pl
+++ builder/test_v1.pl
@@ -8,3 +8,23 @@
 @ /utils/sympy/sympy2latex.py
 @ /utils/sympy/randsympy.py
 @ /utils/plrandom.py
+
+title = Multiplication de deux nombres complexes
+
+before ==
+z1 = rd.randint(-5, 5) + rd.randint(-5, 5)*I
+z2 = rd.randint(-5, 5) + rd.randint(-5, 5)*I
+sol = (z1 * z2).expand()
+==
+
+text ==
+On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
+==
+
+form ==
+{{ input|component }}
+==
+
+evaluator ==
+score, _, feedback = eval_complex(input.value, sol, form="cartesian")
+==
