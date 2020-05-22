46e5091bdf7c014fc96e1b325ec349fb3ae654a9
David DOYEN <David.Doyen@u-pem.fr>
2020-05-22 16:58:06 +0200

    ddoyen modified Yggdrasil/grader/evaluator_v1.pl

--- grader/evaluator_v1.pl
+++ grader/evaluator_v1.pl
@@ -1,122 +1,61 @@
-import sys, json, jsonpickle
+import sys
+import json
 from components import Component
-from builderlib import aux_component1, aux_component2, aux_component
-from builderlib import PickleEncoder
+from pljson import PickleEncoder
+from pljinja import DefaultEnv
 
-from jinja2 import Template
-
+# import the custom JSON encoder (if it exists)
 try:
-    from json_encoder import CustomEncoder, CustomDecoder
+    from json_coder import JSONEncoder
 except ModuleNotFoundError:
-    CustomEncoder = PickleEncoder
-    CustomDecoder = None
+    JSONEncoder = PickleEncoder
 
+# import the custom Jinja environnement (if it exists)
+try:
+    from jinja_env import Env
+except ModuleNotFoundError:
+    Env = DefaultEnv
+
+# import the custom namespace (if it exists)
 try:
     from namespace import namespace
 except ModuleNotFoundError:
     namespace = {}
 
-def format_feedback_rightwrong(score,feedback):
-    tpl="""<div class="alert {}"><strong>{}</strong> <br> {}</div>"""
-    if score==-1:
-        return tpl.format('alert-info','Attention !',feedback)
-    elif score==100:
-        return tpl.format('alert-success','Bonne réponse.',feedback)
-    else:
-        return tpl.format('alert-danger','Mauvaise réponse.',feedback)
-
-def format_feedback_score(score,feedback):
-    if score==-1:
-        tpl="""<div class="alert {}"><strong>{}</strong><br> {}</div>"""
-        return tpl.format('alert-info',score, feedback)
-    tpl="""<div class="alert {}"><strong>Score : {} / 100</strong><br> {}</div>"""
-    if score==100:
-        return tpl.format('alert-success',score, feedback)
-    elif score==0:
-        return tpl.format('alert-danger',score, feedback)
-    else:
-        return tpl.format('alert-warning',score, feedback)
-
-def format_feedback_lightscore(score,feedback):
-    if score==-1:
-        tpl="""<div class="alert {}"><strong>{}</strong> <br> {}</div>"""
-        return tpl.format('alert-info',score, feedback)
-    tpl="""<div class="alert alert-secondary"><strong>Score : {} / 100 <br> </strong> {}</div>"""
-    return tpl.format(score,feedback)
-
-
-
-missing_evaluator_stderr = """\
-The key 'evaluator' was not found in the context.
-When using this grader, the PL must declare a script inside a key 'evaluator'. This script have
-access to every variable declared in the PL and its 'before' script.
-It should declare a variable 'grade' which should contain a tuple (int, feedback) where int is the grade between [0, 100]."""
-
 if __name__ == "__main__":
-
+    
+    # load the JSON exercise dictionary as Python dictionary
     with open(sys.argv[1], "r") as f:
-        dic = json.load(f, cls=CustomDecoder)
+        dic = json.load(f)
     Component.sync_context(dic)
 
+    # add the custom namespace to the Python exercise dictionary
     dic = {**namespace, **dic}
-
-    aux_component1(dic)
     
-    if 'evaluator' in dic:
-            code = dic['evaluator']
+    if 'before' in dic:
+
+        # execute the before script in the exercise dictionary
+        exec(dic['before'], dic)
+        
+        # clean the the exercise dictionary from namespace elements
+        exec("", namespace)
+        for key in namespace:
+            if key in dic and dic[key] == namespace[key]:
+                del dic[key]
+
+    # render some string values of the exercise dictionary with the custom Jinja environment
+    if 'jinja_keys' in dic:
+        jinja_keys = dic['jinja_keys']
     else:
-        print(missing_evaluator_stderr, file=sys.stderr)
-        sys.exit(1)
-
-    exec(code, dic)
-    exec("", namespace)
-    for key in namespace:
-        if key in dic and dic[key] == namespace[key]:
-            del dic[key]
-
-    aux_component2(dic)
+        jinja_keys = ['text', 'form', 'solution']
     
-    dic = json.loads(json.dumps(dic, cls=CustomEncoder))
+    for key in jinja_keys:
+        if key in dic:
+            dic[key] = Env.from_string(dic[key]).render(dic)
 
-    if 'grade' in dic:
-        score = dic['grade'][0]
-        feedback = dic['grade'][1] + " "
-    else:
-        if 'score' in dic:
-            score = dic['score']    
+    # output the Python exercise dictionary as a JSON dictionary (using the custom encoder)
+    with open(sys.argv[2], "w+") as f:
+        json.dump(dic, f, cls=JSONEncoder)
 
-        feedback=" "
-        if 'feedback' in dic:
-            feedback = dic['feedback'] + " "
-
-    if score >= 0:
-        dic['internals']['attempt'] = dic['internals']['attempt'] + 1
-    
-    if dic['settings']['maxattempt'] is not None:
-        if dic['internals']['attempt'] > dic['settings']['maxattempt']:
-            try:
-                dic['internals']['buttons'].remove('submit')
-            except:
-                pass
-            if score < 100 and 'solution' in dic:
-                feedback += dic['solution']
-    
-    ffeedback = feedback
-    if 'feedback' in dic['settings']:
-        if dic['settings']['feedback']=='rightwrong':
-            ffeedback=format_feedback_rightwrong(score,feedback)
-        elif dic['settings']['feedback']=='score':
-            ffeedback=format_feedback_score(score,feedback)
-        elif dic['settings']['feedback']=='lightscore':
-            ffeedback=format_feedback_lightscore(score,feedback)
-
-    with open(sys.argv[3], "w+") as f:
-        json.dump(dic, f)
-    
-    with open(sys.argv[4], "w+") as f:
-        print(str(ffeedback), file=f)
-    
-    print(int(score))
-    
     sys.exit(0)
 
