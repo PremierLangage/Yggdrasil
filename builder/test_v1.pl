688d014e9ef104d33b159b920209404e31f65b41
David DOYEN <David.Doyen@u-pem.fr>
2020-05-22 18:58:13 +0200

    ddoyen modified Yggdrasil/builder/test_v1.pl

--- builder/test_v1.pl
+++ builder/test_v1.pl
@@ -13,13 +13,16 @@
 title = Multiplication de deux nombres complexes
 
 before ==
-z1 = rd.randint(-5, 5) + rd.randint(-5, 5)*I
-z2 = rd.randint(-5, 5) + rd.randint(-5, 5)*I
+z = 2-2*I
+var('x')
+P = x**2-3*x+1
 sol = (z1 * z2).expand()
 ==
 
 text ==
-On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
+$$ {{ z|latex }} $$ 
+
+$$ {{ P|latex }} $$
 ==
 
 input =: Input
