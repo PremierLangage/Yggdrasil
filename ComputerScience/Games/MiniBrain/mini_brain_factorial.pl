839b2d190c686bc68d11937ed412468c70ce757f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-23 11:37:08 +0200

    nborie modified Yggdrasil/ComputerScience/Games/MiniBrain/mini_brain_factorial.pl

--- ComputerScience/Games/MiniBrain/mini_brain_factorial.pl
+++ ComputerScience/Games/MiniBrain/mini_brain_factorial.pl
@@ -22,49 +22,6 @@ text==
 Proposez un code Mini-Brain calculant la **factorielle** de l'entier 
 stocké à la case 10 de la mémoire centrale. Placez votre résultat en case 11 
 de la mémoire centrale avant la fin d'exécution de votre programme.   
-   
-<br />
-
-<pre style="font-size: 0.7em;">
-Mini Brain cheat sheet :
-========================
-
--147 : interpreted directly as a constant value
-$12  : value at address 12 inside the central memory
-a    : the accumulator
-#3   : value stored at register 3
-$#2  : value in central memory at address indexed by value of register 2
-#$47 : value stored at register indexed by value in central memory at address 47
-
-* ld [$? or value] [#?-a]: load value inside the CPU
-* st [#?-a or value] [$?]: store value to central memory
-* mv [#?-a] [#?-a]: internal fast copy inside the CPU
-
-* inc [#?]: increment targeted register (up the flag if zero)
-* dec [#?]: decrement targeted register (up the flag if zero)
-* add [#?-a or value or $?]: add targeted value to the accumulator (up the flag if zero)
-* sub [#?-a or value or $?]: subtract targeted value to the accumulator (up the flag if zero)
-* mul [#?-a or value or $?]: multiply by targeted value the accumulator (up the flag if zero)
-* div [#?-a or value or $?]: divide the accumulator by targeted value (up the flag if zero if exact division)
-* mod [#?-a or value or $?]: replace the accumulator by its remainder when divide by targeted value (up the flag if zero)
-* cmp [#?-a or value or $?]: do nothing (up the flag if the accumulator is smaller than the argument)
-
-* bfup [#?-a or value or $?]: jump to instruction indexed by the value if the flag is up else do nothing and continue
-* bfdn [#?-a or value or $?]: jump to instruction indexed by the value if the flag is down else do nothing and continue
-* bnow [#?-a or value or $?]: jump in all cases to instruction indexed by the value
-
-* exit [#?-a or value or $?]: stop the program with return code the targeted value   
-* - empty instruction - : produce the behavior of exit a
-</pre>
-
-
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script>
-$( function() {
-  $( "#accordion" ).accordion();
-} );
-</script>
 ==
 
 editor.code ==
