e144ec4dadf1c13d4f26bb2162e46fa04a1845b0
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-05-23 10:35:51 +0200

    nborie modified Yggdrasil/ComputerScience/Games/MiniBrain/mini_brain_template.pl

--- ComputerScience/Games/MiniBrain/mini_brain_template.pl
+++ ComputerScience/Games/MiniBrain/mini_brain_template.pl
@@ -88,6 +88,7 @@ def make_minibrain_test(name, action_before_str, check_after_str):
 nb_good = 0
 nb_bad = 0
 # feedback = '<div id="accordion">'
+feedback = ""
 total_cycles = 0
 for i in range(len(all_before)):
     (res, feedback_test, nb_c) = make_minibrain_test(all_before[i][0], all_before[i][1], all_check[i])
