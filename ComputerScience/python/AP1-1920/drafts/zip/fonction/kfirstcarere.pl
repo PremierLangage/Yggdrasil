b551f8400245c3510b4f9e57b645e51e98a8234c
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-05-29 12:12:35 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/fonction/kfirstcarere.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/fonction/kfirstcarere.pl
@@ -0,0 +1,59 @@
+
+
+author=DR
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl 
+
+title = La somme des k premiers carrés (de 0 à (k-1)^2)
+
+code==
+def carre(...):
+
+
+==
+
+text==  
+
+Programmer une fonction **carre** qui prend en paramètre un entier et retourne son carré.<br>
+
+Programmepr ensuite une fonction **premiers_carres** qui : <br>
+* prend en paramètre un entier k <br>
+* imprime les k premiers carrés de 0 à (k-1)^2 <br>
+
+==
+
+soluce==#|python|
+def carre(n):
+    return(n*n)
+
+def premiers_carres(k) :
+    s=0
+    for i in range(k) :
+        s += (carre(i))
+    return s
+
+==
+before==
+import random
+def mon_carre(n):
+    return(n*n)
+
+def premiers_carres(k) :
+    s=0
+    for i in range(k) :
+        s += (carre(i))
+    return s
+
+n=random.randint(14,50)
+c=random.randint(0,10)
+pltest3=""">>> if carre({})!={}\n print("la vfonction carre ne fonctionne pas")  """.format(n,mon_carre(n))
+
+after=before
+==
+
+pltest==
+>>> carre(1)
+1
+
+
