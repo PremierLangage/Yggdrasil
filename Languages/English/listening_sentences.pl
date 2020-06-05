61a80bc2ae0aad66fe27a3d11af1c3307e59d658
David DOYEN <David.Doyen@u-pem.fr>
2020-06-05 16:35:00 +0200

    ddoyen modified Yggdrasil/Languages/English/listening_sentences.pl

--- Languages/English/listening_sentences.pl
+++ Languages/English/listening_sentences.pl
@@ -25,7 +25,7 @@ text ==
 <button onclick="playAudio()" class="btn btn-info"><i class="fas fa-volume-up"></i></button> Transcrire la phrase.
 ==
 
-input = Input
+input =: Input
 
 form ==
 {{ input|component }}
