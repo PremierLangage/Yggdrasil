dc413f4787cfff46398bf9d293445af14ca22e75
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-06-10 14:38:55 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AdamA./lecteurslide.pl

---
+++ ComputerScience/python/AP1-1920/drafts/AdamA./lecteurslide.pl
@@ -0,0 +1,29 @@
+
+
+@ /lib/builder/before.py [builder.py]
+@ /lib/grader/evaluator.py [grader.py]
+
+
+
+@ slide.md
+
+title=les slides 
+before==
+with open ("slide.md" , "r") as f:
+    t=f.readlines()
+
+slides=t.split("***")
+numero=0
+text=slides[numero]
+
+title= f"les slides {numero}"
+
+==
+
+
+evaluator==
+numero+=1
+text=slides[numero]
+
+
+==
