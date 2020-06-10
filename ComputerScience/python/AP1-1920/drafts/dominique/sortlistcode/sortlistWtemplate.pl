0a0771582fe576f0793d966c18df900a8e288073
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-06-10 20:26:52 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/sortlistcode/sortlistWtemplate.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/sortlistcode/sortlistWtemplate.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/sortlistcode/sortlistWtemplate.pl
@@ -2,6 +2,7 @@ extends = /template/sortlist.pl
 
 title = Premiers ministres (SortList)
 
+
 @ codefile.py
 
 before ==
@@ -9,18 +10,16 @@ import random as rd
 import csv
 
 with open('codefile.py', newline='') as file:
-    rows = list(csv.DictReader(file, delimiter=','))
+    l=file.readlines()
 
-lst = rd.sample(range(len(rows)), 5)
-lst.sort()
-sortlist.setdata_from_list([rows[i]["nom"] for i in lst])
+sortlist.setdata_from_list([ "<code>"+x.strip()+"</code>" for x in l])
 ==
 
 text ==
 Classer ces premiers ministres de la Ve République du plus ancien au plus récent (selon la date d'entrée en fonction).
 ==
 
-@ /utils/components/sortlist.py [customsortlist.py]
+
 
 
 evaluator ==
@@ -29,3 +28,19 @@ score = sortlist.eval()
 
 settings.feedback = lightscore
 
+sortlist.css==
+background: #f4f4f4;
+    border: 1px solid #ddd;
+    border-left: 3px solid #f36d33;
+    color: #666;
+    page-break-inside: avoid;
+    font-family: monospace;
+    font-size: 15px;
+    line-height: 1.6;
+    margin-bottom: 1.6em;
+    max-width: 100%;
+    overflow: auto;
+    padding: 1em 1.5em;
+    display: block;
+    word-wrap: break-word;
+==
