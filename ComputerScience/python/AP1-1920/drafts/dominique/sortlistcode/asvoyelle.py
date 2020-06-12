70117a587016424972021e32cf6897278033bcf7
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-06-12 16:03:26 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/sortlistcode/asvoyelle.py

--- ComputerScience/python/AP1-1920/drafts/dominique/sortlistcode/asvoyelle.py
+++ ComputerScience/python/AP1-1920/drafts/dominique/sortlistcode/asvoyelle.py
@@ -2,10 +2,13 @@
 def asvoyelle(s):
     """
     retourne si oui ou non la chaine s contient une voyelle "aeiuoy"
+    >>> asvoyelle("toto")
+    True
     """
     for v in "aeiuoy":
         if v in s:
             return True
     return False
+
 
 
