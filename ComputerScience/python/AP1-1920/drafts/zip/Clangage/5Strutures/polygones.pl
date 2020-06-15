a8233a2034329510d9d67f5c8555d8d768bd8d21
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-06-15 17:47:46 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Clangage/5Strutures/polygones.pl

--- ComputerScience/python/AP1-1920/drafts/zip/Clangage/5Strutures/polygones.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/Clangage/5Strutures/polygones.pl
@@ -15,11 +15,11 @@ int x,y;<br>
 }Point;<br>
 pour représenter un point du plan.<br>
 Ecrire une fonction `distance ` qui reçoit deux *Point* et renvoie leur distance.<br>
-Définir une structure `Polygone` formé<br>
-d'un tableau de *Point* de nom `sommets`;<br>
-d'un entier `nb_sommets`, le nombre effectif de points<br>
+Définir une structure `Polygone` formé : 
+d'un tableau de *Point* de nom `sommets`et
+d'un entier `nb_sommets`, le nombre effectif de points.br>
 
-Ecrire une fonction `perimetre` qui recoit un polygone et renvoie son périmetre
+Ecrire une fonction `perimetre` qui recoit un polygone et renvoie son périmetre.
 ==
 
 editor.code==
@@ -38,8 +38,8 @@ return (sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)));
 
 float perimetre(Polygone p[]){
 int i;
-float perim=distance(p.sommets[p.nb_sommets-1],p.sommets[0]);
-fot(i=0;i<p.nb_sommets-1;i++)
+float perim=distance(p.sommets[p.nb_sommets-1],p.sommets[]);
+fot(i=;i<p.nb_sommets-1;i++)
 perim+=distance(p.sommets[i],p.sommets[i+1]);
 return perim;
 }
@@ -58,16 +58,31 @@ int y;
 }Point;
 ==
 
+codeafter==
 
+int main(int argc, char* argv[]){
+  Point tab[100];
+  int size = ;
+  int x,y;
+  float d;
+
+  while (scanf("%d", &x) == 1){
+    scanf("%d",&y);
+    tab.sommets[size].x = x;
+    tab.sommets[size].y=y;
+    size++;
+  }
+  d=perimetre(tab);
+
+  printf("le perimetre mesure %f\n",d);
+  return ;
+}
+==
 
 tests== 
-[ ["Simple", "1 1 1 1 1 1", ""],
-	["Nul1", "1 1 0 1 -1 1", ""],
-  ["Nul2", "-2 1 1 1 1 1", ""],
-  ["vecteurs aléatoires", " ".join([str(random.randint(-10,10)) for i in range(6)]), "" ],
-  ["vecteurs aléatoires", " ".join([str(random.randint(-10,10)) for i in range(6)]), "" ],
-  ["vecteurs aléatoires", " ".join([str(random.randint(-10,10)) for i in range(6)]), "" ],
-  ["vecteurs aléatoires", " ".join([str(random.randint(-10,10)) for i in range(6)]), "" ] ]
+[ ["Pythagorre", "", "0 0 0 3 4 0 "],
+	
+  ["vecteurs aléatoires","", " ".join([str(random.randint(-10,10)) for i in 2*range(6)]) ] ]
 ==
 
 
