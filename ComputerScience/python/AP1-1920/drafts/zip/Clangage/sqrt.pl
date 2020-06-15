97592ae4de5c403c4a3161deaecf2e1bceb6c1e3
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-06-15 19:09:43 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Clangage/sqrt.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/Clangage/sqrt.pl
@@ -0,0 +1,41 @@
+
+author=Nicolas Borie et Marc Zipstein
+title=Structure pour points entiers en dimension 2
+tag=structure
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+sqrt(5).
+
+==
+
+editor.code==
+
+==
+codebefore==
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+==
+solution==
+
+
+==
+
+codeafter==
+ l
+
+int main(void){
+  printf("%f",sqrt(5));
+return 0;
+}
+==
+
+tests==
+[
+ ["Déclaration et utilisation",""  ,""],
+]
+==
+
+
+
