071a361bcd7ec57b392d561d811f60b3f53b6250
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-06-16 19:00:03 +0200

    drevuz modified Yggdrasil/ComputerScience/C_Initiation/string_4/titre.pl

--- ComputerScience/C_Initiation/string_4/titre.pl
+++ ComputerScience/C_Initiation/string_4/titre.pl
@@ -1,3 +1,6 @@
+
+
+
 title=Mise en titre
 tag=tab
 extends=/ComputerScience/C/template/stdsandboxC.pl
@@ -60,6 +63,7 @@ tests==
  ["aleatoire",""," ".join(["".join([chr(random.randint(97, 122)) for i in range(random.randint(2,10))]) + " " +"".join([ chr(random.randint(65, 90)) for i in range(random.randint(0,1))]) for j in range(5)])],
   ]
 ==
+
 
 
 
