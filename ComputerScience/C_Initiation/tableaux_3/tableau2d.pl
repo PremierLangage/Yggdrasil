685719db7e6f06848baf8c534e3970e9246954bc
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-06-16 11:30:39 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tableau2d.pl

--- ComputerScience/C_Initiation/tableaux_3/tableau2d.pl
+++ ComputerScience/C_Initiation/tableaux_3/tableau2d.pl
@@ -6,13 +6,17 @@ extends=/ComputerScience/C/template/stdsandboxC.pl
 text==
 Ecrire une fonction de protopype `void remplit(char t[][MAX],int li,int col);` 
 ses dimensions effectives et remplit la partie effective aec les lettres minuscules conssécutives.<br>
-Après la lettre $%\tt{z}%$ on repart de la lettre $%\tt{a}%$ 
-exemple (avec MAX 50
+Après la lettre $%\tt{z}%$ on repart de la lettre $%\tt{a}%$ <br>
+Exemples (avec MAX 50)<br>
 remplit(t,3,4) transforme les 3 premières lignes et 4 premières colonnes en<br>
 abcd<br>
 efgh<br>
-
-
+ijkl<br>
+remplit(t,4,10) transforme les 4 premières lignes et 10 premières colonnes en<br>
+abcdefghij<br>
+klmnopqrst<br>
+uvwxyzabcd<br>
+efghijklmn<br>
 ==
 
 codebefore==
