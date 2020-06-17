ff724476ba6badd8028b6cb21c3c7228a7095a4b
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-06-17 08:35:53 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/string_4/palindrome.pl

---
+++ ComputerScience/C_Initiation/string_4/palindrome.pl
@@ -0,0 +1,72 @@
+
+
+author= 
+
+title=Palindrome
+tag=tab
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+Un palindrome est une suite de caractères pouvant indifféremment être lue de gauche à droite ou de droite à gauche.  
+Ainsi $%\tt{elle,été,laval}%$ sont des palindromes.  
+Ecrire une fonction `est_palindrome` qui reçoit une chaine de caractères 
+et renvoie 1 si celle-ci est un palindrome et, 0 sinon.
+==
+
+codebefore==
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 50
+==
+
+editor.code==
+int est_palindrome(char s[]){
+int i,l;
+l=strlen(s);
+for(i=0; i<l/2;i++)
+    if(s[i]!=s[l-1-i])
+        return 0;
+return 1;
+}
+==
+
+solution==
+int est_palindrome(char s[]){
+int i,l;
+l=strlen(s);
+for(i=0; i<l/2;i++)
+    if(s[i]!=s[l-1-i])
+        return 0;
+return 1;
+}
+==
+codeafter==
+int main(void){
+  char s[MAX];
+ scanf("%s",s);
+if (est_palindrome(s))
+   printf("%s est un palindrome\n",s);
+else 
+   printf("%s est un palindrome\n",s);
+  return 0;
+}
+
+==
+
+
+tests==
+[ ["simple éxécution", "","kayak"],
+ ["aleatoire","",random.choice(["elle","velo"])],
+  ]
+==
+
+
+
+
+
+
+
+
+
