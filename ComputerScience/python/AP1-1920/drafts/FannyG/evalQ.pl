d9e6dfd2192000e17f291274db1230594e47a9cf
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-06-24 17:27:29 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
@@ -1,7 +1,21 @@
-
 # author=Fanny Garnevault
+
+@ /utils/sandboxio.py
 @ /grader/evaluator.py [grader.py]
+@ /builder/before.py [builder.py]
+
+inputbox =: Input
+inputbox.type = text
+
+general_feedback = " Merci pour votre réponse." 
+
 title = Evalutation question
 text  = PLEASE OVERRIDE THE TEXT OF THE EXERCISE
 form  = {{ inputbox|component }}
+
+before= 
+
+evaluator==
+grade = (100, general_feedback)
+==
 
