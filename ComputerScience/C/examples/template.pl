1f146c8eb4212dbf43be816ea68cea83d310336c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-06-25 10:44:17 +0200

    nborie modified Yggdrasil/ComputerScience/C/examples/template.pl

--- ComputerScience/C/examples/template.pl
+++ ComputerScience/C/examples/template.pl
@@ -17,9 +17,9 @@ evaluator==#|python|
 exec(editor.code)
 
 nb_bad = 0
-if fonction_eleve(data1) != expexted1:
+if fonction_eleve(int(data1)) is not bool(expexted1):
     nb_bad += 1
-if fonction_eleve(data2) != expexted2:
+if fonction_eleve(int(data2)) is not bool(expexted2):
     nb_bad += 1
 
 grade = (100 - 50*nb_bad, "On pourrait mettre du feedback ici...")
