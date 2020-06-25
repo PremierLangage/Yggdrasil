c643fe9f9db44639af0009ae6845c533e55867b3
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-06-25 12:01:46 +0200

    nborie modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AdamA./minimal.pl

--- ComputerScience/python/AP1-1920/drafts/AdamA./minimal.pl
+++ ComputerScience/python/AP1-1920/drafts/AdamA./minimal.pl
@@ -1,3 +1,6 @@
+@ /utils/sandboxio.py
+grader =@ /grader/evaluator.py
+
 title=Bon ben voilà un titre
 
 text==
@@ -6,4 +9,10 @@ Bon ben voilé un énoncé...
 
 form==
 <input type="text" id="form_pouet" name="fname">
+==
+
+evaluator==#|python|
+feedback = response['pouet']
+note = 100
+grade = (note, feedback)
 ==
