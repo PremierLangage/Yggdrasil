19d1e494d86a15bef9ba19076af5e14e902df5b8
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-06-25 10:26:12 +0200

    nborie modified Yggdrasil/ComputerScience/C/examples/pair.pl

--- ComputerScience/C/examples/pair.pl
+++ ComputerScience/C/examples/pair.pl
@@ -5,7 +5,7 @@ title=Pair ou impair
 text==
 Coder une fonction Python `fonction_eleve` qui prend un 
 seul argument supposé être un entier et return `True` si
-l'argument est pair et False si l'argument est impair.
+l'argument est pair et `False` si l'argument est impair.
 ==
 
 data1=1
