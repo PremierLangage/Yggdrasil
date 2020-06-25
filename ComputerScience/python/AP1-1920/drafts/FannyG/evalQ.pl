b630052544e60d2d7020e8972b2966beac76fa69
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-06-25 14:28:37 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
@@ -20,7 +20,31 @@ form  = {{ inputbox|component }}
 
 before= 
 
-evaluator==
+evaluator==#|python|
+
 grade = (100, general_feedback)
+==
+
+
+
+
+@ /utils/sandboxio.py
+grader =@ /grader/evaluator.py
+
+title=Bon ben voilà un titre
+
+text==
+Bon ben voilé un énoncé...
+==
+
+form==
+<input type="text" id="form_pouet" name="fname">
+==
+
+evaluator==#|python|
+feedback = "J'ai bien lu votre formulaire" + response['pouet']
+note = 100
+erreur de syntaxe....
+grade = (note, feedback)
 ==
 
