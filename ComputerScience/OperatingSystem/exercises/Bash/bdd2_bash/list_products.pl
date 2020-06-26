0eddf2b72f737862b164398952653d2c616eb3e0
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-06-26 12:02:49 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/bdd2_bash/list_products.pl

---
+++ ComputerScience/OperatingSystem/exercises/Bash/bdd2_bash/list_products.pl
@@ -0,0 +1,52 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/OperatingSystem/templates/bash_template.pl
+
+# Add the file containing the db of product items
+@ item.txt
+
+author=Nicolas Borie
+title=
+
+before==#|python|
+import random
+
+experiences = [
+("jaune", "30\n"),
+("vert", "32\n"),
+("blanc", "31\n"),
+
+]
+
+instructions, expected_stdout = random.choice(experiences)
+
+==
+
+
+text==
+Rappel de la structuration du fichier **item.txt**:   
+<br />
+**id;type;couleur;poids;quantité;jour;mois;année**   
+<br />
+
+**Établissez une commande qui affiche le nombre de produits de type {{ instructions }}**. 
+Veuillez à faire en sorte que votre 
+commande ne produise que l'affichage numérique seul sur une 
+ligne (puis un retour à la ligne). N'hésitez à faire plusieurs essais.
+==
+
+
