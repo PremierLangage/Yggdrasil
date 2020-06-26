b4d044c128e5333e46e18dfbfedc907ca8890942
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-06-26 13:33:39 +0200

    drevuz modified Yggdrasil/ComputerScience/python/concept/variable/typeexpression.pl

--- ComputerScience/python/concept/variable/typeexpression.pl
+++ ComputerScience/python/concept/variable/typeexpression.pl
@@ -1,11 +1,39 @@
 
 extends=/model/AMC_TXT.pl
 
+@ buildquestions.py 
 
 
+before == #|python|
+import random as rd
+from customradio import CustomRadio
+from customcheckbox import CustomCheckbox
+from AMC import parse_AMC_TXT
+from buildquestions import buildquestion
 
-before=
-var=expression
+questions = buildquestion(5)
+
+list_questions = parse_AMC_TXT(questions)
+
+if 'nbstep' in globals():
+    list_questions = rd.sample(list_questions, nbstep)
+else:
+    nbstep = len(list_questions)
+
+comp = []
+statement  = []
+
+for i, q in enumerate(list_questions):
+    if q['type'] == "Radio":
+        comp.append(CustomRadio())
+    elif q['type'] == "Checkbox":
+        comp.append(CustomCheckbox())
+    statement.append(q['text'])
+    comp[i].setitems(q['items'])
+    comp[i].setsol_from_index(q['index'])
+    if 'ordered' not in q['options']:
+        comp[i].shuffle()
+==
 
 
 
