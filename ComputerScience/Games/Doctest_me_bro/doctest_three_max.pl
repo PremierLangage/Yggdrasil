fdb7b8d9f5a489d8319f7df8593a9f18e1afc974
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-06-29 09:27:11 +0200

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_three_max.pl

---
+++ ComputerScience/Games/Doctest_me_bro/doctest_three_max.pl
@@ -0,0 +1,60 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=doctest_me_bro_template.pl
+
+title=Doctest challenge : les trois plus grands
+
+text==
+Les élèves avaient la consigne suivante :   
+
+<div style="color:white; background-color: black; padding: 0.5em; border-radius: 0.5em;">
+Écrivez une fonction python <b>trois_max</b> qui prend en argument une liste de nombres 
+entiers et retourne un tuple des trois plus grandes valeurs dans la liste 
+(max, second max, troisième max). Si la liste est trop courte (deux éléments ou moins)
+remplacez petit à petit le troisième max puis le second max puis le max par la 
+valeur None.
+</div>
+
+Proposez des doctests Python pour detecter la validité et les erreurs 
+potentielles des codes rendus par les élèves.
+==
+
+good_code_1==#|python|
+def trois_max(L):
+    """
+    TEST_INCLUSION
+    """
+    ans = [None, None, None]
+    for e in L:
+        if (ans[2] is None) or (ans[2] <  e):
+            ans[2] = e
+            if (ans[1] is None) or (ans[1] <  e):
+                ans[1], ans[2] = ans[2], ans[1]
+                if if (ans[0] is None) or (ans[0] <  e):
+                    ans[0], ans[1] = ans[1], ans[0]
+    return tuple(ans)
+==
+
+bad_code_1==#|python|
+def trois_max(L):
+    """
+    TEST_INCLUSION
+    """
+
+==
+
+
