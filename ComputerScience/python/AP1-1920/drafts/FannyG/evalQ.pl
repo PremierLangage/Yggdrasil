2bd4c4c5c5c8acc4f14b1bbc41c6b4d47b992246
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-06-29 12:19:24 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
@@ -21,15 +21,15 @@ Question(s) à choix multiples (1 seule réponse sélectionnable)
 
 form==
 <p>Question(s) à choix multiples (1 seule réponse sélectionnable)</p>
-<input type="radio" id="form_copie_blanche" name="evaluation" value="copie_blanche">
+<input type="radio" id="form_copie_blanche" name="fevaluation" value="copie_blanche">
 <label for="copie_blanche">Copie blanche</label><br>
-<input type="radio" id="form_insuffisant" name="evaluation" value="insuffisant">
+<input type="radio" id="form_insuffisant" name="fevaluation" value="insuffisant">
 <label for="insuffisant">Insuffisant</label><br>
-<input type="radio" id="form_passable" name="evaluation" value="passable">
+<input type="radio" id="form_passable" name="fevaluation" value="passable">
 <label for="passable">Passable</label><br>
-<input type="radio" id="form_bien" name="evaluation" value="bien">
+<input type="radio" id="form_bien" name="fevaluation" value="bien">
 <label for="bien">Bien</label><br>
-<input type="radio" id="form_tres_bien" name="evaluation" value="tres_bien">
+<input type="radio" id="form_tres_bien" name="fevaluation" value="tres_bien">
 <label for="tres_bien">Très bien</label><br>
 
 <input type="text" id="form_pouet" name="fname">
