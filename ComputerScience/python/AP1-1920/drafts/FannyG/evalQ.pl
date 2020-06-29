570fc397a5a164e27da2076c42a101e8f4f0dd78
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-06-29 11:56:45 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
@@ -27,15 +27,11 @@ choices==
 ==
 
 form==
-<form>
-  <p>Question(s) à choix multiples (1 seule réponse sélectionnable)</p>
-  <input type="radio" id="male" name="gender" value="male">
-  <label for="male">Male</label><br>
-  <input type="radio" id="female" name="gender" value="female">
-  <label for="female">Female</label><br>
-  <input type="radio" id="other" name="gender" value="other">
-  <label for="other">Other</label>
-</form>
+<p>Question(s) à choix multiples (1 seule réponse sélectionnable)</p>
+<input type="radio" id="form_copie_blanche" name="copie_blanche" value="copie_blanche">
+<label for="copie_blanche">Copie blanche</label><br>
+<input type="radio" id="form_insuffisant" name="insuffisant" value="insuffisant">
+<label for="insuffisant">Insuffisant</label><br>
 
 <input type="text" id="form_pouet" name="fname">
 </br></br>
