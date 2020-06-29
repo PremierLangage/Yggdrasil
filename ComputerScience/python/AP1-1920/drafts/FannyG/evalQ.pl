555610c17c7a99f4baac3ea98e90ffe87af90716
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-06-29 11:54:50 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
@@ -7,13 +7,7 @@ extends = /gift/templates/qselectset.pl
 
 general_feedback = " Merci pour votre réponse." 
 horizontal % false
-choices==
-=Copie blanche
-=Insuffisant
-=Passable
-=Bien
-=Très bien
-==
+
 
 title = Evaluation question
 
@@ -24,28 +18,23 @@ Réponse de l'élève
 Question(s) à choix multiples (1 seule réponse sélectionnable)  
 ==
 
-
+choices==
+=Copie blanche
+=Insuffisant
+=Passable
+=Bien
+=Très bien
+==
 
 form==
 <form action="/action_page.php">
-  <p>Please select your gender:</p>
+  <p>Question(s) à choix multiples (1 seule réponse sélectionnable)</p>
   <input type="radio" id="male" name="gender" value="male">
   <label for="male">Male</label><br>
   <input type="radio" id="female" name="gender" value="female">
   <label for="female">Female</label><br>
   <input type="radio" id="other" name="gender" value="other">
   <label for="other">Other</label>
-
-  <br>  
-
-  <p>Please select your age:</p>
-  <input type="radio" id="age1" name="age" value="30">
-  <label for="age1">0 - 30</label><br>
-  <input type="radio" id="age2" name="age" value="60">
-  <label for="age2">31 - 60</label><br>  
-  <input type="radio" id="age3" name="age" value="100">
-  <label for="age3">61 - 100</label><br><br>
-  <input type="submit" value="Submit">
 </form>
 
 <input type="text" id="form_pouet" name="fname">
