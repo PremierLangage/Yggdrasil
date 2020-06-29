300796ac6be7dcf06ade0fb35829bf6bb64b443b
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-06-29 21:47:28 +0200

    nborie modified Yggdrasil/ComputerScience/GenericProgramming/GPtemplate.pl

--- ComputerScience/GenericProgramming/GPtemplate.pl
+++ ComputerScience/GenericProgramming/GPtemplate.pl
@@ -30,7 +30,7 @@ editor.language = python
 editor.codes %=
 [
     { "language": "csharp", "code": "// write a csharp code here" },
-    { "language": "c", "code": "// write a c code here" },
+    { "language": "c", "code": "/* write a c code here */" },
     { "language": "cpp", "code": "// write a cpp code here" },
     { "language": "java", "code": "// write a java code here" },
     { "language": "python", "code": "# write a python code here" },
