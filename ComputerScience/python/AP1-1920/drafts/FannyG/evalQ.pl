f31c71f9adc4f3db4e28784d089efef6d64550e6
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-06-29 11:46:35 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
@@ -32,11 +32,6 @@ before==
 ==
 
 evaluator==#|python|
-grade = (100, "ok")
-==
-
-
-evaluator==#|python|
 feedback = "J'ai bien lu votre formulaire " + response['pouet'] + " " + response['pouet2']
 note = 100
 grade = (note, feedback)
@@ -47,6 +42,61 @@ form==
 <p>Explication de la note :</p>
 <textarea id="form_pouet2" name="Explication" cols=100% rows="5">
 </textarea>
+==
+
+
+radio =: RadioGroup
+horizontal % false
+choices = 
+general_feedback = 
+
+title = PLEASE OVERRIDE THE TITLE OF THE EXERCISE
+text  = PLEASE OVERRIDE THE TEXT OF THE EXERCISE
+form  = {{ radio|component }}
+
+before== #|python|
+import uuid
+import re
+
+lines = choices.split('\n')
+choices = []
+radio.items = []
+pattern = re.compile(r'(?P<type>\=|\~)(?P<content>(?:(?:\\\#)|[^\#])+)(?:\#(?P<feedback>.*))?')
+for line in lines:
+    match = pattern.match(line)
+    if not match:
+        continue
+    id = str(uuid.uuid4())
+    choice = {
+        "id": id,
+        "feedback": match.group('feedback') or '',
+        "right": match.group('type') == '='
+    }
+    choices.append(choice)
+    radio.items.append({
+        "id": id,
+        "content": match.group('content')
+    })
+==
+
+evaluator== #|python|
+score = 0
+feedback = ''
+selectedId = radio.selection
+for index, item in enumerate(radio.items):
+    item['css'] = ''
+    choice = choices[index]
+    if choice['right']:
+        item['css'] = 'success-border anim-pulse'
+    if choice["id"] == selectedId:
+        if choice['feedback']:
+            feedback =  choice["feedback"] + "<br/>"
+        item["css"] = "error-border"
+        if choice["right"]:
+            score = 100
+            item["css"] = "success-border"
+feedback += general_feedback
+grade = (score, f"<p>{feedback}</p>")
 ==
 
 
