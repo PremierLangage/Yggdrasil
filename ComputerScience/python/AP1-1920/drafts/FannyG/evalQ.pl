bcc6c5891c676042dea6f18ced12c7f8c3e70880
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-06-29 11:53:42 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evalQ.pl
@@ -6,7 +6,6 @@
 extends = /gift/templates/qselectset.pl
 
 general_feedback = " Merci pour votre réponse." 
-radio =: RadioGroup
 horizontal % false
 choices==
 =Copie blanche
@@ -28,7 +27,27 @@ Question(s) à choix multiples (1 seule réponse sélectionnable)
 
 
 form==
-{{ radio|component }}
+<form action="/action_page.php">
+  <p>Please select your gender:</p>
+  <input type="radio" id="male" name="gender" value="male">
+  <label for="male">Male</label><br>
+  <input type="radio" id="female" name="gender" value="female">
+  <label for="female">Female</label><br>
+  <input type="radio" id="other" name="gender" value="other">
+  <label for="other">Other</label>
+
+  <br>  
+
+  <p>Please select your age:</p>
+  <input type="radio" id="age1" name="age" value="30">
+  <label for="age1">0 - 30</label><br>
+  <input type="radio" id="age2" name="age" value="60">
+  <label for="age2">31 - 60</label><br>  
+  <input type="radio" id="age3" name="age" value="100">
+  <label for="age3">61 - 100</label><br><br>
+  <input type="submit" value="Submit">
+</form>
+
 <input type="text" id="form_pouet" name="fname">
 </br></br>
 <p>Explication de la note :</p>
