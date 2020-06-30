c5ae8e66fedb03cbf4222eb039c3eee9a44a9437
Essox Lucius <Elucius@aol.fr>
2020-06-30 14:45:47 +0200

    elucius modified Yggdrasil/Mathematics/tests/testA.pl

--- Mathematics/tests/testA.pl
+++ Mathematics/tests/testA.pl
@@ -1,10 +1,20 @@
 extends = /model/mathinput.pl
 
-title = Titre
+title = Un pré
 
-text ==
-Enonce
+before ==
+L = 10 * rd.randint(1,10)
+l = 10 * rd.randint(1,10)
+per = 2*(L+1)
 ==
 
+text ==
+Donner le périmètre d'un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m.
+==
+
+input =: Input
+input.type = number
+
 form ==
+{{input|component}}
 ==
