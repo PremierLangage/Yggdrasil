464abf06dde45894f901367c1066bb1543238ce4
Essox Lucius <Elucius@aol.fr>
2020-07-01 18:31:52 +0200

    elucius modified Yggdrasil/Mathematics/tests/testWIMS.pl

--- Mathematics/tests/testWIMS.pl
+++ Mathematics/tests/testWIMS.pl
@@ -3,14 +3,33 @@ extends = /model/mathinput.pl
 title = Simplification d'une fraction 1
 
 before ==
-f = rd.random.choice(['2','3','4','5','6','7','8','9'])
+from fractions import Fraction
+f = rd.choice(['2', '3', '4', '5', '6', '7', '8', '9', '3/2', '4/3'])
+f = rd.choice([Fraction(f), 1/Fraction(f)])
+a = f.numerator
+b = f.denominator
+c = rd.choice([5, 6, 7, 8, 9, 11, 12, 14, 15])
+aa = a * c
+bb = c * b
 ==
+
+inputbox =: MathInput
 
 text ==
-{{f}}
+On considère la fraction $! \displaystyle A= \frac{ {{aa}} }{ {{bb}} }. !$ <br class="spacer" /> 
+Simplifier cette fraction en la mettant sous la forme d'un entier ou d'une fraction irréductible.<br class="spacer" />
+$! A= !$ 
+{{a}} et {{b}}
 ==
 
-
 form ==
+{{inputbox|component}} 
+==
+
+evaluator ==
+if inputbox.value == a/b:
+    score = 100
+else:
+    score = 0
 ==
 
