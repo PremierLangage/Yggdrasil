20554301d99e77a390c3d130b0e42e23b811eda5
Essox Lucius <Elucius@aol.fr>
2020-07-01 13:23:05 +0200

    elucius modified Yggdrasil/Mathematics/tests/testWIMS.pl

---
+++ Mathematics/tests/testWIMS.pl
@@ -0,0 +1,16 @@
+extends = /model/mathinput.pl
+
+title = Simplification d'une fraction 1
+
+before ==
+f = rd.random.choice(['2','3','4','5','6','7','8','9'])
+==
+
+text ==
+{{f}}
+==
+
+
+form ==
+==
+
