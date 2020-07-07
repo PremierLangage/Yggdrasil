1acfe6ee29f3c4cf7a4a7686ed0e8963a3b13641
Essox Lucius <Elucius@aol.fr>
2020-07-07 15:06:45 +0200

    elucius modified Yggdrasil/Mathematics/tests/testWIMS.pl

--- Mathematics/tests/testWIMS.pl
+++ Mathematics/tests/testWIMS.pl
@@ -1,29 +1,25 @@
 extends = /model/mathinput.pl
 
+
 title = Un pré
 
 author = Sophie Lemaire
 
-before ==
-L = 10 * rd.randint(1,10)
-l = 10 * rd.randint(1,10)
-per = 2 * (L + l)
-==
 
-text ==
-Donner le périmètre d'un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m.
+
+before==
+L = 10*rd.randint(1,10)
+l = 10*rd.randint(1,10)
+per = 2*(L+l)
+
+==
+text==
+ Donner le périmètre d'un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m.
 ==
 
 input =: Input
 input.type = number
-
-form ==
-{{input|component}}
-==
-
-evaluator ==
-if input.value == per:
-    score = 100
-else:
-    score = 0
+form==
+ périmètre ( en m)
+{{input | component}}
 ==
