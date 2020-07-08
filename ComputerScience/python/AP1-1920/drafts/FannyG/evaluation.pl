f0ce4768a1a2e52d497f50a95d229984c4417807
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-08 18:49:54 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -74,6 +74,7 @@ form==
 <input type="radio" id="form_q1" name="f_evaluation" value="tres_bien">
 <label for="tres_bien">Très bien</label><br/>
 
+<br/>
 <p>Explication et commentaire général :</p>
 <textarea id="form_pouet" name="Explication" cols=100% rows="5">
 </textarea>
