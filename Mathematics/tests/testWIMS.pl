43c4ab858850a9b29d37fd591691975ca618cf96
Essox Lucius <Elucius@aol.fr>
2020-07-08 16:06:32 +0200

    elucius modified Yggdrasil/Mathematics/tests/testWIMS.pl

--- Mathematics/tests/testWIMS.pl
+++ Mathematics/tests/testWIMS.pl
@@ -1,36 +1,33 @@
-extends = /model/mathinput.pl
 
-title = Simplification d'une fraction 1
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+title = Un pré
+author = Sophie Lemaire
+input =: Input
+input.type = number
 
-before ==
-from fractions import Fraction
-f = rd.choice(['2', '3', '4', '5', '6', '7', '8', '9', '3/2', '4/3'])
-f = rd.choice([Fraction(f), 1/Fraction(f)])
-a = f.numerator
-b = f.denominator
-c = rd.choice([5, 6, 7, 8, 9, 11, 12, 14, 15])
-aa = a * c
-bb = c * b
+before==
+L = 10*rd.randint(1,10)
+l = 10*rd.randint(1,10)
+per = 2*(L+l)
+
 ==
 
-inputbox =: MathInput
-
-text ==
-On considère la fraction $! \displaystyle A= \frac{ {{aa}} }{ {{bb}} }. !$ <br class="spacer" /> 
-Simplifier cette fraction en la mettant sous la forme d'un entier ou d'une fraction irréductible.<br class="spacer" />
-$! A= !$ 
-{{a}} et {{b}}
+text==
+ Donner le périmètre d'un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m.
 ==
 
-form ==
-{{inputbox|component}} 
+form==
+ périmètre ( en m)
+{{input|component}}
+
 ==
 
-evaluator ==
-if inputbox.value == a/b:
-    score = 100
+evaluator==
+if input.value == per:
+	score = 100
 else:
-    score = 0
-==
+	score = 0
 
+==
 
