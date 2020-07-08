949ca5c55571cece26a70c09ec50224ce4ab631a
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-08 15:18:26 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl

---
+++ ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
@@ -0,0 +1,5 @@
+
+# (2 copies) 1 critere -> copie A ou B la meilleure
+# -compare -> permet d'évaluer deux réponses (textuelle ou autre) 
+#     paramêtres: les réponses biensur, mais aussi les critère d'evaluation: balise criteria --> retourne le gagnant (0 ou 1) et la valeur pour chaque critère et un commentaire (ou un commentaire par critère ?)
+
