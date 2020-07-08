e2c3f62f5cd001d46c40bd0b3bd941d8581ea0f8
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-08 17:26:37 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -3,8 +3,8 @@
 
 
 # permet de faire évaluer une réponse (textuelle ou autre) d'un élève par qqun d'autre par le biais d'une grille critériée 
-#     -paramètres : 
-#           -la réponse de l'élève : balise data
-#           -les critère d'evaluation: balise criteria
-#     -retourne : un dico avec la valeur pour chaque critère et un commentaire.
+# -paramètres : 
+#     -la réponse de l'élève : balise data
+#     -les critère d'evaluation: balise criteria
+# -retourne : un dico avec la valeur pour chaque critère et un commentaire.
 
