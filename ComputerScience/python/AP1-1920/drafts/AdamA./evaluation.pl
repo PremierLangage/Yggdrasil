7f74cad2435edd911fac478cc9a7823bd83518cb
ADAM ADEDJOUMON <aadedjou@etud.u-pem.fr>
2020-07-09 11:43:55 +0200

    aadedjoumon modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/AdamA./evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/AdamA./evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/AdamA./evaluation.pl
@@ -56,7 +56,8 @@ Enoncé de la question :
 <br/>
 Réponse de l'élève :  
 {{data.answer}}  
-a {{criteria[0]["description"]}} a  
+c {{criteria.0.description}} c  
+a {{criteria['0']["descrption"]}} a  
 a {{criteria.0}} a
 ==
 
