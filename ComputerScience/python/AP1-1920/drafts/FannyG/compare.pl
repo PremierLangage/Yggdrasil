6b581e6c9ca36ef583cd9eb9e3a183492faab684
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-09 18:01:28 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
@@ -48,8 +48,6 @@ criteria = {0: {"description": "Age donné", "levels": [
                         ]
                 }
             }
-test = {0: "0", 1: "1", 2: "2"}
-#criteria = [[1,2],[3,4]]
 ==
 
 
