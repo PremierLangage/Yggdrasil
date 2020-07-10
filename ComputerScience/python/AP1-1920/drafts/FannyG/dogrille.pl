10362318668e01012cf70fab2bdc939b185f468d
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-10 10:51:09 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/dogrille.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/dogrille.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/dogrille.pl
@@ -1,2 +1,86 @@
 
 # une éval + une grille = resultat d'une grille -> un exopourprof qui permet d'afficher l'evaluation pour un élève. 
+
+
+
+# author=Fanny Garnevault
+
+@ /utils/sandboxio.py
+@ /grader/evaluator.py [grader.py]
+@ /builder/before.py [builder.py]
+
+general_feedback = " Merci pour votre réponse." 
+
+
+before==#|python|
+# paramètre data contenant la question et la réponse de l'élève
+data = {"question": "Quel âge avez-vous ?", "answer": "J'ai 20 ans."}
+
+# paramètre criteria contenant la grille critériée
+criteria = dict()
+criteria = {0: {"description": "Age donné", "levels": [
+                        {"description": "Inférieur à 20 ans", "points": 0}, 
+                        {"description": "20 ans", "points": 1},
+                        {"description": "Supérieur à 20 ans", "points": 2}
+                        ]
+                },
+            1: {"description": "Orthographe", "levels": [
+                        {"description": "Plusieurs fautes", "points": 0}, 
+                        {"description": "1 faute", "points": 1},
+                        {"description": "Aucune faute", "points": 2}
+                        ]
+                },
+            2: {"description": "Syntaxe", "levels": [
+                        {"description": "La phrase ne possède ni une majuscule, ni un point.", "points": 0}, 
+                        {"description": "La phrase possède soit une majuscule, soit un point.", "points": 1},
+                        {"description": "La phrase possède une majuscule et un point.", "points": 2}
+                        ]
+                }
+            }
+==
+
+
+title = Evaluation question
+
+
+text  ==
+Enoncé de la question :  
+<p style="color: rgb(50, 100, 250);">{{data.question}}<p>
+<br/>
+Réponse de l'élève :  
+<p style="color: rgb(50, 100, 250);">{{data.answer}}<p>
+==
+
+
+form==
+{% for id in criteria.keys() %}
+    <span style="padding-left:30px;">
+            <b>{{criteria[id].description}}</b>
+    </span>
+    <br/>
+    {% for niv in criteria[id].levels %}
+        <input type="radio" id="form_{{id}}" name="f_evaluation{{id}}" value="{{niv.description}}">
+        <label for="{{niv.description}}">{{niv.description}}</label><br/>
+    {% endfor %}
+    <br/>
+{% endfor %}
+
+<br/>
+<p>Explication et commentaire général :</p>
+<textarea id="form_commentaire" name="Commentaire" cols=100% rows="5">
+</textarea>
+==
+
+
+
+evaluator==#|python|
+# response est un dict : ex : {1: 'tres_bien', 'commentaire': 't'}
+# le dict de retour reste response, les critères ayant des id, 
+# feedback = "J'ai bien lu votre formulaire :<br/>-" + response['0'] + "<br/>-" + response['1'] + "<br/>-" + response['2'] + "<br/>-" + response['commentaire']
+note = 100
+# general_feedback
+grade = (note, general_feedback)
+==
+
+
+
