c20a1cd64ac9443fe6b2d09bdfc9ecb567c4fb32
Essox Lucius <Elucius@aol.fr>
2020-07-10 14:34:18 +0200

    elucius modified Yggdrasil/Mathematics/tests/testWIMS.pl

--- Mathematics/tests/testWIMS.pl
+++ Mathematics/tests/testWIMS.pl
@@ -1,31 +1,38 @@
 extends = /model/mathinput.pl
-title = Un pré
-author = Sophie Lemaire
-input =: Input
-input.type = number
+title = Un pré bis
+input0=: Input
+input0.type = text
+input1=: Input
+input1.type = text
 
 before==
-L = 10*rd.randint(1,10)
-l = 10*rd.randint(1,10)
-per = 2*(L+l)
+L =  10 * rd.randint( 1 , 10)
+l =  10 * rd.randint( 1 , 10)
+per =  2 * ( L + l)
+super =  L * l
 
 ==
 
 text==
- Donner le périmètre d'un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m.
+Un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m a une bordure de other cmd et une superficie de other cmd .
 ==
 
 form==
- périmètre ( en m)
-{{input|component}}
+périmètre
+{{input0|component}}
+superficie
+{{input1|component}}
 
 ==
 
 evaluator==
-if input.value == per:
-	score = 100
+if input0.value ==  "per m":
+        score = 100
 else:
-	score = 0
+        score = 0
+if input1.value ==  "super m ^ 2":
+        score = 100
+else:
+        score = 0
 
 ==
-
