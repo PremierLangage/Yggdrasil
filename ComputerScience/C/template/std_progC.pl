e21ad5a98e9e4a02fb2496b5dde161d249f4ff43
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-07-12 17:00:12 +0200

    nborie modified Yggdrasil/ComputerScience/C/template/std_progC.pl

--- ComputerScience/C/template/std_progC.pl
+++ ComputerScience/C/template/std_progC.pl
@@ -248,5 +248,5 @@ feedback += feedback_checks
 grade=((grade_compil*nb_good) // (nb_good + nb_bad), feedback)
 ==
 
-tests.test1.editor.code='int carre(int p){ return p*p; }'
+tests.test1.editor.code = 'int carre(int p){ return p*p; }'
 tests.test1.grade = 100
