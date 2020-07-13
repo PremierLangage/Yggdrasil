f1c8ecdc963b07e1ec66bc8a7355aafd5106f378
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-13 17:27:19 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
@@ -113,8 +113,8 @@ evaluator==#|python|
 try:
     feedback = "Réponses : "
     for num in criteria.keys():
-        feedback += " " + response[num]
-    feedback += " " + response['best_copy'] + " " + response['commentaire']
+        feedback += response[num] + ", "
+    feedback += response['best_copy'] + ", " + response['commentaire']
     note = 100
 except:
     feedback = "Répondez à toutes les questions"
