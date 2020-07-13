05e639132bbbc0c93329f57ad94108ae3fbcc70a
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-07-13 19:42:24 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/examen_ete_2020/short_long_args.pl

--- ComputerScience/C/exercices/examen_ete_2020/short_long_args.pl
+++ ComputerScience/C/exercices/examen_ete_2020/short_long_args.pl
@@ -73,7 +73,7 @@ code_after==
 
 checks_args_stdin==#|python|
 [["Simple test", ["arg1", "argum2", "argument3", "a4"], ""],
- ["Simple test", ["unique"], ""],
+ ["Un seul mot et argument", ["unique"], ""],
  ["Test aléatoire", ["".join([chr(randint(97,122)) for j in range(randint(1, 20))] ) for i in range(2, 10)], ""],
  ["Test aléatoire", ["".join([chr(randint(97,122)) for j in range(randint(1, 20))] ) for i in range(2, 10)], ""],
  ["Test aléatoire", ["".join([chr(randint(97,122)) for j in range(randint(1, 20))] ) for i in range(2, 10)], ""],
