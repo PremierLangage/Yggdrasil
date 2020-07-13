6910ecfd1c61511e98fb7724088c9164a1a91f5b
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-13 17:39:55 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -27,7 +27,7 @@ import json
 #data = json.dumps(data)
 
 # paramètre comment_by_criteria : option pour avoir un commentaire par critère
-comment_by_criteria = False
+comment_by_criteria = True
 
 # paramètre data contenant la question et la réponse de l'élève
 data = {"question": "Quel âge avez-vous ?", "answer": "J'ai 20 ans."}
