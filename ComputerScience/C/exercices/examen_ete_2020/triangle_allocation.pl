4af15a0a6f57aa8f29104ae444bed1dc84141355
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-07-14 12:39:18 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/examen_ete_2020/triangle_allocation.pl

--- ComputerScience/C/exercices/examen_ete_2020/triangle_allocation.pl
+++ ComputerScience/C/exercices/examen_ete_2020/triangle_allocation.pl
@@ -17,87 +17,69 @@
 extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
-title=Parité binaire dans un entier
-tag=bitwise|function
+title=Allocation triangulaire
+tag=allocation|function
 
 text==
-Écrire une fonction C prenant en argument un entier **n** qui retourne **1**
-si l'entier **n** contient un nombre pair de bit à **1** dans son écriture 
-binaire. Votre fonction devra retourner **0** sinon, c'est à dire quand 
-**n** contient un nombre impair de bit à **1**.
+Écrire une fonction C **triangle** qui prend en argument un entier (qui sera 
+toujours strictement positif). Votre fonction devra allouer un tableau à 
+deux dimensions de forme triangulaire. Voici un exemple pour la taille 5 :
+
+    X               ( première ligne [0] : 1 élément )
+    X  X            (  seconde ligne [1] : 2 éléments)
+    X  X  X         (troisième ligne [2] : 3 éléments)
+    X  7  X  X      (   l'elément [3][1] vaut ici 7  )
+    X  X  X  X  X   (cinquième ligne [4] : 5 éléments)
+
+Votre fonction devra donc retourner un tableau doublement indéxé [i][j] 
+(i pour les lignes et j pour les colonnes). La première ligne peut contenir
+1 élément, la seconde ligne 2 éléments, etc... En taille **n**, le tableau
+retourné et fraichement alloué contiendra **n** lignes.
 ==
 
 editor.code==#|c|
-int bin_parity(int n){
+#include ...
+
+int** triangle(int n){
   /* Votre code ici... */
 }
 ==
 
 solution==#|c|
-int bin_parity(int n){
-  if (n == 0)
-    return 1;
-  if ((n%2))
-    return 1 - bin_parity(((unsigned int)n) >> 1);
-  return bin_parity(((unsigned int)n) >> 1);
+#include <stdlib.h>
+
+int** triangle(int n){
+  int** tab = (int**)malloc(sizeof(int*) * n);
+  int i;
+
+  for(i=0 ; i<n ; i++){
+    tab[i] = (int*)malloc(sizeof(int) * (i+1));
+  }
+  return tab; 
 }
 ==
 
 code_before==#|c|
 #include <stdio.h>
-#include <stdlib.h>
 ==
 
 code_after==#|c|
 
-void print_bin(unsigned char u){
-  int i;
-
-  for(i=7 ; i>=0 ; i--)
-    printf("%c", (u & (1<<i))?'1':'0');
-}
-
-void print_area(void* data, size_t size){
-  int i;
-  unsigned char* s = (unsigned char*)data;
-
-  for(i=0 ; i<size ; i++){
-    print_bin(s[i]);
-  }
-  putchar('\n');
-}
-
-
 int main(int argc, char* argv[]){
-  unsigned char s[100];
-  int i;
 
-  for(i=1 ; i<argc ; i++){
-    *(s+i-1) = atoi(argv[i]);
-  }
-
-  
-  print_area((void*)s, argc-1);
-  if (bin_parity( *((int*)s) ))
-    printf("Nombre pair de bit à 1\n");
-  else
-    printf("Nombre impair de bit à 1\n");
 
   return 0;
 }
-
 ==
 
 checks_args_stdin==#|python|
-[["Exécution simple", ["1", "1", "1", "0"], ""],
- ["Pas de bit", ["0", "0", "0", "0"], ""],
- ["32 bits", ["255", "255", "255", "255"], ""],
- ["31 bits", ["255", "255", "255", "254"], ""],
- ["Alternances de bit", ["85", "85", "85", "85"], ""],
- ["Test aléatoire 1", [str(randint(0,255)) for i in range(4)], ""],
- ["Test aléatoire 2", [str(randint(0,255)) for i in range(4)], ""],
- ["Test aléatoire 3", [str(randint(0,12)) for i in range(4)], ""],
- ["Test aléatoire 4", [str(randint(0,255)) for i in range(4)], ""],
- ["Test aléatoire 5", [str(randint(0,255)) for i in range(4)], ""]]
+[["Exécution simple", ["5"], ""],
+ ["Taille minimale", ["1"], ""],
+ ["Test aléatoire 1", [str(randint(2, 11))], ""],
+ ["Test aléatoire 2", [str(randint(2, 11))], ""],
+ ["Test aléatoire 3", [str(randint(2, 11))], ""],
+ ["Test aléatoire 4", [str(randint(2, 11))], ""],
+ ["Test aléatoire 5", [str(randint(2, 11))], ""],
+ ["Test aléatoire 6", [str(randint(2, 11))], ""]]
 ==
 
