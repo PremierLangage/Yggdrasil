8af7a9c0765d302a45c8aad088fa9e72197398e3
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-15 11:57:44 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -1,7 +1,3 @@
-
-# déclarer une var data (ou decorator) (dictionnaire de données pour les tests = réponses élèves)
-
-
 # permet de faire évaluer une réponse (textuelle ou autre) d'un élève par qqun d'autre par le biais d'une grille critériée 
 # -paramètres : 
 #     -la réponse de l'élève : balise data
