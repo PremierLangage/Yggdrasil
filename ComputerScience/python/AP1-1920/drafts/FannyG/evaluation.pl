03b2ec218871022c26f285e980646338555c83b2
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-15 15:25:36 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -133,7 +133,7 @@ if not error:
 if note_student <= 0:
     response['note'] = 0
 else:
-    response['note'] = ceil((note_student*100)/17)
+    response['note'] = ceil((note_student*100)/note_max)
 
 if error:
     grade = (0, "Répondez à toutes les questions")
