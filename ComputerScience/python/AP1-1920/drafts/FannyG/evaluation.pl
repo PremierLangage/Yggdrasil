66e492a12da8ef0d51cbd1781df2c25adb0c29b4
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-15 11:51:19 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -9,10 +9,7 @@
 #     -option pour avoir un commentaire par critère : comment_by_criteria, type -> boolean, par défault false
 # -retourne : un dico avec la valeur pour chaque critère et un commentaire.
 
-# qrevuz : comment calculer les points ? est ce que ça se fait dans cet exo ou alors dans l'atelier,
-# quand on récupère toutes les exos évalués ?
-
-#qrevuz : comment_by_criteria est ce qu'on peut lui donner une valeur par défault
+# qrevuz : comment_by_criteria est ce qu'on peut lui donner une valeur par défault
 
 # author=Fanny Garnevault
 
