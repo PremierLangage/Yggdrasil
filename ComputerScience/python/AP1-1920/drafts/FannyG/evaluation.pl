b376c0187227d7471caabba059db7b66abd4641d
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-15 16:31:01 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -59,6 +59,7 @@ title = Evaluation question
 
 text  ==
 <b>Remplissez tous les champs de l'évaluation</b>
+<br/>
 Enoncé de la question :  
 <p style="color: rgb(50, 100, 250);">{{data.question}}<p>
 <br/>
