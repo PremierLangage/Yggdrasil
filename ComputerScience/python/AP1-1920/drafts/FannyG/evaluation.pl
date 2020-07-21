030b8a3aed9c39ff5729a69e1425bda9924ce765
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-21 16:06:07 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/evaluation.pl
@@ -17,7 +17,7 @@
 
 
 # balise comment_by_criteria : option pour avoir un commentaire par critère
-comment_by_criteria = 0
+comment_by_criteria = 1
 
 before==#|python|
 # paramètre data contenant la question et la réponse de l'élève
