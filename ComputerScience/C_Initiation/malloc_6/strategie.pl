689a15d96008c3285f17122139ef82c848bd2b9f
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-07-21 13:59:57 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/malloc_6/strategie.pl

---
+++ ComputerScience/C_Initiation/malloc_6/strategie.pl
@@ -0,0 +1,95 @@
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+title==
+Stratégie d'allocation
+==
+
+author=Nicolas Borie
+
+text==
+Tentez d'associer, pour chaque type de donnéee C, comment ces dernières doivent être allouées.
+==
+
+# STEP 1
+match =: MatchList
+
+# remove the comment of the next line to display the properties of the component
+# in realtime inside the form.
+
+# match.debug % true
+
+# STEP 2
+before== #|python|
+import random
+match.nodes = []
+expected = []
+
+match.nodes.append({"id": "s1", "content": "Une chaine (char[64]) de moins de 63 caractère", "source": True})
+match.nodes.append({"id": "t1", "content": "aucun malloc", "target": True})
+expected.append({ "source": "s1", "target": "t1" })
+
+match.nodes.append({"id": "s2", "content": "Une zone utilisée comme tableau de floatant", "source": True})
+match.nodes.append({"id": "t2", "content": "un malloc", "target": True})
+expected.append({ "source": "s2", "target": "t2" })
+
+match.nodes.append({"id": "s3", "content": "trois champs nom(char\*), prénom(char\*) et age(int)", "source": True})
+match.nodes.append({"id": "t3", "content": "deux mallocs", "target": True})
+expected.append({ "source": "s3", "target": "t3" })
+
+match.nodes.append({"id": "s5", "content": "Une matrice rectangulaire `m` par `n` (deux entiers)", "source": True})
+match.nodes.append({"id": "t5", "content": "un malloc puis malloc dans un for", "target": True})
+expected.append({ "source": "s5", "target": "t5" })
+
+random.shuffle(match.nodes)
+==
+
+# STEP 3
+form==
+{{ match|component }}
+==
+
+# STEP 4
+evaluator== #|python|
+
+def in_links(conn, links):
+    for e in links:
+        if e['source'] == conn["source"] and  e['target'] == conn["target"]:
+            return True
+    return False
+
+error = 0
+for e in expected:
+    if not in_links(e, match.links):
+        error = error + 1
+
+for e in match.links:
+    e['css'] = 'error-state anim-fade'
+    if in_links(e, expected):
+        e['css'] = 'success-state  anim-flip'
+
+if error == 0:
+    feedback = '<span class="success-state">Bravo, ça devrait en effet bien fonctionner ainsi!</span>'
+    grade = (100, feedback)
+else:
+    feedback = '<span class="error-state">Tout n\'est pas correct, il y a %d mauvaises associations.</span>' % error
+    grade = (0, feedback)
+==
+
+
+
+extracss==
+<style>
+.exercise__instructions, .exercise__feedback {
+    margin-bottom: 16px;
+}
+
+.match-list-component svg path {
+    stroke-width: 4px;
+}
+</style>
+==
+
+
