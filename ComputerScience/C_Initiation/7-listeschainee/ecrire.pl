524832e318a0a1d20c018968fe0172cdfafb5f7e
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-07-22 11:17:24 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/7-listeschainee/ecrire.pl

---
+++ ComputerScience/C_Initiation/7-listeschainee/ecrire.pl
@@ -0,0 +1,78 @@
+
+
+
+title= affiche liste
+tag=affichage d'une liste simplement chainée
+
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+
+text==  
+
+Ecrire une fonction `affiche `
+qui affiche, sur une ligne, les éléments d'une liste chainée passée en parametre.  
+On utilisera les types:  
+typedef struct cel{
+int val;
+struct cel* suivant;
+}Cellule;
+typedef Cellulle* Liste  
+
+==
+
+editor.code==
+....affiche(...) {
+...
+  /* votre code ici... */
+}
+
+==
+
+
+codebefore==
+
+#include <stdlib.h>
+#include <stdio.h>
+
+==
+
+solution==
+
+
+
+int main(void) {
+	int n ;
+    int somme=0;
+    int nb=0;
+    float moy;
+    printf("Entrer une suite d'entiers positifs:\n");
+    scanf("%d",&n);
+    if(n<0) 
+        printf("Suite vide.\n" );
+    else{
+        while (n>=0){
+             somme+=n;
+             nb+=1;
+             scanf("%d",&n);
+           }
+        moy=somme;
+        moy/=nb;
+	    printf("somme=%d moyenne=%f diviseurs\n",somme,moy);
+        }
+	return 0;
+}
+==
+
+
+tests==
+[ ["Basique", "", "10 0 -1"],
+  ["Vide", "", "-1"],
+  ["Grand", "", "100 1 2 3 4 5 6 7 8 9 10 -2"],
+  ["Aléatoire", "", " ".join([str(random.randint(-0,100)) for i in range(random.randint(5,10))])+" -1"]
+  ]
+==
+
+
+
+
+
