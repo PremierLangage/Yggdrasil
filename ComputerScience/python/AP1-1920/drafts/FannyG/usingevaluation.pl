1ed57bcbbc52e32c5439889b281a5d434feb551b
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-22 18:37:42 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/usingevaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/usingevaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/usingevaluation.pl
@@ -4,6 +4,9 @@ extends=evaluation.pl
 data.question= Quel âge avez-vous ?
 data.answer= J'ai 20 ans.
 
+#False pour ne pas avoir des commentaires pour chaque critère
+#sinon mettre autre chose (peut importe le mot par ex : Fals ou True) 
 comment_by_criteria = False
+
 
 
