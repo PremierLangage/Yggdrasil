bee041e8568e9ddd72f1cbd5045dd8ef22a65a8a
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-07-22 18:54:06 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
@@ -19,19 +19,19 @@ before==#|python|
 data = {"question": "Quel âge avez-vous ?", "answer1": "J'ai 20 ans.", "answer2": "g 20 ten"}
 
 # paramètre criteria contenant la grille critériée
-criteria = [{"description": "Age donné", "levels": [
+criteria = [{"id": 0, "description": "Age donné", "levels": [
                         {"description": "Inférieur à 20 ans", "points": 0}, 
                         {"description": "20 ans", "points": 1},
                         {"description": "Supérieur à 20 ans", "points": 2}
                         ]
                 },
-            {"description": "Orthographe", "levels": [
+            {"id": 1, "description": "Orthographe", "levels": [
                         {"description": "Plusieurs fautes", "points": 0}, 
                         {"description": "1 faute", "points": 1},
                         {"description": "Aucune faute", "points": 2}
                         ]
                 },
-            {"description": "Syntaxe", "levels": [
+            {"id": 2, "description": "Syntaxe", "levels": [
                         {"description": "La phrase ne possède ni une majuscule, ni un point.", "points": 0}, 
                         {"description": "La phrase possède soit une majuscule, soit un point.", "points": 1},
                         {"description": "La phrase possède une majuscule et un point.", "points": 2}
