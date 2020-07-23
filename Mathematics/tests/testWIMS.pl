591561de1f4c5ceb217067a63601fb48f035e1c9
Essox Lucius <Elucius@aol.fr>
2020-07-23 16:55:30 +0200

    elucius modified Yggdrasil/Mathematics/tests/testWIMS.pl

--- Mathematics/tests/testWIMS.pl
+++ Mathematics/tests/testWIMS.pl
@@ -1,49 +1,29 @@
-@ /utils/sandboxio.py
-@ /builder/before.py [builder.py]
-@ /grader/evaluator.py [grader.py]
-title = Un pré (units)
-input0=: Input
-input0.type = text
-input1=: Input
-input1.type = text
+extends = /model/mathinput.pl
+title = Un pré
+author = Sophie Lemaire
+input =: Input
+input.type = number
 
-a = 1
-
-before==
-import random as rd
-L = 10 * rd.randint(1 , 10)
-l = 10 * rd.randint(1 , 10)
+before ==
+L = 10 * rd.randint(1,10)
+l = 10 * rd.randint(1,10)
 per = 2 * (L + l)
-super = L * l
-
 ==
 
-text==
-Donner le périmètre et la superficie d'un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m
-{{a}}
-==
-a = 2 
-form==
-périmètre
-{{input0|component}}
-superficie
-{{input1|component}}
-
+text ==
+Donner le périmètre d'un pré rectangulaire de longueur {{L}} m et de largeur {{l}} m.
 ==
 
-evaluator==
 
-test = get_numeric_int(m^2)
-if input0.value == str(per) + " m":
-	score = 100
+
+form ==
+périmètre (en m)
+{{input|component}}
+==
+
+evaluator ==
+if input.value == per:
+    score = 100
 else:
-	score = 0
-if input1.value == "super m ^ 2":
-	score = 100
-else:
-	score = 0
-
+    score = 0
 ==
-
-
-
