3c6354ca57e151b1d53eebf202cc04febe324eba
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-07-27 00:13:26 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/7-listeschainee/ajout_ordonne.pl

--- ComputerScience/C_Initiation/7-listeschainee/ajout_ordonne.pl
+++ ComputerScience/C_Initiation/7-listeschainee/ajout_ordonne.pl
@@ -2,48 +2,49 @@
 
 
 
-
-title=extrait dans liste
-tag=extrait liste simplement chainée
+title= ajout dans une liste triée
+tag=ajout d'une liste simplement chainée
 
 extends=/ComputerScience/C/template/stdsandboxC.pl
 
 
 text==  
 
-Ecrire une fonction `Cellulu * extraitsuivant(int precede)l `, qui extrait, si elle existe la cellule situé après celle contenant x.La fonction renvoie l'adresse de la cellule extraite ou NULL.
-
+Ecrire une fonction `void ajout_trieListe(*lst,int x) `
+qui ajoute un élément x à une liste chainée triée (ordre  croissant) 
+passée en parametre.  
+La liste devra rester trie
 On utilisera les types:  
 typedef struct cel{  
 int val;  
 struct cel* suivant;  
 }Cellule;  
-typedef Cellule* Liste;  
+typedef Cellule* Liste; 
 
+ainsi que la fonction `alloue_Cellule` précedemment écrite.
 ==
 
 editor.code==
-int extrait_suivant(...) {
+....ajout_trie(...) {
 ...
   /* votre code ici... */
 }
 
 ==
 solution==
-Liste extrait_suivant(Liste lst,int x){
-  Liste tmp=NULL;
-
-  if(lst!=NULL){
-    while(lst->suivant!=NULL &&lst->suivant->val!=x)
-        lst=lst->suivant;
-    if(lst->suivant!=NULL){
-        tmp=lst->suivant;
-        lst->suivant=tmp->suivant;
-        tmp->suivant=NULL;
-    }
-  }                           
-  return tmp;
-}
+void ajout_trie(Liste *lst,int x){
+Liste tmp=NULL, index=*lst;
+if(*lst==NULL){
+    *lst=alloue_Cellule(x);
+    return;
+if(x<(*lst)->val){
+    tmp=alloue_Cellule(x);
+    tmp->suivant=(*lst);
+    *lst=tmp;
+    return;
+index=*lst;
+while(index->suivant!=NULL && index->suivant->val<x)
+    index=index->suivant
 ==
 
 codebefore==
@@ -55,6 +56,10 @@ int val;
 struct cel* suivant;  
 }Cellule;  
 typedef Cellule* Liste  ;
+
+==
+
+codeafter==
  Cellule* alloue_Cellule(int x){
 Liste tmp=NULL;
 
@@ -77,29 +82,12 @@ void lire(Liste *lst){
             tmp=tmp->suivant;
             }
     }
-   }
-
-
-
-==
-
-codeafter==
-
-void affiche(Liste lst){
-
-while(lst !=NULL){
-    printf("%d ",lst->val);
-    lst=lst->suivant;
     }
-printf("\n");
 
-}
 
 int main(void) {
-
 	Liste l=NULL;
     lire(&l);
-
     affiche(l);
 	return 0;
 }
@@ -110,7 +98,7 @@ tests==
 [ ["Basique", "", "10 0 -1"],
   ["Vide", "", ""],
   
-  ["Aléatoire", "", " ".join([str(random.randint(-0,100)) for i in range(random.randint(5,20)+10)])]
+  ["Aléatoire", "", " ".join([str(random.randint(-0,100)) for i in range(random.randint(5,20))])+" -1"]
   ]
 ==
 
