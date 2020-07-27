a79d256ba637adfffca0060990088992d633838a
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-07-27 17:36:14 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/7-listeschainee/coupe.pl

---
+++ ComputerScience/C_Initiation/7-listeschainee/coupe.pl
@@ -0,0 +1,106 @@
+
+
+
+
+
+title= coupe d'une liste
+tag=Séprer une liste simplement chainée
+
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+
+text==  
+
+Ecrire une fonction `coupe`qui reçoit deux listes et un entier n strictemment positif.
+la deuxième liste est supposé vide.
+elle coupe la première après la $% n^{\mbox{ième}} %$qui renvoie le nombre d'éléments d'une liste chainée passée en parametre.  
+On utilisera le$s types:  
+typedef struct cel{  
+int val;  
+struct cel* suivant;   
+}Cellule;  
+typedef Cellule* Liste  
+
+==
+
+editor.code==
+....nb_cellules(...) {
+...
+  /* votre code ici... */
+}
+
+==
+solution==
+int nb_cellules(Liste lst){
+int compte=0;
+while(lst !=NULL){
+    compte+=1;
+    lst=lst->suivant;
+    }
+return compte;
+}
+
+==
+
+codebefore==
+
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct cel{  
+int val;  
+struct cel* suivant;  
+}Cellule;  
+typedef Cellule* Liste  ;
+
+==
+
+codeafter==
+ Cellule* alloue_Cellule(int x){
+Liste tmp=NULL;
+
+if((tmp=(Liste)malloc(sizeof(Cellule)))!=NULL){
+    tmp->val=x;
+    tmp->suivant=NULL;
+    }
+return tmp;
+}
+void lire(Liste *lst){
+    Liste tmp=NULL;
+    int x;
+    while(scanf("%d",&x)==1){
+        if(*lst==NULL){
+            *lst=alloue_Cellule(x);
+            tmp=*lst;
+            }
+        else{
+            tmp->suivant=alloue_Cellule(x);
+            tmp=tmp->suivant;
+            }
+    }
+    }
+
+
+int main(void) {
+	Liste l=NULL;
+    int lg;
+    lire(&l);
+    lg=nb_cellules(l);
+    printf("la liste contient %d element%s\n",lg,lg<2?"":"s");
+	return 0;
+}
+==
+
+
+tests==
+[ ["Basique", "", "10 0 -1"],
+  ["Vide", "", ""],
+  
+  ["Aléatoire", "", " ".join([str(random.randint(-0,100)) for i in range(random.randint(5,20))])+" -1"]
+  ]
+==
+
+
+
+
+
+
