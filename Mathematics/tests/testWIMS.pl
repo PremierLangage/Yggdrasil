e75c26fce3e0752cea71fa8be20375d17d95c011
Essox Lucius <Elucius@aol.fr>
2020-07-29 16:26:30 +0200

    elucius modified Yggdrasil/Mathematics/tests/testWIMS.pl

--- Mathematics/tests/testWIMS.pl
+++ Mathematics/tests/testWIMS.pl
@@ -1,4 +1,4 @@
-extends = /model/mathinput.pl
+extends = /model/physicsinput.pl
 title = Un pré
 author = Sophie Lemaire
 input0=: MathInput
@@ -21,6 +21,7 @@ périmètre (en m)
 ==
 
 evaluator==
+unit_dict = {'m': meter,'cm': centimeter, 'g': gram, 'kg': kilogram, 's': second, 'A': ampere, 'K': kelvin, 'mol': mole, 'cd': candela, 'N': newton, 'J': joule, 'W': watt, 'Pa': pascal, 'Hz': hertz, 'C': coulomb, 'V': volt, 'Omega': ohm}
 
-score, _, feedback = eval_expr(input0.value, per)
+score, _, feedback = eval_physical(input0.value, per*meter, tol=1e-2, local_dict=unit_dict)
 ==
