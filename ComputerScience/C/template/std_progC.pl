c806a842af44d0e067b96b3a67a011f592677074
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-08-11 12:48:40 +0200

    nborie modified Yggdrasil/ComputerScience/C/template/std_progC.pl

--- ComputerScience/C/template/std_progC.pl
+++ ComputerScience/C/template/std_progC.pl
@@ -288,7 +288,7 @@ if compil_state != 'error':
             feedback_checks += make_hide_block_on_click("details_check"+str(nb_good+nb_bad), test_c[0], stdin_explain + term_tot, "")
             feedback_checks += '</div>'
 
-    grade_checks = min([(nb_good*100) // (nb_good+nb_bab)) , (100 // (2**nb_bad))])
+    grade_checks = min([((nb_good*100) // (nb_good+nb_bab)) , (100 // (2**nb_bad))])
 feedback += '<p style="margin-bottom: 5px; margin-top: 5px;"><b><u>Tests :</u> ' + str(grade_checks) + '%</b> (cliquer sur les tests pour afficher/réduire leurs détails)</p>'
 feedback += feedback_checks
 
