21d73f22328e31dfea69337b5f77d8db66d70972
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-08-11 07:24:30 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/program/hello.pl

--- ComputerScience/C/exercices/program/hello.pl
+++ ComputerScience/C/exercices/program/hello.pl
@@ -28,19 +28,19 @@ Faites votre premier programme C, ce programme devra écrire
 
 sur la sortie standard et retourner à la ligne. 
 Le retour à la ligne est encodé par le caractère '\n' en C.
-Pour faire de l'affichage, on utilise la fonction *printf*
-dont l'existance est déclaré dans les headers de la 
+Pour faire de l'affichage, on utilise la fonction **printf**
+dont l'existance est déclarée dans les headers de la 
 bibliothèque standard. C'est ainsi grace à la commande
-*#include <stdio.h>* que l'on a le droit d'appeler cette
-fonction par la suite.
+**#include <stdio.h>** que l'on a le droit d'appeler la
+fonction **printf** dans la suite du programme.
 
 
 Un programme C contient toujours une et une seule fonction
-dont le nom est *main*. Cette fonction sera le point d'entrée
-du programme. Un programme C fait exactement tout se qui se
-trouve dans sa fonction *main*, ni plus, ni moins. Ici, il
-vous faut juste faire de l'affichage. Un bon appel à la
-fonction *printf* doit suffire.
+dont le nom est **main**. Cette fonction sera le point d'entrée
+du programme lors de l'exécution. Un programme C opère exactement 
+tout se qui se trouve dans sa fonction **main**, ni plus, ni moins. 
+Ici, il vous faut juste faire de l'affichage. Un bon appel à la
+fonction **printf** doit suffire.
 
 ==
 
