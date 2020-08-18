1e232d49bc68a8fde9117898ce83fd07452b5fea
David DOYEN <David.Doyen@u-pem.fr>
2020-08-18 15:45:52 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl

--- Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl
+++ Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl
@@ -17,61 +17,61 @@ items = list(range(10))
 
 A = rand_finiteset(randint(3, 7), items)
 B = rand_finiteset(randint(3, 7), items)
-CC = rand_finiteset(randint(3, 7), items)
+C = rand_finiteset(randint(3, 7), items)
 
 case = randitem(param['cases'])
 sol =[]
 
 if case == 1:
-    expr = r"(A \cap B) \cup CC"
-    sol = Union(Intersection(A, B), CC)
+    expr = r"(A \cap B) \cup C"
+    sol = Union(Intersection(A, B), C)
 elif case == 2:
-    expr=r"(A \cup B) \cap CC"
-    sol = Intersection(Union(A, B), CC)
+    expr=r"(A \cup B) \cap C"
+    sol = Intersection(Union(A, B), C)
 elif case == 3:
-    expr=r"A \cap (B \cup CC)"
-    sol=Intersection(Union(B,CC),A)
+    expr=r"A \cap (B \cup C)"
+    sol=Intersection(Union(B,C),A)
 elif case==4:
-    expr=r"A \cup (B \cap CC)"
-    sol=Union(Intersection(B,CC),A)
+    expr=r"A \cup (B \cap C)"
+    sol=Union(Intersection(B,C),A)
 
 elif case==5:
-    expr=r"(A \cup B) \setminus CC"
-    sol=CC.complement(Union(A,B))
+    expr=r"(A \cup B) \setminus C"
+    sol=C.complement(Union(A,B))
 elif case==6:
-    expr=r"(A \setminus B) \cup CC"
-    sol=Union(CC,B.complement(A))
+    expr=r"(A \setminus B) \cup C"
+    sol=Union(C,B.complement(A))
 elif case==7:
-    expr=r"A \cup (B \setminus CC)"
-    sol=Union(A,CC.complement(B))
+    expr=r"A \cup (B \setminus C)"
+    sol=Union(A,C.complement(B))
 elif case==8:
-    expr=r"A \setminus (B \cup CC)"
-    sol=Union(B,CC).complement(A)
+    expr=r"A \setminus (B \cup C)"
+    sol=Union(B,C).complement(A)
 
 elif case==9:
-    expr=r"(A \cap B) \setminus CC"
-    sol=CC.complement(Intersection(A,B))
+    expr=r"(A \cap B) \setminus C"
+    sol=C.complement(Intersection(A,B))
 elif case==10:
-    expr=r"(A \setminus B) \cap CC"
-    sol=Intersection(CC,B.complement(A))
+    expr=r"(A \setminus B) \cap C"
+    sol=Intersection(C,B.complement(A))
 elif case==11:
-    expr=r"A \cap (B \setminus CC)"
-    sol=Intersection(A,CC.complement(B))
+    expr=r"A \cap (B \setminus C)"
+    sol=Intersection(A,C.complement(B))
 elif case==12:
-    expr=r"A \setminus (B \cap CC)"
-    sol=Intersection(B,CC).complement(A)
+    expr=r"A \setminus (B \cap C)"
+    sol=Intersection(B,C).complement(A)
 
 
 A_tex=latex(A)
 B_tex=latex(B)
-CC_tex=latex(CC)
+C_tex=latex(C)
 sol_tex=latex(sol)
 sol=list(sol)
 ==
 
 text == 
 On considère les ensembles suivants :
-$$ A= \\{ {{A_tex}} \\},\ B= \\{ {{B_tex}} \\},\ CC= \\{ {{CC_tex}} \\}.$$
+$$ A= \\{ {{A_tex}} \\},\ B= \\{ {{B_tex}} \\},\ C= \\{ {{C_tex}} \\}.$$
 Déterminer $! {{expr}} !$.
 ==
 
