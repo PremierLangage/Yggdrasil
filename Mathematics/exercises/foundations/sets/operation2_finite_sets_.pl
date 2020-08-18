40141f697cd342fbdb06ab41f655a4b8d63296d1
David DOYEN <David.Doyen@u-pem.fr>
2020-08-18 16:41:28 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl

--- Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl
+++ Mathematics/exercises/foundations/sets/operation2_finite_sets_.pl
@@ -20,46 +20,45 @@ B = rand_finiteset(randint(3, 7), items)
 C = rand_finiteset(randint(3, 7), items)
 
 case = randitem(param['cases'])
-sol =[]
 
 if case == 1:
     expr = r"(A \cap B) \cup C"
     sol = Union(Intersection(A, B), C)
 elif case == 2:
-    expr=r"(A \cup B) \cap C"
+    expr = r"(A \cup B) \cap C"
     sol = Intersection(Union(A, B), C)
 elif case == 3:
-    expr=r"A \cap (B \cup C)"
-    sol=Intersection(Union(B,C),A)
-elif case==4:
-    expr=r"A \cup (B \cap C)"
-    sol=Union(Intersection(B,C),A)
+    expr = r"A \cap (B \cup C)"
+    sol = Intersection(Union(B, C),A)
+elif case == 4:
+    expr = r"A \cup (B \cap C)"
+    sol = Union(Intersection(B,C),A)
 
-elif case==5:
-    expr=r"(A \cup B) \setminus C"
-    sol=C.complement(Union(A,B))
-elif case==6:
-    expr=r"(A \setminus B) \cup C"
-    sol=Union(C,B.complement(A))
-elif case==7:
-    expr=r"A \cup (B \setminus C)"
-    sol=Union(A,C.complement(B))
-elif case==8:
-    expr=r"A \setminus (B \cup C)"
-    sol=Union(B,C).complement(A)
+elif case == 5:
+    expr = r"(A \cup B) \setminus C"
+    sol = C.complement(Union(A,B))
+elif case == 6:
+    expr = r"(A \setminus B) \cup C"
+    sol = Union(C,B.complement(A))
+elif case == 7:
+    expr = r"A \cup (B \setminus C)"
+    sol = Union(A,C.complement(B))
+elif case == 8:
+    expr = r"A \setminus (B \cup C)"
+    sol = Union(B,C).complement(A)
 
-elif case==9:
-    expr=r"(A \cap B) \setminus C"
-    sol=C.complement(Intersection(A,B))
-elif case==10:
-    expr=r"(A \setminus B) \cap C"
-    sol=Intersection(C,B.complement(A))
-elif case==11:
-    expr=r"A \cap (B \setminus C)"
-    sol=Intersection(A,C.complement(B))
-elif case==12:
-    expr=r"A \setminus (B \cap C)"
-    sol=Intersection(B,C).complement(A)
+elif case == 9:
+    expr = r"(A \cap B) \setminus C"
+    sol = C.complement(Intersection(A,B))
+elif case == 10:
+    expr = r"(A \setminus B) \cap C"
+    sol = Intersection(C,B.complement(A))
+elif case == 11:
+    expr = r"A \cap (B \setminus C)"
+    sol = Intersection(A,C.complement(B))
+elif case == 12:
+    expr = r"A \setminus (B \cap C)"
+    sol = Intersection(B,C).complement(A)
 ==
 
 text == 
@@ -74,5 +73,5 @@ feedback = feedback_message[error]
 ==
 
 solution ==
-La solution est $! { {{sol|latextex}} } !$.
+La solution est $! { {{sol|latex}} } !$.
 ==
