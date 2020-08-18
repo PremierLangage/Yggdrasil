a8702cc425c32a0f16cd6827a89f7d041176780d
David DOYEN <David.Doyen@u-pem.fr>
2020-08-18 15:47:21 +0200

    ddoyen modified Yggdrasil/model/namespace/namespace_math.py

--- model/namespace/namespace_math.py
+++ model/namespace/namespace_math.py
@@ -1,4 +1,5 @@
 from sympy import *
+del C
 import random as rd
 from randsympy import *
 from evalsympy import *
@@ -6,3 +7,4 @@ from latex2sympy import *
 from sympy2latex import *
 
 namespace = globals().copy()
+
