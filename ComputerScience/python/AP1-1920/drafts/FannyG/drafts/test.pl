de81b2d04827d51c416a6036efa6c2346ad99f85
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-19 15:45:28 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -14,25 +14,7 @@ radio.items = [{"id": "1", "content": "ok"},{"id": "2", "content": "ol"},{"id": 
 
 
 evaluator == #|python|
-selectedId = radio.selection
-
-for index, item in enumerate(radio.items):
-    item['css'] = ''
-    choice = choices[index]
-
-    if choice['right']: # the good anwser
-        if choice['id'] == selectedId : # student anwsered correctly 
-            score = 100
-            item['css'] = "success-border"
-        else:
-            item['css'] = 'success-border anim-pulse'
-    else:
-         if choice['id'] == selectedId :
-            item['css'] = "error-border"
-    if choice['id'] == selectedId and choice['feedback']:
-                feedback =  choice["feedback"] + "<br/>"
-
-
+grade = (score, radio.items[radio.selection])
 ==
 
 
