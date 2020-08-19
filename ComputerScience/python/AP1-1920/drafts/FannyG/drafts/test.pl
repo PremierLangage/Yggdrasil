60b63f3f527ca52d7b82ac09ddac2d60e7c6cb9c
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-19 15:26:53 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -4,12 +4,39 @@
 
 extends = /model/basic.pl
 
+radio =: RadioGroup
 
+before ==
+import random as rd
+import csv
 
+with open('pays_europe.csv', newline='') as file:
+    all_rows = list(csv.DictReader(file, delimiter=','))
 
+sample_rows = rd.sample(all_rows, 4)
 
+pays = sample_rows[0]['pays']
+article = sample_rows[0]['article']
 
+radio.setitems([row['capitale'] for row in sample_rows])
+radio.setsol_from_index(0)
+radio.shuffle()
 
+partitif = {"le": "du ", "la": "de la ", "les": "des ", "l": "de l'"}
+du_pays = partitif[article] + pays
+==
+
+text ==
+Quelle est la capitale {{ du_pays }} ?
+==
+
+form ==
+{{ radio|component }}
+=
+
+evaluator ==
+score = radio.eval()
+==
 
 
 
