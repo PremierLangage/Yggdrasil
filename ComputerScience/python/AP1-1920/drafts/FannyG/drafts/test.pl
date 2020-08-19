e2b730ef8926ccc28c48f5801e3bccab49c56e52
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-19 15:28:42 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -7,23 +7,10 @@ extends = /model/basic.pl
 radio =: RadioGroup
 
 before ==
-import random as rd
-import csv
 
-with open('pays_europe.csv', newline='') as file:
-    all_rows = list(csv.DictReader(file, delimiter=','))
-
-sample_rows = rd.sample(all_rows, 4)
-
-pays = sample_rows[0]['pays']
-article = sample_rows[0]['article']
-
-radio.setitems([row['capitale'] for row in sample_rows])
+radio.setitems(['ok','ol','om'])
 radio.setsol_from_index(0)
 radio.shuffle()
-
-partitif = {"le": "du ", "la": "de la ", "les": "des ", "l": "de l'"}
-du_pays = partitif[article] + pays
 ==
 
 text ==
