71cc5c0bf5a232f0d0e9cfc64fd5da4cdb53111f
David DOYEN <David.Doyen@u-pem.fr>
2020-08-19 15:49:51 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl

--- Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl
+++ Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl
@@ -1,17 +1,16 @@
-extends = /Mathematics/template/mathexpr.pl
+# Author : D. Doyen
+# Tags : sets, finite sets
+# 19/8/2020
+
+extends = /model/mathinput.pl
 
 title = Simplification d'une fraction
 
-lang = fr
-
 before ==
-keyboards_JSON['virtualKeyboards']="elementary"
-input1.config = keyboards_JSON
-
-if param['numbers']=="small":
+if param['numbers'] == "small":
     lstf=[[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1],[3,2],[4,3],[5,4],[5,3],[5,2],[6,5],[7,6],[7,5],[7,4],[7,3],[7,2],[8,7],[8,5],[8,3]]
     lstc=[5,6,7,8,9,11,12,14,15]
-if param['numbers']=="large":
+if param['numbers'] == "large":
     lstf=[[3,2],[4,3],[5,4],[5,3],[5,2],[6,5],[7,6],[7,5],[7,4],[7,3],[7,2],[8,7],[8,5],[8,3],[9,8],[9,7],[9,5],[9,4],[9,2]]
     lstc=[16,18,21,22,24,25,27,28,32,33,35]
 f=randitem(lstf)
@@ -19,7 +18,6 @@ rd.shuffle(f)
 c=randitem(lstc)
 a,b=c*f[0],c*f[1]
 sol=Rational(a,b)
-sol_tex=latex(sol)
 ==
 
 text ==
@@ -33,6 +31,7 @@ score,_,feedback=ans_frac(input1.value,sol)
 solution==
 La solution est $% \displaystyle {{sol_tex}} %$.
 ==
+
 
 
 
