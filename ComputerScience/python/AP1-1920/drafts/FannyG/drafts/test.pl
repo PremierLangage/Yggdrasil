986c0ee77e74f68ca6a49e451111cc7a8811ae7e
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-19 15:47:30 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -1,13 +1,14 @@
 
 @ /utils/sandboxio.py
 @ /grader/evaluator.py [grader.py]
+@ /builder/qselectbuild.py [builder.py]
 
 title = Radio group
 text  = Quelle est la capitale ?
 form  = {{ radio|component }}
 radio =: RadioGroup
 
-before == #|python|
+qselectbuild == #|python|
 radio.items = [{"id": "1", "content": "ok"},{"id": "2", "content": "ol"},{"id": "3", "content": "om"}]
 ==
 
