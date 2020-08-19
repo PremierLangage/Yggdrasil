810ab0192973fde401867d67a3ea4f5cb3d1289f
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-19 15:39:11 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -8,6 +8,19 @@ radio =: RadioGroup
 before ==
 choices = []
 radio.items = []
+
+    id = str(uuid.uuid4())
+    choice = {
+        "id": id,
+        "feedback": match.group('feedback') or '',
+        "right": match.group('type') == '='
+    }
+    choices.append(choice)
+    radio.items.append({
+        "id": id,
+        "content": match.group('content')
+    })
+
 radio.setitems(['ok','ol','om'])
 radio.setsol_from_index(0)
 radio.shuffle()
