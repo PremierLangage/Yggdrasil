1e2adce1fca00da29e4c180aaa2671521b02cf50
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-19 16:14:02 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -13,7 +13,30 @@ form  == #|python|
 
 
 before == #|python|
+criteria = {"0": {"description": "Age donné", "levels": [
+                        {"description": "Inférieur à 20 ans", "points": 0}, 
+                        {"description": "20 ans", "points": 1},
+                        {"description": "Supérieur à 20 ans", "points": 2}
+                        ]
+                },
+            "1": {"description": "Orthographe", "levels": [
+                        {"description": "Plusieurs fautes", "points": 0}, 
+                        {"description": "1 faute", "points": 1},
+                        {"description": "Aucune faute", "points": 2}
+                        ]
+                },
+            "2": {"description": "Syntaxe", "levels": [
+                        {"description": "La phrase ne possède ni une majuscule, ni un point.", "points": 0}, 
+                        {"description": "La phrase possède soit une majuscule, soit un point.", "points": 1},
+                        {"description": "La phrase possède une majuscule et un point.", "points": 2}
+                        ]
+                }
+            }
 radio = []
+for Id in criteria.keys() :
+    tmp =: RadioGroup
+    for lvl in criteria[Id]
+    tmp.items = {"id":Id, "content":}
 radio[0].items = [{"id": 1, "content": "ok"},{"id": 2, "content": "ol"},{"id": 3, "content": "om"}]
 radio[1].items = [{"id": 1, "content": "ek"},{"id": 2, "content": "el"},{"id": 3, "content": "em"}]
 ==
