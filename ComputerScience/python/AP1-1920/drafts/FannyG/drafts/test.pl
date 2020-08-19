aa374d858924e3fbea93e513d0a7c76e1aa14bc1
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-19 17:06:06 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -10,14 +10,7 @@ form  ==
 {{radio}}
 {% for i in range(len(radio)) %} 
     {{type(i)}}
-    {{radio[i]}}
-    <br/>
-    <span style="padding-left:30px;font-size:18px;">
-            <b>{{radio[i].description}}</b>
-    </span>
-    <br/>
-    {{ radio[i]|component }}
-    <br/>
+    
 {% endfor %}
 ==
 
