c653b3ff1a04c406ca0781a1308d61f73961ab09
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-08-19 18:41:33 +0200

    mcisse modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/test.pl
@@ -21,43 +21,65 @@ form  == #|html|
 
 
 before == #|python|
-criteria = {"r0": {"description": "Age donné", "levels": [
-                        {"description": "Inférieur à 20 ans", "points": 0}, 
-                        {"description": "20 ans", "points": 1},
-                        {"description": "Supérieur à 20 ans", "points": 2}
-                        ]
-                },
-            "r1": {"description": "Orthographe", "levels": [
-                        {"description": "Plusieurs fautes", "points": 0}, 
-                        {"description": "1 faute", "points": 1},
-                        {"description": "Aucune faute", "points": 2}
-                        ]
-                },
-            "r2": {"description": "Syntaxe", "levels": [
-                        {"description": "La phrase ne possède ni une majuscule, ni un point.", "points": 0}, 
-                        {"description": "La phrase possède soit une majuscule, soit un point.", "points": 1},
-                        {"description": "La phrase possède une majuscule et un point.", "points": 2}
-                        ]
-                }
-            }
+criteria = {
+    "r0": { 
+        "description": "Age donné",
+        "levels": [
+            {"description": "Inférieur à 20 ans", "points": 0}, 
+            {"description": "20 ans", "points": 1},
+            {"description": "Supérieur à 20 ans", "points": 2}
+        ]
+    },
+"r1": {
+        "description": "Orthographe",
+        "levels": [
+            {"description": "Plusieurs fautes", "points": 0}, 
+            {"description": "1 faute", "points": 1},
+            {"description": "Aucune faute", "points": 2}
+        ]
+    },
+"r2": {
+        "description": "Syntaxe",
+        "levels": [
+            {"description": "La phrase ne possède ni une majuscule, ni un point.", "points": 0}, 
+            {"description": "La phrase possède soit une majuscule, soit un point.", "points": 1},
+            {"description": "La phrase possède une majuscule et un point.", "points": 2}
+        ]
+    }
+}
+
 radio = []
 for Id in criteria.keys() :
     # tmp = {"cid": Id, "selector": "c-radio-group"}
+
+    # La classe RadioGroup est auto importée au début du builder donc
+    # pas besoin de faire un from components import RadioGroup 
     tmp = RadioGroup(cid=Id)
+
     # tmp["description"] = criteria[Id]["description"]
     # tmp["items"] = []
+
+    # tmp est l'instance d'une classe donc la notation dictionnaire [""] ne fonctionne pas
     tmp.description = criteria[Id]["description"]
     tmp.items = []
     for i, lvl in enumerate(criteria[Id]["levels"]) :
         # tmp["items"].append({"id": str(i), "content": lvl["description"]})
         tmp.items.append({"id": str(i), "content": lvl["description"]})
     radio.append(tmp)
+    # dans la version des components qui est sur la v0.7.4 les composants
+    # dans les listes ne sont pas synchronisés il faut donc les ajoutés dans globals
+    # c'est expliqué dans la section "create a component inside a builder" du lien suivant
+    # https://pl.u-pem.fr/components/advanced-usage
     globals()[Id] = tmp
-
 ==
 
 
 evaluator == #|python|
+
+# ATTENTION ICI IL FAUT RECUPERER LES COMPOSANTS DANS GLOBALS ET NON DANS LA LISTE radio
+# radio[0] <=> globals()[radio[0]["id"]]
+
+
 #feedback = ""
 #for d in radio.items : 
 #    if d["id"] == radio.selection :
