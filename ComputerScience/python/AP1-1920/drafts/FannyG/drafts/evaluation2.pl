68e5703ba35ef2c44573e8ba68bf01c5c1139901
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-20 12:15:12 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/evaluation2.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/evaluation2.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/evaluation2.pl
@@ -6,18 +6,6 @@
 title = Radio group
 text  = 
 
-form  == #|html|
-{% for rg in radio %} 
-    <br/>
-    <span style="padding-left:30px;font-size:18px;">
-            <b>{{rg.description}}</b>
-    </span>
-    <br/>
-    {{ rg|component }}
-    <br/>
-{% endfor %}
-==
-
 
 before == #|python|
 criteria = {
@@ -56,57 +44,30 @@ for crit in criteria.values():
 
 radio = []
 for Id in criteria.keys() :
-    # tmp = {"cid": Id, "selector": "c-radio-group"}
-
-    # La classe RadioGroup est auto importée au début du builder donc
-    # pas besoin de faire un from components import RadioGroup 
     tmp = RadioGroup(cid=Id)
-
-    # tmp["description"] = criteria[Id]["description"]
-    # tmp["items"] = []
-
-    # tmp est l'instance d'une classe donc la notation dictionnaire [""] ne fonctionne pas
     tmp.description = criteria[Id]["description"]
     tmp.items = []
     for i, lvl in enumerate(criteria[Id]["levels"]) :
-        # tmp["items"].append({"id": str(i), "content": lvl["description"]})
         tmp.items.append({"id": str(i), "content": lvl["description"]})
-    # dans la version des components qui est sur la v0.7.4 les composants
-    # dans les listes ne sont pas synchronisés il faut donc les ajoutés dans globals
-    # c'est expliqué dans la section "create a component inside a builder" du lien suivant
-    # https://pl.u-pem.fr/components/advanced-usage
     globals()[Id] = tmp
-    # CETTE LIGNE EST UN HACK POUR CONTOURNER LE FAIT QUE LES CLASSES NE SONT PAS SERIALISES PAR LA SANDBOX
     radio.append(vars(tmp))
 ==
 
 
+form  == #|html|
+{% for rg in radio %} 
+    <br/>
+    <span style="padding-left:30px;font-size:18px;">
+            <b>{{rg.description}}</b>
+    </span>
+    <br/>
+    {{ rg|component }}
+    <br/>
+{% endfor %}
+==
+
+
 evaluator == #|python|
-
-# ATTENTION ICI IL FAUT RECUPERER LES COMPOSANTS DANS GLOBALS ET NON DANS LA LISTE radio
-# radio[0] <=> globals()[radio[0]["id"]]
-
-
-#feedback = ""
-#for d in radio.items : 
-#    if d["id"] == radio.selection :
-#        feedback = d["content"]
-#        break
-#grade = (100, feedback)
-
-
-#----------------------------------------------------
-
-
-#for d in response:
-#    print(d)
-#    print(response[d])
-#grade = (100,"")
-
-
-#----------------------------------------------------
-
-
 from math import ceil
 
 error = 0
