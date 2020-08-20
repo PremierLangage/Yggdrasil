1abf21034aadddde05faa02be75410c3a08e9b7d
David DOYEN <David.Doyen@u-pem.fr>
2020-08-20 16:47:29 +0200

    ddoyen modified Yggdrasil/utils/sympy/evalsympy.py

--- utils/sympy/evalsympy.py
+++ utils/sympy/evalsympy.py
@@ -644,8 +644,7 @@ def eval_poly(strans, sol, var='x', domain='R', form='', checkratsimp=True, imag
     >>> eval_poly("x^2 + 2x + 1", P, form="factorized")[1]
     'PolyNotFactorized'
     """
-    local_dict.update({imaginary_unit: sp.I})
-    x = sp.Symbol(var)
+    local_dict.update({imaginary_unit: sp.I, x: sp.Symbol(var)})
     try:
         ans = latex2sympy(strans, local_dict)
         return (0, str(srepr(ans)))
