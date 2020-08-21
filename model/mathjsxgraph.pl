08e693d9f0f095ff4fd70da3c8670b5d18092b3a
David DOYEN <David.Doyen@u-pem.fr>
2020-08-21 15:31:30 +0200

    ddoyen modified Yggdrasil/model/mathjsxgraph.pl

---
+++ model/mathjsxgraph.pl
@@ -0,0 +1,21 @@
+extends = /model/math.pl
+
+@ /utils/components/mathinput.py [custommathinput.py]
+
+input =: MathInput
+input.decorator = CustomMathInput
+
+input.virtualKeyboards = elementary
+
+title = 
+
+text = 
+
+form ==
+{{ input|component}}
+==
+
+settings.feedback = rightwrong
+
+settings.maxattempt % 1
+
