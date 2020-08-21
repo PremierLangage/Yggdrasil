42e12aa24c8ca90906c8a381d02709cadda203d2
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-21 15:04:36 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/evaluation2.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/evaluation2.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/evaluation2.pl
@@ -27,25 +27,35 @@ before==#|python|
 data = {"question": "Quel âge avez-vous ?", "answer": "J'ai 20 ans."}
 
 # paramètre criteria contenant la grille critériée
-criteria = {"0": {"description": "Age donné", "levels": [
-                        {"description": "Inférieur à 20 ans", "points": 0}, 
-                        {"description": "20 ans", "points": 1},
-                        {"description": "Supérieur à 20 ans", "points": 2}
-                        ]
-                },
-            "1": {"description": "Orthographe", "levels": [
-                        {"description": "Plusieurs fautes", "points": 0}, 
-                        {"description": "1 faute", "points": 1},
-                        {"description": "Aucune faute", "points": 2}
-                        ]
-                },
-            "2": {"description": "Syntaxe", "levels": [
-                        {"description": "La phrase ne possède ni une majuscule, ni un point.", "points": 0}, 
-                        {"description": "La phrase possède soit une majuscule, soit un point.", "points": 1},
-                        {"description": "La phrase possède une majuscule et un point.", "points": 2}
-                        ]
-                }
-            }
+criteria =[
+    {
+        "id":0,
+        "description":"Age donné",
+        "levels":[
+            {"id":0, "description":"Inférieur à 20 ans", "points":0},
+            {"id":1, "description":"20 ans", "points":1},
+            {"id":2, "description":"Supérieur à 20 ans", "points":2}
+        ]
+   },
+   {
+        "id":1,
+        "description":"Orthographe",
+        "levels":[
+            {"id":0, "description":"Plusieurs fautes", "points":0},
+            {"id":1, "description":"1 faute", "points":1},
+            {"id":2, "description":"Aucune faute", "points":2}
+        ]
+   },
+   {
+        "id":2,
+        "description":"Syntaxe",
+        "levels":[
+            {"id":0, "description":"La phrase ne possède ni une majuscule, ni un point.", "points":0},
+            {"id":1, "description":"La phrase possède soit une majuscule, soit un point.", "points":1},
+            {"id":2, "description":"La phrase possède une majuscule et un point.", "points":2}
+      ] 
+   }
+]
 
 note_max = 0
 for crit in criteria.values():
@@ -147,6 +157,7 @@ else:
     feedback += "<br/>note = "+str(response['note'])
     grade = (100, feedback)
 ==
+
 
 
 
