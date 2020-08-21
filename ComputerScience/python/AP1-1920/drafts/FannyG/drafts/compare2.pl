c8ebd3b1a418ca95f9e8c864bc39d85352f6f00f
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-21 14:58:58 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/drafts/compare2.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/drafts/compare2.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/drafts/compare2.pl
@@ -20,7 +20,30 @@ before==#|python|
 data = {"question": "Quel âge avez-vous ?", "answer1": "J'ai 20 ans.", "answer2": "g 20 ten"}
 
 # paramètre criteria contenant la grille critériée
-criteria =       "id":1,
+criteria =[
+   {
+      "id":0,
+      "description":"Age donné",
+      "levels":[
+         {
+            "id":0,
+            "description":"Inférieur à 20 ans",
+            "points":0
+         },
+         {
+            "id":1,
+            "description":"20 ans",
+            "points":1
+         },
+         {
+            "id":2,
+            "description":"Supérieur à 20 ans",
+            "points":2
+         }
+      ]
+   },
+   {
+      "id":1,
       "description":"Orthographe",
       "levels":[
          {
