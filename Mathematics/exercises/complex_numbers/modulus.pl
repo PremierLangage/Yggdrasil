bcae152b36a0f4b57aa315841f11009b8a1c2f95
David DOYEN <David.Doyen@u-pem.fr>
2020-08-24 14:49:47 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/complex_numbers/modulus.pl

--- Mathematics/exercises/complex_numbers/modulus.pl
+++ Mathematics/exercises/complex_numbers/modulus.pl
@@ -1,31 +1,32 @@
-extends = /Mathematics/template/mathinput.pl
+# Author: D. Doyen
+# Tags: complex numbers
+# 19/8/2020
+
+extends = /model/mathinput.pl
 
 title = Module d'un nombre complexe
 
-lang = fr
-
 before ==
-keyboards_JSON['virtualKeyboards']="elementary"
-input.config = keyboards_JSON
-
 a=randitem([-1,1])*randitem([1,2,3,4,5,sqrt(2),sqrt(3),sqrt(5)])
 b=randitem([-1,1])*randitem([1,2,3,4,5,sqrt(2),sqrt(3),sqrt(5)])
 
 z=a+b*I
-z_tex=latex(z)
 sol=abs(z)
-sol_tex=latex(sol)
 ==
 
-text = Déterminer le module du nombre complexe $% z= {{z_tex}} %$.
+text ==
+Déterminer le module du nombre complexe $% z= {{ z|latex }} %$.
+==
 
-evaluator==
-score,_,feedback=ans_expr(input.value,sol)
+evaluator ==
+score, error = eval_expr(input.value,sol)
+feedback = feedback_message[error]
 ==
 
 solution==
-Le module de $! z !$ est $% \displaystyle {{sol_tex}} %$.
+Le module de $! z !$ est $% \displaystyle {{ sol|latex }} %$.
 ==
+
 
 
 
