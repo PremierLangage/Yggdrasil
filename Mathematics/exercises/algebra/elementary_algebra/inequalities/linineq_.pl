2ba6b315d22e7ba533b2f862797573f6fe52c735
David DOYEN <David.Doyen@u-pem.fr>
2020-08-24 15:30:40 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/inequalities/linineq_.pl

--- Mathematics/exercises/algebra/elementary_algebra/inequalities/linineq_.pl
+++ Mathematics/exercises/algebra/elementary_algebra/inequalities/linineq_.pl
@@ -9,20 +9,18 @@ a,c=list_randint_norep(2,-6,6,[0,1,-1])
 b,d=list_randint(2,-6,6,[0])
 var('x')
 f=a*x+b
-if param['otherside']=='zero':
+if param['otherside'] == 'zero':
     g=0
-if param['otherside']=='constant':
+if param['otherside'] == 'constant':
     g=d
-if param['otherside']=='linear':
+if param['otherside'] == 'linear':
     g=c*x+d
 ineq=randitem([f>=g,f>g,f<=g,f<g,g>=f,g>f,g<=f,g<f])
 sol=solveset(ineq,x,domain=S.Reals)
-latexineq=latex(ineq)
-sol_tex=latex(sol)
 ==
 
 text ==
-Déterminer l'ensemble des réels $% x %$ tels que $$ {{latexineq}}. $$ Ecrire cet ensemble sous la forme d'un intervalle.
+Déterminer l'ensemble des réels $% x %$ tels que $$ {{ ineq|latex }}. $$ Ecrire cet ensemble sous la forme d'un intervalle.
 ==
 
 evaluator==
@@ -30,7 +28,7 @@ score,_,feedback=ans_rset(input1.value,sol)
 ==
 
 solution==
-Cet ensemble est $! {{sol_tex}} !$.
+Cet ensemble est $! {{sol|latex}} !$.
 ==
 
 
