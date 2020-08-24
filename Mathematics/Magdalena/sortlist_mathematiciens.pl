aef2e1427111c6b3f10c53688ac04ce820a6d95e
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-08-24 15:49:28 +0200

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/sortlist_mathematiciens.pl

---
+++ Mathematics/Magdalena/sortlist_mathematiciens.pl
@@ -0,0 +1,40 @@
+extends = /model/basic.pl
+
+title = Mathematiciens (SortList)
+
+@ mathematiciens.csv
+
+before ==
+import random as rd
+import csv
+
+with open('mathematiciens.csv', newline='') as file:
+    rows = list(csv.DictReader(file, delimiter=','))
+
+lst = rd.sample(range(len(rows)), 5)
+lst.sort()
+sortlist.setdata_from_list([rows[i]["nom"] for i in lst])
+==
+
+text ==
+Classer ces mathématiciens du plus ancien au plus récent (selon la date de naissance).
+==
+
+sortlist =: SortList
+
+@ /utils/components/sortlist.py [customsortlist.py]
+sortlist.decorator = CustomSortList
+
+form ==
+{{ sortlist|component }}
+==
+
+evaluator ==
+score = sortlist.eval()
+==
+
+settings.feedback = lightscore
+
+
+
+
