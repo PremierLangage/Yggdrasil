087c0d85d57a9c61120eb07c608ac80e66a52c5b
David DOYEN <David.Doyen@u-pem.fr>
2020-08-26 16:37:12 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/polynomials/quadratic/quadratic_roots_.pl

--- Mathematics/exercises/algebra/polynomials/quadratic/quadratic_roots_.pl
+++ Mathematics/exercises/algebra/polynomials/quadratic/quadratic_roots_.pl
@@ -3,7 +3,7 @@ extends = quadratic_.pl
 title = Equation quadratique
 
 text ==
-Déterminer les solutions de l'équation $% {{latexP}} = 0 %$. Séparer les différentes solutions par une virgule.
+Déterminer les solutions de l'équation $% {{P|latex}} = 0 %$. Séparer les différentes solutions par une virgule.
 ==
 
 evaluator==
@@ -13,5 +13,6 @@ score,_,feedback=ans_set_expr(input1.value,lstsol,formal=False)
 solution ==
 {{solution_roots}}
 ==
+
 
 
