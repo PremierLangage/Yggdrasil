75bbf78d3accbfbc503cec54c3ee77d30e9dc0dd
David DOYEN <David.Doyen@u-pem.fr>
2020-08-26 16:21:56 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl

--- Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl
+++ Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl
@@ -4,20 +4,21 @@ title = Equations quadratiques
 
 before ==
 var('x')
-sol=[]
-choices=[]
+sol = []
+choices = []
 for i in range(3):
     a=randint(-5,5,[0])
     b,c = list_randint_norep(2,-5,5)
     if b**2-4*a*c<0:
-        sol.append(True)
+        sol.append(i)
     else:
-        sol.append(False)
-    sides=randitem([[a*x**2+b*x+c,0],[a*x**2+b*x,-c],[a*x**2,-b*x-c],[a*x**2+c,-b*x]])
+        sol.append(i)
+    sides = randitem([[a*x**2+b*x+c,0],[a*x**2+b*x,-c],[a*x**2,-b*x-c],[a*x**2+c,-b*x]])
     rd.shuffle(sides)
-    s=r"$$ %s $$" % latex(Eq(*sides))
-    choices.append({"id":str(i),"content":s})
-component.items = choices
+    s = r"$$ %s $$" % latex(Eq(*sides))
+    choices.append(s)
+checkbox.setitems(choices)
+checkbox.setsol_from_index(choices)
 ==
 
 text ==
