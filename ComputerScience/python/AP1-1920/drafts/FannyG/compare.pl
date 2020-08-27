162ec1d74ffdde919b4e0ee7f28c3804770198d5
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-27 11:23:43 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/compare.pl
@@ -24,7 +24,7 @@ text =
 
 before==#|python|
 # paramètre data contenant la question et la réponse de l'élève
-#data = {"question": "Quel âge avez-vous ?", "answer1": "J'ai 20 ans.", "answer2": "g 20 ten"}
+data = {"question": "Quel âge avez-vous ?", "answer1": "J'ai 20 ans.", "answer2": "g 20 ten"}
 
 # paramètre criteria contenant la grille critériée
 criteria =[
@@ -154,6 +154,7 @@ if error:
 else:
     grade = (100, feedback)
 ==
+
 
 
 
