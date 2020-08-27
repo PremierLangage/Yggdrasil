bf517335f53a25d63603d68d5a2860ba0d39ec4d
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-27 10:25:39 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/howUsing/usingcompare.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/howUsing/usingcompare.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/howUsing/usingcompare.pl
@@ -37,6 +37,6 @@ criteria =[
 ]
 #grille criteriee
 criteria[0] = {"id":0,"description":"Age donné","levels":[{"id":0,"description":"Inférieur à 20 ans", "points":0},{"id":1,"description":"20 ans","points":1},{"id":2,"description":"Supérieur à 20 ans","points":2}]}
-criteria[1] = 
-criteria[2] = 
+criteria[1] = {"id":1,"description":"Orthographe","levels":[{"id":0, "description":"Plusieurs fautes", "points":0},{"id":1, "description":"1 faute", "points":1},{"id":2, "description":"Aucune faute", "points":2}]}
+criteria[2] = {"id":2,"description":"Syntaxe","levels":[{"id":0, "description":"La phrase ne possède ni une majuscule, ni un point.", "points":0},{"id":1, "description":"La phrase possède soit une majuscule, soit un point.", "points":1},{"id":2, "description":"La phrase possède une majuscule et un point.", "points":2}]}
 
