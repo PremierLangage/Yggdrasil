043840b3cb2441a1d78fa8a8766d8c9a2c93098d
FANNY GARNEVAULT <fgarneva@etud.u-pem.fr>
2020-08-27 10:15:37 +0200

    fgarnevault modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/FannyG/howUsing/usingevaluation.pl

--- ComputerScience/python/AP1-1920/drafts/FannyG/howUsing/usingevaluation.pl
+++ ComputerScience/python/AP1-1920/drafts/FannyG/howUsing/usingevaluation.pl
@@ -8,6 +8,35 @@ data.answer= J'ai 20 ans.
 #sinon mettre autre chose (peut importe le mot par ex : Fals ou True) 
 comment_by_criteria = True
 
+criteria =[
+    {
+        "id":0,
+        "description":"Age donné",
+        "levels":[
+            {"id":0, "description":"Inférieur à 20 ans", "points":0},
+            {"id":1, "description":"20 ans", "points":1},
+            {"id":2, "description":"Supérieur à 20 ans", "points":2}
+        ]
+   },
+   {
+        "id":1,
+        "description":"Orthographe",
+        "levels":[
+            {"id":0, "description":"Plusieurs fautes", "points":0},
+            {"id":1, "description":"1 faute", "points":1},
+            {"id":2, "description":"Aucune faute", "points":2}
+        ]
+   },
+   {
+        "id":2,
+        "description":"Syntaxe",
+        "levels":[
+            {"id":0, "description":"La phrase ne possède ni une majuscule, ni un point.", "points":0},
+            {"id":1, "description":"La phrase possède soit une majuscule, soit un point.", "points":1},
+            {"id":2, "description":"La phrase possède une majuscule et un point.", "points":2}
+      ] 
+   }
+]
 
 
 
