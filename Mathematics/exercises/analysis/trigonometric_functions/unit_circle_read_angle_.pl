bd417e59d614b0a6180e8153e399b2e47d8c97c2
David DOYEN <David.Doyen@u-pem.fr>
2020-08-28 18:06:40 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
@@ -4,9 +4,8 @@ title = Lire un angle sur le cercle trigonométrique
 
 text ==
 Quelle est la mesure (en radians) de l'angle $% (\overrightarrow{OA},\overrightarrow{OM}) %$ représenté ci-dessous. 
-{{ drawer | component}}
+{{ jxg | component}}
 ==
-
 
 evaluator ==
 score, error = eval_expr(input.value, angle, modulo=2*pi)
