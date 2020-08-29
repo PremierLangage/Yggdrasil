774906b384b6ce2f58e61726aff04c5bc77ed299
David DOYEN <David.Doyen@u-pem.fr>
2020-08-29 19:28:34 +0200

    ddoyen modified Yggdrasil/model/mathinput2.pl

---
+++ model/mathinput2.pl
@@ -0,0 +1,24 @@
+extends = /model/math.pl
+extends = /model/evalsympy_fdbk_msg_fr.pl
+@ /utils/components/mathinput.py [custommathinput.py]
+
+input1 =: MathInput
+input1.decorator = CustomMathInput
+input1.virtualKeyboards = elementary
+
+input1 =: MathInput
+input2.decorator = CustomMathInput
+input2.virtualKeyboards = elementary
+
+title = 
+
+text = 
+
+form ==
+{{ input|component}}
+==
+
+settings.feedback = rightwrong
+
+settings.maxattempt % 1
+
