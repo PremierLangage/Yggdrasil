2d3b6aab3c0ffa3bcac8ba9a28960663268828a7
David DOYEN <David.Doyen@u-pem.fr>
2020-08-29 19:51:08 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_sincos_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_sincos_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_sincos_.pl
@@ -1,14 +1,14 @@
-extends =  unit_circle_read_.pl
+extends =  jxg_angle_unit_circle.pl
+extends =  /model/mathinput2.pl
 
 title = Lire le sinus et le cosinus d'un angle sur le cercle trigonométrique
 
-
 text ==
 Déterminer le sinus et le cosinus de l'angle $% (\overrightarrow{OA},\overrightarrow{OM}) %$ représenté ci-dessous. 
-{{drawer |component}}
+{{ jxg|component }}
 ==
 
-form==
+form ==
 <style>
 .fcontainer {
     display: flex;
@@ -19,20 +19,21 @@ c-math-input {
 }
 </style>
 <div class="fcontainer">
-<span>sinus:</span>{{input1|component}}
+<span>sinus:</span> {{ input1|component }}
 </div>
 <div class="fcontainer">
-<span>cosinus:</span>{{input2|component}}
+<span>cosinus:</span> {{ input2|component }}
 </div>
 ==
 
 evaluator ==
-score,_,feedback=ans_tuple_expr([input1.value,input2.value],[sin(angle),cos(angle)])
+score, error = eval_mult_expr([input1.value,input2.value],[sin(angle),cos(angle)])
 ==
 
 solution ==
 Le sinus vaut $! {{sinangle_tex}} !$ et le cosinus vaut $! {{cosangle_tex}} !$.
 ==
+
 
 
 
