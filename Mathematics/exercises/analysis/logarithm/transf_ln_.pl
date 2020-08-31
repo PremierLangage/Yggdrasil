ac493f2ad700229f0ade2214bfe62cd4cd727d20
David DOYEN <David.Doyen@u-pem.fr>
2020-08-31 10:48:31 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/logarithm/transf_ln_.pl

--- Mathematics/exercises/analysis/logarithm/transf_ln_.pl
+++ Mathematics/exercises/analysis/logarithm/transf_ln_.pl
@@ -61,20 +61,19 @@ Ecrire $% {{expr}} %$ sous la forme  $% \ln(a)%$, où $%a%$ est un nombre.
 ==
 
 evaluator==
-def ans_eval(strans,sol):
+def eval_ans(strans,sol):
     try:
         ans = latex2sympy(strans)
     except:
-        return (-1,"FailedConversion","Votre réponse n'est pas sous la forme attendue")
+        return (-1, "WrongForm")
     if type(ans)!=sp.log:
-        return (-1,"","Votre réponse n'est pas sous la forme attendue")
+        return (-1, "WrongForm")
     if not equal(ans.args[0],sol):
-        return (0,"","")
-    return (100,"","")
+        return (0, "NotEqual")
+    return (100, "Success")
 
-score,_,feedback=ans_eval(input.value,sol)
+score, error = eval_ans(input.value, sol)
 ==
-
 
 wims ==
 
@@ -111,6 +110,7 @@ wims ==
 
 
 ==
+
 
 
 
