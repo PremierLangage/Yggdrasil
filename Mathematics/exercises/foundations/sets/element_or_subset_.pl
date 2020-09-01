25881ec47827c0bf1b7db66058379b087d91c945
David DOYEN <David.Doyen@u-pem.fr>
2020-09-01 16:39:55 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/element_or_subset_.pl

--- Mathematics/exercises/foundations/sets/element_or_subset_.pl
+++ Mathematics/exercises/foundations/sets/element_or_subset_.pl
@@ -64,56 +64,14 @@ for i in range(n):
         rhs[i]=latex(FiniteSet(*rhs[i]))
 ==
 
-old ==
-sol=[]
-lhs=[]
-rhs=[]
-n=5
-case=eval(param['cases'])
-rd.shuffle(case)
-for i in range(n):
-    if case[i]==1:
-        rhs.append(list_randint_norep(randint(3,5),0,9))
-        sol.append(randitem([label_in,label_in,""]))
-        if sol[i]==label_in:
-            lhs.append(randitem(rhs[i]))
-        else:
-            lhs.append(randint(0,9,rhs[i]))
-    elif case[i]==2:
-        rhs.append(list_randint_norep(randint(3,5),0,9))
-        sol.append(randitem([label_subset,label_subset,""]))
-        if sol[i]==label_subset:
-            lhs.append([randitem(rhs[i])])
-        else:
-            lhs.append([randint(0,9,rhs[i])])
-    elif case[i]==3:
-        rhs.append(list_randint_norep(randint(3,5),0,9))
-        sol.append(randitem([label_subset,label_subset,""]))
-        if sol[i]==label_subset:
-            lhs.append(list_randitem_norep(randint(2,len(rhs[i])),rhs[i]))
-        else:
-            b=randint(2,len(rhs[i]))
-            a=randint(1,b-1)
-            lhs.append(list_randint_norep(a,0,9,rhs[i])+list_randitem_norep(b-a,rhs[i]))
-
-for i in range(n):
-    group.add_drop({str(i):""}) 
-    group.add_match_by_content(str(i),sol[i]) 
-    if isinstance(lhs[i],list):
-        lhs[i]=latex(FiniteSet(*lhs[i]))
-    if isinstance(rhs[i],list):
-        rhs[i]=latex(FiniteSet(*rhs[i]))
-
-==
-
 text ==
 Compléter les propositions suivantes avec les {{text1}} {{ group.labels.in | component }} ou {{ group.labels.subset | component }}. Si {{text2}} ne convient, laisser la case vide  .
 ==
-form =
+
 form ==
 <ul>
 {% for i in range(n) %}
-<li> {{ lhs[i]}} {{ group.drops[i|string]|component }} {{ rhs[i] }} </li>
+<li> $! {{ lhs[i]}} !$ {{ group.drops[i|string]|component }} $! {{ rhs[i] }} !$ </li>
 {% endfor %}
 </ul>
 ==
