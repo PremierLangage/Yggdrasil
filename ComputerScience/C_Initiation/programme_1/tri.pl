430c09b755428842763426638bd9ffd29d86456e
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-09-03 15:00:07 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/programme_1/tri.pl

---
+++ ComputerScience/C_Initiation/programme_1/tri.pl
@@ -0,0 +1,76 @@
+
+
+title= Tri de trois valeurs
+tag=boucle,tests
+
+
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+
+text==  
+
+Écrire un programme qui lit trois variables entières  puis les affiche dans l'ordre croissant, séparées par des espaces,
+suivies d'un retour à la ligne.
+
+==
+
+editor.code==
+int main(void) {
+...
+printf("Entrer trios entiers:\n");
+  /* votre code ici... */
+}
+
+==
+
+
+codebefore==
+
+#include <stdlib.h>
+#include <stdio.h>
+
+==
+
+solution==
+
+
+int main(void){
+  int a,b,c;
+  int min,max;
+  printf("Entrez 3 entiers\n");
+  scanf("%d",&a);
+  scanf("%d",&b); scanf("%d",&c);
+  if(a<b){
+    min=a;
+    max=b;
+  }
+  else{
+    min=b;
+    max=a;
+  }
+  if(c<min)
+    printf("%d %d %d\n",c,min,max);
+  else if(c>max)
+    printf("%d %d %d\n",min,max,c);
+  else
+  printf("%d %d %d\n",min,c,max);
+  return 0;
+}
+
+
+==
+
+
+tests==
+[ ["Quelconque", "", "10 0 -1"],
+  ["Decroissante", "", "5 2-1"],
+  ["Croissante", "", "1 2 3"],
+  ["Aléatoire", "",  " ".join([str(random.randint(0,100)) for i in range(3)]
+]
+==
+
+
+
+
+
+
