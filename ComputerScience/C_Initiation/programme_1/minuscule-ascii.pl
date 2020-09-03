b84d8f45a49d5c5224352d6e5314fdb790096533
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-09-03 15:22:08 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/programme_1/minuscule-ascii.pl

---
+++ ComputerScience/C_Initiation/programme_1/minuscule-ascii.pl
@@ -0,0 +1,49 @@
+
+# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#
+author=
+title=Caractères minuscules ASCII
+tag=type,boucle
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+
+Écrire un programme C qui affiche, pour chaque lettre minuscule, sur une ligne les données suivante :
+
+    lettre --> code_décimal_de_la_lettre
+    
+Attention, il y a un espace avant et après la flèche. Il y a un retour à la ligne juste après le 
+code décimal de la lettre.
+==
+
+editor.code==
+#include <stdio.h>
+
+int main(){
+  ...
+  printf(" --> ");
+  ...
+}
+==
+
+solution==
+#include <stdio.h>
+
+int main(){
+  char c;
+
+  for (c='a' ; c<='z' ; c++)
+    printf("%c --> %d\n", c, c);
+  return 0;
+}
+==
+
+tests==
+[["Simple exéécution", "", ""]]
+==
+
+
+
+
+
+
