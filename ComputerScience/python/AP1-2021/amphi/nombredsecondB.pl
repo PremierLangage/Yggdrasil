e80bd2b59972bc94a2492eb2ef98a698a5f8e054
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-04 14:47:59 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-2021/amphi/nombredsecondB.pl

---
+++ ComputerScience/python/AP1-2021/amphi/nombredsecondB.pl
@@ -0,0 +1,55 @@
+
+
+
+# DR 25/09/2019 OK
+#  Author: DR
+@ /utils/sandboxio.py
+@ /grader/evaluator.py [grader.py] 
+@ /builder/before.py [builder.py] 
+
+
+
+
+title = Expression
+
+before ==
+import random as rd
+a=rd.randint(10,20)
+b=rd.randint(10,20)
+==
+
+text ==
+Calculer le nombre de secondes dans une année  **bissextile**. 
+
+Saisissez soit le nombre exacte soit une expression permettant de le calculer.
+==
+
+input =: Input
+input.type = text
+
+form ==
+{{ input | component }}
+==
+
+settings.feedback = rightwrong
+
+evaluator ==
+if len(input.value) == 0 :
+    grade = (0, " essayez encore quelque chose de plus grand.")
+else:
+    try:
+        if int(input.value) == 31622400 :
+            grade=(50,"C'est exact !")
+        else:
+            grade=(0,"Valeur incorrecte")
+
+    except :
+        try :
+            if eval(input.value) == 31622400 :
+                grade=(100,"expression correcte égale à 31622400")
+        except :
+            grade=(0,"expression incorrecte. La réponse est 31622400.")
+==
+
+
+
