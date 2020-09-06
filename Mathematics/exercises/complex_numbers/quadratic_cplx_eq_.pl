30b132f746a6c302b69740736015446ee59b48d7
David DOYEN <David.Doyen@u-pem.fr>
2020-09-07 00:47:18 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/complex_numbers/quadratic_cplx_eq_.pl

--- Mathematics/exercises/complex_numbers/quadratic_cplx_eq_.pl
+++ Mathematics/exercises/complex_numbers/quadratic_cplx_eq_.pl
@@ -7,7 +7,7 @@ param.roots = int
 before ==
 a,b=list_randint(2,-4,4,[0])
 a2,b2=list_randint(2,-4,4,[0])
-while is_equal(a-b*I,a2+b2*I):
+while equal(a-b*I,a2+b2*I):
     a2,b2=list_randint(2,-4,4,[0])
 
 c=randint(-5,5,[0,1,-1])
