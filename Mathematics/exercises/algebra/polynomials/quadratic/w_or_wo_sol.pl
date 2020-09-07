d590a4c7c3c527b4ef52e9cbfb10021c41b670b2
David DOYEN <David.Doyen@u-pem.fr>
2020-09-07 22:29:01 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl

--- Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl
+++ Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl
@@ -12,8 +12,6 @@ for i in range(3):
     b,c = list_randint_norep(2,-5,5)
     if b**2-4*a*c<0:
         sol.append(i)
-    else:
-        sol.append(i)
     sides = randitem([[a*x**2+b*x+c,0],[a*x**2+b*x,-c],[a*x**2,-b*x-c],[a*x**2+c,-b*x]])
     rd.shuffle(sides)
     s = r"$! %s !$" % latex(Eq(*sides))
@@ -25,4 +23,5 @@ checkbox.setsol_from_index(sol)
 text ==
 Sélectionner les équations qui n'ont pas de solution réelle dans la liste ci-dessous.
 ==
+
 
