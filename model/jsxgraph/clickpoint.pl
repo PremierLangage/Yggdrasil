10d897ced5aced379de623a2878af136c931f3a0
David DOYEN <David.Doyen@u-pem.fr>
2020-09-07 10:36:45 +0200

    ddoyen modified Yggdrasil/model/jsxgraph/clickpoint.pl

---
+++ model/jsxgraph/clickpoint.pl
@@ -0,0 +1,32 @@
+extends = /model/mathjsxgraph.pl
+
+script_aux ==
+function getMouseCoords(e) {
+    let cPos = board.getCoordsTopLeftCorner(e);
+    let absPos = JXG.getPosition(e);
+    let dx = absPos[0]-cPos[0];
+    let dy = absPos[1]-cPos[1];
+    return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
+}
+
+function down(e) {
+    let coords = getMouseCoords(e);
+    M.setPosition(JXG.COORDS_BY_USER,[coords.usrCoords[1], coords.usrCoords[2]]);
+}
+
+board.on('down', down)
+==
+
+
+evaluator ==
+x, y = jxg.getpoint('M')
+from math import hypot
+if hypot(x-a, y-b) < 0.1:
+    score = 100
+    jxg.addscript(script_solution, globals())
+else:
+    score = 0
+    jxg.addscript(script_solution, globals())
+feedback = ""
+==
+
