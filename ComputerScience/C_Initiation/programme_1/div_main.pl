4e48336842e7ddc70f1eb952258227de9c13bc04
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-07 19:58:26 +0200

    nborie modified Yggdrasil/ComputerScience/C_Initiation/programme_1/div_main.pl

--- ComputerScience/C_Initiation/programme_1/div_main.pl
+++ ComputerScience/C_Initiation/programme_1/div_main.pl
@@ -18,7 +18,7 @@ Si l'entier est négatif, on ne fait rien !
 0 a une infinité de diviseurs.
 
 **Exemple :**
-```c 
+
 Un nombre : 6
 
 1
@@ -30,7 +30,6 @@ Un nombre : 6
 6
 
 6 a 4 diviseurs
-```
 
 **Rappel :** le code est sensible aux espaces qui n'ont pas nécessité d'exister !
 ==
