03f0ee301282686ed9bd51abe8b6e0f2a589554a
David DOYEN <David.Doyen@u-pem.fr>
2020-09-07 22:00:46 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/polynomials/factorization/common_factor_2terms_1_11.pl

--- Mathematics/exercises/algebra/polynomials/factorization/common_factor_2terms_1_11.pl
+++ Mathematics/exercises/algebra/polynomials/factorization/common_factor_2terms_1_11.pl
@@ -1,10 +1,11 @@
 extends = common_factor_.pl
 
-param.nterms = 2
+param.nterms % 2
 
-param.comfactor = 1
+param.comfactor % 1
 
-param.terms = 1,1
+param.terms % [1,1]
 
-param.obvious =yes
+param.obvious % true
+
 
