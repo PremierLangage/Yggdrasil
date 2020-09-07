cfac20edfc6feedd911793e077d0f537fe131032
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-09-07 15:11:13 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/programme_1/hello.pl

--- ComputerScience/C_Initiation/programme_1/hello.pl
+++ ComputerScience/C_Initiation/programme_1/hello.pl
@@ -13,17 +13,13 @@ text==
 
 Faites votre premier programme C, ce programme devra écrire 
 *Hello World!* sur la sortie standard et retourner à la ligne. <br>
-Le retour à la ligne est encodé par le caractère '\n' en C.<br>
-Pour faire de l'affichage, on utilise la fonction *printf*
-dont l'existance est déclaré  grace à la commande
-*#include <stdio.h>*.
+Le retour à la ligne est encodé par le caractère '\n' en C (vous ne mettrez pas d'espace avant ni après !).<br>
+Pour faire de l'affichage, on utilise la fonction *printf* dont l'existance est déclarée grace à la commande *#include <stdio.h>*.
 
-Un programme C contient toujours une et une seule fonction
-dont le nom est *main*. Cette fonction sera le point d'entrée
-du programme. Un programme C fait exactement tout se qui se
-trouve dans sa fonction *main*, ni plus, ni moins. Ici, il
-vous faut juste faire de l'affichage. Un bon appel à la
-fonction *printf* doit suffire.
+Un programme C contient toujours une et une seule fonction dont le nom est *main*. Cette fonction sera le point d'entrée
+du programme.<br>
+Un programme C fait exactement tout se qui se trouve dans sa fonction *main*, ni plus, ni moins. Ici, il
+vous faut juste faire de l'affichage. Un bon appel à la fonction *printf* doit suffire.
 
 ==
 
@@ -38,6 +34,7 @@ int main(void){
 tests==
 [ ["simple éxécution", "","", "Hello World!\n"] ]
 ==
+
 
 
 
