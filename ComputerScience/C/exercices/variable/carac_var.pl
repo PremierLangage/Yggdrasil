c5477960746594398d6bcad9fd7d35561d0e046e
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-08 11:28:42 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/variable/carac_var.pl

--- ComputerScience/C/exercices/variable/carac_var.pl
+++ ComputerScience/C/exercices/variable/carac_var.pl
@@ -1,6 +1,19 @@
-# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
 #
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
 
 @ /utils/sandboxio.py
 grader  =@ /grader/evaluator.py
@@ -28,7 +41,7 @@ random.shuffle(group.items)
 
 author = Nicolas Borie
 
-title = Les variables en C
+title = Caractéristiques des variables C
 
 text==
 Sélectionnez les éléments qui caractérisent les variables en C. Il s'agit donc de cocher les
@@ -68,4 +81,5 @@ else:
 
 grade = (note, feedback)
 ==
+
 
