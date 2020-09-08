b1172726466a3eded348249694550253bf040e43
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-08 08:37:00 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/program/random_instructions/set_instructions8.pl

--- ComputerScience/C/exercices/program/random_instructions/set_instructions8.pl
+++ ComputerScience/C/exercices/program/random_instructions/set_instructions8.pl
@@ -16,12 +16,13 @@
 
 author=Nicolas Borie
 
-title=Jeu d'instructions arithmétiques
+title=Jeu d'instructions arithmétiques niveau 8
 tag=programme|variable|instruction|arithmétique
 
-extends=/ComputerScience/C/exercices/program/set_instructions.pl
+extends=/ComputerScience/C/exercices/program/random_instructions/set_instructions.pl
 
 nb_variables = 3
 complexity = 20
 instruct_complexity = 5
+
 
