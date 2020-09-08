7502f83a47d1491043ae6365e4178d3bb89b49f4
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-08 08:17:23 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/program/random_instructions/set_instructions1.pl

--- ComputerScience/C/exercices/program/random_instructions/set_instructions1.pl
+++ ComputerScience/C/exercices/program/random_instructions/set_instructions1.pl
@@ -1,6 +1,18 @@
-# Copyright 2016 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2016 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# devinez la valeur d'une variable après un jeu d'instructions
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
 
 author=Nicolas Borie
 
