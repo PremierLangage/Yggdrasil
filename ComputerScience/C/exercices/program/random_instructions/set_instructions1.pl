1cbaff47be8b105f175eec5f7040c1e76493935e
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-08 08:17:50 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/program/random_instructions/set_instructions1.pl

--- ComputerScience/C/exercices/program/random_instructions/set_instructions1.pl
+++ ComputerScience/C/exercices/program/random_instructions/set_instructions1.pl
@@ -17,7 +17,7 @@
 author=Nicolas Borie
 
 title=Jeu d'instructions arithmétiques
-tag=program|variable
+tag=programme|variable|instruction|arithmétique
 
 extends=/ComputerScience/C/exercices/program/set_instructions.pl
 
