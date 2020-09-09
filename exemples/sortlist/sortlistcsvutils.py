6828b98c4b8f17ab2f0d403c63321b51bdaf3c27
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-09 16:04:07 +0200

    drevuz modified Yggdrasil/exemples/sortlist/sortlistcsvutils.py

--- exemples/sortlist/sortlistcsvutils.py
+++ exemples/sortlist/sortlistcsvutils.py
@@ -42,10 +42,12 @@ import uuid
 
 def randomize(items):
     answer=[]
+    n=1
     for e in items:
         # generate random id because students can
         # guest the answer if ids like 1, 2, 3 are used
-        e["id"] = str(uuid.uuid4())
+        e["id"] = n # str(uuid.uuid4())
+        n= n+1
         answer.append(e["id"])
     random.shuffle(items)
     return answer,items
