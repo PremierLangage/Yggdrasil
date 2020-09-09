b8b19717198f0c33d44110a8fba9427591bfc687
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-09 16:31:16 +0200

    drevuz modified Yggdrasil/Mathematics/Magdalena/newsortlist.pl

--- Mathematics/Magdalena/newsortlist.pl
+++ Mathematics/Magdalena/newsortlist.pl
@@ -4,4 +4,10 @@ extends = /exemples/sortlist/template.pl
 columns=nom,dates
 number=4
 
+#@ toto.csv [data.csv]
 @ mathematiciens.csv [data.csv]
+
+title = Trions des mathématiciens 
+
+text= Ordonez les dans l'ordre historique de la date de naissance
+
