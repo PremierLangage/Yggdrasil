4b7513025b73042d87f1167d6a5ef5d8ceca9f2f
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-09 11:58:42 +0200

    drevuz modified Yggdrasil/exemples/sortlist/builder.py

--- exemples/sortlist/builder.py
+++ exemples/sortlist/builder.py
@@ -44,8 +44,8 @@ if __name__ == "__main__":
             reader = csv.reader(csvfile)
             i = next(reader)
         cols=(i[0],i[1])
-    nb= dic.getdefault("number",4)
-        dic['answer'],dic['items']= utils.builditemsandanswer("data.csv",delimiter=delimiter,columns=cols)
+        nb= dic.get("number",4)
+        dic['answer'],dic['items']= utils.builditemsandanswer("data.csv",delimiter=delimiter,columns=cols, number=nb)
 
         
 
