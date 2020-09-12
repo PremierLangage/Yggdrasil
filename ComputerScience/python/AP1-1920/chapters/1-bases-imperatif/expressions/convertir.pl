cec4b4ee8b2b9d1f2fc3d595a9abd13de763f279
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-12 19:40:31 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressions/convertir.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressions/convertir.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressions/convertir.pl
@@ -17,24 +17,24 @@ before==
 ==
 
 title==
-Input Component
+Convertion mpg en l/km
 ==
 
 text==
 
 Aux US la consomation des véhicules est exprimé en Miles par Gallons.
-Nous utilisons des litres aux 100 Kilomêtres. 
+Nous utilisons des litres aux 100 Kilometres. 
 
-On suppose que l'on nous donne la valeur C comme nombre de Miles par Gallon.
-Donner l'expression qui permet de la convertir en litre par 100 Kilomêtres.
+On suppose que l'on nous donne la valeur **C** comme nombre de Miles par Gallon.
+Donner l'expression qui permet de la convertir en litres pour 100 Kilomêtres.
 
 
-0.621371 Mile = 1 Kilomêtres 
+0.621371 Mile = 1 Kilometres 
 
 1 Gallon = 3.78541 litres 
 
 
-Utiliser les constantes suivantes qui vous conviennent:
+Utiliser parmis les constantes suivantes celles qui vous conviennent:
 
     M=0.621371 # Mile dans Km . . 
     G=3.78541 # litre dans Gallon . . 
@@ -52,19 +52,19 @@ M=0.621371 # Mile dans Km
 G=3.78541 # litre dans Gallon
 K=1.60934 # Km dans Mile
 L=0.264172 # Gallon dans litre
-sol=lambda C:100/((C/G)/L)
-f=lambda C:eval(inputbox.value)
+sol=lambda C:100/((C/G)*K)
+f=lambda C: eval(inputbox.value)
 r=True
 delta= -100
 try:
-    delta=f(40) - sol(40)
+    delta= (f(40) - sol(40))
     if -0.01 < delta < 0.01 :
         r=True
 
 except :
     r=False
 if r:
-    grade = (100, '<span class="success-state">Good 👏👏👏'+str(delta)+'</span>')
+    grade = (100, '<span class="success-state">Good 👏👏👏'+str(f(40))+' '+str(sol(40))+'</span>')
 else:
     grade = (0, '<span class="error-state">Bad answer 👎👎👎 '+str(delta)+'</span>')
 ==
