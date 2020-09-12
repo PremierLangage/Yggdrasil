2ba94b5e4b6e336d7c8c7f749abd196d65cb09a2
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-12 17:36:49 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/AP1-2021/medaille.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/AP1-2021/medaille.pl
@@ -0,0 +1,31 @@
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = markdown
+editor.code ==
+# write your code here
+==
+
+
+title= Une médaille 
+
+text==
+
+La fonction metal fournis le niveau sportif "Or","Argent", "Bronze", et "Neige" de participant avec
+ les règles suivantes , il y a trois épreuves ou l'on optient de 0 à 6 points
+
+
+| or                       |     argent . .     | bronze             |              Neige |
+|--------------------------|--------------------|--------------------|--------------------|
+|Plus de 4 à chaque épreuve | Plus de 3 à chaque épreuve|Plus de 2 à chaque épreuve | pas de conditions.  |
+|et au moins 13 au total | et au moins 10 au total |et au moins 7 au total | |
+
+==
+
+form==
+==
+
+before=
