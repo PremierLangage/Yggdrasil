aede4f4ac3a144efab573612992d91d73980f2d4
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-12 08:31:45 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/type/minuscule_ascii.pl

--- ComputerScience/C/exercices/type/minuscule_ascii.pl
+++ ComputerScience/C/exercices/type/minuscule_ascii.pl
@@ -1,9 +1,25 @@
-# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
+
 author=Nicolas Borie
 title=Caractères minuscules ASCII
-tag=type
-extends=/ComputerScience/C/template/stdsandboxC.pl
+
+tag=C|type|
 
 text==
 
@@ -37,9 +53,15 @@ int main(){
 }
 ==
 
-tests==
-[["Simple exéécution", "", ""]]
+code_before==
 ==
+code_after==
+==
+
+checks_args_stdin==
+[["Simple exécution", [], ""]]
+==
+
 
 
 
