4d16d6203fe45edac226c1d863aec0071965a8aa
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-12 17:49:39 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/AP1-2021/medaille.pl

--- ComputerScience/python/AP1-1920/drafts/dominique/AP1-2021/medaille.pl
+++ ComputerScience/python/AP1-1920/drafts/dominique/AP1-2021/medaille.pl
@@ -1,3 +1,4 @@
+# DR 10/09/2020
 tag=function # N'oubliez pas de remplir ce champs svp
 extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 
@@ -9,17 +10,29 @@ La fonction metal fournis le niveau sportif "Or","Argent", "Bronze", et "Neige" 
  les règles suivantes , il y a trois épreuves ou l'on optient de 0 à 6 points
 
 Niveau Or : 4 ou plus a chaque épreuves et au moins  13 au total.
+
 Niveau Argent : 3 ou plus a chaque épreuves et au moins  10 au total.
+
 Niveau Bronze : 2 ou plus a chaque épreuves et au moins  7 au total.
+
 Niveau Neige : pas de conditions. 
 
 Exemple de doctest :
 
-    ‘‘‘
     >>> metal(5,6,4)
     'Or'
-    ‘‘‘
+
 
 ==
 
+pltest0==
+>>> metal(5,6,4)
+'Or'
+>>> metal(5,3,4)
+'Argent'
+>>> metal(3,2,2)
+'Bronze'
+>>> metal(1,6,6)
+'Neige'
+==
 
