b37fd01788e77c16ae4dc57d381519994751adac
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-12 19:34:29 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressions/convertir.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressions/convertir.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressions/convertir.pl
@@ -28,9 +28,18 @@ Nous utilisons des litres aux 100 Kilomêtres.
 On suppose que l'on nous donne la valeur C comme nombre de Miles par Gallon.
 Donner l'expression qui permet de la convertir en litre par 100 Kilomêtres.
 
-0.621371 Mile = 1 Kilomêtres . . 
-1 Gallon = 3.78541 litres . . 
 
+0.621371 Mile = 1 Kilomêtres 
+
+1 Gallon = 3.78541 litres 
+
+
+Utiliser les constantes suivantes qui vous conviennent:
+
+    M=0.621371 # Mile dans Km . . 
+    G=3.78541 # litre dans Gallon . . 
+    K=1.60934 # Km dans Mile . . 
+    L=0.264172 # Gallon dans litre . . 
 
 ==
 
@@ -39,19 +48,25 @@ form==
 ==
 
 evaluator==
-
-f=lambda C:inputbox.value
+M=0.621371 # Mile dans Km
+G=3.78541 # litre dans Gallon
+K=1.60934 # Km dans Mile
+L=0.264172 # Gallon dans litre
+sol=lambda C:100/((C/G)/L)
+f=lambda C:eval(inputbox.value)
 r=True
+delta= -100
 try:
-    if f(40)== sol(40) :
+    delta=f(40) - sol(40)
+    if -0.01 < delta < 0.01 :
         r=True
 
 except :
     r=False
 if r:
-    grade = (100, '<span class="success-state">Good 👏👏👏</span>')
+    grade = (100, '<span class="success-state">Good 👏👏👏'+str(delta)+'</span>')
 else:
-    grade = (0, '<span class="error-state">Bad answer 👎👎👎</span>')
+    grade = (0, '<span class="error-state">Bad answer 👎👎👎 '+str(delta)+'</span>')
 ==
 
 
