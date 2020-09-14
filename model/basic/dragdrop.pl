9c65fcc024700b1e66ba593cd6f19b85b751126a
David DOYEN <David.Doyen@u-pem.fr>
2020-09-14 16:18:24 +0200

    ddoyen modified Yggdrasil/model/basic/dragdrop.pl

---
+++ model/basic/dragdrop.pl
@@ -0,0 +1,19 @@
+extends = /model/basic.pl
+
+radio =: RadioGroup
+radio.decorator = CustomRadio
+
+title =
+
+
+headerbefore ==
+labels = []
+for content in labelscontent.splitlines())
+    labels.append(CustomDragDrop.Label(content=content)=
+
+==
+
+form ==
+
+==
+
