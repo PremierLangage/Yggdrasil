70419be06641913b668e562ea6246f34cffc623e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-14 15:12:33 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/dominique/bof/exempleQCM.pl

---
+++ ComputerScience/python/AP1-1920/drafts/dominique/bof/exempleQCM.pl
@@ -0,0 +1,28 @@
+
+extends = /model/qcm/qcm.pl
+
+good==
+1
+2
+4
+3
+==
+
+bad==
+dqqsd
+é"z
+qzsd
+$$\Latex$$
+==
+title = Un exemple pour Marie 
+
+text==
+
+Lesquels sont des entiers.
+==
+
+nb=4
+nbtrue=1
+
+
+
