5a99d701c4bb853dd55952a372d37ebbb1a2a8fd
David DOYEN <David.Doyen@u-pem.fr>
2020-09-14 18:40:21 +0200

    ddoyen modified Yggdrasil/model/basic/matchlist.pl

---
+++ model/basic/matchlist.pl
@@ -0,0 +1,20 @@
+extends = /model/basic.pl
+
+title =
+
+nbsample = 0
+
+matchlist =: MatchList
+
+@ /utils/components/matchlist.py [custommatchlist.py]
+matchlist.decorator = CustomMatchList
+
+form ==
+{{ matchlist|component }}
+==
+
+evaluator ==
+score = matchlist.eval()
+==
+
+settings.feedback = lightscore
