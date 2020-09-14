cfd616524acdfbfcf980c13b6189730b0b463cd3
David DOYEN <David.Doyen@u-pem.fr>
2020-09-14 14:42:32 +0200

    ddoyen modified Yggdrasil/demo/basic/sortlist1.pl

---
+++ demo/basic/sortlist1.pl
@@ -0,0 +1,17 @@
+extends = /model/basic/sortlist.pl
+
+title ==
+Exemple 1
+==
+
+text ==
+Classer les mots suivants dans l'ordre alphabétique.
+==
+
+sortedlist ==
+Abricot
+Avion
+Ballon
+Cartable
+==
+
