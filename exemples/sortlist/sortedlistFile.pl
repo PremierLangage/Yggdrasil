ab5d67606b0c1b9aa7c6687f3e6558eb98124469
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-15 11:41:21 +0200

    drevuz modified Yggdrasil/exemples/sortlist/sortedlistFile.pl

--- exemples/sortlist/sortedlistFile.pl
+++ exemples/sortlist/sortedlistFile.pl
@@ -9,17 +9,12 @@ En utilisant Drag and Drop, rangez les valeurs dans l'ordre.
 
 before==
 import csv
-import random
-delimiter=",;:" if "delimiter" not in globals() else delimiter 
-nbsample = 4 if "nbsample" not in globals() else int(nbsample)
 
 with open("data.csv","r") as csvfile:
     reader = csv.DictReader(csvfile)
     # Lecture de la column dans l'ordre du fichier
-    lue = [ row[column] for row in reader] 
-    # ordered sample from file 
-    lr=random.sample(range(len(lue)),nbsample)    
-    sortedlist=[ lue[i]  for i in sorted(lr) ]
+    sortedlist = [ row[column] for row in reader] 
+   
 
 ==
 
