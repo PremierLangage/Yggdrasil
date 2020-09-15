b928dac9dedb3403e5bf7bfeccf04f01f9da8350
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-15 12:31:27 +0200

    drevuz modified Yggdrasil/Mathematics/Magdalena/anothersortlistmathematiciens.pl

--- Mathematics/Magdalena/anothersortlistmathematiciens.pl
+++ Mathematics/Magdalena/anothersortlistmathematiciens.pl
@@ -2,6 +2,12 @@
 extends = /model/basic/sortlist.pl
 @ mathematiciens.csv [data.csv]
 column=nom # Choix de la columns 
+nbsample=6
+
+before==
+
+sortedlist.append("Moi")
+==
 
 text==
 En utilisant Drag and Drop, rangez les mathématiciens dans l'ordre de leur date de naissance.
