5c6bbc4104369db4417fa0ebb3a93bf8358f47b9
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-15 09:10:23 +0200

    drevuz modified Yggdrasil/ComputerScience/C_Initiation/fonctions_2/facto.pl

---
+++ ComputerScience/C_Initiation/fonctions_2/facto.pl
@@ -0,0 +1,74 @@
+title= Nombre Factoriel
+tag=function
+
+
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+
+text==  
+ On appelle nombre factoriel un enier $%n%$ pou lequel il existe un entier $%p%$ tel que $%\fact(p)=n%$<br>
+Ecrire une fonction `est_factoriel` qui reçoit un  entier et  renvoie  s'il est factoriel et 0 sinon.
+==
+
+editor.code==
+# Votre code svp
+
+==
+
+
+
+
+codebefore==
+
+#include <stdlib.h>
+#include <stdio.h>
+
+==
+
+solution==
+
+int est_factoriel( int n){
+    int nb=1,f=1;
+    while (f<n){
+        nb++;
+        f*=nb;
+    }
+	return n==f;
+}
+
+
+==
+
+
+codeafter==
+
+
+
+
+int main(void) {
+	int n ;
+scanf("%d",&n);
+if (est_factoriel(n)){
+	printf("%d  est factoriel\n",n);
+    }
+else
+	printf("%d  n'est pas factoriel\n",n);
+return 0;
+}
+==
+
+
+tests==
+[ ["Basique", "", "10"],
+  ["impssible", "", "-1"],
+  ["fatoriel", "", "6"],
+  ["Aléatoire", "",str(random.randint(1, 20))],
+  ["Aléatoire", "", str(random.choice([720, 40,3628800,40320]))] 
+  ]
+==
+
+
+
+
+
+
