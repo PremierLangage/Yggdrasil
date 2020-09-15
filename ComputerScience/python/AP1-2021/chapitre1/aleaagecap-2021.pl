5d7ba2fdfeb12e716d16fa41c6c27ad957953cfb
Marie Van Den Bogaard <mvdbogaa@ulb.ac.be>
2020-09-15 19:53:22 +0200

    mvan den bogaard modified Yggdrasil/ComputerScience/python/AP1-2021/chapitre1/aleaagecap-2021.pl

---
+++ ComputerScience/python/AP1-2021/chapitre1/aleaagecap-2021.pl
@@ -0,0 +1,62 @@
+# Boris Jabot 11/09/2019 OK
+author=Dominique Revuz 
+title= Lecture d'un entier
+
+tag=input|print|variable
+
+extends=/ComputerScience/python/template/soluce.pl
+ 
+
+@ /builder/before.py [builder.py]
+
+soluce==
+a=int(input())
+print("Le capitaine a %d ans. Dans {{X}} ans il aura %d ans." % (a,a+{{x}}))
+print("Le perroquet du capitaine a %d ans." % (((a+{{y}})//{{z}})-{{y}}))
+==
+
+code==
+# lecture au clavier et transformation en entier puis affectation
+age=int(input())
+# Le capitaine a __ ans. Dans {{X}} ans il aura __ ans.
+# Le perroquet du capitaine à __ ans.
+==
+
+
+
+text==
+Ecrivez un programme utilisant une unique variable **age** qui lit cette information au clavier puis affiche :
+
+	Le capitaine a __ ans. Dans {{X}} ans il aura ___ ans.
+
+Puis, sachant que dans {{Y}} ans, le perroquet aura {{Z}} de l’âge du capitaine (arrondi à l'année inférieure) quel est l'âge actuel du perroquet ? Le programme doit affiche:
+
+	Le perroquet du capitaine a __ ans.
+
+
+==
+
+before==
+
+import random 
+X,x= random.choice([("deux",2),("trois",3),("quatre",4) ,("cinq",5)])
+Y,y= random.choice([("deux",2),("trois",3),("quatre",4),("cinq",5)])
+Z,z= random.choice([("la moitié",2),("le tiers",3),("le quart",4)])
+
+
+import jinja2
+soluce= jinja2.Template(soluce).render(globals())
+code=  jinja2.Template(code).render(globals())
+==
+
+
+plsoluce==
+Test 1 |33
+Test 2 |66
+Test 4 |100000
+==
+
+
+
+
+
