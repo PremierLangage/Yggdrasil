ee313d4c91640e12dd2911fad24244730bd6f28c
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-16 08:29:38 +0200

    drevuz modified Yggdrasil/exemples/textselect/exemple1.pl

--- exemples/textselect/exemple1.pl
+++ exemples/textselect/exemple1.pl
@@ -7,9 +7,22 @@ builder =@ /builder/before.py
 
 
 selectable =: Text
-selectable.text = The quick brown fox jumps over the lazy dog.
+selectable.text = The quick brown fox jumps over the lazy red dog.
 selectable.mode = word
 # le caractère séparateur devrait êêtre espace par défaut.
+
+# Propriété du mot 
+predicat=lambda s:'o' in s
+
+# text= Select the names of animals in the sentence 
+#predicat= lambda s: in ["fox","dog"]
+
+# text= Select the names of colors in the sentence 
+#predicat= lambda s: in ["red","brown"]
+
+# text= Select the adjectives in the sentence 
+#predicat= lambda s: in ["red","brown","lazy","quick"]
+
 
 before==
 selectable.separator = ' '
@@ -27,26 +40,27 @@ form==
 {{ selectable|component}}
 ==
 
-predicat=lambda s:'o' in s
 
 evaluator==
 textselected=''
 f=eval(predicat)
+good=sum([ 1 for x in selectable.text.split(selectable.separator) if f(x)])
 bad = 0
 for e in selectable.selections:
     textselected +=  e['content']+' '
     if f(e['content']):
         e['css'] = "success-state"
+        good=good-1
     else:
         bad=bad+1
 
-if bad == 0:
+if bad == 0 and good==0:
     score = 100
     msg = '<span class="success-state animated pulse infinite">Good answer<span>'
 else:
     score = 0
     msg = '<span class="error-state animated pulse infinite">Bad answer</span>'
 
-msg+= textselected
+msg+= str(selectable)
 grade = (score, msg)
 ==
