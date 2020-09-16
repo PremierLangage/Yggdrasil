df0851ded1285c0872b8e65f92abac0a2739fe06
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-16 09:04:21 +0200

    drevuz modified Yggdrasil/exemples/textselect/exemplstance.pl

--- exemples/textselect/exemplstance.pl
+++ exemples/textselect/exemplstance.pl
@@ -1,19 +1,35 @@
 
-
-
-# THIS EXAMPLE IS DEPRECATED SINCE THE COMPONENT WILL BE RENAMED SOON
-
 @ /utils/sandboxio.py
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 
+hints % { "cid": "hints", "selector": "c-hint" }
+hints.label = **Bloqué ?** de l'aide ?.
+hints.confirmMessage ==
+The usage of a hint may affect your final score!
+==
+
+hints.shouldConfirm % false
+hints.confirmTitle = Are you sure you want a hint ?
+hints.confirmOkTitle = Yes
+hints.confirmNoTitle = No
+hints.items %=
+[
+    { "content": "Ce n'est pas un Pangramme." }
+]
+==
+
+
+
+
+
 selectable =: Text
 selectable.text ==
-The quick brown fox jumps over the lazy red dog.
-Portez ce vieux whisky au juge blond qui fume.
-Le vif zéphyr jubile sur les kumquats du clown gracieux.
-Monsieur jack vous dactylographiez bien mieux que votre ami Wolf.
+The quick brown fox jumps over the lazy red dog.<br/>
+Portez ce vieux whisky au juge blond qui fume.<br/>
+Le vif zéphyr jubile sur les kumquats du clown gracieux.<br/>
+Monsieur jack vous dactylographiez bien mieux que votre ami Wolf.<br/>
 De grace Jettez vite les kumquats du clown zéphyr. 
 ==
 
@@ -22,12 +38,12 @@ De grace Jettez vite les kumquats du clown zéphyr.
 selectable.mode = word
 # le caractère séparateur devrait êêtre espace par défaut.
 
-predicat=lambda s: "grace" not in s
+# Propriété du mot 
+predicat=lambda s: "grace" in s
 
 before==
-selectable.separator = ' '
+selectable.separator = '\n'
 
-# Propriété du mot 
 
 
 ==
@@ -41,6 +57,7 @@ Eliminer l'intrus
 ==
 
 form==
+{{ hints|component}}
 {{ selectable|component}}
 ==
 
