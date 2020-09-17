0ca4c34f74858ffad7b52048e0273fb825f78716
KILLIAN BLANCHARD <kblanc01@etud.u-pem.fr>
2020-09-17 16:05:53 +0200

    kblanchard modified Yggdrasil/AAAA/Killian/test.pl

--- AAAA/Killian/test.pl
+++ AAAA/Killian/test.pl
@@ -8,9 +8,8 @@ extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 text = Initialise 2 variables a et b à 2 et 3 puis additionne les dans une variables c
 
 pltest==
->>> a #Super c'est reussi
-2
->>> b
+>>> a #Raté
+>>> b #Encore raté
 3
 >>> c
 5
