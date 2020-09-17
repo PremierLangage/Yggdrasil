825dcede1feddcee0690e5fa32e82e4589d168c3
Doriann ALBERTIN <Doriann.Albertin@univ-mlv.fr>
2020-09-17 15:42:02 +0200

    dalbertin modified Yggdrasil/AAAA/DoriannAlbertin/exo_test.pl

---
+++ AAAA/DoriannAlbertin/exo_test.pl
@@ -0,0 +1,32 @@
+# OCE : test 11/09/2019 OK
+
+author=Albertin
+title= Un truc.
+tag = test
+
+extends=/ComputerScience/python/template/pltest.pl
+@ /utils/sandboxio.py
+
+text==
+
+Ecrire une fonction f patati patata
+
+==
+
+
+# comme ca cela se passera bien
+pltest==
+>>> f(1)
+1
+>>> f(2)
+3
+>>> f(4)
+7
+==
+
+
+
+
+
+
+
