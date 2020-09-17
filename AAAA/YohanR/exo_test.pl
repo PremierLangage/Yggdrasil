72b2111bd2488ebc1e742a9ffee1eeec28553948
www-data <www-data@pl.u-pem.fr>
2020-09-17 15:55:54 +0200

    Anonymous modified Yggdrasil/AAAA/YohanR/exo_test.pl

--- AAAA/YohanR/exo_test.pl
+++ AAAA/YohanR/exo_test.pl
@@ -1,7 +1,7 @@
 # OCE : test 11/09/2019 OK
 
 author=Yohan Rameau
-title= Exercice de test.
+title= Initialisation d'une variable.
 tag=test
 
 extends=/ComputerScience/python/template/pltest.pl
@@ -9,7 +9,7 @@ extends=/ComputerScience/python/template/pltest.pl
 
 text==
 
-Créer un tableau de nombre impair 
+Créer trois variables pim pam et pom contenant respectivement 1 2 et 3.
 
 ==
 
@@ -28,6 +28,20 @@ True
 >>> pom
 3
 ==
+
+pltest2==
+>>> pim # Pim vaut un
+1
+>>> "pam" in globals() #
+True
+>>> pam
+2
+>>> pom
+3
+==
+
+
+
 
 
 
