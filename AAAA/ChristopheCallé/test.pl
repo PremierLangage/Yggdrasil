c3e789da6451197d5c28c39b5e33366033056815
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2020-09-17 15:44:22 +0200

    ccalle modified Yggdrasil/AAAA/ChristopheCallé/test.pl

---
+++ AAAA/ChristopheCallé/test.pl
@@ -0,0 +1,30 @@
+
+author=ccalle
+title=
+tag = test
+
+extends=/ComputerScience/python/template/pltest.pl
+@ /utils/sandboxio.py
+
+text==
+Ecrire une fonction f patati patata
+==
+
+
+# comme ca cela se passera bien
+pltest==
+>>> f(1)
+1
+>>> f(2)
+3
+>>> f(4)
+7
+==
+
+
+
+
+
+
+
+
