d6da68fbd2b07fdfa9487cec1d63b91635bcf6b9
Doriann ALBERTIN <Doriann.Albertin@univ-mlv.fr>
2020-09-17 15:55:48 +0200

    dalbertin modified Yggdrasil/AAAA/DoriannAlbertin/exo_test.pl

--- AAAA/DoriannAlbertin/exo_test.pl
+++ AAAA/DoriannAlbertin/exo_test.pl
@@ -9,16 +9,29 @@ extends=/ComputerScience/python/template/pltest.pl
 text==
 
 Ecrire une fonction f qui à un entier n revoie la somme des entiers positifs strictement inférieurs à n qui ne soient multiples ni de 3 ni de 5.
-Votre fonction doit fonctionner pour n = 10**9
+Votre fonction doit fonctionner pour $$n = 10^9$$.
 
 ==
 
+code==
+def f(n):
+    n5 = (n-1)//5
+    n3 = (n-1)//3
+    n15 = (n-1)//15
+    return int((5*n5*(n5+1) + 3*n3*(n3+1) - 15*n15*(n15+1))/2)
+==
 
 
 # comme ca cela se passera bien
 pltest==
->>> f(10**9)
-1
+>>> f(10)
+23
+>>> f(300)
+20850
+>>> f(113)
+2954
+>>> f(10**9) # f(1 000 000 000)
+233333333166666656
 ==
 
 
