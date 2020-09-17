5ddc19caeaef4a541ff4ba9c1efd0dddd6d825ec
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-09-17 15:40:48 +0200

    adiaw modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/Premier_exo.pl

---
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/Premier_exo.pl
@@ -0,0 +1,44 @@
+# DR: test 24/09/2019 OK
+extends=../../templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+tag=Programmation|Python|Initialisation|Variable
+author = CC + DR
+
+title = Initialisation et constantes
+
+
+text==
+ Jeu de 3 dés  
+==
+
+
+
+editor.height = 80
+
+before== #|python| 
+import random 
+random.seed(seed)
+var = random.choice([1,2,3,4,5,6])
+
+pltest0="""
+>>> "{}" in globals() # La variable {} est elle définie ?
+True
+""".format(var, var)
+
+if type_var is None:
+    pltest1="""
+>>> type({}) is type(None) # La variables est elle du bon type ({}) ?
+True
+""".format(var, type_var, type_var)
+else:
+    pltest1="""
+>>> type({})=={} # La variables est elle du bon type ({}) ?
+True
+""".format(var, type_var, type_var)
+
+
+
+
+==
+
