fac8c7cacb73f00322539bc6e3dc9577eb6b9b79
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2020-09-17 16:05:59 +0200

    ccalle modified Yggdrasil/AAAA/ChristopheCallé/test.pl

--- AAAA/ChristopheCallé/test.pl
+++ AAAA/ChristopheCallé/test.pl
@@ -1,25 +1,25 @@
 
 author=ccalle
-title=
-tag = test
+title=Creation de dictionnaire
+tag=dict|variable
 
 extends=/ComputerScience/python/template/pltest.pl
 @ /utils/sandboxio.py
 
 text==
-Ecrire une fonction f patati patata
+Créer une variable `d` contenant un dictionnaire qui lui-même contient la valeur `5` dans la clé `"valeur"`.
 ==
-
 
 # comme ca cela se passera bien
 pltest==
->>> f(1)
-1
->>> f(2)
-3
->>> f(4)
-7
+>>> "valeur" in d
+True
+>>> d["valeur"] == 5
+True
+>>> len(d.keys()) == 1
+True
 ==
+
 
 
 
