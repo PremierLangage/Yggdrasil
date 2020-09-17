f78938d751683c8a89afef4c7f2e1811120dfadd
Doriann ALBERTIN <Doriann.Albertin@univ-mlv.fr>
2020-09-17 15:46:29 +0200

    dalbertin modified Yggdrasil/AAAA/DoriannAlbertin/exo_test.pl

--- AAAA/DoriannAlbertin/exo_test.pl
+++ AAAA/DoriannAlbertin/exo_test.pl
@@ -1,27 +1,24 @@
 # OCE : test 11/09/2019 OK
 
 author=Albertin
-title= Un truc.
-tag = test
+title= Project Euler 1
 
 extends=/ComputerScience/python/template/pltest.pl
 @ /utils/sandboxio.py
 
 text==
 
-Ecrire une fonction f patati patata
+Ecrire une fonction f qui à un entier n revoie la somme des entiers positifs strictement inférieurs à n qui ne soient multiples ni de 3 ni de 5.
+Votre fonction doit fonctionner pour n = 10**9
 
 ==
 
 
+
 # comme ca cela se passera bien
 pltest==
->>> f(1)
+>>> f(10**9)
 1
->>> f(2)
-3
->>> f(4)
-7
 ==
 
 
