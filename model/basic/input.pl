7acbf3f98164a570fd47c89395b4b8ce086050c5
David DOYEN <David.Doyen@u-pem.fr>
2020-09-18 19:53:01 +0200

    ddoyen modified Yggdrasil/model/basic/input.pl

--- model/basic/input.pl
+++ model/basic/input.pl
@@ -48,5 +48,5 @@ if any([input.value.casefold() == item.casefold() for item in lstsol]):
 else:
     score = 0
 
-feedback = str([input.value.casefold() for item in lstsol])+str([item.casefold() for item in lstsol])
+feedback = str([(input.value).casefold() for item in lstsol])+str([item.casefold() for item in lstsol])
 ==
