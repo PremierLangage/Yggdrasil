29ecf7500a6a9aac5f35dee305027d6433526c40
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-18 17:27:40 +0200

    nborie modified Yggdrasil/ComputerScience/Games/MiniBrain/mini_brain_add.pl

--- ComputerScience/Games/MiniBrain/mini_brain_add.pl
+++ ComputerScience/Games/MiniBrain/mini_brain_add.pl
@@ -32,9 +32,9 @@ a = randint(6, 11)
 b = randint(a+1, a+6)
 c = randint(b+1, b+6)
 
-minibrain_action_before=[ ['test simple aléatoire', 'st '+str(val1)+' $'+str(a)+'\nst '+str(val2)+' $'+str(b)] ]
+minibrain_action_before="[ ['test simple aléatoire', 'st '+str(val1)+' $'+str(a)+'\nst '+str(val2)+' $'+str(b)] ]"
 
-minibrain_check_after = [ [(c, val1+val2)] ]
+minibrain_check_after = '[ [(c, val1+val2)] ]'
 
 ==
 
