e8e2b7e2aac3c83ea7e4ce4797c18f910d88eac9
David DOYEN <David.Doyen@u-pem.fr>
2020-09-18 15:14:09 +0200

    ddoyen modified Yggdrasil/model/basic/input_csv.pl

---
+++ model/basic/input_csv.pl
@@ -0,0 +1,29 @@
+extends = /model/basic.pl
+
+headerbefore ==
+
+==
+
+input =: Input
+
+form ==
+{{ input|component }}
+==
+
+settings.feedback = rightwrong
+
+evaluator ==
+if isinstance(sol, str):
+    if '\n' in sol:
+        lstsol = sol.splitlines()
+    else:
+        lstsol = [sol]
+else:
+    lstsol = sol
+
+if any([input.value.casefold() == item.casefold() for item in lstsol]):
+    score = 100
+else:
+    score = 0
+==
+
