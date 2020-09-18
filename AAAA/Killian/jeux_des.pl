50a1053ea9a8e1f0212e697767e3168b11c6eca9
KILLIAN BLANCHARD <kblanc01@etud.u-pem.fr>
2020-09-18 09:13:21 +0200

    kblanchard modified Yggdrasil/AAAA/Killian/jeux_des.pl

--- AAAA/Killian/jeux_des.pl
+++ AAAA/Killian/jeux_des.pl
@@ -5,7 +5,7 @@ title = Premier Test
 extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 @ /builder/before.py [builder.py]
 
-text = Initialiser une liste "attaquant" de longueur 3 et une liste "defenseur" de longueur 2 qui possèdent chacune des entiers aléatoire de 1 à 6, et écrire une fonction "fight" qui affichera : "L'attaquand a gagné !" si la liste "attaquand" possède le chiffre le plus grand des 2 listes, "Le défenseur a gagné !" si c'est la liste défenseur, "Personne n'a gagné..." sinon.
+text = Initialiser une liste "attaquant" de longueur 3 et une liste "defenseur" de longueur 2 qui possèdent chacune des entiers aléatoire de 1 à 6, et écrire une fonction "fight" qui affichera : "L'attaquant a gagné !" si la liste "attaquand" possède le chiffre le plus grand des 2 listes, "Le défenseur a gagné !" si c'est la liste défenseur, "Personne n'a gagné..." sinon.
 
 pltest0==
 from random import randint
@@ -19,7 +19,7 @@ from random import randint
         False
 
 >>> fight([3,4,5], [2,2])
-L'attaquand a gagné !
+L'attaquant a gagné !
 >>> fight([1,2,3], [3,4])
 Le défenseur a gagné !
 >>> fight([1,1,3], [1,3])
