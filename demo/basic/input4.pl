66a726f42e88778207717a03edb141f8260a2764
David DOYEN <David.Doyen@u-pem.fr>
2020-09-18 16:31:36 +0200

    ddoyen modified Yggdrasil/demo/basic/input4.pl

--- demo/basic/input4.pl
+++ demo/basic/input4.pl
@@ -2,3 +2,16 @@ extends = /model/basic/input.pl
 
 @ pays_europe.csv [data.csv]
 
+title ==
+Exemple 2
+==
+
+text ==
+Quel pays a pour capitale {{ row['capitale'] }} ?
+==
+
+sol ==
+France
+la France
+==
+
