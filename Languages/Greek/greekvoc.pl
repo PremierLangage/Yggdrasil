08f6d544b59b849d42614f18b7933fa1e48b5ce6
David DOYEN <David.Doyen@u-pem.fr>
2020-09-20 15:30:51 +0200

    ddoyen modified Yggdrasil/Languages/Greek/greekvoc.pl

--- Languages/Greek/greekvoc.pl
+++ Languages/Greek/greekvoc.pl
@@ -10,6 +10,7 @@ before ==
 ==
 
 text ==
+{{ mot }}
 Que signifie le mot suivant ?
 <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/gfs-porson" type="text/css"/> 
 <div style="text-align:center;font-size: x-large;font-family:'GFSPorsonRegular';">
