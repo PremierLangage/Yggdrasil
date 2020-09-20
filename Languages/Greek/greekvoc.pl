1bcf141f1fa3e9bb64b2df1bf3c28cec1388d356
David DOYEN <David.Doyen@u-pem.fr>
2020-09-20 15:29:22 +0200

    ddoyen modified Yggdrasil/Languages/Greek/greekvoc.pl

--- Languages/Greek/greekvoc.pl
+++ Languages/Greek/greekvoc.pl
@@ -1,4 +1,4 @@
-extends = /model/basic.pl
+extends = /model/basic/input.pl
 
 @ grec_voc_1.csv [data.csv]
 
@@ -18,9 +18,6 @@ import csv
 with open('data.csv',newline='') as file:
     rows=list(csv.DictReader(file,delimiter=';'))
 
-item=rd.choice(rows)
-mot=" ".join([item['article'],item['mot']])
-traduction=item['traductions']
 choices=[item['traductions'] for item in rows]
 
 input.autocomplete = choices
@@ -30,16 +27,14 @@ text ==
 Que signifie le mot suivant ?
 <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/gfs-porson" type="text/css"/> 
 <div style="text-align:center;font-size: x-large;font-family:'GFSPorsonRegular';">
-{{mot}}
+{{article}} {{mot}}
 </div>
 ==
 
-evaluator ==
-if input.value==traduction:
-    grade=(100,"")
-else:
-    grade=(0,traduction)
+sol ==
+{{traductions}}
 ==
+
 
 
 
