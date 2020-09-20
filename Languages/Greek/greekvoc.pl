2d0a4216cf15bf8da0b9d7840beeddddf19b0f61
David DOYEN <David.Doyen@u-pem.fr>
2020-09-20 15:32:35 +0200

    ddoyen modified Yggdrasil/Languages/Greek/greekvoc.pl

--- Languages/Greek/greekvoc.pl
+++ Languages/Greek/greekvoc.pl
@@ -13,7 +13,6 @@ all_rows
 ==
 
 text ==
-{{ all_rows }}
 Que signifie le mot suivant ?
 <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/gfs-porson" type="text/css"/> 
 <div style="text-align:center;font-size: x-large;font-family:'GFSPorsonRegular';">
