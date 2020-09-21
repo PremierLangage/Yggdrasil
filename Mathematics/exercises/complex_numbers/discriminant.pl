557f95b3feabc06e466b6750524bd4d12b80a82f
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-09-21 10:09:18 +0200

    cpetitjean modified Yggdrasil/Mathematics/exercises/complex_numbers/discriminant.pl

--- Mathematics/exercises/complex_numbers/discriminant.pl
+++ Mathematics/exercises/complex_numbers/discriminant.pl
@@ -5,8 +5,6 @@ title = Discriminant d'une équation quadratique
 lang = fr
 
 before ==
-keyboards_JSON['virtualKeyboards']="complexes"
-input.config = keyboards_JSON
 
 a=rand_complex_int(3)
 b=rand_complex_int(3)
@@ -22,6 +20,7 @@ text ==
 Calculer le discriminant de l'équation $! {{latexP}} = 0 !$.
 ==
 
+input.virtualKeyboards = complex
 
 evaluator==
 score,_,feedback=ans_complex(input.value,sol,form="cartesian")
