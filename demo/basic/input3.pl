f36622ccfeac9cbc723b55976f495771d60e58bb
David DOYEN <David.Doyen@u-pem.fr>
2020-09-22 19:08:03 +0200

    ddoyen modified Yggdrasil/demo/basic/input3.pl

--- demo/basic/input3.pl
+++ demo/basic/input3.pl
@@ -1,32 +1,19 @@
 extends = /model/basic/input.pl
 
 title ==
-Exemple 3
+Exemple 2
 ==
-
-delimiter = |
 
 text ==
-Traduire en latin le mot en gras (avec le bon cas).
-{{solution}}
-{{ phrase }}
+Quel élément chimique a pour symbole **O** ?
 ==
 
-sol ==
-{{ solution }}
+solution ==
+Oxygène
 ==
 
-data ==
-phrase|solution
-Le **maître** appelle l'esclave de son fils.|dominus
-Le maître appelle l'**esclave** de son fils.|servum
-Le maître appelle l'esclave de son **fils**.|filii
-**Esclave**, donne de la nourriture aux chevaux.|serve
-Esclave, donne de la nourriture aux **chevaux**.|equis
-Les **fils** des esclaves devenaient eux-mêmes des esclaves.|filii
-Les fils des **esclaves** devenaient eux-mêmes des esclaves.|servorum
-L'**esclave** donne un livre au fils du maître.|servus
-L'esclave donne un **livre** au fils du maître.|librum
-L'esclave donne un livre au **fils** du maître.|filio
-L'esclave donne un livre au fils du **maître**.|domini
-==
+measure = distance
+
+tolerance % 1
+
+
