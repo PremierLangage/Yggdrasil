aa6fc846d66d02d780509846a9589e00c20fa6f9
David DOYEN <David.Doyen@u-pem.fr>
2020-09-22 10:56:19 +0200

    ddoyen modified Yggdrasil/Languages/English/listening_sentences.pl

--- Languages/English/listening_sentences.pl
+++ Languages/English/listening_sentences.pl
@@ -43,31 +43,37 @@ function playAudio() {
 ==
 
 evaluator ==
-def minimumEditDistance(s1,s2):
-    if len(s1) > len(s2):
-        s1,s2 = s2,s1
-    distances = range(len(s1) + 1)
-    for index2,char2 in enumerate(s2):
-        newDistances = [index2+1]
-        for index1,char1 in enumerate(s1):
-            if char1 == char2:
-                newDistances.append(distances[index1])
+def levenshteinDistance(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    lensum = float(m + n)
+    d = []           
+    for i in range(m+1):
+        d.append([i])        
+    del d[0][0]    
+    for j in range(n+1):
+        d[0].append(j)       
+    for j in range(1,n+1):
+        for i in range(1,m+1):
+            if str1[i-1] == str2[j-1]:
+                d[i].insert(j,d[i-1][j-1])           
             else:
-                newDistances.append(1 + min((distances[index1],
-                                             distances[index1+1],
-                                             newDistances[-1])))
-        distances = newDistances
-    return distances[-1]
+                minimum = min(d[i-1][j]+1, d[i][j-1]+1, d[i-1][j-1]+2)         
+                d[i].insert(j, minimum)
+    ldist = d[-1][-1]
+    ratio = (lensum - ldist)/lensum
+    return {'distance':ldist, 'ratio':ratio}
 
 dist=minimumEditDistance(sol,input.value)
 
-if dist<0.7*len(sol):
+if levenshteinDistance(input.value, sol)['ratio']:
     grade=(100,sol)
 else:
     grade=(0,sol)
 ==
 
 settings.feedback = rightwrong
+
 
 
 
