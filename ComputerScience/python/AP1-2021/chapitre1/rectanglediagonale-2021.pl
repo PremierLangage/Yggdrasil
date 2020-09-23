063d3234941ce2df57c39c7172ed908a84bed7ee
Marie VAN DEN BOGAARD <Marie.Vandenbogaard@univ-mlv.fr>
2020-09-23 18:55:17 +0200

    mvan den bogaard1 modified Yggdrasil/ComputerScience/python/AP1-2021/chapitre1/rectanglediagonale-2021.pl

---
+++ ComputerScience/python/AP1-2021/chapitre1/rectanglediagonale-2021.pl
@@ -0,0 +1,42 @@
+
+
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Diagonale d'un rectangle
+
+tag=input|print|puissance
+
+text==  
+Les dimensions d'un rectangle sont entrées par l'utilisateur, le programme testeur fournira les deux valeurs.
+
+Ecrire un programme qui calcule et affiche le périmètre et la surface du rectangle puis
+la longueur de la diagonale
+sous la forme :
+
+Le périmètre est ?? et la surface est ?? !
+La diagonale mesure ?? !
+(pensez à **)
+==
+
+soluce==
+l=int(input())
+la=int(input())
+print("Le périmètre est ",2*(l+la)," et la surface est ",l*la,"!")
+print("la diagonale mesure ",(l*l+la*la)**0.5,"!")
+==
+mplsoluce0==
+essai1
+2 
+3
+==
+
+mplsoluce1==
+essai1
+2 
+3
+==
+before=
+
