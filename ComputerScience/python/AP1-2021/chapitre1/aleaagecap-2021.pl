495efeb8e90d1950b54d801394ab7364c58de9d6
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-24 16:33:57 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-2021/chapitre1/aleaagecap-2021.pl

--- ComputerScience/python/AP1-2021/chapitre1/aleaagecap-2021.pl
+++ ComputerScience/python/AP1-2021/chapitre1/aleaagecap-2021.pl
@@ -12,14 +12,14 @@ extends=/ComputerScience/python/template/soluce.pl
 soluce==
 a=int(input())
 print("Le capitaine a %d ans. Dans {{X}} ans il aura %d ans." % (a,a+{{x}}))
-print("Le perroquet du capitaine a %d ans." % (((a+{{y}})//{{z}})-{{y}}))
+print("Le perroquet du capitaine a actuellement %d ans." % (((a+{{y}})//{{z}})-{{y}}))
 ==
 
 code==
 # lecture au clavier et transformation en entier puis affectation
 age=int(input())
 # Le capitaine a __ ans. Dans {{X}} ans il aura __ ans.
-# Le perroquet du capitaine a __ ans.
+# Le perroquet du capitaine a actuellement __ ans.
 ==
 
 
@@ -27,11 +27,21 @@ age=int(input())
 text==
 Ecrivez un programme utilisant une unique variable **age** qui lit cette information au clavier puis affiche :
 
-	Le capitaine a __ ans. Dans {{X}} ans il aura ___ ans.
+	"Le_capitaine_a_XX_ans._Dans_{{X}}_ans_il_aura_YY_ans."
 
-Puis, sachant que dans {{Y}} ans, le perroquet aura {{Z}} de l’âge du capitaine (arrondi à l'année inférieure) quel est l'âge actuel du perroquet ? Le programme doit afficher:
+sur **une ligne** (les "_" sont des espaces). 
+Puis, sachant que dans {{Y}} ans, le perroquet aura {{Z}} de l’âge du capitaine (arrondi à l'année inférieure)
+ quel est l'âge actuel du perroquet ? Le programme doit afficher:
 
-	Le perroquet du capitaine a __ ans.
+	"Le_perroquet_du_capitaine_a_actuellement_ZZ_ans.
+
+
+Une petite remarque sur print : 
+
+	print("(",3,")") # affiche ( 3 ) la virgule ajoute un espace.
+	print("(%d)",3) # affiche (3) aucun espace.   
+	a=3
+	print(f"({a})") # affiche (3)   
 
 
 ==
@@ -55,6 +65,7 @@ Test 1 |33
 Test 2 |66
 Test 4 |100000
 ==
+
 
 
 
