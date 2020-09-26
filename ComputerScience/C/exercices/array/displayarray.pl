60b248358f1f331ec7a1156665266be54f26ab32
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-26 23:48:43 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/array/displayarray.pl

--- ComputerScience/C/exercices/array/displayarray.pl
+++ ComputerScience/C/exercices/array/displayarray.pl
@@ -21,15 +21,13 @@ author=Nicolas Borie
 title=Afficher le contenu d'un tableau
 tag=array|affichage|tableau|fonction
 
-
 text==
-Écrire une fonction qui affiche le contenu d'un tableau C. Cette
+Écrire une fonction qui affiche le contenu d'un tableau d'entiers C. Cette
 fonction prendra en paramèrtre un tableau d'entiers ainsi que le
-nombre d'éléments contenu dans ce tableau. Tester votre fonction en
-l'appelant correctement avec le tableau défini comme varible locale de
-la fonction *main*. On commencera l'affichage avec un crochet ouvrant,
-puis les entiers séparé par une virgule et enfin un crochet fermant en
-fin de tableau.
+nombre d'éléments contenus dans ce tableau. On commencera l'affichage 
+avec un crochet ouvrant, puis les entiers séparés par une virgule 
+s'ils sont plusieurs et enfin un crochet fermant en fin de tableau. 
+Ne pas mettre de retour chariot en fin d'affichage.
 ==
 
 code_before==
@@ -37,19 +35,18 @@ code_before==
 ==
 
 code_after==
+int main(int argc, char* argv[]){
+  int tab[] = {6, 2, 3, 7, 7, 9, 2};
 
+  display_array(tab, 7);
+  return 0;
+}
 ==
 
 editor.code==
 #include ...
 
 void display_array(int* array, int nb_term){
-  /* votre code ici... */
-}
-
-int main(int argc, char* argv[]){
-  int tab[] = {6, 2, 3, 7, 7, 9, 2};
-
   /* votre code ici... */
 }
 ==
@@ -60,17 +57,14 @@ solution==
 void display_array(int* array, int nb_term){
   int i;
 
+  if (nb_term == 0){
+    printf("[]");
+    return ;
+  }
   printf("[");
   for (i=0 ; i<nb_term-1 ; i++)
     printf("%d, ", array[i]);
   printf("%d]\n", array[nb_term-1]);
-}
-
-int main(int argc, char* argv[]){
-  int tab[] = {6, 2, 3, 7, 7, 9, 2};
-
-  display_array(tab, 7);
-  return 0;
 }
 ==
 
