ae731a12ca9e6cbeca3212c7b1a35397883deb7a
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-09-27 11:51:04 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/array/sum_of_positives.pl

--- ComputerScience/C/exercices/array/sum_of_positives.pl
+++ ComputerScience/C/exercices/array/sum_of_positives.pl
@@ -1,13 +1,28 @@
-# Copyright 2017 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2017 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Somme des éléments positifs d'un tableau
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
+
 title=Somme des éléments positifs d'un tableau
-tag=array
-extends=/ComputerScience/C/template/stdsandboxC.pl
+tag=tableau|parcours|condition
 
 text==
+
 Écrire une fonction C **sum_of_positives** qui prend en paramètre un
 tableau d'entiers ansi que son nombre d'éléments. La fonction
 retournera la somme des éléments positifs contenus dans le tableau. Il ne faut 
@@ -19,11 +34,9 @@ editor.code==
 ... sum_of_positives(...){
     /* Votre code ici */
 }
-
 ==
 
-solution==
-
+solution==#|c|
 int sum_of_positives(int* tab, int taille){
   int i;
   int somme=0;
@@ -35,12 +48,9 @@ int sum_of_positives(int* tab, int taille){
   
   return somme;
 }
-
-
 ==
 
-codeafter==
-
+code_after==#|c|
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -60,15 +70,14 @@ int main(int argc, char* argv[]){
 
 ==
 
-tests==
-[ ["Exécution simple", "1", ""],
-  ["Quelques éléments", "12 -3 52 0 41", ""],
-  ["Tableau vide", "", ""],
-  ["Aléatoire", " ".join([str(random.randint(-100, 100)) for i in range(random.randint(5, 10))]), ""],
-  ["Aléatoire", " ".join([str(random.randint(-100, 100)) for i in range(random.randint(5, 10))]), ""],
-  ["Aléatoire", " ".join([str(random.randint(-100, 100)) for i in range(random.randint(5, 10))]), ""],
-  ["Aléatoire", " ".join([str(random.randint(-100, 100)) for i in range(random.randint(5, 10))]), ""],
-  ["Aléatoire", " ".join([str(random.randint(-100, 100)) for i in range(random.randint(5, 10))]), ""] ]
+checks_args_stdin==#|python|
+[ ["Exécution simple", ["1"], ""],
+  ["Quelques éléments", ["12", "-3", "52", "0", "41"], ""],
+  ["Tableau vide", [], ""],
+  ["Test aléatoire 1", [str(randint(-100, 100)) for i in range(randint(5, 10))], ""],
+  ["Test aléatoire 2", [str(randint(-100, 100)) for i in range(randint(5, 10))], ""],
+  ["Test aléatoire 3", [str(randint(-100, 100)) for i in range(randint(5, 10))], ""],
+  ["Test aléatoire 4", [str(randint(-100, 100)) for i in range(randint(5, 10))], ""],
+  ["Test aléatoire 5", [str(randint(-100, 100)) for i in range(randint(5, 10))], ""] ]
 ==
-
 
