d5bc8213ee8e7e124b9d0e4b769254ca289b1ec5
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-09-29 13:57:28 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/plateau.pl

--- ComputerScience/C_Initiation/tableaux_3/plateau.pl
+++ ComputerScience/C_Initiation/tableaux_3/plateau.pl
@@ -3,7 +3,7 @@
 title=Plus long plateau
 tag=tableau
 extends=/ComputerScience/C/template/stdsandboxC.pl
-
+author= 
 text==
 Ecrire une fonction de protopype `int plateau(int tab[],int taille,int *debut );` qui reçoit un tableau d'entiers er sa taille,
  renvoie le longueur de la plus longue partie du tableau constitué d'éléments égaux, et place à l'adresse $%{ \tt debut}%$
@@ -72,6 +72,7 @@ tests==
   ["aléatoire ", ""," ".join([str(random.randint(10,20)) for i in range(random.randint(5,10))])+"6"*14+" ".join([str(random.randint(-10,10)) for i in range(random.randint(5,10))])],
   ["aléatoire ", ""," ".join([str(random.randint(-20,20)) for i in range(random.randint(5,20))])], ]
 ==
+
 
 
 
