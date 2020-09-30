2aea78000f89eb02ac9065a62649276418ace9f3
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-09-30 09:00:26 +0200

    drevuz modified Yggdrasil/ComputerScience/C_Initiation/fonctions_2/saisie3.pl

--- ComputerScience/C_Initiation/fonctions_2/saisie3.pl
+++ ComputerScience/C_Initiation/fonctions_2/saisie3.pl
@@ -68,7 +68,7 @@ int main(int argc, char* argv[])
 
 tests==
 [["Exécution simple", "1 9", "5"],
- ["Quelques éléments", "12 2", "Il y a quelque entier 1 qui sont dans 3 ce texte"],
+ ["Quelques éléments", "12 2", "Il y a quelque entiers 1 qui sont dans 3 ce texte, celui la 3.1415 c\'est un float donc 2 entiers."],
  ["Aléatoire",' '.join([str(random.randint(1,4)),str(random.randint(11, 24))]), "XX0XX-1XXX5"]
  ]
 ==
