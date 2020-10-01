7190b0a5998197e18c305efc796611160c135c2c
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-01 16:22:42 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/plateau.pl

--- ComputerScience/C_Initiation/tableaux_3/plateau.pl
+++ ComputerScience/C_Initiation/tableaux_3/plateau.pl
@@ -5,7 +5,7 @@ tag=tableau
 extends=/ComputerScience/C/template/stdsandboxC.pl
 author= 
 text==
-Ecrire une fonction de protopype `int plateau(int tab[],int taille,int *debut );` qui reçoit un tableau d'entiers er sa taille,
+Ecrire une fonction de protopype `int plateau(int tab[],int taille,int *debut );` qui reçoit un tableau d'entiers et sa taille,
  renvoie le longueur de la plus longue partie du tableau constitué d'éléments égaux, et place à l'adresse $%{ \tt debut}%$
  l'indice de son premier élément. <br> En cas d'égalité, on gardera le plus petit indide
 ==
@@ -72,6 +72,7 @@ tests==
   ["aléatoire ", ""," ".join([str(random.randint(10,20)) for i in range(random.randint(5,10))])+"6"*14+" ".join([str(random.randint(-10,10)) for i in range(random.randint(5,10))])],
   ["aléatoire ", ""," ".join([str(random.randint(-20,20)) for i in range(random.randint(5,20))])], ]
 ==
+
 
 
 
