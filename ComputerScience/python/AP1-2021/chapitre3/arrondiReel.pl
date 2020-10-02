9d3d5626a18eb4fe2656defd7982043ddc6054ae
CHRISTOPHE CALLE <ccalle@etud.u-pem.fr>
2020-10-02 15:40:01 +0200

    ccalle modified Yggdrasil/ComputerScience/python/AP1-2021/chapitre3/arrondiReel.pl

--- ComputerScience/python/AP1-2021/chapitre3/arrondiReel.pl
+++ ComputerScience/python/AP1-2021/chapitre3/arrondiReel.pl
@@ -4,7 +4,7 @@
 # Heritage d'un type d'exercice 
 extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
-tag=boucle|float|somme
+tag=boucle|float|somme|math
 
 title = Approximation de $%\frac{\pi^2}{6}%$
 
