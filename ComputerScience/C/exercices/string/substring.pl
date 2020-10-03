6e51309ca786c160985ed4385a9af17d8005d424
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-04 00:41:42 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/string/substring.pl

--- ComputerScience/C/exercices/string/substring.pl
+++ ComputerScience/C/exercices/string/substring.pl
@@ -1,3 +1,20 @@
+#*****************************************************************************
+#  Copyright (C) 2017 Dominique Revuz <dr@univ-mlv.fr>
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
 # Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
 # Copyright 2017 DR
 #
@@ -61,5 +78,6 @@ tests==
   ["Aléatoire", "".join([chr(random.randint(97,102)) for i in range(50)]) + " " + "".join([chr(random.randint(97,102)) for i in range(3)]), "" ],
   ["Aléatoire", "".join([chr(random.randint(97,102)) for i in range(50)]) + " " + "".join([chr(random.randint(97,102)) for i in range(3)]), "" ] ]
 ==
+
 
 
