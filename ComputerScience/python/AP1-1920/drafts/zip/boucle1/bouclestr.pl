706227c8ddaa60c5f6eb386c0906505318ba58e4
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-05 20:05:07 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/boucle1/bouclestr.pl

--- ComputerScience/python/AP1-1920/drafts/zip/boucle1/bouclestr.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/boucle1/bouclestr.pl
@@ -11,7 +11,7 @@ title = Liste des multiples
 
 text==  
 
-Ecrire un programme qui lit un  entier  positif et affiche la chaine de caractères formeé des n premiers multiples de 17
+Ecrire un programme qui lit un  entier  positif et affiche la chaine de caractères formée des n premiers multiples de 17
 (si n vaut 4 on affiche 0173451).
 Si l'entier est négatif, on ne fait rien.
 
@@ -32,6 +32,7 @@ tres court |0
 tres tres court |-42
 long | 110
 ==
+
 
 
 
