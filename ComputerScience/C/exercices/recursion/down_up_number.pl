3f53d6874e2562dc917e646d7e44fdeded014c22
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-05 10:41:20 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/recursion/down_up_number.pl

--- ComputerScience/C/exercices/recursion/down_up_number.pl
+++ ComputerScience/C/exercices/recursion/down_up_number.pl
@@ -1,14 +1,27 @@
-# Copyright 2017 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2017 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Égrener des nombres récursivements
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
+
 title=Égrener des nombres récursivements
 tag=function|recursion
-extends=/ComputerScience/C/template/stdsandboxC.pl
 
 text==
-
 Écrire une fonction récurssive **bas_haut** qui prend en argument un
 entier *n* et qui affiche sur une seule ligne sans retour à la ligne
 tout les entiers de *n* à *1* puis remonte à *n*. Chaque monbre sera
@@ -17,10 +30,11 @@ séparé par un espace.
 Pour *n* qui vaut *7*, cela donnera
 
     7 6 5 4 3 2 1 2 3 4 5 6 7 
-    
+
+
 ==
 
-editor.code==
+editor.code==#|c|
 #include <stdio.h>
 
 ... bas_haut(...){
@@ -28,8 +42,7 @@ editor.code==
 }
 ==
 
-solution==
-
+solution==#|c|
 #include <stdio.h>
     
 void bas_haut(int n){
@@ -43,13 +56,14 @@ void bas_haut(int n){
     printf(" %d", n);
   }	
 }
-
 ==
-    
-codeafter==
 
-#include <stdlib.h>    
-    
+code_before==#|c|
+==
+
+code_after==#|c|
+#include <stdlib.h>
+
 int main(int argc, char* argv[]){
   int a = atoi(argv[1]);
 
@@ -58,14 +72,12 @@ int main(int argc, char* argv[]){
 }
 ==
 
-    
-tests==
-
+checks_args_stdin==#|python|
 [["Basique", "1", ""], 
  ["Moyen", "5", ""],
  ["Erreur", "-2", ""],
  ["Aléatoire", str(random.randint(6, 15)), ""],
  ["Aléatoire", str(random.randint(6, 15)), ""]]
-
 ==
+
 
