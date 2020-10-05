a17bec6179f88edd6e36040ba1f1098be93c8b43
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-05 20:02:39 +0200

    drevuz modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles/dynafor003.pl

---
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/boucles/dynafor003.pl
@@ -0,0 +1,26 @@
+
+
+# DR j'ai fixé la seed dans cet exercice  
+# il est donc statique
+
+extends=../../../templates/looptemplate.pl
+
+
+
+difficulty=2
+taboo=while
+needed=for
+
+title=Boucle for forever
+
+text= Pour ne pas avoir un warning stupid
+
+plsoluce==
+Vérification du comportement fixé |
+==
+
+
+
+
+
+
