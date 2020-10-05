faed96208a7c5d679f7042353f626516e36393c0
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-05 11:23:07 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/recursion/factorial.pl

--- ComputerScience/C/exercices/recursion/factorial.pl
+++ ComputerScience/C/exercices/recursion/factorial.pl
@@ -1,6 +1,20 @@
-# Copyright 2018 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2018 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Factorielle d'un entier positif
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
 title=Factorielle d'un entier positif
@@ -67,4 +81,5 @@ tests==
  ["Aléatoire", str(random.randint(16, 20)), ""]]
 
 ==
+
 
