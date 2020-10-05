b27195afdfb9ad6749c926839194b22493bde3b7
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-05 11:38:21 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/recursion/fibonacci_rec.pl

--- ComputerScience/C/exercices/recursion/fibonacci_rec.pl
+++ ComputerScience/C/exercices/recursion/fibonacci_rec.pl
@@ -1,11 +1,25 @@
-# Copyright 2018 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2018 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Fibonacci recursif naif
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
+
 title=Fibonacci récursif très naïf
-tag=function|recursion
-extends=/ComputerScience/C/template/stdsandboxC.pl
+tag=fonction|recursion|suite|simple
 
 text==
 
@@ -74,4 +88,5 @@ tests==
  ["Aléatoire", str(random.randint(12, 15)), ""]]
 
 ==
+
 
