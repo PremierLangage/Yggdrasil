cc504be91eba1efbd4224ea97ce084334dd6cbee
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-10-06 08:26:45 +0200

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/limit/limit_polyfrac.pl

--- Mathematics/exercises/analysis/limit/limit_polyfrac.pl
+++ Mathematics/exercises/analysis/limit/limit_polyfrac.pl
@@ -6,11 +6,6 @@ before ==
 ninput=3
 lstinput=[input1,input2,input3]
 
-keyboards_JSON['virtualKeyboards']="functions"
-input1.config = keyboards_JSON
-input2.config = keyboards_JSON
-input3.config = keyboards_JSON
-input4.config = keyboards_JSON
 
 var('x')
 latexlim=[]
@@ -28,4 +23,5 @@ for (d,a) in zip(lstd,lsta):
     latexlim.append(latex(lim))
     sol.append(lim.doit())
 ==
+
 
