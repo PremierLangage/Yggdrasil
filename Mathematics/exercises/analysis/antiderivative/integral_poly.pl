f75ab0e526abac317c4325755e6d745da73b60b3
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-10-06 08:34:39 +0200

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/integral_poly.pl

--- Mathematics/exercises/analysis/antiderivative/integral_poly.pl
+++ Mathematics/exercises/analysis/antiderivative/integral_poly.pl
@@ -1,8 +1,6 @@
 extends = integral_.pl
 
 before ==
-keyboards_JSON['virtualKeyboards']="elementary"
-input.config = keyboards_JSON
 
 var('x')
 x0,x1=list_randint_norep(2,-2,2)
@@ -13,4 +11,5 @@ sol=integrate(f,(x,x0,x1)).doit()
 sol_tex=latex(sol)
 x0_tex,x1_tex=latex(x0),latex(x1)
 ==
+
 
