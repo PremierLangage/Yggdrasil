71fb243ef123a0f971ac38d4a8ca8d19442659e4
Tristan FAUTREL <Tristan.Fautrel@univ-mlv.fr>
2020-10-07 17:01:47 +0200

    tfautrel modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/maximumtab.pl

--- ComputerScience/C_Initiation/tableaux_3/maximumtab.pl
+++ ComputerScience/C_Initiation/tableaux_3/maximumtab.pl
@@ -5,7 +5,7 @@ extends=/ComputerScience/C/template/stdsandboxC.pl
 author= 
 text==
 
-Ecrire une fonction `int max_tab(int t[],int taille)` qui reçoit un tableau et sa taille (>0) et renvoie son maximum.
+Ecrire une fonction `int max_tab(int t[], int taille)` qui reçoit un tableau et sa taille (>0) et renvoie son maximum ou -1 si la taille est <= 0.
 ==
 
 codebefore==
@@ -17,13 +17,14 @@ codebefore==
 
 editor.code==
 int max_tab(int tab[], int size){
- ...
-  
+  /* Votre code ici */
 } 
 ==
 solution==
 int max_tab(int tab[], int size){
-
+  if(size <= 0){
+    return -1;
+  }
   int max,i;
   max=tab[0];
   for(i=1;i<size;i++)
@@ -43,8 +44,12 @@ int main(int argc, char* argv[]){
     tab[size] = lu;
     size++;
   }
-
-  printf("Le maximum des entiers est %d\n", max_tab(tab, size));
+  if(size == 0){
+    printf("Max d'un tableau vide : %d", max_tab(tab, size));
+  }
+  else{
+    printf("Le maximum des entiers est %d\n", max_tab(tab, size));
+  }
 
   return 0;
 }
@@ -53,10 +58,11 @@ int main(int argc, char* argv[]){
 
 tests==
 [ ["simple éxécution", "","1 2 3 4 5"],
- 
+  ["tableau vide", "", ""],
   ["aléatoire ", ""," ".join([str(random.randint(-10,10)) for i in range(random.randint(5,20))])],
   ["aléatoire ", ""," ".join([str(random.randint(-20,20)) for i in range(random.randint(5,20))])], ]
 ==
+
 
 
 
