968949e92b4f38b2cb8d5e996163fbc12d9f9e3f
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-07 16:20:40 +0200

    drevuz modified Yggdrasil/model/AMC2/essaitextselect.pl

--- model/AMC2/essaitextselect.pl
+++ model/AMC2/essaitextselect.pl
@@ -11,7 +11,7 @@ questions==
 
 
 
-=*[nb=3] What are the odd numbers ? 
+=*[nb=13] What are the odd numbers ? 
 +=[x for x in range(3,77) if x%2==1 ]
 -=[100,200,300,400,500,600]
 -=[x for x in range(3,77) if x%2==0 ]
