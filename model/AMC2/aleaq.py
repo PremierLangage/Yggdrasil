33f87f096ae153796f8a310c87a9bad1ba99eb3e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-07 15:56:48 +0200

    drevuz modified Yggdrasil/model/AMC2/aleaq.py

--- model/AMC2/aleaq.py
+++ model/AMC2/aleaq.py
@@ -9,6 +9,15 @@
             })
 """
 
+import random
+
+
+def optiondic(l):
+    d={}
+    for x in l:
+        k,v= x.split('=')
+        d[k]=v
+    return d
 
 def buildquestion(question):
     """
@@ -17,5 +26,28 @@ def buildquestion(question):
     """
     if question.get('extended') == False:
         return question
-    if question.get('type') == 'Radio':
+    try:
+        d=optiondic(question.get('option'))
+        nb =d.get("nb",4)
+        if question.get('type') == 'Radio':
+            bonne=question.get('index')
+            good=random.choice(eval(question.get('items')[bonne]))# Une bonne réponce 
+            del question.get('items')[bonne]
+            bads=[]
+            for defi in question.get('items'):
+                bads.append(eval(question.get('items')))# ensemble des mauvaose réponces 
+            bads= random.sample(bads , nb-1) # en choisir n-1
+            random.shuffle(bads)
+            # INSERER good quelque part et noter l'index 
+            index=random.randint(0,len(bads)
+            bads.insert(index,good)
+            q.set('index')=index
+            q.set("items")=bads
+        return q
+
         
+    except Exception as e:
+        print("Problème dans votre question ", str(question))
+        print(e)
+        raise e
+    
