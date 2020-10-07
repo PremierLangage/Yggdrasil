d12db499f64f59c8ff406a85e43ae0166eed6049
Justine Falque <justine.falque@lilo.org>
2020-10-07 18:59:45 +0200

    jfalque modified Yggdrasil/ComputerScience/python/AP1-2021/chapitre3/arrondiReel.pl

--- ComputerScience/python/AP1-2021/chapitre3/arrondiReel.pl
+++ ComputerScience/python/AP1-2021/chapitre3/arrondiReel.pl
@@ -21,7 +21,7 @@ before==
 ==
 
 text==  
-On veut comparer les valeurs (informatique) de deux séries (identiques) qui convergent (mathématiquement) vers $%\frac{\pi^2}{6}%$
+On veut comparer les valeurs (informatiques) de deux séries (identiques) qui convergent (mathématiquement) vers $%\frac{\pi^2}{6}%$
 
 
 $%(1): \sum\limits_{i=1}^{+\infty}\frac{1}{i^2}%$
@@ -61,6 +61,7 @@ un | 1
 petit | 14
 grand | 10001
 ==
+
 
 
 
