34e19c8478821e8e808795ed2d5558e1fa67b6da
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-07 10:35:40 +0200

    drevuz modified Yggdrasil/model/AMC/essaitextselect.pl

---
+++ model/AMC/essaitextselect.pl
@@ -0,0 +1,63 @@
+
+extends = /model/multistep.pl
+
+@ /utils/format/AMC.py
+
+settings.cumulative % false
+
+questions==
+*+ essai de text TextSelect
+
+
+
+before == #|python|
+import random as rd
+from customradio import CustomRadio
+from customcheckbox import CustomCheckbox
+from customtextselect import CustomTextSelect
+from AMC import parse_AMC_TXT
+
+list_questions = parse_AMC_TXT(questions)
+
+if 'nbstep' in globals():
+    list_questions = rd.sample(list_questions, nbstep)
+else:
+    nbstep = len(list_questions)
+
+comp = []
+statement  = []
+
+for i, q in enumerate(list_questions):
+    if q['type'] == "Radio":
+        comp.append(CustomRadio())
+    elif q['type'] == "Checkbox":
+        comp.append(CustomCheckbox())
+    elif  q['type'] == 'TextSelect':
+        comp.append(CustomTextSelect())
+    statement.append(q['text'])
+    comp[i].setitems(q['items'])
+    comp[i].setsol_from_index(q['index'])
+    if 'ordered' not in q['options']:
+        comp[i].shuffle()
+==
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+==
+
+textstep ==
+<strong> Question {{ step + 1 }}. </strong> 
+{{ statement[step] }}
+==
+
+formstep ==
+{{ comp[step]|component }}
+==
+
+evaluatorstep ==
+score = comp[step].eval()
+==
+
+
+
+
