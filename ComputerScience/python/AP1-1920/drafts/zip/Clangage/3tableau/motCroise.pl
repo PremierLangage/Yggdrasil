0bfdc5a5dbb93b244e308dfe82c8cd35fe7d29da
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-10-07 11:48:15 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Clangage/3tableau/motCroise.pl

---
+++ ComputerScience/python/AP1-1920/drafts/zip/Clangage/3tableau/motCroise.pl
@@ -0,0 +1,77 @@
+
+
+
+author=
+difficulty=3
+
+title=Mots croisés.
+tag=string
+extends=/ComputerScience/C/template/stdsandboxC.pl
+
+text==
+  
+    Pour représenter un grille de mots croisés on utilise un tableau
+     rectangulaire de char de taille LIGNES$%\times%$COLONNES.  
+  Dans une grille les mots sont écrits en majuscules, soit horizontalement de gauche à droite, soit verticalement de haut en bas.\\
+  On mémorise :  
+   une case vide par '0';  
+   une case noire par '1';  
+   une case remplie par la lettre majuscule alphabétique contenue.  
+  Ecrire une fonction `int place\_noire(char plateau[][COLONNES],int x,int y)` qui place une case noire sur la case situé sur la  {\tt x}$^{\mbox{ ième}}$ ligne et la {\tt y}$^{\mbox{ ième}}$ colonne.\\
+      La fonction renvoie $1$ si les coordonnées sont correctes et que la case était vide,0 sinon.  
+
+ Ecrire une fonction `int place\_mot(char plateau[][COLONNES],int x,int y,char m[], char direction)}qui place,si possible, le mot aux coordonnées indiquées. Si {\tt direction} vaut{\tt 'h'} , le mot doit \^etre placé horizontalement, si {\tt direction} vaut {\tt 'v'}, le mot doit \^etre placé  verticalement.
+  Un mot  ne peut être placé que si:  
+			    toutes les coordonées sont valides
+			    les cases étaient vides ou contenaientt des lettres 
+                qui correspondent à celle du mot
+
+l'appel `construitpalin(s,6)` laisse s inchangé et renvoie 0
+==
+codebefore==
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+==
+
+editor.code==
+
+==
+
+solution==
+
+
+==
+
+codeafter==
+
+int main(void){
+  char tab[LIGNES][COLONNES]={{},{},{},{},{},{};
+  
+}
+==
+
+
+tests==
+[ ["simple éxécution", "","toto 10 "],
+["alea","","".join([chr(random.randint(97,122)) for i in range(random.randint(1,15))])+" "+str(random.randint(15,32))],
+ 
+ ["aleatoire","",random.choice(["elle","kayak","velo","radar","avion","rotor","serres","solos","suffit"])+" 10"], ]
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
