3377d28daa5a8d8925a73d6100b993d67c0d99e3
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-08 16:40:28 +0200

    drevuz modified Yggdrasil/model/AMC2/dr.pl

---
+++ model/AMC2/dr.pl
@@ -0,0 +1,21 @@
+
+
+
+
+extends= /model/AMC2/QCM-AMCE.pl
+
+title= DR's Exo
+
+questions==
+
+* soir le code suivant combien d'erreur comptez vous ?
+  ```python
+  printf("%d",3)
+  a= b+1
+  ```
+- 0
+- 1
++ 2
+- 3
+==
+
