85662c8262d186a16ec2a8db7c47f5b179c8cb29
Doriann ALBERTIN <Doriann.Albertin@univ-mlv.fr>
2020-10-08 15:57:30 +0200

    dalbertin modified Yggdrasil/AAAA/DoriannAlbertin/test_fichier/test_fichier_ecriture_lecture.pl

--- AAAA/DoriannAlbertin/test_fichier/test_fichier_ecriture_lecture.pl
+++ AAAA/DoriannAlbertin/test_fichier/test_fichier_ecriture_lecture.pl
@@ -5,30 +5,32 @@ title= Ecrire - ouvrir
 
 extends=/ComputerScience/python/template/pltest.pl
 @ /utils/sandboxio.py
+@ fichier.txt
 
 
 
 
 text==
 
-Créer un fichier `texte.txt` avec une ligne `peut on ecrire des fichiers sur une feuille pl ?`.<br/>
-Fermer le fichier.<br/>
-Ouvrir le fichier et print son contenu.
+Créer un fichier `fichier.txt` avec une ligne `dossier`.<br/>
+Fermer le fichier.
 
 ==
 
-soluce==
-fichier = open("texte.txt", 'w')
-fichier.write("peut on ecrire des fichiers sur une feuille pl ?")
-fichier.close()
-fichier = open("texte.txt", 'r')
-line = fichier.readline()
-print(line)
-fichier.close()
-==
 
 pltest==
+>>> with fichier as open('fichier', 'r'):
+        print(fichier.readline())
+dossier
 ==
+
+back==
+with fichier as open(fichier.txt, 'w'):
+    fichier.write(' ')
+    fichier.save()
+
+==
+
 
 
 
