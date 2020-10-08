286de0c1bdb6d52545288ce4693396bd107cb3e2
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-08 14:29:51 +0200

    drevuz modified Yggdrasil/AAAA/DoriannAlbertin/test_fichier/test_fichier_lecture.pl

--- AAAA/DoriannAlbertin/test_fichier/test_fichier_lecture.pl
+++ AAAA/DoriannAlbertin/test_fichier/test_fichier_lecture.pl
@@ -3,7 +3,10 @@ title= Lecture
 
 # Seule la randomisation plante
 
-extends=/ComputerScience/python/template/soluce.pl
+# extends=/ComputerScience/python/template/soluce.pl
+
+extends=/ComputerScience/python/AP1-1920/templates/prosoluce.pl
+
 @ /utils/sandboxio.py
 @ test_text.txt
 @ /builder/before.py [builder.py]
@@ -28,12 +31,14 @@ text==
 
 Ecrire un programme qui lit la {{k}}-ième ligne du fichier test_text.txt et la print.
 
+
 ==
 
 
 plsoluce==
 Le test |
 ==
+
 
 
 
