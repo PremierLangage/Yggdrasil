c8c088aac330a0b586e6a8a8a39c3d54e2a698dc
Tristan FAUTREL <Tristan.Fautrel@univ-mlv.fr>
2020-10-08 12:48:30 +0200

    tfautrel modified Yggdrasil/ComputerScience/C_Initiation/fonctions_2/saisie3.pl

--- ComputerScience/C_Initiation/fonctions_2/saisie3.pl
+++ ComputerScience/C_Initiation/fonctions_2/saisie3.pl
@@ -93,6 +93,7 @@ int main(int argc, char* argv[])
 tests==
 [["Exécution simple", "", "5"],
  ["Quelques éléments", "12 2", "Il y a quelque entiers 1 qui sont dans 3 ce texte, celui la 3.1415 c\'est un float donc 2 entiers."],
+ ["Aléatoire","",'Vive '+' PLATON '.join([str(random.randint(1,4)),str(random.randint(11, 24)),str(random.randint(11, 24))])+" ."],
  ]
 ==
 
