f0eab0c0887037bd488168c597d1193c11cdb152
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-08 21:27:49 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/plateau.pl

--- ComputerScience/C_Initiation/tableaux_3/plateau.pl
+++ ComputerScience/C_Initiation/tableaux_3/plateau.pl
@@ -69,7 +69,7 @@ lg=plateau(tab,size,&deb);
 }
 ==
 
-# L'aléatoire peut sortir des trucs comme "1566666666666666-2"
+# L'aléatoire ne peut plus sortir des trucs comme "1566666666666666-2"
 tests==
 [ ["simple éxécution", "","1 2  2 4 5"],
   ["avec deux ", "","-1 -2"],
