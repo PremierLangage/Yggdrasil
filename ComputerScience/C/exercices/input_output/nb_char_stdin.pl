d96f6bb2146d440b21abaab56a0a253e65440739
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-08 11:28:21 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/input_output/nb_char_stdin.pl

--- ComputerScience/C/exercices/input_output/nb_char_stdin.pl
+++ ComputerScience/C/exercices/input_output/nb_char_stdin.pl
@@ -1,20 +1,31 @@
-# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Compter les caractères présents sur l'entrée standard
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
+
 title=Compter les caractères présents sur l'entrée standard
 tag=input_output
-extends=/ComputerScience/C/template/stdsandboxC.pl
-
-
 
 text==
-
 Écrire un programme qui compte le nombre de caractères contenus sur l'entrée 
 standard. Une fois la lecture terminée, votre programme devra écrire le nombre
 de caractère lu tout seul sur une ligne.
-    
+
 ==
 
 editor.code==
@@ -49,6 +60,7 @@ tests==
  ["Aléatoire", "", "".join([random.choice(['', '\n', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']) for i in range(random.randint(100,1000))])],
  ["Aléatoire", "", "".join([random.choice(['', '\n', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']) for i in range(random.randint(100,1000))])] ]
 ==
+
 
 
 
