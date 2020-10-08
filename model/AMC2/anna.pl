82430314a02e3a2365fb4d56ac7c9729ff25d593
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-10-08 11:59:04 +0200

    adiaw modified Yggdrasil/model/AMC2/anna.pl

--- model/AMC2/anna.pl
+++ model/AMC2/anna.pl
@@ -6,5 +6,7 @@ title= Anna's Exo
 questions==
 
 =** Choisir les mots qui sont des mots clefs de python
++=["assert","async","await","break","class","continue","def","del","else","except","finally"]
+-=["]
 
 ==
