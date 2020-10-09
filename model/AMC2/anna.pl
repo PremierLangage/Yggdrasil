d5507321f88cbda645e58d4010287f96436a372f
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-09 15:55:37 +0200

    drevuz modified Yggdrasil/model/AMC2/anna.pl

--- model/AMC2/anna.pl
+++ model/AMC2/anna.pl
@@ -18,15 +18,12 @@ questions==
 *+ Clicker sur la bonne conjugaison du verbe  
 >Je suis ( aller / {{allé}} ) au bureau hier
 
-=** Choisir les mots qui sont des mots clefs de python
+=**[nbb=2,nbg=2] Choisir les mots qui sont des mots clefs de python
 +=["assert","async","await","break","class","continue","def","del","else","except","finally"]
 -=["whiles","boucle","attend","cause","deff","dep"]
 
 =*[nb=5,group=odd] What are the odd numbers ? 
-+=    l=[]
-    for i in range(1,56,2):
-        l.append(i)
-    return l
++=[x for x in range(3,77) if x%2==1 ]
 -=[x for x in range(3,77) if x%2==0 ]
 
 =*[nb=3,group=odd] What are the even numbers ? 
