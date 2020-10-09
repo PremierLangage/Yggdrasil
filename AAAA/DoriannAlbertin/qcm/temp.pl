3b3fd9c2118dac6a10a2864913aab180720580f6
Doriann ALBERTIN <Doriann.Albertin@univ-mlv.fr>
2020-10-09 16:21:06 +0200

    dalbertin modified Yggdrasil/AAAA/DoriannAlbertin/qcm/temp.pl

---
+++ AAAA/DoriannAlbertin/qcm/temp.pl
@@ -0,0 +1,25 @@
+title= Test_QCM
+
+extends=/model/AMC_TXT.pl
+
+questions==
+* Qu'affiche le programme suivant lors de son execution ?<br/>
+`
+n = -4
+for i in range(6):
+    n = n + 2
+print(n)
+`
+- 4
+- 5
+- 6
+- 7
++ 8
+
+
+
+
+==
+
+
+
