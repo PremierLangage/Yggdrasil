a81f8a822446d05ca36c5fb5f54a0b4acece34ce
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-09 14:32:49 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl

--- ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
+++ ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
@@ -10,7 +10,7 @@ text==
 <p>
 Exemple
 avec MAX valant 5 : <br>
-- si tab contient $%-4,5,10,14,1%$, est_croissante(tab) renvoie $%0%$.<br>
+- si tab contient $%-4,5,10,14,1%$, est_croissante(tab) renvoie $%0%$,<br>
 - Si tab contient $%5,6,8,75,82%$, est_croissante(tab) renvoie $%1%$.<br>
 
 <p>
