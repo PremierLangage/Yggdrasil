52cbab008eeb5a9821ea3bb1d5e09477ef4ade63
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-09 14:28:49 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/compare.pl

--- ComputerScience/C_Initiation/tableaux_3/compare.pl
+++ ComputerScience/C_Initiation/tableaux_3/compare.pl
@@ -1,7 +1,7 @@
 
 title=Comparaison de deux tableaux
 
-tag=Tableau|Fonction|DeclarationFonction|ParcoursTableau
+tag=Tableau|Fonction|DeclarationFonction|TransmissionParametre|ParcoursTableau
 
 extends=/ComputerScience/C/template/stdsandboxC.pl
 author= 
