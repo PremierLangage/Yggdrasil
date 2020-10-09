bafae18235255e349e245409c08a74e3f6f69fb5
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-09 18:17:39 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/array/is_partial_sorted.pl

--- ComputerScience/C/exercices/array/is_partial_sorted.pl
+++ ComputerScience/C/exercices/array/is_partial_sorted.pl
@@ -57,12 +57,9 @@ text+=" {{ editor|component }} "
 
 text==
 Écrire une fonction C **est_partie_triee** qui prend en argument un tableau 
-d'entiers `tab`.
-
-<br />
-
-Votre fonction de devra retourner `1` si le tableau `tab` est trié de manière **{{ mot_croissant }}**
-entre les indices **{{ index_min }}** et **{{ index_max }}** tous deux inclus.
+d'entiers `tab`. Votre fonction de devra retourner `1` si le tableau `tab` 
+est trié de manière **{{ mot_croissant }}** entre les indices 
+**{{ index_min }}** et **{{ index_max }}** tous deux inclus.
 
 <br />
 
