f8f46c25c5af1272681518a87acff0a32719597f
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-10-09 16:03:57 +0200

    adiaw modified Yggdrasil/model/AMC2/anna.pl

--- model/AMC2/anna.pl
+++ model/AMC2/anna.pl
@@ -35,9 +35,19 @@ questions==
 doc==
 
 * AMC avec possibilité d'un seul choix de réponse et donc une seule bonne réponse possible
-** AMC avec possibilité de plusieurs choix de réponse et donc plusieurs bonnes réponses possible
-*+ Cliquer sur les mots ; mettre les bonnes réponses entre accolades {{ }}
+Réponses à mettre sous la forme : + Bonne_Rep
+                                  - Mauvaise Rep
 
+** AMC avec possibilité de plusieurs choix de réponse et donc plusieurs bonnes réponses possible
+Réponses à mettre sous la forme : + Bonne_Rep
+                                  - Mauvaise Rep
+
+*+ Cliquer sur les mots ; mettre les bonnes réponses entre accolades {{ }}
+Réponse à mettre sous la forme : >
+
+=* AMC avec possibilité d'une seule bonne réponse parmi une liste ou une compréhension python
+Réponses à mettre sous la forme : += Liste_de_Bonne_Rep ou compréhension
+                                  -= Liste_de_Mauvaise Rep ou compréhension
 ==
 
 
