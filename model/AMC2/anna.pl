86d9e58b05d862cb5a3eaad0d43ae03fc4bf48ed
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-10-09 12:02:16 +0200

    adiaw modified Yggdrasil/model/AMC2/anna.pl

--- model/AMC2/anna.pl
+++ model/AMC2/anna.pl
@@ -12,9 +12,8 @@ questions==
 +=["assert","async","await","break","class","continue","def","del","else","except","finally"]
 -=["whiles","boucle","attend","cause","deff","dep"]
 
-=*[nb=2,group=odd] What are the odd numbers ? 
+=* What are the odd numbers ? 
 +=[x for x in range(3,77) if x%2==1 ]
-+ 1
 -=[x for x in range(3,77) if x%2==0 ]
 
 ==
