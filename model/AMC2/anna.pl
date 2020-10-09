b7eb9bd8bf1d977f928eee9ce042003fc41e89f6
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-10-09 11:59:32 +0200

    adiaw modified Yggdrasil/model/AMC2/anna.pl

--- model/AMC2/anna.pl
+++ model/AMC2/anna.pl
@@ -6,7 +6,7 @@ title= Anna's Exo
 questions==
 
 *+ Clicker sur la bonne conjugaison du verbe  
->Je suis (aller / {{allé}} ) au bureau hier
+>Je suis ( aller / {{allé}} ) au bureau hier
 
 =** Choisir les mots qui sont des mots clefs de python
 +=["assert","async","await","break","class","continue","def","del","else","except","finally"]
