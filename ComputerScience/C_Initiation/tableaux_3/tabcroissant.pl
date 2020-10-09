28de206bb621eee58969d8c121a34fdfcd038a62
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-09 15:03:46 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl

--- ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
+++ ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
@@ -1,5 +1,7 @@
 title=Tableau Croissant
+
 tag=Tableau|Fonction|DeclarationFonction|TransmissionParametre|ParcoursTableau|ComparaisonContigues
+
 extends=/ComputerScience/C/template/stdsandboxC.pl
 author= 
 text==
@@ -20,7 +22,7 @@ codebefore==
 
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 15
+#define MAX random.randint(0,20)
 
 ==
 
@@ -60,11 +62,14 @@ int main(int argc, char* argv[]){
 tests==
 [ ["croissante 1", "","-1 2 5 8 78"] ,
 ["pas croissante", "","-1 2 8 5 78"] ,
-["croissante 2", ""," ".join([str(0) for i in range(5)]) ],
-["croissante 3", ""," ".join([str(i) for i in range(5)]) ],
-["aléatoire ", ""," ".join([str(random.randint(-20,20)) for i in range(5)])] 
+["croissante 2", ""," ".join([str(0) for i in range(15)]) ],
+["croissante 3", ""," ".join([str(i) for i in range(15)]) ],
+["aléatoire ", ""," ".join([str(random.randint(-20,20)) for i in range(15)])], 
+["aléatoire ", ""," ".join([str(random.randint(0,20)) for i in range(15)])] 
 ]
 ==
+
+
 
 
 
