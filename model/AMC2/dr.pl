fb33755b82cc7e9dc251098891512056f7e7f9bc
Christophe CALLE <Christophe.Calle@univ-mlv.fr>
2020-10-09 16:27:20 +0200

    ccalle1 modified Yggdrasil/model/AMC2/dr.pl

--- model/AMC2/dr.pl
+++ model/AMC2/dr.pl
@@ -12,14 +12,15 @@ text=
 questions==
 
 * soir le code suivant combien d'erreur comptez vous ?
- <span style="font-family:courrier;">
+ <```
+
     1. printf("%d",3)
     2. a= b+1
- </span>
-
+ ```
 - 0
 - 1
 + 2
 - 3
 ==
+
 
