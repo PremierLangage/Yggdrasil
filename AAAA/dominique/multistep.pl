1d7fe5ebd25aa8d62834e510551908416a47dc34
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-09 13:39:06 +0200

    drevuz modified Yggdrasil/AAAA/dominique/multistep.pl

--- AAAA/dominique/multistep.pl
+++ AAAA/dominique/multistep.pl
@@ -110,13 +110,7 @@ def strfromcomp(q):
 
 currentscore=0
 if step> -1:
-
-    # Evaluation de la réponse et stockage pour la suite 
-    q=list_questions[step]
-    score = evaluate(q)
-    scores.append(score)
-    feedbacks += env.from_string(text+" \n {{ "+strfromcomp(q)+" | component }} \n ").render(globals())
-    currentscore=sum(scores)//nbstep
+    pass
 
 step = step+1
 if step<nbstep:
