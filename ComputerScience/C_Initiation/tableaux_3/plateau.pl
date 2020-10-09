b2b02fa9f74489bcd99c36afd55b49e3dd05ae3a
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-10-09 10:04:55 +0200

    mzipstein modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/plateau.pl

--- ComputerScience/C_Initiation/tableaux_3/plateau.pl
+++ ComputerScience/C_Initiation/tableaux_3/plateau.pl
@@ -76,13 +76,9 @@ tests==
   ["avec un ", "","-1"],
   ["test qui plantait", "", "-7 10 17 3 18 20 8 -3 -1 15 -6 -15 8 7 -8 -12 -19 -19"],
   ["aléatoire ", ""," ".join([str(random.randint(-20,20)) for i in range(random.randint(5,20))])],
-  ["aléatoire ", ""," ".join([" ".join([str(random.randint(-20,20))]*(random.randint(1,10))) for i in range(random.randint(5,20))])],
+  ["aléatoire ", ""," ".join([" ".join([str(random.randint(-20,20))]+(random.randint(1,10))) for i in range(random.randint(5,20))])],
    ]
 ==
-
-
-
-
 
 
 
