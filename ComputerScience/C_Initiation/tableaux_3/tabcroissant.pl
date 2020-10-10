38db90ce0029790b78c65a1953a7baf4259d744c
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-10 16:28:23 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl

--- ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
+++ ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
@@ -89,12 +89,12 @@ seed==
 # les tests sont plus longs que MAX<=25 pour assurer qu'ils marchent dans tous les cas
 # mais effectivement la seule partie qui compte c'est jusqu'à MAX
 tests==
-[ ["croissante 1", "",str(dic['seed'])+"\n"+" ".join([str(i) for i in range(25)])] ,
-["presque croissante", "",str(dic['seed'])+"\n"+"1 "+" ".join([str(i) for i in range(25)])] ,
-["pas stricte", "",str(dic['seed'])+"\n"+"0 "+" ".join([str(i) for i in range(25)])] ,
-["aléatoire 1", "",str(dic['seed'])+"\n"+" ".join([str(random.randint(1,10)) for i in range(25)])] ,
-["aléatoire 2", "",str(dic['seed'])+"\n"+" ".join([str(random.randint(1+2**i,2**(i+1))) for i in range(25)])] ,
-["aléatoire 3", "",str(dic['seed'])+"\n"+" ".join([str(random.randint(2**i,3*2**(i))) for i in range(25)])] ,
+[ ["croissante 1", "",str(random.randint(1,100))+"\n"+" ".join([str(i) for i in range(25)])] ,
+["presque croissante", "",str(random.randint(1,100))+"\n"+"1 "+" ".join([str(i) for i in range(25)])] ,
+["pas stricte", "",str(random.randint(1,100))+"\n"+"0 "+" ".join([str(i) for i in range(25)])] ,
+["aléatoire 1", "",str(random.randint(1,100))+"\n"+" ".join([str(random.randint(1,10)) for i in range(25)])] ,
+["aléatoire 2", "",str(random.randint(1,100))+"\n"+" ".join([str(random.randint(1+2**i,2**(i+1))) for i in range(25)])] ,
+["aléatoire 3", "",str(random.randint(1,100))+"\n"+" ".join([str(random.randint(2**i,3*2**(i))) for i in range(25)])] ,
 
 
 ]
