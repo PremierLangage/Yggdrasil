b0aba930c42ba1b2d876847048120cb0d4fc09b0
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-10 16:57:03 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl

--- ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
+++ ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
@@ -99,7 +99,7 @@ seed==
 # les tests sont plus longs que MAX<=25 pour assurer qu'ils marchent
 # mais effectivement la seule partie qui compte c'est jusqu'à MAX
 tests==
-[ ["croissante 1", "",str(dic['seed'])+" "+ " ".join([str(i) for i in range(25)])] ,
+[ ["croissante 1", "",dic['seed']+" "+ " ".join([str(i) for i in range(25)])] ,
 ["presque croissante", "",str(dic['seed'])+" "+ "1 "+" ".join([str(i) for i in range(25)])] ,
 ["pas stricte", "",str(dic['seed'])+" "+ "0 "+" ".join([str(i) for i in range(25)])] ,
 ["aléatoire 1", "",str(dic['seed'])+" "+ " ".join([str(random.randint(1,10)) for i in range(25)])] ,
