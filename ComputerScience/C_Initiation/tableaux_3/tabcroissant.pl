68a6f820f650ae6f650d7f5cc00a0ca0c01f5671
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-10 16:57:40 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl

--- ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
+++ ComputerScience/C_Initiation/tableaux_3/tabcroissant.pl
@@ -92,7 +92,7 @@ int main(int argc, char* argv[]){
 
 
 seed==
-  random.randint(2,10000000)
+  random.randint(500,10000000)
 ==
 
 # MAX était donné comme valeur comme en C dans les tests, ça faisait tout planter, il faut le mettre à la main
