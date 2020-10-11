8ea281cc14a17f11837f121364a76dc34e156e17
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-11 15:49:24 +0200

    nborie modified Yggdrasil/ComputerScience/C/exercices/variable/qcm_questions/test_code.pl

---
+++ ComputerScience/C/exercices/variable/qcm_questions/test_code.pl
@@ -0,0 +1,47 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends = /model/AMC_TXT.pl
+
+author=Nicolas Borie
+title=Quiz sur les variables en langage C
+
+tag=C|cours|langage|variables
+
+questions==
+** Tiens, voici une question avec du code :
+
+
+```
+int i, tab[10];
+
+     for(i=0;i<10;i++)
+
+        scanf("%d",&tab[i]);
+```
+
++ Pouet
++ foo
+- bar
++ ```
+int i, tab[10];
+
+     for(i=0;i<10;i++)
+
+        scanf("%d",&tab[i]);
+```
+==
+
