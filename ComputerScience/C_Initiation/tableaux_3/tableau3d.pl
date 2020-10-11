83f8bea3c8b41286dc91c66582f2149505acf096
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-11 19:52:28 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tableau3d.pl

--- ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
+++ ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
@@ -14,18 +14,10 @@ Ecrire une fonction `raun()` qui :
 <ul>
   <li>reçoit en paramètre un tableau à trois dimensions et retourne un int</li>
   <li>remplie le tableau à trois dimensions avec des valeurs incrémentée de 1 en 1 (on démarre à 1 et le dernier emplacement aura pour valeur N)</li>
-  <li>affiche le tableau de la manière suivante : 
-
-Ligne Z || Colonne X {valeur} | Colonne Y {valeur} |</li>
+  <li>affiche le tableau de la manière suivante : Ligne Z || Colonne X {valeur} | Colonne Y {valeur} |</li>
 </ul>
 
-
-
-
-
-
-
-Exemple pour un tableau tab[2][3][5] on aura : 
+Exemple pour un tableau tab[2][3][5] on aura :<br> 
 
 Ligne 1 || Colonne 1 {1,2,3,4,5} | Colonne 2 {6,7,8,9,10} | Colonne 3 {11,12,13,14,15} |<br>
 Ligne 2 || Colonne 1 {16,17,18,19,20} | Colonne 2 {21,22,23,24,25} | Colonne 3 {26,27,28,29,30} |
