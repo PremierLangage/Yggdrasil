f1f7d9e47406996bb220ce746cff98a9da530438
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-11 19:55:52 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tableau3d.pl

--- ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
+++ ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
@@ -20,8 +20,9 @@ Ecrire une fonction `raun()` qui :
 Exemple pour un tableau tab[2][3][5] on aura :<br> 
 
 Ligne 1 || Colonne 1 {1,2,3,4,5} | Colonne 2 {6,7,8,9,10} | Colonne 3 {11,12,13,14,15} |<br>
-Ligne 2 || Colonne 1 {16,17,18,19,20} | Colonne 2 {21,22,23,24,25} | Colonne 3 {26,27,28,29,30} |
+Ligne 2 || Colonne 1 {16,17,18,19,20} | Colonne 2 {21,22,23,24,25} | Colonne 3 {26,27,28,29,30} |<br>
 
+Dans cet exemple N = 2*3*5 = 30
 ==
 
 editor.code==
@@ -31,5 +32,6 @@ int raun(........, int tab[][COL][N]){
 
 }
 ==
+
 
 
