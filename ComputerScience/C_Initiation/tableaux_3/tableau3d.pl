f392e76c26d57eee7e8c0395d40e427b00e74e29
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-11 19:46:38 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tableau3d.pl

--- ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
+++ ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
@@ -21,7 +21,7 @@ Ligne Z || Colonne X {valeur} | Colonne Y {valeur} |
 
 Exemple pour un tableau tab[2][3][5] on aura : 
 
-Ligne 1 || Colonne 1 {1,2,3,4,5} | Colonne 2 {6,7,8,9,10} | Colonne 3 {11,12,13,14,15} |
+Ligne 1 || Colonne 1 {1,2,3,4,5} | Colonne 2 {6,7,8,9,10} | Colonne 3 {11,12,13,14,15} |<br>
 Ligne 2 || Colonne 1 {16,17,18,19,20} | Colonne 2 {21,22,23,24,25} | Colonne 3 {26,27,28,29,30} |
 
 ==
