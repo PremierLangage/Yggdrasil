7841d8e367d32500e431d33865990c676d2001ce
Justine Falque <justine.falque@lilo.org>
2020-10-11 20:56:41 +0200

    jfalque modified Yggdrasil/ComputerScience/python/AP1-2021/QCM/debut_de_qcm_ap1.pl

---
+++ ComputerScience/python/AP1-2021/QCM/debut_de_qcm_ap1.pl
@@ -0,0 +1,68 @@
+
+
+extends= /model/AMC2/QCM-AMCE.pl
+
+title= QCM PL en construction
+
+questions==
+
+=**[nbg=3,nbb=2] Dans un programme, quels sont les noms de variables que vous utiliseriez pour gérer des données liés au temps? 
++=["tmp","cmpt","hours","heures","years","annees","days","jours","mnt","minutes","sec","seconds","period","duree"]
+-=["and","if","for","while","variable","variable_3","x","a","toto","chat"]
+
+**[group=prenom] Quel est le prénom de Revuz
++Dominique
+-Christian
++Paul
++Edouard
+
+*[group=prenom] Quel est le prénom de Diaw
++Anna
+-Aminata
+
+*+ Clicker sur la bonne conjugaison du verbe  
+>Je suis ( aller / {{allé}} ) au bureau hier
+
+=**[nbb=2,nbg=2] Choisir les mots qui sont des mots clefs de python
++=["assert","async","await","break","class","continue","def","del","else","except","finally"]
+-=["whiles","boucle","attend","cause","deff","dep"]
+
+=*[nb=5,group=odd] What are the odd numbers ? 
++=[x for x in range(3,77) if x%2==1 ]
+-=[x for x in range(3,77) if x%2==0 ]
+
+=*[nb=3,group=odd] What are the even numbers ? 
+-=[x for x in range(3,77) if x%2==1 ]
++=[x for x in range(3,77) if x%2==0 ]
+
+==
+
+doc==
+On respecte le format standart des AMC selon le lien suivant : https://www.auto-multiple-choice.net/auto-multiple-choice.fr/AMC-TXT.shtml
+
+
+* AMC avec possibilité d'un seul choix de réponse et donc une seule bonne réponse possible
+Réponses à mettre sous la forme : + Bonne_Rep
+                                  - Mauvaise Rep
+
+** AMC avec possibilité de plusieurs choix de réponse et donc plusieurs bonnes réponses possible
+Réponses à mettre sous la forme : + Bonne_Rep
+                                  - Mauvaise Rep
+
+*+ Cliquer sur les mots ; mettre les bonnes réponses entre accolades {{ }}
+Réponse à mettre sous la forme : >
+
+=* AMC avec possibilité d'une seule bonne réponse parmi une liste ou une compréhension python sur une ligne
+Réponses à mettre sous la forme : += Liste_de_Bonne_Rep ou compréhension
+                                  -= Liste_de_Mauvaise Rep ou compréhension
+
+Pour chaque question, on peut mettre des options entre [] :
+[group=] pour qu'il choisisse au hasard un exo parmi ceux qui ont le même tag
+
+[nb=] pour déterminer le nombre de réponse pour un radio
+
+[nbb=,nbg=] pour déterminer le nombre de bonne et de mauvaise réponse pour un checkbox 
+==
+
+
+
