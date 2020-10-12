6847e754429f30e03ad1f736a0eff59527b55630
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-12 16:09:49 +0200

    nborie modified Yggdrasil/ComputerScience/C/review/review_assistant.pl

--- ComputerScience/C/review/review_assistant.pl
+++ ComputerScience/C/review/review_assistant.pl
@@ -28,7 +28,7 @@ before==#|python|
 # step 
 step = 0
 
-texts=["Bienvenu sur l'assistant online d'aide à la génération de rapport pour la review de code en langage C."]
+texts=["# Bienvenu sur l'assistant online d'aide à la génération de rapport pour la review de code en langage C."]
 forms=["Votre nom : <input type=text /> <br />"
         "Nom de l'auteur du code : <input type=text /> <br />"
         "Nom du code ou projet relu : <input type=text />"]
