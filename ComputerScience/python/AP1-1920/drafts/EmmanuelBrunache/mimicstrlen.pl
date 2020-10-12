ded2f378d9eea4e99b7e27813099c8b0b4613385
Emmanuel BRUNACHE <emmanuel.brunache@ac-creteil.fr>
2020-10-12 02:03:29 +0200

    ebrunache modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/mimicstrlen.pl

--- ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/mimicstrlen.pl
+++ ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/mimicstrlen.pl
@@ -1,15 +1,15 @@
 author=Emmanuel Brunache
 
 
-title=strlen
+title=Strlen
 tag=string
-extends=/ComputerScience/C/template/stdsandboxC.pl
+extends=/ComputerScience/C/template/std_progC.pl
 
 text==
 
 Ecrire une fonction `longueurchaine` qui reçoit une chaine de caractères renvoie le nombre de caractères de cette chaine. Elle fait comme la fonction strlen mais vous n'avez bien entendu pas le droit de l'utiliser.
 ==
-codebefore==
+code_before==
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,23 +26,21 @@ editor.code==
 ==
 
 solution==
-void longueurchaine(char tab[]){
+int longueurchaine(char tab[]){
   int i=0;
   while(tab[i]!='\0'){
-    i++
+    i++;
   }
-  return i
+  return i;
 }
 
 ==
 
-codeafter==
+code_after==
 
-int main(void){
-  char tab[100];
-  int i;
-scanf("%s",tab);
-i=longueurchaine(tab);
+int main(int argc, char* argv[]){
+    int i;
+    i=longueurchaine(argv[1]);
 
 printf("La chaine est de longueur %d\n",i);
 return 0;
@@ -50,10 +48,9 @@ return 0;
 ==
 
 
-tests==
-[ ["simple éxécution", "","toto o i "],
-["alea","","".join([chr(random.randint(97,122)) for i in range(random.randint(1,15))])+" c C"],
-  ]
+checks_args_stdin==#|python|
+[ ["simple éxécution", ["bonjour toto"],""],
+["alea",["".join([chr(randint(97,122)) for i in range(randint(1,15))])],""]]
 ==
 
 
