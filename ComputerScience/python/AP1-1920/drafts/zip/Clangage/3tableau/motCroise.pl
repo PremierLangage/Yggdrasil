84c0dde32229ecbfd5d8ac56810cd8c035c675a9
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2020-10-12 11:21:51 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/Clangage/3tableau/motCroise.pl

--- ComputerScience/python/AP1-1920/drafts/zip/Clangage/3tableau/motCroise.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/Clangage/3tableau/motCroise.pl
@@ -11,7 +11,26 @@ extends=/ComputerScience/C/template/stdsandboxC.pl
 text==  
     Pour représenter un grille de mots croisés on utilise un tableau
      rectangulaire de char de taille LIGNES$%\times%$COLONNES.  
-  
+Dans une grille les mots sont écrits en majuscules, soit horizontalement de gauc
+he à droite, soit verticalement de haut en bas.  
+On mémorise :  
+   une case vide par 0;  
+   une case noire par 1; 
+   une case remplie par la lettre majuscule alphabétique contenue.  
+  Ecrire une fonction `int place_noire(char plateau[][COLONNES],int x,int y)` qu
+i place une case noire sur la case de coordonnées(x,y) de la grille.  
+      La fonction renvoie 1 si les coordonnées sont correctes et que la case éta
+it vide, 0 sinon.    
+
+ Ecrire une fonction `int place_mot(char plateau[][COLONNES],int x,int y,char m[
+], char direction)}`qui place,si possible, le mot aux coordonnées indiquées.  
+ Si direction vaut 'h', le mot doit être placé horizontalement, si  direction va
+ut 'v', le mot doit être placé  verticalement.  
+  Un mot  ne peut être placé que si:    
+		toutes les coordonées sont valides  
+		les cases étaient vides ou contenaient des lettres 
+                qui correspondent à celle du mot.
+
 ==
 codebefore==
 
@@ -45,6 +64,30 @@ int place_noire(char plateau[][COLONNES],int x,int y){
   return 1;
 }
 
+int place_mot(char plateau[][COLONNES],int x,int y,char m[],char dest){
+  int i,j,l;
+  /*verif*/
+  l=strlen(m);
+  if (dest=='h'){
+    for(i=0;i<l;i++)
+      
+      if(x<0 || x>=LIGNES||y+i<0||y+i>=COLONNES|| plateau[x+i][y]==1||(plateau[x][y+i]!=0 && plateau[x][y+i]!=m[i] ))
+	return 0;
+     for(i=0;i<l;i++)
+       plateau[x][y+i]=m[i];
+     return 1;
+  }
+   if (dest=='v'){
+    for(i=0;i<l;i++)
+      
+      if(x+i<0 || x+i>=LIGNES||y<0||y>=COLONNES|| plateau[x+i][y]==1||(plateau[x+i][y]!=0 && plateau[x+i][y]!=m[i] ))
+	return 0;
+     for(i=0;i<l;i++)
+       plateau[x+i][y]=m[i];
+     return 1;
+   }
+}
+
 ==
 
 solution==
@@ -55,6 +98,29 @@ int place_noire(char plateau[][COLONNES],int x,int y){
   return 1;
 }
 
+int place_mot(char plateau[][COLONNES],int x,int y,char m[],char dest){
+  int i,j,l;
+  /*verif*/
+  l=strlen(m);
+  if (dest=='h'){
+    for(i=0;i<l;i++)
+      
+      if(x<0 || x>=LIGNES||y+i<0||y+i>=COLONNES|| plateau[x+i][y]==1||(plateau[x][y+i]!=0 && plateau[x][y+i]!=m[i] ))
+	return 0;
+     for(i=0;i<l;i++)
+       plateau[x][y+i]=m[i];
+     return 1;
+  }
+   if (dest=='v'){
+    for(i=0;i<l;i++)
+      
+      if(x+i<0 || x+i>=LIGNES||y<0||y>=COLONNES|| plateau[x+i][y]==1||(plateau[x+i][y]!=0 && plateau[x+i][y]!=m[i] ))
+	return 0;
+     for(i=0;i<l;i++)
+       plateau[x+i][y]=m[i];
+     return 1;
+   }
+}
 ==
 
 codeafter==
@@ -72,15 +138,24 @@ if(place_noire(tab,x,y)==1)
  else
   printf("case invalide \n"); 
  affiche(tab);
+  scanf("%s",mot);
+ scanf("%d %d",&x,&y);
+ scanf(" %c",d);
+ if(place_mot(tab,x,y,mot,d))
+   printf("mot valide \n");
+ else
+   printf("mot invalide \n"); 
+ affiche(tab);
  return 0;
 }
 ==
 
 
 tests==
-[ ["simple éxécution"," ","2 2"],
+[ ["simple exécution"," ","2 2 PARADIS 0 0 h"],
  ]
 ==
+
 
 
 
