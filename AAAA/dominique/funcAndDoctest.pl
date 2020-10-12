2142f357da780ac98378fb5735a8244c5376f54a
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-12 15:50:58 +0200

    drevuz modified Yggdrasil/AAAA/dominique/funcAndDoctest.pl

---
+++ AAAA/dominique/funcAndDoctest.pl
@@ -0,0 +1,33 @@
+
+# 
+# 
+
+text==
+
+Ecireune un fonction qui valide le test suivant 
+
+>>> f()
+4
+>>> f(56)
+12
+>>> f(68)
+24
+>>> f("Toto")
+"Et bein ca va pas non !!"
+
+==
+
+pltest==
+>>> f()
+4
+>>> f(56)
+12
+>>> f(68)
+24
+>>> f("Toto")
+'Et bein ca va pas non !!'
+==
+
+
+
+
