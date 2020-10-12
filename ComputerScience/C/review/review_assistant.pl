8e7a19be984fc2ac2f8556503b862711acf9d44b
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-12 19:33:48 +0200

    nborie modified Yggdrasil/ComputerScience/C/review/review_assistant.pl

--- ComputerScience/C/review/review_assistant.pl
+++ ComputerScience/C/review/review_assistant.pl
@@ -64,6 +64,9 @@ forms=['<table style="border-spacing: 30px; border-collapse: separate;">'
        ' {{ group|component }} \n<br />\n'
        ' Précisions le cas échéant (en particulier si vous avez des suggestions d\'amélioration) : <br /> '
        '<textarea id="form_comcrit1" rows="6" cols="80" ></textarea>',
+       ' {{ group|component }} \n<br />\n'
+       ' Précisions le cas échéant (en particulier si vous avez des suggestions d\'amélioration) : <br /> '
+       '<textarea id="form_comcrit2" rows="6" cols="80" ></textarea>',
        'Commentaires ouverts : <br /> '
        '<textarea id="form_comments" rows="6" cols="80" ></textarea>']
 
