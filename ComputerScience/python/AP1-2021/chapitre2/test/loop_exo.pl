e6695687c770e22d1f221287b5b2b7d5981be5d9
Chaohui WANG <Chaohui.Wang@u-pem.fr>
2020-10-13 15:33:34 +0200

    cwang modified Yggdrasil/ComputerScience/python/AP1-2021/chapitre2/test/loop_exo.pl

---
+++ ComputerScience/python/AP1-2021/chapitre2/test/loop_exo.pl
@@ -0,0 +1,39 @@
+
+
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+@ loopgen.py
+title= Boucle d'OR (repetable)
+
+
+text==
+
+Afficher les entiers  de {{b}} à {{d}} parcourus de {{s}} en {{s}} {{predtext}}.
+
+Puis calculez et affichez {{operatortextb}} {{functeur[0]}} {{operatortexta}} .
+
+==
+
+mplsoluce0==
+Vérification 1
+45
+==
+mplsoluce1==
+Vérification 2
+35
+==
+
+
+
+before==
+
+X=3
+front= f"X={X}"
+
+==
+
+soluce==
+
+print(X)
+
+==
