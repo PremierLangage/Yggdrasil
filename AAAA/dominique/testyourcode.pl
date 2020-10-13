b798090feb15741ae449a30446f884c984500ab0
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-13 09:37:24 +0200

    drevuz modified Yggdrasil/AAAA/dominique/testyourcode.pl

--- AAAA/dominique/testyourcode.pl
+++ AAAA/dominique/testyourcode.pl
@@ -1,5 +1,5 @@
 
-extends= /model/progpython.pl
+
 extends= /ComputerScience/python/AP1-1920/templates/prosoluce.pl
 
 
@@ -12,7 +12,16 @@ inputbox.placeholder = Entrée du programme (stdin)
 inputbox.maxlength = 2
 inputbox.appearance = outline
 
-editor.code = Votre code du projet
+
+# création de l'éditeur de code
+editor =: CodeEditor
+editor.language = python
+editor.cid = py_editor_0
+editor.code ==
+# Saisissez votre code ici, et cliquez sur le bouton
+# de validation quand vous avez terminé
+
+==
 
 
 
