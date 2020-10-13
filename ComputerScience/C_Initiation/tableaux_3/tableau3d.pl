ef15316e5ee346bf28c840c5b4c18effee79b408
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-13 10:41:42 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/tableau3d.pl

--- ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
+++ ComputerScience/C_Initiation/tableaux_3/tableau3d.pl
@@ -83,12 +83,11 @@ codeafter==
 
 int main(int argc, char* argv[]){
  
- 	int LI,COL,N;
-
- 	LI=atoi(argv[1]);
- 	COL=atoi(argv[2]);
- 	N=atoi(argv[3]);
-
+ /*récupération des paramètres*/
+ 	int LI=atoi(argv[1]);
+ 	int COL=atoi(argv[2]);
+ 	int N=atoi(argv[3]);
+	
 	int tab[LI][COL][N];
 	
 	raun(LI,COL,N,tab);
