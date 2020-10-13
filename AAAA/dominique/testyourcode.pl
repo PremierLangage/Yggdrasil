94b454dfcc43918e1c68cd6ba7d725d97c8d293d
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-13 08:58:18 +0200

    drevuz modified Yggdrasil/AAAA/dominique/testyourcode.pl

--- AAAA/dominique/testyourcode.pl
+++ AAAA/dominique/testyourcode.pl
@@ -2,3 +2,15 @@
 extends= /model/progpython.pl
 
 
+inputbox =: Input
+inputbox.type = multiline
+inputbox.placeholder = Ans
+inputbox.maxlength = 2
+inputbox.appearance = outline
+
+Ans= Entree du programme
+
+form==
+{{ inputbox | component }}
+{{editor|component}}
+==
