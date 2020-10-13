dbc354fad4ece7354c46d21fa687aa3a37ed3411
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-13 08:59:56 +0200

    drevuz modified Yggdrasil/AAAA/dominique/testyourcode.pl

--- AAAA/dominique/testyourcode.pl
+++ AAAA/dominique/testyourcode.pl
@@ -1,14 +1,18 @@
 
 extends= /model/progpython.pl
 
+title=
+text= Exécuter votre code sur la sandbox 
 
 inputbox =: Input
 inputbox.type = multiline
-inputbox.placeholder = Ans
+inputbox.placeholder = Entrée du programme (stdin)
 inputbox.maxlength = 2
 inputbox.appearance = outline
 
 Ans= Entree du programme
+
+editor.code = Votre code 
 
 form==
 {{ inputbox | component }}
