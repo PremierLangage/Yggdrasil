74920ce2727b517091b84cc48cfcb16bf9279184
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-13 09:30:36 +0200

    drevuz modified Yggdrasil/AAAA/dominique/testyourcode.pl

--- AAAA/dominique/testyourcode.pl
+++ AAAA/dominique/testyourcode.pl
@@ -1,5 +1,7 @@
 
 extends= /model/progpython.pl
+extends= /ComputerScience/python/AP1-1920/templates/prosoluce.pl
+
 
 title=
 text= Exécuter votre code sur la sandbox 
@@ -10,14 +12,11 @@ inputbox.placeholder = Entrée du programme (stdin)
 inputbox.maxlength = 2
 inputbox.appearance = outline
 
-Ans= Entree du programme
-
 editor.code = Votre code du projet
 
 form==
 {{ inputbox | component }}
 {{editor|component}}
 ==
-
-evaluator==
+@ utils/prosolucegrader.py [grader.py]
 
