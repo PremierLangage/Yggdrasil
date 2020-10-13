ee8900de0ed328af7cae1223d6ddaae864da41a1
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-13 11:55:10 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-2021/chapitre3/boucle2croissant.pl

--- ComputerScience/python/AP1-2021/chapitre3/boucle2croissant.pl
+++ ComputerScience/python/AP1-2021/chapitre3/boucle2croissant.pl
@@ -5,7 +5,7 @@ extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
 title = Saisie d'une suite croissante
 
-tag=Boucle|Type||SaisieControlee|While|Conditionnelle
+tag=boucle|type|saisieControlee|while|conditionnelle
 
 text==  
 Ecrire un programme qui effectue la saisie controlée d'un type de variable,
@@ -121,6 +121,7 @@ bannana
 papel
 mango
 ==
+
 
 
 
