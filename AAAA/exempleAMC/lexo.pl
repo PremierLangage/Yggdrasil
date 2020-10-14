41b70f47229583f64c8f3a0e9fc7f6d4b6ab9a67
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-14 14:35:00 +0200

    drevuz modified Yggdrasil/AAAA/exempleAMC/lexo.pl

---
+++ AAAA/exempleAMC/lexo.pl
@@ -0,0 +1,16 @@
+
+extends= /model/AMC2/QCM-AMCE.pl
+
+questions==
+
+* avez vous trouve fcette feuille d'exercice interessante ?
++ Oui 
+- Non 
+
+
+==
+
+
+#@ exe.txt [question3.txt]
+@ question6.txt 
+
