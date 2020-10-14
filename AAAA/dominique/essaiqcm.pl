7004e3cab7a7e4f2d5db92140bc6351075fae5c0
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-14 10:28:52 +0200

    drevuz modified Yggdrasil/AAAA/dominique/essaiqcm.pl

---
+++ AAAA/dominique/essaiqcm.pl
@@ -0,0 +1,27 @@
+
+extends= /model/qcm/qcm.pl
+
+
+title= Exemple pour etienne 
+
+text==
+Choisir la bonne reponse :
+==
+
+nb=4
+nbtrues=1
+
+good==
+bon
+bien
+ok
+cool
+
+==
+
+bad==
+bad
+pas bon
+beurk
+==
+
