475a21708f3d8a4c5e9bc4a6583551d8d3c95e9d
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-10-14 15:52:01 +0200

    mkobylanski modified Yggdrasil/AAAA/exempleAMC/lexo.pl

--- AAAA/exempleAMC/lexo.pl
+++ AAAA/exempleAMC/lexo.pl
@@ -1,6 +1,8 @@
 
 extends= /model/AMC2/QCM-AMCE.pl
 
+titre== Questions
+
 questions==
 
 * avez vous trouve fcette feuille d'exercice interessante ?
@@ -13,4 +15,6 @@ questions==
 
 #@ exe.txt [question3.txt]
 @ question6.txt 
+
+
 
