38dc9f21fed0800290199af7f3511d8e2a00e12e
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-10-14 17:05:33 +0200

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/AMC_questions_ensembles.pl

---
+++ Mathematics/Magdalena/AMC_questions_ensembles.pl
@@ -0,0 +1,32 @@
+
+
+extends = /model/AMC_TXT.pl
+title = Démo AMC-TXT
+questions==
+
+** Voici un texte mathématique :
+Soit $% E, F %$ des ensembles. On dit qu'une partie 
+$%\Gamma%$ de $%E \times F%$ définie un graphe si 
+ $%  (a) \quad  \forall x \in E \quad \exists y \in F \quad (x,y)\in \Gamma%$
+ $%  (b) \quad  \forall x \in E \quad \forall y,y'\in F  
+\quad \mbox{ si } (x,y)\in \Gamma \mbox{ et } (x,y')\in \Gamma, \mbox{ alors } 
+y=y'  %$
+
++ $% E %$ est une variable parlante 
++ Le texte est une définition
++ Dans le $%(a)%$ les variables $%x,y%$ sont muettes
++  Dans le $%(a)%$ les variables $%E,\Gamma%$ sont parlantes
++ La variable $%F%$ de la ligne (a) est le même que  celle de ligne $%(b)%$ 
+- $%E \times F=%$ {$%x\times y | x \in E \; y \in F  %$}
+- Le texte est une démonstration
+- La variable $%x%$ de la ligne (a) est la même que celle de la ligne (b)
+- Dans le $%(b)%$ les variables $%x,\Gamma%$ sont parlantes
+
+**  On définit $%A= %$ {$%0,1%$}$% \times%$ {$%1,2%$}
++ $% A%$ possède 4 éléments
++ $%(0,1)\in A%$
+- $%A=%${$% 0, 1,2 %$}
+- $% 0 \in A%$
+==
+
+
