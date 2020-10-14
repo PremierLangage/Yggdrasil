7fca8bad2db59a8c905e5077981144c5f36711a1
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-10-14 16:04:14 +0200

    protondo modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/QuestionAnalyseCode.pl

--- ComputerScience/C_Initiation/tableaux_3/QuestionAnalyseCode.pl
+++ ComputerScience/C_Initiation/tableaux_3/QuestionAnalyseCode.pl
@@ -1,8 +1,14 @@
 extends=/model/AMC_TXT.pl 
 
+textstep ==
+<strong> Question {{ step + 1 }}. </strong> 
+{{ statement[step] | e }}
+==
+
 title= Compréhension de code 
 
 questions=@ QuestionsAnalyseCode.txt
 
 tag=Tableau|Initialisation|ParcoursTableau|Boucle
+
 
