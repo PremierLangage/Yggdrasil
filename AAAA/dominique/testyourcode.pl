d5c23eb8ea139e662974ceb5c018b5d0f02d5e2c
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-14 16:43:24 +0200

    drevuz modified Yggdrasil/AAAA/dominique/testyourcode.pl

--- AAAA/dominique/testyourcode.pl
+++ AAAA/dominique/testyourcode.pl
@@ -4,7 +4,15 @@ extends= testapp2.pl
 
 
 title=
-text= Exécuter votre code sur la sandbox 
+text==
+
+Tester cotre code contre un autre code.
+
+Dans le premier editeur placer l'entrée que vous voulez comparer.
+Dans le deuxième editeur votre code python.
+
+
+==
 
 inputbox =: Input
 inputbox.type = text
@@ -22,20 +30,11 @@ editor1.code ==
 ==
 
 
-# création de l'éditeur de code
-editor =: CodeEditor
-editor.language = python
-editor.cid = py_editor_0
-editor.code ==
-# Saisissez votre code ici, et cliquez sur le bouton
-# de validation quand vous avez terminé
 
-==
-
-soluce=@ app2dr.py
+#soluce=@ app2dr.py
 
 
-editor.code==
+ducode==
 version="v2"
 while True:
         s=input("$ ")
@@ -71,7 +70,7 @@ form==
 {{ editor  | component }}
 ==
 
-mplsoluce0==
+mplsoluce1==
 Test Version
 version
 v1
