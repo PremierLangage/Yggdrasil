970ef74a180cd9428f0cbd8329d5a175ec84e406
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-15 14:20:36 +0200

    ochampalle modified Yggdrasil/model/AMC2/QCM-AMCE.pl

--- model/AMC2/QCM-AMCE.pl
+++ model/AMC2/QCM-AMCE.pl
@@ -15,16 +15,55 @@ settings.cumulative % false
 # FAITES UN EXTENDS DESSUS ET DEFINISER VOTRE BALISE questions
 # extends=  /model/AMC2/essaitextselect.pl 
 
+questions==
+
+=* Exo avec click sur un mot ici bonne est le bon mot 
+ Vous  pouvez en mettre plusieurs séparer vos mots par des espace 
+ si vous souhaiter mettre un espace ajouter des crochet autour des caractères en question   
+>j'indique avec des doubles curly brackets la {{bonne}} réponse
 
 
+=**[nb=13, group=odd] What are the $%odd%$ numbers ? 
 
++=[x for x in range(3,77) if x%2==1 ]
+-=[100,200,300,400,500,600]
+-=[x for x in range(2,20) if x%2==0 ]
+
+=**[nbb=2,nbg=2] Indiquer les identifiants correctes
++=["un","deux","ident","prout"]
+-=["666","aujourd'hui", "def", "@lt;html@gt;"] 
+-=["!autres","truc%","pour ris"]
+- for
+-=["list","int","dic"]
+
+
+=**[nbb=2,nbg=2] Indiquer les identifiants correctes
++=["un","deux","ident","prout"]
+-=["666","aujourd'hui", "def", "@lt;html@gt;"] 
+-=["!autres","truc%","pour ris"]
+- for
+-=["list","int","dic"]
+
+=**[nb=13, group=odd] What are the odd numbers ? 
++=[x for x in range(3,77) if x%2==1 ]
+-=[100,200,300,400,500,600]
+-=[x for x in range(2,20) if x%2==0 ]
+
+
+=*[nb=2,group=odd] What are the odd numbers ? 
++=[x for x in range(3,77) if x%2==1 ]
++ 1
+-=[x for x in range(3,77) if x%2==0 ]
+
+
+=*[nb=6,group=odd] What are the odd numbers ? 
++=[x for x in range(3,77) if x%2==1 ]
+-=[x for x in range(3,77) if x%2==0 ]
+
+==
 
 
 title= Cher enseignant vous n'avez pas changer le "title" 
-
-@ exe.txt [question7.txt]
-@ exe.txt [question3.txt]
-
 
 before == #|python|
 import random as rd
