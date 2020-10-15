c4ef1f770ad7d8670313eda017ec5d2612f85456
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-15 14:00:38 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/TestAnna.pl

--- ComputerScience/C_Initiation/tableaux_3/TestAnna.pl
+++ ComputerScience/C_Initiation/tableaux_3/TestAnna.pl
@@ -18,33 +18,7 @@ tag=Tableau|Initialisation|ParcoursTableau|Boucle
 
 docXXXXX==
 
-**[group=prenom] Quel est le prénom de Revuz
-+Dominique
--Christian
-+Paul
-+Edouard
-
-*[group=prenom] Quel est le prénom de Diaw
-+Anna
--Aminata
-
-*+ Clicker sur la bonne conjugaison du verbe  
->Je suis ( aller / {{allé}} ) au bureau hier
-
-=**[nbb=2,nbg=2] Choisir les mots qui sont des mots clefs de python
-+=["assert","async","await","break","class","continue","def","del","else","except","finally"]
--=["whiles","boucle","attend","cause","deff","dep"]
-
-=*[nb=5,group=odd] What are the odd numbers ? 
-+=[x for x in range(3,77) if x%2==1 ]
--=[x for x in range(3,77) if x%2==0 ]
-
-=*[nb=3,group=odd] What are the even numbers ? 
--=[x for x in range(3,77) if x%2==1 ]
-+=[x for x in range(3,77) if x%2==0 ]
-
 On respecte le format standart des AMC selon le lien suivant : https://www.auto-multiple-choice.net/auto-multiple-choice.fr/AMC-TXT.shtml
-
 
 * AMC avec possibilité d'un seul choix de réponse et donc une seule bonne réponse possible
 Réponses à mettre sous la forme : + Bonne_Rep
