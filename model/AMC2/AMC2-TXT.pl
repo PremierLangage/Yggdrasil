4549bcd2d794a6ca85b2185fe082fd4a10e1d225
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-10-16 12:21:49 +0200

    adiaw modified Yggdrasil/model/AMC2/AMC2-TXT.pl

--- model/AMC2/AMC2-TXT.pl
+++ model/AMC2/AMC2-TXT.pl
@@ -4,7 +4,14 @@ extends= /model/AMC2/QCM-AMCE.pl
 
 title= Fichiers de questions
 
-text=
+text==
+Bande moule 
+    
+==
+
+questions==
+
+* Qui a les pls
 
 @ exe.txt [question1.txt]
 @ exe.txt [question2.txt]
