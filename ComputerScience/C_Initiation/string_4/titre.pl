a02e81d0dbe05884866893c6fdcc78d369d8caeb
Justine Falque <justine.falque@lilo.org>
2020-10-18 13:46:02 +0200

    jfalque modified Yggdrasil/ComputerScience/C_Initiation/string_4/titre.pl

--- ComputerScience/C_Initiation/string_4/titre.pl
+++ ComputerScience/C_Initiation/string_4/titre.pl
@@ -6,7 +6,7 @@ tag=tab|string
 extends=/ComputerScience/C/template/stdsandboxC.pl
 
 text==
-On définit un mot comme étant une suite de caractères commençant une lettre alphabetique
+On définit un mot comme étant une suite de caractères commençant par une lettre alphabetique
 et une phrase comme une suite de mots séparés par des espaces. Dans un titre tous les mots commencent 
 par une majuscule.
 Ecrire une fonction `void met_en_titre(char s[])` qui reçoit une phrase sous forme d'une chaine de caractères
@@ -57,6 +57,7 @@ tests==
  ["aleatoire",""," ".join(["".join([chr(random.randint(97, 122)) for i in range(random.randint(2,10))]) + " " +"".join([ chr(random.randint(65, 90)) for i in range(random.randint(0,1))]) for j in range(5)])],
   ]
 ==
+
 
 
 
