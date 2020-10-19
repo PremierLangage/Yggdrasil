d39632d2b7d1b0b750c05394cf65e3370709f74d
Clement BOIN <Clement.Boin@univ-mlv.fr>
2020-10-19 12:29:24 +0200

    cboin modified Yggdrasil/ComputerScience/C_Initiation/6_malloc/matrice_alloc.pl

--- ComputerScience/C_Initiation/6_malloc/matrice_alloc.pl
+++ ComputerScience/C_Initiation/6_malloc/matrice_alloc.pl
@@ -68,7 +68,10 @@ int main(int argc, char* argv[]){
 
 tests==
 
-[["Exécution simple", "1 1", "0 "]]
+[["Exécution simple", "1 1", "0 "],
+ ["Matrice vide", "0 0", ""],
+ ["Matrice carée","4 4","0 0 0 0 \n0 0 0 0 0 \n0 0 0 0 \n0 0 0 0 \n]
+ ]
 
 ==
 
