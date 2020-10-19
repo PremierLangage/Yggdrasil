7c3872fb39c09aa807920a5ea29d1ba8dd5750b5
Justine Falque <justine.falque@lilo.org>
2020-10-19 12:15:20 +0200

    jfalque modified Yggdrasil/AAAA/dominique/multistep.pl

--- AAAA/dominique/multistep.pl
+++ AAAA/dominique/multistep.pl
@@ -80,7 +80,7 @@ nbstep = len(list_questions)
 random.shuffle(list_questions)
 
 step= -1 # première étape 
-text= f"Ce test a {nbstep} questions. Pas de retour arrière et un seul essai ! "
+text= f"Ce test contient {nbstep} questions. Pas de retour arrière et un seul essai ! "
 
 
 scores=[]
