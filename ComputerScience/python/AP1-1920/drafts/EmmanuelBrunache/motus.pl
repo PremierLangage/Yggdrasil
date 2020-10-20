91c2d6b7da57cef668ce51d81ed2bcf0f230ed95
Emmanuel BRUNACHE <emmanuel.brunache@ac-creteil.fr>
2020-10-20 14:43:17 +0200

    ebrunache modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/motus.pl

--- ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/motus.pl
+++ ComputerScience/python/AP1-1920/drafts/EmmanuelBrunache/motus.pl
@@ -13,8 +13,9 @@ Ecrire une fonction `int motus(char mystere[],char proposition[],int info_sortie
  - 2 si le caractère de la proposition est à la même place dans le mot mystère.
  - 1 si le caractère de la proposition existe dans le mot mystère mais n'est pas à la bonne place.
 
-Attention: Si une lettre apparait n fois dans le mot mystère, on ne peut avoir au maximum que n chiffres différents de 0 dans info_sortie. Exemple: si le mot mystère est 'bonjour' et que la proposition est 'oooonnn', le tableau info_sortie sera 1200000
-Elle fait comme la fonction strlen mais vous n'avez bien entendu pas le droit de l'utiliser.
+Attention: Si une lettre apparait n fois dans le mot mystère, on ne peut avoir au maximum que n chiffres différents de 0 dans info_sortie. Exemple: si le mot mystère est 'bonjour' et que la proposition est 'oooonnn', le tableau info_sortie sera 1200100.
+
+Par ailleurs, la fonction renvoie 0 si la proposition n'est pas de la même taille que le mot mystère.
 ==
 code_before==
 
@@ -26,19 +27,40 @@ code_before==
 taboo=string.h|strlen
 
 editor.code==
-/* ... */  longueurchaine(char tab[]){
+/* ... */  motus(char mystere[],char proposition[],int info_sortie[]){
 /* A compléter */
 }
 
 ==
 
 solution==
-int longueurchaine(char tab[]){
-  int i=0;
-  while(tab[i]!='\0'){
-    i++;
+int motus(char mystere[],char proposition[],int info_sortie[]){
+  int l_mys=strlen(mystere);
+  int l_prop=strlen(proposition);
+  if (l_mys!=l_prop){
+    return 0;
   }
-  return i;
+  int i;
+  int j;
+  for (i=0;i<l_mys;i++){
+    info_sortie[i]=0;
+    if (mystere[i]==proposition[i]){
+      info_sortie[i]=2;
+      mystere[i]='*';
+    }
+  }
+  for (i=0;i<l_mys;i++){
+    if (info_sortie[i]==0){
+      for(j=0;j<l_mys;j++){
+        if(proposition[i]==mystere[j]){
+          info_sortie[i]=1;
+          mystere[j]='*';
+          break;
+        }
+      }
+    }
+  }
+  return 1;
 }
 
 ==
