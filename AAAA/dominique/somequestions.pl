30865a0b6187fc33b2fa129e4bd4d0e44be6ac6e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-10-21 11:29:24 +0200

    drevuz modified Yggdrasil/AAAA/dominique/somequestions.pl

--- AAAA/dominique/somequestions.pl
+++ AAAA/dominique/somequestions.pl
@@ -1,6 +1,12 @@
 
 
+# Justine soit tu utilise une ligne comme ceci 
 
+# questions=@ justine_question.txt
+
+# soit directement dans la variable pl 
+# ce la te donne plus de liberté car tu peux
+# créer une variable comments qui contient des questions pas finies 
 questions==
 
 * question radio
@@ -16,3 +22,16 @@ questions==
 
 ==
 
+comments==
+
+* Une bonne question mais je n'ai pas de mauvaise réponces 
++ une bonne 
+
+** Une autre bonne question mais je n'ai pas de bonne réponces 
+- une bonne 
+
+
+IL faudrais une question sur l'ensemble de tout les ensembles ...
+
+
+==
