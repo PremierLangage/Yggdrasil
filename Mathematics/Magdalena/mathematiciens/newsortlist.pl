9fbe662f698aacb4b7b5a80c8e6fb10784dc02e1
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-10-21 10:31:27 +0200

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/mathematiciens/newsortlist.pl

--- Mathematics/Magdalena/mathematiciens/newsortlist.pl
+++ Mathematics/Magdalena/mathematiciens/newsortlist.pl
@@ -4,7 +4,7 @@ extends = /exemples/sortlist/template.pl
 
 # la colonne nom doit être dans le bon ordre
 columns=nom,dates
-number=7
+number=6
 
 #@ toto.csv [data.csv]
 @ mathematiciensListeCourte.csv [data.csv]
@@ -12,6 +12,7 @@ number=7
 title = Trions des mathématiciens 
 
 text= Ordonnez les dans l'ordre historique de la date de naissance
+
 
 
 
