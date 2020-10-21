9cefee81795e14433f2dcfabd1550f3088c5d9c5
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-21 13:23:47 +0200

    nborie modified Yggdrasil/ComputerScience/C/examples/addition_enfants.pl

---
+++ ComputerScience/C/examples/addition_enfants.pl
@@ -0,0 +1,39 @@
+# Pour calmer la gosse de mon frère qui me casse les bonbons avec ses additions
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+inputbox =: Input
+inputbox.type = number
+inputbox.placeholder = Answer
+inputbox.maxlength = 4
+inputbox.appearance = outline
+
+before==
+import random
+
+a = random.randint(1, 80)
+b = random.randint(1, 100-a)
+r = a + b
+==
+
+title==
+Addition
+==
+
+text==
+Quel est le résultat de l'addition ** {{ a }} + {{ b }} ** ?
+==
+
+form==
+{{ inputbox|component}}
+==
+
+evaluator==
+if r == inputbox.value:
+    grade = (100, '<span class="success-state">Bravo, c'est bien ça !</span>')
+else:
+    grade = (0, '<span class="error-state">Non, ce n'est pas le bon résultat...</span>')
+==
+
