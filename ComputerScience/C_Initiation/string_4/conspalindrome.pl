28a2b918341f96e9a6cb566f6a97ab625d32961c
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-23 16:42:18 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/string_4/conspalindrome.pl

--- ComputerScience/C_Initiation/string_4/conspalindrome.pl
+++ ComputerScience/C_Initiation/string_4/conspalindrome.pl
@@ -15,7 +15,7 @@ text==
 
 Ecrire une fonction de prototype `int construit_palin(char s[],int taille)` 
 qui reçoit un tableau de taille `taille` contenant une chaine de caractères et la transforme,
-si possible, en palindrome:<br>  
+si possible, en palindrome: 
 <ul>
 <li> si la chaine est déjà un palindrome elle reste inchangée </li>
 <li> sinon, si la taille du tableau est suffisante, on la concatène à son miroir pour que le tableau contienne un palindrome. </li>
