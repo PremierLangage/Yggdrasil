a8d3f2d1b37e2795d26252f085a1c609e5176778
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-23 17:57:42 +0200

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/string_4/Test_conspalindrome.pl

--- ComputerScience/C_Initiation/string_4/Test_conspalindrome.pl
+++ ComputerScience/C_Initiation/string_4/Test_conspalindrome.pl
@@ -21,7 +21,7 @@ si possible, en palindrome:
 <li> sinon, 
   <ul>
     <li> si la taille du tableau est suffisante, on la concatène à son miroir pour que le tableau contienne un palindrome.</li>
-    <li> Si la taille du tableau n'est pas suffisante, on ne fait rien. </li> 
+    <li> si la taille du tableau n'est pas suffisante, on ne fait rien. </li> 
   </ul>
 </li>
 </ul>
