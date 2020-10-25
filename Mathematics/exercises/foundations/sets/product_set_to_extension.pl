7f176904c662d1ab08ef92e4458ffea005e450ba
David DOYEN <David.Doyen@u-pem.fr>
2020-10-25 19:58:10 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/product_set_to_extension.pl

--- Mathematics/exercises/foundations/sets/product_set_to_extension.pl
+++ Mathematics/exercises/foundations/sets/product_set_to_extension.pl
@@ -6,7 +6,7 @@ before ==
 lenA, lenB = randitem([[2,3],[3,2],[2,2]])
 A = rand_finiteset(lenA,list(range(10)))
 B = rand_finiteset(lenB,list(range(10)))
-sol = set(A*B)
+sol = list(ProductSet((A,B))
 ==
 
 text == 
