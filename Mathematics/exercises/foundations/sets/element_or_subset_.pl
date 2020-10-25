1fd76117239b1466b9d486440be556c5548e9e3f
David DOYEN <David.Doyen@u-pem.fr>
2020-10-25 15:25:07 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/element_or_subset_.pl

--- Mathematics/exercises/foundations/sets/element_or_subset_.pl
+++ Mathematics/exercises/foundations/sets/element_or_subset_.pl
@@ -56,11 +56,6 @@ for i in range(n):
             a=randint(1,b-1)
             lhs.append(list_randint_norep(a,0,9,rhs[i])+list_randitem_norep(b-a,rhs[i]))
 
-for i in range(n):
-    if isinstance(lhs[i],list):
-        lhs[i]=latex(FiniteSet(*lhs[i]))
-    if isinstance(rhs[i],list):
-        rhs[i]=latex(FiniteSet(*rhs[i]))
 ==
 
 text ==
