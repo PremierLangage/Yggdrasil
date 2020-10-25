96ea6adb6f0d72c1f5eb547ef33cd2ad20bae71e
David DOYEN <David.Doyen@u-pem.fr>
2020-10-25 19:32:49 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/product_set_to_extension.pl

--- Mathematics/exercises/foundations/sets/product_set_to_extension.pl
+++ Mathematics/exercises/foundations/sets/product_set_to_extension.pl
@@ -1,31 +1,17 @@
-extends = /Mathematics/template/mathbasic.pl
-
-input1 =: MathInput
-
-form==
-{{input1|component}}
-==
+extends = /model/mathinput.pl
 
 title = Produit cartésien
 
-lang = fr
 
 before ==
-keyboards_JSON['virtualKeyboards']="sets"
-input1.config = keyboards_JSON
-
-lenA,lenB=randitem([[2,3],[3,2],[2,2]])
-A=rand_finiteset(lenA,list(range(10)))
-B=rand_finiteset(lenB,list(range(10)))
-sol=list(ProductSet(A,B))
-sol_tex=str(sol)
-sol_tex=sol_tex[1:-1]
-A_tex=latex(A)
-B_tex=latex(B)
+lenA, lenB = randitem([[2,3],[3,2],[2,2]])
+A = rand_finiteset(lenA,list(range(10)))
+B = rand_finiteset(lenB,list(range(10)))
+sol = ProductSet(A,B)
 ==
 
 text == 
-Ecrire en extension l'ensemble $! \\{ {{A_tex}} \\} \times \\{ {{B_tex}} \\}. !$
+Ecrire en extension l'ensemble $! \\{ {{A|lattex}} \\} \times \\{ {{B|latex}} \\}. !$
 ==
 
 evaluator ==
@@ -36,6 +22,7 @@ score,numerror,feedback=ans_struct_expr(input1.value,sol,"composite")
 solution ==
 La solution est $!\{ {{sol_tex}} \}!$.
 ==
+
 
 
 
