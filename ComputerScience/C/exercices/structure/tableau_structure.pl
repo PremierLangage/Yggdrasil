a0668ce2334ad0462ecdbc3bfb3ee730844185ad
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-26 16:43:31 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/structure/tableau_structure.pl

--- ComputerScience/C/exercices/structure/tableau_structure.pl
+++ ComputerScience/C/exercices/structure/tableau_structure.pl
@@ -1,16 +1,30 @@
-# Copyright 2017 Elise Hardy <ehardy@etud.u-pem.fr>
-# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2017 Elise Hardy <ehardy@etud.u-pem.fr>
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Déclaration d'un tableau de structure
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Elise Hardy
+
 title=Tableau de structure
 tag=structure
-extends=/ComputerScience/C/template/stdsandboxC.pl
 
 text==
-Déclarez une énumaration *Fruit* contenant les fruits *pomme, poire, banane, cerise*.
-Puis déclarer une structure *Panier* contenant un tableau nommé *tab* de *Fruit* de taille 4. 
+Déclarez une énumération **Fruit** contenant les fruits *pomme*, *poire*, *banane* et *cerise*.
+Puis déclarer une structure *Panier* contenant un tableau de 4 **Fruit** nommé *tab*. 
 
 ==
 
@@ -51,4 +65,5 @@ int main(int argc, char* argv[]){
 tests==
 [ ["Déclaration et utilisation", "",""] ] 
 ==
+
 
