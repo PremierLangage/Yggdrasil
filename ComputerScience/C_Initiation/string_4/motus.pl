0ed6d328c951945eae6951fa2ec64e5ce913022e
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-26 15:01:16 +0100

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/string_4/motus.pl

--- ComputerScience/C_Initiation/string_4/motus.pl
+++ ComputerScience/C_Initiation/string_4/motus.pl
@@ -12,8 +12,8 @@ extends=/ComputerScience/C/template/std_progC.pl
 text==
 
 Ecrire une fonction `int motus(char mystere[],char proposition[],int info_sortie[])` qui reçoit une chaine de caractères `mystere` à découvrir, 
-une chaine de caractères `proposition` et un tableau d'entiers `info_sortie` de même taille que le mot mystère.
-Le tableau info_sortie[] se remplit de la manière suivante pour chacun des i-ème caractères de la `proposition` :<br>
+une chaine de caractères `proposition` et un tableau d'entiers `info_sortie` de même taille que le mot mystère.<br>
+Pour chacun des i-ème caractères de la `proposition, `Le tableau `info_sortie[]` se remplit de la manière suivante :<br>
  - 0 si le caractère de la proposition n'est pas dans le mot mystere.<br>
  - 1 si le caractère de la proposition existe dans le mot mystère mais n'est pas à la bonne place.<br>
  - 2 si le caractère de la proposition est à la même place dans le mot mystère.<br>
