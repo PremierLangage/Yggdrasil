3a643f7167b35c96b7f5a9c2702d7a46725918ee
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-26 16:03:49 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/structure/structure_dans_structure.pl

--- ComputerScience/C/exercices/structure/structure_dans_structure.pl
+++ ComputerScience/C/exercices/structure/structure_dans_structure.pl
@@ -63,9 +63,9 @@ int main(int argc, char* argv[]){
 ==
 
 checks_args_stdin==#|python|
-[ ["Déclaration et utilisation 1", ["14 3 42"], ""],
+[ ["Déclaration et utilisation 1", ["14", "3 42"], ""],
   ["Déclaration et utilisation 2", ["47 85 913"], ""],
-  ["Aléatoire", " ".join([str(random.randint(0,99)) for i in range(3)]), ""] ] 
+  ["Test aléatoire", [str(randint(0,99)) for i in range(3)], ""] ] 
 ==
 
 
