d3339c932fe29b31c31b212d053751532fb01fd2
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-26 13:34:48 +0100

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/string_4/mots_croises.pl

--- ComputerScience/C_Initiation/string_4/mots_croises.pl
+++ ComputerScience/C_Initiation/string_4/mots_croises.pl
@@ -12,9 +12,9 @@ text==
 Pour représenter une grille de mots croisés on utilise un tableau rectangulaire de `char` de taille LIGNES$%\times%$COLONNES.  
 Dans une grille, les mots sont écrits en majuscules, soit horizontalement de gauche à droite, soit verticalement de haut en bas.  
 On mémorise :
-  une case vide par 0
-  une case noire par 1
-  une case remplie par la lettre majuscule alphabétique contenue
+    une case vide par 0
+    une case noire par 1
+    une case remplie par la lettre majuscule alphabétique contenue
 
 Ecrire une fonction `int place_noire(char plateau[][COLONNES],int x,int y)` qui place
 une case noire sur la case de coordonnées(x,y) de la grille.  
