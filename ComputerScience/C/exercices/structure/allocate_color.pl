421247df467ca0f7419c6678c8e787c01025320e
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-26 15:37:12 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/structure/allocate_color.pl

--- ComputerScience/C/exercices/structure/allocate_color.pl
+++ ComputerScience/C/exercices/structure/allocate_color.pl
@@ -33,7 +33,7 @@ initialisée avec les trois entiers bien renseignés dans les champs
 `R` pour **red**, `G` pour **green**, et `B` pour **blue**.
 ==
 
-editor.code==
+editor.code==#|c|
 typedef ...
 ...
 ... Color;
@@ -44,8 +44,7 @@ typedef ...
 
 ==
 
-solution==
-
+solution==#|c|
 typedef struct coco{
   int red;
   int green;
@@ -53,49 +52,39 @@ typedef struct coco{
 }Color;
 
 Color* allocate_color(int R, int G, int B){
-    
     Color *p=(Color *)malloc(sizeof(struct coco));
+
     p->red = R ;
     p->green = G;
     p->blue = B;
     return p;
 }
-
 ==
 
-codebefore==
-
+code_before==#|c|
 #include <stdio.h>
 #include <stdlib.h>
-
 ==
 
-
-codeafter==
-
- 
+code_after==#|c|
 int main(int argc, char* argv[]){
 Color *s;
 
   s = allocate_color(atoi(argv[2]), atoi(argv[3]), atoi(argv[4]));
-
   printf("%s", argv[1]);
-
   printf(" : (%d, %d, %d)\n", s->red, s->green, s->blue);
-
   return 0;
 }
-
 ==
 
-tests== 
-[ ["Simple", "Rouge 255 0 0", ""],
-	["Simple", "Bleu 0 0 255", ""],
-  ["Simple", "Violet 255 0 255", ""],
-	["Simple", "Noir 0 0 0", ""],
-  ["Couleur aléatoire", "'Couleur aléatoire' " + " ".join([str(random.randint(0,255)) for i in range(3)]), ""],
-  ["Couleur aléatoire", "'Couleur aléatoire' " + " ".join([str(random.randint(0,255)) for i in range(3)]), ""],
-  ["Couleur aléatoire", "'Couleur aléatoire' " + " ".join([str(random.randint(0,255)) for i in range(3)]), ""] ]
+checks_args_stdin==#|python|
+[ ["Test simple rouge", ["Rouge", "255", "0", "0"], ""],
+	["Test simple bleu", ["Bleu 0 0 255"], ""],
+  ["Test simple violet", ["Violet 255 0 255"], ""],
+	["Test simple noir", ["Noir 0 0 0"], ""],
+  ["Couleur aléatoire 1", ["Couleur aléatoire"] + [str(random.randint(0,255)) for i in range(3)], ""],
+  ["Couleur aléatoire 2", ["Couleur aléatoire"] + [str(random.randint(0,255)) for i in range(3)], ""],
+  ["Couleur aléatoire 3", ["Couleur aléatoire"] + [str(random.randint(0,255)) for i in range(3)], ""] ]
 ==
 
 
