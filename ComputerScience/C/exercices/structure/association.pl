61a973fce988cf06896a26565122132271620a6c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-27 20:10:54 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/structure/association.pl

---
+++ ComputerScience/C/exercices/structure/association.pl
@@ -0,0 +1,120 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+title=Variables et modélisation
+
+author=Nicolas Borie
+
+tag=C|variable|identificateurs|modelisation|type
+
+text==
+Un informaticien chevronné, a codé de nombreuses variables C.
+Tentez d'associer, pour chaque type de variable, ce qu'il pourrait modéliser 
+dans la vie réelle de manière naturelle.
+==
+
+# STEP 1
+match =: MatchList
+
+# remove the comment of the next line to display the properties of the component
+# in realtime inside the form.
+
+# match.debug % true
+
+# STEP 2
+before== #|python|
+import random
+match.nodes = []
+expected = []
+
+match.nodes.append({"id": "s1", "content": "struct queue tab[8];", "source": True})
+match.nodes.append({"id": "t1", "content": "un péage autoroutier avec plusieurs guichets", "target": True})
+expected.append({ "source": "s1", "target": "t1" })
+
+match.nodes.append({"id": "s2", "content": "int* tab;", "source": True})
+match.nodes.append({"id": "t2", "content": "un ensenble de nombres", "target": True})
+expected.append({ "source": "s2", "target": "t2" })
+
+match.nodes.append({"id": "s3", "content": "struct card h[2];", "source": True})
+match.nodes.append({"id": "t3", "content": "une main au poker Texas Hold'em", "target": True})
+expected.append({ "source": "s3", "target": "t3" })
+
+match.nodes.append({"id": "s4", "content": "struct node* root;", "source": True})
+match.nodes.append({"id": "t4", "content": "un graphe enraciné", "target": True})
+expected.append({ "source": "s4", "target": "t4" })
+
+match.nodes.append({"id": "s5", "content": "float c[2];", "source": True})
+match.nodes.append({"id": "t5", "content": "un nombre complexe", "target": True})
+expected.append({ "source": "s5", "target": "t5" })
+
+match.nodes.append({"id": "s6", "content": "unsigned char[3];", "source": True})
+match.nodes.append({"id": "t6", "content": "un temps (heures, minutes et secondes)", "target": True})
+expected.append({ "source": "s6", "target": "t6" })
+
+random.shuffle(match.nodes)
+==
+
+# STEP 3
+form==
+{{ match|component }}
+==
+
+# STEP 4
+evaluator== #|python|
+
+def in_links(conn, links):
+    for e in links:
+        if e['source'] == conn["source"] and  e['target'] == conn["target"]:
+            return True
+    return False
+
+error = 0
+for e in expected:
+    if not in_links(e, match.links):
+        error = error + 1
+
+for e in match.links:
+    e['css'] = 'error-state anim-fade'
+    if in_links(e, expected):
+        e['css'] = 'success-state  anim-flip'
+
+if error == 0:
+    feedback = '<span class="success-state">Bravo, ça devrait en effet bien fonctionner ainsi!</span>'
+    grade = (100, feedback)
+else:
+    feedback = '<span class="error-state">Tout n\'est pas correct, il y a %d mauvaises associations.</span>' % error
+    grade = (0, feedback)
+==
+
+
+
+extracss==
+<style>
+.exercise__instructions, .exercise__feedback {
+    margin-bottom: 16px;
+}
+
+.match-list-component svg path {
+    stroke-width: 4px;
+}
+</style>
+==
+
+
