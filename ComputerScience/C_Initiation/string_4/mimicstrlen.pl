272660cd32bbe6d122e73cab7e10364a4d8af519
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-10-27 15:41:05 +0100

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/string_4/mimicstrlen.pl

--- ComputerScience/C_Initiation/string_4/mimicstrlen.pl
+++ ComputerScience/C_Initiation/string_4/mimicstrlen.pl
@@ -3,7 +3,7 @@ author=
 
 title=Longueur de chaine
 
-tag=string|FinDeChaine
+tag=string|tableau
 
 extends=/ComputerScience/C/template/std_progC.pl
 
@@ -55,6 +55,7 @@ checks_args_stdin==#|python|
 [ ["simple éxécution", ["bonjour toto"],""],
 ["alea",["".join([chr(randint(97,122)) for i in range(randint(1,15))])],""]]
 ==
+
 
 
 
