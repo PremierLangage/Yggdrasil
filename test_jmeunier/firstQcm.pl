67acb885de3973d6d0d1ac4018a6fd58d537f1fe
Jean Marc Meunier <jean-marc.meunier@univ-paris8.fr>
2020-10-27 17:50:20 +0100

    jmeunier1 modified Yggdrasil/test_jmeunier/firstQcm.pl

---
+++ test_jmeunier/firstQcm.pl
@@ -0,0 +1,23 @@
+# inspiré de Copyright 2020 Nicolas Borie <nicolas.borie@u-pem.fr>
+author=Jean-Marc Meunier
+
+# choix du modèle de question
+extends=/model/AMC_TXT.pl
+
+title=QCM statistiques descriptives
+questions==
+
+# un seul choix
+* quelle est la définition de la moyenne ?
+- la somme des observations divisée par le nombre de sujets
++ la somme des observations divisée par le nombre d'observations
+- la somme des sujets divisée par le nombre de variables
+- le valeur maximale divisée par le nombre de valeurs
+
+# plusieurs choix
+** Quelle est la définition de la médiane ?
+- la valeur qui coupe la somme des observations en deux parties égales
++ la valeur pour laquelle 50% des observations sont inférieures
++ la valeur pour laquelle 50% des observations sont supérieures
+- la valeur corrrespondant à la moitié de la distribution
+==
