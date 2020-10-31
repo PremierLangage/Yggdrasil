d7030bb3858dfd8ebac126e22fbf37b6de08c337
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-31 10:40:37 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/malloc/add_ordered_set.pl

--- ComputerScience/C/exercices/malloc/add_ordered_set.pl
+++ ComputerScience/C/exercices/malloc/add_ordered_set.pl
@@ -18,87 +18,36 @@ extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
 
-title=Allocation triangulaire
-tag=allocation|function
+title=Ajout ordonné dans un ensemble dynamique
+tag=allocation|fonction|reallocation|realloc|difficile
 
 text==
-Écrire une fonction C **triangle** qui prend en argument un entier (qui sera 
-toujours strictement positif). Votre fonction devra allouer un tableau à 
-deux dimensions de forme triangulaire. Voici un exemple pour la taille 5 :
-
-    X               ( première ligne [0] : 1 élément )
-    X  X            (  seconde ligne [1] : 2 éléments)
-    X  X  X         (troisième ligne [2] : 3 éléments)
-    X  7  X  X      (   l'elément [3][1] vaut ici 7  )
-    X  X  X  X  X   (cinquième ligne [4] : 5 éléments)
-
-Votre fonction devra donc retourner un tableau doublement indéxé `[i][j]` 
-(i pour les lignes et j pour les colonnes). La première ligne peut contenir
-1 élément, la seconde ligne 2 éléments, etc... En taille **n**, le tableau
-retourné et fraichement alloué contiendra **n** lignes.
+L'objectif de cet exercice est de coder une fonction d'insertion dans un 
+ensemble dynamique (sa taille mémoire s'adapte dynamiquement).
 ==
 
 editor.code==#|c|
-#include ...
 
-int** triangle(int n){
-  /* Votre code ici... */
-}
 ==
 
 solution==#|c|
-#include <stdlib.h>
-
-int** triangle(int n){
-  int** tab = (int**)malloc(sizeof(int*) * n);
-  int i;
-
-  for(i=0 ; i<n ; i++){
-    tab[i] = (int*)malloc(sizeof(int) * (i+1));
-  }
-  return tab; 
-}
+typedef struct{
+  int* values;       /* Ordered array containing values of the set */
+  int current_size;  /* Number of elements inside the set */
+  int memory_size;   /* Memory size of array values (offen larger than the current size) */
+}OrderedSet;
 ==
 
 code_before==#|c|
-#include <stdio.h>
+
 ==
 
 code_after==#|c|
 
-int main(int argc, char* argv[]){
-  int taille = atoi(argv[1]);
-  int** tab = triangle(taille);
-  int i, j;
-
-  for (i=0 ; i<taille ; i++){
-    tab[i][0] = 1;
-    tab[i][i] = 1;
-  }
-  for (i=1 ; i<taille ; i++){
-    for (j=1 ; j<i ; j++){
-      tab[i][j] = tab[i-1][j] + tab[i-1][j-1];
-    }
-  }
-  for (i=0 ; i<taille ; i++){
-    for (j=0 ; j<i+1 ; j++){
-      printf("%d ", tab[i][j]);
-    }
-    putchar('\n');
-  }
-  return 0;
-}
 ==
 
 checks_args_stdin==#|python|
-[["Exécution simple", ["5"], ""],
- ["Taille minimale", ["1"], ""],
- ["Test aléatoire 1", [str(randint(2, 12))], ""],
- ["Test aléatoire 2", [str(randint(2, 12))], ""],
- ["Test aléatoire 3", [str(randint(2, 12))], ""],
- ["Test aléatoire 4", [str(randint(2, 12))], ""],
- ["Test aléatoire 5", [str(randint(2, 12))], ""],
- ["Test aléatoire 6", [str(randint(2, 12))], ""]]
+[["Exécution simple", [""], ""]]
 ==
 
 
