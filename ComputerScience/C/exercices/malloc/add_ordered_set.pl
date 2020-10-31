ebce56c677f272af9d2d963091ea1f9e77ef4714
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-10-31 13:34:11 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/malloc/add_ordered_set.pl

--- ComputerScience/C/exercices/malloc/add_ordered_set.pl
+++ ComputerScience/C/exercices/malloc/add_ordered_set.pl
@@ -44,22 +44,46 @@ typedef struct{
   int memory_size;   /* Memory size of values (offen larger than the current size) */
 }Ordered_list;
 
-int add_ordered_set(Ordered_list* s, int val){
+int add_ordered_list(Ordered_list* s, int val){
   /* votre code ici... */
 }
 ==
 
 solution==#|c|
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct{
-  int* values;       /* Ordered array containing values of the set */
-  int current_size;  /* Number of elements inside the set */
+  int* values;       /* Ordered array containing values of the list */
+  int current_size;  /* Number of elements inside the list */
   int memory_size;   /* Memory size of values (offen larger than the current size) */
-}Ordered_set;
+}Ordered_list;
 
-int add_ordered_set(Ordered_set* s, int val){
+int add_ordered_list(Ordered_list* s, int val){
+  int new_size, i, pos;
+  /* reallocation if needed */
   if (s->current_size == s->memory_size){
-
+    new_size = s->memory_size + (s->memory_size / 10) + 1;
+    s->values = realloc(s->values, new_size*sizeof(int));
+    if (s->values == NULL){
+      fprintf(stderr, "Memory reallocation error\n");
+      return 0;
+    }
+    s->s->memory_size = new_size;
   }
+  /* search pos, the insertion position index */
+  i = 0;
+  while (s->value[i] <= val)
+    i++;
+  }
+  pos = i;
+  /* translate by 1 on the right higher values */
+  for(i=s->current_size-1 ; i>=pos ; i--)
+    s->value[i+1] = s->value[i];
+  /* insertion and update the size */
+  s->value[pos] = val;
+  s->current_size++;
+  return 1;
 }
 ==
 
