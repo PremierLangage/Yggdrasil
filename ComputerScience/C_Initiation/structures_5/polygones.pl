6b9d9278528bc79c46938999f3e063882e7cee0d
Marie VAN DEN BOGAARD <Marie.Vandenbogaard@univ-mlv.fr>
2020-11-01 12:01:12 +0100

    mvan den bogaard1 modified Yggdrasil/ComputerScience/C_Initiation/structures_5/polygones.pl

--- ComputerScience/C_Initiation/structures_5/polygones.pl
+++ ComputerScience/C_Initiation/structures_5/polygones.pl
@@ -13,25 +13,30 @@ On utilise la structure<br>
 typedef struct{<br>
 int x,y;<br>
 }Point;<br>
-pour représenter un point du plan. (On suppose qu'elle est déjà définie.)<br>
+pour représenter un point du plan.<br>
 Ecrire une fonction `distance ` qui reçoit deux *Point* et renvoie leur distance.<br>
-Définir une structure `Polygone` formée : 
- - d'un tableau de *Point* de nom `sommets`, et
- - d'un entier `nb_sommets`, le nombre effectif de points. <br>
+Définir une structure `Polygone` formé : 
+d'un tableau de *Point* de nom `sommets`et
+d'un entier `nb_sommets`, le nombre effectif de points.br>
 
 Ecrire une fonction `perimetre` qui recoit un polygone et renvoie son périmetre.
 ==
 
 editor.code==
 typedef struct {
-...
+Point sommets[MAXSOMMETS];
+int nb_sommets;
 }Polygone;
 
-double distance(...){
-  ...
+double distance(Point a ,Point b){
+  return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
 }
-double perimetre(...){
-...
+double perimetre(Polygone p){
+int i;
+double perim=distance(p.sommets[p.nb_sommets-1],p.sommets[0]);
+for(i=0;i<p.nb_sommets-1;i++)
+  perim+=distance(p.sommets[i],p.sommets[i+1]);
+return perim;
 }
   
 ==
