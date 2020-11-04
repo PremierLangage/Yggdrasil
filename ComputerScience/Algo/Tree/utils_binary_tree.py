8c9d464b55290edb0cb03958b090eeaed809bc42
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-04 16:16:23 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/utils_binary_tree.py

--- ComputerScience/Algo/Tree/utils_binary_tree.py
+++ ComputerScience/Algo/Tree/utils_binary_tree.py
@@ -103,12 +103,12 @@ def random_binary_tree(nb_nodes, max_height=None, values=None, nb_total_node=Non
         nb_right = (max_height - 1) - nb_left
         T = BinaryNode(label)
         # left child generation
-        if nb_left > 0:
+        if nb_left > 0 and max_height > 0:
             left, val_left = random_binary_tree(nb_left, max_height=max_height-1, values=values, nb_total_node=nb_total_node)
             values = val_left
             T.left = left
         # right child generation
-        if nb_right > 0:
+        if nb_right > 0 and max_height > 0:
             right, val_right = random_binary_tree(nb_right, max_height=max_height-1, values=values, nb_total_node=nb_total_node)
             values = val_right
             T.right = right
