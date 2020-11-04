98853b5585956233836200837f45065a96cbeb20
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-04 15:39:43 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/transversal_bin_tree.pl

--- ComputerScience/Algo/Tree/transversal_bin_tree.pl
+++ ComputerScience/Algo/Tree/transversal_bin_tree.pl
@@ -19,24 +19,7 @@ grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 viewer =: GraphDrawer
-viewer.graph ==
-digraph BST {
-    node [fontname="Arial"];
-    15 -> 6;
-    null0 [shape=point];
-    6 -> null0;
-    null1 [shape=point];
-    6 -> null1;
-    15 -> 18;
-    18 -> 17;
-    null2 [shape=point];
-    17 -> null2;
-    null3 [shape=point];
-    17 -> null3;
-    null4 [shape=point];
-    18 -> null4;
-}
-==
+viewer.graph=random_binary_tree(15, max_height=5).to_dot_code_BST_point()
 
 before==
 ==
@@ -53,4 +36,5 @@ form==
 evaluator==
 grade = (100, "")
 ==
+
 
