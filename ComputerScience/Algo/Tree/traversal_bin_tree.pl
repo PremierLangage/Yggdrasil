022411a563e7be4757eb7604bc02935889d5d162
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-04 17:57:05 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/traversal_bin_tree.pl

--- ComputerScience/Algo/Tree/traversal_bin_tree.pl
+++ ComputerScience/Algo/Tree/traversal_bin_tree.pl
@@ -76,9 +76,9 @@ form = """
 """
 
 if soluce == eval("["+inputbox.value+"]"):
-    grade = (100, '''<br><span class="success-state">Bravo, c'est exactement cela : '''+str(soluce)[1:-1]+'</span>')
+    grade = (100, ''' <span class="success-state">Bravo, c'est exactement cela : '''+str(soluce)[1:-1]+'</span>')
 else:
-    grade = (0, '''<br><span class="error-state">Désolé, vous avez fait une erreur, le parcours attendu était : '''+str(soluce)[1:-1]+'</span>')
+    grade = (0, ''' <span class="error-state">Désolé, vous avez fait une erreur, le parcours attendu était : '''+str(soluce)[1:-1]+'</span>')
 ==
 
 
