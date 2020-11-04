d797b0c53a00570733f8c5176b05afb09c96a6b3
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-04 15:44:55 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/utils_binary_tree.py

--- ComputerScience/Algo/Tree/utils_binary_tree.py
+++ ComputerScience/Algo/Tree/utils_binary_tree.py
@@ -16,7 +16,7 @@
 
 from random import randint
 
-class BinaryNode(object):
+class BinaryNode():
     """
     A class modeling nodes inside binary trees. The empty node (or empty tree) 
     will be modelized by `None`. Therefore, a leaf is a node having `None` for 
