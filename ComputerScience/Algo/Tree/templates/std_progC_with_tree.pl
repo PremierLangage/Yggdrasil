07f3851f794550f28662ec565002d3d06fabb541
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-06 20:11:28 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl

--- ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl
+++ ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl
@@ -41,14 +41,23 @@ extends=/ComputerScience/C/template/std_progC.pl
 #  with an inclusion of components to draw graphs...
 
 viewer0 =: GraphDrawer
+
 viewer1 =: GraphDrawer
+
 viewer2 =: GraphDrawer
+
 viewer3 =: GraphDrawer
+
 viewer4 =: GraphDrawer
+
 viewer5 =: GraphDrawer
+
 viewer6 =: GraphDrawer
+
 viewer7 =: GraphDrawer
+
 viewer8 =: GraphDrawer
+
 viewer9 =: GraphDrawer
 
 
