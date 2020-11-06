0b3ed8ef14e5b09e557f39077c697e93ca0f4e5f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-06 22:43:58 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl

--- ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl
+++ ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl
@@ -230,6 +230,8 @@ if compil_state != 'error':
                     viewer = GraphDrawer()
                     viewer.graph = Tree_exo.to_dot_code_BST_point()
                     stdin_explain += f" \n\n <c-graph-viewer graph='{viewer.graph}'></c-graph-viewer> \n\n"
+                else:
+                    stdin_explain += "\n\n Arbre trop imposant pour être dessiné \n\n"
             else:
                 stdin_explain = ""
             feedback_checks += make_hide_block_on_click("details_check"+str(nb_good+nb_bad), test_c[0], stdin_explain + terminal_code(terminal_log), "")
