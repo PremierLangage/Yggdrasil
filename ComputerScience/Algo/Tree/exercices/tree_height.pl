659ff3ef5f47ed03c55fb55ea360226e7fcf3849
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-06 22:36:37 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/exercices/tree_height.pl

--- ComputerScience/Algo/Tree/exercices/tree_height.pl
+++ ComputerScience/Algo/Tree/exercices/tree_height.pl
@@ -127,7 +127,9 @@ checks_args_stdin==#|python|
  ["Arbre aléatoire à 8 nœuds", [], random_binary_tree(8)[0].to_string_code().replace(' ', '')],
  ["Arbre aléatoire à 10 nœuds", [], random_binary_tree(10)[0].to_string_code().replace(' ', '')],
  ["Arbre aléatoire à 12 nœuds", [], random_binary_tree(12)[0].to_string_code().replace(' ', '')],
- ["Arbre aléatoire à 14 nœuds", [], random_binary_tree(14)[0].to_string_code().replace(' ', '')] ]
+ ["Arbre aléatoire à 14 nœuds", [], random_binary_tree(14)[0].to_string_code().replace(' ', '')],
+ ["Arbre aléatoire à 100 nœuds", [], random_binary_tree(100)[0].to_string_code().replace(' ', '')]
+ ["Arbre aléatoire à 200 nœuds", [], random_binary_tree(200)[0].to_string_code().replace(' ', '')] ]
 ==
 
 
