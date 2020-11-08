6c76a34b82899e10f9a5efc9142bee4f12a5c8f8
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-08 16:20:30 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Calcul.pl

---
+++ Mathematics/Magdalena/Calcul/Calcul.pl
@@ -0,0 +1,23 @@
+
+extends = /model/AMC_TXT.pl
+title = Applications-1
+questions==
+
+**Cocher les égalités vraies
++ $%(5\times 10^3)^2 =5^2\times (10^3)^2%$
++ $%(5\times 10^3)^2 =25\times 10^6%$
++ $% 5^2 \times 2^2 = (5\times 2)^2 %$
+- $% (5\times 10^3)^2 =5^2 \times 10^{3+2}%$
+- $% 5^2 \times 2^2 = (5 \times 2)^{(2+2)}%$
+- $% (5\times 10^3)^2 =5 \times 10^{3\times 2}%$
+
+** 
++ est nulle 
+- est pas nulle
+- est égal a 1 
+ 
+
+==
+
+
+
