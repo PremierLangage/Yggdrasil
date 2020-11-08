70cf1ccab8ad26723bc65f72df897707536797bc
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-08 16:30:58 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Calcul.pl

--- Mathematics/Magdalena/Calcul/Calcul.pl
+++ Mathematics/Magdalena/Calcul/Calcul.pl
@@ -10,13 +10,25 @@ questions==
 - $% (5\times 10^3)^2 =5^2 \times 10^{3+2}%$
 - $% 5^2 \times 2^2 = (5 \times 2)^{(2+2)}%$
 - $% (5\times 10^3)^2 =5 \times 10^{3\times 2}%$
+** Cocher les égalités vraies
++ $% 2\times 5^2 =50%$
++ $% 3^3=27 %$
+- $% 2\times 5^2 =20%$
+- $% 3^3=9 %$
 
-** 
-+ est nulle 
-- est pas nulle
-- est égal a 1 
+
+
+** Cocher les égalités vraies
++ $% \frac{10^6 \times (10^{-3))^2}{10^{-2}} =100 %$
++ $% \frac{10^6 \times (10^{-7}{10^{2}} =10^{-3} %$
+-  $% \frac{10^6 \times (10^{-7}{10^{2}} =10 %$
+- $% \frac{10^6 \times (10^{-3))^2}{10^{-2}} =10^{-1} %$
  
-
+** Cocher les égalités vraies
++ $% \frac{10^6 \times (10^{-3))^2}{10^{-2}} =100 %$
++ $% \frac{10^6 \times (10^{-7}{10^{2}} =10^{-3} %$
+-  $% \frac{10^6 \times (10^{-7}{10^{2}} =10 %$
+- $% \frac{10^6 \times (10^{-3))^2}{10^{-2}} =10^{-1} %$
 ==
 
 
