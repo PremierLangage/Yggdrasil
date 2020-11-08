df2d555d4ee80ab4a1dd2d3a3314f688795c346d
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-08 19:48:31 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Puissance-simple2.pl

---
+++ Mathematics/Magdalena/Calcul/Puissance-simple2.pl
@@ -0,0 +1,29 @@
+
+
+extends = /model/AMC_TXT.pl
+title = Puissances
+questions==
+
+**Cocher les égalités vraies
++ $% \frac{2^5}{4}=2^3 %$
++ $% \frac{(2\times 3)^5}{4^2 \times 9^3}=3 %$
++ $% \frac{(-2)^5 \times 2^{-6}}{2^{-2}}=-2 %$
+- $% \{ \frac{-3}^5 \times (-3)^{-2}}{3^2}=-3^5 %$
+- $% \{ \frac{3^2}{6} =1 %$
+
+
+** Cocher les égalités vraies
++ $% 2^5\times 3^5= (2\times 3)^5 %$
++ $% \frac{2^5 \times 2^3}{2^4} =2^{4} %$
++  $% 5^6 \times 5^{-8} = 5^{-2}%$
++ $% 5^6 \times 5^{-8} = \frac{1}{25}%$
+- $%  (-2)^3 \times 3^3= (2\times 3)^{3} %$
+- $% \frac{1}{2^{-3}}=\frac{-1}{6}%$
+ 
+
+==
+
+
+
+
+
