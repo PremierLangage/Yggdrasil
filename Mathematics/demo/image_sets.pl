7b2cf05eee8b64f7a5c7241c9c6ee7cc02f96bb3
David DOYEN <David.Doyen@u-pem.fr>
2020-11-09 13:49:40 +0100

    ddoyen modified Yggdrasil/Mathematics/demo/image_sets.pl

--- Mathematics/demo/image_sets.pl
+++ Mathematics/demo/image_sets.pl
@@ -9,13 +9,13 @@ expr2 = randitem([n, n+1, n-1, 2*n, 2*n+1, 2*n-1])
 
 f = Lambda(n, (expr1, expr2))
 
-A = rand_finiteset(3, list(range(6)))
+A = rand_finiteset(2, list(range(6)))
 sol = FiniteSet(*[f(x) for x in A])
 ==
 
 text ==
 On considère la fonction $! f : \mathbb{Z} \times \mathbb{Z} \rightarrow \mathbb{Z} !$ telle que
-$$f(n)= ({{expr1|latex}}, {{expr1|latex}})$$
+$$f(n)= ({{expr1|latex}}, {{expr2|latex}})$$
 Déterminer $! f(\\{ {{A|latex}} \\}). !$
 ==
 
