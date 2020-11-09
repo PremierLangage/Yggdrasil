1c393df2ae7b82f9713e8f75d155338077e674c0
David DOYEN <David.Doyen@u-pem.fr>
2020-11-09 11:35:53 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/product_set_to_extension.pl

--- Mathematics/exercises/foundations/sets/product_set_to_extension.pl
+++ Mathematics/exercises/foundations/sets/product_set_to_extension.pl
@@ -6,8 +6,6 @@ before ==
 lenA, lenB = randitem([[2,3],[3,2],[2,2]])
 A = rand_finiteset(lenA,list(range(10)))
 B = rand_finiteset(lenB,list(range(10)))
-sol = FiniteSet(ProductSet(A,B))
-
 sol_latex = latex(set(ProductSet(A,B)))
 ==
 
@@ -18,6 +16,7 @@ Ecrire en extension l'ensemble $! \\{ {{A|latex}} \\} \times \\{ {{B|latex}} \\}
 input.virtualKeyboards = sets
 
 evaluator ==
+sol = list(ProductSet(A,B))
 score, error = eval_set(input.value, sol)
 feedback = message[error]
 ==
