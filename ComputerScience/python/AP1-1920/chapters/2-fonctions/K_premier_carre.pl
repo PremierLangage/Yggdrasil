e1e9e9b656187f94eb901efd041a4d5607ae08d8
Olivier BOUILLOT <Olivier.Bouillot@u-pem.fr>
2020-11-09 13:51:06 +0100

    obouillot modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/K_premier_carre.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/K_premier_carre.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/K_premier_carre.pl
@@ -4,18 +4,10 @@
 # Heritage d'un type d'exercice 
 extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
-title = Afficher les k premiers carrés (de 0 à (k-1)^2)
+title = Afficher les k premiers carrés $%\big (%$de $%0%$ à $%(k-1)^2 \big )%$
 
 code==
-#
-#
-#
-#
-#
-#
-#
-?? = ??(input("Saisisez un entier :"))
-#
+
 ==
 
 text==  
@@ -24,16 +16,19 @@ text==
 <br>
 
 2.  Programmer ensuite une fonction **premiers_carres** qui : <br>
-    * prend en paramètre un entier $k$ <br>
-    * imprime les k premiers carrés de $%0$ à $%(k-1)^2$ <br>
-$%\dfrac{n!}{p! \times(n-p)!} %$
+    * prend en paramètre un entier $%k%$ ; <br>
+    * imprime les k premiers carrés de $%0%$ à $%(k - 1)^2%$. <br>
+
 3.  Pour finir écrivez un programme qui demande à l'utilisateur
 un entier, appelle la fonction `premiers_carres` et imprime le résultat attendu.<br>
 <br>
 
-Un exemple d'exécution du programme est :
-
-
+Un exemple d'exécution du programme est :<br>
+`Saisissez un entier : 4`<br>
+`0`<br>
+`1`<br>
+`4`<br>
+`9`<br>
 ==
 
 soluce==#|python|
