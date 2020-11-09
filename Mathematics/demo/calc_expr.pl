3f4b92d4c3d6d90266f7fa6ee2a658f63bd212a2
David DOYEN <David.Doyen@u-pem.fr>
2020-11-09 14:09:54 +0100

    ddoyen modified Yggdrasil/Mathematics/demo/calc_expr.pl

--- Mathematics/demo/calc_expr.pl
+++ Mathematics/demo/calc_expr.pl
@@ -8,7 +8,7 @@ b = randint(2, 5)
 c = randint(2, 5)
 s = randitem("+", "-")
 
-expr = "(\fra¢{ {%s}^2 %s %s) }{ %s }" % (a, s, b, c)
+expr = "(\frac{ {%s}^2 %s %s) }{ %s }" % (a, s, b, c)
 sol = simplify(latex2sympy(expr))
 
 ==
