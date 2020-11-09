5cce935bf3e9374b9e02aec2fd990bf611101b64
David DOYEN <David.Doyen@u-pem.fr>
2020-11-09 11:58:46 +0100

    ddoyen modified Yggdrasil/Mathematics/demo/finite_sets.pl

--- Mathematics/demo/finite_sets.pl
+++ Mathematics/demo/finite_sets.pl
@@ -10,6 +10,9 @@ B = rand_finiteset(randint(3, 7), items)
 C = rand_finiteset(randint(3, 7), items)
 
 expr = r"A \cup (B \cap C)"
+
+# La solution doit être stockée dans la variable sol
+# La solution doit être un objet SymPy de type FiniteSet
 sol = Union(Intersection(B, C), A)
 ==
 
