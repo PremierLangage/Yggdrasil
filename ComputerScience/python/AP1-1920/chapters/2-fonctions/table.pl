9bf805d523e9fe1362d0c22a494535dbecb75956
Olivier BOUILLOT <Olivier.Bouillot@u-pem.fr>
2020-11-09 14:34:06 +0100

    obouillot modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/table.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/table.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/table.pl
@@ -6,7 +6,8 @@ extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 @ /builder/before.py [builder.py]
 title = Afficher les $%n%$ premiers multiples d'un entier $%x%$
 
-code==def saisie_entier_str_positif(...):
+code==
+def saisie_entier_str_positif(...):
     ?? = ??(input("Saisissez un entier strictement positif : "))
     ...
 
