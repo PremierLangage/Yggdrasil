e6b7e8b1ab2a54bed6968debbb50220335780362
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-09 15:10:58 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Application_def.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Application_def.pl
@@ -0,0 +1,29 @@
+
+extends = /model/AMC_TXT.pl
+title = Application - vocabulaire
+questions==
+
+** Soit $%E, F %$ deux ensembles non vide disjoints et doit $%f: E \to F %$ une application $%x\in E %$, $% y\in F %$, $% A\subset E %$ et $% B \subset F%$ 
++  $% f(x) %$ a du sens,  c'est un élément de $%F %$
++ $% f(x) %$ est l'image de $%x%$ par $% f %$,  c'est un élément de $%F %$
+- $% f(x) %$ est un élément de $%E %$
+- $% f(A) %$ est un élément de $%F %$
++ $% f(A) %$ est une partie de $%F %$
+- $% f(A) %$ s'appelle l'image de $% A %$ par $%f %$
++ $% f(A) %$ s'appelle l'image directe de $% A %$ par $%f %$
+
+
+
+** Soit $% f: \\{ 1,2,3, 4, 5\\} \to \\{6,7,8,9\\} %$ définie par 
+$$ \left(\begin{array}{ccccc} 1&2&3&4&5\\
+6&6&9&6&7
+\end{array} \right) $$
++ est nulle 
+- est pas nulle
+- est égal a 1 
+ 
+
+==
+
+
+
