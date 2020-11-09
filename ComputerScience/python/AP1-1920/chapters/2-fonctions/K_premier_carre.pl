27159f270e908fd013a9f8c45e613c7c153eba54
Olivier BOUILLOT <Olivier.Bouillot@u-pem.fr>
2020-11-09 13:07:47 +0100

    obouillot modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/K_premier_carre.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/K_premier_carre.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/K_premier_carre.pl
@@ -20,15 +20,19 @@ code==
 
 text==  
 
-Programmer une fonction **carre** qui prend en paramètre un entier et retourne son carré.<br>
+1.  Programmer une fonction **carre** qui prend en paramètre un entier et retourne son carré.<br>
+<br>
 
-Programmer ensuite une fonction **premiers_carres** qui : <br>
-* prend en paramètre un entier k <br>
-* imprime les k premiers carrés de 0 à (k-1)^2 <br>
+2.  Programmer ensuite une fonction **premiers_carres** qui : <br>
+    * prend en paramètre un entier $k$ <br>
+    * imprime les k premiers carrés de $%0$ à $%(k-1)^2$ <br>
+$%\dfrac{n!}{p! \times(n-p)!} %$
+3.  Pour finir écrivez un programme qui demande à l'utilisateur
+un entier, appelle la fonction `premiers_carres` et imprime le résultat attendu.<br>
+<br>
 
-Pour finir écrivez un e : <br>
-* qui demande à l'utilisateur un entier, <br>
-* appelle la fonction `premiers_carres` et imprime le résultat attendu.
+Un exemple d'exécution du programme est :
+
 
 ==
 
@@ -70,6 +74,7 @@ mplsoluce3==
 nul
 0
 ==
+
 
 
 
