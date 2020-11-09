9cac8c8b126cfc65c3f564865f635e00d2a73d8e
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-09 17:49:26 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Application_identite.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Application_identite.pl
@@ -0,0 +1,28 @@
+extends = /model/AMC_TXT.pl
+title = Application  identité et composition
+questions==
+
+
+** Soit $%E%$ un ensemble et $% A%$ une partie de $%E%$
++ $% \forall x \in A%$  $%Id_E(x)=Id_A(x) %$
+- $% \forall x \in E%$   $% Id_E(x)=Id_A(x) %$
+- L'application $%Id_E \circ Id_A %$ est bien définie
++ L'application $% Id_E %$ est bijective
+
+
+** Soit $%E%$ et $%F%$ deux ensembles disjoints, $%f:E\to F %$,  $%g :F\to E%$ et $%h: E\to E%$
+Cocher les expressions vraies
+
++ $% f \circ Id_E =f %$
+- $% Id_E \circ f  = Id_F %$
+- $% Id_E \circ f = f %$
++ $% Id_F \circ f = f%$
+- $% f \circ g = Id_E %$
+- $% f \circ g = Id_F %$
++ $% h \circ Id_E = Id_E \circ h =h %$
++ $% g \circ Id_F = g %$
++ $% g \circ Id_F = Id_E \circ g %$
+
+
+==
+
