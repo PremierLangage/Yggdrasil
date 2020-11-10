1b4adaae63280c0e3fa4d1315cb119ae38a70b55
Olivier BOUILLOT <Olivier.Bouillot@u-pem.fr>
2020-11-10 09:57:19 +0100

    obouillot modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/nbchiffres.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/nbchiffres.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/nbchiffres.pl
@@ -7,21 +7,33 @@ title =Apparition d'un chiffre
 
 
 text==
-On veut déterminer le nombre d'apparition d'un chiffre particulier dans $%2^n%$.
+On veut déterminer le nombre d'occurences d'un chiffre particulier dans
+$%2^n%$, c'est-à-dire le nombre de fois où le chiffre particulier apparait
+dans le nombre $%2^n%$.<br>
 
-Pour cela écrire une fonction  `nombre_chiffres(exp,chiffre)` qui reçoit un entier positif "exp" et un chiffre "chiffre" et renvoie le nombre 
-d'apparition du chiffre dans $%2^{exp}%$ (le code n'utilisera pas de str). 
+Par exemple, le chiffre $%4%$ apparait $%2%$ fois dans le nombre
+$%2^{50} = 1125899906842624%$.<br>
+<br>
+
+Pour cela, écrire une fonction  `nombre_chiffres(exp, chiffre)`
+qui reçoit un entier positif `exp` et un entier `chiffre` à un chiffre
+et renvoie le nombre d'apparition du chiffre dans $%2^{exp}%$.<br>
  
-Si exp n'est pas un entier positif ou si chiffre n'est pas un chiffre, la fonction renvoie None. <br>
+Si `exp` n'est pas un entier positif ou si `chiffre` n'est pas un chiffre,
+la fonction renverra `None`. <br>
+<br>
 
-Exemples<br>
+**Attention : ** Votre code n'utilisera pas de chaine de caractères.
+<br><br>
 
-&gt;&gt;&gt; nombre_chiffres(0,1)==1<br>
-True<br>
-&gt;&gt;&gt; nombre_chiffres(8,1)<br>
-0<br>
-&gt;&gt;&gt; nombre_chiffres(18,4)<br>
-2<br>
+**Exemples :**<br>
+
+`>>> nombre_chiffres(0,1)==1`<br>
+`True`<br>
+`>>> nombre_chiffres(8,1)`<br>
+`0`<br>
+`>>> nombre_chiffres(18,4)`<br>
+`2`<br>
 ==
 
 Xeditor.code==
@@ -62,6 +74,7 @@ pltest1==
 >>> nombre_chiffres(67,9)
 3
 ==
+
 
 
 
