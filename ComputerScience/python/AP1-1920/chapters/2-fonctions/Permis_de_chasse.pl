350a7db032e4a0cc382503ed5b17968c6e9551cd
Olivier BOUILLOT <Olivier.Bouillot@u-pem.fr>
2020-11-10 11:36:20 +0100

    obouillot modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/Permis_de_chasse.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/Permis_de_chasse.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/Permis_de_chasse.pl
@@ -28,34 +28,46 @@ S’il tue une poule il perd 1 point, 5 points pour 1 chien, 10 points pour une 
 L'amende coûte 50 euros par points perdus. <br>
 Si les points perdus sont supérieurs à 100, le permis est annulé.<br>
 <br>
-Écrire une fonction `amende` qui reçoit le nombre de victimes du chasseur et qui renvoie la somme due et le nombre de points perdus. <br>
-Utilisez cette fonction dans un programme principal qui : <br>
-* saisit le nombre de victimes et,<br>
-* qui affiche la somme que le chasseur doit débourser au prorata des points perdus total, ainsi que,<br>
-* le nombre de points restant (si la somme de points perdus est supérieure à 100, la fonction retourne 100).<br>
-<br>
-Écrire ensuite un programme principal qui :
-* demande à l'utilisateur le nombre de victime
-* retourne la somme à payer <br>
-* le nombre de points restants ou l'annulation du permis<br>
-<br>
-Exemple : <br>
 
-Combien de poules?1 <br>
-Combien de chiens?2 <br>
-Combien de vaches?10 <br>
-Combien d'amis?20 <br>
+Écrire une fonction `amende` qui reçoit le nombre de victimes du chasseur
+et qui renvoie la somme due et le nombre de points perdus. Si la somme des points
+perdus est supérieure à 100, la fonction retourne 100.<br>
 <br>
-Payer 12850 euros. <br>
-Permis annulé <br>
+
+Utilisez cette fonction dans un programme principal qui : <br>
+
+*   saisit le nombre de victimes,<br>
+
+*   affiche la somme que le chasseur doit débourser au prorata des points perdus total,<br>
+
+*   affiche le nombre de points restant.<br>
 <br>
-Combien de poules?1 <br>
-Combien de chiens?1 <br>
-Combien de vaches?1 <br>
-Combien d'amis?0 <br>
+
+Écrire ensuite un programme principal qui :
+*   demande à l'utilisateur le nombre de victime
+*   retourne la somme à payer <br>
+*   le nombre de points restants ou l'annulation du permis<br>
 <br>
-Payer 450 euros.<br>
-Points perdus : 9
+
+**Exemple 1 d'exécution du programme :**<br>
+<br>
+`Combien de poules ? 1` <br>
+`Combien de chiens ? 2` <br>
+`Combien de vaches ? 10` <br>
+`Combien d'amis ? 20` <br>
+<br>
+`Payez 12850 euros.` <br>
+`Permis annulé` <br>
+<br>
+**Exemple 2 d'exécution du programme :**<br>
+<br>
+`Combien de poules ? 1` <br>
+`Combien de chiens ? 1` <br>
+`Combien de vaches ? 1` <br>
+`Combien d'amis ? 0` <br>
+<br>
+`Payez 450 euros.`<br>
+`Points perdus : 9`
 ==
 
 code==
@@ -117,6 +129,7 @@ mplsoluce2==
 1
 0
 ==
+
 
 
 
