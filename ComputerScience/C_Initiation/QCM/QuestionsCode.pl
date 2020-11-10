1da0cf6b73af5e49a50a78276f46344ec7911c24
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-11-10 11:49:52 +0100

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/QCM/QuestionsCode.pl

--- ComputerScience/C_Initiation/QCM/QuestionsCode.pl
+++ ComputerScience/C_Initiation/QCM/QuestionsCode.pl
@@ -10,53 +10,11 @@ title= Compréhension de code
 text=
 onepergroup % false
 
-questions==
-
-=*+ Exo avec click sur un mot ici bonne est le bon mot 
- Vous  pouvez en mettre plusieurs séparer vos mots par des espace 
- si vous souhaiter mettre un espace ajouter des crochet autour des caractères en question   
->j'indique avec des doubles curly brackets la {{bonne}} réponse
-
-=**[nb=13, group=odd] What are the $%odd%$ numbers ? 
-
-+=[x for x in range(3,77) if x%2==1 ]
--=[100,200,300,400,500,600]
--=[x for x in range(2,20) if x%2==0 ]
-
-=**[nbb=2,nbg=2] Indiquer les identifiants correctes
-+=["un","deux","ident","prout"]
--=["666","aujourd'hui", "def", "@lt;html@gt;"] 
--=["!autres","truc%","pour ris"]
-- for
--=["list","int","dic"]
-
-
-=**[nbb=2,nbg=2] Indiquer les identifiants correctes
-+=["un","deux","ident","prout"]
--=["666","aujourd'hui", "def", "@lt;html@gt;"] 
--=["!autres","truc%","pour ris"]
-- for
--=["list","int","dic"]
-
-=**[nb=13, group=odd] What are the odd numbers ? 
-+=[x for x in range(3,77) if x%2==1 ]
--=[100,200,300,400,500,600]
--=[x for x in range(2,20) if x%2==0 ]
-
-
-=*[nb=2,group=odd] What are the odd numbers ? 
-+=[x for x in range(3,77) if x%2==1 ]
-+ 1
--=[x for x in range(3,77) if x%2==0 ]
-
-
-=*[nb=6,group=odd] What are the odd numbers ? 
-+=[x for x in range(3,77) if x%2==1 ]
--=[x for x in range(3,77) if x%2==0 ]
-
+questions=@ QCM-1-a-4.txt
 ==
 
 tag=Tableau|Initialisation|ParcoursTableau|Boucle
+
 
 
 
