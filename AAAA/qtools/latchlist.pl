7b28a329716b32029131fae621d72a64620cfc08
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-10 13:19:30 +0100

    drevuz modified Yggdrasil/AAAA/qtools/latchlist.pl

--- AAAA/qtools/latchlist.pl
+++ AAAA/qtools/latchlist.pl
@@ -1,4 +1,16 @@
 
 extends= /exemples/matchlistcsv/matchlistcsv.pl
 
+@ engflac.csv [content.csv]
 
+
+before==
+import random
+from mkl import fromcsv,selectionfromcsv
+
+match.nodes , expected =  selectionfromcsv("content.csv")
+random.shuffle(match.nodes)
+
+text+=str(match.nodes)
+
+==
