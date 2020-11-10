f53452bcbde419cf37d99aceeefdf2dd8fd375bf
Olivier BOUILLOT <Olivier.Bouillot@u-pem.fr>
2020-11-10 11:02:34 +0100

    obouillot modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/2-fonctions/syracuse.pl

--- ComputerScience/python/AP1-1920/chapters/2-fonctions/syracuse.pl
+++ ComputerScience/python/AP1-1920/chapters/2-fonctions/syracuse.pl
@@ -12,33 +12,38 @@ La suite de Syracuse est une suite d'entiers définie par :
 * $%u_{n+1}=u_n/2%$ si $% u_n %$ est pair<br>
 * $%u_{n+1}=3u_n+1%$ si $% u_n %$ est impair<br>
 
-Pour tout entier strictement positif $%u_0%$ testé, la suite converge vers 1. 
-Le premier n tel que $%u_n=1%$ est appelé temps de vol de la suite.
+Pour tout entier strictement positif $%u_0%$ que l'homme a testé,
+la suite converge vers 1. 
+Le premier n tel que $%u_n=1%$ est appelé *temps de vol* de la suite.
 
 Le but de l'exercice est de déterminer le temps de vol pour plusieurs valeurs de $%u_0%$.
 <br>
 <br>
 Pour cela:<br>
+
 * écrire une fontion `saisie_strict_positif()` qui renvoie un entier strictement positif<br>
   entré par l'utilisateur.<br>
+
 * écrire une fontion `syracuse_temps(u)` qui reçoit un entier strictement positifs<br> 
-  et renvoie le temps de vol de la suite pour $%u_0=u%$<br>
+  et renvoie le temps de vol de la suite de Syracuse pour $%u_0=u%$<br>
+
 * utilisez ces fonctions pour écrire un programme qui lit 5 entiers strictement positifs<br>
   et affiche le temps de vol pour chacun de ces entiers.<br> 
 <br>
-Exemples :<br>
+<br>
+**Exemples d'exécution du programme :**<br>
  
-Entrez un entier strictement positif : 5 <br>
-Le temps de vol pour 7 est 16<br>
-Entrez un entier strictement positif : 3<br>
-Le temps de vol pour 3 est 7<br>
-Entrez un entier strictement positif : -1<br>
-Entrez un entier strictement positif : 11<br>
-Le temps de vol pour 14 est 17<br>
-Entrez un entier strictement positif : 155 <br>
-Le temps de vol pour 1256 est 39<br>
-Entrez un entier strictement positif : 1 <br>
-Le temps de vol pour 1 est 0<br>
+`Entrez un entier strictement positif : 5` <br>
+`Le temps de vol pour 7 est 16`<br>
+`Entrez un entier strictement positif : 3`<br>
+`Le temps de vol pour 3 est 7`<br>
+`Entrez un entier strictement positif : -1`<br>
+`Entrez un entier strictement positif : 11`<br>
+`Le temps de vol pour 14 est 17`<br>
+`Entrez un entier strictement positif : 155` <br>
+`Le temps de vol pour 1256 est 39`<br>
+`Entrez un entier strictement positif : 1` <br>
+`Le temps de vol pour 1 est 0`<br>
 ==
 code==
 ??=int(input("Entrez un entier strictement positif : "))
@@ -96,6 +101,7 @@ negatif
 11
 
 ==
+
 
 
 
