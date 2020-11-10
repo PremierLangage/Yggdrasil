93a64c42242eaa5329930c58efb57bfd19419a8e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-10 13:22:30 +0100

    drevuz modified Yggdrasil/AAAA/qtools/latchlist.pl

--- AAAA/qtools/latchlist.pl
+++ AAAA/qtools/latchlist.pl
@@ -11,6 +11,12 @@ from mkl import fromcsv,selectionfromcsv
 match.nodes , expected =  selectionfromcsv("content.csv")
 random.shuffle(match.nodes)
 
+for item in match.nodes:
+    if 'source' in item :
+        content=item['content']
+        #item['content']=f'<audio controls><source src="http://monge.univ-mlv.fr/~dr/flac/{content}"></audio>'
+
+
 text+=str(match.nodes)
 
 ==
