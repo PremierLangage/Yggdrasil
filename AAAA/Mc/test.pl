9e21f7e93e485d80635948fe0d9c6c15925d4699
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2020-11-11 16:14:34 +0100

    mcisse modified Yggdrasil/AAAA/Mc/test.pl

---
+++ AAAA/Mc/test.pl
@@ -0,0 +1,57 @@
+
+#  Author: Coumes Quentin     Mail: qcoumes@etud.u-pem.fr
+
+
+
+
+title= Addition Aléatoire
+
+author = Quentin Coumes
+
+text==
+Combien <i>font</i> ***{{ op1 }} + {{ op2 }}*** ?
+try count = {{ c }}
+==
+
+form==
+**MD**
+<div class="input-group">
+    <input id="form_answer" type="number" class="form-control" value="{{ answers__.answer }}" required/>
+</div>
+==
+
+settings.oneshot=yes
+settings.allow_reroll=yes
+
+
+evaluator==
+import traceback
+import sys
+
+try: 
+    c += 1
+    if int(response['answer']) == op1 + op2:
+        grade = (100, "Bonne réponse")
+    else:
+        grade = (0, "Mauvaise réponse")
+except:
+    print(traceback.format_exc(), file=sys.stderr)
+    grade = (-1, "Merci de rentrer un entier")
+==
+
+
+before==
+import random
+random.seed(seed)
+op1 = random.randint(1, 10)
+op2 = random.randint(1, 10)
+c = 0
+==
+
+@ ~/utils/sandboxio.py
+@ ~/builder/before.py [builder.py]
+@ ~/grader/evaluator.py [grader.py]
+
+
+
+
