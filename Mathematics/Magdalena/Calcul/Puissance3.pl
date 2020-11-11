ab71e7a064aadf1e96cbbbcdfb9368c8ef654be9
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-11 08:21:00 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Puissance3.pl

---
+++ Mathematics/Magdalena/Calcul/Puissance3.pl
@@ -0,0 +1,51 @@
+
+
+extends = /model/AMC_TXT.pl
+title = Calcul - puissances 2 
+questions==
+
+** Cocher les bonnes réponses. $%2^3=...%$
+- $%2000%$
++ $% 2\times 2 \times 2 %$
+- $% 6 %$
++ $% 8 %$
+- $% 3 \times 3 %$
+
+** Cocher les bonnes réponses. $%2^{-2}=...%$
+- $%-4%$
++ $% 0,25 %$
+- $% (-2)\times (-2) %$
++ $% \frac{1}{2\times 2}%$
+- $% 0,02%$
+
+
+** Cocher les bonnes réponses. $%7^{-2}%$=...
+- $% -14 %$
+- $% (-7) \times (-7) %$
+-  $%0,07%$
++ $% \frac{1}{7^2}%$
+
+ 
+
+** Cocher les bonnes réponses. $%\varpi^0%$=...
+- $% \varpi %$
+- $% 0 %$
+-  $%3,14159%$
++ $% 1%$
+
+** Cocher les bonnes réponses. $%\frac{4^3\times 4^2}{4^{-1}}%$=...
+- $% 4^4%$
+- $% 4^7%$
+-  $%4^{-6}%$
++ $% 4^6%$
+
+==
+
+
+
+
+
+
+
+
+
