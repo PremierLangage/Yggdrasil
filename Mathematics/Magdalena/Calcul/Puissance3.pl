375d0118961fd56401e063d67014af087a35078b
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-11 08:32:44 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Puissance3.pl

--- Mathematics/Magdalena/Calcul/Puissance3.pl
+++ Mathematics/Magdalena/Calcul/Puissance3.pl
@@ -17,6 +17,7 @@ questions==
 - $% (-2)\times (-2) %$
 + $% \frac{1}{2\times 2}%$
 - $% 0,02%$
++ $% \frac{1}{4} %$
 
 
 ** Cocher les bonnes réponses. $%7^{-2}%$=...
@@ -27,8 +28,8 @@ questions==
 
  
 
-** Cocher les bonnes réponses. $%\varpi^0%$=...
-- $% \varpi %$
+** Cocher les bonnes réponses. $%\pi^0%$=...
+- $% \pi %$
 - $% 0 %$
 -  $%3,14159%$
 + $% 1%$
@@ -38,6 +39,15 @@ questions==
 - $% 4^7%$
 -  $%4^{-6}%$
 + $% 4^6%$
++ $% \frac{4^5}{4^{-1}}%$
+
+** Cocher les bonnes réponses. $%4^{-3}\times 2,5^{-3}%$=...
+- $% (-4)\times(-4)\times (-4) \times (-2,5)\times (-2,5) \times (-2,5)=30%$
++ $%\frac{1}{4} \times\frac{1}{4} \times\frac{1}{4} \times\frac{1}{2,5} \times\frac{1}{2,5}\times\frac{1}{2,5}%$
+- $% \frac{1}{30} %$
++ $% (4 \times 2,5)^{-3} %$
++ $% 10^{-3} %$
++ $% 0,001 %$
 
 ==
 
