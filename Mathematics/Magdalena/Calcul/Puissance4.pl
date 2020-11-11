ea329ed2395c8ec95f29cb55492c6105e999a04d
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-11 10:31:49 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Puissance4.pl

--- Mathematics/Magdalena/Calcul/Puissance4.pl
+++ Mathematics/Magdalena/Calcul/Puissance4.pl
@@ -8,20 +8,21 @@ questions==
 
  
 
-** Cocher les bonnes réponses. $%\pi^0%$=...
+** Cocher les bonnes réponses. $$\pi^0= $$
 - $% \pi %$
 - $% 0 %$
 -  $%3,14159%$
 + $% 1%$
 
-** Cocher les bonnes réponses. $%\frac{4^3\times 4^2}{4^{-1}}%$=...
+** Cocher les bonnes réponses. $$\frac{4^3\times 4^2}{4^{-1}}=...$$
 - $% 4^4%$
 - $% 4^7%$
 -  $%4^{-6}%$
 + $% 4^6%$
 + $% \frac{4^5}{4^{-1}}%$
 
-** Cocher les bonnes réponses. $%4^{-3}\times 2,5^{-3}%$=...
+** Cocher les bonnes réponses. 
+$$4^{-3}\times 2,5^{-3}=...$$
 - $% (-4)\times(-4)\times (-4) \times (-2,5)\times (-2,5) \times (-2,5)=30%$
 + $% \frac{1}{4} \times\frac{1}{4} \times\frac{1}{4} \times\frac{1}{2,5} \times\frac{1}{2,5}\times\frac{1}{2,5}%$
 - $% \frac{1}{30} %$
