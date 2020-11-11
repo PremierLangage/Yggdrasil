0cdc42a885007346394fcf2663d8e55c69b7f04b
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-11 12:24:08 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Puissance10.pl

--- Mathematics/Magdalena/Calcul/Puissance10.pl
+++ Mathematics/Magdalena/Calcul/Puissance10.pl
@@ -38,20 +38,21 @@ questions==
 + $% 10^{-5}\times 10^{5}%$
 + $% 10^{-5+5}%$
 
-** Cocher les bonnes réponses. $%4^{-3}\times 2,5^{-3}%$=...
-- $% (-4)\times(-4)\times (-4) \times (-2,5)\times (-2,5) \times (-2,5)=30%$
-+ $%\frac{1}{4} \times\frac{1}{4} \times\frac{1}{4} \times\frac{1}{2,5} \times\frac{1}{2,5}\times\frac{1}{2,5}%$
-- $% \frac{1}{30} %$
-+ $% (4 \times 2,5)^{-3} %$
-+ $% 10^{-3} %$
-+ $% 0,001 %$
+** Cocher les bonnes réponses. $%(10^{3})^2\times 5^{-3}\times 2^{-3} %$=...
+- $% 10^{3\times 3} \times (5\times 2)^{-6}%$
++ $% 10^{3 \times 2} \times (5\times 2)^{-3}%$   %$
+- $% 10^5 %$
++ $% 10^3%$
++ $% 1000 %$
 
-** Cocher les bonnes réponses. $$\frac{6\times 10^3 \times 28 \times 10^{-2}}{14 \times 10^{-3}}=...$$
-- $% 12 \times 10^{-2}%$
-+ $%\frac{6 \times 2 \times 14}{14} \times\frac{10^{3-2}{10^{-3}}%$
-- $% 12 \times 10^{-3}%$
-+ $% 12 \times 10^{3-2+3}%$
-+ $% 12 \times 10^{4}%$
+
+** Cocher les bonnes réponses. $$ 2^5 \times (5^2)^3=...$$
+- $% (2\times 5^2)^{5+3}%$
++ $% 2^5 \times 5^6  %$
+- $% (2\times 5)^{5+6}%$
++ $% 2\times 2 \times 2\times 2\times 2 \times 5^2 \times 5^2 \times 5^2%$
++ $% (2\times 5)^5 \times 5%$
++ $% 5 \times 10^5 %$
 
 
 
