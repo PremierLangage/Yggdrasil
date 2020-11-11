33f477b90fbe03df22e6790f42618e07d8b00204
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-11 10:56:59 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Puissance10.pl

---
+++ Mathematics/Magdalena/Calcul/Puissance10.pl
@@ -0,0 +1,68 @@
+
+
+extends = /model/AMC_TXT.pl
+title = Calcul - puissances 10
+questions==
+
+** Cocher les bonnes réponses. $$10^{-3}=...$$
++ $%0,001%$
+- $% (-10)\times (-10) \times (10) %$
+- $% -30%$
++ $% \frac{1}{10^3} %$
++ $% \frac{1}{10} \times\frac{1}{10}\times  \frac{1}{10} %$
+
+** Cocher les bonnes réponses. $$ \frac{1}{10^{-2}}=...$$
+- $%0,O1%$
++ $% 100%$
++ $% 10^2 %$
+- $% 0,02%$
+
+
+** Cocher les bonnes réponses. $$\frac{10^5 \times 10^{-2}}{10^2}=...$$
+- $% 10^5 %$
++ $% 10^{5-2-2} %$
++  $%0,1%$
+- $% 10^{5\times (-2) : 2}%$
+
+ 
+
+** Cocher les bonnes réponses. $$\10^0 =...$$
+- $% 10 %$
+- $% 0 %$
++ $% 1%$
+
+** Cocher les bonnes réponses. $$0,00001\times 10^5=..$$
++ $%1 %$
+- $% 10%$
+-  $%0,1%$
++ $% 10^{-5}\times 10^{5}%$
++ $% 10^{-5+5}%$
+
+** Cocher les bonnes réponses. $%4^{-3}\times 2,5^{-3}%$=...
+- $% (-4)\times(-4)\times (-4) \times (-2,5)\times (-2,5) \times (-2,5)=30%$
++ $%\frac{1}{4} \times\frac{1}{4} \times\frac{1}{4} \times\frac{1}{2,5} \times\frac{1}{2,5}\times\frac{1}{2,5}%$
+- $% \frac{1}{30} %$
++ $% (4 \times 2,5)^{-3} %$
++ $% 10^{-3} %$
++ $% 0,001 %$
+
+** Cocher les bonnes réponses. $$\frac{6\times 10^3 \times 28 \times 10^{-2}}{14 \times 10^{-3}}=...$$
+- $% 12 \times 10^{-2}%$
++ $%\frac{6 \times 2 \times 14}{14} \times\frac{10^{3-2}{10^{-3}}%$
+- $% 12 \times 10^{-3}%$
++ $% 12 \times 10^{3-2+3}%$
++ $% 12 \times 10^{4}%$
+
+
+
+==
+
+
+
+
+
+
+
+
+
+
