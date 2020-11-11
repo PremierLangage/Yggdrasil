63e7d51addee6b8e225857be0f0c6c84028d5388
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-11 08:45:10 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Calcul/Puissance3.pl

--- Mathematics/Magdalena/Calcul/Puissance3.pl
+++ Mathematics/Magdalena/Calcul/Puissance3.pl
@@ -49,6 +49,15 @@ questions==
 + $% 10^{-3} %$
 + $% 0,001 %$
 
+** Cocher les bonnes réponses. $$\frac{6\times 10^3 \times 28 \times 10^{-2}}{14 \times 10^{-3}}=...$$
+- $% 12 \times 10^{-2}%$
++ $%\frac{6 \times 2 \times 14}{14} \times\frac{10^{3-2}{10^{-3}}%$
+- $% 12 \times 10^{-3}%$
++ $% 12 \times 10^{3-2+3}%$
++ $% 12 \times 10^{4}%$
+
+
+
 ==
 
 
