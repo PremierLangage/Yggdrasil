ef632c04ae79392622103ec8a40b0ab9d1b43257
Claire DAVID <Claire.David@u-pem.fr>
2020-11-12 11:49:41 +0100

    cdavid modified Yggdrasil/ComputerScience/BD-ClaireDavid/template/base.pl

---
+++ ComputerScience/BD-ClaireDavid/template/base.pl
@@ -0,0 +1,18 @@
+@ /utils/sandboxio.py
+# imports de bibliothèques
+
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+styles =@ ../styles/bd.css
+
+extracss==
+<style> {{ styles }}</style>
+==
+
+title = 
+text = 
+form = 
+
+
+
