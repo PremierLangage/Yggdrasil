c3e71c07ae6a8b0029d663d63bec72c7889875ca
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-11-12 10:01:58 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_Iterables/extends_list-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_Iterables/extends_list-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_Iterables/extends_list-2021.pl
@@ -10,6 +10,9 @@ text ==
 Ecrire une fonction `tab3` à deux arguments; une liste, un entier n, qui modifie la liste passée en argument en
 lui ajoutant la liste des n premiers éléments de la table de multiplication par 3, commençant par le multiple de 1.
 
+Par exemple, si on avait une liste `l = [9, 5]`, alors après l'appel `tab3(l, 4)`, **qui ne retourne rien et n'affiche rien**, 
+la valeur de `l` doit devenir `[9, 5, 3, 6, 9, 12]`.
+
 taboo: Ne pas utiliser la fonction <font style="color:red;">{{taboo}}</font> des listes.
 ==
 
