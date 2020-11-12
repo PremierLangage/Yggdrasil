0327347cc5f137e5393492f679acb8583688c463
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-12 16:55:58 +0100

    drevuz modified Yggdrasil/ComputerScience/C_Initiation/QCM/QuestionsCode.pl

--- ComputerScience/C_Initiation/QCM/QuestionsCode.pl
+++ ComputerScience/C_Initiation/QCM/QuestionsCode.pl
@@ -2,6 +2,7 @@ extends=/model/AMC2/QCM-AMCE.pl
 #extends=/model/AMC_TXT.pl 
 textstep ==
 <strong> Question {{ step + 1 }}. </strong> 
+{{list_questions[step]}}
 {{ statement[step] | safe }}
 ==
 
@@ -11,6 +12,7 @@ text=
 onepergroup % false
 
 questions=@ QuestionsCode.txt
+
 
 
 
