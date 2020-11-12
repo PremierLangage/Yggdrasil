00282872990c949e25ceb70ac64c81597e89f72c
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-11-12 20:27:18 +0100

    protondo modified Yggdrasil/ComputerScience/C_Initiation/QCM/QuestionAnalyseCode.pl

---
+++ ComputerScience/C_Initiation/QCM/QuestionAnalyseCode.pl
@@ -0,0 +1,16 @@
+extends=/model/AMC_TXT.pl 
+
+textstep ==
+<strong> Question {{ step + 1 }}. </strong> 
+<span data-unselectable="unselectable content">
+{{ statement[step] | safe }}</span>
+==
+
+title= Compréhension de code 
+
+questions=@ QuestionsAnalyseCode.txt
+
+tag=Tableau|Initialisation|ParcoursTableau|Boucle
+
+
+
