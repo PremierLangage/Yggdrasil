83c3fff15c8988fb35c4ad780bb0505ba85d0eac
Claire DAVID <Claire.David@u-pem.fr>
2020-11-12 16:28:20 +0100

    cdavid modified Yggdrasil/ComputerScience/BD-ClaireDavid/exo/ex1.pl

--- ComputerScience/BD-ClaireDavid/exo/ex1.pl
+++ ComputerScience/BD-ClaireDavid/exo/ex1.pl
@@ -17,8 +17,11 @@ allow_reroll =  True
 
 #* on peut ajouter ici un script de definition d'énoncé et solution qui sera excécuté dans le before de l'exo
 generate== #|py|
+# pour ajouter des fonctions dans un fichier .py
+# ajouter le nom du fichier dans base.pl
+#from monfichier import bla
 
-question = "Voilà ma question: c'était quoi déjà"
+question = "Voilà ma question: c'était quoi déjà ?"
 
 solution = "youpi"
 
