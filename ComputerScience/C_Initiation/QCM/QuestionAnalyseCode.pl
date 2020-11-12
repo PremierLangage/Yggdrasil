1daca68656641d85575c7ba301c905775a67673d
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-11-12 20:46:06 +0100

    protondo modified Yggdrasil/ComputerScience/C_Initiation/QCM/QuestionAnalyseCode.pl

--- ComputerScience/C_Initiation/QCM/QuestionAnalyseCode.pl
+++ ComputerScience/C_Initiation/QCM/QuestionAnalyseCode.pl
@@ -2,12 +2,7 @@ extends=/model/AMC_TXT.pl
 
 textstep ==
 <strong> Question {{ step + 1 }}. </strong> 
-<div style="-webkit-touch-callout: none;
-    -webkit-user-select: none;
-    -khtml-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;"> {{ statement[step] | safe }}</div>
+{{ statement[step] | safe }}
 ==
 
 title= Compréhension de code 
