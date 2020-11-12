b1f2b92f588043f1f62980a847cb4b8cbea130ea
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-11-12 10:20:08 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_Iterables/remplace_pair-2021.pl

---
+++ ComputerScience/python/AP1-2021/Chapitre_Iterables/remplace_pair-2021.pl
@@ -0,0 +1,40 @@
+# author FLQ
+
+# comment
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+builder= /builder/before.py
+
+title = Remplace les éléments d'une liste
+text == 
+Ecrire une fonction `remppair` qui prend en argument une liste d'entiers et remplace tous ses éléments pairs par 1.
+
+Par
+==
+
+before==
+# solution --- utilise len
+def remppair(lst) :
+    for i in range(len(lst)) :
+        if lst[i]%2 == 0 :
+            lst[i]=1
+after=before
+==
+
+pltest0==
+>>> lst=[2,6,5,8,9] #
+>>> remppair(lst) #
+>>> lst
+[1, 1, 5, 1, 9]
+==
+
+pltest1==
+>>> lst=[] #
+>>> remppair(lst) #
+>>> lst
+[]
+==
+
+
+
+
+
