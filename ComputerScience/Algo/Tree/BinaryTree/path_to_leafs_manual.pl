b16fa8a6445a78d7ab900fc2f6496d071e8c67b6
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-13 10:31:07 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/BinaryTree/path_to_leafs_manual.pl

--- ComputerScience/Algo/Tree/BinaryTree/path_to_leafs_manual.pl
+++ ComputerScience/Algo/Tree/BinaryTree/path_to_leafs_manual.pl
@@ -29,7 +29,7 @@ before==#|python|
 from random import choice, randint
 from utils_binary_tree import random_binary_tree
 
-Tree_exo = random_binary_tree(randint(8, 14))[0]
+Tree_exo = random_binary_tree(randint(8, 12), max_height=5)[0]
 viewer.graph = Tree_exo.to_dot_code_BST_point()
 
 ==
