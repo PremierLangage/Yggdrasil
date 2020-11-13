f3657553c3d3c0c31646e64eb9509bfd296d3d11
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-13 10:36:32 +0100

    nborie modified Yggdrasil/ComputerScience/Algo/Tree/BinaryTree/path_to_leafs_manual.pl

--- ComputerScience/Algo/Tree/BinaryTree/path_to_leafs_manual.pl
+++ ComputerScience/Algo/Tree/BinaryTree/path_to_leafs_manual.pl
@@ -23,7 +23,7 @@ viewer =: GraphDrawer
 
 inputbox =: Input
 inputbox.type = multiline
-inputbox.appearance = outline
+inputbox.appearance = legacy
 inputbox.placeholder = Un chemin par ligne, espace tiret espace entre chaque valeur
 
 before==#|python|
