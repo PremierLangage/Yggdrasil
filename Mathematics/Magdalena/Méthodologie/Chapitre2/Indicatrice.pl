54943a3d97c4abee15b6b7dacaa3f3aec23be7ac
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-14 16:33:34 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl
@@ -0,0 +1,25 @@
+extends = /model/AMC_TXT.pl
+title = Application  indicatrice d'un ensemble
+questions==
+
+
+** Soit $%E%$ un ensemble et $% A%$ une partie de $%E%$
++ $% 1\!\!1_A: E \to \\{0,1\\} \quad x \mapsto 1 \mbox{ si } x \in A \quad x \mapsto 0 \mbox{ sinon}.%$
+- $% \mathbb{1}_A \in E^{\\{0,1\\}} %$
++ $% \mathbb{1}_A \in \\{0,1\\}^E %$
+- $% \mathbb{1}_A + \mathbb{1}_{A^c}= Id_E %$
+
+
+** 
+
++ $% \mathbb{1}_{\\{1,2,3\\}}(2)=1 %$
+- $% Id_E \circ f  = Id_F %$
+- $% Id_E \circ f = f %$
++ $% Id_F \circ f = f%$
+- $% f \circ g = Id_E %$
+
+
+
+==
+
+
