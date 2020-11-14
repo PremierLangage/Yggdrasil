d13b818737cb40b140de6a84c03b629cf736fe96
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-14 16:42:22 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl
@@ -4,19 +4,19 @@ questions==
 
 
 ** Soit $%E%$ un ensemble et $% A%$ une partie de $%E%$
-+ $% 1_A: E \to \\{0,1\\} \quad x \mapsto 1 \mbox{ si } x \in A \quad x \mapsto 0 \mbox{ sinon}.%$
-- $% \mathbb{1}_A \in E^{\\{0,1\\}} %$
-+ $% \mathbb{1}_A \in \\{0,1\\}^E %$
-- $% 1_A + 1_{A^c}= Id_E %$
++ **1**$%_A: E \to \\{0,1\\} \quad x \mapsto 1 \mbox{ si } x \in A \quad x \mapsto 0 \mbox{ sinon}.%$
+- **1**$%_A \in E^{\\{0,1\\}} %$
++ **1**$%_A \in \\{0,1\\}^E %$
+- **1**$%_A + 1_{A^c}= Id_E %$
 
 
 ** 
 
-+ $% \mathbb{1}_{\\{1,2,3\\}}(2)=1 %$
++ **1**$%_{\\{1,2,3,4\\}}(2)=1 %$
 +  **1**$%_{[1,4]}(\pi)=1 %$
-- $% Id_E \circ f = f %$
-+ $% Id_F \circ f = f%$
-- $% f \circ g = Id_E %$
+-  **1**_{\\{1,2,3,4\\}}(\pi)=0%$
++  **1**$%_{[1,4]}(2)=1 %$ 
+-  **1**$%_{[1,4]}(-2)=1 %$ 
 
 
 
