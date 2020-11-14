9f0bd97e4c4a2188b9dc934025ed8c23152969d3
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-14 16:50:29 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Indicatrice.pl
@@ -7,6 +7,7 @@ questions==
 + **1**$%_A: E \to \\{0,1\\} \quad x \mapsto 1 \mbox{ si } x \in A \quad x \mapsto 0 \mbox{ sinon}.%$
 - **1**$%_A \in E^{\\{0,1\\}} %$
 + **1**$%_A \in \\{0,1\\}^E %$
++ **1**$%_A \in \\{0,1\\}^E %$
 - **1**$%_A +%$ **1**$%_{A^c}= Id_E %$
 
 
