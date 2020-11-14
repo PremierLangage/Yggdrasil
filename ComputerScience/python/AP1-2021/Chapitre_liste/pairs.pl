c572b6e4b03ae1b6dcd8901d28d7af963108369b
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-11-14 23:19:35 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_liste/pairs.pl

---
+++ ComputerScience/python/AP1-2021/Chapitre_liste/pairs.pl
@@ -0,0 +1,27 @@
+
+title= Sous suite des pairs
+extends=/ComputerScience/python/template/pltest.pl
+
+text==
+
+Ecrire une fonction **pairs** qui prend une liste en paramêtre et 
+qui retourne une liste des entiers pairs de la liste.
+
+**Superseded by pair_impair.pl -- Wenjie Fang**
+==
+
+pltest==
+>>> pairs([2,4,5,6,77])
+[2, 4, 6]
+>>> pairs([])
+[]
+>>> import random #
+>>> l=[ random.randint(1,100)*2 for x in range(10)] #
+>>> pairs(l) == l
+True
+>>> pairs([ x+1 for x in l])
+[]
+==
+
+
+
