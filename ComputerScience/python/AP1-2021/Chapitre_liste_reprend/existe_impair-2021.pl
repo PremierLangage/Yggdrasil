ce556e40f0610b03993215cd865e777cef0a8e9e
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-11-14 22:50:48 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_liste_reprend/existe_impair-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_liste_reprend/existe_impair-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_liste_reprend/existe_impair-2021.pl
@@ -5,6 +5,7 @@ text==
 
 Ecrire une fonction ``exist_impairs`` qui prend une liste en paramêtre 
 et qui retourne True si il existe un entier impair dans la liste.
+
 Par exemple, ``exist_impairs([2, 0, 1, 9])`` doit renvoyer ``True``, parce qu'il y a 1 et 9 dans la liste,
 mais ``exist_impairs([2, 0, 1, 9])`` et ``exist_impairs([])`` doivent renvoyer ``False``.
 ==
