abad274ea7ee5181bbbc058bcc2251e2f5425617
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-11-14 23:40:35 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_liste_reprend/soussuite-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_liste_reprend/soussuite-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_liste_reprend/soussuite-2021.pl
@@ -22,8 +22,8 @@ Nous allons ici nous intéresser à deux définitions de la présence d'une list
 
 Ecrivez deux fonctions réalisant le test permettant de vérifier si une liste est une sous liste ou sous suite d'une autre:
 
-- ``sous_liste(l1,l2)`` qui retourne True si ``l2`` est une sous-liste de ``l1``, et False sinon
-- ``sous_suite(l1,l2)`` qui retourne True si ``l2`` est une sous-suite de ``l1``, et False sinon
+- ``sous_liste(l1, l2)`` qui retourne True si ``l2`` est une sous-liste de ``l1``, et False sinon
+- ``sous_suite(l1, l2)`` qui retourne True si ``l2`` est une sous-suite de ``l1``, et False sinon
 
 ==
 
