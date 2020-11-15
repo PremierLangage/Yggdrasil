703916d9cb5b4773beaf7d6afd0c800e7e528cae
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-11-15 20:10:29 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl
@@ -10,8 +10,8 @@ text==
 On appelle "entier de Gauss" un nombre complexe $%z=a+\mathrm{i}b , (a,b) \in \mathbb{N}^2 %$
 
 
-Ecrire une fonction `carres_Gauss(r)` qui renvoie la liste d'entiers de Gauss dont le module de son **carré** (i.e., $% (a^2 + b^2) + 2ab\mathrm{i} %$) est inférieur ou égal à $%r%$.
-La liste est une liste de ces entiers de Gauss représentés en couples (a, b), ordonnées par ordre ascendant. 
+Ecrire une fonction `carres_Gauss(r)` qui renvoie la liste d'entiers de Gauss dont le module de son **carré** (i.e., $% (a^2 - b^2) + 2ab\mathrm{i} %$) est **inférieur ou égal** à $%r%$.
+La liste est une liste de ces entiers de Gauss représentés en couples (a, b), ordonnées **lexicographiquement**, _i.e._ ascendant en première coordonnée, puis ascendant en deuxième coordonnée. 
 
 <pre><code>
 >>> carres_Gauss(2)
