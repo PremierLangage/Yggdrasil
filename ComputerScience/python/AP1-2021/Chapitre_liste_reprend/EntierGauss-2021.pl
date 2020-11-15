fa7be0432aff64d461663fbe2372f510ba07ab4d
Chaohui WANG <Chaohui.Wang@u-pem.fr>
2020-11-15 13:42:54 +0100

    cwang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl
@@ -5,6 +5,7 @@ extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 
 title =Entiers de Gauss
 
+# Ecrire une fonction `carres_Gauss(r)` qui renvoie la liste d'entiers de Gauss de module inférieur ou égal à $%r%$ dont le **carré** est également un entier de Gauss de module **inférieur ou égal** à $%r%$. La liste est une liste de ces entiers, représentés en couples de coordonnées ordonnées **lexicographiquement**. 
 
 text==  
 
