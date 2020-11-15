df056413f2840209c39546604dd4d20a8f04b08b
Chaohui WANG <Chaohui.Wang@u-pem.fr>
2020-11-15 13:33:03 +0100

    cwang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_liste_reprend/EntierGauss-2021.pl
@@ -8,8 +8,8 @@ text==
 
 On appelle "entier de Gauss" un nombre complexe $%z=a+\mathrm{i}b , (a,b) \in \mathbb{N}^2 %$
 
-Ecrire une fonction `carres_Gauss(r)` qui renvoie la liste d'entiers de Gauss de module inférieur ou égal à $%r%$
-dont le **carré** est également un entier de Gauss de module **inférieur ou égal** à $%r%$. La liste est une liste de ces entiers, représentés en couples de coordonnées
+Ecrire une fonction `carres_Gauss(r)` qui renvoie la liste d'entiers de Gauss dont le module est inférieur ou égal à $%r%$.
+La liste est une liste de ces entiers, représentés en couples de coordonnées
 ordonnées **lexicographiquement**. 
 
 <pre><code>
@@ -83,6 +83,7 @@ pltest2==
 >>> carres_Gauss(0)
 [(0, 0)]
 ==
+
 
 
 
