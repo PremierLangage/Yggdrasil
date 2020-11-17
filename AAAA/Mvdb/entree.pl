f209a8b4ee0446d0908db9bf99ceadebba55d9fd
Marie VAN DEN BOGAARD <Marie.Vandenbogaard@univ-mlv.fr>
2020-11-17 15:05:48 +0100

    mvan den bogaard1 modified Yggdrasil/AAAA/Mvdb/entree.pl

---
+++ AAAA/Mvdb/entree.pl
@@ -0,0 +1,26 @@
+
+extends = /model/mathinput2.pl
+
+title = Balise de titre
+
+text==
+Youplaboum rentrez des trucs
+
+==
+
+before==
+
+==
+
+form==
+
+la somme
+{{ input1|component]]
+
+==
+
+
+evaluator==
+
+grade=(100,"Bravo")
+==
