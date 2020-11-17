3d59611cde780e5a5deb3e2cafd0713102db2ca2
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-17 16:56:09 +0100

    drevuz modified Yggdrasil/AAAA/basicAMC/exempledemoAMC.pl

---
+++ AAAA/basicAMC/exempledemoAMC.pl
@@ -0,0 +1,22 @@
+
+extends= /AAAA/dominique/multistep.pl
+
+
+questions==
+# AMC-TXT source file
+Title: My first AMC questionnaire
+
+Presentation: Please answer the following questions
+the best you can.
+
+* What is the capital city of Cameroon?
++ Yaounde
+- Douala
+- Kribi
+
+** From the following numbers, which are positive?
+- -2
++ 2
++ 10
+
+==
