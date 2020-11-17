90f7034bc88aa6217f61e1b84bd26d330f26f70c
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-17 03:47:50 +0100

    drevuz modified Yggdrasil/AAAA/dominique/gift2/bob/Q4.pl

---
+++ AAAA/dominique/gift2/bob/Q4.pl
@@ -0,0 +1,15 @@
+extends = /gift/templates/qmatch.pl
+
+title==
+Q4
+==
+
+text==
+Quel animal mange quoi ?
+==
+
+choices==
+=Chat -> Nourriture pour chats
+=Chien -> Nourriture pour chiens
+=Pigeons -> Nourriture pour oiseaux
+==
