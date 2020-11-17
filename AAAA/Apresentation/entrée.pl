685ed0c93c0f8798387f7e53493e39ee29fa2496
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-17 14:59:17 +0100

    drevuz modified Yggdrasil/AAAA/Apresentation/entrée.pl

--- AAAA/Apresentation/entrée.pl
+++ AAAA/Apresentation/entrée.pl
@@ -4,6 +4,12 @@ extends = /model/mathinput2.pl
 
 title= Balise de titre 
 
+text==
+Un exercice de saisie d'un calcul.
+Donner le produit et somme de {{a}} et {{b}}. 
+
+{{bof}}
+==
 
 # Avant que ce soit présenté à l'élève 
 before==
@@ -16,13 +22,6 @@ b=6
 ==
 
 
-text==
-Un exercice de saisie d'un calcul.
-Donner le produit et somme de {{a}} et {{b}}. 
-
-==
-
-
 form ==
 la somme    
 {{ input1|component}}
@@ -31,9 +30,10 @@ le produit
 ==
 
 
-
 # évaluer la réponse 
 evaluator==
+bof=" ici un commentaire sur le bof "
+
 if input1.value == a+b and input2.value == a*b :
     grade=(100,"Bravo ")
 else:
