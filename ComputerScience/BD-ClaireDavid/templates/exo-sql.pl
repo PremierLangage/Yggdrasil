fc138acb13f242bea6c2d3f002ab3dbec7bb3b81
Nadime FRANCIS <Nadime.Francis@univ-mlv.fr>
2020-11-17 17:11:32 +0100

    nfrancis modified Yggdrasil/ComputerScience/BD-ClaireDavid/templates/exo-sql.pl

--- ComputerScience/BD-ClaireDavid/templates/exo-sql.pl
+++ ComputerScience/BD-ClaireDavid/templates/exo-sql.pl
@@ -255,7 +255,7 @@ else:
     grade = None
     (passed, message) = check_syntax(student_query, cursor)
     if not passed:
-        grade = (0, "<p class = \"error-state\">" + "lootr" + "</p>")
+        grade = (0, "<p class = \"error-state\">" + str(message) + "</p>")
     
     if grade == None:
         (passed, message) = check_schema(student_query, solution, cursor)
