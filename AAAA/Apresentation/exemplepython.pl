ce59b99ba9bcdc3f150491433fb3cbfbde67ee23
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-17 15:10:31 +0100

    drevuz modified Yggdrasil/AAAA/Apresentation/exemplepython.pl

---
+++ AAAA/Apresentation/exemplepython.pl
@@ -0,0 +1,16 @@
+
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+
+
+title= Une fonction 
+
+text==
+Ecrire une fonction **bof** qui retourne 33.
+
+==
+
+pltest==
+>>> bof() == 33
+True
+
+==
