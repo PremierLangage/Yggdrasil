7a03b527edbe1657831e5b9f9828a42ff6a629ab
Claire DAVID <Claire.David@u-pem.fr>
2020-11-18 19:10:04 +0100

    cdavid modified Yggdrasil/ComputerScience/BD-ClaireDavid/templates/exo-requete.pl

--- ComputerScience/BD-ClaireDavid/templates/exo-requete.pl
+++ ComputerScience/BD-ClaireDavid/templates/exo-requete.pl
@@ -76,7 +76,8 @@ form_timeout== #|html|
 #*===========================================================================
 #* override this key to change the instructions below inputbox for simple regex
 form_instructions_sql_query== #|html|
- pouet <br> blabla <br> {{var_schema}}
+ pouet <br> blabla <br>
+ {% templatetag openvariable %} schema {% templatetag closevariable %}
 ==
 
 #*===========================================================================
