229cb16ffdaac1a1f9d92db9ed031d7a82901fda
Claire DAVID <Claire.David@u-pem.fr>
2020-11-18 16:53:34 +0100

    cdavid modified Yggdrasil/ComputerScience/BD-ClaireDavid/exo/ex-serie.pl

--- ComputerScience/BD-ClaireDavid/exo/ex-serie.pl
+++ ComputerScience/BD-ClaireDavid/exo/ex-serie.pl
@@ -49,19 +49,7 @@ questions = question_dic[nb_questions]
 ==
 
 
-#* Texte de l'énoncé
-# ---
-# ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
-# l'automate minimisé du langage solution
-text== #|html|
-On considère le schema suivant. Donnez une requete qui calcule {{ question }}. <br>
-{% if attempt < maxattempt and score != 100 %}
-<p class="warning-state">
-Il vous reste {{ maxattempt - attempt }} tentative(s) avant de voir la solution !
-</p>
-{% endif %}
 
-==
 
 
 
