33fbe6816ff9b4b605c3496fe24a90ec69d1c31b
Nadime FRANCIS <Nadime.Francis@univ-mlv.fr>
2020-11-18 15:40:27 +0100

    nfrancis modified Yggdrasil/ComputerScience/BD-ClaireDavid/scripts/parse_question.py

--- ComputerScience/BD-ClaireDavid/scripts/parse_question.py
+++ ComputerScience/BD-ClaireDavid/scripts/parse_question.py
@@ -11,7 +11,7 @@ def question_to_dic(question_path):
         #if int(lst[0]) not in output:
         #    output[(int(lst[0]))] = []
         #output[int(lst[0])].append(question(lst))
-        output.append[lst]
+        output.append(lst)
     question_file.close()
     return output
 
