fc32c5bbc2e6cd103613713d466914d11d42e697
Claire DAVID <Claire.David@u-pem.fr>
2020-11-18 18:29:17 +0100

    cdavid modified Yggdrasil/ComputerScience/BD-ClaireDavid/exo/ex-simple-requete.pl

--- ComputerScience/BD-ClaireDavid/exo/ex-simple-requete.pl
+++ ComputerScience/BD-ClaireDavid/exo/ex-simple-requete.pl
@@ -24,6 +24,7 @@ allow_reroll =  True
 #   question : description en français de ce que doit calaculer la requête 
 #   solution : exemple de requête solution
 #   fichier_bd : 'nom du fichier.sql' contenant la base de données sur laquelle on veut tester la réponse
+#   schema_bd : 'nom du fichier.txt' contenant la description du schema de la base de données sur laquelle on veut tester la réponse
 #
 #   optionnel :
 #          ordered - boolean pour dire si l'ordre des tuples de la réponse est important (order by)
