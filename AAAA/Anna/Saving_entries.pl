7c95408b207c7197b9de9a5e0c863d072179add5
Anna DIAW <Anna.Diaw@u-pem.fr>
2020-11-19 12:21:37 +0100

    adiaw1 modified Yggdrasil/AAAA/Anna/Saving_entries.pl

--- AAAA/Anna/Saving_entries.pl
+++ AAAA/Anna/Saving_entries.pl
@@ -1,4 +1,4 @@
-extends= /lib/gift/templates/qessay.pl 
+extends= home/lib/gift/templates/qessay.pl 
 
 title = Saving entries
 text  = Etes vous apprenti ou suivez vous le parcours scolaire initial ? Apprenti / Initial
