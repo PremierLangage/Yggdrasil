44d94da739d4ed3296347fc1af026fc8b86bc2b1
Nadime FRANCIS <Nadime.Francis@univ-mlv.fr>
2020-11-19 20:25:34 +0100

    nfrancis modified Yggdrasil/ComputerScience/BD-DavidFrancis/templates/exo-requete.pl

--- ComputerScience/BD-DavidFrancis/templates/exo-requete.pl
+++ ComputerScience/BD-DavidFrancis/templates/exo-requete.pl
@@ -324,10 +324,11 @@ else:
 
     if grade == None:
 #        grade = (100, feedback_success)
-        grade = (100, feedback_success + f'<p class = \"warning-state\"> ordered : {str(ordered)} numbered : {str(numbered)}</p>')
+        grade = (100, feedback_success)
 
 
 ==
+
 
 
 
