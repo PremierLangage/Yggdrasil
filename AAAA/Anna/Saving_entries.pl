97df1232d37ae4be8a1907c6f4ada2cdd783e9ae
Anna DIAW <Anna.Diaw@u-pem.fr>
2020-11-19 12:15:12 +0100

    adiaw1 modified Yggdrasil/AAAA/Anna/Saving_entries.pl

---
+++ AAAA/Anna/Saving_entries.pl
@@ -0,0 +1,24 @@
+@ /utils/sandboxio.py
+@ /grader/evaluator.py [grader.py]
+@ /builder/none.py [builder.py]
+
+inputbox =: Input
+inputbox.type = multiline
+
+general_feedback = " Your answer has been successfully saved. " 
+
+title = Saving entries
+text  = Etes vous apprenti ou suivez vous le parcours scolaire initial ? Apprenti / Initial
+form  = {{ inputbox|component }}
+
+evaluator==
+# TODO sauvegarder la réponse de l'élève dans la session 
+grade = (100, general_feedback)
+==
+
+doc ==
+Ce style d'exo permet de sauvegarder les informations entrées par l'utilisateur
+dans le grader. Il n'évalue ni ne renvoie pas une note. Parfait pour un sondage 
+par exemple.
+==
+
