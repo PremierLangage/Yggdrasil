901dac640a159e2b004bb168086dfd0744f41245
Claire DAVID <Claire.David@u-pem.fr>
2020-11-19 19:55:13 +0100

    cdavid modified Yggdrasil/ComputerScience/BD-DavidFrancis/templates/exo-requete.pl

--- ComputerScience/BD-DavidFrancis/templates/exo-requete.pl
+++ ComputerScience/BD-DavidFrancis/templates/exo-requete.pl
@@ -322,8 +322,6 @@ else:
             grade = (0, '<p class = "error-state"> Le contenu de la réponse est correct, mais les lignes sont dans le mauvais ordre.</p>')
 
 
-    #bla = type(ordered) == type(True)
-    bla = str(type(ordered))
     if grade == None:
 #        grade = (100, feedback_success)
         grade = (100, f'<p class = \"warning-state\"> ordered : {str(ordered)} {str(bla)} numbered : {str(numbered)}</p>')
