3a18b7e9d38b4df2db960d65ea1335b54e1290f6
Doriann ALBERTIN <Doriann.Albertin@univ-mlv.fr>
2020-11-19 16:47:14 +0100

    dalbertin modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/3-iterables/listes/repete.pl

--- ComputerScience/python/AP1-1920/chapters/3-iterables/listes/repete.pl
+++ ComputerScience/python/AP1-1920/chapters/3-iterables/listes/repete.pl
@@ -5,11 +5,11 @@ title = Création d'une liste de répétitions
 
 text==
 
-Ecrire une fonction `repete` à deux paramètres (un entier `n` une liste `ll`)  qui retourne une nouvelle liste composée de n copies de chaque éléments de `l`.
+Ecrire une fonction `repete` à deux paramètres (un entier `n` une liste `l`)  qui retourne une nouvelle liste composée de n copies de chaque éléments de `l`.
 
 Par exemple,  `repete(6,["Non"])` donne `['Non', 'Non', 'Non', 'Non', 'Non', 'Non']`
 
-taboo: Ne pas utiliser la fonction <font style="color:red;">{{taboo}}</font> des listes.==
+taboo: Ne pas utiliser la fonction <font style="color:red;">{{taboo}}</font> des listes.
 ==
 
 taboo=extend
@@ -50,6 +50,7 @@ tests %=
     }
 }
 ==
+
 
 
 
