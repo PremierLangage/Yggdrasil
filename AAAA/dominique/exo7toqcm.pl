8c15916db4f06d4d3ed30e627f7bf7d3ee68f626
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-19 12:15:49 +0100

    drevuz modified Yggdrasil/AAAA/dominique/exo7toqcm.pl

---
+++ AAAA/dominique/exo7toqcm.pl
@@ -0,0 +1,20 @@
+
+extends= /ComputerScience/python/template/qcm_template.pl
+title=Exemple sophistiqué
+nb%3
+nbtrues%1
+text==
+Combien vaut \(\sum_{k=2}^{n} k\) ?
+
+==
+good==
+\(\frac{(n+2)(n-1)}{2}\).
+==
+bad==
+\(\frac{n(n+1)}{2}\).
+\(\frac{n(n-1)}{2}\).
+==
+
+
+
+
