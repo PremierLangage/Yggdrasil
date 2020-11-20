f6ad92f31ae2ad2781529069062f2460fa6cc8ca
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-20 07:50:42 +0100

    drevuz modified Yggdrasil/AAAA/dominique/exo7toqcm.pl

--- AAAA/dominique/exo7toqcm.pl
+++ AAAA/dominique/exo7toqcm.pl
@@ -1,20 +1,16 @@
-
 extends= /ComputerScience/python/template/qcm_template.pl
+author=Auteur(s)
 title=Exemple sophistiqué
 nb%3
 nbtrues%1
 text==
-Combien vaut \(\sum_{k=2}^{n} k\) ?
+Combien vaut $%\sum_{k=2}^{n} k%$ ?
 
 ==
 good==
-\(\frac{(n+2)(n-1)}{2}\).
+$%\frac{(n+2)(n-1)}{2}%$.
 ==
 bad==
-\(\frac{n(n+1)}{2}\).
-\(\frac{n(n-1)}{2}\).
+$%\frac{n(n+1)}{2}%$.
+$%\frac{n(n-1)}{2}%$.
 ==
-
-
-
-
