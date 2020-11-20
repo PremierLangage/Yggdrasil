d0442e8e538c830944491c2dde9d0a20e926a8c9
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-20 17:10:45 +0100

    drevuz modified Yggdrasil/model/template/amc/mathlist.pl

---
+++ model/template/amc/mathlist.pl
@@ -0,0 +1,37 @@
+
+
+extends = /model/basic.pl
+
+
+pairs==
+un§one
+deux§two
+trois§three
+==
+
+before ==
+matchlist.setdata_from_matches([ l.split("§") for l in  pairs.splitlines() if "§" in l ])
+==
+
+text= A DEFINIR 
+title= CECI EST UN PATRON NE PAS UTILISER DIRECTEMENT
+
+matchlist =: MatchList
+
+@ /utils/components/matchlist.py [custommatchlist.py]
+matchlist.decorator = CustomMatchList
+
+form ==
+{{ matchlist|component }}
+==
+
+evaluator ==
+score = matchlist.eval()
+==
+
+settings.feedback = lightscore
+
+
+
+
+
