c8698ad3b1ebd98aa68caeb68e41b1a74a3cbba1
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-20 15:33:17 +0100

    drevuz modified Yggdrasil/AAAA/dominique/Models/exemplevraifaux.pl

--- AAAA/dominique/Models/exemplevraifaux.pl
+++ AAAA/dominique/Models/exemplevraifaux.pl
@@ -16,12 +16,15 @@ answer=Non
 # pour aligner les proposition 
 horizontal % true 
 
-feedback_correct = Bien vu il est gris car il s'est roulé dans la poussière.  
+feedback_correct ==
+Bien vu il est gris car il s'est roulé dans la poussière.  
+<img src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Lol_LOL.png">
+==
 feedback_wrong = Il vous manque des informations.
 general_feedback==
 
 
-<img src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Lol_LOL.png">
+
 Bien entendu c'est une question piège.
 ==
 
