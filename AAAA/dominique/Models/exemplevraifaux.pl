a397241ad4f36ce848301c4501b606536b6354ad
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-20 09:37:00 +0100

    drevuz modified Yggdrasil/AAAA/dominique/Models/exemplevraifaux.pl

---
+++ AAAA/dominique/Models/exemplevraifaux.pl
@@ -0,0 +1,25 @@
+
+
+
+extends = /model/basic/vraifaux.pl
+
+title= Un exemple de proposition vraie ou fausse 
+
+text==
+Le cheval blanc de Henri IV est il blanc ? 
+==
+
+
+answer=Non
+
+
+# pour aligner les proposition 
+horizontal % true 
+
+feedback_correct = Bien vu il est gris car il s'est roulé dans la poussière.  
+feedback_wrong = Il vous manque des informations.
+general_feedback==
+Bien entendu c'est une question piège.
+==
+
+
