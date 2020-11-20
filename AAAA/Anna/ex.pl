b5c1bddb21d7295d97d4ff75407c2208e2a7defe
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-20 07:56:41 +0100

    drevuz modified Yggdrasil/AAAA/Anna/ex.pl

--- AAAA/Anna/ex.pl
+++ AAAA/Anna/ex.pl
@@ -1,3 +1,22 @@
 extends = /demo/template/aleaQCM__template.pl
 @/grader/evaluator.py [grader.py]
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+
+
+title = exemple 
 text = Anna
+
+
+good==
+une bonne réponse , feedback positif 
+
+==
+
+bad==
+mauvaise 1, feedback  negatif
+mauvaise  2, feedback negatif
+mauvaise 3, feedback negatif
+==
+
+
