05dddd1b5a51b2624d93e41df10b908e4bfffbbd
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-22 18:23:34 +0100

    drevuz modified Yggdrasil/model/checkbox/goodbad.pl

--- model/checkbox/goodbad.pl
+++ model/checkbox/goodbad.pl
@@ -11,12 +11,14 @@ before==
 import random
 
 if "good" not in globals():
-    text= "$%\huge \textt{ Il manque une variable good contenant la liste des bonnes reponses }%s"
+    title = " Il manque une variable good contenant la liste des bonnes reponses"
+    text = ""
 elif "bad" not in globals():
-    text= " Il manque une variable good contenant la liste des mauvaises reponses "
+    text = ""
+    title =  " Il manque une variable bad contenant la liste des mauvaises reponses "
 else:
-    good= good.strip("\n").split("\n")
-    bad= bad.strip("\n").split("\n")
+    good = good.strip("\n").split("\n")
+    bad = bad.strip("\n").split("\n")
     # 5 est le nombre de propositions
     # le randint est le nombre de bonnes propositions 
     checkbox.setdata_from_rw(good, bad, 5, random.randint(1, 4))
