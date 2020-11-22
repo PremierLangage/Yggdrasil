7021e34fc385ba8cde9b2200fbc45658bd9cd874
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-22 19:02:24 +0100

    drevuz modified Yggdrasil/model/matchlist/exemplematchlist.pl

---
+++ model/matchlist/exemplematchlist.pl
@@ -0,0 +1,12 @@
+
+extends = /model/matchlist/matchlist.pl
+
+pairs ==
+Nourriture pour chat§chat
+Nourriture pour chien§chien
+Nourriture pour oiseaux&oiseaux
+==
+
+text = Associer les bons annimaux au bonnes nourritures  
+
+
