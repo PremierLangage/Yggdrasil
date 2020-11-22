91cb4461f82a82b86747839162465ce3cc37229f
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-22 18:28:20 +0100

    drevuz modified Yggdrasil/model/checkbox/goodbad.pl

--- model/checkbox/goodbad.pl
+++ model/checkbox/goodbad.pl
@@ -31,6 +31,6 @@ else:
     bad = bad.strip("\n").split("\n")
     # 5 est le nombre de propositions
     # le randint est le nombre de bonnes propositions 
-    checkbox.setdata_from_rw(good, bad, 5, random.randint(1, 4))
+    checkbox.setdata_from_rw(good, bad, 5, random.randint(1, len(good))
 
 ==
