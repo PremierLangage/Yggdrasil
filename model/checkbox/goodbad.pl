94bf815c5fdfbc8d3b552ee29a852a21243e8361
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-22 18:17:41 +0100

    drevuz modified Yggdrasil/model/checkbox/goodbad.pl

---
+++ model/checkbox/goodbad.pl
@@ -0,0 +1,19 @@
+
+extends = /model/checkbox/div3.pl
+
+text= Cochez les réponses positives
+before==
+import random
+
+if "good" not in globals():
+    text= " Il manque une variable good contenant la liste des bonnes reponses "
+elif "bad" not in globals():
+    text= " Il manque une variable good contenant la liste des mauvaises reponses "
+else:
+    good= good.strip("\n").split("\n")
+    bad= bad.strip("\n").split("\n")
+    # 5 est le nombre de propositions
+    # le randint est le nombre de bonnes propositions 
+    checkbox.setdata_from_rw(good, bad, 5, random.randint(1, 4))
+
+==
