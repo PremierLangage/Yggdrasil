6962a7536095295d0e5d2851292ec52be52b8f49
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-23 12:56:19 +0100

    drevuz modified Yggdrasil/AAAA/dominique/mathmatchlist.pl

--- AAAA/dominique/mathmatchlist.pl
+++ AAAA/dominique/mathmatchlist.pl
@@ -7,8 +7,18 @@ before==
 var('x')
 
 f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), exp(x)]
-df=[ diff(U) for U in f]
+gf=[ f"$!{latex(fi)}!$" for fi in f]
+df=[  f"$!{latex(diff(U,x))}!$" for U in f]
 
-pairs=zip(f,df)
+pairs= [ TT for TT in zip(gf,df)]
+
+if type(pairs)==list:
+    matchlist.setdata_from_matches(pairs)
+else:
+    matchlist.setdata_from_matches([ l.split("§") for l in  pairs.splitlines() if "§" in l ])
+
 ==
 
+text==
+
+==
