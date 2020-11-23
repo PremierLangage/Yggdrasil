60bac3a362d91e2912b26c9f3318c014887dac3b
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-23 11:35:57 +0100

    drevuz modified Yggdrasil/Mathematics/demo/deriv.pl

---
+++ Mathematics/demo/deriv.pl
@@ -0,0 +1,32 @@
+
+extends = /model/mathinput.pl
+
+title = Dérivée
+
+text == 
+Donner la dérive de la fonction $!{{f| latex}}!$.
+==
+
+before==
+
+# Sympy création de la variable x 
+var('x')
+# Si j'écrit sin(x) c'est une expression  sympy 
+
+# je créé une liste de fonction (pour tirage aléatoire) dont je vais demander la dérivé 
+lf=[sin(x),cos(x),x**2,x**3]
+# un monome plus aléatoire 
+a=rd.randint(3,100)
+e=rd.randint(1,5)
+mono= a*x**e
+lf.append(mono)
+# Choix aléatoire de la fonciton 
+f=rd.choice(lf)
+# Calcul de la solution 
+sol= diff(f,x)
+==
+
+
+
+
+
