56db862f2f7f5356efd6ec53efb84106df0e7768
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-24 14:19:22 +0100

    drevuz modified Yggdrasil/EXO7/continuite2.pl

---
+++ EXO7/continuite2.pl
@@ -0,0 +1,18 @@
+
+extends= /ComputerScience/python/template/qcm_template.pl
+author=Arnaud Bodin, Abdellah Hanani, Mohamed Mzari
+title=Continuité | 123
+nb%4
+nbtrues%1
+text==
+Quelles sont les assertions vraies concernant le domaine de définition des fonctions suivantes ? (Rappel : le domaine de définition de $%f%$ est le plus grand ensemble $%D_f \subset \Rr%$ sur lequel $%f%$ est définie.)
+
+==
+good==
+Le domaine de définition de $%\exp(\frac{1}{x^2+1})%$ est $%\Rr%$.
+==
+bad==
+Le domaine de définition de $%\sqrt{x^2-1}%$ est $%[1,+\infty[%$.
+Le domaine de définition de $%\frac{1}{\sqrt{(x-1)(x-3)}}%$ est $%]1,3[%$.
+Le domaine de définition de $%\ln(x^3-8)%$ est $%[2,+\infty[%$.
+==
