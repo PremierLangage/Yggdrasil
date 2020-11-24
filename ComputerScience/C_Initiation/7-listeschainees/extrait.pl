9712acf76bfd13d68316c3cbb9ef0158c33d35fd
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-11-24 21:52:57 +0100

    protondo modified Yggdrasil/ComputerScience/C_Initiation/7-listeschainees/extrait.pl

--- ComputerScience/C_Initiation/7-listeschainees/extrait.pl
+++ ComputerScience/C_Initiation/7-listeschainees/extrait.pl
@@ -126,7 +126,7 @@ tests==
   ["tete","","10 10 22 12"],
   ["Aléatoire 1", "", " ".join([str(random.randint(-0,100)) for i in range(random.randint(5,20)+10)])],
     ["Aléatoire 2", "", " ".join([str(random.randint(-0,10)) for i in range(random.randint(10,30)+20)])],
-    ["Aléatoire 3", "", " ".join([str(random.randint(-0,10)) for i in range(random.randint(10,30)+20)])]
+    ["Aléatoire 3", "", " ".join([str(random.randint(-0,500)) for i in range(random.randint(5,10))])]
 
   ]
 ==
