3aa0f7d5443a52b05d53e2e6d4f8fd181f020ce5
Isabelle NAVIZET <Isabelle.Navizet@u-pem.fr>
2020-11-25 17:16:09 +0100

    inavizet modified Yggdrasil/AAAA/Isabelle/association.pl

--- AAAA/Isabelle/association.pl
+++ AAAA/Isabelle/association.pl
@@ -2,6 +2,14 @@ author=DD and DR
 extends= /model/math.pl
 extends= /AAAA/dominique/simplematchlist.pl
 
+#delimiter=§
+
+pairs==
+une info § de l'info correspondante
+bob § robert
+zaza § isabelle
+doudou § dominique===
+
 before==
 
 var('x')
@@ -10,7 +18,7 @@ f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), exp(x),127]
 gf=[ f"$!{latex(fi)}!$" for fi in f]
 df=[  f"$!{latex(diff(U,x))}!$" for U in f]
 
-pairs= [ TT for TT in zip(gf,df)]
+#pairs= [ TT for TT in zip(gf,df)]
 
 # Dans une version future les lignes suivantes ne seront plus nécessaires 
 if type(pairs)==list:
