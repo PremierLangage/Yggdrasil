444d879c66a406d8b7982e644814f2e3e2522fa6
Florence PETIT <Florence.Petit@u-pem.fr>
2020-11-25 18:32:23 +0100

    fpetit1 modified Yggdrasil/AAAA/Petit-Florence/chkBox-html-inline.pl

---
+++ AAAA/Petit-Florence/chkBox-html-inline.pl
@@ -0,0 +1,12 @@
+extends = /model/checkbox/div3.pl
+
+text= Cochez les balises "inline"
+before==
+import random
+good= ['a','good','span','img','input', 'td']
+bad= ['p','h1','div','select','table','ul']
+# 5 est le nombre de propositions
+# le randint est le nombre de bonnes propositions 
+checkbox.setdata_from_rw(good, bad, 5, random.randint(1, 4))
+==
+
