3b0206d7cd828a0538938bcf1939898a119e0707
David DOYEN <David.Doyen@u-pem.fr>
2020-11-25 16:24:51 +0100

    ddoyen modified Yggdrasil/Mathematics/demo/complex_mul.pl

--- Mathematics/demo/complex_mul.pl
+++ Mathematics/demo/complex_mul.pl
@@ -28,7 +28,7 @@ On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2= {{ z2|
 ==
 
 # On choisit le type de clavier virtuel
-input.virtualKeyboards = functions complex
+input.virtualKeyboards = complex
 
 # La clé evaluator contient un script python qui est exécuté quand l'élève valide sa réponse
 # Il faut définir dans ce script un score entre 0 et 100 (ou -1 pour un warning). Et éventuellement un feedback.
