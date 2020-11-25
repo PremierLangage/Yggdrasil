0f46e0fe0c82e5d03ae0afe79fff82571e20352f
Florence PETIT <Florence.Petit@u-pem.fr>
2020-11-25 18:48:51 +0100

    fpetit1 modified Yggdrasil/AAAA/Petit-Florence/chkBox-html-inline.pl

--- AAAA/Petit-Florence/chkBox-html-inline.pl
+++ AAAA/Petit-Florence/chkBox-html-inline.pl
@@ -5,10 +5,10 @@ title= HTML Inline
 text= Cochez les éléments HTML qui sont de type "en ligne" ("inline)
 before==
 import random
-good= ['a','good','span','img','input', 'td']
+good= ['a','span','img','input', 'td']
 bad= ['p','h1','div','select','table','ul']
-# 5 est le nombre de propositions
-# le randint est le nombre de bonnes propositions 
+# 5 est le nombre de propositions totales à afficher
+# le randint est le nombre de bonnes propositions à afficher 
 checkbox.setdata_from_rw(good, bad, 5, random.randint(1, 4))
 ==
 
