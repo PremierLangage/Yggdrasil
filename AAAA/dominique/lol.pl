258930036f96f544929f2cb716ba95bd5bfb8300
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-25 15:31:13 +0100

    drevuz modified Yggdrasil/AAAA/dominique/lol.pl

--- AAAA/dominique/lol.pl
+++ AAAA/dominique/lol.pl
@@ -4,13 +4,15 @@ extends= /model/math.pl
 extends= /AAAA/dominique/simplematchlist.pl
 
 
-before==#|python|
+before==
 
 var('x')
 
 f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), tan(x), exp(x),127]
 gf=[ f"$!{latex(fi)}!$" for fi in f]
 df=[  f"$!{latex(diff(U,x))}!$" for U in f]
+
+
 
 pairs= [ TT for TT in zip(gf,df)]
 
@@ -29,4 +31,8 @@ text==
 Associez chaque fonction à sa dérivée.
 ==
 
+
+evaluator==
+grade=(100," Brovo mon coco \n")
+==
 
