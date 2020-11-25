2c4baa3049d91f120071a61a4e6af1b88eba5e7f
Jean Marc Meunier <jean-marc.meunier@univ-paris8.fr>
2020-11-25 15:23:02 +0100

    jmeunier1 modified Yggdrasil/test_jmeunier/association.pl

---
+++ test_jmeunier/association.pl
@@ -0,0 +1,14 @@
+
+extends = /model/matchlist/matchlist.pl
+
+
+pairs ==
+Nourriture pour chat§chat
+Nourriture pour chien§chien
+Nourriture pour oiseaux§oiseaux
+==
+
+text = Associez les bons annimaux aux bonnes nourritures  
+
+title = Nourrire vos animaux 
+
