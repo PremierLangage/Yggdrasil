ec034590e3b80e36636d56a5054b128cdd32b2b1
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-11-25 12:36:14 +0100

    protondo modified Yggdrasil/ComputerScience/C_Initiation/7-listeschainees/ajout_ordonne.pl

--- ComputerScience/C_Initiation/7-listeschainees/ajout_ordonne.pl
+++ ComputerScience/C_Initiation/7-listeschainees/ajout_ordonne.pl
@@ -124,7 +124,9 @@ tests==
 [ ["Basique", "", "10 0 -1"],
   ["Liste vide", "", "5 "],
   
-  ["Aléatoire", "", " ".join([str(random.randint(-0,100)) for i in range(random.randint(5,30))])+" -1"]
+  ["Aléatoire", "", " ".join([str(random.randint(-0,100)) for i in range(random.randint(5,30))])+" -1"],
+  ["Aléatoire 2", "", " ".join([str(random.randint(101,200)) for i in range(random.randint(5,30))])+" -1"]
+
   ]
 ==
 
