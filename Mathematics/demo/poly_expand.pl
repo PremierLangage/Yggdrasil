6dab9239c17c2397a9dee7bab773d4a5d4ae695c
David DOYEN <David.Doyen@u-pem.fr>
2020-11-25 17:25:48 +0100

    ddoyen modified Yggdrasil/Mathematics/demo/poly_expand.pl

--- Mathematics/demo/poly_expand.pl
+++ Mathematics/demo/poly_expand.pl
@@ -3,19 +3,11 @@ extends = /model/mathinput.pl
 title = Développement
 
 before ==
-# crée le symbole x
-var('x')
-# crée des polynômes aléatoires avec des paramètres donnés (degré, 
-# nombre de coeff, borne sur les coeff, indéterminée)
-# c'est une fonction qu'on a créé dans une bibli `randsympy`
-P = rand_int_poly(2, 2, 5, x)
-Q = rand_int_poly(3, 2, 5, x)
-expr = P * Q
-sol = (expr).expand()
+
 ==
 
 text ==
-Développer $$ {{expr|latex}}.$$
+Développer $$ {{ expr|latex }}.$$
 ==
 
 evaluator ==
