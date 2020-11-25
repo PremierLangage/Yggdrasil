65e9c274449271880fa9a9b4b12807558ef68f2e
David DOYEN <David.Doyen@u-pem.fr>
2020-11-25 17:32:57 +0100

    ddoyen modified Yggdrasil/Mathematics/demo/poly_expand.pl

--- Mathematics/demo/poly_expand.pl
+++ Mathematics/demo/poly_expand.pl
@@ -18,10 +18,10 @@ var('x')
 # crée des polynômes aléatoires avec des paramètres donnés (degré, 
 # nombre de coeff, borne sur les coeff, indéterminée)
 # c'est une fonction qu'on a créé dans une bibli randsympy
-#P = rand_int_poly(2, 2, 5, x)
-#Q = rand_int_poly(3, 2, 5, x)
-#expr = P * Q
-#sol = (expr).expand()
+P = rand_int_poly(2, 2, 5, x)
+Q = rand_int_poly(3, 2, 5, x)
+expr = P * Q
+sol = (expr).expand()
 ==
 
 # Voilà le début de l'énoncé avec un principe de template (Jinja) pour insérer les variables produites dans le script before.
