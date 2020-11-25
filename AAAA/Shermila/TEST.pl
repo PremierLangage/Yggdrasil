2d733d48de5f0eae0056854437919673b611b691
Shermila MOSTARSHEDI <Shermila.Mostarshedi@u-pem.fr>
2020-11-25 15:10:23 +0100

    smostarshedi modified Yggdrasil/AAAA/Shermila/TEST.pl

---
+++ AAAA/Shermila/TEST.pl
@@ -0,0 +1,29 @@
+extends = /model/basic/vraifaux.pl
+
+title= Un exemple de proposition vraie ou fausse 
+
+text==
+Dans l'équation suivante $!x=-1!$ ? 
+$!x=3x+2!$
+==
+
+
+answer=vraifaux
+
+
+# pour aligner les propositions 
+horizontal % true 
+
+feedback_correct ==
+Bien vu il est gris car il s'est roulé dans la poussière.  
+<img src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Lol_LOL.png">
+==
+feedback_wrong = Il vous manque des informations.
+general_feedback==
+
+
+
+Bien entendu c'est une question piège.
+==
+
+
