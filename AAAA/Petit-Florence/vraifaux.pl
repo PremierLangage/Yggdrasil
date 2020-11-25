fda02ff6aefa06093ce3d21f7389d1e8fffc5ec5
Florence PETIT <Florence.Petit@u-pem.fr>
2020-11-25 15:14:27 +0100

    fpetit1 modified Yggdrasil/AAAA/Petit-Florence/vraifaux.pl

---
+++ AAAA/Petit-Florence/vraifaux.pl
@@ -0,0 +1,12 @@
+extends = /model/matchlist/matchlist.pl
+
+pairs ==
+Language pour la structure§HTML
+Language de mise en forme§CSS
+Language de programmation§JavaScript
+==
+
+text = Associez les langages à leur utilisation  
+
+title = Les langages du web
+
