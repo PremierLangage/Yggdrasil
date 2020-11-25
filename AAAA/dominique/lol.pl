05a9bb77d6db5d64f6bc764f6fe18c91448ecb35
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-25 15:22:56 +0100

    drevuz modified Yggdrasil/AAAA/dominique/lol.pl

--- AAAA/dominique/lol.pl
+++ AAAA/dominique/lol.pl
@@ -1,28 +1,31 @@
+author=DD and DR
+extends= /model/math.pl
 
-extends = /model/basic/vraifaux.pl
+extends= /AAAA/dominique/simplematchlist.pl
 
-title= Un exemple de proposition vraie ou fausse 
+
+before==
+
+var('x')
+
+f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), tan(x), exp(x),127]
+gf=[ f"$!{latex(fi)}!$" for fi in f]
+df=[  f"$!{latex(diff(U,x))}!$" for U in f]
+
+pairs= [ TT for TT in zip(gf,df)]
+
+# Dans une version future les lignes suivantes ne seront plus nécessaires 
+if type(pairs)==list:
+    matchlist.setdata_from_matches(pairs)
+else:
+    if "delimiter" not in globals():
+      delimiter='§'
+    # ne prend que les lignes avec un délimiter et construit une paire avec la chaine.
+    matchlist.setdata_from_matches([ l.split(delimiter) for l in  pairs.splitlines() if delimiter in l ])
+
+==
 
 text==
-Le cheval blanc de Henri V est il blanc ? 
-==
-
-
-answer=Non
-
-
-# pour aligner les propositions 
-horizontal % true 
-
-feedback_correct ==
-Bien vu il est gris car il s'est roulé dans la poussière.  
-<img src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Lol_LOL.png">
-==
-feedback_wrong = Il vous manque des informations.
-general_feedback==
-
-
-
-Bien entendu c'est une question piège.
+Associez chaque fonction à sa dérivée.
 ==
 
