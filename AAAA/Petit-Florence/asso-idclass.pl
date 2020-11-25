a4677008ae27796e7e21fec80d4d9479e9575852
Florence PETIT <Florence.Petit@u-pem.fr>
2020-11-25 15:37:00 +0100

    fpetit1 modified Yggdrasil/AAAA/Petit-Florence/asso-idclass.pl

--- AAAA/Petit-Florence/asso-idclass.pl
+++ AAAA/Petit-Florence/asso-idclass.pl
@@ -5,7 +5,6 @@ extends= /AAAA/dominique/simplematchlist.pl
 pairs ==
 id="truc"§#truc
 class="truc"§.truc
-Language de programmation§JavaScript
 ==
 
 text = Associez chaque attribut à son utilisation comme sélecteur  
