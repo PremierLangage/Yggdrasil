75d83db277d817b5aa438695c2ffb329770a9e12
Florence PETIT <Florence.Petit@u-pem.fr>
2020-11-25 18:48:29 +0100

    fpetit1 modified Yggdrasil/AAAA/Petit-Florence/chkBox-html-block.pl

--- AAAA/Petit-Florence/chkBox-html-block.pl
+++ AAAA/Petit-Florence/chkBox-html-block.pl
@@ -5,10 +5,10 @@ title= HTML Block
 text= Cochez les éléments HTML qui sont de type "bloc" ("block)
 before==
 import random
-bad= ['a','good','span','img','input', 'td']
+bad= ['a','span','img','input', 'td']
 good= ['p','h1','div','select','table','ul']
-# 5 est le nombre de propositions
-# le randint est le nombre de bonnes propositions 
+# 5 est le nombre de propositions totales à afficher
+# le randint est le nombre de bonnes propositions à afficher
 checkbox.setdata_from_rw(good, bad, 5, random.randint(1, 4))
 ==
 
