710a7ad8d05b11aa74803708f7f3f9aa27c82725
Pablo ROTONDO <Pablo.Rotondo@univ-mlv.fr>
2020-11-25 12:31:27 +0100

    protondo modified Yggdrasil/ComputerScience/C_Initiation/7-listeschainees/melange.pl

--- ComputerScience/C_Initiation/7-listeschainees/melange.pl
+++ ComputerScience/C_Initiation/7-listeschainees/melange.pl
@@ -160,7 +160,6 @@ int main(void) {
 tests==
 [ ["Basique", "", "0 2 10 6 -1 -2 -3"],
   ["Vide 1", "", "1 4 1 2 3"],
-  ["Vide 2", "", "1 0 1 2 3"],
   ["Aléatoire 1", "", str(random.randint(0,1))+" "+str(random.randint(5,8))+" "+" ".join([str(random.randint(1,100)) for i in range(random.randint(8,20))])],
   ["Aléatoire 2", "", str(random.randint(0,1))+" "+str(random.randint(10,15))+" "+" ".join([str(random.randint(1,100)) for i in range(random.randint(8,20))])]
 
