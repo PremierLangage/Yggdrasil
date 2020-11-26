0b25fb3f42b958eb43f387b30cc55d85f4963b29
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 16:21:52 +0100

    adiaw modified Yggdrasil/AAAA/Anna/plsoluce.pl

--- AAAA/Anna/plsoluce.pl
+++ AAAA/Anna/plsoluce.pl
@@ -7,3 +7,18 @@ extends = /ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
 @ utils/feedback2.py
 @ utils/template.html
+
+title = Python - Factoriel
+text = Ecrire une fonction qui prend en paramètreun entier et renvoie son factoriel.
+
+mplsoluce0==
+def factorielle(n):
+    if n == 0:
+        return 1
+    else:
+        F = 1
+        for k in range(2,n+1):
+            F = F * k
+        return F
+==
+
