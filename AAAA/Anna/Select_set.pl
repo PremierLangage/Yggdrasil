97d7a643497311174f966a67c1f2ff4c2ba6790b
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 13:02:42 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Select_set.pl

---
+++ AAAA/Anna/Select_set.pl
@@ -0,0 +1,18 @@
+extends = /gift/templates/qselectset.pl
+
+title==
+Select Set
+==
+
+text==
+Quelle couleur se trouve entre l'orange et le vert dans le spectre ?
+==
+
+choices==
+=jaune #Bonne réponse; bravo !
+=vert #Bonne réponse; bravo !
+~rouge #Mauvaise réponse, c'est jaune.
+~bleu #Mauvaise réponse, c'est jaune.
+==
+
+
