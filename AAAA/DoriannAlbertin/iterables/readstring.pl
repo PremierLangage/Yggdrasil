508bceb7c4bf5c6af54c0d3c05929c744e77d02c
Doriann ALBERTIN <Doriann.Albertin@univ-mlv.fr>
2020-11-26 22:54:55 +0100

    dalbertin modified Yggdrasil/AAAA/DoriannAlbertin/iterables/readstring.pl

--- AAAA/DoriannAlbertin/iterables/readstring.pl
+++ AAAA/DoriannAlbertin/iterables/readstring.pl
@@ -3,10 +3,20 @@
 extends= /ComputerScience/python/AP1-1920/templates/prosoluce.pl
 builder= /builder/before.py
 
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+
+
 title = Lecture de chaîne de caractères
 text == 
 Ecrire une fonction `read` prenant en paramètre une chaîne de caractères, et affichant chaque caractère de celle-ci succéssivement.
 ==
+
+
+before==
+soluce= presoluce
+==
+
 
 presoluce==
 # solution
