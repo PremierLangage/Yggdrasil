b7144d3acf69eef4c2ab383261264e95c6778d9a
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 11:52:01 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Multiple_choice.pl

---
+++ AAAA/Anna/Multiple_choice.pl
@@ -0,0 +1,21 @@
+extends = /gift/templates/qmultichoice.pl
+
+title==
+Multiple Choice 
+==
+
+text==
+Parmi ces personnes, nommez-en deux qui sont enterrées dans la Grant's tomb.
+==
+
+choices==
+%-100.0%Personne
+%50.0%Grant
+%50.0%L'épouse de Grant
+%-50.0%Le père de Grant
+==
+
+doc == 
+Exo à choix multiple où on peut choisir une ou plusieurs réponses dans celles 
+proposées. 
+==
