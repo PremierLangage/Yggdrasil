565bc931536339655afc72154b7f2851ce463423
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 10:54:49 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Saving_entries.pl

--- AAAA/Anna/Saving_entries.pl
+++ AAAA/Anna/Saving_entries.pl
@@ -1,8 +1,9 @@
 extends= /gift/templates/qessay.pl 
 
 title = Saving entries
-text  = Donner une résumé du texte sur le lien suivant différent de celui qui s'y trouve déjà en 80 mots au maximum : " https://ishyo.wordpress.com/2014/05/06/extrait-tire-du-roman-une-si-longue-lettre-de-mariama-ba/ " 
-
+text  ==
+ Donner une résumé du texte sur le lien suivant différent de celui qui s'y trouve déjà en 80 mots au maximum : " https://ishyo.wordpress.com/2014/05/06/extrait-tire-du-roman-une-si-longue-lettre-de-mariama-ba/ " 
+==
 doc ==
 Ce style d'exo permet de sauvegarder les informations entrées par l'utilisateur
 dans le grader. Il n'évalue ni ne renvoie pas une note mais enregistre juste la
