a8340de2cfbf520bea4752c34e192d63bb7eeb7b
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 14:46:51 +0100

    adiaw modified Yggdrasil/AAAA/Anna/QCM_aleatoire.pl

---
+++ AAAA/Anna/QCM_aleatoire.pl
@@ -0,0 +1,13 @@
+extends = /demo/template/aleaQCM_template.pl
+
+title== 
+True or False ?
+==
+
+text==#|markdown|
+Cette affirmation est-elle vraie ou Fausse ? 
+
+**Marseille est la capitale de la France**
+==
+
+
