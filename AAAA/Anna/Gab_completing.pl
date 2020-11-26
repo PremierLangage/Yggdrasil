f87607c29943d5c9ad7a5d349d259868a38a9708
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 13:36:27 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Gab_completing.pl

--- AAAA/Anna/Gab_completing.pl
+++ AAAA/Anna/Gab_completing.pl
@@ -10,16 +10,25 @@ Compléter le code suivant pour que la fonction puisse être opérationnelle
 ```{r}
 
 def factoriel(n):
+
     """
+
     Calcule le factoriel de l'entier n
+
     """
+
     if n<0:
+
         return None
+
     f=1
+
     for i in <b>_____</b>:
+
         f*=i+1 
+        
     return f 
-    
+
 ```
 
 == 
