fc0c00bc0e9683028b357e3fe3a76f667d8eda77
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 11:28:52 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Matching.pl

---
+++ AAAA/Anna/Matching.pl
@@ -0,0 +1,24 @@
+extends = /gift/templates/qmatch.pl
+
+title==
+Matching
+==
+
+text==
+Relier chaque mot français à sa traduction en anglais
+==
+
+choices==
+=Pomme -> Apple
+=Chien -> Dog
+=Règle -> Rule
+=Chaussure -> Shoes
+==
+
+doc ==
+Exos à choix unique où on doit relier chaque élèment de la première case
+à un élèment de la seconde case. 
+Code couleurs : Après validation de la réponse 
+-> Flèches vertes = réponses justes
+-> Flèches rouges = réponses fausses
+==
