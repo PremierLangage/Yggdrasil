27564561372eba68d83be9a1fa1c88bf3f382d8a
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 10:55:20 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Saving_entries.pl

--- AAAA/Anna/Saving_entries.pl
+++ AAAA/Anna/Saving_entries.pl
@@ -2,7 +2,8 @@ extends= /gift/templates/qessay.pl
 
 title = Saving entries
 text  ==
- Donner une résumé du texte sur le lien suivant différent de celui qui s'y trouve déjà en 80 mots au maximum : " https://ishyo.wordpress.com/2014/05/06/extrait-tire-du-roman-une-si-longue-lettre-de-mariama-ba/ " 
+Donner une résumé du texte sur le lien suivant différent de celui qui s'y trouve déjà en 80 mots au maximum : 
+" https://ishyo.wordpress.com/2014/05/06/extrait-tire-du-roman-une-si-longue-lettre-de-mariama-ba/ " 
 ==
 doc ==
 Ce style d'exo permet de sauvegarder les informations entrées par l'utilisateur
