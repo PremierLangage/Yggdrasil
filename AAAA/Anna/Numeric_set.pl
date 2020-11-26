e84b72e2f67a88f24e82d3bf46c962a2829f6440
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 12:37:47 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Numeric_set.pl

---
+++ AAAA/Anna/Numeric_set.pl
@@ -0,0 +1,22 @@
+extends = /gift/templates/qnumericset.pl
+
+title==
+Numeric Set
+==
+
+text==
+Combien de vers Victor Hugo a t'il écrit au cours de ses 83 ans d'existence ?
+==
+
+choices==
+153837.0:0.0 #Bravo !Vous avez tous les points.
+%50.0%153837.0:100.0 #Il a écrit 153 837 vers. Votre réponse était pas mal proche, vous avez la moitié des points.
+%33.0%153837.0:500.0 #Il a écrit 153 837 vers. Votre réponse était pas mal proche, vous avez le tiers des points.
+%25.0%153837.0:1000.0 #Il a écrit 153 837 vers. Votre réponse était un peu proche, vous avez le quart des points.
+== 
+
+doc == 
+Exo ouvert où la réponse est rentré par l'élève et le résultat immédiatement obtenus. On peut noter selon un interval 
+d'exactitude de la réponse. 
+==
+
