957309e716f476ff6665323a7afa4119899134aa
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 16:17:20 +0100

    adiaw modified Yggdrasil/AAAA/Anna/plsoluce.pl

---
+++ AAAA/Anna/plsoluce.pl
@@ -0,0 +1,9 @@
+extends = /ComputerScience/python/AP1-1920/templates/plsoluce.pl
+@ utils/soluceevaluator.py [grader.py] 
+@ utils/gradesoluce.py
+@ /utils/sandboxio.py
+
+@ /builder/before.py [builder.py]
+
+@ utils/feedback2.py
+@ utils/template.html
