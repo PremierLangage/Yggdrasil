1c9d0ed0735ef71f39b5c2410e3ed4e33eebcd2b
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 11:00:58 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Saving_entries.pl

--- AAAA/Anna/Saving_entries.pl
+++ AAAA/Anna/Saving_entries.pl
@@ -5,6 +5,7 @@ text  ==
 Donner une résumé du texte sur le lien suivant différent de celui qui s'y trouve déjà en 80 mots au maximum : 
 " https://ishyo.wordpress.com/2014/05/06/extrait-tire-du-roman-une-si-longue-lettre-de-mariama-ba/ " 
 ==
+
 doc ==
 Ce style d'exo permet de recueillir les réponses des élèvesdans le grader. 
 Adapté aux sondages ou aux questions ouvertes par exemple.
