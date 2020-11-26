3442b2839f6cc93af30da561a6276bc9fb7983f5
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 13:30:51 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Gab_completing.pl

---
+++ AAAA/Anna/Gab_completing.pl
@@ -0,0 +1,32 @@
+extends = /gift/templates/qshortset.pl
+
+title==
+Q3
+==
+
+text==
+Compléter le code suivant pour que la fonction puisse être opérationnelle
+def factoriel(n):
+    """
+    Calcule le factoriel de l'entier n
+    """
+    if n<0:
+        return None
+    f=1
+    for i in <b>_____</b>:
+        f*=i+1 
+    return f 
+==
+
+choices==
+=range(n) #Bravo
+== 
+
+doc == 
+Exo ouvert où la réponse est rentré par l'élève et le résultat immédiatement obtenus. On peut noter selon un interval 
+d'exactitude de la réponse. 
+Après validation de la réponse : 
+✓ -> réponse correcte 
+x -> réponse fausse
+==
+
