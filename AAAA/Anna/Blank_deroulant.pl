34d0257f57e02f4d7e0d1c0771a1ac2396f6a5ed
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 14:56:58 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Blank_deroulant.pl

---
+++ AAAA/Anna/Blank_deroulant.pl
@@ -0,0 +1,28 @@
+extends = /demo/template/aleaQCM__template.pl
+
+title== 
+True or False ?
+==
+
+text==#|markdown|
+Choisir la bonne réponse
+==
+
+good==
+Napoleon, Oui c'est ca
+Bonapparte, Oui aussi malgré l'orthographe
+
+==
+
+bad==
+Michel Fugain, T'es con ou quoi 
+Quentin Coumes, Peut être un jour
+le pape, pas vraiment
+Johnny, Pour leatitia 
+==
+
+
+nbgood=2
+nbbad=3
+
+
