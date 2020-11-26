f4873b0412260a1de28d7d83f3d2038ac4935c91
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2020-11-26 12:27:15 +0100

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/QCM/ProgC_QCM_2.pl

---
+++ ComputerScience/C_Initiation/QCM/ProgC_QCM_2.pl
@@ -0,0 +1,16 @@
+extends= /model/AMC2/QCM-AMCE.pl
+
+textstep ==
+<strong> Question {{ step + 1 }}. </strong> 
+{{ statement[step] | safe }}
+==
+
+title= 2ème examen d'Introduction à la programmation C
+
+text=
+onepergroup % false
+
+questions=@ ProgC_QCM_2.txt
+
+
+
