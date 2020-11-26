388b3df61e7fdd41c82a0672e3a852ab0f7859b4
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 13:07:24 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Select_set.pl

--- AAAA/Anna/Select_set.pl
+++ AAAA/Anna/Select_set.pl
@@ -9,8 +9,8 @@ De quel savant est cette célèbre citation : "Rien ne se perd, rien ne se crée
 ==
 
 choices==
-=Antoine Lavoisier #Bonne réponse; bravo !
 ~Cheikh Anta Diop #Mauvaise réponse, c'est Antoine Lavoisier.
+=Antoine Lavoisier #Bonne réponse; bravo !
 ~Aristote #Mauvaise réponse, c'est Antoine Lavoisier.
 ==
 
