a62f093422701915107fc18657491c0fab1c9c69
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-11-26 10:39:27 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Saving_entries.pl

--- AAAA/Anna/Saving_entries.pl
+++ AAAA/Anna/Saving_entries.pl
@@ -6,8 +6,7 @@ text  = Etes vous apprenti ou suivez vous le parcours scolaire initial ? Apprent
 doc ==
 Ce style d'exo permet de sauvegarder les informations entrées par l'utilisateur
 dans le grader. Il n'évalue ni ne renvoie pas une note mais enregistre juste la
-réponse de l'élève. Adapté aux sondages
-par exemple.
+réponse de l'élève. Adapté aux sondages ou aux questions ouvertes par exemple.
 ==
 
 
