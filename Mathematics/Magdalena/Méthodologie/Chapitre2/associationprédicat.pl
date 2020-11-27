92742af886db58ec5b7fc7164a4976fca1f9d774
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-27 16:02:21 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl
@@ -5,7 +5,7 @@ extends= /AAAA/dominique/simplematchlist.pl
 pairs ==
 $%\forall n \in \mathbb{N} \quad \exists l \in \mathbb{Z} \quad q_n=l%$§la suite **q** est à valeurs dans les entiers relatifs.
 $%\exists l \in \mathbb{Z} \quad \forall n \in \mathbb{N} \quad  q_n=l%$§la suite **q**  est constante et cette constante est un entier relatif.
-$%\forall l \in \mathbb{Z} \quad \exists n \in \mathbb{N} \quad  q_n=l%$§la suite **q** prend toutes les valeurs entières positives ou nulles.
+$%\forall l \in \mathbb{Z} \quad \exists n \in \mathbb{N} \quad  q_n=l%$§la suite **q** prend toutes les valeurs entières.
 $%\forall l \in \mathbb{Z}^* \quad \exists n \in \mathbb{N} \quad  |q_n|\le  \frac{1}{l} %$§la suite **q** est nulle .
 ==
 
