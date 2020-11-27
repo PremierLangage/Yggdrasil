cc2daec4a75701fc55ae9b9a8be8bf3fe12e5405
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-27 19:04:08 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl
@@ -5,12 +5,16 @@ title = Développement limité d'un produit.
 before ==
 var('x') # crée le symbole x
 n = 3 # Ordre du DL
+
 P = randitem([exp(x)])
 Q = randitem([cos(x)])
+
 P1 = P.series(x,0,n+1) # Attention +1 sur l'ordre (notation O par défault).
 Q1 = Q.series(x,0,n+1) 
+
 P2=P1.subs(O(x**(n+1)),0)
 Q2=Q1.subs(O(x**(n+1)),0)
+
 expr_poly = P*Q # version non développée
 sol = (P2 * Q2).expand() # version développée
 
