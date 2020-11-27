be93859b53e6b19d74a8401dd76dfb0bc9cdd9fc
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-27 16:48:36 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/mouse.pl

---
+++ ComputerScience/C/examples/mouse.pl
@@ -0,0 +1,50 @@
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+inputbox =: Input
+inputbox.type = number
+inputbox.placeholder = Answer
+inputbox.maxlength = 3
+inputbox.appearance = outline
+
+url_clip =$ PereBorie.webm
+
+before==
+
+==
+
+title==
+Analyser une vidéo
+==
+
+text==
+Quand le clip suivant, combien de souris le chat fait-il couiné ?
+
+
+<video><source src="{{ url_clip }}" type="video/webm"></video>
+
+==
+
+form==
+<style>
+.error-state{
+   padding: 10px;
+}
+.success-state{
+   padding: 10px;
+}
+</style>
+
+{{ inputbox|component}}
+==
+
+evaluator==
+if 2 == int(inputbox.value):
+    grade = (100, '<span class="success-state">Mais oui, mais c'est bien sûr !</span>')
+else:
+    grade = (0, '<span class="error-state">Regardez mieux le chat, c'est celui qui a le plus de poil...</span>')
+==
+
+
+
