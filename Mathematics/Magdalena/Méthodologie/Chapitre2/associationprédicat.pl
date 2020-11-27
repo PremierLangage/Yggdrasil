563f458f8eb71ee81d3e8e88b153ac576ffdb474
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-27 16:05:42 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl
@@ -6,7 +6,8 @@ pairs ==
 $%\forall n \in \mathbb{N} \quad \exists l \in \mathbb{Z} \quad q_n=l%$§la suite **q** est à valeurs dans les entiers relatifs.
 $%\exists l \in \mathbb{Z} \quad \forall n \in \mathbb{N} \quad  q_n=l%$§la suite **q**  est constante et cette constante est un entier relatif.
 $%\forall l \in \mathbb{Z} \quad \exists n \in \mathbb{N} \quad  q_n=l%$§la suite **q** prend toutes les valeurs entières.
-$%\forall l \in \mathbb{Z}^* \quad \exists n \in \mathbb{N} \quad  |q_n|\le  \frac{1}{l} %$§la suite **q** est nulle .
+$%\forall l \in \mathbb{Z}^* \quad \forall n \in \mathbb{N} \quad  |q_n|\le  \frac{1}{l} %$§la suite **q** est nulle.
+$%\forall l \in \mathbb{Z}^* \quad \exists n \in \mathbb{N} \quad  |q_n|\le  \frac{1}{l} %$§la suite **q** prend des valeurs arbitrairement proche de 0.
 ==
 
 text = Soit **q** = $% (q_n)_{n \in\mathbb{N}} %$  une suite de nombre rationnels. Associer les prédicats suivants avec leurs significations en français. 
