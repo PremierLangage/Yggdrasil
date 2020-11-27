f22b5256140852071d46b2f5d0b746dafb16be17
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-27 18:47:35 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl
@@ -4,12 +4,13 @@ title = Développer une expression polynomiale
 
 before ==
 var('x') # crée le symbole x
+n = 3 # Ordre du DL
 P = randitem([exp(x), ln(1+x)])
 Q = randitem([cos(x), sin(x)])
-P1 = series(exp(x),x,0,2) # Attention +1 sur l'ordre
-Q1 = series(cos(x),x,0,2) 
-P2=P1.subs(O(x**2),0)
-Q2=Q1.subs(O(x**2),0)
+P1 = series(exp(x),x,0,n+1) # Attention +1 sur l'ordre
+Q1 = series(cos(x),x,0,n+1) 
+P2=P1.subs(O(x^(n+1)),0)
+Q2=Q1.subs(O(x^(n+1)),0)
 expr_poly = P2*Q2 # version non développée
 sol = (P1 * Q1).expand() # version développée
 ==
