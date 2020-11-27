b63860bb3c03c64fb669b64bac3fdd0b49dfa8a2
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-27 19:16:37 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl
@@ -25,7 +25,7 @@ sol = Reg # version développée
 
 text ==
 Calculer la partie régulière du développement limité à l'ordre $!{{ n |latex }}!$ en 0 de la fonction ci-dessous.
-$${{ expr_poly|latex }}$$
+$${{ PQ|latex }}$$
 ==
 
 evaluator ==
