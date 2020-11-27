4a5aae17ba56b94ae1573d5c0c08053396f6c35d
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-27 15:50:57 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/PropositionsVraiesouFausses.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/PropositionsVraiesouFausses.pl
@@ -0,0 +1,16 @@
+extends = /model/checkbox/goodbad.pl
+text= Cochez les énoncés vrais 
+good==# une chaine multiple avec une bonne solution par ligne 
+$% 0\le 3 %$
+$% 0\le 3 %$ ou $% 1+1 \le 1 %$
+$% 1+1 \le 1 %$ $%\implies %$  $% 0\le 3 %$ 
+$% 1+1 \le 1 %$ $%\implies %$  $% 3\le 0 %$ 
+$% \pi \ge 3%$
+==
+bad==
+$% 0\le 3 %$ et  $% 1+1 \le 1 %$
+$% 0\le 3 %$ $%\implies %$   $% 1+1 \le 1 %$
+$% 0\ge 3 %$ ou  $% 1+1 \le 1 %$
+==
+
+
