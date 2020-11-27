49b62009d5f64172154438dc685e60b5e3662981
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-27 19:04:36 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl
@@ -22,8 +22,8 @@ sol = (P2 * Q2).expand() # version développée
 ==
 
 text ==
-Calculer la partie régulière du développement limité à l'ordre $!{{ n |latex }}!$ en 0 de la fonction ci-dessous :
-$${{ expr_poly|latex }}.$$
+Calculer la partie régulière du développement limité à l'ordre $!{{ n |latex }}!$ en 0 de la fonction ci-dessous.
+$${{ expr_poly|latex }}$$
 ==
 
 evaluator ==
