3dbe0e520942bec71eeaf5141cef45895e7dbfe3
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-27 16:01:12 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl
@@ -3,13 +3,13 @@ extends= /AAAA/dominique/simplematchlist.pl
 
 
 pairs ==
-$%\forall n \in \mathbb{N} \quad \exists l \in \mathbb{Z} \quad q_n=l%$§la suite $!(q_n)_{n \in \mathbb{N}!$ est à valeurs dans les entiers relatifs.
-$%\exists l \in \mathbb{Z} \quad \forall n \in \mathbb{N} \quad  q_n=l%$§la suite $%(q_n)_{n \in \mathbb{N}%$ est constante et cette constante est un entier relatif.
-$%\forall l \in \mathbb{Z} \quad \exists n \in \mathbb{N} \quad  q_n=l%$§la suite $%(q_n)_{n \in \mathbb{N}%$ prend toutes les valeurs entières positives ou nulles.
-$%\forall l \in \mathbb{Z}^* \quad \exists n \in \mathbb{N} \quad  |q_n|\le  \frac{1}{l} %$§la suite $%(q_n)_{n \in \mathbb{N}%$ est nulle .
+$%\forall n \in \mathbb{N} \quad \exists l \in \mathbb{Z} \quad q_n=l%$§la suite **q** est à valeurs dans les entiers relatifs.
+$%\exists l \in \mathbb{Z} \quad \forall n \in \mathbb{N} \quad  q_n=l%$§la suite **q**  est constante et cette constante est un entier relatif.
+$%\forall l \in \mathbb{Z} \quad \exists n \in \mathbb{N} \quad  q_n=l%$§la suite **q** prend toutes les valeurs entières positives ou nulles.
+$%\forall l \in \mathbb{Z}^* \quad \exists n \in \mathbb{N} \quad  |q_n|\le  \frac{1}{l} %$§la suite **q** est nulle .
 ==
 
-text = Soit $% (q_n)_{n \in\mathbb{N}} %$  une suite de nombre rationnels. Associer les prédicats suivants avec leurs significations en français. 
+text = Soit **q** = $% (q_n)_{n \in\mathbb{N}} %$  une suite de nombre rationnels. Associer les prédicats suivants avec leurs significations en français. 
 
 title = Sens d'un prédicat
 
