471e124d43deb3797bb2d1eb39d2ec2d1ce0ffb9
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-27 19:00:23 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl

---
+++ Mathematics/exercises/analysis/Taylor_Polynomial/TP_product.pl
@@ -0,0 +1,33 @@
+extends = /model/mathinput.pl
+
+title = Développement limité d'un produit.
+
+before ==
+var('x') # crée le symbole x
+n = 3 # Ordre du DL
+P = randitem([exp(x), ln(1+x)])
+Q = randitem([cos(x), sin(x)])
+P1 = P.series(x,0,n+1) # Attention +1 sur l'ordre (notation O par défault).
+Q1 = Q.series(x,0,n+1) 
+P2=P1.subs(O(x**(n+1)),0)
+Q2=Q1.subs(O(x**(n+1)),0)
+expr_poly = P2*Q2 # version non développée
+sol = (P2 * Q2).expand() # version développée
+
+# Remarque générale : Sympy sait calculer avec les "O", mais je ne sais pas si il connait les "o"...
+==
+
+text ==
+Calculer la partie régulière du développement limité à l'ordre $!n!$ de la fonction ci-dessous 
+$${{ expr_poly|latex }}.$$
+==
+
+evaluator ==
+score, error = eval_poly(input.value, sol, var="x", form="expanded")
+feedback = message[error]
+==
+
+solution ==
+La solution est $! {{ sol|latex}} !$.
+==
+
