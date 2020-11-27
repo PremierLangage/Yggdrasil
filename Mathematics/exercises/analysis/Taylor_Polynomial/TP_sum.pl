19d029f537f1445a09f49f36746e75984a0ed30c
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-27 18:12:06 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl
@@ -7,7 +7,7 @@ var('x') # crée le symbole x
 P = rd.randint(-5, 5) + rd.randint(-5, 5)*x
 Q = rd.randint(-5, 5) + rd.randint(-5, 5)*x
 expr_poly = P*Q # version non développée
-sol = (P * ).expand() # version développée
+sol = (P * Q).expand() # version développée
 ==
 
 text ==
