05922ac233dfd8a3be635d0d143af6cd494490ed
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-11-27 12:28:22 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/associationprédicat.pl
@@ -0,0 +1,13 @@
+extends = /model/matchlist/matchlist.pl
+
+pairs ==
+$%\forall n \in \mathbb{N} \quad \exists l \in \mathbb{Z} \quad q_n=l%$§la suite $%(q_n)_{n \in \mathbb{N}%$ est à valeurs dans les entiers relatifs.
+$%\exists l \in \mathbb{Z} \quad \forall n \in \mathbb{N} \quad  q_n=l%$§la suite $%(q_n)_{n \in \mathbb{N}%$ est constante et cette constante est un entier relatif.
+$%\forall l \in \mathbb{Z} \quad \exists n \in \mathbb{N} \quad  q_n=l%$§la suite $%(q_n)_{n \in \mathbb{N}%$ prend toutes les valeurs entières positives ou nulles.
+$%\forall l \in \mathbb{Z}^* \quad \exists n \in \mathbb{N} \quad  |q_n|\le  \frac{1}{l} %$§la suite $%(q_n)_{n \in \mathbb{N}%$ est nulle .
+==
+
+text = Soit $% (u_n)_{n \im \mathbb{N}} %$  une suite de nombre rationnels. Associer les prédicats suivants avec leurs significations en français. 
+
+title = Sens d'un prédicat
+
