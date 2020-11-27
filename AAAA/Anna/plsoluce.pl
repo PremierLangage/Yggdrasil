fe6591e8c2d819981f45f9fe6d5dbeca3da4d4f3
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2020-11-27 17:08:37 +0100

    drevuz modified Yggdrasil/AAAA/Anna/plsoluce.pl

--- AAAA/Anna/plsoluce.pl
+++ AAAA/Anna/plsoluce.pl
@@ -1,24 +1,16 @@
-extends = /ComputerScience/python/AP1-1920/templates/prosoluce.pl
+extends = /ComputerScience/python/AP1-1920/templates/pltest.pl
 
 before=
 
 title = Python - Factoriel
 text = Ecrire une fonction qui prend en paramètreun entier et renvoie son factoriel.
 
-soluce==
-def factorielle(n):
-    if n == 0:
-        return 1
-    else:
-        F = 1
-        for k in range(2,n+1):
-            F = F * k
-        return F
-==
 
-plsoluce==
-Simple | 4
-Plus lourd | 56
-Zéro | 0
+pltest==
+>>> factorielle(1)
+1
+>>> factorielle(2) == 2
+True
+>>> factorielle(56) == 35
 ==
-
+xs
