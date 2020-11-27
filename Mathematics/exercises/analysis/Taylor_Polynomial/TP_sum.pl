35cd45093182e4848dacbab684ce8704084c8491
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-27 18:11:43 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl

---
+++ Mathematics/exercises/analysis/Taylor_Polynomial/TP_sum.pl
@@ -0,0 +1,25 @@
+extends = /model/mathinput.pl
+
+title = Développer une expression polynomiale
+
+before ==
+var('x') # crée le symbole x
+P = rd.randint(-5, 5) + rd.randint(-5, 5)*x
+Q = rd.randint(-5, 5) + rd.randint(-5, 5)*x
+expr_poly = P*Q # version non développée
+sol = (P * ).expand() # version développée
+==
+
+text ==
+Développer $${{ expr_poly|latex }}.$$
+==
+
+evaluator ==
+score, error = eval_poly(input.value, sol, var="x", form="expanded")
+feedback = message[error]
+==
+
+solution ==
+La solution est $! {{ sol|latex}} !$.
+==
+
