c4e248b5cc3b1a793a261f9efe8130d0e106ceff
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-11-28 14:05:37 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/injectivite.pl

--- AAAA/Quentin/injectivite.pl
+++ AAAA/Quentin/injectivite.pl
@@ -8,51 +8,16 @@ author = Quentin
 title= Injectivité
 
 text==
-La fonction $!f:\mathbb R \to \mathbb R_+!$ $!x\mapsto \exp(x)!$ est-elle injective ?  
+La fonction $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle injective ?  
 ==
-
-
-answer=Non
-
-
+answer=Oui
 # pour aligner les propositions 
 horizontal % true 
-
 feedback_correct ==
 Bonne réponse
 ==
 feedback_wrong = Mauvaise réponse.
 general_feedback==
-
-
-evaluator==
-import traceback
-import sys
-
-try: 
-    c += 1
-    if int(response['answer']) == op1 + op2:
-        grade = (100, "Bonne réponse")
-    else:
-        grade = (0, "Mauvaise réponse")
-except:
-    print(traceback.format_exc(), file=sys.stderr)
-    grade = (-1, "Merci de rentrer un entier")
-==
-
-
-before==
-import random
-random.seed(seed)
-op1 = random.randint(1, 10)
-op2 = random.randint(1, 10)
-c = 0
-==
-
-@ /utils/sandboxio.py
-@ ../../../lib/builder/before.py [builder.py]
-@ ../../../lib/grader/evaluator.py [grader.py]
-
 
 
 
