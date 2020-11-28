ec5a2318e64954802089fa5b2b65f73911c494ec
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-11-28 14:11:21 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/injectivite.pl

--- AAAA/Quentin/injectivite.pl
+++ AAAA/Quentin/injectivite.pl
@@ -6,17 +6,13 @@ author = Quentin
 
 
 title= Injectivité
-
 questions==
-
 
 * La fonction $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle injective ?   
 - Non 
 + Oui 
 
-
 * La fonction $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle surjective ?  
-
 + Non
 - Oui 
 ==
