d64e7ffd7ed0214f80536a511d14836040fd06bd
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-28 12:48:17 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/mouse.pl

--- ComputerScience/C/examples/mouse.pl
+++ ComputerScience/C/examples/mouse.pl
@@ -1,3 +1,19 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
 @ /utils/sandboxio.py
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
