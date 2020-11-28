a08362877882cbd0a8be57fbe65f9ade50a0f495
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-11-28 14:05:57 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/injectivite.pl

--- AAAA/Quentin/injectivite.pl
+++ AAAA/Quentin/injectivite.pl
@@ -13,11 +13,6 @@ La fonction $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle injec
 answer=Oui
 # pour aligner les propositions 
 horizontal % true 
-feedback_correct ==
-Bonne réponse
-==
-feedback_wrong = Mauvaise réponse.
-general_feedback==
 
 
 
