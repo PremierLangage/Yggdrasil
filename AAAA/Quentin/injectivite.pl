4aab463e87635b57c8fcc28024fbb4ba8d34a1b1
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-11-28 14:06:25 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/injectivite.pl

--- AAAA/Quentin/injectivite.pl
+++ AAAA/Quentin/injectivite.pl
@@ -14,6 +14,12 @@ answer=Oui
 # pour aligner les propositions 
 horizontal % true 
 
+text==
+La fonction $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle surjective ?  
+==
+answer=Non
+# pour aligner les propositions 
+horizontal % true 
 
 
 
