c3db8903bd9e4ed6f0d0193a5ea38d30b48e838d
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-11-29 10:01:38 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/injectivite1.pl

---
+++ AAAA/Quentin/injectivite1.pl
@@ -0,0 +1,58 @@
+
+extends = /model/basic.pl
+
+title= Addition Aléatoire
+
+author = Quentin
+
+checkbox =: CheckboxGroup
+checkbox.decorator = CustomCheckbox
+
+
+text==
+La fonction $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle injective ?  
+try count = {{ nb_attempt }}
+==
+
+form ==
+{{ checkbox|component }}
+==
+
+settings.oneshot=yes
+settings.allow_reroll=yes
+before ==#|python|
+# Some globals variables
+import random as rd
+
+nb_attempt=0
+mult3 = ['Vrai']
+other = ['Faux']
+
+checkbox.setdata_from_rw(mult3, other, 5, rd.randint(1, 4))
+==
+evaluator==
+nb_attempt += 1
+score =  checkbox.eval()
+feedback = "ok"
+grade = (score,feedback)
+==
+
+aernfs==
+grade_checks = min([((score*100) // (nb_good+nb_bad)) , (100 // (2**nb_bad))])
+grade_attempt = 50 + (200 // (3+nb_attempt))
+grade=((grade_checks * grade_attempt) // 10000, feedback)
+==
+
+@ /utils/sandboxio.py
+@ ../../../lib/builder/before.py [builder.py]
+
+
+
+
+
+
+
+
+
+
+
