adb178028ad43204cdc3a9e82dcfe681139c710c
David DOYEN <David.Doyen@u-pem.fr>
2020-11-29 13:13:22 +0100

    ddoyen modified Yggdrasil/demo/basic/input5.pl

--- demo/basic/input5.pl
+++ demo/basic/input5.pl
@@ -24,7 +24,7 @@ L'esclave donne un livre au fils du **maître**.|domini
 delimiter = |
 
 text ==
-Traduire en latin, en utilisant le bon cas, le mot en gras.  
+Traduire en latin, en utilisant le bon cas, le mot en gras dans la phrase suivante :
 
 *{{ phrase }}*
 ==
