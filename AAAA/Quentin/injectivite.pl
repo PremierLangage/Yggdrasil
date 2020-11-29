bc4756b70ef10f60e9e663a2764b00bc5e4ceaf2
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-11-29 13:41:46 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/injectivite.pl

--- AAAA/Quentin/injectivite.pl
+++ AAAA/Quentin/injectivite.pl
@@ -19,11 +19,17 @@ questions==
 
 
 
-before ==#|python|
-# Some globals variables
-nb_attempt=0
-==
 
+evaluator==
+nb_attempt += 1
+nb_good = 0
+nb_bad = 0
+grade_checks = 0
+feedback_checks = "ok"
+grade_checks = min([((nb_good*100) // (nb_good+nb_bad)) , (100 // (2**nb_bad))])
+grade_attempt = 50 + (200 // (3+nb_attempt))
+grade=((grade_checks * grade_attempt) // 10000, feedback)
+==
 
 
 
