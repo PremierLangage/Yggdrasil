b650fd90182a6b178ac6f5faa7ffad0f582e77c8
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-29 21:55:54 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
@@ -8,14 +8,14 @@ n = 5 # Ordre du DL
 
 f1=1/(1+x)
 f2=1/(1-x)
-f3=1/(1+x**2)
+# f3=1/(1+x**2) Provoque une ereur, je ne sais pas pourquoi...
 f4=1/(1-x**2)
 
 # P = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
 # Q = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
 
 P = exp(x)
-Q = 1/(1+2*x**2)
+Q = 1/(1+x**2)
 
 
 Som = P+Q
