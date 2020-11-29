cf0445258b1e565aa058474c6ed417f72e6be354
David DOYEN <David.Doyen@u-pem.fr>
2020-11-29 01:20:26 +0100

    ddoyen modified Yggdrasil/demo/basic/input5.pl

---
+++ demo/basic/input5.pl
@@ -0,0 +1,33 @@
+extends = /model/basic/input.pl
+
+title ==
+Exemple 3
+==
+
+delimiter = |
+
+text ==
+Traduire en latin le mot en gras (avec le bon cas).  
+
+{{ phrase }}
+==
+
+sol ==
+{{ solution }}
+==
+
+data ==
+phrase|solution
+Le **maître** appelle l'esclave de son fils.|dominus
+Le maître appelle l'**esclave** de son fils.|servum
+Le maître appelle l'esclave de son **fils**.|filii
+**Esclave**, donne de la nourriture aux chevaux.|serve
+Esclave, donne de la nourriture aux **chevaux**.|equis
+Les **fils** des esclaves devenaient eux-mêmes des esclaves.|filii
+Les fils des **esclaves** devenaient eux-mêmes des esclaves.|servorum
+L'**esclave** donne un livre au fils du maître.|servus
+L'esclave donne un **livre** au fils du maître.|librum
+L'esclave donne un livre au **fils** du maître.|filio
+L'esclave donne un livre au fils du **maître**.|domini
+==
+
