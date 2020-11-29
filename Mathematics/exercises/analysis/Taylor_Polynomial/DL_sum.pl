f0dde67f65c98dbe3316fc2555baa03936d466ac
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-29 21:56:10 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
@@ -11,12 +11,8 @@ f2=1/(1-x)
 # f3=1/(1+x**2) Provoque une ereur, je ne sais pas pourquoi...
 f4=1/(1-x**2)
 
-# P = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
-# Q = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
-
-P = exp(x)
-Q = 1/(1+x**2)
-
+P = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
+Q = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
 
 Som = P+Q
 DLn = Som.series(x,0,n+1) # Attention +1 sur l'ordre (notation O par défault).
