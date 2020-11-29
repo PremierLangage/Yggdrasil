3e737c785c62e0a62a1f94b0a819b3343b6e39ae
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-29 22:08:48 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
@@ -14,8 +14,16 @@ f4=1/(1-x**2)
 P = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
 Q = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f3,f4])
 
-a = randint(-5, 5)
-b = randint(-5, 5)
+
+def my_custom_random():
+    exclude=[0]
+    randInt = randint(-5,5)
+    return my_custom_random() if randInt in exclude else randInt
+
+print(my_custom_random())
+
+a = my_custom_random()
+b = my_custom_random()
 
 Som = a*P+b*Q
 DLn = Som.series(x,0,n+1) # Attention +1 sur l'ordre (notation O par défault).
