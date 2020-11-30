0564445d01b90a0ffd3593b33a7a7570421508b0
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-30 18:51:39 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/Stoi_le_caroussel.pl

--- ComputerScience/C/examples/Stoi_le_caroussel.pl
+++ ComputerScience/C/examples/Stoi_le_caroussel.pl
@@ -36,10 +36,11 @@ nb_quest = min([int(nb_quest_voulu), nb_tot_quest])
 indices_questions = knuth_mixing( subset_index(nb_tot_quest, nb_quest) )
 
 def make_html_answer(question):
-    question["html_form"] = ""
+    question["html_form"] = '''<div style="margin-left:15%; margin-right:15%;">'''
     for it in question["items"]:
         question["html_form"] += '<input type="checkbox" id="'+it+'" name="'+it+'" value="'+it+'">'
         question["html_form"] += '<label for="'+it+'">'+it+'</label><br>'
+    question["html_form"] += '</div>'
 
 comp = []
 statement  = []
