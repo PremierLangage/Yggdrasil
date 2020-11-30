359b1cb04fb493a94d417e727e8d86e8d833ce62
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-30 20:07:58 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/Stoi_le_caroussel.pl

--- ComputerScience/C/examples/Stoi_le_caroussel.pl
+++ ComputerScience/C/examples/Stoi_le_caroussel.pl
@@ -39,7 +39,7 @@ def make_html_answer(question):
     question["html_form"] = '''<br><div style="margin-left:15%; margin-right:15%;">'''
     for it in question["items"]:
         question["html_form"] += '<input type="checkbox" id="'+it+'" name="'+it+'" value="'+it+'">'
-        question["html_form"] += '<label for="'+it+'">'+it+'</label><br>'
+        question["html_form"] += '<label for="'+it+'"> '+it+' </label><br>'
     question["html_form"] += '</div>'
 
 comp = []
