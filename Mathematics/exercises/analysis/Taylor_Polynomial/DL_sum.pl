fb88e2fe656c202f1e58bcd5c08aacb26a84d06b
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-30 09:49:38 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/DL_sum.pl
@@ -1,6 +1,6 @@
 extends = /model/mathinput.pl
 
-title = Développement limité d'une somme.
+title = Développement limité d'un produit.
 
 before ==
 var('x') # crée le symbole x
@@ -11,28 +11,11 @@ f2=1/(1-x)
 # f3=1/(1+x**2) Provoque une ereur, je ne sais pas pourquoi...
 f4=1/(1-x**2)
 
+P = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f4])
+Q = randitem([exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f4])
 
-S = [exp(x),ln(1+x),ln(1-x),cos(x),sin(x),f1,f2,f4]
-
-P = randitem(S)
-Q = randitem([i for i in S if i not in [P]])
-
-
-# La fonction ci-dessous est inutile maintenant, j'ai trouvé plus simple.
-def my_custom_random():
-    exclude=[0]
-    randInt = randint(-5,5)
-    return my_custom_random() if randInt in exclude else randInt
-
-print(my_custom_random())
-
-a = randitem([i for i in range(-5,5) if i not in [0]])
-b = randitem([i for i in range(-5,5) if i not in [0]])
-
-
-Som = a*P*b*Q
-SomS = simplify(Som)
-DLn = SomS.series(x,0,n+1) # Attention +1 sur l'ordre (notation O par défault).
+PQ = P*Q
+DLn = PQ.series(x,0,n+1) # Attention +1 sur l'ordre (notation O par défault).
 
 sol = DLn.subs(O(x**(n+1)),0) # Solution
 
@@ -41,17 +24,18 @@ sol = DLn.subs(O(x**(n+1)),0) # Solution
 
 text ==
 Calculer la partie régulière du développement limité à l'ordre $!{{ n |latex }}!$ en 0 de la fonction ci-dessous.
-$${{ Som|latex }}$$
+$${{ PQ|latex }}$$
 ==
 
 evaluator ==
-score, error = eval_poly(input.value, sol, var="x",)
+score, error = eval_poly(input.value, sol, var="x", form="expanded")
 feedback = message[error]
 ==
 
 solution ==
 La solution est $! {{ sol|latex}} !$.
 ==
+
 
 
 
