1988d7d1f52c8724bafeb8038fbd6f230ecc4908
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2020-11-30 11:33:17 +0100

    cpetitjean modified Yggdrasil/Mathematics/exercises/analysis/Taylor_Polynomial/DL_composition.pl

--- Mathematics/exercises/analysis/Taylor_Polynomial/DL_composition.pl
+++ Mathematics/exercises/analysis/Taylor_Polynomial/DL_composition.pl
@@ -43,5 +43,8 @@ La solution est $! {{ sol|latex}} !$.
 ==
 
 
+# Remarque : eval_expr(input.value, sol) est envisageable, mais l'évaluateur ne vérifie 
+#            pas si la solution de l'étudiant est donnée sous forme simplifiée.
+
 
 
