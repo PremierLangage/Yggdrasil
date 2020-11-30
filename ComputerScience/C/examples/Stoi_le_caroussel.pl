fa342ee14c0e491db5ad64b74c6136295504db9f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-30 15:49:23 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/Stoi_le_caroussel.pl

---
+++ ComputerScience/C/examples/Stoi_le_caroussel.pl
@@ -0,0 +1,43 @@
+
+
+text==
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      Wesh, pose ta question 1 bordaille ?
+    </div>
+
+    <div class="item">
+      Zyva (David), pose ta question 2 alors ?
+    </div>
+
+    <div class="item">
+      Nan, t'avais une question 3 ?
+    </div>
+
+    <div class="item">
+      4 c'est trop, JPP !
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+==
+
