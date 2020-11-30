f589f74a6aca594fc44a81c5b0b9db9da34af091
David DOYEN <David.Doyen@u-pem.fr>
2020-11-30 17:24:07 +0100

    ddoyen modified Yggdrasil/demo/basic/input_data1.pl

---
+++ demo/basic/input_data1.pl
@@ -0,0 +1,23 @@
+extends = /model/basic/input.pl
+
+title ==
+Conjugaison
+==
+
+data ==
+pronom,conjugaison
+je,suis
+tu,es
+il ou elle,est
+nous,sommes
+vous,êtes
+ils ou elles,sont
+==
+
+text ==
+Conjuguer le verbe **être** pour le pronom {{ pronom }}.
+==
+
+solution ==
+{{ conjugaison }}
+==
