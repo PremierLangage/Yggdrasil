5c5eee38ed9a30609183d9c99cbffc5581722813
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-11-30 15:56:38 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/Stoi_le_caroussel.pl

--- ComputerScience/C/examples/Stoi_le_caroussel.pl
+++ ComputerScience/C/examples/Stoi_le_caroussel.pl
@@ -17,10 +17,14 @@ text==
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
   <style>
+  .carousel-inner{
+    margin: 20px;
+  }
+
   .carousel-inner > .item > img,
   .carousel-inner > .item > a > img {
     width: 70%;
-    margin: 2%;
+    margin: auto;
   }
   </style>
 
