0d06a66cf524cb14aaf00c2e50d28470f1fc33dc
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-01 22:49:18 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/macro/expr_eval.pl

--- ComputerScience/C/exercices/macro/expr_eval.pl
+++ ComputerScience/C/exercices/macro/expr_eval.pl
@@ -1,14 +1,29 @@
-# Copyright 2016-2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2016 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Macro à paramètre avec un dièse sur argument
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
+
 title=Macro chaîne depuis argument 
-tag=macro
-extends=/ComputerScience/C/template/stdsandboxC.pl
+tag=macro|dièse|paramètre
 
 text==
-Écrire une macro EVAL prenant un seul paramètre qui sera une
+
+Écrire une macro **EVAL** prenant un seul paramètre qui sera une
 expression C s'évaluant dans les entiers.
 
 En imaginant que cette expression soit `12*2`, votre macro devra
@@ -16,24 +31,24 @@ afficher sur la sortie standard
 `12*2 = 24`
 et **retourner à la ligne** juste derrière le dernier entier. 
     
-Pour rappel, les arguments des macros ne sont pas typé. Le
+Pour rappel, les arguments des macros ne sont pas typés. Le
 préprocesseur ne fait que des inclusions et subistitutions. Lorsqu'une
 macro prend un paramètre **a**, on peut obtenir une chaine de caractère C
 à partir de **a** en utilisant **#a** dans le corps de la macro.
 ==
 
-editor.code==
+editor.code==#|c|
 #define EVAL....
-
 ==
 
-solution==
+solution==#|c|
 #define EVAL(e) printf("%s = %d\n", #e, e)
-
 ==
 
-codeafter==
- 
+code_before==#|c|
+==
+
+code_after==#|c|
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
@@ -50,12 +65,12 @@ int main(int argc, char* argv[]){
   EVAL(153-89);
   return 0;
 }
-
 ==
 
-tests==
-[["quelques expressions", "",""]] 
+checks_args_stdin==#|python|
+[ ["Quelques expressions", "",""] ] 
 ==
+
 
 
 
