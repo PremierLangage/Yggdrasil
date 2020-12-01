47c668643adec69e71931d5ba9249e62f2834d84
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-01 21:48:46 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/macro/square.pl

--- ComputerScience/C/exercices/macro/square.pl
+++ ComputerScience/C/exercices/macro/square.pl
@@ -1,11 +1,25 @@
-# Copyright 2016-2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2016 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Macro à paramètre pour calculer un carré
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
+
 title=Macro pour calculer un carré
-tag=macro
-extends=/ComputerScience/C/template/stdsandboxC.pl
+tag=macro|carré|piège|parenthèse|priorité
 
 text==
 Écrire une macro **SQUARE** qui prend en argument un seul paramètre
@@ -45,5 +59,6 @@ int main(int argc, char* argv[]){
 tests==
 [ ["expressions classiques", "",""] ]
 ==
+
 
 
