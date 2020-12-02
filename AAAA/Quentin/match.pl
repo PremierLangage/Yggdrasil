f7d7a45c4a7cceabe066fd6de7156417a3f1cbdc
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 20:52:39 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/match.pl

--- AAAA/Quentin/match.pl
+++ AAAA/Quentin/match.pl
@@ -4,8 +4,8 @@ extends= simplematchlist.pl
 
 before==
 
-gf=[ f"$![0,\pi]!$", f"$![0,\frac{\pi}{2}]!$",f"$![\frac{\pi}{3},\frac{2\pi}{3}]!$",f"$!\{\pi\}!$",f"$!\{ 0 \}!$" ]
-df=[  f"$![-1,1]!$",f"$![0,1]!$",f"$![-\frac12 , \frac12] !$", f"$! \{-1\}!$",f"$!\{-1\}!$"]
+gf=[ f"$![0,\pi]!$", f"$![0,\frac{\pi}{2}]!$",f"$![\frac{\pi}{3},\frac{2\pi}{3}]!$",f"$!{\pi}!$",f"$!{ 0 }!$" ]
+df=[  f"$![-1,1]!$",f"$![0,1]!$",f"$![-\frac12 , \frac12] !$", f"$! {-1}!$",f"$!{-1}!$"]
 
 pairs= [ TT for TT in zip(gf,df)]
 
