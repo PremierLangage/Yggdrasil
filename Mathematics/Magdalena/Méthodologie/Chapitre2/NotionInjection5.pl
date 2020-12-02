ff7f5438d0865c01f2de0cb55a45f136ba877d12
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-12-02 10:32:02 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/NotionInjection5.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/NotionInjection5.pl
@@ -0,0 +1,11 @@
+extends = /model/checkbox/goodbad.pl
+text= Un enseignant demande à un étudiant de définir la notion d'injectivité. Voici la réponse apportée par l'étudiant : "$%f:E \to F%$ est injective si  $% x,x'\in E,  f(x)=f(x') \implies x=x'%$".
+good==
+Les variables $%x,x'%$ ne sont pas bien quantifiée.
+Ce n'est pas la définition de l'injectivité.
+==
+bad==
+C'est bien la définition de l'injectivité.
+C'est la définition de la surjectivité.
+==
+
