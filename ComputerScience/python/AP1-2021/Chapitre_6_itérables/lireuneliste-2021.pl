b5c95a973861b4a1d1c510f101a967f23d51c834
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-02 12:32:13 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl
@@ -34,7 +34,7 @@ print(input().split())
 before==
 l = ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 from random import randint
-mplsoluce0=" ".join([l[randint(0,len(l)-1)] for j in range(randint(3,7))])
+plsoluce0=" ".join([l[randint(0,len(l)-1)] for j in range(randint(3,7))])
 
 after = before
 ==
