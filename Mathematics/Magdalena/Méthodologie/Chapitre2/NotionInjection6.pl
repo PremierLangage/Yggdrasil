a799244e74cbb669064750c3837004cd0031d094
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-12-02 11:02:02 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/NotionInjection6.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/NotionInjection6.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/NotionInjection6.pl
@@ -2,7 +2,7 @@ extends = /model/checkbox/goodbad.pl
 text= Un enseignant demande à un étudiant de définir la notion d'injectivité. Voici la réponse apportée par l'étudiant : "$%f:E \to F%$ est injective si  $% \forall x,x'\in E,  f(x)=f(x')%$ donc $% x=x'%$".
 good==
 L'énoncé est incorrect car la conjonction "donc" n'est pas synonyme de "$% \implies%$"
-==
+== 
 bad== 
 C'est bien la définition de l'injectivité.
 C'est la définition de la surjectivité.
