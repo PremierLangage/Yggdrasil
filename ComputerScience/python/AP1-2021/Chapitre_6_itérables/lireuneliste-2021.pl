484e739f61026edf9f3500218477dbb05357017b
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-02 12:16:36 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl
@@ -6,15 +6,18 @@ tag=list|input|for
 tog=listinput
 extends=/ComputerScience/python/template/soluce.pl
 text==
-Cet exercice consiste à lire une liste depuis du clavier. Nous allons supposer que notre utilisateur va entrez une seule ligne de texte avec un espace entre chaque valeur de la liste par exemple :
+Cet exercice consiste à lire une liste depuis du clavier. Nous allons supposer que l'utilisateur va entrez une seule ligne de texte,
+avec un espace entre chaque valeur de la liste par exemple :
 
 	un deux 3 quatre fin
+
 ce qui vas nous permettre de creer la liste:
 
-	['un','deux','3','quatre','fin']
-pour cella il suffit de lire la ligne avec **input** puis la découper avec **split** qui créer une liste exactement comme nous le souhaitons.
+	``['un','deux','3','quatre','fin']``
 
-Pour valider l'exercice afficher la liste lue avec **print**.
+Pour cela il suffit de lire la ligne avec **input** puis la découper avec **split** qui créer une liste exactement comme nous le souhaitons.
+
+Pour valider l'exercice, affichez la liste lue avec **print**. Il n'est **pas nécessaire** de créer une fonction.
 ==
 
 soluce==
@@ -41,5 +44,6 @@ par exemple :
 	['Une', 'chaine', 'de', 'caractères', '<-ah', "j'ai", 'mis', 'deux', 'espaces']
 
 ==
+
 
 
