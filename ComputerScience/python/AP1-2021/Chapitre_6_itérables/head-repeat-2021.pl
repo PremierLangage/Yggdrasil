171bae6aed71fcde21d63f14e04cf2334dc5d6e3
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-02 17:40:52 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-repeat-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-repeat-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-repeat-2021.pl
@@ -7,16 +7,29 @@ author=Wenjie Fang
 Title=Répétition au début
 
 text==
-Prenons une chaîne de caractères. Il
+Prenons une chaîne de caractères. Il peut y avoir des motifs répétitifs au début de la chaîne.
+
+Par exemple, la chaîne ``'pikapikachu'`` a le motif ``'pika'`` qui se répète deux fois au début.
+
+Cependant, le motif le plus court n'est pas forcément celui qui se répète le plus.
+Par exemple, dans ``'ananas-ananas-ananas-est-bien'``, même si ``'an'`` est le plus court motif qui se répète plus qu'une fois,
+le motif ``'ananas-'`` se répète le plus (trois fois).
 
 Ecrire une fonction ``rep_debut(s)`` qui prendre un paramètre ``s``, qui est une chaîne de caractères,
-et qui renvoie le nombre max
+et qui renvoie le motif qui se répète le plus de fois. En cas d'égalité, on en prend le plus grand.
+S'il n'y a pas de répétition, on renvoie ``s``.
 ==
 
 tag=
 
 samplesol==
-
+def rep_debut(s):
+    n = len(s)
+    max = 0
+    for i in range(n):
+        motif = s[:i]
+        cnt = 1
+        while cnt * i < n and 
 ==
 
 before==
@@ -26,4 +39,5 @@ before==
 pltest0==
 
 ==
+
 
