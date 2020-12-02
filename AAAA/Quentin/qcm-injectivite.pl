1f702f2d9ea64bce22f8e6682de20e07e5527d51
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 21:15:58 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/qcm-injectivite.pl

--- AAAA/Quentin/qcm-injectivite.pl
+++ AAAA/Quentin/qcm-injectivite.pl
@@ -15,15 +15,15 @@ questions==
 * Soient $!E!$ et $!F!$ deux ensembles. On dit que l'application $!f:E \to F!$ est injective si:
 + $! \forall x,x' \in E, \quad ( f(x)=f(x') \implies x=x')!$
 -  $! \forall x,x' \in E, \quad ( x=x' \implies f(x)=f(x'))!$
--  $! \exists y \in F, \quad \forall x \in E, f(x)=y!$
--  $! \forall y \in F, \quad \exists x \in E, f(x)=y!$
+-  $! \exists y \in F, \quad \forall x \in E, \quad  f(x)=y!$
+-  $! \forall y \in F, \quad \exists x \in E,  \quad f(x)=y!$
 
 
 * Soient $!E!$ et $!F!$ deux ensembles. On dit que l'application $!f:E \to F!$ est surjective si:
 - $! \forall x,x' \in E, \quad ( f(x)=f(x') \implies x=x')!$
 -  $! \forall x,x' \in E, \quad ( x=x' \implies f(x)=f(x'))!$
--  $! \exists y \in F, \quad \forall x \in E, f(x)=y!$
-+  $! \forall y \in F, \quad \exists x \in E, f(x)=y!$
+-  $! \exists y \in F, \quad \forall x \in E,\quad  f(x)=y!$
++  $! \forall y \in F, \quad \exists x \in E,\quad  f(x)=y!$
 
 
 * Soit $!f:\mathbb R \to \mathbb R!$ une fonction paire, c'est-à-dire que $!f(x)=f(-x)!$ pour tout $! x \in \mathbb R!$. La fonction $!f!$ peut-elle être injective ? 
