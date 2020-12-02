055420efe772fdc272ea890d0d69a11b26959f78
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 21:27:38 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/en-quete-de-sens.pl

---
+++ AAAA/Quentin/en-quete-de-sens.pl
@@ -0,0 +1,58 @@
+
+extends = /model/AMC_TXT.pl
+author = Quentin
+title= En quête de sens ...
+questions==
+
+
+* Soient $!E!$ et $!F!$ deux ensembles et soit $!f:E \to F!$ une application. Soient $!A\subset E!$ et $!B \subset F!$. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
+-$!f(A) \subset E!$
++ $!\forall y \in B, \quad !f^{(-1)}(y) \in A_1!$
+- $! A \subset f^{(-1)}(B) !$
+- $! f^{(-1)}(B) = \emptyset !$
+
+
+* On considère l'application $%f : \\left \\{1, 2, 3, 4 \\right \\} \to \\left \\{1, 2, 3, 4\\right \\}%$ définie par
+$!f (1) = 2, f (2) = 3, f (3) = 4, f (4) = 2.!$ Quelle est la bonne réponse ?
+- $!f^{(-1)}(\\{2\\})=\\{1\\}!$
+- $!f^{(-1)}(\\{2\\})=\\{3\\}!$
+- $!f^{(-1)}(\\{2\\})=\\{4\\}!$
++ $!f^{(-1)}(\\{2\\})=\\{1,4\\}!$ 
+
+* On considère l'application $!f : \mathbb R \to \mathbb R!$ définie par
+$!\forall x \in \mathbb  R, \; f (x) = x^2 + 1.!$
+Quelle est la bonne réponse ?
+-$!f(\mathbb R)=\mathbb R!$
+-$!f(\mathbb R)=[0,+\infty[!$
+-$!f(\mathbb R)=]1,+\infty[!$
++$!f(\mathbb R)=[1,+\infty[!$
+
+* On considère l’application $!f : \mathbb R \times \mathbb R \to \mathbb R!$ définie par
+$!f (x, y) = x^2 + y^2.!$
+Quelle est la bonne réponse ?
+- $! f^{(-1)}(\\{1\\}) = \\{(1, 0)\\}  !$
+- $! f^{(-1)}(\\{0\\}) = \\{(0, 1)\\}  !$
++ $! f^{(-1)}(\\{1\\}) !$ est le cercle de rayon 1 et de centre $!(0,0)!$
+
+==
+
+
+
+
+evaluator==
+nb_attempt += 1
+nb_good = 0
+nb_bad = 0
+grade_checks = 0
+feedback_checks = "ok"
+grade_checks = min([((nb_good*100) // (nb_good+nb_bad)) , (100 // (2**nb_bad))])
+grade_attempt = 50 + (200 // (3+nb_attempt))
+grade=((grade_checks * grade_attempt) // 10000, feedback)
+==
+
+
+
+
+
+
+
