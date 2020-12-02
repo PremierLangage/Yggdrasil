bc2eb1c7c315c6d031d0c217ab0b34595ec61898
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-12-02 10:14:33 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/NotionInjection1.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/NotionInjection1.pl
@@ -0,0 +1,12 @@
+extends = /model/checkbox/goodbad.pl
+text= Un enseignant demande à un étudiant de définir la notion d'injectivité. Voici la réponse apportée par l'étudiant : $!"f:E \to F!$ est injective signifie que deux éléments différents de  $%E%$ ne peuvent avoir la même image par $%f."%$ Cochez les bonnes réponses. 
+good==# une chaine multiple avec une bonne solution par ligne 
+C'est bien une définition de l'injectivité.
+C'est une définition insuffisament formalisée.
+==
+bad==
+C'est bien une définition mais elle est fausse. 
+C'est la définition de "$%f%$ est une application". 
+==
+
+
