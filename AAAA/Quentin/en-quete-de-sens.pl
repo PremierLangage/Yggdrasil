a4d9bb94562f24bd590d09cbaa3cae778521b27c
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 22:19:06 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/en-quete-de-sens.pl

--- AAAA/Quentin/en-quete-de-sens.pl
+++ AAAA/Quentin/en-quete-de-sens.pl
@@ -1,6 +1,6 @@
 
 extends = /model/AMC_TXT.pl
-author = Quentin
+author = Enseignants Méthodologie
 title= En quête de sens ...
 questions==
 
@@ -43,6 +43,7 @@ grade_checks = min([((nb_good*100) // (nb_good+nb_bad)) , (100 // (2**nb_bad))])
 grade_attempt = 50 + (200 // (3+nb_attempt))
 grade=((grade_checks * grade_attempt) // 10000, feedback)
 ==
+
 
 
 
