4c8a96ba5920b4945ebd3754314bd4e5dfd66f51
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-12-02 11:29:38 +0100

    mkobylanski modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/AssociationPrédicat2.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/AssociationPrédicat2.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/AssociationPrédicat2.pl
@@ -8,7 +8,7 @@ $%\exists a \in E \quad \forall b \in E \quad  P(a,b)%$§ Un étudiant au moins 
 $%\exists a \in E \quad \forall b \in E \quad  P(b,a)%$§ Au moins un des murs a été regardé par tous les étudiants.
 $%\forall a \in E \quad \forall b \in E \quad  P(a,b)%$§ Tous les étudiants ont regardés tous les murs.
 $%\forall a \in E \quad \exists  b \in E \quad  P(b,a)%$§ Chaque mur a été regardé par au moins un étudiant.
-$%\exists a \in E \quad \forall b \in E \quad  {rm non}P(a,b)%$§ Un étudiant au moins n'a regardé aucun mur.
+$%\exists a \in E \quad \forall b \in E\quad %$ non $%P(a,b)%$§ Un étudiant au moins n'a regardé aucun mur.
 ==
 
 text = Soit $%E%$ l'ensemble des étudiants de la promotion. Le prédicat $% P(a,b) %$ signifie que $%a%$ a regardé le mur de $%b%$. Associer les propositions suivantes avec leur signification en français.
