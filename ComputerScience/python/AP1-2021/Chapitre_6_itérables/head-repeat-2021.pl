20b8c943af2264cb2977161d985814c724850873
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-02 13:10:52 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-repeat-2021.pl

---
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-repeat-2021.pl
@@ -0,0 +1,25 @@
+# Author: Wenjie Fang
+
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+builder=/builder/before.py
+
+author=Wenjie Fang
+Title=Répétition au début
+
+text==
+
+==
+
+tag=
+
+samplesol==
+
+==
+
+before==
+
+==
+
+pltest0==
+
+==
