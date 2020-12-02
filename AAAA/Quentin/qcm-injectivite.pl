58c6f9cae95317cbd54234ec3ef8f02ae23fe08d
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 21:15:02 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/qcm-injectivite.pl

--- AAAA/Quentin/qcm-injectivite.pl
+++ AAAA/Quentin/qcm-injectivite.pl
@@ -12,6 +12,12 @@ questions==
 
 
 
+* Soient $!E!$ et $!F!$ deux ensembles. On dit que l'application $!f:E \to F!$ est injective si:
++ $! \forall x,x' \in E, \quad ( f(x)=f(x') \implies x=x'!$
+-  $! \forall x,x' \in E, \quad ( x=x' \implies f(x)=f(x')!$
+-  $! \exists y \in F, \quad \forall x \in E, f(x)=y!$
+-  $! \forall y \in F, \quad \exists x \in E, f(x)=y!$
+
 * Soit $!f:\mathbb R \to \mathbb R!$ une fonction paire, c'est-à-dire que $!f(x)=f(-x)!$ pour tout $! x \in \mathbb R!$. La fonction $!f!$ peut-elle être injective ? 
 + Non
 - Oui 
