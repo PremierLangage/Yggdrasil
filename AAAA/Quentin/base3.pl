fab1f12394e021b426de53330b8867cfcd24dc82
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 22:08:23 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/base3.pl

---
+++ AAAA/Quentin/base3.pl
@@ -0,0 +1,30 @@
+
+
+
+extends= /model/qcm/qcm.pl
+
+
+title= Base 1/4
+
+
+image =$ /AAAA/Quentin/Images/nsur-ninj.png
+text==
+On considère la fonction $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
+![alt image]({{image}})
+==
+
+nb=4
+nbtrues=0
+
+good==
+==
+
+bad==
+f est surjective
+f est injective
+f est bijective
+f n'est pas une application
+==
+
+
+
