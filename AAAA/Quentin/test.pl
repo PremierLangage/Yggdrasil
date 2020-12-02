b01b12147ccec9d51db4c7bda82ab55b907aa715
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 21:56:27 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/test.pl

--- AAAA/Quentin/test.pl
+++ AAAA/Quentin/test.pl
@@ -1,29 +1,77 @@
-extends = /model/basic/vraifaux.pl
 
-title== 
-Un exemple de proposition vraie ou fausse 
+extends = /model/AMC_TXT.pl
+title = Calcul - puissances 10
+questions==
+
+** Cocher les bonnes réponses. $$10^{-3}=...$$ 
+![alt image]({{image}})
++ $%0,001%$
+- $% (-10)\times (-10) \times (-10) %$
+- $% -30%$
++ $% \frac{1}{10^3} %$
++ $% \frac{1}{10} \times\frac{1}{10}\times  \frac{1}{10} %$
+
+** Cocher les bonnes réponses. $$ \frac{1}{10^{-2}}=...$$
+- $%0,01%$
++ $% 100%$
++ $% 10^2 %$
+- $% 0,02%$
+
+
+** Cocher les bonnes réponses. $$\frac{10^5 \times 10^{-2}}{10^2}=...$$
+- $% 10^5 %$
++ $% 10^{5-2-2} %$
++  $%10%$
+- $% 10^{5\times (-2) : 2} %$
+- $%0,1%$
++ $% 10^1 %$
+
+ 
+
+** Cocher les bonnes réponses. $$10^0 =...$$
+- $% 10 %$
+- $% 0 %$
++ $% 1%$
+
+** Cocher les bonnes réponses. $$0,00001\times 10^5=..$$
++ $%1 %$
+- $% 10%$
+-  $%0,1%$
++ $% 10^{-5}\times 10^{5}%$
++ $% 10^{-5+5}%$
+
+** Cocher les bonnes réponses $$5^8 \times 2^8 =...$$
++ $% (5\times 2)^8 %$
+- $% (5\times 2)^{16} %$
++ $% 10^8 %$
+
+** Cocher les bonnes réponses. $$(10^{3})^2\times 5^{-3}\times 2^{-3} =...$$
+- $% 10^{3\times 3} \times (5\times 2)^{-6} %$
++ $% 10^{3 \times 2} \times (5\times 2)^{-3} %$  
+- $% 10^5 %$
++ $% 10^3%$
++ $% 1000 %$
+
+
+** Cocher les bonnes réponses. $$ 2^5 \times (5^2)^3=...$$
+- $% (2\times 5^2)^{5+3}%$
++ $% 2^5 \times 5^6  %$
+- $% (2\times 5)^{5+6}%$
++ $% 2\times 2 \times 2\times 2\times 2 \times 5^2 \times 5^2 \times 5^2%$
++ $% (2\times 5)^5 \times 5%$
++ $% 5 \times 10^5 %$
+
+
+
 ==
 
-text==
-Le cheval blanc de Henri IV est il blanc ? 
-==
-
-
-answer=Non
-
-
-# pour aligner les propositions 
-horizontal % true 
-
-feedback_correct ==
-Bien vu il est gris car il s'est roulé dans la poussière.  
-<img src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Lol_LOL.png">
-==
-feedback_wrong = Il vous manque des informations.
-general_feedback==
 
 
 
-Bien entendu c'est une question piège.
-==
+
+
+
+
+
+
 
