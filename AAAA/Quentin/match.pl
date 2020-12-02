52970a24672f61181e847bd3894a3871a494a271
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 20:31:46 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/match.pl

--- AAAA/Quentin/match.pl
+++ AAAA/Quentin/match.pl
@@ -6,9 +6,8 @@ before==
 
 var('x')
 
-f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), exp(x),127]
-gf=[ f"$!{latex(fi)}!$" for fi in f]
-df=[  f"$!{latex(diff(U,x))}!$" for U in f]
+gf=[ $![0,\pi]!$, $![0,\frac{\pi}{2}!]$,$![\frac{\pi}{3},\frac{2\pi}{3}]!$,$!\\{\pi\\}!$,$!\\{ 0 \\}!$ ]
+df=[  $![-1,1]!$,$![0,1]!$,$![-\frac12 , \frac12] !$, $! \\{-1\\}!$,$!\\{-1\\}!$]
 
 pairs= [ TT for TT in zip(gf,df)]
 
