22b0bf01cbbad62f5c5422dc50226f56ac0d6a1c
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 20:08:21 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/qcm-image-directe-reciproque.pl

--- AAAA/Quentin/qcm-image-directe-reciproque.pl
+++ AAAA/Quentin/qcm-image-directe-reciproque.pl
@@ -28,9 +28,13 @@ Quelle est la bonne réponse ?
 -$!f(\mathbb R)=]1,+\infty[!$
 +$!f(\mathbb R)=[1,+\infty[!$
 
-* La fonction $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle surjective ?  
-+ Non
-- Oui 
+* On considère l’application $!f : \mathbb R \times \mathbb R \to \mathbb R!$ définie par
+$!f (x, y) = x^2 + y^2.!$
+Quelles sont les bonnes réponses ?
+- $! f^{(-1)}(\{0\}) = \{(0, 0)\}  !$
+- $! f^{(-1)}(\{1\}) = \{(1, 0)\}  !$
+- $! f^{(-1)}(\{0\}) = \{(0, 1)\}  !$
+- $! f^{(-1)}(\{1\}) !$ est le cercle de rayon 1 et de centre $!(0,0)!$
 
 ==
 
