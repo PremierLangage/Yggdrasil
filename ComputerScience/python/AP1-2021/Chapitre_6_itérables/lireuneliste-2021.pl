c5601348e1d7c8b7d0259c42546b7f8798de74a6
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-02 12:49:06 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/lireuneliste-2021.pl
@@ -31,17 +31,13 @@ print(input().split())
 ==
 
 before==#|python|
-==
-
-inputgenerator==
-l=['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+l = ['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
 from random import randint
-print(" ".join([ l[randint(0,len(l)-1)] for j in range(randint(3,7))]))
+randl = [l[randint(0,len(l)-1)] for j in range(randint(3,7))]
+rands = " ".joint(randl)
+plsoluce0 = rands
+after = before
 ==
-
-showinput=True
-
-nomberofgenerator=1
 
 feedback==
 Retenez bien ce code :
