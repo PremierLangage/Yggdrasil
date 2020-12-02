69b3ad0927e957227c2fd17b9f81290d8148ccc0
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-02 20:40:10 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/str-occu-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_6_itérables/str-occu-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/str-occu-2021.pl
@@ -9,7 +9,7 @@ title=Trouver tous les occurrences
 text==
 Un mot peut apparaître plusieures fois dans une phrase, et on veut repérer tous ses occurrences.
 
-Ecrire une fonction ``occurrences(phrase, mot)`` qui retourne une liste des indices où ``mot`` apparaît tel quel dans ``phrase``. La liste retournée doit être en ordre croissant. 
+Ecrire une fonction ``occurrences(phrase, mot)`` qui retourne une liste des indices où ``mot`` apparaît tel quel dans ``phrase``. La liste retournée doit être en ordre croissant. Si ``mot`` est vide, alors on retourne une liste vide.
 
 Par exemple, avec ``phrase = 'Le chat et le chien mangent le cadeau.'`` et ``mot = 'le'``, l'appel ``occurrences(phrase, mot)`` doit retourner la liste ``[11, 28]``. Même si ``phrase`` commence par le mot "Le", ce n'est pas de la bonne casse.
 ==
@@ -18,6 +18,8 @@ tag=find|algo
 
 samplesol==
 def occurrences(p, m):
+    if m == '':
+        return []
     l = []
     ind = p.find(m)
     while ind >= 0:
