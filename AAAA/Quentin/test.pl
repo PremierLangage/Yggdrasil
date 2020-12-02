35446c45030918acd1af3e618d3dfbb81e092210
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 22:02:12 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/test.pl

--- AAAA/Quentin/test.pl
+++ AAAA/Quentin/test.pl
@@ -1,81 +1,31 @@
 
-extends = /model/AMC_TXT.pl
-title = Calcul - puissances 10
-
-questions==
-
-** Cocher les bonnes réponses. $$10^{-3}=...$$ 
-![alt image]({{bij}})
-+ $%0,001%$
-- $% (-10)\times (-10) \times (-10) %$
-- $% -30%$
-+ $% \frac{1}{10^3} %$
-+ $% \frac{1}{10} \times\frac{1}{10}\times  \frac{1}{10} %$
-
-** Cocher les bonnes réponses. $$ \frac{1}{10^{-2}}=...$$
-- $%0,01%$
-+ $% 100%$
-+ $% 10^2 %$
-- $% 0,02%$
+extends= /model/qcm/qcm.pl
 
 
-** Cocher les bonnes réponses. $$\frac{10^5 \times 10^{-2}}{10^2}=...$$
-- $% 10^5 %$
-+ $% 10^{5-2-2} %$
-+  $%10%$
-- $% 10^{5\times (-2) : 2} %$
-- $%0,1%$
-+ $% 10^1 %$
+title= Base 1/4
 
- 
+bbo="Une chaine"
 
-** Cocher les bonnes réponses. $$10^0 =...$$
-- $% 10 %$
-- $% 0 %$
-+ $% 1%$
+text==
+Choisir la bonne reponse :{{bbo}}
+==
 
-** Cocher les bonnes réponses. $$0,00001\times 10^5=..$$
-+ $%1 %$
-- $% 10%$
--  $%0,1%$
-+ $% 10^{-5}\times 10^{5}%$
-+ $% 10^{-5+5}%$
+nb=4
+nbtrues=1
 
-** Cocher les bonnes réponses $$5^8 \times 2^8 =...$$
-+ $% (5\times 2)^8 %$
-- $% (5\times 2)^{16} %$
-+ $% 10^8 %$
-
-** Cocher les bonnes réponses. $$(10^{3})^2\times 5^{-3}\times 2^{-3} =...$$
-- $% 10^{3\times 3} \times (5\times 2)^{-6} %$
-+ $% 10^{3 \times 2} \times (5\times 2)^{-3} %$  
-- $% 10^5 %$
-+ $% 10^3%$
-+ $% 1000 %$
-
-
-** Cocher les bonnes réponses. $$ 2^5 \times (5^2)^3=...$$
-- $% (2\times 5^2)^{5+3}%$
-+ $% 2^5 \times 5^6  %$
-- $% (2\times 5)^{5+6}%$
-+ $% 2\times 2 \times 2\times 2\times 2 \times 5^2 \times 5^2 \times 5^2%$
-+ $% (2\times 5)^5 \times 5%$
-+ $% 5 \times 10^5 %$
-
-
+good==
+bon
+bien
+ok
+cool
 
 ==
 
-
-bij =$ /AAAA/Quentin/bij.png
-
-
-
-
-
-
-
-
+bad==
+bad
+pas bon
+beurk
+==
 
 
 
