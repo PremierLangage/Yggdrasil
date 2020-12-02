9775fba501ac83254abc3c80cf7a8c9e8086bab7
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-02 20:28:14 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/match.pl

---
+++ AAAA/Quentin/match.pl
@@ -0,0 +1,27 @@
+
+extends= /model/math.pl
+extends= simplematchlist.pl
+
+before==
+
+var('x')
+
+f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), exp(x),127]
+gf=[ f"$!{latex(fi)}!$" for fi in f]
+df=[  f"$!{latex(diff(U,x))}!$" for U in f]
+
+pairs= [ TT for TT in zip(gf,df)]
+
+if type(pairs)==list:
+    matchlist.setdata_from_matches(pairs)
+else:
+    matchlist.setdata_from_matches([ l.split("§") for l in  pairs.splitlines() if "§" in l ])
+
+==
+
+text==
+Associez à chaque ensemble de départ $!E!$ un ensemble d'arrivé $!F!$ de façon à ce que $!cos:E\to F!$ soit bijective.
+==
+
+
+
