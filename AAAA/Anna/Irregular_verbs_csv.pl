c5b221e2046fbc0ad4e6d97af336310bbe3656c9
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 13:32:41 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Irregular_verbs_csv.pl

--- AAAA/Anna/Irregular_verbs_csv.pl
+++ AAAA/Anna/Irregular_verbs_csv.pl
@@ -7,3 +7,19 @@ You need to validate 20 preterit form to win
 
 Give the preterite of the verb **to {{ verb }} **
 ==
+
+before ==
+import random as rd
+import csv
+if rd.randint(1,2) == 1 :
+    filename= "data1"
+else:
+    filename= "data"
+
+with open(filename, newline='') as file:
+    all_rows = list(csv.DictReader(file, delimiter=','))
+    
+sample_rows = rd.sample(all_rows, 5)
+
+matchlist.setdata_from_matches([(row['V'], row['V-ED']) for row in sample_rows])
+==
