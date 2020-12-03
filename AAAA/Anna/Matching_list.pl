d8279f2bedc9b2964eef5096f3e507bdba69f544
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 14:25:36 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Matching_list.pl

--- AAAA/Anna/Matching_list.pl
+++ AAAA/Anna/Matching_list.pl
@@ -8,10 +8,16 @@ before==
 
 var('x')
 
-verbs=["to meet", "to know", "to put", "to see", "to give", "to say"]
-pairs=["met", "knew", "put", "saw","gave", "said"]
+f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), exp(x),127]
+gf=[ f"$!{latex(fi)}!$" for fi in f]
+df=[  f"$!{latex(diff(U,x))}!$" for U in f]
 
-matchlist.setdata_from_matches(pairs)
+pairs= [ TT for TT in zip(gf,df)]
+
+if type(pairs)==list:
+    matchlist.setdata_from_matches(pairs)
+else:
+    matchlist.setdata_from_matches([ l.split("§") for l in  pairs.splitlines() if "§" in l ])
 
 ==
 
