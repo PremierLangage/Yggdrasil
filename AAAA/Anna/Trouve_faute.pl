ad531f80dc7ce3e1506968674b7efc5680a4f2ce
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 12:55:48 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Trouve_faute.pl

--- AAAA/Anna/Trouve_faute.pl
+++ AAAA/Anna/Trouve_faute.pl
@@ -20,5 +20,20 @@ consecutive_to_validate = 4
 recall_rule = True
 
 doc==
+On importe le fichier .txt où se trouvent la règle en question, sa description et les exercices.
+On met le nom du/des fichiers dans la balise file_liste entre crochet []
+
+Constitution du fichier .txt :
+->règle : Enoncé de la règle
+-> description : description de la règle
+-> Pour chaque phrase à corriger, on définit : 
+    - mauvais : la phrase avec les fautes   
+    - bon : la phrase correcte 
+    - explication : explication détaillée de la correction
+
+La balise consecutive_to_validate permet de définir le nombre de réponses justes consécutives autorisées
+pour valider l'exercice. 
+
+La balise recall_rule permet de définir si on rappelle la règle en début d'exercice (True) ou pas (False) 
 
 ==
