06ddd43a8ae44a6ebe50c9a3fc7db47cb297293e
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 11:50:28 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Trouve_faute.pl

--- AAAA/Anna/Trouve_faute.pl
+++ AAAA/Anna/Trouve_faute.pl
@@ -1,2 +1,10 @@
 extends = /Languages/French/Orthographe/Trouve_faute_template.pl
 
+title==
+Fautes d'Orthographe    
+==
+
+text ==
+Trouver les fautes d'orthographe dans les phrases suivantes ou cliquer sur correct si vous 
+trouvez qu'elles sont correctes
+==
