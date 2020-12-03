35ab4e3008d8a8992cf34a53d3055008c6a4a9c9
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 11:27:31 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Matching_list.pl

--- AAAA/Anna/Matching_list.pl
+++ AAAA/Anna/Matching_list.pl
@@ -8,9 +8,9 @@ before==
 
 var('x')
 
-f=[to meet, to know, to put, to see, to give, to say]
-gf=[met, knew, put, saw]
-df=[gave, said]
+f=["to meet", "to know", "to put", "to see", "to give", "to say"]
+gf=["met", "knew", "put", "saw"]
+df=["gave", "said"]
 
 pairs= [ TT for TT in zip(gf,df)]
 
