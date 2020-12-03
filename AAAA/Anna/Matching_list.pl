367529bc650a1d0a16bfdebec6eb33acca31fa01
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 09:59:05 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Matching_list.pl

--- AAAA/Anna/Matching_list.pl
+++ AAAA/Anna/Matching_list.pl
@@ -1,2 +1,30 @@
 extends= /AAAA/dominique/mathmatchlist.pl
 
+before==
+
+var('x')
+
+f=[sin(x),cos(x),x**2,x**3, log(x)/log(2), exp(x),127]
+gf=[ f"$!{latex(fi)}!$" for fi in f]
+df=[  f"$!{latex(diff(U,x))}!$" for U in f]
+
+pairs= [ TT for TT in zip(gf,df)]
+
+if type(pairs)==list:
+    matchlist.setdata_from_matches(pairs)
+else:
+    matchlist.setdata_from_matches([ l.split("§") for l in  pairs.splitlines() if "§" in l ])
+
+==
+
+text==
+Associez chaque fonction à sa dérivée.
+==
+
+doc ==
+Exos à choix unique où on doit relier chaque élèment de la première case
+à un élèment de la seconde case. 
+Code couleurs : Après validation de la réponse 
+-> Flèches vertes = réponses justes
+-> Flèches rouges = réponses fausses
+==
