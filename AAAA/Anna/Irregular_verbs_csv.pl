9ac21e662defe13f94051e916309a8ff71044a32
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 13:32:03 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Irregular_verbs_csv.pl

---
+++ AAAA/Anna/Irregular_verbs_csv.pl
@@ -0,0 +1,9 @@
+extends= /Languages/English/irregular_verbs.pl
+
+title=Irregular verbs
+
+text== 
+You need to validate 20 preterit form to win 
+
+Give the preterite of the verb **to {{ verb }} **
+==
