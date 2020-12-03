b04692bf8669c086efaaeae0c1bf8dee01dc8985
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 10:23:15 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Matching_csv.pl

--- AAAA/Anna/Matching_csv.pl
+++ AAAA/Anna/Matching_csv.pl
@@ -6,3 +6,20 @@ Matching List
 
 @ /AAAA/Anna/Irregular_verbs.csv [data]
 @ /AAAA/Anna/Irregular_verbs2.csv [data1]
+
+before ==
+import random as rd
+import csv
+if rd.randint(1,2) == 1 :
+    filename= "data1"
+else:
+    filename= "data"
+
+with open(filename, newline='') as file:
+    all_rows = list(csv.DictReader(file, delimiter=','))
+    
+sample_rows = rd.sample(all_rows, 5)
+
+matchlist.setdata_from_matches([(row['nom'], row['dates']) for row in sample_rows])
+==
+
