2d869ab530992e3c25ef7ee6d8ee87961a0a963e
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 11:12:42 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Matching_csv.pl

--- AAAA/Anna/Matching_csv.pl
+++ AAAA/Anna/Matching_csv.pl
@@ -27,3 +27,19 @@ text ==
 Match each verbs to its preterit
 ==
 
+doc == 
+Exos à choix unique où on doit relier chaque élèment de la première case à un élèment de la seconde case. 
+
+Code couleurs : Après validation de la réponse 
+-> Flèches vertes = réponses justes
+-> Flèches rouges = réponses fausses
+
+Importer le ou les fichiers csv où on doit récupérer les données des champs de gauche et de 
+droite par la syntaxe : @ chemin d'accè vers le fichier 
+
+Dans la balise before définir les critères de choix c'est à dire : 
+- Si on a plusieurs fichiers csv, on utilise la fonction random pour choisir aléatoirement les données entre les différents
+- filename définit le nom du fichier, sample_rows définit les lignes à choisir (le nombre de ligne parmi toutes les lignes)
+- row['nom de la colonne'] pour choisir le colonne qui définit les élèments de la case
+
+==
