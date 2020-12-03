902091ccf2270b95ea0818ea55633866cfe7d38a
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 11:54:37 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Trouve_faute.pl

--- AAAA/Anna/Trouve_faute.pl
+++ AAAA/Anna/Trouve_faute.pl
@@ -1,10 +1,12 @@
 extends = /Languages/French/Orthographe/Trouve_faute_template.pl
 
 title==
-Fautes d'Orthographe    
+Trouve Fautes    
 ==
 
 text ==
 Trouver les fautes d'orthographe dans les phrases suivantes ou cliquer sur correct si vous 
 trouvez qu'elles sont correctes
 ==
+
+@ /AAAA/Anna/Irregular_verbs.csv [data]
