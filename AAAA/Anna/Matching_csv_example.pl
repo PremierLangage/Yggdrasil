d0f59ec1840a2bfde85186ed8a6d5f5553864c3f
ANNA DIAW <adiaw@etud.u-pem.fr>
2020-12-03 15:00:02 +0100

    adiaw modified Yggdrasil/AAAA/Anna/Matching_csv_example.pl

---
+++ AAAA/Anna/Matching_csv_example.pl
@@ -0,0 +1,13 @@
+extends= /AAAA/Anna/Matching_csv_template.pl
+
+title==
+Matching List
+==
+
+text ==
+Match each verbs to its preterit
+==
+
+@ /AAAA/Anna/Irregular_verbs.csv [data]
+@ /AAAA/Anna/Irregular_verbs2.csv [data1]
+
