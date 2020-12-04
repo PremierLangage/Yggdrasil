9507349883705d1c943657d5ec3fe35bb765974f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-04 15:56:02 +0100

    nborie modified Yggdrasil/ComputerScience/Haskell/test_haskell.pl

---
+++ ComputerScience/Haskell/test_haskell.pl
@@ -0,0 +1,47 @@
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = Haskell
+editor.code ==
+module Main where
+
+main :: IO()
+main = putStrLn "Hello World !"
+==
+
+before==
+==
+
+title = Test Haskell
+
+text==
+==
+
+form==
+{{ editor|component }}
+==
+
+evaluator==#|python|
+from subprocess import Popen, PIPE
+
+f = open('hello_world.hs', 'w')
+f.write(editor.code)
+f.close()
+
+process = Popen(['ghc', 'hello_world.hs'], stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+
+ans = stdout.decode()
+
+process = Popen(['./hello_world'], stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+
+ans += '\n<br>\n<br>' + stdout.decode()
+
+grade = (100, ans)
+==
+
+
