1f7ad20a53b4d108872418423405164d57aa09d5
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-05 21:32:05 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-2021.pl

--- ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-2021.pl
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/head-2021.pl
@@ -42,7 +42,7 @@ CHARTE DES DROITS FONDAMENTAUX DE L'UNION EUROPÉENNE
 Le Parlement européen, le Conseil et la Commission proclament solennellement
 en tant que Charte des droits fondamentaux de l'Union européenne le texte
 repris ci-après.
-
+CHARTE DES DROITS FONDAMENTAUX DE L'UNION EUROPÉENNE
 ==
 
 pltest1==
