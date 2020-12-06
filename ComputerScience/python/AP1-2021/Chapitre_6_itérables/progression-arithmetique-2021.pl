8fed11fc27ebd168509b2171e8adfd25c925a626
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-06 17:46:41 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/progression-arithmetique-2021.pl

---
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/progression-arithmetique-2021.pl
@@ -0,0 +1,79 @@
+# Author: Wenjie Fang
+
+extends=/ComputerScience/python/template/pltest.pl
+builder=/builder/before.py
+
+author=Wenjie Fang
+title=Progression arithmétique
+
+text==
+Ecrire une fonction ``progression(p, r, l)`` qui prend en paramètre le premier terme ``p``, la raison ``r`` et la longueur ``l`` **d'une progression arithmétique formée par des entiers**, et retourner la progression sous forme d'une liste. Si ``l <= 0``, alors la fontion retourne une liste vide. La raison ``r`` pourrait être négative.
+
+Par exemple, l'appel ``progression(1, 2, 4)`` doit retourner la liste ``[1, 3, 5, 7]``, et l'appel ``progression(1, 2, -4)`` doit retourner ``[]``.
+
+Cependant, **les boucles sont interdites** dans cette exercice. Vous pouvez utiliser ``range``. Il faut faire attention au cas où ``r`` est négatif.
+==
+
+taboo=for|while
+
+tag=range
+
+samplesol==
+def progression(p, r, l):
+    return list(range(p, p + r*l + r/2, r))
+==
+
+before==
+from random import randint
+
+pp = randint(-10, 10)
+rr = randint(-10, 10)
+ll = randint(-4, 10)
+res = [pp + rr * i for i in range(ll)]
+
+pltest3 = f">>> progression({pp}, {rr}, {ll}) # Random\n{res}\n"
+
+pp = randint(-10, 10)
+rr = randint(-10, 10)
+ll = randint(-4, 10)
+res = [pp + rr * i for i in range(ll)]
+
+pltest4 = f">>> progression({pp}, {rr}, {ll}) # Random\n{res}\n"
+
+pp = randint(-10, 10)
+rr = randint(-10, 10)
+ll = randint(-4, 10)
+res = [pp + rr * i for i in range(ll)]
+
+pltest5 = f">>> progression({pp}, {rr}, {ll}) # Random\n{res}\n"
+
+pp = randint(-10, 10)
+rr = randint(-10, 10)
+ll = randint(-4, 10)
+res = [pp + rr * i for i in range(ll)]
+
+pltest6 = f">>> progression({pp}, {rr}, {ll}) # Random\n{res}\n"
+==
+
+pltest0==
+>>> progression(1, 1, 5) # Normal 1
+[1, 2, 3, 4, 5]
+>>> progression(-5, 2, 7) # Normal 2
+[-5, -3, -1, 1, 3, 5, 7]
+>>> progression(-10, 0, 4) # Normal 3
+[-10, -10, -10, -10]
+==
+
+pltest1==
+>>> progression(0, 2, -3) # Vide 1
+[]
+>>> progression(0, 100000, 0) # Vide 2
+[]
+==
+
+pltest2==
+>>> progression(1, -2, 6) # Négative 1
+[1, -1, -3, -5, -7, -9]
+>>> progression(10, -3, 7) # Négative 2
+[10, 7, 4, 1, -2, -5, -8]
+==
