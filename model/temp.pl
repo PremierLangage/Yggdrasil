7c787aeefd546cf363db272ebb4a22f17e16652c
David DOYEN <David.Doyen@u-pem.fr>
2020-12-06 16:56:53 +0100

    ddoyen modified Yggdrasil/model/temp.pl

--- model/temp.pl
+++ model/temp.pl
@@ -102,55 +102,7 @@ extracss ==
     text-decoration: underline green;
 }
 
-.drag-drop-component {
-    position: relative;
-    cursor: move;
-    display: inline-flex;
-    padding: 8px;
-    box-sizing: border-box;
-    min-width: 3em;
-    min-height: 2em;
-    vertical-align: middle;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    border: 0px solid transparent;
-    border-radius: 0px;
-    margin: 0px;
 
-    &.dnd-drag {
-      opacity: .6;
-    }
-
-    &.dragzone {
-      background-color: AntiqueWhite;
-    }
-
-    &.dropzone {
-        cursor: pointer;
-        border: 1px solid black;
-        &:hover,
-        &.dnd-over
-        {
-            border: 2px solid black;
-        }
-    }
-
-    &.disabled {
-        border: 1px dashed;
-        pointer-events: none;
-        cursor: not-allowed;
-    }
-
-    nge-markdown {
-      pointer-events: none;
-    }
-}
-
-.btn-clear {
-  font-size: 1.5em;
-  cursor: pointer;
-}
 </style>
 ==
 
