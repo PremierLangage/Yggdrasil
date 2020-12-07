75e9af504d2cc14cd6fc5b1dbc1349b104da2ebf
Wenjie FANG <Wenjie.Fang@univ-mlv.fr>
2020-12-07 20:59:01 +0100

    wfang modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_6_itérables/_difficle.pl

---
+++ ComputerScience/python/AP1-2021/Chapitre_6_itérables/_difficle.pl
@@ -0,0 +1,8 @@
+extends=/template/default.pl
+
+title=**Section difficile**
+
+text=Les exercices suivant pourraient être un peu plus difficiles, mais tout à fait faisable avec ce que vous connaissez.
+
+form=
+
