5748d1cf01136669f246af25c04fe129d55995b9
David DOYEN <David.Doyen@u-pem.fr>
2020-12-07 13:23:01 +0100

    ddoyen modified Yggdrasil/demo/basic/dragdrop1.pl

--- demo/basic/dragdrop1.pl
+++ demo/basic/dragdrop1.pl
@@ -4,25 +4,24 @@ title ==
 Exemple 1
 ==
 
-labelcontents ==
-$! > !$
-$! < !$
-==
 
 dropsolutions ==
-$! > !$
-$! > !$
-$! < !$
+homologues
+hétérologues
+spermatozoïde
+ovocyte
+diploïdes
+haploïdes
 ==
 
-text==
-Comparer les expressions suivants avec les symboles {{ labels[0] | component }} et {{ labels[1] | component }}.
-==
 
-form==
-<ul>
-<li> 153 {{ drops[0]|component }} 89 </li>
-<li> 111 {{ drops[1]|component }} 107 </li>
-<li> 99 {{ drops[2]|component }} 180 </li>
-</ul>
+form ==
+On trouve dans une cellule somatique 23 paires de chromosomes: 22 paires de chromosomes {{ drops[0]|component }},  et  une  paire  de  chromosomes  {{ drops[1]|component }}  (les  chromosomes sexuels  X  et  Y). 
+Dans  une  paire  de  chromosomes  homologues  (numérotés  de  1  à  22),  un des  chromosomes vient  du gamète  paternel  ({{ drops[2]|component }}),  l’autre  du gamète  maternel  ({{ drops[3]|component }}).  
+Les cellules  humaines  somatique  sont  dites {{ drops[4]|component }} (contiennent  23 paires  de  chromosomes),  alors  que les cellules germinales (les gamètes) sont dites {{ drops[5]|component }} (23 chromosomes).
+
+<br>
+{% for label in labels %}
+{{ label|component }}
+{% endfor %}
 ==
