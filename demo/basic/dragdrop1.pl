cbfd4363a1c82696d7d43ebb1c3b8919ce6e7216
David DOYEN <David.Doyen@u-pem.fr>
2020-12-07 13:28:34 +0100

    ddoyen modified Yggdrasil/demo/basic/dragdrop1.pl

--- demo/basic/dragdrop1.pl
+++ demo/basic/dragdrop1.pl
@@ -11,6 +11,7 @@ spermatozoïde
 ovocyte
 diploïdes
 haploïdes
+homologues
 ==
 
 shuffle % true
@@ -21,7 +22,7 @@ Compléter le texte suivant avec les étiquettes.
 
 form ==
 On trouve dans une cellule somatique 23 paires de chromosomes: 22 paires de chromosomes {{ drops[0]|component }},  et  une  paire  de  chromosomes  {{ drops[1]|component }}  (les  chromosomes sexuels  X  et  Y). 
-Dans  une  paire  de  chromosomes  homologues  (numérotés  de  1  à  22),  un des  chromosomes vient  du gamète  paternel  ({{ drops[2]|component }}),  l’autre  du gamète  maternel  ({{ drops[3]|component }}).  
+Dans  une  paire  de  chromosomes  {{ drops[6]|component }}, un des  chromosomes vient  du gamète  paternel  ({{ drops[2]|component }}),  l’autre  du gamète  maternel  ({{ drops[3]|component }}).  
 Les cellules  humaines  somatique  sont  dites {{ drops[4]|component }} (contiennent  23 paires  de  chromosomes),  alors  que les cellules germinales (les gamètes) sont dites {{ drops[5]|component }} (23 chromosomes).
 
 <br>
