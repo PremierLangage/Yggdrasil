6fcc838ecb984eabd5f5611f34018665d5f560bb
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-12-10 10:13:33 +0100

    mkobylanski modified Yggdrasil/AAAA/Quentin/en-quete-de-sens.pl

--- AAAA/Quentin/en-quete-de-sens.pl
+++ AAAA/Quentin/en-quete-de-sens.pl
@@ -5,25 +5,25 @@ title= En quête de sens ...
 questions==
 
 
-* Soient $!E!$ et $!F!$ deux ensembles et soit $!f:E \to F!$ une application. Soient $!A\subset E!$ et $!B \subset F!$. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
+** Soient $!E!$ et $!F!$ deux ensembles disjoints et soit $!f:E \to F!$ une application. Soient $!A\subset E!$ et $!B \subset F!$. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
 - $!f(A) \subset E!$
 + $!\forall y \in B, \quad f^{\\{-1\\}}(y) \in A!$
 - $! A \subset f^{\\{-1\\}}(B) !$
 - $! f^{\\{-1\\}}(B) = \emptyset !$
 
-* Soient $!E!$, $!F!$ et $!G!$ trois ensembles et soient $!f:E \to F!$ et $!g:F\to G!$ deux applications. Soient $!A\subset E!$, $!B \subset F!$ et $!C\subset G!$. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
+** Soient $!E!$, $!F!$ et $!G!$ trois ensembles deux à deux disjoints et soient $!f:E \to F!$ et $!g:F\to G!$ deux applications. Soient $!A\subset E!$, $!B \subset F!$ et $!C\subset G!$. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
 -$!(g\circ f)(A)=G!$
 - $!g(B)=\emptyset!$
 + $!(g\circ f)^{\\{-1\\}}(C)=B!$
 - $! f^{\\{-1\\}}(B) \subset A !$
 
 
-* Soient $!E!$, $!F!$ et $!G!$ trois ensembles deux à deux distincts. Soit $!B\subset F!$ avec $!B \neq F!$. Soient $!f:E \to F!$ et $!g:B\to G!$ deux applications. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
+** Soient $!E!$, $!F!$ et $!G!$ trois ensembles deux à deux distincts. Soit $!B\subset F!$ avec $!B \neq F!$. Soient $!f:E \to F!$ et $!g:B\to G!$ deux applications. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
 - $!g^{\\{-1\\}}(G)=B !$
 + $!f!$ est surjective mais $!g\circ f!$ ne l'est pas
 - $! f^{\\{-1\\}}(B)=E!$ et $!g\circ f!$ est surjective
 
-* Soit $!f: \mathbb R\to \mathbb R^2!$ une application. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
+** Soit $!f: \mathbb R\to \mathbb R^2!$ une application. Identifier l'assertion qui n'a pas de sens mathématiquement parlant. 
 - $!f^{\\{-1\\}}(\mathbb R^2)\subset [1,6]!$
 + $!f^{\\{-1\\}}([1,+\infty[)\subset \mathbb R_+!$
 - $!f(\\{2\\})=\\{(4,3)\\}!$
