2cad7eb63854ecc883030c726f341d35a6508785
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-11 16:31:06 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/EXAM-JEUDI/base1.pl

---
+++ AAAA/Quentin/EXAM-JEUDI/base1.pl
@@ -0,0 +1,31 @@
+
+extends= /model/qcm/qcm.pl
+
+
+title= Base 3/4
+
+
+image =$ /AAAA/Quentin/Images/bij.png
+text==
+On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
+![alt image]({{image}})
+==
+
+nb=5
+nbtrues=3
+
+good==
+f est surjective
+f est injective
+f est bijective
+==
+
+bad==
+f n'est pas une application
+f est bien définie mais n'est ni injective ni surjective
+==
+
+
+
+
+
