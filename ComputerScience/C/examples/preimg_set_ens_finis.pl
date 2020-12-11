168f870d699ef0b19f26f0c5ae1fe594e20a0f9d
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-11 23:43:25 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/preimg_set_ens_finis.pl

--- ComputerScience/C/examples/preimg_set_ens_finis.pl
+++ ComputerScience/C/examples/preimg_set_ens_finis.pl
@@ -64,12 +64,12 @@ else:
     fb_inj = "La fonction n'est pas injective, $% "+str(val)+" %$ possède $% "+img_fct.count(val)+" %$ antécédants."
 # feedback surjectivité
 if is_surjective:
-    fb_inj = "La fonction est surjective, les images couvrent totalement l'ensemble d'arrivée."
+    fb_surj = "La fonction est surjective, les images couvrent totalement l'ensemble d'arrivée."
 else:
     val = choice(ensemble_img)
     while val in img_fct:
         val = choice(ensemble_img)
-    fb_inj = "La fonction n'est pas surjective, $% "+str(val)+" %$ n'a pas d'antécédants."
+    fb_surj = "La fonction n'est pas surjective, $% "+str(val)+" %$ n'a pas d'antécédants."
 
 
 def make_preimg(img_fct, fct_name, ensemble_defi, ensemble_img, good=True):
