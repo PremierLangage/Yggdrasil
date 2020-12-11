01b1dfd264904aadf3789fc326b3b85b7cdc24a1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-11 16:56:20 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/fct_discrete.pl

--- ComputerScience/C/examples/fct_discrete.pl
+++ ComputerScience/C/examples/fct_discrete.pl
@@ -15,7 +15,7 @@ lists_symbols = [ ["a", "b", "d", "e", "f", "g", "h"], [1, 2, 3, 4, 5, 6, 7] ]
 # ça va choisir un cardinal puis entre lettres et chiffres
 cardinal_defi = randint(4, 7)
 ensemble_defi = choice(lists_symbols)[:cardinal_defi]
-ensemble_defi_str = " \lbrace " + ",".join([str(e) for e in  ensemble_defi]) + " \rbrace "
+ensemble_defi_str = " \lbrace " + ",".join([str(e) for e in  ensemble_defi]) + "  \rbrace "
 
 # Pareil, choix aléatoire des images
 cardinal_img = randint(4, 7)
