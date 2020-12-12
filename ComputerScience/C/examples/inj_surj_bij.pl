0163c9f6d9a4b1b001f97e12fb8f2ad0712ef91e
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-12 12:11:34 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/inj_surj_bij.pl

--- ComputerScience/C/examples/inj_surj_bij.pl
+++ ComputerScience/C/examples/inj_surj_bij.pl
@@ -11,22 +11,38 @@ before==#|python|
 from random import sample, randint
 
 # Plus il y a de fonction différente, mieux c'est !
+# Il suffit de faire un copier-coller et de respecter les règles suivantes :
+# name --> une chaine latex pour un nom de fonction
+# formula --> une chaine latex, formule en la variable x 
+# dom --> une liste de domaines [E1 ,E2, ..., En] 
+#         (n domaines sou forme de chaine latex) croissant pour l'inclusion
+# codom --> une liste de minimum n codomaine [F1 = f(E1), F2 = f(E2), ..., Fn = f(En) , Fn+1, ...] 
+#           croissant pour l'inclusion avec possiblement plus de codomaine à la fin
+# inj --> une liste de n booléen précisant s'il y a injectivité sur le domaine (Ei)_{1<i<n}
+
+# De ce fait, les injections sont injectives si le booléen dit oui...
+# Les fonctions sont surjectives si le codomaine est celui associé au domaine seulement...
 
 fct_list=[]
 fct_list.append({"name": "cube", 
                  "formula": "x^{3}",
                  "dom" : ["[0, 2]"],
-                 "codom" : ["[0, 8]"],})
+                 "codom" : ["[0, 8]"],
+                 "inj" : [True],})
 
 fct_list.append({"name": "carré", 
                  "formula": "x^{2}",
-                 "dom" : ["[0, 2]", "[-2, 2]", "[0, 2] \\cup \\lbrace -2 \\rbrace"],
-                 "codom" : ["[0, 4]", "[0, 4]", "[0, 4]", "[0, 4] \\cup \\lbrace -4 \\rbrace"],})
+                 "dom" : ["[0, 2]", "[0, 2] \\cup \\lbrace -2 \\rbrace", "[-2, 2]", "\\mathbb{R}"],
+                 "codom" : ["[0, 4]", "[0, 4]", "[0, 4]", "\\mathbb{R}", "[0, 4] \\cup \\lbrace -4 \\rbrace"],
+                 "inj" : [True, False, False],})
 
 fct_list.append({"name": "exp", 
                  "formula": "e^{x}",
                  "dom" : ["[0, + \infty ["],
-                 "codom" : ["[1, + \infty ["],})
+                 "codom" : ["[1, + \infty ["],
+                 "inj" : [True],})
+
+
 
 def make_latex_fct(name, formula, dom, codom):
     """
