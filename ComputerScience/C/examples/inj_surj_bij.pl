8fa9a986800428cf3f32d50fdb3a24c0bdd12a38
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-12 17:50:15 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/inj_surj_bij.pl

--- ComputerScience/C/examples/inj_surj_bij.pl
+++ ComputerScience/C/examples/inj_surj_bij.pl
@@ -35,7 +35,7 @@ fct_list.append({"name": "carré",
                  "dom" : ["[0, 1]", "[0, 2]", "[0, 2] \\cup \\lbrace -2 \\rbrace", "[-2, 2]", "\\mathbb{R}"],
                  "codom" : ["[0, 1]", "[0, 4]", "[0, 4]", "[0, 4]", "\\mathbb{R}_{+}", "[0, 4] \\cup \\lbrace -4 \\rbrace"],
                  "inj" : [True, True, False, False, False],
-                 "fb_inj": ["$% 4 %$ a deux antécédants $% 2 %$ et $% -2 %$."],})
+                 "fb_inj": "$% 4 %$ a deux antécédants $% 2 %$ et $% -2 %$.",})
 
 fct_list.append({"name": "exp", 
                  "formula": "e^{x}",
@@ -60,7 +60,7 @@ fct_list.append({"name": "(c \in \mathbb{R}) \quad C^{ste}",
                  "dom" : ["\{ 0 \}", "[0, 10]", "\\mathbb{R}"],
                  "codom" : ["\\lbrace c \\rbrace", "\\lbrace c \\rbrace", "\\lbrace c \\rbrace", "\\mathbb{R}"],
                  "inj" : [True, False, False],
-                 "fb_inj": ["Tous les éléments du domaine de définition sont des antécédants de $% c %$. Une fonction constante n'est donc plus injective dès que son ensemble de définition contient au moins 2 éléments distincts."],})
+                 "fb_inj": "Tous les éléments du domaine de définition sont des antécédants de $% c %$. Une fonction constante n'est donc plus injective dès que son ensemble de définition contient au moins 2 éléments distincts.",})
 
 
 def make_latex_fct(name, formula, dom, codom):
