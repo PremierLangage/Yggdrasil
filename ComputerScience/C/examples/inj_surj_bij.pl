79b7125962507a891bc76a9dd0f0452a0d769576
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-12 16:21:26 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/inj_surj_bij.pl

--- ComputerScience/C/examples/inj_surj_bij.pl
+++ ComputerScience/C/examples/inj_surj_bij.pl
@@ -74,7 +74,7 @@ def make_latex_fct(name, formula, dom, codom):
     return ans
 
 group.items = []
-fct_sample = sample(fct_list, 3)
+fct_sample = sample(fct_list, 4)
 for fct in fct_sample:
     i = randint(0, len(fct["dom"]) - 1)
     dom = fct["dom"][i]
