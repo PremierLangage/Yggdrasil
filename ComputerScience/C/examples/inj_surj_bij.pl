a13d6453414a9744fcc5d7fc49b2eaf157a1928f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-12 18:01:09 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/inj_surj_bij.pl

--- ComputerScience/C/examples/inj_surj_bij.pl
+++ ComputerScience/C/examples/inj_surj_bij.pl
@@ -127,12 +127,12 @@ for item in group.items:
         else:
             nb_bad += 1
             if "good" in item['id']:
-                fb_detail += """<li><p><span class="error-state" style="padding: 0.8em;">"""+item['content']+"  est bien <b>injective</b>.</span></p></li><br>"
+                fb_detail += """<li><p><span class="error-state" style="padding: 2em;">"""+item['content']+"  est bien <b>injective</b>.</span></p></li><br><br>"
             else:
                 short_name = item['id'][3:]
                 for fct in fct_sample:
                     if fct['name'] == short_name:
-                        fb_detail += """<li><p><span class="error-state" style="padding: 0.8em;">"""+item['content']+"  n'est pas injective. "+fct['fb_inj']+"</span></p></li><br>"
+                        fb_detail += """<li><p><span class="error-state" style="padding: 2em;">"""+item['content']+"  n'est pas injective. "+fct['fb_inj']+"</span></p></li><br><br>"
 
 note = (nb_good*100) // (nb_good+nb_bad)
 if note == 100:
