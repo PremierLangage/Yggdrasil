4174a52b465e5b70e4c613deb2446467a0ca3df6
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2020-12-13 14:12:59 +0100

    nborie modified Yggdrasil/AAAA/Quentin/inj_surj_conseils/qcm_carrousel.pl

---
+++ AAAA/Quentin/inj_surj_conseils/qcm_carrousel.pl
@@ -0,0 +1,44 @@
+# template Tout nouveau malheuresement... feedback pas encore programmé...
+extends=/ComputerScience/C/examples/Stoi_le_caroussel.pl
+
+
+author = Enseignants Méthodologie
+bij =$ /AAAA/Quentin/Images/bij.png
+
+
+title= Injectivité / Surjectivité
+
+questions==
+
+
+
+* Soient $!E!$ et $!F!$ deux ensembles. On dit que l'application $!f:E \to F!$ est injective si:
++ $! \forall x,x' \in E, \quad ( f(x)=f(x') \implies x=x')!$
+-  $! \forall x,x' \in E, \quad ( x=x' \implies f(x)=f(x'))!$
+-  $! \exists y \in F, \quad \forall x \in E, \quad  f(x)=y!$
+-  $! \forall y \in F, \quad \exists x \in E,  \quad f(x)=y!$
+
+
+* Soient $!E!$ et $!F!$ deux ensembles. On dit que l'application $!f:E \to F!$ est surjective si:
+- $! \forall x,x' \in E, \quad ( f(x)=f(x') \implies x=x')!$
+-  $! \forall x,x' \in E, \quad ( x=x' \implies f(x)=f(x'))!$
+-  $! \exists y \in F, \quad \forall x \in E,\quad  f(x)=y!$
++  $! \forall y \in F, \quad \exists x \in E,\quad  f(x)=y!$
+
+
+* Soit $!f:\mathbb R \to \mathbb R!$ une application paire, c'est-à-dire que $!f(x)=f(-x)!$ pour tout $! x \in \mathbb R!$. L'application $!f!$ peut-elle être injective ? 
++ Non
+- Oui 
+
+
+* L'application $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle injective ?   
+- Non 
++ Oui 
+
+* L'application $!f:\mathbb R \to \mathbb R_+!$, $!x\mapsto \exp(x)!$ est-elle surjective ?  
++ Non
+- Oui 
+
+==
+
+
