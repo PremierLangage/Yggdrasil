6ccd0b23f860ef44b9b897e3e2c4fbe70ed08104
David DOYEN <David.Doyen@u-pem.fr>
2020-12-14 17:25:29 +0100

    ddoyen modified Yggdrasil/model/basic/fillgap.pl

--- model/basic/fillgap.pl
+++ model/basic/fillgap.pl
@@ -13,8 +13,8 @@ from itertools import count
 number_count = 1
 counter = count(number_count)
 
-dropsolutions = re.findall(r'\{(.*?)\}', filledtext)
-form = re.sub(r'\{(.*?)\}', lambda x: "{{ drops[" + str(next(counter)) + "]|component }}", filledtext)
+#dropsolutions = re.findall(r'\{(.*?)\}', filledtext)
+#form = re.sub(r'\{(.*?)\}', lambda x: "{{ drops[" + str(next(counter)) + "]|component }}", filledtext)
 
 nbdrops = len(dropsolutions)
 for _ in dropsolutions:
