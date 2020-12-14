54795db8be56e2e13f56fd6c3755bbb204e95ca2
David DOYEN <David.Doyen@u-pem.fr>
2020-12-14 18:33:19 +0100

    ddoyen modified Yggdrasil/model/basic/fillgap.pl

--- model/basic/fillgap.pl
+++ model/basic/fillgap.pl
@@ -6,20 +6,49 @@ filledtext ==
 {hhh} bbbb {hhhj}
 ==
 
+
 footerbefore ==
 from customdragdrop import CustomDragDrop
 drops = []
 labels = []
+
+if isinstance(dropsolutions, str):
+    lstdropsolutions = dropsolutions.splitlines()
+else:
+    lstdropsolutions = dropsolutions
+
+if 'labelcontents' in globals():
+    if isinstance(labelcontents, str):
+        list_labelcontents = labelcontents.splitlines()
+    else:
+        list_labelcontents = labelcontents
+else:
+    list_labelcontents = list(set(lstdropsolutions))
+
+for content in list_labelcontents:
+    labels.append(CustomDragDrop.Label(content=content))
+
+nbdrops = len(lstdropsolutions)
+for _ in lstdropsolutions:
+    drops.append(CustomDragDrop.Drop())
 
 import re
 from itertools import count
 
 counter = count(0)
 
-#lst = re.findall(r'\{(.*?)\}', filledtext)
-
+lst = re.findall(r'\{(.*?)\}', filledtext)
 ==
 
+dropsolutions ==
+homologues
+hétérologues
+spermatozoïde
+ovocyte
+diploïdes
+haploïdes
+homologues
+==
 
 text ==
 {{lst}}
