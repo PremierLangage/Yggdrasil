bbba550996765e259f8810ad6832c756df1f274d
David DOYEN <David.Doyen@u-pem.fr>
2020-12-14 19:54:09 +0100

    ddoyen modified Yggdrasil/model/basic/fillgap.pl

--- model/basic/fillgap.pl
+++ model/basic/fillgap.pl
@@ -20,17 +20,6 @@ text ==
 Compléter le texte suivant avec les étiquettes.
 ==
 
-form ==
-On trouve dans une cellule somatique 23 paires de chromosomes: 22 paires de chromosomes {{ drops[0]|component }},  et  une  paire  de  chromosomes  {{ drops[1]|component }}  (les  chromosomes sexuels  X  et  Y). 
-Dans  une  paire  de  chromosomes  {{ drops[6]|component }}, un des  chromosomes vient  du gamète  paternel  ({{ drops[2]|component }}),  l’autre  du gamète  maternel  ({{ drops[3]|component }}).  
-Les cellules  humaines  somatique  sont  dites {{ drops[4]|component }} (contiennent  23 paires  de  chromosomes),  alors  que les cellules germinales (les gamètes) sont dites {{ drops[5]|component }} (23 chromosomes).
-
-<br>
-{% for label in labels %}
-{{ label|component }}
-{% endfor %}
-==
-
 
 filledtext ==
 On trouve dans une cellule somatique 23 paires de chromosomes: 22 paires de chromosomes {homologues},  et  une  paire  de  chromosomes  {hétérologues}  (les  chromosomes sexuels  X  et  Y). 
@@ -56,6 +45,14 @@ lstdropsolutions = re.findall(r'\{(.*?)\}', filledtext)
 form = filledtext
 form = re.sub(r'\{(.*?)\}', replace , form)
 
+form2 = """
+<br>
+{% for label in labels %}
+{{ label|component }}
+{% endfor %}
+"""
+
+form = form + form2
 
 for content in lstdropsolutions:
     labels.append(CustomDragDrop.Label(content=content))
