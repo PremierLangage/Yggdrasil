031f049ed950ee0901481364d2a3e6a7f72f87cc
David DOYEN <David.Doyen@u-pem.fr>
2020-12-15 14:23:01 +0100

    ddoyen modified Yggdrasil/model/basic/fillgap.pl

--- model/basic/fillgap.pl
+++ model/basic/fillgap.pl
@@ -1,32 +1,10 @@
 extends = /model/basic/dragdrop.pl
 
-title ==
-Chromosomes
-==
-
-dropsolutions ==
-homologues
-hétérologues
-spermatozoïde
-ovocyte
-diploïdes
-haploïdes
-homologues
-==
-
 shuffle % true
 
 text ==
 Compléter le texte suivant avec les étiquettes.
 ==
-
-
-filledtext ==
-On trouve dans une cellule somatique 23 paires de chromosomes: 22 paires de chromosomes {homologues},  et  une  paire  de  chromosomes  {hétérologues}  (les  chromosomes sexuels  X  et  Y). 
-Dans  une  paire  de  chromosomes  {homologues}, un des  chromosomes vient  du gamète  paternel  ({spermatozoïde}),  l’autre  du gamète  maternel  ({ovocyte}).  
-Les cellules  humaines  somatique  sont  dites {diploïdes} (contiennent  23 paires  de  chromosomes),  alors  que les cellules germinales (les gamètes) sont dites {haploïdes} (23 chromosomes).
-==
-
 
 
 footerbefore ==
@@ -61,4 +39,3 @@ nbdrops = len(lstdropsolutions)
 for _ in lstdropsolutions:
     drops.append(CustomDragDrop.Drop())
 ==
-
