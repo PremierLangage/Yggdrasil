b0b6a379530429984f4f02a230b09c9d56fd7da8
Elodie TRINH <elodie.trinh@univ-eiffel.fr>
2020-12-15 15:22:36 +0100

    etrinh modified Yggdrasil/AAAA/Elodie/QCM.pl

---
+++ AAAA/Elodie/QCM.pl
@@ -0,0 +1,15 @@
+extends = /gift/templates/qselectset.pl
+
+title==
+Select Set
+==
+
+text==
+De quel savant est cette célèbre citation : "Rien ne se perd, rien ne se crée, tout se transforme"
+==
+
+choices==
+~Cheikh Anta Diop #Mauvaise réponse, c'est Antoine Lavoisier.
+=Antoine Lavoisier #Bonne réponse; bravo !
+~Aristote #Mauvaise réponse, c'est Antoine Lavoisier.
+==
