2272fdd8009652441dafb53a5de89ed6db1bb155
Venceslas BIRI <Venceslas.Biri@u-pem.fr>
2020-12-15 17:32:25 +0100

    vbiri modified Yggdrasil/AAAA/Vence/first.pl

--- AAAA/Vence/first.pl
+++ AAAA/Vence/first.pl
@@ -1,7 +1,15 @@
 @ /builder/before.py [builder.py]
 @ /grader/evaluator.py [grader.py]
 
+before ==
+import random as rd
+a = rd.randint(10, 20)
+b = rd.randint(10, 20)
+==
+
 title = "Addition"
+
+
 
 text ==
 Donner le produit de 2 et 4
