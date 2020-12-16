307561aa9a4e015bb191e24afb974094bcdc2c70
Quentin DUCHEMIN <Quentin.Duchemin@univ-mlv.fr>
2020-12-16 10:59:48 +0100

    qduchemin modified Yggdrasil/AAAA/Quentin/EXAM-JEUDI/preimage_nico.pl

--- AAAA/Quentin/EXAM-JEUDI/preimage_nico.pl
+++ AAAA/Quentin/EXAM-JEUDI/preimage_nico.pl
@@ -107,8 +107,8 @@ def make_preimg(img_fct, fct_name, ensemble_defi, ensemble_img, good=True):
     else:
         state = "good"
         feedback = " est vraie."
-    prop = " $% "+fct_name+"^{-1}( "+ make_latex_ens(ing_subset)
-    prop+=" ) = "+make_latex_ens(preimg_subset)+" %$ "
+    prop = " $% "+fct_name+"^{ {0}}( "+ make_latex_ens(ing_subset)
+    prop+=" ) = "+make_latex_ens(preimg_subset)+" %$ ".format(make_latex_ens)
     return (ing_subset, state, prop, feedback)
 
 group.items = []
