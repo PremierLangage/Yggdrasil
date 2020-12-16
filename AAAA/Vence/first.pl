1cde560e5c2650bb7fd80bbadc8a533057ee2445
Venceslas BIRI <Venceslas.Biri@u-pem.fr>
2020-12-16 13:27:56 +0100

    vbiri modified Yggdrasil/AAAA/Vence/first.pl

--- AAAA/Vence/first.pl
+++ AAAA/Vence/first.pl
@@ -15,6 +15,18 @@ $! \vec{u} = \left( [1,2,3] \right) !$
 
 ==
 
+input =: MathInput
+
+form ==
+{{ input | component }}
+==
+
+evaluator ==
+grade=(100,input.value)
+==
+
+
+
 input =: Input
 input.type = number
 
@@ -27,4 +39,5 @@ if input.value == 8:
     score = 100
 else:
     score = 0
+grade(score,"la réponse est 100")
 ==
