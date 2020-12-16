12bbf1077e50845e3b340ef42751193b14f4134e
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2020-12-16 16:46:47 +0100

    mkobylanski modified Yggdrasil/AAAA/Quentin/EXAM-JEUDI/carroussel_base.pl

---
+++ AAAA/Quentin/EXAM-JEUDI/carroussel_base.pl
@@ -0,0 +1,48 @@
+# template Tout nouveau malheuresement... feedback pas encore programmé...
+extends=/ComputerScience/C/examples/Stoi_le_caroussel.pl
+
+
+author = Enseignants Méthodologie
+bij =$ /AAAA/Quentin/Images/bij.png
+
+# pour changer le nombre de question, c'est en dessous (10 MAX)
+# si le nombre est plus petit que 10, ce sera un tirage aléatoire
+# sans remise parmi le tas de questions.
+nb_quest_voulu = 5
+
+title= Carroussel+
+
+image1 =$ /AAAA/Quentin/Images/bij.png
+image2 =$ /AAAA/Quentin/Images/non-appli.png
+image3 =$ /AAAA/Quentin/Images/nsur-ninj.png
+
+questions==
+
+** On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
+![alt image]({{image1}})
+
++ f est surjective
++ f est injective
++ f est bijective
+- f n'est pas une application
+- f est bien définie mais n'est ni injective ni surjective
+
+** On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
+![alt image]({{image2}})
+
++ f n'est pas une application
+- f est surjective
+- f est injective
+- f est bijective
+- f est bien définie mais n'est ni injective ni surjective
+
+** On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
+![alt image]({{image3}})
+
++ f est bien définie mais n'est ni injective ni surjective
+- f est surjective
+- f est injective
+- f est bijective
+- f n'est pas une application
+
+==
