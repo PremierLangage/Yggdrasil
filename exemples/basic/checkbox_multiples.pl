5f79aa57e2cd44ae80820841136f5ac66611bbb4
David DOYEN <David.Doyen@u-pem.fr>
2020-12-16 23:19:36 +0100

    ddoyen modified Yggdrasil/exemples/basic/checkbox_multiples.pl

--- exemples/basic/checkbox_multiples.pl
+++ exemples/basic/checkbox_multiples.pl
@@ -5,10 +5,11 @@ title = Multiples de 3 (Checkbox)
 before ==
 import random as rd
 
-mult3 = [str(n) for n in range(50,100) if n % 3 == 0]
-other = [str(n) for n in range(50,100) if n % 3 != 0]
+right = [str(n) for n in range(50,100) if n % 3 == 0]
+wrong = [str(n) for n in range(50,100) if n % 3 != 0]
 
-checkbox.setdata_from_rw(mult3, other, 5, rd.randint(1, 4))
+nbchoices = 5
+nbright = rd.randint(1, 4)
 ==
 
 text ==
