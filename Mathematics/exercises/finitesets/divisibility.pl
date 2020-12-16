c10d7de7b23212be686b319f697917cac1c75d60
David DOYEN <David.Doyen@u-pem.fr>
2020-12-16 18:20:10 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/finitesets/divisibility.pl

--- Mathematics/exercises/finitesets/divisibility.pl
+++ Mathematics/exercises/finitesets/divisibility.pl
@@ -1,8 +1,6 @@
-extends = /template/mathradio.pl
+extends = /model/basic/radio.pl
 
 title = Relation de divisibilité
-
-lang = fr
 
 before ==
 if randint(0,1)==0:
@@ -20,6 +18,10 @@ else:
         if (a%b!=0 and b%a!=0):
             break
     numsol=2
+
+choices = [f"{b}  \ll {a}",
+            f"{a}  \ll {b}",
+            f"{a} et {b} ne sont pas comparables"]
 ==
 
 text == 
@@ -27,32 +29,3 @@ Soit $% \ll %$ la relation d'ordre sur $% \mathbb{N}^* %$ définie par : $% p \l
 
 Comment se comparent les entiers $%{{a}}%$ et $%{{b}}%$ par rapport à cette relation d'ordre.
 ==
-
-form ==
-{{input_1}}
-==
-
-input.1.type = radio
-
-input.1.shuffle = no
-
-input.1.choices ==
-$% {{b}}  \ll {{a}} %$ 
-$% {{a}}  \ll {{b}} %$ 
-$%{{a}}%$ et $%{{b}}%$ ne sont pas comparables
-==
-
-input.1.numsol = {{numsol}}
-
-evaluator ==
-if answer['1']==numsol:
-    score=100
-else:
-    score=0
-feedback = answer
-==
-
-
-
-
-
