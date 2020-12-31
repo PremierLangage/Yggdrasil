7329b61d0116a4d69ad555d39f70575df49f9922
Venceslas BIRI <Venceslas.Biri@u-pem.fr>
2020-12-31 15:49:41 +0100

    vbiri modified Yggdrasil/AAAA/Vence/essai.pl

---
+++ AAAA/Vence/essai.pl
@@ -0,0 +1,28 @@
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+before ==
+import random as rd
+a = rd.randint(10, 20)
+b = rd.randint(10, 20)
+==
+
+title = "Addition"
+
+text ==
+Calculer {{ a }} + {{ b }}.
+==
+
+input =: Input
+input.type = number
+
+form ==
+{{ input|component }}
+==
+
+evaluator ==
+if input.value == a + b:
+    score = 100
+else:
+    score = 0
+==
