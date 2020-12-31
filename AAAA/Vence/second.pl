acaa474fc94d98167e2c317afc78597d230aa4a0
Venceslas BIRI <Venceslas.Biri@u-pem.fr>
2020-12-31 15:27:17 +0100

    vbiri modified Yggdrasil/AAAA/Vence/second.pl

--- AAAA/Vence/second.pl
+++ AAAA/Vence/second.pl
@@ -2,12 +2,6 @@
 
 @ /grader/evaluator2.py [grader.py]
 
-before ==
-import random as rd
-a = round(rd.uniform(-5, 5),2)
-b = rd.randint(-5, 5)
-c = rd.randint(-5, 5)
-==
 
 title = "Premier exercice"
 
