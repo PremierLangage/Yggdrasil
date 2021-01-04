ca44c349b3e1d28cd398700a920c5ffe520acff2
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-04 18:47:32 +0100

    nborie modified Yggdrasil/ComputerScience/C/review/Xpose.pl

--- ComputerScience/C/review/Xpose.pl
+++ ComputerScience/C/review/Xpose.pl
@@ -186,6 +186,24 @@ form==#|html|
 <div class="item active">
   <p style="margin-left:15%; margin-right:15%;"><b><u>L'exposé : </u></b></p>
   <br>
+  Ce vendredi 15 janvier 2021 :<br>
+  <ul>
+    <li>Sujet1 : elève 1, élève 2, élève 3</li>
+    <li>Sujet2 : elève 1, élève 2, élève 3</li>
+    <li>Sujet3 : elève 1, élève 2, élève 3</li>
+    <li>Sujet4 : elève 1, élève 2, élève 3</li>
+    <li>Sujet5 : elève 1, élève 2, élève 3</li>
+    <li>Sujet6 : elève 1, élève 2, élève 3</li>
+    <li>Sujet7 : elève 1, élève 2, élève 3</li>
+    <li>Sujet8 : elève 1, élève 2, élève 3</li>
+    <li>Sujet9 : elève 1, élève 2, élève 3</li>
+    <li>Sujet10 : elève 1, élève 2, élève 3</li>
+    <li>Sujet11 : elève 1, élève 2, élève 3</li>
+    <li>Sujet12 : elève 1, élève 2, élève 3</li>
+  </ul>
+
+  <div style="margin-left:15%; margin-right:15%;">
+  </div>
   <div style="margin-left:15%; margin-right:15%;">
     <label for="from_ev">L'évaluateur (Vous) : </label><br>
       <select name="from_ev" id="from_ev">
