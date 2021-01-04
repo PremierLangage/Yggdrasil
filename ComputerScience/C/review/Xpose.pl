0443f926f613c50514d797031feded3d7654027a
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-04 17:23:02 +0100

    nborie modified Yggdrasil/ComputerScience/C/review/Xpose.pl

--- ComputerScience/C/review/Xpose.pl
+++ ComputerScience/C/review/Xpose.pl
@@ -70,7 +70,6 @@ un mail à **nicolas.borie@univ-eiffel.fr** avec les 4 lignes CSV générés par
 après chaque présentation et assemblées par vos soins.
 
 <br>
-<br>
 
 Remplissez bien correctement les 7 volets avant de valider!
 
