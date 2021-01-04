19b30f6df5ef3f35484d197b4bba64cedec343e1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-04 14:46:45 +0100

    nborie modified Yggdrasil/ComputerScience/C/review/Xpose.pl

--- ComputerScience/C/review/Xpose.pl
+++ ComputerScience/C/review/Xpose.pl
@@ -18,62 +18,19 @@
 extends=/ComputerScience/C/examples/Stoi_le_caroussel.pl
 
 
-author = Enseignants Méthodologie
+author=The ESIPE Dream Team !
 bij =$ /AAAA/Quentin/Images/bij.png
 
 # pour changer le nombre de question, c'est en dessous (10 MAX)
 # si le nombre est plus petit que 10, ce sera un tirage aléatoire
 # sans remise parmi le tas de questions.
-nb_quest_voulu = 4
+nb_quest_voulu = 6
 
-title= carroussel
+title=Grille évaluation Exposés Techniques Avancés
 
-
-image1 =$ /AAAA/Quentin/EXAM-JEUDI/Images/bij.png
-image2 =$ /AAAA/Quentin/EXAM-JEUDI/Images/non-appli.png
-image3 =$ /AAAA/Quentin/EXAM-JEUDI/Images/nsur-ninj.png
-image4 =$ /AAAA/Quentin/EXAM-JEUDI/Images/surj-ninj.png
 
 questions==
 
-** On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
 
-![image1]({{ image1 }})
-
-+ f est surjective
-+ f est injective
-+ f est bijective
-- f n'est pas une application
-- f est bien définie mais n'est ni injective ni surjective
-
-** On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
-
-![image2]({{ image2 }})
-
-+ f n'est pas une application
-- f est surjective
-- f est injective
-- f est bijective
-- f est bien définie mais n'est ni injective ni surjective
-
-** On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
-
-![image3]({{ image3 }})
-
-+ f est bien définie mais n'est ni injective ni surjective
-- f est surjective
-- f est injective
-- f est bijective
-- f n'est pas une application
-
-** On considère l'application $!f:E\to F!$ donnée par le dessin ci-contre. Cocher la (ou les) bonne(s) réponse(s).
-
-![image4]({{ image4 }})
-
-- f est bien définie mais n'est ni injective ni surjective
-+ f est surjective
-- f est injective
-- f est bijective
-- f n'est pas une application
 
 ==
