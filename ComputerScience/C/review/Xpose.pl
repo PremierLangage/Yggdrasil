cbd990291447e2c2a5d17f1545775b4f58be2d81
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-04 17:17:15 +0100

    nborie modified Yggdrasil/ComputerScience/C/review/Xpose.pl

--- ComputerScience/C/review/Xpose.pl
+++ ComputerScience/C/review/Xpose.pl
@@ -24,11 +24,50 @@ builder =@ /builder/before.py
 
 before==#|python|
 
+students = [ ("AGULLO", "Vincent"),
+             ("BATICLE", "Nicolas"),
+             ("BESSODES", "Julien"),
+             ("BOHL", "Kylian"),
+             ("BOURJOT", "Raphaël"),
+             ("CAU", "Guillaume"),
+             ("CRETE", "Jonathan"),
+             ("DA COSTA", "Mélissa"),
+             ("DESCOMBES", "Anaïs"),
+             ("DINDANE", "Chahinaz"),
+             ("DURAND", "Axel"),
+             ("FALL", "Abdou"),
+             ("FAU", "Nicolas"),
+             ("JOLIVET", "Dylan"),
+             ("JOLIVET", "Yohann"),
+             ("KHEROUA", "Karim"),
+             ("KOFFI", "Judicaël"),
+             ("LEMOINE", "Nathan"),
+             ("MARSZAL", "Rémi"),
+             ("MARTI", "Emilie"),
+             ("MARTIN", "Kevin"),
+             ("MATHECOWITSCH", "Guillaume"),
+             ("MECHOUK", "Lisa"),
+             ("MERCHERMEK", "Salim"),
+             ("NGUYEN", "Christelle"),
+             ("OUCH", "Léo"),
+             ("OUZIEL", "Sacha"),
+             ("PERNET", "Nils"),
+             ("QUANTIN", "Benoît"),
+             ("SICOT", "Yoann"),
+             ("SORAN", "Altan"),
+             ("SOUSTRE", "Ludovic"),
+             ("TRAINA", "Tanguy"),
+             ("WINCKLER", "Jean-Philippe") ]
+
 ==
 
 title=Grille évaluation Exposés Techniques Avancés
 
 text==
+Pas de papier cette année... Nous allons économiser (34 + 6)*3 feuilles A4 pour
+les évaluations. En contrepartie, vous devrez chacun, et chaque semaine, envoyer 
+un mail à nicolas.borie@univ-eiffel.fr avec les 4 lignes CSV générés par cet utilitaire 
+après chaque présentation.
 
 ==
 
