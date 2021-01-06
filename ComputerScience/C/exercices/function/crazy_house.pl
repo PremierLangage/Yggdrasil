f9d544467a22aee819708590b24bb87a9f37ad8b
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-06 22:28:28 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/function/crazy_house.pl

--- ComputerScience/C/exercices/function/crazy_house.pl
+++ ComputerScience/C/exercices/function/crazy_house.pl
@@ -63,47 +63,67 @@ comprises). Le placement des espaces et des retours chariots est
 primordial.
 ==
 
-editor.code==
-... cube_it(...){
-  ...
+editor.code==#|c|
+void maison(int n){
+  /* Votre code ici... */
 }
 ==
 
+solution==#|c|
+void maison(int n){
+  int i, j;
 
-solution==
-
-void cube_it(int *p)
-{
-    *p = *p * *p * *p;
+  for (i=0 ; i<n ; i++){
+    for (j=0 ; j<n-1-i ; j++)
+      putchar(' ');
+    putchar('/');
+    for (j=0 ; j<2*i ; j++)
+      putchar(' ');
+    putchar('\\');
+    if (i == n-1){
+      for (j=0 ; j<n ; j++)
+	putchar('_');	
+    }
+    putchar('\n');
+  }
+  for (i=0 ; i<n-1 ; i++){
+    putchar('|');
+    for (j=0 ; j<3*n-2 ; j++)
+      putchar(' ');
+    putchar('|');
+    putchar('\n');
+  }
+  putchar('|');
+  for (j=0 ; j<3*n-2 ; j++)
+    putchar('_');
+  putchar('|');
+  putchar('\n');
 }
-
 ==
 
-code_before==
-
+code_before==#|c|
 #include <stdio.h>
 #include <stdlib.h>
-
 ==
 
-code_after==
-
+code_after==#|c|
 int main(int argc, char* argv[]){
-  int a = atoi(argv[1]);
-  cube_it(&a);
-  printf("Cube --> %d\n",a);
+  int n=atoi(argv[1]);
+
+  maison(n);
   return 0;
 }
-
 ==
 
-checks_args_stdin==
-[["Test basique", ["1"], ""],
- ["Simple nombre négatif", ["-3"], ""],
- ["Test aléatoire 1", [str(randint(1, 33))], ""],
- ["Test aléatoire 2", [str(randint(-50, -12))], ""],
- ["Test aléatoire 3", [str(randint(-100, 100))], ""],
- ["Test aléatoire 4", [str(randint(-100, 100))], ""]]
+checks_args_stdin==#|python|
+[ ["Test basique", ["1"], ""],
+  ["Test basique", ["2"], ""],
+  ["Test basique", ["3"], ""],
+  ["Test basique", ["4"], ""],
+  ["Test basique", ["5"], ""],
+  ["Test aléatoire 1", [str(randint(6,8)], ""],
+  ["Test aléatoire 2", [str(randint(9,11)], ""],
+  ["Test aléatoire 3", [str(randint(12,14)], ""] ]
 ==
 
 
