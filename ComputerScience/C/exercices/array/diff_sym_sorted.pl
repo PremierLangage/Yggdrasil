1b68872325e7e14c273e5e60ad4613cde47ffa7c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-06 14:46:02 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/array/diff_sym_sorted.pl

--- ComputerScience/C/exercices/array/diff_sym_sorted.pl
+++ ComputerScience/C/exercices/array/diff_sym_sorted.pl
@@ -24,21 +24,48 @@ tag=array|affichage|tableau|fonction
 editor.height=280px
 
 text==#|markdown|
-Écrire une fonction qui affiche le contenu d'un tableau d'entiers C. Cette
-fonction prendra en paramèrtre un tableau d'entiers ainsi que le
-nombre d'éléments contenus dans ce tableau. On commencera l'affichage 
-avec un crochet ouvrant, puis les entiers séparés par une virgule 
-s'ils sont plusieurs et enfin un crochet fermant en fin de tableau. 
-Ne pas mettre de retour chariot en fin d'affichage. Les virgules, quand 
-elles existent, sont suivies d'un espace.
+L'objectif est ici de concevoir une fonction performante pour
+visualiser la différence symmétrique entre deux ensembles. On peut
+facilement représenter un ensemble d'entiers avec des tableaux triés
+(strictement d'ailleurs car il ne peut pas y avoir de répétitions). 
+Par exemple, un ensemble  $\lbrace 12, 5, 7, 14, 2 \rbrace$ peut
+être encodé de manière canonique par le tableau C de taille 5 : 
+`[2, 5, 7, 12, 14]`.
 
 
-<pre>
-1,2,3,4 --> [1, 2, 3, 4]   
-5       --> [5]   
-rien    --> []
-</pre>
+La différence symmétrique $A \bigtriangleup B$ entre deux ensembles $A$ et $B$ est par 
+définition 
 
+$$ A \bigtriangleup B := \lbrace x \in A | x \notin B \rbrace \cup \lbrace x \in B | x \notin A \rbrace = (A \smallsetminus B) \cup (B \smallsetminus A) = (A \cup B) \smallsetminus (A \cap B)$$
+
+En francais, les éléments de $A \bigtriangleup B$ sont seulement dans $A$ ou
+seulement dans $B$. Maintenant, si $A$ et $B$ sont deux tableaux triés
+de manière strictement croissante, vous devrez coder une fonction dont le
+prototype sera : 
+
+`void print_diff_sym(int* A, int s1, int* B, int s2)` 
+
+Cette dernière devra afficher le contenu de la différence symmétrique toujours de manière
+triée. Il est possible de la faire efficacement, en seule passe, avec
+double têtes de lecture. Les têtes de lecture pointent toujours sur les
+minimums respectifs de $A$ et de $B$. Si ces derniers sont différents,
+on affiche le plus petit des deux et on avance la tête de lecture
+associée. Si les deux têtes pointent vers le même élément, on affiche
+rien (car l'élément est dans les deux ensembles en même temps) et on
+avance les deux têtes de lecture d'un cran.
+
+Voici des exemples d'éxécution avec des tableaux de taille 10 aléatoires.
+
+```
+nborie@bayer:$ ./pouet 
+A : 1 3 5 6 7 9 13 14 16 18 
+B : 2 3 7 10 13 14 16 18 22 25 
+A diff B : 1 2 5 6 9 10 22 25 
+nborie@bayer:$ ./pouet 
+A : 1 4 6 7 8 12 15 16 18 20 
+B : 2 6 8 9 10 13 17 21 22 26 
+A diff B : 1 2 4 7 9 10 12 13 15 16 17 18 20 21 22 26 
+```
 ==
 
 code_before==
@@ -47,53 +74,18 @@ code_before==
 
 code_after==#|c|
 
-#include <stdlib.h>
-
-int main(int argc, char* argv[]){
-  int* tab=malloc(argc*sizeof(int));
-  int i;
-
-  for(i=0 ; i<argc-1 ; i++)
-    tab[i] = atoi(argv[i+1]);
-
-  display_array(tab, argc-1);
-  return 0;
-}
 ==
 
 editor.code==
-#include ...
 
-void display_array(int* array, int nb_term){
-  /* votre code ici... */
-}
 ==
 
 solution==#|c|
-#include <stdio.h>
 
-void display_array(int* array, int nb_term){
-  int i;
-
-  if (nb_term == 0){
-    printf("[]");
-    return ;
-  }
-  printf("[");
-  for (i=0 ; i<nb_term-1 ; i++)
-    printf("%d, ", array[i]);
-  printf("%d]", array[nb_term-1]);
-}
 ==
 
 checks_args_stdin==#|python|
-[["Exécution simple", ["1", "2", "3", "4"], ""],
- ["Tableau singleton", ["42"], ""],
- ["Tableau vide", [], ""],
- ["Test aléatoire 1", [str(randint(-100, 100)) for i in range(randint(0, 10))], ""],
- ["Test aléatoire 2", [str(randint(-100, 100)) for i in range(randint(0, 10))], ""],
- ["Test aléatoire 3", [str(randint(-100, 100)) for i in range(randint(0, 10))], ""],
- ["Test aléatoire 4", [str(randint(-100, 100)) for i in range(randint(0, 10))], ""]]
+[ ["Exécution simple", ["1", "2", "3", "4"], ""], ]
 ==
 
 
