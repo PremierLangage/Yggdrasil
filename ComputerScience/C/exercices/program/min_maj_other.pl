daf001c1e107e6e8004af05209c89f21f21fb6f9
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-06 14:18:13 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/program/min_maj_other.pl

---
+++ ComputerScience/C/exercices/program/min_maj_other.pl
@@ -0,0 +1,47 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
+
+author=Nicolas Borie
+
+title=Afficher les arguments d'un programme C
+tag=programme|argument|terminal|affichage
+
+text==
+
+==
+
+editor.code==
+
+==
+
+code_before==
+
+==
+
+code_after==
+
+==
+
+solution==
+
+==
+
+checks_args_stdin==
+[ ["Test basique", ["argument1"], ""], ]
+==
+
