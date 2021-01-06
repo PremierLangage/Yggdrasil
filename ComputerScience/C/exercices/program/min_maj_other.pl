52b677738e77f1b3dd81c64938acb05936637509
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-06 14:22:38 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/program/min_maj_other.pl

--- ComputerScience/C/exercices/program/min_maj_other.pl
+++ ComputerScience/C/exercices/program/min_maj_other.pl
@@ -18,30 +18,64 @@ extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
 
-title=Afficher les arguments d'un programme C
-tag=programme|argument|terminal|affichage
+title=Compter trois types de lettres
+tag=programme|argument|terminal|affichage|type|char|chaîne
 
 text==
+Écrire un programme C qui lit son premier argument et compte les
+caractères de différents types dans le premier argument donné au
+programme. Il y aura trois types de caractères à compter : les lettres
+minuscules, les lettres majuscules et le reste du monde. Une fois le
+comptage terminé, votre programme devra faire un résumé de ce qu'il a
+compté sur la sortie standard via l'affichage suivant :
+
+```
+Nombre de majuscules : X   
+Nombre de minuscules : Y   
+Nombre d'autres caractères : Z   
+```
+
+Où X, Y et Z seront remplacés par les entiers qui vont bien. Pas besoin
+d'adapter les pluriels selon les comptes trouvés. 
+==
+
+editor.code==#|c|
 
 ==
 
-editor.code==
+code_before==#|c|
 
 ==
 
-code_before==
+code_after==#|c|
 
 ==
 
-code_after==
+solution==#|c|
+#include <stdio.h>
 
+int main(int argc, char* argv[]){
+  int nb_min=0;
+  int nb_maj=0;
+  int nb_other=0;
+  int i;
+
+  for (i=0 ; argv[1][i] != '\0' ; i++){
+    if ((argv[1][i] >= 'a') && (argv[1][i] <= 'z'))
+      nb_min++;
+    else if ((argv[1][i] >= 'A') && (argv[1][i] <= 'Z'))
+      nb_maj++;
+    else
+      nb_other++;
+  }
+  printf("Nombre de majuscules : %d\n", nb_maj);
+  printf("Nombre de minuscules : %d\n", nb_min);
+  printf("Nombre d'autres caractères : %d\n", nb_other);
+  return 0;
+}
 ==
 
-solution==
-
-==
-
-checks_args_stdin==
+checks_args_stdin==#|python|
 [ ["Test basique", ["argument1"], ""], ]
 ==
 
