74edfdd51693e2c99f49c5cb42588a61091eec41
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-07 09:17:18 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/bitwise/last_pack_size.pl

--- ComputerScience/C/exercices/bitwise/last_pack_size.pl
+++ ComputerScience/C/exercices/bitwise/last_pack_size.pl
@@ -1,6 +1,20 @@
-# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# Taille du dernier paquet de bit à 1
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
 title=Taille du paquet de bits de poids faible
