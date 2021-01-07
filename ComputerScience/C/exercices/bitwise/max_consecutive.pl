c946c5091571d303bf613b9dedac01ea0162fa1a
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-07 09:58:36 +0100

    nborie modified Yggdrasil/ComputerScience/C/exercices/bitwise/max_consecutive.pl

--- ComputerScience/C/exercices/bitwise/max_consecutive.pl
+++ ComputerScience/C/exercices/bitwise/max_consecutive.pl
@@ -1,13 +1,27 @@
-# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
-# nombre maximal de bits consécutif
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
 
 author=Nicolas Borie
-title=Taille du plus grand paquet de bits consécutifs égaux
-tag=bitwise|function
-extends=/ComputerScience/C/template/stdsandboxC.pl
 
-text==
+title=Taille du plus grand paquet de bits consécutifs égaux
+tag=bitwise|fonction|comptage|consécutifs|zone|pointeur|void*
+
+text==#|markdown|
 Écrire une fonction C **max_consecutive** qui prend en argument l'adresse 
 d'une zone mémoire **seq** ainsi que sa taille en octets **nb_oct** et
 retourne le nombre maximum de bits consécutifs de même valeur dans la
@@ -16,15 +30,13 @@ information, il est possible de le faire en une seule lecture.
 
 ==
 
-editor.code==
+editor.code==#|c|
 int max_consecutive(void* seq, size_t nb_oct){
   /* ... votre code ici ... */
 }
-
 ==
 
-solution==
-
+solution==#|c|
 int max_consecutive(void* seq, size_t nb_oct){
   int i, j;
   unsigned char* s = (unsigned char*)seq;
@@ -52,19 +64,14 @@ int max_consecutive(void* seq, size_t nb_oct){
 
   return max_pack;
 }
-
-
 ==
 
-codebefore==
-
+code_before==#|c|
 #include <stdio.h>
 #include <stdlib.h>
-
 ==
 
-codeafter==
-
+code_after==#|c|
 int main(int argc, char* argv[]){
   unsigned char s[100];
   int i;
@@ -75,22 +82,18 @@ int main(int argc, char* argv[]){
   printf("max consecutive : %d\n", max_consecutive((void*)s, argc-1));
   return 0;
 }
-
-
 ==
 
-tests==
-
-[["Exécution simple", "1", ""],
- ["Pas de bit", "0 0 0 0", ""],
- ["64 bits", "255 255 255 255 255 255 255 255", ""],
- ["Alternance", "255 0 255 0 255 0 255 0", ""],
- ["Aléatoire", " ".join([str(random.randint(0,255)) for i in range(random.randint(2, 20))]), ""],
- ["Aléatoire", " ".join([str(random.randint(0,255)) for i in range(random.randint(2, 20))]), ""],
- ["Aléatoire", " ".join([str(random.randint(0,255)) for i in range(random.randint(2, 20))]), ""],
- ["Aléatoire", " ".join([str(random.randint(0,255)) for i in range(random.randint(2, 20))]), ""],
- ["Aléatoire", " ".join([str(random.randint(0,255)) for i in range(random.randint(2, 20))]), ""]]
-
+checks_args_stdin==#|python|
+[["Exécution simple", ["1"], ""],
+ ["Pas de bit", ["0", "0", "0", "0"], ""],
+ ["64 bits", ["255", "255", "255", "255", "255", "255", "255", "255"], ""],
+ ["Alternance", ["255", "0", "255", "0", "255", "0", "255", "0"], ""],
+ ["Test aléatoire 1", [str(randint(0,255)) for i in range(randint(2, 20))], ""],
+ ["Test aléatoire 2", [str(randint(0,255)) for i in range(randint(2, 20))], ""],
+ ["Test aléatoire 3", [str(randint(0,255)) for i in range(randint(2, 20))], ""],
+ ["Test aléatoire 4", [str(randint(0,255)) for i in range(randint(2, 20))], ""],
+ ["Test aléatoire 5", [str(randint(0,255)) for i in range(randint(2, 20))], ""] ]
 ==
 
 
