77cdf9992cbb563fb4e12637f333e08783cd16b6
David DOYEN <David.Doyen@u-pem.fr>
2021-01-08 20:15:26 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
@@ -1,6 +1,14 @@
 extends = /model/math.pl
 extends = /model/basic/radio.pl
+
 title = Vérifier une solution
+
+choices ==
+Oui
+Non, car la 1ère équation n'est pas vérifiée.
+Non, car la 2e équation n'est pas vérifiée.
+Non, car la 3e équation n'est pas vérifiée.
+==
 
 before ==
 n=param['size']
@@ -15,24 +23,16 @@ k=randint(0,n)
 
 if k>0:
     B[k-1]+=randitem([-1,1])
-
-choices=["Oui",
-         "Non, car la 1ère équation n'est pas vérifiée.",
-         "Non, car la 2e équation n'est pas vérifiée.",
-         "Non, car la 3e équation n'est pas vérifiée."]
-
 lstvar=",".join(["x","y","z","t","u","v"][:n])
 tuple_name=["couple","triplet","quadruplet","quintuplet"][n-2]
 
-sys_tex=latexsys(A,B)
-radio.setitems(choices)
-radio.setsol_from_index(k)
+sys_tex = latex_linsys(A, B)
 ==
 
 text ==
 On considère le système suivant (d'inconnues $! {{lstvar}} !$) :
 $$ {{sys_tex}} $$
 
-Le {{tuple_name}} $! {{vec_tex}} !$ est-il solution de ce système ?
+Le {{tuple_name}} $! {{vec|latex}} !$ est-il solution de ce système ?
 ==
 
