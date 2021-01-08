fc01af78b1182374f626240bf011f6602369fbda
David DOYEN <David.Doyen@u-pem.fr>
2021-01-08 20:18:53 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_.pl
@@ -11,28 +11,24 @@ Non, car la 3e équation n'est pas vérifiée.
 ==
 
 before ==
-n=param['size']
-coeffbound = param['coeffbound']
-sparsity= param['sparsity']
-A=rand_int_matrix_invertible(n,3)
-vec=list_randint(n,-3,3,[0])
-B=A*Matrix(vec)
+n = 3
+A = rand_int_matrix_invertible(n, 3)
+vec = list_randint(n,-3,3,[0])
+B = A*Matrix(vec)
 
 vec_tex=str(tuple(vec))
-k=randint(0,n)
+nbsol = randint(0, n)
 
-if k>0:
-    B[k-1]+=randitem([-1,1])
-lstvar=",".join(["x","y","z","t","u","v"][:n])
-tuple_name=["couple","triplet","quadruplet","quintuplet"][n-2]
+if nbsol > 0:
+    B[nbsol-1] += randitem([-2, -1, 1, 2])
 
 sys_tex = latex_linsys(A, B)
 ==
 
 text ==
-On considère le système suivant (d'inconnues $! {{lstvar}} !$) :
+On considère le système suivant (d'inconnues $! x, y, z !$) :
 $$ {{sys_tex}} $$
 
-Le {{tuple_name}} $! {{vec|latex}} !$ est-il solution de ce système ?
+Le triplet $! {{vec|latex}} !$ est-il solution de ce système ?
 ==
 
