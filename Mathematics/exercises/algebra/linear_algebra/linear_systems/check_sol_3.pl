6b8f4be00d614a73fd571d89071d627e10907328
David DOYEN <David.Doyen@u-pem.fr>
2021-01-09 18:31:08 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_3.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_3.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/check_sol_3.pl
@@ -20,7 +20,7 @@ before ==
 n = 3
 bound = 3
 A = randint_matrix_invertible(n, bound, excluded_values=[0])
-vec = list_randint(n, -3, 3, [0])
+vec = list_randint(n, -3, 3)
 B = A*Matrix(vec)
 vec = tuple(vec)
 
