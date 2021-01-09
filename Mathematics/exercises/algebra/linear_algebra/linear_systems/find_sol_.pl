ca30653771ee3f130d1de2bbd1fd5ed69cf08ee2
David DOYEN <David.Doyen@u-pem.fr>
2021-01-09 18:30:05 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/find_sol_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/find_sol_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/find_sol_.pl
@@ -1,31 +1,32 @@
-extends = /model/mathradio.pl
+# Author : D. Doyen
+# Last update : 09/01/21
+# Keywords : linear systems
+
+extends = /model/math/math.pl
+extends = /model/basic/radio.pl
+
 
 title = Trouver une solution parmi plusieurs
 
 before ==
-n=param['size']
-coeffbound = param['coeffbound']
-sparsity= param['sparsity']
-A=rand_int_matrix_invertible(n,3)
+n = 3
+bound = 3
+A = randint_matrix_invertible(n, bound, excluded_values=[0])
 
-vec=list_randint(n,-3,3)
-B=A*Matrix(vec)
-choices=["$! %s !$" % str(tuple(vec))]
+vec = list_randint(n, -3, 3, [0])
+B = A*Matrix(vec)
+choices = ["$! %s !$" % str(tuple(vec))]
 while len(choices)<4:
     vec=list_randint(n,-3,3)
     if vec not in choices:
         choices.append("$! %s !$" % str(tuple(vec)))
 
-lstvar=",".join(["x","y","z","t","u","v"][:n])
-sys_tex=latexsys(A,B)
+sys_tex = latex_linsys(A, B)
 
-radio.setitems(choices)
-radio.setsol_from_index(0)
-radio.shuffle()
 ==
 
 text ==
-On considère le système suivant (d'inconnues $! {{lstvar}} !$) :
+On considère le système suivant (d'inconnues $!x, y, z !$) :
 $$ {{sys_tex}} $$
 
 Parmi les propositions suivantes, laquelle est la solution de ce système ?
