11c2a60e181a2ec36afc6a0be15d868d277be710
Justine Falque <justine.falque@lilo.org>
2021-01-10 20:37:53 +0100

    jfalque modified Yggdrasil/AAAA/Justine/somme_dicos.pl

---
+++ AAAA/Justine/somme_dicos.pl
@@ -0,0 +1,57 @@
+
+
+
+# Author: Justine Falque
+
+extends=/ComputerScience/python/template/pltest.pl
+@ /builder/before.py [builder.py]
+
+#author=Justine Falque
+title="Sommer" deux dictionnaires
+
+text==
+
+Ecrire une fonction ``somme_dicos(d1, d2)`` qui prend en paramètre deux dictionnaires dont les valeurs sont des nombres.
+La fonction renvoie un dictionnaire dont les clés sont toutes les clés de ``d1`` et ``d2``, avec la valeur qui leur
+était associée dans le dictionnaire d'origine si la clé appartient à un seul des deux, et la somme des valeurs associées
+si la clé appartient aux deux dictionnaires.
+
+Par exemple:
+
+{{pltest}}
+==
+
+tag=dictionary|ForIn|algo
+
+samplesol==
+def somme_dicos(d1, d2):
+    d = d2.copy()
+    for cle1 in d1 :
+        if cle1 in d :
+            d[cle1] += d1[cle1]
+        else :
+            d[cle1] = d1[cle1]
+    return d
+==
+
+before==
+==
+
+pltest==
+>>> somme_dicos({'a': 1, 'b': 10}, {'b': 2, 'c':20})
+{'a': 1, 'b': 12, 'c': 20}
+==
+
+pltest0==
+>>> somme_dicos({}, {}) == {}  # Test Avec Deux Vides
+True
+>>> somme_dicos({'a': 3, 'b': 10}, {}) == {'a': 3, 'b': 10}  # Test Avec Un Vide
+True
+>>> somme_dicos({'a': 3, 'b': 10}, {'a': 2, 'c':20}) == {'a': 5, 'b': 10, 'c': 20}  # Test Non Vide 1
+True
+>>> somme_dicos({'a': 3.5, 'b': 10}, {'a': -6, 'c':20}) == {'a': -2.5, 'b': 10, 'c': 20}  # Test Non Vide 2
+True
+==
+
+
+
