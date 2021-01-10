7f99c2f3d2d7f2f0202a8615ee0b581e2e0654d2
Justine Falque <justine.falque@lilo.org>
2021-01-10 22:12:05 +0100

    jfalque modified Yggdrasil/AAAA/Justine/affichage_identite.pl

--- AAAA/Justine/affichage_identite.pl
+++ AAAA/Justine/affichage_identite.pl
@@ -10,15 +10,11 @@ title=Affichage
 
 text==
 
-Ecrire une fonction ``superhero(hero)`` qui prend en paramètre un dictionnaire ``hero`` dont les clés 
+Ecrire une ligne de code qui affiche le contenu d'un dictionnaire ``hero`` dont les clés 
 sont ``'nom'``, ``'prenom'`` et ``'pseudo'`` et qui affiche ``Prénom Nom se fait appeler pseudo.``
 (ne pas oublier pas le point).
 
-Par exemple :
-
-``>>> hero1 = {'prenom': 'Bruce', 'nom': 'Wayne', 'pseudo': 'Batman'}``
-
-``>>> superhero(hero1)``
+Par exemple, si ``hero = {'prenom': 'Bruce', 'nom': 'Wayne', 'pseudo': 'Batman'}``, l'affichage doit être :
 
 ``Bruce Wayne se fait appeler Batman.``
 ==
@@ -26,23 +22,17 @@ Par exemple :
 tag=dictionary|print
 
 soluce==
-def superhero(hero):
-    print(hero['prenom'], hero['nom'], "se fait appeler", hero['pseudo'])
+print(hero['prenom'], hero['nom'], "se fait appeler", hero['pseudo'])
 ==
 
 before==
-==
-
-cache==
 hero1 = {'prenom': 'Bruce', 'nom': 'Wayne', 'pseudo': 'Batman'}
 hero2 = {'prenom': 'Peter', 'nom': 'Parker', 'pseudo': 'Spiderman'}
 hero3 = {'prenom': 'Selina', 'nom': 'Kyle', 'pseudo': 'Catwoman'}
-==
-
-after==
-superhero(hero3)
+hero = hero3
 ==
 
 mplsoluce==
+Test
 ==
 
