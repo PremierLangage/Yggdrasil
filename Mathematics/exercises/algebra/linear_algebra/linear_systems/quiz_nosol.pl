a420dda5b45dd0685543e64f0eac14bccbbe90c0
David DOYEN <David.Doyen@u-pem.fr>
2021-01-10 21:47:53 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl
@@ -0,0 +1,30 @@
+# Author : D. Doyen
+# Last update : 09/01/21
+# Keywords : linear systems
+
+extends = /model/math/math.pl
+extends = /model/basic/radio.pl
+
+
+title = Trouver une solution parmi plusieurs
+
+before ==
+n = 3
+bound = 3
+A = randint_matrix_givenrank(2, 3, 2, magnitude=2)
+
+vec = list_randint(2, -3, 3)
+B = A*Matrix(vec)
+
+
+sys_tex = latex_linsys(A, B)
+
+==
+
+text ==
+On considère le système suivant (d'inconnues $!x, y, z !$) :
+$$ {{sys_tex}} $$
+
+Parmi les propositions suivantes, laquelle est la solution de ce système ?
+==
+
