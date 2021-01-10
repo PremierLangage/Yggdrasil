5974a271136cdf123eb9bbe484be47f008509684
David DOYEN <David.Doyen@u-pem.fr>
2021-01-11 00:43:23 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl
@@ -16,13 +16,13 @@ Aucune solution
 title = Nombre de solutions d'un sytème 3x3
 
 before ==
-n = 3
-bound = 3
-A = randint_matrix_givenrank(3, 2, 2, magnitude=2)
-
-
-vec = list_randint(2, -3, 3)
-B = A*Matrix(vec)
+indexsol = randint(0, 2)
+if indexsol == 0:
+    A = randint_matrix_givenrank(3, 3, , magnitude=2)
+    B = A*Matrix(list_randint(2, -3, 3))
+else:
+    A = randint_matrix_givenrank(3, 2, , magnitude=2)
+    vec = list_randint(2, -3, 3) 
 
 
 sys_tex = latex_linsys(A, B)
@@ -32,5 +32,6 @@ sys_tex = latex_linsys(A, B)
 text ==
 On considère le système suivant (d'inconnues $!x, y, z !$) :
 $$ {{sys_tex}} $$
+Combion de solutions possède ce système ?
 ==
 
