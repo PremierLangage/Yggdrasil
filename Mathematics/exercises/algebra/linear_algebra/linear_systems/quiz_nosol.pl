3c6203f30161399f72dfcafbf76abd08ae633619
David DOYEN <David.Doyen@u-pem.fr>
2021-01-11 00:46:15 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl
@@ -18,11 +18,12 @@ title = Nombre de solutions d'un sytème 3x3
 before ==
 indexsol = randint(0, 2)
 if indexsol == 0:
-    A = randint_matrix_givenrank(3, 3, , magnitude=2)
+    A = randint_matrix_givenrank(3, 3, 3, magnitude=2)
     B = A*Matrix(list_randint(2, -3, 3))
 else:
-    A = randint_matrix_givenrank(3, 2, , magnitude=2)
+    A = randint_matrix_givenrank(3, 3, 2, magnitude=2)
     vec = list_randint(2, -3, 3) 
+    if indexsol == 2:
 
 
 sys_tex = latex_linsys(A, B)
