903bf3c5f4ff95113b58d6b7b90b176ab26f2f1e
David DOYEN <David.Doyen@u-pem.fr>
2021-01-10 23:02:01 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/quiz_nosol.pl
@@ -17,7 +17,7 @@ title = Trouver une solution parmi plusieurs
 before ==
 n = 3
 bound = 3
-A = randint_matrix_givenrank(3, 3, 2, magnitude=1)
+A = randint_matrix_givenrank(2, 3, 2, magnitude=1)
 
 vec = list_randint(3, -3, 3)
 B = A*Matrix(vec)
