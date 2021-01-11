d840322d4d81234991ab3867d290b6783b532369
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-11 15:46:36 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sumaMagda.pl

--- AAAA/ECanovas/sumaMagda.pl
+++ AAAA/ECanovas/sumaMagda.pl
@@ -5,13 +5,14 @@ title= Somme finie
 
 text==
 Donner le résultat de la somme de {{i}} et {{n}}.
-$! \Sigma_{i={{i}}}^{{n}} {{n}} !$
+$! \Sigma_{i={{i}}}^{{n}} {{term}} !$
 ==
 
 before ==
 i = randint(0,5)
 
 term=["k","k^2","k^3"]
+t=term[randint(0,2)]
 C=randint(1,5)
 
 expr=["n","n-1","n+1","2n+1","2n-1",randint(i+1,i+5)]
