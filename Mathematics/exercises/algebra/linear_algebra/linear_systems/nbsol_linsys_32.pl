464a32f54138617a55a5ddeaccd639d0325524d5
David DOYEN <David.Doyen@u-pem.fr>
2021-01-11 14:54:04 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/nbsol_linsys_32.pl

---
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/nbsol_linsys_32.pl
@@ -0,0 +1,29 @@
+# Author : D. Doyen
+# Last update : 09/01/21
+# Keywords : linear systems
+
+extends = nbsol_linsys_.pl
+
+title = Nombre de solutions d'un sytème 3x2
+
+before ==
+n = 3
+m = 2
+indexsol = randitem([1, 2])
+vec = list_randint(m, -2, 2)
+
+if indexsol == 1:
+    A = randint_matrix_givenrank(n, m, n, magnitude=2)
+    B = A*Matrix(vec)
+else:
+    A = randint_matrix_givenrank(n, m, n, magnitude=2)
+    while True:
+        perturbation = [0, randitem([-1,1])]
+        rd.shuffle(perturbation)
+        B = A*Matrix(vec) + Matrix(perturbation)
+        if linsolve((A, B)) == EmptySet:
+            break
+
+system = latex_linsys(A, B)
+lstvar = "x, y, z"
+==
