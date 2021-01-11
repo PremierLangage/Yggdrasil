52475fe79229e75869c9722b63b8a9aedf4f417c
David DOYEN <David.Doyen@u-pem.fr>
2021-01-11 14:21:04 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/linear_algebra/linear_systems/nbsol_linsys_.pl

--- Mathematics/exercises/algebra/linear_algebra/linear_systems/nbsol_linsys_.pl
+++ Mathematics/exercises/algebra/linear_algebra/linear_systems/nbsol_linsys_.pl
@@ -5,6 +5,8 @@
 extends = /model/math/math.pl
 extends = /model/basic/radio.pl
 
+title = Nombre de solutions d'un sytème 3x3
+
 shuffle % false
 
 choices ==
@@ -13,9 +15,28 @@ Une infinité de solutions
 Aucune solution
 ==
 
-title = Nombre de solutions d'un sytème 3x3
+before ==
+indexsol = randint(0, 2)
+vec = list_randint(3, -2, 2)
 
+if indexsol == 0:
+    A = randint_matrix_givenrank(3, 3, 3, magnitude=2)
+    B = A*Matrix(vec)
+elif indexsol == 1:
+    A = randint_matrix_givenrank(3, 3, 2, magnitude=2)
+    B = A*Matrix(vec)
+else:
+    A = randint_matrix_givenrank(3, 3, 2, magnitude=2)
+    while True:
+        perturbation = [0, 0, randitem([-1,1])]
+        rd.shuffle(perturbation)
+        B = A*Matrix(vec) + Matrix(perturbation)
+        if linsolve((A, B)) == EmptySet:
+            break
 
+system = latex_linsys(A, B)
+lstvar = "x, y, z"
+==
 
 text ==
 On considère le système suivant (d'inconnues $!x, y, z !$) :
