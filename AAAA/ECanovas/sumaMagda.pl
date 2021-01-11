6fbfcf1fc6e97f3f372abcb1332acf473098a0f1
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-11 09:15:34 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sumaMagda.pl

--- AAAA/ECanovas/sumaMagda.pl
+++ AAAA/ECanovas/sumaMagda.pl
@@ -1,8 +1,6 @@
 extends = /model/mathinput.pl
 
-
 title= Somme finie
-
 
 text==
 
@@ -10,8 +8,11 @@ Donner le résultat de la somme de {{a}} et {{b}}.
 
 ==
 
-a % 3
-b % 5
+before ==
+n = rand_int(2,11)
+i = rand_int(1,5)
+expr = P * Q
+==
 
 form ==
 la somme    
@@ -22,7 +23,7 @@ la somme
 # évaluer la réponse 
 evaluator==
 
-if int(input1.value) == a+b and int(input2.value) == a*b :
+if int(input1.value) == a+b :
     grade=(100,"Bravo ")
     print("")
 else:
