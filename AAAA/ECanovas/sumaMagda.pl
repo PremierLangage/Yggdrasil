40a3c666498ea6bb9b65ee415ccab20fac4ef2cc
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-11 15:38:07 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sumaMagda.pl

--- AAAA/ECanovas/sumaMagda.pl
+++ AAAA/ECanovas/sumaMagda.pl
@@ -4,32 +4,16 @@ extends = /model/mathinput.pl
 title= Somme finie
 
 text==
-
 Donner le résultat de la somme de {{i}} et {{n}}. 
-
 ==
 
 before ==
-expr=("n","n-1","n+1","2n+1","2n-1")
+expr=["n","n-1","n+1","2n+1","2n-1"]
 n = expr[randint(0,4)]
 i = randint(0,5)
-
 ==
 
-form ==
-la somme    
-#{{ input1|component}}
+form==
+Votre réponse
+{{ input|component }}
 ==
-
-
-# évaluer la réponse 
-evaluator==
-
-if int(input1.value) == i :
-    grade=(100,"Bravo ")
-    print("")
-else:
-    grade=(0," Raté recommencez "+str(input1.value))
-
-==
-
