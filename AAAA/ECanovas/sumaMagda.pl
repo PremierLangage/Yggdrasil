794b425e59f4709a4b096503733bd234a1b1058a
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-11 15:44:32 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sumaMagda.pl

--- AAAA/ECanovas/sumaMagda.pl
+++ AAAA/ECanovas/sumaMagda.pl
@@ -4,13 +4,18 @@ extends = /model/mathinput.pl
 title= Somme finie
 
 text==
-Donner le résultat de la somme de {{i}} et {{n}}. 
+Donner le résultat de la somme de {{i}} et {{n}}.
+$! \Sigma_{i={{i}}}^{{n}} {{n}} !$
 ==
 
 before ==
-expr=["n","n-1","n+1","2n+1","2n-1"]
-n = expr[randint(0,4)]
 i = randint(0,5)
+
+term=["k","k^2","k^3"]
+C=randint(1,5)
+
+expr=["n","n-1","n+1","2n+1","2n-1",randint(i+1,i+5)]
+n = expr[randint(0,5)]
 ==
 
 form==
