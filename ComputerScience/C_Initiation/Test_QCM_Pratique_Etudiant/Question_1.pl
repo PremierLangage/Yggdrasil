b3fcc093a64bb1b8a0e9690da4efeda959371c5d
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2021-01-11 11:43:16 +0100

    ochampalle modified Yggdrasil/ComputerScience/C_Initiation/Test_QCM_Pratique_Etudiant/Question_1.pl

--- ComputerScience/C_Initiation/Test_QCM_Pratique_Etudiant/Question_1.pl
+++ ComputerScience/C_Initiation/Test_QCM_Pratique_Etudiant/Question_1.pl
@@ -6,6 +6,14 @@ group =: RadioGroup
 
 # GENERATE A RANDOM QUESTION
 before==
+group.items.append({
+    "id": 1,
+    "content": "teste"
+})
+group.items.append({
+    "id": 2,
+    "content": "re-teste"
+})
 ==
 
 title = Comment percevez-vous Platon ?
