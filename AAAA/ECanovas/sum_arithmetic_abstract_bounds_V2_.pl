bc67c43db58ba404c61e19a41f472c0b984651da
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-15 10:28:03 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sum_arithmetic_abstract_bounds_V2_.pl

---
+++ AAAA/ECanovas/sum_arithmetic_abstract_bounds_V2_.pl
@@ -0,0 +1,28 @@
+extends = Mathematics/exercices/algebra/finitesums/sum_.pl
+
+title = Somme d'entiers successifs
+
+before ==
+v=randitem([Symbol('i'),Symbol('j'),Symbol('k')])
+n=randitem([Symbol('n'),Symbol('m'),Symbol('p')])
+if param['boundtype']==1:
+    a=randint(1,4)
+    b0=randint(-4,4,[0])
+    b=n+b0
+elif param['boundtype']==2:
+    an,bn=randitem([(n,2*n),(2*n,3*n),(n,3*n),(n,4*n)])
+    a0=randint(1,4)
+    a=a0+an
+    b0=randint(-4,4,[0])
+    b=bn+b0
+n0=int(max([0,max(solve(a-b,n))]))
+S = Sum(v, (v, a, b))
+sol=simplify((b-a+1)*(a+b)/2)
+==
+
+text ==
+Soit un entier $! {{ n|latex}} \geq {{n0}} !$. Calculer $! \displaystyle {{S|latex}} !$.
+==
+
+
+
