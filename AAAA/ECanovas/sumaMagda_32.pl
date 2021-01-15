f8ad5935c6e7fd20a4f7f305a390990fe3360153
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-15 18:13:22 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sumaMagda_32.pl

---
+++ AAAA/ECanovas/sumaMagda_32.pl
@@ -0,0 +1,18 @@
+extends = sum_arithmetic_abstract_bounds_V2_.pl
+
+title= Somme finie Niveau 3.2
+
+param.boundtype % 3.2
+
+text==#|markdown|
+
+Calculer la somme $! A= \sum_{k={{a}}}^{ {{b}}} {{d}}k^{{c}} !$ <br/>
+On suppose $! {{a}}\le {{b}} !$
+<br/>
+{{feedback}}
+<br/>
+Soluce <br/>
+$$ {{S|latex}} $$
+<br/>
+$$ {{Ssol|latex}} $$
+==
