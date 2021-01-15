5e2281f91f035e65ac7ae8bb6460bbe161bf365a
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-15 10:28:08 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sumaMagda.pl

--- AAAA/ECanovas/sumaMagda.pl
+++ AAAA/ECanovas/sumaMagda.pl
@@ -1,59 +1,15 @@
-extends = /model/math.pl
-extends = /model/mathinput.pl
+extends = sum_arithmetic_abstract_bounds_V2_.pl
 
-
-
-mathinput =: MathInput
-
-title= Somme finie
+title= Somme finie Niveau 1
 
 text==
 
-Calculer la somme $! A= {{somm}} !$
-<br/>
-$!{{solu|latex}}!$
-<br/>
-$!{{f|latex}}!$
+Calculer la somme $! A= \sum_{k=a}^b dk^c !$
+
 <br/>
 {{feedback}}
 ==
 
-before ==
-f= symbols('f', cls=Function)
-i, k = symbols('i k', integer=True)
-
-a=1 
-btab= ["n-1","n+1"]
-b=btab[randint(0,len(btab)-1)]
-c= randint(0,3)
-d= randint(1,9)
-
-if c==1 :
- c=""
- somm="\sum_{k={{a}}}^{ {{b}}} {{d}}k"
- f="k"
- if d>1:
-  f=str(d)+"*k"
-else:
- somm="\sum_{k={{a}}}^{ {{b}}} {{d}}k^{{c}}"
- f=str(d)+'*k**'+str(c)
-
-solu=summation(f, (i, a, b))
-==
-
-form==
-Votre réponse
-{{ mathinput|component }}
-==
-
-evaluator==
-if mathinput==solu :
- Bonne réponse !<br/>
- {{feedback}}
-else:
- Mauvaise réponse !<br/>
- {{feedback}}
-==
 
 feedback==
 On rappelle que si $!n \ge 1!$ on a
