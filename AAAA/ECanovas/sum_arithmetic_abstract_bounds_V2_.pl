38b473734c18bbecd36c1ab56b6f4ca4c398a392
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-15 17:42:46 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sum_arithmetic_abstract_bounds_V2_.pl

--- AAAA/ECanovas/sum_arithmetic_abstract_bounds_V2_.pl
+++ AAAA/ECanovas/sum_arithmetic_abstract_bounds_V2_.pl
@@ -12,19 +12,17 @@ k  = symbols('k', integer=True)
 niveau=param['boundtype']
 if niveau==1:
     # a = 1 ,   b= n-1,  n+1, c= 0,1,2,3, d =1,...., 9
-    a=1 
-    b0=randint(-1,1,[0])
-    b=n+b0
+    a=1
     b=randitem([n+1,n-1])
     c= randint(0,3)
     d= randint(1,9)
     f= d*k**c
 elif niveau==2:
-    an,bn=randitem([(n,2*n),(2*n,3*n),(n,3*n),(n,4*n)])
-    a0=randint(1,4)
-    a=a0+an
-    b0=randint(-4,4,[0])
-    b=bn+b0
+    a=randitem([0,2..5])
+    b=randitem([n+1,n-1])
+    c= randint(0,3)
+    d= randint(1,9)
+    f= d*k**c
 #n0=int(max([0,max(solve(a-b,n))]))
 S = Sum(f, (k, a, b)).doit()
 Ssol= simplify(S)
