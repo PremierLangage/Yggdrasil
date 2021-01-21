f0bdbf1422eca1eaca782a8593155128adf6489f
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-21 16:16:22 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sum_V2_.pl

---
+++ AAAA/ECanovas/sum_V2_.pl
@@ -0,0 +1,15 @@
+extends = /model/mathinput.pl
+
+text ==
+Soit un entier $! {{n|latex}} \geq {{n0}} !$. Calculer $! \displaystyle {{S|latex}} !$.
+==
+
+evaluator ==
+score, error = eval_expr(input.value, sol)
+feedback = message[error]
+==
+
+
+solution ==
+La solution est $! {{sol|latex}} !$.
+==
