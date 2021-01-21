87d2e436e2fca2b6e4e9106c3058645aa825e84e
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-21 16:28:52 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/sum_V2_.pl

--- AAAA/ECanovas/sum_V2_.pl
+++ AAAA/ECanovas/sum_V2_.pl
@@ -6,10 +6,12 @@ Soit un entier $! {{n|latex}} \geq {{n0}} !$. Calculer $! \displaystyle {{S|late
 
 evaluator ==
 score, error = eval_expr(input.value, sol)
-feedback = 'OULALA '+  message[error]
+feedback = message[error]
 ==
 
 
 solution ==
 La solution est $! {{sol|latex}} !$.
+
+{{rappels}}
 ==
