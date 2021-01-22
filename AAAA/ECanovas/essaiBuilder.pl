875710aab89bba58773df5937ff879981ddc3370
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-01-22 16:34:51 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/essaiBuilder.pl

--- AAAA/ECanovas/essaiBuilder.pl
+++ AAAA/ECanovas/essaiBuilder.pl
@@ -9,7 +9,12 @@ if __name__ == "__main__":
     with open(sys.argv[1],'r') as f:
     context = json.load(f)
     print(context, file=sys.stderr)
-    sys.exit(0)
+    input_json = sys.argv[1]
+    output_json = sys.argv[2]
+    with open(input_json,'r') as i:
+        with open(output_json,"w") as o:
+            o.write(i.read()) # pas de conversion
+    sys.exit(1)
 ==
 
 text==#|markdown|
