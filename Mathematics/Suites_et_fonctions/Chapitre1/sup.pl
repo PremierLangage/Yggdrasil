e2ca423eb06f6350b9fe3bb6720fdeeb408cf20f
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-01-27 16:57:38 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/Chapitre1/sup.pl

---
+++ Mathematics/Suites_et_fonctions/Chapitre1/sup.pl
@@ -0,0 +1,40 @@
+# Copyright 2020 Magdalena Kobylanski <magdalena.kobylanski@u-pem.fr>
+author=Magdalena Kobylanski
+
+title=Commandes Unix en terminal
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+
+title==
+QCM sur les inégalités 
+==
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+
+==
+
+questions==
+
+** Cocher les propositions vraies : 
++ Toute partie non vide majorée de $%\mathbb{R} %$ admet une borne sup.
+- Toute partie non vide majorée de $%\mathbb{R} %$ admet un plus grand élément.
++ La borne sup d'une partie d'un ensemble ordonné est le plus petit des majorants de cette partie. 
+- Toute partie majorée de $%\mathbb{R} %$ admet une borne sup.
+- Toute partie non vide majorée de $%\mathbb{Q} %$ admet une borne sup.
+
+** Soit $%A%$ une partie de $%\mathbb{R} %$ et $%a %$ un réel. Cocher les propositions vraies : 
+- Si $% a%$ est la borne sup de $%A%$, alors  $% a%$ est le plus grand élément de $%A%$.
++ Si $% a%$ est le plus grand élément de $%A%$, alors  $% a%$ est la borne sup de $%A%$.
++ Si $% a%$ est le plus grand élément de $%A%$, alors $% a \in A%$.
+- Si $% a%$ est la borne sup de $%A%$, alors $% a \in A%$.
+-  Si $% a%$ est la borne sup de $%A%$, alors $% a \notin A%$.
+
+** Cocher les propositions vraies :
+- Si $% f %$ est une fonction numérique croissante et $% I %$ est un intervalle borné, alors $%f(I)%$ est borné et $% \sup(f(I)) = f(\sup(I)) %$
+- Si $% f %$ est une fonction numérique croissante et $% I %$ est un intervalle borné, alors $%f(I)%$ est borné et $% \sup(f(I)) > f(\sup(I)) %$
++ Si $% f %$ est une fonction numérique croissante et $% I %$ est une partie majorée, alors $%f(I)%$ est majorée et $% \sup(f(I)) \le  f(\sup(I)) %$
++  Si $% f %$ est une fonction numérique croissante et $% I %$ est une partie majorée qui admet un plus grand élément, alors $%f(I)%$ admet un plus grand élément et  et $% \max(f(I)) = f(\max(I)) %$
+  
+==
