8ea646af8a991192149e76301e89693cb2912ccc
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-01-27 12:09:58 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/QCM_definition_limite.pl

--- Mathematics/Suites_et_fonctions/QCM_definition_limite.pl
+++ Mathematics/Suites_et_fonctions/QCM_definition_limite.pl
@@ -35,7 +35,7 @@ questions==
 
 ** $%\lim_{x\to 1^+} f(x) = 5 %$ signifie :
 + $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad 1<x\le 1+ \eta  \implies |f(x)-5|\le \varepsilon %$
-+ $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\le \varepsilon %$
+- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\le \varepsilon %$
 - $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad 1<x\le 1+ \eta  \implies |f(x)-3|\le \varepsilon %$
 - $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\ge \varepsilon %$
  
