31caabd05fb5dbd98ba7bcfa84b59fb0ee99e9f8
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-01-27 14:10:33 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/Chapitre1/inegalites.pl

---
+++ Mathematics/Suites_et_fonctions/Chapitre1/inegalites.pl
@@ -0,0 +1,56 @@
+# Copyright 2020 Magdalena Kobylanski <magdalena.kobylanski@u-pem.fr>
+author=Magdalena Kobylanski
+
+title=Commandes Unix en terminal
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+
+title==
+QCM sur les inégalités 
+==
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+
+==
+
+questions==
+
+** Cocher les propositions vraies
+- Si $% a, b %$ sont des réels et $%a \le b %$, alors $% \frac{1}{a} \ge \frac{1}{b} %$
++ Si $% a, b %$ sont des réels strictement négatifs  et $%a \le b %$, alors $% \frac{1}{a} \ge \frac{1}{b} %$
+- Si $%a,b $ sont des réels et $%a \le b %$, alors $% a^2 \ge b^2 %$
++ Si $% a, b %$ sont des réels strictement négatifs  et $%a \le b %$, alors $% a^2 \ge b^2 %$
+- Si $%f%$ est une fonction strictement croissante sur la partie $%I%$ et strictement croissante sur la partie $%J%$, alors $% f%$ est strictement croissante sur $%I \cup J%$.
+
+
+
+
+** Cocher les propositions vraies :
+-  $% \forall a,b,c  \in \mathbb{R} \quad a \le b  \implies ac \le bc %$ 
++ $% \forall a,b  \in \mathbb{R}\; \forall c \in \mathbb{R}^+ \quad a \le b  \implies ac \le bc %$ 
++ $% \forall a,b,c  \in \mathbb{R} \quad a \le b  \implies a + c \le b+ c %$ 
+- $% \forall a,b  \in \mathbb{R}\; \forall c \in \mathbb{R}^+ \quad a \le b  \implies \frac{a}{c} \le \frac{b}{c} %$ 
+
+
+
+
+
+** Cocher les propositions vraies :
+- $% \\{1,-2,3\\} \subset \mathbb{N} %$
+- $% [1,2] \subset \mathbb{N} %$ 
++  $%\\{ \frac{1}{2}, \frac{1}{5}, \frac{7}{4}\\} \subset \mathbb{D} %$ 
+-  $%\\{ \frac{1}{2}, \frac{1}{3}, \frac{7}{4}\\} \subset \mathbb{D} %$ 
++  $%\\{ \sqrt{2}, e, \frac{\pi}{2} \\} \subset \mathbb{R}\setminus \mathbb{Q} %$
+-  $%\\{ \sqrt{2},\sqrt{3},\sqrt{4}  \\} \subset \mathbb{R}\setminus \mathbb{Q} %$  
+
+** Cocher les propositions vraies 
++ Si $% I %$ et $% J %$ sont des intervalles de $%\mathbb{R}%$  alors $% I \cap J%$ est un intervalle de $%\mathbb{R}%$
+- Si $% I %$ et $% J %$ sont des intervalles de $%\mathbb{R}%$  alors $% I \cup J%$ est un intervalle de $%\mathbb{R}%$
+- Si $% I %$ est un intervalle borné de $% \mathbb{R}%$, alors $%a = \inf(A)%$ et $% b = \sup(B)%$ sont des réels et $%I=[a,b]%$
++ Si $% I %$ est un intervalle ni majoré ni minoré  de $% \mathbb{R}%$, alors $%A= \mathbb{R}%$
++ Si $% A %$ et $% B %$ sont des parties bornées de $% \mathbb{R} %$, alors $%\sup (A \cup B)= \max( \sup(A), \sup (B))%$
++ Si $% A %$ et $% B %$ sont des parties bornées de $% \mathbb{R} %$, alors $%\sup (A \cap B)= \min( \sup(A), \sup (B))%$
+==
+
