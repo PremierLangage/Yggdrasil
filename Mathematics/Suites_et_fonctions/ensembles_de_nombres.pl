f6a561d4dd1649ffa69fe528756481af3c0aa042
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-01-27 12:19:58 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/ensembles_de_nombres.pl

---
+++ Mathematics/Suites_et_fonctions/ensembles_de_nombres.pl
@@ -0,0 +1,49 @@
+# Copyright 2020 Magdalena Kobylanski <magdalena.kobylanski@u-pem.fr>
+author=Magdalena Kobylanski
+
+title=Commandes Unix en terminal
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+
+title==
+QCM sur les ensembles de nombre
+==
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+
+Dans toutes les questions $%\mathbb{D}%$ désigne l'ensemble des nombres décimaux (qui s'écrivent avec un nombre fini de décimales derrière la virgule), $! e = \exp(1)!$
+==
+
+questions==
+
+** Parmi les affirmations suivantes, lesquelles sont vraies 
++ $%\frac{1}{2} \in \mathbb{Q}%$
++ $% \frac{1}{3} \in \mathbb{Q}%$
+- $%\frac{1}{2} \in \mathbb{D}%$
++ $% \frac{1}{3} \in \mathbb{Q}%$
++ $% \sqrt{2} \in \mathbb{R} \setminus \mathbb{Q} %$
+- $% 3-7 \in \mathbb{N} %$
+
+
+
+
+
+** $%\lim_{x\to +\infty} f(x) = 3 %$ signifie :
++ $%\forall \varepsilon >0 \quad \exists A \in \mathbb{R} \quad \forall x\in D_f \quad x \ge A  \implies |f(x)-3|\le \varepsilon %$
++ $%\forall \varepsilon >0 \quad \exists A>0 \quad \forall x\in D_f \quad x \ge A  \implies |f(x)-3|\le \varepsilon %$
++ $%\forall \varepsilon >0 \quad \exists \eta >0 \quad \forall x\in D_f \quad x \ge \eta  \implies |f(x)-3|\le \varepsilon %$
+- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x|\le \eta \implies |f(x)-3|\le \varepsilon %$
+- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-3|\ge \eta \implies |f(x)-3|\le A %$
+
+** $%\lim_{x\to 1^+} f(x) = 5 %$ signifie :
++ $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad 1<x\le 1+ \eta  \implies |f(x)-5|\le \varepsilon %$
+- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\le \varepsilon %$
+- $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad 1<x\le 1+ \eta  \implies |f(x)-3|\le \varepsilon %$
+- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\ge \varepsilon %$
+ 
+
+
+
+==
