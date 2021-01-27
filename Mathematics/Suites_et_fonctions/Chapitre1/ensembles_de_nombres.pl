5b5bfc7fc54dd0d65ceaaf426caa86bd4b3361c3
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-01-27 13:46:17 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/Chapitre1/ensembles_de_nombres.pl

---
+++ Mathematics/Suites_et_fonctions/Chapitre1/ensembles_de_nombres.pl
@@ -0,0 +1,59 @@
+# Copyright 2020 Magdalena Kobylanski <magdalena.kobylanski@u-pem.fr>
+author=Magdalena Kobylanski
+
+title=Commandes Unix en terminal
+tag=system|shell
+
+extends=/model/AMC_TXT.pl
+
+title==
+QCM sur les ensembles de nombre
+==
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+
+Dans toutes les questions $%\mathbb{D}%$ désigne l'ensemble des nombres décimaux (qui s'écrivent avec un nombre fini de décimales derrière la virgule), $! e = \exp(1)!$
+==
+
+questions==
+
+** Parmi les affirmations suivantes, lesquelles sont vraies 
++ $%\frac{1}{2} \in \mathbb{Q}%$
++ $% \frac{1}{3} \in \mathbb{Q}%$
++ $%\frac{1}{2} \in \mathbb{D}%$
+- $% \frac{1}{3} \in \mathbb{D}%$
++ $% \sqrt{2} \in \mathbb{R} \setminus \mathbb{Q} %$
+- $% 3-7 \in \mathbb{N} %$
+- $% \frac{e}{2} \in \mathbb{Q} %$
+
+
+
+
+
+** Cocher les propositions vraies :
++ $%\forall x \in \mathbb{D} \quad x \in \mathbb{Q} %$
++ $%\forall x, y \in \mathbb{D}  \quad xy \in \mathbb{D} %$
+- $%\forall x, y \in \mathbb{Q} \setminus \mathbb{D} \quad xy \in  \mathbb{Q} \setminus \mathbb{D} %$
+-  $% \forall x, y \in \mathbb{R} \setminus \mathbb{Q}  \quad x+y \in \mathbb{R} \setminus \mathbb{Q} %$
++ $% \forall x \in \mathbb{Q} \quad -x \in \mathbb{Q} %$
++ $% \forall x \in \mathbb{Q}^* \quad \frac{1}{x} \in \mathbb{Q} %$
+- $% \forall x \in \mathbb{N} \quad -x \in \mathbb{N} %$
++ $% \forall x \in \mathbb{Z}^* \quad \frac{1}{x} \in \mathbb{Z} %$
+
+** Cocher les propositions vraies :
+- $% \\{1,-2,3\\} \subset \mathbb{N} %$
+- $% [1,2] \subset \mathbb{N} %$ 
++  $%\\{ \frac{1}{2}, \frac{1}{5}, \frac{7}{4}\\} \subset \mathbb{D} %$ 
+-  $%\\{ \frac{1}{2}, \frac{1}{3}, \frac{7}{4}\\} \subset \mathbb{D} %$ 
++  $%\\{ \sqrt{2}, e, \frac{\pi}{2} \\} \subset \mathbb{R}\setminus \mathbb{Q} %$
+-  $%\\{ \sqrt{2},\sqrt{3},\sqrt{4}  \\} \subset \mathbb{R}\setminus \mathbb{Q} %$  
+
+** Cocher les propositions vraies 
++ Si $% I %$ et $% J %$ sont des intervalles de $%\mathbb{R}%$  alors $% I \cap J%$ est un intervalle de $%\mathbb{R}%$
+- Si $% I %$ et $% J %$ sont des intervalles de $%\mathbb{R}%$  alors $% I \cup J%$ est un intervalle de $%\mathbb{R}%$
+- Si $% I %$ est un intervalle borné de $% \mathbb{R}%$, alors $%a = \inf(A)%$ et $% b = \sup(B)%$ sont des réeles et $%I=[a,b]%$
++ Si $% I %$ est un intervalle ni majoré ni minoré  de $% \mathbb{R}%$, alors $%A= \mathbb{R}%$
++ Si $% A %$ et $% B %$ sont des parties bornées de $% \mathbb{R} %$, alors $%\sup (A \cup B)= \max( \sup(A), \sup (B))%$
++ Si $% A %$ et $% B %$ sont des parties bornées de $% \mathbb{R} %$, alors $%\sup (A \cap B)= \min( \sup(A), \sup (B))%$
+==
