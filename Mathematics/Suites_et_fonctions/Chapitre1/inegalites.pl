044b6cc4c257441b50078cc02908a1e54c438e52
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-01-28 16:39:38 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/Chapitre1/inegalites.pl

--- Mathematics/Suites_et_fonctions/Chapitre1/inegalites.pl
+++ Mathematics/Suites_et_fonctions/Chapitre1/inegalites.pl
@@ -1,14 +1,17 @@
-# Copyright 2020 Magdalena Kobylanski <magdalena.kobylanski@u-pem.fr>
-author=Magdalena Kobylanski
 
-title=Commandes Unix en terminal
-tag=system|shell
+# template Tout nouveau malheuresement... feedback pas encore programmé...
+extends=/ComputerScience/C/examples/Stoi_le_caroussel.pl
 
-extends=/model/AMC_TXT.pl
 
-title==
-QCM sur les inégalités 
-==
+author = Enseignants Suites et Fonctions (MK)
+
+
+# pour changer le nombre de question, c'est en dessous (10 MAX)
+# si le nombre est plus petit que 10, ce sera un tirage aléatoire
+# sans remise parmi le tas de questions.
+nb_quest_voulu = 4
+
+title= carroussel inégalités
 
 intro ==
 Ce quiz contient {{nbstep}} questions.
