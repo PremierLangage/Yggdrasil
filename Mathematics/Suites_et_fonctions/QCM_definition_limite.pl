8fc1a7720a9374d1d8ac23d686e40fe856dd70f5
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-01-29 15:24:17 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/QCM_definition_limite.pl

--- Mathematics/Suites_et_fonctions/QCM_definition_limite.pl
+++ Mathematics/Suites_et_fonctions/QCM_definition_limite.pl
@@ -7,9 +7,9 @@ tag=system|shell
 
 extends=/ComputerScience/C/examples/Stoi_le_caroussel.pl
 
-title==
-QCM sur la défintion des limites de fonctions
-==
+nb_quest_voulu = 4
+
+title= carroussel sur la défintion des limites de fonctions
 
 intro ==
 Ce quiz contient {{nbstep}} questions.
@@ -24,6 +24,12 @@ questions==
 - $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x|\le \eta \implies |f(x)-2|\le \varepsilon %$
 - $%\forall \varepsilon \ge 0 \quad \exists \eta\ge 0  \quad \forall x\in D_f \quad |x|\le \eta \implies |f(x)-2|\le \varepsilon %$
 
+** $%\lim_{x\to 5^-} f(x) = -\infty %$ signifie :
++ $%\forall A \in \mathbb{R} \quad \exists \eta>0 \quad \forall x\in D_f \quad 5-\eta \le x\le 5 \implies f(x)\le A %$
+- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-5|\le \eta \mbox{ et } x<5 \implies |f(x)-5|\ge \varepsilon %$
++ $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad |x-5|\le \eta \mbox{ et } x<5  \implies f(x)\le -\varepsilon %$
+- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\ge \varepsilon %$
+ 
 
 
 ** $%\lim_{x\to +\infty} f(x) = 3 %$ signifie :
@@ -33,18 +39,13 @@ questions==
 - $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x|\le \eta \implies |f(x)-3|\le \varepsilon %$
 - $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-3|\ge \eta \implies |f(x)-3|\le A %$
 
-** $%\lim_{x\to 1^+} f(x) = 5 %$ signifie :
+* $%\lim_{x\to 1^+} f(x) = 5 %$ signifie :
 + $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad 1<x\le 1+ \eta  \implies |f(x)-5|\le \varepsilon %$
 - $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\le \varepsilon %$
 - $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad 1<x\le 1+ \eta  \implies |f(x)-3|\le \varepsilon %$
 - $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\ge \varepsilon %$
  
-** $%\lim_{x\to 5^-} f(x) = -\infty %$ signifie :
-+ $%\forall A \in \mathbb{R} \quad \exists \eta>0 \quad \forall x\in D_f \quad 5-\eta \le x\le 5 \implies f(x)\le A %$
-- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-5|\le \eta \mbox{ et } x<5 \implies |f(x)-5|\ge \varepsilon %$
-+ $%\forall \varepsilon >0 \quad \exists \eta>0 \quad \forall x\in D_f \quad |x-5|\le \eta \mbox{ et } x<5  \implies f(x)\le -\varepsilon %$
-- $%\forall \varepsilon \ge 0 \quad \exists \eta>0  \quad \forall x\in D_f \quad |x-1|\le \eta \mbox{ et } x>1 \implies |f(x)-5|\ge \varepsilon %$
- 
+
 
 
 ==
