3e0b18473c5fa7cec175b0fca15961515124c67f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-30 10:46:15 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl

--- ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl
@@ -55,18 +55,20 @@ form==#|markdown|
 ==
 
 evaluator==#|python|
+import subprocess
 from utils_bash import display_as_shell_this, frame_message
 
-moyen = ['Linux', 'linux', 'GNU/Linux']
-good = ['Debian', 'debian']
+sp = subprocess.run(["/bin/uname", "-r"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=1)
+spout = sp.stdout.decode()
+# errout = sp.stderr.decode()
+# returncode = sp.returncode
 
-if inputbox.value in good:
+solution = spout
+
+if inputbox.value == solution:
     grade = (100, frame_message("C'est bien cela !", "ok"))
 else:
-    if inputbox.value in moyen:
-        grade = (50, frame_message("C'est presque ça, vous avez donné le système d'exploitation mais pas la distribution.", "warning"))
-    else:
-        grade = (0, frame_message("Ce n'est pas la bonne réponse", "error"))
+    grade = (0, frame_message("Ce n'est pas la bonne réponse...", "error"))
 ==
 
 
