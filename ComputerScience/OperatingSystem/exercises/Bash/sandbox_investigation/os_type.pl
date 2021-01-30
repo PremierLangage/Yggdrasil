d2205d7404d64c8c3e9dc20f66a3057a71d1c9b1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-30 16:34:49 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/os_type.pl

--- ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/os_type.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/os_type.pl
@@ -19,6 +19,9 @@
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
+author=Nicolas Borie
+tag=système|distribution|Unix|terminal
+
 inputbox =: Input
 inputbox.type = text
 inputbox.placeholder = Votre réponse
