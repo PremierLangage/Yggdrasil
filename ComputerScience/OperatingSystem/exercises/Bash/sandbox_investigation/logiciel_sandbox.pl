85b0b576d56ec34546485ccca3b4eaf564420bc2
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-30 16:36:52 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/logiciel_sandbox.pl

---
+++ ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/logiciel_sandbox.pl
@@ -0,0 +1,70 @@
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+@ /utils/sandboxio.py
+
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+author=Nicolas Borie
+tag=système|Unix|terminal|logiciel|utilitaire
+
+group =: CheckboxGroup
+
+# GENERATE A RANDOM QUESTION
+before==
+import random
+group.items = []
+for i in range(4):
+    group.items.append({
+        "id": str(i),
+        "content": str(random.randint(0, 100))
+    })
+==
+
+title=Quels logiciels/utilitaires peut on trouver sur les sandbox ?
+
+text==
+Quels logiciels/utilitaires peut on trouver sur les sandbox ?
+==
+
+# PRESENT THE QUESTION TO THE STUDENT
+form==
+{{ group|component }}
+==
+
+# EVALUATE THE STUDENT ANSWER
+evaluator==
+right = 0
+total = 0
+for item in group.items:
+    checked = item['checked']
+    content = int(item['content'])
+    if content % 2 == 0:
+        total += 1
+        item['css'] = 'success-border animated pulse infinite'
+        if checked:
+            right += 1
+            item['css'] = 'success-border'
+    elif checked:
+        item['css'] = 'error-border'
+
+
+if total == 0:
+    grade = (100, 'Right')
+else:
+    grade = ((right / total) * 100, f"{right} / {total}")
+==
