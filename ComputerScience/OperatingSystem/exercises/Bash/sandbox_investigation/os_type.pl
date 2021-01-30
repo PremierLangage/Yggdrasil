598094c68de820a725c79de33f740290df35abe4
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-30 10:09:30 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/os_type.pl

--- ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/os_type.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/os_type.pl
@@ -1,27 +1,39 @@
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
 @ /utils/sandboxio.py
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 inputbox =: Input
-inputbox.type = number
-inputbox.placeholder = Answer
+inputbox.type = text
+inputbox.placeholder = Votre réponse
 inputbox.maxlength = 2
 inputbox.appearance = outline
 
-before==
-import random
+before==#|python|
 
-a = random.randint(1, 10)
-b = random.randint(1, 10)
-r = a + b
 ==
 
 title==
-Input Component
+Quel est le système d'exploitation ?
 ==
 
 text==
-Enter the result of ** {{ a }} + {{ b }} ** inside the input box.
+Essayer de determiner quel est le système d'exploitation utilisé dans les sandbox docker de PLaTon. 
 ==
 
 form==
