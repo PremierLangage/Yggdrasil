e0d83cc6fb8debc24fde4a87fec40594ad207ae1
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-30 19:40:15 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl

--- ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl
@@ -37,7 +37,7 @@ Quel est la version du noyau ?
 ==
 
 text==#|markdown|
-Il apparait que les sandbox exécutent un noyau Linux, mais quel est la version 
+Il apparait que les sandbox exécutent un noyau Linux, mais quelle est la version 
 de révision précise du noyau ? Une option bien choisie de **uname** (R.T.F.M.) 
 devrait vous permettre de trouver votre bonheur. Attention, on veut la chaine complète 
 décrivant la révision du noyau (il n'a pas que des chiffres et des points normalement).
