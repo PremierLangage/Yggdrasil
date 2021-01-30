981f6ea9cb0e64d7698a4dde57bca0d517643674
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-01-30 18:56:10 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/moo.pl

--- ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/moo.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/moo.pl
@@ -47,7 +47,7 @@ Debian, une distribution gratuite réputée pour sa robustesse.
 
 Donner la commande donc l'exécution sur l'émulateur de terminal donne ça :
 
-<img src="{{ img_moo }}" alt="Avez-vous meuglé aujourd'hui ?" width="50%" />
+<img src="{{ img_moo }}" alt="Avez-vous meuglé aujourd'hui ?" width="50px" />
 
 
 <br>
