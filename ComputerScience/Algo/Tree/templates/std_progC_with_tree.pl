b241decce48a293feb2c6827bb7ca10978d95778
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-02-01 17:35:07 +0100

    drevuz modified Yggdrasil/ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl

--- ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl
+++ ComputerScience/Algo/Tree/templates/std_progC_with_tree.pl
@@ -187,9 +187,8 @@ checks_data = eval(checks_args_stdin)
 
 if compil_state != 'error':
     for test_c in checks_data:
-        f_in=open("stdin_content", "w")
-        f_in.write(test_c[2])
-        f_in.close()
+        with open("stdin_content", "w") as f_in :
+            f_in.write(test_c[2])
         # Use the teacher solution to generated expected output of the test
         command_args = " ".join(["./teacher_prog"] + list(map(lambda x: "'"+x+"'", test_c[1])) )
         sp = subprocess.run(command_args, stdin=open("stdin_content", "r"), stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, timeout=1)
