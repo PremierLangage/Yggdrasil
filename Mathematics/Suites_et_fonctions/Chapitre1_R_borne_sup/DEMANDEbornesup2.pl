deb460861d94c147724911ec39ec17370de9d749
MAGDALENA KOBYLANSKI <Magdalena.Kobylanski@u-pem.fr>
2021-02-02 20:30:17 +0100

    mkobylanski modified Yggdrasil/Mathematics/Suites_et_fonctions/Chapitre1_R_borne_sup/DEMANDEbornesup2.pl

---
+++ Mathematics/Suites_et_fonctions/Chapitre1_R_borne_sup/DEMANDEbornesup2.pl
@@ -0,0 +1,45 @@
+demande d'exercice de Magdalena Kobylanski  
+à partir de 
+
+Mathematics/Suites_et_fonctions/Chapitre1_R_borne_sup/bornesup.csv
+
+Pour chaque ligne proposer l'exercice suivant :
+# On considère la partie $%I = C2%$ de $%\mathbb{R}%$ 
+# v1 
+# I est-elle majorée ? oui /non (réponse C3)
+# I admet-elle une borne supérieure ? oui/non  (réponse : non si - en C5) si oui indiquez la borne sup  : (réponse : C5)
+# La borne supérieure est-elle atteinte ? oui/non (réponse : C7)
+# v2
+# I est-elle minorée ? oui /non (réponse C4)
+# I admet-elle une borne supérieure ? oui/non  (réponse : non si - en C6) si oui indiquez la borne supérieure  : (réponse : C7)
+# La borne supérieure est-elle atteinte ? oui/non (réponse : C8)
+# v3
+# I admet-elle un plus grand élément ? oui /non (réponse : non si - en C7), si oui afficher  p.g.e.(I)= réponse C7
+# v4
+# I admet-elle un plus petit élément ? oui /non (réponse : non si - en C8), si oui afficher  p.p.e.(I)= réponse C8
+# v5
+# choisir au hasard a entre [-100, 100]
+# $%a%$ est-il un majorant de I ?  oui/non (réponse  oui si ((C5 est une valeur numérique) et (a\ge  C5)) ou  (C5=-)
+# v6
+# choisir au hasard a entre [-100, 100]
+# $%a%$ est-il un minorant de I ?  oui/non (réponse  oui si ((C6 est une valeur numérique) et (a\ge  C6)) ou  (C6=-) 
+
+
+Mode entrainement 6 fois de suite.
+
+Parcourir suivant les types (C1 x) croissants. 
+- on commence à x= 1, toutes les versions ont le poids 2.
+- on tire au hasard un exercice de type x, de version v
+- Si type type x version v validé et poids (x,v)>1 ,  le poids diminue de 1, le type successeur de x est mis dans le panier avec des poids 3
+- si type x version v non validé, poids (x,v) augmente de 1
+
+L'exercice est validé avec des étoiles. 
+- 0 étoile si moins de 1 validations 
+- * si validations d'au moins 2 exercices 
+- ** si validation d'au moins 3 dans deux niveaux 
+- *** si validation d'au moins 4 exercices dans deux niveaux
+- **** si validation d'au moins  5 exercices dans trois niveaux 
+- ***** si validation de tous les exercices avec au moins 3 exercices dans trois niveaux
+
+Mode évaluation : 
+- tirer 6 questions 1, 2a , 2b, 2c,  3a, 3b, avec des verrsions variables
