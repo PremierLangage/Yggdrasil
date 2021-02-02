b1071c4aaf80c91fdedd9a5f1865a51584238c03
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-02 14:39:12 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/misc_terminal/cheat_sheat.pl

---
+++ ComputerScience/OperatingSystem/exercises/Bash/misc_terminal/cheat_sheat.pl
@@ -0,0 +1,73 @@
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+@ /utils/sandboxio.py
+
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+author=Nicolas Borie
+
+title=Les réflexes salutaires en terminal
+
+tag=réflexes|bonnes pratiques|terminal|unix
+
+group =: CheckboxGroup
+
+# GENERATE A RANDOM QUESTION
+before==#|python|
+import random
+
+group.items = []
+
+group.items.append({"id": "g1", "content": "Utiliser au maximum la `tab`-completion pour éviter les fautes de frappe."})
+
+random.shuffle(group.items)
+==
+
+
+text==
+Sélectionnez les réflexes qui sauvent l'utilisateur dans un terminal unix...
+==
+
+# PRESENT THE QUESTION TO THE STUDENT
+form==
+{{ group|component }}
+==
+
+# EVALUATE THE STUDENT ANSWER
+evaluator==
+right = 0
+total = 0
+for item in group.items:
+    checked = item['checked']
+    content = int(item['content'])
+    if content % 2 == 0:
+        total += 1
+        item['css'] = 'success-border animated pulse infinite'
+        if checked:
+            right += 1
+            item['css'] = 'success-border'
+    elif checked:
+        item['css'] = 'error-border'
+
+
+if total == 0:
+    grade = (100, 'Right')
+else:
+    grade = ((right / total) * 100, f"{right} / {total}")
+==
+
