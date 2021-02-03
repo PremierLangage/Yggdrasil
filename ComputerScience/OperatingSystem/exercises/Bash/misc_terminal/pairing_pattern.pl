28378967b275efc2d0a8a24f09d0b77d81795fa4
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-03 18:19:15 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/misc_terminal/pairing_pattern.pl

--- ComputerScience/OperatingSystem/exercises/Bash/misc_terminal/pairing_pattern.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/misc_terminal/pairing_pattern.pl
@@ -38,7 +38,7 @@ match.nodes.append({"id": "tpy", "content": "Scripts Python", "target": True,})
 expected.append({ "source": "spy", "target": "tpy" })
 
 match.nodes.append({"id": "s3max", "content": "`? ?? ???`", "source": True,})
-match.nodes.append({"id": "t3max", "content": "fichiers de nom d'au plus 3 lettres", "target": True,})
+match.nodes.append({"id": "t3max", "content": "noms de fichiers ayant au plus 3 lettres", "target": True,})
 expected.append({ "source": "s3max", "target": "t3max" })
 
 match.nodes.append({"id": "spref", "content": "`tree*`", "source": True,})
