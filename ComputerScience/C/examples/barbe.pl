0d081739b1e26a91aea6aab215ee2a0190f39a87
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-04 20:19:20 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/barbe.pl

--- ComputerScience/C/examples/barbe.pl
+++ ComputerScience/C/examples/barbe.pl
@@ -37,13 +37,13 @@ import random
 
 group.items = []
 
-simg1 = '<img src="'+str(url_phot_1)+'" alt="le style1"/>'
+simg1 = '<img src="'+str(url_phot_1)+'" alt="le style1"/>La moustache suspendue assymétrique'
 simg2 = '<img src="'+str(url_phot_2)+'" alt="le style2"/>'
 simg3 = '<img src="'+str(url_phot_3)+'" alt="le style3"/>'
 simg4 = '<img src="'+str(url_phot_4)+'" alt="le style4"/>'
 simg5 = '<img src="'+str(url_phot_5)+'" alt="le style5"/>'
 
-group.items.append({"id": "y1", "content": simg1+"La moustache suspendue assymétrique"})
+group.items.append({"id": "y1", "content": simg1})
 group.items.append({"id": "y2", "content": simg2})
 group.items.append({"id": "y3", "content": simg3})
 group.items.append({"id": "y4", "content": simg4})
