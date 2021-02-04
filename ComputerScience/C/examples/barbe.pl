b92b2e01f8a364ad98c230ca1b2e5b402f7bfe32
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-04 19:19:42 +0100

    nborie modified Yggdrasil/ComputerScience/C/examples/barbe.pl

--- ComputerScience/C/examples/barbe.pl
+++ ComputerScience/C/examples/barbe.pl
@@ -18,8 +18,6 @@
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
-group =: RadioGroup
-
 url_phot_1 =$ /ComputerScience/C/examples/barbe/Style_1.jpg
 url_phot_2 =$ /ComputerScience/C/examples/barbe/Style_2.jpg
 url_phot_3 =$ /ComputerScience/C/examples/barbe/Style_3.jpg
@@ -60,8 +58,23 @@ la dernière manière à la mode pour la barbe durant l'année 2056.
 ==
 
 # PRESENT THE QUESTION TO THE STUDENT
-form==
-{{ group|component }}
+form==#|markdown|
+
+<input type="radio" id="bs1" name="form_bs" value="bs1">
+<label for="other">Le style 1 : <img src="{{ url_phot_1 }}" style="width:20%" alt="le style1"/></label>
+
+<input type="radio" id="bs2" name="form_bs" value="bs2">
+<label for="other">Le style 2 : <img src="{{ url_phot_2 }}" style="width:20%" alt="le style2"/></label>
+
+<input type="radio" id="bs3" name="form_bs" value="bs3">
+<label for="other">Le style 3 : <img src="{{ url_phot_3 }}" style="width:20%" alt="le style3"/></label>
+
+<input type="radio" id="bs4" name="form_bs" value="bs4">
+<label for="other">Le style 4 : <img src="{{ url_phot_4 }}" style="width:20%" alt="le style4"/></label>
+
+<input type="radio" id="bs5" name="form_bs" value="bs5">
+<label for="other">Le style 5 : <img src="{{ url_phot_5 }}" style="width:20%" alt="le style5"/></label>
+
 ==
 
 # EVALUATE THE STUDENT ANSWER
