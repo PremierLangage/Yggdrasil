08e94a13f784fd14c7f3cc33d4ddab55369fd9a8
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-04 18:11:56 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
@@ -46,40 +46,26 @@ text==#|markdown|
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
     <div class="item active" style="height:500px">
-      ** Parmi les assertions suivantes, sélectionnez celle qui sont correctes ?
-- Les commandes Unix travaille page par page.
-- Unix tente de proposer une commande différente pour chaque tâche, simple ou évoluée.
-+ La plupart des commandes en terminal travaillent ligne par ligne.
-+ L'entrée et la sortie standard sont bufferisés ligne par ligne.
-+ En terminal Unix, tout est chaîne de caractères.
-+ Les exécutions de commande Unix sont paramètrées par des arguments et l'entrée standard.
+      
     </div>
 
     <div class="item">
-      Sélectionner dans la liste suivante les programmes fonctionnels sans entrée (fonctionnant sans argument ou entrée standard).
- * mkdir   
- * rm   
- * sort   
- * cut   
- * grep   
- * find   
- * date   
- * cal   
- * pwd   
- * whoami   
+      
     </div>
 
     <div class="item">
-      ** Parmi les commandes suivantes, lesquelles permettent de visualiser un flux ou un fichier ?
-- pwd
-- chmod
-- chgrp
-- mkdir
-+ less
-+ more
-+ cat
-+ head
-+ tail
+      Parmi les commandes suivantes, lesquelles permettent de visualiser un flux ou un fichier ?<br>
+      <ul>
+        <li>pwd</li>
+        <li>chmod</li>
+        <li>chgrp</li>
+        <li>mkdir</li>
+        <li>less</li>
+        <li>more</li>
+        <li>cat</li>
+        <li>head</li>
+        <li>tail</li>
+      </ul>
     </div>
   </div>
 
