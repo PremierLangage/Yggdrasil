a4e0e6d424248877dfc9f9c46c750fce8cd1e207
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-04 18:02:00 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
@@ -19,16 +19,49 @@
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
-
 title==
 Fork me please, do not modify me please !
 ==
 
-text==
+text==#|markdown|
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="la.jpg" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="chicago.jpg" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="ny.jpg" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 
 ==
 
-form==
+form==#|markdown|
 
 ==
 
@@ -38,5 +71,4 @@ note_finale = 100
 
 grade = (note_finale, "Vous avez obtenur la note de "+str(note_finale)+"%<br>")
 ==
-
 
