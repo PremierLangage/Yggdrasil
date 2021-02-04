d2e203b4509b7a574396533979f4edfd62db0b40
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-04 18:03:42 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
@@ -30,6 +30,11 @@ before==
 
 text==#|markdown|
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
   <!-- Indicators -->
   <ol class="carousel-indicators">
