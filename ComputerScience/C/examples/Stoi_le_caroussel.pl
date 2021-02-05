7afa08bbe6ddc69425c0c4b3b074733994a6a385
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-02-05 14:30:53 +0100

    drevuz modified Yggdrasil/ComputerScience/C/examples/Stoi_le_caroussel.pl

--- ComputerScience/C/examples/Stoi_le_caroussel.pl
+++ ComputerScience/C/examples/Stoi_le_caroussel.pl
@@ -205,7 +205,7 @@ text==
 
 Prenez bien le temps de répondre aux {{ nb_quest }} questions avant de valider.
 
-
+<!-- il semble que les lignes suivante ne soit pas nécessaire mais je ne sais pas comment le vérifier-->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
