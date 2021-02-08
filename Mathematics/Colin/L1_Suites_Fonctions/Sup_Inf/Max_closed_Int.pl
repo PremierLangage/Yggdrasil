0e97a8811c77847cfe214d511463309139771f6e
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-02-08 11:34:55 +0100

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
@@ -12,7 +12,7 @@ for toto in range(1):
     b=random.randint(1,10)
     a=random.randint(-10,0)
 
-** Soit $%A=[{a | latex } , {b | latex }[%$ un intervalle dans $% \mathbb{R} %$. 
+** Soit $%A=[{{a | latex }} , {{b | latex }}[%$ un intervalle dans $% \mathbb{R} %$. 
 Cliquez sur les affirmations que vous pensez être correctes :
 + $%A%$ est borné
 - $%{b | latex }%$ est le maximum de $%A%$
