20db8639793b63df84d9f1392993f62b6f9937c9
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-02-08 12:03:54 +0100

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
@@ -5,20 +5,30 @@ tag= Majorant|Minorant|Borne supérieure|Borne inférieure
 
 
 # ce code permet d'ajouter des qestions 
-buildquestions==
-import random
-# questions=""
-for toto in range(2):
+questions==
     b=random.randint(1,10)
     a=random.randint(-10,0)
-* Soit $%A=[{a} , {b}[%$ un intervalle dans $% \mathbb{R} %$. 
+
+    d=random.randint(1,10)
+    c=random.randint(-10,0)
+    e=c+1
+
+** Soit $%A=[{a} , {b}[%$ un intervalle dans $% \mathbb{R} %$. 
 Cliquez sur les affirmations que vous pensez être correctes :
 + $%A%$ est borné
 - $%{b}%$ est le maximum de $%A%$
 + $%{a}%$ est le minimum de $%A%$
 - $%{b}%$ est la borne supérieure de $%A%$
 - $%{a}%$ est la borne inférieure de $%A%$
-"""
 
 
+
+** Soit $%B=]{c} , {d}]%$ un intervalle dans $% \mathbb{R} %$. 
+Cliquez sur les affirmations que vous pensez être correctes :
++ $%B%$ est majoré 
+- $%B%$ est minoré par {e}
+- $%{c}%$ est le minimum de $%B%$
++ $%{d}%$ est le maximum de $%B%$
+- $%{c}%$ est la borne supérieure de $%B%$
+- $%{d}%$ est la borne inférieure de $%B%$
 ==
