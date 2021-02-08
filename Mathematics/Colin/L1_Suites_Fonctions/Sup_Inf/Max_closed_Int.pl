1e5e5400f51ced260022a024aece688c74a07ead
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-02-08 11:46:59 +0100

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
@@ -9,6 +9,10 @@ questions==
     b=random.randint(1,10)
     a=random.randint(-10,0)
 
+    d=random.randint(1,10)
+    c=random.randint(-10,0)
+    e=c+1
+
 ** Soit $%A=[{a} , {b}[%$ un intervalle dans $% \mathbb{R} %$. 
 Cliquez sur les affirmations que vous pensez être correctes :
 + $%A%$ est borné
@@ -16,12 +20,8 @@ Cliquez sur les affirmations que vous pensez être correctes :
 + $%{a}%$ est le minimum de $%A%$
 - $%{b}%$ est la borne supérieure de $%A%$
 - $%{a}%$ est la borne inférieure de $%A%$
-==
 
-questions==
-    d=random.randint(1,10)
-    c=random.randint(-10,0)
-    e=c+1
+
 
 ** Soit $%B=]{c} , {d}]%$ un intervalle dans $% \mathbb{R} %$. 
 Cliquez sur les affirmations que vous pensez être correctes :
