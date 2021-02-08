630b09b8140bd421c40702b87b53a2f81ee7869f
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-02-08 14:37:32 +0100

    drevuz modified Yggdrasil/AAAA/dominique/caroussel/multistep.pl

--- AAAA/dominique/caroussel/multistep.pl
+++ AAAA/dominique/caroussel/multistep.pl
@@ -92,6 +92,7 @@ for i, q in enumerate(list_questions):
 
 
 # entêtes du mnaège
+text += "<div id='debut'></div>"
 text +=  """<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false" >
     <!-- Indicators -->
     <ol class="carousel-indicators">
