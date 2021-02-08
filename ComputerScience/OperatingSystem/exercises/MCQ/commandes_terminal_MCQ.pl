61fdf8ae157898592f2ef29eff177f12cb0aee88
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-08 23:55:19 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/commandes_terminal_MCQ.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/commandes_terminal_MCQ.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/commandes_terminal_MCQ.pl
@@ -1,10 +1,32 @@
-# Copyright 2020 Nicolas Borie <nicolas.borie@u-pem.fr>
-tag=system|unix|terminal|bash
+#*****************************************************************************
+#  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+tag=système|unix|terminal|bash|commande
 
 extends = /ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
 
-title = Quizz sur quelques commandes terminal Unix
-
+#****************************************************************************
+# PANNEAU DE CONTROLE ICI
+#****************************************************************************
+title=Quizz sur quelques commandes terminal Unix
 questions=@ commandes_terminal.txt
-
-
+nb_question=5
+feedback=True
+min_options=4
+max_options=8
+#****************************************************************************
+# FIN DU PANNEAU DE CONTROLE : NE PLUS RIEN TOUCHER APRES
+#****************************************************************************
