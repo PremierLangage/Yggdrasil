ff1ffdb4473c50a696bbb015cb40ae0324035ac9
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-02-08 11:43:44 +0100

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
@@ -12,10 +12,10 @@ questions==
 ** Soit $%A=[{a} , {b}[%$ un intervalle dans $% \mathbb{R} %$. 
 Cliquez sur les affirmations que vous pensez être correctes :
 + $%A%$ est borné
-- $%{b | latex }%$ est le maximum de $%A%$
-+ $%{a | latex }%$ est le minimum de $%A%$
-- $%{b | latex }%$ est la borne supérieure de $%A%$
-- $%{a | latex }%$ est la borne inférieure de $%A%$
+- $%{b}%$ est le maximum de $%A%$
++ $%{a}%$ est le minimum de $%A%$
+- $%{b}%$ est la borne supérieure de $%A%$
+- $%{a}%$ est la borne inférieure de $%A%$
 ==
 
 questions==
@@ -27,8 +27,8 @@ questions==
 Cliquez sur les affirmations que vous pensez être correctes :
 + $%B%$ est majoré 
 - $%B%$ est minoré par {e}
-- $%{c | latex }%$ est le minimum de $%B%$
-+ $%{d | latex }%$ est le maximum de $%B%$
-- $%{c | latex }%$ est la borne supérieure de $%B%$
-- $%{d | latex }%$ est la borne inférieure de $%B%$
+- $%{c}%$ est le minimum de $%B%$
++ $%{d}%$ est le maximum de $%B%$
+- $%{c}%$ est la borne supérieure de $%B%$
+- $%{d}%$ est la borne inférieure de $%B%$
 ==
