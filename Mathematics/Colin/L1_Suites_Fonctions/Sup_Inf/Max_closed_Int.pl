a99ac8569b2cc6e143386253fffef7abb9aa7639
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-02-08 11:36:32 +0100

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
@@ -5,14 +5,11 @@ tag= Majorant|Minorant|Borne supérieure|Borne inférieure
 
 
 # ce code permet d'ajouter des qestions 
-buildquestions==
-import random
-# questions=""
-for toto in range(1):
+questions==
     b=random.randint(1,10)
     a=random.randint(-10,0)
 
-* Soit $%A=[{a} , {b}[%$ un intervalle dans $% \mathbb{R} %$. 
+** Soit $%A=[{a} , {b}[%$ un intervalle dans $% \mathbb{R} %$. 
 Cliquez sur les affirmations que vous pensez être correctes :
 + $%A%$ est borné
 - $%{b | latex }%$ est le maximum de $%A%$
