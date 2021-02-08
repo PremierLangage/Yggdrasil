ad882cb23256e12016c71fdb1d14992cfd871e66
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-02-08 11:53:35 +0100

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
@@ -6,14 +6,23 @@ tag= Majorant|Minorant|Borne supérieure|Borne inférieure
 
 # ce code permet d'ajouter des qestions 
 
+
 questions==
-l=1
+import random
+    b=random.randint(1,10)
+    a=random.randint(-10,0)
 
+    d=random.randint(1,10)
+    c=random.randint(-10,0)
+    e=c+1
 
-** Soit {l} un intervalle dans $% \mathbb{R} %$. 
+** Soit A=[a , b[ un intervalle dans $% \mathbb{R} %$. 
 Cliquez sur les affirmations que vous pensez être correctes :
-+ {l}
-- nope
++ $%A%$ est borné
+- $%{b}%$ est le maximum de $%A%$
++ $%{a}%$ est le minimum de $%A%$
+- $%{b}%$ est la borne supérieure de $%A%$
+- $%{a}%$ est la borne inférieure de $%A%$
 
 
 
