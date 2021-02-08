ea1e1e28bdd97fe3cb327c49d9f1a01c78373deb
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-02-08 11:56:40 +0100

    cpetitjean modified Yggdrasil/Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl

--- Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
+++ Mathematics/Colin/L1_Suites_Fonctions/Sup_Inf/Max_closed_Int.pl
@@ -8,32 +8,22 @@ tag= Majorant|Minorant|Borne supérieure|Borne inférieure
 
 
 questions==
-import random
-# questions=""
-for toto in range(2):
-    b=random.randint(1,10)
-    a=random.randint(-10,0)
 
-    d=random.randint(1,10)
-    c=random.randint(-10,0)
-    e=c+1
+
+
+"""
+for _ in range(3):
+    a=random.randint(1,5)
+    b=random.randint(-5,5)
+    a2=a//random.randint(1,5)
+    func=f"{a}x^2{b}" if b<0 else f"{a}x^2+{b}"
     questions += f"""
-** Soit A=[a , b[ un intervalle dans $% \mathbb{R} %$. 
-Cliquez sur les affirmations que vous pensez être correctes :
-+ $%A%$ est borné
-- $%{b}%$ est le maximum de $%A%$
-+ $%{a}%$ est le minimum de $%A%$
-- $%{b}%$ est la borne supérieure de $%A%$
-- $%{a}%$ est la borne inférieure de $%A%$
+* Soit la fonction $%x  \mapsto  {func}%$ dites si 
+- est {a}-Lipschitzienne 
+- est {b}-Lipschitzienne 
+- est k-Lipschitzienne pour tout k
++ est Lipschitzienne sur l'interval ]-1,1[
+"""
 
 
-
-** Soit $%B=]{c} , {d}]%$ un intervalle dans $% \mathbb{R} %$. 
-Cliquez sur les affirmations que vous pensez être correctes :
-+ $%B%$ est majoré 
-- $%B%$ est minoré par {e}
-- $%{c}%$ est le minimum de $%B%$
-+ $%{d}%$ est le maximum de $%B%$
-- $%{c}%$ est la borne supérieure de $%B%$
-- $%{d}%$ est la borne inférieure de $%B%$
 ==
