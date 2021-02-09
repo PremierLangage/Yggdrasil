950511eaabcc943987b89a0bc832adc8d6e75c3a
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-02-09 15:54:18 +0100

    drevuz modified Yggdrasil/AAAA/dominique/latin/descl123.pl

--- AAAA/dominique/latin/descl123.pl
+++ AAAA/dominique/latin/descl123.pl
@@ -3,6 +3,8 @@ title= Exos de latin  desclinaisons 1,2,3
 
 text=
 
+@ mots.csv
+@ gendec.py
 before==
 
 from gendec import question
@@ -13,5 +15,28 @@ text, sol = question()
 
 ==
 
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+inputbox =: Input
+inputbox.type = text
+inputbox.placeholder = Answer
+inputbox.appearance = outline
+
+
+
+
+form==
+{{ inputbox|component}}
+==
+
+evaluator==
+if sol == inputbox.value:
+    grade = (100, '<span class="success-state">Good 👏👏👏</span>')
+else:
+    grade = (0, f'<span class="error-state">Bad answer  la bonne réponse : {sol}</span>')
+==
 
 
