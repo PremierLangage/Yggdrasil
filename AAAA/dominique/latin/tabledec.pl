5016a4a904f0556d41cfa4de940e0ad6c34917f5
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-02-10 12:14:48 +0100

    drevuz modified Yggdrasil/AAAA/dominique/latin/tabledec.pl

---
+++ AAAA/dominique/latin/tabledec.pl
@@ -0,0 +1,74 @@
+
+title= table
+text==
+|  cas     | Singulier | Pluriel |
+|----------|----------|----------|
+|nominatif|{{ inputbox|component}}|{{ inputbox|component}}|
+|vocatif|{{ inputbox|component}}|{{ inputbox|component}}|
+|accusatif|{{ inputbox|component}}|{{ inputbox|component}}|
+|génitif|{{ inputbox|component}}|{{ inputbox|component}}|
+|datif|{{ inputbox|component}}|{{ inputbox|component}}|
+|ablatif|{{ inputbox|component}}|{{ inputbox|component}}|
+==
+
+form==
+|  cas     | Singulier | Pluriel |
+|----------|----------|----------|
+|nominatif|{{ inputbox|component}}|{{ inputbox|component}}|
+|vocatif|{{ inputbox|component}}|{{ inputbox|component}}|
+|accusatif|{{ inputbox|component}}|{{ inputbox|component}}|
+|génitif|{{ inputbox|component}}|{{ inputbox|component}}|
+|datif|{{ inputbox|component}}|{{ inputbox|component}}|
+|ablatif|{{ inputbox|component}}|{{ inputbox|component}}|
+
+==
+
+title= Exos de latin  desclinaisons 1,2,3
+
+text=
+
+@ mots.csv
+@ gendec.py
+before==
+
+from gendec import question
+
+text, sol = question()
+
+nbq=0
+nbbr=0
+
+==
+
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+inputbox =: Input
+inputbox.type = text
+inputbox.placeholder = Answer
+inputbox.appearance = outline
+
+
+form==
+{{ inputbox|component}}
+==
+
+evaluator==
+nbq += 1
+
+if sol == inputbox.value:
+    nbbr += 1 
+    grade = (100, f'<span class="success-state">Good 👏👏👏 </span> <span > {nbbr}/{nbq} </span>')
+else:
+    grade = (0, f'<span class="error-state">Bad answer</span> <br/><span >{text}<br> la bonne réponse  était : {sol}  {nbbr}/{nbq} </span>')
+
+
+from gendec import question
+
+text, sol = question()
+==
+
+
+ 
