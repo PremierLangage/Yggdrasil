f5276af829c1d28b5f0fc226a38a4b7c070d263f
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-10 20:03:57 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/MCQ/notion_de_fichier_MCQ.pl

--- ComputerScience/OperatingSystem/exercises/MCQ/notion_de_fichier_MCQ.pl
+++ ComputerScience/OperatingSystem/exercises/MCQ/notion_de_fichier_MCQ.pl
@@ -1,9 +1,33 @@
-# Copyright 2019 Nicolas Borie <nicolas.borie@u-pem.fr>
+#*****************************************************************************
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
 tag=system|unix|fichier
 
-extends = /model/AMC_TXT.pl
+extends = /ComputerScience/OperatingSystem/exercises/MCQ/caroussel_template.pl
 
-title = Quizz sur la notion de fichier en Unix
-
+#****************************************************************************
+# PANNEAU DE CONTROLE ICI
+#****************************************************************************
+title=Quizz sur la notion de fichier en Unix
 questions=@ notion_de_fichier.txt
+nb_question=5
+feedback=True
+min_options=4
+max_options=8
+#****************************************************************************
+# FIN DU PANNEAU DE CONTROLE : NE PLUS RIEN TOUCHER APRES
+#****************************************************************************
 
