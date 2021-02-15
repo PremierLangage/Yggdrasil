4d284f83a568aed9926536d0f4e6d3449b67d256
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-15 12:06:55 +0100

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_three_max.pl

--- ComputerScience/Games/Doctest_me_bro/doctest_three_max.pl
+++ ComputerScience/Games/Doctest_me_bro/doctest_three_max.pl
@@ -14,11 +14,15 @@
 #            https://creativecommons.org/licenses/by-sa/3.0/fr/
 #*****************************************************************************
 
-extends=doctest_me_bro_template.pl
+tag=doctest|garantir|test|python|liste|max
 
 title=Doctest challenge : les trois plus grands
 
-text==
+extends=doctest_me_bro_template.pl
+
+author = Nicolas Borie
+
+text==#|markdown|
 Les élèves avaient la consigne suivante :   
 
 <div style="color:white; background-color: black; padding: 0.5em; border-radius: 0.5em;">
