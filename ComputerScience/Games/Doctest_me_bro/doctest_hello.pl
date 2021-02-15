50d8d220e6c82e07a2f45e8e6b67f26b0205d41c
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-02-15 12:55:08 +0100

    nborie modified Yggdrasil/ComputerScience/Games/Doctest_me_bro/doctest_hello.pl

---
+++ ComputerScience/Games/Doctest_me_bro/doctest_hello.pl
@@ -0,0 +1,77 @@
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+tag=doctest|garantir|test|python|simple
+
+title=Doctest challenge : un hello world
+
+extends=doctest_me_bro_template.pl
+
+author = Nicolas Borie
+
+text==#|markdown|
+Les élèves avaient la consigne suivante :   
+
+<div style="color:white; background-color: black; padding: 0.5em; border-radius: 0.5em;">
+Écrivez une fonction Python <b>hello</b> qui ne prend aucun argument et qui affiche 
+`Hello World!` sur la sortie standard (à l'écran) avant de retourner à la ligne. Votre
+fonction ne devra retourner aucune valeur.
+</div>
+
+Proposez des doctests Python pour detecter la validité et les erreurs 
+potentielles des codes rendus par les élèves.
+==
+
+good_code_1==#|python|
+def trois_max(L):
+    """
+    TEST_INCLUSION
+    """
+    print('Hello World!')
+==
+
+bad_code_1==#|python|
+def trois_max(L):
+    """
+    TEST_INCLUSION
+    """
+    print('Hello World!')
+    return 1
+==
+
+bad_code_2==#|python|
+def trois_max(L):
+    """
+    TEST_INCLUSION
+    """
+    print('Hello world!')
+==
+
+bad_code_3==#|python|
+def trois_max(L):
+    """
+    TEST_INCLUSION
+    """
+    print('Hello World!', end="")
+==
+
+bad_code_4==#|python|
+def trois_max(L):
+    """
+    TEST_INCLUSION
+    """
+    print('hello world!')
+==
