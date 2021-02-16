dd0964b44d6952ef760d9e41bee6a90240edd3ec
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2021-02-16 10:25:07 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/algo/ABR/extraction.pl

--- ComputerScience/python/AP1-1920/drafts/zip/algo/ABR/extraction.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/algo/ABR/extraction.pl
@@ -236,10 +236,10 @@ int x;
 checks_args_stdin==#|python|
 [["Arbre feuille", [], "4"],
  ["Abre à 3 nœuds", [], "4 2 5"],
- ["Peigne gauche", [], "12 10 9 8 7 0"],
+ ["Peigne gauche", [], "12 10 9 8 7 4 0"],
  ["Peigne droit", [], "7 8 9 10 12 55"],
- ["Arbre aléatoire ", [],choice (["42 22 52 5 66 70","1 2 3 1 7 42 12"])],
-  ["Arbre aléatoire II", []," ".join([ str(randint(1,100)) for i in range(10) ])]
+ ["Arbre aléatoire ", [],choice (["42 4 22 52 5 66 70","1 2 3 1 7 42 12 4"])],
+  ["Arbre aléatoire II", []," ".join([ str(randint(1,100)) for i in range(10) ])+"4"]
 ]
 ==
 
