3314800ab10d5692a6ac8473f0bca6380028c6c9
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-02-17 21:24:29 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/BorneSup/borneSupMagda.pl

--- AAAA/ECanovas/BorneSup/borneSupMagda.pl
+++ AAAA/ECanovas/BorneSup/borneSupMagda.pl
@@ -36,7 +36,7 @@ evaluator==#|python|
 
 from genQuest import initDonnees,question
 
-if nbQuestion > 0:
+if iQuestion > nbQuestion:
 
   if bReponse == input.value:
     nbbr += 1 
@@ -44,12 +44,11 @@ if nbQuestion > 0:
   else:
     grade = (0, f'<span class="error-state">Bad answer</span> <br/><span >{text}<br> la bonne réponse  était : {bReponse} score {nbbr}/{nbQuestion} </span>')
  
- # boucle sur les 3 questions
-  nbQuestion -= 1
+  # boucle sur les 3 questions
   iQuestion += 1
 
   intituleQuestion, indicQuestion, bReponse = question(iQuestion,uneLigne)
-  text=f'<p><strong>Question {iQuestion}</strong></p><p>{intituleQuestion}</p>'
+  text=f'<p><strong>Question {iQuestion} sur {nbQuestion}</strong></p><p>{intituleQuestion}</p>'
   #form="{{ input|component}}"
 
 else:
