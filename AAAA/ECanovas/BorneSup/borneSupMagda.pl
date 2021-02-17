7aac7f1bf9c39d7f4ca67801132e307940f78cd4
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-02-17 18:32:30 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/BorneSup/borneSupMagda.pl

--- AAAA/ECanovas/BorneSup/borneSupMagda.pl
+++ AAAA/ECanovas/BorneSup/borneSupMagda.pl
@@ -1,8 +1,4 @@
-# 2 modes : Entrainement et Evaluation
 
-# template Tout nouveau malheureusement... feedback pas encore programmé...
-# from @magdalena bornesup1.pl
-# extends=/ComputerScience/C/examples/Stoi_le_caroussel.pl
 extends=/model/mathinput.pl
 
 title= Borne Sup
@@ -23,12 +19,40 @@ with open("bornesup.csv", newline='') as csvfile:
      listeBornesup=list(csv.DictReader(csvfile, delimiter=';'))
 
 uneLigne=random.choice(listeBornesup)
+
+# ENCHAINEMENT Exercice à 3 questions puis reboucle
+nbQuestion=3
+inviteSuite="Valider permet de répondre à la question {{nbQuestion-2}}"
 ==
 
 text==#|markdown|
 {{uneLigne}}
 
-Quel est le sup de l'ensemble {{uneLigne['C1:type']}} càd {{uneLigne['C2:Partie de $%R%$']}}
+<p>Quel est le sup de l'ensemble {{uneLigne['C1:type']}} càd {{uneLigne['C2:Partie de $%R%$']}}</p>
+
+<p>{{inviteSuite}}</p>
 ==
 form==
+==
+evaluator==
+if nbQuestion > 0 :
+ # premiere question
+ if nbQuestion==3:
+  if uneLigne[] == inputbox.value:
+    nbbr += 1 
+    grade = (100, f'<span class="success-state">Good 👏👏👏 </span> <span > {nbbr}/{nbq} </span>')
+  else:
+    grade = (0, f'<span class="error-state">Bad answer</span> <br/><span >{text}<br> la bonne réponse  était : {sol}  {nbbr}/{nbq} </span>')
+ # seconde question
+ text, sol = question()
+else:
+  # réinitialisation des alea mais aussi des définitions , un impact sur l'exécution ?
+  uneLigne=random.choice(listeBornesup)
+  nbQuestion = 3
+  text=""
+  form=""
+  if nbbr == nbq :
+    grade = (100, f'<span class="success-state">BRAVO!tout est bon 👏👏👏 </span> <span > {nbbr}/{nbq} </span>')
+  else:
+    grade = (-nbbr/nbq, f'<span class="error-state">Quelques bonnes réponses tout de même? = {nbbr}/{nbq} </span>')
 ==
