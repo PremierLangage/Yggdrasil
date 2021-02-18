0cd5c8938562e9eaadb3ecfb0a4d56b4d757dfd4
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2021-02-18 11:52:30 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/algo/filsgauche_freredroit/ajout.pl

--- ComputerScience/python/AP1-1920/drafts/zip/algo/filsgauche_freredroit/ajout.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/algo/filsgauche_freredroit/ajout.pl
@@ -21,3 +21,122 @@ extends= /ComputerScience/Algo/Tree/templates/zipsprogCwithtree.pl
 
 #author=Marc Zipstein
 title=Ajout dans un arbre lexical fils gauche frère droit
+tag=recherche
+
+editor.height=300px
+
+text== 
+On représente un lexique avec un arbre fils gauche frère droit. On utilise le type:  
+    typedef struct noeud{
+        char lettre;
+        struct noeud *filsg,*frered;
+}Noeud,*Arbre;
+Les noeuds freressont ordonnés dans l'ordre ascii,  
+On ne traite  pas les accents- on confond "interne" et "interné"
+un mot est une suite de moins de 257 caratères (telle que lue par scanf("%256s,mot))
+Le caratère de fin de mot est conservé dans l'arbre.
+ecrire une fonction nbarbre qui reçoit un arbre et renvoie son nombre de mots
+==
+
+editor.code==#|c|
+
+
+int nbmots(...){
+  
+} 
+
+
+==
+
+solution==#|c|
+
+
+int nbmots(arbre t){
+  if (t == NULL)
+     return 0;
+  if (t->lettre=='\0')
+      return 1+nbmots(t->frered);
+  return nbmots(t->filsg)+nbmots(t->frered) ;
+} 
+==
+
+
+
+code_before==#|c|
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+typedef struct noeud{
+  char letrre;
+  struct noeud *filsg;
+  struct noeud *frered;
+}Noeud,Arbre;
+
+Node * allocate_node(char val){
+  Node * n = malloc(sizeof(Node));
+
+  if (!n){
+    fprintf(stderr, "problème allocation mémoire\n");
+    return NULL;
+  }
+  n->filsg = NULL;
+  n->frere = NULL;
+  n->lettre = val;
+  return n;
+}
+==
+
+code_after==#|c|
+
+void ajoute_branche  (Arbre *a,char *mot){
+  if((*a=allocate_node(*mot))!=NULL){
+    if (*mot != '\0')
+      ajoute_branche  (&((*A)->filsg),mot+1);
+  }
+}
+
+void ajoute_mot(Arbre *a, char *mot){
+    if (*a==NULL)
+	ajoute_branche(a,mot);
+    else {
+	if( (*a)->lettre < *mot) 
+  ajoute_mot(&((*A)->frered),mot);
+else
+  if(( (*a)->lettre == *mot) && (*mot != '\0')) 
+    ajoute_mot(&((*a)->filsg),mot+1);
+  else
+    if(*mot != '\0'){
+      Arbre tmp=NULL;
+      ajoutebranche(& tmp,mot);
+      tmp->frered=*a;
+      *a=tmp;
+    }
+ }
+}
+
+ int build_tree(Arbre * t){
+    char mot|257];
+   
+    while(1==    scanf("%256", &mot))   {
+      if (0==ajoute_mot(t,mot)){
+      fprintf(stderr, "problème allocation mémoire\n");
+      return 0;
+      }
+    }
+    return 1;
+} 
+
+int main(int argn,char * argv[]){
+    Arbre t;
+    build_tree(&t);
+    printf("%d",nbmots(t));
+    return 0;
+    +
+==
+
+
+
+checks_args_stdin==#|python|
+	[["Arbre feuille","","a"],
+	 ["arbre à 3 mots","","le,la,les"]
+	]
