7cfaaed17afb7ee405d8c5118a2d9a982116e41d
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2021-02-19 12:54:31 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/algo/filsgauche_freredroit/pluslong.pl

--- ComputerScience/python/AP1-1920/drafts/zip/algo/filsgauche_freredroit/pluslong.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/algo/filsgauche_freredroit/pluslong.pl
@@ -154,7 +154,7 @@ int main(int argn,char * argv[]){
 
 checks_args_stdin==#|python|
 	[["Arbre feuille","","a"],
-	 ["arbre à 3 mots","","le ,la ,les "],
-   ["arbre aléatoire","","un ,une ,du ,de ,des"+" ,"+choice(['dune',"deux","le","route","un","une"])]
+	 ["arbre à 3 mots","","le la les "],
+   ["arbre aléatoire","","un une du de des"+" "+choice(['dune',"deux","le","route","un","une"])]
 	]
 ==
