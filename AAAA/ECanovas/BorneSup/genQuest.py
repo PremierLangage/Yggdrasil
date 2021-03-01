005f5eb4ca833ebd086bae1cf8aa350c7572a5d7
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-01 18:15:59 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/BorneSup/genQuest.py

--- AAAA/ECanovas/BorneSup/genQuest.py
+++ AAAA/ECanovas/BorneSup/genQuest.py
@@ -37,9 +37,9 @@ def question(numero,uneLigne):
     indication="(On écrira +oo si l'ensemble n'est pas majoré et -oo si l'ensemble est vide)"
     bReponse=uneLigne['C5:sup']
     if uneLigne['C5:sup'] ==' +':
-     bReponse="$%+\infty%$ "
+     bReponse="$%+\infty%$"
     else:
-     bReponse="$%-\infty%$ "
+     bReponse="$%-\infty%$"
   if numero == 2:
     intiQuestion="Donner un majorant de l'ensemble {{uneLigne['C2:Partie de $%R%$']}}"
     indication="(On écrira +oo si l'ensemble n'est pas majoré et - oo si l'ensemble est vide)"
