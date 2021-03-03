afee4cd0bf659be23ebc7f54b69e81b48582b836
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-03-03 10:02:30 +0100

    drevuz modified Yggdrasil/AAAA/ECanovas/BorneSup/exobs.pl

---
+++ AAAA/ECanovas/BorneSup/exobs.pl
@@ -0,0 +1,47 @@
+
+
+extends = /model/mathinput.pl
+
+title= Borne Sup
+
+@ bornesup.csv
+@ genQuest.py
+
+
+
+before ==#|python|
+
+from genQuest import initDonnees, question
+
+uneLigne=initDonnees()
+
+# ENCHAINEMENT Exercice à 3 questions puis reboucle
+nbQuestion=3
+iQuestion=1
+# nombre de bonnes réponses
+nbbr=0
+
+intituleQuestion, indicQuestion, bReponse = question(1,uneLigne)
+
+==
+
+text==#|markdown|
+<p><strong>Question {{iQuestion}} sur {{nbQuestion}}</strong></p>
+<p>{{intituleQuestion}} ?</p>
+==
+form==
+{{ input|component}}
+==
+
+evaluator==
+
+
+if bReponse == input.value:
+    nbbr += 1 
+    kback = f'<span class="success-state">Good 👏👏👏 </span> <span > {nbbr}/{nbQuestion} </span><p>{intituleQuestion}</p>'
+    grade = (100, kback)
+else:
+    kback =  f'<span class="error-state">$%{input.value}%$ et {input.value}</span><p>{intituleQuestion}</p><span >{text}<br> la bonne réponse  était : {bReponse} score {nbbr}/{nbQuestion} </span>'
+    grade = (0,kback)
+
+==
