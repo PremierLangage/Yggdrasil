a469f4d6a326ce0bf0253be781f684462ce3a1d3
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-08 12:00:54 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/BorneSup/genQuest.py

--- AAAA/ECanovas/BorneSup/genQuest.py
+++ AAAA/ECanovas/BorneSup/genQuest.py
@@ -59,7 +59,7 @@ def question(numero,uneLigne):
 
      testReponse=functest2
   if numero == 3:
-    intiQuestion="L'ensemble {{uneLigne['C2:Partie de $%R%$']}} a-t-il un plus grand élément ?"
+    intiQuestion="L'ensemble {{uneLigne['C2:Partie de $%R%$']}} a-t-il un plus grand élément"
     indication=""
     if uneLigne['C5:sup'] =='+':
      bReponse="T"
