ea9740c899d61c72aa6d093452bc79003b8d7019
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-03-09 10:10:19 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/templates/bash_template.pl

--- ComputerScience/OperatingSystem/templates/bash_template.pl
+++ ComputerScience/OperatingSystem/templates/bash_template.pl
@@ -81,7 +81,7 @@ else:
     if not nb_attempt:
         nb_attempt = 1
     else:
-        nb_attempt = int(nb_atempt) + 1
+        nb_attempt = int(nb_attempt) + 1
 
     best_grade = int(best_grade) if best_grade else 0
     note_eff = 50 + (200 // (3+nb_attempt))
