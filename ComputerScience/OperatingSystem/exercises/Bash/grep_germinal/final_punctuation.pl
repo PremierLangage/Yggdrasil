821ed216edab5aad3a62994ef882f346d3c89397
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-03-09 10:51:02 +0100

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/grep_germinal/final_punctuation.pl

--- ComputerScience/OperatingSystem/exercises/Bash/grep_germinal/final_punctuation.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/grep_germinal/final_punctuation.pl
@@ -21,20 +21,23 @@ extends=/ComputerScience/OperatingSystem/templates/bash_template.pl
 author=Nicolas Borie
 title=Ponctuation finale des lignes de Germinal.
 
-before==
+before==#|python|
 import random
 
 things = [("le nombre de lignes se terminant par un point d'interrogation", "190\n"),
           ("le nombre de lignes se terminant par un point d'exclamation", "375\n"),
-          ("le nombre de lignes se terminant par un point", "3234\n")]
+          ("le nombre de lignes se terminant par un point", "2372\n")]
 (name_thing, expected_stdout) = random.choice(things)
 ==
 
-text==
+text==#|markdown|
 Un fichier **Germinal.txt** (contenant le texte intégral de l'oeuvre de E. Zola) a 
 été placé dans le répertoire courant de travail. Établissez une **commande shell** 
 qui affiche **{{ name_thing }}** apparaissant dans le fichier. Débrouillez 
 vous pour que votre résultat numérique soit affiché seul sur une ligne.
 ==
 
-
+solution==
+# dépend de la question tirée ! ? .
+grep Germinal.txt -e "{!?.}$" | wc -l
+==
