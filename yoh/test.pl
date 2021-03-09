b4fde954e3194c443ecd1f92091e74cb41d0d813
YOHAN RAMEAU <yrameau@etud.u-pem.fr>
2021-03-09 10:35:27 +0100

    yrameau modified Yggdrasil/yoh/test.pl

--- yoh/test.pl
+++ yoh/test.pl
@@ -1,29 +1,81 @@
-title=Test 
-author=Yohan Rameau
+#  Author: Coumes Quentin     Mail: qcoumes@etud.u-pem.fr
 
-editor.code==
+title= test
+
+author = Yohan Rameau
+
+text==
+Combien <i>font</i> ***3 + 4*** ?
+==
+
+form==
+<div class="input-group">
+    <input name="answer" id="form_answer" type="number" class="form-control" value="{{ answers__.answer }}" required/>
+</div>
+==
+
+settings.oneshot=yes
+settings.allow_reroll=yes
+
+
+evaluator==
+import traceback
 import sys
 
-if __name__ == "__main__":
-    n = int(sys.argv[1])
-
+try: 
+    if int(response['answer']) == 3 + 4:
+        grade = (100, "Bonne réponse")
+    else:
+        grade = (0, "Mauvaise réponse")
+except:
+    print(traceback.format_exc(), file=sys.stderr)
+    grade = (-1, "Merci de rentrer un entier")
 ==
 
-text ==
-You are given an integer as argument of the program.
+@ ~/utils/sandboxio.py
+@ ~/grader/evaluator.py [grader.py]
 
-Write a script that outputs:
-
-* `Positive` if the given integer is greater than 0.
-* `Negative` if the given integer is lesser than 0.
-* `Null` if the given integer equals 0.
-==
-
-stdout_tests==
-"Positive" "Positive" "5"
-"Negative" "Negative" "-5"
-"Null" "Null" "0"
-!"" "Null" "0"
-!"" "Positive" "42"
-!"" "Negative" "-42"
+tests %=
+{
+    "correct" : {
+        "response" : {
+            "answer" : 7
+        },
+        "grade" : 100,
+        "feedback" : "Bonne réponse\n"
+    },
+    "incorrect" : {
+        "response" : {
+            "answer" : 8
+        },
+        "grade" : 0,
+        "feedback" : "Mauvaise réponse\n"
+    },
+    "not working" : {
+        "response" : {
+            "answer" : 8
+        },
+        "grade" : 50,
+        "feedback" : "Bonne réponse\n",
+        "seed" : 130
+    },
+    "only grade" : {
+        "response" : {
+            "answer" : 7
+        },
+        "grade" : 100
+    },
+    "only feedback" : {
+        "response" : {
+            "answer" : 7
+        },
+        "feedback" : "Bonne réponse\n"
+    },
+    "grader failure" : {
+        "response" : {
+            "answer" : "aa"
+        },
+        "feedback" : "Bonne réponse\n"
+    }
+}
 ==
