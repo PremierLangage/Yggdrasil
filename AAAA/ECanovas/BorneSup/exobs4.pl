eb78e3cbc7f592348699174000281da053906437
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-12 15:36:46 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/BorneSup/exobs4.pl

--- AAAA/ECanovas/BorneSup/exobs4.pl
+++ AAAA/ECanovas/BorneSup/exobs4.pl
@@ -40,7 +40,7 @@ def Convert(string):
     return li 
 bVal=list(bReponse['bonnes'].split(","))
 aVal=list(bReponse['autres'].split(","))
-checkbox.setdata_from_rw(bVal,aVal, 4, rd.randint(1, 4))
+checkbox.setdata_from_rw(bVal,aVal, 4, rd.randint(1, 3))
 
 ==
 
