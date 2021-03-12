3a32055a85718014968c410b9dcfc20a2a39facc
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-03-12 14:07:43 +0100

    nborie modified Yggdrasil/ComputerScience/logic/diagram/eval_diagram_3.pl

--- ComputerScience/logic/diagram/eval_diagram_3.pl
+++ ComputerScience/logic/diagram/eval_diagram_3.pl
@@ -73,7 +73,7 @@ text==#|markdown|
 ==
 
 form==#|markdown|
-Évaluer l'unique sortie **f** de ce circuit logique dont voici les valeurs des 
+Évaluer l'unique sortie <b>f</b> de ce circuit logique dont voici les valeurs des 
 variables en entrées.<br>
 {% for k,v in eval_point.items() %} <b>{{k}}</b> : <b>{{v}}</b>, {% endfor %}
 <center>
