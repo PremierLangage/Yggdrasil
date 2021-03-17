eaba018d20dee49262f83a07c5b187bf7281b37b
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-03-17 22:51:59 +0100

    nborie modified Yggdrasil/ComputerScience/Games/MiniBrain/intermediaire/bin_decomposition.pl

--- ComputerScience/Games/MiniBrain/intermediaire/bin_decomposition.pl
+++ ComputerScience/Games/MiniBrain/intermediaire/bin_decomposition.pl
@@ -48,16 +48,19 @@ n7 = randint(512, 1023)
 
 minibrain_action_before=[ ['décomposition de 1', 'st 1 $20'],
                           ['décomposition de 0', 'st 0 $20'],
-                          ['décomposition de 7', 'st 7 $20'] ]
+                          ['décomposition de 7', 'st 7 $20'], ]
 for n in [n1, n2, n3, n4, n5, n6, n7]:
     minibrain_action_before.append(['décomposition de '+str(n), 'st '+str(n)+' $20'])
 
+minibrain_action_before=str(minibrain_action_before)
 
 minibrain_check_after=[ [(21, 1)],
                         [],
                         [(21, 1), (22, 1), (23, 1)], ]
 for n in [n1, n2, n3, n4, n5, n6, n7]:
     minibrain_check_after.append([(21+i, val) for i, val in enumerate(reversed(bin(n)[2:]))])
+
+minibrain_check_after=str(minibrain_check_after)
 ==
 
 solution==
