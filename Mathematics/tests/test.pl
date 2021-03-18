d167dd85312468d1297e742aa7f9500479c84aba
David DOYEN <David.Doyen@u-pem.fr>
2021-03-18 13:45:29 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/test.pl

--- Mathematics/tests/test.pl
+++ Mathematics/tests/test.pl
@@ -17,21 +17,21 @@ form ==
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">`
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
-<p><span id="answer">x=</span></p>
+<p><span id="my_id">x=</span></p>
 ==
 
 extrajs ==
 <script>
-    var MQ = MathQuill.getInterface(2);
-  var answerSpan = document.getElementById('answer');
-  var answerMathField = MQ.MathField(answerSpan, {
-    handlers: {
-      edit: function() {
-        var enteredMath = answerMathField.latex(); // Get entered math in LaTeX format
-        checkAnswer(enteredMath);
-      }
-    }
-  });
+var MQ = MathQuill.getInterface(2);
+var htmlElement = document.getElementById('my_id');
+var config = {
+  handlers: { edit: function(){ ... } },
+  restrictMismatchedBrackets: true
+};
+var mathField = MQ.MathField(htmlElement, config);
+
+mathField.latex('2^{\\frac{3}{2}}'); // Renders the given LaTeX in the MathQuill field
+mathField.latex(); // => '2^{\\frac{3}{2}}'
 </script>
 ==
 evaluator ==
