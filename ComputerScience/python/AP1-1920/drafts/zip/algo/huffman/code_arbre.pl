50797fd8a137e31b7ce6111e55567c3ef47ab0f2
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2021-03-18 17:19:04 +0100

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/algo/huffman/code_arbre.pl

--- ComputerScience/python/AP1-1920/drafts/zip/algo/huffman/code_arbre.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/algo/huffman/code_arbre.pl
@@ -1,5 +1,4 @@
 
-
 #*****************************************************************************
 #  Copyright (C) 2020 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
 #
@@ -20,23 +19,72 @@ extends=/ComputerScience/C/template/std_progC.pl
 
 
 #author=Marc Zipstein
-title=odage d'un arbre de Huffman
+title=Codage d'un arbre de Huffman
 tag=recherche
 
 editor.height=300px
 
 text== 
-On FOR
-Ecrire une fonction **pluslong** qui reçoit un arbre et renvoie la longueur du pluslong mot.
-
+Ecrire une fonction **code_arbre** qui reçoit un arbre et l'indice de la racine et renvoie une chaine de caractere
+contenant le code de l'arbre. Pour simplifier le code est écrit sur des octets (avec des char) et non bit a bit;on utilise les conventions:
+	0 désigne un nnoeud interne
+	1  désigne une feuille
+	[z] désigne le code ascii de la lettre z.
 ==
-
 editor.code==#|c|
+char * code_arbre_aux(Noeud* a,int racine,int indice,int*i,char *s){
+   if(indice >(racine)/2)/*noeud interne*/{
+     s[*i]='0';(*i)++;
+    s[*i]='\0';
+    code_arbre_aux( a, racine,a[indice].gauche,i,s);
+    code_arbre_aux( a, racine,a[indice].droit,i,s);
+ }
+ else{
+   s[*i]='1';(*i)++;
+   s[*i]='[';(*i)++;
+   s[*i]=a[indice].lettre;(*i)++;
+   s[*i]=']';(*i)++;
+   s[*i]='\0';
+ }
+}
 
+char * code_arbre(Noeud* a,int racine){
+static  char s[512];
+    int i=0;
+  
+    s[0]='\0';
+    code_arbre_aux(a,racine,racine,&i,s);  
+    s[i]='\0';
+    return s;
+}
 ==
 
 solution==#|c|
+char * code_arbre_aux(Noeud* a,int racine,int indice,int*i,char *s){
+   if(indice >(racine)/2)/*noeud interne*/{
+     s[*i]='0';(*i)++;
+    s[*i]='\0';
+    code_arbre_aux( a, racine,a[indice].gauche,i,s);
+    code_arbre_aux( a, racine,a[indice].droit,i,s);
+ }
+ else{
+   s[*i]='1';(*i)++;
+   s[*i]='[';(*i)++;
+   s[*i]=a[indice].lettre;(*i)++;
+   s[*i]=']';(*i)++;
+   s[*i]='\0';
+ }
+}
 
+char * code_arbre(Noeud* a,int racine){
+static  char s[512];
+    int i=0;
+  
+    s[0]='\0';
+    code_arbre_aux(a,racine,racine,&i,s);  
+    s[i]='\0';
+    return s;
+}
 
 ==
 
@@ -48,21 +96,42 @@ code_before==#|c|
 #include <string.h>
 typedef struct noeud{
   char lettre;
-*filsg;
- *frered;
-}Noeud,*Arbre;
-}
+  int frequence;
+int gauche;
+int  droit;
+}Noeud;
 ==
 
 code_after==#|c|
-
+int main(int argn,char *argv[]){
+  int racine,i;
+  Noeud *arbre;
+  char *s;
+  printf("racine :");
+  scanf("%d",&racine);
+  arbre=malloc(racine*sizeof(Noeud));
+  if(arbre !=NULL){
+    for(i=0;i<=(racine)/2;i++){
+      printf("lettre frequence :\n");
+      scanf(" %c %d",&(arbre[i].lettre),&(arbre[i].frequence));
+      arbre[i].gauche=-1;
+    }
+    for (;i<=racine;i++){
+      printf("noeud interne %d\n",i);     
+      scanf("%d%d%d",&(arbre[i].frequence),&(arbre[i].gauche),&(arbre[i].droit));
+      }
+    affiche_arbre(arbre,racine);
+    s=code_arbre(arbre,racine);
+    printf("cod e apres %s\n",s);free(arbre);
+  }
+    return 0;
+}
 ==
 
 
 
 checks_args_stdin==#|python|
-	[["Arbre feuille","","a"],
-	 ["arbre à 3 mots","","le la les "],
-   ["arbre aléatoire","","un une du de des"+" "+choice(['dune',"deux","le","route","un","une"])]
+	[
+    ["Arbre 2 feuilles","","2 a 4 b 5 9 0 1"],
 	]
 ==
