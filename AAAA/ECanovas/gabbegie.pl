c9cdf98e0761cdd25ca43438f9743272f69feba6
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-18 09:39:05 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/gabbegie.pl

---
+++ AAAA/ECanovas/gabbegie.pl
@@ -0,0 +1,18 @@
+extends = /gift/templates/qshortset.pl
+
+title==
+Gab Completing
+==
+
+text==#|markdown|
+Compléter le code suivant pour que la fonction puisse être opérationnelle
+
+il était une fois <b>_____</b> qui  _____
+
+== 
+
+
+choices==
+=lapin #Bravo
+=cerf
+== 
