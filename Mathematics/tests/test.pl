fab3d51b3eb0ec6a693025db8b59733407d348da
David DOYEN <David.Doyen@u-pem.fr>
2021-03-18 13:45:56 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/test.pl

--- Mathematics/tests/test.pl
+++ Mathematics/tests/test.pl
@@ -17,13 +17,13 @@ form ==
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">`
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
-<p><span id="my_id">x=</span></p>
+<p><span id="myid">x=</span></p>
 ==
 
 extrajs ==
 <script>
 var MQ = MathQuill.getInterface(2);
-var htmlElement = document.getElementById('my_id');
+var htmlElement = document.getElementById('myid');
 var config = {
   handlers: { edit: function(){ ... } },
   restrictMismatchedBrackets: true
