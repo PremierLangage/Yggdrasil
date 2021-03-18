6e2f5d600649e53e55228bc2847ef279625fca70
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-03-18 12:48:37 +0100

    nborie modified Yggdrasil/ComputerScience/Games/MiniBrain/intermediaire/gcd.pl

--- ComputerScience/Games/MiniBrain/intermediaire/gcd.pl
+++ ComputerScience/Games/MiniBrain/intermediaire/gcd.pl
@@ -25,7 +25,7 @@ text==#|markdown|
 Proposez un code Mini-Brain calculant le <b>PGCD</b> (le plus grand diviseur commun) de 
 deux entiers. Les deux entiers seront placés en case mémoire <b>30</b> et <b>31</b> de la mémoire 
 centrale. Votre code mini-brain devra placer le résultat en case <b>32</b> une fois ce 
-dernier correctement calculé.
+dernier correctement calculé. Bon courage.
 </p>
 ==
 
@@ -39,29 +39,12 @@ from random import randint
 text += extra_doc
 
 n1 = randint(8, 15)
-n2 = randint(16, 31)
-n3 = randint(32, 63)
-n4 = randint(64, 127)
-n5 = randint(128, 255)
-n6 = randint(256, 511)
-n7 = randint(512, 1023)
-n8 = randint(512, 1023)
-n9 = randint(512, 1023)
-n10 = randint(512, 1023)
 
-minibrain_action_before=[ ['décomposition de 1', 'st 1 $20'],
-                          ['décomposition de 0', 'st 0 $20'],
-                          ['décomposition de 7', 'st 7 $20'], ]
-for n in [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10]:
-    minibrain_action_before.append(['décomposition de '+str(n), 'st '+str(n)+' $20'])
+minibrain_action_before=[ ['décomposition de 1', 'st 1 $20'] ]
 
 minibrain_action_before=str(minibrain_action_before)
 
-minibrain_check_after=[ [(21, 1)],
-                        [],
-                        [(21, 1), (22, 1), (23, 1)], ]
-for n in [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10]:
-    minibrain_check_after.append([(21+i, int(val)) for i, val in enumerate(reversed(bin(n)[2:]))])
+minibrain_check_after=[ [(21, 1)] ]
 
 minibrain_check_after=str(minibrain_check_after)
 ==
