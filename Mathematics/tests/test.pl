18cf104cb751063252770494e5b8b21c80dfcd69
David DOYEN <David.Doyen@u-pem.fr>
2021-03-18 13:35:53 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/test.pl

--- Mathematics/tests/test.pl
+++ Mathematics/tests/test.pl
@@ -16,7 +16,10 @@ $! \frac{ {{a}} }{ {{b}} } !$
 form ==
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">`
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script>
+var MQ = MathQuill.getInterface(2);
+</script>
 <p><span id="answer">x=</span></p>
 
 <script>
