7941ccaabfc3d532e27c57f60ba0f43c51f82fa2
Isabelle NAVIZET <Isabelle.Navizet@u-pem.fr>
2021-03-18 19:30:15 +0100

    inavizet modified Yggdrasil/AAAA/Isabelle/kekuleEditor-2.pl

--- AAAA/Isabelle/kekuleEditor-2.pl
+++ AAAA/Isabelle/kekuleEditor-2.pl
@@ -43,7 +43,7 @@ extrajs==
 <script>
 var composer = new Kekule.Editor.Composer(document.getElementById('composer'));
 composer.setCommonToolButtons(["undo", "redo","zoomIn", "zoomOut"]);
-composer.setChemToolButtons(["manipulate"", "erase", 'bond', 'atomAndFormula', 'ring', 'charge']);
+composer.setChemToolButtons(["manipulate", "erase", 'bond', 'atomAndFormula', 'ring', 'charge']);
 composer.setAllowedObjModifierCategories(["general", "chemStruct", "glyph", "style", "misc"]);
 
 function getSmile() {
@@ -58,6 +58,7 @@ function getSmile() {
 evaluator ==
 grade=(100,"Bonne réponse")
 ==
+
 
 
 
