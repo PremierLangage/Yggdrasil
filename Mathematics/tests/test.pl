07c388cba63fe8951d6c3b69e2644c2b6ec2f24c
David DOYEN <David.Doyen@u-pem.fr>
2021-03-18 13:39:34 +0100

    ddoyen modified Yggdrasil/Mathematics/tests/test.pl

--- Mathematics/tests/test.pl
+++ Mathematics/tests/test.pl
@@ -17,14 +17,12 @@ form ==
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">`
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
-<script>
-var MQ = MathQuill.getInterface(2);
-</script>
 <p><span id="answer">x=</span></p>
 ==
 
 extrajs ==
 <script>
+    var MQ = MathQuill.getInterface(2);
   var answerSpan = document.getElementById('answer');
   var answerMathField = MQ.MathField(answerSpan, {
     handlers: {
