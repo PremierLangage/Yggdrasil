f9f6ce552105841e912c2699d4f41dc81c1a2df3
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-19 16:22:42 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -30,8 +30,8 @@ def gethtmlmenu(enonce,dico):
     
     """
     strtemp=""
-    for itemmenu in re.finditer(r'??',enonce,re.MULTILINE)
-        #strtemp=strtemp+"-"
+    for itemmenu in re.finditer(r'??',enonce,re.MULTILINE):
+        strtemp=strtemp+"-"
         #strtemp=enonce[:itemmenu.start()]
         #strtemp=itemmenu.start()
         #enonce=enonce[item.end():]
