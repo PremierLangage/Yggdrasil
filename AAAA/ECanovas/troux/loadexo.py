b7eaf538c53555e2e10ef0aafa91bf49324a5b3e
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-19 17:36:57 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -20,7 +20,7 @@ def splitenonce(enonce="nothing"): #
         choices.append(choice)
 
     #choices=re.findall(pattern,enonce)
-    enoncetroux=re.sub(r'{.*}', '??', enonce)
+    enoncetroux=re.sub(r'{.*}', '==', enonce)
 
     return enoncetroux,choices
 
@@ -34,7 +34,7 @@ def gethtmlmenu(enonce,dico):
     lst=0
     lines = enonce.split('\n')
     for line in lines:
-        for itemmenu in re.finditer(r'\?\?',line):
+        for itemmenu in re.finditer(r'==',line):
             strtemp=strtemp+line[lst:itemmenu.start()+lst]+ " =="+str(lst)+"-"+str(itemmenu.start()+lst)+"-"+str(itemmenu.end())+line+" EOL<br/>"
             lst=itemmenu.end()+lst
             flg=True
