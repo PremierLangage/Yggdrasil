90594d3dd131813b2241df54aadc7c8cbc61d558
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-19 09:08:11 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -45,7 +45,7 @@ monhtml=re.sub(r'{.*}', '??', text)
 
 # {{un['value']}} et feedback {{un['feedback']}}<br/>"
 menuUn='<select name="pets" id="pet-select"><option value="">--Please choose an option--</option>'
-for it in un
+for it in un:
     menuUn=menuUn+'<option value="">{{it}}</option'
 menuUn=menuUn+'</select>'
 
