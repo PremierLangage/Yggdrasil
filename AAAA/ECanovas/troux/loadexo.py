f7c6dcf6a15b567f482be65676b2023b74179236
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-19 17:36:26 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -34,7 +34,7 @@ def gethtmlmenu(enonce,dico):
     lst=0
     lines = enonce.split('\n')
     for line in lines:
-        for itemmenu in re.finditer("\?\?",line):
+        for itemmenu in re.finditer(r'\?\?',line):
             strtemp=strtemp+line[lst:itemmenu.start()+lst]+ " =="+str(lst)+"-"+str(itemmenu.start()+lst)+"-"+str(itemmenu.end())+line+" EOL<br/>"
             lst=itemmenu.end()+lst
             flg=True
