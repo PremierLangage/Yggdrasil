44ebf4aa2d83ee898bbe9a239be7c884d59c8e25
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-19 09:27:56 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -1,44 +1,30 @@
 
 import csv
 import random 
+import re
 
-def get(path, delimiter=';', categorie=1):
-    """
-    
-    >>> get("data.csv",";",1)
-    
-    """
-    images = {}
-    with open(path,'r') as csvf:
-        reader = csv.DictReader(csvf, delimiter=';')
-        if categorie in [1,2]:
-            alpha='1'
-        else: 
-            alpha='2'
-        l2 = [x for x in reader]
-        l= [x for x in l2 if x['alphabeta']==alpha ]
-
-        return random.choice(l)
-
-def getenonce(categorie=1): # 
+def getenonce(enonce="nothing"): # 
     """
     >>> getenonce(categorie=1)
     
     """
-    # add %$ %$ to the drafs 
-    dico = get("troux.csv",";",categorie)
+    lines = enonce.split('\n')
+    choices = []
+    pattern = re.compile(r'.*{(?:=(?P<value>(?:(?:\\\#)|[^\#])+)(?:\#(?P<feedback>.*))?)}')
+    for line in lines:
+        match = pattern.match(line)
+        if not match:
+            continue
+        choice = {
+            "feedback": match.group('feedback') or '',
+            "value": match.group('value').strip()
+        }
+        choices.append(choice)
 
-    if t==1:
-        enonce=" A partir de la demi equation redox donnee "+ dico['demieq']
-    else:
-        enonce=" Pour le couple redox suivant "+oxred 
-    
-    enonce += " remplir la formule de Nernst avec les étiquettes proposées" 
-    dico['rep']= [ int(x) for x in dico['reponse'].split(',')]
-    del dico['reponse']
-    dico['drags']= dico['drags'].split(',')
-    dico['text']=enonce 
-    return dico
+    #choices=re.findall(pattern,enonce)
+    enoncetroux=re.sub(r'{.*}', '??', enonce)
+
+    return enoncetroux,choices
     
 
 
