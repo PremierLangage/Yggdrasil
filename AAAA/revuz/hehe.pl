ef4dbd1e8e211b64f1cee0af9d65eda68fba42b7
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-03-22 16:39:01 +0100

    drevuz modified Yggdrasil/AAAA/revuz/hehe.pl

--- AAAA/revuz/hehe.pl
+++ AAAA/revuz/hehe.pl
@@ -1,25 +1,8 @@
+extends = /gift/templates/qessay.pl
 
-extends = /Languages/French/Orthographe/Trouve_faute_template.pl
+title= Open question
+text==
 
-author = Anna DIAW
+![le makicata](https://www.photos-animaux.com/data/photos/517/5168/x516752.jpg)
 
-title==
-Trouve Fautes    
 ==
-
-text ==
-Corriger les fautes d'orthographe dans les phrases suivantes ou cliquer sur correct si vous 
-trouvez qu'elles sont correctes
-==
-
-@ /AAAA/Anna/Orthographe.txt 
-
-file_list==
-["Orthographe.txt"]
-==
-
-consecutive_to_validate = 4
-
-recall_rule = True
-
-
