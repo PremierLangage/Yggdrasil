4b10e47e2ecbb9c426be64f479b0fe2fbe28aa41
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-22 16:00:58 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -47,8 +47,8 @@ def defhtmlmenu(indice,dico):
     """
     menu= dico[indice] # tuple de value,feedback
     m=menu[0].split(",") # champ value
-    
-    unMenu='<select name="gab'+indice.string()+'" id="gab-select"><option value="">--Choisir une réponse--</option>'
+    strind=str(indice)
+    unMenu='<select name="gab'+strind+'" id="gab-select"><option value="">--Choisir une réponse--</option>'
     for it in m:
         unMenu=unMenu+"<option value="">" + it +"</option>"
 
