8a6a05d9fcd53c0ee87ca3f4043325c798312195
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-03-22 16:52:18 +0100

    drevuz modified Yggdrasil/AAAA/Beal/java.pl

--- AAAA/Beal/java.pl
+++ AAAA/Beal/java.pl
@@ -48,3 +48,30 @@ class PointTest {
 
 }
 ==
+
+solutionpourpaste==
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX()
+    {
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    public boolean equals(Object o){
+        if (! (o instanceof Point)) {
+            return false;
+        }
+        Point p=(Point)o;
+        return x == p.x && y == p.y ;
+    }
+}
+==
