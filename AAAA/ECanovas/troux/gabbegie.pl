e138aeadd3286df0b87fc574b7e213847e469e78
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-03-22 11:38:50 +0100

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -30,7 +30,7 @@ it=dico
 
 menuUn='<select name="pets" id="pet-select"><option value="">--Please choose an option--</option>'
 for it in dico:
-    menuUn=menuUn+"<option value="">" + str(it['value']) +"</option>"
+    menuUn=menuUn+"<option value="">" + str(it) +"</option>"
 menuUn=menuUn+'</select>'
 form=menuUn
 
