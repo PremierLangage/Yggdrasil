33293f58ffaad8f1d9723cffc2510946a8936fd0
Marianne ABRAMOVICI <Marianne.Abramovici@u-pem.fr>
2021-03-22 16:02:54 +0100

    mabramovici modified Yggdrasil/AAAA/Marianne/linkedin.pl

---
+++ AAAA/Marianne/linkedin.pl
@@ -0,0 +1,15 @@
+extends = /gift/templates/qessay.pl
+
+title= Quel est votre prénom ?
+text==
+indiquez votre prénom d'usage
+
+==
+extends = /model/matchlist/matchlist.pl
+pairs ==
+Facebook§animer sa communauté
+Linkedin§trouver des partenaires professionnels
+Instagram§Mettre en valeur des créations visuelles
+==
+text = Associer les réseaux sociaux et l'usage de base  
+title = Des réseaux sociaux aux objectifs différents
