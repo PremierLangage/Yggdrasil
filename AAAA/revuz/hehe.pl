d82d419adc8d92c06bdaaa2f17c5dca65993376b
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-03-22 15:51:28 +0100

    drevuz modified Yggdrasil/AAAA/revuz/hehe.pl

---
+++ AAAA/revuz/hehe.pl
@@ -0,0 +1,23 @@
+
+extends = /Languages/French/Orthographe/Trouve_faute_template.pl
+
+author = Anna DIAW
+
+title==
+Trouve Fautes    
+==
+
+text ==
+Corriger les fautes d'orthographe dans les phrases suivantes ou cliquer sur correct si vous 
+trouvez qu'elles sont correctes
+==
+
+@ /AAAA/Anna/Orthographe.txt 
+
+file_list==
+["Orthographe.txt"]
+==
+
+consecutive_to_validate = 4
+
+recall_rule = True
