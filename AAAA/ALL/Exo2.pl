4ef8e8b5c281f4036980685060acd9a54ef7ef32
Anne-Laure LOZANO <anne-laure.lozano@u-pem.fr>
2021-03-22 16:03:01 +0100

    alozano modified Yggdrasil/AAAA/ALL/Exo2.pl

---
+++ AAAA/ALL/Exo2.pl
@@ -0,0 +1,19 @@
+
+extends = /gift/templates/qmultichoice.pl
+
+title==
+Multiple Choice 
+==
+
+text==
+Parmi ces propositions, dites celle qui sont des fermetures
+==
+
+choices==
+%25%Persiennes
+%-100.0%Grilles
+%25.0%Volets
+%-100.0%Barreaux
+%25.0%Jalousies
+%25.0%Image
+==
