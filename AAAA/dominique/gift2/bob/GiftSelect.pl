c8a5c945b087e2012753cb526d021d237427ba16
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-03-22 13:11:24 +0100

    drevuz modified Yggdrasil/AAAA/dominique/gift2/bob/GiftSelect.pl

--- AAAA/dominique/gift2/bob/GiftSelect.pl
+++ AAAA/dominique/gift2/bob/GiftSelect.pl
@@ -5,13 +5,12 @@ Q2
 ==
 
 text==
-Quelle couleur se trouve entre l'orange et le vert dans le spectre ?
+Les personnages suivants apparaissent dans les Trois mousquetaires ? 
 ==
 
 choices==
-Les personnages suivants apparaissent dans les Trois mousquetaires ? {
--L'abbé Faria
-+Constance Bonacieux
-+L'abbé d'Herblay
--Le capitaine Némo}
+~L'abbé Faria
+=Constance Bonacieux
+=L'abbé d'Herblay
+~Le capitaine Némo
 ==
