48d91e299fed0a9da6b1a6c54392e43a4f66df4d
David DOYEN <David.Doyen@u-pem.fr>
2021-03-25 14:25:03 +0100

    ddoyen modified Yggdrasil/model/mathquill.pl

--- model/mathquill.pl
+++ model/mathquill.pl
@@ -14,9 +14,7 @@ form ==
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
-<p>Réponse : <span id="math-field" style="font-size:
-
-14pt;padding: 0.2em;"></span></p>
+<p>Réponse : <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p>
 <input type="text" id="form_math" name="form_math" hidden=true><br>
 
 <script>
