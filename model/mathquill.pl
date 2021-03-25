fceaa2efa4077acdd366fb6150d83aa9aa30ace2
David DOYEN <David.Doyen@u-pem.fr>
2021-03-25 14:22:38 +0100

    ddoyen modified Yggdrasil/model/mathquill.pl

--- model/mathquill.pl
+++ model/mathquill.pl
@@ -18,33 +18,6 @@ form ==
 
 14pt;padding: 0.2em;"></span></p>
 <input type="text" id="form_math" name="form_math" hidden=true><br>
-
-<script>
-var MQ = MathQuill.getInterface(2);
-var mathFieldSpan = document.getElementById('math-field');
-var latexSpan = document.getElementById('form_math');
-var oldvalue = latexSpan.value;
-var MQ = MathQuill.getInterface(2); // for backcompat
-var mathField = MQ.MathField(mathFieldSpan, {
-  spaceBehavesLikeTab: true, // configurable
-  leftRightIntoCmdGoes: 'up',
-  restrictMismatchedBrackets: true,
-  sumStartsWithNEquals: true,
-  supSubsRequireOperand: true,
-  charsThatBreakOutOfSupSub: '+-=<>',
-  autoSubscriptNumerals: true,
-  autoCommands: 'pi theta sqrt sum',
-  autoOperatorNames: 'sin cos ln exp',
-  maxDepth: 10,
-  handlers: {
-    edit: function() { // useful event handlers
-      latexSpan.value = mathField.latex(); // simple API
-    }
-  }
-});
-mathField.latex( {{answer.math}} );
-
-</script>
 ==
 
 settings.feedback = rightwrong
