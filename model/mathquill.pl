6b2f60eac79cc225e5f369c496d192632539a2e4
David DOYEN <David.Doyen@u-pem.fr>
2021-03-25 17:31:54 +0100

    ddoyen modified Yggdrasil/model/mathquill.pl

--- model/mathquill.pl
+++ model/mathquill.pl
@@ -9,7 +9,7 @@ form ==
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
 <p>Réponse : <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p>
-<input type="text" id="form_math" name="math" hidden=true><br>
+<input type="text" id="form_response" hidden=true><br>
 
 <script>
 var MQ = MathQuill.getInterface(2);
