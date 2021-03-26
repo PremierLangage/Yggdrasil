55b5b7a285b9af25219c02c277ff9bed013b2a15
David DOYEN <David.Doyen@u-pem.fr>
2021-03-26 08:58:19 +0100

    ddoyen modified Yggdrasil/model/mathquill.pl

--- model/mathquill.pl
+++ model/mathquill.pl
@@ -9,7 +9,7 @@ text =
 form ==
 
 <p>Réponse : <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p>
-<input type="text" id="form_math" value="{{ttt}}"><br>
+<input type="text" id="form_math" hidden=true><br>
 </script>
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
@@ -36,9 +36,7 @@ var mathField = MQ.MathField(mathFieldSpan, {
     }
   }
 });
-mathField.latex(" {{ ttt }} ");
-//mathField.latex("\\frac{1}{3}");
-
+mathField.latex(" {{ prev_value }} ");
 </script>
 ==
 
