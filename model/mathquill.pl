39ee9b07fb7c6c0a580324d2871ebe6b04dab9c0
David DOYEN <David.Doyen@u-pem.fr>
2021-03-26 08:00:49 +0100

    ddoyen modified Yggdrasil/model/mathquill.pl

--- model/mathquill.pl
+++ model/mathquill.pl
@@ -41,33 +41,7 @@ extrajs ==
         {% endif %}
     }
 </script>
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
 
-<script>
-var MQ = MathQuill.getInterface(2);
-var mathFieldSpan = document.getElementById('math-field');
-var latexSpan = document.getElementById('form_math');
-var mathField = MQ.MathField(mathFieldSpan, {
-  spaceBehavesLikeTab: true, // configurable
-  leftRightIntoCmdGoes: 'up',
-  restrictMismatchedBrackets: true,
-  sumStartsWithNEquals: true,
-  supSubsRequireOperand: true,
-  charsThatBreakOutOfSupSub: '+-=<>',
-  autoSubscriptNumerals: true,
-  autoCommands: 'pi theta sqrt sum',
-  autoOperatorNames: 'sin cos ln exp',
-  maxDepth: 10,
-  handlers: {
-    edit: function() { // useful event handlers
-      latexSpan.value = mathField.latex(); // simple API
-    }
-  }
-});
-
-</script>
 ==
 
 settings.feedback = rightwrong
