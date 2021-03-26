1bb5a7fb62b71cab90ab5b5c2512ba491bc13b5f
David DOYEN <David.Doyen@u-pem.fr>
2021-03-26 08:27:20 +0100

    ddoyen modified Yggdrasil/model/mathquill.pl

--- model/mathquill.pl
+++ model/mathquill.pl
@@ -36,7 +36,7 @@ var mathField = MQ.MathField(mathFieldSpan, {
     }
   }
 });
-mathField.latex(" {{ ttt|escape }} ");
+mathField.latex(" {{ ttt | replace("\","\\") }} ");
 //mathField.latex("\\frac{1}{3}");
 
 </script>
