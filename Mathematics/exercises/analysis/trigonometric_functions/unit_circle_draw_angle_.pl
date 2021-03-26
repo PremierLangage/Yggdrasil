4763e5de52da1b0925db9ce4107defa68532d1fb
David DOYEN <David.Doyen@u-pem.fr>
2021-03-26 19:18:40 +0100

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl
@@ -2,15 +2,17 @@ extends = /model/mathjsxgraph.pl
 
 title = Cercle trigonométrique
 
-
 before ==
 lstangle=eval(param['lstangle'])
 angle=randitem(lstangle)
 valangle=float(angle.evalf())
+jxg.setscript(script_init)
 ==
 
-drawer.script ==
-board.setBoundingBox([-1.25,1.25,1.25,-1.25]);
+jxg.attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25]}
+
+
+script_init ==
 var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
 var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
 var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
@@ -19,6 +21,31 @@ var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
 var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
 var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'red',fixed:false});
 var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+==
+
+
+text ==
+Placer le point $% M %$ d'affixe $%{{ z|latex }}%$ dans le plan ci-dessous.
+==
+
+script_solution ==
+board.create('point',[{{xsol}}, {{ysol}}],{size:2,name:'',color:'green'});
+==
+
+evaluator ==
+x, y = jxg.getpoint('M')
+from math import hypot
+if hypot(x-xsol, y-ysol) < 0.1:
+    score = 100
+    jxg.addscript(script_solution, globals())
+else:
+    score = 0
+    jxg.addscript(script_solution, globals())
+feedback = ""
+==
+
+drawer.script ==
+
 ==
 
 form==
