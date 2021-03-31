21dd99395aebfcf018d6281d0d0a47f4df602247
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-03-31 10:21:39 +0200

    nborie modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/bdd2_bash/bdd_size.pl

--- ComputerScience/OperatingSystem/exercises/Bash/bdd2_bash/bdd_size.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/bdd2_bash/bdd_size.pl
@@ -40,7 +40,7 @@ Rappel de la structuration du fichier **item.txt**:
 **Établissez une commande qui affiche le nombre de produits dans la base de données**. 
 Veuillez à faire en sorte que votre 
 commande ne produise que l'affichage numérique seul sur une 
-ligne (puis un retour à la ligne). N'hésitez à faire plusieurs essais.
+ligne (puis un retour à la ligne).
 ==
 
 solution==#|shell|
