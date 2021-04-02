3d8079272f2708259a958fc62d3cde8eb0732b3e
David DOYEN <David.Doyen@u-pem.fr>
2021-04-02 14:13:35 +0200

    ddoyen modified Yggdrasil/model/jinja_env/jinja_env_math.pl

--- model/jinja_env/jinja_env_math.pl
+++ model/jinja_env/jinja_env_math.pl
@@ -1,5 +1,6 @@
 from jinja2 import Environment, BaseLoader
 from sympy2latex import latex
+from sympy.core.basic import Basic
 
 def component(l):
     if isinstance(l,dict):
@@ -9,6 +10,10 @@ def component(l):
         selector = l.selector
         cid = l.cid
     return "<%s cid='%s'></%s>" % (selector, cid, selector)
+
+def latex2(expr):
+    if isinstance(expr, Basic):
+        return 
 
 CustomEnv = Environment(loader=BaseLoader())
 CustomEnv.filters["component"] = component
