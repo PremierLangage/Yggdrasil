3b693168db1b3c2c1d6069f2a8c36361f8d26509
David DOYEN <David.Doyen@u-pem.fr>
2021-04-05 00:10:29 +0200

    ddoyen modified Yggdrasil/demo/math/set/product_set.pl

--- demo/math/set/product_set.pl
+++ demo/math/set/product_set.pl
@@ -4,9 +4,9 @@ title = Produit cartésien
 
 before ==
 lenA, lenB = choice([[2,3],[3,2],[2,2]])
-A = FiniteSet(sample(range(10), lenA))
-B =  FiniteSet(sample(range(10), lenA))
-sol = FiniteSet(*ProductSet(A,B))
+A = FiniteSet(*sample(range(10), lenA))
+B = FiniteSet(*sample(range(10), lenB))
+#sol = FiniteSet(*ProductSet(A,B))
 ==
 
 text == 
