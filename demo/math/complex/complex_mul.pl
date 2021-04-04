af27a56485849a67cdbc7abfd7db5a467d6abe98
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 21:31:30 +0200

    ddoyen modified Yggdrasil/demo/math/complex/complex_mul.pl

---
+++ demo/math/complex/complex_mul.pl
@@ -0,0 +1,15 @@
+extends = /model/math/complex.pl
+
+before ==
+z1 = randint(-5, 5) + randint(-5, 5)*I
+z2 = randint(-5, 5) + randint(-5, 5)*I
+sol = (z1 * z2).expand()
+==
+
+text ==
+On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. 
+
+Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
+==
+
+complex_form = cartesian
