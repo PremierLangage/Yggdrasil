98b457b8309e106c1f103ef0ea0106306dcd62db
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 23:05:50 +0200

    ddoyen modified Yggdrasil/model/math/expr.pl

--- model/math/expr.pl
+++ model/math/expr.pl
@@ -1,11 +1,6 @@
 extends = /model/mathquill.pl
 
 
-before ==
-expr = sin(pi)
-sol = E
-==
-
 input_prefix = Réponse :
 
 checkratsimp = True
@@ -14,6 +9,7 @@ unauthorized_func = ['sin', 'cos', 'tan']
 
 evaluator ==
 from ast import literal_eval
+from evalsympy import eval_expr
 score, error = eval_expr(answers['math'], sol, checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
 feedback = message[error]
 ==
