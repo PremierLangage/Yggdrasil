b7999fabd6b85d8a0eb6348141337ef7824f09e5
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-04-04 10:49:24 +0200

    nborie modified Yggdrasil/ComputerScience/C/examples/ftl.pl

--- ComputerScience/C/examples/ftl.pl
+++ ComputerScience/C/examples/ftl.pl
@@ -70,7 +70,7 @@ if S != "y6":
     feedback = '''<span class="error-state animated pulse infinite" style="padding: 10px">Y'a pas de ces sales limaces de Slugs... Cliquez sur Slug et revalidez !</span>'''
     grade = (-1, feedback)
 else:
-    feedback = '''<span class="success-state animated pulse infinite" style="padding: 10px">Effectivement, qui ferait confiance à Slug aussi !</span>'''
+    feedback = '''<span class="success-state animated pulse infinite" style="padding: 10px">Effectivement, qui ferait confiance &agrave; Slug aussi !</span>'''
     grade = (100, feedback)
 ==
 
