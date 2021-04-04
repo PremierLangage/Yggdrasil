87df6991cbd4b6386d8c84daaaf10938e928a26b
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 23:22:45 +0200

    ddoyen modified Yggdrasil/demo/math/set/product_set.pl

---
+++ demo/math/set/product_set.pl
@@ -0,0 +1,25 @@
+extends = /model/math/set.pl
+
+title = Produit cartésien
+
+before ==
+lenA, lenB = randitem([[2,3],[3,2],[2,2]])
+A = rand_finiteset(lenA,list(range(10)))
+B = rand_finiteset(lenB,list(range(10)))
+sol = FiniteSet(*ProductSet(A,B))
+==
+
+text == 
+Ecrire en extension l'ensemble $! \\{ {{A|latex}} \\} \times \\{ {{B|latex}} \\}. !$
+==
+
+input.virtualKeyboards = sets
+
+evaluator ==
+score, error = eval_set(input.value, sol)
+feedback = message[error]
+==
+
+solution ==
+La solution est $! {{sol|latex}} !$.
+==
