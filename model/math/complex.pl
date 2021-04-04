e7c84347be8b535f8225220f6c8bbd65d7513381
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 20:57:25 +0200

    ddoyen modified Yggdrasil/model/math/complex.pl

--- model/math/complex.pl
+++ model/math/complex.pl
@@ -2,7 +2,15 @@ extends = /model/mathquill.pl
 
 
 before ==
-sol = 3 + I
+z1 = rd.randint(-5, 5) + rd.randint(-5, 5)*I
+z2 = rd.randint(-5, 5) + rd.randint(-5, 5)*I
+sol = (z1 * z2).expand()
+==
+
+text ==
+On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. 
+
+Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
 ==
 
 input_prefix = Réponse :
