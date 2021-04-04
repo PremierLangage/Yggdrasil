b97679471db33d68ca08d79e36ee29919d13472d
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 20:15:33 +0200

    ddoyen modified Yggdrasil/model/math/complex.pl

---
+++ model/math/complex.pl
@@ -0,0 +1,23 @@
+extends = /model/mathquill.pl
+
+
+before ==
+# Script
+==
+
+input_prefix = Réponse :
+imaginary_unit = i
+checkratsimp = True
+symbol_dict = {'e': E}
+unauthorized_func = ['sin', 'cos', 'tan']
+
+evaluator ==
+from ast import literal_eval
+score, error = eval_complex(answers['math'], sol, checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
+feedback = message[error]
+==
+
+solution ==
+La solution est $! {{ sol|latex}} !$.
+==
+
