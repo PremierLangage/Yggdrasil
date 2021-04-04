6034cf8e833e899fad5bb58f6ca71a9d5b276090
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 20:17:06 +0200

    ddoyen modified Yggdrasil/model/math/complex.pl

--- model/math/complex.pl
+++ model/math/complex.pl
@@ -13,7 +13,7 @@ unauthorized_func = ['sin', 'cos', 'tan']
 
 evaluator ==
 from ast import literal_eval
-score, error = eval_complex(answers['math'], sol, checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
+score, error = eval_complex(answers['math'], sol, imaginary_unit="i", form="", checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
 feedback = message[error]
 ==
 
