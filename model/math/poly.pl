2e1a944dcacdb0843195d41c402f76ed9a2b84b7
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 22:41:24 +0200

    ddoyen modified Yggdrasil/model/math/poly.pl

---
+++ model/math/poly.pl
@@ -0,0 +1,28 @@
+extends = /model/mathquill.pl
+
+before ==
+P = rand_int_poly(1, 2, 2, "x")
+Q = rand_int_poly(1, 2, 2, "x")
+expr = P * Q
+==
+
+text ==
+Développer $${{expr|latex}}.$$
+==
+
+evaluator==
+from ast import literal_eval
+score, error = eval_poly(answers['math'], sol, var=poly_var, form=poly_form)
+feedback = message[error]
+==
+
+input_prefix = Réponse :
+poly_var = x
+poly_form = 
+checkratsimp = True
+symbol_dict = {'e': E}
+unauthorized_func = ['sin', 'cos', 'tan']
+
+solution ==
+La solution est $! {{ sol|latex}} !$.
+==
