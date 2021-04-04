d64e7f0e2e59f7bd0a25d3ef72a10c9f0da48b1e
David DOYEN <David.Doyen@u-pem.fr>
2021-04-04 22:14:53 +0200

    ddoyen modified Yggdrasil/model/mathquill.pl

--- model/mathquill.pl
+++ model/mathquill.pl
@@ -57,7 +57,9 @@ feedback = message[error]
 
 headerbefore ==
 import random as rd
-# from randsympy import randint
+from randsympy import randint
+
+from randsympy import randint_complex
 
 from sympy import var, symbols
 from sympy import E, I, pi
