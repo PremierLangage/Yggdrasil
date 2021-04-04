ece07032a238c8fb9b8db8f7f740449d86a983fb
David DOYEN <David.Doyen@u-pem.fr>
2021-04-05 00:08:04 +0200

    ddoyen modified Yggdrasil/demo/math/set/product_set.pl

--- demo/math/set/product_set.pl
+++ demo/math/set/product_set.pl
@@ -3,7 +3,7 @@ extends = /model/math/set.pl
 title = Produit cartésien
 
 before ==
-lenA, lenB = randitem([[2,3],[3,2],[2,2]])
+lenA, lenB = choice([[2,3],[3,2],[2,2]])
 A = FiniteSet(sample(range(10), lenA))
 B =  FiniteSet(sample(range(10), lenA))
 sol = FiniteSet(*ProductSet(A,B))
@@ -11,15 +11,4 @@ sol = FiniteSet(*ProductSet(A,B))
 
 text == 
 Ecrire en extension l'ensemble $! \\{ {{A|latex}} \\} \times \\{ {{B|latex}} \\}. !$
-==
-
-input.virtualKeyboards = sets
-
-evaluator ==
-score, error = eval_set(input.value, sol)
-feedback = message[error]
-==
-
-solution ==
-La solution est $! {{sol|latex}} !$.
 ==
