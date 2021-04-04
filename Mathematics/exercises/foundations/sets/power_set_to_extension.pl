25f0e1c13f879a716d6ab77872ab7def47c3f81e
David DOYEN <David.Doyen@u-pem.fr>
2021-04-05 00:49:57 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/sets/power_set_to_extension.pl

--- Mathematics/exercises/foundations/sets/power_set_to_extension.pl
+++ Mathematics/exercises/foundations/sets/power_set_to_extension.pl
@@ -8,15 +8,6 @@ A = FiniteSet(*sample(range(10), randint(2, 3)))
 sol = A.powerset()
 ==
 
-evaluator ==
-from ast import literal_eval
-from evalsympy import eval_set
-from latex2sympy import FiniteSet2struct, latex2sympy
-score, error = eval_set(answers['math'], sol)
-feedback = answers['math']+str(FiniteSet2struct(sol)) + str(latex2sympy(answers['math']))
-#message[error]
-==
-
 text == 
 Ecrire en extension l'ensemble $! \mathcal{P}(\\{ {{ A|latex }} \\}) !$.
 ==
