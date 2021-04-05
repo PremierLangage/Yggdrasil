6a37486a167e98a367a642294a483cc217d4dc1a
David DOYEN <David.Doyen@u-pem.fr>
2021-04-05 12:32:38 +0200

    ddoyen modified Yggdrasil/utils/graphics/plmpl.py

--- utils/graphics/plmpl.py
+++ utils/graphics/plmpl.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 from io import BytesIO, StringIO
 import base64
 
-
 def fig2base64(fig, format='png', transparent=True, **kwargs):
     file = BytesIO()
     fig.savefig(file, format=format, transparent=transparent, **kwargs)
