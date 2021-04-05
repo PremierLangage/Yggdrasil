8d7f91d272a53d26029d6426016d9dcb7b1b5852
David DOYEN <David.Doyen@u-pem.fr>
2021-04-05 11:08:05 +0200

    ddoyen modified Yggdrasil/demo/math/expr/values_sin_cos.pl

--- demo/math/expr/values_sin_cos.pl
+++ demo/math/expr/values_sin_cos.pl
@@ -3,10 +3,11 @@ extends = /model/math/expr.pl
 title = Valeurs remarquables de sinus et cosinus
 
 before ==
-x = pi/4
-sol = sin(x)
+x = choice([pi/6, pi/4, pi/3, 3*pi/4, 2*pi/3, 5*pi/6])
+f = choice([sin, cos]) 
+sol = f(x)
 ==
 
 text ==
-Quelle est la valeur de sin en $!\displaystyle {{ x|latex }} !$ ?
+Quelle est la valeur de {{ sin|latex }} en $!\displaystyle {{ x|latex }} !$ ?
 ==
