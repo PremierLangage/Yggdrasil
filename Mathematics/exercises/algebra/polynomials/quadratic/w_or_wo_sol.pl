1b6db6ddac34f3ff2e10f54aad206a4ccd66c06e
David DOYEN <David.Doyen@u-pem.fr>
2021-04-05 23:05:15 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl

--- Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl
+++ Mathematics/exercises/algebra/polynomials/quadratic/w_or_wo_sol.pl
@@ -4,18 +4,18 @@ title = Déterminer si une équation quadratique a des solutions
 
 before ==
 var('x')
-sol = []
-choices = []
+indsol = []
+items = []
 for i in range(4):
     a = randint(-3, 3, [0])
     b = randint(-5, 5)
     c = randint(-3, 3)
     if b**2-4*a*c < 0:
-        sol.append(i)
-    sides = randitem([[a*x**2+b*x+c,0],[a*x**2+b*x,-c],[a*x**2,-b*x-c],[a*x**2+c,-b*x]])
+        indsol.append(i)
+    sides = choice([[a*x**2+b*x+c,0],[a*x**2+b*x,-c],[a*x**2,-b*x-c],[a*x**2+c,-b*x]])
     rd.shuffle(sides)
     s = r"$! %s !$" % latex(Eq(*sides))
-    choices.append(s)
+    items.append(s)
 ==
 
 text ==
