2e39cb23187e1db42c7f36871e7126dbec64ce13
David DOYEN <David.Doyen@u-pem.fr>
2021-04-05 10:40:44 +0200

    ddoyen modified Yggdrasil/model/math/expr.pl

--- model/math/expr.pl
+++ model/math/expr.pl
@@ -9,6 +9,7 @@ unauthorized_func = ['sin', 'cos', 'tan']
 
 evaluator ==
 from ast import literal_eval
+from sympy import sympify
 from evalsympy import eval_expr
 score, error = eval_expr(answers['math'], sol, checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
 feedback = message[error]
