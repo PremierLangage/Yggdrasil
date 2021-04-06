933d4d1a690744e4f23f06bb4be5bf8d33d093b1
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 18:07:58 +0200

    ddoyen modified Yggdrasil/demo/graphics/test.pl

--- demo/graphics/test.pl
+++ demo/graphics/test.pl
@@ -3,21 +3,14 @@ extends = /model/math.pl
 title = Graphique
 
 before ==
-
 import matplotlib.pyplot as plt
 import numpy as np
 from plmpl import fig2base64, fig2svg
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, folks')
-plt.grid(True)
 
+fig = plt.figure()
 
-source = fig2base64(plt.gcf(), format="png")
-source = fig2svg(plt.gcf())
+source = fig2base64(fig, format="png")
+source = fig2svg(fig)
 ==
 
 text ==
