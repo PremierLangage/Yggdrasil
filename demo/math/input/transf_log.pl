096b9b20a5f5ef9ae367bd79e586264ab11ebe47
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 14:24:10 +0200

    ddoyen modified Yggdrasil/demo/math/input/transf_log.pl

--- demo/math/input/transf_log.pl
+++ demo/math/input/transf_log.pl
@@ -14,14 +14,14 @@ Ecrire $% {{expr}} %$ sous la forme  $% \ln(a)%$, où $%a%$ est un nombre.
 ==
 
 evaluator==
-def eval_ans(strans,sol):
+def eval_ans(strans, sol):
     try:
         ans = latex2sympy(strans)
     except:
         return (-1, "WrongForm")
-    if type(ans)!=sp.log:
+    if type(ans)! = sp.log:
         return (-1, "WrongForm")
-    if not equal(ans.args[0],sol):
+    if not equal(ans, sol):
         return (0, "NotEqual")
     return (100, "Success")
 
