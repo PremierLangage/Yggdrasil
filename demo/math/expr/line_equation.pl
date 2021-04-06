dea919221c1aceba83d569168a27745eeb529ddd
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 21:48:58 +0200

    ddoyen modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -4,9 +4,9 @@ extends = /model/math/expr.pl
 title = Graphique
 
 before ==
-from plmpl import plotsvg
+from mplsympy import plotsvg
 x = symbols('x')
-a = choice([-1, 1]) * Rational(randint(1, 12), randint(1, 4))
+a = choice([-1, 1]) * (randint(0,1)+ Rational(randint(1, 12), randint(1, 4))
 b = randint(-2, 2)
 sol = a*x+b
 image = plotsvg(sol)
