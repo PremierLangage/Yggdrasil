1d0aaf7e0643832b2a53c9353d44aadd082babc2
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 16:03:02 +0200

    ddoyen modified Yggdrasil/demo/math/input/demo_mathinput.pl

--- demo/math/input/demo_mathinput.pl
+++ demo/math/input/demo_mathinput.pl
@@ -9,10 +9,28 @@ text ==
 Entrer l'expression $! {{ sol|latex}} !$.
 ==
 
+evaluator ==
+from latex2sympy import latex2sympy
+from evalsympy import equal
+from sympy import log
+
+codelatex = answers['math']
+
+try:
+    ans = latex2sympy(codelatex)
+    str_ans = str(ans)
+    srepr_ans = srepr(ans)
+except:
+    str_ans = ""
+    srepr_ans = ""
+
+score = -1
+==
+
 feedback ==
 Code LaTeX : {{ codelatex }}
 
-Objet Sympy : {{ strsympy }}
+Objet Sympy : {{ str_ans }}
 
-Représentation de l'objet Sympy : {{ reprsympy }}
+Représentation : {{ srepr_ans }}
 ==
