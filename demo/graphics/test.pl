4e478c3cc0c42f0f8b51fb406bb4be902611b878
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 20:33:20 +0200

    ddoyen modified Yggdrasil/demo/graphics/test.pl

--- demo/graphics/test.pl
+++ demo/graphics/test.pl
@@ -6,7 +6,7 @@ title = Graphique
 before ==
 from plmpl import plotsvg
 x = symbols('x')
-a = randint(-4, 4, [0])
+a = Rational(randint(-4, 4, [0]),2)
 b = randint(-2, 2)
 image = plotsvg(a*x+b)
 ==
