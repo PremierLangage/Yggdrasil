66ac04b773f66f0395f950679b631044deb5d2cc
KILLIAN BLANCHARD <kblanc01@etud.u-pem.fr>
2021-04-06 09:45:04 +0200

    kblanchard modified Yggdrasil/AAAA/Killian/test_git.pl

---
+++ AAAA/Killian/test_git.pl
@@ -0,0 +1,17 @@
+author = Killian Blanchard
+
+title = Premier Test
+
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+text = Initialise 2 variables a et b ayant respectivement pour valeur 2 et 3, puis additionne les dans une variables c
+
+pltest0==
+>>> a #a doit avoir la valeur 2
+2
+>>> b #b doit avoir la valeur 3
+3
+>>> c #c doit avoir la valeur 5
+5
+==
