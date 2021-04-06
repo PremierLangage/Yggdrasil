b544339d7c28372fbb5c77380a57a2ae3ac8a93d
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 18:12:54 +0200

    ddoyen modified Yggdrasil/demo/graphics/test.pl

--- demo/graphics/test.pl
+++ demo/graphics/test.pl
@@ -6,13 +6,15 @@ before ==
 import matplotlib.pyplot as plt
 import numpy as np
 from plmpl import fig2base64, fig2svg
-t = np.arange(0.0, 2.0, 0.01)
-s = 1 + np.sin(2*np.pi*t)
-plt.plot(t, s)
-#fig = plt.gcf()
+def test():
+    t = np.arange(0.0, 2.0, 0.01)
+    s = 1 + np.sin(2*np.pi*t)
+    plt.plot(t, s)
+    fig = plt.gcf()
+    return fig
 
-#source = fig2base64(fig, format="png")
-source = fig2svg(plt.gcf())
+#source = fig2base64(, format="png")
+source = fig2svg(test())
 ==
 
 text ==
