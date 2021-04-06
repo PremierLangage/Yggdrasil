70e3f59d3b62dfab14f17510d1aaa2b3c2ff03ce
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 21:31:54 +0200

    ddoyen modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -13,21 +13,9 @@ image = plotsvg(sol)
 ==
 
 text ==
+Déterminer l'équation de la droite tracée ci-dessous.
+
 <div class="img-container">
 {{ image }}
 </div>
 ==
-
-form ==
-
-==
-
-
-evaluator ==
-from ast import literal_eval
-from sympy import sympify
-from evalsympy import eval_expr
-score, error = eval_expr(answers['math'], sol, checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
-feedback = message[error]
-==
-
