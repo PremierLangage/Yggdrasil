63d4bb1e35cfe5a8fc165a006b49f9cdc97746c6
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 21:06:19 +0200

    ddoyen modified Yggdrasil/demo/math/expr/line_equation.pl

---
+++ demo/math/expr/line_equation.pl
@@ -0,0 +1,18 @@
+extends = /model/math/math.pl
+
+
+title = Graphique
+
+before ==
+from plmpl import plotsvg
+x = symbols('x')
+a = choice([-1, 1]) * choice([1, 2, 3, Rational(1, 2), Rational(3, 2), Rational(1, 3), Rational(2, 3), Rational(4, 3)])
+b = randint(-2, 2)
+image = plotsvg(a*x+b)
+==
+
+text ==
+<div class="img-container">
+{{image}}
+</div>
+==
