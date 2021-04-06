e67d2f29933377a0258bfb9a48a7d4bd662d49f0
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 09:22:20 +0200

    ddoyen modified Yggdrasil/model/math/poly.pl

--- model/math/poly.pl
+++ model/math/poly.pl
@@ -1,4 +1,10 @@
-extends = /model/mathquill.pl
+extends = /model/math/input.pl
+
+checkratsimp = True
+symbol_dict = {'e': E}
+unauthorized_func = []
+poly_var = x
+poly_form = 
 
 before ==
 P = randint_poly(1, 2, 2)
@@ -17,13 +23,6 @@ from evalsympy import eval_poly
 score, error = eval_poly(answers['math'], sol, var=poly_var, form=poly_form)
 feedback = message[error]
 ==
-
-input_prefix = Réponse :
-poly_var = x
-poly_form = 
-checkratsimp = True
-symbol_dict = {'e': E}
-unauthorized_func = ['sin', 'cos', 'tan']
 
 solution ==
 La solution est $! {{ sol|latex}} !$.
