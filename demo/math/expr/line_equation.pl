0cedf15c6c0a86fccd1b18764bec22d2468372ca
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 21:26:53 +0200

    ddoyen modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -20,17 +20,3 @@ text ==
 form ==
 
 ==
-
-
-
-evaluator ==
-from ast import literal_eval
-from sympy import sympify
-from evalsympy import eval_expr
-score, error = eval_expr(answers['math'], sol, checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
-feedback = message[error]
-==
-
-solution ==
-La solution est $! {{ sol|latex}} !$.
-==
