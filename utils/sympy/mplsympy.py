e236e7ba0c3e2552711bf2d01de4ed7cf39056b4
David DOYEN <David.Doyen@u-pem.fr>
2021-04-06 20:56:19 +0200

    ddoyen modified Yggdrasil/utils/sympy/mplsympy.py

---
+++ utils/sympy/mplsympy.py
@@ -0,0 +1,32 @@
+from io import StringIO
+from sympy.plotting import plot
+import matplotlib.ticker as ticker
+from sympy import symbols
+
+def fig2svg(fig, transparent=True, **kwargs):
+    file = StringIO()
+    fig.savefig(file, format='svg', transparent=transparent, **kwargs)
+    width, height = fig.get_size_inches()
+    width, height = 72*width, 72*height
+    lines = file.getvalue().splitlines()
+    for i, line in enumerate(lines):
+        if line.startswith('<svg'):
+            lines[i] = "<svg viewBox='0 0 %s %s'>" % (width, height)
+            break
+
+    return "\n".join(lines)
+
+def plotsvg(expr, xlim=(-5,5), ylim=(-5,5) ):
+    x = symbols('x')
+    fig = plot(expr, (x,*xlim))._backend.fig
+    ax = fig.gca()
+    ax.grid(True)
+    ax.set_xlim(*xlim)
+    ax.set_ylim(*ylim)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+    #ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+    #ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+    return fig2svg(fig)
