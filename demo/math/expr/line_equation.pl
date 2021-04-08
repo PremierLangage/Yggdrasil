e407210fab311ecf0b5a916ac281700d4f49236e
David DOYEN <David.Doyen@u-pem.fr>
2021-04-08 12:14:41 +0200

    ddoyen modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -5,7 +5,7 @@ title = Déterminer l'équation d'une droite
 before ==
 from mplsympy import plotsvg
 x = symbols('x')
-a = choice([-1, 1]) * Rational(choice(['1/2',1,'3/2',2]))
+a = choice([-1, 1]) * sympify(choice(['1/2',1,'3/2',2]))
 b = randint(-3, 3)
 sol = a*x+b
 image = plotsvg(sol)
