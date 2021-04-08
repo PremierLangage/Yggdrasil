8c855c820bbc0465627ae10d3ac577adc32747ba
David DOYEN <David.Doyen@u-pem.fr>
2021-04-08 17:36:20 +0200

    ddoyen modified Yggdrasil/demo/math/input/demo_mathinput.pl

--- demo/math/input/demo_mathinput.pl
+++ demo/math/input/demo_mathinput.pl
@@ -28,6 +28,14 @@ score = -1
 ==
 
 feedback ==
+Code LaTeX : {{ codelatex }}
+Objet Sympy : {{ str_ans }}
+Représentation : {{ srepr_ans }}
+
+==
+
+
+feedback2 ==
 <ul>
     <li> Code LaTeX : {{ codelatex }} </li>
     <li> Objet Sympy : {{ str_ans }} </li>
