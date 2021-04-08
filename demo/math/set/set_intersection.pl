ed236e8675e929bacaca162a3d376fa22d02491b
David DOYEN <David.Doyen@u-pem.fr>
2021-04-08 14:26:32 +0200

    ddoyen modified Yggdrasil/demo/math/set/set_intersection.pl

--- demo/math/set/set_intersection.pl
+++ demo/math/set/set_intersection.pl
@@ -3,6 +3,7 @@ extends = /model/math/set.pl
 title = Intersection d'ensembles
 
 before ==
+from sympy import Intersection
 A = FiniteSet(*sample(range(10), randint(4, 7)))
 B = FiniteSet(*sample(range(10), randint(4, 7)))
 sol = Intersection(A, B)
