5a2be2588e5edc46d01f7ae16702d09adccde8ae
David DOYEN <David.Doyen@u-pem.fr>
2021-04-08 18:24:14 +0200

    ddoyen modified Yggdrasil/grader/evaluator2.py

--- grader/evaluator2.py
+++ grader/evaluator2.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
         json.dump(dic, f, cls=CustomEncoder)
     
     with open(sys.argv[4], "w+") as f:
-        print(str(ffeedback), file=f)
+        print(ffeedback, file=f)
     
     print(int(score))
     
