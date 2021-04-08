5190c048c9cd77ff11ac657a8a7da1f44919aa87
David DOYEN <David.Doyen@u-pem.fr>
2021-04-08 18:56:16 +0200

    ddoyen modified Yggdrasil/model/math/math.pl

--- model/math/math.pl
+++ model/math/math.pl
@@ -71,4 +71,7 @@ extrajs ==
         {% endif %}
     }
 </script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
 ==
