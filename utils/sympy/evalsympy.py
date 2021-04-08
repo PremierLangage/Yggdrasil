c2beb5de135ccc483299551b44d8692254dc2f52
David DOYEN <David.Doyen@u-pem.fr>
2021-04-08 20:25:48 +0200

    ddoyen modified Yggdrasil/utils/sympy/evalsympy.py

--- utils/sympy/evalsympy.py
+++ utils/sympy/evalsympy.py
@@ -308,11 +308,17 @@ def test(expr):
     args = expr.args
     if len(args) > 2:
         return None
-    if not args[0].is_Integer:
-        return args[0]
-    if args[1].func != sp.Pow or args[1].args[1] != sp.Integer(-1):
-        return "3"
-    return (args[0], args[1].args[0])
+    if args[0].is_Integer:
+        a = args[0]
+        b = args[1]
+    elif args[1].is_Integer:
+        a = args[1]
+        b = args[0]
+    else:
+        return None
+    if b.func != sp.Pow or b.args[1] != sp.Integer(-1):
+        return None
+    return (a, b.args[0])
 
         
     with sp.evaluate(False):
