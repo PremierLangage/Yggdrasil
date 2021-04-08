3bbfaef18d3bd3374d6a271596ea8f01b5800416
David DOYEN <David.Doyen@u-pem.fr>
2021-04-08 23:35:27 +0200

    ddoyen modified Yggdrasil/demo/math/fraction/frac_add.pl

--- demo/math/fraction/frac_add.pl
+++ demo/math/fraction/frac_add.pl
@@ -3,20 +3,18 @@ extends = /model/math/input.pl
 title = Addition de fractions
 
 before ==
-f = [Rational(choice([1, 3, 5, 7]), 2),
-Rational(choice([1, 2, 4, 5, 7, 8]), 3),
-Rational(choice([1, 3, 5, 7]), 4),
-Rational(choice([1, 2, 3, 4, 6, 7, 8]), 5),
-Rational(choice([1, 5, 7]), 6)]
+from sympy import numer, denom
+
+f = [(choice([1, 3, 5, 7]), 2),
+(choice([1, 2, 4, 5, 7, 8]), 3),
+(choice([1, 3, 5, 7]), 4),
+(choice([1, 2, 3, 4, 6, 7, 8]), 5),
+(choice([1, 5, 7]), 6)]
 
 shuffle(f)
-f1, cf2 = list_randitem_norep(2, lstfrac)
 
-f1='\\frac{ %d }{ %d }' % (numer,cf1[1])
-f2='\\frac{ %d }{ %d }' % (cf2[0],cf2[1])
-
-expr = r"\frac{ %d }{ %d } + \frac{ %d }{ %d }" % (
-sol = simplify(latex2sympy(expr))
+expr = r"\frac{ %d }{ %d } + \frac{ %d }{ %d }" % (f[0][0], f[0][1], f[1][0], f[1][1] )
+sol = Rational(f[0]) + Rational(f[1])
 ==
 
 text ==
