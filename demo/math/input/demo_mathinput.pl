af7c3a7be1a2397f5916eae8601166387c6f5766
David DOYEN <David.Doyen@u-pem.fr>
2021-04-09 20:42:12 +0200

    ddoyen modified Yggdrasil/demo/math/input/demo_mathinput.pl

--- demo/math/input/demo_mathinput.pl
+++ demo/math/input/demo_mathinput.pl
@@ -13,7 +13,7 @@ Entrer une expression mathématique.
 evaluator ==
 from latex2sympy import latex2sympy
 from sympy import srepr, fraction
-from evalsympy import test, is_rat_simp2
+from evalsympy import is_rat_simp2
 codelatex = answers['math']
 ans = latex2sympy(codelatex)
 
