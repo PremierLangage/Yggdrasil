8736f33e7f8a51df560aeae0a426711627c5e498
David DOYEN <David.Doyen@u-pem.fr>
2021-04-10 00:05:53 +0200

    ddoyen modified Yggdrasil/demo/math/expr/limit_algebraic_fraction.pl

---
+++ demo/math/expr/limit_algebraic_fraction.pl
@@ -0,0 +1,22 @@
+extends = /model/math/expr.pl
+
+title = Limite d'une fraction rationnelle
+
+before ==
+from randsympy import randint
+from sympy import Limit
+
+var('x')
+
+deg = randint(1, 3)
+f = randint_poly(deg, randint(2, deg + 1), 5, "x")
+deg = randint(1, 3)
+g = randint_poly(deg, randint(2, deg + 1), 5, "x")
+
+lim = Limit(f/g, x, oo)
+sol = lim.doit()
+==
+
+text ==
+Déterminer $$ {{ lim|latex }}. $$
+==
