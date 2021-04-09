a32da270ef3e8c4862052935f19ca875a4771e7a
David DOYEN <David.Doyen@u-pem.fr>
2021-04-09 20:52:00 +0200

    ddoyen modified Yggdrasil/utils/sympy/evalsympy.py

--- utils/sympy/evalsympy.py
+++ utils/sympy/evalsympy.py
@@ -317,11 +317,6 @@ def is_mul_ratsimp(expr):
             if p == sp.Integer(1) and q == sp.Integer(1):
                 p, q = a.p, a.q 
             else:
-                return "Top"
-        if a.is_Integer and :
-            if p == sp.Integer(1):
-                p = a
-            else:
                 return "Top2"
         elif a.func == sp.Pow and a.args[1] == sp.Integer(-1):
             if q == sp.Integer(1):
