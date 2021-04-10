e4042999ee6b1ee3b6489923c0a967e1861db248
David DOYEN <David.Doyen@u-pem.fr>
2021-04-10 20:26:32 +0200

    ddoyen modified Yggdrasil/demo/math/rset/demo_rset.pl

---
+++ demo/math/rset/demo_rset.pl
@@ -0,0 +1,39 @@
+extends = /model/math/input.pl
+
+title = Démonstration math/input
+
+before ==
+
+==
+
+text ==
+Entrer une expression mathématique.
+==
+
+evaluator ==
+from latex2sympy import latex2sympy
+from sympy import srepr, fraction
+from evalsympy import is_rat_simp
+codelatex = answers['math']
+ans = latex2sympy(codelatex)
+
+try:
+    ans = latex2interval(codelatex)
+    str_ans = str(ans)
+    srepr_ans = srepr(ans)
+except:
+    str_ans = ""
+    srepr_ans = ""
+
+score = -1
+==
+
+feedback ==
+<ul>
+    <li> Code LaTeX : {{ codelatex }} </li>
+    <li> Objet Sympy : {{ str_ans }} </li>
+    <li> Représentation : {{ srepr_ans }} </li>
+</ul>
+==
+
+settings.feedback = 
