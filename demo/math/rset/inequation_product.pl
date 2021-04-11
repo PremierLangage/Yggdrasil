26a287baffcd76012939517d1117c9098b327244
David DOYEN <David.Doyen@u-pem.fr>
2021-04-11 17:32:49 +0200

    ddoyen modified Yggdrasil/demo/math/rset/inequation_product.pl

---
+++ demo/math/rset/inequation_product.pl
@@ -0,0 +1,30 @@
+extends = /model/math/input.pl
+
+title = Inéquation produit
+
+
+before ==
+from sympy import solveset
+var('x')
+a, b = sample(range(-6, 6), k=2)
+expr = (x + a) * (x + b)
+
+ineq = choice([expr >= 0, expr > 0, expr <= 0,expr < 0])
+
+sol = solveset(ineq, x, domain=S.Reals)
+==
+
+text ==
+Déterminer l'ensemble des réels $! x !$ tels que $$ {{ ineq|latex }}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
+==
+
+evaluator ==
+score, error =  eval_rset(answers['math'], sol)
+feedback = message[error]
+==
+
+solution ==
+La solution est $! {{ sol|latex }} !$.
+==
+
+
