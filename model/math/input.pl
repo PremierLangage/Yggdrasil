74bab02060667958502cdb009f158323ec88fae6
David DOYEN <David.Doyen@u-pem.fr>
2021-04-11 22:03:18 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -65,7 +65,7 @@ function onReadyPL(nodes) {
             edit: function() {latexSpan.value = mathField.latex(); }
         }
     });
-    mathField.latex("{{ prev_value }}");
+    mathField.latex("ff");
 }
 
 function onBeforeSubmitPL() {
@@ -73,29 +73,6 @@ function onBeforeSubmitPL() {
     return true;
 }
 
-</script>
-==
-
-
-form3 ==
-<p> <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p>
-
-<input type="text" id="form_math" hidden=true>
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1-b/mathquill.min.js" type="text/javascript"></script>
-<script>
-var MQ = MathQuill.getInterface(2);
-var mathFieldSpan = document.getElementById('math-field');
-var latexSpan = document.getElementById('form_math');
-var mathField = MQ.MathField(mathFieldSpan, {
-  charsThatBreakOutOfSupSub: '+-=<>',
-  autoCommands: 'pi theta sqrt sum infty emptyset',
-  autoOperatorNames: 'sin cos ln exp',
-  handlers: {
-    edit: function() {latexSpan.value = mathField.latex(); }
-  }
-});
 </script>
 ==
 
