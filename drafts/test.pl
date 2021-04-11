ba00b5eb9dcc72e61658b2e559cfcfa3918b1ad1
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2021-04-11 15:03:11 +0200

    mcisse modified Yggdrasil/drafts/test.pl

--- drafts/test.pl
+++ drafts/test.pl
@@ -15,10 +15,22 @@ form ==
 <p> Réponse : <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p>
 
 <input type="text" id="form_math" hidden=true>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
 
 <script>
 
 function onReadyPL(nodes) {
+    console.log('ready');
+    const submit = document.querySelector('.action-submit');
+    console.log(submit);
+    submit.addEventListener('click', () => {
+        alert('click');
+    });
+}
+</script>
+
 var MQ = MathQuill.getInterface(2);
 var mathFieldSpan = document.getElementById('math-field');
 var latexSpan = document.getElementById('form_math');
@@ -30,15 +42,6 @@ var mathField = MQ.MathField(mathFieldSpan, {
     edit: function() {latexSpan.value = mathField.latex(); }
   }
 });
-
-    console.log('ready');
-    const submit = document.querySelector('.action-submit');
-    console.log(submit);
-    submit.addEventListener('click', () => {
-        alert('click');
-    });
-}
-</script>
 ==
 
 evaluator ==
