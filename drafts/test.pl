32aea6826e1f2d2dea1e177c54532cd1a3556688
MAMADOU CISSE <mcisse06@etud.u-pem.fr>
2021-04-11 15:21:05 +0200

    mcisse modified Yggdrasil/drafts/test.pl

--- drafts/test.pl
+++ drafts/test.pl
@@ -13,37 +13,37 @@ Instruction
 
 form ==
 <p> Réponse : <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p>
-
 <input type="text" id="form_math" hidden=true>
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
+==
 
+extracss==
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
 ==
 
 extrajs==
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
 
 <script>
-function onReadyPL(nodes) {
+let mathField;
 
-var MQ = MathQuill.getInterface(2);
-var mathFieldSpan = document.getElementById('math-field');
-var latexSpan = document.getElementById('form_math');
-var mathField = MQ.MathField(mathFieldSpan, {
-  charsThatBreakOutOfSupSub: '+-=<>',
-  autoCommands: 'pi theta sqrt sum infty emptyset',
-  autoOperatorNames: 'sin cos ln exp',
-  handlers: {
-    edit: function() {latexSpan.value = mathField.latex(); }
-  }
-});
-    console.log('ready');
-    const submit = document.querySelector('.action-submit');
-    console.log(submit);
-    submit.addEventListener('click', () => {
-        alert('click');
+function onReadyPL(nodes) {
+    const MQ = MathQuill.getInterface(2);
+    const mathFieldSpan = document.getElementById('math-field');
+    const latexSpan = document.getElementById('form_math');
+    mathField = MQ.MathField(mathFieldSpan, {
+        charsThatBreakOutOfSupSub: '+-=<>',
+        autoCommands: 'pi theta sqrt sum infty emptyset',
+        autoOperatorNames: 'sin cos ln exp',
+        handlers: {
+            edit: function() {latexSpan.value = mathField.latex(); }
+        }
     });
 }
 
+function onBeforeSubmitPL() {
+    console.log(mathField);
+    return true;
+}
 </script>
 ==
 
