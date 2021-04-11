f7457f4374af3b3129c850dc772bbc9bb489555c
David DOYEN <David.Doyen@u-pem.fr>
2021-04-11 21:20:10 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -47,6 +47,31 @@ function onBeforeSubmitPL() {
     input.value = mathField.latex();
     return true;
 }
+
+// Hack pour modifier les boutons de contrôle affichés.
+function onReadyPL(nodes) {
+    const actions = nodes.actions;
+    actions.find('.action-save').hide();
+    actions.find('.action-reset').hide();
+    actions.find('.action-next').hide();
+
+    const { origin, pathname }  = document.location;
+    const link = origin + pathname;
+
+    const buttons = actions.find('.btn-group');
+
+    {% if "reroll" in internals.buttons %}
+    buttons.append(`
+        <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
+            <i class="fas fa-dice"></i> Nouveau tirage
+        </a>
+    `);
+    {% endif %}
+    
+    {% if not "submit" in internals.buttons %}
+    actions.find('.action-submit').hide();
+    {% endif %}
+}
 </script>
 ==
 
