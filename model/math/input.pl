3bae6f6375f96932458707ebb42937287dd79fcc
David DOYEN <David.Doyen@u-pem.fr>
2021-04-11 17:17:06 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -16,6 +16,7 @@ form ==
 <p> {{input_prefix}} <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p>
 <input type="text" id="form_math" hidden="true">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
 
 ==
 
@@ -24,7 +25,6 @@ extracss ==
 ==
 
 extrajs ==
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.js" type="text/javascript"></script>
 
 <script>
 let input;
