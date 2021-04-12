c09bdb1a50b6349da09bc2e110e35137a418824c
Isabelle NAVIZET <Isabelle.Navizet@u-pem.fr>
2021-04-12 17:08:54 +0200

    inavizet modified Yggdrasil/AAAA/Isabelle/KekuleViewer-2.pl

--- AAAA/Isabelle/KekuleViewer-2.pl
+++ AAAA/Isabelle/KekuleViewer-2.pl
@@ -23,7 +23,7 @@ form ==
 
 <h2> SMILES </h2>
 <textarea rows="1" cols="50" id="smiles"  style="font-size: 12pt">
-CCN(c1cc(C)ccc1C)S(=O)(=O)c1cc(C(N)=O)n(C)c1
+exemple : CCN(c1cc(C)ccc1C)S(=O)(=O)c1cc(C(N)=O)n(C)c1
 </textarea>
 <div id="kekule" style="width:300px;height:300px"
 		 data-widget="Kekule.ChemWidget.Viewer2D" data-enable-toolbar="false"
