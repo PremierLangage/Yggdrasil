35b8c7abe4ea621d92c5d65c1d3a155ac806f024
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-04-12 12:03:16 +0200

    drevuz modified Yggdrasil/AAAA/dominique/graderbuilder/usingmynewbuilder.pl

---
+++ AAAA/dominique/graderbuilder/usingmynewbuilder.pl
@@ -0,0 +1,42 @@
+
+
+# Méthode 1 privilégiée maintenant 
+@ /builder/simplebefore.py [builder.py]
+@ /grader/simpleeval.py [grader.py]
+
+# Méthode 2 version historique 
+#grader  =@ /grader/evaluator.py
+#builder =@ /builder/before.py
+
+@ /utils/components/checkbox.py [customcheckbox.py]
+
+
+title = Multiples de 3 (Checkbox)
+
+checkbox =: CheckboxGroup
+checkbox.decorator = CustomCheckbox
+
+before ==
+import random as rd
+
+mult3 = [str(n) for n in range(50,100) if n % 3 == 0]
+other = [str(n) for n in range(50,100) if n % 3 != 0]
+
+checkbox.setdata_from_rw(mult3, other, 5, rd.randint(1, 4))
+==
+
+text ==
+Parmi les nombres suivants, lesquels sont des multiples de 3 ?
+==
+
+settings.feedback = lightscore
+
+form ==
+{{ checkbox|component }}
+==
+
+evaluator ==
+feedback = " "
+score = checkbox.eval()
+==
+
