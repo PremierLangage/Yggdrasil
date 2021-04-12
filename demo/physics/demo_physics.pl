789b0a0b4359eae6528a1d3dcb6405bd1f58b9dc
David DOYEN <David.Doyen@u-pem.fr>
2021-04-12 23:41:49 +0200

    ddoyen modified Yggdrasil/demo/physics/demo_physics.pl

--- demo/physics/demo_physics.pl
+++ demo/physics/demo_physics.pl
@@ -23,7 +23,7 @@ codelatex = answers['math']
 try:
     ans = latex2sympy(codelatex, local_dict=sympify(symbol_dict))
     str_ans = str(ans)
-    srepr_ans = srepr(ans)
+    srepr_ans = srepr(volt)
     test_ans = is_rat_simp(ans)
 except:
     str_ans = ""
