c250c46e9d749cec40becc11d529137f00687ee1
Anne-Laure LOZANO <anne-laure.lozano@u-pem.fr>
2021-04-12 15:01:31 +0200

    alozano modified Yggdrasil/AAAA/ALL/ExoFlèche.pl

--- AAAA/ALL/ExoFlèche.pl
+++ AAAA/ALL/ExoFlèche.pl
@@ -1,20 +1,18 @@
-extends= /AAAA/dominique/simplematchlist.pl
+extends = /model/math/expr.pl
 
+title = Calculer la distance entre deux points du plan
 
-title==
-titre de l'exercice...
+before ==
+xA = randint(-5, 5)
+yA = randint(-5, 5)
+xB = randint(-5, 5)
+yB = randint(-5, 5)
+sol = sqrt((xA-xB)**2 + (yA-yB)**2)
 ==
 
-text==
-Relier chaque mot français à sa traduction en anglais
-==
+text ==
+Dans le plan muni d'un repère orthonormé on considère les points de coordonnées 
+$! ( {{ xA }}, {{ yA }} ) !$ et $! ( {{ xB }}, {{ yB }} ) !$.
 
-delimiter % "->"
-delimiter= ->
-
-pairs==
-Pomme -> Apple
-Chien -> Dog
-Règle -> Rule
-Chaussures -> Shoes
+Quelle est la distance entre ces deux points ?
 ==
