0fa1069fd461a5acdcfb5aba58715b50b82d590a
David DOYEN <David.Doyen@u-pem.fr>
2021-04-12 22:24:11 +0200

    ddoyen modified Yggdrasil/model/math/math.pl

--- model/math/math.pl
+++ model/math/math.pl
@@ -43,32 +43,3 @@ style.svg ==
     }
 }
 ==
-
-# HACK : Ce script JS permet de modifier la liste des boutons de contrôle.
-extrajs ==
-<script>
-    function onReadyPL(nodes) {
-        const actions = nodes.actions;
-        actions.find('.action-save').hide();
-        actions.find('.action-reset').hide();
-        actions.find('.action-next').hide();
-
-        const { origin, pathname }  = document.location;
-        const link = origin + pathname;
-
-        const buttons = actions.find('.btn-group');
-
-        {% if "reroll" in internals.buttons %}
-        buttons.append(`
-            <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
-                <i class="fas fa-dice"></i> Nouveau tirage
-            </a>
-        `);
-        {% endif %}
-        
-        {% if not "submit" in internals.buttons %}
-        actions.find('.action-submit').hide();
-        {% endif %}
-    }
-</script>
-==
