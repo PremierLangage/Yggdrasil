57622de7134f99dba499a276af6910da6dc312c2
Anne-Laure LOZANO <anne-laure.lozano@u-pem.fr>
2021-04-12 14:33:42 +0200

    alozano modified Yggdrasil/AAAA/ALL/ExoFlèche.pl

---
+++ AAAA/ALL/ExoFlèche.pl
@@ -0,0 +1,20 @@
+extends= /AAAA/dominique/simplematchlist.pl
+
+
+title==
+titre de l'exercice...
+==
+
+text==
+Relier chaque mot français à sa traduction en anglais
+==
+
+delimiter % "->"
+delimiter= ->
+
+pairs==
+Pomme -> Apple
+Chien -> Dog
+Règle -> Rule
+Chaussures -> Shoes
+==
