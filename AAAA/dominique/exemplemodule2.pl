789fdd21fa63b3318ca0434485dd4b5b2b11cb4f
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-04-12 18:11:49 +0200

    drevuz modified Yggdrasil/AAAA/dominique/exemplemodule2.pl

--- AAAA/dominique/exemplemodule2.pl
+++ AAAA/dominique/exemplemodule2.pl
@@ -12,10 +12,7 @@ Soit les deux point suivant donner la fronction qui passe par les deux points
 
 before==
 x1, y1= [randint(-5,5)]*2
-x2=1
-y2=1
+x2,y2= [randint(-5,5)]*2
 var('x')
 sol = x
-
-
 ==
