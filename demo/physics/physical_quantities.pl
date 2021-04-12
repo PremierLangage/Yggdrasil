0a63dc8a80e00afd7f0d3097d68f28a58c08534b
David DOYEN <David.Doyen@u-pem.fr>
2021-04-12 23:10:28 +0200

    ddoyen modified Yggdrasil/demo/physics/physical_quantities.pl

--- demo/physics/physical_quantities.pl
+++ demo/physics/physical_quantities.pl
@@ -39,7 +39,7 @@ def get_numeric_unit(expr):
 unit_dict = {'m': meter, 'g': gram, 'kg': kilogram, 's': second, 'A': ampere, 'K': kelvin, 'mol': mole, 'cd': candela, 'N': newton, 'J': joule, 'W': watt, 'Pa': pascal, 'Hz': hertz, 'C': coulomb, 'V': volt, 'Omega': ohm}
 
 score = 100
-ans = latex2sympy(input.value, local_dict=unit_dict)
+ans = latex2sympy(answers['math'], local_dict=unit_dict)
 num, unit = get_numeric_unit(ans)
 dim = dimsys_SI.get_dimensional_dependencies(SI.get_dimensional_expr(unit))
 feedback = f"""
