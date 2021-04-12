83778b2722ce8a2741c412bbcaba6891f0ea0e7c
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-04-12 14:26:36 +0200

    drevuz modified Yggdrasil/AAAA/dominique/exemplemodule2.pl

---
+++ AAAA/dominique/exemplemodule2.pl
@@ -0,0 +1,24 @@
+
+extends= /AAAA/dominique/simplematchlist.pl
+
+
+title==
+Matching
+==
+
+text==
+
+# Titre de l'exercice
+
+Relier chaque mot français à sa traduction en anglais
+==
+
+delimiter % ";"
+#delimiter=;
+
+pairs==
+Pomme;Apple
+Chien;Dog
+Règle;Rule
+Chaussure -> Shoes
+==
