fe3327ee5105990e24e606d6ecb18eb675b8c158
David DOYEN <David.Doyen@u-pem.fr>
2021-04-12 02:33:03 +0200

    ddoyen modified Yggdrasil/utils/sympy/latex2sympy.py

--- utils/sympy/latex2sympy.py
+++ utils/sympy/latex2sympy.py
@@ -40,7 +40,7 @@ def str2sympy(s, local_dict={}, evaluate=False):
     #global_dict.update(local_dict)
     def sqrt2(x):
         return sp.sqrt(x, evaluate=False)
-    local_dict=local_dict.update({'sqrt' : sqrt2})
+    local_dict={'sqrt' : sqrt2}
     transformations=prs.standard_transformations + (prs.implicit_multiplication_application,prs.convert_xor)
     #transformations = (prs.standard_transformations + (prs.implicit_multiplication_application,))
     return prs.parse_expr(s,local_dict=local_dict,transformations=transformations,evaluate=False)
