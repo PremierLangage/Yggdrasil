03e73a1456eb351c7269562264404567a258e383
David DOYEN <David.Doyen@u-pem.fr>
2021-04-12 12:20:05 +0200

    ddoyen modified Yggdrasil/demo/math/expr/deriv_calc.pl

---
+++ demo/math/expr/deriv_calc.pl
@@ -0,0 +1,31 @@
+extends = /model/math/expr.pl
+
+title = Calculer la dérivée d'une fonction
+
+before ==
+var('x')
+n = randint(-3, 3, [0])
+g = randitem(sympify([sin(x), cos(x), exp(x))
+h = randitem(sympify([x, x**2])
+f = n*g*h
+sol = diff(f, x)
+==
+
+text ==
+Calculer la dérivée de la fonction
+$$ f : x \mapsto {{f|latex}} .$$
+==
+
+evaluator ==
+score, error = eval_function(answers['math'], sol)
+feedback = message[error]
+==
+
+# message.NotEqual = Toto
+# message.NotExpr = On comprend rien !
+
+
+solution ==
+La dérivée est la fonction
+$$ x \mapsto {{sol|latex}} .$$
+==
