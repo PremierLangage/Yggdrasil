bdf2d4edfa214bdf78dd4ffff8526484469a8fe1
David DOYEN <David.Doyen@u-pem.fr>
2021-04-12 13:04:47 +0200

    ddoyen modified Yggdrasil/demo/math/set/product_set.pl

--- demo/math/set/product_set.pl
+++ demo/math/set/product_set.pl
@@ -9,15 +9,6 @@ B = FiniteSet(*sample(range(10), lenB))
 sol = FiniteSet(*ProductSet(A, B))
 ==
 
-evaluator ==
-from ast import literal_eval
-from evalsympy import eval_set
-from latex2sympy import FiniteSet2struct, latex2sympy
-score, error = eval_set(answers['math'], sol)
-feedback = answers['math']+str(FiniteSet2struct(sol)) + str(latex2sympy(answers['math']))
-#message[error]
-==
-
 text == 
 Ecrire en extension l'ensemble $! \\{ {{A|latex}} \\} \times \\{ {{B|latex}} \\}. !$
 ==
