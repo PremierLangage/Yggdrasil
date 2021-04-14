214c910b784e646f76efad77d4d82c0150ca7085
David DOYEN <David.Doyen@u-pem.fr>
2021-04-14 22:10:59 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_draw_angle_.pl
@@ -16,7 +16,7 @@ jxg.attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25]}
 
 name = M
 
-script_init ==
+script_init4 ==
 var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
 var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
 var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
@@ -25,6 +25,17 @@ var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
 var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
 var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'red',fixed:false});
 var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+==
+
+script_init ==
+board.create('grid', [], {gridX: 0.25, gridY: 0.25});
+board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
+board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
+var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
+var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
+var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
+var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'red',fixed:false});
+board.create('sector',[O,A,M],{color:'orange'});
 ==
 
 
