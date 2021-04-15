5b04b9b405fe84445c212afae6595a1630f73bff
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-15 13:31:17 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -12,8 +12,8 @@ Compléter les trous du texte suivant
 == 
 
 before==
-montexte="il était une fois {:MATHINPUT:=5=3~2} un {=lapin, 2    #avec de grandes oreilles} de \
-<br/>\n    {:MATHINPUT:~48,22=3,5,7} pelage {=blanc-vert,bleu,rouge } blabli blablou {=hiver,ete,automne #oh saison} et toc.."
+montexte="il était une fois {:MATHINPUT:=11,22~2} un {=lapin, 2    #avec de grandes oreilles} de \
+<br/>\n    {:MATHINPUT:~48,49=3,5,7} pelage {=blanc-vert,bleu,rouge } blabli blablou {=hiver,ete,automne #oh saison} et toc.."
 
 from loadexo import *
 
