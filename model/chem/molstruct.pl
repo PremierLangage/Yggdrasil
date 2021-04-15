4c37e4e739703221fb495adc1bfb7422698616cf
David DOYEN <David.Doyen@u-pem.fr>
2021-04-15 16:28:09 +0200

    ddoyen modified Yggdrasil/model/chem/molstruct.pl

--- model/chem/molstruct.pl
+++ model/chem/molstruct.pl
@@ -25,10 +25,12 @@ form ==
 extrajs ==
 <script src="https://unpkg.com/kekule/dist/kekule.js?modules=chemWidget,algorithm"></script>
 <script>
+ function onReadyPL(nodes) {
 var composer = new Kekule.Editor.Composer(document.getElementById('composer'));
 composer.getEditorConfigs().getChemSpaceConfigs().setDefScreenSize2D({x: 50, y: 50});
 composer.setCommonToolButtons(['undo', 'redo']);
 composer.setChemToolButtons(['manipulate', 'erase', 'bond', 'atomAndFormula']);
+}
 
 function getSmile() {
     var mol = composer.exportObjs(Kekule.Molecule)[0];
