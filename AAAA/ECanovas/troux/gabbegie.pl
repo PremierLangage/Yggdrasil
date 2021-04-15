2bd1cd97d2f0a7f1ed8113f1f1dd19f59eeb8956
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-15 14:22:30 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -15,8 +15,7 @@ Compléter les trous du texte suivant
 before==
 #montexte="il était une fois un {=lapin, 2    #avec de grandes oreilles} de <br/>\n   pelage {=blanc-vert,bleu,rouge } blabli blablou {=hiver,ete,automne #oh saison} et toc.."
 
-montexte="il était une fois une expression latex \
-<br/>\n    {:MATHINPUT:=3,5,7} à résoudre et toc.."
+montexte="il était une fois une expression latex <br/>\n    {:MATHINPUT:=3,5,7} à résoudre et toc.."
 
 from loadexo import *
 
@@ -25,14 +24,12 @@ enonc,dico,types = splitenonce(montexte)
 
 # html complet FORM menu 
 #menuForm =gethtmlmenu(enonc,dico)
-# Mathinput FORM
 
+# Mathinput FORM
 mathiForm=getFormMathinput(enonc,types)
 
 #form=mathiForm
 
-#m = defhtmlmenu(2,dico)
-#text=text+"<br/>"+enonc+"<br/><i> Result= " +str(dico)+"</i><br/>"
 text=text+enonc+"<br/><i> Result= " +str(types)+"</i><br/>"+str(mathiForm)
 ==
 
