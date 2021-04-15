d05f5e79df99953d92c3bf6feacfcddc8eb6e589
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2021-04-15 11:46:59 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/math/eqsecond.pl

--- ComputerScience/python/AP1-1920/drafts/zip/math/eqsecond.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/math/eqsecond.pl
@@ -6,8 +6,14 @@ title = Déterminer l'équation d'une courbe
 before ==
 from mplsympy import plotsvg
 var('x')
-b = randint(-3, 3)
-a = randint(-3, 3)
+
+#xA = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
+#yA = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
+#xB=randint(-4,4)
+#yB=randint(-4,4)
+#xC=randint(-4,4)
+#yC=randint(-4,4)
+
 
 xA = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
 yA = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
@@ -15,7 +21,6 @@ xB=randint(-4,4)
 yB=randint(-4,4)
 xC=randint(-4,4)
 yC=randint(-4,4)
-
 
 sol = yA*(x-xB)*(x-xC)/((xA-xB)*(xA-xC))+yB*(x-xA)*(x-xC)/((xB-xA)*(xB-xC))+ yC*(x-xB)*(x-xA)/((xC-xB)*(xC-xA))
 image = plotsvg(sol)
