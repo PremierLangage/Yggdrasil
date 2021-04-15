d7ccdb20403ec56f3c161b7b56e57ad82775c5d2
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-15 16:18:08 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -22,7 +22,7 @@ def splitenonce(enonce="nothing"): #
 
     # FORMAT {:MATHINPUT par ex:=bonne réponse1,bonne réponse2 ~ mauvaise réponse1,...}
     #patterntype = re.compile(r'(?:{\:(?P<type>[\w]+?)\:(?:=(?P<mathivalue>[\,,\(,\),\s,\w,-]+?))*?(?:~(?P<mathibadvalue>[\,,\s,\w,-]+?))*?})')
-    patterntype = re.compile(r'(?:{\:(?P<type>[\w]+?)\:(?:=(?P<mathivalue>[\,,\s,\w,-]+?))*?(?:~(?P<mathibadvalue>[\,,\s,\w,-]+?))*?})')
+    #patterntype = re.compile(r'(?:{\:(?P<type>[\w]+?)\:(?:=(?P<mathivalue>[\,,\s,\w,-]+?))*?(?:~(?P<mathibadvalue>[\,,\s,\w,-]+?))*?})')
 
     #for line in lines:
     #    match = pattern.match(line,re.MULTILINE)
