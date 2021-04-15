bb418ec88e921407c00df6503e5f4ed125b0b014
Marc ZIPSTEIN <Marc.Zipstein@u-pem.fr>
2021-04-15 11:03:10 +0200

    mzipstein modified Yggdrasil/ComputerScience/python/AP1-1920/drafts/zip/math/eqsecond.pl

--- ComputerScience/python/AP1-1920/drafts/zip/math/eqsecond.pl
+++ ComputerScience/python/AP1-1920/drafts/zip/math/eqsecond.pl
@@ -6,16 +6,26 @@ title = Déterminer l'équation d'une courbe
 before ==
 from mplsympy import plotsvg
 var('x')
-xA = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
 b = randint(-3, 3)
+a = randint(-3, 3)
+
+xA = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
 yA = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
-sol = a*x + b
+xB=randint(-4,4)
+yB=randint(-4,4)
+xC=randint(-4,4)
+yC=randint(-4,4)
+
+
+sol = yA*(x-xB)*(x-xC)/((xA-xB)*(xA-xC))+
+      yB*(x-xA)*(x-xC)/((xB-xA)*(xB-xC))+
+      yC*(x-xB)*(x-xA)/(xC-xB)*(xC-xA)
 image = plotsvg(sol)
 ==
 
 text ==
 Déterminer l'équation de la courbe du polynome  de degré deux passant par les points de coordonnées:
- {{(xA,yA)}}
+$! ( {{ xA }}, {{ yA }} )!$$! ( {{ xB }}, {{ yB }} )!$ et $! ( {{ xC }}, {{ yC }} )!$
  
  (en notant $! x !$ la variable d'abscisse et $! y !$ la variable d'ordonnée).
 
