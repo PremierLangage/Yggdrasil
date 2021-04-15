50ebe91d8a5d974b5cc8a80f4b71a2057d147a2b
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-15 11:48:01 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -12,13 +12,13 @@ Compléter les trous du texte suivant
 == 
 
 before==
-montexte="il était une fois  un {=lapin, 2    #avec de grandes oreilles} de \
+montexte="il était une fois {:MATHINPUT:=5=3~2} un {=lapin, 2    #avec de grandes oreilles} de \
 <br/>\n    pelage {=blanc-vert,bleu,rouge } blabli blablou {=hiver,ete,automne #oh saison} et toc.."
 
 from loadexo import *
 
 # du texte vers le dico des réponses/feedbacks et texte
-enonc,dico = splitenonce(montexte)
+enonc,dico,types = splitenonce(montexte)
 
 # html complet FORM menu 
 menuForm =gethtmlmenu(enonc,dico)
@@ -27,5 +27,6 @@ form=menuForm
 
 m = defhtmlmenu(2,dico)
 #text=text+"<br/>"+enonc+"<br/><i> Result= " +str(dico)+"</i><br/>"
+text=text+"<br/><i> Result= " +str(types)+"</i><br/>"
 ==
 
