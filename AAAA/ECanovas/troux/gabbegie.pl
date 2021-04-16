a01e5a0dc9648e15cf263bd35d9a4a1bba36adee
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-16 10:36:52 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -40,10 +40,10 @@ if types != []:
     lesSol=types[0]
     lesSol=lesSol[1].split(',')
 
-text=text+"<br/><small> lesSol= "+str(lesSol)+" "+str(latex2sympy(lesSol[0]))+"</small>"
+#text=text+"<br/><small> lesSol= "+str(lesSol)+" "+str(latex2sympy(lesSol[0]))+"</small>"
 textsol=""
 for sol in lesSol:
-    text=text+"<br/><small> uneSol= str(latex2sympy("+sol+"))</small>"
+    #text=text+"<br/><small> uneSol= str(latex2sympy("+sol+"))</small>"
     textsol=textsol+","+str(latex2sympy(sol))
 sol=""
 ==
