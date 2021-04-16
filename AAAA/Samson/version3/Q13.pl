cd8379c07550cf4d584082186ed7835a18001395
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-04-16 11:02:40 +0200

    drevuz modified Yggdrasil/AAAA/Samson/version3/Q13.pl

---
+++ AAAA/Samson/version3/Q13.pl
@@ -0,0 +1,17 @@
+extends = /gift/templates/qmultichoice.pl
+
+title==
+Forme bilinéaire alternée
+==
+
+text==
+<p>Soit $!E!$ un espace vectoriel, $!u!$, $!v!$ deux vecteurs de $!E!$, et $!f\:E\times E\to \mathbb R!$ une forme bilinéaire <strong>alternée. </strong></p>
+<p>Cocher les assertions justes.</p>
+==
+
+choices==
+%50.0%<p>$!f(u,u)=0!$</p>
+%-50.0%<p>$!f(u,v)=f(v,u)!$</p>
+%50.0%<p>$!f(u,v)=-f(v,u)!$</p>
+%-50.0%<p>$!f(u,v)=f(u)-f(v)!$</p>
+==
