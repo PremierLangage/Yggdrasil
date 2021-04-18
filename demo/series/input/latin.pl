5cfba36790b687b9c0e641ef5fd451ed28626b04
David DOYEN <David.Doyen@u-pem.fr>
2021-04-18 11:52:36 +0200

    ddoyen modified Yggdrasil/demo/series/input/latin.pl

--- demo/series/input/latin.pl
+++ demo/series/input/latin.pl
@@ -1,11 +1,12 @@
 extends = /model/series/input.pl
 
-@ phrases.txt
+@ phrases.txt [data.csv]
 
 title = Traduire des mots en contexte
 
 nbstep % 3
 delimiter % "|"
+
 intro ==
 Pour chacune des {{nbstep}} phrases suivantes, traduire en latin le mot en gras (avec le bon cas).
 ==
