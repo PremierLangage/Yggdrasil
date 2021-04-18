cfe98806984ed15215500f57a00bd0a9bfc96ccd
David DOYEN <David.Doyen@u-pem.fr>
2021-04-18 20:56:38 +0200

    ddoyen modified Yggdrasil/demo/math/complex/complex_mul.pl

--- demo/math/complex/complex_mul.pl
+++ demo/math/complex/complex_mul.pl
@@ -9,9 +9,11 @@ before ==
 z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
 z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
 sol = (z1 * z2).expand()
+expr = log(2)
 ==
 
 text ==
+{{expr|latex}}
 On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. 
 
 Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
