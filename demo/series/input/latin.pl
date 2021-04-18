cd85b17d9e1a6e735c09e83430435dbda36fbc27
David DOYEN <David.Doyen@u-pem.fr>
2021-04-18 11:53:19 +0200

    ddoyen modified Yggdrasil/demo/series/input/latin.pl

--- demo/series/input/latin.pl
+++ demo/series/input/latin.pl
@@ -1,6 +1,19 @@
 extends = /model/series/input.pl
 
-@ phrases.txt [data.csv]
+data ==
+phrase|motlatin
+Le **maître** appelle l'esclave de son fils.|dominus
+Le maître appelle l'**esclave** de son fils.|servum
+Le maître appelle l'esclave de son **fils**.|filii
+**Esclave**, donne de la nourriture aux chevaux.|serve
+Esclave, donne de la nourriture aux **chevaux**.|equis
+Les **fils** des esclaves devenaient eux-mêmes des esclaves.|filii
+Les fils des **esclaves** devenaient eux-mêmes des esclaves.|servorum
+L'**esclave** donne un livre au fils du maître.|servus
+L'esclave donne un **livre** au fils du maître.|librum
+L'esclave donne un livre au **fils** du maître.|filio
+L'esclave donne un livre au fils du **maître**.|domini
+==
 
 title = Traduire des mots en contexte
 
