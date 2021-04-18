b6674cde823b4e0afd94f058a51001d17c15fde2
David DOYEN <David.Doyen@u-pem.fr>
2021-04-18 11:59:51 +0200

    ddoyen modified Yggdrasil/demo/series/input/latin.pl

--- demo/series/input/latin.pl
+++ demo/series/input/latin.pl
@@ -20,9 +20,10 @@ title = Traduire des mots en contexte
 nbstep % 3
 delimiter % "|"
 
-intro ==
-{{all_lines}}
-Pour chacune des {{nbstep}} phrases suivantes, traduire en latin le mot en gras (avec le bon cas).
+textstep ==
+<strong> Phrase {{ step + 1 }}. </strong><br> 
+
+{{ all_lines[step]['phrases'] }}
 ==
 
 textstep ==
