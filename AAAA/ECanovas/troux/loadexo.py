63940dd92fbcc8d5365b8e18690eb4b738844c85
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-22 15:11:44 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -49,7 +49,7 @@ def splitenonce(enonce="nothing"): #
 
 def defFormMathinput(indice,dico):
 
-    input_prefix=str(dico[indice][1])
+    input_prefix=str((dico[indice])[1])
     if indice==0:
         unInput='<p>'+input_prefix+'<span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p> <input type="text" id="form_math2" hidden=true> <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">'
     else:
