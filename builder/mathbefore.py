12d76a3c259d6ebb3382e91984266609ca6ebe79
David DOYEN <David.Doyen@u-pem.fr>
2021-04-22 09:54:04 +0200

    ddoyen modified Yggdrasil/builder/mathbefore.py

--- builder/mathbefore.py
+++ builder/mathbefore.py
@@ -43,7 +43,8 @@ if __name__ == "__main__":
         dic = json.load(f)
     Component.sync_context(dic)
     
-    code = "\n".join([dic.get('headerbefore', ""), dic.get('before', ""), dic.get('footerbefore', "")])
+    before_scripts = dic.get('before_scripts', ['headerbefore', 'before', 'footerbefore'])
+    code = "\n".join([dic.get(name, "") for name in before_scripts])
 
     # execute the script in before key with dic as globals
     exec(code, dic)
