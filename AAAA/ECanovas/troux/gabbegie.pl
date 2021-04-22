bc25e3476e0d92db9c0acd92e9ecb804533cb901
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-22 16:24:53 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -46,6 +46,70 @@ text=enonc+" et "+str(len(types))+ " et " +str(types) +" "+str(textsol)+"<br/>Fo
 
 sol=""
 ==
+extrajs ==
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1-b/mathquill.min.js" type="text/javascript"></script>
+<script>
+var MQ = MathQuill.getInterface(2);
+var mathFieldSpan = document.getElementById('math-field');
+var latexSpan = document.getElementById('form_math');
+var mathField = MQ.MathField(mathFieldSpan, {
+  charsThatBreakOutOfSupSub: '+-=<>',
+  autoCommands: 'pi theta sqrt sum infty infin emptyset alpha',
+  autoOperatorNames: 'sin cos ln exp',
+  handlers: {
+    edit: function() { // useful event handlers
+      latexSpan.value = mathField.latex(); // simple API
+    }
+  }
+});
+mathField.latex("{{ prev_value }}");
+
+var mathFieldSpan2 = document.getElementById('math-field2');
+var latexSpan2 = document.getElementById('form_math2');
+var mathField2 = MQ.MathField(mathFieldSpan, {
+  charsThatBreakOutOfSupSub: '+-=<>',
+  autoCommands: 'pi theta sqrt sum infty infin emptyset alpha',
+  autoOperatorNames: 'sin cos ln exp',
+  handlers: {
+    edit: function() { // useful event handlers
+      latexSpan2.value = mathField2.latex(); // simple API
+    }
+  }
+});
+mathField2.latex("{{ prev_value }}");
+
+</script>
+<script>
+    function onReadyPL(nodes) {
+        const actions = nodes.actions;
+        actions.find('.action-save').hide();
+        actions.find('.action-reset').hide();
+        actions.find('.action-next').hide();
+
+        const { origin, pathname }  = document.location;
+        const link = origin + pathname;
+
+        const buttons = actions.find('.btn-group');
+
+        {% if "reroll" in internals.buttons %}
+        buttons.append(`
+            <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
+                <i class="fas fa-dice"></i> Nouveau tirage
+            </a>
+        `);
+        {% endif %}
+        
+        {% if not "submit" in internals.buttons %}
+        actions.find('.action-submit').hide();
+        {% endif %}
+    }
+
+    function onBeforeSubmitPL() {
+    latexSpan.value = mathField.latex();
+    return true;
+}
+</script>
+==
 
 checkratsimp = True
 symbol_dict = {'e': E}
