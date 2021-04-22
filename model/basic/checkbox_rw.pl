b7d4950ba11713df3059522faaeb99369ca4561b
David DOYEN <David.Doyen@u-pem.fr>
2021-04-22 23:48:44 +0200

    ddoyen modified Yggdrasil/model/basic/checkbox_rw.pl

---
+++ model/basic/checkbox_rw.pl
@@ -0,0 +1,49 @@
+extends = /model/basic.pl
+
+checkbox =: CheckboxGroup
+checkbox.decorator = CustomCheckbox
+
+shuffled % true
+
+footerbefore ==
+checkbox.setdata_from_rw(right.splitlines(), wrong.splitlines(), nbitems, nbright)
+
+if shuffled:
+    checkbox.shuffle()
+==
+
+nbitems % 5
+
+nbright % 2
+
+right ==
+bonjour
+abc
+oui
+NON
+Ciao
+good_morning
+byeBye7
+_UGE_
+==
+
+wrong ==
+Hi!
+au revoir
+6hello6
+def
+for
+good-afternoon
+f()
+==
+
+settings.feedback = lightscore
+
+form ==
+{{ checkbox|component }}
+==
+
+evaluator ==
+feedback = " "
+score = checkbox.eval()
+==
