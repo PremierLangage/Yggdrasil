f4a0a3719a3ac7462d3d00d3e2643f281b11d364
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-22 14:45:21 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/gabbegie.pl

--- AAAA/ECanovas/troux/gabbegie.pl
+++ AAAA/ECanovas/troux/gabbegie.pl
@@ -41,7 +41,7 @@ for sol in lesSol:
     else:
         textsol=textsol+" ou "+str(latex2sympy(sol))
 
-text=enonc+" et "+str(len(types))+ " et " +str(types) +" "+str(textsol)+"<br/>"+str(mathiForm)
+text=enonc+" et "+str(len(types))+ " et " +str(types) +" "+str(textsol)+"<br/>Form="+str(mathiForm)
 
 sol=""
 ==
