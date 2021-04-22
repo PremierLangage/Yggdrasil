296a274adeb8a3f84d97fbbd8e6020f5cc0f781e
eric canovas <eric.canovas@universite-paris-saclay.fr>
2021-04-22 12:07:53 +0200

    ecanovas modified Yggdrasil/AAAA/ECanovas/troux/loadexo.py

--- AAAA/ECanovas/troux/loadexo.py
+++ AAAA/ECanovas/troux/loadexo.py
@@ -46,9 +46,9 @@ def splitenonce(enonce="nothing"): #
 
 def defFormMathinput(indice,dico):
     if indice==0:
-        unInput='<p>'+input_prefix+'<span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p> <input type="text" id="form_math" hidden=true> <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">'
+        unInput='<p>'+input_prefix+'<span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p> <inpOut type="text" id="form_math" hidden=true> <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">'
     else:
-        unInput='<p>'+input_prefix+'<span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p> <input type="text" id="form_math'+str(indice)+'" hidden=true> <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">'
+        unInput='<p>'+input_prefix+'<span id="math-field" style="font-size:14pt;padding: 0.2em;"></span></p> <inpOut type="text" id="form_math'+str(indice)+'" hidden=true> <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">'
     return unInput
 
 def getFormMathinput(enonce,dico):
