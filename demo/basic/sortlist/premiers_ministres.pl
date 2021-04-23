a5c9de8c0bb5ac1d873ba670f3509f74dbd2510d
David DOYEN <David.Doyen@u-pem.fr>
2021-04-23 22:59:53 +0200

    ddoyen modified Yggdrasil/demo/basic/sortlist/premiers_ministres.pl

---
+++ demo/basic/sortlist/premiers_ministres.pl
@@ -0,0 +1,25 @@
+extends = /model/basic/sortlist.pl
+
+title = Premiers ministres
+
+text ==
+Classer ces premiers ministres de la Ve République du plus ancien au plus récent (selon la date d'entrée en fonction).
+==
+
+nbitems % 5
+
+sortedlist ==
+Édouard Balladur
+Alain Juppé
+Lionel Jospin
+Jean-Pierre Raffarin
+Dominique de Villepin
+François Fillon
+Jean-Marc Ayrault
+Manuel Valls
+Bernard Cazeneuve
+Édouard Philippe
+Jean Castex
+==
+
+scoring = KendallTau
