0ff5828ae45a60e50bab6510ba025b15b5c735b9
David DOYEN <David.Doyen@u-pem.fr>
2021-04-23 16:26:30 +0200

    ddoyen modified Yggdrasil/model/basic/sortlist.pl

--- model/basic/sortlist.pl
+++ model/basic/sortlist.pl
@@ -2,27 +2,7 @@ extends = /model/basic.pl
 
 title =
 
-text==
-En utilisnt Drag and Drop, rangez les valeurs dans l'ordre.
-==
-
 nbitems % null
-
-footerbeforeold ==
-import random as rd
-nbsample = int(nbsample)
-
-if isinstance(sortedlist, str): 
-    lst = sortedlist.splitlines()
-else:
-    lst = sortedlist
-if nbsample == 0:
-    nbsample = len(lst)
-
-samplelist= [ lst[i] for i in  sorted(rd.sample(range(len(lst)), nbsample))]
-sortlist.setdata_from_list(samplelist)
-==
-
 
 footerbefore ==
 from random import randint, sample
