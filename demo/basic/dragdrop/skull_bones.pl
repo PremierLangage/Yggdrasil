878467e88637fe7adf32c38b0078b36d1fe6c814
David DOYEN <David.Doyen@u-pem.fr>
2021-04-23 19:24:54 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop/skull_bones.pl

--- demo/basic/dragdrop/skull_bones.pl
+++ demo/basic/dragdrop/skull_bones.pl
@@ -18,7 +18,7 @@ scheme =$ /Biology/media/550px-Cranial_bones_numbers.svg.png
 form ==
  <div class="row">
   <div class="column">
-    <img src="{{ scheme }}" style="max-width: 300px;>
+    <img src="{{ scheme }}" style="max-width: 300px; display: inline-block">
   </div>
   <div class="column">
     <ul>
