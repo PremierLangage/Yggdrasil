51a80637051b783ac08095f0c6e1066575ce8a50
David DOYEN <David.Doyen@u-pem.fr>
2021-04-24 22:37:05 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop/genetics.pl

--- demo/basic/dragdrop/genetics.pl
+++ demo/basic/dragdrop/genetics.pl
@@ -11,7 +11,8 @@ form ==
 L’ensemble des gènes caractéristiques de l’espèce à laquelle appartient un organisme, constitue son {{ drops[0] }}. 
 Chez les individus d’une même espèce, un gène peut cependant exister sous différentes formes présentant de légères modifications de séquence : les allèles. 
 L’ensemble des allèles d’un individu définit son {{ drops[1] }}. Lorsqu’ils s’expriment, lors de la synthèse des protéines, les gènes participent à la construction de l’individu et à la mise en place de son {{ drops[2] }}. 
-</p>
+</p><br>
+
 {% for label in labels %} {{ label }} {% endfor %}
 ==
 
