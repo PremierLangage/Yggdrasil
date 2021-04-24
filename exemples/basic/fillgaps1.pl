b8339d6069a4ed5c1e508ea4c6a7bb650013d376
David DOYEN <David.Doyen@u-pem.fr>
2021-04-25 00:01:30 +0200

    ddoyen modified Yggdrasil/exemples/basic/fillgaps1.pl

---
+++ exemples/basic/fillgaps1.pl
@@ -0,0 +1,17 @@
+extends = /model/basic/filltext.pl
+
+title ==
+Génétique
+==
+
+filledtext ==
+Un gène est un fragment de la molécule d’{ADN}. L’ensemble des gènes caractéristiques de l’espèce à laquelle appartient un organisme, constitue son {génome}. Chez les individus d’une même espèce, un gène peut cependant exister sous différentes formes présentant de légères modifications de séquence (allèles). L’ensemble des allèles d’un individu définit son {génotype}. Lorsqu’ils s’expriment, lors de la synthèse des {protéines}, les gènes participent à la construction de l’individu et à la mise en place de son {phénotype}. 
+==
+
+labval ==
+ARN
+caryotype
+cellules
+chromosomes
+gamètes
+==
