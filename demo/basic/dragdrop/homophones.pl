e8f3d7ad125adf65eb9143a2153ed0aefb2e6e8e
David DOYEN <David.Doyen@u-pem.fr>
2021-04-24 18:30:22 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop/homophones.pl

--- demo/basic/dragdrop/homophones.pl
+++ demo/basic/dragdrop/homophones.pl
@@ -3,12 +3,10 @@ extends = /model/basic/dragdrop.pl
 title = Os du crâne
 
 sol ==
-Ces
-C'est
-Ces
+ces
+c'est
+ces
 ==
-
-scheme =$ /Biology/media/550px-Cranial_bones_numbers.svg.png
 
 form ==
 Je voudrais {{ drops[0] }} chausures pour mon anniversaire.  
