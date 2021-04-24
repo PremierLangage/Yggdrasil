87e861cd744145bc0aeb2b1f6e8edbf9b6610122
David DOYEN <David.Doyen@u-pem.fr>
2021-04-25 00:13:29 +0200

    ddoyen modified Yggdrasil/demo/basic/filltext/genetics.pl

--- demo/basic/filltext/genetics.pl
+++ demo/basic/filltext/genetics.pl
@@ -7,11 +7,9 @@ Compléter le texte suivante avec les bonnes étiquettes.
 ==
 
 form ==
-<p> 
 L’ensemble des gènes caractéristiques de l’espèce à laquelle appartient un organisme, constitue son {génome}. 
 Chez les individus d’une même espèce, un gène peut cependant exister sous différentes formes présentant de légères modifications de séquence : les allèles. 
 L’ensemble des allèles d’un individu définit son {génotype}. Lorsqu’ils s’expriment, lors de la synthèse des protéines, les gènes participent à la construction de l’individu et à la mise en place de son {phénotype}. 
-</p>
 ==
 
 labval ==
