ed13c818159cf5912670ff220ac6fbb97fa083bd
David DOYEN <David.Doyen@u-pem.fr>
2021-04-25 00:01:09 +0200

    ddoyen modified Yggdrasil/model/basic/filltext.pl

---
+++ model/basic/filltext.pl
@@ -0,0 +1,33 @@
+extends = /model/basic/dragdrop.pl
+
+shuffle % true
+
+text ==
+Compléter le texte suivant avec les étiquettes.
+==
+
+before ==
+import re
+
+sol = re.findall(r'\{(.*?)\}', filledtext)
+
+count = -1
+def replace(x):
+    global count
+    count += 1
+    return "{{ drops[" + str(count) + "]}}"
+
+form = re.sub(r'\{(.*?)\}', replace , filledtext)
+
+form2 = """
+
+<br><br>
+
+{% for label in labels %}
+{{ label|component }}
+{% endfor %}
+"""
+
+form = form + form2
+==
+
