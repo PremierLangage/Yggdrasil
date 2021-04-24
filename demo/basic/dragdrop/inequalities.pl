625f8c31536376658ed6a75e8cb754903acd57da
David DOYEN <David.Doyen@u-pem.fr>
2021-04-24 19:09:28 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop/inequalities.pl

--- demo/basic/dragdrop/inequalities.pl
+++ demo/basic/dragdrop/inequalities.pl
@@ -1,23 +1,16 @@
-
+extends = /model/basic/dragdrop.pl
 
 before==
-import random as rd
-
-lt="&lt;"
-gt="&gt;"
 
 n = 4
 
 numbers = []
 sol = []
 for _ in range(n):
-    [a,b] = rd.sample(range(10,100),2)
-    numbers.append([a,b])
+    [a,b] = sample(range(10, 100), 2)
+    numbers.append([a, b])
     if a < b:
-        sol.append(lt)
+        sol.append("$! < !$")
     else:
-        sol.append(gt)
-
-label = Labels([lt,gt])
-drop = Drops(n)
+        sol.append("$! > !$")
 ==
