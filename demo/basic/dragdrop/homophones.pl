7f19b34c383a608b02afc72ee041cadeba8cc218
David DOYEN <David.Doyen@u-pem.fr>
2021-04-24 15:37:30 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop/homophones.pl

---
+++ demo/basic/dragdrop/homophones.pl
@@ -0,0 +1,19 @@
+extends = /model/basic/dragdrop.pl
+
+title = Os du crâne
+
+sol ==
+Ces
+C'est
+Ces
+==
+
+scheme =$ /Biology/media/550px-Cranial_bones_numbers.svg.png
+
+form ==
+Je voudrais {{ drop[0] }} livres pour mon anniversaire
+{{ drop[1] }}
+{{ drop[2] }}
+
+{{ label[0] }}
+==
