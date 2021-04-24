b764179443fb57cdc634da256a9ec8f650bd1f05
David DOYEN <David.Doyen@u-pem.fr>
2021-04-25 00:44:20 +0200

    ddoyen modified Yggdrasil/model/basic/input.pl

--- model/basic/input.pl
+++ model/basic/input.pl
@@ -1,33 +1,5 @@
 extends = /model/basic.pl
 
-headerbefore ==
-import random as rd
-from io import StringIO
-from csv import DictReader
-
-all_rows = None
-
-try:
-    with open('data.csv', newline='') as file:
-        all_rows = list(DictReader(file, delimiter=delimiter))
-except:
-    pass
-
-if 'data' in globals():
-     all_rows = list(DictReader(StringIO(data), delimiter=delimiter))
-
-if all_rows is not None:  
-    row = rd.choice(all_rows)
-    for key, value in row.items():
-        globals()[key] = value
-
-from jinja2 import Environment, BaseLoader
-Env = Environment(loader=BaseLoader())
-solution = Env.from_string(solution).render(globals())
-==
-
-delimiter % ","
-
 casesensitive % false
 
 tolerance % 0
@@ -82,8 +54,3 @@ if any([samestrings(input.value, item, diffmeasure=diffmeasure, tolerance=tolera
 else:
     score = 0
 ==
-
-
-
-
-
