f0d0f3914077b60eb51448aaa687497ae711b5a7
David DOYEN <David.Doyen@u-pem.fr>
2021-04-25 14:48:19 +0200

    ddoyen modified Yggdrasil/model/basic/input.pl

--- model/basic/input.pl
+++ model/basic/input.pl
@@ -1,4 +1,5 @@
 extends = /model/basic.pl
+@ /utils/pltext.py
 
 casesensitive % false
 
@@ -13,33 +14,19 @@ form ==
 settings.feedback = rightwrong
 
 evaluator ==
+from pltext import edit_distance
 
 tolerance = float(tolerance)
 
-def minimumEditDistance(s1,s2):
-    if len(s1) > len(s2):
-        s1,s2 = s2,s1
-    distances = range(len(s1) + 1)
-    for index2,char2 in enumerate(s2):
-        newDistances = [index2+1]
-        for index1,char1 in enumerate(s1):
-            if char1 == char2:
-                newDistances.append(distances[index1])
-            else:
-                newDistances.append(1 + min((distances[index1],
-                                             distances[index1+1],
-                                             newDistances[-1])))
-        distances = newDistances
-    return distances[-1]
 
 def samestrings(str1, str2, diffmeasure="EditDist", tolerance=0, casesensitive=False):
     if not casesensitive:
         str1 = str1.casefold()
         str2 = str2.casefold()
     if diffmeasure == "EditRatio":
-        return minimumEditDistance(str1, str2)/len(str2) <= tolerance
+        return edit_distance(str1, str2)/len(str2) <= tolerance
     else:
-        return minimumEditDistance(str1, str2) <= tolerance
+        return edit_distance(str1, str2) <= tolerance
 
 if isinstance(solution, str):
     if '\n' in solution:
