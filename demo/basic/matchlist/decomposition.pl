643adbf0c56378b773e0911406247787c0069ee5
David DOYEN <David.Doyen@u-pem.fr>
2021-04-26 06:36:51 +0200

    ddoyen modified Yggdrasil/demo/basic/matchlist/decomposition.pl

--- demo/basic/matchlist/decomposition.pl
+++ demo/basic/matchlist/decomposition.pl
@@ -10,7 +10,6 @@ before ==
 from random import randint, sample
 matches = []
 for a in sample(range(10, 20), 4) :
-    a = randint(10, 20)
     b = randint(1, a-1)
     c = a - b
     matches.append([str(a), f"{b} + {c}"])
