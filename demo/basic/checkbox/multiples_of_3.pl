2399e0f977c763cbedc24852b6230f5a0b5cb8e0
David DOYEN <David.Doyen@u-pem.fr>
2021-04-27 12:16:02 +0200

    ddoyen modified Yggdrasil/demo/basic/checkbox/multiples_of_3.pl

---
+++ demo/basic/checkbox/multiples_of_3.pl
@@ -0,0 +1,14 @@
+extends = /model/basic/checkbox.pl
+
+title = Multiples de 3
+
+before ==
+items = sample(range(50, 100), 6)
+for i in range(items):
+    if items[i] % 3 == 0
+        indsol.append(i)
+==
+
+text ==
+Parmi les nombres suivants, lesquels sont des multiples de 3 ?
+==
