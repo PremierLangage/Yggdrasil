7e02721776642a7c19305b45adf7a6e785981c44
David DOYEN <David.Doyen@u-pem.fr>
2021-04-27 13:45:41 +0200

    ddoyen modified Yggdrasil/demo/basic/radio/radio3.pl

---
+++ demo/basic/radio/radio3.pl
@@ -0,0 +1,13 @@
+extends = /model/basic/radio.pl
+
+title ==
+Exemple 3
+==
+
+text ==
+Sélectionner le plus petit nombre de la liste suivante.
+==
+
+before ==
+items = sorted(sample(range(50), 4))
+==
