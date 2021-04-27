6cd05a9b50f017056553b5be135162ad0f709b77
David DOYEN <David.Doyen@u-pem.fr>
2021-04-27 23:48:20 +0200

    ddoyen modified Yggdrasil/model/basic/seltext.pl

--- model/basic/seltext.pl
+++ model/basic/seltext.pl
@@ -108,7 +108,7 @@ var words = document.querySelectorAll("#textselect span")
 for (i = 0; i < words.length; i++) {
     words[i].dataset.selected = false;
 	words[i].addEventListener('click', function() {
-		if (this.dataset.selected) {
+		if (this.dataset.selected == true) {
 			this.style.backgroundColor = "transparent";
             this.data("selected") = false;
 			list.splice( list.indexOf(this.id), 1 ); 
