091008436b54f4be11df13c6eb2fced605e6878a
David DOYEN <David.Doyen@u-pem.fr>
2021-04-28 14:40:12 +0200

    ddoyen modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -10,14 +10,6 @@ seltext ==
 {{L'artiste}} installait {son chevalet} lorsque {la nuit} tombait.
 ==
 
-old ==
-En 1888, {{Vincent Van Gogh}} peint {«Nuit Etoilée»}. 
-Il est difficile de représenter {la nuit} {en couleurs}. 
-Mais comment peindre sans lampadaire ? 
-{{Le peintre}} a eu pour cela {une idée lumineuse}. 
-On dit qu'{{il}} a planté {des bougies} sur {le rebord} de {son chapeau}. 
-{{L'homme}} a ainsi pu peindre grâce {aux minuscules flammes}
-==
 
 
 
