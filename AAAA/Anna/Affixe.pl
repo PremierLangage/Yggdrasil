509a8c71aafa281a369f8bdeff38c6b6362257af
ANNA DIAW <adiaw@etud.u-pem.fr>
2021-04-28 17:18:27 +0200

    adiaw modified Yggdrasil/AAAA/Anna/Affixe.pl

--- AAAA/Anna/Affixe.pl
+++ AAAA/Anna/Affixe.pl
@@ -1,8 +1,8 @@
 extends = /Mathematics/exercises/complex_numbers/affixe.pl
 
 before ==
-xsol = randint(-4, 4, [0])
-ysol = randint(-4, 4, [0])
+xsol = randint(-6, 6, [0])
+ysol = randint(-6, 6, [0])
 z = xsol + ysol*I
 jxg.setscript(script_init + script_aux)
 ==
