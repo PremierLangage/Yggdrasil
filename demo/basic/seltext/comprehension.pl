bf01bc612de35d3cb0dda139e181dbc488d1925c
David DOYEN <David.Doyen@u-pem.fr>
2021-04-28 14:41:05 +0200

    ddoyen modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -7,9 +7,18 @@ Identifier les adjectifs comparatifs dans le texte ci-dessous.
 ==
 
 seltext == 
-{{Lartiste}} installait {son chevalet} lorsque {la nuit} tombait.
+{{L'artiste}} installait {son chevalet} lorsque {la nuit} tombait.
 ==
 
+
+old ==
+En 1888, {{Vincent Van Gogh}} peint {«Nuit Etoilée»}. 
+Il est difficile de représenter {la nuit} {en couleurs}. 
+Mais comment peindre sans lampadaire ? 
+{{Le peintre}} a eu pour cela {une idée lumineuse}. 
+On dit qu'{{il}} a planté {des bougies} sur {le rebord} de {son chapeau}. 
+{{L'homme}} a ainsi pu peindre grâce {aux minuscules flammes}
+==
 
 
 
