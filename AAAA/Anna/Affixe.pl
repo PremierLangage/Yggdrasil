eb4f69ecf2518d0ad3c731681b8ec284f56681cf
ANNA DIAW <adiaw@etud.u-pem.fr>
2021-04-28 17:15:29 +0200

    adiaw modified Yggdrasil/AAAA/Anna/Affixe.pl

--- AAAA/Anna/Affixe.pl
+++ AAAA/Anna/Affixe.pl
@@ -1,4 +1,4 @@
-extends = Yggdrasil/Mathematics/exercises/complex_numbers/affixe.pl
+extends = /Mathematics/exercises/complex_numbers/affixe.pl
 
 xsol = randint(-4, 4, [0])
 ysol = randint(-5, 5, [0])
