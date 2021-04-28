19ff35363033b1bf56fc905fd16d5105b74f87d1
David DOYEN <David.Doyen@u-pem.fr>
2021-04-28 14:38:30 +0200

    ddoyen modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -11,7 +11,7 @@ seltext ==
 {{L’artiste}} installait {son chevalet} lorsque {la nuit} tombait. 
 Mais comment peindre sans lampadaire? 
 {{Le peintre}} a eu pour cela {une idée lumineuse}. 
-On dit qu’{{il}} a planté {des bougies} sur {le rebord} de {son chapeau}. 
+On dit qu'{{il}} a planté {des bougies} sur {le rebord} de {son chapeau}. 
 {{L’homme}} a ainsi pu peindre grâce {aux minuscules flammes}
 ==
 
