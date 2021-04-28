2d1c7f77b38ff54cf8bdd732ee29ff5d2c83b49e
David DOYEN <David.Doyen@u-pem.fr>
2021-04-28 15:56:48 +0200

    ddoyen modified Yggdrasil/model/basic/radio.pl

--- model/basic/radio.pl
+++ model/basic/radio.pl
@@ -10,10 +10,6 @@ shuffled % true
 
 before_scripts % ["importfunc", "before", "process"]
 
-importfunc ==
-from random import randint, choice, choices, sample, shuffle
-==
-
 process ==
 if isinstance(items, str):
     _items_ = items.splitlines()
