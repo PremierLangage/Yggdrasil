65e72f7c97940c76cc8214b5cfa5e7776b0eb355
David DOYEN <David.Doyen@u-pem.fr>
2021-04-28 14:37:31 +0200

    ddoyen modified Yggdrasil/demo/basic/seltext/comprehension.pl

---
+++ demo/basic/seltext/comprehension.pl
@@ -0,0 +1,21 @@
+extends = /model/basic/seltext.pl
+
+title = Grec ancien
+
+text ==
+Identifier les adjectifs comparatifs dans le texte ci-dessous.
+==
+
+seltext == 
+En 1888, {{Vincent Van Gogh}} peint {«Nuit Etoilée»}. 
+Il est difficile de représenter {la nuit} {en couleurs}. 
+{{L’artiste}} installait {son chevalet} lorsque {la nuit} tombait. 
+Mais comment peindre sans lampadaire? 
+{{Le peintre}} a eu pour cela {une idée lumineuse}. 
+On dit qu’{{il}} a planté {des bougies} sur {le rebord} de {son chapeau}. 
+{{L’homme}} a ainsi pu peindre grâce {aux minuscules flammes}
+==
+
+
+
+
