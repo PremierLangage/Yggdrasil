a4044d14a262b3ef20a60e1ddf03c10287cde5f4
David DOYEN <David.Doyen@u-pem.fr>
2021-04-29 13:16:44 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/complex_numbers/cplx_computation_.pl

--- Mathematics/exercises/complex_numbers/cplx_computation_.pl
+++ Mathematics/exercises/complex_numbers/cplx_computation_.pl
@@ -1,9 +1,8 @@
 # Author: D. Doyen
 # Tags: complex numbers
-# 19/8/2020
+# 29/4/2021
 
 extends = /model/math/mathcomplex.pl
-
 
 title = Opération sur les nombres complexes
 
@@ -19,79 +18,79 @@ z2b=conjugate(z2)
 
 formula=randitem(param['formulas'])
 
-if formula=="z1+z2":
-    sol=z1+z2
-    expr="z_1 + z_2"
-elif formula=="z1-z2":
-    sol=z1-z2
-    expr="z_1 - z_2"
-elif formula=="z1*z2":
-    sol=z1*z2
-    expr=r"z_1 \times z_2"
-elif formula=="z^2":
-    sol=z1*z1
-    expr=r"z^2"
-elif formula=="1/z":
-    sol=1/z1
-    expr=r"\frac{1}{z}"
-elif formula=="1/(z1*z2)":
-    sol=1/(z1*z2)
-    expr=r"\frac{1}{z_1 \times z_2}"
-elif formula=="z1/z2":
-    sol=z1/z2
-    expr=r"\frac{z_1}{z_2}"
-elif formula=="(z1+z2)b":
-    sol=z1b+z2b
-    expr="\overline{z_1+ z_2}"
-elif formula=="z1b+z2b":
-    sol=z1b+z2b
-    expr="\overline{z_1}+ \overline{z_2}"
-elif formula=="z1b+z2":
+if formula == "add":
+    sol = z1 + z2
+    expr = "z_1 + z_2"
+elif formula == "sub":
+    sol = z1 - z2
+    expr = "z_1 - z_2"
+elif formula == "mul":
+    sol = z1 * z2
+    expr = r"z_1 \times z_2"
+elif formula == "sq":
+    sol = z1 * z1
+    expr = r"z^2"
+elif formula == "inv":
+    sol = 1/z1
+    expr = r"\frac{1}{z}"
+elif formula == "invmul":
+    sol = 1/(z1*z2)
+    expr = r"\frac{1}{z_1 \times z_2}"
+elif formula == "div":
+    sol = z1/z2
+    expr = r"\frac{z_1}{z_2}"
+elif formula == "conjadd":
+    sol = z1b+z2b
+    expr = "\overline{z_1+ z_2}"
+elif formula == "addconj":
+    sol = z1b+z2b
+    expr = "addmix"
+elif formula == "z1b+z2":
     if randitem([1,2])==1:
-        sol=z1b+z2
-        expr="\overline{z_1} + z_2"
+        sol = z1b+z2
+        expr = "\overline{z_1} + z_2"
     else:
-        sol=z1+z2b
-        expr="z_1 + \overline{z_2}"
-elif formula=="(z1-z2)b":
-    sol=z1b-z2b
-    expr="\overline{z_1 - z_2}" 
-elif formula=="z1b-z2b":
-    sol=z1b-z2b
-    expr="\overline{z_1} - \overline{z_2}"
-elif formula=="z1b-z2":
+        sol = z1+z2b
+        expr = "z_1 + \overline{z_2}"
+elif formula == "conjsub":
+    sol = z1b-z2b
+    expr = "\overline{z_1 - z_2}" 
+elif formula == "subconj":
+    sol = z1b-z2b
+    expr = "\overline{z_1} - \overline{z_2}"
+elif formula == "submix":
     if randitem([1,2])==1:
-        sol=z1b-z2
-        expr="\overline{z_1} - z_2"
+        sol = z1b-z2
+        expr = "\overline{z_1} - z_2"
     else:
-        sol=z1-z2b
-        expr="z_1 - \overline{z_2}"   
-elif formula=="(z1*z2)b":
-    sol=z1b*z2b
-    expr=r"\overline{z_1 \times z_2}" 
-elif formula=="z1b*z2b":
-    sol=z1b*z2b
-    expr=r"\overline{z_1} \times \overline{z_2}"
-elif formula=="z1b*z2":
+        sol = z1-z2b
+        expr = "z_1 - \overline{z_2}"   
+elif formula == "conjmul":
+    sol = z1b*z2b
+    expr = r"\overline{z_1 \times z_2}" 
+elif formula == "mulconj":
+    sol = z1b*z2b
+    expr = r"\overline{z_1} \times \overline{z_2}"
+elif formula == "mulmix":
     if randitem([1,2])==1:
-        sol=z1b*z2
-        expr=r"\overline{z_1} \times z_2"
+        sol = z1b*z2
+        expr = r"\overline{z_1} \times z_2"
     else:
-        sol=z1*z2b
-        expr="z_1 \times \overline{z_2}"  
-elif formula=="(z1/z2)b":
-    sol=z1b*z2b
-    expr=r"\overline{\left(\frac{z_1}{z_2}\right)}" 
-elif formula=="z1b*z2b":
-    sol=z1b*z2b
-    expr=r"\frac{ \overline{z_1} }{ \overline{z_2} }"
-elif formula=="z1b/z2":
+        sol = z1*z2b
+        expr = "z_1 \times \overline{z_2}"  
+elif formula == "conjdiv":
+    sol = z1b*z2b
+    expr = r"\overline{\left(\frac{z_1}{z_2}\right)}" 
+elif formula == "z1b*z2b":
+    sol = z1b*z2b
+    expr = r"\frac{ \overline{z_1} }{ \overline{z_2} }"
+elif formula == "z1b/z2":
     if randitem([1,2])==1:
-        sol=z1b*z2
-        expr=r"\frac{ \overline{z_1} }{ z_2 }"
+        sol = z1b*z2
+        expr = r"\frac{ \overline{z_1} }{ z_2 }"
     else:
-        sol=z1*z2b
-        expr="\frac{ z_1 }{ \overline{z_2} }"  
+        sol = z1*z2b
+        expr = "\frac{ z_1 }{ \overline{z_2} }"  
 
 sol = sol.expand()
 
@@ -102,72 +101,62 @@ else:
 
 ==
 
-
-solution ==
-La solution est $% {{ sol|latex}} %$.
-==
-
 wims ==
-\if{\type=13}{
-\text{nz=1}
-\text{expr=z^2}
-\complex{rep=(\z1)^2}
-}
 
 \if{\type=14}{
 \text{nz=1}
-\text{expr=randitem(\overline{z} \times \z,z \times \overline{z})}
+\text{expr = randitem(\overline{z} \times \z,z \times \overline{z})}
 \complex{rep=(\zb1) * (\z1)}
 }
 
 \if{\type=15}{
 \text{nz=1}
-\text{expr=\overline{z}^2}
+\text{expr = \overline{z}^2}
 \complex{rep=(\zb1)^2}
 }
 \if{\type=16}{
 \text{nz=1}
-\text{expr=\overline{z^2}}
+\text{expr = \overline{z^2}}
 \complex{rep=(\zb1)^2}
 }
 
 \if{\type=17}{
-\text{expr=\frac{z_1}{z_2}}
+\text{expr = \frac{z_1}{z_2}}
 \complex{rep=(\z1)/(\z2)}
 }
 \if{\type=18}{
 \if{randint(1..2)=1}{
-\text{expr=\frac{\overline{z_1}}{z_2}}
+\text{expr = \frac{\overline{z_1}}{z_2}}
 \complex{rep=(\zb1)/(\z2)}
 }{
-\text{expr=\frac{z_1}{\overline{z_2}}}
+\text{expr = \frac{z_1}{\overline{z_2}}}
 \complex{rep=(\z1)/(\zb2)}
 }
 }
 \if{\type=19}{
-\text{expr=\frac{\overline{z_1}}{\overline{z_2}}}
+\text{expr = \frac{\overline{z_1}}{\overline{z_2}}}
 \complex{rep=(\zb1)/(\zb2)}
 }
 \if{\type=20}{
-\text{expr=\overline{\left(\frac{z_1}{z_2}\right)}}
+\text{expr = \overline{\left(\frac{z_1}{z_2}\right)}}
 \complex{rep=(\zb1)/(\zb2)}
 }
 
 \if{\type=21}{
 \text{nz=1}
-\text{expr=\frac{1}{z}}
+\text{expr = \frac{1}{z}}
 \complex{rep=1/(\z1)}
 }
 
 \if{\type=22}{
 \text{nz=1}
-\text{expr=\frac{1}{\overline{z}}}
+\text{expr = \frac{1}{\overline{z}}}
 \complex{rep=1/(\zb1)}
 }
 
 \if{\type=23}{
 \text{nz=1}
-\text{expr=\overline{\left(\frac{1}{z}\right)}}
+\text{expr = \overline{\left(\frac{1}{z}\right)}}
 \complex{rep=1/(\zb1)}
 ==
 
