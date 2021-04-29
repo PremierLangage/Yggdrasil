73c7ccb26d462f51bd0f5be2147b1938f19e46ba
David DOYEN <David.Doyen@u-pem.fr>
2021-04-29 15:55:26 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/complex_numbers/cplx_computation_.pl

--- Mathematics/exercises/complex_numbers/cplx_computation_.pl
+++ Mathematics/exercises/complex_numbers/cplx_computation_.pl
@@ -108,7 +108,10 @@ else:
     text = text2
 ==
 
-
+text1 ==
+On considère le nombre complexe $! z = {{ z1|latex }} !$. 
+Calculer $! \displaystyle {{ expr|latex }} !$ (sous forme algébrique).
+==
 
 text2 ==
 On considère les nombres complexes $! z_1= {{ z1|latex }} !$ et $! z_2= {{ z2|latex }} !$. 
