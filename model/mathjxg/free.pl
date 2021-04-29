3a7c08db85911de5f7b41c0c33240c18b9a67b52
David DOYEN <David.Doyen@u-pem.fr>
2021-04-29 15:50:01 +0200

    ddoyen modified Yggdrasil/model/mathjxg/free.pl

--- model/mathjxg/free.pl
+++ model/mathjxg/free.pl
@@ -18,44 +18,27 @@ jxg.attributes = attributes
 jxg.setscript(script_init)
 ==
 
-attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25]}
+jxg_attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25]}
 
-name = M
+ptname = M
 
-script_init ==
+jxgscript_init ==
 board.create('grid', [], {gridX: 0.25, gridY: 0.25});
-board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
-board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
+board.create('axis',[[0, 0], [1, 0]], {ticks: {visible: false}});
+board.create('axis',[[0, 0], [0, 1]], {ticks: {visible: false}});
 var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-var O = board.create('point', [0,0],{size:1, name: 'O', color: 'black', fixed: true});
-var A = board.create('point', [1,0],{size:1, name: 'A', color: 'black', fixed: true});
-var M = board.create('glider', [1, 0.5, circle],{name:'M',color:'blue',fixed:false});
+var O = board.create('point', [0, 0], {size: 1, name: 'O', color: 'black', fixed: true});
+var A = board.create('point', [1, 0], {size: 1, name: 'A', color: 'black', fixed: true});
+var M = board.create('glider', [1, 0.5, circle], {name:'M', color: 'blue', fixed: false});
 board.create('sector', [O, A, M], {color: 'orange'});
 ==
 
 
-script_solution ==
-board.create('point',[{{xsol}}, {{ysol}}],{name:'', color:'green'});
+jxgscript_solution ==
+board.create('point', [{{xsol}}, {{ysol}}], {name:'', color:'green'});
 ==
 
-form==
-<center>
-{{ jxg|component }}
-</center>
-==
-
-script_init4 ==
-var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
-var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
-var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
-var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
-var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
-var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'red',fixed:false});
-var secOAM = board.create('sector',[O,A,M],{color:'orange'});
-==
-
-form2 ==
+form ==
 <div class="img-container">
 {{ jxg|component }}
 </div>
