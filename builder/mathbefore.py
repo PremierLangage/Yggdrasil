31faed72d2564a3d85501e83bac9d513b77a03e6
David DOYEN <David.Doyen@u-pem.fr>
2021-04-29 15:39:20 +0200

    ddoyen modified Yggdrasil/builder/mathbefore.py

--- builder/mathbefore.py
+++ builder/mathbefore.py
@@ -88,8 +88,7 @@ if __name__ == "__main__":
 
     for key in jinja_keys:
         if key in dic:
-            pass
-            # dic[key] = Env.from_string(dic[key]).render(dic)
+            dic[key] = Env.from_string(dic[key]).render(dic)
 
     with open(outputfilename, "w+") as f:
         json.dump(dic, f, cls=JSONEncoder)
