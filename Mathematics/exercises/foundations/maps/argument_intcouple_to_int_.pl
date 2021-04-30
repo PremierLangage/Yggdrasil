f2aace1c3af6422db04a6d0fc9664bc00b476308
David DOYEN <David.Doyen@u-pem.fr>
2021-04-30 16:26:29 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl

--- Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
+++ Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
@@ -3,15 +3,30 @@ extends = /model/math/tuple.pl
 title = Antécédent
 
 before ==
-#n, m = sample(symbols("n,m,p,q,r"), 2)
+n, m = sample(symbols("n m p q r"), 2)
 
-# expr = choice(sympify(exprlist))
+expr = choice(sympify(exprlist))
 
-var('n m')
+f = Lambda((n, m), expr)
 
+E1 = choice(["N","Z"])
+if E1=="N":
+    x1 = randint(0, 3)
+    E1_tex="\mathbb{N}"
+else:
+    x1 = randint(-3, 3)
+    E1_tex="\mathbb{Z}"
 
-expr = n+m
-y= 2
+E2 = choice(["N","Z"])
+if E2=="N":
+    x2 = randint(0, 3)
+    E2_tex = "\mathbb{N}"
+else:
+    x2 = randint(-3, 3)
+    E2_tex = "\mathbb{Z}"
+
+y = f(x1, x2)
+
 sol = Tuple(3, 4)
 ==
 
