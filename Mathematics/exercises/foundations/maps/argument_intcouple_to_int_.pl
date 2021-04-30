79897d014e882867afee3153b8c342cb5dd43b72
David DOYEN <David.Doyen@u-pem.fr>
2021-04-30 15:38:47 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl

--- Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
+++ Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
@@ -6,30 +6,30 @@ before ==
 n, m = list_randitem_norep(2,symbols("n,m,p,q,r"))
 expr=eval(exprcode)
 expr_tex=latex(expr)
-f = Lambda ( (n,m) , expr)
+f = Lambda((n, m), expr)
 
-E1=randitem("N","Z")
+E1 = choice(["N","Z"])
 if E1=="N":
-    x1=randint(0,3)
+    x1 = randint(0, 3)
     E1_tex="\mathbb{N}"
 else:
-    x1=randint(-3,3)
+    x1 = randint(-3, 3)
     E1_tex="\mathbb{Z}"
 
-E2=randitem("N","Z")
+E2 = choice(["N","Z"])
 if E2=="N":
-    x2=randint(0,3)
-    E2_tex="\mathbb{N}"
+    x2 = randint(0, 3)
+    E2_tex = "\mathbb{N}"
 else:
-    x2=randint(-3,3)
-    E2_tex="\mathbb{Z}"
+    x2 = randint(-3, 3)
+    E2_tex = "\mathbb{Z}"
 
-y=f(x1,x2)
-y_tex=latex(y)
+y = f(x1, x2)
+
 ==
 
 text ==
 On considère la fonction $! f : {{E1_tex}} \times {{E2_tex}} \rightarrow \mathbb{Z} !$ telle que
-$$f({{nn}},{{mm}})= {{expr_tex}}$$
-Déterminer un antécédent de $! {{y_tex}} !$ par  $! f !$
+$$f( {{n|latex}}, {{m|latex}})= {{ expr|latex }}$$
+Déterminer un antécédent de $! {{y|latex}} !$ par  $! f !$
 ==
