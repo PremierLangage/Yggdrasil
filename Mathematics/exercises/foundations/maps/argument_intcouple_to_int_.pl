d0588bcdd82f1f7b9c57922988b7f15998ba9317
David DOYEN <David.Doyen@u-pem.fr>
2021-04-30 16:25:10 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl

--- Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
+++ Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
@@ -12,5 +12,32 @@ var('n m')
 
 expr = n+m
 y= 2
+sol = Tuple(3, 4)
 ==
-test =
+
+text2 ==
+On considère la fonction $! f : {{E1_tex}} \times {{E2_tex}} \rightarrow \mathbb{Z} !$ telle que
+$$f( {{n|latex}}, {{m|latex}})= {{ expr|latex }}$$
+Déterminer un antécédent de $! {{y|latex}} !$ par  $! f !$
+==
+
+old ==
+
+E1 = choice(["N","Z"])
+if E1=="N":
+    x1 = randint(0, 3)
+    E1_tex="\mathbb{N}"
+else:
+    x1 = randint(-3, 3)
+    E1_tex="\mathbb{Z}"
+
+E2 = choice(["N","Z"])
+if E2=="N":
+    x2 = randint(0, 3)
+    E2_tex = "\mathbb{N}"
+else:
+    x2 = randint(-3, 3)
+    E2_tex = "\mathbb{Z}"
+
+y = f(x1, x2)
+==
