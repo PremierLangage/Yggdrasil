b15f77c777c3069868cb489101a4b36af5bcebae
David DOYEN <David.Doyen@u-pem.fr>
2021-04-30 16:28:36 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl

--- Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
+++ Mathematics/exercises/foundations/maps/argument_intcouple_to_int_.pl
@@ -5,7 +5,7 @@ title = Antécédent
 before ==
 n, m = sample(symbols("n m p q r"), 2)
 
-expr = choice(sympify(exprlist))
+expr = choice(sympify(exprlist, local_dict = globals()))
 
 f = Lambda((n, m), expr)
 
@@ -34,25 +34,4 @@ text ==
 On considère la fonction $! f : {{E1_tex}} \times {{E2_tex}} \rightarrow \mathbb{Z} !$ telle que
 $$f( {{n|latex}}, {{m|latex}})= {{ expr|latex }}$$
 Déterminer un antécédent de $! {{y|latex}} !$ par  $! f !$
-==
-
-old ==
-
-E1 = choice(["N","Z"])
-if E1=="N":
-    x1 = randint(0, 3)
-    E1_tex="\mathbb{N}"
-else:
-    x1 = randint(-3, 3)
-    E1_tex="\mathbb{Z}"
-
-E2 = choice(["N","Z"])
-if E2=="N":
-    x2 = randint(0, 3)
-    E2_tex = "\mathbb{N}"
-else:
-    x2 = randint(-3, 3)
-    E2_tex = "\mathbb{Z}"
-
-y = f(x1, x2)
 ==
