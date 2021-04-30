edbfc98b82306117a111c12886c98896ca0634f2
David DOYEN <David.Doyen@u-pem.fr>
2021-04-30 19:39:29 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/inequalities/linineq_.pl

--- Mathematics/exercises/algebra/elementary_algebra/inequalities/linineq_.pl
+++ Mathematics/exercises/algebra/elementary_algebra/inequalities/linineq_.pl
@@ -9,6 +9,7 @@ title = Résoudre une inéquation linéaire
 param.otherside = constant
 
 before ==
+from plrandom import sampleint
 from sympy import S, solveset
 a, c = sample(list(range(-6,-1)) + list(range(2,7)), 2)
 b, d = sample(list(range(-6,0)) + list(range(1,7)), 2)
