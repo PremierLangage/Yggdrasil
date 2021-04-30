176fe5b31c83cbb43eb9d52c9e5fd85b92f66ad2
David DOYEN <David.Doyen@u-pem.fr>
2021-05-01 01:06:09 +0200

    ddoyen modified Yggdrasil/utils/plcsv.py

---
+++ utils/plcsv.py
@@ -0,0 +1,34 @@
+import random
+from io import StringIO
+from csv import DictReader
+rd = random.Random()
+
+
+def csv_choice(f, delimiter=','):
+    """
+    Return a random row from a csv file/string (as a dictionary)
+    """
+
+    if isinstance(f, str):
+        return rd.choice(list(DictReader(StringIO(f), delimiter=delimiter)), k)
+    else:
+        return rd.choice(list(DictReader(f, delimiter=delimiter)))
+
+def csv_sample(f, k, delimiter=','):
+    """
+    Return a random sample from a csv file/string (as a list of dictionaries)
+    """
+    if isinstance(f, str):
+        return rd.sample(list(DictReader(StringIO(f), delimiter=delimiter)), k)
+    else:
+        return rd.sample(list(DictReader(f, delimiter=delimiter)), k)
+
+def csv_col(f, col, delimiter=','):
+    """
+    Return data in sample from a csv file/string (as a list of dictionaries)
+    """
+    if isinstance(f, str):
+        reader = DictReader(StringIO(f), delimiter=delimiter))
+    else:
+        reader = DictReader(f, delimiter=delimiter))
+    return [row[col] for row in reader]
