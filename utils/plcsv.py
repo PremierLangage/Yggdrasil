4df7c3267d43afc04139ed28c9d3c91aa8f57422
David DOYEN <David.Doyen@u-pem.fr>
2021-05-01 01:23:07 +0200

    ddoyen modified Yggdrasil/utils/plcsv.py

--- utils/plcsv.py
+++ utils/plcsv.py
@@ -31,4 +31,4 @@ def csv_col(f, col, delimiter=','):
         reader = DictReader(StringIO(f), delimiter=delimiter)
     else:
         reader = DictReader(f, delimiter=delimiter)
-    return [len(row) for row in reader]
+    return [row[col] for row in reader]
