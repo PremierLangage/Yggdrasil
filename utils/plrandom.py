22f198954f7c49e5d2795eeefe97de183c24479c
David DOYEN <David.Doyen@u-pem.fr>
2021-05-01 15:08:20 +0200

    ddoyen modified Yggdrasil/utils/plrandom.py

--- utils/plrandom.py
+++ utils/plrandom.py
@@ -1,6 +1,4 @@
 import random
-from io import StringIO
-from csv import DictReader
 rd = random.Random()
 
 def randint(a, b, excval=[]):
@@ -24,23 +22,3 @@ def sampleint(a, b, k, excval=[]):
     for i in range(len(bound)-1):
         lst.extend(list(range(bound[i], bound[i+1])))
     return rd.sample(lst, k)
-
-
-def csv_choice(f, delimiter=','):
-    """
-    Return a random row from a csv file/string (as a dictionary)
-    """
-
-    if isinstance(f, str):
-        return rd.choice(list(DictReader(StringIO(f), delimiter=delimiter)), k)
-    else:
-        return rd.choice(list(DictReader(f, delimiter=delimiter)))
-
-def csv_sample(f, k, delimiter=','):
-    """
-    Return a random sample from a csv file/string (as a list of dictionaries)
-    """
-    if isinstance(f, str):
-        return rd.sample(list(DictReader(StringIO(f), delimiter=delimiter)), k)
-    else:
-        return rd.sample(list(DictReader(f, delimiter=delimiter)), k)
