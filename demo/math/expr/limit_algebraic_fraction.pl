8450de2db6dd93c69bc842546c5cc57db3bafac4
David DOYEN <David.Doyen@u-pem.fr>
2021-05-02 19:15:21 +0200

    ddoyen modified Yggdrasil/demo/math/expr/limit_algebraic_fraction.pl

--- demo/math/expr/limit_algebraic_fraction.pl
+++ demo/math/expr/limit_algebraic_fraction.pl
@@ -27,7 +27,7 @@ input_prefix = $! \displaystyle {{ lim|latex }} = !$
 form ==
 {{input_prefix}}
 <span id="math-field" style="font-size:14pt;padding: 0.2em;"></span>
-  <button class="btn btn-outline-dark" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  <button class="btn btn-sm btn-outline-dark" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
    <i class="fas fa-square-root-alt"></i>
   </button>
   <div class="dropdown-menu">
