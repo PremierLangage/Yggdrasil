79fbd3b152f8db0bd107eeee219cd3441d449591
David DOYEN <David.Doyen@u-pem.fr>
2021-05-02 21:25:59 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -25,7 +25,7 @@ form ==
 {{input_prefix}}
 <span id="math-field" style="min-width: 5em;font-size:14pt;padding: 0.2em;"></span>
 {% if keypad|length > 0 %}
-<button class="btn btn-sm btn-outline-primary" style="margin-left: 0.5em" type="button" id="dropdownMenuButton" aria-haspopup="true" aria-expanded="false">
+<button class="btn btn-sm btn-outline-primary" style="margin-left: 0.5em" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 <math><msqrt><mi>x</mi></msqrt></math>
 </button>
 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
