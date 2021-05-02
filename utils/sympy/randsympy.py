8ff13dff76b0d71f9e0661dd2f46a569e9dcdb62
David DOYEN <David.Doyen@u-pem.fr>
2021-05-02 16:19:04 +0200

    ddoyen modified Yggdrasil/utils/sympy/randsympy.py

--- utils/sympy/randsympy.py
+++ utils/sympy/randsympy.py
@@ -1,69 +1,6 @@
 import random as rd
 import sympy as sp
 
-def randitem(items,excluded_values=[]):
-    """
-    Pick a random item from a list.
-    """
-    item=rd.choice(items)
-    while item in excluded_values:
-        item=rd.choice(items)
-    return item
-
-def _list_rand0(n,items,replace,excluded_values):
-    """
-    Generate a list of random items selected from a list.
-    """
-    if replace==True:
-        lst=[]
-        while len(lst)<n:
-            item=rd.choice(items)
-            if item not in excluded_values:
-                lst.append(item)
-    elif replace==False:
-        m=len(excluded_values)
-        lst=rd.sample(items,n+m)
-        if m>0:
-            lst=[x for x in lst if x not in excluded_values]
-            lst=lst[0:n]
-    else:
-        raise ValueError
-    return lst
-    
-def list_randitem(n,items,excluded_values=[]):
-    """
-    Generate a list of random items selected from a list with replacements.
-    """
-    return _list_rand0(n,items,True,excluded_values)
-
-def list_randitem_norep(n,items,excluded_values=[]):
-    """
-    Generate a list of random items selected from a list without replacements.
-    """
-    return _list_rand0(n,items,False,excluded_values)
-
-# Integers
-
-def randint(a,b,excluded_values=[]):
-    """
-    Pick a random integer.
-    """
-    item=rd.choice(range(a,b+1))
-    while item in excluded_values:
-        item=rd.choice(range(a,b+1))
-    return item
-
-def list_randint(n,a,b,excluded_values=[]):
-    """
-    Generate a list of random integers with replacements.
-    """
-    return _list_rand0(n,range(a,b+1),True,excluded_values)
-
-def list_randint_norep(n,a,b,excluded_values=[]):
-    """
-    Generate a list of random integers without replacements.
-    """
-    return _list_rand0(n,range(a,b+1),False,excluded_values)
 
 # Complex numbers
 
@@ -91,8 +28,8 @@ def randint_poly(d, nc, bound, var='x'):
     Generate a random polynomial with integer coefficients.
     """
     x = sp.Symbol(var)
-    c = list_randint(nc, -bound, bound, [0])
-    p = [d] + list_randint_norep(nc-1, 0, d-1)
+    c = sampleint(-bound, bound, nc, [0])
+    p = [d] + sampleint(0, d-1, nc-1)
     return sum([c[i]*x**p[i] for i in range(nc)])
 
 # Matrices
