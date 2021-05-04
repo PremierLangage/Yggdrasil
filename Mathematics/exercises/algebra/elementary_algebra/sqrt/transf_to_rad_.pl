3e5ba076149b91d5e83550c843e55927e3afe923
David DOYEN <David.Doyen@u-pem.fr>
2021-05-04 23:35:16 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/sqrt/transf_to_rad_.pl

--- Mathematics/exercises/algebra/elementary_algebra/sqrt/transf_to_rad_.pl
+++ Mathematics/exercises/algebra/elementary_algebra/sqrt/transf_to_rad_.pl
@@ -25,5 +25,5 @@ text ==
 Ecrire l'expression suivante sous la forme  $% \sqrt{a} %$, où $% a %$ est un entier.
 ==
 
-input_prefix = $! {{expr}} = !$
-input_embed = \sqrt{\MathQuillMathField{ }}
+input_prefix =
+input_embed = {{expr}} = \sqrt{\MathQuillMathField{ }}
