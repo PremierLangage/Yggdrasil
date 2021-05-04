30ac3558a9bd6cc5b76a51ca11adcf6404c9511e
David DOYEN <David.Doyen@u-pem.fr>
2021-05-04 17:10:21 +0200

    ddoyen modified Yggdrasil/demo/math/input/test.pl

---
+++ demo/math/input/test.pl
@@ -0,0 +1,106 @@
+extends = /model/math/math.pl
+
+jinja_keys % ["text", "solution", "input_prefix"]
+
+input_prefix = Réponse :
+
+before =
+
+text =
+
+title =
+
+form ==
+
+==
+
+evaluator ==
+score = 100
+feedback = "blabla"
+==
+
+keypad % []
+
+form ==
+{{input_prefix}}
+<span id="fill-in-the-blank">\sqrt{ \MathQuillMathField{x}^2 + \MathQuillMathField{y}^2 }</span>
+<script>
+  var fillInTheBlank = MQ.StaticMath(document.getElementById('fill-in-the-blank'));
+  fillInTheBlank.innerFields[0].latex() // => 'x'
+  fillInTheBlank.innerFields[1].latex() // => 'y'
+</script>
+
+<span id="math-field" style="min-width: 5em;font-size:14pt;padding: 0.2em;"></span>
+{% if keypad|length > 0 %}
+  <button type="button" class="btn btn-sm btn-outline-primary" style="margin-left: 0.5em" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+<math><msqrt><mi>x</mi></msqrt></math>
+  </button>
+  <div class="dropdown-menu">
+{% for item in keypad %}
+<button class="dropdown-item" onclick="mathField.{{ item.action }}('{{ item.value }}');mathField.focus()">{{ item.label }}</a>
+{% endfor %}
+  </div>
+{% endif %}
+
+
+<input type="text" id="form_math" hidden=true>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1/mathquill.min.css">
+==
+
+
+# HACK : Ce script JS permet de modifier la liste des boutons de contrôle.
+extrajs ==
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1-b/mathquill.min.js" type="text/javascript"></script>
+<script>
+var MQ = MathQuill.getInterface(2);
+var mathFieldSpan = document.getElementById('math-field');
+var latexSpan = document.getElementById('form_math');
+var mathField = MQ.MathField(mathFieldSpan, {
+  charsThatBreakOutOfSupSub: '+-=<>',
+  autoCommands: 'pi theta sqrt sum infty infin emptyset alpha',
+  autoOperatorNames: 'sin cos ln exp',
+  handlers: {
+    edit: function() { // useful event handlers
+      latexSpan.value = mathField.latex(); // simple API
+    }
+  }
+});
+mathField.latex("{{ prev_value }}");
+</script>
+<script>
+    function onReadyPL(nodes) {
+        const actions = nodes.actions;
+        actions.find('.action-save').hide();
+        actions.find('.action-reset').hide();
+        actions.find('.action-next').hide();
+
+        const { origin, pathname }  = document.location;
+        const link = origin + pathname;
+
+        const buttons = actions.find('.btn-group');
+
+        {% if "reroll" in internals.buttons %}
+        buttons.append(`
+            <a type="button" class="btn btn-warning action-reroll" href="`+link+`?action=reroll">
+                <i class="fas fa-dice"></i> Nouveau tirage
+            </a>
+        `);
+        {% endif %}
+        
+        {% if not "submit" in internals.buttons %}
+        actions.find('.action-submit').hide();
+        {% endif %}
+    }
+
+    function onBeforeSubmitPL() {
+    latexSpan.value = mathField.latex();
+    return true;
+}
+</script>
+==
+
+settings.feedback = rightwrong
+
+settings.maxattempt % 1
+
+extends = messages_math.pl
