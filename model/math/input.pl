a002b7b45130a1e930278cbf9b731112e0955a8f
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 01:36:51 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -63,7 +63,14 @@ javascript.mathquill ==
  MathJax = {
    tex: {
      inlineMath: [['$', '$'], ['$!', '!$']]
-   }
+   },
+  startup: {
+    ready: () => {
+      console.log('MathJax is loaded, but not yet initialized');
+      MathJax.startup.defaultReady();
+      console.log('MathJax is initialized, and the initial typeset is queued');
+    }
+  }
  };
 </script>
 
