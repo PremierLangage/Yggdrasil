929947769fec84c7430872744ff74c16399d79fa
David DOYEN <David.Doyen@u-pem.fr>
2021-05-07 17:09:02 +0200

    ddoyen modified Yggdrasil/demo/math/expr/limit_algebraic_fraction.pl

--- demo/math/expr/limit_algebraic_fraction.pl
+++ demo/math/expr/limit_algebraic_fraction.pl
@@ -21,6 +21,6 @@ text ==
 Déterminer la limite suivante.
 ==
 
-input_prefix2 =
+input_prefix = {{ lim|latex }} =
 
 keypad % [{"label": "$! +\\infty !$", "action": "write", "value": "+\\\\infty"}, {"label": "$! -\\infty !$", "action": "write", "value": "-\\\\infty"}]
