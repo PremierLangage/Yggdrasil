b704fa81180765db4c6079f6fd3b29a1454fed8b
David DOYEN <David.Doyen@u-pem.fr>
2021-05-07 20:29:19 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -42,16 +42,7 @@ form ==
 {% endfor %}
   </div>
 {% endif %}
-<div class="btn-group">
-  <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false">
-    Left-aligned but right aligned when large screen
-  </button>
-  <ul class="dropdown-menu dropdown-menu-lg-end">
-    <li><button class="dropdown-item" type="button">Action</button></li>
-    <li><button class="dropdown-item" type="button">Another action</button></li>
-    <li><button class="dropdown-item" type="button">Something else here</button></li>
-  </ul>
-</div>
+
 <input type="text" id="form_math" hidden=true>
 ==
 
