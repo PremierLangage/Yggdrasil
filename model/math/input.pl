a0b8a696c8f462fc4a8fd0e5b08d23c73b14b3b8
David DOYEN <David.Doyen@u-pem.fr>
2021-05-07 20:41:14 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -37,7 +37,7 @@ form ==
   <button type="button" class="btn btn-sm btn-outline-primary" style="margin-left: 0.5em" data-bs-toggle="dropdown" aria-expanded="false">
 <math><msqrt><mi>x</mi></msqrt></math>
   </button>
-  <div class="dropdown-menu dropdown-menu-end" style="max-width: 200px;">
+  <div class="dropdown-menu dropdown-menu-end" style="max-width: 100px;">
 {% for item in keypad %}
 <button class="btn btn-sm btn-outline-secondary" onclick="mathField.{{ item.action }}('{{ item.value }}');mathField.focus()">{{ item.label }}</a>
 {% endfor %}
