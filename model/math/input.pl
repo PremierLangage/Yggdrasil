21961bf8e7c2e5fbf279fe6971c7ded6378af7cc
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 01:16:32 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -59,15 +59,16 @@ style.mathquill ==
 javascript.mathquill ==
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 
+
+<script type="text/javascript" id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js">
+</script>
 <script>
 MathJax = {
   tex: {
     inlineMath: [['$', '$'], ['$!', '!$']]
   }
 };
-</script>  
-<script type="text/javascript" id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js">
-</script>
+</script> 
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1-b/mathquill.min.js" type="text/javascript"></script>
 <script>
@@ -121,6 +122,7 @@ return true;
         {% if not "submit" in internals.buttons %}
         actions.find('.action-submit').hide();
         {% endif %}
+        mathField.reflow()
     }
 </script>
 ==
