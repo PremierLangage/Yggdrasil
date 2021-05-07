1f897009628ffb680a4bfa383cb7020c6e22f993
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 01:57:53 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -59,12 +59,13 @@ style.mathquill ==
 javascript.mathquill ==
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 
-<script type="text/javascript">
-MathJax = {
-    tex2jax: {
-      inlineMath: [ ['$','$'], ["$!","!$"] ]
-    }
-  };
+<script type="text/x-mathjax-config">
+    MathJax.Hub.Config({
+        tex2jax: {
+            inlineMath: [['$','$'], ['$!', '!$']],
+            skipTags: ["script", "noscript", "style"],
+        }
+    });
 </script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1-b/mathquill.min.js" type="text/javascript"></script>
