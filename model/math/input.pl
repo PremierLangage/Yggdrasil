270b7089056dbba1a7d3c8544d5dfafdf1c7a1ad
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 01:53:50 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -64,7 +64,6 @@ MathJax = {
    tex: {
      inlineMath: [['$', '$'], ['$!', '!$']]
    }
-
  };
 </script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
