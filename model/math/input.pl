3cdc8395043f86f7e43fa8dc981e45f9047b57c1
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 23:21:03 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -45,7 +45,7 @@ modal ==
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h4 class="modal-title" id="exampleModalLabel">{{ modaltext(score) }}</h4>
+        <h3 class="modal-title" id="exampleModalLabel">{{ modaltext(score) }}</h3>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
