17e72f94e1f05f45d5b3a779df062150d94d5e0a
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 17:24:33 +0200

    ddoyen modified Yggdrasil/model/math/input_exp.pl

--- model/math/input_exp.pl
+++ model/math/input_exp.pl
@@ -21,8 +21,8 @@ text ==
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
-      <div class="modal-header alert-success">
-        <h5 class="modal-title" id="staticBackdropLabel">Bonne réponse </h5>
+      <div class="modal-header {{ modalclass }}">
+        <h5 class="modal-title" id="staticBackdropLabel"> {{ modaltext }} </h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 
