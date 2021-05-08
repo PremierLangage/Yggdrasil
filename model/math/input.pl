6ff15a386b15e2e230a7da09512fc4ebee28a0fb
David DOYEN <David.Doyen@u-pem.fr>
2021-05-09 00:04:20 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -166,7 +166,7 @@ return true;
         mathField.reflow();
         {% endif %}
         {% if score == -1 %}
-          $('#math-field').popover({content: "{{ feedback }}"}); 
+          $('#math-field').popover({content: "{{ feedback }}", container: ".parent"}); 
           $('#math-field').popover('show'); 
         {% endif %}
 
