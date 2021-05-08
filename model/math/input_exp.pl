ce37d0a3d090b0f223ee2013467462865758e484
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 18:34:04 +0200

    ddoyen modified Yggdrasil/model/math/input_exp.pl

--- model/math/input_exp.pl
+++ model/math/input_exp.pl
@@ -17,7 +17,9 @@ text ==
     Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.
   </div>
 </div>
+==
 
+modal ==
 {% macro modaltext(score) -%}
     {% if score == 100 %}
         Bonne réponse !
