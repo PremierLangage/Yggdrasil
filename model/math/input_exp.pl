ad92f18e63babe650e97d0a284f16173cb28eecd
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 17:12:54 +0200

    ddoyen modified Yggdrasil/model/math/input_exp.pl

--- model/math/input_exp.pl
+++ model/math/input_exp.pl
@@ -18,7 +18,7 @@ text ==
   </div>
 </div>
 
-<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
