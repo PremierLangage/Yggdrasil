d9ae3a2cd2c4c392eb534426f73cc3e61a4614f6
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 14:18:07 +0200

    ddoyen modified Yggdrasil/model/math/input_exp.pl

--- model/math/input_exp.pl
+++ model/math/input_exp.pl
@@ -7,7 +7,7 @@ input_prefix = Réponse :
 before =
 
 text ==
-<div class="alert alert-warning alert-dismissible fade show" id="exampleModal" role="alert">
+<div class="modal alert alert-warning alert-dismissible fade show" id="exampleModal" role="alert">
   <strong>Holy guacamole!</strong> You should check in on some of those fields below.
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
