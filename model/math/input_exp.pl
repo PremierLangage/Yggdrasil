a6306489fc70836c9aa5f78f355b87a8cd489015
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 14:11:14 +0200

    ddoyen modified Yggdrasil/model/math/input_exp.pl

--- model/math/input_exp.pl
+++ model/math/input_exp.pl
@@ -86,7 +86,7 @@ javascript.mathquill ==
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
 <script>
 function onAfterSubmitPL(nodes) {
-var myModal = new bootstrap.Modal(document.getElementById('exampleModal', {backdrop: false});
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {backdrop: false});
 myModal.show();
 }
 </script>
