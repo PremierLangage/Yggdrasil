f133143e90ccf4e3ec91eec44a789e571a73d076
David DOYEN <David.Doyen@u-pem.fr>
2021-05-08 14:12:01 +0200

    ddoyen modified Yggdrasil/model/math/input_exp.pl

--- model/math/input_exp.pl
+++ model/math/input_exp.pl
@@ -7,12 +7,6 @@ input_prefix = Réponse :
 before =
 
 text ==
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -21,11 +15,7 @@ text ==
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        Bonne réponse !
       </div>
     </div>
   </div>
