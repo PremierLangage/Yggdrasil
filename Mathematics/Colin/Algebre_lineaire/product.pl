40cdafdd1a410a20ad25d80064addafa623ec9f8
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2021-05-09 10:12:51 +0200

    cpetitjean modified Yggdrasil/Mathematics/Colin/Algebre_lineaire/product.pl

--- Mathematics/Colin/Algebre_lineaire/product.pl
+++ Mathematics/Colin/Algebre_lineaire/product.pl
@@ -1,16 +1,14 @@
-extends = /model/math/tuple.pl
-
+extends = /model/math/mathmatrix.pl
 title = Image par une application de Z^2 dans Z^2
 
 before ==
-n0 = randint(-5, 5)
-p0 = randint(-5, 5)
-sol = Tuple(n0+p0, n0-p0)
+A = rand_int_matrix(2, 2, 3)
+B = rand_int_matrix(2, 2, 3)
+sol = A*B
 ==
 
 text ==
-On considère la fonction $! f : \mathbb{Z}^2 \rightarrow \mathbb{Z}^2  !$  définie par :
-$$ f(n, p)= (n+p, n-p) .$$
-
-Déterminer $! f( {{ n0 }}, {{ p0 }} ). !$
+Soit les matrices
+$$ A = \left( {{A|latex}}\right) \text{ et } B = \left({{B|latex}}\right) $$ 
+Calculer $! A B !$.
 ==
