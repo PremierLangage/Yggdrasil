23ea07a54f4d8b9d1f71e365f709ae3570399795
David DOYEN <David.Doyen@u-pem.fr>
2021-05-09 18:49:17 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -160,7 +160,7 @@ return true;
         {% endif %}
         
         {% if not "submit" in internals.buttons %}
-        actions.find('.action-submit').addClass("hilite");
+        actions.find('.action-submit').addClass("disabled");
         {% endif %}
         {% if input_embed|length > 0 %}
         mathField.reflow();
