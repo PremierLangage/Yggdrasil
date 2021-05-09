99c4f5e1f8fb7009231d36bf942b5b2e090a6cfb
David DOYEN <David.Doyen@u-pem.fr>
2021-05-09 21:57:50 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -174,7 +174,7 @@ return true;
           //actions.find('.action-submit').popover('show');  
         {% endif %}
 
-        toolbar.insertAdjacentHTML('afterend', '<div id="two">two</div>');  
+        //toolbar.insertAdjacentHTML('afterend', '<div id="two">two</div>');  
 
     }
 </script>
