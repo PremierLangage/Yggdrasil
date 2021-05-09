627e0639d882de92b6249ee07b9272612f825e17
David DOYEN <David.Doyen@u-pem.fr>
2021-05-09 21:52:39 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -177,7 +177,7 @@ return true;
         var para = document.createElement("P");                       // Create a <p> node
         var t = document.createTextNode("This is a paragraph.");      // Create a text node
         para.appendChild(t);                                          // Append the text to <p>
-        toolbar.appendChild(para);  
+        actions.appendChild(para);  
 
     }
 </script>
