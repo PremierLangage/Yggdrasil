1e8d91b417dd132005c897b392679aa474f6e389
David DOYEN <David.Doyen@u-pem.fr>
2021-05-10 12:59:38 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -122,7 +122,7 @@ return true;
         mathField.reflow();
         {% endif %}
         actions.prepend('<hr class="border">');
-        $('br', '.actions').remove();​
+        actions.find('br').remove();
         {% if score == 100 %}
         actions.append('<button type="button" style="float: right;" class="btn success-state animated pulse">Score : {{score}} </button>');
         {% endif %}
