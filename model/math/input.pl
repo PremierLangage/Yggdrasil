cdc35f0c54d6c88dbf3b2ec0f033bd3daf6be8d0
David DOYEN <David.Doyen@u-pem.fr>
2021-05-10 15:35:33 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -132,9 +132,10 @@ return true;
 ==
 
 format_feedback ==
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-  {{ feedback }}
-  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+<div class="alert alert-light alert-dismissible fade show" role="alert">
+  {{ feedback }}  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
 </div>
 ==
 
