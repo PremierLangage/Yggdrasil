1a56fcd7556625d1cb03923d0394813742e2984d
David DOYEN <David.Doyen@u-pem.fr>
2021-05-10 02:49:35 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -177,6 +177,8 @@ return true;
           //actions.find('.action-submit').popover({content: "Coucou !", placement: "top"}); 
           //actions.find('.action-submit').popover('show');  
         {% endif %}
+        actions[0].insertAdjacentHTML('beforeend', '<button type="button" style="float: right;" class="btn btn-outline-secondary">Score : - </button>');
+
         {% if score == 100 %}
           actions[0].insertAdjacentHTML('beforeend', '<button type="button" style="float: right;" class="btn btn-outline-secondary success-state animated pulse">Score : {{score}}</button>');
         {% endif %}
