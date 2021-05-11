8138ce050b84292b15a3ab75fdd6a88314638573
David DOYEN <David.Doyen@u-pem.fr>
2021-05-11 21:05:24 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/testparser.pl

--- Mathematics/exercises/testcomp/testparser.pl
+++ Mathematics/exercises/testcomp/testparser.pl
@@ -3,18 +3,21 @@ grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 before== #|python|
+b = a + a 
 ==
 
 evaluator== #|python|
 grade = (100, 'OK')
 ==
 
-
+#
+a ={int} 3
 
 title== #|html|
 ==
 
 text == #|html|
+{{ b }}
 ==
 
 form == #|html|
