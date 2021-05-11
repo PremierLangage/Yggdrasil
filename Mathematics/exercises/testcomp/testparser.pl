9ca1fc44dc2bf0a6d0938272ffb269c3bf699545
David DOYEN <David.Doyen@u-pem.fr>
2021-05-11 20:54:32 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/testparser.pl

---
+++ Mathematics/exercises/testcomp/testparser.pl
@@ -0,0 +1,15 @@
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+before== #|python|
+==
+
+evaluator== #|python|
+grade = (100, 'OK')
+==
+
+
+
+title== #|html|
+==
