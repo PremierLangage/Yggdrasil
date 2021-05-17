7e00d8e35d8ae1edbad5e627f493fa59663658f8
David DOYEN <David.Doyen@u-pem.fr>
2021-05-17 20:43:54 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/absolute_value/abs_simplification_diff_number.pl

--- Mathematics/exercises/algebra/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
+++ Mathematics/exercises/algebra/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
@@ -1,25 +1,4 @@
-extends = /model/basic/checkbox.pl
-extends = /model/math/importfunc.pl
-
-@ /utils/sympy/evalsympy.py
-@ /utils/sympy/latex2sympy.py
-@ /utils/sympy/sympy2latex.py
-@ /utils/sympy/randsympy.py
-@ /utils/sympy/mplsympy.py
-@ /utils/plrandom.py
-
-importfunc ==
-from sympy import E, I, pi, oo
-from sympy import sqrt, Abs, sin, cos, tan, exp, ln
-from sympy import var, symbols, Symbol
-from sympy import sympify, simplify, Lambda
-from sympy import Integer, Rational, Poly, FiniteSet, Tuple
-from random import choice, choices, sample, shuffle
-from plrandom import randint, sampleint
-from sympy2latex import latex
-from latex2sympy import latex2sympy
-==
-
+extends = /model/math/checkbox.pl
 
 title = Simplification de valeurs abolues
 
