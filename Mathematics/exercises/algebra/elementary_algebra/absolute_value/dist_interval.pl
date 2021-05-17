b9a9b806a1c1da13472d214468ff565d2c2cbad0
David DOYEN <David.Doyen@u-pem.fr>
2021-05-17 22:05:37 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/absolute_value/dist_interval.pl

--- Mathematics/exercises/algebra/elementary_algebra/absolute_value/dist_interval.pl
+++ Mathematics/exercises/algebra/elementary_algebra/absolute_value/dist_interval.pl
@@ -2,31 +2,17 @@ extends = /Mathematics/template/mathexpr.pl
 
 title = Inéquation avec valeur absolue
 
-lang = fr
-
 before ==
-keyboards_JSON['virtualKeyboards']="elementary"
-input1.config = keyboards_JSON
-
-a=randint(-6,6,[0])
-d=randint(1,5)
+from sympy import
+a = randint(-6, 6, [0])
+d = randint(1, 5)
 var('x')
-expr=Abs(x-a)
-
-ineq=randitem([expr<=d,expr<d])
-ineq_tex = latex(ineq)
-sol=solveset(ineq,x,domain=S.Reals)
-sol_tex = latex(sol)
+expr = Abs(x-a)
+ineq = choice([expr <= d, expr < d])
+sol = solveset(ineq, x, domain=S.Reals)
 ==
 
 text ==
-Déterminer l'ensemble des réels $% x %$ tels que $$ {{ineq_tex}}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
+Déterminer l'ensemble des réels $% x %$ tels que $$ {{ineq|latex}}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
 ==
 
-evaluator==
-score,_,feedback=ans_rset(input1.value,sol)
-==
-
-solution ==
-La solution est $% {{sol_tex}} %$.
-==
