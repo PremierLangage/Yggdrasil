f7d8e450662dfbd62ebd1a41721f5888e107a595
David DOYEN <David.Doyen@u-pem.fr>
2021-05-17 09:34:25 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/absolute_value/abs_simplification_diff_number.pl

--- Mathematics/exercises/algebra/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
+++ Mathematics/exercises/algebra/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
@@ -18,6 +18,7 @@ from latex2sympy import latex2sympy
 @ /utils/sympy/sympy2latex.py
 
 before ==
+from sympy import floor
 lst = [choice([2*pi,pi]),choice([2*sqrt(2),sqrt(2)]),choice([sqrt(3),2*sqrt(3)])]
 shuffle(lst)
 indsol = sampleint(0, 3, randint(1, 2))
