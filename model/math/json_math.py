adb8d143d9f91656ca27c0532a29c80375c7e27c
David DOYEN <David.Doyen@u-pem.fr>
2021-05-18 21:44:24 +0200

    ddoyen modified Yggdrasil/model/math/json_math.py

---
+++ model/math/json_math.py
@@ -0,0 +1,19 @@
+import json, jsonpickle
+from sympy import srepr, Basic, Matrix
+
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, (Basic, Matrix)):
+            return {'__SymPy__': True, 'srepr': srepr(obj)}
+        return jsonpickle.Pickler(unpicklable=False).flatten(obj)
+
+class JSONDecoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, dict):
+        if '__SymPy__' in dict:
+            return sympy.sympify(dict['srepr'], locals=namespace, evaluate=False)
+        return dict
