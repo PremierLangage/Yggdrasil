67f8eacaab6c5a71e995eda51a260fbb9126b3a6
David DOYEN <David.Doyen@u-pem.fr>
2021-05-18 09:05:41 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl

--- Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl
+++ Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl
@@ -2,7 +2,7 @@
 # Tags: inequalities
 # 19/8/2020
 
-extends = /model/mathinput.pl
+extends = /model/math/input.pl
 
 title = Encadrement de $% a x + b %$
 
@@ -11,35 +11,19 @@ before ==
 var('x')
 a=randint(-3,3,[0])
 b=randint(-8,8,[0])
-x1,x2=sorted(list_randint_norep(2,-8,8))
-s1,s2=list_randitem(2,['<','<='])
+x1, x2 =sorted(sampleint(-8, 8, 2))
+s1, s2 =choices(['<','<='], 2)
 
 if a>0:
-    sol=[a*x1+b,s1,a*x+b,s2,a*x2+b]
+    sol=[a*x1+b, s1, a*x+b, s2, a*x2+b]
 else:
-    sol=[a*x2+b,s2,a*x+b,s1,a*x1+b]
+    sol=[a*x2+b, s2, a*x+b, s1, a*x1+b]
 
-ineq = latex_ineq([x1,s1,x,s2,x2])
+#ineq = latex_ineq([x1,s1,x,s2,x2])
 expr = latex(a*x+b)
-input.value = expr
 ==
 
-input.virtualKeyboards = relations
 
 text ==
 Soit un nombre $% x %$ tel que $%{{ineq}} %$. Déterminer l'encadrement le plus précis possible de $% {{expr}} %$.
 ==
-
-evaluator==
-score, error = eval_chainineq(input.value,sol)
-feedback = message[error]
-==
-
-
-
-
-
-
-
-
-
