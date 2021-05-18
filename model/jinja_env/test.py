4db2e718b85cee84a3e142cecc3fe72f6facebff
David DOYEN <David.Doyen@u-pem.fr>
2021-05-18 17:30:29 +0200

    ddoyen modified Yggdrasil/model/jinja_env/test.py

---
+++ model/jinja_env/test.py
@@ -0,0 +1,17 @@
+from jinja2 import Environment, BaseLoader
+
+def component(l):
+    if isinstance(l, dict):
+        selector = l["selector"]
+        cid = l["cid"]
+    else:
+        selector = l.selector
+        cid = l.cid
+    return "<%s cid='%s'></%s>" % (selector, cid, selector)
+
+def customlatex(expr):
+    return latex(expr, **latexsettings)
+
+Env = Environment(loader=BaseLoader())
+Env.filters["component"] = component
+Env.filters["latex"] = customlatex
