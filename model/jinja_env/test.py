82ac943afe1185144ff93921418197dded927557
David DOYEN <David.Doyen@u-pem.fr>
2021-05-18 17:52:28 +0200

    ddoyen modified Yggdrasil/model/jinja_env/test.py

--- model/jinja_env/test.py
+++ model/jinja_env/test.py
@@ -12,7 +12,7 @@ def component(l):
 
 @contextfilter
 def customlatex(ctx, expr):
-    return latex(expr)
+    return ctx.latexsettings #latex(expr)
 
 Env = Environment(loader=BaseLoader())
 Env.filters["component"] = component
