c1b4b2a424df84c60bc643e5fc9a662d5d915c2f
David DOYEN <David.Doyen@u-pem.fr>
2021-05-21 00:45:12 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/test.pl

--- Mathematics/exercises/testcomp/test.pl
+++ Mathematics/exercises/testcomp/test.pl
@@ -22,53 +22,14 @@ form== #|html|
     <i class="fas fa-check"></i>
     <span class="ion-hide-md-down">Valider</span>
 </button>
-<ul class="stepper linear">
-  <li class="step active">
-    <div data-step-label="Type something" class="step-title waves-effect waves-dark">Step 1</div>
-    <div class="step-new-content">
-      <div class="row">
-        <div class="md-form col-12 ml-auto">
-          <input id="email-linear" type="email" class="form-control validate" required>
-          <label for="email-linear">Your e-mail</label>
-        </div>
-      </div>
-      <div class="step-actions">
-        <button class="waves-effect waves-dark btn btn-sm btn-primary next-step">CONTINUE</button>
-      </div>
-    </div>
-  </li>
-  <li class="step">
-    <div class="step-title waves-effect waves-dark">Step 2</div>
-    <div class="step-new-content">
-      <div class="row">
-        <div class="md-form col-12 ml-auto">
-          <input id="password-linear" type="password" class="form-control validate" required>
-          <label for="password-linear">Your password</label>
-        </div>
-      </div>
-      <div class="step-actions">
-        <button class="waves-effect waves-dark btn btn-sm btn-primary next-step">CONTINUE</button>
-        <button class="waves-effect waves-dark btn btn-sm btn-secondary previous-step">BACK</button>
-      </div>
-    </div>
-  </li>
-  <li class="step">
-    <div class="step-title waves-effect waves-dark">Step 3</div>
-    <div class="step-new-content">
-      Finish!
-      <div class="step-actions">
-        <button class="waves-effect waves-dark btn btn-sm btn-primary m-0 mt-4" type="button">SUBMIT</button>
-      </div>
-    </div>
-  </li>
-</ul>
 
+<a class="text-success" style="display: block; margin-top: 1em;" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+  Solution
+</a>
+<div class="collapse" id="collapseExample">
+{{ solution }}
+</div>
 
-<script> 
-
-$('.stepper').mdbStepper();
-
-</script>
 ==
 
 
