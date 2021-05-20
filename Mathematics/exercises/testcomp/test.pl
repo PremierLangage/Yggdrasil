a50138f2645dfd7a0a53c75637a56947b7a0117c
David DOYEN <David.Doyen@u-pem.fr>
2021-05-21 00:15:39 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/test.pl

--- Mathematics/exercises/testcomp/test.pl
+++ Mathematics/exercises/testcomp/test.pl
@@ -18,17 +18,51 @@ text== #|html|
 ==
 
 form== #|html|
-<div class="mathfield" id='mathexpr'></div>
-<link rel="stylesheet" href="https://unpkg.com/mathlive/dist/mathlive.core.css">
-<link rel="stylesheet" href="https://unpkg.com/mathlive/dist/mathlive.css">
+<ul class="stepper linear">
+  <li class="step active">
+    <div data-step-label="Type something" class="step-title waves-effect waves-dark">Step 1</div>
+    <div class="step-new-content">
+      <div class="row">
+        <div class="md-form col-12 ml-auto">
+          <input id="email-linear" type="email" class="form-control validate" required>
+          <label for="email-linear">Your e-mail</label>
+        </div>
+      </div>
+      <div class="step-actions">
+        <button class="waves-effect waves-dark btn btn-sm btn-primary next-step">CONTINUE</button>
+      </div>
+    </div>
+  </li>
+  <li class="step">
+    <div class="step-title waves-effect waves-dark">Step 2</div>
+    <div class="step-new-content">
+      <div class="row">
+        <div class="md-form col-12 ml-auto">
+          <input id="password-linear" type="password" class="form-control validate" required>
+          <label for="password-linear">Your password</label>
+        </div>
+      </div>
+      <div class="step-actions">
+        <button class="waves-effect waves-dark btn btn-sm btn-primary next-step">CONTINUE</button>
+        <button class="waves-effect waves-dark btn btn-sm btn-secondary previous-step">BACK</button>
+      </div>
+    </div>
+  </li>
+  <li class="step">
+    <div class="step-title waves-effect waves-dark">Step 3</div>
+    <div class="step-new-content">
+      Finish!
+      <div class="step-actions">
+        <button class="waves-effect waves-dark btn btn-sm btn-primary m-0 mt-4" type="button">SUBMIT</button>
+      </div>
+    </div>
+  </li>
+</ul>
 
-<script type='module'> 
-    import MathLive from 'https://unpkg.com/mathlive/dist/mathlive.mjs';
-    const mf = MathLive.makeMathField('mathexpr', {
-        smartFence: false,
-        inlineShortcuts:{ '*': '\\times'},
-        virtualKeyboardMode: 'manual'
-    });
+<script> 
+$(document).ready(function () {
+$('.stepper').mdbStepper();
+})
 </script>
 ==
 
