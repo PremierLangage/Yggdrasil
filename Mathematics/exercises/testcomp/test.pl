98c9ac25351e0febc5720a2daf74205d0135bf49
David DOYEN <David.Doyen@u-pem.fr>
2021-05-22 18:07:53 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/test.pl

--- Mathematics/exercises/testcomp/test.pl
+++ Mathematics/exercises/testcomp/test.pl
@@ -47,18 +47,6 @@ jinja_keys % ["form", "text"]
 form == #|html|
 <div class="row">
   <div class="col">
- &nbsp; &nbsp; Si, en  entrant &agrave; Verri&egrave;res, le voyageur demande &agrave; qui appartient cette belle fabrique  de clous qui assourdit les gens qui montent la grande rue, on lui r&eacute;pond avec  un accent tra&icirc;nard&nbsp;: Eh&nbsp;! elle est &agrave; M. le maire.</p>
-      <p>
- &nbsp; &nbsp; Pour  peu que le voyageur s'arr&ecirc;te quelques instants dans cette grande rue de  Verri&egrave;res, qui va en montant depuis la rive du Doubs jusque vers le sommet de  la colline, il y a cent &agrave; parier contre un qu'il verra para&icirc;tre un grand homme  &agrave; l'air affair&eacute; et important.</p>
-      <p>
- &nbsp; &nbsp; &Agrave; son  aspect tous les chapeaux se l&egrave;vent rapidement. Ses cheveux sont grisonnants, et  il est v&ecirc;tu de gris. Il est chevalier de plusieurs ordres, il a un grand front,  un nez aquilin, et au total sa figure ne manque pas d'une certaine r&eacute;gularit&eacute;&nbsp;:  on trouve m&ecirc;me, au premier aspect, qu'elle r&eacute;unit &agrave; la dignit&eacute; du maire de  village cette sorte d'agr&eacute;ment qui peut encore se rencontrer avec quarante-huit  ou cinquante ans. Mais bient&ocirc;t le voyageur parisien est choqu&eacute; d'un certain air  de contentement de soi et de suffisance m&ecirc;l&eacute; &agrave; je ne sais quoi de born&eacute; et de  peu inventif. On sent enfin que le talent de cet homme-l&agrave; se borne &agrave; se faire  payer bien exactement ce qu'on lui doit, et &agrave; payer lui-m&ecirc;me le plus tard  possible quand il doit.</p>
-      <p>
- &nbsp; &nbsp; Tel est  le maire de Verri&egrave;res, M. de R&ecirc;nal. Apr&egrave;s avoir travers&eacute; la rue d'un pas grave,  il entre &agrave; la mairie et dispara&icirc;t aux yeux du voyageur. Mais, cent pas plus  haut, si celui-ci continue sa promenade, il aper&ccedil;oit une maison d'assez belle  apparence, et, &agrave; travers une grille de fer attenante &agrave; la maison, des jardins  magnifiques. Au-del&agrave; c'est une ligne d'horizon form&eacute;e par les collines de la  Bourgogne, et qui semble faite &agrave; souhait pour le plaisir des yeux. Cette vue  fait oublier au voyageur l'atmosph&egrave;re empest&eacute;e des petits int&eacute;r&ecirc;ts d'argent  dont il commence &agrave; &ecirc;tre asphyxi&eacute;.</p>
-      <p>
- &nbsp; &nbsp; On lui  apprend que cette maison appartient &agrave; M. de R&ecirc;nal. C'est aux b&eacute;n&eacute;fices qu'il a  faits sur sa grande fabrique de clous, que le maire de Verri&egrave;res doit cette  belle habitation en pierres de taille qu'il ach&egrave;ve en ce moment. Sa famille,  dit-on, est espagnole, antique, et, &agrave; ce qu'on pr&eacute;tend, &eacute;tablie dans le pays  bien avant la conqu&ecirc;te de Louis XIV.</p>
-      <p>
- &nbsp; &nbsp; Depuis  1815 il rougit d'&ecirc;tre industriel&nbsp;: 1815 l'a fait maire de Verri&egrave;res. Les murs  en terrasse qui soutiennent les diverses parties de ce magnifique jardin, qui,  d'&eacute;tage en &eacute;tage, descend jusqu'au Doubs, sont aussi la r&eacute;compense de la  science de M. de R&ecirc;nal dans le commerce du fer.<br />
-<br />
   
   </div>
   <div class="col">
