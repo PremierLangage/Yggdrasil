085a6649038e9264d895964f367eb1fdb018017a
David DOYEN <David.Doyen@u-pem.fr>
2021-05-23 11:44:14 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/test.pl

--- Mathematics/exercises/testcomp/test.pl
+++ Mathematics/exercises/testcomp/test.pl
@@ -45,6 +45,7 @@ step2.form ==
 jinja_keys % []
 
 form == #|html|
+{{steps}}
 {% for i in range(currentstep) %}
 <a class="text-success" style="display: block; margin-top: 1em;" data-toggle="collapse" href="#collapse{{ i }}" role="button" aria-expanded="true" aria-controls="collapse{{ i }}">
   Step {{ i + 1 }}
