c83b570cf0ef1ee62012ef85305460daa2b8be82
David DOYEN <David.Doyen@u-pem.fr>
2021-05-23 14:34:50 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/test.pl

--- Mathematics/exercises/testcomp/test.pl
+++ Mathematics/exercises/testcomp/test.pl
@@ -65,11 +65,10 @@ form == #|html|
 </p>
 {{ steps[i].form |safe }}
 
-{% if stepscore[i] %}
 {% if stepscore[i] == -1 %}
 <a class="text-info" style="display: block; margin-top: 1em;" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="true" aria-controls="collapseExample2">
 Attention !
-{% else %}
+{% elif stepscore[i] >= 0 and stepscore[i] < 100 %}
 <a class="text-danger" style="display: block; margin-top: 1em;" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="true" aria-controls="collapseExample2">
 Erreur
 {% endif %}
