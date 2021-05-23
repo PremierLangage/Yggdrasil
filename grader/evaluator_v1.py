630fd57af44bed11e1707340b5789c053b3e1694
David DOYEN <David.Doyen@u-pem.fr>
2021-05-23 16:25:30 +0200

    ddoyen modified Yggdrasil/grader/evaluator_v1.py

--- grader/evaluator_v1.py
+++ grader/evaluator_v1.py
@@ -1,71 +1,85 @@
-import sys
-import json
+import sys, json
 from components import Component
-from pljson import PickleEncoder
-from pljinja import DefaultEnv
+from builderlib import aux_component1, aux_component2, aux_component
+from jinja2 import Environment, BaseLoader
 
-# import the custom JSON encoder (if it exists)
-try:
-    from json_coder import JSONEncoder, JSONDecoder
-except ModuleNotFoundError:
-    JSONEncoder = PickleEncoder
-    JSONDecoder = None
+Env = Environment(loader=BaseLoader())
 
-# import the custom Jinja environnement (if it exists)
-try:
-    from jinja_env import Env
-except ModuleNotFoundError:
-    Env = DefaultEnv
+from builderlib import PickleEncoder as JSONEncoder
+from builderlib import PickleDecoder as JSONDecoder
 
-# import the custom namespace (if it exists)
-try:
-    from namespace import namespace
-except ModuleNotFoundError:
-    namespace = {}
+missing_evaluator_stderr = """\
+The key 'evaluator' was not found in the context.
+When using this grader, the PL must declare a script inside a key 'evaluator'. This script have
+access to every variable declared in the PL and its 'before' script.
+It should declare a variable 'grade' which should contain a tuple (int, feedback) where int is the grade between [0, 100]."""
 
 if __name__ == "__main__":
-    
-    # load the JSON exercise dictionary as Python dictionary
+
     with open(sys.argv[1], "r") as f:
         dic = json.load(f, cls=JSONDecoder)
     Component.sync_context(dic)
 
-    # add the custom namespace to the Python exercise dictionary
-    dic = {**namespace, **dic}
+    with open(sys.argv[2], "r") as f:
+        dic['answers'] = json.load(f)
+
+    
+    aux_component1(dic)
     
     if 'evaluator' in dic:
-
-        # execute the evaluator script in the exercise dictionary
-        exec(dic['evaluator'], dic)
-        
-        # clean the exercise dictionary from namespace elements
-        exec("", namespace)
-        for key in namespace:
-            if key in dic and dic[key] == namespace[key]:
-                del dic[key]
-
-    # render some string values of the exercise dictionary with the custom Jinja environment
-    jinja_keys = dic.get('jinja_keys', ['text', 'form', 'solution'])
-    
-    for key in jinja_keys:
-        if key in dic:
-            dic[key] = Env.from_string(dic[key]).render(dic)
-
-
-    # output the Python exercise dictionary as a JSON dictionary (using the custom encoder)
-    with open(sys.argv[3], "w+") as f:
-        json.dump(dic, f, cls=JSONEncoder)
-
-    # output feedback
-    feedback = dic.get('feedback', '')
-    with open(sys.argv[4], "w+") as f:
-        print(feedback, file=f)
-    
-    # output score
-    if 'score' not in dic:
-        print("Score not declared.", file=sys.stderr)
+            code = dic['evaluator']
+    else:
+        print(missing_evaluator_stderr, file=sys.stderr)
         sys.exit(1)
 
-    print(int(dic['score']))
+    #code = "\n".join([dic.get('headerevaluator', ""), dic.get('evaluator', ""), dic.get('footerevaluator', "")])
+
+    exec(code, dic)
+    namespace = {}
+    exec("", namespace)
+    for key in namespace:
+        if key in dic and dic[key] == namespace[key]:
+            del dic[key]
+
+    aux_component2(dic)
+    
+
+    if 'score' in dic:
+        score = dic['score']    
+    if 'feedback' in dic:
+        feedback = dic['feedback']  
+    else:
+        feedback = ""
+
+    if score >= 0:
+        dic['internals']['attempt'] = dic['internals']['attempt'] + 1
+    
+    dic['form'] = dic['interface']
+
+    if feedback.strip() != "":
+        dic['form'] = "\n".join([dic['form'], dic['format_feedback']])
+
+    if dic['settings']['maxattempt'] is not None:
+        if dic['internals']['attempt'] > dic['settings']['maxattempt']:
+            if score < 100 and 'solution' in dic:
+                dic['form'] = "\n".join([dic['form'], dic['linksolution']])
+    
+    feedback = Env.from_string(feedback).render(dic)
+    ffeedback = ""
+
+    # dic = json.loads(json.dumps(dic, cls=JSONEncoder))
+
+    # hack for MathQuill
+    if 'answers' in dic:
+        if 'math' in dic['answers']:
+            dic['prev_value'] = dic['answers']['math'].replace("\\", "\\\\")
+
+    with open(sys.argv[3], "w+") as f:
+        json.dump(dic, f, cls=JSONEncoder)
+    
+    with open(sys.argv[4], "w+", encoding='utf-8') as f:
+        print("", file=f)
+    
+    print(int(score))
     
     sys.exit(0)
