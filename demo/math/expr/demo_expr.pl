aa74db3fc1dabcbd217441e5c945c0a326b38873
David DOYEN <David.Doyen@u-pem.fr>
2021-05-24 17:25:02 +0200

    ddoyen modified Yggdrasil/demo/math/expr/demo_expr.pl

--- demo/math/expr/demo_expr.pl
+++ demo/math/expr/demo_expr.pl
@@ -16,7 +16,7 @@ inputblock ==
 {{ mathinput("math2") }}
 ==
 
-mathinputid % ["math", "math2"]
+mathinputid = ["math", "math2"]
 
 javascript.mathquill1 ==
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1-b/mathquill.min.js" type="text/javascript"></script>
