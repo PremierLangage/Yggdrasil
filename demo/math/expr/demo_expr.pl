7086fafc922be85f0afbeb45052a4727ab57e96d
David DOYEN <David.Doyen@u-pem.fr>
2021-05-24 17:22:27 +0200

    ddoyen modified Yggdrasil/demo/math/expr/demo_expr.pl

--- demo/math/expr/demo_expr.pl
+++ demo/math/expr/demo_expr.pl
@@ -31,7 +31,7 @@ MQ.config({charsThatBreakOutOfSupSub: '+-=<>',
 var arrayMathField = []
 var preval = {};
 {% for name in mathinputid %}
-preval[{{ name }}] = " ";
+preval["{{ name }}"] = " ";
 {% endfor %}
 var names = {{ mathinputid }};
 for (let name of names) {
