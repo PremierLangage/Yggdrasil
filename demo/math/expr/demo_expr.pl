f68cfbacf86bb8205ee40b77e4bac2e7471fc12b
David DOYEN <David.Doyen@u-pem.fr>
2021-05-24 17:49:46 +0200

    ddoyen modified Yggdrasil/demo/math/expr/demo_expr.pl

--- demo/math/expr/demo_expr.pl
+++ demo/math/expr/demo_expr.pl
@@ -17,34 +17,3 @@ inputblock ==
 ==
 
 mathinputid % ["math", "math2"]
-
-javascript.mathquill1 ==
-<script src="https://cdnjs.cloudflare.com/ajax/libs/mathquill/0.10.1-b/mathquill.min.js" type="text/javascript"></script>
-<script>
-var MQ = MathQuill.getInterface(2);
-MQ.config({charsThatBreakOutOfSupSub: '+-=<>',
-  autoCommands: 'pi theta sqrt sum infty infin emptyset alpha textS',
-  autoOperatorNames: 'sin cos ln exp lol Hz',
-  });
-
-
-var arrayMathField = []
-var preval = [];
-var names = [];
-{% for name in mathinputid %}
-names.push("{{name}}");
-preval.push("{{ answers[name] }}");
-{% endfor %}
-
-for (let i = 0; i < names.length; i++) {
-{% if input_embed|length > 0 %}
-let mathField = MQ.StaticMath(document.getElementById(names[i]));
-mathField.innerFields[0].latex(preval[i]);
-{% else %}
-let mathField = MQ.MathField(document.getElementById(names[i]));
-mathField.latex(preval[i]);
-{% endif %}
-arrayMathField.push(mathField);
-}
-</script>
-==
