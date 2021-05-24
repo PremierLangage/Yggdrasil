83c037f33a637982b7a3d7e7d50314ae62c294bb
David DOYEN <David.Doyen@u-pem.fr>
2021-05-24 19:29:30 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/testcomp/test.pl

--- Mathematics/exercises/testcomp/test.pl
+++ Mathematics/exercises/testcomp/test.pl
@@ -36,6 +36,10 @@ radio2 =: RadioGroup
 radio2.decorator = CustomRadio
 
 # Step 1
+intro ==
+On considère l'équation
+$${{ expr = 0 |latex }}$$
+==
 
 step1.question ==
 Calculer le discriminant de cette équation.
