978011e7af3342d6f295bd2ec676a4286850df00
David DOYEN <David.Doyen@u-pem.fr>
2021-05-24 18:03:30 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -111,7 +111,7 @@ var preval = [];
 var names = [];
 {% for name in mathinputid %}
 names.push("{{name}}");
-var val = "{{ answers[name]|safe}}";
+var val = "{{ answers[name]|e }}";
 alert(val);
 preval.push(val.replace("/", "//"));
 {% endfor %}
