2355a9a78407b3e10f021b7ff1aac227d36bfc8d
David DOYEN <David.Doyen@u-pem.fr>
2021-05-24 15:14:34 +0200

    ddoyen modified Yggdrasil/builder/mathbefore.py

--- builder/mathbefore.py
+++ builder/mathbefore.py
@@ -47,7 +47,6 @@ if __name__ == "__main__":
     aux_component(dic)
 
     dic['internals'] = {'attempt': 1 }
-    dic['question'] = dic['text']
 
     # render some string values of the exercise dictionary with the custom Jinja environment 
     macros = dic.get('macros', '')
