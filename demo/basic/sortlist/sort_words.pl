22f6f9267e8d9514f0d14856430597363f9b8f86
David DOYEN <David.Doyen@u-pem.fr>
2021-05-25 13:41:42 +0200

    ddoyen modified Yggdrasil/demo/basic/sortlist/sort_words.pl

---
+++ demo/basic/sortlist/sort_words.pl
@@ -0,0 +1,15 @@
+extends = /model/basic/sortlist.pl
+
+title = Ordre alphabétique
+
+question ==
+Classer les mots suivants dans l'ordre alphabétique.
+==
+
+sortedlist ==
+Abricot
+Avion
+Ballon
+Carotte
+Cartable
+==
