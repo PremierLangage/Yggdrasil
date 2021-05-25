dd6fd855d57a65baf49b02771331e726218eccf4
David DOYEN <David.Doyen@u-pem.fr>
2021-05-25 22:32:20 +0200

    ddoyen deleted /srv/local/pl/premierlangage/home/Yggdrasil/model/namespace

--- model/namespace/namespace_physics.py
+++
@@ -1,9 +0,0 @@
-from sympy import *
-from sympy.physics.units import *
-from plrandom import rd
-from randsympy import *
-from evalsympy import *
-from latex2sympy import *
-from sympy2latex import *
-
-namespace = globals().copy()
