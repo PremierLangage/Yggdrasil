e18281fb3022a2c5dfdbd218600550e182b18473
David DOYEN <David.Doyen@u-pem.fr>
2021-05-26 18:38:35 +0200

    ddoyen modified Yggdrasil/model/basic/temp.pl

--- model/basic/temp.pl
+++ model/basic/temp.pl
@@ -66,17 +66,7 @@ function onReadyPL(nodes) {
     actions.find('br').remove();
     body.children('br').remove();
 
-    {% if score >=0 %}
-    actions.append(`<a type="button" class="btn btn-primary action-reroll" href="`+link+`?action=reroll"> Nouveau</a>`);
-    {% else %}
-    actions.append('<button class="btn-group btn btn-primary action-submit"><span class="ion-hide-md-down">Valider</span></button>');
-    {% endif %}    
-    {% if score == 100 %}
-    actions.append('<button type="button" style="float: right;" class="btn success-state animated pulse">Score : {{score}} </button>');
-    {% endif %}
-    {% if score >= 0 and score < 100 %}
-    actions.append('<button type="button" style="float: right;" class="btn neutral-state animated pulse">Score : {{score}} </button>');
-    {% endif %}
+
 }
 </script>
 ==
