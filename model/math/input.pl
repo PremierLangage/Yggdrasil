c2e86834b0aab0595da377486cb648ba02a2c96a
David DOYEN <David.Doyen@u-pem.fr>
2021-05-26 19:29:49 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -53,7 +53,10 @@ Erreur
 <a type="button" class="btn btn-primary action-reroll" href="`+link+`?action=reroll"> Nouveau</a>
 {% else %}
 <button class="btn btn-primary action-submit">Valider</button>
-{% endif %}    
+{% endif %}
+<div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+    <span class="sr-only">Loading...</span>
+</div>   
 {% if score == 100 %}
 <button type="button" style="float: right;" class="btn success-state animated pulse">Score : {{score}} </button>
 {% endif %}
