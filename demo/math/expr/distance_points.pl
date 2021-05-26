15bc54a3dbd500d515a9f5129723b29813d9d85a
David DOYEN <David.Doyen@u-pem.fr>
2021-05-26 16:16:42 +0200

    ddoyen modified Yggdrasil/demo/math/expr/distance_points.pl

--- demo/math/expr/distance_points.pl
+++ demo/math/expr/distance_points.pl
@@ -13,6 +13,5 @@ sol = sqrt((xA-xB)**2 + (yA-yB)**2)
 question == 
 Dans le plan muni d'un repère orthonormé on considère les points de coordonnées 
 $! {{ (xA, yA) }} !$ et $! {{ (xB, yB) }} !$.<br><br>
-
 Quelle est la distance entre ces deux points ?
 ==
