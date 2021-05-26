47faa8cd83aa1846bef963bc8cbc775364934ead
David DOYEN <David.Doyen@u-pem.fr>
2021-05-26 15:18:55 +0200

    ddoyen modified Yggdrasil/demo/math/expr/distance_points.pl

--- demo/math/expr/distance_points.pl
+++ demo/math/expr/distance_points.pl
@@ -10,9 +10,7 @@ yB = randint(-5, 5)
 sol = sqrt((xA-xB)**2 + (yA-yB)**2)
 ==
 
-question = ab
-
-question2 == 
+question == 
 Dans le plan muni d'un repère orthonormé on considère les points de coordonnées 
 $! {{ (xA, yA) }} !$ et $! {{ (xB, yB) }} !$.
 
