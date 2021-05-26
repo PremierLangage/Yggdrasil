ed13404ef52b3c07230bd4c8d90ed2a2c5f66650
David DOYEN <David.Doyen@u-pem.fr>
2021-05-26 16:16:08 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -14,8 +14,11 @@ title = Title
 text = 
 
 form = 
+
 tplpage ==
-<div style="white-space: pre;">{{ question|safe }}</div>
+<div>
+{{ question|safe }}
+</div>
 {% if score >= 0 %}
 <div class="mq-disabled" style="display: block; margin-top: 1em;">
 {{ inputblock|safe }}
