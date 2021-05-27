cf86daf33b6d15a7988ae7f645f7175f099a84bc
David DOYEN <David.Doyen@u-pem.fr>
2021-05-27 13:06:15 +0200

    ddoyen modified Yggdrasil/demo/basic/radio/histoire_fixe.pl

---
+++ demo/basic/radio/histoire_fixe.pl
@@ -0,0 +1,18 @@
+extends = /model/basic/radio.pl
+
+title = Histoire
+
+text ==
+A quel siècle vivait Louis XI ?
+==
+
+items ==
+XIIe siècle
+XIIIe siècle
+XIVe siècle
+XVe siècle
+==
+
+indsol = 3
+
+shuffled = false
