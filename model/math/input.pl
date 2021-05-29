4d6aefb654ee9296dac9c6c6b9f517f64beb4552
David DOYEN <David.Doyen@u-pem.fr>
2021-05-29 17:38:43 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -71,6 +71,16 @@ style.mathquill ==#|html|
             font-size: 8px;
             text-align: center;
         }
+
+        .btn-xs {
+            width: 30px;
+            height: 30px;
+            padding: 6px 0px;
+            border-radius: 15px;
+            font-size: 8px;
+            text-align: center;
+        }
+
 .mq-disabled .mq-editable-field {
 pointer-events: none;
 background-color: #F5F5F5;
