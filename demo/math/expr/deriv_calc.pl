52e2cba1c11654da6449ff89ed48d96ebb1fbb7b
David DOYEN <David.Doyen@u-pem.fr>
2021-06-01 11:09:56 +0200

    ddoyen modified Yggdrasil/demo/math/expr/deriv_calc.pl

--- demo/math/expr/deriv_calc.pl
+++ demo/math/expr/deriv_calc.pl
@@ -9,9 +9,12 @@ g = choice([sin(x), cos(x), exp(x)])
 h = choice([x, x**2])
 f = g * h
 sol = diff(f, x)
+toto = "blabla"
 ==
 
 question ==
 Calculer la dérivée de la fonction 
 $$ f : x \mapsto {{ f|latex }} .$$
+
+{{ toto }}
 ==
