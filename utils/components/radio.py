5f9e384a74be0680559294a7b252e6461a0c509d
David DOYEN <David.Doyen@u-pem.fr>
2021-06-03 00:48:50 +0200

    ddoyen modified Yggdrasil/utils/components/radio.py

--- utils/components/radio.py
+++ utils/components/radio.py
@@ -68,4 +68,55 @@ class CustomRadio(Component):
 
         return score
 
+class Radio(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-radio-group'
+        self.decorator = 'CustomRadio'
+        self.selection = None
+        super().__init__(**kwargs)
+        self.statement =''
+
+    def set_items(self, items):
+        """
+        Set items in the component.
+        """
+        if isinstance(items, str):
+            _items_ = items.splitlines()
+        else:
+            _items_ = items
+        self.items = [{"id": str(uuid4()), "content": str(item)} for item in _items_]
+
+    def set_sol(self, index):
+        """
+        Set the component solution from an index.
+        """
+        self._sol = self.items[index]['id']
+
+    def shuffle(self):
+        """
+        Shuffle the component items.
+        """
+        rd.shuffle(self.items)
+
+    def eval(self, display=True, disabled=True):
+        """
+        Evaluate the answer stored in the component.
+        """
+        score = 0
+        for item in self.items:
+            id = item['id']
+            if id == self._sol and id == self.selection:
+                score = 100
+                if display:
+                    item['css'] = 'success-state icon-check-after'
+            elif display and id != self._sol and id == self.selection:
+                    item['css'] = 'error-state icon-times-after'
+            elif display and id == self._sol and id != self.selection:
+                    item['css'] = 'icon-check-after'
+
+        self.disabled = disabled
+
+        return score
+
 
