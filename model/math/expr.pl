cab783edeb64679cdba487faaf7bf0cf4e1c972e
David DOYEN <David.Doyen@u-pem.fr>
2021-06-15 19:18:14 +0200

    ddoyen modified Yggdrasil/model/math/expr.pl

--- model/math/expr.pl
+++ model/math/expr.pl
@@ -8,7 +8,7 @@ evaluator ==
 from ast import literal_eval
 from sympy import sympify
 from evalsympy import eval_expr
-score, error = eval_expr(answers['math'], sol, checkratsimp=literal_eval(checkratsimp), unauthorized_func=literal_eval(unauthorized_func), local_dict=sympify(symbol_dict))
+score, error = eval_expr(answers['math'], sol, checkratsimp=checkratsimp, unauthorized_func=unauthorized_func, local_dict=sympify(symbol_dict))
 feedback = message[error]
 ==
 
