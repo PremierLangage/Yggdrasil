e03df543b653a9490ab8cf8584e499fb92e4ca7a
David DOYEN <David.Doyen@u-pem.fr>
2021-06-17 11:17:18 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/derivative/computation.pl

--- Mathematics/exercises/analysis/derivative/computation.pl
+++ Mathematics/exercises/analysis/derivative/computation.pl
@@ -1,18 +1,17 @@
-extends = /model/mathquill.pl
+extends = /model/math/expr.pl
 
-title = Calcul de dérivée
-
+title = Calculer la dérivée d'une fonction
 
 before ==
+from sympy import diff
 var('x')
-n=randint(1,4)
-g=randitem([sin(x),cos(x),exp(x),ln(x)])
-h=randitem([x,x**2])
-f=g*h
-sol=diff(f,x)
+g = choice([sin(x), cos(x), exp(x)])
+h = choice([x, x**2])
+f = g * h
+sol = diff(f, x)
 ==
 
-text ==
-Calculer la dérivée de la fonction
-$$ f : x \mapsto {{f|latex}} .$$
+question ==
+Calculer la dérivée de la fonction 
+$$ f : x \mapsto {{ f|latex }} .$$
 ==
