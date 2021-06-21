94b78b6ca88c90ba78108ca5a6543de6a0e12a17
David DOYEN <David.Doyen@u-pem.fr>
2021-06-21 14:21:09 +0200

    ddoyen modified Yggdrasil/demo/basic/checkbox/media.pl

---
+++ demo/basic/checkbox/media.pl
@@ -0,0 +1,16 @@
+extends = /model/basic/input.pl
+
+question ==
+Regarde la vidéo-ci-dessous et réponds à la question.
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZVq2iPdgBOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+Parmi les pays suivants, lesquels n'ont pas participé à la fondation de la C.E.E. ?
+==
+
+items ==
+Luxembourg
+France
+Espagne
+Royaume-Uni
+==
+
+indsol = [2, 3]
