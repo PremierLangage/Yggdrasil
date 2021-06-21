6427804c135bc27935d58a5209bd8e7ee3ccd1a5
David DOYEN <David.Doyen@u-pem.fr>
2021-06-21 14:22:09 +0200

    ddoyen modified Yggdrasil/demo/basic/checkbox/media.pl

--- demo/basic/checkbox/media.pl
+++ demo/basic/checkbox/media.pl
@@ -3,6 +3,7 @@ extends = /model/basic/checkbox.pl
 question ==
 Regarde la vidéo-ci-dessous et réponds à la question.
 <iframe width="560" height="315" src="https://www.youtube.com/embed/ZVq2iPdgBOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
 Parmi les pays suivants, lesquels n'ont pas participé à la fondation de la C.E.E. ?
 ==
 
