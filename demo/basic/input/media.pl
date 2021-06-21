813ac9833b15c560a046a3e0970bab4d9c10bd5f
David DOYEN <David.Doyen@u-pem.fr>
2021-06-21 14:17:01 +0200

    ddoyen modified Yggdrasil/demo/basic/input/media.pl

--- demo/basic/input/media.pl
+++ demo/basic/input/media.pl
@@ -2,8 +2,7 @@ extends = /model/basic/input.pl
 
 question ==
 Qui a écrit *Les Misérables* ?
-[![Alt text](https://img.youtube.com/vi/VID/0.jpg)](https://www.youtube.com/watch?v=ZVq2iPdgBOI)
-
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZVq2iPdgBOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 ==
 
 sol ==
