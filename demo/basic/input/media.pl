a8c6d007869b5c95e131eb2c79579495f1b288e6
David DOYEN <David.Doyen@u-pem.fr>
2021-06-21 14:15:39 +0200

    ddoyen modified Yggdrasil/demo/basic/input/media.pl

---
+++ demo/basic/input/media.pl
@@ -0,0 +1,14 @@
+extends = /model/basic/input.pl
+
+question ==
+Qui a écrit *Les Misérables* ?
+[![Alt text](https://img.youtube.com/vi/VID/0.jpg)](https://www.youtube.com/watch?v=ZVq2iPdgBOI)
+
+==
+
+sol ==
+Victor Hugo
+Hugo
+==
+
+
