144bfaf458017bc982c904d3efe32fb17391c025
David DOYEN <David.Doyen@u-pem.fr>
2021-06-22 11:46:16 +0200

    ddoyen modified Yggdrasil/demo/basic/seltext/verbes.pl

---
+++ demo/basic/seltext/verbes.pl
@@ -0,0 +1,9 @@
+extends = /model/basic/seltext.pl
+
+question ==
+Sélectionner dans ce texte les verbes conjugués au passé simple.
+==
+
+seltext ==
+Il était fort petit, et, quand il {vint} au monde, il n'était guère plus gros que le pouce, ce qui {fit} qu'on l'{appela} le petit Poucet. Ce pauvre enfant était le souffre-douleur de la maison, et on lui donnait toujours tort.
+==
