e2db2f9dcdce619e54f1e3ce27915f0bd09472ef
David DOYEN <David.Doyen@u-pem.fr>
2021-06-22 11:45:39 +0200

    ddoyen modified Yggdrasil/model/basic/seltext.pl

--- model/basic/seltext.pl
+++ model/basic/seltext.pl
@@ -15,24 +15,22 @@ def bracket_words(string):
     lst=re.findall(r"#[^#]*#|\{[^\{\}]*\}|\[[^\]]*\]|[^\{\}\[\]#]+",string)
     for i in range(len(lst)):
         s = lst[i]
-        if s[0] == "[":
+        if s[0] == "#":
             lst[i]=s[1:-1]
-        elif s[0] == "#":
-            pass
-        elif s[0] == "{":
+        elif s[0] == "[" or s[0] == "{":
             pass
         else:
             lst[i] = re.sub(r"(\w+)", r"{\1}", s)
     return "".join(lst)
     
 def read_text_bracket(string):
-    lst = re.findall(r"#[^#]*#|\{[^\{\}]*\}|[^\{\}#]+",string)
+    lst = re.findall(r"\[[^\[\]]*\]|\{[^\{\}]*\}|[^\{\}#]+", string)
     selection=[]
     k=0
     for i in range(len(lst)):
         s = lst[i]
-        if s[0] == "{" or s[0] == "#":
-            if s[0] == "#":
+        if s[0] == "{" or s[0] == "[":
+            if s[0] == "{":
                 selection.append(k)
             p = s[1:-1]
             lst[i] = f'<span data-index="{k}">{p}</span>'
