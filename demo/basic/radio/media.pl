e2d17708fd49ab40de2bbc9894d2000f57cca218
David DOYEN <David.Doyen@u-pem.fr>
2021-06-23 18:05:54 +0200

    ddoyen modified Yggdrasil/demo/basic/radio/media.pl

---
+++ demo/basic/radio/media.pl
@@ -0,0 +1,24 @@
+extends = /model/basic/checkbox.pl
+
+question2 ==
+Regarde la vidéo et réponds à la question.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZVq2iPdgBOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+Parmi les pays suivants, lesquels n'ont pas participé à la fondation de la C.E.E. ?
+==
+
+question ==
+A quel pays appartient ce drapeau ?
+
+<img class="img-40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/640px-Flag_of_France.svg.png">
+==
+
+items ==
+Luxembourg
+France
+Espagne
+Royaume-Uni
+==
+
+indsol = [2, 3]
