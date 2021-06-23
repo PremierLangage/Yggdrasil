ccdc5a4622ba3c3917083a7652b6052fc3aa83dc
David DOYEN <David.Doyen@u-pem.fr>
2021-06-23 18:14:08 +0200

    ddoyen modified Yggdrasil/demo/basic/radio/drapeau.pl

---
+++ demo/basic/radio/drapeau.pl
@@ -0,0 +1,22 @@
+extends = /model/basic/radio.pl
+
+question2 ==
+Regarde la vidéo et réponds à la question.
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/ZVq2iPdgBOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+Parmi les pays suivants, lesquels n'ont pas participé à la fondation de la C.E.E. ?
+==
+
+question ==
+A quel pays appartient ce drapeau ?
+
+<img class="img img-40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/640px-Flag_of_France.svg.png">
+==
+
+items ==
+France
+Italie
+Allemagne
+Espagne
+==
