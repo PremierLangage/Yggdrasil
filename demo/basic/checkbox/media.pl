a7ef17dc8f941e98bd43d80d53febec968ebcf0a
David DOYEN <David.Doyen@u-pem.fr>
2021-06-23 17:26:59 +0200

    ddoyen modified Yggdrasil/demo/basic/checkbox/media.pl

--- demo/basic/checkbox/media.pl
+++ demo/basic/checkbox/media.pl
@@ -1,11 +1,15 @@
 extends = /model/basic/checkbox.pl
 
-question ==
+question2 ==
 Regarde la vidéo et réponds à la question.
 
 <iframe width="560" height="315" src="https://www.youtube.com/embed/ZVq2iPdgBOI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
 Parmi les pays suivants, lesquels n'ont pas participé à la fondation de la C.E.E. ?
+==
+
+question ==
+![](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg/598px-Eug%C3%A8ne_Delacroix_-_Le_28_Juillet._La_Libert%C3%A9_guidant_le_peuple.jpg)
 ==
 
 items ==
