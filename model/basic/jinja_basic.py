aa44f75e46369dff5249ec15996c6bedba3882ed
David DOYEN <David.Doyen@u-pem.fr>
2021-06-25 09:05:06 +0200

    ddoyen modified Yggdrasil/model/basic/jinja_basic.py

---
+++ model/basic/jinja_basic.py
@@ -0,0 +1,13 @@
+from jinja2 import Environment, BaseLoader
+
+def component(l):
+    if isinstance(l, dict):
+        selector = l["selector"]
+        cid = l["cid"]
+    else:
+        selector = l.selector
+        cid = l.cid
+    return "<%s cid='%s'></%s>" % (selector, cid, selector)
+
+Env = Environment(loader=BaseLoader(), autoescape=select_autoescape(default_for_string=True))
+Env.filters["component"] = component
