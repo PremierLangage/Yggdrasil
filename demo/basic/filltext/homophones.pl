7496d6e33f4db33f605441511bdecfc7fbf5707d
David DOYEN <David.Doyen@u-pem.fr>
2021-06-25 10:51:00 +0200

    ddoyen modified Yggdrasil/demo/basic/filltext/homophones.pl

--- demo/basic/filltext/homophones.pl
+++ demo/basic/filltext/homophones.pl
@@ -2,7 +2,9 @@ extends = /model/basic/filltext.pl
 
 question ==
 Compléter les phrases suivantes avec les étiquettes proposées.
+==
 
+filledtext ==
 * Je voudrais {ces} chausures pour mon anniversaire.
 * Tu est toujours en retard, {c'est} agaçant !
 * Je n'aime pas {ces} méthodes.
