d185367e1eacc1462374b25afc49c4723f157a05
David DOYEN <David.Doyen@u-pem.fr>
2021-06-25 11:38:30 +0200

    ddoyen modified Yggdrasil/model/basic/filltext.pl

--- model/basic/filltext.pl
+++ model/basic/filltext.pl
@@ -6,11 +6,9 @@ question ==
 Compléter le texte suivant avec les étiquettes.
 ==
 
-before_scripts = ["importfunc", "before", "process", "process2"]
+before_scripts = ["importfunc", "before", "process"]
 
-process2 ==
-import re
-
+process ==
 sol = re.findall(r'\{(.*?)\}', filledtext)
 
 count = -1
@@ -20,6 +18,40 @@ def replace(x):
     return "{{ cdrops[" + str(count) + "]}}"
 
 part1 = re.sub(r'\{(.*?)\}', replace , filledtext)
+
+from customdragdrop import CustomDragDrop
+cplabels, cpdrops = [], []
+clabels, cdrops= [], []
+
+if isinstance(sol, str):
+    _sol_ = sol.splitlines()
+else:
+    _sol_ = sol
+
+if isinstance(labels, str):
+    _labels_ = labels.splitlines()
+elif isinstance(labels, list):
+    _labels_ = labels
+else:
+    _labels_ = []
+
+_labels_ = list(set(_labels_ + _sol_))
+
+for i, content in enumerate(_labels_):
+    cplabels.append(CustomDragDrop.Label(content=content))
+    selector = cplabels[i].selector
+    cid = cplabels[i].cid
+    clabels.append("<%s cid='%s'></%s>" % (selector, cid, selector))
+
+nbdrops = len(_sol_)
+
+for i in range(nbdrops):
+    cpdrops.append(CustomDragDrop.Drop())
+    selector = cpdrops[i].selector
+    cid = cpdrops[i].cid
+    cdrops.append("<%s cid='%s'></%s>" % (selector, cid, selector))
+
+import re
 
 part2 = "&nbsp;".join(clabels)
 
