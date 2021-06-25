631d14732f9fd705b7d7c74567854d3bfe6994aa
David DOYEN <David.Doyen@u-pem.fr>
2021-06-25 10:47:45 +0200

    ddoyen modified Yggdrasil/demo/basic/dragdrop/homophones.pl

--- demo/basic/dragdrop/homophones.pl
+++ demo/basic/dragdrop/homophones.pl
@@ -1,17 +1,12 @@
 extends = /model/basic/dragdrop.pl
 
-title = Homophones : ces ou c'est
 
 question ==
 Compléter les phrases suivantes avec les étiquettes proposées.
-==
 
-inputblock ==
-<ul>
-<li> Je voudrais {{ drops[0] }} chausures pour mon anniversaire. </li>
-<li> Tu est toujours en retard, {{ drops[1] }} agaçant ! </li>
-<li> Je n'aime pas {{ drops[2] }} méthodes. </li>
-</ul>
+* Je voudrais {{ drops[0] }} chausures pour mon anniversaire.
+* Tu est toujours en retard, {{ drops[1] }} agaçant !
+* Je n'aime pas {{ drops[2] }} méthodes.
 
 {{ labels[0] }} {{ labels[1] }}
 ==
