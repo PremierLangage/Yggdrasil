95cb169f76cb0430680ec632f8d235dc2f3cf53f
David DOYEN <David.Doyen@u-pem.fr>
2021-06-25 15:18:23 +0200

    ddoyen modified Yggdrasil/demo/basic/radio/drapeau.pl

--- demo/basic/radio/drapeau.pl
+++ demo/basic/radio/drapeau.pl
@@ -5,7 +5,7 @@ A quel pays appartient ce drapeau ?
 
 <img class="img img-40" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/640px-Flag_of_France.svg.png">
 
-* bbbb
+* <i> bbbb </i>
 * bbbb
 ==
 
