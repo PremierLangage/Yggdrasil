e0adac0e6cf92e16410aa5c7aa4f0e8e8c9b3620
David DOYEN <David.Doyen@u-pem.fr>
2021-06-26 11:40:53 +0200

    ddoyen modified Yggdrasil/demo/math/input/demo_mathinput.pl

--- demo/math/input/demo_mathinput.pl
+++ demo/math/input/demo_mathinput.pl
@@ -3,12 +3,3 @@ extends = /model/math/input.pl
 question ==
 Entrer une expression mathématique.
 ==
-
-keypad = ["sqrt", "-infty", "+infty"]
-extends = /model/math/input.pl
-
-question ==
-Entrer une expression mathématique.
-==
-
-keypad = []
