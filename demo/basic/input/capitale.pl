8a0290d84b5435b9e4c612643b04043df679aaa9
David DOYEN <David.Doyen@u-pem.fr>
2021-06-29 01:12:43 +0200

    ddoyen modified Yggdrasil/demo/basic/input/capitale.pl

--- demo/basic/input/capitale.pl
+++ demo/basic/input/capitale.pl
@@ -3,7 +3,7 @@ extends = /model/basic/input.pl
 @ /demo/data/pays_europe.csv
 
 before ==
-f = open('pays_europe.csv', newline='')
+f = open('pays_europe.csv')
 row = csv_choice(f)
 capitale = row['capitale']
 sol = row['pays']
