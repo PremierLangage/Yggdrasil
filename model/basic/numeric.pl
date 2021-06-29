c1356251eaa69b69a30d3c1ec8fb11a608a90bbe
David DOYEN <David.Doyen@u-pem.fr>
2021-06-29 22:27:54 +0200

    ddoyen modified Yggdrasil/model/basic/numeric.pl

--- model/basic/numeric.pl
+++ model/basic/numeric.pl
@@ -11,6 +11,20 @@ inputblock ==
 ==
 
 
+style.input ==
+<style>
+.mat-form-field-wrapper{
+  margin: 0 !important;
+  padding-bottom: 0 !important;
+}
+
+.mat-form-field-appearance-outline .mat-form-field-infix {
+    padding-top: 0.8em !important;
+    padding-bottom: 0.6em !important;
+}
+</style>
+==
+
 evaluator ==
 if input.value == sol:
     score = 100
