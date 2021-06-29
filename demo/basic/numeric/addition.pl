7c74d67c4e3d1e06a270628365e3ac9d7c75a195
David DOYEN <David.Doyen@u-pem.fr>
2021-06-29 20:13:32 +0200

    ddoyen modified Yggdrasil/demo/basic/numeric/addition.pl

--- demo/basic/numeric/addition.pl
+++ demo/basic/numeric/addition.pl
@@ -11,17 +11,6 @@ question ==
 Calculer {{ a }} + {{ b }}.
 
 <div class="container">
-  <div class="row justify-content-md-center">
-    <div class="col col-lg-2">
-      1 of 3
-    </div>
-    <div class="col-md-auto">
-      Variable width content
-    </div>
-    <div class="col col-lg-2">
-      3 of 3
-    </div>
-  </div>
   <div class="row">
     <div class="col">
       1 of 3
