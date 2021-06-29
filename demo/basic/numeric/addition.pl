596a5b4235e59283d5e3627dccf7ff423e1bf75f
David DOYEN <David.Doyen@u-pem.fr>
2021-06-29 16:39:52 +0200

    ddoyen modified Yggdrasil/demo/basic/numeric/addition.pl

--- demo/basic/numeric/addition.pl
+++ demo/basic/numeric/addition.pl
@@ -7,6 +7,14 @@ b = randint(10, 50)
 sol = a + b
 ==
 
+inputblock ==
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+==
+
 question ==
 Calculer {{ a }} + {{ b }}. {{v}}
 ==
