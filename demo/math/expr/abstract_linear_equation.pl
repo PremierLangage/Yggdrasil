95fbd6fce02351dbf09c1edb6e051ec5066b4c6a
David DOYEN <David.Doyen@u-pem.fr>
2021-06-30 15:42:29 +0200

    ddoyen modified Yggdrasil/demo/math/expr/abstract_linear_equation.pl

--- demo/math/expr/abstract_linear_equation.pl
+++ demo/math/expr/abstract_linear_equation.pl
@@ -3,7 +3,7 @@ extends = /model/math/expr.pl
 title = Exprimer une inconnue en fonction d'une autre
 
 before ==
-var('x y')
+x, y = symbols('x y')
 a = randint(-5, 5, [0])
 b = randint(-5, 5, [0])
 expr = a*y+b
