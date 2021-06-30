dac2345dce358b3a748e1099203caf53d55fcfa7
David DOYEN <David.Doyen@u-pem.fr>
2021-06-30 14:24:20 +0200

    ddoyen modified Yggdrasil/model/basic/numeric.pl

--- model/basic/numeric.pl
+++ model/basic/numeric.pl
@@ -20,27 +20,14 @@ inputblock ==
 </div>
 ==
 
-solution ==
-La solution est {{ sol }}.
-==
-
-style.input ==
-<style>
-.mat-form-field-wrapper{
-  margin: 0 !important;
-  padding-bottom: 0 !important;
-}
-
-.mat-form-field-appearance-outline .mat-form-field-infix {
-    padding-top: 0.8em !important;
-    padding-bottom: 0.6em !important;
-}
-</style>
-==
-
 evaluator ==
 if input.value == sol:
     score = 100
 else:
     score = 0
+==
+
+
+solution ==
+La solution est {{ sol }}.
 ==
