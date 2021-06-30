44be1c82388459d59450521f54da6eb144b8b120
David DOYEN <David.Doyen@u-pem.fr>
2021-06-30 13:10:41 +0200

    ddoyen modified Yggdrasil/demo/basic/numeric/addition_aleatoire.pl

---
+++ demo/basic/numeric/addition_aleatoire.pl
@@ -0,0 +1,12 @@
+extends = /model/basic/numeric.pl
+
+before ==
+from random import randint
+a = randint(10, 50)
+b = randint(10, 50)
+sol = a + b
+==
+
+question ==
+Calculer {{ a }} + {{ b }}.
+==
