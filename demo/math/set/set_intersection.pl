46306f5c3c3d7d1874b6719d189e19c8dca9cfc9
David DOYEN <David.Doyen@u-pem.fr>
2021-06-30 20:54:55 +0200

    ddoyen modified Yggdrasil/demo/math/set/set_intersection.pl

--- demo/math/set/set_intersection.pl
+++ demo/math/set/set_intersection.pl
@@ -1,20 +1,15 @@
 extends = /model/math/set.pl
 
-title = Déterminer l'intersection de deux ensembles
-
 before ==
 from sympy import Intersection
-A = FiniteSet(*sample(range(10), randint(3, 5)))
-B = FiniteSet(*sample(range(10), randint(3, 5)))
+A = FiniteSet(sampleint(1, 10, randint(3, 5)))
+B = FiniteSet(sampleint(1, 10, randint(3, 5)))
 sol = Intersection(A, B)
 ==
 
 question ==
-On considère les ensembles suivants : $$ A= {{ A|latex }},\ B={{B|latex}}.$$ 
-
+On considère les ensembles $! A= {{ A|latex }} !$ et $! B={{B|latex}} !$. 
 Déterminer $! A \cap B !$.
 ==
 
-# keypad % [{"label": "$! \\varnothing !$", "action": "cmd", "value": "\\empty"}]
-
-keypad % ["emptyset"]
+keypad = ["emptyset"]
