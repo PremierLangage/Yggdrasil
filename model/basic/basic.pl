2d6b0fb8152d0d747179c1177984d2c01c93409a
David DOYEN <David.Doyen@u-pem.fr>
2021-06-30 14:50:37 +0200

    ddoyen modified Yggdrasil/model/basic/basic.pl

--- model/basic/basic.pl
+++ model/basic/basic.pl
@@ -1,13 +1,10 @@
 extends = /model/basic/temp.pl
 
-title =
-
 @ /builder/before1.py [builder.py]
 @ /grader/evaluator1.py [grader.py]
 @ /utils/builderlib.py
 @ /utils/plrandom.py
 @ /utils/plcsv.py
-
 @ /model/basic/jinja_basic.py [jinja_env.py]
 @ /model/basic/json_basic.py [json_encoder.py]
 
@@ -19,11 +16,11 @@ from plrandom import randint, sampleint
 from plcsv import csv_choice, csv_sample, csv_col
 ==
 
+title =
 form = 
 text = 
 
 tplpage =@ template.html
-
 
 style.basic == #|css|
 <style>
