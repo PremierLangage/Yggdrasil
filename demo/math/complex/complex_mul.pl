0f0013fba626933472177bb40d3e8160771c7d31
David DOYEN <David.Doyen@u-pem.fr>
2021-06-30 20:04:10 +0200

    ddoyen modified Yggdrasil/demo/math/complex/complex_mul.pl

--- demo/math/complex/complex_mul.pl
+++ demo/math/complex/complex_mul.pl
@@ -1,7 +1,5 @@
 extends = /model/math/complex.pl
 
-title = Multiplier deux nombres complexes
-
 before ==
 z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
 z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
@@ -10,7 +8,6 @@ sol = (z1 * z2).expand()
 
 question ==
 On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. 
-
 Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
 ==
 
