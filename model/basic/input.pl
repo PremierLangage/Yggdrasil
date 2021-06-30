72f263e7410354111532762284df8857d329a585
David DOYEN <David.Doyen@u-pem.fr>
2021-06-30 12:29:49 +0200

    ddoyen modified Yggdrasil/model/basic/input.pl

--- model/basic/input.pl
+++ model/basic/input.pl
@@ -14,6 +14,20 @@ inputblock ==
 ==
 
 
+style.input ==
+<style>
+.mat-form-field-wrapper{
+  margin: 0 !important;
+  padding-bottom: 0 !important;
+}
+
+.mat-form-field-appearance-outline .mat-form-field-infix {
+    padding-top: 0.8em !important;
+    padding-bottom: 0.6em !important;
+}
+</style>
+==
+
 evaluator ==
 from pltext import edit_distance
 
