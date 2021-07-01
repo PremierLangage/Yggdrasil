d8e1c5385a668d7eb5d90438e76df1bf38120fa7
David DOYEN <David.Doyen@u-pem.fr>
2021-07-01 10:02:26 +0200

    ddoyen modified Yggdrasil/model/math/math.pl

--- model/math/math.pl
+++ model/math/math.pl
@@ -25,8 +25,73 @@ from sympy2latex import latex
 from latex2sympy import latex2sympy
 ==
 
-style.svg ==
+
+style.basic == #|css|
 <style>
+.icon-check-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #006400;
+    margin-left: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-after p::after {
+    font-family: "Font Awesome 5 Free";
+    color: #8B0000;
+    margin-left: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-check-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #006400;
+    margin-right: 1em;
+    content: "\f00c";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.icon-times-before p::before {
+    font-family: "Font Awesome 5 Free";
+    color: #8B0000;
+    margin-right: 1em;
+    content: "\f00d";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.btn-audio::before {
+    font-family: "Font Awesome 5 Free";
+    color: white;
+    content: "\f028";
+    vertical-align: middle;
+    font-weight: 900;
+}
+
+.error-text-unit {
+    background-color: #f2dede;
+    color: #a94442;
+    text-decoration: line-through #a94442;
+}
+
+.success-text-unit {
+    background-color: #dff0d8;
+    color: #3c763d;
+}
+
+.missed-text-unit {
+    color: #3c763d;
+    text-decoration: underline wavy #3c763d;
+}
+
+.highlight-state {
+    color: var(--brand-color-primary);
+    background-color: #cce5ff;
+}
 
 .question p {
     font-size: 16px;
@@ -38,21 +103,68 @@ style.svg ==
     font-weight: 500;
 }
 
-.img-container {
-    width: 60%;
-    margin: auto;
+hr.divide {
+  background-color: lightgray;
+  height: 0.5px;
+  border: 0;
+}
+
+
+.img {
+    margin-top: 0.5em;
+    margin-bottom: 0.5em;
+    height: auto;
+    margin-left: auto;
+    margin-right:auto;
+}
+
+.img-60 {
+    max-width: 60%;
+}
+.img-50 {
+    max-width: 50%;
+}
+.img-40 {
+    max-width: 40%;
 }
 
 @media screen and (max-width: 768px) { /* css appliqué sur les tablettes */
-    .img-container {
-        width: 80%;
+    .img-60 {
+        max-width: 80%;
+    }
+    .img-50 {
+        max-width: 70%;
+    }
+    .img-40 {
+        max-width: 60%;
+    }
+}
+
+@media screen and (max-width: 576px) { /* css appliqué sur les téléphones */
+    .img-60 {
+        max-width: 100%;
+    }
+    .img-50 {
+        max-width: 90%;
+    }
+    .img-40 {
+        max-width: 80%;
+    }
+}
+
+
+@media screen and (max-width: 768px) { /* css appliqué sur les tablettes */
+    .img-40 {
+        max-width: 60%;
+        height: auto;
         margin: auto;
     }
 }
 
 @media screen and (max-width: 576px) { /* css appliqué sur les téléphones */
-    .img-container {
-        width: 100%;
+    .img-40 {
+        max-width: 80%;
+        height: auto;
         margin: auto;
     }
 }
