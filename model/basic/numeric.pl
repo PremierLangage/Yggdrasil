855c8674a1dbcb9d5e13c8a6286f400c6f7ad20f
David DOYEN <David.Doyen@u-pem.fr>
2021-07-02 08:54:11 +0200

    ddoyen modified Yggdrasil/model/basic/numeric.pl

--- model/basic/numeric.pl
+++ model/basic/numeric.pl
@@ -29,11 +29,10 @@ if ans == sol:
     score = 100
 else:
     score = 0
-input.disabled = True
 ==
 
 evalprocess ==
-if scpre = 100:
+if score = 100:
     input.suffix = r'<i class="fas fa-check" style="color: #006400"></i>'
 elif score = 0:
     input.suffix = r'<i class="fas fa-times" style="color: #8B0000"></i>'
