b85bb87fa843ede4c71318583a7c3239eac0cfe6
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 18:51:59 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl
@@ -12,6 +12,21 @@ text==
  Initialisez la variable **sum** avec la somme des variables a et b.
 ==
 
+# création de l'éditeur de code
+editor =: CodeEditor
+editor.language = python
+#editor.cid = py_editor_0
+editor.code ==
+# Saisissez votre code ici, et cliquez sur le bouton
+# de validation quand vous avez terminé
+
+==
+
+# le formulaire de l'exercice contient uniquement l'éditeur
+form==
+{{editor|component}}
+==
+
 grader==#|python|
 run(
     title='Deux nombres',
