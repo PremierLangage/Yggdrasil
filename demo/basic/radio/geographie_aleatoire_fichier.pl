48061155565c41a9a6bac0f6800c5e356b05997d
David DOYEN <David.Doyen@u-pem.fr>
2021-07-05 00:01:25 +0200

    ddoyen modified Yggdrasil/demo/basic/radio/geographie_aleatoire_fichier.pl

--- demo/basic/radio/geographie_aleatoire_fichier.pl
+++ demo/basic/radio/geographie_aleatoire_fichier.pl
@@ -3,8 +3,8 @@ extends = /model/basic/radio.pl
 @ /demo/data/pays_europe.csv
 
 before ==
-with open('pays_europe.csv', newline='') as f:
-    datasample = csv_sample(f, 4)
+f = open('pays_europe.csv')
+datasample = csv_sample(f, 4)
 capitale = datasample[0]['capitale']
 items = [row['pays'] for row in datasample]
 ==
