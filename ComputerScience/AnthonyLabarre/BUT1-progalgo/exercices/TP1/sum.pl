d76b58c00b4475f7c7cf35102aad94d4cb2311ba
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 19:20:13 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl
@@ -12,9 +12,11 @@
 @ /ComputerScience/python/AP1-1920/templates/generic/utils/testgroup.html
 @ /ComputerScience/python/AP1-1920/templates/generic/utils/testitem.html
 
+title = <em>(Pas de titre défini)</em>
+text = <em>(Pas d'énoncé défini)</em>
 
 # création de l'éditeur de code
-editor =: "/python/form/editorform.html"
+editor =: CodeEditor
 editor.code ==
 ==
 
