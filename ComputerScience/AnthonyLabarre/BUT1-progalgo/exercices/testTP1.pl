b9a46930cf5595c205fedcdaaadf0a029821c603
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 17:47:35 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl
@@ -1,18 +1,20 @@
 extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 title= caluler une somme dans une variable
+
+
+@ /builder/before.py [builder.py] 
+
 text==
 En supposant qu'il existe déjà 2 variables x et y, écrire le code qui met leur somme dans une variable nommée s.
 ==
 
 before==
-
 import random 
 
 x,y = 40,2
 result = x+y
 ==
 
-@ /builder/before.py [builder.py]
 pltest==
 >>> s == s # La variable existe  ?
 True
