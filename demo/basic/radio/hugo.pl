c3ed251b6188eade6cc28f592ef6f46ee32bc1cd
David DOYEN <David.Doyen@u-pem.fr>
2021-07-04 12:49:29 +0200

    ddoyen modified Yggdrasil/demo/basic/radio/hugo.pl

---
+++ demo/basic/radio/hugo.pl
@@ -0,0 +1,16 @@
+extends = /model/basic/radio.pl
+
+question ==
+À quel siècle vivait Victor Hugo ?
+==
+
+items ==
+XVIIe siècle
+XVIIIe siècle
+XIXe siècle
+XXe siècle
+==
+
+shuffled = False
+
+indsol = 2
