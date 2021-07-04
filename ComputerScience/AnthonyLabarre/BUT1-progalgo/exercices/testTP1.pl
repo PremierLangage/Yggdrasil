48bb4bc399925d6d9c5881615fa594efa75d71c2
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 17:29:03 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl

---
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl
@@ -0,0 +1,13 @@
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+title= Une fonction qui répond à **LA question**
+text==
+Ecrire une fonction **f** sans paramètre qui retourne l'entier 42.
+==
+pltest==
+>>> f == f # La fonction existe  ?
+True
+>>> f() # f retourne ou affiche  42
+42
+>>> f() == 42 # retourne 42 
+True
+==
