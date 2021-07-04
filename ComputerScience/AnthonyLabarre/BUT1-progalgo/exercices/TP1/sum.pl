453ffbdbdab861539a32a8de81422c2bf78024b6
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 19:05:31 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1/sum.pl
@@ -3,8 +3,25 @@
 #@ /ComputerScience/python/AP1-1920/templates/generic/grader.py
 
 @ /utils/sandboxio.py
+@ builder.py
+@ grader.py
+@ utils/test.py
+@ utils/mockinput.py
+@ utils/testgroup.html
+@ utils/testitem.html
 
-grader  =@ /grader/evaluator.py
+title = <em>(Pas de titre défini)</em>
+text = <em>(Pas d'énoncé défini)</em>
+
+# création de l'éditeur de code
+editor =: CodeEditor
+editor.language = python
+#editor.cid = py_editor_0
+editor.code ==
+# Saisissez votre code ici, et cliquez sur le bouton
+# de validation quand vous avez terminé
+
+==
 
 tag=Programmation|Python|Variable|Somme
 author = 
