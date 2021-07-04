b2d1555f76f5abe3a6d2e7ca0397ca2cb39d46cb
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 18:02:26 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl
@@ -1,23 +1,55 @@
-extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
-title= caluler une somme dans une variable
 
+# DR: test 24/09/2019 OK
+extends=../../templates/pltest.pl
+@ /builder/before.py [builder.py]
 
-@ /builder/before.py [builder.py] 
+tag=Programmation|Python|Initialisation|Variable
+author = CC + DR
+
+title = Initialisation et constantes
+
 
 text==
-En supposant qu'il existe déjà 2 variables x et y, écrire le code qui met leur somme dans une variable nommée s.
+ Initialisez la variable **{{var}}** avec une valeur quelconque
+ de type **{{type_var}}** 
 ==
 
-before==
+
+
+editor.height = 80
+
+before== #|python| 
 import random 
+random.seed(seed)
+type_var = random.choice(["int","str","float","bool",None])
+var = random.choice(["a", "b", "c", "X", "Y","PIERRE","feuille","ciseaux"])
 
-x,y = 40,2
-result = x+y
+pltest0="""
+>>> "{}" in globals() # La variable {} est elle définie ?
+True
+""".format(var, var)
+
+if type_var is None:
+    pltest1="""
+>>> type({}) is type(None) # La variables est elle du bon type ({}) ?
+True
+""".format(var, type_var, type_var)
+else:
+    pltest1="""
+>>> type({})=={} # La variables est elle du bon type ({}) ?
+True
+""".format(var, type_var, type_var)
+
+
+
+
 ==
 
-pltest==
->>> s == s # La variable existe  ?
-True
->>> s == 42 # s a pour valeur x+y
-True
-==
+
+
+
+
+
+
+
+
