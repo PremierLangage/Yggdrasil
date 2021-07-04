b9ace1323c93f5b16e3168531e936cb9fb8b443e
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 19:19:54 +0200

    cpivoteau modified Yggdrasil/ComputerScience/python/AP1-1920/templates/generic/grader.py

--- ComputerScience/python/AP1-1920/templates/generic/grader.py
+++ ComputerScience/python/AP1-1920/templates/generic/grader.py
@@ -24,6 +24,7 @@ import test
 
 
 def _get_student_code(exercise_context: dict):
+    print(dict)
     if "editor" not in exercise_context:
         raise test.GraderError(missing_editor)
     editor_id = exercise_context["editor"].cid
