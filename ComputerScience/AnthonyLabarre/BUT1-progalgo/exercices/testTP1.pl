b731e015c1b7471ce9a42599d58688ff034cf848
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-04 17:38:24 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/testTP1.pl
@@ -3,8 +3,9 @@ title= caluler une somme dans une variable
 text==
 En supposant qu'il existe déjà 2 variables x et y, écrire le code qui met leur somme dans une variable nommée s.
 ==
+
 pltest==
-x,y = 40,2
+>>> x,y = 40,2
 >>> s == s # La variable existe  ?
 True
 >>> s == x+y # s a pour valeur x+y
