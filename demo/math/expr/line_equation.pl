30a2005fc2651c649669e8948c59e6d20d111ec5
David DOYEN <David.Doyen@u-pem.fr>
2021-07-05 17:40:34 +0200

    ddoyen modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -1,10 +1,9 @@
 extends = /model/math/expr.pl
 
-
 before ==
 from mplsympy import plotsvg
 var('x')
-a = choice([-1, 1]) * sympify(choice(['1/2', 1, '3/2', 2]))
+a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
 b = randint(-3, 3)
 sol = a*x + b
 image = plotsvg(sol)
@@ -12,7 +11,7 @@ image = plotsvg(sol)
 
 question ==
 Déterminer l'équation de la droite tracée ci-dessous (en notant $! x !$ la variable d'abscisse et $! y !$ la variable d'ordonnée).
-<div class="img img-40">
+<div class="img img-60">
 {{ image }}
 </div>
 ==
