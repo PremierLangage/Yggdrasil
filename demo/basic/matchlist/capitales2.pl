73972186c0fbedd55c70927e8f7873508a4898a6
David DOYEN <David.Doyen@u-pem.fr>
2021-07-05 17:26:58 +0200

    ddoyen modified Yggdrasil/demo/basic/matchlist/capitales2.pl

---
+++ demo/basic/matchlist/capitales2.pl
@@ -0,0 +1,30 @@
+extends = /model/basic/matchlist.pl
+
+question ==
+Relier chaque pays à sa capitale.
+==
+
+nbmatches = 4
+
+matches ==
+Allemagne,Berlin
+Autriche,Vienne
+Belgique,Bruxelles
+Danemark,Copenhague
+Espagne,Madrid
+Finlande,Helsinki
+France,Paris
+Grèce,Athènes
+Hongrie,Budapest
+Irlande,Dublin
+Italie,Rome
+Norvège,Oslo
+Pays-Bas,Amsterdam
+Pologne,Varsovie
+Portugal,Lisbonne
+Roumanie,Bucarest
+Royaume-Uni,Londres
+Slovaquie,Bratislava
+Suède,Stockholm
+Suisse,Berne
+==
