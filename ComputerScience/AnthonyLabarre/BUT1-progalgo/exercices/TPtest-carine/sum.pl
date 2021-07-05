ee4569850af2a2b6b11efd5953755862b270c78f
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-05 13:19:39 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/sum.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/sum.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/sum.pl
@@ -15,12 +15,7 @@ editor =: CodeEditor
 editor.theme = light
 editor.codes %=
 [
-    { "language": "csharp", "code": "// write a csharp code here" },
-    { "language": "c", "code": "// write a c code here" },
-    { "language": "cpp", "code": "// write a cpp code here" },
-    { "language": "java", "code": "// write a java code here" },
-    { "language": "python", "code": "# write a python code here" },
-    { "language": "javascript", "code": "// write a javascript code here" }
+   
 ]
 ==
 editor.code ==
