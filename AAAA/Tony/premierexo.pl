1f1cccf7128fcfb605fa21bbc2a78f8838ed5f6e
tony fevrier <tony.fevrier62@gmail.com>
2021-07-06 16:27:28 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/premierexo.pl

--- AAAA/Tony/premierexo.pl
+++ AAAA/Tony/premierexo.pl
@@ -15,9 +15,12 @@ P = choice([1,5,4,6,5])
 sol = A+P
 ==
 
-#f pour mettre une variable dans le résultat.
+#f pour mettre une variable dans le résultat. ans est la réponse
 evaluator ==
 
+if not isinstance(ans,int):
+    score =-1
+    feedback = "Apprend ton cours la réponse est un entier"
 if ans != sol :
     score =0
     feedback = f"T es nul c'est {sol}"
