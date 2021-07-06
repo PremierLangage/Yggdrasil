532e6c13f68e30942d0a16d96b5c2fb91b39eb72
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-06 16:06:29 +0200

    drevuz modified Yggdrasil/demo/basic/numeric/addition_aleatoire.pl

--- demo/basic/numeric/addition_aleatoire.pl
+++ demo/basic/numeric/addition_aleatoire.pl
@@ -1,11 +1,16 @@
 extends = /model/basic/numeric.pl
 
+
+# Creation d'exercice 
 before ==
+# TIRAGE ALEATOIRE
 a = randint(10, 50)
 b = randint(10, 50)
+
+# CALCUL DE LA BONNE RÉPONSE 
 sol = a + b
 ==
 
 question ==
-Calculer {{ a }} + {{ b }}.
+Calculer {{ a }} + {{ b }}. a +b . 
 ==
