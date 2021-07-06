fb8a160af502e68a2f0d40d372e902b79f48fdcc
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-06 14:55:50 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
@@ -9,14 +9,19 @@ import random
 random.seed(seed)
 
 
-a = random.randint(1,10)
-b = random.randint(1,10)
+a = random.randint(1,1000)
+b = random.randint(1,1000)
 
 try:
     exec(response['answer'])
 except Exception as e:
     grade=(0,""" Votre code ne compile pas""")
 else:
+    try:
+        if not (type(somme)  is int):
+            grade = (0,'<span class="fail-state">la variable **somme** n\'a pas le bon type</span>')
+    except Exception as e:
+    grade=(0,'<span class="fail-state">la variable **somme** n\'existe pas</span>')
     if somme == a+b :
         grade = (100,'<span class="success-state">Correct !</span>')
     else:
