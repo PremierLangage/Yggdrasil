d41c2c1965123657a08cabfd11376cf165484496
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-06 15:13:16 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
@@ -15,7 +15,7 @@ b = random.randint(1,1000)
 try:
     exec(response['answer'])
 except Exception as e:
-    grade=(0,'<span class="error-state">Le code ne compile pas, il provoque l\'erreur suivante : </span>' + str(e))
+    grade=(0,'<span class="error-state">Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e) + '</span>')
 else:
     try:
         somme = somme+0
