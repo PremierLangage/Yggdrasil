69a5bea30085ef16687086004da223659aeb8d6e
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-06 16:02:37 +0200

    drevuz modified Yggdrasil/demo/basic/numeric/addition.pl

--- demo/basic/numeric/addition.pl
+++ demo/basic/numeric/addition.pl
@@ -1,7 +1,14 @@
 extends = /model/basic/numeric.pl
 
+# deux variables 
+# question qui l'énoncé de l'exercice 
+# sol qui contient une réponse numérique 
+
+
 question ==
-Calculer 15 + 7.
+
+Calculer la somme de 15 et 7.
 ==
 
 sol = 22
+
