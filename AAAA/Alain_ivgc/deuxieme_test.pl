494bcc90075f11fa1f86b7424a079e1b69acabd1
alain virouleau <alain.virouleau@universite-paris-saclay.fr>
2021-07-06 16:42:17 +0200

    avirouleau modified Yggdrasil/AAAA/Alain_ivgc/deuxieme_test.pl

--- AAAA/Alain_ivgc/deuxieme_test.pl
+++ AAAA/Alain_ivgc/deuxieme_test.pl
@@ -18,13 +18,3 @@ d = randint(-3, 3)
 sol = (d-b)/(c-a)
 
 ==
-
-# clé pour entrer un script au moment de l'évaluation de la réponse
-# connaît ce qu'il y a dans before et ce qu'il y a dans ans (la réponse de l'étudiant)
-# voir un exemple dans numeric/findnumber.pl mais je trouve pas ce fichier
-# dans l'evaluator, on donne un score entre 0 et 100, -1 si on veut retourner une erreur de syntaxe
-
-evaluator==
-
-
-==
