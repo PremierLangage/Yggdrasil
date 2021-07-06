2cfdb89822f0d580b85d24447e862c8f263fe574
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-06 16:51:13 +0200

    drevuz modified Yggdrasil/demo/basic/numeric/challenge.pl

--- demo/basic/numeric/challenge.pl
+++ demo/basic/numeric/challenge.pl
@@ -4,8 +4,8 @@ extends = /model/basic/numeric.pl
 before ==
 from math import gcd
 k = choice([1,2,3])
-a = randint(5, 10)
-b = randint(5, 10)
+a = k*randint(5, 10)
+b = k*randint(5, 10)
 c = a*b//gcd(a, b) + randint(1, 5)
 ==
 
@@ -17,12 +17,12 @@ evaluator ==
 if not isinstance(ans, int):
     score = -1
     feedback = "La réponse doit être un entier."
-elif ans % k != 0:
+elif ans % a != 0 or ans % b != 0:
     score = 0
-    feedback = "Ce nombre n'est pas un multiple de " + str(k) + "."
-elif ans < a or ans > b:
+    feedback = f"Ce nombre n'est pas un multiple de {a} et {b}"
+elif ans <= c:
     score = 0
-    feedback = f"Ce nombre n'est pas compris entre {a} et {b}."
+    feedback = f"Ce nombre n'est pas inférieur à {c}."
 else:
     score = 100
 ==
