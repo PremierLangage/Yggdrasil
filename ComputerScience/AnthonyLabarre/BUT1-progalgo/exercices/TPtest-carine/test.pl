c4a49fe4823b98c6e5c0630884b99419e325b4e7
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-06 15:17:27 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
@@ -23,7 +23,7 @@ else:
         grade=(0,'<span class="error-state">la variable  <strong>somme</strong> n\'existe pas</span>')
     else :
         if somme == a+b :
-            grade = (100,u'<span class="success-state">Bonne r&#233;ponse !</span>')
+            grade = (100,u'<span class="success-state"> Bonne r&#233;ponse ! </span>')
         else:
             grade = (0,'<span class="error-state">la variable <strong>somme</strong> n\'a pas la bonne valeur</span>')
 ==
