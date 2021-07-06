ab3b9e814c39eb1f1bd36f6a995c533028ea5a10
David DOYEN <David.Doyen@u-pem.fr>
2021-07-06 13:06:54 +0200

    ddoyen modified Yggdrasil/model/math/poly.pl

--- model/math/poly.pl
+++ model/math/poly.pl
@@ -13,7 +13,7 @@ evaluator ==
 from ast import literal_eval
 from evalsympy import eval_poly
 from sympy import poly
-poly_var = poly(sol).gen
+poly_var = str(poly(sol).gen)
 score, error = eval_poly(answers['math'], sol, var='x', form=poly_form)
 feedback = message[error]
 ==
