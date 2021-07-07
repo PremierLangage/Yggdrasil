f2cfec595636ca9d5eb5b719d4d75f800515f1d9
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 16:29:35 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py
@@ -1,3 +1,13 @@
+
+
+_dico_rmk={
+    ';' : 'le point-virgule est inutile',
+    '(' : 'les parenth&#232;ses sont inutiles',
+    '++' : 'un seul + suffit',
+    '\n' : "le code peut s'écrire sur une seule ligne"
+}
+
+
 def good(msg):
     return u'<span class="success-state"> ' + msg + ' </span>'
 
@@ -6,6 +16,11 @@ def wrong(msg):
 
 def rmk(msg):
     return u'<span style="color:blue;"></br></br> Remarque : ' + msg + ' :) </span>'
+
+def remarks(lst,code):
+    for p in lst:
+        if(p in code):
+            return rmk(_dico_rmk[p])
 
 def grade_wrong(msg):
     return (0, wrong(msg))
