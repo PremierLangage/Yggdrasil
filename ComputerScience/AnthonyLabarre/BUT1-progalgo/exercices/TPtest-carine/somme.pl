e82dffd3afaa4e3075767be577482a23235dcfbf
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2021-07-07 14:28:06 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/somme.pl

---
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/somme.pl
@@ -0,0 +1,76 @@
+# coding: utf-8
+
+@ /utils/sandboxio.py
+
+# pour importer ses propres modules
+@ ../display_tools.py
+
+grader=@ /grader/evaluator.py
+
+
+title== #|html|
+Somme de variables
+==
+
+text== #|html|
+Initialisez la variable **somme** avec la somme des variables **a** et **b** (qui existent déjà).
+==
+
+#un éditeur simple, fond blanc, pas de chois de langage
+form=@ /form/text_editor.html 
+editor.setOptions({
+    maxLines: 1, // make it 1 line
+    autoScrollEditorIntoView: true,
+    highlightActiveLine: false,
+    printMargin: false,
+    showGutter: false,
+    mode: "ace/mode/javascript",
+    theme: "ace/theme/tomorrow_night_eighties"
+});
+
+# Script d'évaluation 
+evaluator== #|python|
+import random
+import sys 
+import display_tools as display
+
+#print("debug : " + response['answer'], file=sys.stderr) 
+
+a = random.randint(1,1000)
+b = random.randint(1,1000)
+
+try:
+    exec(response['answer'])
+except Exception as e:
+    grade = display.grade_wrong('Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e))
+    quit()
+else:
+    try:
+        somme+=0 # permet de lever une erreur si la variable n'existe pas
+    except Exception as e:
+        grade = display.grade_wrong('la variable  <strong>somme</strong> n\'existe pas')
+    else :
+        if somme == a+b :
+            msg = display.good('Bonne r&#233;ponse !')
+            if(';' in response['answer']):
+                msg += display.rmk('le point-virgule est inutile')
+            if('(' in response['answer']):
+                msg += display.rmk('les parenth&#232;ses sont inutiles')
+            if('++' in response['answer']):
+                msg += display.rmk('un seul + suffit')
+            grade = (100, msg)
+        else:
+            if('==' in response['answer']):
+                grade = display.grade_wrong('== ne permet pas de faire une affectation')
+            else:
+                grade = display.grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
+==
+
+
+
+
+
+
+
+
+
