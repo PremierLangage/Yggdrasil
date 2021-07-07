8cfdf8829efc6945bcf8686b49db9cad536a3389
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 09:52:57 +0200

    ddoyen modified Yggdrasil/demo/math/interval/inequation_product.pl

--- demo/math/interval/inequation_product.pl
+++ demo/math/interval/inequation_product.pl
@@ -1,17 +1,16 @@
 extends = /model/math/interval.pl
 
-title = Résoudre une inéquation produit
 
 before ==
 from sympy import solveset, S
 x = Symbol('x')
-a, b = sample(range(-6, 6), k=2)
+a, b = sampleint(-6, 6, 2)
 expr = (x + a) * (x + b)
 ineq = choice([expr >= 0, expr > 0, expr <= 0,expr < 0])
 sol = solveset(ineq, x, domain=S.Reals)
 ==
 
-text ==
+question ==
 Déterminer l'ensemble des réels $! x !$ tels que $$ {{ ineq|latex }}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
 ==
 
