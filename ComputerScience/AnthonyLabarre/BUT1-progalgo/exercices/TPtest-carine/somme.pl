c0a9276af5c3e157764e78f3b13e9a9d1bcde9db
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 17:28:02 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/somme.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/somme.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/somme.pl
@@ -29,6 +29,15 @@ code = response['answer']
 a = random.randint(1,1000)
 b = random.randint(1,1000)
 
+
+def check(code, name, var, val):
+    if var != val :
+        return tools.grade_wrong('la variable <strong>' + name + '</strong> n\'a pas la bonne valeur')
+
+    # ajout des éventuelles remarques de syntaxe
+    return 100, good('Bonne r&#233;ponse !') + remarks([';', '(', '++', '\n'], code)
+
+
 # tente d'executer, puis verifie la présence de la variable demandée. si ok, check valeur et syntaxe.
 try:
     exec(code)
@@ -39,7 +48,7 @@ else:
     if not 'somme' in locals():
         grade = tools.grade_wrong("La variable  <strong>somme</strong> n'existe pas")
     else :
-        grade = tools.check(code, 'somme', somme, a+b)
+        grade = check('somme', somme, a+b)
         del somme # pour eviter que la variable existe si l'on change le code sans refresh.
 ==
 
