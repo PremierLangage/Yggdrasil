605e1504031c276381d352ab348edeb8337ce982
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 15:47:44 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
@@ -26,31 +26,32 @@ import random
 import sys 
 import display_tools as display
 
-#print("debug : " + response['answer'], file=sys.stderr) 
+code = response['answer']
+#print("debug : " + code, file=sys.stderr) 
 
 a = random.randint(1,1000)
 b = random.randint(1,1000)
 
 def check(somme):
-    if somme == a+b :
-        msg = display.good('Bonne r&#233;ponse !')
-        if(';' in response['answer']):
-            msg += display.rmk('le point-virgule est inutile')
-        if('(' in response['answer']):
-            msg += display.rmk('les parenth&#232;ses sont inutiles')
-        if('++' in response['answer']):
-            msg += display.rmk('un seul + suffit')
-        grade = (100, msg)
-    else:
-        if('==' in response['answer']):
-            grade = display.grade_wrong('== ne permet pas de faire une affectation')
-        else:
-            grade = display.grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
-    return grade
+    if('==' in code):
+        return display.grade_wrong('== ne permet pas de faire une affectation')
+    
+    if somme != a+b :
+        return display.grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
+
+    msg = display.good('Bonne r&#233;ponse !')
+    if(';' in code):
+        msg += display.rmk('le point-virgule est inutile')
+    if('(' in code):
+        msg += display.rmk('les parenth&#232;ses sont inutiles')
+    if('++' in code):
+        msg += display.rmk('un seul + suffit')
+    return (100, msg)
 
 
+# tente d'executer, puis verifie la présence de la variable demandée. si ok, check valeur et syntaxe.
 try:
-    exec(response['answer'])
+    exec(code)
 except Exception as e:
     grade = display.grade_wrong('Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e))
 else:
@@ -58,7 +59,7 @@ else:
         grade = display.grade_wrong('la variable  <strong>somme</strong> n\'existe pas')
     else :
         grade = check(somme)
-        del somme
+        del somme # pour eviter que la variable existe si l'on change le code sans refresh.
 ==
 
 
