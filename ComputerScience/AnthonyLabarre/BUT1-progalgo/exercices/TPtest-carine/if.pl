0a1f6bdc57cd17bd7408fab69a8284323e9bd80f
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 17:12:35 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/if.pl

---
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/if.pl
@@ -0,0 +1,52 @@
+# coding: utf-8
+@ /utils/sandboxio.py
+
+# pour importer ses propres modules
+@ ../tools.py
+
+grader=@ /grader/evaluator.py
+
+title== #|html|
+Test et affichage
+==
+
+text== #|html|
+Affichez 'OK' si la variable **x** (qui existe déjà) vaut 42, ou 'KO' dans tous les autres cas.
+==
+
+#un éditeur simple, fond blanc, pas de chois de langage
+form=@ /form/text_editor.html 
+
+# Script d'évaluation 
+evaluator== #|python|
+import random
+import sys 
+import tools # fonctions auxiliaires
+
+code = response['answer']
+#print("debug : ", code, file=sys.stderr) 
+
+code2 = code.replace("print(","foofoo=(")
+
+x = random.randint(41,43)
+
+# tente d'executer, puis verifie la présence de la variable demandée. si ok, check valeur et syntaxe.
+try:
+    exec(code)
+except Exception as e:
+    msg = tools.wrong("Le code ne compile pas, il provoque l'erreur suivante : " + str(e))
+    grade = 0, msg #+ tools.remarks(['==','<-',':='], code)
+else:
+    grade = tools.check(code, 'foofoo', foofoo, 'OK' if x==42 else 'KO')
+    del foofoo # pour eviter que la variable existe si l'on change le code sans refresh.
+==
+
+
+
+
+
+
+
+
+
+
