884ef6efab3536d5690d39bf354064b1767f81c5
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 14:33:41 +0200

    ddoyen modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -7,9 +7,25 @@ Parmi ces villes, lesquelles ne sont pas des capitales ?
 right ==
 Barcelone
 Milan
+New-York
+Montréal
+Saint-Pétersbourg
+Genève
+Bombay
+Shanghai
 ==
 
 wrong ==
 Paris
 Berlin
+Rome
+Madrid
+Moscou
+Ottawa
+Washington
+Pékin
 ==
+
+nbitems = 4
+minright = 1
+maxright = 2
