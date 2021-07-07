3ea3943ff7884c7cb5e85e1c366e4dc4ae18fd53
tony fevrier <tony.fevrier62@gmail.com>
2021-07-07 14:26:57 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/secondeexo.pl

--- AAAA/Tony/Exos/secondeexo.pl
+++ AAAA/Tony/Exos/secondeexo.pl
@@ -1,1 +1,13 @@
 extends = /model/basic/radio.pl
+
+question ==
+Quelle est la couleur du cheval blanc d'Henri 4?
+==
+
+#Par défaut le premier item est la bonne réponse, et les items sont mélangés
+items ==
+blanc
+noir
+je sais pas je suis daltonien
+la réponse d
+==
