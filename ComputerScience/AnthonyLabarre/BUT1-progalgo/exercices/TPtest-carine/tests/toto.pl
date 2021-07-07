d030988d61835c6f366f26eb0a12d5ad162372c4
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 13:59:06 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/toto.pl

---
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/toto.pl
@@ -0,0 +1,24 @@
+
+@ /utils/sandboxio.py
+@ /grader/evaluator.py [grader.py]
+
+form==
+<input id="form_answer" type="number" value="{{ answers__.answer }}" required/>
+==
+
+op1 = 32
+op2 = 26
+
+evalfunc==
+import traceback
+import sys
+
+def evalfunc(dic, answers):
+    try: 
+        if int(answers['answer']) == dic['op1'] + dic['op2']:
+            return (100, "Bonne réponse")
+        return (0, "Mauvaise réponse")
+    except:
+        print(traceback.format_exc(), file=sys.stderr)
+        return(-1, "Merci de rentrer un entier")
+==
