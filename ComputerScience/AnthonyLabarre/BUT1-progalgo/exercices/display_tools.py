9cdc2ce0e7f9066a6e66e795040a485b8c369459
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 10:22:48 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py
@@ -1,2 +1,9 @@
 def good(msg):
     return u'<span class="success-state"> ' + msg + ' </span>'
+
+def wrong(msg):
+    return u'<span class="error-state"> ' + msg + ' </span>'
+
+
+def grade_wrong(msg):
+    return grade = (0, wrong(msg))
