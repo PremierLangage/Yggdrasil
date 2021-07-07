a216d6e72175ff0d53208be5b903a0b41e94f2d7
Jean Marc Meunier <jean-marc.meunier@univ-paris8.fr>
2021-07-07 14:32:05 +0200

    jmeunier1 modified Yggdrasil/AAAA/JM_Meunier/exo.pl

--- AAAA/JM_Meunier/exo.pl
+++ AAAA/JM_Meunier/exo.pl
@@ -10,4 +10,11 @@ Non
 Peut-être
 ==
 
+# paramètres optionnels
+#  pour ne pas mélanger les items mettre shuffeld to false
+
+shuffeld = false
+
+# pour indiquer une autre bonne réponse que la BR par défaut, changer indsol
+indsol = 1
 
