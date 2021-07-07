850cb457455e9276cd4ae7c784f829915410e01f
nathalie Corson <nathalie.corson@univ-lehavre.fr>
2021-07-07 14:29:31 +0200

    ncorson modified Yggdrasil/AAAA/NathC/QCM1.pl

---
+++ AAAA/NathC/QCM1.pl
@@ -0,0 +1,19 @@
+
+extends = /model/basic/radio.pl
+
+
+
+question ==
+Quel pays a pour capitale Paris ? 
+==
+
+items ==
+France
+Allemagne
+Italie
+Espagne
+==
+
+# pour mélanger les réponses :
+
+shuffled  = False
