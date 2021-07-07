ab071e73f76b200a0ba63bb05d930ac565ab1e0e
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 16:48:33 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
@@ -3,7 +3,7 @@
 @ /utils/sandboxio.py
 
 # pour importer ses propres modules
-@ ../../display_tools.py
+@ ../../tools.py
 
 grader=@ /grader/evaluator.py
 
@@ -23,7 +23,7 @@ form=@ /form/text_editor.html
 evaluator== #|python|
 import random
 import sys 
-import display_tools as display
+import tools # fonctions auxiliaires
 
 code = response['answer']
 #print("debug : ", code, file=sys.stderr) 
@@ -31,27 +31,18 @@ code = response['answer']
 a = random.randint(1,1000)
 b = random.randint(1,1000)
 
-# teste si la variable demandée existe (et pas d'erreur de compil, évidement)
-def check(somme):
-    # bonne valeur ?
-    if somme != a+b :
-        return display.grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
-
-    # ajout des éventuelles remarques de syntaxe
-    return 100, display.good('Bonne r&#233;ponse !') + display.remarks([';','('], code)
-
 
 # tente d'executer, puis verifie la présence de la variable demandée. si ok, check valeur et syntaxe.
 try:
     exec(code)
 except Exception as e:
-    msg = display.wrong("Le code ne compile pas, il provoque l'erreur suivante : " + str(e))
-    grade = 0, msg + display.remarks(['==','<-',':='], code)
+    msg = tolls.wrong("Le code ne compile pas, il provoque l'erreur suivante : " + str(e))
+    grade = 0, msg + tolls.remarks(['==','<-',':='], code)
 else:
     if not 'somme' in locals():
-        grade = display.grade_wrong("La variable  <strong>somme</strong> n'existe pas")
+        grade = tolls.grade_wrong("La variable  <strong>somme</strong> n'existe pas")
     else :
-        grade = check(somme)
+        grade = tools.check('somme',somme,a+b)
         del somme # pour eviter que la variable existe si l'on change le code sans refresh.
 ==
 
