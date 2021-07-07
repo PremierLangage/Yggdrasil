289d3ef26b99846eab1362cce17b5e7effd7a0dc
tony fevrier <tony.fevrier62@gmail.com>
2021-07-07 14:36:12 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/secondeexo.pl

--- AAAA/Tony/Exos/secondeexo.pl
+++ AAAA/Tony/Exos/secondeexo.pl
@@ -1,7 +1,8 @@
 extends = /model/basic/radio.pl
 
 #Pour plusieurs réponses sélectionnables  : checkbox. indsol doit être la liste des indices de réponse.
-#checkbox_rw pour entrer les bonnes réponses et les mauvaises : intéressant pour l'aléa : permet de donner un ensemble de bonees et mauvaises réponses et de définir le nb de bonnes/mauvaises réponses qu'il doit prendre.
+#checkbox_rw pour entrer les bonnes réponses et les mauvaises : intéressant pour l'aléa : permet de donner 
+#un ensemble de bonnes et mauvaises réponses et de définir le nb de bonnes/mauvaises réponses qu'il doit prendre. On peut modifier le barême.
 
 question ==
 Quelle est la couleur du cheval blanc d'Henri 4?
