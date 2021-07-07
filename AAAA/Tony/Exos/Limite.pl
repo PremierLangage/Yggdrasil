b47e49b648dc957f5c565fcc642f993cdb613e7a
tony fevrier <tony.fevrier62@gmail.com>
2021-07-07 15:37:01 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/Limite.pl

---
+++ AAAA/Tony/Exos/Limite.pl
@@ -0,0 +1,22 @@
+
+extends = /model/math/expr.pl
+
+before ==
+from sympy import Limit
+
+x = Symbol('x')
+
+f, g = sample([2*x+1, x+2, 2*x**2+1, x**2+1], 2)
+lim = Limit(f/g, x, oo)
+sol = lim.doit()
+==
+
+question ==
+Déterminer la limite suivante.
+==
+
+prefix ==
+$! \displaystyle {{ lim|latex }} = !$
+==
+
+keypad = ["+infty", "-infty", "n'existe pas"]
