32a2d977e91a92297b275f57a6c76a2c0258fbe1
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 16:01:05 +0200

    ddoyen modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -5,10 +5,9 @@ from sympy import Symbol, ln
 from sympy2latex import latex
 v = Symbol('t')
 expr = ln(v) + 1/(1 + v**2)
-expr_latex = latex(expr)
 ==
 
 question ==
 Soit la fonction $! f : \mathbb{R} \rightarrow \mathbb{R} !$ telle que
-$$ f(x) = {{ expr_latex }} . $$
+$$ f(x) = {{ expr|latex }} . $$
 ==
