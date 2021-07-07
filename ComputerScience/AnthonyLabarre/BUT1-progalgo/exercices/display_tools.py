400618e6d561e90c580a4b31c11de5031225d4eb
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 16:34:22 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/display_tools.py
@@ -1,10 +1,13 @@
-
+# dico des remarques ###
 _dico_rmk={
     ';' : 'le point-virgule est inutile',
     '(' : 'les parenth&#232;ses sont inutiles',
     '++' : 'un seul + suffit',
     '\n' : "le code peut s'écrire sur une seule ligne"
 }
+for p in ['==','<-',':='] :
+    _dico_rmk[p]= p + ' ne permet pas de faire une affectation'
+###
 
 def good(msg):
     return u'<span class="success-state"> ' + msg + ' </span>'
@@ -13,7 +16,7 @@ def wrong(msg):
     return u'<span class="error-state"> ' + msg + ' </span>'
 
 def rmk(msg):
-    return u'<span style="color:blue;"></br></br> Remarque : ' + msg + ' :) </span>'
+    return u'<span style="color:blue;"></br></br> Remarque : ' + msg + ' </span>'
 
 def remarks(lst,code):
     msg = ''
