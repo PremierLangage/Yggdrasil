045b3005b45b1e3ea8adab9be8491fd683017999
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 10:43:02 +0200

    ddoyen modified Yggdrasil/demo/math/tuple/image_application.pl

--- demo/math/tuple/image_application.pl
+++ demo/math/tuple/image_application.pl
@@ -6,7 +6,7 @@ p0 = randint(-5, 5)
 sol = Tuple(n0+p0, n0-p0)
 ==
 
-text ==
+question ==
 On considère la fonction $! f : \mathbb{Z}^2 \rightarrow \mathbb{Z}^2  !$  définie par :
 $$ f(n, p)= (n+p, n-p) .$$
 
