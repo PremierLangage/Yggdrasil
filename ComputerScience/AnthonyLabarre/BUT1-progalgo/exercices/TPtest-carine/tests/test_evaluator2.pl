4de88f670bc361c72d99838814a4bfa25048a244
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 15:32:59 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
@@ -26,44 +26,35 @@ import random
 import sys 
 import display_tools as display
 
+#print("debug : " + response['answer'], file=sys.stderr) 
 
 a = random.randint(1,1000)
 b = random.randint(1,1000)
 
-def step1():
-    print("debug : " + response['answer'], file=sys.stderr) 
+try:
+    exec(response['answer'])
+except Exception as e:
+    grade = display.grade_wrong('Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e))
+else:
     try:
-        exec(response['answer'])
-        print("debug : " + somme, file=sys.stderr)       
+        somme+=0 # permet de lever une erreur si la variable n'existe pas
     except Exception as e:
-        return None, display.grade_wrong('Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e))
-    else: 
-        try:
-            somme+=0 # permet de lever une erreur si la variable n'existe pas
-        except Exception as e:
-            return None, display.grade_wrong('la variable  <strong>somme</strong> n\'existe pas')
-        else :
-            return somme, 100
-
-somme,grade=step1()
-
-# def step2(somme):
-#     if somme == a+b :
-#         msg = display.good('Bonne r&#233;ponse !')
-#         if(';' in response['answer']):
-#             msg += display.rmk('le point-virgule est inutile')
-#         if('(' in response['answer']):
-#             msg += display.rmk('les parenth&#232;ses sont inutiles')
-#         if('++' in response['answer']):
-#             msg += display.rmk('un seul + suffit')
-#         grade = (100, msg)
-#     else:
-#         if('==' in response['answer']):
-#             grade = display.grade_wrong('== ne permet pas de faire une affectation')
-#         else:
-#             grade = display.grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
-# somme=step1()
-# step2(somme)
+        grade = display.grade_wrong('la variable  <strong>somme</strong> n\'existe pas')
+    else :
+        if somme == a+b :
+            msg = display.good('Bonne r&#233;ponse !')
+            if(';' in response['answer']):
+                msg += display.rmk('le point-virgule est inutile')
+            if('(' in response['answer']):
+                msg += display.rmk('les parenth&#232;ses sont inutiles')
+            if('++' in response['answer']):
+                msg += display.rmk('un seul + suffit')
+            grade = (100, msg)
+        else:
+            if('==' in response['answer']):
+                grade = display.grade_wrong('== ne permet pas de faire une affectation')
+            else:
+                grade = display.grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
 ==
 
 
