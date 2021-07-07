352cb26cdab6500a678206a9e946fa8be76122c6
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2021-07-07 14:13:14 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1-test-anthony/somme_anthony.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1-test-anthony/somme_anthony.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1-test-anthony/somme_anthony.pl
@@ -18,7 +18,7 @@ Initialisez la variable **somme** avec la somme des variables **a** et **b** (qu
 
 #un éditeur simple, fond blanc, pas de chois de langage
 form=@ /form/text_editor.html 
-editor.setOption("maxLines", 1)
+editor.setOption("maxLines", "1")
 
 # Script d'évaluation 
 evaluator== #|python|
