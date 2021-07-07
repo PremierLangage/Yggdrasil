80c839bf7a436098f9426b97b1326ea809842608
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 16:05:57 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/tests/test_evaluator2.pl
@@ -54,7 +54,7 @@ except Exception as e:
     for p in ['==','<-',':='] :
         if p in code:
             msg += display.rmk(p + ' ne permet pas de faire une affectation')
-    return 0,msg
+    grade 0,msg
 else:
     if not 'somme' in locals():
         grade = display.grade_wrong('la variable  <strong>somme</strong> n\'existe pas')
