b7b2ffdca4ddb71c68cea3140be7a5a9a5abd1ab
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 20:24:26 +0200

    ddoyen modified Yggdrasil/demo/multi/multi.pl

---
+++ demo/multi/multi.pl
@@ -0,0 +1,23 @@
+extends = /model/basic/basic.pl
+@ /utils/components/radio.py [radio.py]
+
+before ==
+radio1 = RadioGroup()
+radio2 = RadioGroup()
+
+items = [1, 2, 3, 4]
+
+radio1.set_items(items)
+radio1.set_sol(1)
+
+radio2.set_items(items)
+radio2.set_sol(1)
+==
+
+inputblock ==
+{{ radio|component }}
+==
+
+evaluator ==
+score = radio.eval()
+==
