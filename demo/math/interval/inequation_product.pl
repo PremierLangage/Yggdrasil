65749d139cd8b3c86fc50390647df0d478b2619a
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 09:53:23 +0200

    ddoyen modified Yggdrasil/demo/math/interval/inequation_product.pl

--- demo/math/interval/inequation_product.pl
+++ demo/math/interval/inequation_product.pl
@@ -14,4 +14,4 @@ question ==
 Déterminer l'ensemble des réels $! x !$ tels que $$ {{ ineq|latex }}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
 ==
 
-keypad % [{"label": "$! \\infty !$", "action": "cmd", "value": "\\infty"}, {"label": "$! \\cup !$", "action": "cmd", "value": "\\cup"}]
+keypad = ["-infty", "+infty", "cup"]
