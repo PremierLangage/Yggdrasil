f8dbe31a3baff3470c8ac6f0ed65a59da989016d
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 15:15:37 +0200

    ddoyen modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -6,9 +6,10 @@ before ==
 f = open('pays_europe.csv')
 row = csv_choice(f)
 pays = row['pays']
+article = row['article']
 sol = row['capitale']
 ==
 
 question ==
-Quel est la capitale de {{ pays }} ?
+Quel est la capitale de { {article }} {{ pays }} ?
 ==
