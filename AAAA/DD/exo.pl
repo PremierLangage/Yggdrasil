b85f21373a6ee4165cdc8639ff716d86c9f56721
David DOYEN <David.Doyen@u-pem.fr>
2021-07-07 16:53:15 +0200

    ddoyen modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -11,3 +11,5 @@ question ==
 On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. 
 Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
 ==
+
+complex_form = cartesian
