b9b7140a120b5e241774c95557a0f7880c6ffcc1
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2021-07-07 13:33:03 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
@@ -2,7 +2,7 @@
 
 @ /utils/sandboxio.py
 
-# pour importer ses propres modules en spécifiant le dossier
+# pour importer ses propres modules
 @ ../display_tools.py
 
 grader=@ /grader/evaluator.py
@@ -34,28 +34,27 @@ b = random.randint(1,1000)
 try:
     exec(response['answer'])
 except Exception as e:
-    grade = grade_wrong('Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e))
-    exit()
-
-try:
-    somme+=0 # permet de lever une erreur si la variable n'existe pas
-except Exception as e:
-    grade = grade_wrong('la variable  <strong>somme</strong> n\'existe pas')
-else :
-    if somme == a+b :
-        msg = display.good('Bonne r&#233;ponse !')
-        if(';' in response['answer']):
-            msg += '<span style="color:blue;"></br></br> Remarque : le point-virgule est inutile :)</span>'
-        if('(' in response['answer']):
-            msg += '<span style="color:blue;"></br></br> Remarque : les parenth&#232;ses sont inutiles :)</span>'
-        if('++' in response['answer']):
-            msg += '<span style="color:blue;"></br></br> Remarque : un seul + suffit :)</span>'
-        grade = (100, msg)
-    else:
-        if('==' in response['answer']):
-            grade = grade_wrong('== ne permet pas de faire une affectation')
+    grade = display.grade_wrong('Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e))
+else:
+    try:
+        somme+=0 # permet de lever une erreur si la variable n'existe pas
+    except Exception as e:
+        grade = display.grade_wrong('la variable  <strong>somme</strong> n\'existe pas')
+    else :
+        if somme == a+b :
+            msg = display.good('Bonne r&#233;ponse !')
+            if(';' in response['answer']):
+                msg += '<span style="color:blue;"></br></br> Remarque : le point-virgule est inutile :)</span>'
+            if('(' in response['answer']):
+                msg += '<span style="color:blue;"></br></br> Remarque : les parenth&#232;ses sont inutiles :)</span>'
+            if('++' in response['answer']):
+                msg += '<span style="color:blue;"></br></br> Remarque : un seul + suffit :)</span>'
+            grade = (100, msg)
         else:
-            grade = grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
+            if('==' in response['answer']):
+                grade = display.grade_wrong('== ne permet pas de faire une affectation')
+            else:
+                grade = display.grade_wrong('la variable <strong>somme</strong> n\'a pas la bonne valeur')
 ==
 
 
