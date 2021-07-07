3cba80cfe66f338625e4c9dc7235970616db2b23
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-07 13:44:16 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TPtest-carine/test.pl
@@ -35,7 +35,7 @@ try:
     exec(response['answer'])
 except Exception as e:
     grade = display.grade_wrong('Le code ne compile pas, il provoque l\'erreur suivante : ' + str(e))
-    return
+    quit()
 else:
     try:
         somme+=0 # permet de lever une erreur si la variable n'existe pas
