a7c0e3ea645833d437e8edf4d1d8d5b3f47c305b
Anthony LABARRE <Anthony.Labarre@u-pem.fr>
2021-07-07 14:20:36 +0200

    alabarre modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1-test-anthony/somme_anthony.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1-test-anthony/somme_anthony.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP1-test-anthony/somme_anthony.pl
@@ -24,7 +24,7 @@ text== #|html|
 Initialisez la variable **somme** avec la somme des variables **a** et **b** (qui existent déjà).
 <script>
     editor.setOption("maxLines", 1);
-        editor.setTheme("dark");
+        form.setTheme("dark");
 
 </script>
 
