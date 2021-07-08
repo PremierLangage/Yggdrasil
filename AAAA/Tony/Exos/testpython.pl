227965634c4c1e8fdcc367606e4a982bce89d967
tony fevrier <tony.fevrier62@gmail.com>
2021-07-08 15:15:14 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/testpython.pl

--- AAAA/Tony/Exos/testpython.pl
+++ AAAA/Tony/Exos/testpython.pl
@@ -1,6 +1,8 @@
 
 extends = /ComputerScience/python/AP1-1920/templates/pltest.pl 
 
+title = "a"
+
 
 #tests de la fonction que l'étudiant va entrer : copier coller les retours du terminal.
 pltest == 
