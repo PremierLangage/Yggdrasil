c1dce96bfc18614f12f538d340a43f7b592f16c6
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-08 14:58:00 +0200

    drevuz modified Yggdrasil/AAAA/dominique/AEXO/exodepython.pl

--- AAAA/dominique/AEXO/exodepython.pl
+++ AAAA/dominique/AEXO/exodepython.pl
@@ -1,2 +1,15 @@
-extend= /ComputerScience/python/AP1-1920/templates/pltest.pl
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
 
+title= Premier exercice de python
+
+
+pltest==
+>>> f()
+3
+>>> f()+2
+5
+==
+
+text==
+Ecrivez une fonction f qui retourne 3.
+==
