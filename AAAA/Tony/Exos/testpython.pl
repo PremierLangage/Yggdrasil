3dfd98d79b1eee31f20c997f541b86c96490b166
tony fevrier <tony.fevrier62@gmail.com>
2021-07-08 15:14:50 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/testpython.pl

--- AAAA/Tony/Exos/testpython.pl
+++ AAAA/Tony/Exos/testpython.pl
@@ -1,7 +1,7 @@
 
 extends = /ComputerScience/python/AP1-1920/templates/pltest.pl 
 
-"""
+
 #tests de la fonction que l'étudiant va entrer : copier coller les retours du terminal.
 pltest == 
 >>> f()
@@ -11,4 +11,3 @@ pltest ==
 text == 
 Ecrivez une fonction qui rend 3 
 ==
-"""
