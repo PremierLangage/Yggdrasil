244614887e94e9e5ddae9d9853ffb6b6f80846ac
David DOYEN <David.Doyen@u-pem.fr>
2021-07-09 19:23:48 +0200

    ddoyen modified Yggdrasil/demo/multi/multi.pl

--- demo/multi/multi.pl
+++ demo/multi/multi.pl
@@ -10,10 +10,11 @@ items = [1, 2, 3, 4]
 
 numeric1._sol = 0
 numeric2._sol = 10
+
 ==
 
 inputblock ==
-{{numeric.score }}
+{{ numeric1.score }}
 <div class="row">
   <div class="col-sm-6">{{ numeric1|component }}</div>
   <div class="col-sm-6">{{ numeric2|component }}</div>
