ab60803bca99066a55b6a22446b37f4a2b01dde8
David DOYEN <David.Doyen@u-pem.fr>
2021-07-09 19:45:00 +0200

    ddoyen modified Yggdrasil/demo/multi/multi.pl

--- demo/multi/multi.pl
+++ demo/multi/multi.pl
@@ -1,24 +1,23 @@
 extends = /model/basic/basic.pl
-@ /utils/components/inputfields.py [radio.py]
+@ /utils/components/inputfields.py [numeric.py]
 
 before ==
-from radio import Radio
-radio1 = Radio()
-radio2 = Radio()
+from numeric import Numeric
+numeric1 = Numeric()
+numeric2 = Numeric()
 
 items = [1, 2, 3, 4]
 
-radio1.set_items(items)
-radio2.set_items(items)
-radio1.set_sol(0)
-radio2.set_sol(0)
+numeric1._sol = 0
+numeric2._sol = 10
+numeric1.score = 100
 ==
 
 inputblock ==
-{{ radio1.score }}
+{{ numeric1.score }}
 <div class="row">
-  <div class="col-sm-6">{{ radio1|component }}</div>
-  <div class="col-sm-6">{{ radio2|component }}</div>
+  <div class="col-sm-6">{{ numeric1|component }}</div>
+  <div class="col-sm-6">{{ numeric2|component }}</div>
 </div>
 ==
 
@@ -27,5 +26,5 @@ score = 0
 ==
 
 solution ==
-{{ radio1 }}
+{{ numeric1 }}
 ==
