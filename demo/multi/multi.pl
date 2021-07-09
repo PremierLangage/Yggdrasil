4c72367bbc69c3daf67711c6dc2e93bfffbab873
David DOYEN <David.Doyen@u-pem.fr>
2021-07-09 19:17:10 +0200

    ddoyen modified Yggdrasil/demo/multi/multi.pl

--- demo/multi/multi.pl
+++ demo/multi/multi.pl
@@ -1,27 +1,27 @@
 extends = /model/basic/basic.pl
-@ /utils/components/radio.py [radio.py]
+@ /utils/components/inputfields.py [inputfields.py]
 
 before ==
-from radio import Radio
-radio1 = Radio()
-radio2 = Radio()
+from inputfields import Numeric
+numeric1 = Numeric()
+numeric2 = Numeric()
 
 items = [1, 2, 3, 4]
 
-radio1.set_items(items)
-radio1.set_sol(1)
+numeric1.set_items(items)
+numeric1.set_sol(1)
 
-radio2.set_items(items)
-radio2.set_sol(1)
+numeric2.set_items(items)
+numeric2.set_sol(1)
 ==
 
 inputblock ==
 <div class="row">
-  <div class="col-sm-6">{{ radio1|component }}</div>
-  <div class="col-sm-6">{{ radio2|component }}</div>
+  <div class="col-sm-6">{{ numeric1|component }}</div>
+  <div class="col-sm-6">{{ numeric2|component }}</div>
 </div>
 ==
 
 evaluator ==
-score = 0.5*radio1.eval() + 0.5*radio2.eval()
+score = 0.5*numeric1.eval() + 0.5*numeric2.eval()
 ==
