d65b95a11912e27908a7ecfe6d92937c9a2693ca
David DOYEN <David.Doyen@u-pem.fr>
2021-07-12 14:57:01 +0200

    ddoyen modified Yggdrasil/utils/components/radio.py

--- utils/components/radio.py
+++ utils/components/radio.py
@@ -60,69 +60,15 @@ class Radio(Component):
 
         return score
 
-# Old class
-class CustomRadio(Component):
-
-    def __init__(self, **kwargs):
-        self.selector = 'c-radio-group'
-        self.decorator = 'CustomRadio'
-        self.selection = None
-        super().__init__(**kwargs)
-        self.statement =''
-
-    def setStatement(self, s):
-        self.statement = s 
-
-    def setitems(self, items):
+    def show(self):
         """
-        Set items in the component.
+        Evaluate the answer.
         """
-        if isinstance(items, str):
-            _items_ = items.splitlines()
-        else:
-            _items_ = items
-        self.items = [{"id": str(uuid4()), "content": str(item)} for item in _items_]
-
-    def setsol_from_index(self, index):
-        """
-        Set the component solution from an index.
-        """
-        self._sol = self.items[index]['id']
-
-    def setsol_from_content(self, content):
-        """
-        Set the component solution from a content.
-        """
-        self._sol = next(item['id'] for item in self.items if item['content'] == content)
-
-    def shuffle(self):
-        """
-        Shuffle the component items.
-        """
-        rd.shuffle(self.items)
-
-    def sort(self):
-        """
-        Sort the component items.
-        """
-        self.items.sort(key = lambda item: item['content'])
-
-    def eval(self, display=True, disabled=True):
-        """
-        Evaluate the answer stored in the component.
-        """
-        score = 0
         for item in self.items:
             id = item['id']
             if id == self._sol and id == self.selection:
-                score = 100
-                if display:
-                    item['css'] = 'success-state icon-check-after'
-            elif display and id != self._sol and id == self.selection:
-                    item['css'] = 'error-state icon-times-after'
-            elif display and id == self._sol and id != self.selection:
-                    item['css'] = 'icon-check-after'
-
-        self.disabled = disabled
-
-        return score
+                item['css'] = 'success-state icon-check-after'
+            elif id != self._sol and id == self.selection:
+                item['css'] = 'error-state icon-times-after'
+            elif id == self._sol and id != self.selection:
+                item['css'] = 'icon-check-after'
