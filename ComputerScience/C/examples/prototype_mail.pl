b5ea56db8519385c492e6e30d838c17dd80605da
Nicolas BORIE <Nicolas.Borie@u-pem.fr>
2021-07-12 20:28:01 +0200

    nborie modified Yggdrasil/ComputerScience/C/examples/prototype_mail.pl

--- ComputerScience/C/examples/prototype_mail.pl
+++ ComputerScience/C/examples/prototype_mail.pl
@@ -71,6 +71,6 @@ form==#|markdown|
 evaluator==#|python|
 form = "Nickel, vous avez bien terminé votre questionnaire. Finalisez cela en cliquant sur le bouton qui suit"
 
-grade = (100, '<input type="button" value="Send Email" onclick="sendEmail('+inputbox.value+')">')
+grade = (100, '<input type="button" value="Send Email" onclick="sendEmail("'+inputbox.value+'")">')
 ==
 
