afac0c9e849dacc5c0f33658c9fbb73778f7eaa9
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-13 11:49:10 +0200

    drevuz modified Yggdrasil/AAAA/dominique/TESTAP1/testapp1.pl

---
+++ AAAA/dominique/TESTAP1/testapp1.pl
@@ -0,0 +1,37 @@
+
+
+
+title= structure de controle 
+
+text=
+
+before==
+trois,cinq = random.sample([3,5,7,11,13],2)
+bob, buzz = random.sample(["pim","pam","pom","avant","marie","par","contre","orga","tp1","tp2","jacques"],2)
+
+text=f"""
+Ecrire un code qui lit un entier et le place dans la variable v puis:  
+- affiche {bob} si v est divisible par {trois}.  
+- affiche {buzz} si v est divisible par {cinq}.  
+- affiche {bob}{buzz} si v est divisible par {cinq} et {trois}.   
+- affiche la valeur de v sinon.  
+"""
+
+soluce= f"""
+s=""
+if v % {trois} == 0:
+    s+= "{bob}"
+if v % {cinq} == 0:
+    s+= "{buzz}"
+if s=="":
+    print(v)
+else:
+    print(s)
+"""
+
+mplsoluce0= f" Test de {trois} \n{trois}\n"
+mplsoluce1= f" Test de {cinq} \n{cinq}\n"
+mplsoluce2= f" Test de Produit \n{cinq*trois}\n"
+mplsoluce3= f" Test de Hors \n{cinq*trois+1}\n"
+
+==
