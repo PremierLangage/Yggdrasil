94a7c8982293c37737157981c891b4bcaa7b39f4
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-13 11:39:39 +0200

    drevuz modified Yggdrasil/AAAA/dominique/TESTENTREE/exo1.pl

--- AAAA/dominique/TESTENTREE/exo1.pl
+++ AAAA/dominique/TESTENTREE/exo1.pl
@@ -11,8 +11,6 @@ before==
 trois,cinq = random.sample([3,5,7,11,13],2)
 fizz,buzz = random.sample(["pim","pam","pom","avant","marie","par","contre","orga","tp1","tp2","jacques"],2)
 text=f"""
-
-
 Ecrire un code qui lit un entier et le place dans la variable v puis:  
 - affiche { fizz } si v est divisible par { trois }.  
 - affiche { buzz } si v est divisible par { cinq }.  
