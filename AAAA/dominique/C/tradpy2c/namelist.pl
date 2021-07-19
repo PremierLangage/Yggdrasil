ebfe0835ec5e282b30d2a631dd739f98bd220440
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-19 15:35:44 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/namelist.pl

--- AAAA/dominique/C/tradpy2c/namelist.pl
+++ AAAA/dominique/C/tradpy2c/namelist.pl
@@ -4,4 +4,55 @@
 
 @ a.py
 @ a.c
-
+@ b.py
+@ b.c
+@ c.py
+@ c.c
+kiki==
+@ d.py
+@ d.c
+@ e.py
+@ e.c
+@ f.py
+@ f.c
+@ g.py
+@ g.c
+@ h.py
+@ h.c
+@ i.py
+@ i.c
+@ j.py
+@ j.c
+@ k.py
+@ k.c
+@ l.py
+@ l.c
+@ m.py
+@ m.c
+@ n.py
+@ n.c
+@ o.py
+@ o.c
+@ p.py
+@ p.c
+@ q.py
+@ q.c
+@ r.py
+@ r.c
+@ s.py
+@ s.c
+@ t.py
+@ t.c
+@ u.py
+@ u.c
+@ v.py
+@ v.c
+@ w.py
+@ w.c
+@ x.py
+@ x.c
+@ y.py
+@ y.c
+@ z.py
+@ z.c
+==
