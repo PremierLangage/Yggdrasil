07932326f0a63b72ad5b911f619115b467de0aed
tony fevrier <tony.fevrier62@gmail.com>
2021-07-19 18:46:20 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/primitive.pl

--- AAAA/Tony/Exos/primitive.pl
+++ AAAA/Tony/Exos/primitive.pl
@@ -4,11 +4,30 @@ before ==
 from sympy import integrate,diff
 from random import choice, randint
 x = Symbol('x')
-n = randint(0,100)
-f = choice([sin(x),cos(x),x**n,ln(x), exp(x)])
+ch = randint(0,5)
+d1,d2,d3 = randint(-5,5),randint(-5,5),randint(-5,5)
+al = randint(1,6)
+n= randint(1,6)
+
+g = choice([sin(x),cos(x),x**n,ln(x), exp(x), d1*x**2+d2*x+d3])
+dg = diff(g)
+
+if ch == 0:
+    f = al*dg*sin(g)
+elif ch == 1:
+    f = al*dg*cos(g)
+elif ch == 2:
+    f = al*dg*exp(g)
+elif ch == 3:
+    f =al*dg/g
+elif ch == 4:
+    f = al*dg*g**n
+else:
+    f = al*dg/g**n
+
 sol = integrate(f, x) 
 
-u'sin(u), exp,/u,u'un u'/un
+u'sin(u), exp,/u,u'un u'/un, cb lin
 ==
 
 question ==
