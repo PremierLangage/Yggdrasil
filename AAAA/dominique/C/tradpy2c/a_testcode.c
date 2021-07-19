a68d6b56e4b0ad9a2f61848757c9bb4339a65f01
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-19 15:43:30 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/a_testcode.c

---
+++ AAAA/dominique/C/tradpy2c/a_testcode.c
@@ -0,0 +1,22 @@
+/*
+
+void a(int n)
+{
+    printf("%d\n",n);
+}
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+  int tab[]={ 3,5,9,12, 131,789 };
+  int i=0;
+
+  for (i=0 ; i< 6 ; i++){
+    a(i);
+  }
+
+  return 0;
+}
