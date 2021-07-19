2b04404104cc6cec3854e31ed6a96cae1e556307
tony fevrier <tony.fevrier62@gmail.com>
2021-07-19 18:37:17 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/primitive.pl

--- AAAA/Tony/Exos/primitive.pl
+++ AAAA/Tony/Exos/primitive.pl
@@ -1,12 +1,14 @@
 extends = /model/math/expr.pl
 
 before ==
-from sympy import integrate,sin,cos, ln, sqrt, exp 
+from sympy import integrate,diff
 from random import choice, randint
 x = Symbol('x')
 n = randint(0,100)
-f = choice([sin(x),cos(x),x**n,ln(x), 1/x,1/(2*sqrt(x)), exp(x)])
+f = choice([sin(x),cos(x),x**n,ln(x), exp(x)])
 sol = integrate(f, x) 
+
+u'sin(u), exp,/u,u'un u'/un
 ==
 
 question ==
@@ -21,3 +23,4 @@ Si vous voulez ajouter une constante à votre fonction, il faut donner un réel.
 equality = "UpToConstant"
 
 message.NotEqualUpToConstant = La réponse n'est pas une primitive.
+
