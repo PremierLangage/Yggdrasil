78949ab62f3c20c3db4d6a821bb5301f8aba784d
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-19 16:27:29 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/trad1.pl

--- AAAA/dominique/C/tradpy2c/trad1.pl
+++ AAAA/dominique/C/tradpy2c/trad1.pl
@@ -44,7 +44,7 @@ with open(fname+".c","r") as fsolo:
 with open(fname+"_testcode.c","r") as ftst:
     code_after = ftst.read()
 
-text+=code_after
+text+= "\n Testing code :   \n"+ re.sub("FUNC",funname,code_after)
 
 code_before += "\n#define FUNC "+funname+"\n"
 editor.code += "\n ... "+funname+"( ... )\n ... \n"
