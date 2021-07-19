83f56d9792b304b11cb2f7e8b44330757c2ca02e
tony fevrier <tony.fevrier62@gmail.com>
2021-07-19 18:04:18 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/primitive.pl

--- AAAA/Tony/Exos/primitive.pl
+++ AAAA/Tony/Exos/primitive.pl
@@ -1,30 +1,18 @@
-
 extends = /model/math/expr.pl
 
 before ==
-from sympy import Symbol, diff, integrate, sin
-
+from sympy import integrate
 x = Symbol('x')
- 
 f = sin(x) 
-sol = integrate (f,x) 
+sol = integrate(f, x) 
 ==
 
-#Double dollar pour mettre le latex centré.
 question ==
 Déterminer une primitive de 
 
 $! f \mapsto \displaystyle {{ f|latex }} !$
 ==
-  
 
-evaluator ==  
-from sympy import diff
- 
-if diff(ans-sol,x) == 0:
-    score = 100
-else: 
-    score = 0
-    feedback = f"T es nul c'est {sol}"
-==
+equality = "UpToConstant"
 
+message.NotEqualUpToConstant = La réponse n'est pas une primitive.
