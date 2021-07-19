4c7d499aaeb9423ef4f82e4015c2440a022f3f02
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-19 16:23:24 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/trad1.pl

--- AAAA/dominique/C/tradpy2c/trad1.pl
+++ AAAA/dominique/C/tradpy2c/trad1.pl
@@ -31,7 +31,8 @@ before==
 import re
 from random import choice 
 #fname = choice(['a','b'])
-funname = choice(['bar','foo','toto','titi','tata','Zuver','Neirob', 'labouil', 'champyulesis', 'uge', 'eiffel'])
+funname=choice( ['Sed', 'ut', 'perspiciatis', 'unde', 'omnis', 'iste', 'natus', 'error', 'sit', 'voluptatem', 'accusantium', 'doloremque', 'laudantium', 'totam', 'rem', 'aperiam', 'eaque', 'ipsa', 'quae', 'ab', 'illo', 'inventore', 'veritatis', 'et', 'quasi', 'architecto', 'beatae', 'vitae', 'dicta', 'sunt', 'explicabo'])
+#funname = choice(['bar','foo','toto','titi','tata','Zuver','Neirob', 'labouil', 'champyulesis', 'uge', 'eiffel', 'lorem',])
 with open(fname+".py","r") as fpy:
     soso = fpy.read()
 
