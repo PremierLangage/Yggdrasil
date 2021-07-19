23d466d134e47cc0a6a7330307fd90225e14a672
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-19 16:40:02 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/b_testcode.c

--- AAAA/dominique/C/tradpy2c/b_testcode.c
+++ AAAA/dominique/C/tradpy2c/b_testcode.c
@@ -12,15 +12,17 @@ int b(int p, int q){
 
 #include <stdio.h>
 #include <stdlib.h>
+#define CUT_HERE int Z88;
+CUT_HERE
 
-int main(int argc, char* argv[]){
-  int i=0;
-  int nb_term = argc-1;
+int main(int argc, char* argv[]){  
+  int i=0;  
+  int nb_term = argc-1;  
 
+  for (i=0 ; i<nb_term ; i++){  
+    printf("%d", FUNC(atoi(argv[i+1]),i) );  
+  }  
 
-  for (i=0 ; i<nb_term ; i++){
-    printf("%d", FUNC(atoi(argv[i+1]),i) );
-  }
+  return 0;  
+}  
 
-  return 0;
-}
