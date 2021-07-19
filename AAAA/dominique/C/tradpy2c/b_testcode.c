7a55102f4bbf9685d8a56075380d40d32e7c354b
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-19 16:05:38 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/b_testcode.c

---
+++ AAAA/dominique/C/tradpy2c/b_testcode.c
@@ -0,0 +1,26 @@
+
+/*
+
+int b(int p, int q){
+
+    return p+q;
+}
+
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){
+  int i=0;
+  int nb_term = argc-1;
+
+
+  for (i=0 ; i<nb_term ; i++){
+     FUNC(atoi(argv[i+1]),i);
+  }
+
+  return 0;
+}
