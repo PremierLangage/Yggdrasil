710d967ae6a33b68e468bc3e3fb218fc113a73d1
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 18:05:02 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/complex_mult_exp*.pl

---
+++ AAAA/Tony/Exos/complex_mult_exp*.pl
@@ -0,0 +1,23 @@
+
+extends = /model/math/complex.pl
+
+before ==
+z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
+z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
+sol = (z1 * z2).expand()
+==
+
+question ==
+On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2|latex }} !$. 
+Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
+==
+
+complex_form = cartesian
+
+solution ==
+$! z_1 \times z_2  = ( {{ z1|latex }} ) \times ( {{ z2|latex }} ) = {{ sol|latex }} !$
+==
+
+message.NotEqual ==
+T'es nul !
+==
