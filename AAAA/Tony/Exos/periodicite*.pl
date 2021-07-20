6eb0d9c178478bd1d46ed49bb4c2c2f4f1ba5780
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 16:58:59 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/periodicite*.pl

---
+++ AAAA/Tony/Exos/periodicite*.pl
@@ -0,0 +1,43 @@
+extends= /model/basic/checkbox_rw.pl
+
+
+title= Parité de fonctions
+
+text==
+Parmi ces fonctions, lesquelles sont périodiques?
+==
+
+nbitems = 4
+minright = 0
+maxright = 4
+
+right== 
+$!f:x\mapsto \sin(x)!$
+$!f:x\mapsto \tan(x)!$
+$!f:x\mapsto \cos(x)!$
+$!f:x\mapsto \sin(x)\sin(2x)!$
+$!f:x\mapsto 6x^2!$
+$!f:x\mapsto 0!$
+$!f:x\mapsto 1!$
+==
+
+wrong==
+$!f:x\mapsto \sin(x^2)!$
+$!f:x\mapsto \frac{x^2}{2+3x^4}!$
+$!f:x\mapsto x(1+x)!$
+$!f:x\mapsto e^x!$
+$!f:x\mapsto \ln(x)!$
+$!f:x\mapsto x^3!$
+$!f:x\mapsto x^5!$
+$!f:x\mapsto x^{11}!$
+$!f:x\mapsto \frac{x^2}{2x+3x^4}!$
+$!f:x\mapsto \sqrt{x}!$
+$!f:x\mapsto 3x!$
+$!f:x\mapsto 5x-2!$
+$!f:x\mapsto e^{-x^2}!$
+$!f:x\mapsto \sqrt{x^2+4}!$
+==
+
+
+
+
