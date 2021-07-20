900cb7b7799bee78e5278bedb7c325861a65d9fa
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-20 14:38:42 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/scanfpy2C.pl

---
+++ AAAA/dominique/C/tradpy2c/scanfpy2C.pl
@@ -0,0 +1,65 @@
+
+
+
+
+
+
+extends= trad1.pl
+
+tag=boucle
+
+text==
+Écrire un code C équivalent a celui-ci écrit en python :
+==
+
+
+code_before==
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]){  
+
+
+==
+
+code_after==#|c|
+
+
+
+return 0;
+}
+
+==
+
+
+before=
+
+
+
+checks_args_stdin==#|python|
+[["Exécution simple", ["2"], ""],
+ ]
+==
+
+
+before3==
+import random
+prime= random.sample([2,3,5,7,11,13],2)
+
+end="\\n"
+openbrace = '\n{\n'
+closebrace = '\n}\n'
+
+solution= f"""
+printf("Bonjour les L2 maths-info\n")
+
+"""
+
+editor1.code = f"""
+i = int(input("entrez un entier"))
+print("votre entier :",i)
+"""
+
+
+
+==
