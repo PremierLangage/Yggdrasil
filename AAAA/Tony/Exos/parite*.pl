88f840385cc659a7ab3563d202451e621fae5476
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 16:46:40 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/parite*.pl

--- AAAA/Tony/Exos/parite*.pl
+++ AAAA/Tony/Exos/parite*.pl
@@ -12,14 +12,16 @@ Parmi ces fonctions, lesquelles sont paires?
 nb=4
 nbtrues = 2
 
-good==
-from random import randint
-a = randint(1,5)
-
-$!f:x\mapsto \cos(a*x)!$
-$!f:x\mapsto \sin(a*x^2)!$
-$!f:x\mapsto \frac{a*x^2}{2+3x^4}!$
-$!f:x\mapsto \sin(a*x)\sin(2x)!$
+good== 
+$!f:x\mapsto \cos(x)!$
+$!f:x\mapsto \sin(x^2)!$
+$!f:x\mapsto \frac{x^2}{2+3x^4}!$
+$!f:x\mapsto \sin(x)\sin(2x)!$
+$!f:x\mapsto 6x^2!$
+$!f:x\mapsto 0!$
+$!f:x\mapsto 1!$
+$!f:x\mapsto e^{-x^2}!$
+$!f:x\mapsto \sqrt{x^2+4}!$
 ==
 
 bad==
@@ -27,6 +29,14 @@ $!f:x\mapsto \sin(x)!$
 $!f:x\mapsto \tan(x)!$
 $!f:x\mapsto x(1+x)!$
 $!f:x\mapsto e^x!$
+$!f:x\mapsto \ln(x)!$
+$!f:x\mapsto x^3!$
+$!f:x\mapsto x^5!$
+$!f:x\mapsto x^11!$
+$!f:x\mapsto \frac{x^2}{2x+3x^4}!$
+$!f:x\mapsto \sqrt{x}!$
+$!f:x\mapsto 3x!$
+$!f:x\mapsto 5x-2!$
 ==
 
 
