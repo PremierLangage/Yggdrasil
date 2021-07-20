e6de0df0d6cbafe1db6d874682246c5271e68e90
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 17:10:05 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/edo_proprietes*.pl

---
+++ AAAA/Tony/Exos/edo_proprietes*.pl
@@ -0,0 +1,39 @@
+extends= /model/basic/checkbox_rw.pl
+
+
+title= Propriétés d'équations différentielles
+
+text==
+Parmi ces équations linéaires, lesquelles sont linéaires d'ordre 1 homogène?
+==
+
+nbitems = 4
+minright = 0
+maxright = 4
+
+right== 
+$!y'-xy = 0!$
+$!y'=-2y!$
+$!y'-\cos(x)y = 0!$
+$!y'-x^2y = 0!$
+$!2y' = \sin(x)y!$
+$!y'+e^xy = 0!$
+$!y'-3y = 0!$
+$!y'-5xy = 0!$
+$!y'-3xy = 0!$
+==
+
+wrong==
+$!y'-xy = \cos(x)!$
+$!y'-3y = x^2 !$
+$!y''-y = 0!$
+$!y''+2y'-y = 0!$
+$!y''-y = 3!$
+$!y''+xy'-y = 0!$
+$!y'-3y = e^x !$
+$!y'-xy = \sin(x) !$
+==
+
+
+
+
