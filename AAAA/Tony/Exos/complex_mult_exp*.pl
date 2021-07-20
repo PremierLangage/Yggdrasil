e5efd4cab2109890f62f1bbe2c9ab5085792d765
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 18:07:45 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/complex_mult_exp*.pl

--- AAAA/Tony/Exos/complex_mult_exp*.pl
+++ AAAA/Tony/Exos/complex_mult_exp*.pl
@@ -2,9 +2,11 @@
 extends = /model/math/complex.pl
 
 before ==
-z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
-z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
-sol = (z1 * z2).expand()
+a, b, c = choice([2,3,4,6,-2,-3,-4,-6]), choice([2,3,4,6,-2,-3,-4,-6]), choice([2,3,4,6,-2,-3,-4,-6])
+z1 = randint(-3, 3, [0])*e**(I*pi/a)  
+z2 = randint(-3, 3, [0])*e**(I*pi/b)  
+z3 = randint(-3, 3, [0])*e**(I*pi/c)  
+sol = (z1 * z2/z3).expand()
 ==
 
 question ==
@@ -12,7 +14,7 @@ On considère les nombres complexes $! z_1 = {{ z1|latex }} !$ et $! z_2 = {{ z2
 Calculer $! z_1 \times z_2 !$ (sous forme algébrique).
 ==
 
-complex_form = cartesian
+complex_form = exponential
 
 solution ==
 $! z_1 \times z_2  = ( {{ z1|latex }} ) \times ( {{ z2|latex }} ) = {{ sol|latex }} !$
