e1349f4dff5ebf79d7578f6ebf82f142def44e4b
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-20 15:07:16 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/d_testcode.c

---
+++ AAAA/dominique/C/tradpy2c/d_testcode.c
@@ -0,0 +1,21 @@
+
+/*
+
+int c(int x, int y){ return x > y; }
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define CUT_HERE int Z88;
+CUT_HERE
+
+int main(int argc, char* argv[]){  
+  int i=0;  
+
+    i= FUNC();
+    printf("Entier saisi:%d\n",i)
+
+  return 0;  
+}  
