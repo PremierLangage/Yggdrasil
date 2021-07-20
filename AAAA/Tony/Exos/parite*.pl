bb9a32df3de2a226d2a8c6b777434edf5153bc5f
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 16:29:49 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/parite*.pl

---
+++ AAAA/Tony/Exos/parite*.pl
@@ -0,0 +1,29 @@
+extends= /model/qcm/qcm.pl
+
+
+title= Parité de fonctions
+
+text==
+Parmi ces fonctions, lesquelles sont paires?
+==
+
+a = randint(1,5)
+nb=4
+nbtrues=a
+
+good==
+bon
+bien
+ok
+cool
+
+==
+
+bad==
+bad
+pas bon
+beurk
+==
+
+
+
