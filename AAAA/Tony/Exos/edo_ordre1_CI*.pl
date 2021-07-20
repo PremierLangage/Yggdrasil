1d911a1942020796bd322fe7d33b717e4ec33e9a
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 17:36:28 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/edo_ordre1_CI*.pl

--- AAAA/Tony/Exos/edo_ordre1_CI*.pl
+++ AAAA/Tony/Exos/edo_ordre1_CI*.pl
@@ -3,10 +3,11 @@ extends = /model/math/expr.pl
 
 before ==
 from sympy import dsolve
-from plrandom import randint
+
 
 sol = 1
 ==
+#from plrandom import randint
 #x = Symbol('x')  
 #a= randint(-3,3,[0])
 #b= randint(2,5)
