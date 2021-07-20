e4939bd6187ac05a8e01fafd5e6167cf495a81e8
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-20 16:35:55 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/whilepy2C.pl

--- AAAA/dominique/C/tradpy2c/whilepy2C.pl
+++ AAAA/dominique/C/tradpy2c/whilepy2C.pl
@@ -108,35 +108,4 @@ checks_args_stdin==#|python|
 ==
 
 
-before3==
-import random
-prime= random.sample([2,3,5,7,11,13],2)
 
-end="\\n"
-openbrace = '\n{\n'
-closebrace = '\n}\n'
-
-solution= f"""
-if (x % {prime[0]} == 0)
-{openbrace}
-    printf("Divisible par {prime[0]}{end}");
-{closebrace}
-else 
-{openbrace}
-if (x % {prime[1]} == 0 )
-{openbrace}
-    printf("Divisible par {prime[1]}{end}");
-{closebrace}
-{closebrace}
-
-"""
-
-editor1.code = f"""
-if x % {prime[0]} == 0:
-    print("Divisible par {prime[0]}")
-elif x % {prime[1]} == 0 :
-    print("Divisible par {prime[1]}")
-"""
-
-
-==
