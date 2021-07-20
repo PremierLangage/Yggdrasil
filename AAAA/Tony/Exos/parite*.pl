6dccd9ed95cab87f2a5745dfca96f2c72f1ec420
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 17:09:47 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/parite*.pl

--- AAAA/Tony/Exos/parite*.pl
+++ AAAA/Tony/Exos/parite*.pl
@@ -12,26 +12,30 @@ minright = 0
 maxright = 4
 
 right== 
-$!y'-xy = 0!$
-$!y'=-2y!$
-$!y'-\cos(x)y = 0!$
-$!y'-x^2y = 0!$
-$!2y' = \sin(x)y!$
-$!y'+e^xy = 0!$
-$!y'-3y = 0!$
-$!y'-5xy = 0!$
-$!y'-3xy = 0!$
+$!f:x\mapsto \cos(x)!$
+$!f:x\mapsto \sin(x^2)!$
+$!f:x\mapsto \frac{x^2}{2+3x^4}!$
+$!f:x\mapsto \sin(x)\sin(2x)!$
+$!f:x\mapsto 6x^2!$
+$!f:x\mapsto 0!$
+$!f:x\mapsto 1!$
+$!f:x\mapsto e^{-x^2}!$
+$!f:x\mapsto \sqrt{x^2+4}!$
 ==
 
 wrong==
-$!y'-xy = \cos(x)!$
-$!y'-3y = x^2 !$
-$!y''-y = 0!$
-$!y''+2y'-y = 0!$
-$!y''-y = 3!$
-$!y''+xy'-y = 0!$
-$!y'-3y = e^x !$
-$!y'-xy = \sin(x) !$
+$!f:x\mapsto \sin(x)!$
+$!f:x\mapsto \tan(x)!$
+$!f:x\mapsto x(1+x)!$
+$!f:x\mapsto e^x!$
+$!f:x\mapsto \ln(x)!$
+$!f:x\mapsto x^3!$
+$!f:x\mapsto x^5!$
+$!f:x\mapsto x^{11}!$
+$!f:x\mapsto \frac{x^2}{2x+3x^4}!$
+$!f:x\mapsto \sqrt{x}!$
+$!f:x\mapsto 3x!$
+$!f:x\mapsto 5x-2!$
 ==
 
 
