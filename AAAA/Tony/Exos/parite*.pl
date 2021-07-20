9a7f9626074078ad8417246a7e7600e80c6bf37c
tony fevrier <tony.fevrier62@gmail.com>
2021-07-20 16:36:46 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/parite*.pl

--- AAAA/Tony/Exos/parite*.pl
+++ AAAA/Tony/Exos/parite*.pl
@@ -7,26 +7,26 @@ text==
 Parmi ces fonctions, lesquelles sont paires?
 ==
 
-#before ==
-#from random import randint
-#a = randint(1,5)
-#==
+before ==
 from random import randint
+a = randint(1,5)
+== 
+
 nb=4
 nbtrues = randint(1,5)
 
 good==
-$!f:x\mapsto \sin(x)!$
-$!f:x\mapsto \sin(x)!$
-$!f:x\mapsto \sin(x)!$
-$!f:x\mapsto \sin(x)!$
+$!f:x\mapsto \cos(a*x)!$
+$!f:x\mapsto \sin(x^2)!$
+$!f:x\mapsto \frac{x^2}{2+3x^4}!$
+$!f:x\mapsto \sin(x)\sin(2x)!$
 ==
 
 bad==
 $!f:x\mapsto \sin(x)!$
-$!f:x\mapsto \sin(x)!$
-$!f:x\mapsto \sin(x)!$
-$!f:x\mapsto \sin(x)!$
+$!f:x\mapsto \tan(x)!$
+$!f:x\mapsto x(1+x)!$
+$!f:x\mapsto e^x!$
 ==
 
 
