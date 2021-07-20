1c1d9936edb3db20184168b450dc16fc48ad31e9
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-20 10:54:21 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/lines/l1.c

--- AAAA/dominique/C/tradpy2c/lines/l1.c
+++ AAAA/dominique/C/tradpy2c/lines/l1.c
@@ -1,8 +1,8 @@
 
 
 if (x % 3 == 0){
-    printf("Divisible par 3\n")
+    printf("Divisible par 3\n");
 }
 else if (x % 5 == 0 ){
-    printf("Divisible par 5\n")
+    printf("Divisible par 5\n");
 }
