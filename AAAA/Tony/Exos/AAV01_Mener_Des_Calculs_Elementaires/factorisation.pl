649d259ce85196d168a55c0da0d6d2639d184f0b
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 16:15:12 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/factorisation.pl

--- AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/factorisation.pl
+++ AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/factorisation.pl
@@ -9,10 +9,9 @@ a,b,c,d = sample([-3,-2,-1,1,2,3],4)
 P = (a*x+b)**2-(c*x+d)**2
 Q = (a*x+b)**2-c*a*x-c*b
 R = choice([P,Q])
-if P == R:
-    sol =  ((a+c)*x+(b+d))*((a-c)*x+(b-d)) 
-else: 
-    sol = (a*x+b)*((a-c*a)*x+(b-c*b))
+
+sol = factor(R)
+
 ==
 
 question ==
