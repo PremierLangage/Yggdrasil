b709ffaca0478f9a7558b932f3fbdceeb0f3ec32
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 12:41:20 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Connaitre.pl

--- AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Connaitre.pl
+++ AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Connaitre.pl
@@ -12,22 +12,17 @@ minright = 0
 maxright = 4
 
 right== 
-$!\lim_{x\rightarrow +\infty}exp(x)!$
+1
+1
+1
+1
 ==
 
-wrong==
-$!f:x\mapsto \sin(x)!$
-$!f:x\mapsto \tan(x)!$
-$!f:x\mapsto x(1+x)!$
-$!f:x\mapsto e^x!$
-$!f:x\mapsto \ln(x)!$
-$!f:x\mapsto x^3!$
-$!f:x\mapsto x^5!$
-$!f:x\mapsto x^{11}!$
-$!f:x\mapsto \frac{x^2}{2x+3x^4}!$
-$!f:x\mapsto \sqrt{x}!$
-$!f:x\mapsto 3x!$
-$!f:x\mapsto 5x-2!$
+wrong== 
+1
+1
+11
+1
 ==
 
 
