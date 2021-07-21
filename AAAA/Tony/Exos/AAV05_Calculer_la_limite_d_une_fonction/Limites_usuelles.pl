99aa83d1f53bf14cd6b8a11ec4e683462f9c4502
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 15:06:32 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Limites_usuelles.pl

---
+++ AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Limites_usuelles.pl
@@ -0,0 +1,37 @@
+extends= /model/basic/checkbox_rw.pl
+
+
+title = Connaître les limites de fonctions usuelles
+
+text==
+Parmi ces limites lesquelles valent $!+\infty!$?
+==
+
+nbitems = 4
+minright = 0
+maxright = 4
+
+right== 
+$!\lim\limits_{x\rightarrow +\infty}\exp(x)!$ 
+$!\lim\limits_{x\rightarrow +\infty}x^2!$ 
+$!\lim\limits_{x\rightarrow -\infty}x^2!$ 
+$!\lim\limits_{x\rightarrow +\infty}\ln(x)!$ 
+$!\lim\limits_{x\rightarrow +\infty}\sqrt{x}!$ 
+$!\lim\limits_{x\rightarrow +\infty}x^3!$ 
+$!\lim\limits_{x\rightarrow 0^+}\frac{1}{x}!$
+$!\lim\limits_{x\rightarrow +\infty}0!$   
+==
+
+wrong== 
+$!\lim\limits_{x\rightarrow 0^+}\ln(x)!$ 
+$!\lim\limits_{x\rightarrow +\infty}\sin(x)!$ 
+$!\lim\limits_{x\rightarrow +\infty}\cos(x)!$ 
+$!\lim\limits_{x\rightarrow -\infty}\exp(x)!$ 
+$!\lim\limits_{x\rightarrow 0}\sqrt{x}!$ 
+$!\lim\limits_{x\rightarrow 0}\frac{1}{x}!$
+$!\lim\limits_{x\rightarrow +\infty}1!$  
+==
+
+
+
+
