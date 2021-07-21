04394a98acebbd0c1f1ae3853e0bf181ec783f66
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 15:51:43 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV06_Discuter_du_domaine_d_etude_d_une_fonction/composee.pl

--- AAAA/Tony/Exos/AAV06_Discuter_du_domaine_d_etude_d_une_fonction/composee.pl
+++ AAAA/Tony/Exos/AAV06_Discuter_du_domaine_d_etude_d_une_fonction/composee.pl
@@ -6,7 +6,7 @@ title = Savoir calculer une fonction composée (fonction de \R -> \R)
 before ==
 var('x')
 f =  choice([exp(x),ln(x),sin(x),cos(x),sqrt(x+1),tan(x)]) 
-g = choice([x**2+1,exp(x),ln(x),sin(x),cos(x),sqrt(x),x**3+2*x**2-5,x**4+x-12])
+g = choice([x**2,exp(x),ln(x),sin(x),cos(x),sqrt(x),2*x+1])
 sol = f.subs(x,g)
 ==
 
