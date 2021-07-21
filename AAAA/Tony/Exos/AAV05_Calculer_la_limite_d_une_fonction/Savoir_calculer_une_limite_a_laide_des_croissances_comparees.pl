f02f4ba8ca7d2720fcc643f536feef8c1f76e011
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 13:00:04 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Savoir_calculer_une_limite_a_laide_des_croissances_comparees.pl

--- AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Savoir_calculer_une_limite_a_laide_des_croissances_comparees.pl
+++ AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Savoir_calculer_une_limite_a_laide_des_croissances_comparees.pl
@@ -16,8 +16,8 @@ $!\lim\limits_{x\rightarrow +\infty}\frac{\exp(x)}{x}!$
 $!\lim\limits_{x\rightarrow +\infty}\frac{\exp(x)}{x^2}!$ 
 $!\lim\limits_{x\rightarrow -\infty}-\frac{\exp(-x)}{x**3}!$ 
 $!\lim\limits_{x\rightarrow +\infty}\frac{x^3}{\ln(x)}!$  
-$!\lim\limits_{x\rightarrow +\infty} x\exp(x)}!$ 
-$!\lim\limits_{x\rightarrow +\infty} x\ln(x)}!$ 
+$!\lim\limits_{x\rightarrow +\infty} x\exp(x)!$ 
+$!\lim\limits_{x\rightarrow +\infty} x\ln(x)!$ 
 ==
 
 wrong== 
@@ -25,7 +25,7 @@ $!\lim\limits_{x\rightarrow 0^+}x\ln(x)!$
 $!\lim\limits_{x\rightarrow 0^+}x^2\ln(x)!$ 
 $!\lim\limits_{x\rightarrow -\infty}\frac{\exp(x)}{x}!$ 
 $!\lim\limits_{x\rightarrow -\infty}\frac{\exp(x)}{x^2}!$ 
-$!\lim\limits_{x\rightarrow +\infty}x\exp(-x)}!$ 
+$!\lim\limits_{x\rightarrow +\infty}x\exp(-x)!$ 
 $!\lim\limits_{x\rightarrow +\infty}\frac{x^2}{\exp(x)}!$ 
 $!\lim\limits_{x\rightarrow +\infty}\frac{\ln(x)}{x}!$ 
 $!\lim\limits_{x\rightarrow +\infty}\frac{\ln(x)}{x^2}!$ 
