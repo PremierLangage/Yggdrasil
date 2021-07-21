7c49c784b877c903086ea2e25b3d5033e93c8f5e
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 11:44:43 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV07_Calculer_la_derivee_d_une_fonction_et_d_en_etudier_le_signe/Savoir_calculer_une_derivee_dun_produit.pl

---
+++ AAAA/Tony/Exos/AAV07_Calculer_la_derivee_d_une_fonction_et_d_en_etudier_le_signe/Savoir_calculer_une_derivee_dun_produit.pl
@@ -0,0 +1,30 @@
+
+extends = /model/math/expr.pl
+
+before ==
+from sympy import Symbol, diff, sin
+
+x = Symbol('x')
+
+
+f,g  =  sample([x,x**2+1,exp(x),ln(x),sin(x),cos(x),sqrt(x),x**3,x**4],2) 
+h = f*g
+sol = diff(h,x)
+==
+
+#Double dollar pour mettre le latex centré.
+question ==
+Déterminer la dérivée de
+
+$! h :x\mapsto \displaystyle {{ h|latex }} !$. Si $!\log!$ apparaît, elle désigne le logarithme népérien.
+==
+ 
+#solution pour modifier le message de solution
+solution == 
+La dérivée est {{sol|latex}}
+==
+
+message.notExpr==
+Ich bin ein berliner
+==
+ 
