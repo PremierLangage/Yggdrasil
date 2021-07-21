b1efb6c32a66a18876b05db9ff544e6befc3cb32
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 11:43:41 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV07_Calculer_la_derivee_d_une_fonction_et_d_en_etudier_le_signe/Savoir_calculer_une_derivee_dun_quotient.pl

---
+++ AAAA/Tony/Exos/AAV07_Calculer_la_derivee_d_une_fonction_et_d_en_etudier_le_signe/Savoir_calculer_une_derivee_dun_quotient.pl
@@ -0,0 +1,30 @@
+
+extends = /model/math/expr.pl
+
+before == 
+
+x = Symbol('x')
+
+a = randint(-5,5,[0])
+f,g  =  sample([x+a,x**2+a,exp(x),ln(x),sin(x),cos(x),sqrt(x),x**3+a,x**4+a],2) 
+h = f/g
+sol = diff(h,x)
+==
+
+#Double dollar pour mettre le latex centré.
+question ==
+Déterminer la dérivée de
+
+$! h \mapsto \displaystyle {{ h|latex }} !$. Si $!\log!$ apparaît, elle désigne le logarithme népérien.
+==
+ 
+#solution pour modifier le message de solution
+solution == 
+La dérivée est {{sol|latex}}
+==
+
+message.notExpr==
+Ich bin ein berliner
+==
+ 
+
