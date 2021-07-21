42d2b846bacc3b082936f26571db2d87d6ba1cd7
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 12:54:42 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Savoir_calculer_une_limite_a_laide_des_croissances_comparees.pl

---
+++ AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Savoir_calculer_une_limite_a_laide_des_croissances_comparees.pl
@@ -0,0 +1,34 @@
+extends= /model/basic/checkbox_rw.pl
+
+
+title = Limites usuelles
+
+text==
+Parmi ces limites lesquelles valent $!+\infty!$?
+==
+
+nbitems = 4
+minright = 0
+maxright = 4
+
+right== 
+$!\lim\limits_{x\rightarrow +\infty}\frac{\exp(x)}{x}!$ 
+$!\lim\limits_{x\rightarrow +\infty}\frac{\exp(x)}{x^2}!$ 
+$!\lim\limits_{x\rightarrow +\infty}-\frac{\exp(-x)}{x**3}!$ 
+$!\lim\limits_{x\rightarrow +\infty}\frac{x**3}{\ln(x)}!$  
+==
+
+wrong== 
+$!\lim\limits_{x\rightarrow 0^+}x\ln(x)!$ 
+$!\lim\limits_{x\rightarrow -\infty}\frac{\exp(x)}{x}!$ 
+$!\lim\limits_{x\rightarrow -\infty}\frac{\exp(x)}{x^2}!$ 
+$!\lim\limits_{x\rightarrow +\infty}x\exp(-x)}!$ 
+$!\lim\limits_{x\rightarrow +\infty}\frac{x^2}{\exp(x)}!$ 
+$!\lim\limits_{x\rightarrow +\infty}\frac{\ln(x)}{x}!$ 
+$!\lim\limits_{x\rightarrow +\infty}\frac{\ln(x)}{x^2}!$ 
+==
+
+
+
+
+
