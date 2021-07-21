4d47896a73b1b18f688aa355d991ecf2d3708ddc
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 12:28:28 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Connaitre_les_limites_de_fonctions_usuelles.pl

--- AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Connaitre_les_limites_de_fonctions_usuelles.pl
+++ AAAA/Tony/Exos/AAV05_Calculer_la_limite_d_une_fonction/Connaitre_les_limites_de_fonctions_usuelles.pl
@@ -4,13 +4,10 @@ before ==
 from sympy import Limit
 
 x = Symbol('x')
-
-a,b,c,d,e,f = randint(-5,5),randint(-5,5,[0]),randint(-5,5),randint(-5,5), randint(-5,5,[0]), randint(-5,5) 
-n = randint(1,5)
-f = choice([exp(x),exp(-x), x**n, sin(x),cos(x), ln(x)]) 
-
+n = randint(1,5) 
 a = choice([oo, -oo])
-lim = choice([Limit(exp(x), x, a)])
+b = choice([oo,0])
+lim = choice([Limit(exp(x), x, a), Limit(sin(x), x, a), Limit(x**n, x, a), Limit(cos(x), x, a), Limit(ln(x), x, b), Limit(sqrt(x), x, b)])
 sol = lim.doit()
 ==
 
