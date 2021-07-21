059397b4d1c00ff3a66197967e5bd3b58910b0e5
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 15:38:41 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV07_Calculer_la_derivee_d_une_fonction_et_d_en_etudier_le_signe/Savoir_calculer_une_derivee_dune_fonction_composee.pl

--- AAAA/Tony/Exos/AAV07_Calculer_la_derivee_d_une_fonction_et_d_en_etudier_le_signe/Savoir_calculer_une_derivee_dune_fonction_composee.pl
+++ AAAA/Tony/Exos/AAV07_Calculer_la_derivee_d_une_fonction_et_d_en_etudier_le_signe/Savoir_calculer_une_derivee_dune_fonction_composee.pl
@@ -8,7 +8,7 @@ from sympy import Symbol, diff
 x = Symbol('x')
 
 f =  choice([exp(x),ln(x),sin(x),cos(x),sqrt(x+1),tan(x)]) 
-g = choice([x**2+1,exp(x),ln(x),sin(x),cos(x),sqrt(x),x**3+2x**2-5,x**4+x-12])
+g = choice([x**2+1,exp(x),ln(x),sin(x),cos(x),sqrt(x),x**3+2*x**2-5,x**4+x-12])
 h = f.subs(x,g)
 sol = diff(h,x)
 ==
