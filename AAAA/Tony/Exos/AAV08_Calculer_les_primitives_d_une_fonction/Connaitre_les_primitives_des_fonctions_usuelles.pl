f0aae15a56272134730f5abbf8ae1776d0f8fe51
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 15:30:17 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV08_Calculer_les_primitives_d_une_fonction/Connaitre_les_primitives_des_fonctions_usuelles.pl

---
+++ AAAA/Tony/Exos/AAV08_Calculer_les_primitives_d_une_fonction/Connaitre_les_primitives_des_fonctions_usuelles.pl
@@ -0,0 +1,27 @@
+#Calcul de primitives usuelles
+extends = /model/math/expr.pl
+
+title = Connaître les primitives des fonctions usuelles
+before ==
+from sympy import integrate,sin,cos, ln, sqrt, exp 
+from random import choice, randint
+x = Symbol('x')
+n = randint(0,100)
+f = choice([sin(x),cos(x),x**n,ln(x), 1/x,1/(2*sqrt(x)), exp(x)])
+sol = integrate(f, x) 
+==
+
+question ==
+Déterminer une primitive de 
+
+$! f \mapsto \displaystyle {{ f|latex }} !$.
+
+Attention,il ne faut pas utiliser la notation $!C!$. 
+Si vous voulez ajouter une constante à votre fonction, il faut donner un réel.
+==
+
+equality = "UpToConstant"
+
+message.NotEqualUpToConstant = La réponse n'est pas une primitive.
+
+keypad = ["sqrt"]
