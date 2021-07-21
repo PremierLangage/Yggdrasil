c8b11f365fbcc8624d416aabcb22a795bdd07481
tony fevrier <tony.fevrier62@gmail.com>
2021-07-21 15:55:03 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/poly_expansion.pl

---
+++ AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/poly_expansion.pl
@@ -0,0 +1,16 @@
+extends = /model/math/poly.pl
+
+before ==
+x = Symbol('x')
+P = randint(-3,3,[0])*x**2 + randint(-3, 3, [0])*x + randint(-3, 3, [0])
+Q = randint(-3, 3, [0])*x + randint(-3, 3, [0])
+expr = P * Q
+sol = expr.expand()
+==
+
+question ==
+Développer l'expression suivante :
+$$ {{ expr|latex }}. $$
+==
+
+poly_form = "expanded"
