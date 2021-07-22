b3f39662784c9517b023bfa3bc55d6359ad68922
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 16:01:39 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Positivite_integrale.pl

--- AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Positivite_integrale.pl
+++ AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Positivite_integrale.pl
@@ -21,7 +21,9 @@ $!\int_{-\pi}^\pi \sin(x) dx!$
 $!\int_1^e \ln(5x)dx!$ 
 $!\int_1^e x\ln(4x)dx!$ 
  $!\int_0^{\pi} \sin(x)^5dx!$  
- $!\int_1^e \frac{3x+125x^3}{x^2}dx!$    
+ $!\int_1^e \frac{3x+125x^3}{x^2}dx!$  
+ $!\int_{3}^4 (x-3)(x-2) dx!$   
+  $!\int_{1}^2 x^2-1 dx!$
 ==
 
 wrong== 
