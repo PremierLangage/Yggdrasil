d4b634bd9d18e9d3b763f3ab68293a23930ea26e
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 10:18:10 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl

---
+++ AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl
@@ -0,0 +1,26 @@
+#Auteur  = inconnu, récupéré de la base commune, repris par Tony Février et Jeanne Parmentier
+#Date = 22/07/2021
+extends = /model/math/input.pl
+
+title = Equation avec valeur absolue
+
+before ==
+a,b=   1,2
+d=randint(1,5)
+var('x')
+lhs=Abs(a*x+b)
+lstsol=list(solveset(Eq(lhs,d),x,domain=S.Reals))
+solution=r"Les solutions sont $! \displaystyle %s !$ et $! \displaystyle %s !$." % (latex(lstsol[0]),latex(lstsol[1]))
+==
+#list_randint(2,-6,6,[0,1,-1])
+
+text ==
+Déterminer la ou les solutions de l'équation $$ {{lhs|latex}} = {{d}}. $$
+Séparer les différentes solutions par une virgule.
+==
+
+evaluator ==
+score, error = eval_set(input.value, lstsol, wobracket=True)
+feedback = feedback_message[error]
+==
+
