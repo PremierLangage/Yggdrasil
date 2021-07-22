e78999653aa66e395549d5a675416305df3126fa
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 11:22:37 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal2.pl

--- AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal2.pl
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal2.pl
@@ -7,8 +7,7 @@ Calculer le produit scalaire de deux vecteurs de norme $!{{p}}!$ et $!{{q}}!$ et
 ==
  
   
-before ==
-from math import pi,cos 
+before == 
 p =  randint(1,5)
 q = randint(1,5) 
 th = choice([pi/6, pi/4, pi/3, 3*pi/4, 2*pi/3, 5*pi/6])
