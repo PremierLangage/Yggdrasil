f5397c1043cd341f8c7132da6ab3dadf3884704e
jeanne parmentier <jeanne.parmentier@villebon-charpak.fr>
2021-07-22 10:28:18 +0200

    jparmentier modified Yggdrasil/AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl

--- AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl
+++ AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl
@@ -1,5 +1,6 @@
 #Auteur  = inconnu, récupéré de la base commune, repris par Tony Février et Jeanne Parmentier
 #Date = 22/07/2021
+#Etat = tourne dans le vide quand on met une réponse
 extends = /model/math/mathinput.pl
 
 title = Equation avec valeur absolue
