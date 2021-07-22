49b3a5cca71365ec14804dd104c8c06b440ff3d7
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 15:46:03 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP2.pl

--- AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP2.pl
+++ AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP2.pl
@@ -4,7 +4,7 @@ extends= /model/basic/checkbox_rw.pl
 title = Savoir trouver les fonctions pour faire une IPP
 
 text==
-Lesquelles de ces intégrales se traitent par IPP?
+Lesquelles de ces intégrales se traitent par une ou plusieurs IPP?
 ==
 
 nbitems = 4
@@ -13,7 +13,14 @@ maxright = 4
 
 right== 
 $!\int_0^1 2x\sin(3x)dx!$ 
-  
+$!\int_0^1 x\cos(x)dx!$ 
+$!\int_0^1 x^2\sin(4x)dx!$ 
+$!\int_0^1 x^2\cos(3x)dx!$ 
+$!\int_0^1 x\exp(-5x)dx!$ 
+$!\int_0^1 x^3\exp(-5x)dx!$ 
+$!\int_1^e \ln(5x)dx!$ 
+$!\int_1^e x\ln(4x)dx!$ 
+$!\int_1^e \ln(12x)dx!$  
 ==
 
 wrong== 
