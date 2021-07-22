2a0f7d3cc551b72eeb817cb0b2b1da6b258eaec0
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 16:07:24 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Frac_rationnelle.pl

--- AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Frac_rationnelle.pl
+++ AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Frac_rationnelle.pl
@@ -8,12 +8,7 @@ from sympy import integrate,diff
 from random import choice, randint
 x = Symbol('x')  
 k= randint(1,6)
-
-f = choice([x*sin(k*x),x*cos(k*x),x*exp(k*x),x**2*sin(k*x),x**2*cos(k*x),x**2*exp(k*x),ln(k*x), x*ln(k*x)])
-if f in [ln(k*x),x*ln(k*x)]:
-    a,b = 1,exp(1)  
-else:
-    a,b = 0,pi
+ 
 
 sol = integrate(f, (x,a, b)) 
 ==
