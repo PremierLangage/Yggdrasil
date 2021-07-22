97091ac77df2e72909ce415bae2a9b78cde155b1
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 11:17:32 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal2.pl

---
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal2.pl
@@ -0,0 +1,18 @@
+title = "Produit scalaire"
+
+extends = /model/basic/numeric.pl
+
+question ==  
+Calculer le produit scalaire de deux vecteurs de norme $!{{p}}!$ et $!{{q}}!$ et d'angle $!{{th}}!$
+==
+ 
+  
+before == 
+p =  randint(1,5)
+q = randint(1,5)
+th =  pi/choice([1,2,3,4,6])
+sol = p*q*cos(th) 
+ 
+==
+ 
+
