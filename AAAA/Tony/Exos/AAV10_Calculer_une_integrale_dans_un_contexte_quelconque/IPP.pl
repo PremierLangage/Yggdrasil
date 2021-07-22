99e3b5b5f64eb1e70c8dd6deba074c4339a5974a
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 15:14:21 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP.pl

--- AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP.pl
+++ AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP.pl
@@ -1,9 +1,7 @@
-
-#Calcul de primitives.
+ 
 extends = /model/math/expr.pl
 
-title = Savoir calculer une intégrale connaissant la primitive
-#A réglier : sympy n'a pas l'éair  de prendre ln(abs(x)). Pb lié à la valeur absolue.
+title = Savoir calculer une intégrale connaissant la primitive 
 
 before ==
 from sympy import integrate,diff
@@ -15,8 +13,7 @@ g = choice([x,x**2])
 f = choice([g*sin(k*x),g*cos(k*x),g*exp(k*x), ln(k*x),g*ln(k*x)]) 
 sol = integrate(f, (x,0, pi)) 
 ==
-
-#Type d'intégrale : pol d'ordre 2, fonction d'un multiple de x^2 ou x, u'/u avec u fonction classique
+ 
 
 
 question ==
