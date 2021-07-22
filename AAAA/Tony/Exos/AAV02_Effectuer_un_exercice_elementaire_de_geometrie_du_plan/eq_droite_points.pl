a3c916ea30dc2e7bd32b00e844fa62af92d4b62a
jeanne parmentier <jeanne.parmentier@villebon-charpak.fr>
2021-07-22 11:36:17 +0200

    jparmentier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/eq_droite_points.pl

---
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/eq_droite_points.pl
@@ -0,0 +1,28 @@
+
+#Auteur  = inconnu, récupéré de la base commune, repris par Tony Février et Jeanne Parmentier
+#Date = 22/07/2021
+#Etat = marche
+
+extends = /model/math/expr.pl
+
+before ==
+from mplsympy import plotsvg
+var('x')
+a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
+b = randint(-3, 3)
+sol = a*x + b
+xA = randint(-2, 2)
+xB = randint(-2, 2,[xA])
+yA = a*xA + b
+yB = a*xB + b
+==
+
+question ==
+Déterminer l'équation de la droite passant par les points $! ( {{ xA }}, {{ yA }} ) !$ et $! ( {{ xB }}, {{ yB }} ) !$.
+On note $! x !$ la variable d'abscisse et $! y !$ la variable d'ordonnée.
+
+==
+
+prefix ==
+$! y = !$
+==
