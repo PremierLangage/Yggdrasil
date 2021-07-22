cfae662e31a36fd7f4e921685e1894ee9c508e95
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 15:44:05 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP2.pl

---
+++ AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/IPP2.pl
@@ -0,0 +1,26 @@
+extends= /model/basic/checkbox_rw.pl
+
+
+title = Savoir trouver les fonctions pour faire une IPP
+
+text==
+Lesquelles de ces intégrales se traitent par IPP?
+==
+
+nbitems = 4
+minright = 0
+maxright = 4
+
+right== 
+$!\int_0^1 2x\sin(3x)dx!$ 
+  
+==
+
+wrong== 
+ 
+==
+
+
+
+
+
