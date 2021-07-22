970358e09936aa0ed19decfa690c7cdf3b979193
Dominique REVUZ <Dominique.Revuz@u-pem.fr>
2021-07-22 11:05:47 +0200

    drevuz modified Yggdrasil/AAAA/dominique/C/tradpy2c/d.py

--- AAAA/dominique/C/tradpy2c/d.py
+++ AAAA/dominique/C/tradpy2c/d.py
@@ -1,6 +1,12 @@
 
 
 
-def lecture():
-    return int(input("prompt>"))
+
+def FUNC(n):
+    if n>0:
+        if n%2 ==1 :
+            print(1)
+        else:
+            print(0)
+        return FUNC(n//2)
 
