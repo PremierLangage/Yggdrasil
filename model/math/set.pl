04dc7c705fa9fd2d011c6c3c1b61ad4417d8c40d
David DOYEN <David.Doyen@u-pem.fr>
2021-07-22 13:42:28 +0200

    ddoyen modified Yggdrasil/model/math/set.pl

--- model/math/set.pl
+++ model/math/set.pl
@@ -5,10 +5,14 @@ input_prefix = Réponse :
 checkratsimp = True
 symbol_dict = {'e': E}
 unauthorized_func = ['sin', 'cos', 'tan']
+wobracket = False
+local_dict = "{'e': E, 'i': I}"
 
 evaluator ==
 from ast import literal_eval
 from evalsympy import eval_set
+from sympy import sympify
+local_dict = simpify(local_dict)
 score, error = eval_set(answers['math'], sol, wobracket=wobracket, local_dict=local_dict)
 feedback = message[error]
 ==
