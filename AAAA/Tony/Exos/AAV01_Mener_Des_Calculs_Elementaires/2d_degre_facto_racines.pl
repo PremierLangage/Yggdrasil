20158df90c84cdad9a5eeec98b51b95c374aacc7
jeanne parmentier <jeanne.parmentier@villebon-charpak.fr>
2021-07-22 11:22:55 +0200

    jparmentier modified Yggdrasil/AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/2d_degre_facto_racines.pl

---
+++ AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/2d_degre_facto_racines.pl
@@ -0,0 +1,21 @@
+#Auteur  = inconnu, récupéré de la base commune, repris par Tony Février et Jeanne Parmentier
+#Date = 22/07/2021
+#Etat = marche
+extends = /model/math/poly.pl
+
+title = Savoir résoudre une équation du second degré
+
+before ==
+x = Symbol('x')
+P = x + randint(-3, 3,[0])
+Q = x + randint(-3, 3,[0])
+sol = randint(-2, 2,[0]) * P * Q
+expr = sol.expand()
+==
+
+question ==
+Factoriser l'expression suivante :
+$$ {{ expr|latex }}. $$
+==
+
+poly_form = "factorized"
