efeee26e8ef3e86ab31c361d223d7dae05aa8c94
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 16:01:08 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Positivite_integrale.pl

--- AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Positivite_integrale.pl
+++ AAAA/Tony/Exos/AAV10_Calculer_une_integrale_dans_un_contexte_quelconque/Positivite_integrale.pl
@@ -4,7 +4,7 @@ extends= /model/basic/checkbox_rw.pl
 title = Savoir utiliser la positivité et croissance de l'intégrale
 
 text==
-Lesquelles de ces intégrales sont positives?
+Lesquelles de ces intégrales sont positives ou nulles?
 ==
 
 nbitems = 4
@@ -16,7 +16,8 @@ $!\int_0^1 x\exp(x^2)dx!$
 $!\int_0^{\pi} 2x\sin(3x)dx!$ 
 $!\int_0^{\frac{\pi}{2}} x\cos(x)dx!$ 
 $!\int_{-12}^{-1} 3x^2+x^4dx!$  
-$!\int_{-1}^2 x^3 dx!$ 
+$!\int_{-1}^1 x^3 dx!$ 
+$!\int_{-\pi}^\pi \sin(x) dx!$ 
 $!\int_1^e \ln(5x)dx!$ 
 $!\int_1^e x\ln(4x)dx!$ 
  $!\int_0^{\pi} \sin(x)^5dx!$  
@@ -30,7 +31,9 @@ $!\int_0.5^1 \ln(x)dx!$
  $!\int_0^1 cos(4x)-1 dx!$  
   $!\int_0^1 sin(x)-2 dx!$  
  $!\int_{-1}^0 \frac{x}{x^2+4}dx!$ 
-    $!\int_{-1}^1 -\sqrt{x+2} dx!$  
+ $!\int_{-1}^1 -\sqrt{x+2} dx!$  
+ $!\int_{-1}^1 x^2-1 dx!$ 
+ $!\int_{2}^3 (x-3)(x-2) dx!$ 
 ==
 
 
