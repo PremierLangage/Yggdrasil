b54fb0060b97a1871459970e99cb0bb91a48205e
tony fevrier <tony.fevrier62@gmail.com>
2021-07-22 10:21:19 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl

--- AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl
+++ AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/abs_equation.pl
@@ -1,6 +1,6 @@
 #Auteur  = inconnu, récupéré de la base commune, repris par Tony Février et Jeanne Parmentier
 #Date = 22/07/2021
-extends = /model/math/input.pl
+extends = /model/math/mathinput.pl
 
 title = Equation avec valeur absolue
 
