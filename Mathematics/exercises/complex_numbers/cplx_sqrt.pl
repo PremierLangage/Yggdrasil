bd7b0a6e1b8e6b8e7208f05dacfc2b5dbcfaabc5
David DOYEN <David.Doyen@u-pem.fr>
2021-07-22 13:54:45 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/complex_numbers/cplx_sqrt.pl

--- Mathematics/exercises/complex_numbers/cplx_sqrt.pl
+++ Mathematics/exercises/complex_numbers/cplx_sqrt.pl
@@ -5,26 +5,21 @@
 
 extends = /model/math/set.pl
 
-param.roots = int
-
 wobracket = True
 
 before ==
 a=randint(1,6)
 b=randint(-6,6,[0])
 z=((a+b*I)**2).expand()
-z_tex=latex(z)
 sol=[a+b*I,-a-b*I]
-sol0_tex=latex(sol[0])
-sol1_tex=latex(sol[1])
 ==
 
 question ==
-Calculer les racines carrées de $! {{z_tex}} !$ (sous forme algèbrique).  Séparer les deux racines par une virgule.
+Calculer les racines carrées de $! {{z|tex}} !$ (sous forme algèbrique).  Séparer les deux racines par une virgule.
 ==
 
 solution ==
-Les racines carrées sont $! {{sol0_tex}} !$ et $! {{sol1_tex}} !$.
+Les racines carrées sont $! {{sol[0]|latex}} !$ et $! {{sol[0]|latex}} !$.
 ==
 
 
