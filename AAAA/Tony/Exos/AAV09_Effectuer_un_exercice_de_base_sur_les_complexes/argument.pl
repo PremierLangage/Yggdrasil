1b69f5c5dd303417c47eb99d411259ac1e5ed35b
jeanne parmentier <jeanne.parmentier@villebon-charpak.fr>
2021-07-22 16:36:27 +0200

    jparmentier modified Yggdrasil/AAAA/Tony/Exos/AAV09_Effectuer_un_exercice_de_base_sur_les_complexes/argument.pl

---
+++ AAAA/Tony/Exos/AAV09_Effectuer_un_exercice_de_base_sur_les_complexes/argument.pl
@@ -0,0 +1,27 @@
+# Author: D. Doyen
+# Tags: complex numbers
+# 19/8/2020
+
+extends = /model/math/input.pl
+
+title = Argument d'un nombre complexe
+
+before ==
+z=randitem([I,-I,1,-1,1+I,1-I,I-1,-1-I])
+z=randint(1,3)*z
+sol=arg(z)
+==
+
+text = Déterminer un argument du nombre complexe $% z= {{ z|latex }} %$.
+
+evaluator==
+score, error = eval_expr(input.value,sol,modulo=2*pi)
+feedback = message[error]
+==
+
+solution==
+Un argument de $! z !$ est $% \displaystyle {{ sol|latex }} %$.
+==
+
+
+
