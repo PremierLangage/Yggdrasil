0848753c5ae5b32292191015dd8f6be9c7207126
jeanne parmentier <jeanne.parmentier@villebon-charpak.fr>
2021-07-22 11:03:37 +0200

    jparmentier modified Yggdrasil/AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/poly_factorization.pl

--- AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/poly_factorization.pl
+++ AAAA/Tony/Exos/AAV01_Mener_Des_Calculs_Elementaires/poly_factorization.pl
@@ -1,11 +1,14 @@
+#Auteur  = inconnu, récupéré de la base commune, repris par Tony Février et Jeanne Parmentier
+#Date = 22/07/2021
+#Etat = marche. Voir pour enelver les racines nulles et rajouter un coefficient devant ? 
 extends = /model/math/poly.pl
 
 title = Savoir résoudre une équation du second degré
 
 before ==
 x = Symbol('x')
-P = x + randint(-2, 2)
-Q = x + randint(-2, 2)
+P = x + randint(-3, 3,[0])
+Q = x + randint(-3, 3,[0])
 sol = P * Q
 expr = sol.expand()
 ==
