b178c17f248fd58343e85883ca543e6fc47f96b5
David DOYEN <David.Doyen@u-pem.fr>
2021-07-23 18:42:53 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/exponential/transf_exp_.pl

--- Mathematics/exercises/analysis/exponential/transf_exp_.pl
+++ Mathematics/exercises/analysis/exponential/transf_exp_.pl
@@ -22,82 +22,13 @@ elif formula=="exp(p)/exp(q)":
 ==
 
 question ==
-Ecrire $%\displaystyle {{expr}} %$ sous la forme  $% \exp(a)%$, où $%a%$ est un nombre.
+Ecrire $%\displaystyle {{expr}} %$ sous la forme  $! \exp(a) !$, où $! a !$ est un nombre.
 ==
 
 embed ==
 \exp{ # }
 ==
 
-wims ==
-
-\if{\type=55}{
-\integer{u=randitem(-1,1)*randint(2..4)}
-\text{expr=randitem((exp(\p))^{\u} \times exp(\q),exp(\q) \times (exp(\p))^{\u})}
-\text{a=simplify((\p)*(\u) +\q)}
-}
-\if{\type=56}{
-\integer{u=randitem(-1,1)*randint(2..4)}
-\if{randitem(1,2)=1}{
-\text{expr=\frac{(exp(\p))^{\u}}{exp(\q)}}
-\text{a=simplify(\p*(\u)-(\q))}
-}{
-\text{expr=\frac{exp(\q)}{(exp(\p))^{\u}}}
-\text{a=simplify(\q-(\p)*(\u))}
-}
-}
-\if{\type=57}{
-\text{l1=shuffle(2,3,4)}
-\integer{u=randitem(-1,1)*\l1[1]}
-\integer{v=randitem(-1,1)*\l1[2]}
-\text{expr=(exp(\p))^{\u} \times (exp(\q))^{\v}}
-\text{a=simplify((\p)*(\u)+(\q)*(\v))}
-}
-\if{\type=58}{
-\text{l1=shuffle(2,3,4)}
-\integer{u=randitem(-1,1)*\l1[1]}
-\integer{v=randitem(-1,1)*\l1[2]}
-\text{expr=\frac{(exp(\p))^{\u}}{(exp(\q))^{\v}}}
-\text{a=simplify((\p)*(\u)-(\q)*(\v))}
-}
-\if{\type=59}{
-\text{typerep=algexp}
-\integer{q=abs(\q)}
-\text{expr=randitem(exp(\p)\times \q,\q \times exp(\p))}
-\text{a=simplify(\p+ln(\q))}
-}
-\if{\type=60}{
-\text{typerep=algexp}
-\integer{q=abs(\q)}
-\if{randitem(1,2)=1}{
-\text{expr=\frac{exp(\p)}{\q}}
-\text{a=simplify(\p-ln(\q))}
-}{
-\text{expr=\frac{\q}{exp(\p)}}
-\text{a=simplify(ln(\q)-\p)}
-}
-}
-\if{\type=61}{
-\text{typerep=algexp}
-\integer{q=abs(\q)}
-\integer{u=randitem(-1,1)*randint(2..4)}
-\text{expr=randitem(exp(\p)^{\u}\times \q,\q \times exp(\p)^{\u})}
-\text{a=simplify((\p)*(\u)+ln(\q))}
-}
-\if{\type=62}{
-\text{typerep=algexp}
-\integer{q=abs(\q)}
-\integer{u=randitem(-1,1)*randint(2..4)}
-\if{randitem(1,2)=1}{
-\text{expr=\frac{exp(\p)^{\u}}{\q}}
-\text{a=simplify((\p)*(\u)-ln(\q))}
-}{
-\text{expr=\frac{\q}{exp(\p)^{\u}}}
-\text{a=simplify(ln(\q)-(\p)*(\u))}
-}
-}
-
-==
 
 
 
