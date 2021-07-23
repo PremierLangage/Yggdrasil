18a4af1233759f3bb73821c46dedefe41151ab04
David DOYEN <David.Doyen@u-pem.fr>
2021-07-23 18:28:04 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/exponential/exp_equation.pl

--- Mathematics/exercises/analysis/exponential/exp_equation.pl
+++ Mathematics/exercises/analysis/exponential/exp_equation.pl
@@ -1,10 +1,17 @@
-extends = /model/mathinput.pl
+# Author: D. Doyen
+# Tags: exponetial, equation
+# Résoudre uen équation avec exponentielle
+# Parameters:
+# - nbsize: taille des nombres
+# 19/7/2021
 
-title = Equation avec exponentielle
+extends = /model/math/expr.pl
 
 before ==
+from sympy import solveset, S
 var('x')
-a,b=list_randint(2,-8,8,[0])
+a = randint(-8,8,[0])
+b= randint(-8,8,[0])
 lhs = exp(a*x+b)
 rhs = randint(1,8)
 sol = list(solveset(lhs-rhs,x,domain=S.Reals))[0]
