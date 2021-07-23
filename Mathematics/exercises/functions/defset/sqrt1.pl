a2a68c736504b3b5d1a04663a070dcbe36a0ec37
David DOYEN <David.Doyen@u-pem.fr>
2021-07-23 22:54:35 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/functions/defset/sqrt1.pl

--- Mathematics/exercises/functions/defset/sqrt1.pl
+++ Mathematics/exercises/functions/defset/sqrt1.pl
@@ -6,22 +6,16 @@
 extends = /model/math/interval.pl
 
 before ==
+from sympy.solvers.inequalities import solve_univariate_inequality
 a=randint(-6,6,[0,1,-1])
 b=randint(-6,6,[0])
-x=symbols('x')
+var('x')
 f=sqrt(a*x+b)
-latexf=latex(f)
-from sympy.solvers.inequalities import solve_univariate_inequality
-x=symbols('x')
 sol = solve_univariate_inequality(a*x+b >= 0, x, relational=False)
-ssol=str(sol)
 ==
 
-text ==
-{{ssol}}
-Déterminer l'ensemble de définition de la fonction $%f : x \mapsto {{latexf}}%$.
-==
+question ==
+Déterminer l'ensemble de définition (maximale) de la fonction 
 
-evaluator==
-score, feedback, _ = eval_rset(input.value, sol)
+$$ f : x \mapsto {{ f|latex }}.$$
 ==
