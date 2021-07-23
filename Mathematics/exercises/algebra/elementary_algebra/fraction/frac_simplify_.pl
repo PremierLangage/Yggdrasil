b9b7781cc13fbc7e12079c54588e6de09e6e8c0e
David DOYEN <David.Doyen@u-pem.fr>
2021-07-23 10:34:15 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl

--- Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl
+++ Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl
@@ -2,9 +2,7 @@
 # Tags : fractions
 # 19/8/2020
 
-extends = /model/mathquill.pl
-
-title = Simplification d'une fraction
+extends = /model/mathq/expr.pl
 
 before ==
 if param['numbers'] == "small":
@@ -13,9 +11,9 @@ if param['numbers'] == "small":
 if param['numbers'] == "large":
     lstf=[[3,2],[4,3],[5,4],[5,3],[5,2],[6,5],[7,6],[7,5],[7,4],[7,3],[7,2],[8,7],[8,5],[8,3],[9,8],[9,7],[9,5],[9,4],[9,2]]
     lstc=[16,18,21,22,24,25,27,28,32,33,35]
-f=randitem(lstf)
-rd.shuffle(f)
-c=randitem(lstc)
+f=choice(lstf)
+shuffle(f)
+c=choice(lstc)
 a,b=c*f[0],c*f[1]
 sol=Rational(a,b)
 ==
