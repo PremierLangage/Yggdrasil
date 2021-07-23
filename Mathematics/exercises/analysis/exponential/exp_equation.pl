320ccd8c143cdc6b0019d0a6ac11d1a4add3b222
David DOYEN <David.Doyen@u-pem.fr>
2021-07-23 18:28:19 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/exponential/exp_equation.pl

--- Mathematics/exercises/analysis/exponential/exp_equation.pl
+++ Mathematics/exercises/analysis/exponential/exp_equation.pl
@@ -17,18 +17,6 @@ rhs = randint(1,8)
 sol = list(solveset(lhs-rhs,x,domain=S.Reals))[0]
 ==
 
-text ==
+question ==
 Déterminer la solution de l'équation $$ {{lhs|latex}} = {{rhs|latex}}.$$
 ==
-
-evaluator ==
-score, error = eval_expr(input.value, sol)
-feedback = feedback_message[error]
-==
-
-solution ==
-La solution est $! {{ sol|latex }} !$.
-==
-
-
-
