57877eca73f44df578b9511a015856a19f40542e
David DOYEN <David.Doyen@u-pem.fr>
2021-07-23 10:50:38 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl

--- Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl
+++ Mathematics/exercises/algebra/elementary_algebra/fraction/frac_simplify_.pl
@@ -2,16 +2,16 @@
 # Tags: fractions, irreducible fraction, gcd
 # Simplifier une fraction
 # Parameters:
-# - numbers: taille des nombres
+# - nbsize: taille des nombres
 # 19/7/2021
 
 extends = /model/math/expr.pl
 
 before ==
-if param['numbers'] == "small":
+if param['nbsize'] == "small":
     lstf=[[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1],[3,2],[4,3],[5,4],[5,3],[5,2],[6,5],[7,6],[7,5],[7,4],[7,3],[7,2],[8,7],[8,5],[8,3]]
     lstc=[5,6,7,8,9,11,12,14,15]
-if param['numbers'] == "large":
+if param['nbsize'] == "large":
     lstf=[[3,2],[4,3],[5,4],[5,3],[5,2],[6,5],[7,6],[7,5],[7,4],[7,3],[7,2],[8,7],[8,5],[8,3],[9,8],[9,7],[9,5],[9,4],[9,2]]
     lstc=[16,18,21,22,24,25,27,28,32,33,35]
 f=choice(lstf)
