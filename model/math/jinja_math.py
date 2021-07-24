6199e87a84fe03b494dfa4c0096d781eaeeb85d4
David DOYEN <David.Doyen@u-pem.fr>
2021-07-24 11:11:48 +0200

    ddoyen modified Yggdrasil/model/math/jinja_math.py

---
+++ model/math/jinja_math.py
@@ -0,0 +1,19 @@
+from jinja2 import Environment, BaseLoader, contextfilter
+from sympy2latex import latex
+
+def component(l):
+    if isinstance(l, dict):
+        selector = l["selector"]
+        cid = l["cid"]
+    else:
+        selector = l.selector
+        cid = l.cid
+    return "<%s cid='%s'></%s>" % (selector, cid, selector)
+
+@contextfilter
+def customlatex(ctx, expr):
+    return latex(expr, **ctx.get('latexsettings', {}))
+    
+Env = Environment(loader=BaseLoader())
+Env.filters["component"] = component
+Env.filters["latex"] = customlatex
