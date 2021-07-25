0e6be5d7d3132b4c5ef5a2e8468ebfd529178a14
David DOYEN <David.Doyen@u-pem.fr>
2021-07-25 11:48:05 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl
@@ -1,6 +1,6 @@
 extends = /model/jsxgraph/expr.pl
 
-param.lstangle = [pi/4,pi/2,3*pi/4,pi,5*pi/4,3*pi/2,7*pi/4]
+# param.lstangle = [pi/4,pi/2,3*pi/4,pi,5*pi/4,3*pi/2,7*pi/4]
 
 before ==
 lstangle=eval(param['lstangle'])
@@ -12,9 +12,9 @@ yM=float(sinangle)
 jxg.setscript(script, globals())
 ==
 
-jxg.attributes % {"boundingbox": [-1.25, 1.25, 1.25, -1.25], "showNavigation": false, "axis": false, "grid": false}
+# jxg.attributes % {"boundingbox": [-1.25, 1.25, 1.25, -1.25], "showNavigation": false, "axis": false, "grid": false}
 
-jinja_keys % ["question", "script", "text", "inputblock", "solution", "prefix"]
+# jinja_keys % ["question", "script", "text", "inputblock", "solution", "prefix"]
 
 
 script ==
