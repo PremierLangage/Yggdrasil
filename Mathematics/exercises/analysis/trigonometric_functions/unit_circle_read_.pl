e41dc660ed7fc7289a8b4453b7ff90b182e2d8ba
David DOYEN <David.Doyen@u-pem.fr>
2021-07-25 11:49:34 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl
@@ -16,7 +16,16 @@ yM=float(sinangle)
 
 # jinja_keys % ["question", "script", "text", "inputblock", "solution", "prefix"]
 
-
+script ==
+var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
+var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
+var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
+var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
+var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
+var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
+var M = board.create('point',[ {{xM}} , {{yM}} ],{size:1,name:'M',color:'black',fixed:true});
+var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+==
 
 
 
