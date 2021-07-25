1873b6d679fb24452c7cd73b79c29e5610797f14
David DOYEN <David.Doyen@u-pem.fr>
2021-07-25 11:48:48 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_.pl
@@ -9,7 +9,7 @@ cosangle=cos(angle).evalf()
 sinangle=sin(angle).evalf()
 xM=float(cosangle)
 yM=float(sinangle)
-#jxg.setscript(script, globals())
+jxg.setscript(script, globals())
 ==
 
 # jxg.attributes % {"boundingbox": [-1.25, 1.25, 1.25, -1.25], "showNavigation": false, "axis": false, "grid": false}
@@ -22,10 +22,6 @@ var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
 var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
 var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
 var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
-var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
-var M = board.create('point',[ {{xM}} , {{yM}} ],{size:1,name:'M',color:'black',fixed:true});
-var secOAM = board.create('sector',[O,A,M],{color:'orange'});
 ==
 
 
