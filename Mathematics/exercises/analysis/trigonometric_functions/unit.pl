9ebbbdef6c71a7237dcbfd9e816e0724454f4720
David DOYEN <David.Doyen@u-pem.fr>
2021-07-25 12:35:52 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit.pl
@@ -1,11 +1,31 @@
 extends = /model/math/expr.pl
-
 jxg =: MathDrawer
 @ /utils/components/jsxgraph.py [customjsxgraph.py]
 jxg.decorator = CustomJSXGraph
 
+param.lstangle = [pi/4,pi/2,3*pi/4,pi,5*pi/4,3*pi/2,7*pi/4]
+
 before ==
-sol = 1
+lstangle=eval(param['lstangle'])
+angle=choice(lstangle)
+cosangle=cos(angle).evalf()
+sinangle=sin(angle).evalf()
+xM=float(cosangle)
+yM=float(sinangle)
+sol = 0
+jxg.setscript(script, globals())
 ==
 
+jxg.attributes % {"boundingbox": [-1.25, 1.25, 1.25, -1.25], "showNavigation": false, "axis": false, "grid": false}
+
+script ==
+var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
+var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
+var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
+var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
+var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
+var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
+var M = board.create('point',[ {{xM}} , {{yM}} ],{size:1,name:'M',color:'black',fixed:true});
+var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+==
 
