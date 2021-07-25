95dc66f6144b0c76f5465d688c550e648a575b8c
David DOYEN <David.Doyen@u-pem.fr>
2021-07-25 10:31:55 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/functions/defset/ln1.pl

---
+++ Mathematics/exercises/analysis/functions/defset/ln1.pl
@@ -0,0 +1,23 @@
+# Author: D. Doyen
+# Tags: definition set, logarithm, linear inequality
+# Déterminer l'ensemble de définition (maximal) d'une fonction
+# 19/7/2021
+
+extends = /model/math/interval.pl
+
+before ==
+from sympy.solvers.inequalities import solve_univariate_inequality
+a=randint(-6,6,[0,1,-1])
+b=randint(-6,6,[0])
+var('x')
+f=ln(a*x+b)
+sol = solve_univariate_inequality(a*x+b > 0, x, relational=False)
+==
+
+question ==
+Déterminer l'ensemble de définition (maximal) de la fonction 
+
+$$f : x \mapsto {{ f|latex }}.$$
+==
+
+latexsettings.interv_rev_brack % "False"
