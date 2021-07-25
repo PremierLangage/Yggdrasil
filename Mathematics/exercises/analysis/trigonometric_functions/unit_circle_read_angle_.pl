6eedb6b1d80dbebfd55b7a5d547ca35d710a3159
David DOYEN <David.Doyen@u-pem.fr>
2021-07-25 11:55:59 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
@@ -2,4 +2,7 @@ extends =  unit_circle_read_.pl
 
 question ==
 Quelle est la mesure (en radians) de l'angle $% (\overrightarrow{OA},\overrightarrow{OM}) %$ représenté ci-dessous. 
+<div class="img img-60">
+{{ jxg|component }}
+</div>
 ==
