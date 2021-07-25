017ac9a745e016d7471ca4e3516077b42a83e1c5
David DOYEN <David.Doyen@u-pem.fr>
2021-07-25 11:44:40 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
@@ -1,7 +1,5 @@
 extends =  unit_circle_read_.pl
-extends =  /model/math/expr.pl
 
 question ==
 Quelle est la mesure (en radians) de l'angle $% (\overrightarrow{OA},\overrightarrow{OM}) %$ représenté ci-dessous. 
-{{ jxg|component }}
 ==
