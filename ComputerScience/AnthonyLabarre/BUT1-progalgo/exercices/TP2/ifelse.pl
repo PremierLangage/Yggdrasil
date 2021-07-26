59e701d4fce2036829465eb7383256ceca3af302
Carine PIVOTEAU <Carine.Pivoteau@u-pem.fr>
2021-07-26 11:48:49 +0200

    cpivoteau modified Yggdrasil/ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP2/ifelse.pl

--- ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP2/ifelse.pl
+++ ComputerScience/AnthonyLabarre/BUT1-progalgo/exercices/TP2/ifelse.pl
@@ -22,35 +22,47 @@ evaluator== #|python|
 import random
 import sys 
 import re
+import string
 import tools # fonctions auxiliaires
 
+_foo = ''
+
+def _foo_print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False):
+    global _foo
+    _foo += sep.join(map(str,objects)) + end
+
 code = response['answer']
-code2 = re.sub("print *\(", "_foo=(", code)
 
 # tente d'executer, puis verifie la présence de la variable demandée. si ok, check valeur et syntaxe.
-try:
-    x=42
-    exec(code2)
-except Exception as e:
-    msg = tools.wrong("Le code ne compile pas, il provoque l'erreur suivante : " + str(e))
-    grade = 0, msg 
-else:
-    if not '_foo' in locals():
-         grade = tools.grade_wrong("il manque au moins un cas")    
-    else:
-        if _foo != 'OK':
-            grade = tools.grade_wrong('Hum... vous affichez ' + _foo + ' pour la valeur x = ' + str(x))
+if not 'print' in code :
+    grade = tools.grade_wrong("Hum... quelle fonction utilisez-vous pour l'affichage ?")    
+else: 
+    if not '(' in code :
+        grade = tools.grade_wrong("La fonction print s'utilise avec des parenthèès en python 3 !")    
+    else: 
+        try:
+            x=42
+            exec(code.replace('print','_foo_print'))
+        except Exception as e:
+            msg = tools.wrong("Le code ne compile pas, il provoque l'erreur suivante : " + str(e))
+            grade = 0, msg 
         else:
-            del _foo
-            x=random.choice([-10,-2,0,1,32,4,2,41,43,100,4222222])
-            exec(code2)
-            if not '_foo' in locals():
-                grade = tools.grade_wrong("il manque au moins un cas")    
+            if '_foo' == '':
+                grade = tools.grade_wrong("Il manque probablement un cas")    
             else:
-                if _foo != 'KO':
+                if _foo != 'OK':
                     grade = tools.grade_wrong('Hum... vous affichez ' + _foo + ' pour la valeur x = ' + str(x))
                 else:
-                    grade= 100, tools.good('Bonne r&#233;ponse !')
+                    del _foo
+                    x=random.choice([-10,-2,0,1,32,4,2,41,43,100,4222222])
+                    exec(code2)
+                    if '_foo' == '':
+                        grade = tools.grade_wrong("Il manque probablement un cas") 
+                    else:
+                        if _foo != 'KO':
+                            grade = tools.grade_wrong('Hum... vous affichez ' + _foo + ' pour la valeur x = ' + str(x))
+                        else:
+                            grade= 100, tools.good('Bonne r&#233;ponse !')
 ==
 
 
