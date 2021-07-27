81af480ab952d1cc759110b7c772c37690388437
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2021-07-27 11:18:04 +0200

    ochampalle modified Yggdrasil/AAAA/OCE/caroussel_template_OCE_V2.pl

--- AAAA/OCE/caroussel_template_OCE_V2.pl
+++ AAAA/OCE/caroussel_template_OCE_V2.pl
@@ -161,9 +161,9 @@ for radio in ListStructQuestion:
     text += '''<p><b><u>'''
     text += 'Question ' + str(nb+1) + '</u></b></p><br>'''
     text += '<div>'+q_mix_item[nb][0]+'</div><br>'
-    # text += '<br>'+ str(radio.cid) +'</br>'
     selector=radio.selector
     cid=radio.cid
+    text += '<br>'+ str(radio.cid) +'</br>'
     text += f"""<div><{selector} cid='{cid}'></{selector}></div>"""
     # text += list_questions[indices_questions[0]]["html_form"]
     text += '''</div>'''
