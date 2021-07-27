b20d6c6428fda036a26a61603f2652a351a6e5b1
David DOYEN <David.Doyen@u-pem.fr>
2021-07-27 21:26:49 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl

--- Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl
+++ Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl
@@ -16,7 +16,7 @@ interv = choice([Interval.Lopen(x1, x2), Interval.Ropen(x1, x2)])
 expr = a*x+b
 
 sol = imageset(x, a*x+b, interv) 
-
+sol_chainineq = latex_chainineq(expr, sol) 
 ineq = latex_chainineq(x, interv) 
 ==
 
