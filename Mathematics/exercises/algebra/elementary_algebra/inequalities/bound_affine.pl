acf965d99b40df81f04f78391da9a6c14b99a268
David DOYEN <David.Doyen@u-pem.fr>
2021-07-27 19:47:58 +0200

    ddoyen modified Yggdrasil/Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl

--- Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl
+++ Mathematics/exercises/algebra/elementary_algebra/inequalities/bound_affine.pl
@@ -12,16 +12,7 @@ b=randint(-8,8,[0])
 x1, x2 = sorted(sampleint(-8, 8, 2))
 s1, s2 = choices(['<','<='], k=2)
 
-if a>0:
-    elem = [a*x1+b, s1, s2, a*x2+b]
-else:
-    elem = [a*x2+b, s2, s1, a*x1+b]
-
-sol = Interval(elem[0], elem[3])
-if elem[1] == '<':
-    sol.left_open = True
-if elem[2] == '<':
-    sol.left_open = True  
+sol = imageset(x, a*x+b, Interval(x1, x2)) 
 
 ineq = " ".join(latex([x1, s1, x, s2, x2]))
 expr = a*x+b
