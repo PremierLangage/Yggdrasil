3253ef970a87f3fd783091acfd7ee8ee7711a5ff
David DOYEN <David.Doyen@u-pem.fr>
2021-07-27 13:04:04 +0200

    ddoyen modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -192,6 +192,8 @@ function onBeforeSubmitPL() {
     if (mathField instanceof MQ.StaticMath) {
       if (mathField.innerFields.length == 1) {
       mathFieldInput.value = mathField.innerFields[0].latex();
+            mathFieldInput.value = "aa";
+
     } else {
       mathFieldInput.value = "aa";
     }
