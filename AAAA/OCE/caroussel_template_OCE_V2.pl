acf6a3b4a1c41948214141a971b74675d41fa06a
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2021-07-27 09:55:14 +0200

    ochampalle modified Yggdrasil/AAAA/OCE/caroussel_template_OCE_V2.pl

--- AAAA/OCE/caroussel_template_OCE_V2.pl
+++ AAAA/OCE/caroussel_template_OCE_V2.pl
@@ -156,19 +156,21 @@ for nb in range (nb_quest):
         radio.items.append({"id": "b"+str(i), "content": s})
     random.shuffle(radio.items)
     ListStructQuestion[nb].items=radio.items.copy()
-# autre boucle pour la constitution du texte
-# Affichage des questions
-if(nb==0):# Seule la première question est active
-    text += '''<div class="item active">'''
-else:
-    text += '''<div class="item">'''
-text += '''<p><b><u>'''
-text += 'Question ' + str(nb+1) + '</u></b></p><br>'''
-text += '<div>'+q_mix_item[nb][0]+'</div><br>'
-# text += '<br>'+ str(radio.cid) +'</br>'
-text += f"""<div><{radio.selector} cid='{radio.cid}'></{radio.selector}></div>"""
-# text += list_questions[indices_questions[0]]["html_form"]
-text += '''</div>'''
+
+for nb in range (nb_quest):
+    # autre boucle pour la constitution du texte
+    # Affichage des questions
+    if(nb==0):# Seule la première question est active
+        text += '''<div class="item active">'''
+    else:
+        text += '''<div class="item">'''
+    text += '''<p><b><u>'''
+    text += 'Question ' + str(nb+1) + '</u></b></p><br>'''
+    text += '<div>'+q_mix_item[nb][0]+'</div><br>'
+    # text += '<br>'+ str(radio.cid) +'</br>'
+    text += f"""<div><{ListStructQuestion[nb].selector} cid='{ListStructQuestion[nb].cid}'></{ListStructQuestion[nb].selector}></div>"""
+    # text += list_questions[indices_questions[0]]["html_form"]
+    text += '''</div>'''
 ==
 
 
