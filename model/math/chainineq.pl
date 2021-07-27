540dede7aa36a06b00ec4931174637c2430e0d95
David DOYEN <David.Doyen@u-pem.fr>
2021-07-27 15:15:08 +0200

    ddoyen modified Yggdrasil/model/math/chainineq.pl

--- model/math/chainineq.pl
+++ model/math/chainineq.pl
@@ -26,11 +26,6 @@ from sympy import S, solveset, Intersection
 var('x')
 b1, b2 = answers['math'].split(',')
 
-try:
-    S1 = solveset(latex2sympy(b1 + ' x'),x,domain=S.Reals))
-    S2 = solveset(latex2sympy('x ' + b2),x,domain=S.Reals))
-except:
-    pass
 
 feedback = str(latex2sympy(b1 + ' x')) # str(Intersection(S1, S2))
 score = -1
