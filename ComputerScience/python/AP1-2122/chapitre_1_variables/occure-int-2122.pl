ccc3127eb993726b946e56f4963cc27319e480eb
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2021-08-17 17:21:32 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-2122/chapitre_1_variables/occure-int-2122.pl

---
+++ ComputerScience/python/AP1-2122/chapitre_1_variables/occure-int-2122.pl
@@ -0,0 +1,50 @@
+# Non-testé parce que PL ne semble pas marcher pour le moment...
+# author=Wenjie Fang
+
+title=Distinguer le six !
+
+tag=input|print|variable|if|elif|else|conditionnelles
+
+extends=/ComputerScience/python/template/soluce.pl
+ 
+@ /builder/before.py [builder.py]
+
+soluce==  
+n=int(input())
+if n > 6:
+	print("Trop grand !")
+elif n < 6:
+    print("Trop petit !")
+else:
+  	print("Tout juste !")
+==
+
+code==
+# lecture au clavier, affectation
+nombre=int(input())
+==
+
+text==
+Ecrivez un programme utilisant un **if elif else** et 
+une unique variable **nombre** qui lit un nombre du clavier et qui le compare avec 6.
+
+- Si **nombre** est plus petit que 6, votre programme doit afficher "Trop petit !".
+- Si **nombre** est exactement 6, votre programme doit afficher "Tout juste !".
+- Si **nombre** est plus grand que 6, votre programme doit afficher "Trop grand !".
+==
+
+before==
+==
+
+plsoluce==
+Test 1 |10
+Test 2 |6
+Test 3 |6
+Test 4 |-2
+==
+
+
+
+
+
+
