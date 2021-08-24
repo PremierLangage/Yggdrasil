38c28f3aaac598f27cc2ee0c767f52f176d7a3d7
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2021-08-24 15:29:33 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-2122/chapitre_1_variables/rectanglefixe-2021.pl

---
+++ ComputerScience/python/AP1-2122/chapitre_1_variables/rectanglefixe-2021.pl
@@ -0,0 +1,34 @@
+
+#author=zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/template/soluce.pl
+
+title = Rectangle fixe
+
+tag = print
+
+text== 
+Les dimensions d'un rectangle sont 3 et 7.
+
+Ecrire un programme qui calcule et affiche le périmètre et la surface du rectangle
+sous la forme :
+
+Le périmètre est ?? et la surface est ?? !
+==
+code==
+"Le périmètre est ?? et la surface est ?? !"
+==
+
+soluce==
+print("Le périmètre est 20 et la surface est 21 !")
+==
+
+plsoluce==
+Le test |Le périmètre est 20 et la surface est 21 !
+==
+
+
+
+
+
