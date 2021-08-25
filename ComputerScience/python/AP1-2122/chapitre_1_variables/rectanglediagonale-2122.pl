7ee7bd1b7a9ab2b3af66dcf5008a503e77ecc826
Olivier CHAMPALLE <olivier.champalle@u-pem.fr>
2021-08-25 09:18:18 +0200

    ochampalle modified Yggdrasil/ComputerScience/python/AP1-2122/chapitre_1_variables/rectanglediagonale-2122.pl

---
+++ ComputerScience/python/AP1-2122/chapitre_1_variables/rectanglediagonale-2122.pl
@@ -0,0 +1,40 @@
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+
+title = Diagonale d'un rectangle
+
+tag=input|print|puissance
+
+text==  
+Ecrire un programme qui lit les dimensions d'un rectangle au clavier, puis calcule et affiche le périmètre et la surface du rectangle ainsi que
+la longueur de la diagonale sous la forme :
+
+Le périmètre est ?? et la surface est ?? ! 
+
+La diagonale mesure ?? !
+(Pensez à **)
+
+<u>**Information**</u> : Le programme testeur fournira les deux valeurs pour évaluation. 
+==
+
+soluce==
+l=int(input())
+la=int(input())
+print("Le périmètre est",2*(l+la),"et la surface est",l*la,"!")
+print("La diagonale mesure",(l*l+la*la)**0.5,"!")
+==
+mplsoluce0==
+essai1
+2 
+3
+==
+
+mplsoluce1==
+essai1
+2 
+3
+==
+before=
+
