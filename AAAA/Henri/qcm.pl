8361c7a4ecd61d1d30f8b42b6c7b69d7318d69e8
Henri DERYCKE <Henri.Derycke@u-pem.fr>
2021-09-08 11:21:02 +0200

    hderycke modified Yggdrasil/AAAA/Henri/qcm.pl

--- AAAA/Henri/qcm.pl
+++ AAAA/Henri/qcm.pl
@@ -2,17 +2,19 @@ extends = /model/basic/checkbox_rw.pl
 
 question ==
 Le fichier  suivant contient potentiellement des erreurs de syntaxes. 
-		
-```C
-#include <stdio.h>
+
+<link rel="stylesheet" href="/path/to/styles/default.css">
+<script src="/path/to/highlight.min.js"></script>
+<script>hljs.highlightAll();</script>
+
+<pre><code class="language-c">#include &lt;stdio.h&gt;
 
 int main(void) {
     int i;
-    for(i=0; i<3; i+=1)
+    for(i=0; i&lt;3; i+=1)
        printf("%d",i);
     return 0
-}
-```
+}</code></pre>
 
 Combien y en a t-il ?
 ==
