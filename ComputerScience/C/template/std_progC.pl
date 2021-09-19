68cd3784f61e55af39bca0b8162e39622fb2d4f5
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-19 10:16:14 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/template/std_progC.pl

--- ComputerScience/C/template/std_progC.pl
+++ ComputerScience/C/template/std_progC.pl
@@ -80,7 +80,7 @@ nb_attempt=0
 # Place here your favorite C compiler
 compiler="gcc"
 # PLace here the compilation flags
-cflags=["-Wall", "-std=c17"]
+cflags=["-Wall", "-ainsi"]
 # Place here library flags
 libflags=[]
 
