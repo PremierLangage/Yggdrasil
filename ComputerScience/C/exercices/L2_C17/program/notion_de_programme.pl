95deaa67f54a14fe82fd118058acaa9352e2b197
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-19 19:28:33 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/program/notion_de_programme.pl

---
+++ ComputerScience/C/exercices/L2_C17/program/notion_de_programme.pl
@@ -0,0 +1,28 @@
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+author=Prog C2 Team
+
+title=Premiers exercices sur la notion de programme
+
+introduction==
+Les exercices qui suivent illustrent la notion de programme en C. Ce sont des 
+exercices simples, parfois des questions de cours, et de découverte du langage C.
+
+Attention, le compilateur utilisé sera ici `gcc` mais ce dernier sera utilisé avec
+les flags `-Wall` et `-std=c17`. Ainsi, même si les sandbox n'utilisent pas 
+encore le compilateur `clang`, les exercices seront bien sur le standard C 2017.
+==
