dea0224aa3491ce7e1929f335b3e1bfca9ca0eda
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-19 19:30:11 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/program/hello.pl

---
+++ ComputerScience/C/exercices/L2_C17/program/hello.pl
@@ -0,0 +1,19 @@
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/exercices/program/hello.pl
+
+extends=/ComputerScience/C/template/std_progC17_clang.pl
