f528a37d9fdb585e4b0d7e87860faa4e9880190f
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-19 10:07:31 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/template/std_progC17_clang.pl

---
+++ ComputerScience/C/template/std_progC17_clang.pl
@@ -0,0 +1,22 @@
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+#*****************************************************************************
+# This is an adaptation of general template for prog C L2 - S3
+# --> clang and -std=c17
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
