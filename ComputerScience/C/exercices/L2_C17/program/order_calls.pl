dae7a09138224d1a962220e4d3307d50109e04cf
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-19 20:07:37 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/program/order_calls.pl

--- ComputerScience/C/exercices/L2_C17/program/order_calls.pl
+++ ComputerScience/C/exercices/L2_C17/program/order_calls.pl
@@ -14,7 +14,7 @@
 #            https://creativecommons.org/licenses/by-sa/3.0/fr/
 #*****************************************************************************
 
-extends=/ComputerScience/C/template/std_progC.pl
+extends=/ComputerScience/C/template/std_progC17_clang.pl
 
 author=Nicolas Borie
 title=Ordonner des appels de fonctions
