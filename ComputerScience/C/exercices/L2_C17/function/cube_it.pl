c10927df0f735e9027cc8a315ae875350d2d61ee
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-20 14:23:48 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/function/cube_it.pl

--- ComputerScience/C/exercices/L2_C17/function/cube_it.pl
+++ ComputerScience/C/exercices/L2_C17/function/cube_it.pl
@@ -22,21 +22,21 @@ author=Dominique Revuz
 title=Mettre au cube une valeur pointée
 tag=function|pointer|variable
 
-text==
+text==#|markdown|
 Écrire une function **cube_it** qui prend l'adresse d'un entier (pointeur vers 
 un entier) et remplace la valeur pointée par son cube (c'est à dire l'ancienne 
 valeur élevée à la puissance 3). Faire un choix cohérent pour le prototype de 
 la fonction.
 ==
 
-editor.code==
+editor.code==#|c|
 ... cube_it(...){
-  ...
+  // Votre code ici...
 }
 ==
 
 
-solution==
+solution==#|c|
 
 void cube_it(int *p)
 {
