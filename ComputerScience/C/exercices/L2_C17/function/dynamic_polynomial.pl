3ca254661f243f1ecedf4bbfbcbc3ae8047906d0
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-20 15:35:39 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/function/dynamic_polynomial.pl

--- ComputerScience/C/exercices/L2_C17/function/dynamic_polynomial.pl
+++ ComputerScience/C/exercices/L2_C17/function/dynamic_polynomial.pl
@@ -110,4 +110,11 @@ checks_args_stdin==#|python|
  ["Test aléatoire", [str(randint(-10, 10)) for i in range(3)], ""]]
 ==
 
+astuces==#|python|
+[
+  { "content": """En informatique, tout produit doit bien être représenté avec l'opérateur `*`, ce dernier ne peut pas être élidé."""},
+  { "content": """Trois arguments de type entier : `int a, int b, int c` semble une bonne solution."""},
+  { "content": """Une ligne suffit `return ...` avec l'expression arithmétique bien paranthèsée contenant bien pour les `*` et les `+`."""}
+]
+==
 
