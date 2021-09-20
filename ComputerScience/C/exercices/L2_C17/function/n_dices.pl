9d3c17b13ddb3cea236da0b0776f29baeff7a117
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-21 00:03:44 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/function/n_dices.pl

--- ComputerScience/C/exercices/L2_C17/function/n_dices.pl
+++ ComputerScience/C/exercices/L2_C17/function/n_dices.pl
@@ -98,8 +98,9 @@ checks_args_stdin==#|python|
 
 astuces==#|python|
 [
-  { "content": """"""},
-  { "content": """"""},
+  { "content": """Une manière raisonnable de résoudre ce problème est de faire une fonction récursive."""},
+  { "content": """Pour un seul dé, il n'y a qu'une seule manière d'obtenir les valeurs comprises entre 1 et 6 toutes deux incluses."""},
+  { "content": """Pour plus de dé, selon les 6 valeurs possibles du dernier dés, il faut procéder à un appel récursif et sommer le nombre de manières à chaque fois."""}
 ]
 ==
 
