2e8211b41d3b6e2e8d3e641600c23e6bd6287ec9
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-20 15:40:32 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/template/std_progC17_clang.pl

--- ComputerScience/C/template/std_progC17_clang.pl
+++ ComputerScience/C/template/std_progC17_clang.pl
@@ -47,6 +47,7 @@ if "taboo" in globals():
 
 if "astuces" in globals():
     hints.items = eval(astuces)
+    text+='<br><br>'
     text+=" {{ hints|component}} \n<br>"
     nb_hints = len(hints.items)
 else:
