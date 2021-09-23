02f5a34a1fac5848461ea1278698301ea7b83a88
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-23 18:35:26 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/function/crazy_house.pl

--- ComputerScience/C/exercices/L2_C17/function/crazy_house.pl
+++ ComputerScience/C/exercices/L2_C17/function/crazy_house.pl
@@ -19,7 +19,7 @@ extends=/ComputerScience/C/template/std_progC17_clang.pl
 author=Nicolas Borie
 
 title=La maison qui rend fou
-tag=function|variable|output|sortie|affichage|boucle|precision
+tag=fonction|variable|output|sortie|affichage|boucle|precision
 
 editor.height=350px
 
