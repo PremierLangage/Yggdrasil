6e5e22f9ab3bdfaa20bfd385dfe1ecce4b5cf311
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-24 17:29:37 +0200

    nborie1 modified Yggdrasil/ComputerScience/C_Initiation/tableaux_3/mirroir.pl

--- ComputerScience/C_Initiation/tableaux_3/mirroir.pl
+++ ComputerScience/C_Initiation/tableaux_3/mirroir.pl
@@ -1,20 +1,3 @@
-#*****************************************************************************
-#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
-#
-#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
-#  Creative Commons CC-by-SA 3.0
-#
-#    This code is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#
-#  The full text of the CC-By-SA 3.0 is available at:
-#
-#            https://creativecommons.org/licenses/by-sa/3.0/
-#            https://creativecommons.org/licenses/by-sa/3.0/fr/
-#*****************************************************************************
-
-extends=/ComputerScience/C/template/std_progC17_clang.pl
 
 title=Miroir d'un tableau
 
