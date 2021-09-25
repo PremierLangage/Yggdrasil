e5187e60873549e097edd6246ee93da79b20bac6
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-25 12:46:21 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/array/tab2d.pl

--- ComputerScience/C/exercices/L2_C17/array/tab2d.pl
+++ ComputerScience/C/exercices/L2_C17/array/tab2d.pl
@@ -1,42 +1,60 @@
-title=Tableau 2d de caractères
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
 
-tag=Tableau|Fonction|DeclarationFonction|TransmissionParametre|ParcoursTableau
+extends=/ComputerScience/C/template/std_progC17_clang.pl
 
-extends=/ComputerScience/C/template/stdsandboxC.pl
+title=Tableau de caractères à deux dimensions
 
-author= 
+tag=tableau|fonction|rectangle|matrice
 
-text==
+author=Prog C L2 Team
+
+text==#|markdown|
 Ecrire une fonction de protopype `void remplit(char tab[MAX][MAX],int li,int col)` 
 qui remplit la partie effective avec des lettres minuscules consécutives.<br>
 Après la lettre $%\tt{z}%$ on repart de la lettre $%\tt{a}%$ <br>
 Exemples (avec MAX 50)<br>
-remplit(tab,3,4) transforme les 3 premières lignes et 4 premières colonnes en<br>
-abcd<br>
-efgh<br>
-ijkl<br>
-remplit(tab,4,10) transforme les 4 premières lignes et 10 premières colonnes en<br>
-abcdefghij<br>
-klmnopqrst<br>
-uvwxyzabcd<br>
-efghijklmn<br>
+remplit(tab,3,4) transforme les 3 premières lignes et 4 premières colonnes en   
+
+    a b c d   
+    e f g h   
+    i j k l   
+
+remplit(tab,4,10) transforme les 4 premières lignes et 10 premières colonnes en   
+
+    a b c d e f g h i j   
+    k l m n o p q r s t   
+    u v w x y z a b c d   
+    e f g h i j k l m n   
 ==
 
-codebefore==
-
+code_before==#|c|
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX 50
-
 ==
 
-editor.code==
+editor.code==#|c|
 void remplit(char tab[][MAX],int li,int col){
-
+  // Votre code ici...
 }
 ==
-solution==
+
+solution==#|c|
 void remplit(char tab[MAX][MAX],int li,int col){
   char l='a';
   int d=0;
@@ -49,7 +67,8 @@ void remplit(char tab[MAX][MAX],int li,int col){
     }
 }
 ==
-codeafter==
+
+code_after==#|c|
 void affiche (char tab[][MAX],int li,int col){
   int i,j;
   for(i= 0;i<li;i++){
@@ -67,14 +86,11 @@ int main(void){
   affiche(tab,li,col);
   return 0;
 }
-
 ==
 
-
-tests==
+checks_args_stdin==#|python|
 [ ["simple éxécution", "","3 4"],
-["aléatoire ", "",str(random.randint(4,8))+" "+str(random.randint(9,15))],
-]
+  ["aléatoire ", "",str(random.randint(4,8))+" "+str(random.randint(9,15))] ]
 ==
 
 
