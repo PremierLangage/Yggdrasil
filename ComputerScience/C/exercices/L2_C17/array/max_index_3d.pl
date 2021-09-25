87fc2e4368198b73554953f72c3790b7cb808301
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-25 22:37:51 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/array/max_index_3d.pl

--- ComputerScience/C/exercices/L2_C17/array/max_index_3d.pl
+++ ComputerScience/C/exercices/L2_C17/array/max_index_3d.pl
@@ -83,7 +83,7 @@ int main(int argc, char* argv[]){
   for (m1=0 ; m1<s1 ; m1++)
     for (m2=0 ; m2<s2 ; m2++)
       for (m3=0 ; m3<s3 ; m3++)
-        t[m1][m2][m3] = rand() % (s1*s2*s3 / 5);
+        t[m1][m2][m3] = rand() % (s1*s2*s3 / 2);
 
   index_max_3d(t, s1, s2, s3, &m1, &m2, &m3);
 
