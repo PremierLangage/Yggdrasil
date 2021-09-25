8b75f3211f91b6d4d92a9323f0a615d04a3ecd43
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-25 13:12:03 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/array/tab2d.pl

--- ComputerScience/C/exercices/L2_C17/array/tab2d.pl
+++ ComputerScience/C/exercices/L2_C17/array/tab2d.pl
@@ -28,6 +28,7 @@ qui remplit la partie effective (de taille `li x col`) de la matrice `tab` avec 
 minuscules consécutives. La méthode de remplissage doit aller ligne par ligne de la gauche 
 vers la droite et aussi de haut en bas. Après la lettre `z` atteinte, on repart de la lettre `a`   
 
+<br>
 
 <u>Exemples :</u> (avec `#define MAX 50` )   
 `remplir(tab, 3, 4)` transforme les 3 premières lignes et 4 premières colonnes de `tab` en   
