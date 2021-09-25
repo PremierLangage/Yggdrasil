77a89d07590184b93a838aa4321e169f56381d05
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-25 11:49:43 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/array/inversions.pl

--- ComputerScience/C/exercices/L2_C17/array/inversions.pl
+++ ComputerScience/C/exercices/L2_C17/array/inversions.pl
@@ -80,13 +80,13 @@ int main(int argc, char* argv[]){
 checks_args_stdin==#|python|
 [ ["Simple éxécution", [], "1 2 3 4 5"],
   ["Deux négatifs", [], "-1 -2"],
-  ["Test aléatoire négatif", [], " ".join([str(random.randint(-30,-5)) for i in range(random.randint(5,10))])],
-  ["Test aléatoire positif", [], " ".join([str(random.randint(0,20)) for i in range(random.randint(5,10))])],
-  ["Test aléatoire 1", [], " ".join([str(random.randint(-20,20)) for i in range(random.randint(5,10))])],
-  ["Test aléatoire 2", [], " ".join([str(random.randint(-10,10)) for i in range(random.randint(5,10))])],
-  ["Test aléatoire 3", [], " ".join([str(random.randint(-100,100)) for i in range(random.randint(10,20))])],
-  ["Test aléatoire 4", [], " ".join([str(random.randint(-10,10)) for i in range(random.randint(5,20))])],
-  ["Test aléatoire 5", [], " ".join([str(random.randint(-20,20)) for i in range(random.randint(5,20))])], ]
+  ["Test aléatoire négatif", [], " ".join([str(randint(-30,-5)) for i in range(randint(5,10))])],
+  ["Test aléatoire positif", [], " ".join([str(randint(0,20)) for i in range(randint(5,10))])],
+  ["Test aléatoire 1", [], " ".join([str(randint(-20,20)) for i in range(randint(5,10))])],
+  ["Test aléatoire 2", [], " ".join([str(randint(-10,10)) for i in range(randint(5,10))])],
+  ["Test aléatoire 3", [], " ".join([str(randint(-100,100)) for i in range(randint(10,20))])],
+  ["Test aléatoire 4", [], " ".join([str(randint(-10,10)) for i in range(randint(5,20))])],
+  ["Test aléatoire 5", [], " ".join([str(randint(-20,20)) for i in range(randint(5,20))])], ]
 ==
 
 astuces==#|python|
