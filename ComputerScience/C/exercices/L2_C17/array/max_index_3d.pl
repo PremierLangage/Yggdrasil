876ad3d12a7e239369d6adbf3dc34eeb18efc6b7
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-25 22:27:22 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/array/max_index_3d.pl

--- ComputerScience/C/exercices/L2_C17/array/max_index_3d.pl
+++ ComputerScience/C/exercices/L2_C17/array/max_index_3d.pl
@@ -22,11 +22,18 @@ tag=tableau|fonction|parcours|complexe|dimension|pointeur
 
 author=Nicolas Borie
 
-text==#|mardown|
+text==#|markdown|
 Écrire une fonction   
 `int index_max_3d(int t[M][M][M], int s1, int s2, int s3, int* m1, int* m2, int* m3)`   
 qui prend en argument un tableau à trois dimension `t`, puis trois entiers pour délimiter une zone de travail,
-et enfin trois pointeurs vers un entiers.
+et enfin trois pointeurs vers un entier. Votre fonction devra rechercher les coordonnées du 
+maximum `(i, j, k)` où `i` est compris entre `0` et `s1-1` (tous deux inclus), 
+`j` est compris entre `0` et `s2-1` (tous deux inclus) et `k` est compris entre 
+`0` et `s3-1` (tous deux inclus). Dans le cas où l'élément maximal serait présent plusieurs
+fois, on recherchera l'occurence de coordonnées `(i, j, k)` minimales pour l'ordre lexicographique.
+Une fois l'occurence trouvée, mettez à jour les trois pointeurs `(m1, m2, m3)` pour qu'ils 
+pointent vers ces trois coordonnées. Finalement, retournez la valeur maximale du tableau de 
+la zone de recherche.
 ==
 
 code_before==#|c|
