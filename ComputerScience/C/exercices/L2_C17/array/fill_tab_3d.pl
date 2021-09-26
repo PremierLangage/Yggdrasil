2cbb37e248e264f86137041dd7a3ac12c7ba5299
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-26 07:34:42 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/array/fill_tab_3d.pl

--- ComputerScience/C/exercices/L2_C17/array/fill_tab_3d.pl
+++ ComputerScience/C/exercices/L2_C17/array/fill_tab_3d.pl
@@ -130,7 +130,8 @@ int main(int argc, char* argv[]){
 checks_args_stdin==#|python|
 [ ["Simple éxécution", ["1", "2", "3"],""],
   ["Test aléatoire 1", [str(randint(9,15)), str(randint(2,4)), str(randint(2,5))], ""],
-  ["Test aléatoire 2", [str(randint(9,15)), str(randint(2,4)), str(randint(2,5))], ""] ]
+  ["Test aléatoire 2", [str(randint(9,15)), str(randint(2,4)), str(randint(2,5))], ""],
+  ["Test aléatoire 3", [str(randint(9,15)), str(randint(2,4)), str(randint(2,5))], ""] ]
 ==
 
 astuces==#|python|
