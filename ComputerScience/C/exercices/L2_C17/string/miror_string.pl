4c6f78936ad275fcecc3d2cf128579008863f5f0
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-26 21:01:33 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/string/miror_string.pl

--- ComputerScience/C/exercices/L2_C17/string/miror_string.pl
+++ ComputerScience/C/exercices/L2_C17/string/miror_string.pl
@@ -67,6 +67,7 @@ int main(int argc, char* argv[]){
 
 checks_args_stdin==#|python|
 [ ["Simple éxécution", ["bonjour toto"], ""],
+  ["Palindrome", [choice(["radar", "elle", "laval", "coloc", "kayak", "rotor"])], ""],
   ["Test aléatoire 1",["".join([chr(randint(97,122)) for i in range(randint(1,15))])], ""] ]
 ==
 
