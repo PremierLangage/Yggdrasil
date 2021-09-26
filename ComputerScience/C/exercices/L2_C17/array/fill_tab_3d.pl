4f65a5679717360dd3b591ca9e4c9463eb87c01b
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-26 07:44:01 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/array/fill_tab_3d.pl

--- ComputerScience/C/exercices/L2_C17/array/fill_tab_3d.pl
+++ ComputerScience/C/exercices/L2_C17/array/fill_tab_3d.pl
@@ -39,6 +39,8 @@ Exemple pour un tableau <strong>tab[2][3][5]</strong>, on aura, après avoir app
 <br>
 
 
+    [ 1, 2, 3, 4, 5] [ 6, 7, 8, 9,10] [11,12,13,14,15]
+    [16,17,18,19,20] [21,22,23,24,25] [26,27,28,29,30]
 
 ==
 
