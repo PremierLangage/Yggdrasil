31cd47bb81bf628b12966318afd9953912c608e4
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-09-26 21:13:13 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/string/strlenght.pl

--- ComputerScience/C/exercices/L2_C17/string/strlenght.pl
+++ ComputerScience/C/exercices/L2_C17/string/strlenght.pl
@@ -95,7 +95,7 @@ astuces==#|python|
 [
   { "content": """Une simple boucle `for` suffit pour écrire une telle fonction."""},
   { "content": """Le critère d'arrêt porte sur la détection du caractère de fin de chaîne : `\0`."""},
-  { "content": """une boucle comme la suivante est raisonnable : `for(i=0 ; s[i] != '\0'; i++)`."""}
+  { "content": """une boucle comme la suivante est raisonnable : `for(i=0 ; s[i] != '\\0'; i++)`."""}
 ]
 ==
 
