80379b960551a01bd31c4cd4d099beada8973dfa
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-09-28 14:50:49 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/chapitre_2_conditionnelles/dynafor2-2021.pl

---
+++ ComputerScience/python/AP1-2122/chapitre_2_conditionnelles/dynafor2-2021.pl
@@ -0,0 +1,25 @@
+
+# DR j'ai fixé la seed dans cet exercice  
+# il est donc statique
+
+extends=/ComputerScience/python/AP1-1920/templates/looptemplate.pl
+tag=InstructionsRepetitives|ForInRange
+
+exoseed % 7
+difficultymax=2
+taboo=while
+needed=for
+
+title=Boucle for 3/3
+
+text= Pour ne pas avoir un warning stupid
+
+plsoluce==
+Vérification du comportement fixé |
+==
+
+
+
+
+
+
