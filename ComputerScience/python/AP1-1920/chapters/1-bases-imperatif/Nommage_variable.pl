65517cd54327f56d69fa1ca7416a35fb150222af
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-09-28 14:10:50 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/Nommage_variable.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/Nommage_variable.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/Nommage_variable.pl
@@ -7,6 +7,33 @@ title=Nommage de variables
 text=Parmi les chaines suivantes, sélectionnez celles qui sont des noms de variables valides.
 
 mybuild==
+
+def mot_aleatoire(longueur):
+    """Renvoie un mot aléatoire (lettres minuscules et majuscules) de la 
+    longueur demandée."""
+    return "".join(choice(ascii_letters) for _ in range(longueur))
+
+
+# initialisation des "bases de données" de noms corrects et incorrects
+corrects = {  # mots aléatoires
+    mot_aleatoire(randint(4, 10)) for _ in range(15)
+} | {  # mots aléatoires avec des chiffres à la fin
+    mot_aleatoire(randint(4, 10)) + chaine_nombres_aleatoire(randint(1, 5))
+    for _ in range(15)
+} | {  # mots aléatoires séparés par des "_"
+    mot_aleatoire(randint(4, 10)) +  "_" + mot_aleatoire(randint(4, 10)) 
+    for _ in range(15)
+}
+incorrects = set(
+    kwlist  # les mots-clés de Python
+) | {  # mots aléatoires avec des chiffres au début
+    chaine_nombres_aleatoire(randint(1, 5)) + mot_aleatoire(randint(4, 10))
+    for _ in range(15)
+}
+good = list(corrects)
+bad= list(incorrects)
+
+
 nbtrues = random.randint(2,3)
 ==
 
