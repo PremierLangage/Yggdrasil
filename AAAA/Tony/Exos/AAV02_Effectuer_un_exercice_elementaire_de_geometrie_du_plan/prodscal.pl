aea03d2df2f70c0dc8fe33cf79f0c4a147e0f2bd
Tony Février <tony.fevrier@universite-paris-saclay.fr>
2021-09-30 10:52:15 +0200

    tfévrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal.pl

--- AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal.pl
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal.pl
@@ -9,8 +9,8 @@ Calculer le produit scalaire des deux vecteurs $!{{p}}!$ par $!{{q}}!$
   
 before ==
 n = choice([2,3])
-p =  [randint(-5,5) for i in range(n)]
-q = [randint(-5,5) for i in range(n)] 
+p =  randint(-5,5) for i in range(n)
+q = randint(-5,5) for i in range(n) 
 
 if n == 2:
     sol = p[0]*q[0] + p[1]*q[1] 
