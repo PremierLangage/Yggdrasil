41ff2fbdc4d48289fbdc8b1134e1a62664955ff6
Tony Février <tony.fevrier@universite-paris-saclay.fr>
2021-09-30 22:42:27 +0200

    tfévrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl

--- AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl
@@ -3,14 +3,14 @@ title = "Combinaison linéaire"
 extends = /model/math/expr.pl
 
 question ==  
-Calculer la combinaison linéaire $!{{a}}u+{{b}}v!$ où $%u={{u}}%$ et $!v = {{v}}!$  
+Calculer la combinaison linéaire $!{{a}}u+{{b}}v!$ où $!u={{u}}!$ et $!v = {{v}}!$  
 ==
  
   
 before == 
-from sympy import Matrix 
-u = Matrix([[randint(-2,2),randint(-2,2)]])
-v = Matrix([[randint(-2,2),randint(-2,2)]]) 
+from numpy import array 
+u = array([[randint(-2,2),randint(-2,2)]])
+v = array([[randint(-2,2),randint(-2,2)]]) 
 a,b = randint(-5,5),randint(-5,5) 
 
  
