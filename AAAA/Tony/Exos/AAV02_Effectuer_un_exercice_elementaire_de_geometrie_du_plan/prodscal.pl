ee85e8ec6acb3ab2899abda6ef70f9a26317f2c9
Tony Février <tony.fevrier@universite-paris-saclay.fr>
2021-09-30 10:55:39 +0200

    tfévrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal.pl

--- AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal.pl
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/prodscal.pl
@@ -3,18 +3,16 @@ title = "Produit scalaire"
 extends = /model/math/expr.pl
 
 question ==  
-Calculer le produit scalaire des deux vecteurs $!{{p}}!$ par $!{{q}}!$ 
+Calculer le produit scalaire des deux vecteurs $!({{p}})!$ par $!({{q}})!$ 
 ==
  
   
-before ==
-n = choice([2,3])
-p =  randint(-5,5) for i in range(n)
-q = randint(-5,5) for i in range(n) 
+before == 
 
-if n == 2:
-    sol = p[0]*q[0] + p[1]*q[1] 
-else: 
-    sol = p[0]*q[0] + p[1]*q[1] + p[2]*q[2]
+for in 
+p1,p2 = randint(-5,5),randint(-5,5) 
+q1,q2 = randint(-5,5),randint(-5,5)
+ 
+sol = p1*q1+p2*q2  
 ==
  
