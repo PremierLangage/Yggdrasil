9ac365cc013abd5d45502441db6750c9581b01d8
Tony Février <tony.fevrier@universite-paris-saclay.fr>
2021-09-30 11:00:20 +0200

    tfévrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl

---
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl
@@ -0,0 +1,21 @@
+title = "Combinaison linéaire"
+
+extends = /model/math/expr.pl
+
+question ==  
+Calculer la combinaison linéaire des deux vecteurs $!{{a(p1,p2)}}+b!$ par $!{{q1,q2}}!$ 
+==
+ 
+  
+before == 
+ 
+
+p1,p2 = randint(-2,2),randint(-2,2) 
+q1,q2 = randint(-2,2),randint(-2,2) 
+a,b = randint(-5,5),randint(-5,5) 
+
+ 
+sol = 
+==
+ 
+
