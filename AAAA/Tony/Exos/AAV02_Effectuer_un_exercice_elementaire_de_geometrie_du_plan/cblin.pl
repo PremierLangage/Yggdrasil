0b0ced55cd33bd2b358f2ceaab147c0ce32f24c4
Tony Février <tony.fevrier@universite-paris-saclay.fr>
2021-09-30 22:56:47 +0200

    tfévrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl

--- AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl
@@ -11,7 +11,7 @@ before ==
 from numpy import array 
 p1,p2 = randint(-2,2),randint(-2,2)
 q1,q2 = randint(-2,2),randint(-2,2)
-a,b = randint(-5,5),randint(-5,5) 
+a,b = randint(1,5),randint(1,5) 
 
 sol = [a*p1 + b*p2 , a*q1 + b*q2]
 ==
