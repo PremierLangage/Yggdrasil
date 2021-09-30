8cb989402be2c1cd638375840cf7015f5dc38a26
Tony Février <tony.fevrier@universite-paris-saclay.fr>
2021-09-30 22:19:56 +0200

    tfévrier modified Yggdrasil/AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl

--- AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl
+++ AAAA/Tony/Exos/AAV02_Effectuer_un_exercice_elementaire_de_geometrie_du_plan/cblin.pl
@@ -10,8 +10,8 @@ Calculer la combinaison linéaire $!{{a}}u+{{b}}v!$ où $!u={{u}}!$ et $!v = {{v
 before == 
 from sympy import Matrix 
 x, x1, x2, x3 = symbols('x x1 x2 x3')
-u = Matrix([[randint(-2,2),randint(-2,2)]])
-v = Matrix([[randint(-2,2),randint(-2,2)]]) 
+u = Matrix([randint(-2,2),randint(-2,2)])
+v = Matrix([randint(-2,2),randint(-2,2)]) 
 a,b = randint(-5,5),randint(-5,5) 
 
  
