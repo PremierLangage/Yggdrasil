f658edfa58c6d7670fea1e8d3d12a8b71c61dd68
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-10-02 18:03:25 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/string/is_palindromic.pl

--- ComputerScience/C/exercices/L2_C17/string/is_palindromic.pl
+++ ComputerScience/C/exercices/L2_C17/string/is_palindromic.pl
@@ -71,8 +71,8 @@ int main(void){
 
 checks_args_stdin==#|python|
 [ 
-  ["Un petit palindrome", [], choice(["elle", "kayak", "radar", "rotor", "serres", "solos", "laval"])],
-  ["Pas un palindrome", [], choice(["elle est belle", "un kayak", "velo", "deux radar", "avion", "le rotor", "michel serres", "Bruno solos", "suffit"])],
+  ["Un petit palindrome", [], choice(["elle", "kayak", "radar", "rotor", "serres", "solos", "laval"]) ],
+  ["Pas un palindrome", [], choice(["elle est belle", "un kayak", "velo", "deux radar", "avion", "le rotor", "michel serres", "Bruno solos", "suffit"]) ],
 ]
 ==
 
