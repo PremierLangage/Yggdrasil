0e4543c5e5d25e009015e3706c5de5ee12af5b54
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-10-02 22:29:09 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/string/is_a_number.pl

---
+++ ComputerScience/C/exercices/L2_C17/string/is_a_number.pl
@@ -0,0 +1,101 @@
+#*****************************************************************************
+#  Copyright (C) 2021 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
+
+author=Nicolas Borie
+
+title=Reconnaître un nombre entier
+tag=string|chaine|conversion|verification
+
+text==
+Écrire une fonction **is_a_number** qui prend en argument une chaîne 
+de caractères C (en ascii) et retourne 1 si la chaine forme un nombre 
+valide en base 10. La fonction retournera 0 sinon.
+
+On considéra comme valide tout entier positif ou negatif. Voici des
+entrées valides :
+    
+    0, 1, 324, -1, -0, -5243134, ...
+
+Voici des entrées non valides :
+
+    a12, 23d, -e1, 023, +23, 12a0, -0123, ...
+
+<br />
+
+
+==
+
+editor.code==#|c|
+int is_a_number(char* s){
+  /* votre code ici... */
+}
+==
+
+solution==#|c|
+
+int is_a_number(char* s){
+  int i=0;
+
+  if (s[i] == '-')
+    i++;
+  if (s[i] == '0')
+    return s[i+1] == '\0';
+  while (s[i] != '\0'){
+    if (s[i] < '0' || s[i] > '9')
+      return 0;
+    i++;
+  }
+  return 1;
+}
+==
+
+code_before==#|c|
+
+==
+
+code_after==#|c|
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+  if (is_a_number(argv[1])){
+    printf("%s est un nombre valide.\n", argv[1]);
+  }
+  else{
+    printf("%s n'est pas un nombre valide.\n", argv[1]);
+  }
+  return 0;
+}
+==
+
+checks_args_stdin==#|python|
+[ ["Test basique", ["0"], ""],
+  ["Plus d'une centaine", ["123"], ""],
+  ["Avec des lettres", ["12a0"], ""],
+  ["Petit négatif", ["-47"], ""],
+  ["Très grand nombre", ["12172364512235126361273223736"], ""],
+  ["Fort négatif", ["-999999999999999"], ""],
+  ["Test aléatoire 1", [choice(["781","-1274","997421","-75.43","3.1415","12521z","-124b","82c12","-124b","-142c1","-12.21"])], ""],
+  ["Test aléatoire 2", [choice(["781","-1274","997421","-75.43","3.1415","12521z","-124b","82c12","-124b","-142c1","-12.21"])], ""],
+  ["Test aléatoire 3", [choice(["781","-1274","997421","-75.43","3.1415","12521z","-124b","82c12","-124b","-142c1","-12.21"])], ""],
+  ["Test aléatoire 4", [choice(["781","-1274","997421","-75.43","3.1415","12521z","-124b","82c12","-124b","-142c1","-12.21"])], ""],
+  ["Test aléatoire 5", [choice(["781","-1274","997421","-75.43","3.1415","12521z","-124b","82c12","-124b","-142c1","-12.21"])], ""] ]
+==
+
+
+
