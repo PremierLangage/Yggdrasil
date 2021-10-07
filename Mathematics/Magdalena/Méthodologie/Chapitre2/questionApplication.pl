40458bc13dbf60ff9faaaff513587fd8cc520013
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-10-07 12:05:21 +0200

    drevuz2 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/questionApplication.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/questionApplication.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/questionApplication.pl
@@ -8,6 +8,10 @@ questions==
 - $% f(x) %$ est un élément de $%E %$
 - $% f(A) %$ est un élément de $%F %$
 + $% f(A) %$ est une partie de $%F %$
+
+
+** Soit $%E, F %$ deux ensembles non vide disjoints et doit $%f: E \to F %$ une application $%x\in E %$, $% y\in F %$, $% A\subset E %$ et $% B \subset F%$ 
+
 - $% f(A) %$ s'appelle l'image de $% A %$ par $%f %$
 + $% f(A) %$ s'appelle l'image directe de $% A %$ par $%f%$
 - $% f^{\\{-1\\}}(y) %$ a du sens, c'est l'antécédent de $% y%$ par $% f %$
