83e1a3e341ec409042c1e49ba4ffa18e5172c3c5
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-08 08:30:10 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Divers/qcm1.pl

---
+++ Mathematics/Magdalena/Divers/qcm1.pl
@@ -0,0 +1,16 @@
+
+extends = /model/basic/checkbox_rw.pl
+
+question ==
+Parmi ces villes, lesquelles ne sont pas des capitales ?
+==
+
+right ==
+Barcelone
+Milan
+==
+
+wrong ==
+Paris
+Berlin
+==
