10ea5061943361206ebf203b8b5c43413a414052
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-09 09:32:50 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Question-Ecriture.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Question-Ecriture.pl
@@ -0,0 +1,38 @@
+questions==
+
+** Cochez les bonnes réponses. Ecrire formellement $%n%$ est un entier pair
++  $% n\in \mathbb{N} %$  $%\exists k \in \mathbb{N} \quad n =2k%$
+- $% n\in \mathbb{N} %$  $%\forall k \in \mathbb{N} \quad n =2k%$
++  $% n\in \mathbb{N} %$  $%\exists l \in \mathbb{N} \quad n =2l%$
+- $%n=2k%$
+
+
+** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels pairs
+
+- $% \\{n \in \mathbb{N} \;  | \;  \forall k \in \mathbb{N} \quad  n=2k \\}%$ 
++ $% \\{n \in \mathbb{N} \;  | \;  \exists k \in \mathbb{N} \quad  n=2k \\}%$ 
+- $% \\{ \forall n \in \mathbb{N} \;  | \;  \exists k \in \mathbb{N} \quad  n=2k \\}%$
+- $% \\{n \in \mathbb{N} \;  | \;  \exists k \in \mathbb{N} \quad  2k \\}%$ 
++ $% \\{2k \; |\; k \in \mathbb{N} \\}%$ 
++ $%  \\{2n \; |\; n \in \mathbb{N} \\}%$ 
+- $% \\{n, k \in \mathbb{N} \;  | \;  n=2k \\}%$ 
+
+
+
+** Cochez les bonnes réponses. 
+Soit $%E, F %$ deux ensembles non vide disjoints et soit $%f: E \to F %$ une application $%x\in E %$, $% y\in F %$, $% A\subset E %$ et $% B \subset F%$ 
+
+- $% f(A) =B%$
+- $% f^{\\{-1\\}}(B)=A %$
++ $% f^{\\{-1\\}}(B) %$ a du sens, c'est l'image réciproque de $%B%$
+- $% f^{\\{-1\\}}(A)%$ a du sens
+- $% f^{\\{-1\\}}(B)\subset F %$ 
++ $% f^{\\{-1\\}}(B) \subset E %$ 
+
+
+** 
+
+ 
+
+==
+
