8ee363cca6e3701c2d92d404a2e66b503281a5ed
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-09 10:08:35 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Pair_Impair.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Pair_Impair.pl
@@ -0,0 +1,35 @@
+extends = /model/AMC_TXT.pl
+title = Ecriture Pairs et Impairs 
+questions==
+
+
+
+questions==
+
+** Cochez les bonnes réponses. Ecrire formellement $%n%$ est un entier pair
++  $% n\in \mathbb{N} \quad \exists k \in \mathbb{N} \quad n =2k%$
+- $% n\in \mathbb{N} \quad \forall k \in \mathbb{N} \quad n =2k%$
++  $% n\in \mathbb{N} \quad \exists l \in \mathbb{N} \quad n =2l%$
+- $%n=2k%$
+
+
+** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels pairs
+
+- $% \\{n \in \mathbb{N} \quad  | \\;  \forall k \in \mathbb{N} \quad  n=2k \\}%$  
+- $% \\{ \forall n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k \\}%$
+- $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  2k \\}%$ 
++ $% \\{2k \\; |\\; k \in \mathbb{N} \\}%$ 
++ $%  \\{2n \\; |\\; n \in \mathbb{N} \\}%$ 
+- $% \\{n, k \in \mathbb{N} \\;  | \\;  n=2k \\}%$ 
+
+** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels impairs
+
+- $% \\{n \in \mathbb{N} \quad  | \\;  \forall k \in \mathbb{N} \quad  n=2k+1 \\}%$ 
++ $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k+1 \\}%$ 
+- $% \\{ \exists n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k+1 \\}%$
+- $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  2k+1 \\}%$  
++ $%  \\{2n+1 \\; |\\; n \in \mathbb{N} \\}%$ 
+- $% \\{n, k \in \mathbb{N} \\;  | \\;  n=2k+1 \\}%$ 
+
+==
+
