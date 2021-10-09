209cc518734b179c45c50454ddb2b5bdcf335f5f
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-09 09:57:22 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Question-Ecriture.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Question-Ecriture.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Question-Ecriture.pl
@@ -23,6 +23,11 @@ questions==
 + $%  \\{2n \\; |\\; n \in \mathbb{N} \\}%$ 
 - $% \\{n, k \in \mathbb{N} \\;  | \\;  n=2k \\}%$ 
 
+** Cochez les bonnes réponses. Ecrire en revenant aux éléments premiers du langage que l'entier $%a%$ divise l'entier $%b%$ .
+- $% a, b  \in \mathbb{N}  \quad a= k\times b %$
+-  $% a, b  \in \mathbb{N}  \quad \exists k \in \mathbb{N} \quad  a= k\times b %$
+-  $% a, b  \in \mathbb{N}  \quad \forall k \in \mathbb{N} \quad  b= k\times a %$
++  $% a, b  \in \mathbb{N}  \quad \exists k \in \mathbb{N} \quad  b= k\times a %$
 
 
 ** Cochez les bonnes réponses. Ecrire formellement que l'entier $%p%$ est premier en utilisant le symbole divise $% | %$ .
@@ -31,14 +36,10 @@ questions==
 - $% \exists d \in \mathbb{N}^* \quad d | p \quad (d=1 \\; \mbox{ ou } \\;  d=n) %$
 - $% \\{ d \in \mathbb{N} \\; | \\; d | p \\} = \\{1, p\\} %$
 + $% \\{ d \in \mathbb{N} \\; | \\; d | p \\} = \\{1, p\\} \quad  %$ et  $% \quad (p \ge 2) %$
-+ $%p \ge 2 %$ et $% (\forall d \in \mathbb{N} \quad \mbox{ si } d | p \mbox{ alors } \\; d=1  \\; \mbox{ ou } \\; d=p)%$.
++ $%p \ge 2 %$ et $% (\forall d \in \mathbb{N} \\; \mbox{ si } d | p, \\;  \mbox{ alors } \\; d=1  \\; \mbox{ ou } \\; d=p)%$.
  
 
 
-** Cochez les bonnes réponses. Ecrire en revenant aux éléments premiers du langage que l'entier $%a%$ divise l'entier $%b%$ .
-- $% a, b  \in \mathbb{N}  \quad a= k\times b %$
--  $% a, b  \in \mathbb{N}  \quad \exists k \in \mathbb{N} \quad  a= k\times b %$
--  $% a, b  \in \mathbb{N}  \quad \forall k \in \mathbb{N} \quad  b= k\times a %$
--  $% a, b  \in \mathbb{N}  \quad \exists k \in \mathbb{N} \quad  b= k\times a %$
+
 ==
 
