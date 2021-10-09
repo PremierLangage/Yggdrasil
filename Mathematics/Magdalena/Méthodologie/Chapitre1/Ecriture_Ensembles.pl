9dffa0f49458755926fbc7da8201ae66e3c53208
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-09 10:25:09 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Ensembles.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Ensembles.pl
@@ -0,0 +1,36 @@
+
+extends = /model/AMC_TXT.pl
+title = Ecriture d'ensembles
+questions==
+
+
+
+questions==
+
+** Cochez les bonnes réponses. Ecrire formellement l'ensemble des nombres qui s'écrivent comme la somme de deux carrés d'entiers.
++  $% \\{n^2+p^2 \\; |\\; n, p \in \mathbb{N} \\}%$
++  $% \\{ n \in \mathbb{N} \\; |\\; \exists  k, p \in \mathbb{N} \\; n=  k^2+p^2\\}%$
+-  $% \\{ n \in \mathbb{N} \\; |\\; k^2+p^2 \\; |\\; k, p \in \mathbb{N} \\}%$
+-  $% \\{ \forall n \in \mathbb{N} \\; |\\; n= k^2+p^2 \\; |\\; k, p \in \mathbb{N} \\}%$
+
+
+** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels pairs
+
+- $% \\{n \in \mathbb{N} \quad  | \\;  \forall k \in \mathbb{N} \quad  n=2k \\}%$  
+- $% \\{ n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k \\}%$
+- $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  2k \\}%$ 
++ $% \\{2k \\; |\\; k \in \mathbb{N} \\}%$ 
++ $%  \\{2n \\; |\\; n \in \mathbb{N} \\}%$ 
+- $% \\{ \forall n, k \in \mathbb{N} \\;  | \\;  n=2k \\}%$ 
+
+** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels impairs
+
+- $% \\{n \in \mathbb{N} \quad  | \\;  \forall k \in \mathbb{N} \quad  n=2k+1 \\}%$ 
++ $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k+1 \\}%$ 
+- $% \\{ \exists n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k+1 \\}%$
+- $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  2k+1 \\}%$  
++ $%  \\{2n+1 \\; |\\; n \in \mathbb{N} \\}%$ 
+- $% \\{n, k \in \mathbb{N} \\;  | \\;  n=2k+1 \\}%$ 
+
+==
+
