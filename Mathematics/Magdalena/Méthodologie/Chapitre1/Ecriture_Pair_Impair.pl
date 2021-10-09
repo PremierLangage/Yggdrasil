82a7fe659c5b56b0932eff384c2d5c9b42e817f7
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-09 10:10:25 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Pair_Impair.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Pair_Impair.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Pair_Impair.pl
@@ -16,11 +16,11 @@ questions==
 ** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels pairs
 
 - $% \\{n \in \mathbb{N} \quad  | \\;  \forall k \in \mathbb{N} \quad  n=2k \\}%$  
-- $% \\{ \forall n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k \\}%$
+- $% \\{ n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k \\}%$
 - $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  2k \\}%$ 
 + $% \\{2k \\; |\\; k \in \mathbb{N} \\}%$ 
 + $%  \\{2n \\; |\\; n \in \mathbb{N} \\}%$ 
-- $% \\{n, k \in \mathbb{N} \\;  | \\;  n=2k \\}%$ 
+- $% \\{ \forall n, k \in \mathbb{N} \\;  | \\;  n=2k \\}%$ 
 
 ** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels impairs
 
