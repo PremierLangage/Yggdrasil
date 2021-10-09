305783bdda842f653b014cde713aaad9fc178ebb
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-09 12:45:01 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_choix.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_choix.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_choix.pl
@@ -1,17 +1,11 @@
 extends = /model/basic/filltext.pl
 
 question ==
-Compléter le texte suivante avec les bonnes étiquettes.
+Compléter l'écriture de l'ensemble à l'aide des étiquettes.
 ==
 
 filledtext ==
-L’ensemble des gènes caractéristiques de l’espèce à laquelle appartient un organisme, 
-constitue son {génome}. 
-Chez les individus d’une même espèce, un gène peut cependant exister sous différentes formes 
-présentant de légères modifications de séquence : les allèles. 
-L’ensemble des allèles d’un individu définit son {génotype}. 
-Lorsqu’ils s’expriment, lors de la synthèse des protéines, les gènes participent 
-à la construction de l’individu et à la mise en place de son {phénotype}. 
+L'ensemble des nombres pairs s'écrit {$%\\{%$} {$% n \in \mathbb{n} %$}
 ==
 
 labels ==
