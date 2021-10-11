29f038099cd4740b0f81b2f881c342e1e891f56c
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 09:07:44 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
@@ -0,0 +1,15 @@
+extends = /model/basic/seltext.pl
+
+question ==
+Sélectionner dans ce texte les mots ou groupes de mots qui désignent Van Gogh.
+==
+
+seltext == 
+En 1888, {Vincent Van Gogh} peint [«Nuit Etoilée»]. 
+Il est difficile de représenter [la nuit] [en couleurs].
+{L'artiste} installait [son chevalet] lorsque [la nuit] tombait.
+Mais comment peindre sans lampadaire ? 
+{Le peintre} a eu pour cela [une idée lumineuse]. 
+On dit qu'{il} a planté [des bougies] sur [le rebord] de [son chapeau]. 
+{L'homme} a ainsi pu peindre grâce [aux minuscules flammes].
+==
