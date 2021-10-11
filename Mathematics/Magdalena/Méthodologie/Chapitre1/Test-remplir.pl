33908e23bca355a320e3c574ff9ea697091c48c8
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 10:38:18 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Test-remplir.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Test-remplir.pl
@@ -0,0 +1,19 @@
+extends = /model/basic/filltext.pl
+
+title = Génétique
+
+text ==
+Compléter le texte suivante avec les bonnes étiquettes.
+==
+
+filledtext ==
+Soit $!f:X\to Y!$ une application. 
+$!X!$ est {l'ensemble de départ} de l'application et $!Y!$ est l'ensemble d'arrivé.
+
+==
+
+labval ==
+caryotype
+ADN
+ARN
+==
