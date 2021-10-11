05b7a8227e11f3f1f739b6e57535066d45b8c60f
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 10:41:39 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Test-remplir.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Test-remplir.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Test-remplir.pl
@@ -9,7 +9,7 @@ Compléter le texte suivante avec les bonnes étiquettes.
 filledtext ==
 Soit $!f:X\to Y!$ une application. 
 $!X!$ est {l'ensemble de départ} de l'application et $!Y!$ est {l'ensemble d'arrivé}. 
-Soit $!a\in X!$. Posons $!b=f(a)!$. Alors $!b!$ est {l'image de $!a!$ par $!f!$} et 
+Soit $!a\in X!$. Posons $!b=f(a)!$. Alors $!b!$ est {l'image de} $!a!$ par $!f!$ et 
 
 ==
 
