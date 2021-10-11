334fb00ed6e9c9681b737d53d35ffbbbdaae8017
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 09:41:23 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
@@ -5,8 +5,6 @@ Sélectionner dans ce texte la première occurence d'une variable à l'intérieu
 ==
 
 seltext == 
-$!n \in \{{ 2k+1 \\; |\\;  k \in \mathbb{N} \}}!$
-Soit #{$!n!$}# un entier naturel impair. 
-Alors , il existe donc un entier #{$!k!$}# tel que 
+$!n \in \\{ 2k+1 \\; |\\;  k \in \mathbb{N} \}}!$
 
 ==
