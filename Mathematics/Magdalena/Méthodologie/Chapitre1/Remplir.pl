510063b2bcabc38ed9593376beee6ec2bed5965e
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 09:13:18 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
@@ -5,12 +5,5 @@ Sélectionner dans ce texte la première occurence d'une variable.
 ==
 
 seltext == 
-Soit {$!n!$} un entier naturel impair. Alors 
-En 1888, {Vincent Van Gogh} peint [«Nuit Etoilée»]. 
-Il est difficile de représenter [la nuit] [en couleurs].
-{L'artiste} installait [son chevalet] lorsque [la nuit] tombait.
-Mais comment peindre sans lampadaire ? 
-{Le peintre} a eu pour cela [une idée lumineuse]. 
-On dit qu'{il} a planté [des bougies] sur [le rebord] de [son chapeau]. 
-{L'homme} a ainsi pu peindre grâce [aux minuscules flammes].
+Soit {$!n!$} un entier naturel impair. Alors $!n!\subset \\{!$ {$!n!$} $!| \exists !$ {$!k!$} $! \in \mathbb{N} \quad n=2k\\}!$.
 ==
