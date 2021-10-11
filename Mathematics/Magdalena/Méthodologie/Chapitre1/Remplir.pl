14d29693c06f715b14df77aa476cca621b017741
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 09:47:38 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
@@ -6,7 +6,7 @@ Sélectionner dans ce texte la première occurence d'une variable à l'intérieu
 
 seltext ==
 Soit #{$!n!$}# un entier naturel impair. 
-Alors , il existe donc un entier #{$!k!$}# tel que 
+Alors, il existe un entier #{$!k!$}# tel que $%n=2k+1%$. 
 
 $!n \in \\{ 2k+1 \\; |\\;  k \in \mathbb{N} \}}!$
 
