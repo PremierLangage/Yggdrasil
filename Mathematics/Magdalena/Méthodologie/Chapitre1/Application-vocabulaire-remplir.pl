68785b5f4d85009e58ecb65f4a08f206726c0233
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 14:48:43 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Application-vocabulaire-remplir.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Application-vocabulaire-remplir.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Application-vocabulaire-remplir.pl
@@ -10,7 +10,7 @@ filledtext ==
 Soit $!f:X\to Y!$ une application. 
 $!X!$ est {l'ensemble de départ} de l'application et $!Y!$ est {l'ensemble d'arrivé}. 
 Soit $!a\in X!$. Posons $!b=f(a)!$. 
-Alors {$!b!$} est {l'image de} $!a!$ par $!f!$ et $!a!$ est {un antécédent} de $!b!$ par $!f!$.
+Alors {\leftbrace a \rightbrace} est {l'image de} $!a!$ par $!f!$ et $!a!$ est {un antécédent} de $!b!$ par $!f!$.
 
 ==
 
