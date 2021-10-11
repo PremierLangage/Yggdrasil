90d837f6da9f578c3f898247c3ec065e411a307b
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 09:15:14 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
@@ -5,5 +5,5 @@ Sélectionner dans ce texte la première occurence d'une variable à l'intérieu
 ==
 
 seltext == 
-Soit {$!n!$} un entier naturel impair. Alors $!n!\subset \\{!$ {$!n!$} $!| \exists !$ {$!k!$} $! \in \mathbb{N} \quad n=2k\\}!$.
+Soit {$!n!$} un entier naturel impair. Alors $!n \subset \\{!$ {$!n!$} $!| \exists !$ {$!k!$} $! \in \mathbb{N} \quad n=2k\\}!$.
 ==
