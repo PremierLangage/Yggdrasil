6f1f83ad30659679cd6a16894f0ca515ace77885
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-11 17:23:28 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Application_vocabulaire_remplir.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Application_vocabulaire_remplir.pl
@@ -0,0 +1,19 @@
+extends = /model/basic/filltext.pl
+
+title = Application Vocabulaire Etiquettes
+
+text ==
+Compléter le texte suivante avec les bonnes étiquettes.
+==
+
+filledtext ==
+Soit $!f:X\to Y!$ une application. 
+$!X!$ est {l'ensemble de départ} de l'application et $!Y!$ est {l'ensemble d'arrivé}. 
+Soit $!a\in X!$. Posons $!b=f(a)!$. 
+Alors {$!b!$} est {l'image de} $!a!$ par $!f!$ et $!a!$ est {un antécédent} de $!b!$ par $!f!$.
+
+==
+
+labval ==
+ARB
+==
