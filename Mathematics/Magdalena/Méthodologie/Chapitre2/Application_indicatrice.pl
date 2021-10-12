8d69f15467cc963ab84331e6ff122789ee5550ce
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-12 08:05:08 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Application_indicatrice.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre2/Application_indicatrice.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Application_indicatrice.pl
@@ -19,13 +19,12 @@ questions==
 - $% {\bf 1}_[0,1](\frac{\pi}{2})=1 %$
 + $% {\bf 1}_[0,1](\frac{\pi}{2})=0 %$
 
-** Cocher les bonnes réponses
+** Soit $%E%$ un ensemble non vide et $%A, B%$ des parties de $%E%$. Cocher les bonnes réponses~: 
 
-+ $% {\bf 1}_[0,1](\sqrt{2})=0 %$
-- $% {\bf 1}_[0,2](\sqrt{2})= \sqrt{2} %$
-+ $% {\bf 1}_[0,2](\sqrt{2})= 1%$
-- $% {\bf 1}_[0,1](\frac{\pi}{2})=1 %$
-+ $% {\bf 1}_[0,1](\frac{\pi}{2})=0 %$
++ $% {\bf 1}_A= 1 - {\bf 1}_{A^c} %$
+- $% {\bf 1}_{A \cup B}= {\bf 1}_{A} + {\bf 1}_{B} %$
++ $% {\bf 1}_{A \cap B}= {\bf 1}_{A} \times {\bf 1}_{B} %$
+
 
 
 ==
