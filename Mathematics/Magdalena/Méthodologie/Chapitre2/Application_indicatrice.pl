703ffbc3d2515807fc2db03f00410107a76789e9
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-12 08:02:33 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre2/Application_indicatrice.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre2/Application_indicatrice.pl
@@ -0,0 +1,34 @@
+
+extends = /model/AMC_TXT.pl
+title = Application indicatrice
+questions==
+
+
+** Soit $%E%$ un ensemble non vide et $% A%$ une partie de $%E%$. Cocher les bonnes réponses~: 
++ $% \forall x \in A \quad %$  $%{\bf 1}_A(x)=1 %$
+- $% \forall x \in A \quad {\bf 1}_A(x)=x %$
++ $% \forall x \in A^c \quad %$  $%{\bf 1}_A(x)=0 %$
+- $% \forall x \in E \quad %$  $%{\bf 1}_A(x)= {\bf 1}_E(x)%$
+
+
+** Cocher les bonnes réponse~:
+
++ $% {\bf 1}_[0,1](\sqrt{2})=0 %$
+- $% {\bf 1}_[0,2](\sqrt{2})= \sqrt{2} %$
++ $% {\bf 1}_[0,2](\sqrt{2})= 1%$
+- $% {\bf 1}_[0,1](\frac{\pi}{2})=1 %$
++ $% {\bf 1}_[0,1](\frac{\pi}{2})=0 %$
+
+** Cocher les bonnes réponses
+
++ $% {\bf 1}_[0,1](\sqrt{2})=0 %$
+- $% {\bf 1}_[0,2](\sqrt{2})= \sqrt{2} %$
++ $% {\bf 1}_[0,2](\sqrt{2})= 1%$
+- $% {\bf 1}_[0,1](\frac{\pi}{2})=1 %$
++ $% {\bf 1}_[0,1](\frac{\pi}{2})=0 %$
+
+
+==
+
+
+
