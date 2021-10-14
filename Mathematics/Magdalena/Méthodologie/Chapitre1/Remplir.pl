6761243854b7eba9b4f0e8c25a91713a1446d206
David DOYEN <david.doyen@univ-eiffel.fr>
2021-10-14 11:17:28 +0200

    ddoyen1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Remplir.pl
@@ -6,7 +6,7 @@ Sélectionner dans ce texte la première occurence d'une variable à l'intérieu
 
 seltext ==
 Soit #{$!n!$}# un entier naturel impair. 
-Alors, il existe un entier #{$!k!$}# tel que {$!k\in \mathbb N!$}. 
+Alors, il existe un entier #$!k!$# tel que {$!k\in \mathbb N!$}. 
 
-{$!n!$} {$!\in!$} {$!\{!$} {$!2k+1!$}  {$!\; | \;!$}  {$!k!$} {$!\in \mathbb N!$}
+{$!n!$} {$!\in!$} {$!\{!$}} {$!2k+1!$}  {$!\; | \;!$}  {$!k!$} {$!\in \mathbb N!$}
 ==
