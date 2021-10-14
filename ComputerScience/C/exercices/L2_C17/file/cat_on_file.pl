151406b6aab2343ef8937cd61f3b10268c9806d4
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-10-14 23:09:55 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/file/cat_on_file.pl

--- ComputerScience/C/exercices/L2_C17/file/cat_on_file.pl
+++ ComputerScience/C/exercices/L2_C17/file/cat_on_file.pl
@@ -72,7 +72,7 @@ checks_args_stdin==#|python|
 
 astuces==#|python|
 [
-  { "content": """La première étape consiste à ouvrir le fichier dont le nom est dans `argv[1]`. Il faudra ouvrir ce fichier en mode lecture `"r"`."""},
+  { "content": """La première étape consiste à ouvrir le fichier dont le nom est dans `argv[1]`. Il faudra ouvrir ce fichier en mode lecture en utilisant donc un second argument `"r"`."""},
   { "content": """La stratégie consiste à lire le fichier caractère par caractère tant que l'on atteint pas le caractère de fin de fichier EOF."""},
   { "content": """Pour code cette fonction, on peut facilement utiliser `fopen`, `fgetc`, `putchar` et `fclose`. Deux variables locales peuvent suffire : un entier pour stocker le caractère et une variable de type `FILE*`."""}
 ]
