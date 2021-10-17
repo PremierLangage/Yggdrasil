851efb2206e55144d0fbb55b0ad88b7c23960a3c
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-10-17 07:41:47 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/file/nb_lines_file.pl

--- ComputerScience/C/exercices/L2_C17/file/nb_lines_file.pl
+++ ComputerScience/C/exercices/L2_C17/file/nb_lines_file.pl
@@ -18,7 +18,7 @@ extends=/ComputerScience/C/template/std_progC17_clang.pl
 
 title=Compter le nombre de lignes d'un fichier
 
-tag=fichier|programme|lignes
+tag=fichier|programme|lignes|comptage|lecture
 
 author=Nicolas Borie
 
