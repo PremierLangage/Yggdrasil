df6f0d9e4cfbbe4bbe1f3028e45ca8b636849015
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-10-18 00:00:12 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/chapitre_3_boucles/boucleif-2122.pl

---
+++ ComputerScience/python/1NSI-2122/chapitre_3_boucles/boucleif-2122.pl
@@ -0,0 +1,56 @@
+
+# author zip
+
+# Heritage d'un type d'exercice 
+extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
+# extends = /model/math/input.pl
+@ /utils/plrandom.py
+
+title = nombres divisibles
+
+text =
+
+code =
+
+mplsoluce0 =
+
+textf==  
+
+Ecrire un programme qui lit {nb_entiers} entiers et affiche combien parmi eux sont divisibles par {diviseur}
+
+(Remarque: on utilise l=int(input()) pour lire un chiffre dans ce programme)
+==
+
+codef==
+
+
+print("Il y a",nb,"multiples de {diviseur}")
+==
+
+before == 
+from random import randint, choice
+from plrandom import sampleint
+nb_entiers = randint(9,25)
+diviseur = choice([3,5,7,11])
+code = codef.format(diviseur = diviseur)
+table = sampleint(1,1000,nb_entiers)
+soluce = f"table = {table}\ndiviseur = {diviseur}\nnb_entiers={nb_entiers}\n"+solucef
+mplsoluce0 = mplsolucef.format(table='\n'.join(str(val) for val in table))
+text = textf.format(diviseur = diviseur, nb_entiers = nb_entiers) + mplsoluce
+==
+
+
+
+solucef==
+nb=0
+for i in range(nb_entiers):
+    l=int(input())
+    if l%diviseur==0:
+       nb+=1
+print("Il y a",nb,"multiples de",diviseur)
+==
+
+mplsolucef==
+test
+{table}
+==
