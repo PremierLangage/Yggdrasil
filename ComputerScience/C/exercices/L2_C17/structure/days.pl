cd6a8a4a0668a5771e7c394c04fe77f23393e394
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-10-17 22:18:16 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/structure/days.pl

---
+++ ComputerScience/C/exercices/L2_C17/structure/days.pl
@@ -0,0 +1,65 @@
+#*****************************************************************************
+#  Copyright (C) 2017 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
+
+author=Nicolas Borie
+
+title=Structure C pour les jours de la semaine
+tag=structure|énumération|enum
+
+text==
+Déclarer et définir une **structure** C nommée **Jour**. Cette structure
+devra être une énumération des jours de la semaine dont les éléments
+seront le nom de chaque jour en francais et tout en minuscules :
+*lundi*, *mardi*, *mercredi*, ...    
+
+==
+
+editor.code==#|c|
+typedef ...
+==
+
+solution==#|c|
+typedef enum {lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche} Jour;
+==
+
+code_before==#|c|
+
+==
+
+code_after==#|c|
+#include <stdio.h>
+    
+int main(int argc, char* argv[]){
+
+  printf("Jour %d\n", lundi);
+  printf("Jour %d\n", mardi);
+  printf("Jour %d\n", mercredi);
+  printf("Jour %d\n", jeudi);
+  printf("Jour %d\n", vendredi);
+  printf("Jour %d\n", samedi);
+  printf("Jour %d\n", dimanche);
+  
+  return 0;
+}
+==
+
+checks_args_stdin==#|python|
+[ ["Déclaration et utilisation", [], ""] ] 
+==
+
+
