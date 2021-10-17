118022a07f777fdd44606e13a10529f2a4b00a63
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-10-17 23:02:07 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/python/AP1-2122/chapitre_2_conditionnelles/boucleif.pl

--- ComputerScience/python/AP1-2122/chapitre_2_conditionnelles/boucleif.pl
+++ ComputerScience/python/AP1-2122/chapitre_2_conditionnelles/boucleif.pl
@@ -6,19 +6,21 @@ extends=/ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
 title = nombres divisibles
 
-before=
+before == 
+nb_entiers = randint(9,25)
+diviseur = choice([3,5,7,11])
+==
 
 text==  
 
-Ecrire un programme qui lit 12 entiers et affiche combien parmi eux sont divisibles par 7
+Ecrire un programme qui lit {nb_entiers} entiers et affiche combien parmi eux sont divisibles par {diviseur}
 
 (Remarque: on utilise l=int(input()) pour lire un chiffre dans ce programme)
 ==
 code==
 
 
-
-print("Il y a",nb,"multiples de 7")
+print("Il y a",nb,"multiples de {diviseur}")
 ==
 soluce==
 nb=0
