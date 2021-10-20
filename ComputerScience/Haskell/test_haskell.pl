34c5f976c27eb0393115bbcd24b9c9358474c0d1
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-10-20 19:35:15 +0200

    nborie1 modified Yggdrasil/ComputerScience/Haskell/test_haskell.pl

--- ComputerScience/Haskell/test_haskell.pl
+++ ComputerScience/Haskell/test_haskell.pl
@@ -31,7 +31,7 @@ f = open('hello_world.hs', 'w')
 f.write(editor.code)
 f.close()
 
-process = Popen(['ghc', 'hello_world.hs', '-o', 'hello_world'], stdout=PIPE, stderr=PIPE, shell=True)
+process = Popen(['ghc', 'hello_world.hs'], stdout=PIPE, stderr=PIPE, shell=True)
 stdout, stderr = process.communicate()
 
 ans = stdout.decode()
