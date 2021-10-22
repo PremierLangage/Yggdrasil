56640dd2db7c0739655e9253cdd08d920c4196e0
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-10-22 11:02:22 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_4_fonctions/Volume_sphere.pl

--- ComputerScience/python/AP1-2021/Chapitre_4_fonctions/Volume_sphere.pl
+++ ComputerScience/python/AP1-2021/Chapitre_4_fonctions/Volume_sphere.pl
@@ -22,9 +22,9 @@ Vous devez écrire deux fonctions,<br>
 **volume_sphere** qui retourne le volume de la sphere 
 en faisant appel à la fonction **cube**. <br>
 
-Vous écrirez un programme :
-    * demandant à un utilisateur de rentrer un réel strictement positif, naturellement un volume ne peut pas être négatif !, faire une boucle de lecture.
-    * appelant la fonction **volume_sphere** et affichant le résultat.<br>
+Vous écrirez un programme demandant à un utilisateur de rentrer un réel strictement positif,
+ naturellement un volume ne peut pas être négatif !, faire une boucle de lecture. 
+Le programme  appelant la fonction **volume_sphere** et affichant le résultat.<br>
 
 ==
 before==
