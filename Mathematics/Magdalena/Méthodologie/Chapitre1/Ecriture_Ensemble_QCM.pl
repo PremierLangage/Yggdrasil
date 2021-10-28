e63315c4118d0c9d30f5b043c6c5f414960be9b5
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-28 16:28:51 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Ensemble_QCM.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Ensemble_QCM.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Ensemble_QCM.pl
@@ -3,8 +3,6 @@ title = Ecriture d'ensembles
 questions==
 
   
-(c) l’ensemble des nombres qui sont diviseurs de 50 et inférieurs à 23 (d’abord en extension, puis en compréhension
-à l’aide du symbole divise "$%\mid%$") ;
 (d) l’ensemble des couples dont la première coordonnée est un nombre rationnel strictement plus grand que π et
 la seconde coordonnée est un nombre relatif dont le reste de la division euclidienne par 5 vaut 2 ;
 (e) L’ensemble des nombres complexes dont l’argument est congru à π
@@ -14,21 +12,21 @@ l’exponentielle complexe et enfin à l’aide d’une représentation paramét
 
 questions==
 
-** Cochez les bonnes réponses. Ecrire en compréhension l'ensemble des diviseurs de 66 à l'aide du symbole "$%|%$".
+** Cochez les bonnes réponses. Ecrire *en compréhension* l'ensemble des diviseurs de 66 à l'aide du symbole "$%|%$".
 +  $% \\{n \in \mathbb{N} \\; \lvert \quad n \mid 66 \\}%$ 
 -  $% \\{  n \mid 66 \quad |\quad n \in \mathbb{N} \\}%$
 -  $% \\{ n \in \mathbb{N} \quad |\quad 66 \mid n \\}%$
 -  $%\\{1, 2, 3, 6, 11, 22, 33, 66\\} %$
 
 
-** Cochez les bonnes réponses. Ecrire en compréhension l’ensemble des diviseurs de 66 avec les éléments premiers du langage. 
+** Cochez les bonnes réponses. Ecrire *en compréhension* l’ensemble des diviseurs de 66 avec les éléments premiers du langage. 
 
 - $% \\{n \in \mathbb{N} \\; |\\; \exists k \in \mathbb{N} \quad n= 66 \times k  \\}%$ 
 +  $% \\{n \in \mathbb{N} \\; |\\; \exists k \in \mathbb{N} \quad n\times k = 66  \\}%$
 -  $% \\{n \in \mathbb{N} \quad  k \in \mathbb{N} \\; |\\; n\times k = 66  \\}%$
 -  $% \\{n \in \mathbb{N} \\; |\\;  k \in \mathbb{N}  \quad n\times k = 66  \\}%$
 
-** Cochez les bonnes réponses. Ecrire formellement l’ensemble des entiers relatifs multiples de 3 et impairs.
+** Cochez les bonnes réponses. Ecrire *formellement* l’ensemble des entiers relatifs multiples de 3 et impairs.
 
 - $% \\{n \in \mathbb{Z} \\;  | \\;  \exists k \in \mathbb{Z} \quad  n=3k \\; \mbox{ et } n= 2k+1 \\}%$ 
 + $% \\{n \in \mathbb{Z} \\;  | \\;  (\exists k \in \mathbb{Z} \quad  n=3k) \\; \mbox{ et } \\; (\exists k \in \mathbb{Z} \quad  n=2k+1) \\}%$ 
@@ -37,11 +35,7 @@ questions==
 + $%  \\{6k+3 \\; |\\; k \in \mathbb{Z} \\}%$ 
 - $% \\{3k+1 \\; |\\; k \in \mathbb{Z} \\}%$ 
 
-
-
-
-
-** Cochez les bonnes réponses. Ecrire formellement l’ensemble des entiers naturels qui sont diviseurs de 50 et inférieurs à 23.
+** Cochez les bonnes réponses. Ecrire *formellement* l’ensemble des entiers naturels qui sont diviseurs de 50 et inférieurs à 23.
 
 - $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{Z} \quad  n=50 k \\; \mbox{ et } n\le 23 \\}%$ 
 + $% \\{n \in \mathbb{N} \\;  | \\;  (\exists k \in \mathbb{Z} \quad  n\times k =50) \\; \mbox{ et } \\; (n \le 23) \\}%$ 
@@ -49,6 +43,19 @@ questions==
 -  $% \\{k \in \mathbb{N} \\;  | \\;  n \le 23 \quad \mbox{ et } n \mid 50 \\}%$  
 + $%  \\{k \in \mathbb{N} \\;  | \\;  k \le 23 \quad \mbox{ et } k \mid 50 \\}%$
 - $% \\{k \le 23 \\; |\\; k \mid 50  \\}%$ 
+
+** Cochez les bonnes réponses. Ecrire *formellement*  l’ensemble des couples dont la première coordonnée est un nombre rationnel strictement plus grand que $%\pi%$ et
+la seconde coordonnée est un nombre relatif dont le reste de la division euclidienne par 5 vaut 2.
+
+- $% \\{(p,n) \in \mathbb{Q}\times \mathbb{Z} \\;  | \\;  \exists p \le  \pi  \quad  \exists k \in \mathbb{Z} \\; \mbox{ et } n= 5k+2  \\}%$ 
++ $% \\{(p,n) \in \mathbb{Q}\times \mathbb{Z} \\;  | \\;  p \le  \pi  \quad  \exists k \in \mathbb{Z} \\; \mbox{ et } n= 5k+2  \\}%$ 
++ $% \\{X \in \mathbb{Q}\times \mathbb{Z}  \\;  | \\;  \exists p \in \mathbb{Q}  \quad n \in \mathbb{Z} \quad X= (p,n) \quad p \le  \pi  \quad n \in \mathbb{Z} \quad  \exists k \in \mathbb{Z} \\; \mbox{ et } n= 5k+2  \\}%$
++  $% \\{(p,5k+2)  \\;  | \\;   p\in \mathbb{Q} \quad p \le  \pi  \quad  k \in \mathbb{Z}  \\}%$  
++ $%  \\{(p,n)  \\;  | \\;  p \in \mathbb{Q} \quad p \le  \pi  \quad n \in \mathbb{Z} \quad  \exists k \in \mathbb{Z} \\; \mbox{ et } n= 5k+2  \\}%$ 
+- $% $% \\{(p,5k+2)  \\;  | \\;   p\in \mathbb{R} \quad p \le  \pi  \quad  k \in \mathbb{Z}  \\}%$  
+
+
+
 
 
 ==
