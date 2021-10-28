598ce64560c9af7f4de44a32a26b686fa7362148
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-10-28 11:12:38 +0200

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Ensemble_QCM.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre1/Ecriture_Ensemble_QCM.pl
@@ -0,0 +1,45 @@
+extends = /model/AMC_TXT.pl
+title = Ecriture d'ensembles
+questions==
+
+’ensemble des diviseurs de 66 (d’abord en extension, puis en compréhension avec le symbole divise "|", puis
+en compréhension uniquement avec les éléments premiers du langage) ;
+(b) l’ensemble des multiples impairs de 3 (d’abord en compréhension puis de manière paramétrique). Préciser
+deux éléments de l’ensembles et deux éléments n’appartenant pas à l’ensemble.
+(c) l’ensemble des nombres qui sont diviseurs de 50 et inférieurs à 23 (d’abord en extension, puis en compréhension
+à l’aide du symbole divise "|") ;
+(d) l’ensemble des couples dont la première coordonnée est un nombre rationnel strictement plus grand que π et
+la seconde coordonnée est un nombre relatif dont le reste de la division euclidienne par 5 vaut 2 ;
+(e) L’ensemble des nombres complexes dont l’argument est congru à π
+2 modulo 2π, d’abord en compréhension
+et à l’aide de l’application argument (notée arg), puis à l’aide d’une représentation paramétrique utilisant
+l’exponentielle complexe et enfin à l’aide d’une représentation paramétrique cartésienne.
+
+questions==
+
+** Cochez les bonnes réponses. Ecrire en compréhension l'ensemble des diviseurs de 66 à l'aide du symbole "$%|%$".
++  $% \\{n \in \mathbb{N} \\; |\\; n\, | \, 66 \\}%$ 
+-  $% \\{  n\, | \, 66 \\; |\\; n \in \mathbb{N} \\}%$
+-  $% \\{ n \in \mathbb{N} \\; |\\; 66\, | \, n \\}%$
+-  $%\\{1, 2, 3, 11\} %$
+
+
+** Cochez les bonnes réponses. Ecrire en compréhension l’ensemble des diviseurs de 66 avec les éléments premiers du langage. 
+
+- $% \\{n \in \mathbb{N} \\; |\\; \exists k \in \mathbb{N} \; n= 66 \times k  \\}%$ 
++  $% \\{n \in \mathbb{N} \\; |\\; \exists k \in \mathbb{N} \; n\times k = 66  \\}%$
+-  $% \\{n \in \mathbb{N} \\,  k \in \mathbb{N} \\; |\\; n\times k = 66  \\}%$
+-  $% \\{n \in \mathbb{N} \\; |\\;  k \in \mathbb{N}  \; n\times k = 66  \\}%$
+
+** Cochez les bonnes réponses. Ecrire formellement l'ensemble des entiers naturels impairs
+
+- $% \\{n \in \mathbb{N} \quad  | \\;  \forall k \in \mathbb{N} \quad  n=2k+1 \\}%$ 
++ $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k+1 \\}%$ 
+- $% \\{ \exists n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  n=2k+1 \\}%$
+- $% \\{n \in \mathbb{N} \\;  | \\;  \exists k \in \mathbb{N} \quad  2k+1 \\}%$  
++ $%  \\{2n+1 \\; |\\; n \in \mathbb{N} \\}%$ 
+- $% \\{n, k \in \mathbb{N} \\;  | \\;  n=2k+1 \\}%$ 
+
+==
+
+
