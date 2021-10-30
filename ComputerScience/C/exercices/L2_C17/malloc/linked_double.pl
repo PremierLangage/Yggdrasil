6c0713ee807c95a1597db1e29d14c8d52c4d3077
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-10-30 18:31:44 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/malloc/linked_double.pl

---
+++ ComputerScience/C/exercices/L2_C17/malloc/linked_double.pl
@@ -0,0 +1,98 @@
+#*****************************************************************************
+#  Copyright (C) 2019 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+extends=/ComputerScience/C/template/std_progC.pl
+
+author=Nicolas Borie
+
+title=Cellule pour liste chaînée de double
+tag=malloc|structure|chaînage|double
+
+editor.height=300px
+
+text==
+Écrire une fonction d'allocation pour une **cellule de liste chainée** 
+contenant des doubles (un double par cellule). Une fonction d'allocation
+raisonnable prépare de la mémoire pérenne, renseigne le double dans
+la cellule et initialise la cellule de manière à ce qu'elle ne possède
+pas de suivant. Finalement, on retourne l'adresse de la cellule correctement
+allouée et initialisée.
+==
+
+editor.code==#|c|
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cell{
+  double value;
+  struct cell* next;
+}Cell, *List;
+
+... Allocate_cell(double val){
+  /* ... votre code ici ... */
+}
+==
+
+solution==#|c|
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct cell{
+  double value;
+  struct cell* next;
+}Cell, *List;
+
+Cell* Allocate_cell(double val){
+  Cell* ans;
+
+  ans = (Cell*)malloc( sizeof(Cell) );
+  if (ans == NULL)
+    return ans;
+  ans->next = NULL;
+  ans->value = val;
+  return ans;
+}
+==
+
+code_before==#|c|
+#define _POSIX_C_SOURCE 200112L
+==
+
+code_after==#|c|
+int main(int argc, char* argv[]){
+  Cell* ans=NULL;
+  
+  printf("Allocation\n");
+  ans = Allocate_cell(strtof(argv[1], NULL));
+  printf("Utilisation\n");
+  printf("value : %f\n", ans->value);
+  printf("next : %p\n", (void*)ans->next); 
+  printf("Libération\n");
+  free(ans);
+  
+  return 0;
+}
+==
+
+checks_args_stdin==#|python|
+[["Exécution simple", ["3.141592"], ""],
+ ["Test aléatoire 1", [str(100*random())], ""],
+ ["Test aléatoire 2", [str(100*random())], ""],
+ ["Test aléatoire 3", [str(100*random())], ""]]
+==
+
+
+
