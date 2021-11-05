28786f4de398b9a0d6f62f6df965d59040029343
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2021-11-05 09:52:16 +0100

    nborie1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/expressions/guess_type.pl

--- ComputerScience/C/exercices/L2_C17/expressions/guess_type.pl
+++ ComputerScience/C/exercices/L2_C17/expressions/guess_type.pl
@@ -14,5 +14,67 @@
 #            https://creativecommons.org/licenses/by-sa/3.0/fr/
 #*****************************************************************************
 
+@ /utils/sandboxio.py
+
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+# STEP 1
+match =: MatchList
+
+before==#|python|
+import random
+match.nodes = []
+expected = []
+
+match.nodes.append({"id": "100", "content": "`float`", "target": True})
+
+match.nodes.append({"id": "1", "content": "%d * %d" % (a, b), "source": True})
+
+match.nodes.append({"id": "100", "content": "`float`", "target": True})
+
+expected.append({ "source": sourceId, "target": targetId })
+random.shuffle(match.nodes)
+==
+
+title==
+Match List Component
+==
+
+text==
+*Link each operation to the appropriate result.*
+==
+
+form==
+{{ match|component }}
+==
+
+evaluator== #|python|
+
+def in_links(conn, links):
+    for e in links:
+        if e['source'] == conn["source"] and  e['target'] == conn["target"]:
+            return True
+    return False
+
+error = 0
+for e in expected:
+    if not in_links(e, match.links):
+        error = error + 1
+
+for e in match.links:
+    e['css'] = 'error-state animated fadeIn'
+    if in_links(e, expected):
+        e['css'] = 'success-state  animated rotateIn'
+
+if error == 0:
+    grade = (100, '<span class="success-state animated pulse infinite">Good answser</span>')
+else:
+    grade = (0, '<span class="error-state animated pulse infinite">Bad answer, you made %d mistakes</span>' % error)
+==
+
+
+
+
 
 
