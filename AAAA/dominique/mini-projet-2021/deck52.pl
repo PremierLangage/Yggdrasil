b4def4448b9ba55d49fa85fb4658d7fead2b2576
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-08 15:37:28 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/deck52.pl

--- AAAA/dominique/mini-projet-2021/deck52.pl
+++ AAAA/dominique/mini-projet-2021/deck52.pl
@@ -2,32 +2,22 @@
 
 
 
-
-
-extends = /ComputerScience/python/template/pltest.pl
-
-title= deck32 un jeux de 32 cartes 
+title = deck52
 
 text==
 
-Ecrire la fonction **deck32** correspondant au doctest suivant
-
-```python
-    """Create a 32  card set 
-    color in ["pique","coeur","carreau","trèfle"]
-    height in [7,...,10,"valet","dame","roi"]
-    (Color, heigth)
-    returns a list of 32 pairs 
-    """
+Ecrire la fonction deck52  de docktest suivant :
 ```
+deck52 retourne une liste de 52 pairs (color, height) ou
 
+ color in ["pique","coeur","carreau","trèfle"] 
 
+ height in ["As",2,3,4,5,6,7,8,9,10,"Valet","Dame","Roi"] 
+
+```
 ==
-
 
 pltest==
->>> deck32() # Test simple 
-[('pique', 7), ('pique', 8), ('pique', 9), ('pique', 10), ('pique', 'valet'), ('pique', 'dame'), ('pique', 'roi'), ('coeur', 7), ('coeur', 8), ('coeur', 9), ('coeur', 10), ('coeur', 'valet'), ('coeur', 'dame'), ('coeur', 'roi'), ('carreau', 7), ('carreau', 8), ('carreau', 9), ('carreau', 10), ('carreau', 'valet'), ('carreau', 'dame'), ('carreau', 'roi'), ('trèfle', 7), ('trèfle', 8), ('trèfle', 9), ('trèfle', 10), ('trèfle', 'valet'), ('trèfle', 'dame'), ('trèfle', 'roi')]
+>>> deck52()
+[('pique', 'As'), ('pique', 2), ('pique', 3), ('pique', 4), ('pique', 5), ('pique', 6), ('pique', 7), ('pique', 8), ('pique', 9), ('pique', 10), ('pique', 'Valet'), ('pique', 'Dame'), ('pique', 'Roi'), ('coeur', 'As'), ('coeur', 2), ('coeur', 3), ('coeur', 4), ('coeur', 5), ('coeur', 6), ('coeur', 7), ('coeur', 8), ('coeur', 9), ('coeur', 10), ('coeur', 'Valet'), ('coeur', 'Dame'), ('coeur', 'Roi'), ('carreau', 'As'), ('carreau', 2), ('carreau', 3), ('carreau', 4), ('carreau', 5), ('carreau', 6), ('carreau', 7), ('carreau', 8), ('carreau', 9), ('carreau', 10), ('carreau', 'Valet'), ('carreau', 'Dame'), ('carreau', 'Roi'), ('trèfle', 'As'), ('trèfle', 2), ('trèfle', 3), ('trèfle', 4), ('trèfle', 5), ('trèfle', 6), ('trèfle', 7), ('trèfle', 8), ('trèfle', 9), ('trèfle', 10), ('trèfle', 'Valet'), ('trèfle', 'Dame'), ('trèfle', 'Roi')]
 ==
-
-
