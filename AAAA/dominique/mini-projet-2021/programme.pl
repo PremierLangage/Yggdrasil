1282fd3c6f977aed8eced8689794a9548cdfb1df
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-09 20:15:46 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/programme.pl

---
+++ AAAA/dominique/mini-projet-2021/programme.pl
@@ -0,0 +1,43 @@
+
+
+
+extends = /model/opentext.pl
+title= Le programme final
+
+text==
+
+SI vous avez réussi à écrire toutes les fonctions vous pouvez vous attaquer a un programme qui utilise ces fonctions pour simuler une partie.
+
+Voici un squette du programme principal.
+
+# Deux équipes de deux joueurs = (nord,sud) et (est, ouest)
+    while partie pas terminé :
+        # Choisir le joeur qui parle en premier (normalement le joeur a gauche du distributeur parle en premier, et ce role tourne dans le sens horraire)
+        # création du jeu pour la manche courrante 
+        # melanger le jeu 
+        # distribution des 5 premières  cartes
+        # affichage de "la retourne" 
+        # Pour chaque joueurs dans l'ordre nord, est, sud, ouest   (0,1,2,3)
+        # Afficher le jeu et la retourne 
+        # demander si le joueur "prend" 
+        # Si personne n'a pris deuxième tour avec le choix de l'atout
+        # si personne n'a pris : continue
+        # a partir d'ici quelqu'un a pris
+        # distribution des cartes restante avec la retourne au preneur
+        # calcul de la prime belote et rebelote 
+        # jeu
+        # 
+        # pour chaque tour "donner la main" au joueur qui parle en premier puis aux gangnant du plis précédent
+            # lancer le plis c'est "l'entame" le joueur qui à la main choisi la premier carte du plis 
+            # les autres joueurs dans l'ordre horraire fournissent une carte en suivant les règles
+
+
+        # Dix de der calculer l'équipe qui gagne le dernier plis
+        # Belote est gangnée dans tout les cas.
+        # Calcul des points des deux paquets de plis
+
+        # affecter le bon nombre de points à la bon équipe
+
+# féliciter l'équipe gagnante 
+
+==
