1070780b338eb3a2a9c15904d848749baf3156a3
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-09 17:14:10 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/distribue.pl

---
+++ AAAA/dominique/mini-projet-2021/distribue.pl
@@ -0,0 +1,23 @@
+
+
+extends = func.pl
+
+funcname=deal
+title= Distribution
+
+doctest==
+la fonction  deal(deck, nb_cartes=5, nbjoueurs=4):
+
+    Distribue nb_cartes cartes du deck a nbjoueurs 
+
+    par défaut cela distribue 5 cartes a 4 joueurs
+
+    retourne une liste de nbjoueurs listes de nb_cartes 
+==
+
+pltest==
+>>> deal([('7', 'Coeur'), ('7', 'Carreau'), ('7', 'Trèfle'), ('7', 'Pique')],nb_cartes=2,nbjoueurs=2)
+[[('7', 'Pique'), ('7', 'Trèfle')], [('7', 'Carreau'), ('7', 'Coeur')]]
+>>> deal([('Valet', 'Coeur'), ('Valet', 'Carreau'), ('Valet', 'Trèfle'), ('Valet', 'Pique'), ('Dame', 'Coeur'), ('Dame', 'Carreau'), ('Dame', 'Trèfle'), ('Dame', 'Pique'), ('Roi', 'Coeur'), ('Roi', 'Carreau'), ('Roi', 'Trèfle'), ('Roi', 'Pique'), ('10', 'Coeur'), ('10', 'Carreau'), ('10', 'Trèfle'), ('10', 'Pique'), ('As', 'Coeur'), ('As', 'Carreau'), ('As', 'Trèfle'), ('As', 'Pique')]) # 5 cartes 4 joueurs
+[[('As', 'Pique'), ('As', 'Trèfle'), ('As', 'Carreau'), ('As', 'Coeur'), ('10', 'Pique')], [('10', 'Trèfle'), ('10', 'Carreau'), ('10', 'Coeur'), ('Roi', 'Pique'), ('Roi', 'Trèfle')], [('Roi', 'Carreau'), ('Roi', 'Coeur'), ('Dame', 'Pique'), ('Dame', 'Trèfle'), ('Dame', 'Carreau')], [('Dame', 'Coeur'), ('Valet', 'Pique'), ('Valet', 'Trèfle'), ('Valet', 'Carreau'), ('Valet', 'Coeur')]] 
+==
