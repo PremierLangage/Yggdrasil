ebfb7b66e933678c56c99684633069ee88663a21
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-09 16:55:17 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/func.pl

---
+++ AAAA/dominique/mini-projet-2021/func.pl
@@ -0,0 +1,24 @@
+
+
+
+
+
+extends = /ComputerScience/python/template/pltest.pl
+
+title= Distribution 
+
+text==
+
+
+Ecrire une fonction **{{funcname}}** qui vérifie le doctest suivant :
+
+{{doctest}}
+==
+
+
+pltest==
+>>> deal([],3)
+>>> deal([(1,2),(3,4)],1)
+[(1,2)],[(3,4)]
+==
+
