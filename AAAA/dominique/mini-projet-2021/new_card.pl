27505e73ac4280069eb15a375d5c38a151648578
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-09 17:03:14 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/new_card.pl

---
+++ AAAA/dominique/mini-projet-2021/new_card.pl
@@ -0,0 +1,22 @@
+
+
+
+
+extends = func.pl
+
+funcname=new_card
+title= new_card 
+
+doctest==
+Prend en paramêtres heigth et color 
+retourne une nouvelle carte un tuple (heigth,  color)
+==
+
+
+pltest==
+>>> new_card("roi","pique")
+('roi', 'pique')
+>>> new_card(5,6)
+(5, 6)
+==
+
