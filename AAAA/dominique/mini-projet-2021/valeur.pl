bb0108f9e130be03c55f4b79d4786cb0dacd2411
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-09 17:23:46 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/valeur.pl

---
+++ AAAA/dominique/mini-projet-2021/valeur.pl
@@ -0,0 +1,21 @@
+
+
+
+
+
+extends = func.pl
+
+funcname=value
+title= Les points d'une carte 
+
+doctest==
+
+    la fonction value(card,atout)
+    
+    retourne la valeur de la carte pour l'atout atout.
+    
+==
+
+pltest==
+
+==
