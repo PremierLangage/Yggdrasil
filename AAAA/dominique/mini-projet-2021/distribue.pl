bb4b07609a857c4153a68aa8a68c698fb2ebe412
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-10 07:40:50 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/distribue.pl

--- AAAA/dominique/mini-projet-2021/distribue.pl
+++ AAAA/dominique/mini-projet-2021/distribue.pl
@@ -15,18 +15,20 @@ doctest==
 
     retourne une liste de nbjoueurs listes de nb_cartes.
 
-    **Attention il faut retirer les cartes du paquet (deck) une par une pour cela vous utiliserez la fonction pop().**
-    
+    **Attention il faut retirer les cartes du paquet (deck) une par une pour cela vous utiliserez la fonction deck.pop() sans paramètres.**
+
 ==
 
 pltest==
 >>> deal([('7', 'Coeur'), ('7', 'Carreau'), ('7', 'Trèfle'), ('7', 'Pique')],nb_cartes=2,nbjoueurs=2)
-[[('7', 'Pique'), ('7', 'Trèfle')], [('7', 'Carreau'), ('7', 'Coeur')]]
+[[('7', 'Pique'), ('7', 'Carreau')], [('7', 'Trèfle'), ('7', 'Coeur')]]
 >>> deal([('Valet', 'Coeur'), ('Valet', 'Carreau'), ('Valet', 'Trèfle'), ('Valet', 'Pique'), ('Dame', 'Coeur'), ('Dame', 'Carreau'), ('Dame', 'Trèfle'), ('Dame', 'Pique'), ('Roi', 'Coeur'), ('Roi', 'Carreau'), ('Roi', 'Trèfle'), ('Roi', 'Pique'), ('10', 'Coeur'), ('10', 'Carreau'), ('10', 'Trèfle'), ('10', 'Pique'), ('As', 'Coeur'), ('As', 'Carreau'), ('As', 'Trèfle'), ('As', 'Pique')]) # 5 cartes 4 joueurs
-[[('As', 'Pique'), ('As', 'Trèfle'), ('As', 'Carreau'), ('As', 'Coeur'), ('10', 'Pique')], [('10', 'Trèfle'), ('10', 'Carreau'), ('10', 'Coeur'), ('Roi', 'Pique'), ('Roi', 'Trèfle')], [('Roi', 'Carreau'), ('Roi', 'Coeur'), ('Dame', 'Pique'), ('Dame', 'Trèfle'), ('Dame', 'Carreau')], [('Dame', 'Coeur'), ('Valet', 'Pique'), ('Valet', 'Trèfle'), ('Valet', 'Carreau'), ('Valet', 'Coeur')]] 
+[[('As', 'Pique'), ('10', 'Pique'), ('Roi', 'Pique'), ('Dame', 'Pique'), ('Valet', 'Pique')], [('As', 'Trèfle'), ('10', 'Trèfle'), ('Roi', 'Trèfle'), ('Dame', 'Trèfle'), ('Valet', 'Trèfle')], [('As', 'Carreau'), ('10', 'Carreau'), ('Roi', 'Carreau'), ('Dame', 'Carreau'), ('Valet', 'Carreau')], [('As', 'Coeur'), ('10', 'Coeur'), ('Roi', 'Coeur'), ('Dame', 'Coeur'), ('Valet', 'Coeur')]]
 ==
 
 pltest2==
->>> deal([x for x in range(120)],nb_cartes=2, nbjoueurs=4):
+>>> deal([1,2,3,4,5],nb_cartes=1, nbjoueurs=1)
+[[5]]
+>>> deal([x for x in range(120)],nb_cartes=2, nbjoueurs=4)
 [[119, 118], [117, 116], [115, 114], [113, 112]]
 ==
