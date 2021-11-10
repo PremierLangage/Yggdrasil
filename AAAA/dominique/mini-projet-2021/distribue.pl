adb5c7fcf5c1d08cb6de3934f5b46f14fdd562a5
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-10 07:41:39 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/distribue.pl

--- AAAA/dominique/mini-projet-2021/distribue.pl
+++ AAAA/dominique/mini-projet-2021/distribue.pl
@@ -15,7 +15,9 @@ doctest==
 
     retourne une liste de nbjoueurs listes de nb_cartes.
 
-    **Attention il faut retirer les cartes du paquet (deck) une par une pour cela vous utiliserez la fonction deck.pop() sans paramètres.**
+**Attention il faut retirer les cartes du paquet (deck) une par une
+
+pour cela vous utiliserez la fonction deck.pop() sans paramètres.**
 
 ==
 
