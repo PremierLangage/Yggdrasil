edc5a5d8ec0fdfc025aee00fb008f1c7b5008e78
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-12 12:09:07 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/mini-projet-2021/comptepoints.pl

--- AAAA/dominique/mini-projet-2021/comptepoints.pl
+++ AAAA/dominique/mini-projet-2021/comptepoints.pl
@@ -9,20 +9,21 @@ title= Compte du nombre de points des plis gagnés
 
 doctest==
 
-La fonction comptepoints(pliss,dixdeder, atout) retourne le nombre de points contenu dans une liste de plis.
+La fonction comptepoints(pliss, atout ,dixdeder=0) retourne le nombre de points contenu dans une liste de plis.
 Chaque plis étant une liste de cartes.
 
-Utiliser la fonction valeur que vous avez déjà écrite.
+Utilisez la fonction **valeur** que vous avez déjà écrite.
 
+Ajouter la valeur de dixdeder.
 ==
 
 pltest==
 >>> HEIGTH= ( "7", "8", "9","Valet", "Dame", "Roi", "10","As") #
 >>> COLOR= ("Coeur", "Carreau", "Trèfle", "Pique") # 
 >>> HEIGTHA= ( "7", "8", "Dame", "Roi", "10", "As", "9", "Valet") #
->>> comptepoints([(h,c) for h in HEIGTH for c in COLOR],10,"pique")
+>>> comptepoints([(h,c) for h in HEIGTH for c in COLOR],"pique",10)
 130
->>> comptepoints([(h,c) for h in HEIGTH for c in COLOR],10,"Pique")
+>>> comptepoints([(h,c) for h in HEIGTH for c in COLOR],"Pique",10)
 162
 ==
 
