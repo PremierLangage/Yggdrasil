dd9f4512e3410e9207fe6b8fca4aafbf5c7d7773
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-13 14:06:31 +0100

    ddoyen1 modified Yggdrasil/Languages/Greek/greekvoc.pl

--- Languages/Greek/greekvoc.pl
+++ Languages/Greek/greekvoc.pl
@@ -12,9 +12,15 @@ with open('grec_voc_1.csv', newline='') as f:
 article = row['article']
 mot = row['mot']
 sol = row['traductions']
+i = items.index(sol)
+try:
+    history.append(i)
+except:
+    history = []
 ==
 
 text ==
+{{ history }}
 Que signifie le mot suivant ?
 <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/gfs-porson" type="text/css"/> 
 <div style="text-align:center;font-size: x-large;font-family:'GFSPorsonRegular';">
