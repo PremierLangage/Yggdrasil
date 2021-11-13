832830f94e5d00d8317e2b8bbdb8bb580ce5edd8
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-13 16:42:48 +0100

    ddoyen1 modified Yggdrasil/Languages/Greek/greekvoc.pl

--- Languages/Greek/greekvoc.pl
+++ Languages/Greek/greekvoc.pl
@@ -13,6 +13,7 @@ article = row['article']
 mot = row['mot']
 sol = row['traductions']
 i = items.index(sol)
+
 ==
 
 tplpage =@ tplvoc.html
@@ -24,4 +25,23 @@ Que signifie le mot suivant ?
 <div style="text-align:center;font-size: x-large;font-family:'GFSPorsonRegular';">
 {{article}} {{mot}}
 </div>
+==
+
+evaluator ==
+if ans == sol:
+    score = 100
+elif ans not in input.autocomplete:
+    feedback = "La réponse doit faire partie des propositions."
+    score = -1
+else:
+    score = 0
+
+if score != -1:
+    with open('grec_voc_1.csv', newline='') as f:
+        row = csv_choice(f, delimiter=";")
+        f.seek(0)
+        items = csv_col(f, "traductions", delimiter=";")
+    article = row['article']
+    mot = row['mot']
+    sol = row['traductions']
 ==
