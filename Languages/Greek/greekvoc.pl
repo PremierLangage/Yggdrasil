ae6faa873ecf76b096d9cf9423061649a5d2c627
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-13 16:55:46 +0100

    ddoyen1 modified Yggdrasil/Languages/Greek/greekvoc.pl

--- Languages/Greek/greekvoc.pl
+++ Languages/Greek/greekvoc.pl
@@ -23,10 +23,13 @@ tplpage =@ tplvoc.html
 question ==
 {{ history }}
 Que signifie le mot suivant ?
-<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/gfs-porson" type="text/css"/> 
 <div style="text-align:center;font-size: x-large;font-family:'GFSPorsonRegular';">
 {{article}} {{mot}}
 </div>
+==
+
+style.greek ==
+<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/gfs-porson" type="text/css"/> 
 ==
 
 evaluator ==
