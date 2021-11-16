eeaa1ad37e780f52c508d4d9e34aa80923d2416d
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-16 22:50:04 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/Doctestexo/modeldoctest.pl

--- AAAA/dominique/Doctestexo/modeldoctest.pl
+++ AAAA/dominique/Doctestexo/modeldoctest.pl
@@ -91,7 +91,7 @@ def subnlbybr(str):
     """
     if str :
        str = "  \n".join(str.split("\n"))
-       return "&nbsp;".join(str.split(" "))
+       return str
     return None
 
 text_f1  = subnlbybr(f1)
