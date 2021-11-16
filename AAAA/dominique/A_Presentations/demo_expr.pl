d1009009a89e9850cb005e1d0a345f756cfc2f78
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-16 15:33:19 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/demo_expr.pl

---
+++ AAAA/dominique/A_Presentations/demo_expr.pl
@@ -0,0 +1,19 @@
+
+extends = /model/math/expr.pl
+
+title = Calculer la distance entre deux points du plan
+
+before ==
+xA = randint(-5, 5)
+yA = randint(-5, 5)
+xB = randint(-5, 5)
+yB = randint(-5, 5)
+sol = sqrt((xA-xB)**2 + (yA-yB)**2)
+==
+
+text ==
+Dans le plan muni d'un repère orthonormé on considère les points de coordonnées 
+$! ( {{ xA }}, {{ yA }} ) !$ et $! ( {{ xB }}, {{ yB }} ) !$.
+
+Calculer et donner la distance enclidienne entre ces deux points ?
+==
