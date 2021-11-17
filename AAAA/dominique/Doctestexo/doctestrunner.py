16efc76b619b8e4e78b8c0cd025dfd601b363b0f
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-11-17 08:44:47 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/Doctestexo/doctestrunner.py

--- AAAA/dominique/Doctestexo/doctestrunner.py
+++ AAAA/dominique/Doctestexo/doctestrunner.py
@@ -20,13 +20,13 @@ class PldoubleRunner(doctest.DocTestRunner):
         if not self.runf1test():
             if self.etat != "fail":
                 return 0,self.etat
-            return 10," Votre test est en échec pour la permière version"
+            return 10," <span class="error-state"> Votre test est en échec pour la permière version </span>"
         if not self.runf2test():
             if self.etat != "pass":
                 return 0,self.etat
-            return 25," Votre test pass pour la deuxi&eacute;me version ce qui n'est pas voulu"
+            return 25,"<span class="error-state">Votre test pass pour la deuxi&eacute;me version ce qui n'est pas voulu </span>"
 
-        return 100, "Bravo !"
+        return 100, "<span class="success-state">Bravo ! Bientôt tester pro.</span>"
 
     def runf1test(self):
         """
