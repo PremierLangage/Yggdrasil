287cf9a46305f9eeefbd70d2089905b0384e1345
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-17 19:29:22 +0100

    ddoyen1 modified Yggdrasil/model/multistep.pl

--- model/multistep.pl
+++ model/multistep.pl
@@ -1,8 +1,8 @@
 @ /utils/sandboxio.py
 @ /utils/components/scoring.py
-@ /utils/components/radio.py [customradio.py]
-@ /utils/components/checkbox.py [customcheckbox.py]
-@ /utils/components/textselect.py [customtextselect.py]
+@ /utils/components/radio.py
+@ /utils/components/checkbox.py
+@ /utils/components/textselect.py
 
 
 #extends = temp.pl
