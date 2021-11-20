d900d8c3d558700d7339f47af9456696f2302989
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-20 13:15:33 +0100

    ddoyen1 modified Yggdrasil/model/quiz/quiz.pl

--- model/quiz/quiz.pl
+++ model/quiz/quiz.pl
@@ -24,3 +24,36 @@ text =
 tplpage =@ template.html
 
 style.basic =@ /demo/css/basic.html
+
+before == #|python|
+import random as rd
+from radio import Radio
+from checkbox import Checkbox
+from AMC import parse_AMC_TXT
+
+list_questions = parse_AMC_TXT(questions)
+
+if 'nbstep' in globals():
+    list_questions = rd.sample(list_questions, nbstep)
+else:
+    nbstep = len(list_questions)
+
+comp = []
+statement  = []
+
+for i, q in enumerate(list_questions):
+    if q['type'] == "Radio":
+        comp.append(Radio())
+        comp[i].set_items(q['items'])
+        comp[i].set_sol(q['index'])
+    elif q['type'] == "Checkbox":
+        comp.append(Checkbox())
+        comp[i].set_items(q['items'])
+        comp[i].set_sol(q['index'])
+    elif  q['type'] == 'TextSelect':
+        # comp.append(CustomTextSelect())
+        continue # no implemented yet 
+    statement.append(q['text'])
+    if 'ordered' not in q['options']:
+        comp[i].shuffle()
+==
