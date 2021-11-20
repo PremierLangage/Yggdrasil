579936ca615f3813bb24ec82939ad0e1df3a4af8
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-20 13:02:45 +0100

    ddoyen1 modified Yggdrasil/model/quiz/quiz.pl

---
+++ model/quiz/quiz.pl
@@ -0,0 +1,26 @@
+extends = /model/basic/temp.pl
+
+@ /builder/before1.py [builder.py]
+@ /grader/evaluator1.py [grader.py]
+@ /utils/builderlib.py
+@ /utils/plrandom.py
+@ /utils/plcsv.py
+@ /model/basic/jinja_basic.py [jinja_env.py]
+@ /model/basic/json_basic.py [json_encoder.py]
+
+before_scripts % ["importfunc", "before", "process"]
+eval_scripts % ["getans", "evaluator", "evalprocess"]
+
+importfunc == #|python|
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from plcsv import csv_choice, csv_sample, csv_col
+==
+
+title =
+form = 
+text = 
+
+tplpage =@ template.html
+
+style.basic =@ /demo/css/basic.html
