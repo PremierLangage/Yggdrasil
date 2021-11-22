c42a5998afecf6a36f92e9b916d089b3ed43d858
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2021-11-22 16:20:07 +0100

    tsaillard1 modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -11,5 +11,5 @@ Il est difficile de représenter [la nuit] [en couleurs].
 Mais comment peindre sans lampadaire ? 
 {Le peintre} a eu pour cela [une idée lumineuse]. 
 On dit qu'il a planté [des bougies] sur [le rebord] de [son chapeau]. 
-{L'homme} a ainsi pu peindre grâce [aux minuscules flammes].
+{L'homme} a ainsi pu peindre grâce [aux m{inus}cules flammes].
 ==
