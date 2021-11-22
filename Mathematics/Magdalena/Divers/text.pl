d6237ed6733ef047f97d368f407dead486989bde
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-11-22 09:28:45 +0100

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Divers/text.pl

--- Mathematics/Magdalena/Divers/text.pl
+++ Mathematics/Magdalena/Divers/text.pl
@@ -7,12 +7,12 @@ Sélectionner dans ce texte les mots qui désignent Van Gogh
 ==
 
 seltext == 
-En 1888, #Vincent Van Gogh# peint {«Nuit Etoilée»}. 
-Il est difficile de représenter {la nuit} {en couleurs}.
-#L'artiste# installait {son chevalet} lorsque {la nuit} tombait.
+En 1888, Vincent Van Gogh #peint# {«Nuit Etoilée»}. 
+Il #est# difficile de représenter {la nuit} {en couleurs}.
+L'artiste #installait# {son chevalet} lorsque {la nuit} #tombait#.
 Mais comment peindre sans lampadaire ? 
-#Le peintre# a eu pour cela {une idée lumineuse}. 
-On dit qu'#il# a planté {des bougies} sur {le rebord} de {son chapeau}. 
-#L'homme# a ainsi pu peindre grâce {aux minuscules flammes}
+Le peintre #a eu# pour cela {une idée lumineuse}. 
+On #dit# qu'il #a planté# {des bougies} sur {le rebord} de {son chapeau}. 
+L'homme #a ainsi pu# peindre grâce {aux minuscules flammes}.
 ==
 
