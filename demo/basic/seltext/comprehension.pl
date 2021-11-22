502cd76e83ebc1ad51590cfcb1a09528653baf52
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2021-11-22 16:29:07 +0100

    adiaw2 modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -11,6 +11,6 @@ Il est difficile de représenter [la nuit] [en couleurs].
 Mais comment peindre sans lampadaire ? 
 {Le peintre} a eu pour cela [une idée lumineuse]. 
 On dit qu'{il} a planté [des bougies] sur [le rebord] de [son chapeau]. 
-{L'homme} a ainsi pu peindre grâce [aux minuscules flammes]
+{L'homme} a ainsi pu peindre grâce [aux minuscules flammes].
 Mon super test pour les \{accolades} et les \[crochets].
 ==
