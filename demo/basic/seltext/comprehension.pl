5afa50a4fb0fd3065d0dd3aa4823b5f3e019f733
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2021-11-22 16:28:23 +0100

    adiaw2 modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -10,6 +10,7 @@ Il est difficile de représenter [la nuit] [en couleurs].
 {L'artiste} installait [son chevalet] lorsque [la nuit] tombait.
 Mais comment peindre sans lampadaire ? 
 {Le peintre} a eu pour cela [une idée lumineuse]. 
-On dit qu'il a planté [des bougies] sur [le rebord] de [son chapeau]. 
-{L'homme} a ainsi pu peindre grâce [aux minuscules flammes].
+On dit qu'{il} a planté [des bougies] sur [le rebord] de [son chapeau]. 
+{L'homme} a ainsi pu peindre grâce [aux minuscules flammes]
+Mon super test pour les \{accolades\} et les \[crochets\].
 ==
