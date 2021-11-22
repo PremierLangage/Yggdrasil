a2952d19507b740f672b153a008d77f1e8ef61e2
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-22 12:29:18 +0100

    ddoyen1 modified Yggdrasil/model/math/input2.pl

--- model/math/input2.pl
+++ model/math/input2.pl
@@ -208,18 +208,8 @@ function onBeforeSubmitPL() {
 
 extends = messages_math.pl
 
-extends = /model/math/input.pl
-
 question ==
 Entrer une expression mathématique.
 ==
 
-evaluator ==
-score = -1
-feedback = answers['math']
-solution = answers['math']
-==
-
-embed ==
-# x + 1 #
-==
+keypad = ["sqrt", "infty", "emptyset"]
