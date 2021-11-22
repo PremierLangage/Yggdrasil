6cf36ba94139db5984c3a2e1f524760cb58e795a
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2021-11-22 14:21:23 +0100

    adiaw2 modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -5,9 +5,9 @@ Sélectionner dans ce texte les mots ou groupes de mots qui désignent Van Gogh.
 ==
 
 seltext == 
-En 1888, {Vincent Van Gogh} peint [{«Nuit Etoilée»]. 
+En 1888, {Vincent Van Gogh} peint [{«Nuit Etoilée»}]. 
 Il est difficile de représenter [la nuit] [en couleurs].
-{L'artiste} installait [son chevalet] lorsque [la nuit] tombait.
+{L'artiste} installait [son {chevalet] lorsque [la nuit] tombait.
 Mais comment peindre sans lampadaire ? 
 {Le peintre} a eu pour cela [une idée lumineuse]. 
 On dit qu'il a planté [des bougies] sur [le rebord] de [son chapeau]. 
