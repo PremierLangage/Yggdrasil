10eb62d6f31cf681eb95cd318bbce365a269530e
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-11-22 10:13:44 +0100

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/good-bad.pl

---
+++ Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/good-bad.pl
@@ -0,0 +1,13 @@
+extends = /model/checkbox/goodbad.pl
+title= goodbad
+text= Votre énoncé comme d'habitude
+good==# une chaine multiple avec une bonne solution par ligne 
+bonne1
+bonne2
+bonne3
+==
+bad==
+mauvaise 1 
+mauvaise 2
+mauvaise 3
+==
