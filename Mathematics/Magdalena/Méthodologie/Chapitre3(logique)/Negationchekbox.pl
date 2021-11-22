3cdc0b79aacbc69645768d6056f98fa920423632
Magdalena KOBYLANSKI <magdalena.kobylanski@univ-eiffel.fr>
2021-11-22 10:10:39 +0100

    mkobylanski1 modified Yggdrasil/Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/Negationchekbox.pl

--- Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/Negationchekbox.pl
+++ Mathematics/Magdalena/Méthodologie/Chapitre3(logique)/Negationchekbox.pl
@@ -1,4 +1,4 @@
-extends = /model/basic/vraifaux.pl
+extends = /model/checkbox/goodbad.pl
 text= Soit $%P,Q,R%$ des propositions. Parmi les énoncés suivants lequels correspondent à la négation de $% [P \mbox{ et } (\mbox{non}Q  \mbox{ ou } R) ]%$ 
 good==# une chaine multiple avec une bonne solution par ligne 
 $% \mbox{non}P \mbox{ ou } (Q  \mbox{ et } \mbox{non}R) %$
