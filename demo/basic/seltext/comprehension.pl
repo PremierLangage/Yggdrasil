e1da3c610b4e2aa0f3eea64386e4f0bc33926ca9
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2021-11-22 16:18:34 +0100

    tsaillard1 modified Yggdrasil/demo/basic/seltext/comprehension.pl

--- demo/basic/seltext/comprehension.pl
+++ demo/basic/seltext/comprehension.pl
@@ -9,7 +9,7 @@ En 1888, {Vincent Van Gogh} peint [«Nuit Etoilée»].
 Il est difficile de représenter [la nuit] [en couleurs].
 {L'artiste} installait [son {chevalet}] lorsque [la nuit] tombait.
 Mais comment peindre sans lampadaire ? 
-{Le peintre} a {eu [{pour}] cela} [une idée lumineuse]. 
+{Le \{}peintre} a eu pour cela [une idée lumineuse]. 
 On dit qu'il a planté [des bougies] sur [le rebord] de [son chapeau]. 
 {L'homme} a ainsi pu peindre grâce [aux minuscules flammes].
 ==
