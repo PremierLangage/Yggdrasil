0d4190298f6a924ee35b125e68b3686feafde52f
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-25 00:08:40 +0100

    ddoyen1 modified Yggdrasil/model/math/input2.pl

--- model/math/input2.pl
+++ model/math/input2.pl
@@ -144,7 +144,7 @@ javascript.mathquill2 ==
 <script>
 function onBeforeSubmitPL() {
   // copie les valeurs des champs MathField dans des éléments input
-  window.alert('ggg');
+  window.alert(String(Object.keys(setOfMathFields).length));
   Object.values(setOfMathFields).forEach(function(mathField) {
     var mathFieldInput = document.getElementById('form_'+mathField.el().id);
     if (mathField instanceof MQ.MathField) {
