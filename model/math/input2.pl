ae8cdbbf6a34471bdeb4c63da303663241748a25
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-25 10:11:42 +0100

    ddoyen1 modified Yggdrasil/model/math/input2.pl

--- model/math/input2.pl
+++ model/math/input2.pl
@@ -155,7 +155,6 @@ function onBeforeSubmitPL() {
     var mathFieldInput = document.getElementById('form_'+mathField.el().id);
       mathFieldInput.value = mathField.latex();
       mathFieldInput.value = "toto";
-      window.alert(String(mathField.el().id));
     if (mathField instanceof MQ.StaticMath) {
       if (mathField.innerFields.length == 1) {
       mathFieldInput.value = mathField.innerFields[0].latex();
