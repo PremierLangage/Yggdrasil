f06795341f752b8c22830c0a2e22cb5cfaa78b3e
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-25 10:08:46 +0100

    ddoyen1 modified Yggdrasil/model/math/jinja_math.py

--- model/math/jinja_math.py
+++ model/math/jinja_math.py
@@ -55,10 +55,6 @@ if (typeof setOfMathFields == "undefined") {
    setOfMathFields = {};
 }
 setOfMathFields.{{ id }} = mathField_{{ id }}
-if (typeof names == "undefined") {
-   names = "";
-}
-names = names + "mathField_{{ id }}"
 </script>
 """
     return Template(html).render(id=id, prefix=prefix, embed ="", keypad=keypad, value=value)
