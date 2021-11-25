7fb3364d3760e7d7b7e70a237a54ab1c65b0c57e
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-25 16:06:04 +0100

    ddoyen1 modified Yggdrasil/grader/evaluator1.py

--- grader/evaluator1.py
+++ grader/evaluator1.py
@@ -41,6 +41,13 @@ if __name__ == "__main__":
     feedback = Env.from_string(feedback).render(dic)
     ffeedback = ""
 
+    for key in dic.get('jinja_keys', ['question', 'solution']):
+        if key in dic:
+            key2 = f"_{key}_"
+            if isinstance(dic[key] , str):
+                dic[key] = dic[key2].replace("{% raw %}", "")
+                dic[key] = dic[key].replace("{% endraw %}", "")
+                
 
     #dic['question'] = "Coucou !"
     # dic = json.loads(json.dumps(dic, cls=JSONEncoder))
