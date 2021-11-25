7ee07d273561816da7f112982cc83272c061bbb0
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-25 15:10:18 +0100

    ddoyen1 modified Yggdrasil/builder/before1.py

--- builder/before1.py
+++ builder/before1.py
@@ -72,14 +72,14 @@ if __name__ == "__main__":
     for key in dic.get('jinja_keys', ['question', 'solution']):
         if key in dic:
             key2 = f"_{key}_"
-            dic[key2] = dic[key]
+            dic[key2] = "{% raw %}" + dic[key] + "{% endraw %}"
             if isinstance(dic[key] , str):
                 dic[key] = Env.from_string(macros + dic[key]).render(dic)
             elif isinstance(dic[key] , dict):
                 for k in dic[key]:
                     dic[key][k] = Env.from_string(macros + dic[key][k]).render(dic)
 
-    #dic['form'] = dic['tplpage']
+    dic['form'] = dic['tplpage']
 
     with open(outputfilename, "w+") as f:
         json.dump(dic, f, cls=JSONEncoder)
