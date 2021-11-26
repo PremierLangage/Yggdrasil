c72a1311a5faf56944a393fa4f64cd35a5cb9ea5
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-26 15:45:57 +0100

    ddoyen1 modified Yggdrasil/grader/evaluator1.py

--- grader/evaluator1.py
+++ grader/evaluator1.py
@@ -11,7 +11,8 @@ from json_encoder import JSONEncoder, JSONDecoder
 if __name__ == "__main__":
 
     with open(sys.argv[1], "r") as f:
-        dic = json.load(f, cls=JSONDecoder)
+        dic = json.load(f)
+        #dic = json.load(f, cls=JSONDecoder)
     Component.sync_context(dic)
 
     with open(sys.argv[2], "r") as f:
