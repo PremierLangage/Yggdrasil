f1e646ec0483530adf3104002a8ef6ad1cae4517
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-26 10:55:05 +0100

    ddoyen1 modified Yggdrasil/grader/evaluator1.py

--- grader/evaluator1.py
+++ grader/evaluator1.py
@@ -47,9 +47,9 @@ if __name__ == "__main__":
         if key in dic:
             key2 = f"_{key}_"
             if isinstance(dic[key] , str):
-                dic[key] = dic[key2].replace(r"{% raw %}", "")
-                dic[key] = dic[key].replace(r"{% endraw %}", "")
-                dic[key] = "" #Env.from_string(macros + dic[key]).render(dic)
+                #dic[key] = dic[key2].replace(r"{% raw %}", "")
+                #dic[key] = dic[key].replace(r"{% endraw %}", "")
+                dic[key] = " " #Env.from_string(macros + dic[key]).render(dic)
 
 
     # dic = json.loads(json.dumps(dic, cls=JSONEncoder))
