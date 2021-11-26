74180ab385d6eb4087600472d8517755654c250e
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-26 15:17:28 +0100

    ddoyen1 modified Yggdrasil/utils/components/mathinput.py

--- utils/components/mathinput.py
+++ utils/components/mathinput.py
@@ -1,8 +1,10 @@
+from uuid import uuid4
 
 class MathInput():
 
     def __init__(self, **kwargs):
         self.__MathInput__ = True
+        self.id = str(uuid4())
         self.keypad = []
         self.value = ""
         self.prefix = ""
