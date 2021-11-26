17f8b6a80ff6447fdabe1d95b4bf998d2701b918
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-26 14:09:54 +0100

    ddoyen1 modified Yggdrasil/utils/components/mathinput.py

--- utils/components/mathinput.py
+++ utils/components/mathinput.py
@@ -6,12 +6,6 @@ class MathInput():
         self.keypad = []
         self.value = ""
         self.prefix = ""
-        if 'inlineShortcuts' in kwargs:
-            keyboards_JSON['inlineShortcuts'] = kwargs['inlineShortcuts']
-        if 'virtualKeyboards' in kwargs:
-            keyboards_JSON['virtualKeyboards'] = kwargs['virtualKeyboards']
-        self.config = keyboards_JSON
-        super().__init__(**kwargs)
 
 
 
