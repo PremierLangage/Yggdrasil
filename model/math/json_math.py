cf734fe20ecda2c6dc64fff39fbcbb7bfd4e2c99
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-26 14:04:07 +0100

    ddoyen1 modified Yggdrasil/model/math/json_math.py

--- model/math/json_math.py
+++ model/math/json_math.py
@@ -1,11 +1,14 @@
 import json, jsonpickle
 from sympy import srepr, Basic, Matrix, sympify
+from mathinput import MathInput
 
 class JSONEncoder(json.JSONEncoder):
 
     def default(self, obj):
         if isinstance(obj, (Basic, Matrix)):
             return {'__SymPy__': True, 'srepr': srepr(obj)}
+        if isinstance(obj, MathInput):
+            return vars(obj)
         return jsonpickle.Pickler(unpicklable=False).flatten(obj)
 
 class JSONDecoder(json.JSONDecoder):
@@ -13,7 +16,9 @@ class JSONDecoder(json.JSONDecoder):
     def __init__(self, *args, **kwargs):
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
 
-    def object_hook(self, dict):
-        if '__SymPy__' in dict:
+    def object_hook(self, dic):
+        if '__SymPy__' in dic:
             return sympify(dict['srepr'], evaluate=False)
+        if '__MathInput__' in dic:
+            return MathInput(dic)
         return dict
