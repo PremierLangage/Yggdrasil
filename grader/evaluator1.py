531839cf7d2e6c48a774db24e9cd159aeebe362c
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-26 12:49:22 +0100

    ddoyen1 modified Yggdrasil/grader/evaluator1.py

--- grader/evaluator1.py
+++ grader/evaluator1.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
                 try:
                     dic[key] = Env.from_string(dic[keyk]).render(dic)
                 except:
-                    dic[key] = ""
+                    dic[key] = "bug"
 
 
     # dic = json.loads(json.dumps(dic, cls=JSONEncoder))
