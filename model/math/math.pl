be3adaaca09f0a4f0608554503cfa3f1d9618bce
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-27 19:56:50 +0100

    ddoyen1 modified Yggdrasil/model/math/math.pl

--- model/math/math.pl
+++ model/math/math.pl
@@ -5,6 +5,7 @@
 @ /model/math/jinja_math.py [jinja_env.py]
 @ /model/math/json_math.py [json_encoder.py]
 @ /utils/components/mathinput/mathinput.py
+@ /utils/components/mathinput/mathinput.html
 @ /utils/sympy/evalsympy.py
 @ /utils/sympy/latex2sympy.py
 @ /utils/sympy/sympy2latex.py
