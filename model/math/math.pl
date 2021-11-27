77178ed18f5dd0cb9d601c5f33b95c965011b722
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-27 21:42:30 +0100

    ddoyen1 modified Yggdrasil/model/math/math.pl

--- model/math/math.pl
+++ model/math/math.pl
@@ -6,8 +6,6 @@
 @ /model/math/json_math.py [json_encoder.py]
 @ /utils/components/mathinput/mathinput.py
 @ /utils/components/mathinput/mathinput.html
-@ /utils/components/mathembed/mathembed.py
-@ /utils/components/mathembed/mathembed.html
 @ /utils/sympy/evalsympy.py
 @ /utils/sympy/latex2sympy.py
 @ /utils/sympy/sympy2latex.py
