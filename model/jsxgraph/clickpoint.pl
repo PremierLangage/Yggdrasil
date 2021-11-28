380ccef55c8b321bce9f80531497b0990f576de8
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-28 14:33:27 +0100

    ddoyen1 modified Yggdrasil/model/jsxgraph/clickpoint.pl

--- model/jsxgraph/clickpoint.pl
+++ model/jsxgraph/clickpoint.pl
@@ -4,9 +4,17 @@ ptname = M
 
 tol = 0.1
 
-script ==
-var ax1 = board.create('line', [[0,0],[1,0]]);
-board.create('ticks',[ax1, 5], {minorTicks:4, majorHeight:10, minorHeight:4});
+jxg.attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25]}
+
+script_init ==
+board.create('grid', [], {gridX: 0.25, gridY: 0.25});
+board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
+board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
+var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
+var O = board.create('point', [0,0],{size:1, name: 'O', color: 'black', fixed: true});
+var A = board.create('point', [1,0],{size:1, name: 'A', color: 'black', fixed: true});
+var M = board.create('glider', [1, 0.5, circle],{name:'M',color:'blue',fixed:false});
+board.create('sector', [O, A, M], {color: 'orange'});
 ==
 
 script_aux ==
