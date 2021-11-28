96f26689d5125150805da807b21a59725bc0589f
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-28 15:01:24 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/test.pl

--- Mathematics/exercises/elementary_mathematics/numbers/test.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/test.pl
@@ -1,38 +1,30 @@
-extends = /model/math/expr.pl
-jxg =: MathDrawer
-@ /utils/components/jsxgraph.py [customjsxgraph.py]
-jxg.decorator = CustomJSXGraph
-
-param.lstangle = [pi/4,pi/2,3*pi/4,pi,5*pi/4,3*pi/2,7*pi/4]
+extends = /model/jsxgraph/clickpoint.pl
 
 before ==
-lstangle=eval(param['lstangle'])
-angle=choice(lstangle)
-cosangle=cos(angle).evalf()
-sinangle=sin(angle).evalf()
-xM=float(cosangle)
-yM=float(sinangle)
-sol = angle
-jxg.setscript(script, globals())
+lstangle = eval(param['lstangle'])
+angle = choice(lstangle)
+valangle = float(angle.evalf())
+xsol = float(cos(valangle))
+ysol = float(sin(valangle))
+jxg.setscript(script_init)
 ==
 
-jxg.attributes % {"boundingbox": [-1.25, 1.25, 1.25, -1.25], "showNavigation": false, "axis": false, "grid": false}
+jxg.attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25]}
 
-script ==
-var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
-var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
-var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
+name = M
+
+script_init ==
+board.create('grid', [], {gridX: 0.25, gridY: 0.25});
+board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
+board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
 var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
-var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
-var M = board.create('point',[ {{xM}} , {{yM}} ],{size:1,name:'M',color:'black',fixed:true});
-var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+var O = board.create('point', [0,0],{size:1, name: 'O', color: 'black', fixed: true});
+var A = board.create('point', [1,0],{size:1, name: 'A', color: 'black', fixed: true});
+var M = board.create('glider', [1, 0.5, circle],{name:'M',color:'blue',fixed:false});
+board.create('sector', [O, A, M], {color: 'orange'});
 ==
 
-question ==
-Quelle est la mesure (en radians) de l'angle $% (\overrightarrow{OA},\overrightarrow{OM}) %$ représenté ci-dessous. 
-<div class="img">
-{{ jxg|component }}
-</div>
-==
 
+script_solution ==
+board.create('point',[{{xsol}}, {{ysol}}],{name:'', color:'green'});
+==
