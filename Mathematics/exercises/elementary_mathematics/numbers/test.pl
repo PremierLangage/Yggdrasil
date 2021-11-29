46a2d5e0e52830af7b21eaafda81b6e4c4adc97b
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-29 13:10:58 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/test.pl

--- Mathematics/exercises/elementary_mathematics/numbers/test.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/test.pl
@@ -11,7 +11,7 @@ jxg.attributes % {"showNavigation":false, "boundingbox":[-0.25,0.25,3.25,-0.25],
 name = M
 
 script_init ==
-var ax = board.create('line', [[0,0],[0,1]]);
+var ax = board.create('line', [[0,0],[1,0]]);
 board.create('ticks',[ax, 5], {minorTicks:4, majorHeight:10, minorHeight:4});
 var M = board.create('glider', [1., 0., ax],{name:'M',color:'blue',fixed:false});
 
