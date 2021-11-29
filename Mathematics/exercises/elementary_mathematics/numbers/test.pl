3254a2ef5a4d2a2eb2b1e959c9ed3b984117d539
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-29 13:33:35 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/test.pl

--- Mathematics/exercises/elementary_mathematics/numbers/test.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/test.pl
@@ -14,7 +14,7 @@ question ==
 <script type="text/javascript" src="https://jsxgraph.uni-bayreuth.de/distrib/jsxgraphcore.js"></script>
 <div id="jxgbox2" class="jxgbox" style="width:500px; height:100px;"></div>
 <script type="text/javascript">
-var board2 = JXG.JSXGraph.initBoard('jxgbox', {boundingbox:[-1,4,5,-4], keepaspectratio: true, grid:true});
+var board2 = JXG.JSXGraph.initBoard('jxgbox2', {boundingbox:[-1,4,5,-4], keepaspectratio: true, grid:true});
 var ax = board2.create('line', [[0,0],[1,0]]);
 board2.create('ticks',[ax, 5], {fixed:true, minorTicks:4, majorHeight:10, minorHeight:4});
 var M = board2.create('glider', [1., 0., ax], {name:'M',color:'blue',fixed:false});
