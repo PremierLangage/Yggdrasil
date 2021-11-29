0aa0a14e97ff55f7fc92c1eafb6723cfe525781c
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2021-11-29 10:26:41 +0100

    adiaw2 modified Yggdrasil/model/AMC2/QCM-AMCE.pl

--- model/AMC2/QCM-AMCE.pl
+++ model/AMC2/QCM-AMCE.pl
@@ -113,25 +113,7 @@ statement  = []
 rd.shuffle(list_questions)
 for i, q in enumerate(list_questions):
     q=buildquestion(q) # Gestion de l'aléa 
-    if q['type'] == "Radio":
-        comp.append(Radio())
-        statement.append(q['text'])
-        comp[i].set_items(q['items'])
-        comp[i].set_sol(q['index'])
-        if 'ordered' not in q['options']:
-            comp[i].shuffle()
-    elif q['type'] == "Checkbox":
-        comp.append(Checkbox())
-        statement.append(q['text'])
-        comp[i].set_items(q['items'])
-        comp[i].set_sol(q['index'])
-        if 'ordered' not in q['options']:
-            comp[i].shuffle()
-    elif  q['type'] == 'TextSelect':
-        cst = CustomTextSelect()
-        statement.append(q['text'])
-        cst.setdata_from_textDR(q['items'][0])
-        comp.append(cst)
+    print(q)
 ==
 
 display=
