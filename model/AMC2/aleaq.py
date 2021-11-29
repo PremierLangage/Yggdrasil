fad37fc14ee4f844c588db1c7e6d14e76e3822c9
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2021-11-29 10:18:14 +0100

    adiaw2 modified Yggdrasil/model/AMC2/aleaq.py

--- model/AMC2/aleaq.py
+++ model/AMC2/aleaq.py
@@ -76,7 +76,7 @@ def buildquestion(questionp):
     """
     #if question.get('extended') == False:
     #    return question
-    question=defaultdict(list)
+    question=dict(questionp)
     try:
         
         d=optiondic(question.get('options'))
