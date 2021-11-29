7395416d1ca5420eb6d5717c435831b1f3295c2f
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-29 13:10:41 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/test.pl

--- Mathematics/exercises/elementary_mathematics/numbers/test.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/test.pl
@@ -6,12 +6,12 @@ ysol = 0
 jxg.setscript(script_init+script_aux)
 ==
 
-jxg.attributes % {"showNavigation":false, "boundingbox":[-1.25,1.25,1.25,-1.25], "axis":false}
+jxg.attributes % {"showNavigation":false, "boundingbox":[-0.25,0.25,3.25,-0.25], "axis":false}
 
 name = M
 
 script_init ==
-var ax = board.create('line', [[0,0],[3,1]]);
+var ax = board.create('line', [[0,0],[0,1]]);
 board.create('ticks',[ax, 5], {minorTicks:4, majorHeight:10, minorHeight:4});
 var M = board.create('glider', [1., 0., ax],{name:'M',color:'blue',fixed:false});
 
