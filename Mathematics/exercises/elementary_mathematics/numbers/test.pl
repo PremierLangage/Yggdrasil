6711b4ab295c4d5cdc90c80f3a9c615f00400cfe
David DOYEN <david.doyen@univ-eiffel.fr>
2021-11-29 15:51:40 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/test.pl

--- Mathematics/exercises/elementary_mathematics/numbers/test.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/test.pl
@@ -15,23 +15,11 @@ b = randint(1, c-1)
 jxg.attributes =  {"showNavigation":False, "boundingbox":[-0.5,4,n+0.5,-4], "axis":False, "keepAspectRatio":False}
 jxg.setscript(script_init + "\n"+ script_aux, locals())
 ==
-jxg3.attributes % {"showNavigation":false, "boundingbox":[-0.5,4,5.5,-4], "axis":false, "keepAspectRatio":false}
-
 
 name = M
+
 question ==
 Place le point $! M !$ d'abscisse $! \displaystyle \frac{ {{b}} }{ {{c}} } !$.
-==
-question2 ==
-<link rel="stylesheet" type="text/css" href="https://jsxgraph.uni-bayreuth.de/distrib/jsxgraph.css" />
-<script type="text/javascript" src="https://jsxgraph.uni-bayreuth.de/distrib/jsxgraphcore.js"></script>
-<div id="jxgbox2" class="jxgbox" style="width:100%; height:100px;"></div>
-<script type="text/javascript">
-var board2 = JXG.JSXGraph.initBoard('jxgbox2', {showNavigation:false, boundingbox:[-0.5,4,5.5,-4], grid:false});
-var ax = board2.create('line', [[0,0],[1,0]], {fixed:true});
-board2.create('ticks',[ax, 1], {label: {anchorX: 'middle', anchorY: 'top', fontSize: 12, offset: [0, 30]}, anchor : 0., drawZero: true, drawLabels:true, minorTicks:2, majorHeight:20, minorHeight:10});
-var M = board2.create('glider', [1., 0., ax], {name:'M',color:'blue',fixed:false});
-</script>
 ==
 
 
