c62218395d964717a947f63c73f9b0baae64a6eb
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-04 22:35:47 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl

---
+++ Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl
@@ -0,0 +1,20 @@
+extends = /model/math/expr.pl
+
+before ==
+a = randint(1, 3)
+c = randint(2, 6)
+b = randint(1, c-1)
+f = a + Rat(b, c)
+==
+
+question ==
+Ecrire l'expression suivante sous la forme  $% \sqrt{a} %$, où $% a %$ est un entier.
+==
+
+prefix ==
+$! {{ f|latex }} = !$
+==
+
+embed ==
+# + \frac{ # }{{{ c }}}
+==
