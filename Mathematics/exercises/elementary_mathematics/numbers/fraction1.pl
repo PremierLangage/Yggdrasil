3f704afd696fa3312b90ee148710d2f0f617e7aa
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-04 22:44:23 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl

--- Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl
@@ -4,7 +4,8 @@ before ==
 a = randint(1, 3)
 c = randint(2, 6)
 b = randint(1, c-1)
-f = a + Rational(b, c)
+r = Rational(b, c)
+f = a + r
 sol = 1
 ==
 
@@ -18,4 +19,18 @@ $! \displaystyle {{ f|latex }} = !$
 
 embed ==
 # + \frac{ # }{ {{c}} }
+==
+
+evaluator ==
+aa, bb = answers['math']
+if str(a) == aa and str(b) == bb:
+    score = 100
+else:
+    score = 0
+score, error = eval_expr(answers['math'], sol, checkratsimp=checkratsimp,equality=equality, unauthorized_func=unauthorized_func, modulo=modulo, local_dict=sympify(symbol_dict))
+feedback = ""
+==
+
+embed ==
+$$ {{f|latex}} = {{a|latex}} + {{ r|latex }} $$
 ==
