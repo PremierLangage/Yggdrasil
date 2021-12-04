745557d1f23986a9f2c8198195ff9fa4f18b429f
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-04 22:39:02 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl

--- Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/fraction1.pl
@@ -5,13 +5,16 @@ a = randint(1, 3)
 c = randint(2, 6)
 b = randint(1, c-1)
 f = a + Rational(b, c)
+sol = 1
 ==
 
 question ==
 Ecrire l'expression suivante sous la forme .
 ==
 
-
+prefix ==
+$! {{ f|latex }} = !$
+==
 
 embed ==
 \frac{1}{1+\sqrt{#}}
