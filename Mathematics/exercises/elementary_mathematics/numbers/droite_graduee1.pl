ad48059f15dccffdd8dfea74acbb18b0c4e3ea2d
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-04 23:11:43 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/droite_graduee1.pl

--- Mathematics/exercises/elementary_mathematics/numbers/droite_graduee1.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/droite_graduee1.pl
@@ -18,34 +18,6 @@ jxg.attributes =  {"showNavigation":False, "boundingbox":[-0.2,4,n+0.2,-4], "axi
 jxg.setscript(script_init + "\n"+ script_aux, locals())
 ==
 
-name = M
-
 question ==
 Place le point $! M !$ d'abscisse $! \displaystyle \frac{ {{b}} }{ {{c}} } !$.
-==
-
-
-script_init ==
-var ax = board.create('line', [[0,0],[1,0]], {fixed:true});
-board.create('ticks',[ax, 1], {label: {anchorX: 'middle', anchorY: 'top', fontSize: 12, offset: [0, 30]}, anchor : 0., drawZero: true, drawLabels:true, minorTicks:{{ k }}, majorHeight:20, minorHeight:10});
-var M = board.create('glider', [0., 0., ax], {name:'M',color:'blue',fixed:false, label: {anchorX: 'middle', anchorY: 'bottom', fontSize: 12, offset: [0, -30]}});
-==
-
-script_solution ==
-board.create('point',[{{xsol}}, {{ysol}}],{name:'', color:'green'});
-==
-
-inputblock ==
-{{ jxg|component }}
-==
-
-style.jxgcss ==
-<style>
-.jsxgraph-component {
-    width: 500px;
-    max-width: 100%;
-    height: 100px;
-    margin: 0 auto;
-}
-</style>
 ==
