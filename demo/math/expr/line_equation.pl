baceba3096d0e92a3096118d10911fda8c5a8820
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 00:17:48 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -8,21 +8,10 @@ var('x')
 a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
 b = randint(-3, 3)
 sol = a*x + b
-xlim = (-5, 5)
-ylim = (-5, 5)
-fig = plot2fig(sol, x,-5,5)
-ax = fig.gca()
-ax.grid(True)
-ax.set_xlim(*xlim)
-ax.set_ylim(*ylim)
-ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
-ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
-#ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-#ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-ax.set_xlabel('')
-ax.set_ylabel('')
-image = fig2svg(fig)
-#image = plotsvg(sol)
+p = plot(sol, x,-5,5)
+p.append(plot(x,x,-3,1))
+
+image = plot2svg(p)
 ==
 
 question ==
