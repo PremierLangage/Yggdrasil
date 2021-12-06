15558b7bff45f23210384ca86cf8937956678137
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-06 20:57:20 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/intialisation2021.pl

---
+++ ComputerScience/python/AP1-2122/Remediation/intialisation2021.pl
@@ -0,0 +1,55 @@
+
+
+# DR: test 24/09/2019 OK
+extends= /ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+tag=Programmation|Python|Initialisation|Variable
+author = CC + DR
+
+title = Initialisation et constantes
+
+
+text==
+ Initialisez la variable **{{var}}** avec une valeur quelconque
+ de type **{{type_var}}** 
+==
+
+
+
+editor.height = 80
+
+before== #|python| 
+import random 
+random.seed(seed)
+type_var = random.choice(["int","str","float","bool",None])
+var = random.choice(["a", "b", "c", "X", "Y","PIERRE","feuille","ciseaux"])
+
+pltest0= f"""
+>>> "{var}" in globals() # La variable {var} est elle définie ?
+True
+"""
+
+if type_var is None:
+    pltest1= f"""
+>>> type({var}) is type(None) # La variable {var} est elle du type {type_var} ?
+True
+"""
+else:
+    pltest1= f"""
+>>> type({var})=={type_var} # La variables est elle du bon type ({type_var}) ?
+True
+"""
+
+
+
+
+==
+
+
+
+
+
+
+
+
