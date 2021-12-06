98c6568a4b5e54db4cc0041da8880fb3524ce64f
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 00:43:51 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -1,14 +1,14 @@
 extends = /model/math/expr.pl
 
 before ==
-from mplsympy import plotsvg, fig2svg, plot2svg
+from mplsympy import plotsvg, fig2svg, plot2svg, plot2
 from sympy.plotting import plot
 import matplotlib.ticker as ticker
 var('x')
 a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
 b = randint(-3, 3)
 sol = a*x + b
-p = plot(sol, (x,-5,5))
+p = plot2(sol, (x,-5,5))
 #p.append(plot(x,x,-3,1)[0])
 #image = plot2svg(0)
 ==
