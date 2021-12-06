fb295eef3ab1b25af2a927da209aa35bd4b13e8d
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-06 21:27:43 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/pow.pl

---
+++ ComputerScience/python/AP1-2122/Remediation/pow.pl
@@ -0,0 +1,50 @@
+
+
+
+
+
+author= DR
+
+
+title=Une fonction 
+
+tag=function # N'oubliez pas de remplir ce champs svp
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+
+text==
+Ecrivez une fonction pow qui  prend deux paramêtres $$x , y$$ et retourne $$x^y$$ 
+==
+before==
+import random 
+fname,function, lamb = random.choice([
+["pow","s", lambda x,y:x**y],
+])
+
+
+
+
+p=random.randint(23,77)
+pltest0= f""">>> {fname}({p},5)\n{lamb(p,5)}\n"""
+p+=17
+pltest1= f""">>> {fname}({p},4) - {lamb(p,4)} <0.1 # Hidden value test\nTrue\n"""
+
+==
+
+
+
+editor.code==
+
+==
+editor.height=145
+
+
+
+
+
+
+
+
+
+
