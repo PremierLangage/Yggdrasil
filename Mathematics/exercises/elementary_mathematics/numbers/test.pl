61f0efd2819f2a8e4461bd658bbcbbd80e29e4ce
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-06 13:54:21 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/elementary_mathematics/numbers/test.pl

--- Mathematics/exercises/elementary_mathematics/numbers/test.pl
+++ Mathematics/exercises/elementary_mathematics/numbers/test.pl
@@ -1,51 +1,19 @@
-extends = /model/jsxgraph/clickpoint.pl
-
-before ==
-n = 1
-k = choice([3, 5, 5, 7, 7])
-if k == 3:
-    c = choice([4, 4, 2])
-elif k == 5:
-    c = choice([6, 6, 3, 3, 3, 2])
-elif k == 7:
-    c = choice([8, 8, 4, 4, 4, 2])
-b = randint(1, c-1)
-
-xsol = b/c
-ysol = 0
-
-jxg.attributes =  {"showNavigation":False, "boundingbox":[-0.2,4,n+0.2,-4], "axis":False, "keepAspectRatio":False}
-jxg.setscript(script_init + "\n"+ script_aux, locals())
-==
-
-name = M
-
-question ==
-Place le point $! M !$ d'abscisse $! \displaystyle \frac{ {{b}} }{ {{c}} } !$.
-==
 
 
-script_init ==
-var ax = board.create('line', [[0,0],[1,0]], {fixed:true});
-board.create('ticks',[ax, 1], {label: {anchorX: 'middle', anchorY: 'top', fontSize: 12, offset: [0, 30]}, anchor : 0., drawZero: true, drawLabels:true, minorTicks:{{ k }}, majorHeight:20, minorHeight:10});
-var M = board.create('glider', [0., 0., ax], {name:'M',color:'blue',fixed:false, label: {anchorX: 'middle', anchorY: 'bottom', fontSize: 12, offset: [0, -30]}});
-==
-
-script_solution ==
-board.create('point',[{{xsol}}, {{ysol}}],{name:'', color:'green'});
-==
 
 inputblock ==
-{{ jxg|component }}
-==
+<div id='outer' style='max-width: 500px; margin: 0 auto;'>
+<div id='jxgbox' class='jxgbox' style='height: 0; padding-bottom: 100%'></div>
+</div>
+<button onClick="board.toFullscreen('outer')">Fullscreen</button>
 
-style.jxgcss ==
-<style>
-.jsxgraph-component {
-    width: 500px;
-    max-width: 100%;
-    height: 100px;
-    margin: 0 auto;
-}
-</style>
+<script language="Javascript" type='text/javascript'>
+var board = JXG.JSXGraph.initBoard('jxgbox', {
+    axis:true,
+    boundingbox:[-5,5,5,-5],
+    fullscreen: { id: 'outer' },
+    showFullscreen: true
+});
+var p = board.create('point', [-2, 3], {});
+</script>
 ==
