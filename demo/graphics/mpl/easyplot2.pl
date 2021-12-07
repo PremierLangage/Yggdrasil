ea285937bab289bd84796f036e2654cbdf36314d
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 19:29:49 +0100

    ddoyen1 modified Yggdrasil/demo/graphics/mpl/easyplot2.pl

---
+++ demo/graphics/mpl/easyplot2.pl
@@ -0,0 +1,19 @@
+extends = /model/basic/basic.pl
+@ /utils/graphics/plmpl.py
+
+before ==
+import matplotlib.pyplot as plt
+from plmpl import fig2svg, easyplot
+from sympy import pi, cos
+
+f = lambda x : 1 + cos(pi*x)
+easyplot(plt.gcf(), f, 0, 4)
+
+image = fig2svg(plt.gcf())
+==
+
+question ==
+<div class="img w50">
+{{ image }}
+</div>
+==
