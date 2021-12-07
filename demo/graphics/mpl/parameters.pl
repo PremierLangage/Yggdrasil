bea09697b03d272cfff23da4f41d03a8488c0c32
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 22:54:23 +0100

    ddoyen1 modified Yggdrasil/demo/graphics/mpl/parameters.pl

--- demo/graphics/mpl/parameters.pl
+++ demo/graphics/mpl/parameters.pl
@@ -8,12 +8,11 @@ from numpy import cos, pi
 
 f = lambda x : x**2
 easyplot(plt.gcf(), f, -2.5, 2.5)
+plt.title('Title')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Title')
 plt.xlim(-3, 3)
 plt.ylim(0, 6)
-plt.grid(True)
 plt.xticks(range(-3, 4))
 plt.yticks(range(0, 7))
 plt.grid(True)
