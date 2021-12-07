88cd5c9a6b81ee347fb469ce192613135ebdcfae
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 13:06:02 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -1,7 +1,7 @@
 extends = /model/math/expr.pl
 
 before ==
-from mplsympy import plotsvg, fig2svg, plot2svg, plot2
+from mplsympy import plotsvg, fig2svg, plot2svg, plot2, easyplot
 from sympy.plotting import plot
 import matplotlib.ticker as ticker
 
