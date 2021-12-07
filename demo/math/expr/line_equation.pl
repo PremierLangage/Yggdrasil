51ecbb30162d1c3b63337b73934551fa46d53690
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 13:00:04 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -20,10 +20,11 @@ def easyplot(ax, expr, xmin, xmax, npts=3):
     for t0 in t:
         #s.append(expr.subs({sb[0]:t0}))
         s.append(a*t0+b)
-    ax.plot(t, s)
+    #ax.plot(t, s)
     return t, s
 
 t,s = easyplot(plt.gca(), sol, -1, 1)
+plt.plot(t,s)
 plt.grid(True)
 
 image = fig2svg(plt.gcf())
