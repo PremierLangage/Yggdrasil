88ff6ad49943b66eb9a69c4be9609b6b9eb39348
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 11:58:12 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -4,6 +4,7 @@ before ==
 from mplsympy import plotsvg, fig2svg, plot2svg, plot2
 from sympy.plotting import plot
 import matplotlib.ticker as ticker
+
 var('x')
 a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
 b = randint(-3, 3)
