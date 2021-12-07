e8b212b4dbf9381cd37404f2559261172d5a58d6
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 18:48:19 +0100

    ddoyen1 modified Yggdrasil/demo/graphics/mpl/plot1.pl

--- demo/graphics/mpl/plot1.pl
+++ demo/graphics/mpl/plot1.pl
@@ -8,7 +8,7 @@ import numpy as np
 
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2*np.pi*t)
-plt(s, t)
+plt.plot(s, t)
 plt.xlabel('time (s)')
 plt.ylabel('voltage (mV)')
 plt.title('About as simple as it gets, folks')
