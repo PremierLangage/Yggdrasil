f78cc28dffcbd82b6c89cade877d20e9aff3ccc2
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 18:46:27 +0100

    ddoyen1 modified Yggdrasil/demo/graphics/mpl/plot1.pl

---
+++ demo/graphics/mpl/plot1.pl
@@ -0,0 +1,22 @@
+extends = /model/basic/basic.pl
+
+before ==
+import matplotlib.pyplot as plt
+from plmpl import fig2svg
+import numpy as np
+
+t = np.arange(0.0, 2.0, 0.01)
+s = 1 + np.sin(2*np.pi*t)
+plt(s, t)
+plt.xlabel('time (s)')
+plt.ylabel('voltage (mV)')
+plt.title('About as simple as it gets, folks')
+
+image = fig2svg(plt.gcf())
+==
+
+question ==
+<div class="img w40">
+{{ image }}
+</div>
+==
