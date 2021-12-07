c3e75627776937c2c1fb4c6141282d4e3fdfbd24
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-07 15:47:26 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/liste1.pl

---
+++ ComputerScience/python/AP1-2122/Remediation/liste1.pl
@@ -0,0 +1,29 @@
+author= DR
+title=En place
+
+tag=function # N'oubliez pas de remplir ce champs svp
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+
+text==
+
+Ecrire une fonction place qui prend deux paramêtres une liste et une valeur : $%l et v%$.
+Qui ajoute $%v%$ {{position}} dans la liste $%l%$.
+
+==
+
+before==
+position, f= random.choice([
+('au début',lambda l,v:l.insert(0,v)),
+('en deuxième position',lambda l,v:l.insert(1,v)),
+('a la fin',lambda l,v:l.insert(len(l),v))]
+
+pltest= f"""
+>>> place([1,2,3],4)
+{f([1,2,3],4)}
+>>> place([],1)
+[1]
+"""
+==
+
