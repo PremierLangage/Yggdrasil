101004fc6d0ed5a0079ccd99967e35d11c4b297e
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 19:25:31 +0100

    ddoyen1 modified Yggdrasil/demo/graphics/mpl/easyplot1.pl

--- demo/graphics/mpl/easyplot1.pl
+++ demo/graphics/mpl/easyplot1.pl
@@ -4,8 +4,9 @@ extends = /model/basic/basic.pl
 before ==
 import matplotlib.pyplot as plt
 from plmpl import fig2svg
+from math import sin
 
-f = lambda(x, 1+sin(x))
+f = lambda x : 1 + sin(x)
 easyplot(plt.gcf(), f, 0, 4)
 
 image = fig2svg(plt.gcf())
