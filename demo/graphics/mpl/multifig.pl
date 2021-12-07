a4b589bbe04a9b8e8ee2f90516864d528f48ab7e
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-07 23:00:42 +0100

    ddoyen1 modified Yggdrasil/demo/graphics/mpl/multifig.pl

---
+++ demo/graphics/mpl/multifig.pl
@@ -0,0 +1,23 @@
+extends = /model/basic/basic.pl
+@ /utils/graphics/plmpl.py
+
+before ==
+import matplotlib.pyplot as plt
+from plmpl import fig2svg, easyplot
+from numpy import sin, cos
+
+f1 = lambda x : sin(x)
+f2 = lambda x : cos(x)
+
+easyplot(plt.gcf(), f1, -3, 3)
+image1 = fig2svg(plt.gcf())
+
+easyplot(plt.gcf(), f2, -3, 3)
+image2 = fig2svg(plt.gcf()
+==
+
+question ==
+<div class="img w50">
+{{ image }}
+</div>
+==
