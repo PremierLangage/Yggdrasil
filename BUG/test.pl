c2fba58d4aaca622b35cac240ccf501be2d21521
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-08 18:35:49 +0100

    ddoyen1 modified Yggdrasil/BUG/test.pl

--- BUG/test.pl
+++ BUG/test.pl
@@ -1,8 +1,6 @@
 @ /builder/before.py [builder.py]
 @ /grader/evaluator1.py [grader.py]
 @ /utils/builderlib.py
-@ /utils/plrandom.py
-@ /utils/plcsv.py
 @ /model/basic/jinja_basic.py [jinja_env.py]
 @ /model/basic/json_basic.py [json_encoder.py]
 
