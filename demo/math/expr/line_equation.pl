a1777c523a9eb9f8043cffec71b317e16b234ffa
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-08 08:36:59 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/line_equation.pl

--- demo/math/expr/line_equation.pl
+++ demo/math/expr/line_equation.pl
@@ -9,10 +9,6 @@ a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
 b = randint(-3, 3)
 sol = a*x + b
 
-from matplotlib.figure import Figure
-#fig = Figure()
-
-plt.figure(randint(1, 80))
 n = 5
 plt.xlim(-n, n)
 plt.ylim(-n, n)
@@ -21,7 +17,6 @@ plt.xticks(range(-n, n+1))
 plt.yticks(range(-n, n+1))
 easyplot(plt.gcf(), sol, -n, n)
 image = fig2svg(plt.gcf())
-#plt.close()
 ==
 
 question ==
