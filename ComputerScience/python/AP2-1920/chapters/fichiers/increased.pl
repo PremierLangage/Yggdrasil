5bb13e23439640e830987681c4dee9a66291d823
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-08 17:38:59 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP2-1920/chapters/fichiers/increased.pl

--- ComputerScience/python/AP2-1920/chapters/fichiers/increased.pl
+++ ComputerScience/python/AP2-1920/chapters/fichiers/increased.pl
@@ -47,7 +47,7 @@ with open("data.txt","w") as f:
         else:
             start += random.randint(1,10)
             augment += 1
-    print(star, file=f)
+    print(start, file=f)
 pltest=f"""
 >>> mesure()
 (2000, {augment-1})
