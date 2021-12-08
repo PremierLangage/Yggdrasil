e286544cd5ab2c41d88fe701b806c67ba213e9b8
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-08 17:42:15 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP2-1920/chapters/fichiers/increased.pl

--- ComputerScience/python/AP2-1920/chapters/fichiers/increased.pl
+++ ComputerScience/python/AP2-1920/chapters/fichiers/increased.pl
@@ -13,24 +13,24 @@ title= Données ça monte et cela descend
 
 text==
 
+
 Vous avez un fichier de données "data".
 
-Vous devez afficher sur une ligne combien il y a de lignes dans le fichier 
-sur une ligne combien de fois l'entier sur la ligne a été plus grand que le précédent "augmentations". 
+le nombre d'augmentations est le nomrbe de fois l'entier sur la ligne a été plus grand que le précédent. 
 
 ecrire une fonction "mesure" qui retourne un tuple avec le nombre de ligne et d'augmentations.
 
 exemple:
 200 (première valeur )  
-210 (augmentation)
-215 (augmentation)
-207 (diminution)
-214 (augmentation)
-216 (augmentation)
-213 (diminution)
+210 (augmentation)  
+215 (augmentation)  
+207 (diminution)  
+214 (augmentation)  
+216 (augmentation)  
+213 (diminution)  
 
-7 lignes
-4 augmentations
+7 lignes  
+4 augmentations   
 
 ==
 
