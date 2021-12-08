58914faf6db0820791e9b2637e44d256f158edb8
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-08 17:45:55 +0100

    ddoyen1 modified Yggdrasil/BUG/test.pl

---
+++ BUG/test.pl
@@ -0,0 +1,23 @@
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+@ /utils/sandboxio.py
+
+title =
+
+keylist % [1, 2]
+
+before ==
+
+==
+
+text ==
+{{ keylist }}
+==
+
+form ==
+
+==
+
+evaluator ==
+def
+==
