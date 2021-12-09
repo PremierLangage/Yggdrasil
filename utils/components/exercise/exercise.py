aec89e070c46ad2723ffda3ad58ad652d58d66b9
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-09 15:17:46 +0100

    ddoyen1 modified Yggdrasil/utils/components/exercise/exercise.py

---
+++ utils/components/exercise/exercise.py
@@ -0,0 +1,13 @@
+from uuid import uuid4
+
+class Ex:
+
+    count = 0
+    instances = []
+
+    def __init__(self, **kwargs):
+        self.__Ex__ = True
+        self.id = kwargs.get('id', "Ex"+str(Ex.count))
+        Ex.count += 1
+        Ex.instances.append(self)
+        self.evaluator = kwargs.get('evaluator', '')
