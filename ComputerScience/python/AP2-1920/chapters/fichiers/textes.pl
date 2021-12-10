7569315be32beb93c67558e4e50f5bbdc8ab12f2
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-10 08:58:58 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP2-1920/chapters/fichiers/textes.pl

--- ComputerScience/python/AP2-1920/chapters/fichiers/textes.pl
+++ ComputerScience/python/AP2-1920/chapters/fichiers/textes.pl
@@ -193,3 +193,65 @@ Considérons les sommes d'une fenêtre coulissante à trois mesures. Combien de 
 
 text2_2==
 ==
+
+
+text4_1==
+Vous êtes déjà à près de 1,5 km (presque un mile) sous la surface de l'océan, déjà si profond que vous ne pouvez pas voir la lumière du soleil. Ce que vous pouvez voir, cependant, est un calmar géant qui s'est attaché à l'extérieur de votre sous-marin.
+
+Peut-être qu'il veut jouer au bingo ?
+
+Le bingo se joue sur un ensemble de planches composées chacune d'une grille de numéros 5x5. Les numéros sont choisis au hasard et le numéro choisi est marqué sur tous les tableaux sur lesquels il apparaît. (Les nombres peuvent ne pas apparaître sur tous les tableaux.) Si tous les numéros d'une rangée ou d'une colonne d'un tableau sont marqués, ce tableau gagne. (Les diagonales ne comptent pas.)
+
+Le sous-marin dispose d'un sous-système de bingo pour aider les passagers (actuellement, vous et le calmar géant) à passer le temps. Il génère automatiquement un ordre aléatoire dans lequel tirer des nombres et un ensemble aléatoire de planches (votre entrée de puzzle). Par exemple:
+
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3, 26,1
+
+22 13 17 11 0
+ 8 2 23 4 24
+21 9 14 16 7
+ 6 10 3 18 5
+ 1 12 20 15 19
+
+ 3 15 0 2 22
+ 9 18 13 17 5
+19 8 7 25 23
+20 11 10 24 4
+14 21 16 12 6
+
+14 21 17 24 4
+10 16 15 9 19
+18 8 23 26 20
+22 11 13 6 5
+ 2 0 12 3 7
+
+Une fois les cinq premiers numéros tirés (7, 4, 9, 5 et 11), il n'y a pas de gagnants, mais les tableaux sont marqués comme suit (indiqués ici côte à côte pour économiser de l'espace) :
+
+22 13 17 11  0     3 15  0  2 22    14 21 17 24 4
+ 8  2 23  4 24     9 18 13 17  5    10 16 15 9 19
+21  9 14 16  7    19  8  7 25 23    18 8 23 26 20
+ 6 10  3 18  5    20 11 10 24  4    22 11 13 6 5
+ 1 12 20 15 19    14 21 16 12  6     2  0 12 3 7
+
+Après le tirage des six numéros suivants (17, 23, 2, 0, 14 et 21), il n'y a toujours pas de gagnant :
+
+22 13 17 11  0    3 15  0  2 22   14 21 17 24  4
+ 8  2 23  4 24    9 18 13 17  5   10 16 15  9 19
+21  9 14 16  7   19  8  7 25 23   18  8 23 26 20
+ 6 10  3 18  5   20 11 10 24  4   22 11 13  6  5
+ 1 12 20 15 19   14 21 16 12  6    2  0 12  3  7
+
+Enfin, 24 est tiré :
+
+22 13 17 11  0    3 15  0  2 22   14 21 17 24  4
+ 8  2 23  4 24    9 18 13 17  5   10 16 15  9 19
+21  9 14 16  7   19  8  7 25 23   18  8 23 26 20
+ 6 10  3 18  5   20 11 10 24  4   22 11 13  6  5
+ 1 12 20 15 19   14 21 16 12  6    2  0 12  3  7
+
+
+À ce stade, le troisième tableau gagne car il a au moins une ligne ou une colonne complète de nombres marqués (dans ce cas, toute la ligne du haut est marquée : 14 21 17 24 4).
+
+Le score du tableau gagnant peut maintenant être calculé. Commencez par trouver la somme de tous les nombres non marqués sur ce tableau ; dans ce cas, la somme est de 188. Ensuite, multipliez cette somme par le nombre qui vient d'être appelé lorsque le tableau a gagné, 24, pour obtenir le score final, 188 * 24 = 4512.
+
+Pour garantir la victoire contre le calmar géant, déterminez quel tableau gagnera en premier. Quel sera votre score final si vous choisissez cette planche ?
+== 
