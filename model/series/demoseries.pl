1f19442caf2852aee9cc775ac83dfaef67273ff9
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-10 19:38:52 +0100

    ddoyen1 modified Yggdrasil/model/series/demoseries.pl

--- model/series/demoseries.pl
+++ model/series/demoseries.pl
@@ -22,10 +22,10 @@ comp = []
 for i in range(nbstep):
     row = sample_lines[i]
     qq = rf"""
-<strong> Phrase {i + 1}. </strong><br> 
-
-{ row['phrase'] }
-"""
+    <strong> Phrase {i + 1}. </strong><br>
+    
+    { row['phrase'] }
+    """
     questions.append(qq)
     sol.append(row['rep'])
     comp.append(Input())
@@ -34,22 +34,17 @@ step = 0
 ==
 
 classexo ==
-class Ex:
+def ex_eval(step):
+    if comp[step-1].value == sol[step-1]:
+        return 100
+    else:
+        return 0
 
-    def __init__(self, step):
-        self.step = step
+def ex_disable(step):
+    comp[step-1].disable()
 
-    def eval(self):
-        if comp[self.step-1].value == sol[self.step-1]:
-            return 100
-        else:
-            return 0
-
-    def disable(self):
-        comp[self.step-1].disable()
-
-    def show(self):
-        comp[self.step-1].show()
+def ex_show(self):
+    comp[step-1].show()
 ==
 
 intro ==
