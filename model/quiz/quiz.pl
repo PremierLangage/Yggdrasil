5d51b7b859eb78e945fa24e1197c2347123848ba
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-10 16:37:20 +0100

    ddoyen1 modified Yggdrasil/model/quiz/quiz.pl

--- model/quiz/quiz.pl
+++ model/quiz/quiz.pl
@@ -104,4 +104,52 @@ questions ==
 - 1
 + 2
 - 3
+
+* Quelle est la capitale du Cameroun ?
++ Yaoundé
+- Douala
+- Kribi
+
+** Parmi les nombres suivants, lesquels sont positifs ?
++ 2
+- -2
++ 10
+
+*[ordered] Combien font un plus un ?
+- 0
+- 1
++ 2
+- 3
+
+* Quelle est la capitale du Cameroun ?
++ Yaoundé
+- Douala
+- Kribi
+
+** Parmi les nombres suivants, lesquels sont positifs ?
++ 2
+- -2
++ 10
+
+*[ordered] Combien font un plus un ?
+- 0
+- 1
++ 2
+- 3
+
+* Quelle est la capitale du Cameroun ?
++ Yaoundé
+- Douala
+- Kribi
+
+** Parmi les nombres suivants, lesquels sont positifs ?
++ 2
+- -2
++ 10
+
+*[ordered] Combien font un plus un ?
+- 0
+- 1
++ 2
+- 3
 ==
