6a3ac451f492f4cac3ec797b3801f752f5587ecd
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-12 19:14:05 +0100

    ddoyen1 modified Yggdrasil/model/quiz/demoquiz.pl

---
+++ model/quiz/demoquiz.pl
@@ -0,0 +1,30 @@
+
+
+before == #|python|
+nbstep = 4
+
+with open('phrases.txt') as f:
+    sample_lines = csv_sample(f, nbstep, delimiter='|')
+
+
+sol = []
+solutions = []
+feedbacks = []
+questions = []
+comp = []
+
+for i in range(nbstep):
+    row = sample_lines[i]
+    qq = rf"""
+<strong> Phrase {i + 1}. </strong><br>
+
+{ row['phrase'] }
+"""
+    questions.append(qq)
+    sol.append(row['rep'])
+    solutions.append(row['rep'])
+    feedbacks.append('')
+    comp.append(Input())
+
+step = 0
+==
