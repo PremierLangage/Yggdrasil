8ebbd9ed00b6183fa2564da261eaedc47fd3aed0
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-13 14:18:43 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/premiertriplet.pl

---
+++ ComputerScience/python/AP1-2122/Remediation/premiertriplet.pl
@@ -0,0 +1,63 @@
+
+
+
+
+
+author= DR
+title=Deux listes again
+
+tag=function # N'oubliez pas de remplir ce champs svp
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+@ /builder/before.py [builder.py]
+
+
+text==
+
+Ecrire une fonction **ptriplet** qui prend un itérable en paramêtres .  
+
+Et qui retourne un tuple contenant la valeur et l'indice du premier triplet de valeurs identiques. 
+
+
+exemple :
+
+    >>> ptriplet("aaabcd")
+    ('a', 0)
+
+
+==
+
+before==
+from random import randint
+def makes():
+    l=[chr(randint(0,25)+97) for i in range(20)]
+    u= randint(10,15)
+    for x in range(2):
+        l.insert(l[u],u)
+    return ''.join(l)
+
+
+def ftriple(s):
+    i=0
+    while i+2< len(s):
+        if l[i]==l[i+1] and l[i]==l[i+2]:
+            return (l[i],i)
+for yy in range(3):
+    s=makes()
+    pltest += f"""
+    >>> ptriplet({s})
+    {ftriple(s)}
+    """ 
+
+texte += pltest
+==
+
+pltest==
+>>> ptriplet("aaabcd")
+('a', 0)
+==
+
+
+
+code==
+
+==
