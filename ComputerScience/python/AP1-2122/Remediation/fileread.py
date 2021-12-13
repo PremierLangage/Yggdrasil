8bbdf847cbef4c3afc6294d86303675a74317ac9
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-13 15:53:38 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/fileread.py

--- ComputerScience/python/AP1-2122/Remediation/fileread.py
+++ ComputerScience/python/AP1-2122/Remediation/fileread.py
@@ -6,4 +6,28 @@ def f2l(filename):#
             l.append(line.strip())
     return l
 
-CORRECT=1050
+
+
+from string import ascii_lowercase
+from random import randint, choice
+def produce(s):
+    """Produce a liste of string of len = len(s)
+    
+    The frequency of letter s[i] is the highest in column i.
+    
+    we think we are not lucky ...
+    
+    each time we choose a letter  in 10% we choose s[i] else we choose random letter in ascii_lowercase
+    
+    """ 
+    lowers =list(ascii_lowercase)
+    l=[]
+    for i in range(1000):
+        ll=[]
+        for x in range(len(s)):
+            if randint(1,10) < 2 :
+                ll.append(s[x])
+            else:
+                ll.append(choice(lowers ))
+        l.append(''.join(ll))
+    return l
