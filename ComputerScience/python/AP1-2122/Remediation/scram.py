881e3cfc67a45a2f0341442073fb9c268d259d31
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-13 15:35:14 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/scram.py

---
+++ ComputerScience/python/AP1-2122/Remediation/scram.py
@@ -0,0 +1,28 @@
+
+from string import ascii_lowercase
+from random import randint, choice
+def produce(s):
+    """Produce a liste of string of len = len(s)
+    
+    The frequency of letter s[i] is the highest in column i.
+    
+    we think we are not lucky ...
+    
+    each time we choose a letter  in 10% we choose s[i] else we choose random letter in ascii_lowercase
+    
+    """ 
+    lowers =list(ascii_lowercase)
+    l=[]
+    for i in range(1000):
+        ll=[]
+        for x in range(len(s)):
+            if randint(1,10) == 1 :
+                ll.append(s[x])
+            else:
+                ll.append(choice(lowers ))
+        l.append(''.join(ll))
+    return l
+
+if __main__ == "__main__":
+    s=choice(["BONJOUR","GUSTAVE","EIFFEL","JOYEUXNOEL","BONNEANNEE"])
+    print(produce(s))
