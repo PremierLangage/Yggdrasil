cb0fd139cc123dc08e515a42abfa0b827cefef87
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-13 17:10:23 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/brouille.pl

--- ComputerScience/python/AP1-2122/Remediation/brouille.pl
+++ ComputerScience/python/AP1-2122/Remediation/brouille.pl
@@ -16,9 +16,9 @@ extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 text==
 
 La ligne de communication est très brouillé et beaucoup de caractères sont changés.
-Mais comme nous pouvons recevoir beaucoup de fois le message nous proposons de chercher pour chaque position quelle est la lettre la plus fréquente.
-
-Et de retourner le mot construit avec ces lettres.
+Mais comme nous pouvons recevoir beaucoup de fois le message. 
+Nous nous proposons d'envoyer beaucoup de fois le message puis de chercher pour chaque position quelle
+ est la lettre la plus fréquente, et de fabriquer le mot construit avec ces lettres.
 
 Ecrire une fonction **Oeufbrouile** qui prend une liste de chaines et retourne la chaine construite avec les lettres les plus fréquentes à chaque position.
 
