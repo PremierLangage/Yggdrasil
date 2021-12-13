af991cc50351c2334b54ff0eca7efb378504ea36
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-13 14:46:59 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/quelquetriangles.pl

--- ComputerScience/python/AP1-2122/Remediation/quelquetriangles.pl
+++ ComputerScience/python/AP1-2122/Remediation/quelquetriangles.pl
@@ -16,7 +16,7 @@ text==
 
 Par définition des triangles la somme des longueurs deux coté est toujours plus grande que la longueur du troisième coté.
 
-Ecrie une fonction **check** qui prend une liste de chaines de caractère contenant trois entiers et qui retourne le nombre de ligne contneant des triangles.
+Ecrie une fonction **check** qui prend une liste de chaines de caractère contenant trois entiers et qui retourne le nombre de chaine contenant des triangles.
 
 Exemple de chaine :
 
@@ -26,42 +26,3 @@ Exemple de chaine :
 ==
 
 before==
-from random import randint
-def makes():
-    l=[chr(randint(0,25)+97) for i in range(16)]
-    u= randint(4,15)
-    for x in range(2):
-        l.insert(u,l[u])
-    return ''.join(l)
-
-def ftriple(l):
-    i=0
-    while i+2< len(l):
-        if l[i]==l[i+1] and l[i]==l[i+2]:
-            return (l[i],i)
-        i += 1
-        
-for yy in range(3):
-    s=makes()
-    pltest += f"""
-    >>> ptriplet("{s}")
-    {ftriple(s)}
-    """ 
-==
-
-pltest==
->>> ptriplet("aaabcd")
-('a', 0)
->>> ptriplet("abcdefghjkilnaopqusrtxwz")
-==
-
-
-
-code==
-def ptriplet(l):
-    i=0
-    while i+2< len(l):
-        if l[i]==l[i+1] and l[i]==l[i+2]:
-            return (l[i],i)
-        i += 1
-==
