580973d2a0ba70643d0a84e12fc7e9832427c940
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2021-12-13 15:05:23 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/Remediation/quelquetriangles.pl

--- ComputerScience/python/AP1-2122/Remediation/quelquetriangles.pl
+++ ComputerScience/python/AP1-2122/Remediation/quelquetriangles.pl
@@ -14,7 +14,7 @@ extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 
 text==
 
-Par définition des triangles la somme des longueurs deux coté est toujours plus grande que la longueur du troisième coté.
+Une des propriétés des triangles est la somme des longueurs deux cotés est toujours plus grande que la longueur du troisième coté.
 
 Ecrie une fonction **check** qui prend une liste de chaines de caractère contenant trois entiers et qui retourne le nombre de chaine contenant des triangles.
 
@@ -23,6 +23,20 @@ Exemple de chaine :
     "5 10 25" qui n'est pas un triangle 
     "685  748  471" qui peut en être un
 
+en cas d'égalité ce n'est pas un triangle.
 ==
 
-before==
+pltest==
+>>> def file2list():#
+...    with open("triangles.txt","r") as f:
+...        l = []
+...        for line in f:
+...            l.append(line.strip())
+...    return l
+>>> R=check(file2listq())#
+>>> if r > 1050: #
+...     print("Trop grand")
+>>> elif r < 1050: #
+...     print("trop petit")
+==
+
