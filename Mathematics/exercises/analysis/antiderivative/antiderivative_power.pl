e0ee0ef9b3aa627336d4d9128c930dc76c050905
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-14 13:41:40 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_power.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_power.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_power.pl
@@ -3,7 +3,7 @@ extends = antiderivative_.pl
 before ==
 x = Symbol('x')
 a = randint(-5, 5, [0])
-b, c = sampleint(-5, 5, 2, [0])
+b, c = sample([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5], 2)
 d = randint(4,7)
 f = a*(b*x+c)**d
 sol = simplify(Rational(a, b*(d+1))*(b*x + c)**(d+1))
