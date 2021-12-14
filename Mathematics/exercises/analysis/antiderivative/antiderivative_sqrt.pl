aee542e6c1d95af0b9ccaf6623fda2e067b37bc3
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-14 13:42:57 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_sqrt.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_sqrt.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_sqrt.pl
@@ -5,26 +5,22 @@ title = Calcul de primitive
 lang = fr
 
 before ==
+x = Symbol('x')
+a = randint(-5, 5, [0])
+b, c = sample([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5], 2)
 
-
-var('x')
-a=randint(-5,5,[0])
-b,c=list_randint_norep(2,-5,5,[0])
-
-b0=Rational(-c,b)
-if b>0:
+b0 = Rational(-c,b)
+if b > 0:
     dom=r"\rbrack %s ,+\infty\lbrack" % latex(b0)
 else:
     dom=r"\rbrack -\infty, %s \lbrack" % latex(b0)
 
-f=randitem([a*sqrt(b*x+c),a/sqrt(b*x+c)])
-f=simplify(f)
-f_tex=latex(f)
+f = choice([a*sqrt(b*x+c), a/sqrt(b*x+c)])
+f = simplify(f)
 sol=integrate(f,x).doit()
-sol_tex=latex(sol)
 ==
 
-text ==
+question ==
 Calculer une primitive de la fonction
 $$ f : x \mapsto {{f_tex}} $$
 sur l'intervalle $! {{dom}} !$.
