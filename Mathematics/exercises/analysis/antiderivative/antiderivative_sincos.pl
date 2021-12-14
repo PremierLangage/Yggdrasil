3b8814905d8e33e0a17ed301608d80b0fd4d558a
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-14 13:26:11 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_sincos.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_sincos.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_sincos.pl
@@ -3,14 +3,11 @@ extends = antiderivative_.pl
 title = Calcul de primitive
 
 before ==
-
-var('x')
-a=randitem([Rational(1,2),Rational(1,3),Rational(1,4),2,3,4])
-b=randint(-3,3,[0])
-f=randitem([sin(a*x+b),cos(a*x+b),sin(a*x)+b,cos(a*x)+b])
-f_tex=latex(f)
-sol=integrate(f,x).doit()
-sol_tex=latex(sol)
+x = Symbol('x')
+a = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
+b = randint(-3, 3, [0])
+f = choice([sin(a*x+b),cos(a*x+b),sin(a*x)+b,cos(a*x)+b])
+sol = integrate(f, x).doit()
 ==
 
 
