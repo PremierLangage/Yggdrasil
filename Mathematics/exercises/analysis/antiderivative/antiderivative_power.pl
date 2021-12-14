f88d06014e103bd1b84f883cf5a18748d4f69248
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-14 13:35:36 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_power.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_power.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_power.pl
@@ -1,22 +1,12 @@
 extends = antiderivative_.pl
 
-title = Calcul de primitive
-
-lang = fr
-
-
-
 before ==
-
-var('x')
-a=randint(-5,5,[0])
-b,c=list_randint_norep(2,-5,5,[0])
-d=randint(4,7)
-
-f=a*(b*x+c)**d
-f_tex=latex(f)
-sol=simplify(Rational(a,b*(d+1))*(b*x+c)**(d+1))
-sol_tex=latex(sol)
+x = Symbol('x')
+a = randint(-5, 5, [0])
+b, c = sampleint(2, -5, 5, [0])
+d = randint(4,7)
+f = a*(b*x+c)**d
+sol = simplify(Rational(a, b*(d+1))*(b*x + c)**(d+1))
 ==
 
 
