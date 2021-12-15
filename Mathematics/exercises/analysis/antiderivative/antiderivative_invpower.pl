ea3dd0278a5c54a6419b2bcece5df579707b3d25
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-15 12:00:27 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_invpower.pl

---
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_invpower.pl
@@ -0,0 +1,23 @@
+extends = antiderivative_.pl
+
+before ==
+x = Symbol('x')
+a = randint(-5, 5, [0])
+b, c = sample([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5], 2)
+
+b0 = Rational(-c, b)
+if b > 0:
+    dom=r"\rbrack %s ,+\infty\lbrack" % latex(b0)
+else:
+    dom=r"\rbrack -\infty, %s \lbrack" % latex(b0)
+
+f = a/(b*x+c)**n
+f = simplify(f)
+sol = integrate(f,x).doit()
+==
+
+question ==
+Calculer une primitive de la fonction
+$$ f : x \mapsto {{ f|latex}} $$
+sur l'intervalle $! {{dom}} !$.
+==
