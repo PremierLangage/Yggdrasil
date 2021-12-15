86ab4c82f5e2fd81342b4795b567dca5a1c2d630
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-15 11:24:43 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_sin_cos_exp.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_sin_cos_exp.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_sin_cos_exp.pl
@@ -7,7 +7,7 @@ from sympy import integrate
 x = Symbol('x')
 a = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
 b = randint(-3, 3, [0])
-f = choice([exp(a*x+b), sin(a*x+b),cos(a*x+b)])
+f = choice([exp(a*x+b), sin(a*x+b), cos(a*x+b)])
 sol = integrate(f, x).doit()
 ==
 
