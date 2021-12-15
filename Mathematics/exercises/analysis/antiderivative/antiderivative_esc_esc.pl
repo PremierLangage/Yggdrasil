ef3d25e93329d296a70d1684db858e0f1ac9ce8b
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-15 11:33:22 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_esc_esc.pl

---
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_esc_esc.pl
@@ -0,0 +1,16 @@
+extends = antiderivative_.pl
+
+title = Calcul de primitive
+
+before ==
+from sympy import integrate
+x = Symbol('x')
+a = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
+b = randint(-3, 3, [0])
+i = randint(0, 2)
+u = [exp(a*x), sin(a*x), cos(a*x)][i]
+du = [exp(a*x), cos(a*x), sin(a*x)][i]
+g = choice([exp, sin, cos])
+f = du*g(u)
+sol = integrate(f, x).doit()
+==
