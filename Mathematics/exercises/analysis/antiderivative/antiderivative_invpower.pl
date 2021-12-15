9a758d61f0863eb64c02f424f8f94fb999ee8b2a
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-15 12:00:53 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_invpower.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_invpower.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_invpower.pl
@@ -4,6 +4,7 @@ before ==
 x = Symbol('x')
 a = randint(-5, 5, [0])
 b, c = sample([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5], 2)
+d = randint(4,7)
 
 b0 = Rational(-c, b)
 if b > 0:
@@ -11,7 +12,7 @@ if b > 0:
 else:
     dom=r"\rbrack -\infty, %s \lbrack" % latex(b0)
 
-f = a/(b*x+c)**n
+f = a/(b*x+c)**d
 f = simplify(f)
 sol = integrate(f,x).doit()
 ==
