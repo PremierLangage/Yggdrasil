c27bd8bebe8d87e076c19602eac8f61258a30dae
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-15 11:33:25 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_esc.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_esc.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_esc.pl
@@ -7,11 +7,7 @@ from sympy import integrate
 x = Symbol('x')
 a = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
 b = randint(-3, 3, [0])
-i = randint(0, 2)
-u = [exp(a*x), sin(a*x), cos(a*x)][i]
-du = [exp(a*x), cos(a*x), sin(a*x)][i]
-g = choice([exp, sin, cos])
-f = du*g(u)
+f = [exp(a*x+b), sin(a*x+b), cos(a*x+b)])
 sol = integrate(f, x).doit()
 ==
 
