c9e5686adc92a5a78c12d8dfa52a6af94f949a78
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-15 12:27:37 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_power_sincos.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_power_sincos.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_power_sincos.pl
@@ -3,12 +3,17 @@ extends = antiderivative_.pl
 before ==
 from sympy import integrate
 x = Symbol('x')
-a = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
-b = randint(-3, 3, [0])
+c = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
+if randint(0, 1) == 1:
+    a = c
+    b = 1
+else:
+    a = 1
+    b = c
 i = randint(0, 1)
 u = [sin(a*x), cos(a*x)][i]
 du = [cos(a*x), sin(a*x)][i]
 d = randint(1, 3)
-f = du*(u)**d
+f = b*du*(u)**d
 sol = integrate(f, x).doit()
 ==
