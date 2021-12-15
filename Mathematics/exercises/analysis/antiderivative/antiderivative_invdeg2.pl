c4c9971f51379715e08b6e757063dd27efeb5e59
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-15 13:19:29 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/antiderivative/antiderivative_invdeg2.pl

--- Mathematics/exercises/analysis/antiderivative/antiderivative_invdeg2.pl
+++ Mathematics/exercises/analysis/antiderivative/antiderivative_invdeg2.pl
@@ -3,7 +3,7 @@ extends = antiderivative_.pl
 before ==
 x = Symbol('x')
 a = randint(-5, 5, [0])
-b, c = sample([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5], 2)
+b, c = sample([1, 2, 3, 4, 5], 2)
 
 f = a/(b*x**2+c)
 f = simplify(f)
