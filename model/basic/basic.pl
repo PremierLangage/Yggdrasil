943b8ba2eb02d3b567ff1d722149793d85fec753
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-18 16:51:16 +0100

    ddoyen1 modified Yggdrasil/model/basic/basic.pl

--- model/basic/basic.pl
+++ model/basic/basic.pl
@@ -7,6 +7,7 @@ extends = /model/basic/temp.pl
 @ /utils/plcsv.py
 @ /model/basic/jinja_basic.py [jinja_env.py]
 @ /model/basic/json_basic.py [json_encoder.py]
+@ /utils/components/multicomp.py 
 
 before_scripts % ["importfunc", "before", "process"]
 eval_scripts % ["getans", "evaluator", "evalprocess"]
