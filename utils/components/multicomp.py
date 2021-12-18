1efd1af0b2f8c2b95d69fa1a8b7f03b9ec659122
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-18 17:51:52 +0100

    ddoyen1 modified Yggdrasil/utils/components/multicomp.py

--- utils/components/multicomp.py
+++ utils/components/multicomp.py
@@ -4,3 +4,29 @@ class MultiComp:
     def __init__(self, **kwargs):
         self.__MultiComp__ = True
         self.comp = kwargs.get('comp', [])
+
+class DropGroup:
+
+    def __init__(self, **kwargs):
+        self.__MultiComp__ = True
+        self.comp = kwargs.get('comp', [])
+
+    def eval(self):
+        n = len(drp.comp)
+        num_right = 0
+        num_wrong = 0
+
+        for i in range(n):
+            if self.comp[i].content == self._sol[i]:
+                num_right += 1
+                self.comp[i].css = "success-state"
+            else:
+                num_wrong +=1
+                self.comp[i].css = "error-state"
+
+        if num_wrong > 0 :
+            score = 0
+        else:
+            score = 100
+            
+        return score
