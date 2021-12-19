26108fcb0555cc7b4c353cadf3d0930a80ba0e42
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-19 18:44:22 +0100

    ddoyen1 modified Yggdrasil/model/quiz/AMC.pl

---
+++ model/quiz/AMC.pl
@@ -0,0 +1,121 @@
+extends = /model/quiz/basic.pl
+
+@ /utils/format/AMC.py
+@ /utils/components/scoring.py
+@ /utils/components/radio.py
+@ /utils/components/checkbox.py
+
+before_scripts = ["before"]
+eval_scripts = ["evaluator"]
+
+importfunc == #|python|
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from plcsv import csv_choice, csv_sample, csv_col
+==
+
+title =
+form = 
+text = 
+
+tplpage =@ template2.html
+
+style.basic =@ /demo/css/basic.html
+
+before == #|python|
+import random as rd
+from radio import Radio
+from checkbox import Checkbox
+from AMC import parse_AMC_TXT
+from exercise import Ex
+
+state = "active"
+
+list_questions = parse_AMC_TXT(questions)
+
+if 'nbstep' in globals():
+    list_questions = rd.sample(list_questions, nbstep)
+else:
+    nbstep = len(list_questions)
+
+comp = []
+statement  = []
+lstex =[]
+
+for i, q in enumerate(list_questions):
+    if q['type'] == "Radio":
+        comp.append(Radio())
+        comp[i].set_items(q['items'])
+        comp[i].set_sol(q['index'])
+    elif q['type'] == "Checkbox":
+        comp.append(Checkbox())
+        comp[i].set_items(q['items'])
+        comp[i].set_sol(q['index'])
+    elif  q['type'] == 'TextSelect':
+        # comp.append(CustomTextSelect())
+        continue # no implemented yet 
+    statement.append(q['text'])
+    if 'ordered' not in q['options']:
+        comp[i].shuffle()
+    ex = Ex()
+    ex.question = q['text']
+    ex.evaluator = "score = comp[i].eval()"
+    lstex.append(ex)
+
+step = 0
+score = -10
+scores=[]
+==
+
+intro ==
+Début du quiz
+==
+
+evaluator ==
+class Exo:
+
+    def __init__(self, step):
+        self.step = step
+
+    def eval(self):
+        return comp[self.step-1].eval()
+
+    def disable(self):
+        comp[self.step-1].disable()
+
+    def show(self):
+        comp[self.step-1].show()
+
+if step == 0:
+    step = 1
+
+if step <= nbstep:
+    ex = Exo(step)
+    qscore = ex.eval()
+    if qscore != -1:
+        ex.disable()
+        ex.show()
+        scores.append(qscore)
+        step += 1
+
+if step == nbstep + 1:
+    state = "grade"
+==
+
+questions ==
+* Quelle est la capitale du Cameroun ?
++ Yaoundé
+- Douala
+- Kribi
+
+** Parmi les nombres suivants, lesquels sont positifs ?
++ 2
+- -2
++ 10
+
+*[ordered] Combien font un plus un ?
+- 0
+- 1
++ 2
+- 3
+==
