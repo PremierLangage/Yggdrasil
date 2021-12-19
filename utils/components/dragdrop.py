b71b7a593385351327fa7a36c1b6156fd3e8ad70
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-19 19:40:54 +0100

    ddoyen1 modified Yggdrasil/utils/components/dragdrop.py

--- utils/components/dragdrop.py
+++ utils/components/dragdrop.py
@@ -1,6 +1,67 @@
 from components import Component
 from random import shuffle
 from uuid import uuid4
+from multicomp import MultiComp
+
+class DropGroup(MultiComp):
+
+    def __init__(self, param=None, **kwargs):
+        self.__MultiComp__ = "DropGroup"
+        self.comp = kwargs.get('comp', [])
+        self._sol = kwargs.get('_sol', [])
+        if isinstance(param, int):
+            self.comp = [CustomDragDrop.Drop() for _ in range(param)]
+        elif isinstance(param, list):
+            self.comp = [CustomDragDrop.Drop() for _ in range(len(param))]
+            self._sol = param
+    
+    def eval(self):
+        n = len(self.comp)
+        num_right = 0
+        num_wrong = 0
+
+        for i in range(n):
+            if self.comp[i].content == self._sol[i]:
+                num_right += 1
+                self.comp[i].css = "success-state"
+            else:
+                num_wrong +=1
+                self.comp[i].css = "error-state"
+
+        if num_wrong > 0 :
+            score = 0
+        else:
+            score = 100
+            
+        return score
+
+class LabelGroup(MultiComp):
+
+    def __init__(self, param=None, **kwargs):
+        self.__MultiComp__ = "LabelGroup"
+        self.comp = kwargs.get('comp', [])
+        if isinstance(param, list):
+            self.comp = [CustomDragDrop.Label(content=content) for content in param]
+
+    
+import re
+
+def process_filledtext(filledtext, name):
+    sol = []
+    counter = 0
+    newstring = ''
+    start = 0
+    for m in re.finditer(r"{([^{}]+)}", filledtext):
+        end, newstart = m.span()
+        newstring += filledtext[start:end]
+        rep = "{{ "+ name + ".comp[" + str(counter) + "]|component }}"
+        sol.append(m.group(1)) 
+        newstring += rep
+        start = newstart
+        counter += 1
+    newstring += filledtext[start:]
+    return sol, newstring
+
 
 class MyComponent(Component):
 
