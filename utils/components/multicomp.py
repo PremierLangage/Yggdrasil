633b2ddaacb718b501b87c9b2ea6adcff430c50f
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-19 13:37:25 +0100

    ddoyen1 modified Yggdrasil/utils/components/multicomp.py

--- utils/components/multicomp.py
+++ utils/components/multicomp.py
@@ -47,3 +47,20 @@ class LabelGroup(MultiComp):
             self.comp = [CustomDragDrop.Label(content=content) for content in param]
 
     
+import re
+
+def process_filledtext(filledtext, name)
+    sol = []
+    counter = 0
+    newstring = ''
+    start = 0
+    for m in re.finditer(r"{([^{}]+)}", filledtext):
+        end, newstart = m.span()
+        newstring += filledtext[start:end]
+        rep = "{{ "+ name + ".comp[" + str(counter) + "]|component }}"
+        sol.append(m.group(1)) 
+        newstring += rep
+        start = newstart
+        counter += 1
+    newstring += filledtext[start:]
+    return sol, newstring
