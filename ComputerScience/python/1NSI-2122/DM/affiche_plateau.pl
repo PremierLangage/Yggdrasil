5e375a10bfbc7c15564c23abaac9b06f6fad7804
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-12-20 22:58:23 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM/affiche_plateau.pl

--- ComputerScience/python/1NSI-2122/DM/affiche_plateau.pl
+++ ComputerScience/python/1NSI-2122/DM/affiche_plateau.pl
@@ -17,10 +17,8 @@ doctest==
 ==
 
 pltest==
->>> from unittest.mock import patch #
->>> with patch('builtins.input', side_effect=['Bob', 'Alice']):    joueurs = entree_joueurs() #
->>> joueurs
-['Bob', 'Alice']
+>>> affiche_plateau(['|', '|', '|', '|', '|']) 
+| | | | |
 ==
 
 
