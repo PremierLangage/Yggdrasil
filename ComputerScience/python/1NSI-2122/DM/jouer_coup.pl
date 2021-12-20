8bd8eaed4b48f0e0ecae4edff617e17e1ac41336
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-12-20 21:47:41 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM/jouer_coup.pl

---
+++ ComputerScience/python/1NSI-2122/DM/jouer_coup.pl
@@ -0,0 +1,37 @@
+extends = func.pl
+
+funcname= joueur_coup
+title= Joueur le coup
+
+doctest==
+    la fonction jouer_coup(nb_alumette, plateau) enlève de 
+    la liste plateau (sur place) le nombre d'alumettes nb_alumette.
+    Si le nombre d'alumettes choisis par le joueur est:
+     - inférieur au égal à k, et
+     - inférieur ou égal au nombre d'alumettes disponibles, c'est à dire à la 
+     longueur de la liste plateau, et
+     - supérieur ou égal à 0
+     alors la fonction retourne True
+     sinon la fonction retourne False
+
+    exemple:
+    >>> coup_possible(4, ['|', '|', '|', '|', '|'],3)
+    False
+    >>> coup_possible(3, ['|', '|', '|', '|', '|'],3)
+    True
+    >>> coup_possible(3, ['|', '|'],3)
+    False
+    
+==
+
+pltest==
+>>> coup_possible(4, ['|', '|', '|', '|', '|'],3)
+False
+>>> coup_possible(3, ['|', '|', '|', '|', '|'],3)
+True
+>>> coup_possible(3, ['|', '|'],3)
+False
+==
+
+
+
