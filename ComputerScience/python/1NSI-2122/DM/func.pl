e2181096fdb38762032c5115277a60d47a0c1c82
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-12-20 21:19:04 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM/func.pl

---
+++ ComputerScience/python/1NSI-2122/DM/func.pl
@@ -0,0 +1,28 @@
+
+
+
+
+
+extends = /ComputerScience/python/template/pltest.pl
+
+title= Distribution 
+
+text==
+
+
+Ecrire une fonction **{{funcname}}** qui vérifie le doctest suivant :
+
+{{doctest}}
+
+[https://www.belote.com/regles-et-variantes/les-regles-de-la-belote-classique/  ](https://www.belote.com/regles-et-variantes/les-regles-de-la-belote-classique/)
+
+==
+
+
+pltest==
+>>> deal([],3)
+>>> deal([(1,2),(3,4)],1)
+[(1,2)],[(3,4)]
+==
+
+
