00475c010b0a34952defee1eea51f5a6c3a2ee8d
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-12-20 23:02:49 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM/affiche_plateau.pl

--- ComputerScience/python/1NSI-2122/DM/affiche_plateau.pl
+++ ComputerScience/python/1NSI-2122/DM/affiche_plateau.pl
@@ -8,7 +8,9 @@ funcname= affiche_plateau
 title= Affichage du plateau
 
 doctest==
-    la fonction affiche_plateau(plateau) 
+    la fonction affiche_plateau(plateau) affiche une chaine de 
+    caractère composé des éléments de la liste plateau séparés
+    d'un espace.
 
     exemple:
     >>> affiche_plateau(['|', '|', '|', '|', '|']) 
