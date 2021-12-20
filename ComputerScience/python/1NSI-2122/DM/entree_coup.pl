3103e50b07d058dbe3b5b2c0cf69ae0740e8cb14
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-12-21 00:24:26 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM/entree_coup.pl

--- ComputerScience/python/1NSI-2122/DM/entree_coup.pl
+++ ComputerScience/python/1NSI-2122/DM/entree_coup.pl
@@ -35,7 +35,7 @@ def coup_possible(nb_alumette, plateau,k):
     else:
         return False
 
-pltest=""">>> from unittest.mock import patch #\n>>> with patch('builtins.input', side_effect=['5', '3']):    coup = entree_coup(['Bob', 'Alice'], 1, ['|', '|', '|', '|'], 3) #\n>>> coup\n3"""
+pltest=""">>> a\n>>> from unittest.mock import patch #\n>>> with patch('builtins.input', side_effect=['5', '3']):    coup = entree_coup(['Bob', 'Alice'], 1, ['|', '|', '|', '|'], 3) #\n>>> coup\n3"""
 ==
 
 
