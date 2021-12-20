55ece578945b163c11b3aa751d257222569aa186
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-12-20 22:35:49 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM/entree_joueurs.pl

--- ComputerScience/python/1NSI-2122/DM/entree_joueurs.pl
+++ ComputerScience/python/1NSI-2122/DM/entree_joueurs.pl
@@ -18,7 +18,7 @@ doctest==
 pltest==
 >>> from unittest.mock import patch
 >>> with patch('builtins.input', side_effect=['Bob', 'Alice']):
-        joueurs = entree_joueurs() 
+    joueurs = entree_joueurs() 
 >>> joueurs
 ['Bob', 'Alice']
 ==
