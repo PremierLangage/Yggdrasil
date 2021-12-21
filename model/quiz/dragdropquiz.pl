351951aba9fdf9f0c83ea0ff3f7a133998eb4814
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-21 17:14:49 +0100

    ddoyen1 modified Yggdrasil/model/quiz/dragdropquiz.pl

--- model/quiz/dragdropquiz.pl
+++ model/quiz/dragdropquiz.pl
@@ -33,7 +33,7 @@ for i in range(nbstep):
 
 { row['phrase'] }
 """
-    _, _ = process_filledtext("{AA} et {BB}", f"drp[{i}].comp")
+    ii, _ = process_filledtext("{AA} et {BB}", f"drp[{i}].comp")
     questions.append(qq)
     drp.append(DropGroup(["AA", "BB"]))
     inputblocks.append("{{ drp[" + str(i) + "].comp[0]|component }}")
