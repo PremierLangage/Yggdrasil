70747aa664a6c69a07c83a356f1d2653a8c698cf
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-22 15:09:11 +0100

    ddoyen1 modified Yggdrasil/utils/components/dragdrop.py

--- utils/components/dragdrop.py
+++ utils/components/dragdrop.py
@@ -15,6 +15,22 @@ class DropGroup(MultiComp):
             self.comp = [CustomDragDrop.Drop() for _ in range(len(param))]
             self._sol = param
 
+    def set_filledtext(filledtext):
+        sol = []
+        counter = 0
+        newstring = ''
+        start = 0
+        for m in re.finditer(r"{([^{}]+)}", filledtext):
+            end, newstart = m.span()
+            newstring += filledtext[start:end]
+            rep = "{{ "+ name + ".comp[" + str(counter) + "]|component }}"
+            sol.append(m.group(1)) 
+            newstring += rep
+            start = newstart
+            counter += 1
+        newstring += filledtext[start:]
+        return sol, newstring
+
     def paste_embed(self):
         lst = []
         for comp in self.comp:
