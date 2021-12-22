c3b1f2d66317f0bd029e2e01f8d50ab9c8f14e96
EMMANUEL BRUNACHE <moodleNotif@monlycee.net>
2021-12-22 21:39:31 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM/jeu_de_nim.pl

---
+++ ComputerScience/python/1NSI-2122/DM/jeu_de_nim.pl
@@ -0,0 +1,105 @@
+extends = pltest_before.pl
+# extends = /ComputerScience/python/template/pltest.pl
+
+title= Jeu de Nim complet
+
+befor==
+def init_plateau(n):
+    plateau = ["|"] * n
+    return plateau
+
+def entree_joueurs():
+    joueurs=[]
+    for i in range (2):
+        joueur = input("Entrez le nom du joueur " + str(i + 1) + " ")
+        joueurs.append(joueur)
+    return joueurs
+
+def coup_possible(nb_alumette, plateau,k):
+        if nb_alumette <= len(plateau) and nb_alumette <= k and nb_alumette > 0:
+            return True
+        else:
+            return False
+
+def entree_coup(joueurs, joueur_actif, plateau, k):
+    coup = int(input(joueurs[joueur_actif] + " combien voulez vous en enlever"))
+    while not coup_possible(coup, plateau, 3):
+        coup = int(input(joueurs[joueur_actif] + " combien voulez vous en enlever"))
+    return coup
+
+def affiche_plateau(plateau):
+    print(" ".join(plateau))
+
+def jouer_coup(nb_alumette, plateau):
+    for i in range(nb_alumette):
+        plateau.pop()
+
+def victoire(plateau):
+    if len(plateau)==0:
+        return True
+    return False
+==
+
+text==
+
+
+Ecrire une fonction **tour** qui vérifie le doctest suivant :
+
+    la fonction tour(joueurs, joueur_actif, plateau, k) réalise un tour complet
+    du jeu de nim:
+     1. Demande à entrer un coup au joueur actif joueur_actif.
+     2. Joue le coup choisi par le joueur actif.
+     3. Affiche le plateau.
+     4. Test si il y a une fin de jeu (victoire).
+    Cette fonction retourne True si il y a une fin de jeu (victoire), False sinon.
+
+    Considérez que toutes les fonctions précédemment demandées sont faites et 
+    fonctionnent correctement
+
+    exemple:
+    >>> plateau = ['|', '|', '|', '|']
+    >>> fin = tour(['Bob', 'Alice'], 1, plateau, 3)
+    Alice combien voulez vous en enlever ? 5
+    Impossible.
+    Alice combien voulez vous en enlever ? 3
+    >>> fin
+    False
+    >>> plateau
+    ['|']
+    >>> fin = tour(['Bob', 'Alice'], 0, plateau, 3)
+    Bob combien voulez vous en enlever ? 1
+    >>> fin
+    True
+    >>> plateau
+    []
+
+
+==
+
+
+
+pltest==
+>>> from unittest.mock import patch #
+>>> plateau = ['|', '|', '|', '|'] #
+>>> with patch('builtins.input', side_effect=['5', '3']):
+...    fin = tour(['Bob', 'Alice'], 1, plateau, 3) #
+>>> fin
+False
+>>> plateau
+['|']
+>>> with patch('builtins.input', side_effect=['1']):
+...    fin = tour(['Bob', 'Alice'], 0, plateau, 3) #
+>>> fin
+True
+>>> plateau
+[]
+==
+
+
+
+
+
+
+
+
+
