3014a870b92550eaff5806104a7cf98df2938ea4
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-28 23:16:58 +0100

    ddoyen1 modified Yggdrasil/utils/builderlib.py

--- utils/builderlib.py
+++ utils/builderlib.py
@@ -57,6 +57,9 @@ def getnewcomp(obj):
                 obj[i] = {"cid": item.cid, "name": name, "selector": item.selector} 
             if isinstance(obj[i], (Ex, MultiComp)):
                 newcomp = newcomp + getnewcomp(obj[i])
+    elif isinstance(obj, ExDragDrop):
+        newcomp = newcomp + getnewcomp(obj.drops)
+        newcomp = newcomp + getnewcomp(obj.labels)
     elif isinstance(obj, Ex):
         if isinstance(obj.input, list):
             newcomp = newcomp + getnewcomp(obj.input)
@@ -67,9 +70,7 @@ def getnewcomp(obj):
             obj.input = {"cid": item.cid, "name": name, "selector": item.selector}
     elif isinstance(obj, MultiComp):
         newcomp = newcomp + getnewcomp(obj.comp)
-    elif isinstance(obj, ExDragDrop):
-        newcomp = newcomp + getnewcomp(obj.drops)
-        newcomp = newcomp + getnewcomp(obj.labels)
+
     return newcomp
 
 def comp2dic(obj):
@@ -81,6 +82,9 @@ def comp2dic(obj):
                 obj[i] = {"cid": item.cid, "name": item.name, "selector": item.selector} 
             if isinstance(obj[i], (Ex, MultiComp)):
                 comp2dic(obj[i])
+    elif isinstance(obj, ExDragDrop):
+        comp2dic(obj.drops)
+        comp2dic(obj.labels)
     elif isinstance(obj, Ex):
         if isinstance(obj.input, list):
             comp2dic(obj.input)
@@ -89,9 +93,6 @@ def comp2dic(obj):
             obj.input = {"cid": item.cid, "name": item.name, "selector": item.selector}
     elif isinstance(obj, MultiComp):
         comp2dic(obj.comp)
-    elif isinstance(obj, ExDragDrop):
-        comp2dic(obj.drops)
-        comp2dic(obj.labels)
 
 def dic2comp(obj, dic):
     newcomp = []
