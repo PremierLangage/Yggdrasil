8d642c386e32c2bcc64517fc59d7347a6b89c17e
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-28 14:57:45 +0100

    ddoyen1 modified Yggdrasil/model/basic/temp.pl

--- model/basic/temp.pl
+++ model/basic/temp.pl
@@ -1,7 +1,7 @@
 # HACK pour modifier l'apparence de la page html de l'exercice
 @ /utils/components/multicomp.py 
 @ /utils/components/dragdrop.py [customdragdrop.py]
-@ /utils/exercises/ex.py 
+@ /utils/exercises/ex.py
 @ /utils/exercises/exradio.py
 
 style.page == #|css|
