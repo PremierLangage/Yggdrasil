0ad1d0dc90e3389700bca1c6ec8b67be7617f4ec
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-28 14:54:31 +0100

    ddoyen1 modified Yggdrasil/utils/exercises/exradio.py

--- utils/exercises/exradio.py
+++ utils/exercises/exradio.py
@@ -8,3 +8,58 @@ class ExRadio(Ex):
             self.input = kwargs['input']
         else:
             self.input = Radio()
+            
+    def set_items(self, items):
+        """
+        Set the list of items.
+        """
+        if isinstance(items, str):
+            _items_ = items.splitlines()
+        else:
+            _items_ = items
+        self.items = [{"id": str(uuid4()), "content": str(item)} for item in _items_]
+
+    def set_sol(self, index):
+        """
+        Set the solution (from its index in the list of items).
+        """
+        self._sol = self.items[index]['id']
+
+    def shuffle(self):
+        """
+        Shuffle the list of items.
+        """
+        rd.shuffle(self.items)
+
+    def fill(self, items, indsol=0, shuffled=True):
+        """
+        Set the list of items and the solution.
+        """
+        self.set_items(items)
+        self.set_sol(indsol)
+        if shuffled:
+            self.shuffle()
+
+    def eval(self):
+        """
+        Evaluate the answer.
+        """
+        for item in self.items:
+            id = item['id']
+            if id == self._sol and id == self.selection:
+                self.score = 100
+                return 100
+        self.score = 0
+        return 0
+
+    def show(self):
+        """
+        Display visual feedback.
+        """
+        self.input.show()
+
+    def disable(self):
+        """
+        Disable the component.
+        """
+        self.input.disabled = True            
