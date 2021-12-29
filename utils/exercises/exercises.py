f78fe30c0310c54585c3095b081beacfd306eca5
David DOYEN <david.doyen@univ-eiffel.fr>
2021-12-29 15:06:56 +0100

    ddoyen1 modified Yggdrasil/utils/exercises/exercises.py

--- utils/exercises/exercises.py
+++ utils/exercises/exercises.py
@@ -78,13 +78,13 @@ class ExInput(Ex):
 
     def __init__(self, **kwargs):
         self.__Ex__ = "Input"
+        self.question = kwargs.get('question', '')
         if 'input' in kwargs:
             self.input = kwargs['input']
             self.inputblock = kwargs.get('inputblock', '')
         else:
             self.input = Input()
             self.inputblock = self.input.render()
-        #super().__init__(**kwargs)
             
     def eval(self):
         if self.input.value == self.input._sol:
