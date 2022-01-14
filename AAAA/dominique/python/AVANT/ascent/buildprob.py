2e14be09ce3ca86e0f67db90b27f196f253884e4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-01-14 15:02:53 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/AVANT/ascent/buildprob.py

---
+++ AAAA/dominique/python/AVANT/ascent/buildprob.py
@@ -0,0 +1,37 @@
+
+
+"""
+Ce code permet de fabriquer le fichier de data et de produire les deux solutions.
+
+et le pltest 
+"""
+
+import random
+
+def doit(filename):
+    s="(()()((()()"
+    level=3
+    step= len(s)
+    ssol = -1
+    for i in range(7000):
+        step += 1
+        if random.randint(1,2)== 1:
+            s+="("
+            level +=1 
+        else:
+            s+=")"               
+            level -= 1
+        if level == -1 and ssol == -1:
+            ssol = step
+
+    with open(filename,"w") as f:
+        f.write(s)
+
+    pltest= f"""
+    >>> question1()
+    le Père Noël arrive au {level} étage
+    >>> question2()
+    Aux {ssol} ième le Père Noïl entre au sous-sol
+    """
+
+    return pltest
