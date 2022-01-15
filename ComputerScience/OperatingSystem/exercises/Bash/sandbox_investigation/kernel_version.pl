b14f41feaadf8e521e478d376432cba7afa2ca63
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2022-01-15 20:40:32 +0100

    nborie1 modified Yggdrasil/ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl

--- ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl
+++ ComputerScience/OperatingSystem/exercises/Bash/sandbox_investigation/kernel_version.pl
@@ -47,7 +47,9 @@ décrivant la révision du noyau (il n'a pas que des chiffres et des points norm
 Pour cela, utiliser 
 <a href="https://pl.u-pem.fr/filebrowser/demo/38096/" target="blank">
 l'émulateur de terminal sur sandbox
-</a>.
+</a>. Attention, PLaTon n'est pas multi-onglet. Il faut bien fermer vos onglets sur 
+l'émulateur de terminal et actualiser l'énoncé de l'exercice avant de soumettre votre 
+réponse.
 
 ==
 
