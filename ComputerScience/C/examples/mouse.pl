d6d1070c13aa018d3e340f53c0b00f04724f5b3b
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2022-01-16 11:29:30 +0100

    nborie1 modified Yggdrasil/ComputerScience/C/examples/mouse.pl

--- ComputerScience/C/examples/mouse.pl
+++ ComputerScience/C/examples/mouse.pl
@@ -66,7 +66,7 @@ evaluator==#|python|
 if 2 == int(inputbox.value):
     grade = (100, '''<span class="success-state">Mais oui, c'est clair!</span> <br> <br>''')
 else:
-    grade = (0, '''<span class="error-state">Regardez mieux! Le chat, c'est celui qui a le plus de poil...</span> <br> <br>''')
+    grade = (0, '''<span class="error-state">Regardez mieux! Le chat, c'est celui qui a le plus de poils...</span> <br> <br>''')
 ==
 
 
