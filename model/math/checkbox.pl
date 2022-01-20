418de749d99e297c786a8679750475ab3924e161
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-20 17:35:30 +0100

    ddoyen1 modified Yggdrasil/model/math/checkbox.pl

--- model/math/checkbox.pl
+++ model/math/checkbox.pl
@@ -2,8 +2,6 @@ extends = /model/basic/checkbox.pl
 
 @ /model/math/jinja_math.py [jinja_env.py]
 @ /model/math/json_math.py [json_encoder.py]
-@ /utils/components/mathinput/mathinput.py
-@ /utils/components/mathinput/mathinput.html
 @ /utils/sympy/evalsympy.py
 @ /utils/sympy/latex2sympy.py
 @ /utils/sympy/sympy2latex.py
