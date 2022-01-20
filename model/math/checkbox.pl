c088b049777a1063c6e4368137319d016c012497
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-20 17:39:18 +0100

    ddoyen1 modified Yggdrasil/model/math/checkbox.pl

--- model/math/checkbox.pl
+++ model/math/checkbox.pl
@@ -8,3 +8,22 @@ extends = /model/basic/checkbox.pl
 @ /utils/sympy/randsympy.py
 @ /utils/components/mathinput/mathinput.py
 @ /utils/components/mathinput/mathinput.html
+
+before_scripts % ["mathimport", "importfunc", "before", "process"]
+text =
+form = 
+latexsettings.ln_notation = True
+latexsettings.inv_trig_style = full
+
+mathimport ==
+from sympy import E, I, pi, oo
+from sympy import sqrt, Abs, sin, cos, tan, exp, ln
+from sympy import Symbol, symbols, var
+from sympy import sympify, simplify, Lambda
+from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval
+from sympy import integrate
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from sympy2latex import latex
+from latex2sympy import latex2sympy
+==
