d15f7ed11cba7e5f904bed351bc30ab695d5c559
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-20 17:35:22 +0100

    ddoyen1 modified Yggdrasil/model/math/checkbox.pl

--- model/math/checkbox.pl
+++ model/math/checkbox.pl
@@ -1,53 +1,10 @@
-extends = /model/math/math.pl
-extends = /model/basic/temp.pl
+extends = /model/basic/checkbox.pl
 
-@ /utils/components/scoring.py
-@ /utils/components/checkbox.py [checkbox.py]
-
-doc == #|python|
-{
-    "name": "checkbox",
-    "keys": {
-        "items": {
-            "type": "(str, list)",
-            "default": "",
-            "description": "Liste des items."
-        },
-        "indsol": {
-            "type": "list",
-            "default": [],
-            "description": "Indices des solutions dans la liste des items."
-        },
-        "shuffled": {
-            "type": "bool",
-            "default": True,
-            "description": "Valeur indiquant si les items seront mélangés."
-        }
-    }
-}
-==
-
-checkbox =: CheckboxGroup
-checkbox.decorator = Checkbox
-
-shuffled = True
-
-process ==
-from ast import literal_eval
-
-indsol = literal_eval(str(indsol))
-shuffled = literal_eval(str(shuffled))
-
-checkbox.set_items(items)
-checkbox.set_sol(indsol)
-if shuffled:
-    checkbox.shuffle()
-==
-
-inputblock ==
-{{ checkbox|component }}
-==
-
-evaluator ==
-score = checkbox.eval()
-==
+@ /model/math/jinja_math.py [jinja_env.py]
+@ /model/math/json_math.py [json_encoder.py]
+@ /utils/components/mathinput/mathinput.py
+@ /utils/components/mathinput/mathinput.html
+@ /utils/sympy/evalsympy.py
+@ /utils/sympy/latex2sympy.py
+@ /utils/sympy/sympy2latex.py
+@ /utils/sympy/randsympy.py
