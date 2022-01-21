361995d734641c6e3f127fc716ab375579da232d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 13:55:39 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_numbers.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_numbers.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_numbers.pl
@@ -10,8 +10,8 @@ title = Déterminer si une proposition est vraie
 before ==
 n, m = 40, 5
 numbers = set(range(m, n - m + 1))
-even = set(range(0, m + 1, 2)).intersection(numbers)
-odd = set(range(1, m + 1, 2)).intersection(numbers)
+even = set(range(0, n + 1, 2)).intersection(numbers)
+odd = set(range(1, n + 1, 2)).intersection(numbers)
 case = randint(1, 4)
 
 k = randint(3, 4)
