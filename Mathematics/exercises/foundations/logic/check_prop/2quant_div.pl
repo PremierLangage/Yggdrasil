20103e4f6c03a588d80fba532f8281d823e479de
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 20:03:56 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl

--- Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
+++ Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
@@ -10,17 +10,24 @@ title = Déterminer si une proposition est vraie
 before ==
 import numpy as np
 res = 4*[0]
+res2 = 5*[0]
 for i in range(1000):
     A = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 3))
     B = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 3))
+    k = 0
     if any(all([b % a == 0 for a in A]) for b in B):
+        k = k + 1
         res[0] += 1
     if any(all([a % b == 0 for a in A]) for b in B):
+        k = k + 1
         res[1] += 1
     if all(any([b % a == 0 for a in A]) for b in B):
+        k = k + 1
         res[2] += 1
     if all(any([a % b == 0 for a in A]) for b in B):
+        k = k + 1
         res[3] += 1
+    res2[k]+=1
 items, indsol = [], []
 
 ==
