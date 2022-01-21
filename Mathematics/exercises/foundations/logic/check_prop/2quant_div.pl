44b8b17f370d9fa0ef56a283673f3cd4f6614335
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 20:19:05 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl

--- Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
+++ Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
@@ -8,11 +8,11 @@ extends = /model/math/checkbox.pl
 title = Déterminer si une proposition est vraie
 
 before ==
+items, indsol = [], []
+A = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
+B = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
 k = 0
-for i in range(1):
-    items, indsol = [], []
-    A = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
-    B = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
+for q in range(3):
     if q == 0:
         items.append(r"\exists b \in B,\ \forall a \in A, a|b")
         valprop = any(all([b % a == 0 for a in A]) for b in B):
