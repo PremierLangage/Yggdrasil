117c2b8edb98b1a01eeac50f1d2f339c9bcb9008
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 20:18:13 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl

--- Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
+++ Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
@@ -15,33 +15,19 @@ for i in range(1):
     items, indsol = [], []
     A = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
     B = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
-    if q == 1:
+    if q == 0:
         items.append(r"\exists b \in B,\ \forall a \in A, a|b")
         valprop = any(all([b % a == 0 for a in A]) for b in B):
-    elif q == 2:
+    elif q == 1:
         items.append(r"\exists b \in B,\ \forall a \in A, b|a")
         valprop = any(all([a % b == 0 for a in A]) for b in B):
-        k = k + 1
-        res[1] += 1
-    if all(any([b % a == 0 for a in A]) for b in B):
-        k = k + 1
-        res[2] += 1
-    if all(any([a % b == 0 for a in A]) for b in B):
-        k = k + 1
-        res[3] += 1
-    if any(all([b % a == 0 for a in B]) for b in A):
-        k = k + 1
-        res[4] += 1
-    if any(all([a % b == 0 for a in B]) for b in A):
-        k = k + 1
-        res[5] += 1
-    if all(any([b % a == 0 for a in B]) for b in A):
-        k = k + 1
-        res[6] += 1
-    if all(any([a % b == 0 for a in B]) for b in A):
-        k = k + 1
-        res[7] += 1
-    res2[k]+=1
+    elif q == 2:
+        items.append(r"\forall b \in B,\ \exists a \in A, a|b")
+        valprop = all(any([b % a == 0 for a in A]) for b in B):
+    elif q == 3:
+        items.append(r"\forall b \in B,\ \exists a \in A, b|a")
+        valprop = all(any([a % b == 0 for a in A]) for b in B):
+
 
 ==
 
