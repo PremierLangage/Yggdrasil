4990ed43ea09d64769f8f74e962a567504b81186
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 20:16:33 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl

--- Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
+++ Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
@@ -11,14 +11,16 @@ before ==
 import numpy as np
 res = 8*[0]
 res2 = 9*[0]
-for i in range(1000):
+for i in range(1):
+    items, indsol = [], []
     A = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
     B = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
-    k = 0
-    if any(all([b % a == 0 for a in A]) for b in B):
-        k = k + 1
-        res[0] += 1
-    if any(all([a % b == 0 for a in A]) for b in B):
+    if q == 1:
+        items.append(r"\exists b \in B,\ \forall a \in A, a|b")
+        valprop = any(all([b % a == 0 for a in A]) for b in B):
+    elif q == 2:
+        items.append(r"\exists b \in B,\ \forall a \in A, b|a")
+        valprop = any(all([a % b == 0 for a in A]) for b in B):
         k = k + 1
         res[1] += 1
     if all(any([b % a == 0 for a in A]) for b in B):
@@ -27,7 +29,6 @@ for i in range(1000):
     if all(any([a % b == 0 for a in A]) for b in B):
         k = k + 1
         res[3] += 1
-
     if any(all([b % a == 0 for a in B]) for b in A):
         k = k + 1
         res[4] += 1
@@ -41,7 +42,6 @@ for i in range(1000):
         k = k + 1
         res[7] += 1
     res2[k]+=1
-items, indsol = [], []
 
 ==
 
