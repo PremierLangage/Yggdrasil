afb0e46b22c6cd8b7c2f14b6faacde093efa36ed
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 16:15:23 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl
@@ -20,6 +20,51 @@ nf = len(f)
 inveven = [x if f[x] % 2 == 0 for x in range(nf)]
 invodd = [x if f[x] % 2 == 1 for x in range(nf)]
 
+k = randint(3, 4)
+if case == 1:
+    A = sample(invodd, k)
+elif case == 2:
+    A = sample(invodd, k)
+else:
+    A = sample(numbers, k)
+
+minA, maxA = min(A), max(A)
+items, indsol = [], []
+
+k = 0
+for q in [randint(0, 1), randint(2, 3), randint(4, 5), randint(6, 7)]:
+    if q == 0:
+        items.append(r"$! \exists x \in A,\ x \textrm{ est pair} !$")
+        valprop = any([x%2 == 0 for x in A])
+    elif q == 1:
+        items.append(r"$! \exists x \in A,\ x \textrm{ est impair} !$")
+        valprop = any([x%2 != 0 for x in A])
+    elif q == 2:
+        items.append(r"$! \forall x \in A,\ x \textrm{ est pair} !$")
+        valprop = all([x%2 == 0 for x in A])
+    elif q == 3:
+        items.append(r"$! \forall x \in A,\ x \textrm{ est impair} !$")
+        valprop = all([x%2 != 0 for x in A])
+    elif q == 4:
+        d = choice([randint(minA, maxA), randint(maxA, n)])
+        items.append(rf"$! \exists x \in A,\ x \ge {d} !$")
+        valprop = any([x >= d for x in A])
+    elif q == 5:
+        d = choice([randint(1, minA), randint(minA, maxA)])
+        items.append(rf"$! \exists x \in A,\ x \le {d} !$")
+        valprop = any([x <= d for x in A])
+    elif q == 6:
+        d = choice([randint(1, minA), randint(minA, maxA)])
+        items.append(rf"$! \forall x \in A,\ x \ge {d} !$")
+        valprop = all([x >= d for x in A])
+    elif q == 7:
+        d = choice([randint(minA, maxA), randint(maxA, n)])
+        items.append(rf"$! \forall x \in A,\ x \le {d} !$")
+        valprop = all([x <= d for x in A])
+    if valprop:
+        indsol.append(k)
+    k += 1
+
 indsol=[]
 items =['', '']
 
