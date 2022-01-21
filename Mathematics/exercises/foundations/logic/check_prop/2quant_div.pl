1a47ec00978499ba829dff5e413ce9d5e09ebc02
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 19:56:36 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl

--- Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
+++ Mathematics/exercises/foundations/logic/check_prop/2quant_div.pl
@@ -9,6 +9,7 @@ title = Déterminer si une proposition est vraie
 
 before ==
 import numpy as np
+res = 4*[0]
 for i in range(1000):
     A = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
     B = sample([2, 3, 4, 5, 6, 10, 12, 15, 20, 24, 30], randint(2, 4))
@@ -16,14 +17,21 @@ for i in range(1000):
     gcdB = np.gcd.reduce(B)
     lcmA = np.lcm.reduce(A)
     lcmB = np.lcm.reduce(B)
-    
+    if any(all([a for a in A if b % a == 0]) for b in B)):
+        res[0] += 1
+    if any(all([a for a in A if a % b == 0]) for b in B)):
+        res[1] += 1
+    if all(any([a for a in A if b % a == 0]) for b in B)):
+        res[0] += 1
+    if all(any([a for a in A if a % b == 0]) for b in B)):
+        res[1] += 1
 items, indsol = [], []
 
 ==
 
 question ==
 On considère l'ensemble 
-$$ {{ A|latex }}. $$
+$$ {{ res }}. $$
 Sélectionner les propositions qui sont vraies dans la liste ci-dessous.
 ==
 
