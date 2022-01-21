49a720702178107c0e6d3f4d2805a8bb0547cfbf
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 17:32:35 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl
@@ -12,7 +12,7 @@ n, m = 40, 5
 numbers = set(range(m, n - m + 1))
 even = set(range(0, n + 1, 2)).intersection(numbers)
 odd = set(range(1, n + 1, 2)).intersection(numbers)
-
+x = 0
 neven = randint(3, 4)
 nodd = randint(3, 4)
 f = choices(list(even), k=neven) + choices(list(odd), k=nodd)
