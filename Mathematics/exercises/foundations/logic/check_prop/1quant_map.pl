32c701553846b29435b1e7df5f5b26e5af1234b8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-21 18:05:11 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_map.pl
@@ -41,29 +41,29 @@ for q in [randint(0, 1), randint(2, 3), randint(4, 5), randint(6, 7)]:
         items.append(r"$! \exists x \in %s ,\ f(x) \textrm{ est pair} !$" % latexA)
         valprop = any([f[x]%2 == 0 for x in A])
     elif q == 1:
-        items.append(r"$! \exists x \in %s,\ x \textrm{ est impair} !$" % latexA)
+        items.append(r"$! \exists x \in %s,\ f(x) \textrm{ est impair} !$" % latexA)
         valprop = any([f[x]%2 != 0 for x in A])
     elif q == 2:
-        items.append(r"$! \forall x \in %s,\ x \textrm{ est pair} !$" % latexA)
+        items.append(r"$! \forall x \in %s,\ f(x) \textrm{ est pair} !$" % latexA)
         valprop = all([f[x]%2 == 0 for f[x] in A])
     elif q == 3:
-        items.append(r"$! \forall x \in %s,\ x \textrm{ est impair} !$" % latexA)
+        items.append(r"$! \forall x \in %s,\ f(x) \textrm{ est impair} !$" % latexA)
         valprop = all([f[x]%2 != 0 for x in A])
     elif q == 4:
         d = choice([randint(minA, maxA), randint(maxA, n)])
-        items.append(r"$! \exists x \in %s,\ x \ge %s !$" % (latexA, d))
+        items.append(r"$! \exists x \in %s,\ f(x) \ge %s !$" % (latexA, d))
         valprop = any([f[x] >= d for x in A])
     elif q == 5:
         d = choice([randint(1, minA), randint(minA, maxA)])
-        items.append(r"$! \exists x \in %s,\ x \le %s !$" % (latexA, d))
+        items.append(r"$! \exists x \in %s,\ f(x) \le %s !$" % (latexA, d))
         valprop = any([f[x] <= d for x in A])
     elif q == 6:
         d = choice([randint(1, minA), randint(minA, maxA)])
-        items.append(r"$! \forall x \in %s,\ x \ge %s !$" % (latexA, d))
+        items.append(r"$! \forall x \in %s,\ f(x) \ge %s !$" % (latexA, d))
         valprop = all([f[x] >= d for x in A])
     elif q == 7:
         d = choice([randint(minA, maxA), randint(maxA, n)])
-        items.append(r"$! \forall x \in %s,\ x \le %s !$" % (latexA, d))
+        items.append(r"$! \forall x \in %s,\ f(x) \le %s !$" % (latexA, d))
         valprop = all([f[x] <= d for x in A])
     if valprop:
         indsol.append(k)
