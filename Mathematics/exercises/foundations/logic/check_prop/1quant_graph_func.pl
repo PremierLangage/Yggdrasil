caef177ac8c03269211060a4d575f41b73203189
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-27 20:43:01 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
@@ -1,8 +1,19 @@
 extends = /model/math/expr.pl
 
 before ==
+import scipy
 from plmpl import fig2svg, easyplot
 import matplotlib.pyplot as plt
+
+import numpy as np
+
+x = [0, 1, 9]
+y = [-1, 3, 4]
+f = interpolate.PchipInterpolator(x, y)
+
+xnew = np.arange(0,9,0.1)
+import matplotlib.pyplot as plt
+plt.plot(x,y,'',xnew,f(xnew),'-')
 
 var('x')
 a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
@@ -15,7 +26,6 @@ plt.xlim(-n, n)
 plt.grid(True)
 plt.xticks(range(-n, n+1))
 plt.yticks(range(-10, 10, 2))
-easyplot(plt.gcf(), f, -n, n)
 image = fig2svg(plt.gcf())
 ==
 
