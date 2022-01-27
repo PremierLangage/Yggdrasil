8e8b55597b01a9cca40cd0b11ffb9f4b6006de1a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-27 19:29:47 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 var('x')
 a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
 b = randint(-3, 3)
-sol = a*x + b
+f = (x-2)**2 + 1
 
 n = 5
 plt.clf()
@@ -16,7 +16,7 @@ plt.ylim(-n, n)
 plt.grid(True)
 plt.xticks(range(-n, n+1))
 plt.yticks(range(-n, n+1))
-easyplot(plt.gcf(), sol, -n, n)
+easyplot(plt.gcf(), f, -n, n)
 image = fig2svg(plt.gcf())
 ==
 
