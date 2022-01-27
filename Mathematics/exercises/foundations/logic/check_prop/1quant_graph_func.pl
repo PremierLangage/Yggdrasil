aa60326fd72e4d47e6c3a88237ebbc470d18d8e2
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-27 20:44:50 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
@@ -15,17 +15,6 @@ f = interpolate.PchipInterpolator(x, y)
 xnew = np.arange(0,9,0.1)
 import matplotlib.pyplot as plt
 
-var('x')
-a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
-b = randint(-3, 3)
-f = (x-2)**2 + 1
-sol =  0
-n = 3
-plt.clf()
-plt.xlim(-n, n)
-plt.grid(True)
-plt.xticks(range(-n, n+1))
-plt.yticks(range(-10, 10, 2))
 plt.plot(x,y,'',xnew,f(xnew),'-')
 image = fig2svg(plt.gcf())
 ==
