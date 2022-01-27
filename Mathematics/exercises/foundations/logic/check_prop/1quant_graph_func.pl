ec8e645fea2eee34771f12bf14b1fe6cd766a5ab
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-27 19:28:46 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl

---
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
@@ -0,0 +1,33 @@
+extends = /model/math/expr.pl
+
+before ==
+from plmpl import fig2svg, easyplot
+import matplotlib.pyplot as plt
+
+var('x')
+a = choice([-1, 1]) * choice([Rational(1, 2), 1, Rational(3, 2), 2])
+b = randint(-3, 3)
+sol = a*x + b
+
+n = 5
+plt.clf()
+plt.xlim(-n, n)
+plt.ylim(-n, n)
+plt.grid(True)
+plt.xticks(range(-n, n+1))
+plt.yticks(range(-n, n+1))
+easyplot(plt.gcf(), sol, -n, n)
+image = fig2svg(plt.gcf())
+==
+
+question ==
+{{t}} {{s}}
+Déterminer l'équation de la droite tracée ci-dessous (en notant $! x !$ la variable d'abscisse et $! y !$ la variable d'ordonnée).
+<div class="img w60">
+{{ image }}
+</div>
+==
+
+prefix ==
+$! y = !$
+==
