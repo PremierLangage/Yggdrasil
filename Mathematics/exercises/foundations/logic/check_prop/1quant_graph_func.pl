4d3d2a8d21b3c2186f827ebd02ff17f82816b6fc
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-28 10:56:06 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
@@ -53,7 +53,7 @@ image = fig2svg(plt.gcf())
 ==
 
 question ==
-{{minf}} {{maxf}} {{t}}
+{{a1, b1}} {{a2, b2}}
 Déterminer l'équation de la droite tracée ci-dessous (en notant $! x !$ la variable d'abscisse et $! y !$ la variable d'ordonnée).
 <div class="img w60">
 {{ image }}
