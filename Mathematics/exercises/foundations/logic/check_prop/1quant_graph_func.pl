aa11d26023a76437cb6e2d32f201011494c70372
David DOYEN <david.doyen@univ-eiffel.fr>
2022-01-28 15:23:35 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl

--- Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
+++ Mathematics/exercises/foundations/logic/check_prop/1quant_graph_func.pl
@@ -7,17 +7,18 @@ from sympy.polys.polyfuncs import interpolate
 from sympy.calculus.util import minimum, maximum
 
 var('x')
-# pts = []
-# while len(pts) < 10:
+# lst_pts = []
+# while len(lst_pts) < 10:
 #    x0, x1, x2, x3 = randint(-5, -4), randint(-2,-1), randint(1,2), randint(4,5)
 #    y0, y1, y2, y3 = randint(-4, 4), randint(-4, 4), randint(-4, 4), randint(-4, 4)
 #    f = interpolate([(x0, y0), (x1, y1), (x2, y2), (x3, y3)], x)
 #    minf = minimum(f, x, Interval(-5,5)).evalf()
 #    maxf = maximum(f, x, Interval(-5,5)).evalf()
 #    if minf > -5 and maxf < 5:
-#        pts.append([(x0, y0), (x1, y1), (x2, y2), (x3, y3)])
+#        lst_pts.append([(x0, y0), (x1, y1), (x2, y2), (x3, y3)])
 
 pts = choice([[(-4, 0), (-2, -3), (1, -1), (4, 2)], [(-5, 4), (-1, -1), (2, 0), (4, 1)], [(-5, 1), (-2, 2), (2, 2), (5, -1)], [(-5, -4), (-1, -3), (2, -1), (5, 0)], [(-5, -1), (-1, 1), (1, 2), (4, 4)], [(-4, -4), (-1, -1), (1, 3), (5, -4)], [(-5, -2), (-2, 0), (2, 0), (4, 2)], [(-5, 3), (-1, -2), (2, 1), (5, 0)], [(-4, -2), (-2, -1), (2, 0), (5, 0)], [(-4, 3), (-2, 0), (2, -4), (5, 2)]])
+f = interpolate(pts, x)
 
 len1 = randint(2, 6)
 a1 = randint(-5, 5-len1)
