0e5135e191c369ae6f6054cec985994b33a41275
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-01 19:36:20 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/logic/check_prop/2quant_grades.pl

--- Mathematics/exercises/foundations/logic/check_prop/2quant_grades.pl
+++ Mathematics/exercises/foundations/logic/check_prop/2quant_grades.pl
@@ -22,7 +22,7 @@ mintable = min([min(line) for line in table])
 maxtable = max([max(line) for line in table])
 
 k = 0
-for q in [2, 3]:
+for q in [4, 5]:
     valprop = choice([True, False])
     if q == 0:
         if valprop:
@@ -48,7 +48,30 @@ for q in [2, 3]:
         else:
             c = choice([maxmin_mat+1, maxmin_mat+2])
         items.append(rf"$! \exists m \in M, \forall e \in E,\ \textrm{{note}}(e, m) \ge {c} !$")
-      
+    if q == 4:
+        if valprop:
+            c = choice([minmax_el, minmax_el-1])
+        else:
+            c = choice([minmax_el+1, minmax_el+2])
+        items.append(rf"$! \forall e \in E, \exists m \in M,\ \textrm{{note}}(e, m) \ge {c} !$")
+    elif q == 5:
+        if valprop:
+            c = choice([maxmin_el+1, maxmin_el+2])
+        else:
+            c = choice([maxmin_el, maxmin_el-1])
+        items.append(rf"$! \forall e \in E, \exists m \in M,\ \textrm{{note}}(e, m) \le {c} !$")
+    if q == 6:
+        if valprop:
+            c = choice([minmax_mat, minmax_mat+1])
+        else:
+            c = choice([minmax_mat-1, minmax_mat-2])
+        items.append(rf"$! \forall m \in M, \exists e \in E,\ \textrm{{note}}(e, m) \ge {c} !$")
+    elif q == 7:
+        if valprop:
+            c = choice([maxmin_mat, maxmin_mat-1])
+        else:
+            c = choice([maxmin_mat+1, maxmin_mat+2])
+        items.append(rf"$! \forall m \in M, \exists e \in E,\ \textrm{{note}}(e, m) \le {c} !$")      
     if valprop:
         indsol.append(k)
     k += 1
