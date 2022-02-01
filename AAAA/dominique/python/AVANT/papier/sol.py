ca8df173d7b20a55eb29f036693be9d8ab633c3d
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-02-01 16:16:04 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/AVANT/papier/sol.py

---
+++ AAAA/dominique/python/AVANT/papier/sol.py
@@ -0,0 +1,58 @@
+
+
+def deuxpluspetits(a,b,c):
+    """
+    >>> deuxpluspetits(1,2,3)
+    [1, 2]
+    >>> deuxpluspetits(2,2,2)
+    [2, 2]
+    """
+    l = [a,b,c]
+    l.remove(max(l))
+    return l
+
+
+def paperbox():
+    import random
+    
+    a,b,c = [random.randint(1,30),random.randint(1,30),random.randint(1,30)]
+    cotes = [a*b,a*c,b*c]
+    m = min(cotes)
+    
+    ruban = 2*sum(deuxpluspetits(a,b,c))+a*b*c
+    return 2*(sum(cotes))+m, f"{a}x{b}x{c}", ruban
+
+def doit(filename):
+    total=0
+    totalr = 0
+    with open(filename,"w") as f:
+        for i in range(8000):
+            p,t,r = paperbox()
+            print(t,file=f)
+            total += p
+            totalr += r
+    pltest= f"""
+    >>> Question1()== {total} #
+    True
+    >>> Question2()==  {totalr} #
+    True
+    """
+
+    return pltest
+
+                
+if __name__== "__main__":
+
+    with open("data","r") as f:
+        total = 0
+        truban = 0
+        for line in f:
+            a,b,c = [int(c) for c in line.strip().split("x")]
+            cotes = [a*b,a*c,b*c]
+            m = min(cotes)
+            papier = 2*(sum(cotes))+m
+            total += papier
+            truban += 2*sum(deuxpluspetits(a,b,c))+a*b*c
+   # print(total)
+   # print(truban)
+
