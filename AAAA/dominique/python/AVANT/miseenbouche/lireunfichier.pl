2fbbd07cd4c130d9b2e03a5c6a9cba009a453424
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-02-01 13:59:54 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/AVANT/miseenbouche/lireunfichier.pl

--- AAAA/dominique/python/AVANT/miseenbouche/lireunfichier.pl
+++ AAAA/dominique/python/AVANT/miseenbouche/lireunfichier.pl
@@ -14,33 +14,47 @@ extends = /ComputerScience/python/template/pltest.pl
 text==
 L'objectif de cet exercice est de verifier que vous êtes capable lire et d'écrire dans un fichier.
 
-Vous avez {n} fonctions a écrire :
+Vous avez 2 fonctions a écrire :
 
-func1 qui lit le fichier "data1.txt" et qui compte les lignes 
+func1 qui lit le fichier "data1.txt" et qui retourne le nombre de lignes du fichier
 
+func2  lit  "data2.txt" qui contient des entiers (un par ligne) retourne la somme des ces entiers.
+
+func3  qui lit le fichier  "data2.txt" et recopie dans un fichier "data3.txt" tous les entiers pairs.
 
 
 ==
 
 
 before==
-def createfilewithlignes(filename,n,text=""):
+def createfilewithlignes(filename,n,fun= lambda x:x):
     with open(filename, "w") as f :
         for i in range(n):
-            print("ligne ",i,text, file=f)
+            print("ligne ",i,fun('hehe'), file=f)
 
 import random
 a=random.randint(5,123)
-createfilewithlignes("data1.txt",a,text="Hehe")
+createfilewithlignes("data1.txt",a,fun= lambda x: return "hehe" )
 
+createfilewithlignes("data2.txt",a,fun= lambda x: return random.randint(5,200))
+
+with open("data2.txt","r") as f:
+    for l in f:
+        total += int(l.strip())
 
 pltest0= f"""
-
->>> func1()
-{a}
 >>> func1() == {a}
 True
 """
+
+pltest0= f"""
+>>> func2()
+{total}
+>>> func2() == {total}
+True
+"""
+
+
 
 ==
 
