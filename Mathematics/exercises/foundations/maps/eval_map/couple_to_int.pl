feca35c5e2197292ee2eb284ebfff2f019383d42
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-04 15:18:03 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/foundations/maps/eval_map/couple_to_int.pl

---
+++ Mathematics/exercises/foundations/maps/eval_map/couple_to_int.pl
@@ -0,0 +1,30 @@
+extends = /model/math/tuple.pl
+
+title = Antécédent
+
+before ==
+v1, v2 = sorted(sample(['n', 'm', 'p', 'q'], 2))
+a = Symbol(v1)
+b = Symbol(v2)
+
+[2*a - b, a**2+b, b**2 a*b, (a+1)*b, (a-1)*b]
+
+expr = choice(lst)
+f = Lambda((a, b), expr)
+
+x1 = randint(-4, 4)
+x2 = randint(-4, 4)
+sol = f(x1, x2)
+==
+
+question ==
+On considère la fonction $! f : \mathbb{Z} \times \mathbb{Z} \rightarrow \mathbb{Z} !$ telle que
+$$f( {{ v1 }}, {{ v2 }})= {{ expr|latex }}$$
+Déterminer $! f ( {{ x1 }}, {{ x2 }}) !$
+==
+
+
+
+solution ==
+La solution est.
+==
