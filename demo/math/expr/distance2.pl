43a91142cdb70a7795fa24efd8f3f26976a2c0cf
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-08 19:04:22 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/distance2.pl

--- demo/math/expr/distance2.pl
+++ demo/math/expr/distance2.pl
@@ -2,11 +2,4 @@ extends = /model/math/expr2.pl
 
 title = Calculer la distance entre deux points du plan
 
-before ==
-xA = randint(-5, 5)
 
-==
-
-question == 
-Dans le plan muni d'un repère orthonormé on considère les points de coordonnées 
-==
