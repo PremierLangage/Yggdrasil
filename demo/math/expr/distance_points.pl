e48e46bb0b1e0e1de13a05c1e4e9c5aa227b5aca
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-08 17:49:04 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/distance_points.pl

--- demo/math/expr/distance_points.pl
+++ demo/math/expr/distance_points.pl
@@ -2,3 +2,17 @@ extends = /model/math/expr2.pl
 
 title = Calculer la distance entre deux points du plan
 
+before ==
+xA = randint(-5, 5)
+yA = randint(-5, 5)
+xB = randint(-5, 5)
+yB = randint(-5, 5)
+sol = sqrt((xA-xB)**2 + (yA-yB)**2)
+==
+
+question == 
+Dans le plan muni d'un repère orthonormé on considère les points de coordonnées 
+$! {{ (xA, yA) }} !$ et $! {{ (xB, yB) }}. !$
+<br>
+Quelle est la distance entre ces deux points ?
+==
