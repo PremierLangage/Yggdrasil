159284eef1772c80a4b46ad9a7f5fa0ac2cae3a0
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-08 19:41:13 +0100

    ddoyen1 modified Yggdrasil/model/math/input2.pl

--- model/math/input2.pl
+++ model/math/input2.pl
@@ -4,7 +4,18 @@ extends = /model/basic/temp.pl
 jinja_keys = ["inputblock"]
 
 before ==
-x = 1
+xA = randint(-5, 5)
+yA = randint(-5, 5)
+xB = randint(-5, 5)
+yB = randint(-5, 5)
+sol = sqrt((xA-xB)**2 + (yA-yB)**2)
+==
+
+question == 
+Dans le plan muni d'un repère orthonormé on considère les points de coordonnées 
+$! {{ (xA, yA) }} !$ et $! {{ (xB, yB) }}. !$
+<br>
+Quelle est la distance entre ces deux points ?
 ==
 
 keypad = ["+infty", "-infty"]
