4b1d21957b3bd4079e78a62da521b5d8b702642d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-08 17:49:43 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/distance_points.pl

--- demo/math/expr/distance_points.pl
+++ demo/math/expr/distance_points.pl
@@ -1,2 +1,11 @@
 extends = /model/math/expr2.pl
 
+title = Calculer la distance entre deux points du plan
+
+before ==
+xA = randint(-5, 5)
+yA = randint(-5, 5)
+xB = randint(-5, 5)
+yB = randint(-5, 5)
+sol = sqrt((xA-xB)**2 + (yA-yB)**2)
+==
