49c44071d385a87784973753309f0d42099553e4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-08 17:16:19 +0100

    ddoyen1 modified Yggdrasil/model/math/expr2.pl

---
+++ model/math/expr2.pl
@@ -0,0 +1,21 @@
+extends = /model/math/input2.pl
+
+checkratsimp = True
+symbol_dict = {'e': E}
+unauthorized_func = []
+equality = ""
+modulo = 0
+
+evaluator ==
+from ast import literal_eval
+from sympy import sympify
+from evalsympy import eval_expr
+if modulo != 0:
+    modulo = sympify(modulo)
+score, error = eval_expr(answers['math'], sol, checkratsimp=checkratsimp,equality=equality, unauthorized_func=unauthorized_func, modulo=modulo, local_dict=sympify(symbol_dict))
+feedback = message[error]
+==
+
+solution ==
+La solution est $! {{ sol|latex }} !$.
+==
