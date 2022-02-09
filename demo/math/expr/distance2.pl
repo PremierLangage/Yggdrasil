88ca30aeabed28fd4c147629a134272ae87e026a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-09 15:46:01 +0100

    ddoyen1 modified Yggdrasil/demo/math/expr/distance2.pl

--- demo/math/expr/distance2.pl
+++ demo/math/expr/distance2.pl
@@ -3,6 +3,10 @@ extends = /model/math/expr2.pl
 title = Calculer la distance entre deux points du plan
 
 before ==
+input1 = MathInput()
+input1.prefix = "Réponse 1 :"
+input2 = MathInput()
+input2.prefix = "Réponse 1 :"
 xA = randint(-5, 5)
 yA = randint(-5, 5)
 xB = randint(-5, 5)
