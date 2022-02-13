537d29a865cc7bccd4e0465dc76b99938a63eb99
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-13 23:50:34 +0100

    ddoyen1 modified Yggdrasil/utils/components/mathinput/mathinput.py

--- utils/components/mathinput/mathinput.py
+++ utils/components/mathinput/mathinput.py
@@ -39,9 +39,9 @@ class MathInput:
         score = self.score
         msg = self.feedback
         if score == 100:
-            self.suffix = r'<i class="fas fa-check" style="margin-left: 0.5em; color: green"></i>'
+            self.suffix = r'<i class="fas fa-check" style="margin-left: 0.5em; color: #43A047"></i>'
         elif score == -1:
-            self.suffix = rf'<i class="fas fa-exclamation-circle" style="margin-left: 0.5em; color: orange; cursor: pointer;" data-toggle="popover" data-content="{msg}"></i>'
+            self.suffix = rf'<i class="fas fa-exclamation-circle" style="margin-left: 0.5em; color: #FFB300; cursor: pointer;" data-toggle="popover" data-content="{msg}"></i>'
         else:
             self.suffix = rf'<i class="fas fa-times" style="margin-left: 0.5em; color: #E53935; cursor: pointer;" data-toggle="popover" data-content="{msg}"></i>'
     
