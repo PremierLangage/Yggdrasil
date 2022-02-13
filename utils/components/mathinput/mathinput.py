1ae104971aae4fb71feceee8d652e3a149120323
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-13 22:41:53 +0100

    ddoyen1 modified Yggdrasil/utils/components/mathinput/mathinput.py

--- utils/components/mathinput/mathinput.py
+++ utils/components/mathinput/mathinput.py
@@ -12,7 +12,8 @@ std_keypad = {
 
 class MathInput:
 
-
+    message = {}
+    
     def __init__(self, **kwargs):
         self.__MathInput__ = True
         self.id = kwargs.get('id', "MathInput" + str(uuid4().hex))
@@ -62,5 +63,4 @@ class MathInput:
         elif self.type == "complex":
             score, error = eval_complex(self.value, self.sol, **self.evalparam)
         self.score = score
-        self.feedback = MathInput.message.get('error', f"Error: {error}")
-        self.feedback = str(MathInput.message)
+        self.feedback = MathInput.message.get(error, f"Error: {error}")
