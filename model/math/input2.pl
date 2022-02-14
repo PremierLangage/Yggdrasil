456b9a49aacf4da50e8727b814c89a9738fb13d0
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-14 15:37:42 +0100

    ddoyen1 modified Yggdrasil/model/math/input2.pl

--- model/math/input2.pl
+++ model/math/input2.pl
@@ -81,7 +81,7 @@ function onBeforeSubmitPL() {
 
 javascript.popover ==
 <script>
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
