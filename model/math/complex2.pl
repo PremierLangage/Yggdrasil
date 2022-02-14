ed8aa86f7cc19a12603d5a2a2b80298b170ff8f4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-14 10:48:39 +0100

    ddoyen1 modified Yggdrasil/model/math/complex2.pl

---
+++ model/math/complex2.pl
@@ -0,0 +1,29 @@
+# Model for single math input field
+# with evaluation as a complex number
+# (using MathInput class)
+
+extends = /model/math/input.pl
+
+# Evaluation parameters
+input_prefix = Réponse :
+imaginary_unit = i
+complex_form = 
+checkratsimp = True
+symbol_dict = {'e': E}
+unauthorized_func = ['sin', 'cos', 'tan']
+
+# Latex settings
+latexsettings.imaginary_unit = i
+
+evaluator ==
+from ast import literal_eval
+from sympy import sympify
+from evalsympy import eval_complex
+score, error = eval_complex(answers['math'], sol, imaginary_unit=imaginary_unit, form=complex_form, checkratsimp=checkratsimp, unauthorized_func=unauthorized_func, local_dict=sympify(symbol_dict))
+feedback = message[error]
+==
+
+solution ==
+La solution est $! {{ sol|latex}} !$.
+==
+
