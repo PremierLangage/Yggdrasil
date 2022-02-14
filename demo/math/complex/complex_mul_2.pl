19d416932f038c8a1d2050d17f1015ca4512fca8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-14 09:20:41 +0100

    ddoyen1 modified Yggdrasil/demo/math/complex/complex_mul_2.pl

--- demo/math/complex/complex_mul_2.pl
+++ demo/math/complex/complex_mul_2.pl
@@ -8,7 +8,7 @@ for i in range(3):
     z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
     z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
     s = latex(z1 * z2)
-    inputs[i].prefix = "$! {s} !$"
+    inputs[i].prefix = f"$! {s} !$"
     inputs[i].sol = (z1 + z2).expand()
     inputs[i].type = "complex"
 ==
