ce9634d82aa7071e9c38e45e01d1a318e53c6ca7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-14 11:59:11 +0100

    ddoyen1 modified Yggdrasil/model/math/input2.pl

--- model/math/input2.pl
+++ model/math/input2.pl
@@ -4,7 +4,7 @@ extends = /model/basic/temp.pl
 jinja_keys = ["inputblock", "question", "solution"]
 
 before_scripts = ["mathimport", "init_input", "before"]
-eval_scripts = ["auto_eval", "evaluator"]
+eval_scripts = ["evaluator"]
 
 input_prefix = Réponse :
 keypad = ["+infty", "-infty"]
