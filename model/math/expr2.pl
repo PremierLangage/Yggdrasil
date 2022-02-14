14f2096e926dfb3dcaf2686acaf30904c91ca0bd
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-14 21:45:24 +0100

    ddoyen1 modified Yggdrasil/model/math/expr2.pl

--- model/math/expr2.pl
+++ model/math/expr2.pl
@@ -10,3 +10,10 @@ symbol_dict = {'e': E}
 unauthorized_func = []
 equality = ""
 modulo = 0
+
+evalparam ==
+from sympy import sympify
+if modulo != 0:
+    modulo = sympify(modulo)
+input.evalparam = {'equality': equality, 'modulo': modulo, 'checkratsimp': checkratsimp, 'unauthorized_func': unauthorized_func, 'local_dict': sympify(symbol_dict)}
+==
