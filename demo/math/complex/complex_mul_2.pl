55192f0ddd71dde9e6586dfc738a10f2977a7067
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-15 14:12:43 +0100

    ddoyen1 modified Yggdrasil/demo/math/complex/complex_mul_2.pl

--- demo/math/complex/complex_mul_2.pl
+++ demo/math/complex/complex_mul_2.pl
@@ -4,11 +4,12 @@ title = Nombres Complexes
 
 before == #|python|
 inputs = [MathInput() for _ in range(3)]
+prefixes = []
 for i in range(3):
     z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
     z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
     s = latex(z1 * z2)
-    inputs[i].prefix = f"$! {s} = !$"
+    prefixes.append(f"$! {s} = !$")
     inputs[i].sol = (z1 * z2).expand()
     inputs[i].type = "complex"
     inputs[i].evalparam = {'form': 'cartesian'}
@@ -21,7 +22,7 @@ Calculer les produits de nombres complexes suivants (sous forme algébrique).
 solution ==
 {% for input in inputs %}
 <div style="display: block; margin-bottom: 1em;">
-{{ input.prefix }} 
+{{ prefixes[loop.index0] }} 
 $! {{ input.sol|latex }} !$
 </div>
 {% endfor %}
