2445ab88ad517b144d235de983ee9cef94c72278
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-15 10:29:13 +0100

    ddoyen1 modified Yggdrasil/model/math/expr.pl

--- model/math/expr.pl
+++ model/math/expr.pl
@@ -1,21 +1,19 @@
-extends = /model/math/input.pl
+# Model for a single math input field
+# with an evaluation as an expression
 
+extends = /model/math/input2.pl
+
+# Evaluation parameters
+input_type = "expr"
 checkratsimp = True
 symbol_dict = {'e': E}
 unauthorized_func = []
 equality = ""
 modulo = 0
 
-evaluator ==
-from ast import literal_eval
+evalparam ==
 from sympy import sympify
-from evalsympy import eval_expr
 if modulo != 0:
     modulo = sympify(modulo)
-score, error = eval_expr(answers['math'], sol, checkratsimp=checkratsimp,equality=equality, unauthorized_func=unauthorized_func, modulo=modulo, local_dict=sympify(symbol_dict))
-feedback = message[error]
-==
-
-solution ==
-La solution est $! {{ sol|latex }} !$.
+input.evalparam = {'equality': equality, 'modulo': modulo, 'checkratsimp': checkratsimp, 'unauthorized_func': unauthorized_func, 'local_dict': sympify(symbol_dict)}
 ==
