1f937a24d377525cd3ee7be89aea595056433f13
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-15 19:02:37 +0100

    ddoyen1 modified Yggdrasil/builder/before1.py

--- builder/before1.py
+++ builder/before1.py
@@ -75,11 +75,11 @@ if __name__ == "__main__":
                 dic[key] = Env.from_string(macros + dic[key]).render(dic)
             elif isinstance(dic[key] , dict):
                 for k in dic[key]:
-                    dic[key2][k] = "{% raw %}" + dic[key1][k] + "{% endraw %}"
+                    dic[key2][k] = "{% raw %}" + dic[key][k] + "{% endraw %}"
                     dic[key][k] = Env.from_string(macros + dic[key][k]).render(dic)
             elif isinstance(dic[key] , list):
                 for i in range(len(dic[key])):
-                    dic[key2][i] = "{% raw %}" + dic[key1][i] + "{% endraw %}"
+                    dic[key2][i] = "{% raw %}" + dic[key][i] + "{% endraw %}"
                     dic[key][i] = Env.from_string(macros + dic[key][i]).render(dic)
 
     dic['form'] = dic['tplpage']
