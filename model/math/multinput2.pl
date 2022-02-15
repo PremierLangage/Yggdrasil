791f2b1bf2c548f68e1eb5e36a3aa4fe4387a762
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-15 15:59:32 +0100

    ddoyen1 modified Yggdrasil/model/math/multinput2.pl

--- model/math/multinput2.pl
+++ model/math/multinput2.pl
@@ -1,3 +1,27 @@
 extends = input2.pl
 
 
+evaluator ==
+from mathinput import MathInput
+MathInput.message = message
+
+def average(lst):
+    return sum(lst)/len(lst)
+
+for input in inputs:
+    input.value = answers[input.id]
+    input.eval()
+
+if -1 in [input.score for input in inputs]:
+    score = -1
+    for input in inputs:
+        if input.score == -1:
+            input.display_feedback()
+        else:
+            input.hide_feedback()
+else:
+    score = int(average([input.score for input in inputs]))
+    for input in inputs:
+        input.display_feedback()
+        input.disable()
+==
