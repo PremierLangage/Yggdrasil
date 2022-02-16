a6bf464fc99eb33484ad43e42f7deb231e13fa53
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-16 11:56:29 +0100

    ddoyen1 modified Yggdrasil/demo/multi/multi.pl

--- demo/multi/multi.pl
+++ demo/multi/multi.pl
@@ -2,8 +2,8 @@ extends = /model/basic/multinput.pl
 @ /utils/components/inputfields.py [numeric.py]
 
 before ==
-from numeric import Numeric
-inputs = [Numeric() for _ in range(2)]
+from input import Input
+inputs = [Input() for _ in range(2)]
 prefixes = ["", ""]
 inputs[0]._sol = 0
 inputs[1]._sol = 10
