83d1adb093f5b46d30de9654cd7720dcf90b380e
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-16 22:18:03 +0100

    ddoyen1 modified Yggdrasil/model/math/aux_mathinput.pl

---
+++ model/math/aux_mathinput.pl
@@ -0,0 +1,34 @@
+style.mathquill =@ /utils/components/mathinput/mathinput.css.html
+
+javascript.mathinput ==
+<script>
+// Active les popovers.
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
+
+// Fonction appelée quand l'exercice est soumis (bouton valider).
+function onBeforeSubmitPL() {
+  // Copie les valeurs des champs MathField dans des éléments input.
+  Object.values(setOfMathFields).forEach(function(mathField) {
+    var mathFieldInput = document.getElementById('form_'+mathField.el().id);
+      mathFieldInput.value = mathField.latex();
+
+    if (mathField instanceof MQ.StaticMath) {
+      if (mathField.innerFields.length == 1) {
+      mathFieldInput.value = mathField.innerFields[0].latex();
+    } else {
+      mathFieldInput.value = [mathField.innerFields[0].latex(),mathField.innerFields[1].latex()];
+    }
+    };
+  });
+
+  // Cache les popovers avant que les éléments correspondants soient détruits du DOM.
+  $(function () {
+    $('[data-toggle="popover"]').popover('hide')
+  });
+
+  return true;
+}
+</script>
+==
