bef930ab43dfb3bed3f39e54fc82584c2b169366
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-17 15:41:00 +0100

    ddoyen1 modified Yggdrasil/grader/evaluator_serial.py

--- grader/evaluator_serial.py
+++ grader/evaluator_serial.py
@@ -52,8 +52,7 @@ if __name__ == "__main__":
         elif isinstance(d, list):
             for i in range(len(d)):
                 if isinstance(d[i], dict) and 'cid' in d[i]:
-                    #d[i] = Component(**answers[d[i]['cid']])
-                    pass
+                    d[i] = Component(**answers[d[i]['cid']])
                 else:
                     deserialize(d[i])
 
