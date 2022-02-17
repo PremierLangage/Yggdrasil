ed04c1f192879b79195b5ef38d6f169ceafffdd2
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-17 12:58:13 +0100

    ddoyen1 modified Yggdrasil/demo/serialcomponent/test.pl

---
+++ demo/serialcomponent/test.pl
@@ -0,0 +1,19 @@
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+@ /utils/sandboxio.py
+
+title =
+
+keylist % [1, 2]
+
+before ==
+from components import Input
+==
+
+text ==
+{{ keylist }}
+==
+
+form ==
+
+==
