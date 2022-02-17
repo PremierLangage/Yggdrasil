2f0ace27316c8b1fb4aeed67c4c711299088ad5b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-17 09:37:36 +0100

    ddoyen1 modified Yggdrasil/demo/math/multinput/infsup.pl

---
+++ demo/math/multinput/infsup.pl
@@ -0,0 +1,25 @@
+extends = /model/math/multinput.pl
+
+title = Nombres Complexes
+
+before == #|python|
+a = randint(-4, 0)
+b = a + randint(1, 4) 
+f = choice([x**2+1, x**2-1, 1-x**2, -1-x**2])
+inputs = [MathInput() for _ in range(2)]
+prefixes = []
+
+from sympy.calculus.util import minimum, maximum
+
+var('x')
+
+prefixes[0] = "\inf A"
+inputs[0].sol = minimum(f, x, Interval(a,b))
+prefixes[1] = "\sup A"
+inputs[1].sol = maximum(f, x, Interval(a,b))
+==
+
+question == 
+On considère l'ensemble
+$$ A = \{ {{ f|latex }} | x \in [a, b] \}
+==
