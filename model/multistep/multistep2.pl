c78e03be74f65a2322b8b9584539d3b85dec626b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-18 20:42:05 +0100

    ddoyen1 modified Yggdrasil/model/multistep/multistep2.pl

--- model/multistep/multistep2.pl
+++ model/multistep/multistep2.pl
@@ -11,16 +11,7 @@ jinja_keys = ["intro"]
 
 before == #|python|
 from exmath import ExMath
-sequence = [ExMath(), ExMath()]
-sequence[0].question = "toto"
-sequence[0].input.sol = sqrt(3)
-sequence[0].solution = "solution"
-sequence[0].inputblock = sequence[0].input.render()
 
-sequence[1].question = "toto"
-sequence[1].input.sol = sqrt(3)
-sequence[1].solution = "solution"
-sequence[1].inputblock = sequence[0].input.render()
 # Créer champs de réponse
 #inputblock = input.render()
 ==
