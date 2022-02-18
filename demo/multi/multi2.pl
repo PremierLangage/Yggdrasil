e793d71adb60db996b44faa6ba58ff98f9cdb1e7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-18 15:58:58 +0100

    ddoyen1 modified Yggdrasil/demo/multi/multi2.pl

--- demo/multi/multi2.pl
+++ demo/multi/multi2.pl
@@ -3,10 +3,11 @@ extends = /model/basic/multinput.pl
 
 before ==
 from inputfields import Input, MultInputField
-inputs = [Input() for _ in range(2)]
+doubleinput = MultInputField()
+doubleinput.inputs = [Input() for _ in range(2)]
 prefixes = ["", ""]
-inputs[0].sol = "0"
-inputs[1].sol = "10"
+doubleinput.inputs[0].sol = "0"
+doubleinput.inputs[1].sol = "10"
 ==
 
 solution ==
