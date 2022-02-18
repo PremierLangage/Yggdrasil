2f25a2a1c48486052229fa8a5226d6bbf3fe96ba
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-18 20:35:06 +0100

    ddoyen1 modified Yggdrasil/model/multistep/multistep2.pl

---
+++ model/multistep/multistep2.pl
@@ -0,0 +1,23 @@
+extends = /model/math/math.pl
+extends = /model/basic/temp.pl
+extends = /model/math/messages_math.pl
+extends = /model/math/aux_mathinput.pl
+
+@ /utils/components/radio.py [radio.py]
+
+jinja_keys = ["intro", "questions", "solutions", "inputblocks"]
+
+evaluator ==
+scorestep = sequence[istep].eval()
+if scorestep >= 0:
+    scores.append(scorestep)
+    istep += 1
+
+if istep >= nbsteps:
+    istep = nbsteps-1
+    state = "final"
+    score = sum(scores)/len(scores)
+
+==
+
+tplpage =@ template2.html
