758a0ffc48dd8b4c44476ab20e02158d10ffe665
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-18 15:49:55 +0100

    ddoyen1 modified Yggdrasil/demo/multi/multi.pl

--- demo/multi/multi.pl
+++ demo/multi/multi.pl
@@ -5,8 +5,8 @@ before ==
 from inputfields import Input
 inputs = [Input() for _ in range(2)]
 prefixes = ["", ""]
-inputs[0]._sol = "0"
-inputs[1]._sol = "10"
+inputs[0].sol = "0"
+inputs[1].sol = "10"
 ==
 
 solution ==
