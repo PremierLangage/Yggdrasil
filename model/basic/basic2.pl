14c91328b0d1cd355c49b4c1bd30941d584dae09
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-19 17:05:31 +0100

    ddoyen1 modified Yggdrasil/model/basic/basic2.pl

--- model/basic/basic2.pl
+++ model/basic/basic2.pl
@@ -8,7 +8,7 @@ extends = /model/basic/temp.pl
 @ /model/basic/jinja_basic.py [jinja_env.py]
 @ /model/basic/json_basic.py [json_encoder.py]
 
-@ /utils/inputfieldsinputfields.py
+@ /utils/inputfields/inputfields.py
 @ /utils/exercises/exercises.py
 
 before_scripts = ["importfunc", "initinput", "before", "process"]
