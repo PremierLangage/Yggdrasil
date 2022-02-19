2c5b52512ab3246f4a8a874152d52a747b149f4c
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-19 16:05:36 +0100

    ddoyen1 modified Yggdrasil/model/basic/basic2.pl

---
+++ model/basic/basic2.pl
@@ -0,0 +1,26 @@
+extends = /model/basic/temp.pl
+
+@ /builder/before2.py [builder.py]
+@ /grader/evaluator2.py [grader.py]
+@ /utils/builderlib.py
+@ /utils/plrandom.py
+@ /utils/plcsv.py
+@ /model/basic/jinja_basic.py [jinja_env.py]
+@ /model/basic/json_basic.py [json_encoder.py]
+
+before_scripts % ["importfunc", "before", "process"]
+eval_scripts % ["getans", "evaluator", "evalprocess"]
+
+importfunc == #|python|
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from plcsv import csv_choice, csv_sample, csv_col
+==
+
+title =
+form = 
+text = 
+
+tplpage =@ template.html
+
+style.basic =@ basic.css.html
