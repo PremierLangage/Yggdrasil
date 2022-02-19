a2ace3d5eb280ff31b0c90e4e46276dce630b897
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-19 20:29:06 +0100

    ddoyen1 modified Yggdrasil/model/basic/temp2.pl

---
+++ model/basic/temp2.pl
@@ -0,0 +1,86 @@
+# HACK pour modifier l'apparence de la page html de l'exercice
+@ /utils/components/multicomp.py
+@ /utils/components/dragdrop.py [customdragdrop.py]
+@ /utils/components/scoring.py
+
+
+title = 
+
+style.page == #|css|
+<style>
+
+.exercise__header{
+  display: none;
+}
+
+.exercise__instructions{
+  display: none;
+}
+
+.exercise__body{
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+}
+
+.exercise__title {
+   font-size: 20px;
+}
+
+.exercise__feedback {
+ margin-bottom: 0px !important;
+}
+
+.exercise__actions {
+ display: none;  
+}
+
+.exercise__actions .btn-group {
+ display: none;  
+}
+
+.action-save {
+   display: none;
+}
+.action-reset {
+   display: none;
+}
+.action-next {
+   display: none;
+}
+.action-download-env {
+   display: none;
+}
+
+.neutral-state {
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  color: #6c757d !important;
+  border-color: #6c757d !important;
+}
+</style>
+==
+
+javascript.onreadypl == #|js|
+<script>
+function onReadyPL(nodes) {
+    const actions = nodes.actions;
+    const body = nodes.body;
+    const buttons = actions.find('.btn-group');
+    const { origin, pathname }  = document.location;
+    const link = origin + pathname;
+
+    actions.find('.action-save').remove();
+    actions.find('.action-reset').remove();
+    actions.find('.action-next').remove();
+    actions.find('.action-download-env').remove();
+    actions.find('.action-submit').remove();
+    actions.find('br').remove();
+    body.children('br').remove();
+
+        {% if embed|length > 0 %}
+    mathField.reflow();
+    {% endif %}
+
+}
+</script>
+==
