d772d42a4c4d21fbcead23f366e2aa0760eac157
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-19 16:06:37 +0100

    ddoyen1 modified Yggdrasil/model/basic/basic2.pl

--- model/basic/basic2.pl
+++ model/basic/basic2.pl
@@ -8,8 +8,8 @@ extends = /model/basic/temp.pl
 @ /model/basic/jinja_basic.py [jinja_env.py]
 @ /model/basic/json_basic.py [json_encoder.py]
 
-before_scripts % ["importfunc", "before", "process"]
-eval_scripts % ["getans", "evaluator", "evalprocess"]
+before_scripts =["importfunc", "initinput", "before", "process"]
+eval_scripts = ["getans", "evaluator", "evalprocess"]
 
 importfunc == #|python|
 from random import choice, choices, sample, shuffle
