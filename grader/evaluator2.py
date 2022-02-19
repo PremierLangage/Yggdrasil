b6e1a3e20543d8b0e2174926a1bb0c5e644af137
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-19 16:03:15 +0100

    ddoyen1 modified Yggdrasil/grader/evaluator2.py

--- grader/evaluator2.py
+++ grader/evaluator2.py
@@ -1,161 +1,73 @@
-import sys, json, jsonpickle
+import sys, json
 from components import Component
 from builderlib import aux_component1, aux_component2, aux_component
-from builderlib import PickleEncoder, ComponentEnv
+from jinja2 import Environment, BaseLoader
 
-from jinja2 import Template
+# import Jinja environnement
+from jinja_env import Env
 
-try:
-    from json_encoder import CustomEncoder, CustomDecoder
-except ModuleNotFoundError:
-    CustomEncoder = PickleEncoder
-    CustomDecoder = None
-
-# Import the custom Jinja environnement
-try:
-    from jinja_env import CustomEnv as Env
-except ModuleNotFoundError:
-    Env = ComponentEnv
-
-try:
-    from namespace import namespace
-except ModuleNotFoundError:
-    namespace = {}
-
-def format_feedback_rightwrong(score,feedback):
-    tpl="""<div class="alert {}"><strong>{}</strong> <br> {}</div>"""
-    if score==-1:
-        return tpl.format('alert-info','Attention !',feedback)
-    elif score==100:
-        return tpl.format('alert-success','Bonne réponse.',feedback)
-    else:
-        return tpl.format('alert-danger','Mauvaise réponse.',feedback)
-
-def format_feedback_score(score,feedback):
-    if score==-1:
-        tpl="""<div class="alert {}"><strong>{}</strong><br> {}</div>"""
-        return tpl.format('alert-info',score, feedback)
-    tpl="""<div class="alert {}"><strong>Score : {} / 100</strong><br> {}</div>"""
-    if score==100:
-        return tpl.format('alert-success',score, feedback)
-    elif score==0:
-        return tpl.format('alert-danger',score, feedback)
-    else:
-        return tpl.format('alert-warning',score, feedback)
-
-def format_feedback_lightscore(score,feedback):
-    if score==-1:
-        tpl="""<div class="alert {}"><strong>{}</strong> <br> {}</div>"""
-        return tpl.format('alert-info',score, feedback)
-    tpl="""<div class="alert alert-secondary"><strong>Score : {} / 100 <br> </strong> {}</div>"""
-    return tpl.format(score,feedback)
-
-
-
-missing_evaluator_stderr = """\
-The key 'evaluator' was not found in the context.
-When using this grader, the PL must declare a script inside a key 'evaluator'. This script have
-access to every variable declared in the PL and its 'before' script.
-It should declare a variable 'grade' which should contain a tuple (int, feedback) where int is the grade between [0, 100]."""
+from json_encoder import JSONEncoder, JSONDecoder
 
 if __name__ == "__main__":
 
     with open(sys.argv[1], "r") as f:
-        dic = json.load(f, cls=CustomDecoder)
+        dic = json.load(f, cls=JSONDecoder)
     Component.sync_context(dic)
 
     with open(sys.argv[2], "r") as f:
         dic['answers'] = json.load(f)
 
-    dic = {**namespace, **dic}
-
     aux_component1(dic)
-    
-    if 'evaluator' in dic:
-            code = dic['evaluator']
-    else:
-        print(missing_evaluator_stderr, file=sys.stderr)
-        sys.exit(1)
 
-    #code = "\n".join([dic.get('headerevaluator', ""), dic.get('evaluator', ""), dic.get('footerevaluator', "")])
+    eval_scripts = dic.get('eval_scripts', ['evaluator'])
+    code = "\n".join([dic.get(name, "") for name in eval_scripts])
 
     exec(code, dic)
+    namespace = {}
     exec("", namespace)
     for key in namespace:
         if key in dic and dic[key] == namespace[key]:
             del dic[key]
 
     aux_component2(dic)
-    
-    if 'grade' in dic:
-        score = dic['grade'][0]
-        feedback = dic['grade'][1] + " "
-    else:
-        if 'score' in dic:
-            score = dic['score']    
-        if 'feedback' in dic:
-            feedback = dic['feedback']  
-        else:
-            feedback=" "
 
-    if score >= 0:
-        dic['internals']['attempt'] = dic['internals']['attempt'] + 1
-    
-    if dic['settings']['maxattempt'] is not None:
-        if dic['internals']['attempt'] > dic['settings']['maxattempt']:
-            try:
-                dic['internals']['buttons'].remove('submit')
-            except:
-                pass
-            if score < 100 and 'solution' in dic:
-                feedback += dic['solution']
-            elif score < 100 and 'message' in dic:
-                if 'Solution' in dic['message']:
-                    feedback += dic['message']['Solution']
-    
+    if 'score' in dic:
+        score = dic['score']    
+    if 'feedback' in dic:
+        feedback = dic['feedback']  
+    else:
+        feedback = ""
+      
     feedback = Env.from_string(feedback).render(dic)
-    if 'feedback' in dic['settings']:
-        if dic['settings']['feedback']=='rightwrong':
-            ffeedback=format_feedback_rightwrong(score,feedback)
-        elif dic['settings']['feedback']=='score':
-            ffeedback=format_feedback_score(score,feedback)
-        elif dic['settings']['feedback']=='lightscore':
-            ffeedback=format_feedback_lightscore(score,feedback)
-        else:
-            ffeedback = feedback
-    else:
-        ffeedback = feedback
+    ffeedback = ""
 
-    # dic = json.loads(json.dumps(dic, cls=CustomEncoder))
+    macros = dic.get('macros', '')
 
-    # hack for MathQuill
-    if 'answers' in dic:
-        if 'math' in dic['answers']:
-            dic['prev_value'] = dic['answers']['math'].replace("\\", "\\\\")
+    try:
+        for key in dic.get('jinja_keys', ['question', 'solution']):
+            if key in dic:
+                key2 = f"_{key}_"
+                if key2 in dic:
+                    if isinstance(dic[key] , str):
+                        dic[key] = dic[key2].replace(r"{% raw %}", "")
+                        dic[key] = dic[key].replace(r"{% endraw %}", "")         
+                        dic[key] = Env.from_string(macros+dic[key]).render(dic)
+                    elif isinstance(dic[key] , dict):
+                        for k in dic[key]:
+                            dic[key][k] = dic[key2][k].replace(r"{% raw %}", "")
+                            dic[key][k] = dic[key][k].replace(r"{% endraw %}", "")         
+                            dic[key][k] = Env.from_string(macros+dic[key][k]).render(dic)
+    except:
+        pass
 
-
-
-    # render some string values of the exercise dictionary with the custom Jinja environment
-    jinja_keys = dic.get('jinja_keys', ['text', 'form', 'solution'])
-
-    for key in jinja_keys:
-        if key in dic:
-            dic[key] = Env.from_string(dic[key]).render(dic)
+    dic['form'] = dic['tplpage']
 
     with open(sys.argv[3], "w+") as f:
-        json.dump(dic, f, cls=CustomEncoder)
-    
+        json.dump(dic, f, cls=JSONEncoder)
+
     with open(sys.argv[4], "w+", encoding='utf-8') as f:
-        print(ffeedback, file=f)
+        print("", file=f)
     
     print(int(score))
     
     sys.exit(0)
-
-
-
-
-
-
-
-
