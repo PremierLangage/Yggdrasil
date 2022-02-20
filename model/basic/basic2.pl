75e9df10aaddf923f86a0d20533a66cc78ea112f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 11:29:23 +0100

    ddoyen1 modified Yggdrasil/model/basic/basic2.pl

--- model/basic/basic2.pl
+++ model/basic/basic2.pl
@@ -3,6 +3,10 @@ extends = /model/basic/temp2.pl
 @ /builder/before2.py [builder.py]
 @ /grader/evaluator2.py [grader.py]
 @ /utils/builderlib2.py [builderlib.py]
+@ /utils/components/multicomp.py
+@ /utils/inputfields/inputfields.py
+@ /utils/components/dragdrop.py [customdragdrop.py]
+@ /utils/exercises/exercises.py
 @ /utils/plrandom.py
 @ /utils/plcsv.py
 @ /model/basic/jinja_basic.py [jinja_env.py]
