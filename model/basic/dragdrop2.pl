431a9c795faf272623b3641a61f67bf9e06cefff
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 16:24:24 +0100

    ddoyen1 modified Yggdrasil/model/basic/dragdrop2.pl

--- model/basic/dragdrop2.pl
+++ model/basic/dragdrop2.pl
@@ -1,92 +1,55 @@
-extends = /model/basic/basic.pl
-@ /utils/components/scoring.py
-@ /utils/components/dragdrop.py [customdragdrop.py]
+extends = /model/basic/basic2.pl
 
-settings.feedback = lightscore
-
-title =
-
-labels = []
-
-before_scripts = ["importfunc", "before", "process"]
-jinja_keys = ["question", "inputblock"]
-
-importfunc ==
-from random import randint, choice, choices, sample, shuffle
-==
-
-inputblock = 
-
-process ==
-from multicomp import LabelGroup, DropGroup
-
-if isinstance(sol, str):
-    _sol_ = sol.splitlines()
-else:
-    _sol_ = sol
-
-if isinstance(labels, str):
-    _labels_ = labels.splitlines()
-elif isinstance(labels, list):
-    _labels_ = labels
-else:
-    _labels_ = []
-
-_labels_ = list(set(_labels_ + _sol_))
-
-lab = LabelGroup(_labels_)
-drp = DropGroup(_sol_)
-==
-
-
-
-evaluator ==
-score = drp.eval()
-==
+# Main keys
 
 question ==
-Compléter les phrases suivantes avec les étiquettes proposées.
-
-* Je voudrais {{ drp.comp[0]|component }} chausures pour mon anniversaire.
-* Tu est toujours en retard,{{ drp.comp[1]|component }} agaçant !
-* Je n'aime pas {{ drp.comp[2]|component }} méthodes.
-
-{{ lab.comp[0]|component }} {{ lab.comp[1]|component }}
+Quelle est la réponse ?
 ==
 
-sol ==
-ces
-c'est
-ces
+items ==
+Item 0
+Item 1
+Item 2
 ==
 
-style.dragdrop ==
-<style>
-.drag-drop-component {
-    position: relative;
-    cursor: move;
-    display: inline-flex;
-    padding: 0px;
-    box-sizing: border-box;
-    min-width: 3em;
-    min-height: 2em;
-    vertical-align: middle;
-    text-align: center;
-    justify-content: center;
-    align-items: center;
-    border: 0px solid transparent;
-    border-radius: 0px;
-    margin: 0px;
-}
+indsol = 1
 
-.drag-drop-component.droppable {
-    border: 0px solid #000;
-}
+shuffled = True
 
-.drag-drop-component.droppable.dnd-over,.drag-drop-component.droppable:hover{
-    border:0px solid #000
-}
+# Input block
 
-</style>
+inputblock == #|html|
+{{ input|component }}
 ==
 
+# Before scripts
+
+before_scripts = ["importfunc", "initinput", "before", "process"]
+
+importfunc == #|py|
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from plcsv import csv_choice, csv_sample, csv_col
+==
+
+initinput == #|py|
+from dragdrop import DragDropGroup
+input = DragDropGroup()
+==
+
+before == #|py|
+# This script can be used to generate
+# any keys (items, indsol, etc.)
+==
+
+process == #|py|
+
+==
+
+# Evaluation scripts
+
+evaluator == #|py|
+score = input.eval()
+input.display_feedback()
+input.disable()
+==
