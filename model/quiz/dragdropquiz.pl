823ade5da6140528c71bd8aa859152306150a86f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 21:44:30 +0100

    ddoyen1 modified Yggdrasil/model/quiz/dragdropquiz.pl

--- model/quiz/dragdropquiz.pl
+++ model/quiz/dragdropquiz.pl
@@ -1,7 +1,7 @@
 extends = /model/quiz/basic.pl
 
 start == #|python|
-from exercises import ExDragDrop
+from steps import StepDragDrop
 
 nbstep = 4
 
@@ -10,16 +10,12 @@ solutions = []
 feedbacks = []
 
 for i in range(nbstep):
-    sequence.append(ExDragDrop())
+    sequence.append(StepDropGroup())
     sequence[i].question = "Compléter"
-    sequence[i].set_labels(["habet", "habent"])
-    sequence[i].set_filledtext("Pastor non pecuniam {habet}.")
+    sequence[i].input.set_drops(1)
+    sequence[i].input.set_labels(["habet", "habent"])
+    #sequence[i].set_filledtext("Pastor non pecuniam {habet}.")
     sequence[i].inputblock = sequence[i].drops_html() + "\n\n" + sequence[i].labels_html()
-    #questions.append("Compléter")
-    #lab.append(LabelGroup(["habet", "habent"]))
-    #drp.append(DropGroup())
-    #drp[i].set_filledtext("Pastor non pecuniam {habet}.")
-    #inputblocks.append(drp[i].paste_embed() +"\n\n" + lab[i].paste_all())
     solutions.append("")
     feedbacks.append("")
 ==
