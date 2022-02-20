bbfd15666037541f1d5a394a83fece2a036007f7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 10:56:52 +0100

    ddoyen1 modified Yggdrasil/model/basic/radio2.pl

---
+++ model/basic/radio2.pl
@@ -0,0 +1,59 @@
+extends = /model/basic/basic2.pl
+@ /utils/inputfields/radio.py
+
+# Main keys
+
+question ==
+Quelle est la réponse ?
+==
+
+items ==
+Item 0
+Item 1
+Item 2
+==
+
+indsol = [0, 1]
+
+shuffled = True
+
+# Input block
+
+inputblock == #|html|
+{{ input|component }}
+==
+
+# Before scripts
+
+before_scripts = ["importfunc", "initinput", "before", "process"]
+
+importfunc == #|py|
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from plcsv import csv_choice, csv_sample, csv_col
+==
+
+initinput == #|py|
+from radio import Radio
+input = Radio()
+==
+
+before == #|py|
+# This script can be used to generate
+# any keys (items, indsol, etc.)
+==
+
+process == #|py|
+input.set_items(items)
+input.set_sol(indsol)
+if shuffled:
+    input.shuffle()
+==
+
+# Evaluation scripts
+
+evaluator == #|py|
+score = input.eval()
+input.display_feedback()
+input.disable()
+==
