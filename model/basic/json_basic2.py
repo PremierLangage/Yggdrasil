6788a17c3a5335ebc7950d00f371e35c67ddfd8e
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 15:36:18 +0100

    ddoyen1 modified Yggdrasil/model/basic/json_basic2.py

---
+++ model/basic/json_basic2.py
@@ -0,0 +1,39 @@
+import json, jsonpickle
+from customdragdrop import MultiComp, DropGroup, LabelGroup
+from exercises import Ex, ExRadio, ExCheckbox, ExInput, ExDragDrop
+from multinput import MultInput
+
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj, (MultiComp, Ex)):
+            return vars(obj)
+        return jsonpickle.Pickler(unpicklable=False).flatten(obj)
+
+class JSONDecoder(json.JSONDecoder):
+
+    def __init__(self, *args, **kwargs):
+        json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+
+    def object_hook(self, dic):
+        if '__MultiComp__' in dic:
+            if dic['__MultiComp__'] == 'DropGroup':
+                return DropGroup(None, **dic)
+            if dic['__MultiComp__'] == 'LabelGroup':
+                return LabelGroup(None, **dic)
+            else:
+                return MultiComp(**dic)
+        if '__MultInput__' in dic:
+            return MultInput(**dic)
+        if '__Ex__' in dic:
+            if dic['__Ex__'] == 'Radio':
+                return ExRadio(**dic)
+            elif dic['__Ex__'] == 'Checkbox':
+                return ExCheckbox(**dic)
+            elif dic['__Ex__'] == 'Input':
+                return ExInput(**dic)
+            elif dic['__Ex__'] == 'DragDrop':
+                return ExDragDrop(**dic)
+            else:
+                return Ex(**dic)
+        return dic
