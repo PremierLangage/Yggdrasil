7ba39ed909ae2bc3cdd5f50fe610b9255dcb9c8d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 15:06:04 +0100

    ddoyen1 modified Yggdrasil/utils/inputfields/textinput.py

---
+++ utils/inputfields/textinput.py
@@ -0,0 +1,40 @@
+from components import Component
+
+class TextInput(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-input'
+        self.decorator = 'Input'
+        super().__init__(**kwargs)
+
+    def eval(self):
+    # TODO : reprendre les modes d'évaluations
+    # plus sophistiqués du modèle input
+        if self.sol == self.value:
+            self.score = 100
+        else:
+            self.score = 0
+        return self.score
+
+    def display_feedback(self):
+        if self.score == 100:
+            self.suffix = r'<i class="fas fa-check" style="color: var(--sucsess)"></i>'
+        elif self.score >= 0:
+            self.suffix = r'<i class="fas fa-times" style="color: var(--danger)"></i></i>'
+
+    def hide_feedback(self):
+        self.suffix = ""
+
+    def disable(self):
+        self.disabled = True
+
+    def render(self):
+        """
+        Return the HTML code of the component.
+        """
+        selector = self.selector
+        cid = self.cid
+        return f"<{selector} cid='{cid}'></{selector}>"
+
+class Numeric:
+    pass
