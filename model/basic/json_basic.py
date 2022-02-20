98ffddb2111986f248c63dae4efa41303ca35700
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 15:36:39 +0100

    ddoyen1 modified Yggdrasil/model/basic/json_basic.py

--- model/basic/json_basic.py
+++ model/basic/json_basic.py
@@ -16,24 +16,4 @@ class JSONDecoder(json.JSONDecoder):
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
 
     def object_hook(self, dic):
-        if '__MultiComp__' in dic:
-            if dic['__MultiComp__'] == 'DropGroup':
-                return DropGroup(None, **dic)
-            if dic['__MultiComp__'] == 'LabelGroup':
-                return LabelGroup(None, **dic)
-            else:
-                return MultiComp(**dic)
-        if '__MultInput__' in dic:
-            return MultInput(**dic)
-        if '__Ex__' in dic:
-            if dic['__Ex__'] == 'Radio':
-                return ExRadio(**dic)
-            elif dic['__Ex__'] == 'Checkbox':
-                return ExCheckbox(**dic)
-            elif dic['__Ex__'] == 'Input':
-                return ExInput(**dic)
-            elif dic['__Ex__'] == 'DragDrop':
-                return ExDragDrop(**dic)
-            else:
-                return Ex(**dic)
         return dic
