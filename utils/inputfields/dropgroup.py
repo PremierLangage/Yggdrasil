600aee0a4c3004560fc0195cdb38237f488a8f2c
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-20 16:38:49 +0100

    ddoyen1 modified Yggdrasil/utils/inputfields/dropgroup.py

---
+++ utils/inputfields/dropgroup.py
@@ -0,0 +1,62 @@
+from components import Component
+
+class Drop(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-drag-drop'
+        self.droppable = True
+        self.cloneable = False
+        super().__init__(**kwargs)
+
+class Label(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-drag-drop'
+        self.droppable = False
+        self.cloneable = False
+        super().__init__(**kwargs)
+
+
+class DropGroup():
+
+    def __init__(self, **kwargs):
+        self.__DragDropGroup__ = True
+        self.drops = kwargs.get('drops', [])
+        self.labels = kwargs.get('labels', [])
+        self.sol = kwargs.get('sol', [])
+
+    def set_drops(self, n):
+        self.drops = [Drop() for _ in range(n)]
+
+    def set_labels(self, lst):
+        self.labels = [Label(content=content) for content in lst]
+
+    def eval(self):
+        n = len(self.drops)
+        num_right = 0
+        num_wrong = 0
+
+        for i in range(n):
+            if self.drops[i].content == self._sol[i]:
+                num_right += 1
+                self.drops[i].css = "success-state"
+            else:
+                num_wrong +=1
+                self.drops[i].css = "error-state"
+
+        if num_wrong > 0 :
+            score = 0
+        else:
+            score = 100
+            
+        return score
+
+    def display_feedback(self):
+        pass
+
+    def hide_feedback(self):
+        pass
+
+
+    def disable(self):
+        pass
