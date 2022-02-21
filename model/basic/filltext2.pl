da67d3a7d6292f43e30518eee57699a51c3e49e5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-21 11:58:42 +0100

    ddoyen1 modified Yggdrasil/model/basic/filltext2.pl

--- model/basic/filltext2.pl
+++ model/basic/filltext2.pl
@@ -74,23 +74,3 @@ score = input.eval()
 input.display_feedback()
 input.disable()
 ==
-
-
-process ==
-
-if isinstance(labels, str):
-    _labels_ = labels.splitlines()
-elif isinstance(labels, list):
-    _labels_ = labels
-else:
-    _labels_ = []
-
-drp = DropGroup()
-drp.set_filledtext(filledtext)
-
-_labels_ = list(set(_labels_ + drp._sol))
-lab = LabelGroup(_labels_)
-
-
-inputblock = drp.paste_embed() +"\n\n" + lab.paste_all()
-==
