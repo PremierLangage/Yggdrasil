216e1dc1a4d579d10bdafeb0ef531375caa1a0ac
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-21 17:49:32 +0100

    ddoyen1 modified Yggdrasil/model/basic/hackpage.pl

---
+++ model/basic/hackpage.pl
@@ -0,0 +1,74 @@
+# HACK pour modifier l'apparence de la page html de l'exercice
+
+title = 
+
+style.page == #|html|
+<style>
+.exercise__header{
+  display: none;
+}
+
+.exercise__instructions{
+  display: none;
+}
+
+.exercise__body{
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+}
+
+.exercise__title {
+   font-size: 20px;
+}
+
+.exercise__feedback {
+ margin-bottom: 0px !important;
+}
+
+.exercise__actions {
+ display: none;  
+}
+
+.exercise__actions .btn-group {
+ display: none;  
+}
+
+.action-save {
+   display: none;
+}
+.action-reset {
+   display: none;
+}
+.action-next {
+   display: none;
+}
+.action-download-env {
+   display: none;
+}
+</style>
+==
+
+javascript.onreadypl == #|js|
+<script>
+function onReadyPL(nodes) {
+    const actions = nodes.actions;
+    const body = nodes.body;
+    const buttons = actions.find('.btn-group');
+    const { origin, pathname }  = document.location;
+    const link = origin + pathname;
+
+    actions.find('.action-save').remove();
+    actions.find('.action-reset').remove();
+    actions.find('.action-next').remove();
+    actions.find('.action-download-env').remove();
+    actions.find('.action-submit').remove();
+    actions.find('br').remove();
+    body.children('br').remove();
+
+    {% if embed|length > 0 %}
+    mathField.reflow();
+    {% endif %}
+
+}
+</script>
+==
