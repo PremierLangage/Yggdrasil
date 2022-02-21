7071ff4ca8cd99d78757e3a1e1daf181252b5407
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-21 18:44:55 +0100

    ddoyen1 modified Yggdrasil/demo/quiz/AMC.pl

---
+++ demo/quiz/AMC.pl
@@ -0,0 +1,23 @@
+extends = /model/quiz/AMC.pl
+
+intro ==
+Ce quiz est composé de 3 questions sur la géographie de l'Europe
+==
+
+quiz ==
+* Quelle est la capitale du Cameroun ?
++ Yaoundé
+- Douala
+- Kribi
+
+** Parmi les nombres suivants, lesquels sont positifs ?
++ 2
+- -2
++ 10
+
+*[ordered] Combien font un plus un ?
+- 0
+- 1
++ 2
+- 3
+==
