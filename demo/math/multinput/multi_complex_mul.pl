666a7ba54ffc549f3481540f0953e7641b2795b5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-21 16:31:19 +0100

    ddoyen1 modified Yggdrasil/demo/math/multinput/multi_complex_mul.pl

---
+++ demo/math/multinput/multi_complex_mul.pl
@@ -0,0 +1,21 @@
+extends = /model/math/multinput.pl
+
+title = Nombres Complexes
+
+before == #|python|
+n = 3
+inputs = [MathInput() for _ in range(3)]
+prefixes = []
+for i in range(4):
+    z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
+    z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
+    s = latex(z1 * z2)
+    prefixes.append(f"$! {s} = !$")
+    inputs[i].sol = (z1 * z2).expand()
+    inputs[i].type = "complex"
+    inputs[i].evalparam = {'form': 'cartesian'}
+==
+
+question == 
+Calculer les produits de nombres complexes suivants (sous forme algébrique).
+==
