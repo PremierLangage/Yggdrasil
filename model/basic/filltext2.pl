08517e3e51a56b00726b8399b927a4003cd98506
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-21 11:58:18 +0100

    ddoyen1 modified Yggdrasil/model/basic/filltext2.pl

--- model/basic/filltext2.pl
+++ model/basic/filltext2.pl
@@ -3,7 +3,13 @@ extends = /model/basic/basic2.pl
 # Main keys
 
 question ==
-Quelle est la réponse ?
+Compléter les phrases suivantes avec les étiquettes proposées.
+==
+
+filledtext ==
+* Je voudrais {ces} chausures $! \frac 3 4 !$ pour mon anniversaire.
+* Tu est toujours en retard, {c'est} agaçant !
+* Je n'aime pas {ces} méthodes.
 ==
 
 
