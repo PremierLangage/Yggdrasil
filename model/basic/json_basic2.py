53a756c12f2ef2b53335e8f44c8bc7f59cf0ded7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-21 09:17:56 +0100

    ddoyen1 modified Yggdrasil/model/basic/json_basic2.py

--- model/basic/json_basic2.py
+++ model/basic/json_basic2.py
@@ -1,7 +1,7 @@
 import json, jsonpickle
 from dropgroup import DropGroup
 from steps import Step, StepRadio, StepCheckbox, StepTextInput, StepDropGroup
-from multinput import MultInput
+from inputgroup import InputGroup
 
 class JSONEncoder(json.JSONEncoder):
 
