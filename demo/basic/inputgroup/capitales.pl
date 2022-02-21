8bc064f2aaddb2e83eb8a89ff62203857ccabce3
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-21 15:34:56 +0100

    ddoyen1 modified Yggdrasil/demo/basic/inputgroup/capitales.pl

--- demo/basic/inputgroup/capitales.pl
+++ demo/basic/inputgroup/capitales.pl
@@ -9,7 +9,7 @@ inputs[1].sol = "Rome"
 inputblock ==
 <div class="container">
   <div class="row py-1">
-    <div class="align-middle col-md-auto align-bottom">
+    <div class="align-middle col-md-auto">
         France
     </div>
     <div class="col">
