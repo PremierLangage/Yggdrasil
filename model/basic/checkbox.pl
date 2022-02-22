51044875ac4cb8d45ee5d0056e5c8c0a8de1cce6
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-22 22:49:47 +0100

    ddoyen1 modified Yggdrasil/model/basic/checkbox.pl

---
+++ model/basic/checkbox.pl
@@ -0,0 +1,81 @@
+extends = /model/basic/basic.pl
+
+# Main keys
+
+question ==
+Quelle est la réponse ?
+==
+
+items ==
+Item 0
+Item 1
+Item 2
+==
+
+indsol = [0, 1]
+
+shuffled = True
+
+# Input block
+
+inputblock == #|html|
+{{ input|component }}
+==
+
+# Before scripts
+
+before_scripts = ["importfunc", "initinput", "before", "process"]
+
+importfunc == #|py|
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from plcsv import csv_choice, csv_sample, csv_col
+==
+
+initinput == #|py|
+from checkbox import Checkbox
+input = Checkbox()
+==
+
+before == #|py|
+# This script can be used to generate
+# any keys (items, indsol, etc.)
+==
+
+process == #|py|
+input.set_items(items)
+input.set_sol(indsol)
+if shuffled:
+    input.shuffle()
+==
+
+# Evaluation scripts
+
+evaluator == #|py|
+score = input.eval()
+input.display_feedback()
+input.disable()
+==
+
+doc == #|python|
+{
+    "name": "checkbox",
+    "keys": {
+        "items": {
+            "type": "(str, list)",
+            "default": "",
+            "description": "Liste des items."
+        },
+        "indsol": {
+            "type": "list",
+            "default": [],
+            "description": "Indices des solutions dans la liste des items."
+        },
+        "shuffled": {
+            "type": "bool",
+            "default": True,
+            "description": "Valeur indiquant si les items seront mélangés."
+        }
+    }
+}
+==
