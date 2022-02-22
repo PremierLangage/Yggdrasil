16a01ce23238b7677a756bf890ab6ac66f58b35a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-22 16:57:47 +0100

    ddoyen1 modified Yggdrasil/model/quiz/basic.pl

--- model/quiz/basic.pl
+++ model/quiz/basic.pl
@@ -1,22 +1,5 @@
 extends = /model/basic/hackpage.pl
-
-@ /builder/before2.py [builder.py]
-@ /grader/evaluator2.py [grader.py]
-@ /utils/builderlib2.py [builderlib.py]
-@ /utils/steps/steps.py
-
-@ /utils/plrandom.py
-@ /utils/plcsv.py
-@ /model/basic/jinja_basic.py [jinja_env.py]
-@ /model/basic/json_basic2.py [json_encoder.py]
-
-@ /utils/inputfields/radio.py
-@ /utils/inputfields/checkbox.py
-@ /utils/inputfields/textinput.py
-@ /utils/inputfields/numinput.py
-@ /utils/inputfields/dropgroup.py
-@ /utils/inputfields/inputgroup.py
-@ /utils/inputfields/scoring.py
+extends = /model/basic/utils.pl
 
 before_scripts = ["importfunc", "init", "start"]
 eval_scripts = ["control", "final"]
