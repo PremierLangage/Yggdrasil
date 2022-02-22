f9f0c25adaf2956d0cb13115346af13b2bed3034
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-22 16:52:58 +0100

    ddoyen1 modified Yggdrasil/model/basic/basic2.pl

--- model/basic/basic2.pl
+++ model/basic/basic2.pl
@@ -1,33 +1,6 @@
 extends = /model/basic/hackpage.pl
+extends = /model/basic/utils.pl
 
-@ /builder/before2.py [builder.py]
-@ /grader/evaluator2.py [grader.py]
-@ /utils/builderlib2.py [builderlib.py]
-
-@ /utils/plrandom.py
-@ /utils/plcsv.py
-@ /utils/sympy/evalsympy.py
-@ /utils/sympy/latex2sympy.py
-@ /utils/sympy/sympy2latex.py
-@ /utils/sympy/randsympy.py
-@ /utils/graphics/plmpl.py
-
-@ /model/basic/jinja_basic.py [jinja_env.py]
-@ /model/basic/json_basic2.py [json_encoder.py]
-
-@ /utils/inputfields/radio.py
-@ /utils/inputfields/checkbox.py
-@ /utils/inputfields/textinput.py
-@ /utils/inputfields/numinput.py
-@ /utils/inputfields/dropgroup.py
-@ /utils/inputfields/inputgroup.py
-@ /utils/inputfields/scoring.py
-@ /utils/inputfields/mathinput.py
-@ /utils/components/mathinput/mathinput.py
-@ /utils/components/mathinput/mathinput.html
-
-@ /utils/steps/steps.py
-@ /utils/steps/stepmath.py
 
 before_scripts = ["importfunc", "initinput", "before", "process"]
 eval_scripts = ["getans", "evaluator", "evalprocess"]
