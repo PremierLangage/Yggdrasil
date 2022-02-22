f2afbc8806042a0b0f4cbddcf5f024f55a836959
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-22 16:40:45 +0100

    ddoyen1 modified Yggdrasil/model/basic/json_basic2.py

--- model/basic/json_basic2.py
+++ model/basic/json_basic2.py
@@ -1,11 +1,24 @@
 import json, jsonpickle
+from sympy import srepr, Basic, Matrix, sympify
 from dropgroup import DropGroup
-from steps import Step, StepRadio, StepCheckbox, StepTextInput, StepDropGroup
+from mathinput import MathInput
 from inputgroup import InputGroup
+from steps import Step, StepRadio, StepCheckbox, StepTextInput, StepDropGroup
+from stepmath import StepMath
 
 class JSONEncoder(json.JSONEncoder):
 
     def default(self, obj):
+        if isinstance(obj, (Basic, Matrix)):
+            return {'__SymPy__': True, 'srepr': srepr(obj)}
+        if isinstance(obj, MathInput):
+            return vars(obj)
+        if isinstance(obj, StepMath):
+            dic = vars(obj)
+            for k, v in dic.items():
+                if isinstance(v, dict):
+                    dic[k] = self.default(v)
+            return dic
         if isinstance(obj, (Step, DropGroup)):
             return vars(obj)
         return jsonpickle.Pickler(unpicklable=False).flatten(obj)
@@ -16,6 +29,15 @@ class JSONDecoder(json.JSONDecoder):
         json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
 
     def object_hook(self, dic):
+        if '__SymPy__' in dic:
+            return sympify(dic['srepr'], evaluate=False)
+        if '__MathInput__' in dic:
+            return MathInput(**dic)
+        if '__Step__' in dic:
+            #for k, v in dic.items():
+            #    if isinstance(v, dict):
+            #        dic[k] = self.object_hook(v)
+
         if '__DropGroup__' in dic:
             return DropGroup(**dic)
         if '__InputGroup__' in dic:
@@ -29,6 +51,8 @@ class JSONDecoder(json.JSONDecoder):
                 return StepTextInput(**dic)
             elif dic['__Step__'] == 'DropGroup':
                 return StepDropGroup(**dic)
+            elif dic['__Step__'] == 'Math':
+                return StepMath(**dic)
             else:
                 return Step(**dic)
         return dic
