150ab4632fd0bab0b9a9b87e29b4ae5707075b3f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-22 15:04:35 +0100

    ddoyen1 modified Yggdrasil/demo/basic/inputgroup/capitales.pl

--- demo/basic/inputgroup/capitales.pl
+++ demo/basic/inputgroup/capitales.pl
@@ -4,13 +4,13 @@ question ==
 Quelles sont les capitales des pays suivants ?
 ==
 
-before ==
+before == #|py|
 inputs = [TextInput(), TextInput()]
 inputs[0].sol = "Paris"
 inputs[1].sol = "Rome"
 ==
 
-inputblock ==
+inputblock == #|html|
 <div style="display:flex; align-items: center; margin-bottom: 1em;">
   <div> France : </div>
   <div style="margin-left: 1em;"> {{ inputs[0]|component } }</div>
