4139582dd0500c9764252ad509d835d8634c9bc2
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-22 11:28:03 +0100

    ddoyen1 modified Yggdrasil/model/multistep/basic.pl

--- model/multistep/basic.pl
+++ model/multistep/basic.pl
@@ -40,9 +40,7 @@ sequence[1].question = "Quelle est la capitale de l'Italie ?"
 sequence[1].input.sol = "Rome"
 sequence[1].solution = "Paris"
 
-sequence[2].question = "Quelle est la superficie de la France ?"
-sequence[2].set_sol(0)
-sequence[2].set_items(["100 000 km²", "500 000 km²", "1 000 000 km²"])
+
 ==
 
 init ==
