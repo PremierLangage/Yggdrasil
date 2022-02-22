84652ea6157e1cccbca815fead5e19cdb4f0c180
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-22 11:24:23 +0100

    ddoyen1 modified Yggdrasil/model/multistep/basic.pl

--- model/multistep/basic.pl
+++ model/multistep/basic.pl
@@ -41,8 +41,8 @@ sequence[1].input.sol = "Rome"
 sequence[1].solution = "Paris"
 
 sequence[2].question = "Quelle est la superficie de la France ?"
-sequence[2].input.set_sol(0)
-sequence[2].input.set_items(["100 000 km²", "500 000 km²", "1 000 000 km²"])
+sequence[2].set_sol(0)
+sequence[2].set_items(["100 000 km²", "500 000 km²", "1 000 000 km²"])
 ==
 
 init ==
