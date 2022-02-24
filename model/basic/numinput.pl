fee4b913eb79dce39749ebe8dbc8e6259bf7fe41
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-24 19:13:37 +0100

    ddoyen1 modified Yggdrasil/model/basic/numinput.pl

---
+++ model/basic/numinput.pl
@@ -0,0 +1,64 @@
+extends = /model/basic/basic.pl
+
+# Main keys
+
+question ==
+Quelle est la réponse ?
+==
+
+# Input block
+
+prefix ==
+Réponse :
+==
+
+inputblock ==
+<div class="container">
+  <div class="row align-items-center">
+    <div class="col-auto" style="padding:0;">
+{{ prefix }} 
+    </div>
+    <div class="col-xs" style="padding-left:0.5em; padding-right:0;">
+{{ input|component }}
+    </div>
+  </div>
+</div>
+==
+
+solution ==
+La solution est {{ sol }}.
+==
+
+
+# Before scripts
+
+before_scripts = ["importfunc", "initinput", "before", "process"]
+
+importfunc == #|py|
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from plcsv import csv_choice, csv_sample, csv_col
+==
+
+initinput == #|py|
+from numinput import NumInput
+input = NumInput()
+input.disabled = True
+==
+
+before == #|py|
+# This script can be used to generate
+# any keys (items, indsol, etc.)
+==
+
+process == #|py|
+input.sol = sol
+==
+
+# Evaluation scripts
+
+evaluator == #|py|
+score = input.eval()
+input.display_feedback()
+input.disable()
+==
