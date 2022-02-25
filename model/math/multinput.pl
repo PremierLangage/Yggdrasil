a430cffc1912726ea5bdf4660189035d158469b5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-25 16:12:05 +0100

    ddoyen1 modified Yggdrasil/model/math/multinput.pl

--- model/math/multinput.pl
+++ model/math/multinput.pl
@@ -2,7 +2,7 @@ extends = /model/math/input.pl
 
 before_scripts = ["mathimport", "init_input", "before", "process"]
 
-template.inputblock ==
+templates.inputblock ==
 {% for input in inputs %}
 <div style="display: block; margin-bottom: 1em;">
 {{ prefixes[loop.index0] }} 
@@ -17,7 +17,7 @@ from mathinput import MathInput
 
 process ==
 from jinja_env import Env
-inputblock = Env.from_string(template.inputblock).render(globals())
+inputblock = Env.from_string(templates.inputblock).render(globals())
 ==
 
 evaluator ==
@@ -39,13 +39,13 @@ if -1 in [input.score for input in inputs]:
             input.display_feedback()
         else:
             input.hide_feedback()
-        inputblock = Env.from_string(template.inputblock).render(globals())
+        inputblock = Env.from_string(templates.inputblock).render(globals())
 else:
     score = int(average([input.score for input in inputs]))
     for input in inputs:
         input.display_feedback()
         input.disable()
-        inputblock = Env.from_string(template.inputblock).render(globals())
+        inputblock = Env.from_string(templates.inputblock).render(globals())
 
 ==
 
