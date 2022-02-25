aee1086fc78be91722b579b6c800bf011b1fc78a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-25 16:33:30 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/functions/infsup/infsup.pl

---
+++ Mathematics/exercises/analysis/functions/infsup/infsup.pl
@@ -0,0 +1,34 @@
+extends = /model/math/input.pl
+
+title = Bornes d'un ensemble
+
+before == #|python|
+a = randint(-4, 0)
+b = a + randint(1, 4)
+var('x')
+f = choice([x**2+1, x**2-1, 1-x**2, -1-x**2])
+
+
+inputs = [MathInput() for _ in range(2)]
+prefixes = ["$! \inf A  = !$", "$! \sup A = !$"]
+
+from sympy.calculus.util import minimum, maximum
+
+inputs[0].sol = minimum(f, x, Interval(a,b))
+inputs[1].sol = maximum(f, x, Interval(a,b))
+==
+
+question == 
+On considère l'ensemble
+$$ A = \{ {{ f|latex }} \: | \: x \in [{{ a }}, {{ b }}] \}. $$
+Déterminer les bornes de cet ensemble.
+==
+
+inputblock ==
+{% for input in inputs %}
+<div style="display: block; margin-bottom: 1em;">
+{{ prefixes[loop.index0] }} 
+{{ input|mathinput }}
+</div>
+{% endfor %}
+==
