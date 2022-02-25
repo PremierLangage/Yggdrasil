1bc4f0356dd2d252ca0aec12ce70e5985b4b8a28
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-25 19:59:02 +0100

    ddoyen1 modified Yggdrasil/model/basic/hackpage.pl

--- model/basic/hackpage.pl
+++ model/basic/hackpage.pl
@@ -86,19 +86,6 @@ javascript.onbeforesubmit == #|js|
 <script>
 // Fonction appelée quand l'exercice est soumis (bouton valider).
 function onBeforeSubmitPL() {
-  // Copie les valeurs des champs MathField dans des éléments input.
-  Object.values(setOfMathFields).forEach(function(mathField) {
-    var mathFieldInput = document.getElementById('form_'+mathField.el().id);
-      mathFieldInput.value = mathField.latex();
-
-    if (mathField instanceof MQ.StaticMath) {
-      if (mathField.innerFields.length == 1) {
-      mathFieldInput.value = mathField.innerFields[0].latex();
-    } else {
-      mathFieldInput.value = [mathField.innerFields[0].latex(),mathField.innerFields[1].latex()];
-    }
-    };
-  });
 
   // Cache les popovers avant que les éléments correspondants soient détruits du DOM.
   $(function () {
