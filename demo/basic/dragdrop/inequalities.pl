266ca97008440f0e46dfa6b415822f039fd47ca0
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-28 12:59:18 +0100

    ddoyen1 modified Yggdrasil/demo/basic/dragdrop/inequalities.pl

--- demo/basic/dragdrop/inequalities.pl
+++ demo/basic/dragdrop/inequalities.pl
@@ -14,7 +14,7 @@ for i in range(4):
 
 question ==
 Comparer les nombres suivants en utilisant le symbole adapté ({{ lab.comp[0]|component }} ou {{ lab.comp[1]|component }}).
-{% for drop in drp.comp %}
-* {{ numbers[loop.index0][0] }} {{ drop|component }} {{ numbers[loop.index0][1] }}
+{% for i in range[4] %}
+* {{ numbers[i][0] }} {{ input.drops[i]|component }} {{ numbers[i][1] }}
 {% endfor %}
 ==
