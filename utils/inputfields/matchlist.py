e88659897dbd9a887644d8d174945703a8c4ef71
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-28 22:29:39 +0100

    ddoyen1 modified Yggdrasil/utils/inputfields/matchlist.py

--- utils/inputfields/matchlist.py
+++ utils/inputfields/matchlist.py
@@ -3,64 +3,112 @@ from uuid import uuid4
 from components import Component
 from scoring import *
 
-class CustomSortList(Component):
+class MatchList(Component):
 
     def __init__(self, **kwargs):
-        self.selector = 'c-sort-list'
-        self.decorator = 'CustomSortList'
-        self.items = []
+        self.selector = 'c-match-list'
+        self.decorator = 'MatchList'
+        self.nodes = []
         self._sol = []
         super().__init__(**kwargs)
 
-    def setdata_from_list(self, lst):
+    def set_sources(self, sources):
         """
-        Load items and set solution from an ordered list.
+        Set source nodes.
         """
-        self.items = []
+        for source in sources:
+            self.nodes.append({
+                "id": str(uuid4()),
+                "content": str(source),
+                "source": True
+            })
+
+    def set_targets(self, targets):
+        """
+        Set target nodes.
+        """
+        for target in targets:
+            self.nodes.append({
+                "id": str(uuid4()),
+                "content": str(target),
+                "target": True,
+                "multiple" : False
+            })
+
+    def add_targets(self, targets):
+        """
+        Add target nodes.
+        """
+        contents = [node['content'] for node in self.nodes if node.get('target', False)]
+        contents = []
+        for target in targets:
+            if target not in contents:
+                self.nodes.append({
+                    "id": str(uuid4()),
+                    "content": str(target),
+                    "target": True,
+                    "multiple" : False
+                })
+
+    def set_multiple(self):
+        """
+        Add target nodes.
+        """
+        for node in self.nodes:
+            node['multiple'] = True
+
+    def setdata_from_matches(self, matches):
+        """
+        Load matched pairs of items in the component.
+        """ 
+        self.nodes = []
         self._sol = []
-        for content in lst:
-            id = str(uuid4())
-            self.items.append({"id": id, "content": content})
-            self._sol.append(id)
-        rd.shuffle(self.items)
+        for source, target in matches:
+            sourceid = str(uuid4())
+            targetid = str(uuid4())
+            self.nodes.append({
+                "id": sourceid,
+                "content": str(source),
+                "source": True
+            })
+            self.nodes.append({
+                "id": targetid,
+                "content": str(target),
+                "target": True,
+                "multiple" : False
+            })
+            self._sol.append({'source': sourceid, 'target': targetid})
+        rd.shuffle(self.nodes)
 
     def shuffle(self):
-        """
-        Shuffle the component items.
-        """
-        rd.shuffle(self.items)
+        rd.shuffle(self.nodes)
 
-    def eval(self, display=True, scoring="KendallTau", disabled=True):
+    def eval(self, display=True, scoring="RightMinusWrong", disabled=True):
         """
         Evaluate the answer stored in the component.
-        """
+        """ 
+        nbright, nbwrong = 0, 0
+        rightsource = []
+        for link in self.links:
+            if {'source': link['source'], 'target': link['target']} in self._sol:
+                rightsource.append(link['source'])
 
-        if display:
-            for i, e in enumerate(self.items):
-
-                id = e['id']
-
-                if id == self._sol[i]:
-                    e['css'] = 'success-state'
-                    css_state="success"
+        for node in self.nodes:
+            if 'source' in node and node['source']:
+                if node['id'] in rightsource:
+                    node['css'] = 'success-state icon-check-before'
+                    nbright += 1
                 else:
-                    e['css'] = 'error-state'
-                    css_state="danger"
+                    node['css'] = 'error-state icon-times-before'
+                    nbwrong += 1
 
-                e['content']=  """<div class="d-flex justify-content-between align-items-center">
-                        <span class="badge badge-%s"> %s </span>
-                        <span> %s </span>
-                        <span></span>
-                    </div>""" % (css_state,str(1+self._sol.index(id)),e['content'])
-        
-        order = [self._sol.index(item['id']) for item in self.items]
-
-        if scoring == "ExactOrder":
-            score = exact_order(order)
-        elif scoring == "KendallTau":
-            score = kendall_tau(order)         
+        if scoring == "AllOrNothing":
+            score = all_or_nothing(nbright, nbwrong)
+        elif scoring == "RightMinusWrong":
+            score = right_minus_wrong(nbright, nbwrong, nbsol=len(self._sol))          
+        elif scoring == "Custom":
+            score = custom_scoring(nbright, nbwrong, nbsol=len(self._sol), nbitems=len(self.items))
         else:
             raise ValueError(f"'{scoring}' is not a valid scoring")
 
         return score
-
