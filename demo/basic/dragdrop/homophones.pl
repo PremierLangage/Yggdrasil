2f0c8ba283c643a3bb48abc65edf6fc777b15f6e
David DOYEN <david.doyen@univ-eiffel.fr>
2022-02-28 12:56:49 +0100

    ddoyen1 modified Yggdrasil/demo/basic/dragdrop/homophones.pl

--- demo/basic/dragdrop/homophones.pl
+++ demo/basic/dragdrop/homophones.pl
@@ -15,5 +15,6 @@ inputblock == #|html|
 * Tu est toujours en retard, {{ input.drops[1]|component }} agaçant !
 * Je n'aime pas {{ input.drops[2]|component }} méthodes.
 
-{{ input.labels[0]|component }} {{ input.labels[1]|component }}
+
+{% for label in input.labels %} {{ label|component }} {% endfor %}
 ==
