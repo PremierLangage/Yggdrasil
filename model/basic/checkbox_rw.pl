4c3837012235e4f32f8e843389aa748dd3b3fb4b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-01 16:13:27 +0100

    ddoyen1 modified Yggdrasil/model/basic/checkbox_rw.pl

--- model/basic/checkbox_rw.pl
+++ model/basic/checkbox_rw.pl
@@ -55,3 +55,33 @@ score = input.eval()
 input.display_feedback()
 input.disable()
 ==
+
+# API documentation
+
+apidoc == #|json|
+{
+    "name": "checkbox",
+    "keys": {
+        "items": {
+            "type": "(str, list[str])",
+            "default": "",
+            "description": "Liste des items. Elle peut être saisie comme une liste ou comme une chaîne multilignes (chaque ligne correspondant à un item)."
+        },
+        "indsol": {
+            "type": "list[int]",
+            "default": "[]"
+            "description": "Indice des solutions dans la liste des items (la numérotation commence à 0)."
+        },
+        "shuffled": {
+            "type": "bool",
+            "default": True,
+            "description": "Valeur indiquant si les items seront mélangés."
+        },
+        "scoring": {
+            "type": "('AllOrNothing', 'RightMinusWrong', 'CorrectItems')",
+            "default": "'RightMinusWrong'",
+            "description": "Barème de l'exercice."
+        }
+    }
+}
+==
