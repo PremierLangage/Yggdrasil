458a957ae38ca252decf6d1183e47689902b8674
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-01 21:26:37 +0100

    ddoyen1 modified Yggdrasil/demo/basic/checkbow_rw/capitales.pl

---
+++ demo/basic/checkbow_rw/capitales.pl
@@ -0,0 +1,31 @@
+extends = /model/basic/checkbox.pl
+
+question ==
+Parmi ces villes, lesquelles sont des capitales d'états européens ?
+==
+
+right ==
+Paris
+Rome
+Madrid
+Berlin
+Londres
+Bruxelles
+Berne
+==
+
+wrong ==
+Lyon
+Milan
+Barcelone
+Munich
+Liverpool
+Anvers
+Genève
+==
+
+nbitems = 5
+
+minright = 1
+
+maxright = 3
