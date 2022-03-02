b1f0aa1c44acc539b6ed810b4436680890e00e5d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-02 15:30:29 +0100

    ddoyen1 modified Yggdrasil/demo/basic/filltext/genetics.pl

--- demo/basic/filltext/genetics.pl
+++ demo/basic/filltext/genetics.pl
@@ -7,8 +7,10 @@ Compléter le texte suivant avec les bonnes étiquettes.
 filledtext ==
 L’ensemble des gènes caractéristiques de l’espèce à laquelle appartient un organisme, constitue son {génome}. 
 Chez les individus d’une même espèce, un gène peut cependant exister sous différentes formes présentant de légères modifications de séquence : les allèles. 
-L’ensemble des allèles d’un individu définit son {génotype}. Lorsqu’ils s’expriment, lors de la synthèse des protéines, les gènes participent à la construction de l’individu et à la mise en place de son {phénotype}. 
+L’ensemble des allèles d’un individu définit son [génotype]. Lorsqu’ils s’expriment, lors de la synthèse des protéines, les gènes participent à la construction de l’individu et à la mise en place de son {phénotype}. 
 ==
+
+delimiters = ["\[", "\]"]
 
 labels ==
 caryotype
