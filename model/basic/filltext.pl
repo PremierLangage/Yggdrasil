5c2b6f75810233cb912f614ad1a675a6f7bfa4f8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-02 15:44:57 +0100

    ddoyen1 modified Yggdrasil/model/basic/filltext.pl

--- model/basic/filltext.pl
+++ model/basic/filltext.pl
@@ -66,6 +66,36 @@ input.display_feedback()
 input.disable()
 ==
 
+# API documentation
+
+apidoc == #|json|
+{
+    "name": "filltext",
+    "keys": {
+        "filledtext": {
+            "type": "str",
+            "default": "",
+            "description": "Texte complet, avec les parties à compléter spécifiées par les délimiteurs définies dans la clé `delimiters`."
+        },
+        "delimiters": {
+            "type": "lst[str]",
+            "default": "['{', '}']",
+            "description": "Délimiteurs utilisés pour spécifier les parties à compléter."
+        },
+        "labels": {
+            "type": "(str, list[str])",
+            "default": "[]",
+            "description": "Liste d'étiquettes supplémentaires. Elle peut être saisie comme une liste ou comme une chaîne multilignes (chaque ligne correspondant à un item)."
+        },
+        "scoring": {
+            "type": "('AllOrNothing', 'RightMinusWrong', 'CorrectItems')",
+            "default": "'RightMinusWrong'",
+            "description": "Barème de l'exercice."
+        }
+    }
+}
+==
+
 style.dragdrop ==
 <style>
 .drag-drop-component {
