a9c4a984a2221fbf835cb41bb1947998ab5fbf6c
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-02 12:36:57 +0100

    ddoyen1 modified Yggdrasil/demo/basic/textinput/listening.pl

---
+++ demo/basic/textinput/listening.pl
@@ -0,0 +1,16 @@
+extends = /model/basic/textinput.pl
+
+audiofile =$ /demo/media/english_sentence.mp3
+
+question ==
+<audio id="player" src="{{ audiofile }}"></audio> 
+<button onclick="player.play()" class="btn btn-sm btn-info icon-audio"></button> Transcrire la phrase.
+==
+
+sol ==
+I'm gonna make him an offer he can't refuse
+==
+
+diffmeasure = "EditRatio"
+
+tol = 0.1
