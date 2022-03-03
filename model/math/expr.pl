7a25f6c38d653f49bd97b09a5c039877e67a5125
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-03 14:57:22 +0100

    ddoyen1 modified Yggdrasil/model/math/expr.pl

--- model/math/expr.pl
+++ model/math/expr.pl
@@ -18,3 +18,26 @@ if modulo != 0:
     modulo = sympify(modulo)
 input.evalparam = {'equality': equality, 'modulo': modulo, 'checkratsimp': checkratsimp, 'unauthorized_func': unauthorized_func, 'local_dict': sympify(symbol_dict)}
 ==
+
+apidoc == #|json|
+{
+    "name": "expr",
+    "keys": {
+        "sol": {
+            "type": "Expr",
+            "default": "",
+            "description": "Bonne réponse. Elle doit être définie dans le script `before` comme un objet SymPy de type Expr."
+        },
+        "equality": {
+            "type": "('', 'UpToConstant', 'Modulo')",
+            "default": "''",
+            "description": "Type d'égalitée utilisé : égalité stricte (''), égalité à une constante près ('UpToConstant'), égalité modulo un nombre ('Modulo')."
+        },
+        "modulo": {
+            "type": "(int, float)",
+            "default": "0",
+            "description": "Nombre utilisé dans le cas d'une égalité modulo un nombre."
+        }
+    }
+}
+==
