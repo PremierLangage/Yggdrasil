c3eb84fd3278a463ecf1293d88d072999629d8ea
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-04 18:18:52 +0100

    ddoyen1 modified Yggdrasil/model/math/tuple.pl

--- model/math/tuple.pl
+++ model/math/tuple.pl
@@ -11,3 +11,23 @@ evalparam ==
 from sympy import sympify
 input.evalparam = {'checksize':checksize, 'local_dict': sympify(symbol_dict)}
 ==
+
+# API documentation
+
+apidoc == #|json|
+{
+    "name": "tuple",
+    "keys": {
+        "sol": {
+            "type": "(list, tuple, Tuple",
+            "default": "",
+            "description": "Bonne réponse. Elle doit être définie dans le script `before` comme une liste/tuple d'objets SymPy ou un objet SymPy de type Tuple."
+        },
+        "checksize": {
+            "type": "bool",
+            "default": "'False",
+            "description": "Valeur indiquant si une réponse de taille incorrecte déclenche un avertissement."
+        }
+    }
+}
+==
