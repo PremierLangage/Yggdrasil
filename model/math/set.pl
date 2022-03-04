4ad1c4c42ff59bb71e03ae16a7f59eff25d2b3f4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-04 11:22:46 +0100

    ddoyen1 modified Yggdrasil/model/math/set.pl

--- model/math/set.pl
+++ model/math/set.pl
@@ -13,3 +13,21 @@ evalparam ==
 from sympy import sympify
 input.evalparam = {'wobracket': wobracket, 'checkratsimp': checkratsimp, 'local_dict': sympify(symbol_dict)}
 ==
+
+apidoc == #|json|
+{
+    "name": "set",
+    "keys": {
+        "sol": {
+            "type": "FiniteSet",
+            "default": "",
+            "description": "Bonne réponse. Elle doit être définie dans le script `before` comme un objet SymPy de type FiniteSet."
+        },
+        "wobracket": {
+            "type": "('', 'UpToConstant', 'Modulo')",
+            "default": "''",
+            "description": "Type d'égalitée utilisé : égalité stricte (''), égalité à une constante près ('UpToConstant'), égalité modulo un nombre ('Modulo')."
+        }
+    }
+}
+==
