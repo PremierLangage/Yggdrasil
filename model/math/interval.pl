84c889cb8226729421f52af4f57638dd572079d5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-04 16:44:29 +0100

    ddoyen1 modified Yggdrasil/model/math/interval.pl

--- model/math/interval.pl
+++ model/math/interval.pl
@@ -25,18 +25,3 @@ apidoc == #|json|
     }
 }
 ==
-
-extends = /model/math/input.pl
-
-checkratsimp = True
-
-evaluator ==
-from evalsympy import eval_interval
-score, error = eval_interval(answers['math'], sol)
-feedback = message[error]
-==
-
-solution ==
-La solution est $! {{ sol|latex}} !$.
-==
-
