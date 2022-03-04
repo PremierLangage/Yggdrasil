ee8fc82be44be52187bee8b8a1508358836b2dcd
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-04 10:59:59 +0100

    ddoyen1 modified Yggdrasil/model/math/complex.pl

--- model/math/complex.pl
+++ model/math/complex.pl
@@ -19,3 +19,26 @@ evalparam ==
 from sympy import sympify
 input.evalparam = {'imaginary_unit': imaginary_unit, 'form': complex_form, 'checkratsimp': checkratsimp, 'unauthorized_func': unauthorized_func, 'local_dict': sympify(symbol_dict)}
 ==
+
+apidoc == #|json|
+{
+    "name": "complex",
+    "keys": {
+        "sol": {
+            "type": "Complex",
+            "default": "",
+            "description": "Bonne réponse. Elle doit être définie dans le script `before` comme un objet SymPy de type Complex."
+        },
+        "complex_form": {
+            "type": "('', 'Cartesian', 'Exponential')",
+            "default": "''",
+            "description": "Forme attendue de la réponse de l'élève : pas de forme particulière (''), forme cartésienne ('Cartesian'), forme exponentielle ('Exponential')."
+        },
+        "imaginary_unit": {
+            "type": "str",
+            "default": "'i'",
+            "description": "Nom de l'unité imaginaire utilisée pour interpréter la réponse."
+        }
+    }
+}
+==
