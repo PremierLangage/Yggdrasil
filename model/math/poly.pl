5304a49a4c84d79b6b32f69c3b7c8ec757f6788c
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-04 12:42:04 +0100

    ddoyen1 modified Yggdrasil/model/math/poly.pl

--- model/math/poly.pl
+++ model/math/poly.pl
@@ -16,3 +16,31 @@ from sympy import poly
 poly_var = str(poly(sol).gen)
 input.evalparam = {'var': poly_var, 'form': poly_form}
 ==
+
+apidoc == #|json|
+{
+    "name": "poly",
+    "keys": {
+        "sol": {
+            "type": "(Expr, Poly)",
+            "default": "",
+            "description": "Bonne réponse. Elle doit être définie dans le script `before` comme un objet SymPy de type Expr ou Poly."
+        },
+        "poly_form": {
+            "type": "('', 'Expanded', 'Factorized')",
+            "default": "''",
+            "description": "Forme attendue de la réponse : pas de forme particulière (''), forme développée ('Expanded'), forme factorisée ('Factorized')."
+        },
+        "poly_domain": {
+            "type": "('R', 'C')",
+            "default": "'R'",
+            "description": "Domaine du polynôme : réels ('R'), complexes ('C')."
+        },
+        "poly_var": {
+            "type": "(str, None)",
+            "default": "None",
+            "description": "Variable du polynôme. Si cette clé vaut None, la variable est détectée automatiquement."
+        },
+    }
+}
+==
