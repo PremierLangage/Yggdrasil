e6be0d16c239c56123723c394fe5094d73ea9067
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-04 10:37:11 +0100

    ddoyen1 modified Yggdrasil/model/doc/docgen.pl

--- model/doc/docgen.pl
+++ model/doc/docgen.pl
@@ -28,7 +28,7 @@ before== #|python|
 doc = eval(apidoc)
 from jinja2 import Environment, BaseLoader, contextfilter, Template
 Env = Environment(loader=BaseLoader())
-text = "{% raw %} <p>\n" + Env.from_string(text).render({'doc': doc}) + "{% endraw %}"
+text = "\n" + Env.from_string(text).render({'doc': doc}) + ""
 ==
 
 evaluator== #|python|
