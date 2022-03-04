801ee0ab4a70c422cdda9e65134063e0981edab8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-05 00:01:23 +0100

    ddoyen1 modified Yggdrasil/model/math/matrix.pl

--- model/math/matrix.pl
+++ model/math/matrix.pl
@@ -94,3 +94,28 @@ style.matrix ==
 }
 </style>
 ==
+
+# API documentation
+
+apidoc == #|json|
+{
+    "name": "matrix",
+    "keys": {
+        "sol": {
+            "type": "FiniteSet",
+            "default": "",
+            "description": "Bonne réponse. Elle doit être définie dans le script `before` comme un objet SymPy de type Matrix."
+        },
+        "resizable": {
+            "type": "bool",
+            "default": "'False",
+            "description": "Valeur indiquant si le champ de réponse est redimensionnable."
+        },
+        "initsize": {
+            "type": "lst[int, int]",
+            "default": "[2, 2]",
+            "description": "Dimention initiale du champ de réponse. Cette clé n'est utile que si le champ de réponse est redimensionnable. Autrement, la dimension du champ de réponse matrice est automatiquement égale à la dimension de sol."
+        }
+    }
+}
+==
