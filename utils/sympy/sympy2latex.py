c27548e5fe782ed5f8c3b481abda677d16219fc7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-04 17:10:04 +0100

    ddoyen1 modified Yggdrasil/utils/sympy/sympy2latex.py

--- utils/sympy/sympy2latex.py
+++ utils/sympy/sympy2latex.py
@@ -11,7 +11,7 @@ class CustomLatexPrinter(LatexPrinter):
     """
 
     def __init__(self, settings=None):
-        LatexPrinter.__init__(self, settings)
+        #LatexPrinter.__init__(self, settings)
         #if 'interv_rev_brack' in settings:
         #    self._settings['interv_rev_brack'] = settings['interv_rev_brack']
         #else:
