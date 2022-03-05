1cec1a9ec87c98c5978b78e0e20c4f847fcdab92
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-06 00:31:29 +0100

    ddoyen1 modified Yggdrasil/utils/inputfields/jxginput.py

--- utils/inputfields/jxginput.py
+++ utils/inputfields/jxginput.py
@@ -43,7 +43,11 @@ class JXGInput(Component):
         return self.score
 
     def display_feedback(self):
-        self.add_script("board.create('point',[{{xsol}}, {{ysol}}],{size:2,name:'',color:'green'});", {'xsol':self.sol[0], 'ysol':self.sol[1]})
+        script = """
+        M.setAttribute({color: red});
+        board.create('point',[{{xsol}}, {{ysol}}],{size:2,name:'',color:'green'});
+        """
+        self.add_script(, {'xsol':self.sol[0], 'ysol':self.sol[1]})
 
 
 
