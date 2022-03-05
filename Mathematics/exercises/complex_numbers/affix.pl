360ca207d4d809e4bd2d6a725aacf99ff57f3885
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-06 00:20:11 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/complex_numbers/affix.pl

--- Mathematics/exercises/complex_numbers/affix.pl
+++ Mathematics/exercises/complex_numbers/affix.pl
@@ -11,17 +11,16 @@ ysol = randint(-4, 4, [0])
 z = xsol + ysol*I
 ==
 
-attributes = {"showNavigation":False, "boundingbox":[-6,6,6,-6]}
-
-
-script_init ==
-board.create('grid',[],{gridX:1,gridY:1});
-board.create('axis',[[0,0],[1,0]],{name:'Re',withLabel:true,label:{position:'urt',offset:[-5,10]},ticks:{visible: false}});
-board.create('axis',[[0,0],[0,1]],{name:'Im',withLabel:true,label:{position:'urt',offset:[10,0]},ticks:{visible: false}});
-const M = board.create('point',[0, 0],{size:2,name:'M',color:'blue'});
-==
-
-
 question ==
 Placer le point $! M !$ d'affixe $! {{ z|latex }} !$ dans le plan ci-dessous.
 ==
+
+attributes = {"showNavigation":False, "boundingbox":[-6,6,6,-6]}
+
+script_init ==
+board.create('grid',[],{gridX:1, gridY:1});
+board.create('axis',[[0,0],[1,0]],{name:'Re',withLabel:true,label:{position:'urt',offset:[-5,10]},ticks:{visible: false}});
+board.create('axis',[[0,0],[0,1]],{name:'Im',withLabel:true,label:{position:'urt',offset:[10,0]},ticks:{visible: false}});
+board.create('point',[0, 0],{size:2,name:'M',color:'blue'});
+==
+
