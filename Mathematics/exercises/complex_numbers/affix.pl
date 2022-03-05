aa1f95441611e3eb66c3cb2e8a9b2840edfd4b99
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-06 00:48:04 +0100

    ddoyen1 modified Yggdrasil/Mathematics/exercises/complex_numbers/affix.pl

--- Mathematics/exercises/complex_numbers/affix.pl
+++ Mathematics/exercises/complex_numbers/affix.pl
@@ -17,10 +17,10 @@ Placer le point $! M !$ d'affixe $! {{ z|latex }} !$ dans le plan ci-dessous.
 
 attributes = {"showNavigation":False, "boundingbox":[-6,6,6,-6]}
 
-script_init ==
-board.create('grid',[],{gridX:1, gridY:1});
-board.create('axis',[[0,0],[1,0]],{name:'Re',withLabel:true,label:{position:'urt',offset:[-5,10]},ticks:{visible: false}});
-board.create('axis',[[0,0],[0,1]],{name:'Im',withLabel:true,label:{position:'urt',offset:[10,0]},ticks:{visible: false}});
-var M = board.create('point',[0, 0],{size:2,name:'M',color:'blue'});
+jxgscript == #|js|
+board.create('grid', [], {gridX: 1, gridY: 1});
+board.create('axis', [[0, 0], [1, 0]], {name: 'Re', withLabel: true, label: {position:'urt', offset: [-5, 10]}, ticks:{visible: false}});
+board.create('axis', [[0, 0], [0, 1]], {name: 'Im', withLabel: true, label: {position: 'urt', offset: [10, 0]}, ticks:{visible: false}});
+var M = board.create('point', [0, 0], {size: 2, name: 'M', color: 'blue'});
 ==
 
