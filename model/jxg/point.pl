33b6a887609ae51ec12f309fb84532a4465d9f27
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-05 19:47:02 +0100

    ddoyen1 modified Yggdrasil/model/jxg/point.pl

--- model/jxg/point.pl
+++ model/jxg/point.pl
@@ -71,7 +71,7 @@ board.create('point',[{{xsol}}, {{ysol}}],{size:2,name:'',color:'green'});
 ==
 
 inputblock ==
-<div style="width=300px;">
+<div style="width: 300px;">
 {{ input|component }}
 </div>
 ==
