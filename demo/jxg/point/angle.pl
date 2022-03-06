261e71cd00d025c1edb35b4c31ddd4844cbd76b5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-06 21:24:10 +0100

    ddoyen1 modified Yggdrasil/demo/jxg/point/angle.pl

--- demo/jxg/point/angle.pl
+++ demo/jxg/point/angle.pl
@@ -1,20 +1,23 @@
 extends = /model/jxg/point.pl
 
 before ==
-angle=choice([pi/4,pi/2,3*pi/4,pi,3*pi/2])
-angle_float = angle.evalf()
-xsol = cos(angle_float)
-ysol = sin(angle_float)
-==
-
-jxgscript ==
-var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
-var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
-var psol = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'blue',fixed:false});
-var secOAM = board.create('sector',[O,A,M],{color:'orange'});
+angle = choice([pi/4, pi/2, 3*pi/4, pi, 3*pi/2])
+xsol = cos(angle.evalf())
+ysol = sin(angle.evalf())
 ==
 
 question ==
-Placer le point M de sorte que l'angle $% (\overrightarrow{OA},\overrightarrow{OM}) %$ ait une mesure égale à $% \displaystyle {{angle.latex}} %$. 
+Placer le point M de sorte que l'angle $! (\overrightarrow{OA},\overrightarrow{OM}) !$ ait une mesure égale à $% \displaystyle {{angle.latex}} %$. 
+==
+
+attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
+
+tol = 0.2
+
+jxgscript == #|js|
+var circle = board.create('circle', [[0, 0], [0, 1]], {strokeColor: 'blue', fixed: true});
+var O = board.create('point', [0, 0], {size: 1, name: 'O', color: 'black', fixed: true});
+var A = board.create('point', [1, 0], {size: 1, name: 'A', color: 'black', fixed: true});
+var psol = board.create('glider', [1, 0.5, circle], {size: 2, name: 'M', color: 'blue', fixed: false});
+var secOAM = board.create('sector', [O, A, psol], {color: 'orange'});
 ==
