01cb89ed21f7f0a7e2778615bbc7eb05a7bb2044
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-06 19:01:32 +0100

    ddoyen1 modified Yggdrasil/model/doc/docgen.pl

--- model/doc/docgen.pl
+++ model/doc/docgen.pl
@@ -6,39 +6,68 @@ builder =@ /builder/before.py
 
 apidoc == #|json|
 {
-    "name": "textinput",
+    "name": "input",
     "keys": {
-        "xsol": {
-            "type": "float",
+        "before": {
+            "type": "str",
             "default": "",
-            "description": "Abscisse du point solution."
+            "description": "Script Python permettant de générer les clés de l'exercice."
         },
-        "ysol": {
-            "type": "float",
+        "title": {
+            "type": "str",
             "default": "",
-            "description": "Ordonnée du point solution."
+            "description": "Titre de l'exercice."
         },
-        "attributes": {
+        "question": {
+            "type": "str",
+            "default": "",
+            "description": "Template HTML contenant l'énoncé de l'exercice."
+        },
+        "inputblock": {
+            "type": "str",
+            "default": "",
+            "description": "Template HTML contenant l'énoncé de l'exercice."
+        },
+        "solution": {
+            "type": "str",
+            "default": "",
+            "description": "Template HTML contenant la correction de l'exercice."
+        },
+        "evaluator": {
+            "type": "str",
+            "default": "",
+            "description": "Script Python permettant d'évaluer la réponse de l'exercice."
+        },
+        "keypad": {
+            "type": "list",
+            "default": "[]",
+            "description": "Liste des boutons du clavier virtuel attaché au champ de réponse."
+        },
+        "embed": {
+            "type": "str",
+            "default": "''",
+            "description": "Formule dans laquelle est insérée le champ de réponse."
+        },
+        "checkratsimp": {
+            "type": "bool",
+            "default": "True",
+            "description": "Valeur indiquant si l'évaluation vérifie que les valeurs rationnelles sont simplifiées dans la réponse saisie."
+        },
+        "symbol_dict": {
+            "type": "dict",
+            "default": "{'e': E}",
+            "description": "Dictionnaire des symboles utilisés pour convertir la réponse saisie en expression SymPy."
+        },
+        "unauthorized_func": {
+            "type": "list[str]",
+            "default": "[]",
+            "description": "Liste des fonctions non autorisées."
+        },
+        "latexsettings": {
             "type": "dict",
             "default": "{}",
-            "description": "Paramètres du panneau graphiqe interactif."
-        },
-        "jxgscript": {
-            "type": "str",
-            "default": "",
-            "description": "Script JSXGraph."
-        },
-        "pointname": {
-            "type": "str",
-            "default": "",
-            "description": "Nom du point à placer."
-        },
-        "tol": {
-            "type": "float",
-            "default": "0.1",
-            "description": "Erreur maximum (en distance euclidienne) pour considérer une réponse comme correcte."
+            "description": "Dictionnaire des paramètres de conversion SymPy vers LaTeX."
         }
-
     }
 }
 ==
