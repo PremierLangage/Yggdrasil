9a1222da1581d3fbd05882ac72d8d6aca30903b5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-06 19:53:39 +0100

    ddoyen1 modified Yggdrasil/model/doc/docgen.pl

--- model/doc/docgen.pl
+++ model/doc/docgen.pl
@@ -3,70 +3,24 @@ grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 # API documentation
-
 apidoc == #|json|
 {
-    "name": "input",
+    "name": "AMC_TXT",
     "keys": {
-        "before": {
-            "type": "str",
-            "default": "",
-            "description": "Script Python permettant de générer les clés de l'exercice."
-        },
         "title": {
             "type": "str",
-            "default": "",
-            "description": "Titre de l'exercice."
+            "default": "''",
+            "description": "Titre du quiz."
         },
-        "question": {
-            "type": "str",
-            "default": "",
-            "description": "Template HTML contenant l'énoncé de l'exercice."
-        },
-        "inputblock": {
-            "type": "str",
-            "default": "",
-            "description": "Template HTML contenant l'énoncé de l'exercice."
-        },
-        "solution": {
-            "type": "str",
-            "default": "",
-            "description": "Template HTML contenant la correction de l'exercice."
-        },
-        "evaluator": {
-            "type": "str",
-            "default": "",
-            "description": "Script Python permettant d'évaluer la réponse de l'exercice."
-        },
-        "keypad": {
-            "type": "list",
-            "default": "[]",
-            "description": "Liste des boutons du clavier virtuel attaché au champ de réponse."
-        },
-        "embed": {
+        "intro": {
             "type": "str",
             "default": "''",
-            "description": "Formule dans laquelle est insérée le champ de réponse."
+            "description": "Texte d'introduction du quiz."
         },
-        "checkratsimp": {
-            "type": "bool",
-            "default": "True",
-            "description": "Valeur indiquant si l'évaluation vérifie que les valeurs rationnelles sont simplifiées dans la réponse saisie."
-        },
-        "symbol_dict": {
-            "type": "dict",
-            "default": "{'e': E}",
-            "description": "Dictionnaire des symboles utilisés pour convertir la réponse saisie en expression SymPy."
-        },
-        "unauthorized_func": {
-            "type": "list[str]",
-            "default": "[]",
-            "description": "Liste des fonctions non autorisées."
-        },
-        "latexsettings": {
-            "type": "dict",
-            "default": "{}",
-            "description": "Dictionnaire des paramètres de conversion SymPy vers LaTeX."
+        "quiz": {
+            "type": "str",
+            "default": "''",
+            "description": "Questions du quiz au format AMC-TXT."
         }
     }
 }
