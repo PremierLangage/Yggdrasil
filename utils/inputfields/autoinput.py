b96c8514e7d99983f85ca3cd8c4ce961a000ebf8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-06 23:55:04 +0100

    ddoyen1 modified Yggdrasil/utils/inputfields/autoinput.py

--- utils/inputfields/autoinput.py
+++ utils/inputfields/autoinput.py
@@ -7,7 +7,19 @@ class AutoInput(Component):
         self.decorator = 'AutoInput'
         super().__init__(**kwargs)
 
+    def set_items(self, items):
+        """
+        Set the items for the autocomplete list.
+        """
+        if isinstance(items, str):
+            self.autocomplete = items.splitlines()
+        else:
+            self.autocomplete = items
+
     def eval(self):
+        """
+        Evaluate the input field.
+        """
         if self.sol == self.value:
             self.score = 100
         else:
@@ -15,26 +27,29 @@ class AutoInput(Component):
         return self.score
 
     def display_feedback(self):
+        """
+        Display visual feedback.
+        """
         if self.score == 100:
             self.suffix = r'<i class="fas fa-check" style="color: var(--success)"></i>'
         elif self.score >= 0:
             self.suffix = r'<i class="fas fa-times" style="color: var(--danger)"></i></i>'
 
-    def set_items(self, items):
-        if isinstance(items, str):
-            self.autocomplete = items.splitlines()
-        else:
-            self.autocomplete = items
-
     def hide_feedback(self):
+        """
+        Hide visual feedback.
+        """
         self.suffix = ""
 
     def disable(self):
+        """
+        Disable the input field.
+        """
         self.disabled = True
 
     def render(self):
         """
-        Return the HTML code of the component.
+        Return the HTML code of the input field.
         """
         selector = self.selector
         cid = self.cid
