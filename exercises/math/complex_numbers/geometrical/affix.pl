61522b199f91614f982dbbb4729754342ac001ee
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-07 14:52:00 +0100

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/geometrical/affix.pl

--- exercises/math/complex_numbers/geometrical/affix.pl
+++ exercises/math/complex_numbers/geometrical/affix.pl
@@ -3,8 +3,6 @@
 # complex numbers, affix, complex plane, geometrical representation
 # Placer dans le plan un point d'affixe donné. 
 
-extends = /model/jxg/point.pl
-
 before ==
 xsol = randint(-4, 4, [0])
 ysol = randint(-4, 4, [0])
@@ -17,10 +15,15 @@ Placer le point $! M !$ d'affixe $! {{ z|latex }} !$ dans le plan ci-dessous.
 
 attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 
+tol = 0.2
+
+pointname = "M"
+
 jxgscript == #|js|
 board.create('grid', [], {gridX: 1, gridY: 1});
 board.create('axis', [[0, 0], [1, 0]], {name: 'Re', withLabel: true, label: {position:'urt', offset: [-5, 10]}, ticks:{visible: false}});
 board.create('axis', [[0, 0], [0, 1]], {name: 'Im', withLabel: true, label: {position: 'urt', offset: [10, 0]}, ticks:{visible: false}});
 var psol = board.create('point', [0, 0], {size: 2, name: 'M', color: 'blue'});
 ==
+
 
