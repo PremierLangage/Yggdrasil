16e74d392ffb3c5ace42a7331a9dbe6dd9774eea
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-07 17:58:01 +0100

    ddoyen1 modified Yggdrasil/exercises/math/analysis/logarithm/ln_equation.pl

--- exercises/math/analysis/logarithm/ln_equation.pl
+++ exercises/math/analysis/logarithm/ln_equation.pl
@@ -11,7 +11,7 @@ x = Symbol('x')
 a = randint(-8, 8, [0])
 b = randint(-8, 8, [0])
 lhs = ln(a*x + b)
-rhs = randint(-8,8,[0])
+rhs = randint(-8, 8, [0])
 sol = list(solveset(lhs-rhs, x, domain=S.Reals))[0]
 ==
 
