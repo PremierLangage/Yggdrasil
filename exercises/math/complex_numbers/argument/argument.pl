c12c4cf03fef80a2fe9c0d9ba5b13a46f840d0dd
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-07 15:45:24 +0100

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/argument/argument.pl

---
+++ exercises/math/complex_numbers/argument/argument.pl
@@ -0,0 +1,21 @@
+# Author: D. Doyen
+# Tags: complex numbers
+# 19/8/2020
+
+extends = /model/math/expr.pl
+
+equality = "Modulo"
+
+modulo = 2*pi
+
+title = Argument d'un nombre complexe
+
+before ==
+z = choice([I,-I,1,-1,1+I,1-I,I-1,-1-I])
+z = randint(1, 3)*z
+sol = arg(z)
+==
+
+question ==
+Déterminer un argument du nombre complexe $! z= {{ z|latex }} !$.
+==
