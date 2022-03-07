52ba24a686df2ae908cf4d9795c7135f547206ec
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-07 17:00:23 +0100

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/transformations/cart2exp.pl

--- exercises/math/complex_numbers/transformations/cart2exp.pl
+++ exercises/math/complex_numbers/transformations/cart2exp.pl
@@ -7,7 +7,7 @@ extends = /model/math/complex.pl
 
 complex_form = "Exponential"
 
-title = "Forme exponentielle vers cartésienne"
+title = "Forme cartésienne vers exponentielle"
 
 before == #|py|
 from sympy import arg
@@ -18,18 +18,4 @@ sol = abs(z)*exp(I*arg(z))
 
 question == 
 Ecrire sous forme exponentielle le nombre complexe $! \large {{z|latex}} !$.
-==
-
-before ==
-a = randint(1,3)
-theta = randitem([pi,pi/2,3*pi/2])
-
-with evaluate(False):
-    sol = a*exp(I*theta)
-
-z = expand_complex(sol)
-==
-
-text == 
-Ecrire sous forme exponentielle le nombre complexe $! \\large {{z|latex}} !$.
 ==
