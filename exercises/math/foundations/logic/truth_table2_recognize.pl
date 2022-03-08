ac3cae25742028b54faa1c55a0f4fa1f0c6aa5b8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-08 11:48:35 +0100

    ddoyen1 modified Yggdrasil/exercises/math/foundations/logic/truth_table2_recognize.pl

--- exercises/math/foundations/logic/truth_table2_recognize.pl
+++ exercises/math/foundations/logic/truth_table2_recognize.pl
@@ -30,9 +30,9 @@ question ==
 Soit P et Q deux propositions logiques. On considère une proposition $! T(P,Q) !$, construite à partir des propositions P et Q, dont la table de vérité est donnée ci-dessous. 
 <table class="table table-bordered">
 <tr>
-<td style ="white-space: nowrap; width: 1%;"> $! P !$ </td>
-<td> $! Q !$ </td>
-<td> $! T(P,Q) !$ </td>
+<td style="white-space: nowrap; width: 1%;"> $! P !$ </td>
+<td style="white-space: nowrap; width: 1%;"> $! Q !$ </td>
+<td style="white-space: nowrap; width: 1%;"> $! T(P,Q) !$ </td>
 </tr>
 {% for i in range(4) %}
 <tr>
