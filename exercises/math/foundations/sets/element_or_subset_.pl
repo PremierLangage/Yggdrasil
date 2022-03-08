9a4527ba6bdd94b6706be28ae695a08df537b243
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-08 15:44:33 +0100

    ddoyen1 modified Yggdrasil/exercises/math/foundations/sets/element_or_subset_.pl

--- exercises/math/foundations/sets/element_or_subset_.pl
+++ exercises/math/foundations/sets/element_or_subset_.pl
@@ -22,8 +22,8 @@ labels = [label_in, label_subset, ""]
 sol=[]
 lhs=[]
 rhs=[]
-nbdrops = 2
-cases = [3, 3] #param['cases']
+nbdrops = len(cases)
+cases = param['cases']
 
 shuffle(cases)
 for i in range(len(cases)):
