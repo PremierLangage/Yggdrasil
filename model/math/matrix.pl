d6459f038b1db544671dccf3f2b8538b27f85eb0
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-09 16:13:55 +0100

    ddoyen1 modified Yggdrasil/model/math/matrix.pl

--- model/math/matrix.pl
+++ model/math/matrix.pl
@@ -18,7 +18,7 @@ from sympy import E, I, pi, oo
 from sympy import sqrt, Abs, sin, cos, tan, exp, ln
 from sympy import Symbol, symbols, var
 from sympy import sympify, simplify, Lambda
-from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval
+from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval, Matrix
 from sympy import integrate
 from random import choice, choices, sample, shuffle
 from plrandom import randint, sampleint
