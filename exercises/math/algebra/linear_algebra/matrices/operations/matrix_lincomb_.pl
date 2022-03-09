6287ac1fa4bc370769fe303153f50b8b9890fefe
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-09 16:24:00 +0100

    ddoyen1 modified Yggdrasil/exercises/math/algebra/linear_algebra/matrices/operations/matrix_lincomb_.pl

---
+++ exercises/math/algebra/linear_algebra/matrices/operations/matrix_lincomb_.pl
@@ -0,0 +1,29 @@
+extends = /model/math/matrix.pl
+
+title==
+Produit de matrices
+==
+
+before ==
+if isinstance(param['size'][0], list):
+    n, m = randitem(param['size'])
+else:
+    n, m = param['size']
+    
+coeffbound = param['coeffbound']
+
+mat.setzeros(n, m)
+
+a, b = sample([-2, -1, 1, 2], 2])
+var('A B')
+expr = simplify(a*A + b*B)
+MA = randint_matrix(n, m, coeffbound)
+MB = randint_matrix(n, m, coeffbound)
+sol = a*MA+b*MB
+==
+
+question ==
+Soit les matrices
+$$ A = \left( {{ MA|latex }}\right) \text{ et } B = \left({{ MB|latex }}\right) $$ 
+Calculer $! {{expr|latex}} !$.
+==
