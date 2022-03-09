02547977df7a636a857f76d8603774ba971f75f2
EMMANUEL BRUNACHE <3573@monlycee.net>
2022-03-09 16:01:43 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/codage_caractères/affiche_caractere.pl

---
+++ ComputerScience/python/1NSI-2122/codage_caractères/affiche_caractere.pl
@@ -0,0 +1,29 @@
+
+extends = /ComputerScience/python/template/pltest.pl
+
+title= Affiche caractere 
+
+text==
+
+
+Ecrire une fonction **affiche_caractere(point_de_code)** qui affiche le caractere de point de code `point_de_code`.
+
+
+==
+
+taboo=isupper|islower
+
+reponse==
+def affiche_caractere(point_de_code):
+    print(chr(point_de_code))
+==
+
+pltest==
+>>> affiche_caractere(8765)
+
+>>> affiche_caractere(0x0976)
+
+>>> affiche_caractere(0b1001110)
+==
+
+
