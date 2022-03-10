7b9ea9fa9a64061c6031aef85cc362165427f5ea
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-10 14:50:43 +0100

    ddoyen1 modified Yggdrasil/exercises/math/foundations/maps/finite_map_composition.pl

---
+++ exercises/math/foundations/maps/finite_map_composition.pl
@@ -0,0 +1,81 @@
+extends = /model/math/dropgroup
+
+title = Composition d'applications
+
+before ==
+n=randint(4,5)
+
+row1 = list(range(1,n+1))
+row2f = list_randint(n,1,n)
+row2g = list_randint(n,1,n)
+
+tablef.add_row(['$! x !$'] + row1)
+tablef.add_row(['$! f(x) !$'] + row2f)
+tablef_html = table.get_html_string(header=False, attributes={"class":"mytable"})
+
+tableg.add_row(['$! x !$'] + row1)
+tableg.add_row(['$! g(x) !$'] + row2g)
+tableg_html = table.get_html_string(header=False, attributes={"class":"mytable"})
+
+case=randitem([1,2])
+sol=[]
+if case==1:
+    for i in range(n):
+        sol.append(row2f[row2g[i]-1])
+    h="f \circ g"
+else:
+    for i in range(n):
+        sol.append(row2g[row2f[i]-1])
+    h="g \circ f"
+==
+
+question ==
+{{namedrag}}
+On considère les applications  $! f:\\{1,\ldots,{{n}} \\}\rightarrow \\{1,\ldots,{{n}} \\} !$ et $! g:\\{1,\ldots,{{n}} \\}\rightarrow \\{1,\ldots,{{n}} \\} !$ définies par les tableaux ci-dessous.
+<br>
+{{tablef|safe}}
+<br>
+{{tableg|safe}}
+<br>
+Déterminer l'application $! {{h}} !$.
+==
+
+form ==
+<table>
+<tr>
+<th> $! x !$ </th>
+{% for item in row1 %}
+<td> {{item|safe}} </td>
+{% endfor %}
+</tr>
+<tr>
+<th> $! {{h}} (x) !$ </th>
+{% for e in drop %}
+<td> {{e|component}} </td>
+{% endfor %}
+</tr>
+</table>
+
+{% for d in drag -%}
+{{ d|component }}
+{%- endfor %}
+==
+
+evaluator==
+drop=[]
+for i in range(1,n+1):
+    name="drop"+str(i)
+    drop.append(locals()[name])
+
+feedback=""
+score=100
+for i in range(n):
+    if drop[i].content==str(sol[i]):
+        drop[i].css = "success-state"
+    else:
+        score=0
+        drop[i].css = "error-state" 
+==
+
+
+
