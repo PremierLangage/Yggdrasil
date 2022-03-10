7437834ba0759f3a854eb47d73983be284ddfb2d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-10 13:48:42 +0100

    ddoyen1 modified Yggdrasil/exercises/math/foundations/maps/finite_map_image.pl

--- exercises/math/foundations/maps/finite_map_image.pl
+++ exercises/math/foundations/maps/finite_map_image.pl
@@ -3,22 +3,26 @@ extends = /model/math/set.pl
 title = Image directe d'une application
 
 before ==
-from jinja2 import Template
-n=randint(5,9)
-m=randint(5,9)
-row1=list(range(1,n+1))
-row2=[randint(1,m) for _ in range(n)]
-A=FiniteSet(*sample(row1, randint(3,4)))
-sol =FiniteSet(*[row2[x-1] for x in A])
+from prettytable import PrettyTable
+table = PrettyTable()
 
-table= Template(tpltable).render(row1=row1,row2=row2)
+n = randint(5, 9)
+m = randint(5, 9)
+row1 = list(range(1,n+1))
+row2 = [randint(1,m) for _ in range(n)]
+A = FiniteSet(*sample(row1, randint(3,4)))
+sol = FiniteSet(*[row2[x-1] for x in A])
+
+table.add_row(['x'] + row1)
+table.add_row(['f(x)'] + row2)
+table_html = table.get_html_string()
 ==
 
 question ==
 On considère l'application $! f: \{1,\ldots,{{n}} \}\rightarrow \mathbb{N} !$ définie par le tableau ci-dessous.
 
 <br>
-{{table|safe}}
+{{table_html|safe}}
 <br>
 
 Déterminer l'image directe de l'ensemble $! {{A|latex}} !$ par $! f !$.
