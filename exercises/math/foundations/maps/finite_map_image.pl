547a7e9a3be08cdac1b43a3ec9447d143de41476
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-10 13:40:23 +0100

    ddoyen1 modified Yggdrasil/exercises/math/foundations/maps/finite_map_image.pl

--- exercises/math/foundations/maps/finite_map_image.pl
+++ exercises/math/foundations/maps/finite_map_image.pl
@@ -52,3 +52,19 @@ tpltable ==
 </tr>
 </table>
 ==
+
+extracss==
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+table {
+    margin: auto;
+}
+th, td {
+  padding: 0.5em;
+  text-align:center;
+}
+</style>
+==
