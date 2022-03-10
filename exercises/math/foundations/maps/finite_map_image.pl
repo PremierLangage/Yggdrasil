13c1648ba70067cdf710fd9a9e1fd44203f653e2
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-10 13:56:24 +0100

    ddoyen1 modified Yggdrasil/exercises/math/foundations/maps/finite_map_image.pl

--- exercises/math/foundations/maps/finite_map_image.pl
+++ exercises/math/foundations/maps/finite_map_image.pl
@@ -30,20 +30,3 @@ On considère l'application $! f: \{1,\ldots,{{n}} \}\rightarrow \mathbb{N} !$ d
 
 Déterminer l'image directe de l'ensemble $! {{A|latex}} !$ par $! f !$.
 ==
-
-
-style2.table ==
-<style>
-table, th, td {
-  border: 1px solid black;
-  border-collapse: collapse;
-}
-table {
-    margin: auto;
-}
-th, td {
-  padding: 0.5em;
-  text-align:center;
-}
-</style>
-==
