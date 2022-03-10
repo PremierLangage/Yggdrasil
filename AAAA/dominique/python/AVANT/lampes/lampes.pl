ef8b1d993e39157aaff33262249d1fea8ae85cc7
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-03-10 13:46:20 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/AVANT/lampes/lampes.pl

---
+++ AAAA/dominique/python/AVANT/lampes/lampes.pl
@@ -0,0 +1,30 @@
+
+
+
+extends = /ComputerScience/python/template/pltest.pl
+@ /builder/before.py [builder.py]
+
+@sol.py [build.py]
+@ennonce.md
+@data1
+
+@exemple
+
+title= Un jardin bien illuminé.
+
+text==
+
+{{lenonce}}
+
+==
+
+
+before==
+import build
+pltest0,pltest1=build.doit()
+
+with open("ennonce.md","r") as f:
+    lenonce= f.read()
+
+==
+
