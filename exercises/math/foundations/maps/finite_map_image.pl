a70a0c95e2fa2c2ca2b860a424ad2da34d1c6231
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-10 13:42:25 +0100

    ddoyen1 modified Yggdrasil/exercises/math/foundations/maps/finite_map_image.pl

--- exercises/math/foundations/maps/finite_map_image.pl
+++ exercises/math/foundations/maps/finite_map_image.pl
@@ -1,4 +1,4 @@
-extends = /model/math/expr.pl
+extends = /model/math/sets.pl
 
 title = Image directe d'une application
 
@@ -11,7 +11,7 @@ m=randint(5,9)
 row1=list(range(1,n+1))
 row2=[randint(1,m) for _ in range(n)]
 A=FiniteSet(*sample(row1, randint(3,4)))
-imA=FiniteSet(*[row2[x-1] for x in A])
+sol =FiniteSet(*[row2[x-1] for x in A])
 
 table= Template(tpltable).render(row1=row1,row2=row2)
 ==
