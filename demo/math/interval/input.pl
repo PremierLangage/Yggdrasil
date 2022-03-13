2e239ebbe7b7567d5e7a9ebd648743c3679fb363
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-13 15:52:35 +0100

    ddoyen1 modified Yggdrasil/demo/math/interval/input.pl

---
+++ demo/math/interval/input.pl
@@ -0,0 +1,15 @@
+extends = /model/math/interval.pl
+
+keypad = ["-infty", "+infty"]
+
+before ==
+a = choice([0, 1, -oo])
+b = choice([3, 5, oo])
+left_open = choice([True, False])
+right_open = choice([True, False])
+sol = Interval(a, b, left_open, right_open)
+==
+
+question ==
+Entrer l'intervalle $! {{ sol|latex }} !$.
+==
