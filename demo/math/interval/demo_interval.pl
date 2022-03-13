f9225b00b49a0e8103b13ac043c819eff0558afb
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-13 15:38:18 +0100

    ddoyen1 modified Yggdrasil/demo/math/interval/demo_interval.pl

--- demo/math/interval/demo_interval.pl
+++ demo/math/interval/demo_interval.pl
@@ -1,7 +1,11 @@
 extends = /model/math/interval.pl
 
 before ==
-sol = Interval.Ropen(0, 2)
+a = choice([0, 1, -oo])
+b = choice([3, 5, oo])
+left_open = choice([True, False])
+right_open = choice([True, False])
+sol = Interval(a, b, left_open, right_open)
 ==
 
 question ==
