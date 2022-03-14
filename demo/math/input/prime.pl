e6cd653115612515e6d3f12abbd4f00e7aad4686
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-14 21:13:49 +0100

    ddoyen1 modified Yggdrasil/demo/math/input/prime.pl

---
+++ demo/math/input/prime.pl
@@ -0,0 +1,34 @@
+extends = /model/math/input.pl
+
+title = Transformer une somme de logarithmes en un logarithme
+
+before ==
+k = randint(3, 6)
+a = randint(25, 75)
+b = a + randint(6, 10)
+sol = 0
+==
+
+question ==
+Donner un entier premier, compris entre {{ a }} et {{ b }} (au sens large).
+==
+
+evaluator ==
+from latex2sympy import latex2sympy
+
+try:
+    ans = latex2sympy(input.value)
+except:
+    score = -1
+    feedback = "La réponse doit être un entier."
+else:
+    if not ans.is_Integer:
+        score = -1
+        feedback = "La réponse doit être un entier."
+    elif not (a <= ans <= b):
+        score = 0
+        feedback = f"La réponse doit être comprise entre {a} et {b}."
+    elif not (a <= ans <= b):
+        score = 0
+        feedback = "La réponse doit être un nombre premier."
+==
