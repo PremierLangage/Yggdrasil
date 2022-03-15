4e35ae7d478a0d9e42e3736a239ed67faad9b63e
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-15 09:23:47 +0100

    ddoyen1 modified Yggdrasil/model/math/input.pl

--- model/math/input.pl
+++ model/math/input.pl
@@ -1,19 +1,37 @@
-extends = /model/basic/utils.pl
-extends = /model/basic/hackpage.pl
-extends = messages_math.pl
+# Model for a single math input field
+# with a custom evaluation
 
-# Specific keys
+extends = /model/math/input0.pl
 
 
-keypad = []
+eval_scripts = ["evalparam", "getinput", "evaluator", "ending"]
 
-embed = ""
+getinput ==
+from mathinput import MathInput
+MathInput.message = message
+input.value = answers[input.id]
+if 'sol' in locals():
+    input.sol = sol
+==
 
-prefix = Réponse :
+evaluator ==
+#
+==
 
-latexsettings.ln_notation = True
-latexsettings.inv_trig_style = full
+ending ==
+if 'score' in locals():
+    input.score = score
+    if 'feedback' in locals():
+        input.feedback = feedback
+else:
+    score = input.eval()
+input.display_feedback()
+if score >= 0:
+  input.disable()
+==
 
+solution ==
+==
 
 input_type = "expr"
 
