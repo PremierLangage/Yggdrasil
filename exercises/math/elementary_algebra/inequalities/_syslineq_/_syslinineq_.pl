51284d66964eb0edca34b592e5b5d4ec28aa50e1
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-17 15:33:14 +0100

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/inequalities/_syslineq_/_syslinineq_.pl

--- exercises/math/elementary_algebra/inequalities/_syslineq_/_syslinineq_.pl
+++ exercises/math/elementary_algebra/inequalities/_syslineq_/_syslinineq_.pl
@@ -18,7 +18,7 @@ param.nonempty = True
 # Autres clés
 
 before == #|py|
-from sympy import S, solveset, Intersection
+from sympy import S, solveset, Intersection, EmptySet
 
 var('x')
 a = randint(-6, 6, [-1, 0, 1])
@@ -39,26 +39,31 @@ else:
 ineq1 = choice([f>=g, f>g, f<=g, f<g, g>=f, g>f, g<=f, g<f])
 sol1 = solveset(ineq1, x, domain=S.Reals)
 
-var('x')
-a = randint(-6, 6, [-1, 0, 1])
-b = randint(-6, 6, [0])
-c = randint(-6, 6, [-1, 0, 1, a])
-d = randint(-6, 6, [0, b])
-f = a*x + b
+while True:
+    var('x')
+    a = randint(-6, 6, [-1, 0, 1])
+    b = randint(-6, 6, [0])
+    c = randint(-6, 6, [-1, 0, 1, a])
+    d = randint(-6, 6, [0, b])
+    f = a*x + b
 
-if param['otherside2'] == 'zero':
-    g = 0
-elif param['otherside2'] == 'cst':
-    g = d
-elif param['otherside2'] == 'aff':
-    g = c*x + d
-else:
-    raise ValueError("Incorrect value for parameter otherside.")
+    if param['otherside2'] == 'zero':
+        g = 0
+    elif param['otherside2'] == 'cst':
+        g = d
+    elif param['otherside2'] == 'aff':
+        g = c*x + d
+    else:
+        raise ValueError("Incorrect value for parameter otherside.")
 
-ineq2 = choice([f>=g, f>g, f<=g, f<g, g>=f, g>f, g<=f, g<f])
-sol2 = solveset(ineq2, x, domain=S.Reals)
-
-sol = Intersection(sol1, sol2)
+    ineq2 = choice([f>=g, f>g, f<=g, f<g, g>=f, g>f, g<=f, g<f])
+    sol2 = solveset(ineq2, x, domain=S.Reals)
+    sol = Intersection(sol1, sol2)
+    if param['nonempty']:
+        if sol != EmptySet:
+            break
+    else:
+        break
 ==
 
 question ==
