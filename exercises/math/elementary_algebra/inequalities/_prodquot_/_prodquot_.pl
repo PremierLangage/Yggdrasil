4af9bac15028302715e7f5fdf90f0701e9fd0a46
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-17 17:04:54 +0100

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/inequalities/_prodquot_/_prodquot_.pl

--- exercises/math/elementary_algebra/inequalities/_prodquot_/_prodquot_.pl
+++ exercises/math/elementary_algebra/inequalities/_prodquot_/_prodquot_.pl
@@ -9,32 +9,33 @@ param.type = "product"
 
 param.bounds = "int"
 
-before ==
+before == #|py|
 from sympy import S, solveset
 
 var('x')
 
-ok=False
 if param['bounds'] == 'int':
     b, d = sampleint(-6, 6, 2, [0])
     P, Q = (x+b), (x+d)
-if param['bounds']=='intrat':
-    while not ok:
+if param['bounds'] == 'intrat':
+    while True:
         b, d = [randint(-6, 6, [0]) for i in range(2)]
         a, c = [randint(-6, 6, [0, 1, -1]) for i in range(2)]
-        if b*c!=d: ok=True
-    P,Q=(x+b),(c*x+d)
-if param['bounds']=='rat':
-    while not ok:
+        if b*c !=d:
+            break
+    P, Q = (x+b), (c*x+d)
+if param['bounds'] =='rat':
+    while True:
         b, d = [randint(-6, 6, [0]) for i in range(2)]
         a, c = [randint(-6, 6, [0, 1, -1]) for i in range(2)]
-        if b*c!=a*d: ok=True
-    P,Q=(a*x+b),(c*x+d)
+        if b*c != a*d:
+            break
+    P, Q = (a*x+b), (c*x+d)
 
-if param['type']=="product":
-    expr=P*Q
-elif param['type']=="quotient":
-    expr=P/Q
+if param['type'] == "prod":
+    expr = P*Q
+elif param['type']=="quot":
+    expr = P/Q
 
 ineq = choice([expr >= 0, expr > 0, expr <= 0, expr < 0])
 sol = solveset(ineq, x, domain=S.Reals)
