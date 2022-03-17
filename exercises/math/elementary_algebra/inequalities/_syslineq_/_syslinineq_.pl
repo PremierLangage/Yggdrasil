7b125ec20802b7c526a04baf9088a969f9e75033
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-17 15:24:01 +0100

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/inequalities/_syslineq_/_syslinineq_.pl

--- exercises/math/elementary_algebra/inequalities/_syslineq_/_syslinineq_.pl
+++ exercises/math/elementary_algebra/inequalities/_syslineq_/_syslinineq_.pl
@@ -17,17 +17,17 @@ c = randint(-6, 6, [-1, 0, 1, a])
 d = randint(-6, 6, [0, b])
 f = a*x + b
 
-if param['otherside'] == 'zero':
+if param['otherside1'] == 'zero':
     g = 0
-elif param['otherside'] == 'cst':
+elif param['otherside1'] == 'cst':
     g = d
-elif param['otherside'] == 'aff':
+elif param['otherside1'] == 'aff':
     g = c*x + d
 else:
-    raise ValueError("Incorrect value for parameter otherside.")
+    raise ValueError("Incorrect value for parameter otherside1.")
 
 ineq1 = choice([f>=g, f>g, f<=g, f<g, g>=f, g>f, g<=f, g<f])
-sol1 = solveset(ineq, x, domain=S.Reals)
+sol1 = solveset(ineq1, x, domain=S.Reals)
 
 var('x')
 a = randint(-6, 6, [-1, 0, 1])
@@ -36,23 +36,21 @@ c = randint(-6, 6, [-1, 0, 1, a])
 d = randint(-6, 6, [0, b])
 f = a*x + b
 
-if param['otherside'] == 'zero':
+if param['otherside2'] == 'zero':
     g = 0
-elif param['otherside'] == 'cst':
+elif param['otherside2'] == 'cst':
     g = d
-elif param['otherside'] == 'aff':
+elif param['otherside2'] == 'aff':
     g = c*x + d
 else:
     raise ValueError("Incorrect value for parameter otherside.")
 
-ineq1 = choice([f>=g, f>g, f<=g, f<g, g>=f, g>f, g<=f, g<f])
-sol2 = solveset(ineq, x, domain=S.Reals)
+ineq2 = choice([f>=g, f>g, f<=g, f<g, g>=f, g>f, g<=f, g<f])
+sol2 = solveset(ineq2, x, domain=S.Reals)
 
-sol1=solveset(ineq1,x,domain=S.Reals)
-sol2=solveset(ineq2,x,domain=S.Reals)
-sol=Intersection(sol1,sol2)
+sol = Intersection(sol1, sol2)
 ==
 
 question ==
-Déterminer l'ensemble des réels $% x %$ tels que $$ {{ineq1|latex}}\quad\text{ et }\quad{{ineq2|latex}}. $$ Ecrire cet ensemble sous la forme d'un intervalle (ou de l'ensemble vide).
+Déterminer l'ensemble des réels $% x %$ tels que $$ {{ ineq1|latex }}\quad\text{ et }\quad{{ ineq2|latex }}. $$ Ecrire cet ensemble sous la forme d'un intervalle (ou de l'ensemble vide).
 ==
