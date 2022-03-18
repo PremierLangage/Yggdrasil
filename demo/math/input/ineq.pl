d8f3e0cadd0b57aa0308c21a1a6b56539a142b24
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-18 16:10:15 +0100

    ddoyen1 modified Yggdrasil/demo/math/input/ineq.pl

---
+++ demo/math/input/ineq.pl
@@ -0,0 +1,13 @@
+extends = /model/math/input.pl
+
+before ==
+sol = 0
+==
+
+question ==
+On considère
+==
+
+solution ==
+$! z_1 \times z_2  = ( {{ z1|latex }} ) \times ( {{ z2|latex }} ) = {{ sol|latex }} !$
+==
