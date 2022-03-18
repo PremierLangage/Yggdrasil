a98741955b5039ab172b9e63eb30b34a58f9c5c7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-18 18:23:43 +0100

    ddoyen1 modified Yggdrasil/demo/math/input/ineq.pl

--- demo/math/input/ineq.pl
+++ demo/math/input/ineq.pl
@@ -46,7 +46,7 @@ except:
     feedback = "La réponse doit être un entier."
 else:
     score = 0
-    feedback = str(solveset(ineq, x, domain=S.Reals))
+    feedback = "" str(solveset(ineq, x, domain=S.Reals))
 ==
 
 keypad = ["+infty", "-infty"]
