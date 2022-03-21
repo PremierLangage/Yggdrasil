7045211450a5b998ac6c0e8c4f5248cd84705c26
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-21 17:51:46 +0100

    ddoyen1 modified Yggdrasil/model/jxg/vector.pl

---
+++ model/jxg/vector.pl
@@ -0,0 +1,90 @@
+extends = /model/basic/hackpage.pl
+extends = /model/basic/utils.pl
+
+# Specific keys
+
+attributes = {}
+
+jxgscript ==
+==
+
+pointname = ""
+
+tol = 0.05
+
+# Question and input block
+
+question ==
+==
+
+inputblock ==
+{{ input|component }}
+==
+
+# Before scripts
+
+before_scripts = ["mathimport", "initinput", "before", "process"]
+
+mathimport ==
+from sympy import E, I, pi, oo
+from sympy import sqrt, Abs, sin, cos, tan, exp, ln
+from sympy import Symbol, symbols, var
+from sympy import sympify, simplify, Lambda
+from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval
+from sympy import integrate
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from sympy2latex import latex
+from latex2sympy import latex2sympy
+==
+
+initinput ==
+from jxginput import JXGInput
+input = JXGInput()
+==
+
+
+process ==
+input.attributes = attributes
+input.evalparam = {'tol': tol}
+input.pointname = pointname
+input.set_script(jxgscript + script_aux, globals())
+==
+
+script_aux ==
+function getMouseCoords(e) {
+    let cPos = board.getCoordsTopLeftCorner(e);
+    let absPos = JXG.getPosition(e);
+    let dx = absPos[0]-cPos[0];
+    let dy = absPos[1]-cPos[1];
+    return new JXG.Coords(JXG.COORDS_BY_SCREEN, [dx, dy], board);
+}
+
+function down(e) {
+    let coords = getMouseCoords(e);
+    psol.setPosition(JXG.COORDS_BY_USER,[coords.usrCoords[1], coords.usrCoords[2]]);
+}
+
+board.on('down', down)
+==
+
+tplpage =@ /model/tplpage/basicmath.html
+
+evaluator ==
+input.sol = [xsol, ysol]
+score = input.eval()
+input.display_feedback()
+input.disable()
+==
+
+
+style.jxgcss ==
+<style>
+.jsxgraph-component {
+    width: 400px;
+    height: 400px;
+    margin: 0 auto;
+    border: 1px solid;
+}
+</style>
+==
