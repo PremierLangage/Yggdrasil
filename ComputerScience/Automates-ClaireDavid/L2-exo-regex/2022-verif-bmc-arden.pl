8d6a281252365c980bb9dbdaf0ef79d1c9e9867b
Claire DAVID <claire.david@univ-eiffel.fr>
2022-03-21 22:02:07 +0100

    cdavid1 modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/2022-verif-bmc-arden.pl

--- ComputerScience/Automates-ClaireDavid/L2-exo-regex/2022-verif-bmc-arden.pl
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/2022-verif-bmc-arden.pl
@@ -1,7 +1,7 @@
 extends = ../templates/new_editor-L2.pl
 
 #* titre de l'exercice
-title = Vérification solution BMC ou Arden
+title = Vérification de la solution de BMC ou Arden du partiel 2022
 
 #* Ajouter ici l'automate solution et autres variables de l'exo
 generate== #|py|
