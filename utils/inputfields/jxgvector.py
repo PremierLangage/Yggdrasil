bb72b4edf8ea7360b8de93fca1db2e5eb7671a6c
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-21 21:50:43 +0100

    ddoyen1 modified Yggdrasil/utils/inputfields/jxgvector.py

---
+++ utils/inputfields/jxgvector.py
@@ -0,0 +1,33 @@
+from jxginput import JXGInput
+
+class JXGVector(JXGInput):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-math-drawer'
+        self.decorator = 'JXGVector'
+        self.pointnames = ['A', 'B']
+        self.evalparam = {}
+        super().__init__(**kwargs)
+
+    def eval(self):
+        """
+        Evaluate the input field
+        """
+        x0, y0 = self.get_point(self.pointnames[0])
+        x1, y1 = self.get_point(self.pointnames[1])
+        xsol, ysol = self.sol
+        tol = self.evalparam.get('tol', 0.1)
+        if hypot(x1-x0-xsol, y1-y0-ysol) < tol:
+            self.score = 100
+        else:
+            self.score = 0
+        return self.score
+
+    def display_feedback(self):
+        """
+        Display visual feedback.
+        """ 
+        if self.score == 100:
+            pass
+        else:
+            pass
