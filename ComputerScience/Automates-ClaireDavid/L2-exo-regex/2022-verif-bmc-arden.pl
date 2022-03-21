4d0a728d1b2fd16d54e8342b0119b49ff7f8ba28
Claire DAVID <claire.david@univ-eiffel.fr>
2022-03-21 21:42:13 +0100

    cdavid1 modified Yggdrasil/ComputerScience/Automates-ClaireDavid/L2-exo-regex/2022-verif-bmc-arden.pl

---
+++ ComputerScience/Automates-ClaireDavid/L2-exo-regex/2022-verif-bmc-arden.pl
@@ -0,0 +1,85 @@
+extends = ../templates/new_editor-L2.pl
+
+#* titre de l'exercice
+title = Vérification solution BMC ou Arden
+
+#* Ajouter ici l'automate solution et autres variables de l'exo
+generate== #|py|
+
+#pour definir le type de réponse attendue (automaton par défaut)
+answer_type = "simple-regex"
+
+alphabet = "a,b"
+
+automate1 = '''
+    #states
+    1
+    2
+    3
+    4
+    #initials
+    1;2
+    #accepting
+    3;4
+    #alphabet
+    a
+    b
+    #transitions
+    1:a>2
+    1:b>3
+    1:b>4
+    2:a>2
+    2:a>3
+    3:b>4
+    4:b>4
+    4:a>3
+'''
+
+#langage solution -- le nom solution est forcé par le template
+solution = Automaton.parse('(a*ba*b)*a*')
+
+#message de l'énoncé décrivant le langage
+message = "des mots qui ont un nombre pair de b."
+==
+
+#* Texte de l'énoncé
+# ---
+# ajouter {{ solution_view|component }} dans ce qui suit pour l'afficher
+# l'automate minimisé du langage solution
+text ==
+On considère l'alphabet $!\Sigma= \\{ {{ alphabet }} \\}!$ .<br/>
+Donnez une expression reconnaissant le langage donné par l'automate ci-dessus <br>
+{% if attempt < maxattempt and score != 100 %}
+<p class="warning-state">
+Il vous reste {{ maxattempt - attempt }} tentative(s) !
+</p>
+{% endif %}
+==
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
