10f7152a32a8fab1694350f46a33a0ecb9237aaa
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-21 22:45:12 +0100

    ddoyen1 modified Yggdrasil/demo/jxg/vector/add_vectors.pl

--- demo/jxg/vector/add_vectors.pl
+++ demo/jxg/vector/add_vectors.pl
@@ -20,11 +20,12 @@ attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 
 jxgscript ==
 board.create('grid',[],{gridX:1,gridY:1});
-var O = board.create('point',[0, 0],{visible:false, fixed:true});
-var V = board.create('point',[ {{vx}} , {{vy}} ],{size:0,name:'v', fixed:true});
-var OV = board.create('arrow',[O, V], {color:'orange'});
-var U = board.create('point',[ {{ux}} , {{uy}} ],{size:0,name:'u', fixed:true});
-var OU = board.create('arrow',[O, U], {color:'purple'});
+var V1 = board.create('point',[0, 0],{visible:false, fixed:true});
+var V2 = board.create('point',[ {{vx}} , {{vy}} ],{size:0,name:'v', fixed:true});
+var V = board.create('arrow',[O, V], {color:'orange'});
+var U1 = board.create('point',[0, 0],{visible:false, fixed:true});
+var U2 = board.create('point',[ {{ux}} , {{uy}} ],{size:0,name:'u', fixed:true});
+var U = board.create('arrow',[O, U], {color:'purple'});
 
 var pt1 = board.create('point', [0, 0], {visible: true, name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
 var pt2 = board.create('point', [1, 1], {visible: true, name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
