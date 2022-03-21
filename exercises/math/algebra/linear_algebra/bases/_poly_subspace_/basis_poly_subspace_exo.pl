b74ad586806a3c42f69d5ca4fea5b4d1e4e64550
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-21 16:14:36 +0100

    ddoyen1 modified Yggdrasil/exercises/math/algebra/linear_algebra/bases/_poly_subspace_/basis_poly_subspace_exo.pl

--- exercises/math/algebra/linear_algebra/bases/_poly_subspace_/basis_poly_subspace_exo.pl
+++ exercises/math/algebra/linear_algebra/bases/_poly_subspace_/basis_poly_subspace_exo.pl
@@ -3,21 +3,17 @@ extends = /model/math/input_eval.pl
 title = Base d'un sous-espace de $! \mathbb{R}_{2}[X] !$
 
 before ==
-keyboards_JSON['virtualKeyboards']="sets"
-input.config = keyboards_JSON
-
-x0,x1=list_randint_norep(2,-2,2)
-a=1
-b=randitem([-1,1])
+x0, x1 = sampleinte(2, -2, 2)
+a =1
+b = choice([-1,1])
 n = 2
 m = 2
-A=Matrix([[a*x0**2+b*x1**2,a*x0+b*x1,a+b]])
-B=Matrix([[0]])
-eq=latex_lincomb([a,b],["P(%d)" % x0 ,"P(%d)" % x1])
-
+A = Matrix([[a*x0**2+b*x1**2,a*x0+b*x1,a+b]])
+B = Matrix([[0]])
+eq = latex_lincomb([a,b],["P(%d)" % x0 ,"P(%d)" % x1])
 ==
 
-text ==
+question ==
 On considère le sous-espace de $! \mathbb{R}_{{n}}[X] !$ formé des polynômes $! P !$ tels que
 $$ {{eq}} =0 .$$
 Déterminer une base de ce sous-espace (écrire cette base comme un ensemble, entre accolades).
