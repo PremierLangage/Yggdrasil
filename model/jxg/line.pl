7a06c870f80f16e5bff5fe6c98c1e5f9ccfcfd65
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-22 16:33:45 +0100

    ddoyen1 modified Yggdrasil/model/jxg/line.pl

---
+++ model/jxg/line.pl
@@ -0,0 +1,111 @@
+extends = /model/basic/hackpage.pl
+extends = /model/basic/utils.pl
+@ /utils/inputfields/jxgvector.py
+
+
+# Specific keys
+
+attributes = {}
+
+jxgscript ==
+==
+
+pointnames = ["A", "B"]
+
+tol = 0.05
+
+# Question and input block
+
+question ==
+==
+
+inputblock ==
+{{ input|component }}
+==
+
+# Before scripts
+
+before_scripts = ["mathimport", "initinput", "before", "process"]
+
+mathimport ==
+from sympy import E, I, pi, oo
+from sympy import sqrt, Abs, sin, cos, tan, exp, ln
+from sympy import Symbol, symbols, var
+from sympy import sympify, simplify, Lambda
+from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval
+from sympy import integrate
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from sympy2latex import latex
+from latex2sympy import latex2sympy
+==
+
+initinput ==
+from jxgvector import JXGVector
+input = JXGVector()
+==
+
+
+process ==
+input.attributes = attributes
+input.evalparam = {'tol': tol}
+input.pointnames = pointnames
+input.set_script(jxgscript, globals())
+==
+
+
+
+tplpage =@ /model/tplpage/basicmath.html
+
+evaluator ==
+input.sol = sol
+score = input.eval()
+input.display_feedback()
+input.disable()
+==
+
+
+style.jxgcss ==
+<style>
+.jsxgraph-component {
+    width: 400px;
+    height: 400px;
+    margin: 0 auto;
+    border: 1px solid;
+}
+</style>
+==
+
+apidoc == #|json|
+{
+    "name": "textinput",
+    "keys": {
+        "sol": {
+            "type": "lst[float, float]",
+            "default": "",
+            "description": "Coordonnées du vecteur solution."
+        },
+        "attributes": {
+            "type": "dict",
+            "default": "{}",
+            "description": "Paramètres du panneau graphiqe interactif."
+        },
+        "jxgscript": {
+            "type": "str",
+            "default": "",
+            "description": "Script JSXGraph."
+        },
+        "pointnames": {
+            "type": "lst[str, str]",
+            "default": "",
+            "description": "Noms des points extrémités du vecteur à construire."
+        }
+        "tol": {
+            "type": "float",
+            "default": "0.1",
+            "description": "Erreur maximum (en norme euclidienne) pour considérer une réponse comme correcte."
+        }
+
+    }
+}
+==
