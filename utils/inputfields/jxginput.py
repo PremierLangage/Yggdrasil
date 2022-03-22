e1047842bdc967a25449e90ffa2c665c842899fe
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-22 16:32:47 +0100

    ddoyen1 modified Yggdrasil/utils/inputfields/jxginput.py

--- utils/inputfields/jxginput.py
+++ utils/inputfields/jxginput.py
@@ -29,34 +29,6 @@ class JXGInput(Component):
         """
         return (self.points[name]['x'], self.points[name]['y'])
 
-    def eval(self):
-        """
-        Evaluate the input field
-        """
-        x, y = self.get_point(self.pointname)
-        xsol, ysol = self.sol
-        tol = self.evalparam.get('tol', 0.1)
-        if hypot(x-xsol, y-ysol) < tol:
-            self.score = 100
-        else:
-            self.score = 0
-        return self.score
-
-    def display_feedback(self):
-        """
-        Display visual feedback.
-        """ 
-        if self.score == 100:
-            script = """
-            psol.setAttribute({color: 'green'});
-            """
-        else:
-            script = """
-            psol.setAttribute({color: 'red'});
-            board.create('point',[{{xsol}}, {{ysol}}],{size:2,name:'',color:'green'});
-            """
-        self.add_script(script, {'xsol':self.sol[0], 'ysol':self.sol[1]})
-
     def disable(self):
         """
         Disable the input field.
