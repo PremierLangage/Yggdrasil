f0f925bab99bcb609a283afecd8644f2f125ed4e
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-23 21:30:21 +0100

    ddoyen1 modified Yggdrasil/BUG/list_inheritance0.pl

---
+++ BUG/list_inheritance0.pl
@@ -0,0 +1,21 @@
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+@ /utils/sandboxio.py
+
+title =
+
+keylist % [1, 2]
+
+keylist % [1, 2]
+
+before ==
+
+==
+
+text ==
+{{ keylist }}
+==
+
+form ==
+
+==
