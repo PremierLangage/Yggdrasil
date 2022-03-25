3a5712852a1ab2500a894ceec444416449bdce46
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-25 17:19:17 +0100

    ddoyen1 modified Yggdrasil/demo/math/serexp/test.pl

--- demo/math/serexp/test.pl
+++ demo/math/serexp/test.pl
@@ -3,7 +3,7 @@ extends = /model/math/expr.pl
 before ==
 from sympy import O, series, sin, cos
 var('x')
-f = choice([sin(x), cos(x)])
+f = choice([sin(x), cos(x), 1/(1-x)])
 n = randint(2, 3)
 sol = series(f, x, 0, n+1).removeO()
 ==
