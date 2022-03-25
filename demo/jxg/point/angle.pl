c47ccf3d8661f77b22982aa1d3548713e345cf39
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-25 09:52:38 +0100

    ddoyen1 modified Yggdrasil/demo/jxg/point/angle.pl

--- demo/jxg/point/angle.pl
+++ demo/jxg/point/angle.pl
@@ -20,5 +20,5 @@ var circle = board.create('circle', [[0, 0], [0, 1]], {strokeColor: 'blue', fixe
 var O = board.create('point', [0, 0], {size: 1, name: 'O', color: 'black', fixed: true});
 var A = board.create('point', [1, 0], {size: 1, name: 'A', color: 'black', fixed: true});
 var pt = board.create('glider', [1, 0.5, circle], {size: 2, name: 'M', color: 'blue', fixed: false});
-var secOAM = board.create('sector', [O, A, psol], {color: 'orange'});
+var secOAM = board.create('sector', [O, A, pt], {color: 'orange'});
 ==
