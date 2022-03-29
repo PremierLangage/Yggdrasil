db333b4455e7d1e3377dacf3ded5050f45b465c4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-29 14:24:47 +0200

    ddoyen1 modified Yggdrasil/demo/math/expr/limit_algebraic_fraction.pl

--- demo/math/expr/limit_algebraic_fraction.pl
+++ demo/math/expr/limit_algebraic_fraction.pl
@@ -2,9 +2,7 @@ extends = /model/math/expr.pl
 
 before ==
 from sympy import Limit
-
-x = Symbol('x')
-
+var('x')
 f, g = sample([2*x+1, x+2, 2*x**2+1, x**2+1], 2)
 lim = Limit(f/g, x, oo)
 sol = lim.doit()
