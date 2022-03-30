4a9a14ef5ac52664b73db4e8eef3b70d4abca169
EMMANUEL BRUNACHE <3573@monlycee.net>
2022-03-30 16:31:21 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM_Reversi/coups_possibles.pl

--- ComputerScience/python/1NSI-2122/DM_Reversi/coups_possibles.pl
+++ ComputerScience/python/1NSI-2122/DM_Reversi/coups_possibles.pl
@@ -1,7 +1,71 @@
+@ /builder/before.py [builder.py]
 extends = func.pl
 
 funcname= coups_possibles
 title= Quels sont les coups possibles ?
+
+before== #|python| 
+from random import randint
+
+def initialise_jeu(taille):
+    assert taille % 2 == 0
+    jeu = {
+        "plateau": [],
+        "joueur actif": 0,
+        "joueurs":  [{
+                "nom": "joueur1",
+                "couleur": "white",
+                "score": 2 
+            },
+            {
+                "nom": "joueur2",
+                "couleur": "red",
+                "score": 2 
+            }],
+        "parametres":{
+            'framerate': 10,
+            'plateau' : taille,
+            'taille_fenetre' : 640
+            }
+        }
+    for i in range(taille):
+        jeu['plateau'].append([-1] * taille)
+    jeu['plateau'][taille // 2 - 1][taille // 2 - 1] = 0
+    jeu['plateau'][taille // 2 ][taille // 2 ] = 0
+    jeu['plateau'][taille // 2 ][taille // 2 - 1] = 1
+    jeu['plateau'][taille // 2 - 1][taille // 2 ] = 1
+    return jeu
+
+def coups_possibles(jeu):
+    plateau = jeu['plateau']
+    joueur_actif = jeu['joueur_actif']
+    dico = {}
+    autrejoueur = 1 - joueur_actif
+    directions = [(-1, -1),(-1, 0),(-1, 1),(0, -1),(0, 1),(1, -1),(1, 0),(1, 1)]
+    for i in range(len(plateau)):
+        for j in range(len(plateau)):
+            if plateau[i][j] != -1:
+                continue
+            for dir in directions:
+                k = 1
+                while plateau[i + k * dir[0]][j + k * dir[1]] == autrejoueur:
+                    k += 1
+                if k != 1 and plateau[i + k * dir[0]][j + k * dir[1]] ==joueur_actif:
+                    if (i,j) in dico:
+                        dico[(i,j)].append(dir)
+                    else:
+                        dico[(i,j)] = [dir]
+    return dico
+
+taille = randint(3,8) * 2
+jeu = initialise_jeu(taille)
+
+pltest += """
+>>> initialise_jeu({})
+{}
+""".format(taille, jeu)
+==
+
 
 doctest==
     la fonction coups_possibles(jeu) retourne la liste des coups possibles.
