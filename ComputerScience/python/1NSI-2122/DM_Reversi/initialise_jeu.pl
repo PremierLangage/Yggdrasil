8400815bf6da9e8a0cd82f2478064f1617e6e9fd
EMMANUEL BRUNACHE <3573@monlycee.net>
2022-03-30 16:11:01 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM_Reversi/initialise_jeu.pl

--- ComputerScience/python/1NSI-2122/DM_Reversi/initialise_jeu.pl
+++ ComputerScience/python/1NSI-2122/DM_Reversi/initialise_jeu.pl
@@ -1,8 +1,40 @@
 
+@ /builder/before.py [builder.py]
 extends = func.pl
 
 funcname= initialise_jeu
 title= Initialise le jeu
+
+before==
+def initialise_jeu(taille):
+    assert taille % 2 == 0
+    jeu = {
+        "plateau": [],
+        "joueur actif": 0,
+        "joueurs":  [{
+                "nom": "joueur1",
+                "couleur": "white",
+                "score": 2 
+            },
+            {
+                "nom": "joueur2",
+                "couleur": "red",
+                "score": 2 
+            }],
+        "parametres":{
+            'framerate': 10,
+            'plateau' : taille,
+            'taille_fenetre' : 640
+            }
+        }
+    for i in range(taille):
+        jeu['plateau'].append([-1] * taille)
+    jeu['plateau'][taille // 2 - 1][taille // 2 - 1] = 0
+    jeu['plateau'][taille // 2 ][taille // 2 ] = 0
+    jeu['plateau'][taille // 2 ][taille // 2 - 1] = 1
+    jeu['plateau'][taille // 2 - 1][taille // 2 ] = 1
+    return jeu
+==
 
 doctest==
     la fonction initialise_jeu(taille) initialise le dictionnaire jeu tel que défini dans la page d'accueil.
