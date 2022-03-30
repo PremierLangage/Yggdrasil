0339c07c3c860e042b0349a4d969214cef73fd6c
EMMANUEL BRUNACHE <3573@monlycee.net>
2022-03-30 15:59:17 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM_Reversi/initialise_jeu.pl

---
+++ ComputerScience/python/1NSI-2122/DM_Reversi/initialise_jeu.pl
@@ -0,0 +1,68 @@
+
+extends = func.pl
+
+funcname= initialise_jeu
+title= Initialise le jeu
+
+doctest==
+    la fonction initialise_jeu(taille) initialise le dictionnaire jeu tel que défini dans la page d'accueil.
+    Deux joueurs appelés "joueur1" et "joueur2".
+    Un plateau de taille par taille.
+
+    exemple:
+    >>> initialise_jeu(4)
+    {"plateau": [
+        [-1, -1, -1, -1],
+        [-1,  0,  1, -1],
+        [-1,  1,  0, -1],
+        [-1, -1, -1, -1]
+        ],
+    "joueur actif": 0,
+    "joueurs":  [{
+            "nom": "joueur1",
+            "couleur": "white",
+            "score": 2 
+        },
+        {
+            "nom": "joueur2",
+            "couleur": "red",
+            "score": 2 
+        }],
+    "parametres":{
+        'framerate': 10,
+        'plateau' : 4,
+        'taille_fenetre' : 640
+        }
+    }
+    
+==
+
+pltest==
+>>> initialise_jeu(4)
+{"plateau": [
+    [-1, -1, -1, -1],
+    [-1,  0,  1, -1],
+    [-1,  1,  0, -1],
+    [-1, -1, -1, -1]
+    ],
+"joueur actif": 0,
+"joueurs":  [{
+        "nom": "joueur1",
+        "couleur": "white",
+        "score": 2 
+    },
+    {
+        "nom": "joueur2",
+        "couleur": "red",
+        "score": 2 
+    }],
+"parametres":{
+    'framerate': 10,
+    'plateau' : 4,
+    'taille_fenetre' : 640
+    }
+}
+==
+
+
+
