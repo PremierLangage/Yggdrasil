32e50cfe4d0520ef713c221d9b11f3b13c469149
EMMANUEL BRUNACHE <3573@monlycee.net>
2022-03-30 15:51:04 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/python/1NSI-2122/DM_Reversi/coups_possibles.pl

--- ComputerScience/python/1NSI-2122/DM_Reversi/coups_possibles.pl
+++ ComputerScience/python/1NSI-2122/DM_Reversi/coups_possibles.pl
@@ -66,33 +66,6 @@ pltest1==
 [(0, 2), (1, 3), (2, 0), (3, 1)]
 ==
 
-pltest2==
->>> jeu = {
-... "plateau": [
-...     [-1, -1, -1, -1],
-...     [-1,  0,  0, -1],
-...     [-1,  1,  0, -1],
-...     [-1, -1, -1, -1]
-...     ],
-... "joueur actif": 0,
-... "joueurs":  [{
-...         "nom": "joueur1",
-...         "couleur": "white",
-...         "score": 2 
-...     },
-...     {
-...         "nom": "joueur2",
-...         "couleur": "red",
-...         "score": 2 
-...     }],
-... "parametres":{
-...     'framerate': 10,
-...     'plateau' : 4,
-...     'taille_fenetre' : 640
-...     }
-... } # 
->>> coups_possibles(jeu)
-[(2, 0), (3, 1)]
-==
+
 
 
