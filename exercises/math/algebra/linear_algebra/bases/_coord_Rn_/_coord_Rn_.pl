d014c01a326c4d6a6f4e97a8a6f826fea3782baf
David DOYEN <david.doyen@univ-eiffel.fr>
2022-03-31 08:50:08 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/linear_algebra/bases/_coord_Rn_/_coord_Rn_.pl

---
+++ exercises/math/algebra/linear_algebra/bases/_coord_Rn_/_coord_Rn_.pl
@@ -0,0 +1,23 @@
+extends = /model/math/tuple.pl
+
+before ==
+from randsympy import randmat_invertible, randmat
+n = param['n']
+A = randmat_invertible(n, 2, mindet=0, maxdet=6)
+
+B = randmat(n, 1, 2, [0])
+
+sol = list(linsolve((A, B)))[0]
+
+sys_tex=latexsys(A,B)
+basis = str(tuple([tuple(A.col(i)) for i in range(n)]))
+vec = str(tuple(B))
+==
+
+question ==
+On se place dans le $!\mathbb{R}!$-espace vectoriel $! \mathbb{R}^{{n}} !$ et on considère la base 
+
+$$ \mathcal{B} = {{basis}}.$$
+
+Déterminer les coordonnées du vecteur $! {{vec}} !$ dans cette base (écrire les coordonnées sous forme d'un {{tuple_name}} de nombres).
+==
