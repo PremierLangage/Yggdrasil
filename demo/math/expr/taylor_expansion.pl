a79bfa98c65e5bc07b81d1637d45f0fecb593ed6
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-01 10:24:52 +0200

    ddoyen1 modified Yggdrasil/demo/math/expr/taylor_expansion.pl

--- demo/math/expr/taylor_expansion.pl
+++ demo/math/expr/taylor_expansion.pl
@@ -4,7 +4,7 @@ before ==
 from sympy import O, series
 var('x')
 f = choice([sin(x), cos(x), exp(x), 1/(1-x)])
-f = sin(cos(x))
+f = cos(sin(x))
 n = randint(5, 6)
 sol = series(f, x, 0, n+1).removeO()
 ==
