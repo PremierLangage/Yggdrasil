f4f071af463c90fcda45b224b130a4155ca79203
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-01 10:12:05 +0200

    ddoyen1 modified Yggdrasil/demo/math/expr/taylor_expansion.pl

--- demo/math/expr/taylor_expansion.pl
+++ demo/math/expr/taylor_expansion.pl
@@ -15,7 +15,7 @@ Ecrire le développement limité de de $! {{ f|latex }} !$ autour de $! 0 !$ à 
 ==
 
 embed ==
-# + o({{ x }}^{{ n }} [x \to 0])
+# + o({{ x }}^{{ n }} [x \rightarrow 0])
 ==
 
 prefix ==
