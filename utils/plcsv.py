1cf6cdda03023389df9be54d96fe4dbc8d2e797b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-02 13:15:34 +0200

    ddoyen1 modified Yggdrasil/utils/plcsv.py

--- utils/plcsv.py
+++ utils/plcsv.py
@@ -4,31 +4,31 @@ from csv import DictReader
 rd = random.Random()
 
 
-def csv_choice(f, delimiter=','):
+def csv_choice(f, **kwargs):
     """
     Return a random row from a csv file/string (as a dictionary)
     """
 
     if isinstance(f, str):
-        return rd.choice(list(DictReader(StringIO(f), delimiter=delimiter)), k)
+        return rd.choice(list(DictReader(StringIO(f), **kwargs)), k)
     else:
-        return rd.choice(list(DictReader(f, delimiter=delimiter)))
+        return rd.choice(list(DictReader(f, **kwargs)))
 
-def csv_sample(f, k, delimiter=','):
+def csv_sample(f, k, **kwargs):
     """
     Return a random sample from a csv file/string (as a list of dictionaries)
     """
     if isinstance(f, str):
-        return rd.sample(list(DictReader(StringIO(f), delimiter=delimiter)), k)
+        return rd.sample(list(DictReader(StringIO(f), **kwargs)), k)
     else:
-        return rd.sample(list(DictReader(f, delimiter=delimiter)), k)
+        return rd.sample(list(DictReader(f, **kwargs)), k)
 
 def csv_col(f, col, delimiter=','):
     """
     Return data in sample from a csv file/string (as a list of dictionaries)
     """
     if isinstance(f, str):
-        reader = DictReader(StringIO(f), delimiter=delimiter)
+        reader = DictReader(StringIO(f), **kwargs)
     else:
-        reader = DictReader(f, delimiter=delimiter)
+        reader = DictReader(f, **kwargs)
     return [row[col] for row in reader]
