3aed52f854768eaf084df4d062a2212c3b8aeb4d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-03 18:07:08 +0200

    ddoyen1 modified Yggdrasil/model/math/input0.pl

--- model/math/input0.pl
+++ model/math/input0.pl
@@ -23,12 +23,15 @@ from sympy import sqrt, Abs, sin, cos, tan, exp, ln, atan, acos, asin
 from sympy import Symbol, symbols, var
 from sympy import sympify, simplify, factor, Lambda
 from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval, Matrix
+from sympy import Sum, Limit
 from sympy import diff, integrate
 from random import choice, choices, sample, shuffle
 from plrandom import randint, sampleint
 from sympy2latex import CustomLatexPrinter
 latex = CustomLatexPrinter(latexsettings).doprint
 from latex2sympy import latex2sympy
+from randsympy import randpoly
+from randysmpy import randmat, randmat_invertible, randmat_fullrank, randmat_givenrank
 ==
 
 style.basic =@ /model/css/basic.css.html
