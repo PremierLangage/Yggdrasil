42d6b58fec270c968b616310d63ec3e7719ba379
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-03 18:11:17 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/linear_algebra/linear_systems/_linsolve_/linsolve_2_5_nonzero_rat.pl

---
+++ exercises/math/algebra/linear_algebra/linear_systems/_linsolve_/linsolve_2_5_nonzero_rat.pl
@@ -0,0 +1,20 @@
+extends = linsys_.pl
+
+title = Système linéaire 2 x 2
+
+param.size = 2
+
+param.coeffboundA = 5
+
+param.coeffboundB = 3
+
+param.sparsity = 0
+
+param.typesol = "rat"
+
+param.maxdet = 6
+
+param.mindet = 2
+
+
+
