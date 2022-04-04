f34fa04cc0627830e3dc3f364450312ed9489d03
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-04 10:34:01 +0200

    ddoyen1 modified Yggdrasil/model/math/radio.pl

--- model/math/radio.pl
+++ model/math/radio.pl
@@ -5,17 +5,21 @@ before_scripts = ["mathimport", "initinput", "before", "process"]
 latexsettings.ln_notation = True
 latexsettings.inv_trig_style = full
 
-mathimport ==
+mathimport == #|py|
 from sympy import E, I, pi, oo
-from sympy import sqrt, Abs, sin, cos, tan, exp, ln
+from sympy import sqrt, Abs, sin, cos, tan, exp, ln, atan, acos, asin
 from sympy import Symbol, symbols, var
 from sympy import sympify, simplify, factor, Lambda
-from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval
-from sympy import integrate
+from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval, Matrix
+from sympy import Sum, Limit
+from sympy import diff, integrate
 from random import choice, choices, sample, shuffle
 from plrandom import randint, sampleint
-from sympy2latex import latex
+from sympy2latex import CustomLatexPrinter
+latex = CustomLatexPrinter(latexsettings).doprint
 from latex2sympy import latex2sympy
+from randsympy import randpoly
+from randsympy import randmat, randmat_invertible, randmat_fullrank, randmat_givenrank
 ==
 
 tplpage =@ /model/tplpage/basicmath.html
