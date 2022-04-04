ce32f1c85e899f7079c47d17b8d8b086ea28c0a1
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-04 10:58:56 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/linear_algebra/linear_systems/_nbsol_/nbsol_linsys_22.pl

--- exercises/math/algebra/linear_algebra/linear_systems/_nbsol_/nbsol_linsys_22.pl
+++ exercises/math/algebra/linear_algebra/linear_systems/_nbsol_/nbsol_linsys_22.pl
@@ -9,23 +9,26 @@ title = Nombre de solutions d'un sytème 2x2
 before ==
 from sympy2latex import latex_linsys
 n = 2
-indexsol = randint(0, 2)
 vec = [randint(-2, 2) for _ in range(n)]
 
-if indexsol == 0:
-    A = randmat_givenrank(n, n, n, magnitude=2)
-    B = A*Matrix(vec)
-elif indexsol == 1:
-    A = randmat_givenrank(n, n, n-1, magnitude=2)
-    B = A*Matrix(vec)
-else:
-    A = randmat_givenrank(n, n, n-1, magnitude=2)
-    while True:
-        perturbation = [0, 0, choice([-1,1])]
-        shuffle(perturbation)
-        B = A*Matrix(vec) + Matrix(perturbation)
-        if linsolve((A, B)) == EmptySet:
-            break
+def generate_data();
+    indexsol = randint(0, 2)
+    if indexsol == 0:
+        A = randmat_givenrank(n, n, n, magnitude=2)
+        B = A*Matrix(vec)
+    elif indexsol == 1:
+        A = randmat_givenrank(n, n, n-1, magnitude=2)
+        B = A*Matrix(vec)
+    else:
+        A = randmat_givenrank(n, n, n-1, magnitude=2)
+        while True:
+            perturbation = [0, 0, choice([-1,1])]
+            shuffle(perturbation)
+            B = A*Matrix(vec) + Matrix(perturbation)
+            if linsolve((A, B)) == EmptySet:
+                break
+    system = latex_linsys(A, B)
+    return indexsol, system
 
 system = latex_linsys(A, B)
 lstvar = "x, y"
