8213ae1e365ba95e9d41e98131ee2a1923d6e6f5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-05 10:14:09 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/linear_algebra/linear_systems/_nbsol_/nbsol_linsys_22.pl

--- exercises/math/algebra/linear_algebra/linear_systems/_nbsol_/nbsol_linsys_22.pl
+++ exercises/math/algebra/linear_algebra/linear_systems/_nbsol_/nbsol_linsys_22.pl
@@ -6,6 +6,30 @@ extends = /model/math/radio.pl
 
 title = Nombre de solutions d'un sytème
 
+extends = /model/basic/basic.pl
+
+before_scripts = ["mathimport", "initinput", "before", "process"]
+
+
+mathimport == #|py|
+from sympy import E, I, pi, oo
+from sympy import sqrt, Abs, sin, cos, tan, exp, ln, atan, acos, asin
+from sympy import Symbol, symbols, var
+from sympy import sympify, simplify, factor, Lambda
+from sympy import Integer, Rational, Poly, FiniteSet, Tuple, Interval, Matrix
+from sympy import Sum, Limit
+from sympy import diff, integrate
+from random import choice, choices, sample, shuffle
+from plrandom import randint, sampleint
+from sympy2latex import CustomLatexPrinter
+latex = CustomLatexPrinter(latexsettings).doprint
+from latex2sympy import latex2sympy
+from randsympy import randpoly
+from randsympy import randmat, randmat_invertible, randmat_fullrank, randmat_givenrank
+==
+
+tplpage =@ /model/tplpage/basicmath.html
+
 before ==
 from radio import Radio
 
