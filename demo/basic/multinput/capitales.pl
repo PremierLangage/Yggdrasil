38fda42a1c61700ae3796dc157483f040deb1bd8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-05 15:13:05 +0200

    ddoyen1 modified Yggdrasil/demo/basic/multinput/capitales.pl

--- demo/basic/multinput/capitales.pl
+++ demo/basic/multinput/capitales.pl
@@ -5,12 +5,15 @@ Quelles sont les capitales des pays suivants ?
 ==
 
 before == #|py|
+import sys
+test = sys.version
 inputs = [TextInput(), TextInput()]
 inputs[0].sol = "Paris"
 inputs[1].sol = "Rome"
 ==
 
 inputblock == #|html|
+{{ test }}
 <div style="display:flex; align-items: center; margin-bottom: 1em;">
   <div> France : </div>
   <div style="margin-left: 1em;"> {{ inputs[0]|component }}</div>
