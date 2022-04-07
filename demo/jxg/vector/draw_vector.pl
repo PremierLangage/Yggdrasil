479857d442324c8a5e3784b47203894fc2175222
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-07 15:45:39 +0200

    ddoyen1 modified Yggdrasil/demo/jxg/vector/draw_vector.pl

--- demo/jxg/vector/draw_vector.pl
+++ demo/jxg/vector/draw_vector.pl
@@ -9,16 +9,8 @@ sol = sampleint(-5, 5, 2)
 attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 
 jxgscript == #|js|
-board.create('grid',[],{gridX:1,gridY:1});
-var V1 = board.create('point',[ {{v1x}} , {{v1y}} ],{visible:false, name:'v1', fixed:true});
-var V2 = board.create('point',[ {{v2x}} , {{v2y}} ],{size:0,name:'v', fixed:true});
-var V = board.create('segment',[[ {{v1x}} , {{v1y}} ], [ {{v2x}} , {{v2y}} ]], {color:'orange', lastArrow: {type: 2, size: 6}});
-var U1 = board.create('point',[ {{u1x}} , {{u1y}} ],{visible:false, name:'u1'});
-var U2 = board.create('point',[ {{u2x}} , {{u2y}} ],{size:0,name:'u'});
-var U = board.create('segment',[U1, U2], {color:'purple', lastArrow: {type: 2, size: 6}});
-
-var pt1 = board.create('point', [-3, -3], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
-var pt2 = board.create('point', [-1, -3], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
+var pt1 = board.create('point', [-2, 2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
+var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
 var vec = board.create('segment',[pt1, pt2],{color:'blue', lastArrow: {type: 2, size: 6}});
 ==
 
