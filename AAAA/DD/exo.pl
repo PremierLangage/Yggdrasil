c11723509db27f374f55dc6b9d4755bcfd2bc80c
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-07 10:51:10 +0200

    ddoyen1 modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -1,13 +1,47 @@
-extends = /model/math/interval.pl
+extends = /model/math/multimathinput.pl
+title = Calcul de limites
 
-before ==
-a = randint(1, 3)
-b = randint(-3, 3)
-sol = Interval(-oo, b-a)
+param.form = [0, 1, 2]
+
+before == #|py|
+from sympy import atan, Limit
+n = 3
+inputs = [MathInput() for _ in range(n)]
+prefixes = []
+
+var('x')
+inf = [ln(x), exp(x), x]
+f = [atan(x), x/(x+1)]
+b = [sin(x), cos(x)]
+
+
+def rand_expr(i):
+    inf1, inf2 = sample(inf, 2)
+    f1, f2 = sample(f, 2)
+    b1, b2 = sample(b, 2)
+    if i == 0:
+        return inf1 - inf2
+    elif i == 1:
+        return choice([-1, 1])*inf1 + choice([-1, 1])*f1
+    elif i == 2:
+        return choice([-1, 1])*inf1 + choice([-1, 1])*b1
+    elif i == 3:
+        return inf1/inf2 + choice([-1, 1])*f1
+
+cases = [0, 1, 3]
+shuffle(cases)
+
+for i in range(n):
+    lim = Limit(rand_expr(cases[i]), x, oo)
+    s = latex(lim)
+    prefixes.append(f"$! \displaystyle {s} = !$")
+    inputs[i].sol = lim.doit()
+    inputs[i].type = "expr"
+    inputs[i].evalparam = {}
 ==
 
 question ==
-Ecrire sous forme d'intervalle l'ensemble des réels $! x  + {{ a }} \le {{ b }}  !$.
+Calculer les limites suivantes.
 ==
 
-keypad = ["-infty", "+infty"]
+latexsettings.ln_notation = True
