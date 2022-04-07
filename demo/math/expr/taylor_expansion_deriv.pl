48d1ebbdcb44733b04218d7737b7cc4e64c07d18
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-07 11:11:36 +0200

    ddoyen1 modified Yggdrasil/demo/math/expr/taylor_expansion_deriv.pl

---
+++ demo/math/expr/taylor_expansion_deriv.pl
@@ -0,0 +1,28 @@
+extends = /model/math/expr.pl
+
+before ==
+from sympy import O, series
+var('x')
+f = choice([sin(x), cos(x), exp(x), 1/(1-x)])
+f = cos(sin(x))
+n = randint(5, 6)
+sol = series(f, x, 0, n+1).removeO()
+==
+
+equality = ""
+
+question ==
+Ecrire le développement limité de de $! {{ f|latex }} !$ autour de $! 0 !$ à l'ordre $! {{ n }} !$.
+==
+
+embed ==
+# + o({{ x }}^{{ n }}) \quad [x \rightarrow 0]
+==
+
+prefix ==
+$! {{ f|latex }} = !$
+==
+
+solution ==
+$! {{ sol|latex }} + o({{ x }}^{{ n }})  \quad [x \rightarrow 0]!$.
+==
