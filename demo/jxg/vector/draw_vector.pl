289e42362df8d2bac1e39f1ffa30030217947e4f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-07 17:05:02 +0200

    ddoyen1 modified Yggdrasil/demo/jxg/vector/draw_vector.pl

--- demo/jxg/vector/draw_vector.pl
+++ demo/jxg/vector/draw_vector.pl
@@ -7,11 +7,11 @@ sol = sampleint(-5, 5, 2)
 attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 
 jxgscript == #|js|
-var pt1 = board.create('point', [-2, 2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
+var pt1 = board.create('point', [-2, -2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
 var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
 var vec = board.create('segment',[pt1, pt2],{color:'blue', lastArrow: {type: 2, size: 6}});
 ==
 
 question ==
-Modifier le vecteur ci-dessous pour en faire un vecteur de coordonnées $! {{ sol[0] }}, {{ sol[1] }} !$. 
+Modifier le vecteur ci-dessous pour en faire un vecteur de coordonnées $! ( {{ sol[0] }}, {{ sol[1] }} ) !$. 
 ==
