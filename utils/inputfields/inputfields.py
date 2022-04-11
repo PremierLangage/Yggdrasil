af3e886c983861aa93130d8ac4ba1a6b3b98cf28
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-11 15:12:09 +0200

    ddoyen1 modified Yggdrasil/utils/inputfields/inputfields.py

---
+++ utils/inputfields/inputfields.py
@@ -0,0 +1,84 @@
+
+
+import random as rd
+from uuid import uuid4
+from components import Component
+from scoring import *
+
+class Serializable):
+    pass
+
+
+class Radio(Serializable):
+
+    def __init__(self, **kwargs):
+        self.data = {'selector' = 'c-radio-group'
+        super().__init__(**kwargs)
+
+    def set_items(self, items):
+        """
+        Set the list of items used as choices.
+        """
+        if isinstance(items, list):
+            self.data['items'] = [{"id": str(uuid4()), "content": str(item)} for item in items]
+        elif isinstance(items, str):
+            self.data['items'] = [{"id": str(uuid4()), "content": str(item)} for item in items.splitlines()]
+
+    def set_sol(self, index):
+        """
+        Set the solution item by giving its index in the list of items.
+        """
+        self.sol = self.items[index]['id']
+
+    def shuffle(self):
+        """
+        Shuffle the items.
+        """
+        rd.shuffle(self.data['items'])
+
+    def eval(self):
+        """
+        Evaluate the answer.
+        """
+        for item in self.data['items']:
+            id = item['id']
+            if id == self.sol and id == self.selection:
+                self.score = 100
+                return 100
+        self.score = 0
+        return 0
+
+    def display_feedback(self):
+        """
+        Display visual feedback.
+        """
+        for item in self.data['items']:
+            id = item['id']
+            if id == self.sol:
+                item['css'] = 'icon-success-after'
+            elif id != self.sol and id == self.data['selection']:
+                item['css'] = 'icon-fail-after'
+
+    def hide_feedback(self):
+        """
+        Hide visual feedback.
+        """
+        for item in self.data['items']:
+            s = item['css'] 
+            s = s.replace('icon-success-after', '')
+            s = s.replace('icon-fail-after', '')
+            item['css'] = s
+
+    def disable(self):
+        """
+        Disable the input field.
+        """
+        self.data['disabled'] = True
+
+    def render(self):
+        """
+        Return the HTML code of the input field.
+        """
+        selector = self.data['selector']
+        cid = self.data['cid']
+        return f"<{selector} cid='{cid}'></{selector}>"
