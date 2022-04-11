d4c122825b62aedbf1583096663ac877e58ba63d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-11 15:21:09 +0200

    ddoyen1 modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -1,7 +1,9 @@
 extends = /model/basic/numinput.pl
 @ /builder/before3.py [builder.py]
+@ /utils/inputfields/inputfields.py
 
 before == #|py|
+from inputfields import Radio
 sol = 3
 test = [NumInput(value=34), NumInput()]
 ==
