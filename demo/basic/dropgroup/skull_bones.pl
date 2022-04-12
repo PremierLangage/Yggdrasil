7a344109a9b91fec448530e9786bf57c8b7a60ef
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 09:53:25 +0200

    ddoyen1 modified Yggdrasil/demo/basic/dropgroup/skull_bones.pl

---
+++ demo/basic/dropgroup/skull_bones.pl
@@ -0,0 +1,31 @@
+extends = /model/basic/dropgroup.pl
+
+title = Os du crâne
+
+sol ==
+Os frontal
+Os pariétal
+Os sphénoïde
+Os temporal
+Os occipital
+Os ethmoïde
+==
+
+scheme =$ skull_bones.png
+
+inputblock ==
+<div class="row">
+<div class="column">
+<img src="{{ scheme }}" style="max-width: 300px; vertical-align:top; display: inline-block">
+</div>
+<div class="column">
+<ul>
+{% for drop in inputfield.drops %}
+<li> {{ loop.index }}. {{ drop|html }} </li>
+{% endfor %}
+</ul>
+</div>
+</div> 
+
+{% for label in inputfield.labels %} {{ label|html }} {% endfor %}
+==
