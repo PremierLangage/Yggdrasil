dd861fea3cd1571f17852b719b2d7bf9a0976ed6
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 16:00:44 +0200

    ddoyen1 modified Yggdrasil/utils/steps/steps.py

--- utils/steps/steps.py
+++ utils/steps/steps.py
@@ -1,11 +1,11 @@
 from jinja_env import Env
 from basicinput import *
-
+from serializable import Serializable
 from uuid import uuid4
 import re
 import random as rd
 
-class Step:
+class Step(Serializable):
 
     def __init__(self, **kwargs):
         self.question = kwargs.get('question', '')
