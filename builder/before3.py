d2435f8bbcba597d462299183340c2749496142a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 13:13:46 +0200

    ddoyen1 modified Yggdrasil/builder/before3.py

--- builder/before3.py
+++ builder/before3.py
@@ -75,18 +75,13 @@ if __name__ == "__main__":
 
     for key in dic.get('jinja_keys', ['question', 'solution']):
         if key in dic:
-            key2 = f"_{key}_"
             if isinstance(dic[key] , str):
-                #dic[key2] = "{% raw %}" + dic[key] + "{% endraw %}"
                 dic[key] = Env.from_string(macros + dic[key]).render(dic)
             elif isinstance(dic[key] , dict):
-                #dic[key2] = dic[key].copy()
                 for k in dic[key]:
-                    #dic[key2][k] = "{% raw %}" + dic[key2][k] + "{% endraw %}"
                     dic[key][k] = Env.from_string(macros + dic[key][k]).render(dic)
             elif isinstance(dic[key] , list):
                 for i in range(len(dic[key])):
-                    #dic[key2][i] = "{% raw %}" + dic[key][i] + "{% endraw %}"
                     dic[key][i] = Env.from_string(macros + dic[key][i]).render(dic)
 
     dic['form'] = dic['tplpage']
