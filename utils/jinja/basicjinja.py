8f84e25600b6fccdb20b2943288e604547d227c3
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 17:24:03 +0200

    ddoyen1 modified Yggdrasil/utils/jinja/basicjinja.py

---
+++ utils/jinja/basicjinja.py
@@ -0,0 +1,18 @@
+from jinja2 import Environment, BaseLoader
+
+def component(l):
+    if isinstance(l, dict):
+        selector = l["selector"]
+        cid = l["cid"]
+    else:
+        selector = l.selector
+        cid = l.cid
+    return "<%s cid='%s'></%s>" % (selector, cid, selector)
+
+def html(l):
+    return l.render()
+
+    
+Env = Environment(loader=BaseLoader())
+Env.filters["html"] = html
+Env.filters["component"] = component
