57805678eee347b6668b9452c773d365d26367a8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 09:19:43 +0200

    ddoyen1 modified Yggdrasil/utils/inputfields/basicinput.py

--- utils/inputfields/basicinput.py
+++ utils/inputfields/basicinput.py
@@ -296,3 +296,45 @@ class AutoInput(TextInput):
         else:
             self.score = 0
         return self.score
+
+class NumInput(Component):
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-input'
+        self.decorator = 'NumInput'
+        self.type = 'number'
+        self.evalparam = {}
+        self.feedback = kwargs.get('feedback', '')
+        super().__init__(**kwargs)
+
+    def eval(self):
+        diffmeasure = self.evalparam.get('diffmeasure', 'AbsError')
+        tol = self.evalparam.get('tol', 0)
+        if diffmeasure == 'AbsError':
+            diff = abs(self.sol - self.value)
+        elif diffmeasure == 'RelError':
+            diff = abs(self.sol - self.value)/abs(self.sol)
+        if diff <= tol:
+            self.score = 100
+        else:
+            self.score = 0
+        return self.score
+
+    def display_feedback(self):
+        msg = self.feedback
+        if self.score == 100:
+            self.suffix = r'<i class="fas fa-check" style="color: var(--success)"></i>'
+        elif self.score >= 0:
+            self.suffix = r'<i class="fas fa-times" style="color: var(--danger)"></i></i>'
+            self.suffix = rf"""<i class="fas fa-times" style="margin-left: 0.5em; color: #E53935; cursor: pointer;" data-toggle="popover" data-placement="bottom" data-content="{msg}"></i>"""
+    
+    def hide_feedback(self):
+        self.suffix = ""
+
+    def disable(self):
+        """
+        Disable the input field.
+        """
+        self.data['type'] = 'text' # HACK : doesn't work when the type is 'number'
+        self.data['value'] = str(self.data['value'])
+        self.data['disabled'] = True
