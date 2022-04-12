282eac5f7188e8f28262489411d5c4acfccf9155
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 18:42:08 +0200

    ddoyen1 modified Yggdrasil/model/jxg/input0.pl

--- model/jxg/input0.pl
+++ model/jxg/input0.pl
@@ -4,6 +4,7 @@
 @ /builder/before3.py [builder.py]
 @ /grader/evaluator3.py [grader.py]
 @ /utils/inputfields/basicinput.py
+@ /utils/inputfields/mathinput.py
 @ /utils/inputfields/jxginput.py
 @ /utils/jinja/mathjinja.py  [jinja_env.py]
 @ /utils/json/mathjson.py [json_encoder.py]
@@ -16,7 +17,6 @@
 @ /utils/sympy/randsympy.py
 @ /utils/graphics/plmpl.py
 
-@ /utils/inputfields/mathinput.py
 @ /utils/components/mathinput/mathinput.html
 
 # Specific keys
