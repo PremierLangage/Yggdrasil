f661af52ec66ca8aa826f72fbbb6bbeb3add05b9
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 15:39:56 +0200

    ddoyen1 modified Yggdrasil/utils/inputfields/mathinput.py

--- utils/inputfields/mathinput.py
+++ utils/inputfields/mathinput.py
@@ -1,6 +1,8 @@
 from uuid import uuid4
 from evalsympy import eval_expr, eval_complex, eval_poly, eval_set, eval_tuple, eval_interval, eval_chainineq
 from jinja2 import Template
+from sympy import Matrix
+from evalsympy import eval_matrix
 
 std_keypad = {
     "emptyset": {"label": "$! \\varnothing !$", "action": "cmd", "value": "\\empty"},
@@ -101,3 +103,69 @@ class MathInput:
 
     def get_value(self):
         return self.value
+
+
+class MatrixInput(Component):
+
+    message = {}
+
+    def __init__(self, **kwargs):
+        self.selector = 'c-math-matrix'
+        self.decorator = 'MatrixInput'
+        self.sol = kwargs.get('sol', None)
+        super().__init__(**kwargs)
+
+    def set_matrix(self, M):
+        """
+        Set a matrix.
+        """
+        self.matrix = []
+        if isinstance(M, list):
+            for row in M:
+                self.matrix.append([{'value': str(value)} for value in row])
+        elif isinstance(M, Matrix):
+            for i in range(len(M.col(0))):
+                self.matrix.append([{'value': str(value)} for value in M.row(i)])
+
+    def set_zeros(self, rows, cols=None):
+        """
+        Set a matrix of zeros.
+        """
+        if cols == None:
+            cols = rows
+        self.set_matrix([cols * [0] for _ in range(rows)])
+
+    def get_value(self):
+        """
+        Get the matrix.
+        """
+        return [[item['value'] for item in row] for row in self.matrix]
+
+
+    def eval(self):
+        """
+        Evaluate the input field.
+        """
+        score, error = eval_matrix(Matrix(self.get_value()), self.sol, **self.evalparam)
+        self.score = score
+        self.feedback = MatrixInput.message.get(error, f"Error: {error}")
+        return self.score
+        
+    def display_feedback(self):
+        """
+        Display visual feedback.
+        """
+        pass
+
+    def hide_feedback(self):
+        """
+        Hide visual feedback.
+        """
+        pass
+
+    def disable(self):
+        """
+        Disable the input field.
+        """
+        self.disabled = True
+
