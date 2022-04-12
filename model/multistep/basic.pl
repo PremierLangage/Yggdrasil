70cb673a3a73e171e185a46ed033062e3db5513b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 16:13:05 +0200

    ddoyen1 modified Yggdrasil/model/multistep/basic.pl

--- model/multistep/basic.pl
+++ model/multistep/basic.pl
@@ -1,5 +1,16 @@
 extends = /model/basic/hackpage.pl
-extends = /model/basic/utils.pl
+
+@ /utils/plrandom.py
+@ /utils/plcsv.py
+@ /utils/sympy/sympy2latex.py
+
+@ /builder/before3.py [builder.py]
+@ /grader/evaluator3.py [grader.py]
+@ /utils/inputfields/basicinput.py
+@ /utils/steps/steps.py
+@ /model/math/jinja_math.py [jinja_env.py]
+@ /utils/json/basicjson.py [json_encoder.py]
+@ /utils/json/serializable.py
 
 
 before_scripts = ["importfunc", "init", "before", "process"]
