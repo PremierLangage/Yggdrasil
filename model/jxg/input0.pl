ce48efaa142d809d2333481a0f56fab4b6b8a00a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-12 18:22:40 +0200

    ddoyen1 modified Yggdrasil/model/jxg/input0.pl

---
+++ model/jxg/input0.pl
@@ -0,0 +1,20 @@
+@ /utils/std/plrandom.py
+@ /utils/std/plcsv.py
+
+@ /builder/before3.py [builder.py]
+@ /grader/evaluator3.py [grader.py]
+@ /utils/inputfields/basicinput.py
+@ /utils/inputfields/jxginput.py
+@ /utils/jinja/mathjinja.py  [jinja_env.py]
+@ /utils/json/mathjson.py [json_encoder.py]
+@ /utils/json/serializable.py
+
+
+@ /utils/sympy/evalsympy.py
+@ /utils/sympy/latex2sympy.py
+@ /utils/sympy/sympy2latex.py
+@ /utils/sympy/randsympy.py
+@ /utils/graphics/plmpl.py
+
+@ /utils/inputfields/mathinput.py
+@ /utils/components/mathinput/mathinput.html
