64331aa5980c3e5468db5e75ec1cf81678d62571
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-04-13 22:42:27 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/python/SNT-2122/chapitre_4_fonctions/tri3valeurs.pl

---
+++ ComputerScience/python/SNT-2122/chapitre_4_fonctions/tri3valeurs.pl
@@ -0,0 +1,48 @@
+author=Emmanuel Brunache
+name= Tri de 2 valeurs
+title= Tri de 2 valeurs # N'oubliez pas de remplir ce champs svp
+tag=def|parameters|return # N'oubliez pas de remplir ce champs svp
+extends=/ComputerScience/python/template/pltest.pl
+piste=verte
+text==
+
+Écrire une fonction **tri3valeurs(val1, val2, val3)** qui prend comme arguments trois entiers et qui retourne les valeurs de la plus petite à la plus grande.
+
+Exemples :
+
+    >>> tri3valeurs(5, 2, 1)
+    (1, 2, 5)
+    >>> tri2valeurs(10, 10, 3)
+    (3, 10, 10)
+    >>> tri2valeurs(3, 7, 4)
+    (3, 4, 7)
+    >>> tri2valeurs(30, -7, 2)
+    (-7, 2, 30)
+
+==
+
+pltest==
+    >>> tri2valeurs(5, 2)
+    (2, 5)
+    >>> tri2valeurs(10, 10)
+    (10, 10)
+    >>> tri2valeurs(3, 7)
+    (3, 7)
+    >>> tri2valeurs(30, -7)
+    (-7, 30)
+==
+
+
+testcode==
+def tri2valeurs(val1, val2):
+    if val1 > val2:
+        return val2, val1
+    else:
+        return val1, val2
+==
+
+
+
+
+
+
