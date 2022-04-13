90f9c4cec30c921f0d56708c53f06840945bd560
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-13 14:13:03 +0200

    ddoyen1 modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -1,23 +1,5 @@
 extends = /model/basic/basic.pl
 
 question ==
-<div ng-app="myApp" ng-controller="personCtrl">
-
-First Name: <input type="text" ng-model="firstName"><br>
-Last Name: <input type="text" ng-model="lastName"><br>
-<br>
-Full Name: {{fullName()}}
-
-</div>
-
-<script>
-var app = angular.module('myApp', []);
-app.controller('personCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-    $scope.fullName = function() {
-        return $scope.firstName + " " + $scope.lastName;
-    };
-});
-</script>
+ggg
 ==
