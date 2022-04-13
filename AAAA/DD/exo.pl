77288e7ff3edb5d29fb40347139ea46210a2fac6
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-13 14:15:18 +0200

    ddoyen1 modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -1,15 +1,16 @@
 extends = /model/basic/basic.pl
 
 question ==
+{% raw %}
 <div ng-app="myApp" ng-controller="personCtrl">
 
 First Name: <input type="text" ng-model="firstName"><br>
 Last Name: <input type="text" ng-model="lastName"><br>
 <br>
-
+Full Name: {{fullName()}}
 
 </div>
-
+{% endraw %}
 ==
 
 javascript.toto ==
