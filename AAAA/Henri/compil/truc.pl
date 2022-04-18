0e30fa75bf208e52f5cefe43573802c9ff2d29c8
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2022-04-18 22:46:00 +0200

    hderycke1 modified Yggdrasil/AAAA/Henri/compil/truc.pl

--- AAAA/Henri/compil/truc.pl
+++ AAAA/Henri/compil/truc.pl
@@ -13,11 +13,13 @@ form==
 grader==#|py|
 import json 
 import sys
-with open(sys.argv[2], 'r') as f:
-    answers = json.load(f)
-print(answers, file=sys.stderr)
-with open(sys.argv[4], 'w+') as f:
-    print(answers, file=f)
+try:
+    with open(sys.argv[2], 'r') as f:
+        answers = json.load(f)
+    with open(sys.argv[4], 'w+') as f:
+        print(answers, file=f)
+except Exception as e:
+    print(e, file=sys.stderr)
 ==
 
 settings.allow_reroll = true
