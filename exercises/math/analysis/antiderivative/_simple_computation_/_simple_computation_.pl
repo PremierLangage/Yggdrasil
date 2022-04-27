b89b35ae640011a7e09059e402ca80bc915067b6
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-27 09:55:39 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/antiderivative/_simple_computation_/_simple_computation_.pl

--- exercises/math/analysis/antiderivative/_simple_computation_/_simple_computation_.pl
+++ exercises/math/analysis/antiderivative/_simple_computation_/_simple_computation_.pl
@@ -21,15 +21,7 @@ before ==
 from sympy import integrate
 var('x')
 alpha = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
-if randint(0, 1) == 1:
-    a = c
-    b = 1
-else:
-    a = 1
-    b = c
-i = randint(0, 2)
-u = [exp(a*x), sin(a*x), cos(a*x)][i]
-du = [exp(a*x), cos(a*x), sin(a*x)][i]
+a = 1
 
 lst_vx = [exp(x),
 sin(x),
