790036808a082b417c70b7b2dcb48477867424ff
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-27 20:02:00 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/antiderivative/computation/_sum_ref_/_sum_ref_.pl

---
+++ exercises/math/analysis/antiderivative/computation/_sum_ref_/_sum_ref_.pl
@@ -0,0 +1,76 @@
+extends = /model/math/expr.pl
+
+title = Calcul de primitive
+
+param.form = [0, 1, 2]
+param.u = [0, 1, 2]
+
+# Paramètres
+# 0 : 1
+# 1 : x
+# 2 : x^2
+# 3 : x^n
+# 4 : sin(x)
+# 5 : cos(x)
+# 6 : e^x
+
+
+question ==
+Calculer une primitive de la fonction
+$$ f : x \mapsto {{ f|latex }} .$$
+==
+
+solution ==
+Les primitives de cette fonction sont les fonctions de la forme $$ x \mapsto {{ sol|latex }} + k $$
+
+où $ k $ est une constante réelle.
+==
+
+equality = "UpToConstant"
+
+message.NotEqualUpToConstant = La réponse n'est pas une primitive.
+
+before ==
+from sympy import integrate
+var('x')
+alpha = choice ([Rational(1,2), Rational(1,3), Rational(1,4), 2, 3, 4])
+a = 1
+b = 2
+n = 3
+r = Rational(2,3)
+
+lst_vx = [exp(x),
+sin(x),
+cos(x),
+1/x,
+x,
+x**2,
+x**n,
+1/x**2,
+1/x**n,
+1/sqrt(x),
+x**r,
+sqrt(x)]
+
+i = choice(param['form'])
+vx = lst_vx[i]
+v = Lambda(x, vx)
+
+lst_ux = [a*x,
+a*x**2,
+a*x + b,
+a*x**2 + b,
+a*x**2 + b*x,
+a*x**n,
+a*sin(x),
+a*cos(x),
+a*exp(x)]
+
+i = choice(param['u'])
+ux = lst_ux[i]
+
+f = alpha*diff(ux, x)*v(ux)
+
+
+sol = integrate(f, x).doit()
+==
