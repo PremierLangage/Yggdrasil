590502178dfc7ac88414f1438475dc73b5cd7710
David DOYEN <david.doyen@univ-eiffel.fr>
2022-04-28 18:00:59 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/antiderivative/computation/_sum_ref_/_sum_ref_.pl

--- exercises/math/analysis/antiderivative/computation/_sum_ref_/_sum_ref_.pl
+++ exercises/math/analysis/antiderivative/computation/_sum_ref_/_sum_ref_.pl
@@ -80,6 +80,6 @@ f, positive = generate_f(param['nbterms'], param['typeterms'], param['addmon'])
 if positive:
     sur_intervalle = r"sur l'intervalle $! ]0, +\infty[ !$."
 
-interv = continuous_domain(f, x, S.Reals)
+interv = continuous_domain(f, x, S.Reals).args
 sol = integrate(f, x).doit().expand()
 ==
