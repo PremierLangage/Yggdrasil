37a23a75d52ac4fd5b32214ba0e73a17a34fd503
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-02 10:53:21 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/limits/limit_polyfrac.pl

--- exercises/math/analysis/limits/limit_polyfrac.pl
+++ exercises/math/analysis/limits/limit_polyfrac.pl
@@ -16,8 +16,8 @@ d0=randint(2,7)
 d = [[d0,d0]]+[[randint(2,7),randint(2,7)] for _ in range(2)] 
 
 for i in range(n):
-    f = randpoly(d[i][0],randint(2,3),5,x)
-    g = randpoly(d[i][1],randint(2,3),5,x)
+    f = randpoly(d[i][0],randint(2,3),5)
+    g = randpoly(d[i][1],randint(2,3),5)
     lim = Limit(f/g, x, oo)
     s = latex(lim)
     prefixes.append(f"$! \displaystyle {s} = !$")
