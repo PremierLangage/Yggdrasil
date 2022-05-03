50a0fc5b1f35af948f1cc2bc282f4862f1b77992
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-03 17:45:20 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/computation/add.pl

--- exercises/math/complex_numbers/computation/add.pl
+++ exercises/math/complex_numbers/computation/add.pl
@@ -5,20 +5,8 @@ title = Addition
 before == #|python|
 inputs = [MathInput(type="complex", evalparam={'form': 'cartesian'}) for _ in range(3)]
 
-prefixes = []
-for i in range(3):
-    if i < 2:
-        z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
-        z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
-        s = latex(z1 * z2)
-    else:
-        z1 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
-        z2 = z1
-        s = latex(z1**2)
-    prefixes.append(f"$! {s} = !$")
-    inputs[i].sol = (z1 * z2).expand()
-
 from sympy import conjugate
+from sympy2latex, latex_lincomb
 
 n = 5
 z1 = randint(-n, n, [0]) + randint(-n, n, [0])*I
