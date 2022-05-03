217a08a7ead1ec66ae4a886aa1ac3d7720853b95
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-03 16:57:18 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/computation/div.pl

--- exercises/math/complex_numbers/computation/div.pl
+++ exercises/math/complex_numbers/computation/div.pl
@@ -15,7 +15,7 @@ for i in range(3):
         z2 = randint(-3, 3, [0]) + randint(-3, 3, [0])*I
     s1 = latex(z1)
     s2 = latex(z2)
-    prefixes.append(r"$! \frac{ %s }{ %s } = !$" % (s1, s2))
+    prefixes.append(r"$! \displaystyle \frac{ %s }{ %s } = !$" % (s1, s2))
     inputs[i].sol = (z1/z2).expand()
 ==
 
