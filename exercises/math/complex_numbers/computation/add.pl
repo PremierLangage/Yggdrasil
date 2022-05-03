e748ded02913191673c695f5ffc2da36460324c6
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-03 17:46:53 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/computation/add.pl

--- exercises/math/complex_numbers/computation/add.pl
+++ exercises/math/complex_numbers/computation/add.pl
@@ -21,16 +21,16 @@ prefixes = []
 sols = []
 
 prefixes.append(f"$! z_1 + z_2 = !$")
-sols.append( z1 + z2).expand()
+sols.append((z1 + z2).expand())
 
 prefixes.append(f"$! z_1 - z_2 = !$")
-sols.append( z1 - z2).expand()
+sols.append((z1 - z2).expand())
 
 coeff = [randint(-1, 1), choice([-3, -2, 2, 3])]
 shuffle(coeff )
 s = latex_lincomb(coeff , ["z_1", "z_2"])
 prefixes.append(f"$! {s} = !$")
-sols.append(coeff[0]*z1 + coeff[1]*z2).expand()
+sols.append((coeff[0]*z1 + coeff[1]*z2).expand())
 
 for i in range(3):
     inputs[i].sol = sols[i]
