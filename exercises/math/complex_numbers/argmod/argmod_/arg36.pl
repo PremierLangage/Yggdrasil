0834b2f19118bd1ebef1f7e7ae1fc04708757d79
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-04 20:08:20 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/argmod/argmod_/arg36.pl

---
+++ exercises/math/complex_numbers/argmod/argmod_/arg36.pl
@@ -0,0 +1,4 @@
+extends = argmod_.pl
+
+param.z0 = "2 + sqrt(3)*I, 2 - sqrt(3)*I, -2 + sqrt(3)*I, -2 - sqrt(3)*I, 2*I + sqrt(3), 2*I - sqrt(3), -2*I + sqrt(3), -2*I - sqrt(3)"
+param.coeff = "1, 2, 1/2, sqrt(2), 1/sqrt(3), sqrt(3), 1/2*sqrt(3)"
