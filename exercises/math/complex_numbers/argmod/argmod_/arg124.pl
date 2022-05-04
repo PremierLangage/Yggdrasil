a31f19515cd854960e7b1e0788a97069831baec3
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-04 18:57:11 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/argmod/argmod_/arg124.pl

--- exercises/math/complex_numbers/argmod/argmod_/arg124.pl
+++ exercises/math/complex_numbers/argmod/argmod_/arg124.pl
@@ -1,4 +1,4 @@
 extends = setpoint_from_argmod_.pl
 
-param.z0 = "[I, -I, 1, -1, 1+I, 1-I, I-1, -1-I]"
-param.coeff = "[1, 2, 3, 1/2, 1/3, sqrt(2), sqrt(3)]"
+param.z0 = "I, -I, 1, -1, 1+I, 1-I, I-1, -1-I"
+param.coeff = "1, 2, 3, 1/2, 1/3, sqrt(2), sqrt(3)"
