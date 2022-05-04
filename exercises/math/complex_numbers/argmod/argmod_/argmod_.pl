6917934b138eca0c45852901e2f5a2df10a487a9
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-04 18:47:31 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/argmod/argmod_/argmod_.pl

--- exercises/math/complex_numbers/argmod/argmod_/argmod_.pl
+++ exercises/math/complex_numbers/argmod/argmod_/argmod_.pl
@@ -10,6 +10,9 @@ title = "Argument d'un nombre complexe"
 before ==
 from sympy import arg
 
+param.z0 = "[I, -I, 1, -1, 1+I, 1-I, I-1, -1-I]"
+param.coeff = "[1, 2, 3, 1/2, 1/3, sqrt(2), sqrt(3)]"
+
 z = choice(eval(param['coeff']))* choice(eval(param['z0']))
 z = z.expand()
 
