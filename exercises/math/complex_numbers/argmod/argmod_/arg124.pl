5f5456638a6ce6d0437db0bfd660c7c5a2df9aee
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-04 18:44:13 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/argmod/argmod_/arg124.pl

--- exercises/math/complex_numbers/argmod/argmod_/arg124.pl
+++ exercises/math/complex_numbers/argmod/argmod_/arg124.pl
@@ -1,3 +1,4 @@
 extends = setpoint_from_argmod_.pl
 
-param.arguments = [0, pi/4, pi/2, 3*pi/4, pi,5*pi/4, 3*pi/2, 7*pi/4]
+param.arg = "[0, pi/4, pi/2, 3*pi/4, pi,5*pi/4, 3*pi/2, 7*pi/4]"
+param.mod = "[1, 2, 3, 1/2, 1/3, sqrt(2), sqrt(3)]"
