f20db8155fe5dedfb17fa9be814470dacea05684
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-04 16:05:07 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/argmod/argmod_/argmod_.pl

--- exercises/math/complex_numbers/argmod/argmod_/argmod_.pl
+++ exercises/math/complex_numbers/argmod/argmod_/argmod_.pl
@@ -14,27 +14,15 @@ title = "Argument d'un nombre complexe"
 before ==
 from sympy import arg
 
-from sympy import conjugate
-from sympy2latex import latex_lincomb
-
-n = 5
-z1 = randint(-n, n, [0]) + randint(-n, n, [0])*I
-z2 = randint(-n, n, [0]) + randint(-n, n, [0])*I
-while z1 == z2:
-    z2 = randint(-n, n, [0]) + randint(-n, n, [0])*I
-
-#z1b = conjugate(z1)
-#z2b = conjugate(z2)
 
 prefixes = []
-inputs = []
+inputs = [MathInput(type="expr"), MathInput(type="expr", evalparam={'equality': "Modulo", 'modulo': 2*pi})]
 
-inputs = [MathInput(type="expr"), MathInput(type="expr", evalparam = {'equality': "Modulo", 'modulo': 2*pi})]
 prefixes.append("module :")
-inputs
+inputs[0] =  arg(z)
 
-prefixes.append(f"$! z_1 - z_2 = !$")
-sols.append((z1 - z2).expand())
+prefixes.append("argument :")
+inputs[1] =  arg(z)
 
 
 z = choice([I,-I,1,-1,1+I,1-I,I-1,-1-I])
