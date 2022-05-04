d0be20b5e077a63fc2b0e244010c28719031254a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-04 12:04:25 +0200

    ddoyen1 modified Yggdrasil/Mathematics/exercises/complex_numbers/point_arg_mod_.pl

--- Mathematics/exercises/complex_numbers/point_arg_mod_.pl
+++ Mathematics/exercises/complex_numbers/point_arg_mod_.pl
@@ -42,5 +42,5 @@ board.create('axis', [[0, 0], [0, 1]], {name: 'Im', withLabel: true, label: {pos
 
 //board.create('axis',[[0,0],[1,0]],{name:'Re',withLabel:true,label:{position:'urt',offset:[-5,10]},ticks:{visible: false}});
 //board.create('axis',[[0,0],[0,1]],{name:'Im',withLabel:true,label:{position:'urt',offset:[10,0]},ticks:{visible: false}});
-var M = board.create('point',[0,0],{size:2,name:'M',color:'blue'});
+var pt = board.create('point', [0, 0], {size: 2, name: 'M', color: 'blue'});
 ==
