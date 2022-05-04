e376eed1cb456e0b8d68bc5fa10d22a4e99e4aa8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-04 16:02:54 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/argmod/argmod_/argmod_.pl

--- exercises/math/complex_numbers/argmod/argmod_/argmod_.pl
+++ exercises/math/complex_numbers/argmod/argmod_/argmod_.pl
@@ -13,6 +13,30 @@ title = "Argument d'un nombre complexe"
 
 before ==
 from sympy import arg
+
+from sympy import conjugate
+from sympy2latex import latex_lincomb
+
+n = 5
+z1 = randint(-n, n, [0]) + randint(-n, n, [0])*I
+z2 = randint(-n, n, [0]) + randint(-n, n, [0])*I
+while z1 == z2:
+    z2 = randint(-n, n, [0]) + randint(-n, n, [0])*I
+
+#z1b = conjugate(z1)
+#z2b = conjugate(z2)
+
+prefixes = []
+inputs = []
+
+inputs = [MathInput(type="expr"), MathInput(type="expr")]
+prefixes.append("module :")
+inputs
+
+prefixes.append(f"$! z_1 - z_2 = !$")
+sols.append((z1 - z2).expand())
+
+
 z = choice([I,-I,1,-1,1+I,1-I,I-1,-1-I])
 z = randint(1, 3)*z
 sol = arg(z)
