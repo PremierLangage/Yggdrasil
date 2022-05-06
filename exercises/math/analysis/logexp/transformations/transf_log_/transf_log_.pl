2b46ec24edc85bd6279ecea1e084363f917c1a5a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-06 12:32:53 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/logexp/transformations/transf_log_/transf_log_.pl

--- exercises/math/analysis/logexp/transformations/transf_log_/transf_log_.pl
+++ exercises/math/analysis/logexp/transformations/transf_log_/transf_log_.pl
@@ -5,47 +5,11 @@
 
 extends = /model/math/multimathinput.pl
 
+param.types = [0, 1, 2, 3]
+
 before ==
-
-inputs = [MathInput(type="complex", evalparam={'form': 'cartesian'}) for _ in range(3)]
-
-# formula=choice(eval(param['formulas']))
-def generate2(formula):
-    p, q = sampleint(2,5, 2)
-    if formula=="u*ln(p)":
-        u=randint(2,4)
-        sol=p**u
-        expr="%d \ln(%d)" % (u,p)
-    elif formula=="-u*ln(p)":
-        u=randint(-4,-2)
-        sol=p**u
-        expr="%d \ln(%d)" % (u,p)
-    elif formula=="ln(p)+ln(q)":
-        sol=p*q
-        expr="\ln(%d) + \ln(%d)" % (p,q)
-    elif formula=="ln(p)-ln(q)":
-        sol=p/q
-        expr=choice(["\ln( %(p)s ) - \ln( %(q)s )","-\ln( %(q)s ) + \ln( %(p)s )"]) % {"p":p,"q":q}
-    elif formula=="u*ln(p)+ln(q)":
-        u,v=sample([1,randint(2,4)],2)
-        sol=p**u*q
-        expr="%d \ln(%d)+\ln(%d)" % (u,p,q)
-    elif formula=="u*ln(p)-ln(q)":
-        s1,s2=sample(2,[-1,1])
-        u,v=sample([s1*1,s2*randint(2,4)], 2)
-        sol=p**u*q**v
-        expr=latex(u*ln(p)+v*ln(q))
-    elif formula=="u*ln(p)+vln(q)":
-        u,v=sampleint(2, 4, 2)
-        sol=p**u*q**v
-        expr="%d \ln(%d)+%d \ln(%d)" % (u,p,v,q)
-    elif formula=="u*ln(p)-vln(q)":
-        s1,s2=sample(2,[-1,1])
-        u,v=sampleint(2, 4, 2)
-        u,v=s1*u,s2*v
-        sol=p**u*q**v
-        expr="%d \ln(%d)+%d \ln(%d)" % (u,p,v,q)
-    return expr, sol
+n = len(param['types'])
+inputs = [MathInput(type="complex", evalparam={'form': 'cartesian'}) for _ in range(n)]
 
 def generate(c):
     p, q = sampleint(2, 5, 2)
