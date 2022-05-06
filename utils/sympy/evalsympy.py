6b7bf595232b81fb8a4a7dbb90443fd09bb7406d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-06 15:01:48 +0200

    ddoyen1 modified Yggdrasil/utils/sympy/evalsympy.py

--- utils/sympy/evalsympy.py
+++ utils/sympy/evalsympy.py
@@ -576,9 +576,9 @@ def eval_expr(strans, sol, checkratsimp=True, equality="", modulo=0, unauthorize
         return (-1, "NotExpr")
     if not isinstance(ans, sp.Expr):
         return (-1, "NotExpr")
-    if embedfunc is not None:
-        if not isinstance(ans, emebedfunc):
-            return (-1, "NotExpr")
+    #if embedfunc is not None:
+    #    if not isinstance(ans, emebedfunc):
+    #        return (-1, "NotExpr")
     if ans.has(sp.S.Infinity, sp.S.NegativeInfinity):
         if ans != sp.S.Infinity and ans != sp.S.NegativeInfinity:
             return (-1, "InftyOp")
