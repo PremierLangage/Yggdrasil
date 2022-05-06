bc8fc66c678ed745dd4048dd8b15246b968d3863
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-06 18:03:37 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/logexp/transformations/transf_exp_/transf_exp_.pl

--- exercises/math/analysis/logexp/transformations/transf_exp_/transf_exp_.pl
+++ exercises/math/analysis/logexp/transformations/transf_exp_/transf_exp_.pl
@@ -46,7 +46,7 @@ def eval_ans(strans, sol):
         return (-1, "NotExpr")
     if not isinstance(ans, sp.Expr):
         return (-1, "NotExpr")
-    if not isinstance(ans, (sp.exp, sp.Exp1)):
+    if not isinstance(ans, (sp.exp, sp.E)):
         return (-1, "NotExpr")
     if isinstance(ans, sp.exp) and ans.args[0].has(sp.exp):
         return (-1, "NotExpr")
