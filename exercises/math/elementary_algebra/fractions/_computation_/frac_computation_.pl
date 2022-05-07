17d2c4a67885854587abd66a06e4df03d511993d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-07 19:21:15 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/fractions/_computation_/frac_computation_.pl

--- exercises/math/elementary_algebra/fractions/_computation_/frac_computation_.pl
+++ exercises/math/elementary_algebra/fractions/_computation_/frac_computation_.pl
@@ -33,43 +33,44 @@ param.formulas % ["f+f"]
 
 
 before ==
-lstfrac=[(choice([1,3,5,7]),2),
-(choice([1,2,4,5,7,8]),3),
-(choice([1,3,5,7]),4),
-(choice([1,2,3,4,6,7,8]),5),
-(choice([1,5,7]),6)]
-cf1,cf2,cf3=sample(lstfrac, 3)
-f1='\\frac{ %d }{ %d }' % (cf1[0],cf1[1])
-f2='\\frac{ %d }{ %d }' % (cf2[0],cf2[1])
-f3='\\frac{ %d }{ %d }' % (cf3[0],cf3[1])
+def generate(i):
+    lstfrac=[(choice([1,3,5,7]),2),
+    (choice([1,2,4,5,7,8]),3),
+    (choice([1,3,5,7]),4),
+    (choice([1,2,3,4,6,7,8]),5),
+    (choice([1,5,7]),6)]
+    cf1,cf2,cf3=sample(lstfrac, 3)
+    f1='\\frac{ %d }{ %d }' % (cf1[0],cf1[1])
+    f2='\\frac{ %d }{ %d }' % (cf2[0],cf2[1])
+    f3='\\frac{ %d }{ %d }' % (cf3[0],cf3[1])
 
-e1=randint(1,3)
-e1=str(e1)
-s1=choice(["-","+"])
-s2=choice(["-","+"])
-p1,p2=sample([f1,e1],2)
-q1,q2,q3=sample([f1,f2,e1],3)
+    e1=randint(1,3)
+    e1=str(e1)
+    s1=choice(["-","+"])
+    s2=choice(["-","+"])
+    p1,p2=sample([f1,e1],2)
+    q1,q2,q3=sample([f1,f2,e1],3)
 
 
-lst_expr = ['%s %s %s' % (f1,s1,f2),
-'%s \times %s' % (f1,f2),
-'\\frac{ %s }{ %s }' % (f1,f2),
-'%s %s %s' % (p1,s1,p2),
-'%s \\times %s' % (p1,p2),
-'\\frac{ %s }{ %s }' % (p1,p2),
-r'%s %s %s %s %s' % (f1,s1,f2,s2,f3),
-r'%s %s %s %s %s' % (q1,s1,q2,s2,q3),
-choice([r"\frac{ %s }{ %s %s %s }" % (f3,f1,s1,f2), r"\frac{ %s %s %s }{ %s }" % (f1,s1,f2,f3)]),
-choice([r"\frac{ %s }{ %s %s %s }" % (f3,p1,s1,p2), r"\frac{ %s %s %s }{ %s }" % (p1,s1,p2,f3)]),
-choice([r"%s \times \left( %s %s %s \right)" % (f3,f1,s1,f2), r"\left( %s %s %s \right)\times  %s" % (f1,s1,f2,f3)]),
-choice([r"%s \times %s %s %s" % (f3,f1,s1,f2), r"%s %s %s \times %s" % (f1,s1,f2,f3)]),
-choice([r"%s \times \left(%s %s %s \right)" % (f3,p1,s1,p2), r"\left(%s %s %s \right) \times %s" % (p1,s1,p2,f3)]),
-r'%s - \left(%s %s %s \right)' % (f1,f2,s1,f3),
-choice([r"%s - \left(%s %s %s \right)" % (f3,p1,s1,p2), r" - \left(%s %s %s \right) +%s" % (p1,s1,p2,f3)])]
+    lst_expr = ['%s %s %s' % (f1,s1,f2),
+    '%s \times %s' % (f1,f2),
+    '\\frac{ %s }{ %s }' % (f1,f2),
+    '%s %s %s' % (p1,s1,p2),
+    '%s \\times %s' % (p1,p2),
+    '\\frac{ %s }{ %s }' % (p1,p2),
+    r'%s %s %s %s %s' % (f1,s1,f2,s2,f3),
+    r'%s %s %s %s %s' % (q1,s1,q2,s2,q3),
+    choice([r"\frac{ %s }{ %s %s %s }" % (f3,f1,s1,f2), r"\frac{ %s %s %s }{ %s }" % (f1,s1,f2,f3)]),
+    choice([r"\frac{ %s }{ %s %s %s }" % (f3,p1,s1,p2), r"\frac{ %s %s %s }{ %s }" % (p1,s1,p2,f3)]),
+    choice([r"%s \times \left( %s %s %s \right)" % (f3,f1,s1,f2), r"\left( %s %s %s \right)\times  %s" % (f1,s1,f2,f3)]),
+    choice([r"%s \times %s %s %s" % (f3,f1,s1,f2), r"%s %s %s \times %s" % (f1,s1,f2,f3)]),
+    choice([r"%s \times \left(%s %s %s \right)" % (f3,p1,s1,p2), r"\left(%s %s %s \right) \times %s" % (p1,s1,p2,f3)]),
+    r'%s - \left(%s %s %s \right)' % (f1,f2,s1,f3),
+    choice([r"%s - \left(%s %s %s \right)" % (f3,p1,s1,p2), r" - \left(%s %s %s \right) +%s" % (p1,s1,p2,f3)])]
 
-expr = lst_expr[3]
-
-sol=simplify(latex2sympy(expr))
+    expr = lst_expr[i]
+    sol=simplify(latex2sympy(expr))
+    return expr, sol
 ==
 
 question ==
