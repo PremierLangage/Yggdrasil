f077474e152e55c2f7ad54c6bdb336b96e3a2303
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-08 11:28:58 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/fractions/simplify_mul_/simplify_mul_.pl

--- exercises/math/elementary_algebra/fractions/simplify_mul_/simplify_mul_.pl
+++ exercises/math/elementary_algebra/fractions/simplify_mul_/simplify_mul_.pl
@@ -12,22 +12,28 @@ title = Simplifier des fractions
 param.types = [0, 0, 1, 1]
 
 before ==
+from sympy import fraction
 types = param['types']
 n = len(types)
 inputs = [MathInput(type="expr") for _ in range(n)]
 
 def generate(i):
+    denom=101
+    num=0
     if i == 0:
-        denom=101
-        num=0
         while (denom>100) or (num>100):
             a,b,c,d=sample([10,12,14,15,16,18,20,21,24,25,27,28,30,32,33,35,36,40,42,44,45,48,49,50,52,54,55,56,60,63,64,65,70,72,80,81], 4)
             sol=Rational(a*c,b*d)
             num,denom=fraction(sol)
             expr=r"\frac{ %d }{ %d } \times \frac{ %d }{ %d }" % (a,b,c,d)
     if i ==1:
-        lstf=[[3,2],[4,3],[5,4],[5,3],[5,2],[6,5],[7,6],[7,5],[7,4],[7,3],[7,2],[8,7],[8,5],[8,3],[9,8],[9,7],[9,5],[9,4],[9,2]]
-        lstc=[16,18,21,22,24,25,27,28,32,33,35]
+
+        while (denom>100) or (num>100):
+            a,b,c,d,e,f=sample([10,12,14,15,16,18,20,21,24,25,27,28,30,32,33,35,36,40,42,44,45,48,49,50,52,54,55,56,60,63,64,65,70,72,80,81], 6)
+            sol=Rational(a*c*e,b*d*f)
+            num,denom=fraction(sol)
+
+        expr=r"\frac{ %d }{ %d } \times \frac{ %d }{ %d } \times \frac{ %d }{ %d }" % (a,b,c,d,e,f)
     f=choice(lstf)
     shuffle(f)
     c=choice(lstc)
