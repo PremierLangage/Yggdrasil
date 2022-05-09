b9daae3e1d3b215ef1a694716c63a0d53223969f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-09 15:00:47 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/trigo/inequations/simple_ineq_/simple_eq_.pl

--- exercises/math/analysis/trigo/inequations/simple_ineq_/simple_eq_.pl
+++ exercises/math/analysis/trigo/inequations/simple_ineq_/simple_eq_.pl
@@ -8,7 +8,7 @@ extends = /model/math/interval.pl
 
 param.rhs = "Rational(1, 2), -Rational(1, 2)"
 
-param.interval = "(-2*pi,2*pi),(0,2*pi),(-pi,pi)"
+param.interval = "(-pi,pi),"
 
 param.interval_type = closed
 
