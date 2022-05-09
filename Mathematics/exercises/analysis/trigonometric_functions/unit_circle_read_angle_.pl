fed46e9079811f53cac824602643c9e229193d3d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-09 11:51:20 +0200

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/unit_circle_read_angle_.pl
@@ -3,6 +3,7 @@ extends = /model/math/expr.pl
 param.lstangle = [pi/4,pi/2,3*pi/4,pi,5*pi/4,3*pi/2,7*pi/4]
 
 before ==
+from jxginput import JXGViewer
 jxg = JXGViewer()
 
 lstangle=eval(param['lstangle'])
