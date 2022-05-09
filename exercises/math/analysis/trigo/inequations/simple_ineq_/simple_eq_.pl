1573a70f6533b8205356fda928beea9aca5f82fc
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-09 15:00:37 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/trigo/inequations/simple_ineq_/simple_eq_.pl

--- exercises/math/analysis/trigo/inequations/simple_ineq_/simple_eq_.pl
+++ exercises/math/analysis/trigo/inequations/simple_ineq_/simple_eq_.pl
@@ -19,7 +19,6 @@ lhs = choice([cos(x),sin(x)])
 rhs =choice(eval(param['rhs']))
 a,b=choice(eval(param['interval']))
 interv=Interval(a,b)
-sol=FiniteSet(*list(solveset(lhs-rhs,x,domain=S.Reals).intersect(interv)))
 ineq = choice([lhs >= rhs, lhs <= rhs])
 sol = solveset(ineq, x, domain=S.Reals).intersect(interv)
 ==
