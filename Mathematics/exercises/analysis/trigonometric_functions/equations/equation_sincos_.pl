490af92291c351be252940c27ecc9b1f055cf0be
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-09 14:01:59 +0200

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/equations/equation_sincos_.pl

--- Mathematics/exercises/analysis/trigonometric_functions/equations/equation_sincos_.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/equations/equation_sincos_.pl
@@ -8,7 +8,7 @@ extends = /model/math/set.pl
 
 param.rhs = [0,-1,1]
 
-param.interval = "[(-2*pi,2*pi),(0,2*pi),(-pi,pi)]"
+param.interval = "(-2*pi,2*pi),(0,2*pi),(-pi,pi)"
 
 param.interval_type = closed
 
