8210a21f8ea7d9679748982a06a04fd4f0d5f8c7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-09 14:04:47 +0200

    ddoyen1 modified Yggdrasil/Mathematics/exercises/analysis/trigonometric_functions/equations/equation_sincos_346_closed.pl

--- Mathematics/exercises/analysis/trigonometric_functions/equations/equation_sincos_346_closed.pl
+++ Mathematics/exercises/analysis/trigonometric_functions/equations/equation_sincos_346_closed.pl
@@ -2,6 +2,6 @@ extends = equation_sincos_.pl
 
 param.rhs = "Rational(1,2),-Rational(1,2),sqrt(2)/2,-sqrt(2)/2,sqrt(3)/2,-sqrt(3)/2"
 
-param.interval = "(-pi,pi)"
+param.interval = "((-pi,pi),)"
 
 param.interval_type = closed
