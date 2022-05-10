92f016d16e5092b9c36ee0c0c79eff1ae32a1b08
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 14:03:17 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/limits/sum_leading_/sum_leading_.pl

--- exercises/math/analysis/limits/sum_leading_/sum_leading_.pl
+++ exercises/math/analysis/limits/sum_leading_/sum_leading_.pl
@@ -1,7 +1,7 @@
 extends = /model/math/multimathinput.pl
 title = Calcul de limites
 
-param.terms = "ln(x), exp(x), x**randint(1, 4), sqrt(x), 1/x, exp(-x), 1/ln(x)"
+param.terms = "ln(x), exp(x), x**randint(1, 4), sqrt(x), 1/x, exp(-x), 1/ln(x), randint(-2,2,[0])"
 
 param.nbterms = [2, 2, 2, 3]
 
