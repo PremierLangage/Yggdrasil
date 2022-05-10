1f7d7a21dc48849c97a965eb3eb02a386784ed12
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 09:33:33 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/trigo/unit_circle/draw_angle_/draw_angle_.pl

--- exercises/math/analysis/trigo/unit_circle/draw_angle_/draw_angle_.pl
+++ exercises/math/analysis/trigo/unit_circle/draw_angle_/draw_angle_.pl
@@ -23,20 +23,3 @@ var A = board.create('point', [1,0],{size:1, name: 'A', color: 'black', fixed: t
 var M = board.create('glider', [1, 0.5, circle],{name:'M',color:'blue',fixed:false});
 board.create('sector', [O, A, M], {color: 'orange'});
 ==
-
-
-script_solution ==
-board.create('point',[{{xsol}}, {{ysol}}],{name:'', color:'green'});
-==
-
-
-script_init4 ==
-var grid = board.create('grid',[],{gridX:0.25,gridY:0.25});
-var Ox = board.create('axis',[[0,0],[1,0]],{ticks:{visible:false}});
-var Oy = board.create('axis',[[0,0],[0,1]],{ticks:{visible:false}});
-var circle = board.create('circle',[[0,0],[0,1]],{strokeColor:'blue',fixed:true});
-var O = board.create('point',[0,0],{size:1,name:'O',color:'black',fixed:true});
-var A = board.create('point',[1,0],{size:1,name:'A',color:'black',fixed:true});
-var M = board.create('glider',[1,0.5,circle],{size:2,name:'M',color:'red',fixed:false});
-var secOAM = board.create('sector',[O,A,M],{color:'orange'});
-==
