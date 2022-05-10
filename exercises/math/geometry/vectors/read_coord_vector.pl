3dddfba3f4f215914d748535a449116f459bb057
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 15:45:18 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/vectors/read_coord_vector.pl

--- exercises/math/geometry/vectors/read_coord_vector.pl
+++ exercises/math/geometry/vectors/read_coord_vector.pl
@@ -14,7 +14,7 @@ jxg.set_script(script, globals())
 
 attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 
-jxgscript == #|js|
+script == #|js|
 var pt1 = board.create('point', [{{xA}}, {{yA}}], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
 var pt2 = board.create('point', [{{xB}}, {{yB}}], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
 var vec = board.create('segment', [pt1, pt2], {color:'blue', lastArrow: {type: 2, size: 6}});
