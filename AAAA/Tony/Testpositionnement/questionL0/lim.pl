a080b180deaf0d31b42e0ce8bf3aca16d6d697f3
tony fevrier <tony.fevrier62@gmail.com>
2022-05-10 19:12:34 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL0/lim.pl

--- AAAA/Tony/Testpositionnement/questionL0/lim.pl
+++ AAAA/Tony/Testpositionnement/questionL0/lim.pl
@@ -3,22 +3,38 @@
 #Etat =
 #Note pour David : ce serait bien de pouvoir dupliquer un exercice dans un même dossier
 
-extends= /model/basic/checkbox_rw.pl
+#extends= /model/basic/checkbox_rw.pl
 
  
-question==
-Parmi ces limites, lesquelles valent $!+\infty!$? 
-==
+#question==
+#Parmi ces limites, lesquelles valent $!+\infty!$? 
+#==
  
 
-right== 
-$!\lim\limits_{x\rightarrow 2} \frac{1}{(x-2)^2}!$
-$!\lim\limits_{x\rightarrow +\infty} e^{-x}+\ln(x)+x-2!$ 
+#right== 
+#$!\lim\limits_{x\rightarrow 2} \frac{1}{(x-2)^2}!$
+#$!\lim\limits_{x\rightarrow +\infty} e^{-x}+\ln(x)+x-2!$ 
+#==
+
+#wrong==
+#$!\lim\limits_{x\rightarrow 0} \frac{1}{x}!$
+#$!\lim\limits_{x\rightarrow +\infty} \frac{x^2+2}{2x-x^2}!$ 
+#==
+
+extends = /model/basic/checkbox_rw.pl
+
+question ==
+Parmi ces villes, lesquelles ne sont pas des capitales ?
 ==
 
-wrong==
-$!\lim\limits_{x\rightarrow 0} \frac{1}{x}!$
-$!\lim\limits_{x\rightarrow +\infty} \frac{x^2+2}{2x-x^2}!$ 
+right ==
+Barcelone
+Milan
+==
+
+wrong ==
+Paris
+Berlin
 ==
 
 
