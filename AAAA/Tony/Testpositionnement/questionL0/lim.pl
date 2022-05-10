fb4f49bfc674f63f869a838ed0c32b76c24503ed
tony fevrier <tony.fevrier62@gmail.com>
2022-05-10 19:08:52 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL0/lim.pl

---
+++ AAAA/Tony/Testpositionnement/questionL0/lim.pl
@@ -0,0 +1,32 @@
+#Auteur  = inconnu, récupéré de la base commune, repris par Tony Février et Jeanne Parmentier
+#Date = 22/07/2021
+#Etat =
+#Note pour David : ce serait bien de pouvoir dupliquer un exercice dans un même dossier
+
+extends= /model/basic/checkbox_rw.pl
+
+
+title= Savoir reconnaitre les caractéristiques d'une ED (linéarité, ordre…) : ordre
+
+text==
+Parmi ces limites, lesquelles valent $!+\infty!$? 
+==
+ 
+
+right== 
+
+$!\lim\limits_{x\rightarrow 2} \frac{1}{(x-2)^2}!$
+$!\lim\limits_{x\rightarrow +\infty} e^{-x}+\ln(x)+x-2!$ 
+
+==
+
+wrong==
+$!\lim\limits_{x\rightarrow 0} \frac{1}{x}!$
+$!\lim\limits_{x\rightarrow +\infty} \frac{x^2+2}{2x-x^2}!$ 
+
+==
+
+
+
+
+
