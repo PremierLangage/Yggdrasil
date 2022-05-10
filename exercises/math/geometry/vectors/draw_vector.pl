35721536b323200c38afbd0280412c18a8353eee
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 15:38:03 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/vectors/draw_vector.pl

---
+++ exercises/math/geometry/vectors/draw_vector.pl
@@ -0,0 +1,19 @@
+extends = /model/jxg/vector.pl
+
+title = Tracer un vecteur
+
+before ==
+sol = sampleint(-5, 5, 2)
+==
+
+attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
+
+jxgscript == #|js|
+var pt1 = board.create('point', [-2, -2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
+var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
+var vec = board.create('segment', [pt1, pt2], {color:'blue', lastArrow: {type: 2, size: 6}});
+==
+
+question ==
+Modifier le vecteur ci-dessous pour en faire un vecteur de coordonnées $! ( {{ sol[0] }}, {{ sol[1] }} ) !$. 
+==
