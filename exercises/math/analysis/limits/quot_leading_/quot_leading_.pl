ab46d4a18f9937a2ae84039b3a3f44b0089bc39f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 14:26:10 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/limits/quot_leading_/quot_leading_.pl

--- exercises/math/analysis/limits/quot_leading_/quot_leading_.pl
+++ exercises/math/analysis/limits/quot_leading_/quot_leading_.pl
@@ -1,7 +1,7 @@
 extends = /model/math/multimathinput.pl
 title = Calcul de limites
 
-param.terms = "ln(x), exp(x), x**randint(1, 4), sqrt(x), 1/x, exp(-x), 1/ln(x), randint(-2,2,[0])"
+param.terms = "ln(x), exp(x), x**randint(1, 4), sqrt(x), exp(-x), randint(-2,2,[0])"
 
 param.nbterms = [2, 2, 2, 3]
 
