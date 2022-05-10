dc0afa79e9be89b7e39144fe9e066e2018e8e8fa
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 14:51:31 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/limits/quot_leading_/log_exp_power.pl

--- exercises/math/analysis/limits/quot_leading_/log_exp_power.pl
+++ exercises/math/analysis/limits/quot_leading_/log_exp_power.pl
@@ -1,5 +1,5 @@
 extends = quot_leading_.pl
 
-param.terms = "ln(x), exp(x), x**randint(1, 4), sqrt(x), 1/x, exp(-x), 1/ln(x), randint(-2,2,[0])"
+param.terms = "ln(x), exp(x), x**randint(1, 4), sqrt(x), exp(-x), randint(-2,2,[0])"
 
 param.nbterms = [[2,1], [2,2], [2,2], [2,3]]
