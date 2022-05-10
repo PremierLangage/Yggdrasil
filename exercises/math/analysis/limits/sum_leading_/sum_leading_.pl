ca0633d409d304dc75be5d9db883749349b37f26
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 12:02:26 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/limits/sum_leading_/sum_leading_.pl

--- exercises/math/analysis/limits/sum_leading_/sum_leading_.pl
+++ exercises/math/analysis/limits/sum_leading_/sum_leading_.pl
@@ -12,7 +12,7 @@ inputs = [MathInput() for _ in range(n)]
 prefixes = []
 
 var('x')
-inf = [ln(x), exp(x), x]
+terms = [ln(x), exp(x), x]
 f = [atan(x), x/(x+1)]
 b = [sin(x), cos(x)]
 f0 = [1/ln(x), 1/exp(x), 1/x]
@@ -20,7 +20,8 @@ f0 = [1/ln(x), 1/exp(x), 1/x]
 
 def rand_expr(i):
     f = sample(terms, 2)
-    return f[0] + f[1]
+    c = sample([-2, -1, 1, 2], 2)
+    return c[0]*f[0] + c[1]*f[1]
 
 cases = [0, 1, 3]
 shuffle(cases)
