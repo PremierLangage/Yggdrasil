c555e7287164f85596223a294fa84fa744fb2793
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 15:42:19 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/vectors/read_coord_vector.pl

--- exercises/math/geometry/vectors/read_coord_vector.pl
+++ exercises/math/geometry/vectors/read_coord_vector.pl
@@ -1,16 +1,18 @@
-extends = /model/jxg/vector.pl
+extends = /model/jxg/tuple.pl
 
 title = Lire les coordonnées d'un vecteur
 
 before ==
+xA, yA = randint(-5, 5), randint(-5, 5)
+xB, yB = randint(-5, 5), randint(-5, 5)
 sol = sampleint(-5, 5, 2)
 ==
 
 attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 
 jxgscript == #|js|
-var pt1 = board.create('point', [-2, -2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
-var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
+var pt1 = board.create('point', [{{xA}}, {{yA}}], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
+var pt2 = board.create('point', [{{xB}}, {{yB}}], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
 var vec = board.create('segment', [pt1, pt2], {color:'blue', lastArrow: {type: 2, size: 6}});
 ==
 
