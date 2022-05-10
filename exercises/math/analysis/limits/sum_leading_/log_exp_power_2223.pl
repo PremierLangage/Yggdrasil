8d11acf7b3758100e241320dce7b0ff11810cfd4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-10 14:17:42 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/limits/sum_leading_/log_exp_power_2223.pl

--- exercises/math/analysis/limits/sum_leading_/log_exp_power_2223.pl
+++ exercises/math/analysis/limits/sum_leading_/log_exp_power_2223.pl
@@ -1,5 +1,4 @@
-extends = /model/math/multimathinput.pl
-title = Calcul de limites
+extends = sum_leading.pl
 
 param.terms = "ln(x), exp(x), x**randint(1, 4), sqrt(x), 1/x, exp(-x), 1/ln(x), randint(-2,2,[0])"
 
