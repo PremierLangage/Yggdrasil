410f282cbb685ad3ae0be56d94295254b632f95f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-11 22:50:14 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/lines/line.pl

--- exercises/math/geometry/lines/line.pl
+++ exercises/math/geometry/lines/line.pl
@@ -11,7 +11,7 @@ attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 jxgscript == #|js|
 var pt1 = board.create('point', [-2, -2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
 var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
-var vec = board.create('segment', [pt1, pt2], {color:'blue', lastArrow: {type: 2, size: 6}});
+var line = board.create('line', [pt1, pt2], {color:'blue', lastArrow: {type: 2, size: 6}});
 ==
 
 question ==
