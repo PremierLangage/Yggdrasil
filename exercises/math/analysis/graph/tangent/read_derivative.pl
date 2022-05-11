f001a9d8622ecd81f7c675d046402a3fab4bb8ab
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-11 17:17:54 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/graph/tangent/read_derivative.pl

--- exercises/math/analysis/graph/tangent/read_derivative.pl
+++ exercises/math/analysis/graph/tangent/read_derivative.pl
@@ -1,7 +1,7 @@
 extends = /model/math/expr.pl
 
 script ==
-var sf = board.jc.snippet("- x**3", true, 'x', true);
+var sf = board.jc.snippet(- x**3, true, 'x', true);
 var f = board.create('functiongraph', [sf, -5, 5]);
 var g = board.create('glider', [0, 0, f], {name:'',style:8});
 var t = board.create('tangent', [g], {dash:2,strokeColor:'#aa0000'});
