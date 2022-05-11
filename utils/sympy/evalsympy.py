a97265a12c76064a63ec0b3eab73889cdfddcfb5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-11 19:27:33 +0200

    ddoyen1 modified Yggdrasil/utils/sympy/evalsympy.py

--- utils/sympy/evalsympy.py
+++ utils/sympy/evalsympy.py
@@ -828,7 +828,7 @@ def eval_numeric(strans, sol, tol=0.1, diffmeasure='AbsError'):
         ans = latex2sympy(strans)
     except:
         return (-1, "NotExpr")
-    if not isinstance(ans, sp.Float):
+    if not isinstance(ans, (sp.Float, sp.Integer)):
         return (-1, "NotExpr")
     if diffmeasure == 'AbsError':
         diff = abs(sol - ans)
