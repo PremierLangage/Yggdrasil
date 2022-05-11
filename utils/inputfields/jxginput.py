2483b6743c5080b397497e04a2490b742d13b7a8
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-11 22:35:59 +0200

    ddoyen1 modified Yggdrasil/utils/inputfields/jxginput.py

--- utils/inputfields/jxginput.py
+++ utils/inputfields/jxginput.py
@@ -171,3 +171,45 @@ class JXGVector(JXGInput):
             board.create('segment',[pt1, [{{x1}} + {{x}}, {{y1}} + {{y}}]],{color:'green', lastArrow: {type: 2, size: 6}});
             """
             self.add_script(script, {'x':self.sol[0], 'y':self.sol[1], 'x1':x1, 'y1':y1})
+
+class JXGLine(JXGInput):
+
+    def __init__(self, **kwargs):
+        self.evalparam = {}
+        super().__init__(**kwargs)
+
+    def eval(self):
+        """
+        Evaluate the input field
+        """
+        x0, y0 = self.get_point(self.pointnames[0])
+        x1, y1 = self.get_point(self.pointnames[1])
+        xsol0, ysol0 = self.sol[0]
+        xsol1, ysol1 = self.sol[1]
+        tol = self.evalparam.get('tol', 0.1)
+        if hypot(x1-x0-xsol, y1-y0-ysol) < tol and :
+            self.score = 100
+        else:
+            self.score = 0
+        return self.score
+
+    def display_feedback(self):
+        """
+        Display visual feedback.
+        """
+        if self.score == 100:
+            script = """
+            pt1.setAttribute({color: 'green'});
+            pt2.setAttribute({color: 'green'});
+            vec.setAttribute({color: 'green'});
+            """
+            self.add_script(script)
+        else:
+            x1, y1 = self.get_point(self.pointnames[0])
+            script = """
+            pt1.setAttribute({color: 'red'});
+            pt2.setAttribute({color: 'red'});
+            vec.setAttribute({color: 'red'});
+            board.create('segment',[pt1, [{{x1}} + {{x}}, {{y1}} + {{y}}]],{color:'green', lastArrow: {type: 2, size: 6}});
+            """
+            self.add_script(script, {'x':self.sol[0], 'y':self.sol[1], 'x1':x1, 'y1':y1})
