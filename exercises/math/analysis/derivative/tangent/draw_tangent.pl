7b3daf78d181a69dd88dd818c67fa3398b434a6e
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-11 23:15:45 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/derivative/tangent/draw_tangent.pl

---
+++ exercises/math/analysis/derivative/tangent/draw_tangent.pl
@@ -0,0 +1,55 @@
+extends = /model/math/numeric.pl
+
+
+jxgscript == #|js|
+var f = board.jc.snippet('{{ expr }}', true, 'x', true);
+var pt1 = board.create('point', [-2, -2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
+var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
+var line = board.create('line', [pt1, pt2], {color:'blue'});
+==
+
+before ==
+from jxginput import JXGViewer
+from math import floor, ceil
+from sympy.polys.polyfuncs import interpolate
+import numpy as np
+
+var('x')
+
+pts = choice([[(-5, 2), (-1, 3), (1, 0), (4, -3)], [(-5, -1), (-1, -2), (2, 3), (5, -2)], [(-5, -2), (-1, 2), (1, -2), (5, 1)], [(-5, 4), (-2, -4), (2, 1), (5, 1)], [(-4, -4), (-1, -2), (2, 1), (4, 3)], [(-4, -1), (-1, 2), (2, 1), (5, 4)], [(-4, 3), (-2, 2), (1, -3), (5, 2)], [(-5, -4), (-1, 3), (1, 3), (5, -2)], [(-4, -4), (-1, -3), (2, 0), (5, 4)], [(-4, 1), (-1, 3), (2, -2), (5, 1)]])
+f = interpolate(pts, x)
+expr = str(f).replace("**", "^")
+while True:
+    a = randint(-4, 4)
+    sol = diff(f, x).evalf(subs={x:a})
+    if abs(sol) > 0.3:
+        break
+
+jxg = JXGViewer()
+jxg.set_attributes(attributes)
+jxg.set_script(script, globals())
+==
+
+attributes = {"showNavigation": True, "boundingbox":[-5, 5, 5, -5]}
+
+tol = 0.15
+
+question ==
+On considère la fonction $! f !$ dont le graphe est tracé ci-desssous. En utilisant l'outil graphique qui affiche la tangente à la courbe, déterminer une valeur approchée de $! f( {{a}} ) !$ (à 15% près).
+{{ jxg|html }}
+==
+
+style.jxgcss ==
+<style>
+.jsxgraph-component {
+    width: 400px;
+    height: 400px;
+    margin: 0 auto;
+    border: 1px solid;
+}
+</style>
+==
+
+prefix ==
+$! f( {{a}} ) \approx !$
+==
