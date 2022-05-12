210138dcd3028f5ac3be36f14ea7c663adcab533
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 18:45:25 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/lines/equation/from_2pts.pl

--- exercises/math/geometry/lines/equation/from_2pts.pl
+++ exercises/math/geometry/lines/equation/from_2pts.pl
@@ -17,3 +17,31 @@ sol = Line.equation()
 question ==
 Déterminer une équation de la droite passant par $! ({{xA}}, {{yA}}) !$ et $! ({{xB}}, {{yB}}) !$.
 ==
+
+evaluator ==
+import sympy as sp
+from evalsympy import equal, is_rat_simp
+from latex2sympy import latex2sympy
+
+def eval_ans(strans, sol):
+    try:
+        p = strans.split('=')
+        if len(p) != 2:
+            return (-1, "NotEq")
+        ans = sp.Eq(latex2sympy(p[0]), latex2sympy(p[0]))
+    except:
+        return (-1, "NotEq")
+    try:
+        ans = sp.Line(ans)
+    except:
+        return (-1, "NotLineEq")
+    return (100, "Success")
+
+score, error = eval_ans(inputfield.value, sol)
+feedback = message[error]
+
+==
+
+message.NotEq = "La réponse doit être une équation."
+
+message.NotLinEq = "La réponse n'est pas une équation de droite."
