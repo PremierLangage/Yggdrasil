b6d0826147569ba2e45325dfb6b545d6e5f429f9
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 19:20:15 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/lines/equation/from_2pts.pl

--- exercises/math/geometry/lines/equation/from_2pts.pl
+++ exercises/math/geometry/lines/equation/from_2pts.pl
@@ -37,8 +37,7 @@ def eval_ans(strans, sol):
         ans = sp.Line(ans)
     except:
         return (0, "NotLineEq")
-    if not sp.Line(sp.Eq(sol,0)).equals(ans):
-        return (0, "NotEq", ans.equation(), sp.Line(sp.Eq(sol,0)).equation() )
+
     return (100, "Success")
 
 score, error, eq1, eq2 = eval_ans(input.value, sol)
