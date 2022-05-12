f006d6a835e21bcb42e5f93e6ad37286bc5faef1
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 11:07:17 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/derivative/tangent/draw_tangent.pl

--- exercises/math/analysis/derivative/tangent/draw_tangent.pl
+++ exercises/math/analysis/derivative/tangent/draw_tangent.pl
@@ -6,8 +6,8 @@ jxgscript == #|js|
 var f = board.jc.snippet('{{ expr }}', true, 'x', true);
 var c = board.create('functiongraph', [f, -5, 5]);
 
-var pt1 = board.create('point', [-2, -2], {color:'back', name: 'A', size: 0.5, withLabel: false});
-var pt2 = board.create('point', [2, 2], {color:'black', name: 'B', size: 0.5, withLabel: false});
+var pt1 = board.create('point', [-2, -2], {color:'back', name: 'A', size: 0.8, withLabel: false});
+var pt2 = board.create('point', [2, 2], {color:'black', name: 'B', size: 0.8, withLabel: false});
 var line = board.create('line', [pt1, pt2], {color:'black'});
 ==
 
