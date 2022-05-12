f81259ea66ee3be7fed0d7456178220f3111bf4b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 10:50:00 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/derivative/tangent/read_derivative.pl

--- exercises/math/analysis/derivative/tangent/read_derivative.pl
+++ exercises/math/analysis/derivative/tangent/read_derivative.pl
@@ -36,7 +36,7 @@ attributes = {"showNavigation": True, "boundingbox":[-5, 5, 5, -5]}
 tol = 0.15
 
 question ==
-On considère la fonction $! f !$ dont le graphe est tracé ci-desssous. En utilisant l'outil graphique qui affiche la tangente à la courbe, déterminer une valeur approchée de $! f( {{a}} ) !$ (à 15% près).
+On considère la fonction $! f !$ dont la courbe représentative est tracée ci-desssous. En utilisant l'outil graphique qui affiche la tangente à la courbe, déterminer une valeur approchée de $! f( {{a}} ) !$ (à 15% près).
 <br><br>
 {{ jxg|html }}
 ==
