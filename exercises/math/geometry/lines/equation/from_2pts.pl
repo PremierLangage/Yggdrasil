13655f05beda23b3f90daace4d3c834b60a7c4e0
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 19:58:46 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/lines/equation/from_2pts.pl

--- exercises/math/geometry/lines/equation/from_2pts.pl
+++ exercises/math/geometry/lines/equation/from_2pts.pl
@@ -1,7 +1,5 @@
 extends = equation_.pl
 
-title = Déterminer une équation d'une droite
-
 before ==
 from sympy import Line
 var('x')
@@ -18,35 +16,3 @@ sol = line.equation()
 question ==
 Déterminer une équation de la droite passant par $! ({{xA}}, {{yA}}) !$ et $! ({{xB}}, {{yB}}) !$.
 ==
-
-evaluator ==
-import sympy as sp
-from evalsympy import equal, is_rat_simp
-from latex2sympy import latex2sympy
-
-def eval_ans(strans, sol):
-    try:
-        p = strans.split('=')
-        if len(p) != 2:
-            return (-1, "NotEq")
-        ans = sp.Eq(latex2sympy(p[0]), latex2sympy(p[1]))
-    except:
-        return (-1, "NotEq")
-    try:
-        ans = sp.Line(ans)
-    except:
-        return (0, "NotLineEq")
-    if not sp.Line(sp.Eq(sol,0)).equals(ans):
-        return (0, str(ans.equation())+str(sp.Line(sp.Eq(sol,0)).equation()))
-    return (100, "Success")
-
-score, error = eval_ans(input.value, sol)
-feedback = message[error]
-==
-
-solution ==
-Une équation de cette droite est : $${{ sol|latex }} = 0.$$
-==
-
-message.NotEq = "La réponse doit être une équation."
-message.NotLineEq = "La réponse n'est pas une équation de droite."
