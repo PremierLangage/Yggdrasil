7af28bc450ce54bea1d00030ba3db9626b71626b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 10:37:55 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/derivative/tangent/eq_tangent_simple.pl

--- exercises/math/analysis/derivative/tangent/eq_tangent_simple.pl
+++ exercises/math/analysis/derivative/tangent/eq_tangent_simple.pl
@@ -4,8 +4,8 @@ title = Déterminer l'équation d'une tangente
 
 before ==
 var('x')
-a = choice([1, 2, Rational(1, 2), Rational(1, 3)])
-b = choice([1, 2, 3, 4])
+a = choice([-1, 1]) * choice([1, 2, Rational(1, 2), Rational(1, 3)])
+b = choice([-1, 1]) *  choice([1, 2, 3, 4])
 f = choice([a*x**3+b*x, a*x**3+b*x**2 ])
 x0 = randint(-2, 2)
 dfx0 = diff(f, x).subs(x, x0)
@@ -13,7 +13,6 @@ sol = simplify(f.subs(x, x0) + dfx0*(x-x0))
 ==
 
 question ==
-{{sol|latex }}
 Soit la fonction $! f: x \mapsto {{ f|latex }} !$. Déterminer l'équation de la tangente à la courbe représentative de $! f !$ au point d'abscisse $! {{ x0 }} !$.
 ==
 
