b21adf35eb0116a150faff92b163398f6ff2fbf5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 20:38:32 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/ode/1st_linear.pl

---
+++ exercises/math/analysis/ode/1st_linear.pl
@@ -0,0 +1,21 @@
+extends = /model/math/expr.pl
+
+title = Déterminer une solution particulière
+
+before ==
+from sympy import Function
+var('t')
+y = symbols('y', cls=Function)
+a = randint(-3, 3, [0])
+b = randint(-3, 3, [0])
+expr = y(t).diff(t) + a*y(t) + b
+==
+
+question ==
+Déterminer une solution particulière $! {{ expr|latex }} = 0 !$.
+==
+
+
+prefix ==
+$! y'(t) = !$
+==
