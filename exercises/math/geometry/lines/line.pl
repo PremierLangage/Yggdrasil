5ad124ff483d2f75837252bb6859f35e4befd587
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-12 09:43:13 +0200

    ddoyen1 modified Yggdrasil/exercises/math/geometry/lines/line.pl

--- exercises/math/geometry/lines/line.pl
+++ exercises/math/geometry/lines/line.pl
@@ -20,7 +20,7 @@ Tracer la droite d'équation $! y = {{f|latex}} !$.
 attributes = {"showNavigation": False, "boundingbox":[-6, 6, 6, -6]}
 
 jxgscript == #|js|
-var pt1 = board.create('point', [-2, -2], {color:'blue', name: 'A', size: 0.5, withLabel: false, snapToGrid: true});
-var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false, snapToGrid: true});
+var pt1 = board.create('point', [-2, -2], {color:'blue', name: 'A', size: 0.5, withLabel: false});
+var pt2 = board.create('point', [2, 2], {color:'blue', name: 'B', size: 0.5, withLabel: false});
 var line = board.create('line', [pt1, pt2], {color:'blue'});
 ==
