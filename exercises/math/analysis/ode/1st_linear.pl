77aafcfd3ecb40989b7a002fb46f1511af333210
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-14 18:40:23 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/ode/1st_linear.pl

--- exercises/math/analysis/ode/1st_linear.pl
+++ exercises/math/analysis/ode/1st_linear.pl
@@ -25,6 +25,67 @@ inputblock ==
 {{ inputs[2]|html }}
 ==
 
+evaluator ==#|py|
+
+import sympy as sp
+from sympy.calculus.util import function_range
+from latex2sympy import latex2sympy
+from evalsympy import eval_expr
+
+def eval_ans0(strans, expr):
+    try:
+        ans = latex2sympy(strans)
+    except:
+        return (-1, "NotExpr")
+    try:
+	    res = sp.checkodesol(expr, ans)
+    except:
+        return (-1, "NotExpr")
+    if not res[0]:
+	    return (0, "NotSol")
+    return (100, "Success")
+
+def eval_ans1(strans, expr):
+    try:
+        ans = latex2sympy(strans, local_dict={'e':sp.E})
+    except:
+        return (-1, "NotExpr")
+    try:
+	    res = sp.checkodesol(expr, ans)
+    except:
+        return (-1, "NotExpr")
+    if not res[0]:
+	    return (0, str(res[1]))
+    y0 = ans.subs(t, 0)
+    if function_range(y0, k, sp.S.Reals) != sp.S.Reals:
+        return (0, "NotGenSol")
+    return (100, "Success")
+
+def eval_expr(strans, sol, local_dict={'e':sp.E}):
+    try:
+        ans = latex2sympy(strans, local_dict={'e':sp.E})
+    except:
+        return (-1, "NotExpr")
+    try:
+	    res = sp.checkodesol(expr, ans)
+    except:
+        return (-1, "NotExpr")
+    if not res[0]:
+	    return (0, str(res[1]))
+    y0 = ans.subs(t, 0)
+    if function_range(y0, k, sp.S.Reals) != sp.S.Reals:
+        return (0, "NotGenSol")
+    return (100, "Success")
+
+score, error = eval_ans(input.value, expr)
+feedback = error #message[error]
+
+for input in inputs:
+    input.value = answers[input.id] # HACK
+    input.eval()
+==
+
+
 evaluator ==
 import sympy as sp
 from sympy.calculus.util import function_range
