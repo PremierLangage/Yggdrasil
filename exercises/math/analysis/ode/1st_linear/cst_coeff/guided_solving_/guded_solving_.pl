b50e2f0c04a8a0a34a6d8180724bc7746564bf93
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-15 10:06:14 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/ode/1st_linear/cst_coeff/guided_solving_/guded_solving_.pl

--- exercises/math/analysis/ode/1st_linear/cst_coeff/guided_solving_/guded_solving_.pl
+++ exercises/math/analysis/ode/1st_linear/cst_coeff/guided_solving_/guded_solving_.pl
@@ -35,13 +35,13 @@ $! g(t) = !$ {{ inputs[2]|html }}
 
 solution ==
 1. Solution particulière de cette équation.<br><br>
-$! y_{p}(t) = !$ {{ sols[0]|latex }}
+$! y_{p}(t) =  {{ sols[0]|latex }} !$
 <br><br>
 2. Solution générale.<br><br>
-$! y(t) = !$ {{ sols[1]|latex }}
+$! y(t) = {{ sols[1]|latex }}!$
 <br><br>
 3. Unique solution $g$ de cette équation telle que $! g(0) = {{y0|latex}} !$.<br><br>
-$! g(t) = !$ {{ sols[1]|latex }}
+$! g(t) = {{ sols[1]|latex }} !$
 ==
 
 evaluator ==#|py|
