7b53131179d309608016a1a637c207ae9363360b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-15 16:35:42 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/functions/variation/demo.pl

--- exercises/math/analysis/functions/variation/demo.pl
+++ exercises/math/analysis/functions/variation/demo.pl
@@ -16,5 +16,30 @@ $$f : x \mapsto {{ f|latex }}.$$
 ==
 
 inputblock ==
-
+<table border="1">
+<tbody>
+<tr>
+<td> {{inputs[0]|html}} </td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td><svg width="100" height="100">
+  <line x1="0" y1="0" x2="100" y2="100" style="stroke:rgb(255,0,0);stroke-width:2" />
+ </svg></td>
+<td>&nbsp;</td>
+<td><svg width="100" height="100">
+  <line x1="0" y1="100" x2="100" y2="0" style="stroke:rgb(255,0,0);stroke-width:2" />
+ </svg></td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
 ==
