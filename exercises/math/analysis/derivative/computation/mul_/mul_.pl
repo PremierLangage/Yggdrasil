4af8e7f81a97df3d46de3cf013e40021b8422621
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-18 11:18:57 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/derivative/computation/mul_/mul_.pl

--- exercises/math/analysis/derivative/computation/mul_/mul_.pl
+++ exercises/math/analysis/derivative/computation/mul_/mul_.pl
@@ -72,8 +72,7 @@ def generate_fog(i, j):
 ii, jj, kk, ll = param['types']
 j1, j2 = choice(ii), choice(jj)
 k1, k2 = choice(kk), choice(ll)
-with evaluate(False):
-    f = generate_fog(j1, j2) * generate_fog(k1, k2)
+f = generate_fog(j1, j2) * generate_fog(k1, k2)
 
 sol = diff(f, x).factor()
 ==
