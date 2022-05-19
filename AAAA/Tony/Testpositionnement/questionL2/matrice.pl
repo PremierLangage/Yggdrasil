f6129e84316417bb6ecc7e08f456ee75ba7ed901
tony fevrier <tony.fevrier62@gmail.com>
2022-05-19 22:50:10 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/matrice.pl

--- AAAA/Tony/Testpositionnement/questionL2/matrice.pl
+++ AAAA/Tony/Testpositionnement/questionL2/matrice.pl
@@ -2,39 +2,34 @@ extends= /model/basic/checkbox_rw.pl
 
  
 question==
-Quelles fonctions sont solutions de l'équation suivantes $y''-5y'+6y=0$?
+L'application $§\begin{array}{ll}
+\varphi : &\R_2[X]\longrightarrow \R_2[X] \\
+   &P \mapsto P+P'
+\end{array}§$ a pour matrice dans la base canonique : 
 ==
  
 nbitems=4
-minright = 2
-maxright = 2
+minright = 1
+maxright = 1
 
 right==
-$!e^{2x}!$
-$!e^{2x}-e^{3x}!$
+$!\begin{pmatrix}
+    1&1&0\\0&1&2\\0&0&1
+    \end{pmatrix}!$ 
 ==
 
 wrong==
-$!5xe^{2x}-e^{2x}!$
-$!10e^{-2x}+e^{-3x}!$
+$!\begin{pmatrix}
+    1&1&0\\0&1&0\\0&2&1
+    \end{pmatrix!$
+$!\begin{pmatrix}
+    2&2&1\\0&2&1\\0&0&2
+    \end{pmatrix}!$
+$!\begin{pmatrix}
+    -1&1&1\\1&2&1\\4&5&6
+    \end{pmatrix}!$
 ==
 
 scoring = RightMinusWrong
 
-L'application $ \begin{array}{ll}
-\varphi : &\R_2[X]\longrightarrow \R_2[X] \\
-   &P \mapsto P+P'
-\end{array}$ a pour matrice dans la base canonique : 
-  \begin{reponses}
-    \bonne{$\begin{pmatrix}
-    1&1&0\\0&1&2\\0&0&1
-    \end{pmatrix}$}
-    \mauvaise{$\begin{pmatrix}
-    1&1&0\\0&1&0\\0&2&1
-    \end{pmatrix}$}
-    \mauvaise{$\begin{pmatrix}
-    2&2&1\\0&2&1\\0&0&2
-    \end{pmatrix}$}
-    \mauvaise{$\begin{pmatrix}
-    -1&1&1\\1&2&1\\4&5&6
-    \end{pmatrix}$}
+ 
