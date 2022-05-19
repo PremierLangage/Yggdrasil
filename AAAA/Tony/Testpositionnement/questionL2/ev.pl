5f4d5c69b4d9039bc3f6fac32877ec63729be071
tony fevrier <tony.fevrier62@gmail.com>
2022-05-19 23:17:55 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/ev.pl

--- AAAA/Tony/Testpositionnement/questionL2/ev.pl
+++ AAAA/Tony/Testpositionnement/questionL2/ev.pl
@@ -17,7 +17,7 @@ $!\{y\in C^2(\mathbb{R})|y''+2y'-y=0\}!$
 
 wrong==
 $!\mathbb{R}_2[X]!$
-$!\{(x,y,z)\in\R^3|x+y=0,z=0\}!$
+$!\{(x,y,z)\in\mathbb{R}^3|x+y=0,z=0\}!$
 ==
 
 scoring = RightMinusWrong
