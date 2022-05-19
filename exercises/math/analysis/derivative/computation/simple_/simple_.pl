d8b2114ac9437e6632ed7abb0217351e4c7125b3
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-19 18:06:11 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/derivative/computation/simple_/simple_.pl

--- exercises/math/analysis/derivative/computation/simple_/simple_.pl
+++ exercises/math/analysis/derivative/computation/simple_/simple_.pl
@@ -29,7 +29,8 @@ def generate_fog(i, j):
 
     poly1 = choice(choice(lst_poly1))
 
-    lst_f=[x**randint(1, 3),
+    lst_f=[randint(-5, 5, [0])
+    x**randint(1, 3),
     x**randint(4, 8),
     x**(-n),
     x**r,
