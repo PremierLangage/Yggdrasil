990befd33f5d7986f9510a9108883343b2cf9561
tony fevrier <tony.fevrier62@gmail.com>
2022-05-19 11:19:09 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL0/systemelin.pl

--- AAAA/Tony/Testpositionnement/questionL0/systemelin.pl
+++ AAAA/Tony/Testpositionnement/questionL0/systemelin.pl
@@ -11,8 +11,6 @@ minright = 2
 maxright = 2
 
 right==
-$!\begin{System}  2x + y = 6 \\  3x + y = 7\end{System}!$
-#$!\begin{cases}x+y  &=2 \\ x-3y &=4\end{cases}!$
 $!\left \{\begin{array}{rcl}x+y&=&1 \\ x+2y&=&2\end{array}\right.!$
 #$!\left \{\begin{array}{rcl}x+y-z&=&0 \\ y+z&=&0\\ 2z&=&0\end{array}\right.!$
 
