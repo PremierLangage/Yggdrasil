5b627fea79f8701f34f9ed23150bb271bc7a3b0d
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-23 18:22:08 +0200

    ddoyen1 modified Yggdrasil/exercises/math/analysis/derivative/computation/simple_compos_/simple_.pl

--- exercises/math/analysis/derivative/computation/simple_compos_/simple_.pl
+++ exercises/math/analysis/derivative/computation/simple_compos_/simple_.pl
@@ -123,7 +123,6 @@ for i in range(n):
             par[j]= choice(par[j])
     f = generate_f(par)
     inputs[i].sol = diff(f, x)
-    prefixes.append(rf"$! {latex(f)} !$")
     expr.append(latex(f))
 
 ==
