135ff9881c04daa3e757d3a6971f298e2ed46ba7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-24 17:33:58 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/absolute_value/equation/absdeg1_cst.pl

--- exercises/math/elementary_algebra/absolute_value/equation/absdeg1_cst.pl
+++ exercises/math/elementary_algebra/absolute_value/equation/absdeg1_cst.pl
@@ -6,15 +6,15 @@ wobracket = True
 
 before ==
 from sympy import solveset, Eq, S
+var('x')
 a = randint(-6, 6, [0,1,-1])
 b = randint(-6, 6, [0,1,-1])
-d = randint(1, 5)
-var('x')
-lhs = Abs(a*x+b)
+rhs = randint(1, 5)
+lhs = Abs(a*x + b)
 sol = solveset(Eq(lhs,d),x,domain=S.Reals)
 ==
 
 question ==
-Déterminer la ou les solutions de l'équation $$ {{lhs|latex}} = {{d}}. $$
+Déterminer la ou les solutions de l'équation $$ {{ lhs|latex }} = {{ rhs|latex }}. $$
 Séparer les différentes solutions par une virgule.
 ==
