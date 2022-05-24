daf93f0eddd70f3b03fd059ed9dccd8b31feb7d4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-24 18:15:17 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/absolute_value/abs_simplification_diff_number.pl

--- exercises/math/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
+++ exercises/math/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
@@ -14,7 +14,7 @@ for i in range(3):
     b = choice([randint(1,floor(a)),randint(ceiling(a),ceiling(a)+2)])
     a, b = sample([a,b], 2)
     inputs[i].sol = Abs(a-b)
-    prefixes.append(rf"$! |{latex(a)} - {latex(b)}| !$")
+    prefixes.append(rf"$! |{latex(a)} - {latex(b)}| = !$")
 ==
 
 question ==
