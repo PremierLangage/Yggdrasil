f914445e08089744c17f4c410f72214425e82955
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-24 18:14:14 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/absolute_value/abs_simplification_diff_number.pl

--- exercises/math/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
+++ exercises/math/elementary_algebra/absolute_value/abs_simplification_diff_number.pl
@@ -9,7 +9,7 @@ inputs = [MathInput(type="expr") for _ in range(n)]
 lst = [choice([2*pi,pi]),choice([2*sqrt(2),sqrt(2)]),choice([sqrt(3),2*sqrt(3)])]
 shuffle(lst)
 items = []
-for i in range(len(lst)):
+for i in range(3):
     a = lst[i]
     b = choice([randint(1,floor(a)),randint(ceiling(a),ceiling(a)+2)])
     a, b = sample([a,b], 2)
