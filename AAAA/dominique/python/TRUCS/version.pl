dd6e3d1b8ca180b0cf39a5be4d0d1b68eaa657fb
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-05-25 14:48:58 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/python/TRUCS/version.pl

---
+++ AAAA/dominique/python/TRUCS/version.pl
@@ -0,0 +1,18 @@
+
+
+
+
+title= Lire la doc 
+
+text==
+
+Le python que vous utilisez est d'une certaine version.
+
+Ecrivez le code qui met dans la variable VERSION le numéro de version du python qui s'exécute.
+==
+
+pltest==
+>>> import sys
+>>> sys.version() == VERSION # Verification 
+true
+==
