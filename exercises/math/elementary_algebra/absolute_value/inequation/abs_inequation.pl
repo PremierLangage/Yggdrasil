9837d3cb0a0b9ce58fcb7bcc5886e283cce0d6f1
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-25 09:08:46 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/absolute_value/inequation/abs_inequation.pl

---
+++ exercises/math/elementary_algebra/absolute_value/inequation/abs_inequation.pl
@@ -0,0 +1,23 @@
+extends = /Mathematics/template/mathexpr.pl
+
+title = Inéquation avec valeur absolue
+
+lang = fr
+
+before ==
+keyboards_JSON['virtualKeyboards']="elementary"
+input1.config = keyboards_JSON
+
+a,b=list_randint(2,-6,6,[0,1,-1])
+d=randint(1,5)
+var('x')
+expr=Abs(a*x+b)
+
+ineq=rchoice([expr>=d,expr>d,expr<=d,expr<d])
+ineq_tex = latex(ineq)
+sol=solveset(ineq,x,domain=S.Reals)
+==
+
+question ==
+Déterminer l'ensemble des réels $! x !$ tels que $$ {{ ineq|latex }}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
+==
