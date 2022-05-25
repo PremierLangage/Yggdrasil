7f057f8e81a3c860d29a0fe7d9803b5ebaeb5f8b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-25 09:09:29 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/absolute_value/inequation/abs_inequation.pl

--- exercises/math/elementary_algebra/absolute_value/inequation/abs_inequation.pl
+++ exercises/math/elementary_algebra/absolute_value/inequation/abs_inequation.pl
@@ -8,11 +8,10 @@ before ==
 a,b=list_randint(2,-6,6,[0,1,-1])
 d=randint(1,5)
 var('x')
-expr=Abs(a*x+b)
+expr = Abs(a*x+b)
 
-ineq=rchoice([expr>=d,expr>d,expr<=d,expr<d])
-ineq_tex = latex(ineq)
-sol=solveset(ineq,x,domain=S.Reals)
+ineq = choice([expr>=d, expr>d, expr<=d, expr<d])
+sol = solveset(ineq, x, domain=S.Reals)
 ==
 
 question ==
