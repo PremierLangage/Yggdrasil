2f0063d6d2c232f0bed5804ea945bf41c80f9dce
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-05-25 14:42:41 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/PLATON/tojson/tojson.pl

--- AAAA/dominique/PLATON/tojson/tojson.pl
+++ AAAA/dominique/PLATON/tojson/tojson.pl
@@ -14,7 +14,7 @@ Initialisez les variables jour, mois, annee avec la date du jour.
 
 ==
 
-@ /AAAA/dominique/PLATON/tojson/before.py [builder.py]
+#@ /AAAA/dominique/PLATON/tojson/before.py [builder.py]
 @ before.py [grader.py]
 before==
 import sys
