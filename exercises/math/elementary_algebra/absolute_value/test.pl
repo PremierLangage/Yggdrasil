39832df31cb0efef0b46683101116748ea2bb014
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-25 12:25:39 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/absolute_value/test.pl

--- exercises/math/elementary_algebra/absolute_value/test.pl
+++ exercises/math/elementary_algebra/absolute_value/test.pl
@@ -27,21 +27,6 @@ for i in range(n):
 
 ==
 
-before ==
-from sympy import floor, ceiling
-n = 3
-inputs = [MathInput(type="expr") for _ in range(n)]
-lst = [choice([2*pi,pi]),choice([2*sqrt(2),sqrt(2)]),choice([sqrt(3),2*sqrt(3)])]
-shuffle(lst)
-prefixes = []
-for i in range(3):
-    a = lst[i]
-    b = choice([randint(1,floor(a)),randint(ceiling(a),ceiling(a)+2)])
-    a, b = sample([a,b], 2)
-    inputs[i].sol = Abs(a-b)
-    prefixes.append(rf"$! |{latex(a)} - {latex(b)}| = !$")
-==
-
 question ==
 Réécrire les expressions suivantes sans utiliser de valeur absolue.
 ==
