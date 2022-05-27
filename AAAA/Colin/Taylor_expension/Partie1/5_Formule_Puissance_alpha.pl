a6f26390191212a7e829670ab076fbad7f78fd86
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 12:05:57 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/5_Formule_Puissance_alpha.pl

--- AAAA/Colin/Taylor_expension/Partie1/5_Formule_Puissance_alpha.pl
+++ AAAA/Colin/Taylor_expension/Partie1/5_Formule_Puissance_alpha.pl
@@ -28,11 +28,11 @@ $! {{ f|latex }} = !$
 ==
 
 solution ==
-$$ {{ f|latex }} = {{ sol|latex }} + o({{ x }}^{{ n }})  \quad [x \rightarrow 0]!$$
+$$ {{ f|latex }} = {{ sol|latex }} + o({{ x }}^{{ n }})  \quad [x \rightarrow 0] $$
 
 En effet, il s'agit d'appliquer la formule de référence :
 
-$$ (1+x)^{ {{a|latex}} } = 1+ {{a|latex}} x+ \frac{ {{a|latex}} ({{a|latex}} - 1)}{2!}x^2 +\frac{ {{a|latex}} ( {{a|latex}} -1) ({{a|latex}} - 2)}{3!}x^3 + o(x^3) . $$
+$$ (1+x)^{ a } = 1+ a x+ \frac{ a(a-1) }{2!}x^2 +\frac{ a(a-1)(a-2)}{3!}x^3 + o(x^3) \quad  \text{avec }  a = {{a|latex}}.  $$
 ==
 
 latexsettings.order = 'ilex'
