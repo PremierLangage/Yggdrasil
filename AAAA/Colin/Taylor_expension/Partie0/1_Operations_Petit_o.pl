9dc4dfe5e21eeb4cdd2769f1f9b940974de78f0e
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 11:35:14 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/1_Operations_Petit_o.pl

--- AAAA/Colin/Taylor_expension/Partie0/1_Operations_Petit_o.pl
+++ AAAA/Colin/Taylor_expension/Partie0/1_Operations_Petit_o.pl
@@ -26,7 +26,7 @@ $! o(x^{{n|latex}}) + o(x^{{m|latex}}) = o( !$
 ==
 
 solution ==
-$! o({{ sol|latex }})  !$.
+$! o(x^{{n|latex}}) + o(x^{{m|latex}}) = o({{ sol|latex }})  !$.\\
 
 
 En effet, il faut appliquer ici la règle suivante : Si $! n \leq m!$ alors $! o(x^n) + o(x^m) = o(x^n) !$.
