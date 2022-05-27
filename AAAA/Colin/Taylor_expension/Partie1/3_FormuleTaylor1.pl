3ba1955e182a4cd229d7ea54221d0cc5fb00ee6e
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 11:03:30 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl

--- AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl
+++ AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl
@@ -1,5 +1,5 @@
 # Auteur : Colin Petitjean
-# Sujet : développements limités - Manipuler la formule de Taylor-Young 1
+# Sujet : Développements Limités - Manipuler la formule de Taylor-Young 1
 
 title = DL - Formule de Taylor-Young 1
 
