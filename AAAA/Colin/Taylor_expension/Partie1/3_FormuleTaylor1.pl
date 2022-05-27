7b48bbd25ea721ae562fbcbddcdc8562b933bf7f
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 12:19:27 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl

--- AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl
+++ AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl
@@ -19,7 +19,7 @@ sol = a0 + a1 * x + a2 * x**2/2 + a3 * x**3/6 + a4 * x**4/24
 question ==
 Soit $! f !$ une fonction de classe $!\mathcal C^{{n|latex}} !$ sur $!\mathbb R!$ telle que 
 $$ f(0) = {{a0|latex}}, \quad f^{'}(0) = {{a1|latex}}, \quad f^{''}(0) = {{a2|latex}}, \quad f^{(3)}(0) = {{a1|latex}}, \quad f^{(4)}(0) = {{a4|latex}}.$$ 
-A l'aide de la formule de Taylor-Young, donner le développement limité de $! f !$ en 0 et à l'ordre $! {{ n }} !$.
+A l'aide de la formule de Taylor-Young, donner le développement limité de $! f !$ en 0 et à l'ordre $! {{ n|latex }} !$.
 ==
 
 embed ==
