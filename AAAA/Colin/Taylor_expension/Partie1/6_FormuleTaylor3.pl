1491ab3781913ae915e468dcdeb66e0ec84ff7f5
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 12:22:54 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/6_FormuleTaylor3.pl

--- AAAA/Colin/Taylor_expension/Partie1/6_FormuleTaylor3.pl
+++ AAAA/Colin/Taylor_expension/Partie1/6_FormuleTaylor3.pl
@@ -29,7 +29,7 @@ A l'aide de la formule de Taylor-Young, donner le développement limité de $! f
 ==
 
 embed ==
-# + o( (x- {{a |latex }})^{{ n|latex }}) \quad [x \rightarrow {{a|latex}}]
+# + o( ({{X |latex }})^{{ n|latex }}) \quad [x \rightarrow {{a|latex}}]
 ==
 
 prefix ==
@@ -37,7 +37,7 @@ $! f(x) = !$
 ==
 
 solution ==
-$! f(x) = {{ sol|latex }} + o((x - {{a|latex}})^{{ n|latex }})  \quad [x \rightarrow {{a | latex}}]!$.
+$! f(x) = {{ sol|latex }} + o(({{X|latex}})^{{ n|latex }})  \quad [x \rightarrow {{a | latex}}]!$.
 ==
 
 latexsettings.order = 'ilex'
