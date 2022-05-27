247cbab8c73fe50d67022b4b24bd7fdf681d2e10
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 10:12:54 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl

--- AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl
+++ AAAA/Colin/Taylor_expension/Partie1/3_FormuleTaylor1.pl
@@ -2,13 +2,18 @@ extends = /model/math/expr.pl
 
 before ==
 var('x')
-n = randint(3, 4)
-for i 
-sol = series(f, x, 0, n+1).removeO()
+n = 4
+a0 = randint(-10, 10)
+a1 = randint(-10, 10)
+a2 = randint(-10, 10)
+a3 = randint(-10, 10)
+a4 = randint(-10, 10)
+sol = a0 + a1 * x + a2 * x**2/2 + a3 * x**3/6 + a4 * x**4/24
 ==
 
 question ==
-Ecrire le développement limité de $! {{ f|latex }} !$ autour de $! 0 !$ à l'ordre $! {{ n }} !$.
+Soit $! f !$ une fonction de classe $!\mathcal C^n !$ sur $!\mathbb R!$ telle que $!f^{(i)}(0) = ai!$, pour $! i \in \llbracket 0, n\rrbracket!$. 
+A l'aide de la formule de Taylor, donner le développement limité de $f$ en 0 et à l'ordre $! {{ n }} !$.
 ==
 
 embed ==
@@ -16,7 +21,7 @@ embed ==
 ==
 
 prefix ==
-$! {{ f|latex }} = !$
+$! f(x) = !$
 ==
 
 solution ==
