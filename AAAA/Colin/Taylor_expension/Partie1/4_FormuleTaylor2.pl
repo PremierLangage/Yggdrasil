d5060b17af04b625e3bdbd24b3d86fc998fb4438
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 11:27:27 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/4_FormuleTaylor2.pl

--- AAAA/Colin/Taylor_expension/Partie1/4_FormuleTaylor2.pl
+++ AAAA/Colin/Taylor_expension/Partie1/4_FormuleTaylor2.pl
@@ -17,13 +17,13 @@ for i in range(n+1) :
     L.append(b)
 
 f = L[0] + L[1]*x + L[2]*x**2 + L[3]*x**3 + L[4]*x**4
-sol = 0
+sol = L[m]*factorial(m)
 ==
 
 question ==
 Soit $! f !$ une fonction de classe $!\mathcal C^{{n|latex}} !$ sur $!\mathbb R!$ ayant pour développement limité en 0 et à l'ordre 4 :
 $$ f(x) = {{f|latex}} + o(x^4).$$
-Retrouver à partir de la formule de Taylor-Youngla vameur de  $! f^{ {{m|latex}}}  !$.
+Retrouver à partir de la formule de Taylor-Youngla valeur de  $! f^{ {{m|latex}}}(0)  !$.
 ==
 
 embed ==
