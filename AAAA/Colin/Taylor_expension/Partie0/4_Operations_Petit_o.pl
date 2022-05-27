dabd7946175522875d8bf5f8d0a0ee9c6aa57cd3
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 09:47:59 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/4_Operations_Petit_o.pl

---
+++ AAAA/Colin/Taylor_expension/Partie0/4_Operations_Petit_o.pl
@@ -0,0 +1,35 @@
+
+# Auteur : Colin Petitjean
+# Sujet : développements limités - Manipuler les petits o
+
+extends = /model/math/expr.pl
+
+title = DL - Opérations sur les petits 0.
+
+before ==
+var('x')
+n = randint(2, 6)
+m = randint(1,20)
+sol = x**n
+==
+
+question ==
+Simplifier l'expression ci-dessous (pour x qui tend vers $! 0 !$) en l'écrivant sous la forme
+ $! o(x^n) !$ pour un $! n !$ bien choisi.
+==
+
+embed ==
+# ) \quad [x \rightarrow 0]
+==
+
+prefix ==
+$! {{m|latex}} \times o(x^{{n|latex}}) = o( !$
+==
+
+solution ==
+$! o({{ sol|latex }})  !$.
+==
+
+latexsettings.order = 'ilex'
+
+
