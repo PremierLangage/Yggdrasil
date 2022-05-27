413ab50ffcd18ec64e93b3c56f6479a49c95fd35
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 08:49:26 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/1_Operations_Petit_o.pl

--- AAAA/Colin/Taylor_expension/Partie1/1_Operations_Petit_o.pl
+++ AAAA/Colin/Taylor_expension/Partie1/1_Operations_Petit_o.pl
@@ -2,3 +2,26 @@
 # Sujet : développements limités - Manipuler les petits o
 
 title = DL - Opérations sur les petits 0.
+
+extends = limit_.pl
+
+before ==
+ninput=3
+lstinput=[input1,input2,input3]
+
+
+var('x')
+latexlim=[]
+sol=[]
+
+lstd=list_randint_norep(3,2,7) 
+lsta=list_randitem_norep(3,[oo,oo,-oo,-oo])
+
+for (d,a) in zip(lstd,lsta):
+    f=rand_int_poly(d,randint(2,3),5,x)
+    lim=Limit(f, x, a)
+    latexlim.append(latex(lim))
+    sol.append(lim.doit())
+==
+
+
