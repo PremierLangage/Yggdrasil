c8ddba356304b3c35d5dd015b077a713ed567b85
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-05-27 11:31:16 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie1/4_FormuleTaylor2.pl

--- AAAA/Colin/Taylor_expension/Partie1/4_FormuleTaylor2.pl
+++ AAAA/Colin/Taylor_expension/Partie1/4_FormuleTaylor2.pl
@@ -23,7 +23,7 @@ sol = L[m]*factorial(m)
 question ==
 Soit $! f !$ une fonction de classe $!\mathcal C^{{n|latex}} !$ sur $!\mathbb R!$ ayant pour développement limité en 0 et à l'ordre $! {{ n }} !$ :
 $$ f(x) = {{f|latex}} + o(x^4).$$
-Retrouver à partir de la formule de Taylor-Youngla valeur de  $! f^{ ({{m|latex}}) }(0)  !$.
+Retrouver à partir de la formule de Taylor-Young la valeur de  $! f^{ ({{m|latex}}) }(0)  !$.
 ==
 
 
