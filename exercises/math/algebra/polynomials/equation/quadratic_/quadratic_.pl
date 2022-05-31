f02783f9ed338f64ef3f618701c7227655379673
David DOYEN <david.doyen@univ-eiffel.fr>
2022-05-31 16:02:37 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/polynomials/equation/quadratic_/quadratic_.pl

--- exercises/math/algebra/polynomials/equation/quadratic_/quadratic_.pl
+++ exercises/math/algebra/polynomials/equation/quadratic_/quadratic_.pl
@@ -20,12 +20,11 @@ from sympy import factor, content, solveset, S
 var('x')
 
 q, r = sample([1, 2, 3, 4, 5], 2)
-q=choice([-1,1])*q
-r=choice([-1,1])*r
+q = choice([-1,1])*q
+r =choice([-1,1])*r
 [b, a], [d, c] = sample([[1,2],[3,2],[5,2],[1,3],[2,3],[4,3],[5,3],[1,4],[3,4],[5,4],[1,5],[2,5],[3,5],[4,5]], k=2)
 a=choice([-1,1])*a
 c=choice([-1,1])*c
-
 
 if param['roots'] == "int":
     P = (x-q)*(x-r)
