fb28dd2e4fd07f219c83acde2c7d72e4b74b707b
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-06-03 10:06:06 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/template/monpltestdetest.pl

---
+++ ComputerScience/python/AP1-2122/template/monpltestdetest.pl
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+extends = pltest2022.pl 
+
+title= Test de pltest2022 
+text==
+
+le test utilise le code suivant :
+
+    def f(n):
+        return 2*n
+    
+    k = f(6)
+    def g(p):
+        return f(p-1)+f(p+1)
+    
+    s='chaine'
+
