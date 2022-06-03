a7592c7a592027997e423895b708ee2f39f8413e
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-03 14:58:15 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/test.pl

--- AAAA/Gaudet/test.pl
+++ AAAA/Gaudet/test.pl
@@ -8,8 +8,5 @@ nombres = [randint(0, 100) for i in range(4)]
 sol = min(nombres)
 indsol = nombres.index(sol)
 items = nombres
-==
-
-question ==
-Quel est le plus petit nombre?
+question = "Plus petit nombre?"
 ==
