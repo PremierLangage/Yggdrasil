b7a0a03a25150c4da2eb7859b55864bc7ccba2ce
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-03 15:06:00 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/test.pl

--- AAAA/Gaudet/test.pl
+++ AAAA/Gaudet/test.pl
@@ -3,7 +3,8 @@ extends = /model/basic/radio.pl
 title = "Test"
 
 before ==
-from random import randint
+from random import randint, seed
+seed(seed)
 nombres = [randint(0, 100) for i in range(4)]
 sol = min(nombres)
 indsol = nombres.index(sol)
