e7add58c19077c4fce360f88cf19c6866d4bbeda
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-03 14:56:58 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/test.pl

--- AAAA/Gaudet/test.pl
+++ AAAA/Gaudet/test.pl
@@ -6,12 +6,10 @@ before ==
 from random import randint
 nombres = [randint(0, 100) for i in range(4)]
 sol = min(nombres)
+indsol = nombres.index(sol)
+items = nombres
 ==
 
 question ==
 Quel est le plus petit nombre?
 ==
-
-items = {{ nombres }}
-
-indsol = 0
