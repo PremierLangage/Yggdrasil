10d1f7b8f6dc5fb1dffe43e5b040b83f33969c9b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-06 15:01:01 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/power/sign_power.pl

---
+++ exercises/math/elementary_algebra/power/sign_power.pl
@@ -0,0 +1,36 @@
+extends = /model/math/checkbox.pl
+
+title = Signe d'une puissance
+
+
+before ==
+def latex_number(s,x,y):
+    tex=""
+    if x<0:
+        tex="%s ( %d )^{ %d }" % (s,x,y)
+    else:
+        tex="%s %d ^{ %d }" % (s,x,y)
+    return tex
+
+sol = sample([1, 2, 3, 4], randint(1, 4))
+items = []
+for i in range(len(sol)):
+    with evaluate(False):
+        a=randint(2,20)
+        b=randint(5,15)
+        p,q=2*b,2*b+1
+        r=randitem([p,q])
+        nn=randitem([("",-a,q),("-",-a,p),("-",a,r)])
+        np=randitem([("",-a,p),("-",-a,q)])
+        if i in sol:
+            s=latex_number(*nn)
+        else:
+            s=latex_number(*np)
+    items.append("$$ "+s+" $$")
+==
+
+question ==
+Sélectionner les nombres négatifs dans la liste ci-dessous.
+==
+
+
