81fd61b3de6fdc224eb644ca2fdec0a9dc6b5c23
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-06 12:32:32 +0200

    ddoyen1 modified Yggdrasil/exercises/math/elementary_algebra/absolute_value/dist_interval.pl

--- exercises/math/elementary_algebra/absolute_value/dist_interval.pl
+++ exercises/math/elementary_algebra/absolute_value/dist_interval.pl
@@ -1,8 +1,6 @@
 extends = /model/math/interval.pl
 
-title = Inéquation avec valeur absolue
-
-lang = fr
+title = Intervalle et distance
 
 before ==
 from sympy import solveset, S
@@ -16,5 +14,5 @@ sol = solveset(ineq, x, domain=S.Reals)
 ==
 
 question ==
-Déterminer l'ensemble des réels $! x !$ tels que $$ {{ ineq|latex }}. $$ Ecrire cet ensemble sous la forme d'un intervalle ou d'une réunion d'intervalles.
+Ecrire sous la forme d'un intervalle l'ensemble des réels $! x !$ tels que $$ {{ ineq|latex }}. $$
 ==
