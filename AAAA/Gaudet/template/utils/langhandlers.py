b02a2d36e9fcd2b49e95e967dd6033268faaf872
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-07 17:08:00 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/utils/langhandlers.py

---
+++ AAAA/Gaudet/template/utils/langhandlers.py
@@ -0,0 +1,29 @@
+import subprocess
+
+class CHandler():
+    def __init__(self, source_code, func_name=None):
+        self.exec_name = '_test'
+        self.exec_cmd = ['./' + self.exec_name]
+        self.src = source_code
+    
+    def compile(self):
+        compiler = 'gcc'
+        c_flags = ['-Wall', '-ansi']
+        l_flags = ['-lm']
+
+        # Compilation directement à partir de l'entrée standard
+        cmd = [compiler, '-o', self.exec_name, '-xc', '-'] + c_flags + l_flags
+        proc = subprocess.run(cmd, input=self.src, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+        return proc.returncode == 0, proc.stdout
+
+class PythonHandler():
+    def __init__(self, source_code):
+        self.exec_name = '_test.py'
+        self.exec_cmd = ['python', self.exec_name]
+        self.src = source_code
+    
+    def compile(self):
+        with open(self.exec_name, 'w') as output:
+            output.write(self.src)
+        return True, ''
