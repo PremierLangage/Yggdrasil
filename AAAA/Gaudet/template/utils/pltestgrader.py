884bb0f8d65bf0d059810991e3423182da729460
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-07 17:00:45 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/utils/pltestgrader.py

--- AAAA/Gaudet/template/utils/pltestgrader.py
+++ AAAA/Gaudet/template/utils/pltestgrader.py
@@ -47,12 +47,12 @@ if __name__ == "__main__":
         pltest = dic['pltest']
         tester = CodingGamesTestRunner(student,pltest)
         testname = dic['testname'] if 'testname' in dic else "Groupe de test 1"
-        a, b = tester.runpltest(testname)
+        a, b = tester.runtests(testname)
     elif "pltest0" in dic:
         pltest = dic['pltest0']
         tester = CodingGamesTestRunner(student,pltest)
         testname = dic['testname0'] if 'testname0' in dic else "Groupe de test 0"
-        a, b = tester.runpltest(testname)
+        a, b = tester.runtests(testname)
     else:
         a, b= True, ""
 
