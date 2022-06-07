0a2e521a1908a2d497f6f698b1ac68245ee282c4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-07 09:41:49 +0200

    ddoyen1 modified Yggdrasil/utils/sympy/sympy2latex.py

--- utils/sympy/sympy2latex.py
+++ utils/sympy/sympy2latex.py
@@ -104,6 +104,58 @@ class CustomLatexPrinter(LatexPrinter):
             return r"\left%s%s, %s\right%s" % \
                     (left, self._print(i.start), self._print(i.end), right)
 
+    def _print_Pow(self, expr):
+        # Treat x**Rational(1,n) as special case
+        if expr.exp.is_Rational and abs(expr.exp.p) == 1 and expr.exp.q != 1 \
+                and self._settings['root_notation']:
+            base = self._print(expr.base)
+            expq = expr.exp.q
+
+            if expq == 2:
+                tex = r"\sqrt{%s}" % base
+            elif self._settings['itex']:
+                tex = r"\root{%d}{%s}" % (expq, base)
+            else:
+                tex = r"\sqrt[%d]{%s}" % (expq, base)
+
+            if expr.exp.is_negative:
+                #return r"\frac{1}{%s}" % tex
+                return tex
+            else:
+                return tex
+                
+        elif self._settings['fold_frac_powers'] \
+            and expr.exp.is_Rational \
+                and expr.exp.q != 1:
+            base = self.parenthesize(expr.base, PRECEDENCE['Pow'])
+            p, q = expr.exp.p, expr.exp.q
+            # issue #12886: add parentheses for superscripts raised to powers
+            if expr.base.is_Symbol:
+                base = self.parenthesize_super(base)
+            if expr.base.is_Function:
+                return self._print(expr.base, exp="%s/%s" % (p, q))
+            return r"%s^{%s/%s}" % (base, p, q)
+        elif expr.exp.is_Rational and expr.exp.is_negative and \
+                expr.base.is_commutative:
+            # special case for 1^(-x), issue 9216
+            if expr.base == 1:
+                return r"%s^{%s}" % (expr.base, expr.exp)
+            # special case for (1/x)^(-y) and (-1/-x)^(-y), issue 20252
+            if expr.base.is_Rational and \
+                    expr.base.p*expr.base.q == abs(expr.base.q):
+                if expr.exp == -1:
+                    return r"\frac{1}{\frac{%s}{%s}}" % (expr.base.p, expr.base.q)
+                else:
+                    return r"\frac{1}{(\frac{%s}{%s})^{%s}}" % (expr.base.p, expr.base.q, abs(expr.exp))
+            # things like 1/x
+            return self._print_Mul(expr)
+        else:
+            if expr.base.is_Function:
+                return self._print(expr.base, exp=self._print(expr.exp))
+            else:
+                tex = r"%s^{%s}"
+                return self._helper_print_standard_power(expr, tex)
+
     def _print_ImaginaryUnit(self, expr):
         return self._settings['imaginary_unit_latex']
 
