a0ab676251cbc154926ec03cec7b69bf3f1de839
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-08 10:42:51 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/utils/cgtestgrader.py

--- AAAA/Gaudet/template/utils/cgtestgrader.py
+++ AAAA/Gaudet/template/utils/cgtestgrader.py
@@ -6,17 +6,6 @@ from sandboxio import output, get_context, get_answers
 from cgtest import CodingGamesTestRunner
 import langhandlers
 
-missing_evaluator_stderr = """\
-The key 'evaluator' was not found in the context.
-When using this grader, the PL must declare a script inside a key 'evaluator'. This script have
-access to every variable declared in the PL and its 'before' script.
-It should declare a variable 'grade' which should contain a tuple (int, feedback) where int is the grade between [0, 100]."""
-missing_grade_stderr = """\
-'evaluator' did not declare the variable 'grade'. 
-The script have access to every variable declared in the PL and its 'before' script.
-It should declare a variable 'grade' which should contain a tuple (int, feedback) where int is the grade between [0, 100]."""
-
-
 if __name__ == "__main__":
     if len(sys.argv) < 5:
         msg = ("Sandbox did not call grader properly:\n"
