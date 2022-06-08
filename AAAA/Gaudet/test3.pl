9e1ca8fd8fe77d4db0d80ee43986cf7da5d775ad
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-08 16:54:57 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/test3.pl

--- AAAA/Gaudet/test3.pl
+++ AAAA/Gaudet/test3.pl
@@ -11,10 +11,10 @@ la sortie standard
 ==
 
 cgtest==
-    >>> test('1 2') # Somme simple
-    '3'
-    >>> test('-6 4') # Somme avec négatif
-    '-2'
-    >>> from random import randint; a = randint(0, 100); test('2 ' + str(a)) == str(2 + a) # Test aléatoire
-    True
-    ==
+>>> test('1 2') # Somme simple
+'3'
+>>> test('-6 4') # Somme avec négatif
+'-2'
+>>> from random import randint; a = randint(0, 100); test('2 ' + str(a)) == str(2 + a) # Test aléatoire
+True
+==
