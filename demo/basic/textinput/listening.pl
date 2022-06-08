b2e394240c77dffa259b50c102c53a18d35c35fb
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-08 23:30:12 +0200

    ddoyen1 modified Yggdrasil/demo/basic/textinput/listening.pl

--- demo/basic/textinput/listening.pl
+++ demo/basic/textinput/listening.pl
@@ -4,7 +4,6 @@ audiofile =$ /demo/media/english_sentence.mp3
 
 question ==
 <audio id="player" src="{{ audiofile }}"></audio> 
-<button onclick="player.play()" style="font-size:24px">Play <i class="fa fa-play"></i></button>
 <button onclick="player.play()" class="btn btn-sm btn-info fa fa-play-circle"></button> Transcrire la phrase.
 ==
 
