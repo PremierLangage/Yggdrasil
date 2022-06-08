3c11759e8910b78146edd3063251e6cc0709b999
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-08 14:02:44 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/test2.pl

--- AAAA/Gaudet/test2.pl
+++ AAAA/Gaudet/test2.pl
@@ -7,14 +7,15 @@ extends= template/cgtemplate.pl
 
 
 text==
-Ecrire un programme qui prend une liste d'entiers séparés par des retours à la ligne et qui affiche le max
+Ecrire un programme qui prend une liste d'entiers séparés par des retours à la ligne et qui affiche le max.
+Le premier nombre envoyé correspondant à la taille de la liste
 ==
 
 before==
 cgtest= f"""
->>> test('1\n2\n3\n4\n') # Test 1
+>>> test('4\n1\n2\n3\n4\n') # Test 1
 '4'
->>> test('45\n2\n987\n-4\n') # Test 2
+>>> test('4\n45\n2\n987\n-4\n') # Test 2
 '987'
 """
 ==
