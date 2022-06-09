a882c05250b8dfee46cddda4bc75854dd50c7993
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-09 15:18:57 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/test3.pl

--- AAAA/Gaudet/test3.pl
+++ AAAA/Gaudet/test3.pl
@@ -6,8 +6,31 @@ title=Test
 tag=function
 
 text==
-Ecrire un programme qui prend en entrée deux entiers séparés par un retour à la ligne et qui écrit leur somme dans
-la sortie standard
+Ecrire un programme qui prend une grille 2x2 de nombres et qui renvoie la ligne ou colonne avec le total le plus élevé
+
+Format d'entrée : les deux premiers numéros sont le nombre de lignes puis de colonnes, le contenu de la grille
+est ensuite énuméré un par un, de gauche à droite, de haut en bas
+
+Exemple :
+3
+3
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+Correspond à la grille :
+1 2 3
+4 5 6
+7 8 9
+
+Format de sortie : somme max trouvée
+
 ==
 
 cgtest==
