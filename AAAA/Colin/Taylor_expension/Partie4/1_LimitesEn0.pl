b1c427af33413e2a1f4e0a1e5a1ce9a915342ef1
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-09 15:26:55 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie4/1_LimitesEn0.pl

--- AAAA/Colin/Taylor_expension/Partie4/1_LimitesEn0.pl
+++ AAAA/Colin/Taylor_expension/Partie4/1_LimitesEn0.pl
@@ -10,37 +10,24 @@ before ==
 from sympy import series
 var('x')
 
-n = 3
-a=choice([-2,-1,1,2])
-b=choice([-2,-1,1,2])
-p=choice([1,2,3])
-q=choice([1,2,3])
+f = choice([2x^2/(exp(x)-exp(-x)), 
+(ln(1+x)-x)/(x^2), 
+ln(1+sin(x))/ln(1+x) ])
 
-y= x**p
-z= x**q
-
-f = choice([1/(1+y), exp(y), cos(y), sin(y), ln(1+y), atan(y), acos(y), asin(y)])
-g = choice([1/(1+z), exp(z), cos(z), sin(z), ln(1+z), atan(z), acos(z), asin(z)])
-
-h=a*f+b*g
-
-sol = series(h, x, 0, n+1).removeO()
+sol = limit(f, x, 0)
 ==
 
 question ==
-Ecrire le développement limité de $! {{ h|latex }} !$ autour de $! 0 !$ à l'ordre $! {{ n|latex }} !$.
+Déterminer la limite ci-dessous :
 ==
 
-embed ==
-# + o(x^{{ n|latex }}) \quad [x \rightarrow 0]
-==
 
 prefix ==
-$! {{ h|latex }} = !$
+$! \lim\limits_{x \to 0} {{ f|latex }} = !$
 ==
 
 solution ==
-$! {{ h|latex }} = {{ sol|latex }} + o(x^{{ n|latex }})  \quad [x \rightarrow 0]!$.
+$! \lim\limits_{x \to 0} {{ f|latex }} =  {{ sol|latex }} !$
 ==
 
 latexsettings.order = 'ilex'
