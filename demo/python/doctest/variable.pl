a034a0335a3561eeb38181c66852082686308723
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-09 10:05:58 +0200

    ddoyen1 modified Yggdrasil/demo/python/doctest/variable.pl

---
+++ demo/python/doctest/variable.pl
@@ -0,0 +1,16 @@
+extends=/ComputerScience/python/template/pltest.pl
+
+titl e= Initialisation d'une variable
+
+
+text ==
+Initialisez la variable `X` avec la chaîne `toto`.
+==
+
+# comme ca cela se passera bien
+pltest ==
+>>> X==X
+True
+>>> "Toto"==X
+True
+==
