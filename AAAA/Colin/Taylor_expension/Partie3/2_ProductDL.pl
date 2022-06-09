bcbb89bea1572bc6bb7a5355c80570d798f5e943
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-09 14:13:07 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie3/2_ProductDL.pl

---
+++ AAAA/Colin/Taylor_expension/Partie3/2_ProductDL.pl
@@ -0,0 +1,46 @@
+# Auteur : Colin Petitjean
+# Sujet : Produit de Développements Limités
+# Maj : 9/06/2022
+
+title = Produit de développements limités
+
+extends = /model/math/expr.pl
+
+before ==
+from sympy import series
+var('x')
+
+n = 3
+a=choice([-2,-1,1,2])
+b=choice([1,2,3])
+c=choice([-2,-1,1,2])
+d=choice([1,2,3])
+
+y=a*x**b
+z=c*x**d
+
+f = choice([1/(1+y), exp(y), cos(y), sin(y), ln(1+y), atan(y), acos(y), asin(y)])
+g = choice([1/(1+z), exp(z), cos(z), sin(z), ln(1+z), atan(z), acos(z), asin(z)])
+
+h=f+g
+
+sol = series(h, x, 0, n+1).removeO()
+==
+
+question ==
+Ecrire le développement limité de $! {{ h|latex }} !$ autour de $! 0 !$ à l'ordre $! {{ n|latex }} !$.
+==
+
+embed ==
+# + o(x^{{ n|latex }}) \quad [x \rightarrow 0]
+==
+
+prefix ==
+$! {{ h|latex }} = !$
+==
+
+solution ==
+$! {{ h|latex }} = {{ sol|latex }} + o(x^{{ n|latex }})  \quad [x \rightarrow 0]!$.
+==
+
+latexsettings.order = 'ilex'
