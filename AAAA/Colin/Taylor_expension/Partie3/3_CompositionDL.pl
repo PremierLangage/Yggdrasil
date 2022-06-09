7b54907ea498b7c772daa544dede16b903f13b10
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-09 14:38:14 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie3/3_CompositionDL.pl

--- AAAA/Colin/Taylor_expension/Partie3/3_CompositionDL.pl
+++ AAAA/Colin/Taylor_expension/Partie3/3_CompositionDL.pl
@@ -12,8 +12,8 @@ var('x')
 
 n = 3
 
-g = choice([exp(x)-1, cos(x)-1, sin(x), ln(1+x), \ln(1-x)])
-f = choice([1/(1+g), 1/(1-g), ln(1+g), \ln(1-g), cos(g), sin(g), ln(1+g), \ln(1-g)])
+g = choice([exp(x)-1, cos(x)-1, sin(x), ln(1+x), ln(1-x)])
+f = choice([1/(1+g), 1/(1-g), ln(1+g), ln(1-g), cos(g), sin(g), ln(1+g), ln(1-g)])
 
 sol = series(f, x, 0, n+1).removeO()
 ==
