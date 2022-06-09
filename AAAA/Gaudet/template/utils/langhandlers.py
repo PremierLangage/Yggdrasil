a6cfaff089b27d4a0522c16bac5fe36a746d01e9
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-09 11:56:42 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/utils/langhandlers.py

--- AAAA/Gaudet/template/utils/langhandlers.py
+++ AAAA/Gaudet/template/utils/langhandlers.py
@@ -58,7 +58,7 @@ available_languages = {
 }
 
 def get_language_handler(language, source_code):
-    """Returns an instance of the handler for the language and source code given as argument"""
+    """Returns an instance of the handler for the language and source code given as arguments"""
     if language not in available_languages:
         raise LanguageNotImplemented
     return available_languages[language](source_code)
