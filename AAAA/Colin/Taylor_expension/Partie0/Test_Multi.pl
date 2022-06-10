b376775405b4de6105aba44df215548e53affbfa
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 12:48:47 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl

--- AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
+++ AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
@@ -3,11 +3,11 @@ extends = /model/math/multimathinput.pl
 title = Nombres Complexes
 
 before == #|python|
-n = 2
+e = 2
 inputs = [MathInput(type="expr") for _ in range(n)]
 prefixes = []
 
-for i in range(n):
+for i in range(e):
     if i==0 :
         var('x')
         n = randint(1, 10)
@@ -21,11 +21,17 @@ for i in range(n):
         inputs[i].sol = s
         inputs[i].set_embed(r"#) \quad [x \rightarrow 0]")
     else :
-        a=2
-        b=3
-        ss = latex(a * b)
-        prefixes.append(f"$! {ss} = !$")
-        inputs[i].sol = a*b
+        var('x')
+        n = randint(1, 10)
+        m=n+1
+        Y1 = x**n
+        Y11=latex(Y1)
+        Y2 = x**m
+        Y22=latex(Y2)
+        ss = x**n
+        prefixes.append(f"$! o({Y11}) - o({Y11}) = o( !$")
+        inputs[i].sol = ss
+        inputs[i].set_embed(r"#) \quad [x \rightarrow 0]")
 ==
 
 question == 
@@ -37,6 +43,11 @@ solution ==
 $$ o({{X1|latex}}) + o({{X2|latex}}) = o({{ s|latex }})  .$$
 
 En effet, il faut appliquer ici la règle suivante : Si $! n \leq m!$ alors $! o(x^n) + o(x^m) = o(x^n) !$.
+$$ o({{Y1|latex}}) - o({{Y1|latex}}) = o({{ ss|latex }})  .$$
+
+Il faut appliquer ici la règle suivante : $! o(x^n) - o(x^n) = o(x^n) !$.
+En effet, la différence de deux petits "o" d'une même puissance n'est bien souvent pas égale à 0. Par exemple :
+$$2{{Y2|latex}} = o({{Y1|latex}}), \quad {{Y2|latex}} = o({{Y1|latex}}), \quad \text{et } \quad 2{{Y2|latex}} - {{Y2|latex}} = {{Y2|latex}} \neq 0.$$
 ==
 
 latexsettings.order = 'ilex'
