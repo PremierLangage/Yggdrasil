da5a9f67d33824f22b622f66f2bdafa895f463d1
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 19:25:32 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie4/4_DA.pl

--- AAAA/Colin/Taylor_expension/Partie4/4_DA.pl
+++ AAAA/Colin/Taylor_expension/Partie4/4_DA.pl
@@ -10,7 +10,8 @@ before ==
 from sympy import series
 var('x')
 n=2
-f = choice([cos(x), sin(x)])
+f = choice([exp(1/x), ln(1+1/x) , ln(1- 1/x), 1/(1+1/x) , 1/(1-1/x), 1/(1+1/x**2), 1/(1-1/x**2), sqrt(1+1/x), 1/sqrt(1+1/x), sqrt(1-1/x), 1/sqrt(1-1/x)])
+a=choice([oo,-oo])
 sol = series(f, x, a, n+1).removeO()
 ==
 
