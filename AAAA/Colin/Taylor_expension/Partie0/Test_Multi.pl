50d621e3370226c0024d60cc581fb6dfddf1393f
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 11:49:42 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl

--- AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
+++ AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
@@ -9,11 +9,17 @@ prefixes = []
 
 for i in range(n):
     if i==1 :
-    a=1
-    b=2
-    s = latex(a * b)
-    prefixes.append(f"$! {s} = !$")
-    inputs[i].sol = a*b
+        a=1
+        b=2
+        s = latex(a * b)
+        prefixes.append(f"$! {s} = !$")
+        inputs[i].sol = a*b
+    else
+        a=2
+        b=3
+        s = latex(a * b)
+        prefixes.append(f"$! {s} = !$")
+        inputs[i].sol = a*b
 ==
 
 question == 
