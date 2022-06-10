aa952f1208df697b2cc6c58ae81a9cd9364ce327
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 11:18:05 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/5_Operations_Petit_o.pl

--- AAAA/Colin/Taylor_expension/Partie0/5_Operations_Petit_o.pl
+++ AAAA/Colin/Taylor_expension/Partie0/5_Operations_Petit_o.pl
@@ -22,7 +22,7 @@ sol = x**min(n+m,l)
 
 question ==
 Simplifier l'expression ci-dessous (pour x qui tend vers $! 0 !$) en l'écrivant sous la forme
- $! o(x^n) !$ pour un $! n !$ bien choisi.
+ $! o(x^n) !$ avec $! n \in \mathbb N !$ le plus grand possible.
 ==
 
 embed ==
