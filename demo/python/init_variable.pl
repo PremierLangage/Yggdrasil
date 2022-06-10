fd47990f123019da10e616e8641a4ea8f48853b7
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-10 18:16:51 +0200

    ddoyen1 modified Yggdrasil/demo/python/init_variable.pl

--- demo/python/init_variable.pl
+++ demo/python/init_variable.pl
@@ -3,9 +3,10 @@ extends = /ComputerScience/python/AP1-1920/templates/generic/generic.pl
 title = Initialisation variable
 
 text ==
-Créer une variable `X` contenant la chaîne `toto`.<br><br>
+Créer une variable `X` contenant la chaîne `toto`.
 ==
 
 grader==#|python|
+begin_test_group("Tests")
 run(values={'X': 'toto'})
 ==
