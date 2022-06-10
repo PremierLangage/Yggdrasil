d4153744d982b9ab5ebe53ab6499428acd38374a
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-10 19:42:30 +0200

    ddoyen1 modified Yggdrasil/demo/math/tuple/linsys.pl

--- demo/math/tuple/linsys.pl
+++ demo/math/tuple/linsys.pl
@@ -3,15 +3,15 @@ extends = /model/math/tuple.pl
 title = Système linéaire
 
 before ==
-from randsympy import randint_matrix, randint_matrix_inv
+from randsympy import randmat, randmat_inv
 from sympy import linsolve
 from sympy2latex import latex_linsys
 
 n = 2
 lstvar = ",".join(["x", "y", "z", "t"][:n])
 
-A = randint_matrix_inv(n, 10, [1])
-B = randint_matrix(n, 1, 5, [0])
+A = randmat_inv(n, 10, [1])
+B = randmat(n, 1, 5, [0])
 
 sol = list(linsolve((A, B)))[0]
 
