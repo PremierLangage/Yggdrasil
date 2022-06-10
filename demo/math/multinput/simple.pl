862cc5f3fbd7760b796864b20920362e45fdbdac
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-10 10:30:17 +0200

    ddoyen1 modified Yggdrasil/demo/math/multinput/simple.pl

---
+++ demo/math/multinput/simple.pl
@@ -0,0 +1,17 @@
+extends = /model/math/multimathinput.pl
+
+title = Nombres Complexes
+
+before == #|python|
+n = 3
+inputs = [MathInput(type="expr") for _ in range(n)]
+prefixes = []
+for i in range(n):
+        prefixes.append(f"Question {i}")
+    inputs[i].sol = 0
+    inputs[I].embed = " \sqrt{x} + # + 3"    
+==
+
+question == 
+Calculer les produits de nombres complexes suivants (sous forme algébrique).
+==
