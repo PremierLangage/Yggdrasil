005bf246ce2f02ae80df040c10a5d143f2c6f390
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-10 18:15:30 +0200

    ddoyen1 modified Yggdrasil/demo/python/filtre_pairs.pl

--- demo/python/filtre_pairs.pl
+++ demo/python/filtre_pairs.pl
@@ -21,8 +21,6 @@ import random as rd
 def ref(lst):
     return [num for num in lst if num % 2 == 0]     
 
-run(title='Évaluation du code', output='')
-
 begin_test_group("Tests de base")
 run('filtre_pairs([2, 21, 5, 7, 4, 4, 13, 0])', title = 'Test 1', result = [2, 4, 4, 0])
 run('filtre_pairs([3, 13, 7])', title = 'Test 2', result = [])
