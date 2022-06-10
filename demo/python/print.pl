d13e3596e611fc1c4b67c876a244ab8d53333663
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-10 18:11:57 +0200

    ddoyen1 modified Yggdrasil/demo/python/print.pl

--- demo/python/print.pl
+++ demo/python/print.pl
@@ -3,7 +3,7 @@ extends = /ComputerScience/python/AP1-1920/templates/generic/generic.pl
 title = Affichage
 
 text ==
-Afficher "Hello!" sur une première ligne et "Goodbye!" sur une seconde ligne.<br>
+Afficher "Hello!" sur une première ligne et "Goodbye!" sur une seconde ligne.<br><br>
 ==
 
 grader==#|python|
