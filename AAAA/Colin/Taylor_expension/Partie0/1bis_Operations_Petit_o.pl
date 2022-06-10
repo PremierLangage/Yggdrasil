8b22851bcb35e36996d2600c4bc9d5cece7d7477
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 12:20:42 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/1bis_Operations_Petit_o.pl

--- AAAA/Colin/Taylor_expension/Partie0/1bis_Operations_Petit_o.pl
+++ AAAA/Colin/Taylor_expension/Partie0/1bis_Operations_Petit_o.pl
@@ -33,7 +33,7 @@ $$ o({{X1|latex}}) - o({{X1|latex}}) = o({{ sol|latex }})  .$$
 
 Il faut appliquer ici la règle suivante : $! o(x^n) - o(x^n) = o(x^n) !$.
 En effet, la différence de deux petits "o" d'une même puissance n'est bien souvent pas égale à 0. Par exemple :
-$$2{{X2|latex}} = o({{X1|latex}}), \quad {{X2|latex}} = o({{X1|latex}}), \quad \text{et } \quad 2{{X2|latex}} - {{X2|latex}} = {{X2|latex}}\limits_{\neq 0} = o({{X1|latex}}).$$
+$$2{{X2|latex}} = o({{X1|latex}}), \quad {{X2|latex}} = o({{X1|latex}}), \quad \text{et } \quad 2{{X2|latex}} - {{X2|latex}} = {{X2|latex}} \neq 0.$$
 ==
 
 latexsettings.order = 'ilex'
