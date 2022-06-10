899d961ddcc5adc47a71098bbb424d4cd635b723
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 12:38:43 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl

--- AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
+++ AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
@@ -16,7 +16,7 @@ for i in range(n):
         X11 = latex(X1)
         X2 = x**m
         X22 = latex(X2)
-        prefixes.append(f"$! {X11} \cdot {X22}  !$")
+        prefixes.append(f"$! o({X11}) + o({X22})  !$")
         inputs[i].sol = X1 * X2
     else :
         a=2
