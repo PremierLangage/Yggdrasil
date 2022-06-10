e3bcf907b0fea72beeba008b0aa20132161d9da2
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 12:34:26 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl

--- AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
+++ AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
@@ -14,7 +14,8 @@ for i in range(n):
         m = randint(1,10)
         X1 = x**n
         X2 = x**m
-        prefixes.append($! {{X1}} * {{X2}} !$)
+        s=latex(X1 * X2)
+        prefixes.append(f"$! {s} !$")
         inputs[i].sol = X1 * X2
     else :
         a=2
