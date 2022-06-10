d65ac06a5a9606d4ecd639ec4313e5351994ecd4
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 16:04:23 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/Multi_Mix.pl

--- AAAA/Colin/Taylor_expension/Partie0/Multi_Mix.pl
+++ AAAA/Colin/Taylor_expension/Partie0/Multi_Mix.pl
@@ -7,7 +7,7 @@ extends = /model/math/multimathinput.pl
 title = Opérations sur les petits "o" : Mix.
 
 before == #|python|
-e = 3
+e = 2
 inputs = [MathInput(type="expr") for _ in range(e)]
 prefixes = []
 
@@ -34,17 +34,6 @@ for i in range(e):
 question == 
 Simplifier les expressions ci-dessous (pour $!x!$ qui tend vers $! 0 !$) en les écrivant sous la forme
  $! o(x^n) !$ avec $! n \in \mathbb N !$ le plus grand possible.
-==
-
-solution ==
-$$ o({{X1|latex}}) + o({{X2|latex}}) = o({{ s|latex }})  .$$
-
-En effet, il faut appliquer ici la règle suivante : Si $! n \leq m!$ alors $! o(x^n) + o(x^m) = o(x^n) !$.
-$$ o({{Y1|latex}}) - o({{Y1|latex}}) = o({{ ss|latex }})  .$$
-
-Il faut appliquer ici la règle suivante : $! o(x^n) - o(x^n) = o(x^n) !$.
-En effet, la différence de deux petits "o" d'une même puissance n'est bien souvent pas égale à 0. Par exemple :
-$$2{{Y2|latex}} = o({{Y1|latex}}), \quad {{Y2|latex}} = o({{Y1|latex}}), \quad \text{et } \quad 2{{Y2|latex}} - {{Y2|latex}} = {{Y2|latex}} \neq 0.$$
 ==
 
 solution ==
