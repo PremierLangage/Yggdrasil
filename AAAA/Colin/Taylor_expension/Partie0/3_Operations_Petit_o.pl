acdfb7ed5420a41e065a76ba37cb08712e86467d
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 11:24:07 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/3_Operations_Petit_o.pl

--- AAAA/Colin/Taylor_expension/Partie0/3_Operations_Petit_o.pl
+++ AAAA/Colin/Taylor_expension/Partie0/3_Operations_Petit_o.pl
@@ -14,7 +14,7 @@ sol = x**(n+m)
 ==
 
 question ==
-Simplifier l'expression ci-dessous (pour x qui tend vers $! 0 !$) en l'écrivant sous la forme
+Simplifier l'expression ci-dessous (pour $!x!$ qui tend vers $! 0 !$) en l'écrivant sous la forme
  $! o(x^n) !$ avec $! n \in \mathbb N !$ le plus grand possible.
 ==
 
