4dbbf86c7df1f136974bcfe740eaa65b9a0d7fec
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 11:48:26 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl

---
+++ AAAA/Colin/Taylor_expension/Partie0/Test_Multi.pl
@@ -0,0 +1,22 @@
+extends = /model/math/multimathinput.pl
+
+title = Nombres Complexes
+
+before == #|python|
+n = 2
+inputs = [MathInput(type="expr") for _ in range(n)]
+prefixes = []
+
+for i in range(n):
+    if i=1 :
+    a=1
+    b=2
+    s = latex(a * b)
+    prefixes.append(f"$! {s} = !$")
+    inputs[i].sol = a*b
+==
+
+question == 
+Simplifier l'expression ci-dessous (pour $!x!$ qui tend vers $! 0 !$) en l'écrivant sous la forme
+ $! o(x^n) !$ avec $! n \in \mathbb N !$ le plus grand possible.
+==
