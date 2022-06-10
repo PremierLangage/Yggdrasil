8dee0cd8600dccc0b11adc86440476c422e49564
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-10 16:06:52 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie0/Multi_Mix.pl

--- AAAA/Colin/Taylor_expension/Partie0/Multi_Mix.pl
+++ AAAA/Colin/Taylor_expension/Partie0/Multi_Mix.pl
@@ -7,7 +7,7 @@ extends = /model/math/multimathinput.pl
 title = Opérations sur les petits "o" : Mix.
 
 before == #|python|
-e = 2
+e = 3
 inputs = [MathInput(type="expr") for _ in range(e)]
 prefixes = []
 
@@ -20,9 +20,9 @@ for i in range(e):
     S=n+m
     a=randint(2,10)
     b=randint(-10,-2)
-    l=randint(11,8)
-    k=randint(2,7)
-    s = x**min(n+m,l)
+    l=randint(2,8)
+    k=randint(2,8)
+    s = x**min(n+m,l,k)
     xn = latex(Xn)
     xm = latex(Xm)
     prefixes.append(f"$! {a} o({xn}) \cdot {xm} + x^{k} {b} o(x^{l})= o( !$")
@@ -34,6 +34,17 @@ for i in range(e):
 question == 
 Simplifier les expressions ci-dessous (pour $!x!$ qui tend vers $! 0 !$) en les écrivant sous la forme
  $! o(x^n) !$ avec $! n \in \mathbb N !$ le plus grand possible.
+==
+
+solution ==
+$$ o({{X1|latex}}) + o({{X2|latex}}) = o({{ s|latex }})  .$$
+
+En effet, il faut appliquer ici la règle suivante : Si $! n \leq m!$ alors $! o(x^n) + o(x^m) = o(x^n) !$.
+$$ o({{Y1|latex}}) - o({{Y1|latex}}) = o({{ ss|latex }})  .$$
+
+Il faut appliquer ici la règle suivante : $! o(x^n) - o(x^n) = o(x^n) !$.
+En effet, la différence de deux petits "o" d'une même puissance n'est bien souvent pas égale à 0. Par exemple :
+$$2{{Y2|latex}} = o({{Y1|latex}}), \quad {{Y2|latex}} = o({{Y1|latex}}), \quad \text{et } \quad 2{{Y2|latex}} - {{Y2|latex}} = {{Y2|latex}} \neq 0.$$
 ==
 
 solution ==
