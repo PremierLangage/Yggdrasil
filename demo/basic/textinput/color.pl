bb487e7bf1e3fcb899978a62518fc04bd9b1f7ac
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-13 16:59:16 +0200

    ddoyen1 modified Yggdrasil/demo/basic/textinput/color.pl

--- demo/basic/textinput/color.pl
+++ demo/basic/textinput/color.pl
@@ -1,14 +1,13 @@
 extends = /model/basic/textinput.pl
 
 before ==
-color = choice(["blue", "red", "green", "yellow"])
-sol = color
+sol = choice(["blue", "red", "green", "yellow"])
 ==
 
 question ==
 Ecrire le nom de la couleur en anglais.
 
 <svg width="50" height="50">
-<rect width="50" height="500" style="fill:{{ color }}" />
+<rect width="50" height="500" style="fill:{{ sol }}" />
 </svg> 
 ==
