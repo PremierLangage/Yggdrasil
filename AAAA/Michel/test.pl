e9be3fcf4272ed72248b46b8e357e1914bbc8f14
www-data <www-data@pl.u-pem.fr>
2022-06-14 17:38:48 +0200

    demo09 modified Yggdrasil/AAAA/Michel/test.pl

---
+++ AAAA/Michel/test.pl
@@ -0,0 +1,17 @@
+extends = /model/math/expr.pl
+
+title = Dérivation
+
+before ==
+from sympy import diff
+var('x')
+g = choice([sin(x), cos(x), exp(x)])
+h = choice([x, x**2])
+f = g * h
+sol = diff(f, x)
+==
+
+question ==
+Calculer la dérivée de la fonction 
+$$ f : x \mapsto {{ f|latex }} .$$
+==
