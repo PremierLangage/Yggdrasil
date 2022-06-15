5028d33b49bbbe9bf2b54f63272ad25a8e75d4d7
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-15 15:17:07 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractivegrader.py

--- AAAA/Gaudet/template/cginteractivegrader.py
+++ AAAA/Gaudet/template/cginteractivegrader.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
     # Compilation
     success, compile_feedback = handler.compile()
     if not success:
-        feedback.addTestSyntaxError('Compilation', 'La compilation du code a échoué', compile_feedback)
+        feedback.addTestSyntaxError('Compilation', compile_feedback, 'La compilation du code a échoué')
         output(0, feedback.render(), context)
         sys.exit(1)
     
