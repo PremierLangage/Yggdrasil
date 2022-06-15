8ea65d32c2d033b1b8ded6603065dac59fb4326f
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-15 14:20:20 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractivegrader.py

--- AAAA/Gaudet/template/cginteractivegrader.py
+++ AAAA/Gaudet/template/cginteractivegrader.py
@@ -15,6 +15,27 @@ if __name__ == "__main__":
     context = get_context()
     context['response'] = get_answers()
 
-    
+    student_code = response[editor.cid]['code']
+    if 'cgtest' not in globals() :
+        print('cgtest missing, please specify tests to run', file=sys.stderr)
+        sys.exit(1)
+
+    feedback = ''
+
+    # Get language used and the corresponding handler
+    lang = response[editor.cid]['language']
+    handler = langhandlers.get_language_handler(lang, student_code)
+
+    # Compilation
+    success, compile_feedback = handler.compile()
+    if not success:
+        feedback += """<div style="border:1px solid black;padding:1%;margin:1%;background-color:Tomato;border-radius:4px;color:black;">
+        La compilation/vérification du code a échoué<br>
+        Voici le détail:
+        <pre style="background: #DDD"><code>
+        """
+        feedback += compile_feedback
+        feedback += "</code></pre><div>"
+        score = 0
     
     output('score', 'feedback', context)
