424b5ae9b848c6d56cb3d1d9cc046e8638aac89f
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-15 14:16:15 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractivegrader.py

---
+++ AAAA/Gaudet/template/cginteractivegrader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import sys, json, jsonpickle, time
+
+from sandboxio import output, get_context, get_answers
+
+if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        msg = ("Sandbox did not call grader properly:\n"
+               +"Usage: python3 grader.py [input_json] [answer_jsonfile] [output_json] [feedback_file]")
+        print(msg, file=sys.stderr)
+        sys.exit(1)
+    
+    context = get_context()
+    context['response'] = get_answers()
+
+
+    
+    output('score', 'feedback', context)
