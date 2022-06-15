760394a0a46bbdac1ac2873a179295354058924b
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-15 14:04:21 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/utils/cginteractive.py

---
+++ AAAA/Gaudet/template/utils/cginteractive.py
@@ -0,0 +1,63 @@
+from asyncio import create_subprocess_exec, wait_for, TimeoutError
+from asyncio.subprocess import PIPE
+
+class InvalidCGBinaryExecution(Exception):
+    pass
+
+class CGInteractiveBinary():
+    cmd_nobuf = ['stdbuf', '-oL']
+
+    def __init__(self, cmd):
+        """Initialises a CGInteractiveBinary instance
+        cmd must be a list of strings corresponding to the command to use to start the binary"""
+        self.cmd = cmd
+        self.proc = None
+
+    async def start(self, forcelinebuffers=True):
+        """Starts the process"""
+        finalcmd = []
+        if forcelinebuffers: finalcmd += self.cmd_nobuf
+        finalcmd += self.cmd
+
+        self.proc = await create_subprocess_exec(*finalcmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    async def stop(self):
+        """Kills the process"""
+        if self.proc.returncode is None:
+            self.proc.kill()
+            await self.proc.wait()
+
+    async def send(self, *args):
+        """Sends a string to the running process. Takes a variable amount of arguments, each is turned into their
+        string representation if they're not a string, and arguments are all joined together into one string, with newlines
+        inserted in between.
+        A newline is also added at the end of the string"""
+        # Building query
+        query = ''
+        for arg in args:
+            arg = str(arg)
+            query += arg
+            if not arg.endswith('\n'): query += '\n'
+        # Sending query
+        try:
+            self.proc.stdin.write(query.encode())
+            await self.proc.stdin.drain()
+        except BrokenPipeError:
+            if self.proc.returncode is None:
+                raise InvalidCGBinaryExecution("Cannot sent data to subprocess: pipe is broken")
+            else:
+                raise InvalidCGBinaryExecution("Cannot send data to subprocess: subprocess terminated prematurely")
+
+    async def receive(self, timeout=1):
+        """Receives data from the running process. The string to receive must be marked with a newline to indicate
+        the end of it"""
+        try:
+            output = await wait_for(self.proc.stdout.readline(), timeout)
+        except TimeoutError:
+            raise InvalidCGBinaryExecution("Timeout while waiting for subprocess answer")
+        return output.decode()
+
+    async def query(self, query, timeout=1):
+        """Combines a send and a receive together"""
+        await self.send(query)
+        return await self.receive(timeout=timeout)
