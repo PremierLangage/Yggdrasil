a30a5f81b35bbcc13ca9cef9737d458803e9a28e
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-15 14:53:14 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractivegrader.py

--- AAAA/Gaudet/template/cginteractivegrader.py
+++ AAAA/Gaudet/template/cginteractivegrader.py
@@ -64,8 +64,6 @@ if __name__ == "__main__":
         sys.exit(1)
     testcases = context['testcases']
 
-    feedback = ''
-
     # Get language used and the corresponding handler
     lang = response[editor.cid]['language']
     handler = langhandlers.get_language_handler(lang, student_code)
@@ -89,6 +87,8 @@ if __name__ == "__main__":
     evalscript = dic.get('evalscript', None)
     if evalscript is None:
         raise ValueError("Failed to load eval script")
+    
+
 
     # Load test cases
     testcases = parseTestcases(testcases)
