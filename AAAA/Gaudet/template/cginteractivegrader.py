e48c3cc786497d6ca743faf2cc07886e36347362
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-15 15:15:35 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractivegrader.py

--- AAAA/Gaudet/template/cginteractivegrader.py
+++ AAAA/Gaudet/template/cginteractivegrader.py
@@ -72,14 +72,8 @@ if __name__ == "__main__":
     # Compilation
     success, compile_feedback = handler.compile()
     if not success:
-        feedback = """<div style="border:1px solid black;padding:1%;margin:1%;background-color:Tomato;border-radius:4px;color:black;">
-        La compilation/vérification du code a échoué<br>
-        Voici le détail:
-        <pre style="background: #DDD"><code>
-        """
-        feedback += compile_feedback
-        feedback += "</code></pre><div>"
-        output(0, feedback, context)
+        feedback.addTestError('Compilation', 'La compilation du code a échoué', compile_feedback)
+        output(0, feedback.render(), context)
         sys.exit(1)
     
     # Load eval script
