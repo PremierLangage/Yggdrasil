72e78ed7327e9559e3fd91a6dd23af785e570281
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-16 14:31:24 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl

--- AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl
+++ AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl
@@ -19,13 +19,6 @@ Comme d'habitude vous avez un fichier "exemple" pour lequel la bonne reponse est
 
 ==
 
-choices==
-153837.0:0.0 #Bravo !Vous avez tous les points.
-%50.0%153837.0:100.0 #Il a écrit 153 837 vers. Votre réponse était pas mal proche, vous avez la moitié des points.
-%33.0%153837.0:500.0 #Il a écrit 153 837 vers. Votre réponse était pas mal proche, vous avez le tiers des points.
-%25.0%153837.0:1000.0 #Il a écrit 153 837 vers. Votre réponse était un peu proche, vous avez le quart des points.
-== 
-
 
 before==
 import g
