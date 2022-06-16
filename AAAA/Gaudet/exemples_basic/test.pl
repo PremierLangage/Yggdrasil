dcf2c8998d9c491c97e671a32c2be67448d6c3ef
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-16 16:20:05 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/exemples_basic/test.pl

---
+++ AAAA/Gaudet/exemples_basic/test.pl
@@ -0,0 +1,24 @@
+extends= ../template/cgbasic.pl
+
+author=CG
+title=Test
+
+tag=function # N'oubliez pas de remplir ce champs svp
+
+text==
+Ecrire un programme qui écrit le successeur de l'entier envoyé en entrée
+==
+
+languages==
+==
+
+cgtest==
+>>> test('1') # Test 1
+'2'
+>>> test('2') # Test 2
+'3'
+>>> test('-3') # Test négatif
+'-2'
+>>> from random import randint; n = randint(0, 100); test(str(n)) == str(n + 1) # Test aléatoire
+True
+==
