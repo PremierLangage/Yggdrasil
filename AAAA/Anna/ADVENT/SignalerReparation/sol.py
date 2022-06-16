e94f23a3a6a58f69678d0b8559e915cf38aabfc7
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-16 11:41:36 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/SignalerReparation/sol.py

--- AAAA/Anna/ADVENT/SignalerReparation/sol.py
+++ AAAA/Anna/ADVENT/SignalerReparation/sol.py
@@ -3,7 +3,7 @@ import random
 def builddata(filename):
     with open(filename, "w") as file:
         for x in range(random.randint(0, 200)):
-            g.write(str(random.randint(x, 2020))+"\n")
+            file.write(str(random.randint(x, 2020))+"\n")
 
 def fromFileToList(filename):
     with open(filename, "r") as file:
