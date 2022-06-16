0400492548d484a2857b5bbb99eb4296ae6e996b
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-16 11:03:05 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractive.pl

--- AAAA/Gaudet/template/cginteractive.pl
+++ AAAA/Gaudet/template/cginteractive.pl
@@ -4,8 +4,10 @@
 doc==
 Ce template permet d'écrire des exercices pouvant être résolus dans un langage au choix de l'étudiant.
 
-Par rapport à la version basique, celle-ci permet plus d'interactivité avec le programme étudiant. Cela nécessite cependant d'écrire un script
-Python permettant de communiquer avec et d'évaluer le programme de l'étudiant et est donc plus complexe dans son utilisation.
+Par rapport à la version basique, celle-ci permet plus d'interactivité avec le programme étudiant en réalisant plus qu'une seule entrée, et une seule sortie.
+Cela nécessite cependant d'écrire un script Python permettant de communiquer avec et d'évaluer le programme de l'étudiant et est donc plus complexe dans son utilisation.
+
+
 
 La liste des langages utilisables actuellement est : c, cpp, python, java, ocaml
 ==
