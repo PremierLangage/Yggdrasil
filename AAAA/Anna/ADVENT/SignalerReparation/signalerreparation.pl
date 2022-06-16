9398f5a26533653fdfd1d71b0c1475593404498a
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-16 14:33:26 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl

--- AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl
+++ AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl
@@ -21,12 +21,37 @@ Comme d'habitude vous avez un fichier "exemple" pour lequel la bonne reponse est
 
 
 before==
-with open("ennonce.md","r") as f:
-    lenonce = f.read()
+import random
 
-r = sol.getQ()
+def builddata(filename):
+    with open(filename, "w") as file:
+        for x in range(random.randint(0, 200)):
+            file.write(str(random.randint(x, 2020))+"\n")
 
-choices = r
+def fromFileToList(filename):
+    with open(filename, "r") as file:
+        return [x for x in file.readlines()]
+
+def getResults(array):
+    for i in range(len(array)):
+        for j in range(i+1,len(array)):
+            if array[i] + array[j] == 2020:
+                return array[i] * array[j]
+
+def getQ():
+    builddata("data")
+    valeurs = fromFileToList("data")
+    return getResults(valeurs)
+
+
+def build():
+    r = getQ()
+    return r
+
+
+arr = fromFileToList("exemple")
+res = getResults(arr)
+print(res)
 
 ==
 
