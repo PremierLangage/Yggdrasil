b33154fa3d70a4631e8499d7a97272d5ed70ae41
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-16 11:52:31 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractive.pl

--- AAAA/Gaudet/template/cginteractive.pl
+++ AAAA/Gaudet/template/cginteractive.pl
@@ -33,6 +33,8 @@ celui-ci peut être changé au besoin. En cas de timeout, le test est automatiqu
 
 * student.query(query, timeout=1) : méthode raccourci qui à la fois envoie une chaine de caractère (\n rajouté automatiquement si besoin) et attend une réponse directement après
 
+Ces méthodes doivent être appelées avec le mot clé await car elles sont asynchrones
+
 ***** testcases *****
 
 Ceci doit être une liste Python contenant les différents cas de tests à réaliser. Chaque test a le format suivant:
