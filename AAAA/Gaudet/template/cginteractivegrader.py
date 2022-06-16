3cfef327636b6d2796ed4433d198e383f00ebfc5
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-16 16:09:06 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractivegrader.py

--- AAAA/Gaudet/template/cginteractivegrader.py
+++ AAAA/Gaudet/template/cginteractivegrader.py
@@ -87,8 +87,8 @@ async def runtests(cmd, feedback, testcases):
         elif result == TestStatus.FAIL:
             feedback.addTestFailure(testname, 'Echec', 'Validation')
         elif result == TestStatus.ERROR:
-            feedback.addTestError(testname, "Erreur : le programme n'a pas répondu. Causes possibles: boucle infinie, arrêt prématuré du programme, " +
-                                            "oubli d'un retour à la ligne en fin d'écriture sur la sortie standard", 'Validation')
+            feedback.addTestError(testname, "Erreur : le programme n'a pas répondu. Causes possibles: oubli d'un \\n boucle infinie, arrêt prématuré du programme" ,
+                                            'Validation')
         else:
             raise AssertionError("Shouldn't be possible")
     
