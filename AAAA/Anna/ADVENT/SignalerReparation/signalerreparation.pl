2a2a8374062d716989e88b3527d7778a331f1c53
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-16 16:03:30 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl

--- AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl
+++ AAAA/Anna/ADVENT/SignalerReparation/signalerreparation.pl
@@ -26,7 +26,24 @@ choices ==
 
 
 before==
-import random
+import re
+lines = choices.split('\n')
+choices = []
+liste = []
+mode = None
+pattern = re.compile(r'(?:%(?P<score>.*)%)?(?P<v1>.*)(?P<mode>\:|\.\.)(?P<v2>(?:(?:\\\#)|[^\#])+)(?:\#(?P<feedback>.*))?')
+for line in lines:
+    match = pattern.match(line)
+    if not match:
+        continue
+    choice = {
+        "score": match.group('score') or 100,
+        "v1": match.group('v1').strip(),
+        "mode": match.group('mode'),
+        "v2": match.group('v2').strip(),
+        "feedback": match.group('feedback') or ''
+    }
+    choices.append(choice)
 
 def builddata():
     tab = [random.randint(x, 2020) for x in range(200)]
