d5ec61bae74149401b98ddc6ebe16961e2b4dabd
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-16 10:47:26 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/testinteractive.pl

--- AAAA/Gaudet/testinteractive.pl
+++ AAAA/Gaudet/testinteractive.pl
@@ -8,6 +8,12 @@ tag=function # N'oubliez pas de remplir ce champs svp
 text==
 Le but est d'écrire un programme qui devine un nombre en un nombre le plus limité possible d'essais en implémentant une stratégie de recherche dichotomique.
 
+Votre programme recevera d'abord un nombre N donnant l'intervalle sur lequel le nombre peut être tiré (de 0 à N exclu)
+
+Après cela, votre programme doit envoyé un nombre indiquant sa tentative et il recevra une réponse en retour:
+* "<" si le nombre à deviner est plus petit
+* ">" si le nombre à deviner est plus grand
+* "=" si le nombre est correct
 
 ==
 
