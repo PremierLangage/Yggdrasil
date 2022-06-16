5c0fa8b2dc40c4b18a38fb389dccf446d44d5462
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-16 11:22:28 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cginteractivegrader.py

--- AAAA/Gaudet/template/cginteractivegrader.py
+++ AAAA/Gaudet/template/cginteractivegrader.py
@@ -52,7 +52,7 @@ async def runtests(cmd, feedback, testcases):
                 else:
                     feedback.addTestFailure(testname, 'fail', 'pass')
         if failed:
-            feedback.addTestFailure(testname, 'not attempted due to failure beforehand', 'pass')
+            feedback.addTestFailure(testname, 'not attempted due to previous failure', 'pass')
     
     return tests_success * 100 // tests_success
 
