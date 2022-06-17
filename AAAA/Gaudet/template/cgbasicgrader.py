af4a8e3730b642e47183de61ea9f1148d7a810b6
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-17 15:54:47 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cgbasicgrader.py

---
+++ AAAA/Gaudet/template/cgbasicgrader.py
@@ -0,0 +1,16 @@
+import sys
+from sandboxio import output, get_context, get_answers
+
+if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        msg = ("Sandbox did not call grader properly:\n"
+               +"Usage: python3 grader.py [input_json] [answer_jsonfile] [output_json] [feedback_file]")
+        print(msg, file=sys.stderr)
+        sys.exit(1)
+    
+    # Get all required data from context
+    context = get_context()
+    response = get_answers()
+    editor = context['editor']
+
+    student_code = response[editor.cid]['code']
