7f5b29200f6926f6cb156e816be33139cdc77da0
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-17 14:22:13 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cgbasic.pl

--- AAAA/Gaudet/template/cgbasic.pl
+++ AAAA/Gaudet/template/cgbasic.pl
@@ -77,15 +77,9 @@ feedback = FeedBack(name="Tests")
 # Compilation
 success, compile_feedback = handler.compile()
 if not success:
-    
-    feedback += """<div style="border:1px solid black;padding:1%;margin:1%;background-color:Tomato;border-radius:4px;color:black;">
-    La compilation/vérification du code a échoué<br>
-    Voici le détail:
-    <pre style="background: #DDD"><code>
-    """
-    feedback += compile_feedback
-    feedback += "</code></pre><div>"
-    score = 0
+    feedback.addTestSyntaxError('Compilation', compile_feedback.replace('\n', '<br>'), 'La compilation du code a échoué')
+    output(0, feedback.render(), context)
+    sys.exit(1)
 
 # Execution of tests
 else:
