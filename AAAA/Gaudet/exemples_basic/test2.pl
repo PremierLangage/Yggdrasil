111c5c072b504e38dfcf236f5ac7b236a73fcbfa
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-17 16:12:15 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/exemples_basic/test2.pl

--- AAAA/Gaudet/exemples_basic/test2.pl
+++ AAAA/Gaudet/exemples_basic/test2.pl
@@ -10,10 +10,7 @@ Ecrire un programme qui prend une liste d'entiers séparés par des retours à l
 Le premier nombre envoyé correspondant à la taille de la liste
 ==
 
-cgtest==
->>> test('4\n1\n2\n3\n4\n') # Test 1
-'4'
->>> test('4\n64\n21\n1\n89\n3\n73\n') # Test 2
-'89'
+testcases==
+[('4\n1\n2\n3\n4\n', '4', 'Test 1'),
+('4\n64\n21\n1\n89\n3\n73\n', '89', 'Test 2')]
 ==
-
