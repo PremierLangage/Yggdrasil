0a776220fa256686aef7beed2c0d8deb4366aaae
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-20 14:54:32 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cgbasic.pl

--- AAAA/Gaudet/template/cgbasic.pl
+++ AAAA/Gaudet/template/cgbasic.pl
@@ -11,7 +11,7 @@ entrée et sorties standards.
 La batterie de test est à mettre dans la variable testcases sous forme d'une liste python contenant
 des tuples de la forme (chaine a mettre dans stdin, chaine à recevoir dans stdout, chaine correspondant au nom du test)
 
-La variable before peut être utilisée pour exécuter un script Python pendant la construction de l'exercice
+La variable before peut être utilisée pour exécuter un script Python pendant la construction de l'exercice.
 
 La liste des langages utilisables actuellement est : c, cpp, python, java, ocaml
 ==
