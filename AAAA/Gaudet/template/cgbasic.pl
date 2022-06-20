e6660d57e2bdd38b00cac23ebcf76e59875a253b
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-06-20 14:54:45 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cgbasic.pl

--- AAAA/Gaudet/template/cgbasic.pl
+++ AAAA/Gaudet/template/cgbasic.pl
@@ -6,7 +6,7 @@ Ce template permet de faire des exercices pouvant être résolu avec différents
 de l'étudiant. 
 
 Le programme étudiant est compilé et exécuté pour faire les tests. La communication se fait directement par
-entrée et sortie standards.
+entrée et sortie standard.
 
 La batterie de test est à mettre dans la variable testcases sous forme d'une liste python contenant
 des tuples de la forme (chaine a mettre dans stdin, chaine à recevoir dans stdout, chaine correspondant au nom du test)
