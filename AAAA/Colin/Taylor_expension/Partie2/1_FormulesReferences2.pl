992c0100405cf003e535b905514e854793321e5e
Colin PETITJEAN <Colin.Petitjean@univ-mlv.fr>
2022-06-23 08:35:50 +0200

    cpetitjean modified Yggdrasil/AAAA/Colin/Taylor_expension/Partie2/1_FormulesReferences2.pl

--- AAAA/Colin/Taylor_expension/Partie2/1_FormulesReferences2.pl
+++ AAAA/Colin/Taylor_expension/Partie2/1_FormulesReferences2.pl
@@ -17,7 +17,7 @@ a=choice([-2,-1,2])
 y=a*x**p
 f = choice([sin(y), cos(y), exp(y), 1/(1-y) , 1/(1+y),(1+y)**(1/2),(1+y)**(-1/2)])
 
-sol = series(f, y, 0, n+1).removeO()
+sol = series(f, x, 0, n+1).removeO()
 ==
 
 question ==
