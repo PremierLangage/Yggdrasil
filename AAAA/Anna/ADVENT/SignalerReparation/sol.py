01645c7dfc1df5df39dd43ede0170c34bdebc5f4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-06-27 13:37:13 +0200

    drevuz2 modified Yggdrasil/AAAA/Anna/ADVENT/SignalerReparation/sol.py

--- AAAA/Anna/ADVENT/SignalerReparation/sol.py
+++ AAAA/Anna/ADVENT/SignalerReparation/sol.py
@@ -2,19 +2,23 @@ import random
 import re
 
 def builddata():
+    """
+    a list of integers with at least a pair that addeds to 2020
+
+    """
     tab = [random.randint(1, 2020) for x in range(200)]
-  
+    tab.append(2020-tab[0])
     return tab
 
 def buildfile(filename):
     tab = builddata()
-    with open(filename, "w") as file:
-        for x in range(200):
-            file.write(str(tab[x])+"\n")
+    with open(filename, "w") as f:
+        for x in tab:
+            print(x,file=f)
 
 def fromFileToList(filename):
-    with open(filename, "r") as file:
-        return [int(x[:-1]) for x in file.readlines()]
+    with open(filename, "r") as f:
+        return [int(x[:-1]) for x in f.readlines()]
 
 def getResults(array):
     for i in range(len(array)):
