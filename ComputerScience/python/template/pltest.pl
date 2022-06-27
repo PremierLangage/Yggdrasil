b535069e278784b19d80e4b3fa0d4a518d90b2ab
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-27 15:18:49 +0200

    adiaw2 modified Yggdrasil/ComputerScience/python/template/pltest.pl

--- ComputerScience/python/template/pltest.pl
+++ ComputerScience/python/template/pltest.pl
@@ -45,6 +45,7 @@ Si vous souhaitez faire des tests sur le code de l'élève la variable __student
 @ feedback2.py
 @ /ComputerScience/python/template/anothertemplate.html [template.html]
 @ displayExample.html
+@ displayData.html
 
 @ /utils/sandboxio.py
 @ /builder/none.py [builder.py]
