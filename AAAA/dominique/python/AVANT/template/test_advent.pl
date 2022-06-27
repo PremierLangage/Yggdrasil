2753d9d42412675877bc346a9fb5c61af639de45
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-06-27 16:34:04 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/python/AVANT/template/test_advent.pl

---
+++ AAAA/dominique/python/AVANT/template/test_advent.pl
@@ -0,0 +1,56 @@
+
+
+# Boris Jabot 10/09/2019 OK
+# Copyright 2016 Dominique Revuz <dr@univ-mlv.fr>
+#author=
+title=Une fonction carre
+
+tag=function # N'oubliez pas de remplir ce champs svp
+extends= advent.pl
+@ /builder/before.py [builder.py]
+piste=verte
+text==
+Ecrivez une fonction `carre` qui retourne le carré de son paramêtre
+==
+before==
+import random 
+p=random.randint(23,77)
+pltest2=""">>> carre({})\n{}\n""".format(p, p**2)
+p+=17
+pltest3=""">>> carre({})=={}# Hidden value test\nTrue\n""".format(p, p**2)
+
+==
+
+pltest0==
+>>> import types #
+>>> "carre" in globals() # carre existe 
+True
+>>> type(carre)  # est carre est une fonction 
+<type 'function'>
+==
+
+pltest1==
+>>> carre(510) == 260100 # le carre de 510
+True
+>>> carre(0)
+0
+>>> carre(-10)
+100
+>>> 
+==
+
+
+editor.code==
+
+==
+editor.height=145
+
+
+
+
+
+
+
+
+
+
