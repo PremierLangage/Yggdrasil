99b2ac5a3972bb60567eff1d6dc101405e5b4cc4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-06-28 14:35:02 +0200

    drevuz2 modified Yggdrasil/AAAA/Anna/ADVENT/trucsadr/muulti.pl

---
+++ AAAA/Anna/ADVENT/trucsadr/muulti.pl
@@ -0,0 +1,28 @@
+
+
+
+
+extends= /AAAA/Gaudet/template/cgbasic.pl
+
+author=DR
+title=Test avec stdin dans un fichier
+
+tag=function
+
+@ fichier1
+
+text==
+Ecrire un programme qui prend une liste d'entiers séparés par des retours
+ à la ligne et qui affiche le max.
+Le premier nombre envoyé correspondant à la taille de la liste
+==
+
+testcases==
+[('fichier1', '4000', 'Test 1'),
+('fichier1', '89', 'Test 2')]
+==
+
+# testcase1 = [f"('''{exemple}''','{exemplesol}','Tst exemple question1')",
+
+f"  data sol ]
+
