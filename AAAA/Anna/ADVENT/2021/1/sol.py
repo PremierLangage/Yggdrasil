9092bcf77432c9f362cee3868d04d8e14977b060
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-28 12:11:02 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/1/sol.py

--- AAAA/Anna/ADVENT/2021/1/sol.py
+++ AAAA/Anna/ADVENT/2021/1/sol.py
@@ -8,8 +8,10 @@ def builddata():
     interval = random.randint(1,10)
     incOrDec = random.randint(0, 2)
     for x in range (500):
-        if (val > 0) : 
-            if (incOrDec == 0) : 
+        if (val > 0) :
+            while (interval == 0) :
+                 interval = random.randint(1,10)
+            elif (incOrDec == 0) : 
                 val = val + interval
             elif (incOrDec == 1) : 
                 val = val - interval
