8d802ee57aa42385021ef8219fecef6bb0bfcd01
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-06-28 13:42:55 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/python/AVANT/template/advent.pl

--- AAAA/dominique/python/AVANT/template/advent.pl
+++ AAAA/dominique/python/AVANT/template/advent.pl
@@ -3,19 +3,21 @@
 
 @  /ComputerScience/python/template/pltest_doc.py
 @  /ComputerScience/python/template/pltestgrader.py [grader.py]
-
+@  /ComputerScience/python/template/feedback2.py
 
 
 @ advent.html [template.html]
 @ /utils/sandboxio.py 
-
+@ /builder/none.py [builder.py]
 
 editor.fontsize=14
-
+# une interface standard d'exercice avec un editeur pour la réponse
 form=@ editor.html
 
 settings.allow_reroll=1
+
+
+
+
 @ /builder/before.py [builder.py]
-
-
 
