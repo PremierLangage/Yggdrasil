886146410c86ffe875d861c9d821a162f27cfb4e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-06-28 14:17:44 +0200

    drevuz2 modified Yggdrasil/AAAA/Gaudet/exemples_basic/testAdvent.pl

---
+++ AAAA/Gaudet/exemples_basic/testAdvent.pl
@@ -0,0 +1,19 @@
+
+
+extends= /AAAA/Gaudet/template/cgbasic.pl
+
+author=DR
+title=Test avec stdin dans un fichier
+
+tag=function
+
+text==
+Ecrire un programme qui prend une liste d'entiers séparés par des retours
+ à la ligne et qui affiche le max.
+Le premier nombre envoyé correspondant à la taille de la liste
+==
+
+testcases==
+[('data', '4000', 'Test 1'),
+('exemple', '89', 'Test 2')]
+==
