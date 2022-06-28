2d918cca55659ad2687b08a9506424782d6f0770
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-28 11:51:33 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/1/sol.py

--- AAAA/Anna/ADVENT/2021/1/sol.py
+++ AAAA/Anna/ADVENT/2021/1/sol.py
@@ -6,7 +6,7 @@ def builddata():
     val = random.randint(0, 200)
     tab.append(val)
     interval = random.randint(1, 11)
-    incOrDec = random.randint(0, 2)
+    incOrDec = random.randint(0, 1)
     for x in range (1999):
         if (val > 0) :
             if (incOrDec == 0) : 
