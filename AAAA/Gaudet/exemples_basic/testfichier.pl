3394f716db1bfb9c18c0fc9ebf1c0f1a993b1f27
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-06-28 13:05:46 +0200

    drevuz2 modified Yggdrasil/AAAA/Gaudet/exemples_basic/testfichier.pl

---
+++ AAAA/Gaudet/exemples_basic/testfichier.pl
@@ -0,0 +1,17 @@
+
+extends= ../template/cgbasic.pl
+
+author=DR
+title=Test d'entrée standard a a partir d'un fichier
+
+tag=function
+
+text==
+Ecrire un programme qui prend une liste d'entiers séparés par des retours à la ligne et qui affiche le max.
+Le premier nombre envoyé correspondant à la taille de la liste
+==
+
+testcases==
+[(data, '4000', 'Test 1'),
+(exemple, '89', 'Test 2')]
+==
