4e01bb69f84ff4586c1a21748fac964e5d219d72
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-06-29 15:07:17 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2020/1/sol.py

--- AAAA/Anna/ADVENT/2020/1/sol.py
+++ AAAA/Anna/ADVENT/2020/1/sol.py
@@ -36,7 +36,7 @@ def fromFileToList(filename):
         return [int(x[:-1]) for x in file.readlines()]
 
 def ToList(filec):
-    raise Exception(str(filec))
+    raise Exception(list(filec.split(" ")))
     return [int(x.strip()) for x in filec if x] 
 
 
