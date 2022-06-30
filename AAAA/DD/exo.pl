718efa2b125ba43061dd7dc1dfe16e6ee838f7f5
David DOYEN <david.doyen@univ-eiffel.fr>
2022-06-30 10:53:17 +0200

    ddoyen1 modified Yggdrasil/AAAA/DD/exo.pl

--- AAAA/DD/exo.pl
+++ AAAA/DD/exo.pl
@@ -1,29 +1,23 @@
-extends = /model/basic/basic.pl
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
 
-question ==
-{% raw %}
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-<div ng-app="myApp" ng-controller="personCtrl">
+mathinput =: MathInput
 
-First Name: <input type="text" ng-model="firstName"><br>
-Last Name: <input type="text" ng-model="lastName"><br>
-<br>
-
-Full Name: {{ '{{fullName()}}' }}
-</div>
-<script>
-var app = angular.module('myApp', []);
-app.controller('personCtrl', function($scope) {
-    $scope.firstName = "John";
-    $scope.lastName = "Doe";
-    $scope.fullName = function() {
-        return $scope.firstName + " " + $scope.lastName;
-    };
-});
-</script>
-{% endraw %}
+before==
 ==
 
-javascript.toto ==
+title==
+Math Input Component
+==
 
+text==
+==
+
+form==
+{{ mathinput|component}}
+==
+
+evaluator==
+grade = (100, mathinput.value);
 ==
