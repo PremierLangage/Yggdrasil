b93a276c4765325129f392bcd960bac6b9967589
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-01 09:59:57 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2020/1/signalerreparation.pl

--- AAAA/Anna/ADVENT/2020/1/signalerreparation.pl
+++ AAAA/Anna/ADVENT/2020/1/signalerreparation.pl
@@ -49,7 +49,7 @@ with open("ennonce1.md","r") as f:
 
 testcases, donnees = build.buildQ1(lexemple,data)
 
-if q1done : 
+if testSuccess : 
     with open("ennonce2.md","r") as f:
         lenonce += f.read()
     testcases, donnees = build.buildQ2(lexemple,data)
