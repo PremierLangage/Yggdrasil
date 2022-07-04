40971fcfbe56270240d5e4b71434d8ba19a8d3f0
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-07-04 14:35:30 +0200

    cgaudet1 modified Yggdrasil/utils/sympy/latex2sympy.py

--- utils/sympy/latex2sympy.py
+++ utils/sympy/latex2sympy.py
@@ -27,7 +27,6 @@ def str2sympy(s, local_dict={}, evaluate=False):
     >>> str2sympy("{0, pi, 2 pi}")
     [0, pi, 2*pi]
     """
-    print(s, file=sys.stderr)
     if s.strip() == "":
         return None
 
