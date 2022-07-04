e710f16329edaa25f4d2f29f6a42be46467e39a5
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-04 14:57:00 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/1/sol.py

--- AAAA/Anna/ADVENT/2021/1/sol.py
+++ AAAA/Anna/ADVENT/2021/1/sol.py
@@ -28,7 +28,7 @@ def ToList(filec):
 
 def Question1(array):
     isIncreased = 0
-    for i in range(len(array) - 1):
+    for i in range(len(array)):
         if array[i] < array[i+1]:
             isIncreased += 1
     return isIncreased
