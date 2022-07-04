9d2a5cfcb69f235b3e321d5c84b26eb452a479e6
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-04 14:41:17 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/1/balayage.pl

--- AAAA/Anna/ADVENT/2021/1/balayage.pl
+++ AAAA/Anna/ADVENT/2021/1/balayage.pl
@@ -61,7 +61,7 @@ import sys
 def Question1():
     array = [int(x[:-1]) for x in sys.stdin.readlines()]
     isIncreased = 0
-    for i in range(len(array) - 1):
+    for i in range(len(array)):
         if array[i] < array[i+1]:
             isIncreased += 1
     return isIncreased
