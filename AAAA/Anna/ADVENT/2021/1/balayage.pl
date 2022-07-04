6a57f79eabeec246874366038911cb39ef7bb962
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-04 14:36:26 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/1/balayage.pl

--- AAAA/Anna/ADVENT/2021/1/balayage.pl
+++ AAAA/Anna/ADVENT/2021/1/balayage.pl
@@ -9,7 +9,7 @@ lexemple =@exemple
 #@/AAAA/dominique/python/AVANT/template/advent.pl [template.html]
 
 
-title= Balayage sonar
+title= Signaler Reparation
 
 text==
 
@@ -17,8 +17,8 @@ text==
 
 La liste des informations est dans le fichier "data" ci-dessous.
 
-Vous pouvez tester votre fonction avec le fichier "exemple" ci-dessous dont le résultat est '' pour la question 1 
-et '' pour la question 2.
+Vous pouvez tester votre fonction avec le fichier "exemple" ci-dessous dont le résultat est 145875 pour la question 1 
+et 241861950 pour la question 2.
 
 <details><summary>exemple</summary><blockquote>
   {{lexemple}}
@@ -27,14 +27,6 @@ et '' pour la question 2.
 <details><summary>data</summary><blockquote>
   {{donnees}}
 </blockquote></details>
-
-Vous pouvez par exemple ecrire : 
-
-    def Question1():
-        array = [int(x[:-1]) for x in sys.stdin.readlines()]
-        ...VOTRE CODE
-    if __name__ == '__main__':
-        print(Question1())  
 
 ==
 
@@ -57,18 +49,30 @@ import build
 
 changeText = True
 with open("ennonce2.md","r") as f:
-    lenonce += f.read()
+    lenonce = f.read()
 
 testcases, donnees = build.buildQ2(lexemple,data)
 ==
 
 
 
-editor.code ==
+solution ==
 import sys
 def Question1():
+    array = [int(x[:-1]) for x in sys.stdin.readlines()]
+    for i in range(len(array)):
+            for j in range(i+1,len(array)):
+                if array[i] + array[j] == 2020:
+                    return array[i] * array[j]
 
 def Question2():
+    array = [int(x[:-1]) for x in sys.stdin.readlines()]
+    for i in range( 0,len(array)-2): 
+        for j in range(i + 1, len(array)-1): 
+            for k in range(j + 1, len(array)):
+                if array[i] + array[j] + array[k] == 2020:
+                    return array[i] * array[j] * array[k]    
+                    
 
 if __name__ == '__main__':
     print(Question1())  
