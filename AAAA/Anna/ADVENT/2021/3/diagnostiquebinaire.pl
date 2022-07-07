50dc6becbaa5cfd185448acb1b6268225f97f610
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 10:11:10 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/3/diagnostiquebinaire.pl

---
+++ AAAA/Anna/ADVENT/2021/3/diagnostiquebinaire.pl
@@ -0,0 +1,87 @@
+extends= /AAAA/Gaudet/template/cgbasic.pl
+
+@ennonce1.md
+@ennonce2.md
+@sol.py [build.py]
+@exemple 
+lexemple =@exemple
+
+#@/AAAA/dominique/python/AVANT/template/advent.pl [template.html]
+
+
+title= Signaler Reparation
+
+text==
+
+{{lenonce}}
+
+La liste des informations est dans le fichier "data" ci-dessous.
+
+Vous pouvez tester votre fonction avec le fichier "exemple" ci-dessous dont le résultat est 1154 pour la question 1 
+et  pour la question 2.
+
+<details><summary>exemple</summary><blockquote>
+  {{lexemple}}
+</blockquote></details>
+
+<details><summary>data</summary><blockquote>
+  {{donnees}}
+</blockquote></details>
+
+==
+
+
+
+before==
+import build
+
+data = build.builddata()
+
+with open("ennonce1.md","r") as f:
+    lenonce= f.read()
+
+testcases, donnees = build.buildQ1(lexemple,data)
+changeText = False
+==
+
+postevaluator==
+import build
+
+changeText = True
+with open("ennonce2.md","r") as f:
+    lenonce = f.read()
+
+testcases, donnees = build.buildQ2(lexemple,data)
+==
+
+
+
+solution ==
+import sys
+def Question1():
+    array = [int(x) for x in sys.stdin.readlines()]
+    isIncreased = 0
+    for i in range(1, len(array)):
+        if array[i] > array[i-1]:
+            isIncreased += 1
+    return isIncreased
+    
+if __name__ == '__main__':
+    print(Question1())  
+
+def Question2():
+    array = [int(x) for x in sys.stdin.readlines()]
+    isIncreased = 0
+    for i in range(3, len(array)):
+        a = array[i - 3] + array[i - 2] + array[i - 1]
+        b = array[i - 2] + array[i - 1] + array[i]
+        if a < b:
+            isIncreased += 1
+    return isIncreased
+                    
+
+if __name__ == '__main__':
+    print(Question2())  
+    
+  
+==
