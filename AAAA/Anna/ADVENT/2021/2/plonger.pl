fc04e049f55b8d2b102caa4752ce9e799b995004
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 14:45:17 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/2/plonger.pl

--- AAAA/Anna/ADVENT/2021/2/plonger.pl
+++ AAAA/Anna/ADVENT/2021/2/plonger.pl
@@ -69,36 +69,20 @@ if __name__ == '__main__':
     print(Question1())  
 
 import sys
-from itertools import zip_longest
 
-def Question2():
-    array = [x[:-1] for x in sys.stdin.readlines()]
-    ar1 = array
-    ar2 = array
-    position = 0
-    while position < len(array[0]):
-        if len(ar1) == 1 and len(ar2) == 1:
-            break
-        if len(ar1) > 1:
-            map1 = {k: [] for k in [0,1]}
-            for elem in ar1:
-                bit = int(elem[position])
-                map1[bit].append(elem)
-            if len(map1[0]) > len(map1[1]):
-                ar1 = map1[0]
-            else:
-                ar1 = map1[1]
-        if len(ar2) > 1:
-            map2 = {k: [] for k in [0,1]}
-            for elem in ar2:
-                bit = int(elem[position])
-                map2[bit].append(elem)
-            if len(map2[1]) < len(map2[0]):
-                ar2 = map2[1]
-            else:
-                ar2 = map2[0]
-        position += 1
-    return int(ar1[0], 2) * int(ar2[0], 2)
+def Question1(): 
+    array = [x.split(' ') for x in sys.stdin.readlines()]
+    aim = 0
+    position = [0,0]
+    for input in array:
+        if input[0] == "avant":
+            position[0] += int(input[1])
+            position[1] += (int(input[1]) * aim)
+        elif input[0] == "haut":
+            aim -= int(input[1])
+        elif input[0] == "bas":
+            aim += int(input[1])
+    return position[0] * position[1]
 
 if __name__ == '__main__':
     print(Question2())  
