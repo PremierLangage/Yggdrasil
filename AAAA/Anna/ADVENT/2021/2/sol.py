26afa06bf9a6011f8a29eec851ee74188b1a48ff
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 13:23:22 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/2/sol.py

--- AAAA/Anna/ADVENT/2021/2/sol.py
+++ AAAA/Anna/ADVENT/2021/2/sol.py
@@ -1,54 +1,76 @@
 import random
 import re
+from itertools import zip_longest
 
 def builddata():
     tab = []
+    val = random.randint(0, 20)
     for x in range(1000):
-        val = random.randint(0, 2)
-        while (interval == 0) :
-            interval = random.randint(1,10)
-        if (incOrDec == 0) : 
-            val = val + interval
-        elif (incOrDec == 1) : 
-            if (val - interval < 0) :
-                val = val + interval 
-            else : 
-                val = val - interval 
-        if (val != tab[-1]) : 
-            tab.append(val)
-        interval = random.randint(1,10)
-        incOrDec = random.randint(0, 2)
+        strnb = []
+        for y in range(val):
+            strnb.append(str(random.randint(0, 1)))
+        myst = ''.join([v for v in strnb])
+        tab.append(myst)
     return tab
 
 def ToList(filec):
-    return [int(x) for x in filec.split('\n')] 
+    return [x for x in filec.split('\n')] 
 
-def Question1(array):
-    isIncreased = 0
-    for i in range(1, len(array)):
-        if array[i] > array[i-1]:
-            isIncreased += 1
-    return isIncreased
+def polarity(array):
+    polarity = []
+    for string in array:
+        polarity = [
+            s + (1 if x == '1' else -1)
+            for s, x in zip_longest(polarity, string, fillvalue=0)
+        ]
+    return polarity
+
+def Question1(array): 
+    pol = polarity(array)
+    gamma = sum([2**i for i, x in enumerate(reversed(pol)) if x > 0])
+    epsilon = sum([2**i for i, x in enumerate(reversed(pol)) if x < 0])
+    return gamma * epsilon
 
 def Question2(array):
-    isIncreased = 0
-    for i in range(3, len(array)):
-        a = array[i - 3] + array[i - 2] + array[i - 1]
-        b = array[i - 2] + array[i - 1] + array[i]
-        if a < b:
-            isIncreased += 1
-    return isIncreased
+    ar1 = array
+    ar2 = array
+    position = 0
+    while position < len(array[0]):
+        if len(ar1) == 1 and len(ar2) == 1:
+            break
+        if len(ar1) > 1:
+            map1 = {k: [] for k in [0,1]}
+            for elem in ar1:
+                bit = int(elem[position])
+                map1[bit].append(elem)
+            if len(map1[0]) > len(map1[1]):
+                ar1 = map1[0]
+            else:
+                ar1 = map1[1]
+        if len(ar2) > 1:
+            map2 = {k: [] for k in [0,1]}
+            for elem in ar2:
+                bit = int(elem[position])
+                map2[bit].append(elem)
+            if len(map2[1]) < len(map2[0]):
+                ar2 = map2[1]
+            else:
+                ar2 = map2[0]
+        position += 1
+    return int(ar1[0], 2) * int(ar2[0], 2)
+
 
 def buildQ1(fichier,data):
     val = ToList(fichier)
     print(Question1(val))
-    dataliteral = '\n'.join([ str(v) for v in data])
+    dataliteral = '\n'.join([v for v in data])
     return f"""[('''{fichier}''','{Question1(val)}','Tst exemple question1'),
      ('''{dataliteral}''','{Question1(data)}','Tst data question1')]""", dataliteral
 
 def buildQ2(fichier,data):
     val = ToList(fichier)
-    dataliteral = '\n'.join([ str(v) for v in data])
+    dataliteral = '\n'.join([ v for v in data])
     return f"""[('''{fichier}''','{Question2(val)}','Tst exemple question2'),
      ('''{dataliteral}''','{Question2(data)}','Tst data question2')]""", dataliteral
+
 
