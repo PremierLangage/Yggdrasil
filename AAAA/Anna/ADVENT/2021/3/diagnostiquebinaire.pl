a1a974b6889224dc45a2084e7157cdc6d4d32e3b
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 12:22:28 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/3/diagnostiquebinaire.pl

--- AAAA/Anna/ADVENT/2021/3/diagnostiquebinaire.pl
+++ AAAA/Anna/ADVENT/2021/3/diagnostiquebinaire.pl
@@ -45,13 +45,7 @@ changeText = False
 ==
 
 postevaluator==
-import build
 
-changeText = True
-with open("ennonce2.md","r") as f:
-    lenonce = f.read()
-
-testcases, donnees = build.buildQ2(lexemple,data)
 ==
 
 
