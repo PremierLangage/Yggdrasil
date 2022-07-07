9767dc78659d93da604dd9eef4632940df50e6e3
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 10:45:49 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/3/sol.py

--- AAAA/Anna/ADVENT/2021/3/sol.py
+++ AAAA/Anna/ADVENT/2021/3/sol.py
@@ -19,7 +19,8 @@ def Question1(array):
     gamma_map = {k: 0 for k in range(bitlen)}
     for elem in array:
         for i in range(bitlen):
-            bit = elem[i]
+            v = [int(x) for x in str(num)]
+            bit = v[i]
             gamma_map[bitlen - i - 1] += bit
     gamma = 0
     for pos, val in gamma_map.items():
