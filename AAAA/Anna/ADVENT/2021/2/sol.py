783415f4967df7785d4745e6e7075f341a0164a6
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 13:48:12 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/2/sol.py

--- AAAA/Anna/ADVENT/2021/2/sol.py
+++ AAAA/Anna/ADVENT/2021/2/sol.py
@@ -9,7 +9,7 @@ def builddata():
     for x in range(1000):
         val = random.randint(0, 2)
         if val == 0:
-            mystr += 'avant '
+            subtab.append('avant ')
         if val == 1:
             mystr += 'bas '  
         if val == 2:
