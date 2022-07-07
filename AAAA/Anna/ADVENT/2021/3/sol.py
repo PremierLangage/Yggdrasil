556a731d28fbebe508e32becba7ba69ac300c5fa
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 11:07:15 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/3/sol.py

--- AAAA/Anna/ADVENT/2021/3/sol.py
+++ AAAA/Anna/ADVENT/2021/3/sol.py
@@ -10,7 +10,7 @@ def builddata():
             strnb.append(str(random.randint(0, 1)))
         myst = ''.join([v for v in strnb])
         tab.append(myst)
-        tab.appen
+        tab.append(myst.type)
     return tab
 
 def ToList(filec):
