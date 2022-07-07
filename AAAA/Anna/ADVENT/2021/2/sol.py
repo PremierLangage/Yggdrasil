c74d9df86538cba5b7be9be83b7cad37f6f9b4c7
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 13:37:57 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/2/sol.py

--- AAAA/Anna/ADVENT/2021/2/sol.py
+++ AAAA/Anna/ADVENT/2021/2/sol.py
@@ -26,13 +26,13 @@ def ToList(filec):
 def Question1(array): 
     position = [0,0]
     for elem in array:
-    input = elem.split(" ")
-    if input[0][0] == "avant":
-        position[0] += int(input[1])
-    elif input[0][0] == "haut":
-        position[1] -= int(input[1])
-    elif input[0][0] == "bas":
-        position[1] += int(input[1])
+        input = elem.split(" ")
+        if input[0][0] == "avant":
+            position[0] += int(input[1])
+        elif input[0][0] == "haut":
+            position[1] -= int(input[1])
+        elif input[0][0] == "bas":
+            position[1] += int(input[1])
     return position[0] * position[1]
 
 def Question2(array):
