24a21a8b2ce1f618ed707cd4d7acd05d51ae9569
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 10:24:50 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/3/sol.py

--- AAAA/Anna/ADVENT/2021/3/sol.py
+++ AAAA/Anna/ADVENT/2021/3/sol.py
@@ -11,36 +11,3 @@ def builddata():
         tab.append('\n'.join([ str(v) for v in strnb]))
     return tab
 
-def ToList(filec):
-    return [int(x) for x in filec.split('\n')] 
-
-def Question1(array):
-    isIncreased = 0
-    for i in range(1, len(array)):
-        if array[i] > array[i-1]:
-            isIncreased += 1
-    return isIncreased
-
-def Question2(array):
-    isIncreased = 0
-    for i in range(3, len(array)):
-        a = array[i - 3] + array[i - 2] + array[i - 1]
-        b = array[i - 2] + array[i - 1] + array[i]
-        if a < b:
-            isIncreased += 1
-    return isIncreased
-
-def buildQ1(fichier,data):
-    val = ToList(fichier)
-    print(Question1(val))
-    dataliteral = '\n'.join([ str(v) for v in data])
-    return f"""[('''{fichier}''','{Question1(val)}','Tst exemple question1'),
-     ('''{dataliteral}''','{Question1(data)}','Tst data question1')]""", dataliteral
-
-def buildQ2(fichier,data):
-    val = ToList(fichier)
-    dataliteral = '\n'.join([ str(v) for v in data])
-    return f"""[('''{fichier}''','{Question2(val)}','Tst exemple question2'),
-     ('''{dataliteral}''','{Question2(data)}','Tst data question2')]""", dataliteral
-
-
