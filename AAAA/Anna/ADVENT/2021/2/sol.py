3e6f6b27652a04cc83b63d7787e67ef2be79db44
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-07 14:40:01 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/2/sol.py

--- AAAA/Anna/ADVENT/2021/2/sol.py
+++ AAAA/Anna/ADVENT/2021/2/sol.py
@@ -4,8 +4,9 @@ from itertools import zip_longest
 
 def builddata():
     tab = []
-    subtab = []
+    
     for x in range(1000):
+        subtab = []
         val = random.randint(0, 2)
         val1 = random.randint(1, 9)
         if val == 0:
@@ -38,13 +39,18 @@ def Question2(array):
 
 def buildQ1(fichier,data):
     val = ToList(fichier)
-    dataliteral = '\n'.join([v[0] + ' ' + v[1] for v in data])
+    literal = '\n'
+    for input in data:
+        dataliteral = literal.join(input)
     return f"""[('''{fichier}''','{Question1(val)}','Tst exemple question1'),
      ('''{dataliteral}''','{Question1(data)}','Tst data question1')]""", dataliteral
 
 def buildQ2(fichier,data):
     val = ToList(fichier)
-    dataliteral = '\n'.join([v[0] + ' ' + v[1] for v in data])
+    dataliteral = '\n'
+    for input in data:
+        dataliteral.join(input[0])
+        dataliteral.join(input[1])
     return f"""[('''{fichier}''','{Question2(val)}','Tst exemple question2'),
      ('''{dataliteral}''','{Question2(data)}','Tst data question2')]""", dataliteral
 
