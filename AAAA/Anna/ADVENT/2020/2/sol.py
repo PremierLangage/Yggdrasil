1e4b626e9ad1560312b1fb6abc5cbd90994aaef4
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-08 07:44:46 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2020/2/sol.py

--- AAAA/Anna/ADVENT/2020/2/sol.py
+++ AAAA/Anna/ADVENT/2020/2/sol.py
@@ -67,8 +67,7 @@ def buildQ1(fichier,data):
 def buildQ2(fichier,data):
     val = ToList(fichier)
     dataliteral = fichier
-    return f"""[('''{fichier}''','{Question2(val)}','Tst exemple question2'),
-     ('''{dataliteral}''','{Question2(data)}','Tst data question2')]""", dataliteral
+    return f"""[('''{fichier}''','{Question2(val)}','Tst exemple question2')""", dataliteral
 
 
 
