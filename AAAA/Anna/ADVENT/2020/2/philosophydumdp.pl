b9b7e2b36731309f2c9e0b4eaf716cab056ada19
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-07-08 07:10:17 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2020/2/philosophydumdp.pl

--- AAAA/Anna/ADVENT/2020/2/philosophydumdp.pl
+++ AAAA/Anna/ADVENT/2020/2/philosophydumdp.pl
@@ -35,7 +35,7 @@ et 4125600 pour la question 2.
 before==
 import build
 
-data = build.builddata(lexemple)
+data = build.toList(lexemple)
 
 with open("ennonce1.md","r") as f:
     lenonce= f.read()
