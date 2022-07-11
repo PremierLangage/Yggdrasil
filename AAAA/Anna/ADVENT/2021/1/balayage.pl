56de850ef0b58efd6906643d56000629e9336e12
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-07-11 10:25:58 +0200

    drevuz2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/1/balayage.pl

--- AAAA/Anna/ADVENT/2021/1/balayage.pl
+++ AAAA/Anna/ADVENT/2021/1/balayage.pl
@@ -21,7 +21,10 @@ La liste des informations est dans le fichier "data" ci-dessous.
 Vous pouvez tester votre fonction avec le fichier "exemple" ci-dessous dont le résultat est 1154 pour la question 1 
 et 1127 pour la question 2.
 
-<details><summary>exemple</summary><blockquote style="font-size:0.5em;white-space: pre-line">
+<details><summary>exemple</summary>
+<blockquote id="ex_value" 
+onclick="let a=document.getElementById(id);a.content.select();document.execCommand('copy')"
+style="font-size:0.5em;white-space: pre-line">
   {{lexemple}}
 </blockquote></details>
 
