87c22eb5f8df1b34ac8d595933ffd6a88830757d
tony fevrier <tony.fevrier62@gmail.com>
2022-07-22 16:07:22 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/evPB.pl

--- AAAA/Tony/Testpositionnement/questionL2/evPB.pl
+++ AAAA/Tony/Testpositionnement/questionL2/evPB.pl
@@ -11,13 +11,13 @@ minright = 2
 maxright = 2
 
 right== 
-$!\{(x,y,z)\in\mathbb{R}^3|x+y=0\}!$
-$!\{y\in C^2(\mathbb{R})|y''+2y'-y=0\}!$
+$!\\{(x,y,z)\in\mathbb{R}^3|x+y=0\\}!$
+$!\\{y\in C^2(\mathbb{R})|y''+2y'-y=0\\}!$
 ==
 
 wrong==
 $!\mathbb{R}_2[X]!$
-$!\{(x,y,z)\in\mathbb{R}^3|x+y=0,z=0\}!$
+$!\\{(x,y,z)\in\mathbb{R}^3|x+y=0,z=0\\}!$
 ==
 
 scoring = CorrectItems
