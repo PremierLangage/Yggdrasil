1f9b7151ab711b90323bc6e283a6467d4a75ce6c
tony fevrier <tony.fevrier62@gmail.com>
2022-07-22 16:21:03 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/lineairePB.pl

--- AAAA/Tony/Testpositionnement/questionL2/lineairePB.pl
+++ AAAA/Tony/Testpositionnement/questionL2/lineairePB.pl
@@ -10,22 +10,19 @@ minright = 3
 maxright = 3
 
 right==
-$!\begin{array}{ll}\varphi : &\R_2[X]\longrightarrow \R_2[X] \\\\&P \mapsto P'+P\end{array}!$
+$!\begin{array}{ll}\varphi : &\mathbb{R}_2[X]\longrightarrow \mathbb{R}_2[X] \\\\&P \mapsto P'+P\end{array}!$
 $!\begin{array}{ll}
-\\varphi : &C^0([0,1])\\longrightarrow \\R \\\\
+\\varphi : &C^0([0,1])\\longrightarrow \mathbb{R} \\\\
    &f \mapsto \int_0^1f(t)dt
 \end{array}!$
 $!\begin{array}{ll}
-\\varphi : &\\R^3\\longrightarrow \\R \\\\
+\\varphi : &\mathbb{R}^3\\longrightarrow \mathbb{R} \\\\
    &(x,y,z) \mapsto x-2y+3z
    \end{array}!$
 ==
 
 wrong==
-$!\begin{array}{ll}
-\varphi : &\R^2\longrightarrow \R \\\\
-   &(x,y) \mapsto x+y+1
-   \end{array}!$ 
+$!\begin{array}{ll}\varphi : &\mathbb{R}^2\longrightarrow \mathbb{R} \\\\&(x,y) \mapsto x+y+1\end{array}!$ 
 ==
 
 scoring = CorrectItems 
