dc7eac04c98e03616d7842924e5c4049afeaa37b
tony fevrier <tony.fevrier62@gmail.com>
2022-07-22 16:15:56 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/matricePB.pl

--- AAAA/Tony/Testpositionnement/questionL2/matricePB.pl
+++ AAAA/Tony/Testpositionnement/questionL2/matricePB.pl
@@ -13,21 +13,13 @@ minright = 1
 maxright = 1
 
 right==
-$!\begin{pmatrix}
-    1&1&0\\\\0&1&2\\\\0&0&1
-    \end{pmatrix}!$ 
+$!\begin{pmatrix}    1&1&0\\\\0&1&2\\\\0&0&1    \end{pmatrix}!$ 
 ==
 
 wrong==
-$!\begin{pmatrix}
-    1&1&0\\\\0&1&0\\\\0&2&1
-    \end{pmatrix!$
-$!\begin{pmatrix}
-    2&2&1\\\\0&2&1\\\\0&0&2
-    \end{pmatrix}!$
-$!\begin{pmatrix}
-    -1&1&1\\\\1&2&1\\\\4&5&6
-    \end{pmatrix}!$
+$!\begin{pmatrix}    1&1&0\\\\0&1&0\\\\0&2&1    \end{pmatrix!$
+$!\begin{pmatrix}    2&2&1\\\\0&2&1\\\\0&0&2    \end{pmatrix}!$
+$!\begin{pmatrix}    -1&1&1\\\\1&2&1\\\\4&5&6    \end{pmatrix}!$
 ==
 
 scoring = CorrectItems
