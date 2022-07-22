e3e66aa2e3929494dab453012f496e210340ba78
tony fevrier <tony.fevrier62@gmail.com>
2022-07-22 16:17:30 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/matricePB.pl

--- AAAA/Tony/Testpositionnement/questionL2/matricePB.pl
+++ AAAA/Tony/Testpositionnement/questionL2/matricePB.pl
@@ -3,7 +3,7 @@ extends= /model/basic/checkbox_rw.pl
  
 question==
 L'application $!\begin{array}{ll}
-\varphi : &\R_2[X]\longrightarrow \R_2[X] \\\\
+\varphi : &\mathbb{R}_2[X]\longrightarrow \mathbb{R}_2[X] \\\\
    &P \mapsto P+P'
 \end{array}!$ a pour matrice dans la base canonique : 
 ==
@@ -17,7 +17,7 @@ $!\begin{pmatrix}    1&1&0\\\\0&1&2\\\\0&0&1    \end{pmatrix}!$
 ==
 
 wrong==
-$!\begin{pmatrix}1&1&0\\\\0&1&0\\\\0&2&1end{pmatrix}!$
+$!\begin{pmatrix}   1&1&0\\\\0&1&0\\\\0&2&1 \end{pmatrix}!$
 $!\begin{pmatrix}    2&2&1\\\\0&2&1\\\\0&0&2    \end{pmatrix}!$
 $!\begin{pmatrix}    -1&1&1\\\\1&2&1\\\\4&5&6    \end{pmatrix}!$
 ==
