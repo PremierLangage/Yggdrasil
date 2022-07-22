291ebee44777b594a3b97c0679f3800f57fd34c5
tony fevrier <tony.fevrier62@gmail.com>
2022-07-22 16:10:51 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/lineairePB.pl

--- AAAA/Tony/Testpositionnement/questionL2/lineairePB.pl
+++ AAAA/Tony/Testpositionnement/questionL2/lineairePB.pl
@@ -11,15 +11,15 @@ maxright = 3
 
 right==
 $!\begin{array}{ll}
-\\varphi : &\\R_2[X]\longrightarrow \\R_2[X] \\\\
+\\varphi : &\\R_2[X]\\longrightarrow \\R_2[X] \\\\
    &P \mapsto P'+P
 \end{array}!$
 $!\begin{array}{ll}
-\varphi : &C^0([0,1])\longrightarrow \R \\\\
+\\varphi : &C^0([0,1])\\longrightarrow \\R \\\\
    &f \mapsto \int_0^1f(t)dt
 \end{array}!$
 $!\begin{array}{ll}
-\varphi : &\R^3\longrightarrow \R \\\\
+\\varphi : &\\R^3\\longrightarrow \\R \\\\
    &(x,y,z) \mapsto x-2y+3z
    \end{array}!$
 ==
