140eb8937f50ab85a29b397862eacabb4b1d69b6
tony fevrier <tony.fevrier62@gmail.com>
2022-08-26 13:15:13 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL1/systemelin.pl

---
+++ AAAA/Tony/Testpositionnement/questionL1/systemelin.pl
@@ -0,0 +1,32 @@
+extends= /model/basic/checkbox_rw.pl
+
+
+title = Solutions de systèmes linéaires
+
+question==
+Lesquels de ces vecteurs sont solutions du système $!\left \\{
+\begin{array}{rcl}
+x+y-z&=&0 \\\\
+y+z&=&0
+\end{array}
+\right.!$?
+==
+ 
+
+nbitems=4
+minright = 2
+maxright = 2
+
+right==
+$!(0,0,0)!$
+$!(1,1,2)!$
+==
+
+wrong==
+$!(2,1,-1)!$
+$!(0,1,0)!$
+==
+
+scoring = CorrectItems
+
+ 
