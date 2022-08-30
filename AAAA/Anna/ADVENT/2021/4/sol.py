97e22aa9fcef9bfed0d6a3b8cb34868e4ec16f22
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-08-30 17:04:23 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -60,7 +60,7 @@ def Question1(array):
     for j in range(len(array) - 2):
         boards[j] = Board()
         boards[j].read_from_lines(lines[(2 + j*6):(2+5+(j+1)*6)])
-    winner_index, called_number = find_first_winner(array[0], boards)    
+    winner_index, called_number = find_first_winner(called_numbers, boards)    
     print('score', boards[winner_index].calculate_score(called_number))
 
 def Question2(array):
