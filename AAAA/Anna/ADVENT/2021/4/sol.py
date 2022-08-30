137ccffda8f5a75d4ab54b2de32f42653dd32cf9
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-08-30 17:05:59 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -77,32 +77,12 @@ def Question1(array):
     return boards[winner_index].calculate_score(called_number))
 
 def Question2(array):
-    ar1 = array
-    ar2 = array
-    position = 0
-    while position < len(array[0]):
-        if len(ar1) == 1 and len(ar2) == 1:
-            break
-        if len(ar1) > 1:
-            map1 = {k: [] for k in [0,1]}
-            for elem in ar1:
-                bit = int(elem[position])
-                map1[bit].append(elem)
-            if len(map1[0]) > len(map1[1]):
-                ar1 = map1[0]
-            else:
-                ar1 = map1[1]
-        if len(ar2) > 1:
-            map2 = {k: [] for k in [0,1]}
-            for elem in ar2:
-                bit = int(elem[position])
-                map2[bit].append(elem)
-            if len(map2[1]) < len(map2[0]):
-                ar2 = map2[1]
-            else:
-                ar2 = map2[0]
-        position += 1
-    return int(ar1[0], 2) * int(ar2[0], 2)
+    boards = dict()
+    for j in range(len(array) - 2):
+        boards[j] = Board()
+        boards[j].read_from_lines(lines[(2 + j*6):(2+5+(j+1)*6)])
+    winner_index, called_number = find_last_winner(array[0], boards)    
+    return boards[winner_index].calculate_score(called_number))
 
 
 def buildQ1(fichier,data):
