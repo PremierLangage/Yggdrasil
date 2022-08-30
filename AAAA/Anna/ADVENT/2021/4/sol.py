e8576e299b7a8711b3e983da3d319d237e2fdb12
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-08-30 17:04:13 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -25,11 +25,43 @@ def builddata():
 def ToList(filec):
     return [x for x in filec.split('\n')] 
 
+class Board:
+    def __init__(self):
+        self.board = np.zeros((5,5), dtype=int)
+        self.marked = np.zeros((5,5))
+
+    def read_from_lines(self, lines):
+        for i in range(5):
+            line_entries = [int(entry) for entry in lines[i].split(' ') if entry != '']
+            self.board[i] = line_entries
+    
+    def check_called_number(self, called_number):
+        if called_number in self.board:
+            indices = np.where(self.board == called_number)
+            self.marked[indices[0], indices[1]] = 1
+
+    def check_win(self):
+        return self.marked.all(axis=0).any() or self.marked.all(axis=1).any()
+
+    def calculate_score(self, called_number):
+        return (self.board * (self.marked==0)).sum() * called_number
+
+def find_first_winner(called_numbers, boards):
+    for called_number in called_numbers:
+        for j in range(len(boards)):
+            boards[j].check_called_number(called_number)
+            if boards[j].check_win():
+                print(f"Board {j+1} won!")
+                print(boards[j].marked)
+                return j, called_number
+
 def Question1(array): 
-    pol = polarity(array)
-    gamma = sum([2**i for i, x in enumerate(reversed(pol)) if x > 0])
-    epsilon = sum([2**i for i, x in enumerate(reversed(pol)) if x < 0])
-    return gamma * epsilon
+    boards = dict()
+    for j in range(len(array) - 2):
+        boards[j] = Board()
+        boards[j].read_from_lines(lines[(2 + j*6):(2+5+(j+1)*6)])
+    winner_index, called_number = find_first_winner(array[0], boards)    
+    print('score', boards[winner_index].calculate_score(called_number))
 
 def Question2(array):
     ar1 = array
