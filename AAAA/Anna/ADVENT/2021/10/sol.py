6777988a490093dfb8fd978c95fe419090fc6a83
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-08-30 13:41:31 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/10/sol.py

--- AAAA/Anna/ADVENT/2021/10/sol.py
+++ AAAA/Anna/ADVENT/2021/10/sol.py
@@ -41,9 +41,9 @@ def Question2(array):
 
 def buildQ1(fichier,data):
     val = ToList(fichier)
-    dataliteral = '\n'.join([ str(v) for v in data])
+   
     return f"""[('''{fichier}''','{Question1(val)}','Tst exemple question1'),
-     ('''{dataliteral}''','{Question1(data)}','Tst data question1')]""", dataliteral
+     ]""", dataliteral
 
 def buildQ2(fichier,data):
     val = ToList(fichier)
