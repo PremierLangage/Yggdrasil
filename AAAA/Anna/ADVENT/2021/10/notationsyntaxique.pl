b8eab7c416c5180e63f7d7d3e4225515b353575b
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-08-30 14:13:29 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/10/notationsyntaxique.pl

--- AAAA/Anna/ADVENT/2021/10/notationsyntaxique.pl
+++ AAAA/Anna/ADVENT/2021/10/notationsyntaxique.pl
@@ -33,7 +33,7 @@ La liste des informations est dans le fichier "data" ci-dessous.
 
 Vous pouvez tester votre fonction avec le fichier "exemple" ci-dessous
  dont le résultat est 288291 pour la question 1 
-et 1127 pour la question 2.
+et 820045242 pour la question 2.
 
 <div style="font-size:0.5em">
 <details><summary>Fichier exemple <button onclick="docopy('exemple')">Copy exemple</button></summary><blockquote id="exemple" style="white-space: pre-line" 
