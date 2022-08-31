ed581373ab4e63354f9398a9ade79416a5a76e94
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-08-31 17:52:01 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -24,7 +24,7 @@ def builddata():
     return final_tab
 
 def ToList(filec):
-    return [entry.strip() for entry in filec.split('\n')]
+    return [entry.strip() for entry in filec.split('\n\n')]
 
 class Board:
     def __init__(self):
