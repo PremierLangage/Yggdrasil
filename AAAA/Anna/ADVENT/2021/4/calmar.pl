26381cee4c84375b1f5ad52c4254979afd64439b
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 01:45:57 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/calmar.pl

--- AAAA/Anna/ADVENT/2021/4/calmar.pl
+++ AAAA/Anna/ADVENT/2021/4/calmar.pl
@@ -34,7 +34,7 @@ et 7075 pour la question 2.
 before==
 import build
 tab = build.builddata()
-called_numbers,boards,data = build.called_numbers,boards,data
+called_numbers,boards,data = build.tabToBoards(tab)
 
 with open("ennonce1.md","r") as f:
     lenonce= f.read()
