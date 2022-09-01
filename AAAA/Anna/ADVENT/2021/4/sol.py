62fc4f3c6cad86c62dda25b3ec6301c098422075
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 01:53:08 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -36,7 +36,7 @@ def builddata():
                 val = random.randint(0, 99)
                 if val not in numbers : 
                     numbers.append(val)
-                    num += str(val) + " "
+                    num += str(val) + ","
                 lit += str(val) + " "
             lit += "\n"
             tab.append(lit)
