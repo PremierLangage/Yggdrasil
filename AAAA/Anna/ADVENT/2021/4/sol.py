fcb4ce34c210d43d1cc8194e1f81fd695eaec953
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 01:51:32 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -40,7 +40,7 @@ def builddata():
                 lit += str(val) + " "
             lit += "\n"
             tab.append(lit)
-    final_tab.append(numbers)
+    final_tab.append(num)
     final_tab.extend(tab)
     return final_tab
 
