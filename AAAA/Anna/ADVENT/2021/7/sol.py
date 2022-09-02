8a9be3d5c82ac1049e3d9da075382f90c26b4f8f
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 07:30:41 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/7/sol.py

--- AAAA/Anna/ADVENT/2021/7/sol.py
+++ AAAA/Anna/ADVENT/2021/7/sol.py
@@ -35,33 +35,7 @@ def calculate_fuel2(positions):
     return min_fuel
 
 def Question2(array):
-    ar1 = array
-    ar2 = array
-    position = 0
-    while position < len(array[0]):
-        if len(ar1) == 1 and len(ar2) == 1:
-            break
-        if len(ar1) > 1:
-            map1 = {k: [] for k in [0,1]}
-            for elem in ar1:
-                bit = int(elem[position])
-                map1[bit].append(elem)
-            if len(map1[0]) > len(map1[1]):
-                ar1 = map1[0]
-            else:
-                ar1 = map1[1]
-        if len(ar2) > 1:
-            map2 = {k: [] for k in [0,1]}
-            for elem in ar2:
-                bit = int(elem[position])
-                map2[bit].append(elem)
-            if len(map2[1]) < len(map2[0]):
-                ar2 = map2[1]
-            else:
-                ar2 = map2[0]
-        position += 1
-    return int(ar1[0], 2) * int(ar2[0], 2)
-
+    return calculate_fuel2(np.array(positions))
 
 def buildQ1(fichier,data):
     val = ToList(fichier)
