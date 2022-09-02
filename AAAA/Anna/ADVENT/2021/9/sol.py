35f2927d51548a36238f34b7096d6775200bc4fa
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 10:57:57 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/9/sol.py

--- AAAA/Anna/ADVENT/2021/9/sol.py
+++ AAAA/Anna/ADVENT/2021/9/sol.py
@@ -17,77 +17,10 @@ def builddata():
 def ToList(filec):
     return [x.strip() for x in filec.split('\n')] 
 
-def create_grid(lines):
-    grid = np.zeros((len(lines), len(lines[0])), dtype=int)
-    for index, line in enumerate(lines):
-        entries = [int(entry) for entry in list(line)]
-        grid[index] = entries[0:len(lines)]
-    return grid
-
-def get_neighbors(x, y, grid):
-    neighbors = []
-    if x+1 < grid.shape[0]:
-        neighbors.append(grid[x+1, y])
-    if x-1 >= 0:
-        neighbors.append(grid[x-1, y])
-    if y+1 < grid.shape[1]:
-        neighbors.append(grid[x, y+1])
-    if y-1 >= 0:
-        neighbors.append(grid[x, y-1])
-    return neighbors
-
-def is_low_point(x, y, grid):
-    neighbors = get_neighbors(x, y, grid)
-    return all([grid[x, y] < neighbor for neighbor in neighbors])
-        
-def get_score(x, y, grid):
-    return grid[x, y] + 1
-
-def Question1(lines):
-    score = 0
-    grid = create_grid(lines)
-    for x in range(grid.shape[0]):
-        for y in range(grid.shape[1]):
-            if is_low_point(x, y, grid):
-                score += get_score(x, y, grid)
-    return score
-
-def count_basin_neighbors(x, y, grid, visited):
-    # print('x', x, 'y', y)
-    visited[x, y] = 1
-    count = 0
-    if x+1 < grid.shape[0]:
-        if grid[x+1, y] < 9:
-            if not visited[x+1, y]:
-                count += 1 + count_basin_neighbors(x+1, y, grid, visited)
-    if x-1 >= 0:
-        if grid[x-1, y] < 9:
-            if not visited[x-1, y]:
-                count += 1 + count_basin_neighbors(x-1, y, grid, visited)
-    if y+1 < grid.shape[1]:
-        if grid[x, y+1] < 9:
-            if not visited[x, y+1]:
-                count += 1 + count_basin_neighbors(x, y+1, grid, visited)
-    if y-1 >= 0:
-        if grid[x, y-1] < 9:
-            if not visited[x, y-1]:
-                count += 1 + count_basin_neighbors(x, y-1, grid, visited)
-    return count
-
-def get_basin_size(x, y, grid):
-    visited = np.zeros(grid.shape, dtype=int)
-    size = count_basin_neighbors(x, y, grid, visited) + 1
-    return size
-
-def Question2(lines):
-    basin_sizes = []
-    grid = create_grid(lines)
-    for x in range(grid.shape[0]):
-        for y in range(grid.shape[1]):
-            if is_low_point(x, y, grid):
-                basin_sizes.append(get_basin_size(x, y, grid))
-    basin_sizes.sort(reverse=True)
-    return basin_sizes[0]*basin_sizes[1]*basin_sizes[2]
+def create_grid(tab):
+    grid = []
+    for input in tab:
+        grid.append(list(map(int, input)))
 
 def buildQ1(fichier,data):
     val = ToList(fichier)
