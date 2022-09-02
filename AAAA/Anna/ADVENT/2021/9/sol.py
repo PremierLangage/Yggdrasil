266aa460275c14ad4f8c2afa653905a012dd8883
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 11:07:01 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/9/sol.py

--- AAAA/Anna/ADVENT/2021/9/sol.py
+++ AAAA/Anna/ADVENT/2021/9/sol.py
@@ -21,7 +21,7 @@ def ToList(filec):
 def create_grid(tab):
     grid = []
     for input in tab:
-        grid.append(list(map(int, input.split('\n'))))
+        grid.append(list(map(int, input.strip(''))))
     return grid
 
 def get_adjacents(i, j):
