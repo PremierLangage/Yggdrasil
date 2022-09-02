538a6440ac55c11398598158730642fa8b37eea8
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 04:50:28 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/5/sol.py

--- AAAA/Anna/ADVENT/2021/5/sol.py
+++ AAAA/Anna/ADVENT/2021/5/sol.py
@@ -24,6 +24,23 @@ def enter_line(grid, x1, y1, x2, y2):
         for x in range(min(x1, x2), max(x1, x2)+1):
             grid[y1, x] += 1
 
+def enter_line_extended(grid, x1, y1, x2, y2):
+    if x1 == x2:
+        for y in range(min(y1, y2), max(y1, y2)+1):
+            grid[y, x1] += 1
+        return
+    if y1 == y2:
+        for x in range(min(x1, x2), max(x1, x2)+1):
+            grid[y1, x] += 1
+        return
+    line_length = abs(x1-x2)+1
+    step_x = 1 if x2-x1 > 0 else -1
+    step_y = 1 if y2-y1 > 0 else -1
+    for i in range(line_length):
+        new_x = x1 + i*step_x
+        new_y = y1 + i*step_y
+        grid[new_y, new_x] += 1
+
 def Question1(lines): 
     max_x = 0
     max_y = 0
@@ -38,7 +55,7 @@ def Question1(lines):
         enter_line(grid, x1, y1, x2, y2)
     return (grid >= 2).sum()
 
-    
+
 def Question2(array):
     seats = (v
         .split()
