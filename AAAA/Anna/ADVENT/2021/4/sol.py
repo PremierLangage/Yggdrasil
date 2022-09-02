a75b0801d581155e7515c5945c28da8e3652cf73
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 02:34:55 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -102,7 +102,7 @@ def buildQ1(fichier,called_numbers,boards,tab):
 
 def buildQ2(fichier,called_numbers,boards,tab):
     val1,val2  = ToBoards(fichier)
-    dataliteral = '\n'.join([ v for v in tab]
+    dataliteral = ([ v for v in tab]
     return f"""[('''{fichier}''','{Question2(val1,val2)}','Tst exemple question2'),
      ('''{dataliteral}''','{Question2(called_numbers,boards)}','Tst data question2')]""", dataliteral
 
