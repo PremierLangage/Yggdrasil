a54b45de163896035e5aabd793c67acadb0aaf29
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 05:42:50 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/5/hydrothermal.pl

--- AAAA/Anna/ADVENT/2021/5/hydrothermal.pl
+++ AAAA/Anna/ADVENT/2021/5/hydrothermal.pl
@@ -40,8 +40,8 @@ data = build.builddata()
 with open("ennonce1.md","r") as f:
     lenonce= f.read()
 
-#with open("exemple","r") as f:
-#    lexemple= [v.strip('\n') for v in f.readlines()]
+with open("exemple","r") as f:
+    lexemple= [v.strip('\n') for v in f.readlines()]
 
 l = '\n'.join([ v for v in lexemple])
 
