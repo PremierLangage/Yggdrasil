5df33834562fea23cc818b4894fd76de4bdd8968
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 03:23:06 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -10,7 +10,7 @@ class Board:
     def read_from_lines(self, lines):
         for i in range(5):
             line_entries = [int(entry) for entry in lines[i].split(' ') if entry != '']
-            if (len(line_entries == 5)):
+            if (line_entries):
                 self.board[i] = line_entries
     
     def check_called_number(self, called_number):
