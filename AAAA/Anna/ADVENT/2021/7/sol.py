b4be65fcf99b7cd3bf90f06abe4dc7fa9efb50f5
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 10:18:51 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/7/sol.py

--- AAAA/Anna/ADVENT/2021/7/sol.py
+++ AAAA/Anna/ADVENT/2021/7/sol.py
@@ -38,7 +38,7 @@ def calculate_fuel2(positions):
             min_pos = pos
     return min_fuel
 
-def Question2(lines):
+def Question2(array):
     positions = [int(entry) for entry in lines[0].strip().split(',')]
     return calculate_fuel2(np.array(positions))
 
