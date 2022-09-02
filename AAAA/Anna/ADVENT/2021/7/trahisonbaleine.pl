30251e394f8732175836841542c9508938761baa
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 07:41:41 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/7/trahisonbaleine.pl

--- AAAA/Anna/ADVENT/2021/7/trahisonbaleine.pl
+++ AAAA/Anna/ADVENT/2021/7/trahisonbaleine.pl
@@ -58,55 +58,80 @@ testcases, donnees = build.buildQ2(lexemple,data)
 
 solution ==
 import sys
+import random
+import re
+import numpy as np
 from itertools import zip_longest
 
-def Question1():
-    array = [x[:-1] for x in sys.stdin.readlines()]
-    polarity = []
-    for string in array:
-        polarity = [
-            s + (1 if x == '1' else -1)
-            for s, x in zip_longest(polarity, string, fillvalue=0)
-        ]
-    gamma = sum([2**i for i, x in enumerate(reversed(polarity)) if x > 0])
-    epsilon = sum([2**i for i, x in enumerate(reversed(polarity)) if x < 0])
-    return gamma * epsilon
+def builddata():
+    tab = []
+    for x in range(1500):
+        val = random.randint(0, 1500)
+        tab.append(val)
+    return tab
+
+def ToList(filec):
+    return [int(x) for x in filec.split(",")] 
     
+def calculate_fuel(positions):
+    min_pos = -1
+    min_fuel = np.infty
+    for pos in range(min(positions), max(positions)+1):
+        fuel_needed = (np.abs(positions - pos)).sum()
+        if fuel_needed < min_fuel:
+            min_fuel = fuel_needed
+            min_pos = pos
+    return  min_fuel
+
+def Question1():
+    array = [x for x in sys.stdin.readlines()]
+    positions = []
+    for x in array[0].split(','):
+        if (x != ''):
+            positions.append(int(x))
+    return calculate_fuel(np.array(positions))
+
 if __name__ == '__main__':
     print(Question1())  
 
 
+
+
+
 import sys
+import random
+import re
+import numpy as np
 from itertools import zip_longest
 
+def builddata():
+    tab = []
+    for x in range(1500):
+        val = random.randint(0, 1500)
+        tab.append(val)
+    return tab
+
+def ToList(filec):
+    return [int(x) for x in filec.split(",")] 
+
+def calculate_fuel2(positions):
+    min_pos = -1
+    min_fuel = np.infty
+    for pos in range(min(positions), max(positions)+1):
+        distances = (np.abs(positions - pos))
+        fuel_needed = sum([(n*(n+1))/2 for n in distances])
+        if fuel_needed < min_fuel:
+            min_fuel = fuel_needed
+            min_pos = pos
+    return min_fuel
+
 def Question2():
-    array = [x[:-1] for x in sys.stdin.readlines()]
-    ar1 = array
-    ar2 = array
-    position = 0
-    while position < len(array[0]):
-        if len(ar1) == 1 and len(ar2) == 1:
-            break
-        if len(ar1) > 1:
-            map1 = {k: [] for k in [0,1]}
-            for elem in ar1:
-                bit = int(elem[position])
-                map1[bit].append(elem)
-            if len(map1[0]) > len(map1[1]):
-                ar1 = map1[0]
-            else:
-                ar1 = map1[1]
-        if len(ar2) > 1:
-            map2 = {k: [] for k in [0,1]}
-            for elem in ar2:
-                bit = int(elem[position])
-                map2[bit].append(elem)
-            if len(map2[1]) < len(map2[0]):
-                ar2 = map2[1]
-            else:
-                ar2 = map2[0]
-        position += 1
-    return int(ar1[0], 2) * int(ar2[0], 2)
+    array = [x for x in sys.stdin.readlines()]
+    positions = []
+    for x in array[0].split(','):
+        if (x != ''):
+            positions.append(int(x))
+    return calculate_fuel2(np.array(positions))
 
 if __name__ == '__main__':
     print(Question2())  
