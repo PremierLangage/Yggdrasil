335970ee0ecb90ef91f11511fbff18ac6c2641b2
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 04:01:57 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/4/sol.py

--- AAAA/Anna/ADVENT/2021/4/sol.py
+++ AAAA/Anna/ADVENT/2021/4/sol.py
@@ -107,7 +107,7 @@ def buildQ1(fichier,tab):
     numbersliteral = '\n'.join([n for n in raw_data[0][0]])
     list_of_cards_literal = '\n'
     for card in raw_data[1:]:
-        raws = car.split('\n')
+        raws = card.split('\n')
         list_of_cards_literal.join([int(n) for n in raw_data[0][1:][0].split(" ")])
     dataliteral = '\n'.join(numbersliteral).join(list_of_cards_literal)
     return f"""[('''{fichier}''','{Question1(val1,val2)}','Tst exemple question1'),
