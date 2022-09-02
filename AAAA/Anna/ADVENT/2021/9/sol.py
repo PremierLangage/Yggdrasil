747a7ecb3fafc319d696cad8f5817a08e7e61e74
ANNA DIAW <anna.diaw2@edu.univ-eiffel.fr>
2022-09-02 11:00:47 +0200

    adiaw2 modified Yggdrasil/AAAA/Anna/ADVENT/2021/9/sol.py

--- AAAA/Anna/ADVENT/2021/9/sol.py
+++ AAAA/Anna/ADVENT/2021/9/sol.py
@@ -44,21 +44,25 @@ def Question1(tab):
                 result += 1 + grid[j][i]
     return result
 
-groups = []
-def count_groups(i, j):
-    if j < 0 or j >= len(grid) or i < 0 or i >= len(grid[0]) or grid[j][i] == 9 or grid[j][i] == -1:
-        return
-    grid[j][i] = -1
-    groups[len(groups)-1] += 1
-    count_groups(i+1, j)
-    count_groups(i-1, j)
-    count_groups(i, j+1)
-    count_groups(i, j-1)
-for i in range(0, len(grid)):
-    for j in range(0, len(grid[0])):
-        groups.append(0)
-        count_groups(j, i)
-print(math.prod(sorted(groups, reverse=True)[:3]))
+def Question2(tab);
+    groups = []
+    grid = create_grid(tab)
+    def count_groups(i, j):
+        if j < 0 or j >= len(grid) or i < 0 or i >= len(grid[0]) or grid[j][i] == 9 or grid[j][i] == -1:
+            return
+        grid[j][i] = -1
+        groups[len(groups)-1] += 1
+        count_groups(i+1, j)
+        count_groups(i-1, j)
+        count_groups(i, j+1)
+        count_groups(i, j-1)
+
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[0])):
+            groups.append(0)
+            count_groups(j, i)
+
+    return math.prod(sorted(groups, reverse=True)[:3])
 
 def buildQ1(fichier,data):
     val = ToList(fichier)
