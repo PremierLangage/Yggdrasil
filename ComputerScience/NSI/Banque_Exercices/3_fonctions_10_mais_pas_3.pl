f4da6439eb03fcf48447c41bb45de83d9b4204ab
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-04 22:55:28 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/3_fonctions_10_mais_pas_3.pl

---
+++ ComputerScience/NSI/Banque_Exercices/3_fonctions_10_mais_pas_3.pl
@@ -0,0 +1,54 @@
+
+#author=OCE
+#réécriture par Emmanuel Brunache
+
+# Heritage d'un type d'exercice 
+extends = /ComputerScience/python/template/pltest.pl
+@ /builder/before.py [builder.py]
+
+title = Dix mais pas trois
+
+code==
+
+
+==
+
+needed=def
+
+text==  
+
+Programmer une fonction `dix_mais_pas_trois` qui teste si un entier (passé en paramètre) est divisible par 10 mais pas par 3.
+
+Retourne `True` si c'est le cas, `False` sinon.
+
+==
+
+solution==#|python|
+def dix_mais_pas_trois(n):
+    return n%10==0 and n%3!=0
+
+n = int(input("Saisie : "))
+print(dix_mais_pas_trois(n))
+==
+
+before==
+def dix_mais_pas_trois(n):
+    return n%10==0 and n%3!=0
+
+pltest = """>>> dix_mais_pas_trois(2)
+{}
+>>> dix_mais_pas_trois(3)
+{}
+>>> dix_mais_pas_trois(30)
+{}
+>>> dix_mais_pas_trois(40)
+{}
+""".format(dix_mais_pas_trois(2), dix_mais_pas_trois(3), dix_mais_pas_trois(30), dix_mais_pas_trois(40))
+==
+
+
+
+
+
+
+
