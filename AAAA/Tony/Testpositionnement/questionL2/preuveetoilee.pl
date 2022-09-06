465b99fd079bc86e958aeb62cba0b8f4246266a0
tony fevrier <tony.fevrier62@gmail.com>
2022-09-06 11:20:45 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL2/preuveetoilee.pl

--- AAAA/Tony/Testpositionnement/questionL2/preuveetoilee.pl
+++ AAAA/Tony/Testpositionnement/questionL2/preuveetoilee.pl
@@ -7,6 +7,7 @@ Voici la preuve de "Si $!|q|<1!$ alors $!\sum q^n!$ converge". Parmi les argumen
  
 $!\begin{array}{ll}\forall N\in\mathbb{N},\quad (1-q)S_N&\underset{(1)}{=}\sum_{n=0}^N(1-q)q^n\\\\&\underset{(2)}{=}\sum_{n=0}^N(q^n-q^{n+1})\\\\&\underset{(3)}{=}\sum_{n=0}^N q^n-\sum_{n=0}^N q^{n+1}\\\\&\underset{(4)}{=}\sum_{n=0}^N q^n-\sum_{n=1}^{N+1} q^{n}\\\\&\underset{(5)}{=} 1-q^{N+1}.\end{array}!$
 
+Donc $!(1-q)S_N!$ tend vers $!1!$ donc $!(S_N)!$ converge. 
 ==
  
 
@@ -17,8 +18,7 @@ maxright = 4
 right== 
 car pour $!|q|<1!$, la suite $!(q^n)!$ converge.
 par linéarité de la somme
-car tous les termes sont communs exceptés le premier et le dernier
-par linéarité de l'intégrale
+car tous les termes sont communs exceptés le premier et le dernier 
 par changement d'indices
 ==
 
