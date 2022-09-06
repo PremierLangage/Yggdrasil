e946ac6037fb249784c353b13c9f3c2863ea6d42
tony fevrier <tony.fevrier62@gmail.com>
2022-09-06 09:02:05 +0200

    tfevrier modified Yggdrasil/AAAA/Tony/Testpositionnement/questionL1/pourcentage.pl

--- AAAA/Tony/Testpositionnement/questionL1/pourcentage.pl
+++ AAAA/Tony/Testpositionnement/questionL1/pourcentage.pl
@@ -16,12 +16,12 @@ maxright = 2
 
 right==
 La variation du volume total du concombre est supérieure à $!40 \\%!$
-La variation du volume d'eau du concombre est inférieure à $!10 \\%!$
 ==
 
 wrong== 
 La variation du volume total du concombre est inférieure à $!30 \\%!$
 Le volume de la partie sans eau a changé.
+La variation du volume d'eau du concombre est inférieure à $!10 \\%!$
 ==
 
 scoring = CorrectItems
