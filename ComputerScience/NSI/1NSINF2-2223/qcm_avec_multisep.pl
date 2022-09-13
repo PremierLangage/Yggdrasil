7c3b9a2662eb5a99d547b5c677bdbbffc9625c17
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-13 09:46:44 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/1NSINF2-2223/qcm_avec_multisep.pl

--- ComputerScience/NSI/1NSINF2-2223/qcm_avec_multisep.pl
+++ ComputerScience/NSI/1NSINF2-2223/qcm_avec_multisep.pl
@@ -151,7 +151,7 @@ if step> -1:
     currentscore=sum(scores)//nbstep
 
 step = step+1
-print(list_questions , file=sys.stderr)
+print(step , file=sys.stderr)
 if step<nbstep:
     q=list_questions[step]
     print(q['type'] , file=sys.stderr)
