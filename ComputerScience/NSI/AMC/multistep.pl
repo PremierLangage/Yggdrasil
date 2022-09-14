a56040ced6c66aabd713b4c5c906264d3f43fd9e
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 19:05:52 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/AMC/multistep.pl

--- ComputerScience/NSI/AMC/multistep.pl
+++ ComputerScience/NSI/AMC/multistep.pl
@@ -1,7 +1,7 @@
 @ /utils/sandboxio.py
 @ /utils/components/scoring.py
 @ /utils/components/radio.py
-@ /utils/components/checkbox.py [customcheckbox.py]
+@ /utils/components/checkbox.py
 @ /utils/components/textselect.py [customtextselect.py]
 
 
