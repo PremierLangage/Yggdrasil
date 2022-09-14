3153aff0ce513ea8518c325ca87fc531c6dc600b
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2022-09-14 10:58:55 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/template/cgbasicgrader.py

--- AAAA/Gaudet/template/cgbasicgrader.py
+++ AAAA/Gaudet/template/cgbasicgrader.py
@@ -41,7 +41,8 @@ if __name__ == "__main__":
     from random import *
     from pathlib import *
     testcases = eval(context['testcases'])
-    showWanted = context.get('showWanted', False)
+    showWanted = context.get('showWanted', True)
+    showWanted = False
     testSuccess = 0
 
     for test, want, name in testcases:
