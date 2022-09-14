f70bd5e0575645c6cc947cf1d62a0c9ed0fb4e28
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 19:04:10 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/AMC/multistep.pl

---
+++ ComputerScience/NSI/AMC/multistep.pl
@@ -0,0 +1,23 @@
+@ /utils/sandboxio.py
+@ /utils/components/scoring.py
+@ /utils/components/radio.py
+@ /utils/components/checkbox.py 
+@ /utils/components/textselect.py [customtextselect.py]
+
+
+#extends = temp.pl
+
+@ /utils/sandboxio.py
+@ /builder/beforestep.py [builder.py]
+@ /grader/evaluatorstep.py [grader.py]
+
+settings.cumulative % false
+
+text =
+form =
+
+title = 
+
+intro ==
+Ce quiz contient {{nbstep}} questions.
+==
