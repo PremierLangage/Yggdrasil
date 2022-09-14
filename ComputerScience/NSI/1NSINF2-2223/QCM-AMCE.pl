663665c400e61833d6606ee79e1eaf63b14f139b
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 18:02:16 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/1NSINF2-2223/QCM-AMCE.pl

--- ComputerScience/NSI/1NSINF2-2223/QCM-AMCE.pl
+++ ComputerScience/NSI/1NSINF2-2223/QCM-AMCE.pl
@@ -90,7 +90,7 @@ title= Cher enseignant vous n'avez pas changer le "title"
 
 before == #|python|
 import random as rd
-#from radio import Radio as CustomRadio
+from radio import Radio as CustomRadio
 from checkbox import CustomCheckbox as CustomCheckbox
 #from customtextselect import CustomTextSelect as CustomTextSelect
 from AMC import parse_AMC_TXT
