246099222a17c69f65e67f36c9fb6991797fa226
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 23:20:18 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
@@ -39,7 +39,7 @@ import sys
 cpt = 0
 for i in range(N):
     box = inputs[i]
-    print(box.eval(),file=sys.stderr)
+    print(inputs[i].eval(),file=sys.stderr)
     try:
         dummy= box.value
     except Exception as e:
