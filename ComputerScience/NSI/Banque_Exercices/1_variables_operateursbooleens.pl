6417ae4db69e9ed4275beea253ab6aba81a03a3d
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 23:15:41 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
@@ -11,7 +11,7 @@ builder =@ /builder/before.py
 nbechec%0
 
 before== #|python|
-from input import Input as CustomInput
+from input import Input
 import random
 essai = 1
 N = 3
@@ -30,7 +30,8 @@ solved=[ False for n in range(N)]
 isfloat = [ False for n in range(N)]
 inputs = []
 for i in range(N):
-    inputs.append(CustomInput())
+    inputs.append(Input())
+inputs[0].eval
 ==
 
 evaluator== #|python|
