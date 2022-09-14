9a90265170f0254a54c0be90e1e864f3724cf92d
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 19:15:23 +0200

    ebrunache2 modified Yggdrasil/utils/components/checkbox.py

--- utils/components/checkbox.py
+++ utils/components/checkbox.py
@@ -6,7 +6,7 @@ from scoring import *
 class Checkbox(Component):
     pass
 
-class CustomCheckbox(Checkbox):
+class CustomCheckbox(Component):
 
     def __init__(self, **kwargs):
         self.selector = 'c-checkbox-group'
