d1103d7901fea583d9acff1f0a8f225077d867d4
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 22:15:02 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
@@ -78,7 +78,7 @@ form== #|html|
 <ul>
 {% for input in inputs %}
 {% if not solved[loop.index0]%}
-<li>  {{enonce[loop.index0]}}   </li>
+<li>  {{enonce[loop.index0]}} = {{ input|component }}  </li>
 {% endif %}
 {% endfor %}
 </ul>
