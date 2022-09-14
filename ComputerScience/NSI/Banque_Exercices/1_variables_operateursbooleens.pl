ce22e2fc8054b7927cad8efd3e9cb0d3b9749a1c
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-09-14 22:45:43 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_operateursbooleens.pl
@@ -27,7 +27,7 @@ solved=[ False for n in range(N)]
 isfloat = [ False for n in range(N)]
 inputs = []
 for i in range(N):
-    inp = {"cid": i, "selector": "c-input"}
+    inp = TextInput()
     globals()[f"input{i}"] = inp
     inputs.append(inp)
 ==
