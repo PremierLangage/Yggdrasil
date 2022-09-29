6df9616e05a9bc9d0896b17702fe07788b5f7b2e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-09-29 12:09:33 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/bascule_en.pl

---
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/bascule_en.pl
@@ -0,0 +1,17 @@
+
+
+
+
+title= A question
+
+text==
+The goal of this exercise is to have a line of code of the form:
+
+     var = expression
+
+such that the value of *var* passes successively from the value **{{val1}}** to the value **{{val2}}**
+then from the value **{{val2}}** to the value **{{val1}}**, each time the line is executed.
+
+Write the expression that does this.
+We assume that var is initialized to 0.
+==
