cdb5361fc4ad6066877a7711ad1b23c215f03509
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-09-29 12:10:18 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/bascule_en.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/bascule_en.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/bascule_en.pl
@@ -2,7 +2,7 @@
 
 
 
-title= A question
+title= A swing
 
 text==
 The goal of this exercise is to have a line of code of the form:
@@ -12,6 +12,6 @@ The goal of this exercise is to have a line of code of the form:
 such that the value of *var* passes successively from the value **{{val1}}** to the value **{{val2}}**
 then from the value **{{val2}}** to the value **{{val1}}**, each time the line is executed.
 
-Write the expression that does this.
+Write the __expression__ that does this.
 We assume that var is initialized to 0.
 ==
