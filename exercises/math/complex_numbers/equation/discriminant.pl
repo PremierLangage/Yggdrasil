da871b344644eac2daf04d0a289627c5a0c88c86
David DOYEN <david.doyen@univ-eiffel.fr>
2022-09-30 15:44:55 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/equation/discriminant.pl

--- exercises/math/complex_numbers/equation/discriminant.pl
+++ exercises/math/complex_numbers/equation/discriminant.pl
@@ -8,12 +8,11 @@ a = rand_complex_int(3)
 b = rand_complex_int(3)
 c = rand_complex_int(4)
 x = Symbol('z')
-latexP=latex(poly(a*x**2+b*x+c,x))
+P = a*x**2+b*x+c
 
-sol=(b*b-4*a*c).expand()
-sol_tex=latex(sol)
+sol = (b*b-4*a*c).expand()
 ==
 
 question ==
-Calculer le discriminant de l'équation $! {{latexP}} = 0 !$.
+Calculer le discriminant de l'équation $! {{P|latex}} = 0 !$.
 ==
