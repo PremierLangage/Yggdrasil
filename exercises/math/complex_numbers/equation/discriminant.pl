9d27637c6a66ed701b820798a71eee1c17a8c409
David DOYEN <david.doyen@univ-eiffel.fr>
2022-09-30 15:33:07 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/equation/discriminant.pl

---
+++ exercises/math/complex_numbers/equation/discriminant.pl
@@ -0,0 +1,31 @@
+extends = /model/mathinput.pl
+
+title = Discriminant d'une équation du second degré
+
+
+before ==
+
+a=rand_complex_int(3)
+b=rand_complex_int(3)
+c=rand_complex_int(4)
+var('x')
+latexP=latex(poly(a*x**2+b*x+c,x))
+
+sol=(b*b-4*a*c).expand()
+sol_tex=latex(sol)
+==
+
+text ==
+Calculer le discriminant de l'équation $! {{latexP}} = 0 !$.
+==
+
+evaluator==
+score, error = eval_set_complex(input.value, sol,wobracket=True)
+feedback = message[error]
+==
+
+solution ==
+Le discriminant est $! {{sol_tex}} !$.
+==
+
+
