f8e2ea3b6a390e82d4152a0bb9f6740f78126850
Thomas SAILLARD <thomas.saillard2@univ-eiffel.fr>
2022-10-06 11:21:56 +0200

    tsaillard2 modified Yggdrasil/AAAA/Orhan/grader.py

--- AAAA/Orhan/grader.py
+++ AAAA/Orhan/grader.py
@@ -54,5 +54,9 @@ if __name__ == "__main__":
     if 'grade' not in dic:
         print(missing_grade_stderr, file=sys.stderr)
         sys.exit(1)
+
+    a = ""
+    with open(sys.argv[2], "r") as f:
+        a = f.read()
     
-    output(dic['grade'][0], str(open("answer.json", "r")), dic)
+    output(dic['grade'][0], a, dic)
