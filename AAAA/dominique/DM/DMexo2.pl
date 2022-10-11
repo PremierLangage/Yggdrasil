22ae8e9c870dc9bf679d9363b5829c4236afdd8c
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-10-11 15:30:51 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/DM/DMexo2.pl

---
+++ AAAA/dominique/DM/DMexo2.pl
@@ -0,0 +1,29 @@
+
+extends=DMexo1.pl
+
+
+
+
+title= DM - Print terrain 
+tag=DM
+
+text==#|markdown|
+Écrire une function **nbmines_t** qui prend  
+
+- l'adresse d'un vecteur d'entier (pointeur vers un tableau de hauteur pointeurs sur des tableaux de largeur entiers)  
+- un entier representant la hauteur du terrain  
+- un entier la largeur  
+- un entier i  
+- un entier j  
+et qui retourne le nombre de mines sur les 8 cases adjacente de la case de coordonées i,j
+affiche le terrain sous forme d'entiers par exemple 
+
+    0 0 9 9 0 9 0 0 0 0 
+    0 2 0 3 0 0 0 0 0 0 
+    0 1 9 0 0 0 0 0 0 0 
+    0 0 0 0 0 0 0 0 9 9 
+    0 0 0 0 0 0 0 0 9 0 
+    0 0 0 0 0 0 0 0 0 0 
+
+rappel: on réalise un https://fr.wikipedia.org/wiki/D%C3%A9mineur_(genre_de_jeu_vid%C3%A9o)
+==
