c431bdcaf47fd1c8e833d9e3bb5626e1baa66a39
Julien ROUPIN <julien.roupin@univ-eiffel.fr>
2022-10-12 11:42:27 +0200

    jroupin modified Yggdrasil/AAAA/dominique/DM/DMexoD.pl

--- AAAA/dominique/DM/DMexoD.pl
+++ AAAA/dominique/DM/DMexoD.pl
@@ -173,8 +173,17 @@ int main(int argc, char* argv[]){
 
 
 checks_args_stdin==#|python|
-[["Test basique", ["jeudedepart.ga"], ""],
-["Test encore ", ["gaga.ga"], ""]
+[
+["Test basique 1", ["jeudedepart.ga"], "1 2 3 4"],
+["Test basique 2", ["jeudedepart.ga"], "1 2 1 2"],
+["Test basique 3", ["jeudedepart.ga"], "0 4 2 2"],
+["Test basique 4", ["jeudedepart.ga"], "0 1 4 3"],
+["Test basique 5", ["jeudedepart.ga"], "0 0 0 0"],
+["Test supplémentaire 1", ["gaga.ga"], "1 2 3 4"],
+["Test supplémentaire 2", ["gaga.ga"], "1 2 1 2"],
+["Test supplémentaire 3", ["gaga.ga"], "0 4 2 2"],
+["Test supplémentaire 4", ["gaga.ga"], "0 1 4 3"],
+["Test supplémentaire 5", ["gaga.ga"], "0 0 0 0"],
 ]
 ==
 
