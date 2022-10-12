61503905669890361ff56157d7ce659752a6e500
Arthur RODELET CAUSSE <arthur.rodelet-causse@univ-eiffel.fr>
2022-10-12 14:46:10 +0200

    arodelet causse modified Yggdrasil/AAAA/dominique/DM/DMexoD.pl

--- AAAA/dominique/DM/DMexoD.pl
+++ AAAA/dominique/DM/DMexoD.pl
@@ -183,13 +183,14 @@ checks_args_stdin==#|python|
 ["Test basique 3", ["jeudedepart.ga"], "0 4 2 2"],
 ["Test basique 4", ["jeudedepart.ga"], "0 1 4 3"],
 ["Test basique 5", ["jeudedepart.ga"], "0 0 0 0"],
-["Test basique 5", ["jeudedepart.ga"], "0 2 0 2"],
+["Test basique 6", ["jeudedepart.ga"], "0 2 0 2"],
 ["Test supplémentaire 1", ["gaga.ga"], "1 2 3 4"],
 ["Test supplémentaire 2", ["gaga.ga"], "1 2 1 2"],
 ["Test supplémentaire 3", ["gaga.ga"], "0 4 2 2"],
 ["Test supplémentaire 4", ["gaga.ga"], "0 1 4 3"],
 ["Test supplémentaire 5", ["gaga.ga"], "0 0 0 0"],
-["Test supplémentaire 5", ["gaga.ga"], "0 2 0 2"],
+["Test supplémentaire 6", ["gaga.ga"], "0 2 0 2"],
+["Test supplémentaire 7", ["gaga.ga"], "1 1 1 1"],
 ]
 ==
 
