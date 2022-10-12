fb1bfe134a87d7606320583e43cf330643550725
Julien ROUPIN <julien.roupin@univ-eiffel.fr>
2022-10-12 11:37:14 +0200

    jroupin modified Yggdrasil/AAAA/dominique/DM/DMexoD.pl

--- AAAA/dominique/DM/DMexoD.pl
+++ AAAA/dominique/DM/DMexoD.pl
@@ -128,9 +128,6 @@ int **random_t(int H, int L, int M)
 // impression de la matrice de jeu dans un fichier
 void fprint_t(FILE *f, int h, int *t[], int l)
 {
-    int c;
-    int bb = 0;
-
     for (int i = 0; i < h; i++)
     {
         for (int j = 0; j < l; j++)
