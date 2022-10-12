a44f96363444164d0d66b6d640f630fc6aa004b8
Arthur RODELET CAUSSE <arthur.rodelet-causse@univ-eiffel.fr>
2022-10-12 14:54:02 +0200

    arodelet causse modified Yggdrasil/AAAA/dominique/DM/DMexo_hasmine.pl

--- AAAA/dominique/DM/DMexo_hasmine.pl
+++ AAAA/dominique/DM/DMexo_hasmine.pl
@@ -161,13 +161,13 @@ checks_args_stdin==#|python|
 ["Test basique 3", ["game.ga"], "0 3"],
 ["Test basique 4", ["game.ga"], "9 7"],
 ["Test basique 5", ["game.ga"], "0 0"],
-["Test basique 5", ["game.ga"], "15 12"],
+["Test basique 6", ["game.ga"], "15 12"],
 ["Test supplémentaire 1", ["ex_flag.ga"], "3 4"],
 ["Test supplémentaire 2", ["ex_flag.ga"], "1 2"],
 ["Test supplémentaire 3", ["ex_flag.ga"], "0 3"],
 ["Test supplémentaire 4", ["ex_flag.ga"], "9 7"],
 ["Test supplémentaire 5", ["ex_flag.ga"], "0 0"],
-["Test supplémentaire 5", ["ex_flag.ga"], "15 12"],
+["Test supplémentaire 6", ["ex_flag.ga"], "15 12"],
 ]
 ==
 
