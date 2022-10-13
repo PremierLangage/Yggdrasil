32f8b7665c796e9665febf47c83a00d7f8702da1
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-13 09:36:31 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/lindev/lin.pl

--- exercises/math/complex_numbers/lindev/lin.pl
+++ exercises/math/complex_numbers/lindev/lin.pl
@@ -1,4 +1,17 @@
+extends = /model/math/expr.pl
+
+title = Discriminant d'une équation du second degré
+
+complex_form = "Cartesian"
 
 before ==
-expr.rewrite(sin, exp).rewrite(cos, exp).expand().rewrite(exp, sin).simplify()
+x = Symbol('x')
+expr = sin(2x)*sin(3*x)
+
+sol = expr.rewrite(sin, exp).rewrite(cos, exp).expand().rewrite(exp, sin).simplify()
+
+==
+
+question ==
+Linéariser l'expression $! {{expr|latex}} !$.
 ==
