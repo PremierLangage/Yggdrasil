cd718c3407606a65fb36f318ec36a65a3af03ed1
Valentin LEMARIE <valentin.lemarie@univ-eiffel.fr>
2022-10-16 19:53:05 +0200

    vlemarie modified Yggdrasil/AAAA/Valentin.L/DL/DL_feuille2.pl

---
+++ AAAA/Valentin.L/DL/DL_feuille2.pl
@@ -0,0 +1,35 @@
+# Auteur : Lemarié Valentin
+# Sujet : Somme de Développements Limités
+# Maj : 16/10/2022
+
+title = Calculs de développements limités
+extends = /model/math/expr.pl
+
+before ==
+from sympy import series
+var('x')
+
+n = 4
+
+h=(1+2*x)**(1/(1+x))
+
+sol = series(h, x, 0, n+1).removeO()
+==
+
+question ==
+Ecrire le développement limité de $! {{ h|latex }} !$ autour de $! 0 !$ à l'ordre $! {{ n|latex }} !$.
+==
+
+embed ==
+# + o(x^{{ n|latex }}) \quad [x \rightarrow 0]
+==
+
+prefix ==
+$! {{ h|latex }} = !$
+==
+
+solution ==
+$! {{ h|latex }} = {{ sol|latex }} + o(x^{{ n|latex }})  \quad [x \rightarrow 0]!$.
+==
+
+latexsettings.order = 'ilex'
