ab454426183d99a62a11036af6d3af56bfc3fea4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-10-16 18:49:53 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/DM/DMexo3.pl

--- AAAA/dominique/DM/DMexo3.pl
+++ AAAA/dominique/DM/DMexo3.pl
@@ -9,15 +9,16 @@ title= Test de fin de partie
 
 text== 
 
-Quand le nombre de drapeau est égale au nombre de mines. 
-Et que les drapeaux sont biens placés le joueur à gagné !
+Quand le nombre de drapeau est égale au nombre de mines.   
+Et que les drapeaux sont biens placés le joueur à gagné !  
 
-La fonction **victoire** prend en parametre :
+La fonction **victoire** prend en parametre :  
 - la grille  
-- un entier avec le nombre de mines
+- un entier avec le nombre de mines  
 - deux entiers représentant les dimensions de la grille  
-
-et retourne 
+ 
+ 
+et retourne  
 oui(1) si la grille contient une configuration de victoire,  
 si par contre le nombre de drapeaux est égal aux nombre de mines mais qu'un drapeau est mal placé retourne (-1) BOOM !!  
 sinon retourne 0 dans les autres cas.
