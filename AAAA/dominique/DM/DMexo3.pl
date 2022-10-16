1d01b83c0edd1999b75b5e288016d7ce944fd17a
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-10-16 18:53:51 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/DM/DMexo3.pl

--- AAAA/dominique/DM/DMexo3.pl
+++ AAAA/dominique/DM/DMexo3.pl
@@ -132,7 +132,7 @@ int main(int argc, char* argv[]){
     else {
     t =random_victory(H,L,M,seed,9);
     }
-    v  = victoire(H,L,M,t);
+    v  = victoire(t,H,L,M);
     switch(v){
     case 0:  printf("Dud\n"); break;
     case 1: printf("Victoire \n"); break;
