d59b8a0e41ca951220f4168bf32079e884f0f3b3
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-18 00:32:34 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/euler/euler/euler_.pl

--- exercises/math/complex_numbers/euler/euler/euler_.pl
+++ exercises/math/complex_numbers/euler/euler/euler_.pl
@@ -12,7 +12,7 @@ while theta1+theta2 == 0:
     theta1 = pi * choice([-1, 1]) * sympify(choice(['1/4', '3/4', '1/3', '2/3', '4/3', '1/6', '5/6']))
     theta2 = pi * choice([-1, 1]) * sympify(choice(['1/4', '3/4', '1/3', '2/3', '4/3', '1/6', '5/6']))
 
-z = exp(I*theta1) + exp(I*theta2)
+z = 1 + exp(I*theta2)
 
 prefixes = []
 inputs = [MathInput(type="expr"), MathInput(type="expr", evalparam={'equality': "", 'modulo': 2*pi})]
