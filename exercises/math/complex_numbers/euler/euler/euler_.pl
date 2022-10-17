7219cbb287f18e2b00975c94c7ea2088d18cc80f
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-18 00:42:51 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/euler/euler/euler_.pl

--- exercises/math/complex_numbers/euler/euler/euler_.pl
+++ exercises/math/complex_numbers/euler/euler/euler_.pl
@@ -7,21 +7,21 @@ title = Calculer module et argument
 before ==
 from sympy import arg
 
-theta1, theta2 = 0, 0
-while theta1+theta2 == 0:
-    theta1 = pi * choice([-1, 1]) * sympify(choice(['1/4', '3/4', '1/3', '2/3', '4/3', '1/6', '5/6']))
-    theta2 = pi * choice([-1, 1]) * sympify(choice(['1/4', '3/4', '1/3', '2/3', '4/3', '1/6', '5/6']))
+a, b = 0, 0
+while a + b == 0:
+    a = choice([-1, 1]) * sympify(choice(['1/4', '3/4', '1/3', '2/3', '4/3', '1/6', '5/6']))
+    b = choice([-1, 1]) * sympify(choice(['1/4', '3/4', '1/3', '2/3', '4/3', '1/6', '5/6']))
 
-z = 1 + exp(I*pi/4)
+z =  exp(I*pi*a) + exp(I*pi*b)
 
 prefixes = []
 inputs = [MathInput(type="expr"), MathInput(type="expr", evalparam={'equality': "", 'modulo': 2*pi})]
 
 prefixes.append("module :")
-inputs[0].sol =  abs(z.expand())
+inputs[0].sol =  abs(2*cos((a-b)/2))
 
 prefixes.append("argument :")
-inputs[1].sol =  arg(z.expand())
+inputs[1].sol =  (a+b)/2
 ==
 
 question ==
