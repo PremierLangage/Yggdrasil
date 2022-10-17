9fe99e56e52938bc151536f3c748b20349c06ce2
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-17 23:56:41 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/lindev/linearization/linearization_.pl

--- exercises/math/complex_numbers/lindev/linearization/linearization_.pl
+++ exercises/math/complex_numbers/lindev/linearization/linearization_.pl
@@ -2,21 +2,6 @@ extends = /model/math/input_eval.pl
 
 title = Linéarisation d'expressions trigonométriques
 
-expressions ==
-sin(2*x)*sin(3*x)
-sin(x)*sin(3*x)
-sin(x)*sin(2x)
-cos(2*x)*cos(3*x)
-cos(x)*cos(3*x)
-cos(x)*cos(2x)
-sin(2*x)*cos(3*x)
-sin(x)*cos(3*x)
-sin(x)*cos(2x)
-cos(2*x)*sin(3*x)
-cos(x)*sin(3*x)
-cos(x)*sin(2x)
-==
-
 before ==
 x = Symbol('x')
 expr = sympify(choice(expressions.split()))
