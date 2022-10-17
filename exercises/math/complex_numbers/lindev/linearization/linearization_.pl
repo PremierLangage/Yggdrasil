4f573668ba57b03fcd151890c4a141b648294c2b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-17 23:41:49 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/lindev/linearization/linearization_.pl

--- exercises/math/complex_numbers/lindev/linearization/linearization_.pl
+++ exercises/math/complex_numbers/lindev/linearization/linearization_.pl
@@ -6,9 +6,17 @@ complex_form = "Cartesian"
 
 expressions ==
 sin(2*x)*sin(3*x)
-cos(2*x)*cos(3*x)
 sin(x)*sin(3*x)
+sin(x)*sin(2x)
+cos(2*x)*cos(3*x)
 cos(x)*cos(3*x)
+cos(x)*cos(2x)
+sin(2*x)*cos(3*x)
+sin(x)*cos(3*x)
+sin(x)*cos(2x)
+cos(2*x)*sin(3*x)
+cos(x)*sin(3*x)
+cos(x)*sin(2x)
 ==
 
 before ==
