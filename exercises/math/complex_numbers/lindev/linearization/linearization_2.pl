c83a0ed2f835a60bdc7297d7e0fe2446e9a547b4
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-17 23:56:27 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/lindev/linearization/linearization_2.pl

--- exercises/math/complex_numbers/lindev/linearization/linearization_2.pl
+++ exercises/math/complex_numbers/lindev/linearization/linearization_2.pl
@@ -1,16 +1,28 @@
-extends = /linearization_.pl
+extends = linearization_.pl
 
 expressions ==
-sin(2*x)*sin(3*x)
-sin(x)*sin(3*x)
-sin(x)*sin(2x)
-cos(2*x)*cos(3*x)
-cos(x)*cos(3*x)
-cos(x)*cos(2x)
-sin(2*x)*cos(3*x)
-sin(x)*cos(3*x)
-sin(x)*cos(2x)
-cos(2*x)*sin(3*x)
-cos(x)*sin(3*x)
-cos(x)*sin(2x)
+sin(2*x)*sin(3*x)^2
+sin(x)*sin(3*x)^2
+sin(x)*sin(2x)^2
+cos(2*x)*cos(3*x)^2
+cos(x)*cos(3*x)^2
+cos(x)*cos(2x)^2
+sin(2*x)*cos(3*x)^2
+sin(x)*cos(3*x)^2
+sin(x)*cos(2x)^2
+cos(2*x)*sin(3*x)^2
+cos(x)*sin(3*x)^2
+cos(x)*sin(2x)^2
+sin(2*x)^2*sin(3*x)
+sin(x)^2*sin(3*x)
+sin(x)^2*sin(2x)
+cos(2*x)^2*cos(3*x)
+cos(x)^2*cos(3*x)
+cos(x)^2*cos(2x)
+sin(2*x)^2*cos(3*x)
+sin(x)^2*cos(3*x)
+sin(x)^2*cos(2x)
+cos(2*x)^2*sin(3*x)
+cos(x)^2*sin(3*x)
+cos(x)^2*sin(2x)
 ==
