72e5209c240fd67b46a44dabf88befe9fe259983
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-17 23:44:54 +0200

    ddoyen1 modified Yggdrasil/exercises/math/complex_numbers/lindev/linearization/linearization_2.pl

---
+++ exercises/math/complex_numbers/lindev/linearization/linearization_2.pl
@@ -0,0 +1,16 @@
+extends = /linearization_.pl
+
+expressions ==
+sin(2*x)*sin(3*x)
+sin(x)*sin(3*x)
+sin(x)*sin(2x)
+cos(2*x)*cos(3*x)
+cos(x)*cos(3*x)
+cos(x)*cos(2x)
+sin(2*x)*cos(3*x)
+sin(x)*cos(3*x)
+sin(x)*cos(2x)
+cos(2*x)*sin(3*x)
+cos(x)*sin(3*x)
+cos(x)*sin(2x)
+==
