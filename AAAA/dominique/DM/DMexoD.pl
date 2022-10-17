636ec3da03d2a5d85b2de27492551a72f65ed417
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2022-10-17 10:28:39 +0200

    hderycke1 modified Yggdrasil/AAAA/dominique/DM/DMexoD.pl

--- AAAA/dominique/DM/DMexoD.pl
+++ AAAA/dominique/DM/DMexoD.pl
@@ -33,15 +33,16 @@ text==#|markdown|
 
 et ajoute un drapeau aux coordonées choisient si la case est non découverte, enlève le drapeau si il y a un drapeau, et ne fait rien si la case est découverte.   
 
-Pour rappel, le code est le suivant :   
-Si une case est pas découverte :  
-      9 si il y a une mine   
-     -9 si il y a une mine et un drapeau  
-    -10 si il y a un drapeau  
-      0 sinon  
-Si une case est découverte:  
-    1 - 8  nombre de mines autour de la case  
-    -11 pour 0 mines   
+Pour rappel, le code est le suivant :
+
+  - Si une case est pas découverte :
+    - 9 si il y a une mine   
+    - -9 si il y a une mine et un drapeau  
+    - -10 si il y a un drapeau  
+    - 0 sinon  
+  - Si une case est découverte:  
+    - 1 - 8  nombre de mines autour de la case  
+    - -11 pour 0 mines   
 
 ==
 
