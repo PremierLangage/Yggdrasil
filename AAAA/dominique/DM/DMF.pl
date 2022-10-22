32ecdd5d88a46aa24c37f153e0105e7f5954409c
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-10-22 07:12:11 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/DM/DMF.pl

---
+++ AAAA/dominique/DM/DMF.pl
@@ -0,0 +1,20 @@
+
+
+
+# objectif les fonctions 
+
+# int fprintGame(Game*)
+# Game *allocGame(int H, int L) # allocation du vecteur
+# Game *readGame(FILE)
+# void randomizeGame(Game *, int seed)
+# int saveGame(FILE)
+# Game *readGame(Filename)
+# int saveGame(Filename)
+
+
+
+code_after==
+int main(int c, char *argv[]){
+
+}
+==
