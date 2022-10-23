c83c37a1cc41d55b77ba4c62be66b53e5f2a2979
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-23 16:21:22 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_.pl

--- exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_.pl
+++ exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_.pl
@@ -7,8 +7,8 @@ extends = /model/math/multimathinput.pl
 
 title = Division euclidienne
 
-param.z0 = "I, -I, 1, -1, 1+I, 1-I, I-1, -1-I"
-param.coeff = "1, 2, 3, 1/2, 1/3, sqrt(2), sqrt(3)"
+param.genP = "I, -I, 1, -1, 1+I, 1-I, I-1, -1-I"
+param.genQ = "1, 2, 3, 1/2, 1/3, sqrt(2), sqrt(3)"
 
 before ==
 from randsympy import randpoly
