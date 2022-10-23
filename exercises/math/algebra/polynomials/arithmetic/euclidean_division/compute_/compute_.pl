ce8b23266f5a8be48ae3cf43204e5215e074ae1b
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-23 16:11:49 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_.pl

--- exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_.pl
+++ exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_.pl
@@ -11,14 +11,20 @@ param.z0 = "I, -I, 1, -1, 1+I, 1-I, I-1, -1-I"
 param.coeff = "1, 2, 3, 1/2, 1/3, sqrt(2), sqrt(3)"
 
 before ==
+from randsympy import randpoly
+
+var('X')
+P = randpoly(3, randint(2,3), 5, X)
+Q = randpoly(1, 2, 5, X)
+
 prefixes = []
 inputs = [MathInput(type="poly"), MathInput(type="poly")]
 
 prefixes.append("quotien :")
-inputs[0].sol =  abs(z)
+inputs[0].sol =  P//Q
 
 prefixes.append("rest :")
-inputs[1].sol =  arg(z)
+inputs[1].sol =  P%Q
 ==
 
 question ==
