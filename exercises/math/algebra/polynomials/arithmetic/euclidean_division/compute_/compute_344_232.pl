b80bcc17c98b00f8bb5ca51a4e20603bf80cadfd
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-23 16:51:30 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_344_232.pl

--- exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_344_232.pl
+++ exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_344_232.pl
@@ -2,7 +2,3 @@ extends = compute_.pl
 
 param.genP = "Poly(randpoly(3, 4, 4, X), X)"
 param.genQ = "Poly(randpoly(2, 3, 2, X), X)"
-
-@ /exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_344_232.pl
-@ /exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_434_222.pl
-@ /exercises/math/algebra/polynomials/arithmetic/euclidean_division/compute_/compute_534_322.pl
