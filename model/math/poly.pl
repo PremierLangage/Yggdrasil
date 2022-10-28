04e19ef9296f5feeba058c418fae23b20d310506
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-28 15:43:56 +0200

    ddoyen1 modified Yggdrasil/model/math/poly.pl

--- model/math/poly.pl
+++ model/math/poly.pl
@@ -10,10 +10,11 @@ symbol_dict = {'e': E}
 unauthorized_func = []
 poly_form =
 poly_domain = "R"
+poly_var = "x"
 
 evalparam ==
 from sympy import poly
-poly_var = str(poly(sol).gen)
+
 input.evalparam = {'var': poly_var, 'form': poly_form, 'domain': poly_domain}
 ==
 
