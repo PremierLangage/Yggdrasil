c3d370cfcd13debafbaacbbee863ac4339bedf29
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-10-28 15:13:52 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/DM/DMF.pl

--- AAAA/dominique/DM/DMF.pl
+++ AAAA/dominique/DM/DMF.pl
@@ -7,6 +7,7 @@
 # Game *allocGame(int H, int L) # allocation du vecteur
 # Game *readGame(FILE)
 # void randomizeGame(Game *, int seed)
+# Nombre de mines de randomizeGame
 # int saveGame(FILE)
 # Game *readGame(Filename)
 # int saveGame(Filename)
@@ -27,7 +28,7 @@ if (strcmp(argv[1],"allocGame")==0){
 
 checks_args_stdin==#|python|
 [
-["Test basique 1", ["allocGame"], "1 2 3 4"],
+["Test basique 1", ["allocGame"], "10 10 22"],
 
 ]
 ==
