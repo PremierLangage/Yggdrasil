517246a2e197914812329c32cb22904afc3d384b
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-10-28 11:37:33 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/DM/DMF.pl

--- AAAA/dominique/DM/DMF.pl
+++ AAAA/dominique/DM/DMF.pl
@@ -16,7 +16,7 @@
 code_after==
 int main(int c, char *argv[]){
 
-if (strcmp(aegv[1],"allocGame")==0){
+if (strcmp(argv[1],"allocGame")==0){
 
 }
 
@@ -28,17 +28,6 @@ if (strcmp(aegv[1],"allocGame")==0){
 checks_args_stdin==#|python|
 [
 ["Test basique 1", ["allocGame"], "1 2 3 4"],
-["Test basique 2", ["jeudedepart.ga"], "1 2 1 2"],
-["Test basique 3", ["jeudedepart.ga"], "0 4 2 2"],
-["Test basique 4", ["jeudedepart.ga"], "0 1 4 3"],
-["Test basique 5", ["jeudedepart.ga"], "0 0 0 0"],
-["Test basique 6", ["jeudedepart.ga"], "0 2 0 2"],
-["Test supplémentaire 1", ["gaga.ga"], "1 2 3 4"],
-["Test supplémentaire 2", ["gaga.ga"], "1 2 1 2"],
-["Test supplémentaire 3", ["gaga.ga"], "0 4 2 2"],
-["Test supplémentaire 4", ["gaga.ga"], "0 1 4 3"],
-["Test supplémentaire 5", ["gaga.ga"], "0 0 0 0"],
-["Test supplémentaire 6", ["gaga.ga"], "0 2 0 2"],
-["Test supplémentaire 7", ["gaga.ga"], "1 1 1 1"],
+
 ]
 ==
