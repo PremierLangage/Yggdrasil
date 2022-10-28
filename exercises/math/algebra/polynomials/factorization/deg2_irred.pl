6ed2faae485959486890415dbacfd9335cb8af67
David DOYEN <david.doyen@univ-eiffel.fr>
2022-10-28 15:24:21 +0200

    ddoyen1 modified Yggdrasil/exercises/math/algebra/polynomials/factorization/deg2_irred.pl

--- exercises/math/algebra/polynomials/factorization/deg2_irred.pl
+++ exercises/math/algebra/polynomials/factorization/deg2_irred.pl
@@ -19,13 +19,10 @@ for i in range(4):
     if b**2-4*a*c < 0:
         indsol.append(i)
     P = a*x**2+b*x+c
-    if randint(0, 1) == 1:
-        Q = randint(1, 3) *x + randint(-3, 3, [0])
-        P = choice([Q*P, P*Q])
     items.append(r"$! %s !$" % latex(P))
 ==
 
 question ==
-Sélectionner les polynômes qui ne sont pas sous forme factorisée (dans $! \mathbb{R} !$). 
+Sélectionner les polynômes qui sont irréductibles dans $! \mathbb{R} !$ (c'est-à-dire qui ne sont pas factorisables dans $! \mathbb{R} !$). 
 ==
 
