a9747c8b97c37a0da5cc7ce4dd4d12ea28694906
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2022-10-31 15:18:42 +0100

    hderycke1 modified Yggdrasil/AAAA/Henri/progc/test_eval_c.pl

--- AAAA/Henri/progc/test_eval_c.pl
+++ AAAA/Henri/progc/test_eval_c.pl
@@ -58,14 +58,6 @@ if "taboo" in globals():
     text+=str(taboo)
     text+='</div> <br />\n'
 
-if "astuces" in globals():
-    hints.items = eval(astuces)
-    text+='<br><br>'
-    text+=" {{ hints|component}} \n<br>"
-    nb_hints = len(hints.items)
-else:
-    nb_hints = 0
-
 text+=" {{ editor|component }} "
 ==
 
@@ -461,25 +453,7 @@ if student_compile.error():
 else:
     feedback += feedback_checks
 
-# Calcul de la partie : note d'autonomie
-if nb_hints > 0:
-    count_hint = 0
-    for e in hints.items:
-        if 'consumed' in e:
-            count_hint += 1
-    grade_alone = 50 + (50*(nb_hints - count_hint) // (nb_hints))
-    feedback += '<p style="margin-bottom: 5px; margin-top: 5px;"><b><u>Autonomie :</u> ' + str(grade_alone) + '%</b></p>'
-    if count_hint == 0:
-        feedback += '<div class="success-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
-        feedback += 'Sans indice</div>'
-    else: 
-        feedback += '<div class="warning-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
-        if count_hint == 1:
-            feedback += '1 indice utilisé</div>'
-        else:
-            feedback += str(count_hint)+' indices utilisés</div>'
-else:
-    grade_alone = 100
+grade_alone = 100
 
 grade_attempt = 50 + (200 // (3+nb_attempt))
 
