720bcf74c763de2c018339ca055d58eaee295a89
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2022-10-31 14:57:08 +0100

    hderycke1 modified Yggdrasil/AAAA/Henri/progc/test_eval_c.pl

--- AAAA/Henri/progc/test_eval_c.pl
+++ AAAA/Henri/progc/test_eval_c.pl
@@ -212,8 +212,269 @@ astuces==#|python|
 ==
 
 
-
-
 evaluator==#|python|
-grade = (90, 'Pouet')
+import subprocess
+from std_progC_utils import make_hide_block_on_click, terminal_code, subnlbybr
+
+# principals signals
+signals = {
+    2: "SIGINT",
+    3: "SIGQUIT",
+    4: "SIGILL",
+    6: "SIGABRT",
+    8: "SIGFPE",
+    9: "SIGKILL",
+    11: "SIGSEGV",
+    13: "SIGPIPE",
+    14: "SIGALRM",
+    15: "SIGTERM"
+};
+
+# Update nb attempt
+nb_attempt += 1 # count each try....
+
+class CompileResult:
+    def __init__(self, returncode=0, spout="", errout="", taboo=False, cflags=[]):
+        self.returncode = returncode
+        self.spout = spout
+        self.errout = errout
+        self.taboo = taboo
+        self.cflags = cflags
+
+    def success(self):
+        return not self.tabtaboo_error() and len(self.spout) + len(errout) == 0
+
+    def warning(self):
+        return not self.taboo_error() and not self.success() and "error:" not in self.errout
+
+    def error(self):
+        return self.taboo_error() or "error:" in self.errout
+
+    def taboo_error(self):
+        return bool(self.taboo)
+
+    def nb_warning(self):
+        return (self.spout + self.errout).count("warning")
+
+    def grade(self):
+        if self.success():
+            return 100
+        if self.error():
+            return 0
+        return max(0, 100 - self.nb_warning() * 10)
+
+    def texte(self):
+        if self.success():
+            return 'Compilation réussie'
+        if self.taboo_error():
+            return 'Compilation échouée, non respect du taboo : %s' % (self.taboo,)
+        if self.error():
+            return 'Compilation échouée'
+        w = self.nb_warning()
+        return 'Compilation réussie avec %d warning%s' % (w,'s' if w > 1 else '')
+
+    def formatted_feedback(self):
+        class_state = 'success' if self.success() else 'taboo-error' if self.taboo_error() else 'error' if self.error else 'warning'
+        feedback = '<p style="margin-bottom: 5px;"><b><u>Compilation :</u> ' + str(self.grade()) + '%</b> '
+        if not self.success():
+            feedback += '(cliquer au dessous pour dérouler les détails)</p>'
+        feedback += '<div class="' + class_state + '-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
+        if self.taboo_error():
+            feedback += "<b>Refus de compilation :</b> non respect du taboo : " + self.taboo
+        elif not self.success():
+            feedback += make_hide_block_on_click("compil_ans", self.texte() + ' avec flags ' + ' '.join(self.cflags), terminal_code(self.spout+self.errout), "")
+        else:
+            feedback += make_hide_block_on_click("compil_ans", self.texte() + ' avec flags ' + ' '.join(self.cflags), "C'était parfait, le compilateur n'a rien dit...", "")
+        feedback += '</div>'
+        return feedback
+
+    def __mul__(self, other):
+        if not self or not other:
+            return self
+        if self.returncode != 0 or self.taboo_error():
+            return self
+        return Compilation(
+            other.returncode,
+            self.spout + other.spout,
+            self.errout + other.errout
+        )
+
+    def __bool__(self):
+        return returncode == 0
+
+class Source:
+    def __init__(self, name, code="", before="", after=""):
+        self.name = name
+        self.code = code
+        self.before = before
+        self.after = after
+        self.built = False
+
+    def write(self):
+        with open(self.name, 'ro') as f:
+            f.write(self.before)
+            f.write('\n\n')
+            f.write(self.code)
+            f.write('\n\n')
+            f.write(self.after)
+
+    def build(self, compiler, cflags=["-Wall", "-ansi"]):
+        command_args = [compiler, self.name, "-c", "-o", self.name + ".o"] + cflags
+        sp = subprocess.run(command_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        spout = sp.stdout.decode()
+        errout = sp.stderr.decode()
+        returncode = sp.returncode
+        if returncode == 0:
+            self.built = True
+        return CompileResult(returncode, spout, errout)
+
+class Program:
+    def __init__(self, name, sources):
+        self.name = name
+        self.sources = sources
+
+    def link(self, compiler, ldflags=[]):
+        command_args = [compiler, "-o", self.name] + ldflags + [ src.name + '.o' for src in sources ]
+        sp = subprocess.run(command_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        spout = sp.stdout.decode()
+        errout = sp.stderr.decode()
+        returncode = sp.returncode
+        return CompileResult(returncode, spout, errout)
+
+    def run(self, argv):
+        command_args = ['./' + self.name] + argv
+        sp = subprocess.run(command_args, stdin=open("stdin_content", "r"), stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=1)
+        try: 
+            expected_ouput = sp.stdout.decode() + sp.stderr.decode()
+        except:
+            expected_ouput = "Impossible de décoder la sortie standard"
+        if -sp.returncode in signals:
+            expected_ouput += "Process exited with UNIX signal ("+str(-sp.returncode)+") "+signals[-sp.returncode]
+        elif sp.returncode < 0:
+            expected_ouput += "Process exited with UNIX signal ("+str(-sp.returncode)+")"
+        return expected_ouput
+
+
+src_student = Source("src_student.c", editor.code, code_before, code_before)
+src_teacher = Source("src_teacher.c", solution, code_before, code_before)
+
+# Compile the teacher solution
+assert src_teacher.build()
+pgr_teacher = Program("teacher_prog", [src_teacher])
+assert pgr_teacher.link()
+
+# Compile the student proposition
+student_build = src_student.build()
+pgr_student = Program("student_prog", [src_student])
+student_compile = student_build * pgr_student.link()
+
+grade_compil = student_compile.grade()
+text_compil = student_compile.text()
+
+# Tests
+nb_good = 0
+nb_bad = 0
+grade_checks = 0
+
+feedback_checks = ""
+
+# data test generation : must be in a separate place
+# since it need a new eval at each attempt to be really
+# random !!!
+from random import *
+checks_data = eval(checks_args_stdin)
+
+if not student_compile.error():
+    for test_c in checks_data:
+        f_in=open("stdin_content", "w")
+        f_in.write(test_c[2])
+        f_in.close()
+        # Use the teacher solution to generated expected output of the test
+        expected_ouput = pgr_teacher.run(test_c[1])
+
+        # Now execute the student programm
+        spout = pgr_student.run(test_c[1])
+
+        if spout == expected_ouput:
+            nb_good += 1
+            feedback_checks += '<div class="success-state" style="margin: 2px;padding: 5px; border: 1px solid #155724 transparent;">'
+            terminal_log = "Platon@debian~$> ./a.out " + " ".join(test_c[1]) + "\n"
+            terminal_log += expected_ouput
+            if len(test_c[2]) > 0:
+                stdin_explain = "Contenu de l'entrée standard durant l'exécution : <br />"
+                stdin_explain += subnlbybr(test_c[2])
+                if test_c[2][-1] != '\n':
+                    stdin_explain += "<br />"
+            else:
+                stdin_explain = ""
+            feedback_checks += make_hide_block_on_click("details_check"+str(nb_good+nb_bad), test_c[0], stdin_explain + terminal_code(terminal_log), "")
+            feedback_checks += '</div>'
+        else:
+            nb_bad += 1
+            feedback_checks += '<div class="error-state" style="margin: 2px;padding: 5px; border: 1px solid #155724 transparent;">'
+            term_tot = "Attendu : <br />"
+            terminal_log = "Platon@debian~$> ./a.out " + " ".join(test_c[1]) + "\n"
+            terminal_log += expected_ouput
+            if len(test_c[2]) > 0:
+                stdin_explain = "Contenu de l'entrée standard durant l'exécution : <br />"
+                stdin_explain += subnlbybr(test_c[2])
+                if test_c[2][-1] != '\n':
+                    stdin_explain += "<br />"
+            else:
+                stdin_explain = ""
+            term_tot += terminal_code(terminal_log)
+            term_tot += "Obtenu : <br />"
+            terminal_log = "Platon@debian~$> ./a.out " + " ".join(test_c[1]) + "\n"
+            terminal_log += spout
+            term_tot += terminal_code(terminal_log)
+            feedback_checks += make_hide_block_on_click("details_check"+str(nb_good+nb_bad), test_c[0], stdin_explain + term_tot, "")
+            feedback_checks += '</div>'
+
+    grade_checks = min([((nb_good*100) // (nb_good+nb_bad)) , (100 // (2**nb_bad))])
+feedback += '<p style="margin-bottom: 5px; margin-top: 5px;"><b><u>Tests :</u> ' + str(grade_checks) + '%</b> (cliquer sur les tests pour afficher/réduire leurs détails)</p>'
+
+if compil_state == 'error':
+    feedback += '<div class="error-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
+    feedback += '<b>Erreur à la compilation :</b> Pas de test lancé</div>'
+else:
+    feedback += feedback_checks
+
+# Calcul de la partie : note d'autonomie
+if nb_hints > 0:
+    count_hint = 0
+    for e in hints.items:
+        if 'consumed' in e:
+            count_hint += 1
+    grade_alone = 50 + (50*(nb_hints - count_hint) // (nb_hints))
+    feedback += '<p style="margin-bottom: 5px; margin-top: 5px;"><b><u>Autonomie :</u> ' + str(grade_alone) + '%</b></p>'
+    if count_hint == 0:
+        feedback += '<div class="success-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
+        feedback += 'Sans indice</div>'
+    else: 
+        feedback += '<div class="warning-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
+        if count_hint == 1:
+            feedback += '1 indice utilisé</div>'
+        else:
+            feedback += str(count_hint)+' indices utilisés</div>'
+else:
+    grade_alone = 100
+
+grade_attempt = 50 + (200 // (3+nb_attempt))
+
+feedback += '<p style="margin-bottom: 5px; margin-top: 5px;"><b><u>Efficacité :</u> ' + str(grade_attempt) + '%</b></p>'
+
+if nb_attempt == 1:
+    feedback += '<div class="success-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
+    feedback += '1 tentative</div>'
+    all_grade = [(grade_compil * grade_checks * grade_attempt * grade_alone) // 1000000]
+else:
+    feedback += '<div class="warning-state" style="padding: 5px; border: 1px solid #155724 transparent;">'
+    feedback += str(nb_attempt)+' tentatives</div>'
+    all_grade.append((grade_compil * grade_checks * grade_attempt * grade_alone) // 1000000)
+
+# overall grade !
+feedback = '<p style="margin-bottom: 5px; margin-top: 5px;"><b><u>Note actuelle :</u> ' + str(max(all_grade)) + '/100</b></p>' + feedback
+
+grade=((grade_compil * grade_checks * grade_attempt * grade_alone) // 1000000, feedback)
+grade= (grade_alone, feedback)
 ==
