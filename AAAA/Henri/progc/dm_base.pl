1ce4eec9fc7f04b2161f8fbb7b00dfd8c5c698f7
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2022-11-04 15:31:34 +0100

    hderycke1 modified Yggdrasil/AAAA/Henri/progc/dm_base.pl

--- AAAA/Henri/progc/dm_base.pl
+++ AAAA/Henri/progc/dm_base.pl
@@ -1,169 +1,19 @@
 extends = eval_mult_c.pl
 
-headers.struct==#|c|
-#ifndef __STRUCT__
-#define __STRUCT__
-struct _game
-{
-    int termine;
-    int H;
-    int L;
-    int M;
-    int **t;
-};
+title = DM Démineur
 
-typedef struct _game Game;
-#endif
-==
-headers.mallocGame==#|c|
-void *mallocGame(int H, int L, int M);
-==
-sources.mallocGame==#|c|
-#include <stdlib.h>
-#include "struct.h"
-// allocation de la structure game
-// et d'une matrice de taille H*L et initialisation à 0
-void *mallocGame(int H, int L, int M)
-{
-    Game *g = malloc(sizeof(Game));
-    g->termine = 0;
-    g->H = H;
-    g->L = L;
-    g->M = M;
-    g->t = calloc(1, H * sizeof(int *));
-    for (int i = 0; i < H; i++)
-    {
-        g->t[i] = calloc(1, L * sizeof(int));
-    }
-    return g;
+headers.nb_mines_t==#|c|
+int nb_mines_t(int h, int l, int *t[], int i, int j);
+== sources.nb_mines_t == #|c|
+#include "has_mine_t.h"
+int nb_mines_t(int h, int l, int *t[], int i, int j) {
+    return has_mine_t(h, l, t, i - 1, j - 1) + has_mine_t(h, l, t, i - 1, j) +
+           has_mine_t(h, l, t, i - 1, j + 1) + has_mine_t(h, l, t, i, j - 1) +
+           has_mine_t(h, l, t, i, j + 1) + has_mine_t(h, l, t, i + 1, j - 1) +
+           has_mine_t(h, l, t, i + 1, j) + has_mine_t(h, l, t, i + 1, j + 1);
 }
 ==
-headers.freeGame==#|c|
-#include "struct.h"
-void freeGame(Game *g);
-==
-sources.freeGame==#|c|
-#include <stdlib.h>
-#include "struct.h"
-// libération de la mémoire
-void freeGame(Game *g)
-{
-    for (int i = 0; i < g->H; i++)
-    {
-        free(g->t[i]);
-    }
-    free(g->t);
-    free(g);
-}
 
-==
-headers.read_t==#|c|
-#include <stdio.h>
-int **read_t(FILE *f, int *H, int *L, int *M);
-==
-sources.read_t==#|c|
-#include <stdio.h>
-#include <stdlib.h>
-
-int **read_t(FILE *f, int *H, int *L, int *M)
-{
-    int **t;
-    fscanf(f, "%d %d %d", H, L, M);
-    t = malloc(*H * sizeof(int *));
-    for (int i = 0; i < *H; i++)
-    {
-        t[i] = malloc(*L * sizeof(int));
-    }
-
-    for (int i = 0; i < *H; i++)
-    {
-        for (int j = 0; j < *L; j++)
-        {
-            fscanf(f, "%d", &(t[i][j]));
-        }
-    }
-    return t;
-}
-
-==
-headers.readGame==#|c|
-#include <stdio.h>
-#include "struct.h"
-
-Game *readGame(FILE *f);
-==
-sources.readGame==#|c|
-#include <stdio.h>
-#include "struct.h"
-#include "mallocGame.h"
-//  allocation de la structure et lecture du fichier
-Game *readGame(FILE *f)
-{
-    int H, L, M;
-    fscanf(f, "%d %d %d", &H, &L, &M);
-    Game *g = mallocGame(H, L, M);
-    int **t = g->t;
-    for (int i = 0; i < H; i++)
-    {
-        for (int j = 0; j < L; j++)
-        {
-            fscanf(f, "%d", &(t[i][j]));
-        }
-    }
-    return g;
-}
-
-==
-headers.hasmine_t==#|c|
-int hasmine_t(int h, int l, int *t[], int i, int j);
-==
-sources.hasmine_t==#|c|
-int hasmine_t(int h, int l, int *t[], int i, int j)
-{
-    if ((i < 0) || (i > h - 1) || (j < 0) || (j > l - 1))
-        return 0;
-
-    // mine ou mine sous drapeau
-    return t[i][j] == 9 || t[i][j] == -9;
-}
-==
-headers.hasmine_g==#|c|
-#include "struct.h"
-int hasmine_g(Game *g, int i, int j);
-==
-sources.hasmine_g==#|c|
-#include "struct.h"
-#include "hasmine_t.h"
-int hasmine_g(Game *g, int i, int j)
-{
-    return hasmine_t(g->H, g->L, g->t, i, j);
-}
-
-==
-headers.nbmines_t==#|c|
-int nbmines_t(int h, int l, int *t[], int i, int j);
-==
-sources.nbmines_t==#|c|
-#include "hasmine_t.h"
-int nbmines_t(int h, int l, int *t[], int i, int j)
-{
-    return hasmine_t(h, l, t, i - 1, j - 1) + hasmine_t(h, l, t, i - 1, j) + hasmine_t(h, l, t, i - 1, j + 1) + hasmine_t(h, l, t, i, j - 1) + hasmine_t(h, l, t, i, j + 1) + hasmine_t(h, l, t, i + 1, j - 1) + hasmine_t(h, l, t, i + 1, j) + hasmine_t(h, l, t, i + 1, j + 1);
-}
-
-==
-headers.nbmines_g==#|c|
-#include "struct.h"
-int nbmines_g(Game *g, int i, int j);
-==
-sources.nbmines_g==#|c|
-#include "struct.h"
-#include "nbmines_t.h"
-int nbmines_g(Game *g, int i, int j)
-{
-    return nbmines_t(g->H, g->L, g->t, i, j);
-}
-
-==
 headers.print_t==#|c|
 void print_t(int h, int *t[], int l);
 ==
@@ -174,10 +24,8 @@ sources.print_t==#|c|
 
 void fprint_t(FILE *f, int h, int *t[], int l)
 {
-    for (int i = 0; i < h; i++)
-    {
-        for (int j = 0; j < l; j++)
-        {
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < l; j++) {
             fprintf(f, "%d ", t[i][j]);
         }
         fprintf(f, "\n");
@@ -189,37 +37,8 @@ void print_t(int h, int *t[], int l)
 {
     fprint_t(stdout, h, t, l);
 }
+==
 
-
-==
-headers.saveGame==#|c|
-#include <stdio.h>
-#include "struct.h"
-void saveGame(FILE *f, Game *g);
-==
-sources.saveGame==#|c|
-#include <stdio.h>
-#include "struct.h"
-void saveGame(FILE *f, Game *g)
-{
-    fprintf(f, "%d %d %d\n", g->H, g->L, g->M);
-    fprint_t(f, g->H, g->t, g->L);
-}
-
-==
-headers.printGame==#|c|
-#include "struct.h"
-void printGame(Game *g);
-==
-sources.printGame==#|c|
-#include "struct.h"
-#include "saveGame.h"
-void printGame(Game *g)
-{
-    saveGame(stdout, g);
-}
-
-==
 headers.save_t==#|c|
 // sauvegarde dans un fichier
 void save_t(FILE *f, int **t, int H, int L, int M);
@@ -233,8 +52,8 @@ void save_t(FILE *f, int **t, int H, int L, int M)
     fprintf(f, "%d %d %d\n", H, L, M);
     fprint_t(f, H, t, L);
 }
-
 ==
+
 headers.alloc_t==#|c|
 int **alloc_t(int H, int L);
 ==
@@ -276,57 +95,57 @@ int **random_t(int H, int L, int M)
     }
     return t;
 }
+==
 
+headers.read_t==#|c|
+#include <stdio.h>
+int **read_t(FILE *f, int *H, int *L, int *M);
 ==
-headers.randomGame==#|c|
-#include "struct.h"
-Game *randomGame(int H, int L, int M);
-==
-sources.randomGame==#|c|
-#include "struct.h"
-#include "mallocGame.h"
-#include "random_t.h"
-Game *randomGame(int H, int L, int M)
-{
-    Game *g = mallocGame(H, L, M);
-    g->t = random_t(H, L, M);
-    return g;
-}
-
-==
-headers.restartGame==#|c|
-#include "struct.h"
-void restartGame(Game *g)
-==
-sources.restartGame==#|c|
+sources.read_t==#|c|
+#include <stdio.h>
 #include <stdlib.h>
 
-#include "struct.h"
-void restartGame(Game *g)
+int **read_t(FILE *f, int *H, int *L, int *M)
 {
-    int i, j;
-    for (i = 0; i < g->H; i++)
-        for (j = 0; j < g->L; j++)
-            g->t[i][j] = 0;
-    for (i = 0; i < g->M; i++)
+    int **t;
+    fscanf(f, "%d %d %d", H, L, M);
+    t = malloc(*H * sizeof(int *));
+    for (int i = 0; i < *H; i++)
     {
-        int h, l;
-        do
-        {
-            h = rand() % g->H;
-            l = rand() % g->L;
-        } while (g->t[h][l] == 9);
-        g->t[h][l] = 9;
+        t[i] = malloc(*L * sizeof(int));
     }
-}
 
+    for (int i = 0; i < *H; i++)
+    {
+        for (int j = 0; j < *L; j++)
+        {
+            fscanf(f, "%d", &(t[i][j]));
+        }
+    }
+    return t;
+}
 ==
-headers.P==#|c|
-int P(int **t, int H, int L, int i, int j);
+
+headers.has_mine_t==#|c|
+int has_mine_t(int h, int l, int *t[], int i, int j);
 ==
-sources.P==#|c|
-#include "nbmines_t.h"
-int P(int **t, int H, int L, int i, int j)
+sources.has_mine_t==#|c|
+int has_mine_t(int h, int l, int *t[], int i, int j)
+{
+    if ((i < 0) || (i > h - 1) || (j < 0) || (j > l - 1))
+        return 0;
+
+    // mine ou mine sous drapeau
+    return t[i][j] == 9 || t[i][j] == -9;
+}
+==
+
+headers.pied_t==#|c|
+int pied_t(int **t, int H, int L, int i, int j);
+==
+sources.pied_t==#|c|
+#include "nb_mines_t.h"
+int pied_t(int **t, int H, int L, int i, int j)
 {
     // case découverte pas d'action
     if ((t[i][j] > 0 && t[i][j] < 9) || t[i][j] == -11)
@@ -341,18 +160,18 @@ int P(int **t, int H, int L, int i, int j)
     if (t[i][j] < -8) // -9 ou -10
         return 0;
     // case pas découverte et pas de drapeau
-    t[i][j] = nbmines_t(H, L, t, i, j);
+    t[i][j] = nb_mines_t(H, L, t, i, j);
     if (t[i][j] == 0)
         t[i][j] = -11;
     return 0;
 }
 
 ==
-headers.D==#|c|
-void D(int **t, int H, int L, int i, int j);
+headers.drapeau_t==#|c|
+void drapeau_t(int **t, int H, int L, int i, int j);
 ==
-sources.D==#|c|
-void D(int **t, int H, int L, int i, int j)
+sources.drapeau_t==#|c|
+void drapeau_t(int **t, int H, int L, int i, int j)
 { // click droit sur la case i,j
     // placement d'un drapeau
     if (t[i][j] == 9)
@@ -378,85 +197,17 @@ void D(int **t, int H, int L, int i, int j)
     }
     // sinon pas d'effet sur le terrain
 }
+==
 
+headers.print_end_t==#|c|
+void print_end_t(int H, int L, int **t);
 ==
-headers.PG==#|c|
-#include "struct.h"
-int PG(Game *g, int i, int j);
-==
-sources.PG==#|c|
-#include "struct.h"
-#include "P.h"
-
-int PG(Game *g, int i, int j)
-{ // click gauche sur la case i,j
-    return P(g->t, g->H, g->L, i, j);
-}
-
-==
-headers.playGame==#|c|
-#include "struct.h"
-int playGame(Game *p);
-==
-sources.playGame==#|c|
+sources.print_end_t==#|c|
 #include <stdio.h>
 
-#include "struct.h"
-// return if the game is over (1) or not (0)
-int playGame(Game *p)
-{
-    int i, j;
-    int formats = 0;
-    do
-    {
-        printf("i j ? ");
-        scanf("%d %d", &i, &j);
-        if (formats != 2)
-        {
-            printf("Erreur de d'entree, veuillez reessayer");
-        }
-    } while (formats != 2);
+#include "nb_mines_t.h"
 
-    return PG(p, i, j);
-}
-
-==
-headers.printEndGame==#|c|
-#include "struct.h"
-void printEndGame(Game *g);
-==
-sources.printEndGame==#|c|
-#include <stdio.h>
-
-#include "struct.h"
-#include "nbmines_t.h"
-// affiche le terrain après explosion
-void printEndGame(Game *g)
-{
-    for (int i = 0; i < g->H; i++)
-    {
-        for (int j = 0; j < g->L; j++)
-        {
-            if (g->t[i][j] == 9)
-                printf("%c", 'X');
-            else if (g->t[i][j] == -9)
-                printf("%c", 'O');
-            else
-                printf("%c", nbmines_g(g, i, j) + '0');
-        }
-        printf("\n");
-    }
-}
-==
-headers.printEnd_t==#|c|
-void printEnd_t(int H, int L, int **t);
-==
-sources.printEnd_t==#|c|
-#include <stdio.h>
-
-#include "nbmines_t.h"
-
-void printEnd_t(int H, int L, int **t)
+void print_end_t(int H, int L, int **t)
 {
     for (int i = 0; i < H; i++)
     {
@@ -467,18 +218,18 @@ void printEnd_t(int H, int L, int **t)
             else if (t[i][j] == -9)
                 printf("%c", 'O');
             else
-                printf("%c", nbmines_t(H, L, t, i, j) + '0');
+                printf("%c", nb_mines_t(H, L, t, i, j) + '0');
         }
         printf("\n");
     }
 }
 
 ==
-headers.nbdrapeau==#|c|
-int nbdrapeau(int H, int L, int **t);
+headers.nb_drapeau_t==#|c|
+int nb_drapeau_t(int H, int L, int **t);
 ==
-sources.nbdrapeau==#|c|
-int nbdrapeau(int H, int L, int **t)
+sources.nb_drapeau_t==#|c|
+int nb_drapeau_t(int H, int L, int **t)
 {
     int i, j, n = 0;
     for (i = 0; i < H; i++)
@@ -488,15 +239,16 @@ int nbdrapeau(int H, int L, int **t)
     return n;
 }
 ==
+
 headers.verif_t==#|c|
 int verif_t(int H, int L, int M, int **t);
 ==
 sources.verif_t==#|c|
-#include "nbdrapeau.h"
+#include "nb_drapeau_t.h"
 int verif_t(int H, int L, int M, int **t)
 {
     int i, j;
-    if (nbdrapeau(H, L, t) != M)
+    if (nb_drapeau_t(H, L, t) != M)
         return 0;
 
     for (i = 0; i < H; i++)
@@ -506,18 +258,247 @@ int verif_t(int H, int L, int M, int **t)
     return 0;
 }
 ==
-headers.verifGame==#|c|
-#include "struct.h"
-int verifGame(Game *g);
-==
-sources.verifGame==#|c|
-#include "struct.h"
-#include "nbdrapeau.h"
 
-int verifGame(Game *g)
+# Game version
+
+headers.struct==#|c|
+#ifndef __STRUCT__
+#define __STRUCT__
+struct _game
+{
+    int termine;
+    int H;
+    int L;
+    int M;
+    int **t;
+};
+
+typedef struct _game Game;
+#endif
+==
+
+headers.malloc_game==#|c|
+void *malloc_game(int H, int L, int M);
+==
+sources.malloc_game==#|c|
+#include <stdlib.h>
+#include "struct.h"
+// allocation de la structure game
+// et d'une matrice de taille H*L et initialisation à 0
+void *malloc_game(int H, int L, int M)
+{
+    Game *g = malloc(sizeof(Game));
+    g->termine = 0;
+    g->H = H;
+    g->L = L;
+    g->M = M;
+    g->t = calloc(1, H * sizeof(int *));
+    for (int i = 0; i < H; i++)
+    {
+        g->t[i] = calloc(1, L * sizeof(int));
+    }
+    return g;
+}
+==
+
+headers.free_game==#|c|
+#include "struct.h"
+void free_game(Game *g);
+==
+sources.free_game==#|c|
+#include <stdlib.h>
+#include "struct.h"
+// libération de la mémoire
+void free_game(Game *g)
+{
+    for (int i = 0; i < g->H; i++)
+    {
+        free(g->t[i]);
+    }
+    free(g->t);
+    free(g);
+}
+==
+
+headers.read_game==#|c|
+#include <stdio.h>
+#include "struct.h"
+
+Game *read_game(FILE *f);
+==
+sources.read_game==#|c|
+#include <stdio.h>
+#include "struct.h"
+#include "malloc_game.h"
+//  allocation de la structure et lecture du fichier
+Game *read_game(FILE *f)
+{
+    int H, L, M;
+    fscanf(f, "%d %d %d", &H, &L, &M);
+    Game *g = malloc_game(H, L, M);
+    int **t = g->t;
+    for (int i = 0; i < H; i++)
+    {
+        for (int j = 0; j < L; j++)
+        {
+            fscanf(f, "%d", &(t[i][j]));
+        }
+    }
+    return g;
+}
+==
+
+headers.nb_mines_game==#|c|
+#include "struct.h"
+int nb_mines_game(Game *g, int i, int j);
+==
+sources.nb_mines_game==#|c|
+#include "struct.h"
+#include "nb_mines_t.h"
+int nb_mines_game(Game *g, int i, int j)
+{
+    return nb_mines_t(g->H, g->L, g->t, i, j);
+}
+==
+
+headers.save_game==#|c|
+#include <stdio.h>
+#include "struct.h"
+void save_game(FILE *f, Game *g);
+==
+sources.save_game==#|c|
+#include <stdio.h>
+#include "struct.h"
+void save_game(FILE *f, Game *g)
+{
+    fprintf(f, "%d %d %d\n", g->H, g->L, g->M);
+    fprint_t(f, g->H, g->t, g->L);
+}
+==
+
+headers.print_game==#|c|
+#include "struct.h"
+void print_game(Game *g);
+==
+sources.print_game==#|c|
+#include "struct.h"
+#include "save_game.h"
+void print_game(Game *g)
+{
+    save_game(stdout, g);
+}
+==
+
+headers.has_mine_game==#|c|
+#include "struct.h"
+int has_mine_game(Game *g, int i, int j);
+==
+sources.has_mine_game==#|c|
+#include "struct.h"
+#include "has_mine_t.h"
+int has_mine_game(Game *g, int i, int j)
+{
+    return has_mine_t(g->H, g->L, g->t, i, j);
+}
+==
+
+headers.random_game==#|c|
+#include "struct.h"
+Game *random_game(int H, int L, int M);
+==
+sources.random_game==#|c|
+#include "struct.h"
+#include "malloc_game.h"
+#include "random_t.h"
+Game *random_game(int H, int L, int M)
+{
+    Game *g = malloc_game(H, L, M);
+    g->t = random_t(H, L, M);
+    return g;
+}
+==
+
+headers.restart_game==#|c|
+#include "struct.h"
+void restart_game(Game *g)
+==
+sources.restart_game==#|c|
+#include <stdlib.h>
+
+#include "struct.h"
+void restart_game(Game *g)
 {
     int i, j;
-    if (nbdrapeau(g->H, g->L, g->t) != g->M)
+    for (i = 0; i < g->H; i++)
+        for (j = 0; j < g->L; j++)
+            g->t[i][j] = 0;
+    for (i = 0; i < g->M; i++)
+    {
+        int h, l;
+        do
+        {
+            h = rand() % g->H;
+            l = rand() % g->L;
+        } while (g->t[h][l] == 9);
+        g->t[h][l] = 9;
+    }
+}
+==
+
+headers.pied_game==#|c|
+#include "struct.h"
+int pied_game(Game *g, int i, int j);
+==
+sources.pied_game==#|c|
+#include "struct.h"
+#include "pied_t.h"
+
+int pied_game(Game *g, int i, int j)
+{ // click gauche sur la case i,j
+    return pied_t(g->t, g->H, g->L, i, j);
+}
+==
+
+headers.print_end_game==#|c|
+#include "struct.h"
+void print_end_game(Game *g);
+==
+sources.print_end_game==#|c|
+#include <stdio.h>
+
+#include "struct.h"
+#include "nb_mines_t.h"
+// affiche le terrain après explosion
+void print_end_game(Game *g)
+{
+    for (int i = 0; i < g->H; i++)
+    {
+        for (int j = 0; j < g->L; j++)
+        {
+            if (g->t[i][j] == 9)
+                printf("%c", 'X');
+            else if (g->t[i][j] == -9)
+                printf("%c", 'O');
+            else
+                printf("%c", nb_mines_game(g, i, j) + '0');
+        }
+        printf("\n");
+    }
+}
+==
+
+headers.verif_game==#|c|
+#include "struct.h"
+int verif_game(Game *g);
+==
+sources.verif_game==#|c|
+#include "struct.h"
+#include "nb_drapeau_t.h"
+
+int verif_game(Game *g)
+{
+    int i, j;
+    if (nb_drapeau_t(g->H, g->L, g->t) != g->M)
         return 0;
     for (i = 0; i < g->H; i++)
         for (j = 0; j < g->L; j++)
@@ -525,106 +506,10 @@ int verifGame(Game *g)
                 return 1; // boom mine sans drapeau
     return 0;
 }
-
 ==
-headers.play_t==#|c|
-int play_t(int H, int L, int **t);
-==
-sources.play_t==#|c|
-#include <stdio.h>
 
-#include "P.h"
-
-// return if the game is over (1) or not (0)
-int play_t(int H, int L, int **t)
-{
-    int i, j;
-    int formats = 0;
-    do
-    {
-        printf("i j ? ");
-        formats = scanf("%d %d", &i, &j);
-        if (formats != 2)
-        {
-            printf("Erreur de d'entree, veuillez reessayer");
-        }
-    } while (formats != 2);
-
-    return P(t, H, L, i, j);
-}
-==
-headers.v_t==#|c|
-void v_t();
-==
-sources.v_t==#|c|
-#include <stdio.h>
-
-#include "random_t.h"
-#include "print_t.h"
-#include "verif_t.h"
-#include "play_t.h"
-#include "printEnd_t.h"
-
-void v_t()
-{
-    int H = 10, L = 10, M = 20;
-    int **t;
-    t = random_t(H, L, M);
-    print_t(H, t, L);
-    while (!verif_t(H, L, M, t))
-    {
-        if (play_t(H, L, t))
-        {
-            printEnd_t(H, L, t);
-            printf("BOOM !!\n");
-        }
-        else
-            print_t(H, t, L);
-    }
-}
-
-==
-headers.vgame==#|c|
-void vgame();
-==
-sources.vgame==#|c|
-#include "randomGame.h"
-#include "playGame.h"
-#include "printGame.h"
-#include "verifGame.h"
-#include "printEndGame.h"
-
-void vgame()
-{
-    int terminer = 0; // pas fini
-    int victoire = 0; // pas encore gagné
-    Game *g = randomGame(10, 10, 10);
-    printGame(g);
-
-    while (!terminer)
-    {
-        terminer = playGame(g);
-        // option
-        // si le nombre de drapeau est égal au nombre de mines
-        // verifier que les drapeaux sont bien placés
-        // si c'est le cas, gagner la partie !
-        if (verifGame(g))
-        {
-            terminer = 1;
-            victoire = 1;
-        }
-        printGame(g);
-    }
-    printEndGame(g);
-}
-
-==
 sources.main==#|c|
-#include "v_t.h"
-int main()
-{
-    // vgame();
-    // v_t();
+int main() {
     return 0;
 }
 ==
