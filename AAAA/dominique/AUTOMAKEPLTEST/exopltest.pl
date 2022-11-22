5ebc89d0a40fa5137f4c9a956fa3d3940276ebad
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-11-22 17:08:07 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/AUTOMAKEPLTEST/exopltest.pl

--- AAAA/dominique/AUTOMAKEPLTEST/exopltest.pl
+++ AAAA/dominique/AUTOMAKEPLTEST/exopltest.pl
@@ -3,3 +3,17 @@
 
 extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 
+title = gros templates
+
+text==
+
+ceic est in test de la valeur de toto
+qui doit valoir 12 !!
+
+==
+
+pltest==
+>>> toto== 12 
+True
+==
+
