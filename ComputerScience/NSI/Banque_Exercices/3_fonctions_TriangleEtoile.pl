ce1501966576d8a10bb0762955d128c89714ab8a
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-12-04 23:49:32 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/3_fonctions_TriangleEtoile.pl

--- ComputerScience/NSI/Banque_Exercices/3_fonctions_TriangleEtoile.pl
+++ ComputerScience/NSI/Banque_Exercices/3_fonctions_TriangleEtoile.pl
@@ -17,6 +17,7 @@ On veut dessiner à l'aide d'étoiles '\*' et de plus "+" des triangles de la fo
 \*<br>
 
 Pour cela:
+
 - écrire une fonction `ligne_etoile(n)` qui reçoit un entier et renvoie une chaine formée de :
 1 caractère étoile ('\*'), 2 caractères étoiles ('\*'),...,n caractères étoile ('*') séparés par des caratères plus ('+').<br>
     
