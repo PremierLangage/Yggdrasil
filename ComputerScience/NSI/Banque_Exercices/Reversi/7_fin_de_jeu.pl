28887cfee17a5a78bf0a0a68f7c7f84f801b952d
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2022-12-13 17:40:53 +0100

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Reversi/7_fin_de_jeu.pl

--- ComputerScience/NSI/Banque_Exercices/Reversi/7_fin_de_jeu.pl
+++ ComputerScience/NSI/Banque_Exercices/Reversi/7_fin_de_jeu.pl
@@ -8,55 +8,55 @@ title=Fin de jeu
 
 pltest==
 >>> jeu = {
-    "plateau": [
-        [None, None, None, None],
-        [None,  'joueur1',  'joueur2', None],
-        [None,  'joueur2',  'joueur1', None],
-        [None, None, None, None]
-        ],
-    "joueur actif": "joueur1",
-    "joueur1":  {
-            "nom": "joueur1",
-            "couleur": "white",
-            "score": 2 
-        },
-    "joueur2":  {
-            "nom": "joueur2",
-            "couleur": "red",
-            "score": 2 
-        },
-    "parametres":{
-        'framerate': 10,
-        'plateau' : 4,
-        'taille_fenetre' : 640
-        }
-    }
+...    "plateau": [
+...        [None, None, None, None],
+...        [None,  'joueur1',  'joueur2', None],
+...        [None,  'joueur2',  'joueur1', None],
+...        [None, None, None, None]
+...        ],
+...    "joueur actif": "joueur1",
+...    "joueur1":  {
+...            "nom": "joueur1",
+...            "couleur": "white",
+...            "score": 2 
+...        },
+...    "joueur2":  {
+...            "nom": "joueur2",
+...            "couleur": "red",
+...            "score": 2 
+...        },
+...    "parametres":{
+...        'framerate': 10,
+...        'plateau' : 4,
+...        'taille_fenetre' : 640
+...        }
+...    }
 >>> fin_de_jeu(jeu)
 False
 >>> jeu = {
-    "plateau": [
-        [None, None, None, None],
-        [None,  'joueur2',  'joueur2', None],
-        [None,  'joueur2',  'joueur2', None],
-        [None, None, None, None]
-        ],
-    "joueur actif": "joueur1",
-    "joueur1":  {
-            "nom": "joueur1",
-            "couleur": "white",
-            "score": 2 
-        },
-    "joueur2":  {
-            "nom": "joueur2",
-            "couleur": "red",
-            "score": 2 
-        },
-    "parametres":{
-        'framerate': 10,
-        'plateau' : 4,
-        'taille_fenetre' : 640
-        }
-    }
+...    "plateau": [
+...        [None, None, None, None],
+...        [None,  'joueur2',  'joueur2', None],
+...        [None,  'joueur2',  'joueur2', None],
+...        [None, None, None, None]
+...        ],
+...    "joueur actif": "joueur1",
+...    "joueur1":  {
+...            "nom": "joueur1",
+...            "couleur": "white",
+...            "score": 2 
+...        },
+...    "joueur2":  {
+...            "nom": "joueur2",
+...            "couleur": "red",
+...            "score": 2 
+...        },
+...    "parametres":{
+...        'framerate': 10,
+...        'plateau' : 4,
+...        'taille_fenetre' : 640
+...        }
+...    }
 >>> fin_de_jeu(jeu)
 True
 ==
