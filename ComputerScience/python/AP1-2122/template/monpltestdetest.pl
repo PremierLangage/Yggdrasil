476f9e5dee8c55acc64c63f1b8b6cd505159e9af
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-12-13 15:26:21 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/template/monpltestdetest.pl

--- ComputerScience/python/AP1-2122/template/monpltestdetest.pl
+++ ComputerScience/python/AP1-2122/template/monpltestdetest.pl
@@ -33,7 +33,7 @@ stopfirsterror= False
 testname0=Tests de base sans commentaires
 pltest0==
 >>> def _hid(): pass#
->>> 'g' in globals() == True and type(g)== type(_hid) ## IL faut definir une fonction g. #
+>>> 'g' in globals() == True and type(g)== type(_hid) # IL faut definir une fonction g. #
 >>> f(10) == 2
 True
 >>> f(12)
