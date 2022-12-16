4a4a288a4194c1a56e0be66eff1f1e778853d784
LUCAS OGER <lucas.oger@edu.univ-eiffel.fr>
2022-12-16 12:38:10 +0100

    loger1 modified Yggdrasil/AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-3.pl

---
+++ AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-3.pl
@@ -0,0 +1,30 @@
+title = Changement de variables
+extends = /model/math/expr.pl
+extends = /model/math/input0.pl
+
+before ==
+from sympy import series
+from sympy import Integral
+var('x')
+
+a = randint(1, 5)
+b = randint(a+1, 10)
+
+h = Integral(choice([x*exp(x), 2*atan(x), x*ln(x)]), (x, a, b))
+
+sol = h.doit()
+==
+
+question ==
+Calculer l'intégrale suivante.
+==
+
+embed ==
+==
+
+prefix ==
+$! {{ h|latex }} = !$
+==
+
+latexsettings.order = 'ilex'
+
