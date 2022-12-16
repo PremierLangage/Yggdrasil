57a64d1a8dba915fa3c7f920d44dd49f3d0d96fd
LUCAS OGER <lucas.oger@edu.univ-eiffel.fr>
2022-12-16 13:17:08 +0100

    loger1 modified Yggdrasil/AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-4.pl

--- AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-4.pl
+++ AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-4.pl
@@ -17,19 +17,23 @@ h = choice([Integral(c/((x+1)*(x+2)), (x, a, b)),
     Integral(d/(x**2 + 5*x + 6), (x, a, b)), 
     Integral((c*(x**3) + d*(x**2) + (c-d)*x)/(x**2 + 1), (x, a, b))])
 
-if h == Integral(c/((x+1)*(x+2)), (x, a, b)) :
-    y = $! A/(x+1) + B/(x+2) !$
-if h == Integral(d/(x**2 + 5*x + 6), (x, a, b)) :
-    y = "$! A/(x+2) + B/(x+3) !$"
-if h == Integral((c*(x**3) + d*(x**2) + (c-d)*x)/(x**2 + 1), (x, a, b)) :
-    y = "$! A*x + B + (C*x)/(x**2 + 1) + D/(x**2 + 1) !$"
-
 sol = h.doit()
 ==
 
-question ==
-Calculer l'intégrale suivante, en mettant sous la forme $! {{ y|latex }} !$.
-==
+if h == Integral(c/((x+1)*(x+2)), (x, a, b)) :
+    question ==
+    Calculer l'intégrale suivante, en mettant sous la forme $! A/(x+1) + B/(x+2) !$.
+    ==
+
+if h == Integral(d/(x**2 + 5*x + 6), (x, a, b)) :
+    question ==
+    Calculer l'intégrale suivante, en mettant sous la forme $! A/(x+2) + B/(x+3) !$.
+    ==
+
+if h == Integral((c*(x**3) + d*(x**2) + (c-d)*x)/(x**2 + 1), (x, a, b)) :
+    question ==
+    Calculer l'intégrale suivante, en mettant sous la forme $! A*x + B + (C*x)/(x**2 + 1) + D/(x**2 + 1) !$.
+    ==
 
 embed ==
 ==
