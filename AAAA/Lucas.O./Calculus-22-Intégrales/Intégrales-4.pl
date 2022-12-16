913ea3de448eaeb8b67ba8d0674786106dc76183
LUCAS OGER <lucas.oger@edu.univ-eiffel.fr>
2022-12-16 13:11:33 +0100

    loger1 modified Yggdrasil/AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-4.pl

--- AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-4.pl
+++ AAAA/Lucas.O./Calculus-22-Intégrales/Intégrales-4.pl
@@ -14,14 +14,14 @@ c = randint(1, 6)
 d = randint(1, 4)
 
 h = choice([Integral(c/((x+1)*(x+2)), (x, a, b)),
-    Integral(, (x, a, b)), 
-    Integral(, (x, a, b))])
+    Integral(d/(x**2 + 5*x + 6), (x, a, b)), 
+    Integral((c*(x**3) + d*(x**2) + (c-d)*x)/(x**2 + 1), (x, a, b))])
 
 if h == Integral(c/((x+1)*(x+2)), (x, a, b)) :
-    y = 
-if h == Integral((cos(x))**3, (x, a, b)) :
-    y = sin(x)
-if h == Integral(sqrt(x)/(x+1)**2, (x, a, b)) :
+    y = A/(x+1) + B/(x+2)
+if h == Integral(d/(x**2 + 5*x + 6), (x, a, b)) :
+    y = A/(x+2) + B/(x+3)
+if h == Integral((c*(x**3) + d*(x**2) + (c-d)*x)/(x**2 + 1), (x, a, b)) :
     y = sqrt(x)
 
 sol = h.doit()
