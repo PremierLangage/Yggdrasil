e8bc4b1f8c9395ecc547b03e4b2981e12f5ce725
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2022-12-21 15:20:55 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/concept/variable/initialisation.pl

--- ComputerScience/python/concept/variable/initialisation.pl
+++ ComputerScience/python/concept/variable/initialisation.pl
@@ -18,8 +18,13 @@ type_var = random.choice(["int","str","float","bool"])
 var = random.choice(["a", "b", "c", "X", "Y"])
 
 
-text=f"""Initialisez la variable `{ var}` avec une valeur de type `{ type_var }` quelconque"""
+text=f"""Initialisez la variable {var} avec une valeur de type { type_var } quelconque. """
 
+
+pltest=f"""
+>>> type({})=={} # Vérification du type 
+True
+"""
 
 
 ==
