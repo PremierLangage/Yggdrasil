db3a6602d9e5b061eb4012e2b5cce0622166cf59
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-01-06 10:22:14 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/template/monpltestdetest.pl

--- ComputerScience/python/AP1-2122/template/monpltestdetest.pl
+++ ComputerScience/python/AP1-2122/template/monpltestdetest.pl
@@ -15,7 +15,7 @@ Ce test a pour objectif de tester les capacités du model pltest2022.
 En particulier les options #PL.
 Voici les tests réalisés:
 
-pltest0 = {{pltest0}}
+pltest0 = {{pltest0 | html}}
 Test cachés :
 pltest1 = {{pltest1}}
 
