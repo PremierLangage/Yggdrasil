c8e66eb983ba4af0158532bf10c9dd5ef76dec0e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-01-06 10:14:19 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2122/template/plrunner.py

--- ComputerScience/python/AP1-2122/template/plrunner.py
+++ ComputerScience/python/AP1-2122/template/plrunner.py
@@ -97,7 +97,7 @@ class PlRunner(doctest.DocTestRunner):
                 return None
             found = line.split("#", 1) # couper sur le premier #
             if found[1].startswith("#"): # Numéro du test
-                return " "
+                return " test caché à vous de trouvez le problème "
             else:
                 return found[1]
 
