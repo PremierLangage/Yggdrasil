9c8bb3f8d7a74525d1f25bc246c9b8ef08ec552e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-01-23 12:27:46 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/showcasecours/initialisations/niveau1.pl

---
+++ AAAA/dominique/python/showcasecours/initialisations/niveau1.pl
@@ -0,0 +1,26 @@
+
+extends= ../template/pltest.pl
+
+title= Initialisation de variables 
+
+text==
+
+Pour créer une variable en python il faut l'initialiser, le plus simple est de faire une affectation.   
+
+Par exemple:
+```
+a = 1
+```
+
+
+==
+
+
+
+
+before==
+
+
+
+
+==
