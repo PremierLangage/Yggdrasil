8dcdc01ce49b1d479819909ba692024fe36aceb5
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-02-08 11:47:31 +0100

    drevuz2 modified Yggdrasil/ComputerScience/Algo/Tree/Arbre_de_recherche/extraction.pl

--- ComputerScience/Algo/Tree/Arbre_de_recherche/extraction.pl
+++ ComputerScience/Algo/Tree/Arbre_de_recherche/extraction.pl
@@ -16,7 +16,7 @@
 #            https://creativecommons.org/licenses/by-sa/3.0/fr/
 #**************************************************************************
 #extends=/ComputerScience/Algo/Tree/templates/zipsprogCwithtree.pl
-extends=/ComputerScience/C/template/std_progC.pl
+extends= minimum_abr.pl
 #author=Marc Zipstein
 title=Extraire dans un arbre binaire de recherche
 tag=recherche
@@ -27,6 +27,8 @@ text==
 Écrire une fonction C **extraire** qui extrait une valeur d'un **arbre binaire de recherche**.
 La fonction renvoie 1 en cas d'extraction réussie et 0 sinon.  
 S'il y a deux fils, la valeur sera remplacée par le minimum du sous-arbre droit.
+On pourra utiliser la fonction extraire_minimum écrite plus tot (vous n'avez pas besoin d'en mettre une copie dans votre réponse).
+
 
 
 On utilisera le type   
@@ -60,6 +62,7 @@ int extraire(Tree *t,int val){
   return  extraire(&((*t)->right),val);
   if(a->value!=val)
   return 0;
+  
    if((*t)->left==NULL && (*t)->right==NULL){
     free(*t);
     *t=NULL;return 1;
@@ -84,125 +87,6 @@ return 1;
 
 ==
 
-code_before==#|c|
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define TCODE 300
-typedef struct node{
-  int value;
-  struct node * left;
-  struct node * right;
-}Node, *Tree;
-
-Node * allocate_node(int val){
-  Node * n = malloc(sizeof(Node));
-
-  if (!n){
-    fprintf(stderr, "problème allocation mémoire\n");
-    return NULL;
-  }
-  n->left = NULL;
-  n->right = NULL;
-  n->value = val;
-  return n;
-}
-
-int extraire_minimum(Tree *t,int *min){
-  Tree a,tmp;
-  if(*t==NULL)
-    return 0;
-  a=*t;
-  if(a->left==NULL){
-    *min=(*t)->value;
-    *t=(*t)->right;
-    free(a);
-    return 1;
-  }
-  while(a->left->left!=NULL)
-    a=a->left;
-  *min=a->left->value;
-  tmp=a->left;
-  a->left=a->left->right;
-  free(tmp);
-  return 1;
-}
-
-==
-
-code_after==#|c|
-
-
-int ajoute(Tree *t,int valeur){
-  if (*t == NULL){
-    if ((*t=allocate_node(valeur))==NULL)
-     return 0;
-    return 1;
-  }
-  if (((*t)->value >valeur))
-    return ajoute(&((*t)->left),valeur);
-  if (((*t)->value <valeur))
-    return ajoute(&((*t)->right),valeur);
-
-  return 1;
-} 
-
-int build_tree(Tree* t){
-    int val;
-   
-    while(1==    scanf("%d", &val))   {
-      
-      if (0==ajoute(t,val)){
-      fprintf(stderr, "problème allocation mémoire\n");
-      return 0;
-      }
-    }
-    return 1;
-}
-void arbre_vers_code_aux(Tree t,char* s){ 
-      char c;
-      if(t->left !=NULL && t->right!=NULL)  c='d';
-  else if(t->left !=NULL && t->right==NULL) c='l';
-  else if(t->left ==NULL && t->right!=NULL) c='r';
-  else c='f';
-  sprintf(s+strlen(s),"%c",c);
-  sprintf(s+strlen(s),"%d",t->value); 
-  if(t->left)
-       arbre_vers_code_aux(t->left,s+strlen(s));
- if(t->right)
-       arbre_vers_code_aux(t->right,s+strlen(s));
-}
-
-char *arbre_vers_code(Tree t){
-  static char s[300] ;
-    s[0]='\0';
-  if(NULL!=t)
-    arbre_vers_code_aux(t,s);
-  s[strlen(s)]= 0;
-  return s;
-}
-   
-int main(int argc, char* argv[]){
-  Tree t=NULL;
-  char *code;
-
-
-
-  build_tree(&t);
- 
-   code=arbre_vers_code(t);
-    
-  fprintf(stderr,"arbre avant %s\n",code) ;
-
- extraire(&t,4);
-   
-   code=arbre_vers_code(t);
-    
-  fprintf(stderr,"arbre après %s\n",code) ;
- 
-  return 0;
-}
-==
 
 checks_args_stdin==#|python|
 [["Arbre feuille", [], "4"],
