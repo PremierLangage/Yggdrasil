ceb1cbe797944f2b8f686c1ab1a9d3ffc323f160
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-02-22 14:48:26 +0100

    drevuz2 modified Yggdrasil/exemples/checkboxgroupcsv/checkbox2023.pl

--- exemples/checkboxgroupcsv/checkbox2023.pl
+++ exemples/checkboxgroupcsv/checkbox2023.pl
@@ -10,5 +10,31 @@ extracss ==
     color: red !important;
     font-size: 24px !important;
 }
+.correct {
+ color: green !important;
+    font-size: 24px !important;
+}
 </style>
+==
+
+evaluator==
+right = 0
+total = 0
+for item in group.items:
+    checked = item['checked']
+    iindex = item['index']
+    if iindex in indexs:
+        total += 1
+        item['css'] = 'correct'
+        if checked:
+            right += 1
+            item['css'] = 'success-border'
+    elif checked:
+        item['css'] = 'error-border'
+
+
+if total == 0:
+    grade = (100, 'Right')
+else:
+    grade = ((right / total) * 100, f"{right} / {total}")
 ==
