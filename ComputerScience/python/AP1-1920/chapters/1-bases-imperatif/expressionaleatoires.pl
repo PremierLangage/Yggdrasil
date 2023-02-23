2bd7d478ff0b503a6346e366411983c8712c6bd8
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-02-23 14:12:42 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressionaleatoires.pl

--- ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressionaleatoires.pl
+++ ComputerScience/python/AP1-1920/chapters/1-bases-imperatif/expressionaleatoires.pl
@@ -18,7 +18,7 @@ solved=[ False for n in range(N)]
 isfloat = [ False for n in range(N)]
 inputs = []
 for i in range(N):
-    inp = MathInput()
+    inp = Input()
     globals()[f"input{i}"] = inp
     inputs.append(inp)
 ==
