275de9d77ffdc3b186e6ca5d313b108a80b2b36b
Orhan UYAR <orhan.uyar@univ-eiffel.fr>
2023-02-27 11:31:44 +0100

    ouyar1 modified Yggdrasil/AAAA/Orhan/fix_expressions_booleennes.pl

--- AAAA/Orhan/fix_expressions_booleennes.pl
+++ AAAA/Orhan/fix_expressions_booleennes.pl
@@ -84,8 +84,7 @@ form== #|html|
 {% for i in inputs %}
 <!--{% if not solved[loop.index0]%}-->
 <p>{{i}}</p>
-{% set r = globals()[i] %}
-<p>{{ getattr(i) }}</p>
+<p>{{ getattr(i) }}</pw>
 
 <li>  {{enonce[loop.index0]}} = </li>
 <!--{% endif %}-->
