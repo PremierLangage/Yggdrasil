6ec9e6a8e97e592978765325e75a38355357434a
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-03-03 15:28:48 +0100

    tsaillard modified Yggdrasil/AAAA/Thomas/test.pl

--- AAAA/Thomas/test.pl
+++ AAAA/Thomas/test.pl
@@ -4,6 +4,27 @@ version = 0.0.1
 title = Untitled
 statement = No statement
 formState =: CodeEditor
+
+form = CodeEditor
+
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = python
+editor.code ==
+# write your code here
+==
+
+grader == 
+
+def grade(..):
+    return 1
+
+
+==
+
+
+
 text ==
 Coucou les coupains
 ==
