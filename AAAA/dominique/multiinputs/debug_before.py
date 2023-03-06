707ffbddf423f49a60480015454a0d991529daea
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-03-06 11:45:49 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/multiinputs/debug_before.py

--- AAAA/dominique/multiinputs/debug_before.py
+++ AAAA/dominique/multiinputs/debug_before.py
@@ -34,6 +34,7 @@ if __name__ == "__main__":
             exec(dic['before'],dic)
         except Exception as e:
             print(str(e), file=sys.stderr)
+            sys.exit(1)
         exec("", glob)
         for key in glob:
             if key in dic and dic[key] == glob[key]:
