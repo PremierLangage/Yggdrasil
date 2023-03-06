1e3b854c63748628a024f927c099366481ff2431
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-03-06 15:46:31 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/multiinputs/mulitgrp.pl

--- AAAA/dominique/multiinputs/mulitgrp.pl
+++ AAAA/dominique/multiinputs/mulitgrp.pl
@@ -15,7 +15,7 @@ from string import ascii_letters
 nb_variables=3
 nb_questions = 4
 inputs = []
-affectations = { var : random.choice(["True","False"]) for var in ascii_letter[:nb_variables] }
+affectations = { var : random.choice(["True","False"]) for var in ascii_letters[:nb_variables] }
 for i in range(nb_questions):
     txt,val = 1,2
 res = ["True","False","Error","True"]
