44c899d59048d09f413f00447534106d55f447e4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-03-06 15:10:46 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/multiinputs/mulitgrp.pl

--- AAAA/dominique/multiinputs/mulitgrp.pl
+++ AAAA/dominique/multiinputs/mulitgrp.pl
@@ -23,7 +23,7 @@ for i in range(nb_questions):
     for X in ["True","False","Error"]:
         Ri= str(uuid.uuid4())
         globals()[f"input{i}"].items.append({
-            "id":Ri.hex,
+            "id":Ri,
             "content": str(X)
         })
         if X==res[i]: 
