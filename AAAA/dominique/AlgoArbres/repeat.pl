c47bf5cea944b5b61da930aa2a3423806ed35987
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-03-27 10:57:25 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/AlgoArbres/repeat.pl

---
+++ AAAA/dominique/AlgoArbres/repeat.pl
@@ -0,0 +1,40 @@
+
+extends = /ComputerScience/Algo/Tree/BinaryTree/string_code_bin_tree.pl
+
+evaluator==#|python|
+form = """
+<style>
+.error-state{
+   padding: 10px;
+}
+.success-state{
+   padding: 10px;
+}
+</style>
+"""
+
+def reduce_string(s):
+    """
+    Return a string built from `s` but containing only ascii letter `d,l,r,f` 
+    and number.
+    """
+    ans = ""
+    for c in s:
+        if c in ['d','l','r','f','0','1','2','3','4','5','6','7','8','9']:
+            ans += c
+    return ans
+
+def cmp_str_delim(s1, s2):
+    """
+    to comparate two string code of binary tree but be persimive. 
+    """
+    return reduce_string(s1) == reduce_string(s2)
+
+if cmp_str_delim(soluce, inputbox.value):
+    grade = (100, ''' <span class="success-state">Bravo, c'est exactement cela : '''+str(soluce)+'</span>')
+else:
+    grade = (0, ''' <span class="error-state">Désolé, vous avez fait une erreur, la réponse attendue était : '''+str(soluce)+'</span>')
+
+exec(before,globals())
+
+==
