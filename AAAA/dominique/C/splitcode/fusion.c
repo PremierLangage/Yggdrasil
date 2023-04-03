9357036a57f065366b0ea4645d9d1c99ac17136a
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-03 16:09:25 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/splitcode/fusion.c

---
+++ AAAA/dominique/C/splitcode/fusion.c
@@ -0,0 +1,74 @@
+
+
+// PL:enonce==
+/*
+//Ecrire une fonction *fusion* qui prend deux arbres binaires de recherche en paramêtres et retourne un nouvel arbre
+//qui contient tout les éléments présents dans au moins un des deux arbres. Le type d'arbre a utiliser est :
+*/
+typedef struct _noeud {
+    struct _noeud *gauche;
+    struct _noeud *droite;
+    int valeur;
+} Noeud,*Arbre;
+// PL:==
+
+// PL:code_before==
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+// PL:==
+
+// PL:prototype==
+Arbre fusion(Arbre a1, Arbre a2);
+// PL:==
+
+// PL:solution==
+Arbre fusion(Arbre a1, Arbre a2) {
+    if (a1 == NULL) return a2;
+    if (a2 == NULL) return a1;
+
+    Arbre result = malloc(sizeof(Noeud));
+    if (result == NULL) {
+        fprintf(stderr, "Erreur: impossible d'allouer la mémoire\n");
+        exit(EXIT_FAILURE);
+    }
+
+    result->valeur = a1->valeur + a2->valeur;
+    result->gauche = fusion(a1->gauche, a2->gauche);
+    result->droite = fusion(a1->droite, a2->droite);
+
+    return result;
+}
+// PL:==
+
+// PL:code_after==
+int main() {
+    Arbre a1 = malloc(sizeof(Noeud));
+    a1->valeur = 1;
+    a1->gauche = NULL;
+    a1->droite = malloc(sizeof(Noeud));
+    a1->droite->valeur = 3;
+    a1->droite->gauche = NULL;
+    a1->droite->droite = NULL;
+
+    Arbre a2 = malloc(sizeof(Noeud));
+    a2->valeur = 2;
+    a2->gauche = malloc(sizeof(Noeud));
+    a2->gauche->valeur = 1;
+    a2->gauche->gauche = NULL;
+    a2->gauche->droite = NULL;
+    a2->droite = malloc(sizeof(Noeud));
+    a2->droite->valeur = 3;
+    a2->droite->gauche = NULL;
+    a2->droite->droite = NULL;
+
+    Arbre result = fusion(a1, a2);
+    printf("%d %d %d %d\n", result->valeur, result->gauche->valeur, result->gauche->gauche == NULL, result->gauche->droite == NULL);
+    printf("%d %d %d %d\n", result->droite->valeur, result->droite->gauche == NULL, result->droite->droite == NULL, result->droite->gauche == NULL ? 1 : result->droite->gauche->valeur);
+
+    return 0;
+}
+// PL:==
+
