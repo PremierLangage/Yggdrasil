0fd3dd65569795c38c17f18e7170ca39336c877a
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-04 09:29:10 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/splitcode/fusion.c

--- AAAA/dominique/C/splitcode/fusion.c
+++ AAAA/dominique/C/splitcode/fusion.c
@@ -4,7 +4,13 @@
 /*
 Ecrire une fonction *fusion* qui prend deux arbres binaires de recherche en paramêtres et retourne un nouvel arbre
 qui contient tout les éléments présents dans au moins un des deux arbres. Le type d'arbre a utiliser est :
-
+```
+typedef struct _noeud {
+    struct _noeud *gauche;
+    struct _noeud *droite;
+    int valeur;
+} Noeud,*Arbre;
+```
 
 
 // PL:==
