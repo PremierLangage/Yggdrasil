bf9ce4a72ff879d995eb4e0f4bdffc12bce1ce4c
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-07 10:26:56 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/database_utils.py

--- AAAA/Thomas/database_utils.py
+++ AAAA/Thomas/database_utils.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 def get_session(table_class, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
     engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
-    if not inspect(engine).has_table(engine, table_class.__tablename__):
+    if not inspect(engine).has_table(table_class.__tablename__):
         declarative_base().metadata.create_all(engine)
     return sessionmaker(bind=engine)
