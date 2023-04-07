a37ee08861faab4b1b318e6e888633fd906562ed
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-07 11:10:06 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/test_db.pl

--- AAAA/Thomas/test_db.pl
+++ AAAA/Thomas/test_db.pl
@@ -30,7 +30,7 @@ class Response(Base):
     def __repr__(self):
           return f"{self.student_id} à répondu : {self.response}"
 
-session = get_session(table_class = Response, Base)
+session = get_session(table_class = Response, base=Base)
 if not session.query(Response).filter(Response.student_id == 0).all() : 
     session.add(Response(student_id = 0, response = "Salut ! Comment ça va?"))
     session.commit()
