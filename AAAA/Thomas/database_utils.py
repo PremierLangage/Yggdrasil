c424ed5b5c798d846731f32caf6ddef5188abdd8
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-12 15:29:15 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/database_utils.py

--- AAAA/Thomas/database_utils.py
+++ AAAA/Thomas/database_utils.py
@@ -4,6 +4,16 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
+class Response(Base):
+    base = Base
+    __tablename__ = "test_db_student_1" 
+    id = Column("id", Integer, primary_key=True)
+    student_id = Column("student_id", Integer)
+    response = Column("response", String)
+
+    def __repr__(self):
+          return f"{self.student_id} à répondu : {self.response}"
+
 def get_session(table_class, base, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
     engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
     if not inspect(engine).has_table(table_class.__tablename__):
