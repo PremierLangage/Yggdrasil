ce56f41ab53962e2747c7c8ed9792ef5bb43c0b6
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-12 12:58:12 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/database_utils.py

--- AAAA/Thomas/database_utils.py
+++ AAAA/Thomas/database_utils.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine, ForeignKey, inspect
 from sqlalchemy.orm import sessionmaker
 
+Base = declarative_base()
+
 def get_session(table_class, base, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
     engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
     if not inspect(engine).has_table(table_class.__tablename__):
