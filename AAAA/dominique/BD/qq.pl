116fdc1256b62e887bb4006781ffc5654e8f8c31
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-16 09:58:49 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/BD/qq.pl

--- AAAA/dominique/BD/qq.pl
+++ AAAA/dominique/BD/qq.pl
@@ -9,7 +9,7 @@ before==
 import sqlachemy
 version = sqlalchemy.__version__
 
-text = f"Version de sqlachemy:{sqlalchemy.__version__}
+text = f"Version de sqlachemy:{sqlalchemy.__version__}"
 
 ==
 
