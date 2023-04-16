3cab4870640ffc82292f3f66bf731b4e85448f6b
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-16 09:57:15 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/BD/qq.pl

---
+++ AAAA/dominique/BD/qq.pl
@@ -0,0 +1,14 @@
+
+
+
+title= introspection BD 
+builder =@ /builder/bbefore.py
+
+before==
+
+import sqlachemy
+version = sqlalchemy.__version__
+
+text = f"Version de sqlachemy:{sqlalchemy.__version__}
+
+==
