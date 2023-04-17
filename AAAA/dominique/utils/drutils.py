dd2791ca7ddfd9349b6de9c09c10c072f1ed6765
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-17 17:56:46 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/utils/drutils.py

--- AAAA/dominique/utils/drutils.py
+++ AAAA/dominique/utils/drutils.py
@@ -5,8 +5,25 @@ def raiseifabsent(name, comment=" variable obligatoire"):
         raise Exception(" Missing variable :"+name+comment)
 
 
-def additems(group, listitem):
-    
+def additems(group, listitem, step):
 
+    group.items = []
+    for num,item for enumerate(listitem) :
+    group.items.append({
+        "id": f"crit{step}_{}",
+        "content": "L'auteur a laissé des éléments inutiles ou des fichiers mal identifiés dans ses contenus."
+    })
+    group.items.append({
+        "id": "crit1ok",
+        "content": "Les productions transmises et leurs éléments sont corrects et bien identifiés."
+    })
+    group.items.append({
+        "id": "crit1ok+",
+        "content": "Les productions transmises sont particulièrement bien structurées et la précision du nommage <br />ne laisse aucun doute sur le contenu des répertoires et fichiers."
+    })
+    group.items.append({
+        "id": "crit1ok++",
+        "content": "Les contenus transmis sont à la fois concis et complets. Un maximum d'éléments sont générés <br />automatiquement rendant la production minimale en taille lors de sa diffusion."
+    })
 
     
