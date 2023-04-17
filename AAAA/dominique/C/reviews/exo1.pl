5419bb95ee48a97c4d2178a1642510eb04fffbf8
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-17 17:43:32 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/exo1.pl

--- AAAA/dominique/C/reviews/exo1.pl
+++ AAAA/dominique/C/reviews/exo1.pl
@@ -32,5 +32,28 @@ forms = firstform + buildform(formfills)
 
 evaluator==
 
+if step==0:
+        report += "# Rapport de relecture de code\n\n"
+    report_html += "<h1>Rapport de relecture de code</h1><br />"
+    if len(response['code']) > 0:
+        report += "Production soumise à la relecture : **"+response['code']+"**\n"
+        report_html += "Production soumise à la relecture : <b>"+response['code']+"</b><br />"
+    if len(response['author']) > 0:
+        report += "Projet/code produit par **"+response['author']+"**\n"
+        report_html += "Projet/code produit par <b>"+response['author']+"</b><br />"
+    report += "Relecture opérée"
+    report_html += "Relecture opérée"
+    if len(response['nom']) > 0:
+        report += " par **"+response['nom']+"**"
+        report_html += " par <b>"+response['nom']+"</b>"
+    report += " le "+str(date.today().day)+" "+mois[date.today().month - 1]+" "+str(date.today().year)
+    report_html += " le "+str(date.today().day)+" "+mois[date.today().month - 1]+" "+str(date.today().year)
+    report += "\n\n"
+    report_html += "<br /><br />"
+
+if step <len(texts):
+    
+    form=f''' {{ group|component }} \n<br />\nPrécisions le cas échéant (en particulier si vous avez des suggestions d\'amélioration) : <br /> 
+       '<textarea id="form_comcrit{step}" rows="6" cols="80" ></textarea>'''
 
 ==
