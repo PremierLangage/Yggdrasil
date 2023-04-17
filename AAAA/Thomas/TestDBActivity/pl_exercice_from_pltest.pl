3e24907e70159d3cbf17bedbe2f7afcb1094af68
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-17 17:32:29 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl

--- AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
+++ AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
@@ -5,6 +5,26 @@ author = Thomas SAILLARD
 
 title = Exercice python sur les listes (test pour intéraction avec la base de données)
 
+text==
+
+Ecrire un deux fonctions carre et cube qui calcul le carre et le cube du parametre 
+
+Créer un fichier qui hérite de celui ci /ComputerScience/python/template/pltest.pl pour créer vos premier tests.
+
+```
+    pltest==
+    >>> f == f # La fonction existe  ?
+    True
+    >>> f() # f retourne ou affiche  42
+    42
+    >>> f() == 42 # retourne 42 
+    True
+    ==
+```
+
+
+==
+
 pltest==
 >>> f == f # La fonction existe  ?
 True
