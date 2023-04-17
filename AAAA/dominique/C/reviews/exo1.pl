d54f3879ce261e258adec4bab88d43a9999217b0
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-17 17:37:06 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/exo1.pl

--- AAAA/dominique/C/reviews/exo1.pl
+++ AAAA/dominique/C/reviews/exo1.pl
@@ -8,12 +8,20 @@ form=
 builder=@ /builder/bbefore.py
 grader=
 
+
+first==
+## **Bienvenu sur l'assistant online d'aide à la génération de rapport pour la review de code en langage C.**
+<br />
+Commencez par vous présenter, précisez l'auteur du code reviewé ainsi qu'une description pour la production évaluée.
+
+Prennez votre temps pour valider pas à pas votre rapport mais ATTENTION n'actualiser la page à aucun moment, vous perdriez votre début de rapport et cela réinitialiserait le générateur à la première étape.
+==
 before==
 from drutils import raiseifabsent
 
 raiseifabsent('texts', ' Liste de critères ')
 texts = first + texts
 raiseifabsent('formfills','liste de choix des différents critères ')
-
+froms = fristform + buildform(formfills)
 
 ==
