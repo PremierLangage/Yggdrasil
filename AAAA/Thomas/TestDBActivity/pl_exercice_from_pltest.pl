00cb4a6c98a6ebe0b11ef283b185a275c1933d1f
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-17 17:30:22 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl

--- AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
+++ AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
@@ -5,5 +5,13 @@ author = Thomas SAILLARD
 
 title = Exercice python sur les listes (test pour intéraction avec la base de données)
 
+pltest==
+>>> f == f # La fonction existe  ?
+True
+>>> f() # f retourne ou affiche  42
+42
+>>> f() == 42 # retourne 42 
+True
+==
 
 
