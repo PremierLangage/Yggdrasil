72f0f311f5417bdab0ad9f6ef8230d50e3969637
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-17 12:42:09 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDB/database_utils.py

--- AAAA/Thomas/TestDB/database_utils.py
+++ AAAA/Thomas/TestDB/database_utils.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine, ForeignKey, inspect
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import mapped_column
 
 Base = declarative_base()
 
@@ -14,6 +15,14 @@ class Response(Base):
 
     def __repr__(self):
           return f"{self.student_id} à répondu : {self.response}"
+
+
+class Evalutaion(Base):
+    base = Base
+    id = Column("id", Integer, primary_key=True)
+    student_id = Column("student_id", Integer)
+    eval = Column("eval", String)
+
 
 def get_session(table_class, base, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
     engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
