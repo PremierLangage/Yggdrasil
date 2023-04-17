d0f6ef0d607d8ff8c70d933787e27d6da47f8408
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-17 17:39:43 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/utils/drutils.py

--- AAAA/dominique/utils/drutils.py
+++ AAAA/dominique/utils/drutils.py
@@ -5,3 +5,8 @@ def raiseifabsent(name, comment=" variable obligatoire"):
         raise(" Missing variable :"+name+comment)
 
 
+def buildform(formfill):
+
+    theform=''' {{ group|component }} \n<br />\nPrécisions le cas échéant (en particulier si vous avez des suggestions d\'amélioration) : <br /> 
+       '<textarea id="form_comcrit{level}" rows="6" cols="80" ></textarea>'''
+    
