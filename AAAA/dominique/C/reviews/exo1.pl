f00bd9dbc69401567cae66332dc9b1f60784e375
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-17 17:26:58 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/exo1.pl

--- AAAA/dominique/C/reviews/exo1.pl
+++ AAAA/dominique/C/reviews/exo1.pl
@@ -1,2 +1,17 @@
 
 builder= @ /builder/bbfore.py
+
+
+before==
+
+if not 'texts' in globals():
+    printf("Ce model necessite une liste de textes de description de chaque critères.")
+    print(" '**Critére 1** : Description du critère 1' ")
+    exit(1)
+texts = first + texts
+
+if not 'formsfill' in globals():
+    printf("Ce model necessite une liste de textes de description de chaque critères.")
+    print(" '**Critére 1** : Description du critère 1' ")
+    exit(1)
+==
