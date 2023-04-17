5c003008d2e5124347515ff85a7219a54e71dc40
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-17 18:05:11 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/exo1.pl

--- AAAA/dominique/C/reviews/exo1.pl
+++ AAAA/dominique/C/reviews/exo1.pl
@@ -27,9 +27,9 @@ Prennez votre temps pour valider pas à pas votre rapport mais ATTENTION n'actua
 before==
 from drutils import raiseifabsent, additems
 texts=["critère1", "The other criture"]
-raiseifabsent('texts', ' Liste de critères ')
+raiseifabsent('texts', ' Liste de critères ', globals())
 texts = first + texts
-raiseifabsent('formfills','liste de choix des différents critères ')
+raiseifabsent('formfills','liste de choix des différents critères ', globals())
 forms = firstform + formfills
 
 ==
