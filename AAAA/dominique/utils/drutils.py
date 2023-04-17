bda8220d9b681f2e2b1a81c47f9cd32577f972de
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-17 17:58:06 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/utils/drutils.py

--- AAAA/dominique/utils/drutils.py
+++ AAAA/dominique/utils/drutils.py
@@ -1,5 +1,6 @@
 
 
+
 def raiseifabsent(name, comment=" variable obligatoire"):
     if name not in globals():
         raise Exception(" Missing variable :"+name+comment)
@@ -8,22 +9,10 @@ def raiseifabsent(name, comment=" variable obligatoire"):
 def additems(group, listitem, step):
 
     group.items = []
-    for num,item for enumerate(listitem) :
-    group.items.append({
-        "id": f"crit{step}_{}",
-        "content": "L'auteur a laissé des éléments inutiles ou des fichiers mal identifiés dans ses contenus."
-    })
-    group.items.append({
-        "id": "crit1ok",
-        "content": "Les productions transmises et leurs éléments sont corrects et bien identifiés."
-    })
-    group.items.append({
-        "id": "crit1ok+",
-        "content": "Les productions transmises sont particulièrement bien structurées et la précision du nommage <br />ne laisse aucun doute sur le contenu des répertoires et fichiers."
-    })
-    group.items.append({
-        "id": "crit1ok++",
-        "content": "Les contenus transmis sont à la fois concis et complets. Un maximum d'éléments sont générés <br />automatiquement rendant la production minimale en taille lors de sa diffusion."
-    })
-
+    for num,item in enumerate(listitem) :
+        group.items.append({
+            "id": f"crit{step}_{num}",
+            "content": item
+        })
+    return group
     
