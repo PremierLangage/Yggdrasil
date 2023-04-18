8ce71e92e9d2b266ca385e624c2b10aa0acda870
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-18 17:20:38 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/exo1.pl

--- AAAA/dominique/C/reviews/exo1.pl
+++ AAAA/dominique/C/reviews/exo1.pl
@@ -73,6 +73,7 @@ if step==0:
     report += "\n\n"
     report_html += "<br /><br />"
     step = 1
+    text=texts[1]
     form="{{ group|component }} \n"+f''' <br />\nPrécisions le cas échéant (en particulier si vous avez des suggestions d\'amélioration) : <br /> 
     '<textarea id="form_comcrit{step}" rows="6" cols="80" ></textarea>'''
     group= additems(group,formfills[step], step)
@@ -91,7 +92,7 @@ elif step <len(texts):
     if name in response and len(response[name]) > 0:
         report += response[name]+"\n\n"
         report_html += response[name]+"<br /><br />"
-    text= report_html+texts[step]
+    text=texts[step]
     form=f''' {{ group|component }} \n<br />\nPrécisions le cas échéant (en particulier si vous avez des suggestions d\'amélioration) : <br /> 
        '<textarea id="form_comcrit{step}" rows="6" cols="80" ></textarea>'''
     group= additems(group,formfills[step], step)
