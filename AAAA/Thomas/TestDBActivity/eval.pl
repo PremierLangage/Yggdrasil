372c3b3a956b723779ca0dc22d6a9eb5e03f0f86
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-18 13:59:13 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/eval.pl

--- AAAA/Thomas/TestDBActivity/eval.pl
+++ AAAA/Thomas/TestDBActivity/eval.pl
@@ -1,90 +1,164 @@
+# /!\data et criteria à supprimer après les tests (dans la balise before)
+
+# permet de faire évaluer une réponse (textuelle ou autre) d'un élève par qqun d'autre par le biais d'une grille critériée 
+# -paramètres : 
+#     -data : la réponse de l'élève
+#     -criteria : les critère d'evaluation
+# -balise :
+#     -comment_by_criteria, type -> string, par défault False : option pour avoir un commentaire par critère
+# -retourne : un dico avec la valeur pour chaque critère (et potentiel commentaire respectif), un commentaire et une note.
+
+#author=Fanny Garnevault
+
 
 @ /utils/sandboxio.py
-grader  =@ /grader/evaluator.py
-builder =@ /builder/before.py
+@ /grader/evaluator.py [grader.py]
+@ /builder/before.py [builder.py]
 
-group =: RadioGroup
-group.horizontal = True
+title = Evaluation question
+text  = 
 
-# GENERATE A RANDOM QUESTION
-before==
-import random
-import uuid
+# balise comment_by_criteria : option pour avoir un commentaire par critère
+comment_by_criteria = True
 
-A = random.randint(0, 10)
-B = random.randint(0, 10)
-R = uuid.uuid4()
 
-# append random false answers to group.items array.
-group.items = []
-for i in range(4):
-    n = random.randint(0, 100)
-    while n == A * B:
-        n = random.randint(0, 100)
-    group.items.append({
-        "id": uuid.uuid4(), # generate a random id instead of an hardcoded to avoid cheat
-        "content": str(n)
-    })
+before == #|python|
+# paramètre data contenant la question et la réponse de l'élève
+#data = {"question": "Quel âge avez-vous ?", "answer": "J'ai 20 ans."}
 
-# append random right answer to group.items array.
-group.items.append({
-    "id": R,
-    "content": str(A * B)
-})
+# paramètre criteria contenant la grille critériée
+criteria =[
+    {
+        "id":0,
+        "description":"Lisibilité du code",
+        "levels":[
+            {"id":0, "description":"Le code est illisible.", "points":0},
+            {"id":1, "description":"Le code est assez lisible.", "points":1},
+            {"id":2, "description":"Le code est très lisible.", "points":2}
+        ]
+   },
+   {
+        "id":1,
+        "description":"Nommage des variables",
+        "levels":[
+            {"id":0, "description":"Aucun effort de nommage.", "points":0},
+            {"id":1, "description":"Les noms de variable sont corrects mais peut mieux faire.", "points":1},
+            {"id":2, "description":"Le nom de chaque est précisement choisi.", "points":2}
+        ]
+   },
+   {
+        "id":2,
+        "description":"Présence de commentaires",
+        "levels":[
+            {"id":0, "description":"Le code n'est pas du tout commenté.", "points":0},
+            {"id":1, "description":"Le code possède quelques commentaires mais est partiellement flou.", "points":1},
+            {"id":2, "description":"Le code est très bien commenté, facilement compréhensible.", "points":2}
+      ] 
+   }
+]
+note_max = 0
+for crit in criteria:
+    note_intermediaire = crit['levels'][0]['points']
+    for niv in crit['levels']:
+        if note_intermediaire < niv['points']:
+            note_intermediaire = niv['points']
+    note_max += note_intermediaire
 
-# shuffle the items
-random.shuffle(group.items)
+radio = []
+for value in criteria :
+    tmp = RadioGroup(cid=str(value["id"]))
+    tmp.description = value["description"]
+    tmp.items = []
+    for elem in value["levels"] :
+        tmp.items.append({"id": str(elem["id"]), "content": elem["description"]})
+    globals()[str(value["id"])] = tmp
+    radio.append(vars(tmp))
 ==
 
-title = Radio Group Component
 
-text==
-Select the result of **{{ A }}** x **{{ B }}**.
+form  == #|html|
+<b style="color: red;">Remplissez tous les champs de l'évaluation</b>
+<br/><br/>
+Enoncé de la question :  
+<p style="color: rgb(50, 100, 250);">{{data.question}}<p>
+<br/>
+Réponse de l'élève :  
+<p style="color: rgb(50, 100, 250);">{{data.answer}}<p>
+<br/>
+
+{% for rg in radio %} 
+    <br/>
+    <span style="padding-left:30px;font-size:18px;">
+            <b>{{rg.description}}</b>
+    </span>
+    <br/>
+    {{ rg|component }}
+    {% if comment_by_criteria != "False" %}
+        <p>Justificatif :</p>
+        <textarea id="form_commentaire_{{rg.cid}}" name="justificatif" cols=30% rows="2"></textarea>
+    {% endif %}
+    <br/><br/>
+{% endfor %}
+
+<br/>
+<p>Explication et commentaire général :</p>
+<textarea id="form_commentaire" name="Commentaire" cols=100% rows="5"></textarea>
 ==
 
-# PRESENT THE QUESTION TO THE STUDENT
-form==
-{{ group|component }}
-==
 
-# EVALUATE THE STUDENT ANSWER
-evaluator==
-S = group.selection
-score = 0
-feedback = '<span class="error-state animated pulse infinite">Bad answer</span>'
+evaluator == #|python|
+from math import ceil
 
-for item in group.items:
-    item['css'] = ''
-    if item['id'] == S:
-        if S == R:
-            item['css'] = 'success-border'
-            score = 100
-            feedback = '<span class="success-state animated pulse infinite">Good answer</span>'
+error = 0
+note_student = 0
+
+# vérifie que le correcteur a répondu à toutes les réponses radio
+# en même temps on calcule les points de la copie évaluée en fonction des réponses cliquées
+feedback = "Réponses : "
+for crit in criteria:
+    num = str(crit["id"])
+    tmpId = response[num]['selection']
+    if tmpId == '':
+        error = 1
+        break
+    else:
+        for i, e in enumerate(response[num]['items']):
+            if e['id'] == tmpId:
+                feedback += "<br/>" + e['content']
+                break
+        # calcul des points de la copie
+        for niv in crit['levels']:
+            if response[num]['items'][i]['content'] == niv['description']:
+                note_student += niv['points']
+                break
+
+# vérifie que le correcteur a répondu à toutes les réponses textarea des radio
+if comment_by_criteria != "False" and not error:
+    for crit in criteria:
+        num = str(crit["id"])
+        if response["commentaire_"+num] == "":
+            error = 1
+            break
         else:
-            item['css'] = 'error-border'
-    elif item['id'] == R:
-        item['css'] = 'success-border animated pulse infinite'
+            feedback += "<br/>justificatif " +num+ " : " + response["commentaire_"+num]
 
-grade = (score, feedback)
+# vérifie que le correcteur a répondu à la dernière textarea du commentaire général
+if not error:
+    if response["commentaire"] == "":
+        error = 1
+    else:
+        feedback += "<br/>commentaire : " + response['commentaire']
+
+# règle de trois pour le calcul de la note de la copie sur 100 (entre 0 et 100)
+if note_student <= 0:
+    response['note'] = 0
+else:
+    response['note'] = ceil((note_student*100)/note_max)
+
+if error:
+    grade = (0, "Répondez à toutes les questions")
+else:
+    feedback += "<br/>note = "+str(response['note'])
+    grade = (100, feedback)
 ==
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
