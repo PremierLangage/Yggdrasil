0074cf92d250c0efc38bca0c02bb93403c16692c
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-18 13:52:21 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl

--- AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
+++ AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
@@ -39,7 +39,6 @@ from database_utils import get_session, Base, Response
 from sqlalchemy import Column, String, Integer
 import sys
 
-
 if int(grade) == 100:
     with get_session(table_class = Response, base=Base) as session:
         session.add(Response(student_id = user__id if user__id else session__id, value = answer))
