912139d615aef23ff742e34f93a0b322ae3ff55e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-19 15:38:36 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/buildcrit.pl

---
+++ AAAA/dominique/C/reviews/buildcrit.pl
@@ -0,0 +1,39 @@
+
+
+
+
+before==
+
+
+## une liste de noms de variables 
+## pour produire un formulaire de saisie
+
+def make_form(namelist): # type: (List[(str,str)]) -> str
+    form = '<table style="border-spacing: 30px; border-collapse: separate;">'
+    for name in namelist:
+        if len(name[1]) == 0:
+            form += f'<tr><td>{name[0]}</td><td> <input type=text id="form_{name[0]}" /> </td></tr>'
+        else:
+            form += f'<tr><td>{name[0]}</td><td> <input title="{name[1]}" type=text id="form_{name[0]}" /> </td></tr>'
+    form += '</table>'
+    return form
+
+def make_evaluator(response, namelist):
+    d={}
+    for name in namelist:
+        d[name] = response.get(name, "")
+    return d
+
+def streval(response, namelist):
+    d = make_evaluator(response, namelist)
+    s=""
+    for name in namelist:
+        s += f"{name} = {d[name]}<BR> "
+    return s
+
+
+    
+form = make_form([('nom',' votre nom'),('sujet',' le titre du travaiil a accomplir')])
+text= "remplir les champs" 
+
+==
