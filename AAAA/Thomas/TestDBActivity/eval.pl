30c364d429926602a11a27855a89d0526ae1e408
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-19 17:21:18 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/eval.pl

--- AAAA/Thomas/TestDBActivity/eval.pl
+++ AAAA/Thomas/TestDBActivity/eval.pl
@@ -150,7 +150,7 @@ with get_session(table_class = Evaluation, base=Base) as session:
         session.add(Evaluation(student_id = user__id if user__id else session__id, eval = str(response), response_id = response_id))
         session.commit()
         feedback = '<span class="success-state center">Votre formulaire à bien été transmit</span>'
-        form = ""
+        form = "<br/><br/><br/><br/><br/><br/>"
     except: 
         feedback = '<span class="error-state center">Le serveur a rencontré une erreur, merci de réessayer. Si le problème persiste veuillez contacter votre professeur</span>'
 
