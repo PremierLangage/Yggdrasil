28e807cd0a3841e7e36613208714fc53e2b1e563
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-19 15:53:50 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/buildcrit.pl

--- AAAA/dominique/C/reviews/buildcrit.pl
+++ AAAA/dominique/C/reviews/buildcrit.pl
@@ -3,6 +3,9 @@
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
+@ make_stuff.py [tools.py]
+
+
 author = Dominique Revuz 2023
 
 title = Générateur de grilles critèriées
@@ -17,28 +20,6 @@ before==
 ## une liste de noms de variables 
 ## pour produire un formulaire de saisie
 
-def make_form(namelist): # type: (List[(str,str)]) -> str
-    form = '<table style="border-spacing: 30px; border-collapse: separate;">'
-    for name in namelist:
-        title =''
-        if len(name[1]) != 0:
-            title=f'title="{name[1]}"'
-        form += f'<tr><td>{name[0]}</td><td> <input required {title} type=text id="form_{name[0]}" /> </td></tr>'
-    form += '</table>'
-    return form
-
-def make_evaluator(response, namelist):
-    d={}
-    for name in namelist:
-        d[name] = response.get(name, "")
-    return d
-
-def streval(response, namelist):
-    d = make_evaluator(response, namelist)
-    s=""
-    for name in namelist:
-        s += f"{name} = {d[name]}<BR> "
-    return s
 
 
 listofnames =[('nom',' votre nom'),('sujet',' le titre/description du travail a accomplir')]
