93dbfbd9b940d02066181d093e137394c1272d3b
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-04-19 09:52:53 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/reviews/exo1.pl

--- AAAA/dominique/C/reviews/exo1.pl
+++ AAAA/dominique/C/reviews/exo1.pl
@@ -22,9 +22,10 @@ firstform==
 <tr><td>Votre <b>nom</b> (en tant que relecteur) </td><td> <input type=text id="form_nom" /> </td></tr>
 <tr><td>Nom de l'<b>auteur</b> de la production </td><td> <input type=text id="form_author" /> </td></tr>
 <tr><td>Nom du code ou <b>projet</b> relu </td><td> <input type=text id="form_code" /> </td></tr>
+<tr><td>Note</td><td> <input type=number id="form_note" /> </td></tr>
 </table>
 ==
-#<tr><td>Note</td><td> <input type=number minInteger=0 maxInteger=100 id="form_note" /> </td></tr>
+#
 
 first==
 ## **Bienvenu sur l'assistant online d'aide à la génération de rapport pour la review de code en langage C.**
