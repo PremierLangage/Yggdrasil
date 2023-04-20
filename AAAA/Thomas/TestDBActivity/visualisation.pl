4907441575b1c390a7cdc93aedf8c5b7ff3d6efa
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-20 15:58:02 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/visualisation.pl

--- AAAA/Thomas/TestDBActivity/visualisation.pl
+++ AAAA/Thomas/TestDBActivity/visualisation.pl
@@ -14,6 +14,7 @@ before==#|py|
 from database_utils import get_session, Base, Response
 
 responses = list()
+display_data = list()
 
 with get_session(table_class= Response, base=Base) as session:
     if user__role == "teacher":
@@ -21,38 +22,36 @@ with get_session(table_class= Response, base=Base) as session:
     else :
         responses = session.query(Response).filter(Response.student_id == user__id).all()
 
-display_data = list()
+    for response in responses:
+        rep_dic = dict()
+        code_editor = CodeEditor()
+        code_editor.code = response.value
+        code_editor.theme = "light"
 
-for response in responses:
-    rep_dic = dict()
-    code_editor = CodeEditor()
-    code_editor.code = response.value
-    code_editor.theme = "light"
-
-    rep_dic["response"] = code_editor
-    rep_dic["student_id"] = response.student_id
-    evals_list = list()
-    for evaluation in response.evaluations:
-        eval_dic = dict()
-        eval_dic["student_id"] = evaluation.student_id
-        
-        crit_list = list()
-
-        for i in range(100):
-            radio = evaluation.get(str(i), None)
-            if not radio:
-                break
+        rep_dic["response"] = code_editor
+        rep_dic["student_id"] = response.student_id
+        evals_list = list()
+        for evaluation in response.evaluations:
+            eval_dic = dict()
+            eval_dic["student_id"] = evaluation.student_id
             
-            crit = dict()
-            radio["disabled"] = True
-            crit["radio"] = radio
-            crit["comment"] = evaluation.get("commentaire_"+str(i), None)
-            crit_list.append(crit)
-        
-        eval_dic["crit_list"] = crit_list or None
-        evals_list.append(eval_dic)
-    rep_dic["evaluations"] = evals_list
-    display_data.append(rep_dic)
+            crit_list = list()
+
+            for i in range(100):
+                radio = evaluation.get(str(i), None)
+                if not radio:
+                    break
+                
+                crit = dict()
+                radio["disabled"] = True
+                crit["radio"] = radio
+                crit["comment"] = evaluation.get("commentaire_"+str(i), None)
+                crit_list.append(crit)
+            
+            eval_dic["crit_list"] = crit_list or None
+            evals_list.append(eval_dic)
+        rep_dic["evaluations"] = evals_list
+        display_data.append(rep_dic)
 
 aa = list(map(str, range(20)))
 
