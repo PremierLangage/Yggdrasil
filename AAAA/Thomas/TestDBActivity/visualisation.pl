9435b0d0fd3a8e70a41a96cd956043402ae1102d
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-20 12:26:19 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/visualisation.pl

--- AAAA/Thomas/TestDBActivity/visualisation.pl
+++ AAAA/Thomas/TestDBActivity/visualisation.pl
@@ -8,13 +8,12 @@ Ce n'est pas un exercice à proprement parler juste une façon de visualiser les
 before==#|py|
 from database_utils import get_session, Base, Response
 
-
+"""
 with get_session(table_class= Response, base=Base) as session:
     if session.query(Response).filter(Response.student_id == user__id).first() : 
         form = ''
         text = 'Vous avez déjà soumis une réponse. Veuillez passer à la partie évaluation.'
-
-del session
+"""
 ==
 
 form==
