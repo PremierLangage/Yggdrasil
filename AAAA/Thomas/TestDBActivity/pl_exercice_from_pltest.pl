4eec87afb7c685582b4dab06ba749a15720384d9
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-20 10:35:57 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl

--- AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
+++ AAAA/Thomas/TestDBActivity/pl_exercice_from_pltest.pl
@@ -42,7 +42,7 @@ from database_utils import get_session, Base, Response
 with get_session(table_class= Response, base=Base) as session:
     if session.query(Response).filter(Response.student_id == user__id).first() : 
         form = ''
-        text = 'Vous avez déjà soumis une réponse. Veuillez passer à la partie évaluation'
+        text = 'Vous avez déjà soumis une réponse. Veuillez passer à la partie évaluation.'
 
 del session
 ==
