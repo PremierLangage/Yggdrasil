4544d21c09af4d1d6c2966807ceb8b0aa9796eb3
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-20 15:40:02 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/visualisation.pl

--- AAAA/Thomas/TestDBActivity/visualisation.pl
+++ AAAA/Thomas/TestDBActivity/visualisation.pl
@@ -8,12 +8,43 @@ Ce n'est pas un exercice à proprement parler juste une façon de visualiser les
 before==#|py|
 from database_utils import get_session, Base, Response
 
-"""
+responses = list()
+
 with get_session(table_class= Response, base=Base) as session:
-    if session.query(Response).filter(Response.student_id == user__id).first() : 
-        form = ''
-        text = 'Vous avez déjà soumis une réponse. Veuillez passer à la partie évaluation.'
-"""
+    if user__role == "teacher":
+        responses = session.query(Response).all()
+    else :
+        responses = session.query(Response).filter(Response.student_id == user__id).all()
+
+display_data = list()
+
+for response in responses:
+    rep_dic = dict()
+    rep_dic["response"] = response.value
+    rep_dic["student_id"] = response.student_id
+    evals_list = list()
+    for evaluation in evaluations:
+        eval_dic = dict()
+        eval_dic["student_id"] = evaluation.student_id
+        
+        crit_list = list()
+
+        for i in range(100):
+            radio = evaluation.get(str(i), None)
+            if not radio:
+                break
+            
+            crit = dict()
+            radio["disabled"] = True
+            crit["radio"] = radio
+            crit["comment"] = evaluation.get("commentaire_"+str(i), None)
+            crit_list.append(crit)
+        
+        eval_dic["crit_list"] = crit_list or None
+        evals_list.append(eval_dic)
+    rep_dic["evals"] = evals_list
+    display_data.append(rep_dic)
+
 ==
 
 grader==
@@ -22,6 +53,14 @@ grader==
 
 form==
 <p>Vous êtes un {{user__role}}</p>
+
+{% for student_response in display_data %}
+    {% for evaluation in student_response.evaluations %}
+
+
+{% endfor %} 
+
+
 
 
 ==
