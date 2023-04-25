087f44aedbb0452ac8f65dd2305f8c2c4c6015f4
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 11:48:28 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/anothertest/test.pl

--- AAAA/Thomas/anothertest/test.pl
+++ AAAA/Thomas/anothertest/test.pl
@@ -1,55 +1,13 @@
 
-
+@ test3.html
 @ /utils/sandboxio.py
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 group =: RadioGroup
 
-# GENERATE A RANDOM QUESTION
-before==#|py|
-
-import os
-os.environ["MPLCONFIGDIR"] = "/tmp/matplotlib-pkrkyoye"
-
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import mpld3
-from mpld3 import plugins
-np.random.seed(9615)
-
-# generate df
-N = 100
-df = pd.DataFrame((.1 * (np.random.random((N, 5)) - .5)).cumsum(0),
-                  columns=['a', 'b', 'c', 'd', 'e'],)
-
-# plot line + confidence interval
-fig, ax = plt.subplots()
-ax.grid(True, alpha=0.3)
-
-for key, val in df.items():
-    l, = ax.plot(val.index, val.values, label=key)
-    ax.fill_between(val.index,
-                    val.values * .5, val.values * 1.5,
-                    color=l.get_color(), alpha=.4)
-
-# define interactive legend
-
-handles, labels = ax.get_legend_handles_labels() # return lines and labels
-interactive_legend = plugins.InteractiveLegendPlugin(zip(handles,
-                                                         ax.collections),
-                                                     labels,
-                                                     alpha_unsel=0.5,
-                                                     alpha_over=1.5, 
-                                                     start_visible=True)
-
-
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_title('Interactive legend', size=20)
-
-prout = mpld3.fig_to_html(fig)
+with open("test3.html", "r") as f:
+    form = f.read()
 ==
 
 title = Radio Group Component
@@ -60,7 +18,6 @@ Select the result of **{{ A }}** x **{{ B }}**.
 
 # PRESENT THE QUESTION TO THE STUDENT
 form==
-{{prout}}
 ==
 
 # EVALUATE THE STUDENT ANSWER
