8b2b4f56bf6ebf64a6d344d003f100d45425d8b6
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 12:28:16 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/anothertest/test.pl

--- AAAA/Thomas/anothertest/test.pl
+++ AAAA/Thomas/anothertest/test.pl
@@ -10,51 +10,39 @@ import os
 import sys
 
 os.environ["MPLCONFIGDIR"] = "/tmp/matplotlib-ogs20b_w"
-print("AHHHHHHHHHHHHH1" ,  file=sys.stderr)
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import mpld3
 from mpld3 import plugins
+import jsonpickle
 np.random.seed(9615)
-print("AHHHHHHHHHHHHH2" ,  file=sys.stderr)
 
 # generate df
 N = 100
 df = pd.DataFrame((.1 * (np.random.random((N, 5)) - .5)).cumsum(0),
                   columns=['a', 'b', 'c', 'd', 'e'],)
-print("AHHHHHHHHHHHHH3" ,  file=sys.stderr)
 
 # plot line + confidence interval
 fig, ax = plt.subplots()
 ax.grid(True, alpha=0.3)
-print("AHHHHHHHHHHHHH4" ,  file=sys.stderr)
 
 for key, val in df.items():
     l, = ax.plot(val.index, val.values, label=key)
     ax.fill_between(val.index,
                     val.values * .5, val.values * 1.5,
                     color=l.get_color(), alpha=.4)
-print("AHHHHHHHHHHHHH5" ,  file=sys.stderr)
 
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_title('Interactive legend', size=20)
-print("AHHHHHHHHHHHHH" ,  file=sys.stderr)
 
 with open('test3.html', 'w') as f:
     print(mpld3.fig_to_html(fig), file=f)
 
-print("AHHHHHHHHHHHHH69" ,  file=sys.stderr)
-
-
-
 with open("test3.html", "r") as f:
     form = f.read()
-
-
-print("AHHHHHHHHHHHHH70" ,  file=sys.stderr)
 
 ==
 
