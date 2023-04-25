b60d0b84db7df54da1ea2809950e9b895018c749
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 13:53:24 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/anothertest/test.pl

--- AAAA/Thomas/anothertest/test.pl
+++ AAAA/Thomas/anothertest/test.pl
@@ -6,6 +6,48 @@ grader  =@ /grader/evaluator.py
 builder =@ before.py
 
 before ==#|py|
+import os
+import sys
+
+os.environ["MPLCONFIGDIR"] = "/tmp/matplotlib-ogs20b_w"
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mpld3
+from mpld3 import plugins
+import jsonpickle
+np.random.seed(9615)
+
+# generate df
+df = pd.DataFrame({
+    'length': [1.5, 0.5, 1.2, 0.9, 3],
+    'width': [0.7, 0.2, 0.15, 0.2, 1.1]
+    }, index=['pig', 'rabbit', 'duck', 'chicken', 'horse'])
+hist = df.hist(bins=3)
+# plot line + confidence interval
+fig, ax = plt.subplots()
+ax.grid(True, alpha=0.3)
+
+for key, val in df.items():
+    l, = ax.plot(val.index, val.values, label=key)
+    ax.fill_between(val.index,
+                    val.values * .5, val.values * 1.5,
+                    color=l.get_color(), alpha=.4)
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+
+d = {"form", mpld3.fig_to_html(fig)}
+with open('form.html', 'w') as f:
+    print(d, file=f)
+
+"""
+with open('form.html', 'w') as f:
+    print(jsonpickle.encode(d, unpicklable=False), file=f)
+"""
+del d
+
 
 with open("test.html", "r") as f:
     form = f.read()
