1c33e5e4f8aa1577fdc603a5bf60e9bfadb65c7d
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 12:02:32 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/anothertest/before.py

--- AAAA/Thomas/anothertest/before.py
+++ AAAA/Thomas/anothertest/before.py
@@ -42,9 +42,11 @@ if __name__ == "__main__":
         sys.exit(1)
     
     print("ANTONINNNNNNNNN. --------", file=sys.stderr)
-    sys.exit(69)
 
     with open(output_json, "w+") as f:
         f.write(jsonpickle.encode(dic, unpicklable=False))
+    
+    print("FFFFFFFFFFFFFFFFFFFFFFF", file=sys.stderr)
+    sys.exit(69)
     
     sys.exit(0)
