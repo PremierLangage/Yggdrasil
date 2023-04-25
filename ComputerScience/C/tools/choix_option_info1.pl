30b1f270e28d117380c03251b1bf6bdb30c2edc8
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-04-25 09:20:44 +0200

    nborie1 modified Yggdrasil/ComputerScience/C/tools/choix_option_info1.pl

--- ComputerScience/C/tools/choix_option_info1.pl
+++ ComputerScience/C/tools/choix_option_info1.pl
@@ -14,7 +14,53 @@
 #            https://creativecommons.org/licenses/by-sa/3.0/fr/
 #*****************************************************************************
 
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
 
+################### Modifier ici ###########################
+author=ESIEE info AD&D année 1
+
+title=Vos choix d'options personnels
+
+text ==
+Sans consultation de vos maitres d'apprentissage et seulement en interrogeant vos goûts personnels, veuillez 
+classer les trois options de votre premier voeux (l'option qui vous interesse le plus) à votre troisième voeux 
+(l'option qui vous interesse le moins).
+==
+
+question=Quel est votre parfum préféré ?
+
+items==
+Vanille
+Chocolat
+Fraise
+Pomme
+==
+
+############################################
+
+group =: RadioGroup
+
+before==#|python|
+
+group.items = []
+
+for i, item in enumerate(items.splitlines()):
+    group.items.append({ "id": i, "content": item })
+==
+
+form ==
+
+{{ question }}
+
+{{ group|component }}
+==
+
+# EVALUATE THE STUDENT ANSWER
+evaluator == #|py|
+score = 100
+feedback = '<span class="success-state">Réponse enregistrée</span>'
 
 
 
