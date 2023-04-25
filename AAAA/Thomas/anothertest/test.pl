921bfda28004ffe9bb73577f3aab99e5e09a9616
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 11:49:50 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/anothertest/test.pl

--- AAAA/Thomas/anothertest/test.pl
+++ AAAA/Thomas/anothertest/test.pl
@@ -1,5 +1,4 @@
 
-@ test3.html
 @ /utils/sandboxio.py
 grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
@@ -7,6 +6,36 @@ builder =@ /builder/before.py
 group =: RadioGroup
 
 before ==
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mpld3
+from mpld3 import plugins
+np.random.seed(9615)
+
+# generate df
+N = 100
+df = pd.DataFrame((.1 * (np.random.random((N, 5)) - .5)).cumsum(0),
+                  columns=['a', 'b', 'c', 'd', 'e'],)
+
+# plot line + confidence interval
+fig, ax = plt.subplots()
+ax.grid(True, alpha=0.3)
+
+for key, val in df.items():
+    l, = ax.plot(val.index, val.values, label=key)
+    ax.fill_between(val.index,
+                    val.values * .5, val.values * 1.5,
+                    color=l.get_color(), alpha=.4)
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Interactive legend', size=20)
+
+with open('test3.html', 'w') as f:
+    print(mpld3.fig_to_html(fig), file=f)
+
+
 with open("test3.html", "r") as f:
     form = f.read()
 
