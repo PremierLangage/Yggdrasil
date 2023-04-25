6bf12138f7a9e4fa2fecce2db422551b5afe98a3
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 11:22:08 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/TestDBActivity/eval.pl

--- AAAA/Thomas/TestDBActivity/eval.pl
+++ AAAA/Thomas/TestDBActivity/eval.pl
@@ -108,6 +108,46 @@ for value in criteria :
         tmp.items.append({"id": str(elem["id"]), "content": elem["description"]})
     globals()[str(value["id"])] = tmp
     radio.append(vars(tmp))
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import mpld3
+from mpld3 import plugins
+np.random.seed(9615)
+
+# generate df
+N = 100
+df = pd.DataFrame((.1 * (np.random.random((N, 5)) - .5)).cumsum(0),
+                  columns=['a', 'b', 'c', 'd', 'e'],)
+
+# plot line + confidence interval
+fig, ax = plt.subplots()
+ax.grid(True, alpha=0.3)
+
+for key, val in df.items():
+    l, = ax.plot(val.index, val.values, label=key)
+    ax.fill_between(val.index,
+                    val.values * .5, val.values * 1.5,
+                    color=l.get_color(), alpha=.4)
+
+# define interactive legend
+
+handles, labels = ax.get_legend_handles_labels() # return lines and labels
+interactive_legend = plugins.InteractiveLegendPlugin(zip(handles,
+                                                         ax.collections),
+                                                     labels,
+                                                     alpha_unsel=0.5,
+                                                     alpha_over=1.5, 
+                                                     start_visible=True)
+
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title('Interactive legend', size=20)
+
+form = mpld3.fig_to_html(fig)
+
 ==
 
 
