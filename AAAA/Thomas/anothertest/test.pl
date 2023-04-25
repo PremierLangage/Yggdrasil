749f2ecde121dd9991e441734827d096333e5556
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 12:32:18 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/anothertest/test.pl

--- AAAA/Thomas/anothertest/test.pl
+++ AAAA/Thomas/anothertest/test.pl
@@ -37,9 +37,12 @@ for key, val in df.items():
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_title('Interactive legend', size=20)
+
 d = {"form", mpld3.fig_to_html(fig)}
 with open('form.html', 'w') as f:
     print(jsonpickle.encode(d, unpicklable=False), file=f)
+
+del d
 
 ==
 
