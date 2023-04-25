74f7732ae7aae067cedc5ef6d906225f4f2308a5
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-25 14:43:45 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/anothertest/test.pl

--- AAAA/Thomas/anothertest/test.pl
+++ AAAA/Thomas/anothertest/test.pl
@@ -24,7 +24,7 @@ df = pd.DataFrame({
     'length': [1.5, 0.5, 1.2, 0.9, 3],
     'width': [0.7, 0.2, 0.15, 0.2, 1.1]
     }, index=['pig', 'rabbit', 'duck', 'chicken', 'horse'])
-hist = df.hist(bins=3)
+hist = df.hist()
 # plot line + confidence interval
 fig, ax = plt.subplots()
 ax.grid(True, alpha=0.3)
