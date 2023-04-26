3906715522715a2cdb447e20801c67f33218a9d6
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-26 12:29:48 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl

--- AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
@@ -59,7 +59,7 @@ if user__role == "teacher" :
     data = {v:{} for v in range(int(NUMBER_QUESTIONS))}
 
     for a in answers : 
-        form += str(a)
+        form += str(a) + "<br>"
     """
     for answer in answers:
         for k, v in json.loads(answer[0]):
