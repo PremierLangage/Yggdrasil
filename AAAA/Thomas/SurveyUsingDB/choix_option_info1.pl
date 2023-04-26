93c8880b24a0b6a9b239706abd5ce25cea3a7d52
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-26 12:44:34 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl

--- AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
@@ -67,7 +67,7 @@ if user__role == "teacher" :
         for k, v in json.loads(str(answer[0])).items():
             data[v][k] = data.get(k, 0) + 1
 
-    graph = draw_hist(data[0])
+    graph = draw_hist({'Géomatique': 4})
     graph = mpld3.fig_to_html(graph)
 
 
