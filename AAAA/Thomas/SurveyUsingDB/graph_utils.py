df93a893e26e6bc0eb530ed952a6a8c41977e454
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-26 14:38:03 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/graph_utils.py

--- AAAA/Thomas/SurveyUsingDB/graph_utils.py
+++ AAAA/Thomas/SurveyUsingDB/graph_utils.py
@@ -39,3 +39,38 @@ def draw_camembert(data):
 
     plt.axis('equal')
     return plt.gcf()
+
+
+def draw_graphs():
+    keys = list(data.keys())
+    values = list(data.values())
+
+    # Colors initialisation (20 differents colors)
+    cmap = plt.colormaps['tab20c']
+    colors = [cmap(i) for i in range(len(keys))]
+
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(15, 5), gridspec_kw={'width_ratios': [14, 20, 1], 'wspace': 0.3})
+
+    # Sous-graphe 1 : Camembert
+    axs[0].pie(values, labels=data, colors=colors, 
+            autopct='%1.1f%%', startangle=90)
+    axs[0].axis('equal')
+
+    # Sous-graphe 2 : Histogramme
+    # je redéfinis un set de clés pour intégrer un affichage 1 ligne sur 2
+    _keys = [('|\n' if i % 2 else '') + f"{list(data.keys())[i]}" for i in range(len(data.keys()))]
+
+    axs[1].bar(_keys, values, color=colors, tick_label=_keys)
+    axs[1].set_title('Histogramme')
+    for i, v in enumerate(values):
+        axs[1].text(i, v+.05,  str(v), color='blue', fontweight='bold', ha="center")
+    
+    # Sous-graphe 3 : Légende personnalisée
+    legend_handles = []
+    for i in range(len(keys)):
+        legend_handles.append(axs[2].bar(0, 0, color=colors[i % len(colors)]))
+    axs[2].legend(legend_handles, keys, loc='center')
+    axs[2].axis('off')
+
+    plt.show()
+    return fig
