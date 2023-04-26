64ee5975d3d16a5f9e09af7d6b3f0f455fa72d2b
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-26 12:45:40 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl

--- AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
@@ -90,8 +90,7 @@ for i in range(int(NUMBER_QUESTIONS)):
 form==
 {% if user__role == "teacher" %}
     {{ graph|safe }}
-    <br>
-    {{ data }}
+
 {% else %}
     {% if answer != None %}
         <span class="success-state">Vous avez déjà répondu à ce sondage</span>
