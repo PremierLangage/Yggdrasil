01107b0fa45a0041f3db9d573d339f5f413aa245
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-26 17:01:47 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/database_utils.py

--- AAAA/Thomas/SurveyUsingDB/database_utils.py
+++ AAAA/Thomas/SurveyUsingDB/database_utils.py
@@ -3,7 +3,7 @@ from typing import List
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
-from sqlalchemy import Text, Integer, ForeignKey
+from sqlalchemy import Text, Integer, ForeignKey, String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
 
@@ -11,8 +11,14 @@ Base = declarative_base()
 
 class Response(Base):
     base = Base
-    __tablename__ = "test_db_choix_option_1" 
+    __tablename__ = "test_db_choix_option_2" 
     id : Mapped[int] = mapped_column(primary_key=True)
+    
+    username : Mapped[str] = mapped_column(String)
+    firstname : Mapped[str] = mapped_column(String)
+    lastname : Mapped[str] = mapped_column(String)
+    email : Mapped[str] = mapped_column(String)
+
     student_id : Mapped[int] = mapped_column(Integer)
     value : Mapped[Text] = mapped_column(Text)    
     def __repr__(self):
