9604529d9cdbb1a7f5fd9364602afe1bdb47d6b9
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-26 14:32:50 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl

--- AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_option_info1.pl
@@ -89,8 +89,32 @@ for i in range(int(NUMBER_QUESTIONS)):
     radio.append(vars(tmp))
 ==
 
-form==
+form==#|html|
 {% if user__role == "teacher" %}
+    <script>
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+// Start file download.
+document.getElementById("dwn-btn").addEventListener("click", function(){
+    // Generate download of hello.txt file with some content
+    var text = document.getElementById("text-val").value;
+    var filename = "hello.txt";
+    
+    download(filename, text);
+}, false);
+    </script>
+
     {{ hist|safe }}
     {{ camembert|safe }}
     <br>
