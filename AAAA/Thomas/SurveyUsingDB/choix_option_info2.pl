b8bc05e94c96d82f626bc5d25c9d7c6dddad01e5
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-04-26 16:03:00 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl

--- AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl
@@ -74,7 +74,7 @@ if user__role == "teacher" :
     camembert = draw_camembert(data[0])
     camembert = mpld3.fig_to_html(camembert)
 
-    graphs = draw_graphs(data[0])
+    graphs = [draw_graphs(data[0]) for i in range(NUMBER_QUESTIONS)]
     
 
 
