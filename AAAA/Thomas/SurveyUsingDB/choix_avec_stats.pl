e69f1042fc1a738a67fb23fc521261a8ddbe088a
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-04-27 17:19:34 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl

--- AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
@@ -129,13 +129,15 @@ form==#|html|
         animation: load-animation 2s ease-in-out;
     }
     .answer {
-        display: flex;
+        /* display: flex;
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        background-color: light-grey;
+        background-color: light-grey; */
     }
     .graph {
+        /* border: 2px outset #d4d4d4;
+        transform-origin: center !important; */
     }
     .c_btn {
         cursor: pointer;
