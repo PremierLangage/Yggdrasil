e7fe8ad3706f7bfe9424ce4a25f8c0d05f63f7bd
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-04-27 09:03:31 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/graph_utils.py

--- AAAA/Thomas/SurveyUsingDB/graph_utils.py
+++ AAAA/Thomas/SurveyUsingDB/graph_utils.py
@@ -56,7 +56,7 @@ def draw_graphs(data):
     cmap = plt.colormaps['tab20c']
     colors = [cmap(i) for i in range(len(keys))]
 
-    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(WIDTH, HEIGHT), gridspec_kw={'width_ratios': [4, 5, 1], 'wspace': 0.5})
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(WIDTH, HEIGHT), gridspec_kw={'width_ratios': [8, 10, 1], 'wspace': 0.5})
 
     # Sous-graphe 1 : Camembert
     axs[0].pie(values, colors=colors, 
