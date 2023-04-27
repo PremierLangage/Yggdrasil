9bc925c9f5cbbdb0c377e9fe7be1743095a39274
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-04-27 14:52:01 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl

--- AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
@@ -12,9 +12,9 @@ title= dummy title
 
 # Ecrire un énoncé ici
 text ==
-Vous pouvez définir des questions au format suivant : `question_X`
+Vous devez définir des questions au format suivant : `question_X`
 
-Où X est un identifiant se devant d'être unique et n'ayant aucune incidence dans la suite du code.
+Où `X` est un identifiant se devant d'être unique et n'ayant aucune incidence dans la suite du code.
 
 ---
 
