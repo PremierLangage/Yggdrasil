cd78bf470d06c47d534afdb0ce1e656305ce7620
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-04-27 10:24:43 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl

--- AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl
@@ -117,7 +117,7 @@ form==#|html|
     }
     path.mpld3-path:hover {
         animation: smooth-zoom 2s ease-in-out infinite;
-        transform-origin: 25% 0% !important;
+        transform-origin: top !important;
         margin: 0 auto !important; /* Centrer horizontalement */
     }
     .answer {
