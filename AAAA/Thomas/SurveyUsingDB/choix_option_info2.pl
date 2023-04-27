8f49b8a57df0071df37edbd11130959b2e979a11
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-04-27 11:29:58 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl

--- AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_option_info2.pl
@@ -156,9 +156,11 @@ form==#|html|
     <br>
     <br>
 
+<div class="exercise__actions text-center">
     <div class="btn btn-primary" id="dwn-btn"> 
         <i class="fas fa-download"></i>
         <span class="ion-hide-md-down">Téléchargement des choix des élèves</span>
+    </div>
     </div>
     <script>
         function download(filename, text) {
