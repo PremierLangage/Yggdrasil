5e94528d71939d5d750c675109a1e8a1a492f4f9
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-04-28 10:52:14 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/graph_utils.py

--- AAAA/Thomas/SurveyUsingDB/graph_utils.py
+++ AAAA/Thomas/SurveyUsingDB/graph_utils.py
@@ -106,7 +106,6 @@ def generate_html(data):
 <html>
     <head><title>Graphs</title>    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/plotly.js/1.33.1/plotly.min.js" integrity="sha512-V0j9LhrK9IMNdFYZqh+IqU4cjo7wdxyHNyH+L0td4HryBuZ7Oq6QxP2/CWr6TituX31+gv5PnolvERuTbz8UNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdn.plot.ly/plotly-locale-fr-latest.min.js"></script>
     </head>
 <body>
     <div style="display:flex;flex-wrap:wrap;">
