a66fb1ac6af33d9e1504176d77f57507f4a7a8e5
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-04-28 11:47:52 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl

--- AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
@@ -149,8 +149,8 @@ form==#|html|
         <div class="answer">
             <div>{{  radio[i].question|safe }}</div>
             <!-- <div class="graph"> {{ graphs[i]|safe }} </div> -->
-            <iframe srcdoc={{graphs[i]}} width="100%" height="400" frameborder="0" scrolling="no"></iframe>
-            <pre><code class="language-html">{{graphs[i]}}</code></pre>
+            <iframe srcdoc='{{graphs[i]}}' width="100%" height="400" frameborder="0" scrolling="no"></iframe>
+            
         </div>
     {% endfor %}
     <br>
