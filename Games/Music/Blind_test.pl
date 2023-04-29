a8242ad1dc5aa366189be17d1d0788d378bf1062
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-04-29 10:33:50 +0200

    nborie1 modified Yggdrasil/Games/Music/Blind_test.pl

--- Games/Music/Blind_test.pl
+++ Games/Music/Blind_test.pl
@@ -19,7 +19,7 @@ grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 countdown % { "cid": "countdown", "selector": "c-countdown" }
-countdown.time % 30
+countdown.time % 33
 countdown.hidden % false
 
 autoSubmit== #|js|
@@ -61,7 +61,32 @@ form ==
 ==
 
 evaluator== #|python|
+# I did found this relatively efficient Livenshtein distance using Numpy
+import numpy as np
 
+def levenshtein(chaine1, chaine2):
+    taille_chaine1 = len(chaine1) + 1
+    taille_chaine2 = len(chaine2) + 1
+    levenshtein_matrix = np.zeros ((taille_chaine1, taille_chaine2))
+    for x in range(taille_chaine1):
+        levenshtein_matrix [x, 0] = x
+    for y in range(taille_chaine2):
+        levenshtein_matrix [0, y] = y
+    for x in range(1, taille_chaine1):
+        for y in range(1, taille_chaine2):
+            if chaine1[x-1] == chaine2[y-1]:
+                levenshtein_matrix [x,y] = min(
+                    levenshtein_matrix[x-1, y] + 1,
+                    levenshtein_matrix[x-1, y-1],
+                    levenshtein_matrix[x, y-1] + 1
+                )
+            else:
+                levenshtein_matrix [x,y] = min(
+                    levenshtein_matrix[x-1,y] + 1,
+                    levenshtein_matrix[x-1,y-1] + 1,
+                    levenshtein_matrix[x,y-1] + 1
+                )
+    return (levenshtein_matrix[taille_chaine1 - 1, taille_chaine2 - 1])
 
 
 
