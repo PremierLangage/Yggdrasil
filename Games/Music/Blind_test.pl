4907f7e78f84faf66747b4f3cc10d1570c328605
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-04-29 09:38:18 +0200

    nborie1 modified Yggdrasil/Games/Music/Blind_test.pl

--- Games/Music/Blind_test.pl
+++ Games/Music/Blind_test.pl
@@ -14,3 +14,59 @@
 #            https://creativecommons.org/licenses/by-sa/3.0/fr/
 #*****************************************************************************
 
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+countdown % { "cid": "countdown", "selector": "c-countdown" }
+countdown.time % 10
+countdown.hidden % false
+
+autoSubmit== #|js|
+const btn = document.querySelector('.action-submit');
+btn.click()
+==
+
+logOnConsole==#|js|
+// you may open the browser console to see the message
+console.log('hello world');
+==
+
+before== #|python|
+countdown.actions = [
+    { "time": 5, "action": logOnConsole },
+    { "time": 0, "action": autoSubmit }
+]
+counter = 0
+==
+
+title = Count Down Component
+text==#|html|
+<center>
+<div>
+<img src="{{ url_phot }}" style="width:30%" alt="les instruments de musiques de l'enfer"/><br>
+<audio controls>
+  <source src="{{ url_clip }}" type="audio/mp3">
+  Votre navigateur ne supporte pas la lecture du clip audio.
+</audio>
+</div>
+</center>
+==
+
+
+
+form ==
+ {{ countdown|component }}
+==
+
+evaluator== #|python|
+remaining = countdown.time
+# reset timer
+for e in countdown.actions:
+    e['consumed'] = False
+
+countdown.time = 10
+counter += 1
+grade = (100, f'<h3>remaining: {remaining}, counter: {counter}</h3>')
+
+==
