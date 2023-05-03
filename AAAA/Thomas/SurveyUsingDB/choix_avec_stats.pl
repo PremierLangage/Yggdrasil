f9fefdef2a4b6b9e4d1dbd56d6611147621efb93
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-03 18:19:34 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl

--- AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
+++ AAAA/Thomas/SurveyUsingDB/choix_avec_stats.pl
@@ -150,7 +150,7 @@ form==#|html|
 </style>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/plotly.js/1.33.1/plotly.min.js" integrity="sha512-V0j9LhrK9IMNdFYZqh+IqU4cjo7wdxyHNyH+L0td4HryBuZ7Oq6QxP2/CWr6TituX31+gv5PnolvERuTbz8UNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <div class="test">
-        {{ graphs[0] }}
+        {{ graphs[0]|safe }}
     </div>
 
     {% for i in range(NUMBER_QUESTIONS) %}
