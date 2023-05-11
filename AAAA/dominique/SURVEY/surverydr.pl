98a5ef23bd490ea53740d09b68a842f9b1293e24
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-05-11 17:01:17 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/SURVEY/surverydr.pl

--- AAAA/dominique/SURVEY/surverydr.pl
+++ AAAA/dominique/SURVEY/surverydr.pl
@@ -4,11 +4,13 @@
 
 extends= choixsurvey.pl
 
-question1=<b>Quel est votre premier choix d'option ?</b>
+NUMBER_QUESTIONS=1
 
-question2=<b>Quel est votre second choix d'option ?</b>
+question_1=<b>Quel est votre premier choix d'option ?</b>
 
-question3=<b>Quel est votre troisième choix d'option ?</b>
+question_2=<b>Quel est votre second choix d'option ?</b>
+
+question_3=<b>Quel est votre troisième choix d'option ?</b>
 
 items==
 Géomatique
