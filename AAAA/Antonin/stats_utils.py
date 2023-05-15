9330dca78a681266dfbff004a63468b8a2f0f22e
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-15 14:00:34 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats_utils.py

--- AAAA/Antonin/stats_utils.py
+++ AAAA/Antonin/stats_utils.py
@@ -15,7 +15,7 @@ class StatInput():
 class Stat():
     def __init__(self, data : List[StatInput]):
         """
-        :param data:    list(tuple(str, int)), list of couple key, amount of value
+        :param data:    list(StatInput), list of data
         """
         self.questions = [v for q, v in globals().items() if q.startswith("question_")]
         self.question_amount = len(questions)
