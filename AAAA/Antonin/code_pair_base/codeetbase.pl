23b8cd36f513e6418b86363ea3991f3a8a5eb53e
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-15 16:19:33 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/code_pair_base/codeetbase.pl

---
+++ AAAA/Antonin/code_pair_base/codeetbase.pl
@@ -0,0 +1,23 @@
+extends = /AAAA/Thomas/template/pltest.pl
+
+pltest==
+>>> square(2)
+4
+>>> square(10)
+100
+==
+
+text==
+
+Ecrire une fonction square
+
+==
+
+after==
+
+# code
+et l'enonce le titre + text 
+
+
+
+==
