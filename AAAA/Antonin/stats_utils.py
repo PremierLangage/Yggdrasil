e78abbf1045659c9bd166a02aacd1b4db77a9162
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-15 14:12:12 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats_utils.py

--- AAAA/Antonin/stats_utils.py
+++ AAAA/Antonin/stats_utils.py
@@ -1,4 +1,3 @@
-from database_utils import get_session, Base, Response
 from graph_utils import generer_histogramme
 from typing import List, Tuple
 
@@ -23,9 +22,6 @@ class Stat():
         :param data:    list(StatInput), list of data
         """
         self.data = data
-        self.has_answered = False
-        with get_session(table_class= Response, base=Base) as session:
-            self.has_answered = (session.query(Response).filter(Response.student_id == user__id).first()) != None
         self.graph = []
     
     def generate_graph(self):
