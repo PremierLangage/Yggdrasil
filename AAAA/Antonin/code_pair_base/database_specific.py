bf02cf19a086df4a6b9ec8088219ec0fe16777b2
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-15 16:33:20 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/code_pair_base/database_specific.py

--- AAAA/Antonin/code_pair_base/database_specific.py
+++ AAAA/Antonin/code_pair_base/database_specific.py
@@ -6,3 +6,7 @@ class CodeEditorResponse(dbu.Response):
     text : Mapped[Text] = mapped_column(Text)    
     code : Mapped[Text] = mapped_column(Text)    
 
+dbu.Response.__tablename__ = "codeEditor_response_1" 
+
+# TODO sauvegarde 
+# TODO get_session
