95587af3fa82ac27445fa907b08d72c5656fa14c
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-15 11:37:39 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats_utils.py

--- AAAA/Antonin/stats_utils.py
+++ AAAA/Antonin/stats_utils.py
@@ -3,6 +3,6 @@ from graph_utils import gen_graph_html_plotly
 
 QUESTIONS = [v for q, v in globals().items() if q.startswith("question_")]
 NUMBER_QUESTIONS = len(QUESTIONS)
-
+HAS_ANSWERED = False
 with get_session(table_class= Response, base=Base) as session:
     HAS_ANSWERED = (session.query(Response).filter(Response.student_id == user__id).first()) != None
