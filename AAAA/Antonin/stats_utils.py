992fc19530055e910d4cb1f577c586975597685a
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-15 13:52:16 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats_utils.py

--- AAAA/Antonin/stats_utils.py
+++ AAAA/Antonin/stats_utils.py
@@ -1,17 +1,16 @@
 from database_utils import get_session, Base, Response
 from graph_utils import gen_graph_html_plotly
 
+class StatInput():
+    def __init__(self, title: str, data : list):
+        
 
 class Stat():
-    def __init__(self, globals_dict : dict):
+    def __init__(self, question_data):
+        self.questions = [v for q, v in globals().items() if q.startswith("question_")]
+        self.question_amount = len(questions)
+        self.has_answered = False
+        with get_session(table_class= Response, base=Base) as session:
+            self.has_answered = (session.query(Response).filter(Response.student_id == user__id).first()) != None
 
-# globals
-# Here are defined globals values 
-QUESTIONS = [v for q, v in globals().items() if q.startswith("question_")]
-NUMBER_QUESTIONS = len(QUESTIONS)
-HAS_ANSWERED = False
-# ---
-
-with get_session(table_class= Response, base=Base) as session:
-    HAS_ANSWERED = (session.query(Response).filter(Response.student_id == user__id).first()) != None
 
