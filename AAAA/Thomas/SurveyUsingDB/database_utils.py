64a2573d27d6787850e37aa2b1d8088cdc2e9cbf
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-16 10:55:41 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/database_utils.py

--- AAAA/Thomas/SurveyUsingDB/database_utils.py
+++ AAAA/Thomas/SurveyUsingDB/database_utils.py
@@ -9,6 +9,13 @@ from sqlalchemy.orm import mapped_column, relationship
 
 Base = declarative_base()
 
+def get_session(table_class, base, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
+    engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
+    if not inspect(engine).has_table(table_class.__tablename__):
+        base.metadata.create_all(engine)
+    return sessionmaker(bind=engine)()
+
+
 class Response(Base):
     base = Base
     __tablename__ = "test_db_choix_option_68" 
@@ -30,8 +37,3 @@ class Response(Base):
     def __repr__(self):
           return f"{self.student_id} a obtenu un score de : {self.grade}."
 
-def get_session(table_class, base, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
-    engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
-    if not inspect(engine).has_table(table_class.__tablename__):
-        base.metadata.create_all(engine)
-    return sessionmaker(bind=engine)()
