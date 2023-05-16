6827ffeed05d440e8ae6e8a747c6525f2719e9d1
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-16 11:56:39 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl

--- AAAA/Antonin/activities/RadioSurvey.pl
+++ AAAA/Antonin/activities/RadioSurvey.pl
@@ -2,3 +2,21 @@ extends = /AAAA/Antonin/stats.pl
 
 # Ecrire un titre ici
 title= Test title
+
+before==#|python|
+QUESTIONS = [v for q, v in globals().items() if q.startswith("question_")]
+NUMBER_QUESTIONS = len(QUESTIONS)
+
+radio = []
+for i in range(len(QUESTIONS)):
+    tmp = RadioGroup(cid=str(i))
+    tmp.question = QUESTIONS[i]
+    tmp.items = []
+    for j, item in enumerate(items.splitlines()):        
+        tmp.items.append({ "id": j+1, "content": item })
+    globals()[str(i)] = tmp
+    radio.append(vars(tmp))
+
+if user__role == "teacher":
+    data = {v:{} for v in range(NUMBER_QUESTIONS)}
+    answers_csv = f"username,firsname,lastname,email,{','.join(QUESTIONS)}\\n"
