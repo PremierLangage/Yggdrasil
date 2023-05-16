25a47ec6fce4e79da901a1a52b1b4c850c207f16
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-16 16:51:36 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl

--- AAAA/Antonin/activities/RadioSurvey.pl
+++ AAAA/Antonin/activities/RadioSurvey.pl
@@ -63,7 +63,7 @@ if user__role == "teacher":
  
     for answer in answers:
         for k, v in json.loads(str(answer[0])).items():
-            data[v][k] = data[v].get(k, 0) + 1
+            data[v].append(v)
     globals()["data"] = data
 ==
 
