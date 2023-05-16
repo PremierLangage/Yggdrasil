11a1b0c0b90903a29568877da330ce6de1c181df
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-16 10:42:47 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/SurveyUsingDB/database_utils.py

--- AAAA/Thomas/SurveyUsingDB/database_utils.py
+++ AAAA/Thomas/SurveyUsingDB/database_utils.py
@@ -13,18 +13,22 @@ class Response(Base):
     base = Base
     __tablename__ = "test_db_choix_option_68" 
     id : Mapped[int] = mapped_column(primary_key=True)
-    
+    # User Informations
     username : Mapped[str] = mapped_column(String)
     firstname : Mapped[str] = mapped_column(String)
     lastname : Mapped[str] = mapped_column(String)
     email : Mapped[str] = mapped_column(String)
     student_id : Mapped[int] = mapped_column(Integer)
-    
+
+    # Exercise Informations:
     title : Mapped[str] = mapped_column(String)
-    statement : Mapped[Text] = mapped_column(Text)    
+    statement : Mapped[Text] = mapped_column(Text)   
+
+    # Grade value
+    grade : Mapped[int] = mapped_column(Integer)
 
     def __repr__(self):
-          return f"{self.student_id} à répondu : {self.value}."
+          return f"{self.student_id} a obtenu un score de : {self.grade}."
 
 def get_session(table_class, base, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
     engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
