f09b6757f2e3cb7c180ae1760274dece32181895
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-16 16:56:20 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl

--- AAAA/Antonin/activities/RadioSurvey.pl
+++ AAAA/Antonin/activities/RadioSurvey.pl
@@ -68,7 +68,6 @@ if user__role == "teacher":
 ==
 
 formstudent==#|html|
-{{ answers }} 
 {% if False and HAS_ANSWERED != None %}
         <span class="success-state">Vous avez déjà répondu à ce sondage</span>
     {% else %}
