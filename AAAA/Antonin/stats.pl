08c1fb68ecc0be30e739ad9929e42ca0f67bd09b
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-16 14:05:07 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -62,13 +62,16 @@ formstudent==#|html|
 ==
 
 before_stat==#|python|
-
 import os, sys, time, json
 from database_utils import get_session, Base, Response
 from stats_utils import Stat, StatInput
 
 with get_session(table_class= Response, base=Base) as session:
     HAS_ANSWERED = (session.query(Response).filter(Response.student_id == user__id).first()) != None
+==
+
+before==#|python|
+#Nothing, work is done in implementations
 ==
 
 before_graph==
