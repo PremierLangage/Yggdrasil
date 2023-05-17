c286d8d20b9a955ff983c9d8b8a3fdc51759b3f6
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-17 16:12:43 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -13,6 +13,8 @@ editor.code ==
 # write your code here
 ==
 
+__save_editor_code = editor.code
+
 before==#|python|
 from database_utils import get_session, Base, Response
 
@@ -22,9 +24,12 @@ if user__role == "teacher":
 
     with get_session(table_class=Response, base=Base) as session:
         answers = session.query(Response.grade).all()
-"""    data["grade"][1] = list(map(lambda x : x['_data'][0], answers))
+    
+    for answer in answers:
+        for k, v in json.loads(str(answer[0])).items()
+    data["grade"][1] = list(map(lambda x : x['_data'][0], answers))
     [data["grade"][0].append(x) for x in data["grade"][1] if x not in data["grade"][0]]
-    globals()["data"] = data"""
+    globals()["data"] = data
 ==
 
 formstudent==#|html|
@@ -33,11 +38,18 @@ formstudent==#|html|
 ==
 
 evaluator==#|py|
+from database_utils import get_session, Base, RadioResponse
+import json
+
+score = 100
+feedback = '<span class="success-state">Réponse enregistrée</span>'
+if __save_editor_code == editor.code:
 
 if "if" in editor.code:
-    grade = (100, f"student code:\n{editor.code}")
+    score = 100
 else:
-    grade=(0," non vous n'avez pas répondu a la question")
+    feedback = '<span class="error-state">Vous n\'avez rien saisis, annulation de l\'envois de la réponse</span>'
+    score = 0
 
-
+grade = (score, feedback)
 ==
