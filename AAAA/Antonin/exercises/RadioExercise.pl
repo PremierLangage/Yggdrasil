7bc122bf90a5ac47172494467417539aa949d2d2
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-17 13:54:10 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/exercises/RadioExercise.pl

--- AAAA/Antonin/exercises/RadioExercise.pl
+++ AAAA/Antonin/exercises/RadioExercise.pl
@@ -4,66 +4,6 @@ title = Exercice d'exemple | RadioGroup
 
 text==#|markdown|
 Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL [`RadioSurvey.pl`](filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl) dont voici la documentation:
-
-Ici vous pouvez définir une activité de RadioGroup incluant le module de Statistique:
-
-    - exercice radio : selection de choix répondant à une liste de question
-
-    - affichage de graphe (histogramme) représentant les données pour l'enseignant
-
-    - récupération des entrées au format `csv`
-
----
-
-**Manuel utilisateur:**
-
-Pour ce faire vous devez définir les variables globales suivantes:
-
-- Un ensemble de question au format suivant : **`question_X`**:
-
-    Où X est un identifiant se devant d'être unique et n'ayant aucune incidence dans la suite du code.
-
-
-    Exemple:
-
-    > ```question_1 = Que pensez-vous de ce tutoriel ?```
-
-    > ```question_plus_pertinente = Est-ce que vous aussi vous avez du mal à finir vos...```
-
-- Une liste de réponses possibles dans une variable **`items`**, une réponse équivaut à une ligne:
-
-    Exemple:
-
-    <code>
-    
-    items==
-
-    Bien
-
-    Passable
-
-    Mal (faute de goût objectivement)
-
-    42
-
-    La couleur bleu
-
-    ==
-
-    </code>
-
-**Options:**
-
--  **`unique_answers`** : `Boolean`   -   Défaut: `False`
-
-    > L'utilisateur peut-il choisir la même réponse pour des questions différentes
-
-    Exemple:
-
-    > ```unique_answers = True```
-    
----
-
 ==
 
 question_1 = Que pensez vous de cet exercice?
