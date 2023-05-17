a754f35ddf1466aa9dbb9710d6ab48abe956a2c2
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-17 11:20:21 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/exercises/RadioExercise.pl

--- AAAA/Antonin/exercises/RadioExercise.pl
+++ AAAA/Antonin/exercises/RadioExercise.pl
@@ -16,5 +16,5 @@ Très satisfaisant
 Satisfaisant
 Moyennement satisfaisant
 Mediocre
-Je ne pense pas qu'il y ai de bonnes ou de mauvaises situations, moi si je devais résumer ma vie avec vous aujourd'hui je dirais d'abord que c'est des rencontres de gens qui m'ont tendu la main peut-être à un moment où je ne pouvais pas ou j'étais chez moi .. Mais c'est assez curieux de se dire que le hasard, les rencontres forgent une destiné . Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste parfois on ne trouve pas l'interlocuteur en face je dirais le miroir qui vous aide à avancer. Alors ce n'est pas mon cas comme je le disais la parce que moi au contraire j'ai pu et je dis merci à la vie je lui dis merci je chante la vie, je danse la vie, je ne suis qu'amour. Mais quand beaucoup de gens me dise: "mais comment fais-tu pour avoir cette humanité? ", mais je leur répond tout simplement, je leur dis mais c'est ce goût de l'amour, c'est ce goût donc qui ma poussé aujourd'hui à entreprendre une construction mécanique et qui sait, demain peut-être à me mettre au service de la communauté.. Et à faire le don, le don de soi.
+La couleur bleu
 ==
