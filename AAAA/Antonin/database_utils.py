2b78670d5d054c337af7307f5653bbf53742e978
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-17 17:10:08 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/database_utils.py

--- AAAA/Antonin/database_utils.py
+++ AAAA/Antonin/database_utils.py
@@ -16,7 +16,7 @@ def get_session(table_class, base, db_url = "activities-db", db_name = "activity
 
 class Response(Base):
     base = Base 
-    __tablename__ = "test_db_choix_option_111"
+    __tablename__ = "test_db_choix_option_12"
 
     id : Mapped[int] = mapped_column(primary_key=True)
     # User Informations:
