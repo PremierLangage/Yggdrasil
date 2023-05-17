ee6694fb6fa585a3da0b09e7cceb15d4c8154405
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-17 14:18:35 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/exercises/RadioExerciseUnique.pl

--- AAAA/Antonin/exercises/RadioExerciseUnique.pl
+++ AAAA/Antonin/exercises/RadioExerciseUnique.pl
@@ -6,7 +6,7 @@ text==#|markdown|
 Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL [`RadioSurvey.pl` dont voici la documentation](filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl) avec le choix de réponse unique.
 ==
 
-# Ici on définit que chaque choix ne sera selectionnable qu'une seule fois
+# Ici on définit que chaque choix ne sera selectionnable qu'une seule fois (voir la doc)
 unique_choice = True
 
 question_1 = Que pensez vous de cet exercice?
