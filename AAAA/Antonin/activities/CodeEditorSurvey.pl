104b7d51ae3a22f5c45609f951c0cfa39452f7a5
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-17 15:39:14 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -5,3 +5,14 @@ title= Dummy Title RadioSurvey
 text==
 TODO # Enonce / Tutoriel d'utilisation d'un RadioSurvey
 ==
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = python
+editor.code ==
+# write your code here
+==
+
+before==#|python|
+
+==
