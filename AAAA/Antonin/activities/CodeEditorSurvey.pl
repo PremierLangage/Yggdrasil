71fa38da12e91c039ba1c4f2c21a99ccffb0ed2d
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-22 15:55:41 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -29,8 +29,9 @@ if user__role == "teacher":
     [data["grade"][0].append(x) for x in data["grade"][1] if x not in data["grade"][0]]
     data["grade"][0].sort()
 
-    print("in", file=sys.stderr)
     with get_session(table_class=CodeEditorResponse, base=Base) as session:
+        print("in", file=sys.stderr)
+
         answers = session.execute(text(f"SELECT count(*) FROM {CodeEditorResponse.__tablename__} GROUP BY student_id;")).all()
     print(answers, file=sys.stderr)
     
