b0d746cc3c9474f9a69436dc2e5317e6ddf4d36a
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-22 11:46:53 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/database_utils.py

--- AAAA/Antonin/database_utils.py
+++ AAAA/Antonin/database_utils.py
@@ -33,9 +33,6 @@ class Response(Base):
     # Grade value:
     grade : Mapped[int] = mapped_column(Integer)
 
-    response_type : Mapped[str] = mapped_column(String)
-    __mapper_args__ = {'polymorphic_on': response_type}
-
     def __repr__(self):
         return f"{self.student_id} a obtenu un score de : {self.grade}."
 
@@ -57,9 +54,6 @@ class CodeEditorResponse(Base):
 
     # Grade value:
     grade : Mapped[int] = mapped_column(Integer)
-
-    response_type : Mapped[str] = mapped_column(String)
-    __mapper_args__ = {'polymorphic_on': response_type}
 
     def __repr__(self):
         return f"{self.student_id} a obtenu un score de : {self.grade}."
