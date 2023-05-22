69ba750639ff2a1320dbbdad696b2c64b5d67190
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-05-22 13:25:41 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/exercices/sondage.pl

---
+++ AAAA/dominique/A_Presentations/exercices/sondage.pl
@@ -0,0 +1,13 @@
+
+
+
+champs==
+sont très hétérogènes en niveaux
+ont besoin d’entraînement
+ne sont pas engagés
+n'apprennent pas le cours
+s'ennuient
+se perdent
+apprennent mal 
+ne sont pas critiques
+==
