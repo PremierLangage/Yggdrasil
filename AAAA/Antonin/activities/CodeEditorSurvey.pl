adb205d9008aa4da75dcb9a58a5876a6a133a3b8
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-22 11:52:34 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -27,8 +27,7 @@ if user__role == "teacher":
         answers = session.query(CodeEditorResponse.score).all()
     
     for answer in answers:
-        for k, v in json.loads(str(answer[0])).items():
-            data["grade"][1].append(k)
+        data["grade"][1].append(ansswer)
     [data["grade"][0].append(x) for x in data["grade"][1] if x not in data["grade"][0]]
     globals()["data"] = data
 ==
