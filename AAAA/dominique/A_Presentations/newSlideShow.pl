0cadb8006863b37f514f30dd22c1140bcf402a6d
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-05-22 11:18:11 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/newSlideShow.pl

--- AAAA/dominique/A_Presentations/newSlideShow.pl
+++ AAAA/dominique/A_Presentations/newSlideShow.pl
@@ -110,7 +110,10 @@ if numero<len(slides):
     title= ''
     grade=(0,"")
 else:
-    grade=(100,"<h1> Vos questions </h1>")
+    grade=(100,"""<h1> Vos questions </h1>
+    <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Ffr%2Fphotos%2Fpoint-d-interrogation&psig=AOvVaw02tlFlir--T9epxB6EMnfc&ust=1684833439754000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMDF6ejLiP8CFQAAAAAdAAAAABAI" >
+    
+    """)
 ==
 
 
