b030ba091e0e0d7147cf035d82b2fe334bd0f441
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-05-22 17:20:00 +0200

    tsaillard modified Yggdrasil/AAAA/Antonin/database_name.py

--- AAAA/Antonin/database_name.py
+++ AAAA/Antonin/database_name.py
@@ -1,8 +1,9 @@
-
 database_name = "this_is_a_placeholder_for_the_futur_name"
 
 def set_database_name(name):
+    global database_name
     database_name = name
+
 
 def get_database_name():
     return database_name
