c511f4121288fa898bc7ee66b3e2b94876885536
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-05-22 11:32:08 +0200

    tsaillard modified Yggdrasil/AAAA/Antonin/database_utils.py

--- AAAA/Antonin/database_utils.py
+++ AAAA/Antonin/database_utils.py
@@ -41,12 +41,12 @@ class Response(Base):
 
 class CodeEditorResponse(Response):
     __mapper_args__ = {'polymorphic_identity' : 'codeEditor'}
-    id : Mapped[int] = mapped_column(ForeignKey(Response.__tablename__))
+    id : Mapped[int] = mapped_column(ForeignKey(Response.__tablename__), primary_key=True)
     score : Mapped[int] = mapped_column(Integer)
 
 
 
 class RadioResponse(Response):
     __mapper_args__ = {'polymorphic_identity' : 'radio'}
-    id : Mapped[int] = mapped_column(ForeignKey(Response.__tablename__))
+    id : Mapped[int] = mapped_column(ForeignKey(Response.__tablename__), primary_key=True)
     value : Mapped[Text] = mapped_column(Text)
