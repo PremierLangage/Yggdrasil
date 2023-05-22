969f331538aae2a46ed074dc4c60c4b68100defb
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-22 15:07:18 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -39,18 +39,25 @@ formstudent==#|html|
 {{ editor|component }}
 ==
 
-evaluator==#|py|
+evaluator_before==#|py|
 from database_utils import get_session, Base, CodeEditorResponse
-score = 100
 feedback = '<span class="success-state">Réponse enregistrée</span>'
+==
+
+evaluator==#|py|
+
+score = 100
 if editor.codes[0]['defaultCode'] == editor.code:
     feedback = '<span class="error-state">Vous n\'avez rien saisis, annulation de l\'envois de la réponse</span>'
-    score = 0
+    score = -1
 else:
     if "if" in editor.code:
         score = 100
     else:
         score = 0
+==
+evaluator_after==#|py|
+if score >= 0:
     with get_session(table_class = CodeEditorResponse, base=Base) as session:
         session.add(
             CodeEditorResponse(
