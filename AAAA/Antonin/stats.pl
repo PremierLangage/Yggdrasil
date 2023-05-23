1dab75ee67242ee3227ad728a18bb1dcf8839f03
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 10:11:04 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -72,6 +72,9 @@ options==#|markdown|
 ==
 
 options+=#|markdown|
+
+**[Options stats.pl](https://pl-preprod.u-pem.fr/filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/stats.pl)**
+
 -  **`include_stats_score`** : `Boolean`   -   Défaut: `False`
 
     > Afficher le graphe représentant le score des utilisateurs
