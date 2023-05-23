653b95b1924b999694f3a9fea2adde339a6fa305
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-05-23 17:21:06 +0200

    tsaillard deleted /srv/local/pl/premierlangage/home/Yggdrasil/AAAA/Antonin/database_name.py

--- AAAA/Antonin/database_name.py
+++
@@ -1,9 +0,0 @@
-database_name = "this_is_a_placeholder_for_the_futur_name"
-
-def set_database_name(name):
-    global database_name
-    database_name = name
-
-
-def get_database_name():
-    return database_name
