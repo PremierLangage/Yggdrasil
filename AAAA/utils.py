0a8302bd9c199225417e65b07500dea5af4db047
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 11:21:52 +0200

    ajean1 modified Yggdrasil/AAAA/utils.py

--- AAAA/utils.py
+++ AAAA/utils.py
@@ -14,5 +14,5 @@ def log(message : str):
     print(f"Log:\n{message}\n", file=sys.stderr)
 
 def log_print():
-    print(f"Exited program to display logging" )
+    print(f"Exited program to display logging", file=sys.stderr)
     sys.exit(42)
