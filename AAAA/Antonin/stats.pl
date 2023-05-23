50e145e90cf22c31cea8916638f8d9ded4cd0471
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 10:15:30 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -30,24 +30,23 @@ Pour ce faire vous devez définir les variables globales suivantes:
 
 - **`data`** : dictionnaire contenant les données au format : 
 
-    > **key   :** str : nom graphe
+    - **key   :** str : nom graphe
 
-    > **value :** list : Couple contenant les labels (valeurs possibles) et les values (valeurs)
+    - **value :** list : Couple contenant les labels (valeurs possibles) et les values (valeurs)
         
-        > **labels :** list : valeurs possibles (seront représentées sur l'histogramme)
+        - **labels :** list : valeurs possibles (seront représentées sur l'histogramme)
 
-        > **values :** list : valeurs à afficher dans l'histogramme
+        - **values :** list : valeurs à afficher dans l'histogramme
 
     Exemple: 
     
-    > ```data % {"test1": [[0,1,2,3], [1,2,2,3,3,3]], "test": [["rien", "coucou", "test", "a"], ["coucou", "test", "a", "a"]]}```
+    - ```data % {"test1": [[0,1,2,3], [1,2,2,3,3,3]], "test": [["rien", "coucou", "test", "a"], ["coucou", "test", "a", "a"]]}```
 
 - **`answer_csv`** : string représentant le format d'une ligne du csv
 
-
     Exemple: 
     
-    > ```answer_csv="username,firsname,lastname,email,title,statement,grade\\n"```
+    - ```answer_csv="username,firsname,lastname,email,title,statement,grade\\n"```
 
 - **`formstudent`** : block html au format string représentant la partie jouable par un utilisateur lambda
 
@@ -77,19 +76,19 @@ options+=#|markdown|
 
 -  **`include_stats_score`** : `Boolean`   -   Défaut: `False`
 
-    > Afficher le graphe représentant le score des utilisateurs
+    - Afficher le graphe représentant le score des utilisateurs
 
     Exemple:
 
-    > ```include_stats_score = True```
+    - ```include_stats_score = True```
 
 -  **`include_stats_participation`** : `Boolean`   -   Défaut: `False`
 
-    > Afficher le graphe représentant le taux de participation des utilisateurs
+    - Afficher le graphe représentant le taux de participation des utilisateurs
 
     Exemple:
 
-    > ```include_stats_score = True```
+    - ```include_stats_score = True```
 ==
 
 # Flags:
