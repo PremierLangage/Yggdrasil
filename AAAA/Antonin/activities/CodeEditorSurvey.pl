6275554e2358fef359574b08e41511e3b922c4b3
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 09:51:27 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -53,6 +53,7 @@ Pour ce faire vous devez définir les variables globales suivantes:
 **Options:**
 
 > Héritée de **[stats.pl](https://pl-preprod.u-pem.fr/filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/stats.pl)**
+
 -  **`include_stats_score`** : `Boolean`   -   Défaut: `False`
 
     > Afficher le graphe représentant le score des utilisateurs
