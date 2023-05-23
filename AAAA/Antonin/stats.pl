ac96703dadc480f2a5ec56d334ddc40961db4441
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 12:33:05 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -150,7 +150,7 @@ from database_utils import get_session, Base, Response, CodeEditorResponse
 from stats_utils import Stat, StatInput
 from utils import *
 log(dataType["type"])
-log(eval(dataType["type"]).__str__())
+log(eval(dataType["type"]))
 log_print()
 with get_session(table_class= Response, base=Base) as session:
     HAS_ANSWERED = (session.query(Response).filter(Response.student_id == user__id).first()) != None
