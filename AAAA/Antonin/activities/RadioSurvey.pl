d51598a21434103a6aff7b9c2813fd65a9b57652
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 10:52:52 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl

--- AAAA/Antonin/activities/RadioSurvey.pl
+++ AAAA/Antonin/activities/RadioSurvey.pl
@@ -166,7 +166,7 @@ from database_utils import get_session, Base, RadioResponse
 import json, sys
 
 answer = {}
-print(answer, file=ssys.stderr)
+print(answer, file=sys.stderr)
 for i, response in enumerate(radio):
     answer[ response[str(i)]['items'][ int(response[str(i)]['selection'])-1 ]['content'] ] = i
 
