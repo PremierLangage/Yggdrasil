b73a8340fc6e708d9dd7af62c2a2f7fa07741557
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 10:30:33 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -61,7 +61,7 @@ Pour ce faire vous devez définir les variables globales suivantes:
 
 options==#|python|
 
-**[Options stats.pl](https://pl-preprod.u-pem.fr/filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/stats.pl)**
+[Options stats.pl](https://pl-preprod.u-pem.fr/filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/stats.pl)
 
 -  **`include_stats_score`** : `Boolean`   -   Défaut: `False`
 
