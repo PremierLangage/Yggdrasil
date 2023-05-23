11dfb807e3e8e6e02afececc7342085120081d73
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 11:57:08 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -89,25 +89,25 @@ api==#|markdown|
 
     - **`answer`** : `Dictionnaire`     -   Défaut:     `{}`
 
-    - Contient les réponses de l'utilisateur, au format: `clé : valeur`, où:
+        - Contient les réponses de l'utilisateur, au format: `clé : valeur`, où:
         
-        - **clé** est l'indice de la question *`(0 : nombre de question - 1)`*
+            - **clé** est l'indice de la question *`(0 : nombre de question - 1)`*
 
-        - **valeur** est le nom de la réponse selectionnée par l'utilisateur
+            - **valeur** est le nom de la réponse selectionnée par l'utilisateur
 
     - **`editor.code`** : `str`     -   Défaut:     `# write your code here`
 
-    - Contient le code rentré par l'étudiant
+        - Contient le code rentré par l'étudiant
 
 - Sortie:
 
     - **`score`** : `int`     -   Défaut:     `100`
 
-    - Définis le score de l'étudiant, si le score est inférieur à 0, on considère que c'est une erreur et la réponse ne sera pas enregistrée.
+        - Définis le score de l'étudiant, si le score est inférieur à 0, on considère que c'est une erreur et la réponse ne sera pas enregistrée.
 
     - **`feedback`** : `str`     -   Défaut:     <span class="success-state">Réponse enregistrée</span>
 
-    - Définis le message affichée à l'étudiant après évaluation de son score.
+        - Définis le message affichée à l'étudiant après évaluation de son score.
 
 ==
 
