48770bf70645347b072a1e473c96636fe423360f
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 09:52:30 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -52,23 +52,23 @@ Pour ce faire vous devez définir les variables globales suivantes:
 
 **Options:**
 
-> Héritée de **[stats.pl](https://pl-preprod.u-pem.fr/filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/stats.pl)**
+- Héritées de **[stats.pl](https://pl-preprod.u-pem.fr/filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/stats.pl)**
 
--  **`include_stats_score`** : `Boolean`   -   Défaut: `False`
+    -  **`include_stats_score`** : `Boolean`   -   Défaut: `False`
 
-    > Afficher le graphe représentant le score des utilisateurs
+        > Afficher le graphe représentant le score des utilisateurs
 
-    Exemple:
+        Exemple:
 
-    > ```include_stats_score = True```
+        > ```include_stats_score = True```
 
--  **`include_stats_participation`** : `Boolean`   -   Défaut: `False`
+    -  **`include_stats_participation`** : `Boolean`   -   Défaut: `False`
 
-    > Afficher le graphe représentant le taux de participation des utilisateurs
+        > Afficher le graphe représentant le taux de participation des utilisateurs
 
-    Exemple:
+        Exemple:
 
-    > ```include_stats_score = True```
+        > ```include_stats_score = True```
 
 ---
 ==
