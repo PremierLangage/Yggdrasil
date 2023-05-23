1901584517c680d9d24443c8ebc9dc392db42774
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-23 15:31:33 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -149,8 +149,8 @@ with open("database_utils.py", "r+") as f:
 from database_utils import *
 from stats_utils import Stat, StatInput
 from utils import *
-log(get_response(dataType).__tablename__)
-log(eval(dataType).__tablename__)
+
+log(eval(dataType))
 
 log_print()
 with get_session(table_class= Response, base=Base) as session:
