b337fc8538fa33144ddaa6890536b237aa6699e1
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-24 10:46:54 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -24,68 +24,8 @@ Ici vous pouvez définir une activité intégrant un système de statistiques:
 
 ---
 
-**Manuel utilisateur:**
-
-Pour ce faire vous devez définir les variables globales suivantes:
-
-- **`data`** : dictionnaire contenant les données au format : 
-
-    - **key   :** str : nom graphe
-
-    - **value :** list : Couple contenant les labels (valeurs possibles) et les values (valeurs)
-        
-        - **labels :** list : valeurs possibles (seront représentées sur l'histogramme)
-
-        - **values :** list : valeurs à afficher dans l'histogramme
-
-    Exemple: 
-    
-    - ```data % {"test1": [[0,1,2,3], [1,2,2,3,3,3]], "test": [["rien", "coucou", "test", "a"], ["coucou", "test", "a", "a"]]}```
-
-- **`answer_csv`** : string représentant le format d'une ligne du csv
-
-    Exemple: 
-    
-    - ```answer_csv="username,firsname,lastname,email,title,statement,grade\\n"```
-
-- **`formstudent`** : block html au format string représentant la partie jouable par un utilisateur lambda
-
-    Exemple: 
-    
-    `formstudent==#|html|`
-
-    `<h2> Hello World ! </h2>`
-    
-    `==`
-
-
-<u>**Options:**</u>
-
-{{options}}
-
----
-==
-
-options==#|markdown|
-
-[Options stats.pl](https://pl-preprod.u-pem.fr/filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/stats.pl)
-
--  **`include_stats_score`** : `Boolean`   -   Défaut: `False`
-
-    - Afficher le graphe représentant le score des utilisateurs
-
-    Exemple:
-
-    - ```include_stats_score = True```
-
--  **`include_stats_participation`** : `Boolean`   -   Défaut: `False`
-
-    - Afficher le graphe représentant le taux de participation des utilisateurs
-
-    Exemple:
-
-    - ```include_stats_participation = True```
-==
+user_manual @= docs/user_manual.md
+options @= docs/options.md
 
 # Flags:
 # Statistiques de score:
