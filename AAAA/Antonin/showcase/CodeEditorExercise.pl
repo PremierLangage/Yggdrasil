6351e7285bee64caf5d2319addae492c01a43554
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-24 15:56:47 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/showcase/CodeEditorExercise.pl

---
+++ AAAA/Antonin/showcase/CodeEditorExercise.pl
@@ -0,0 +1,31 @@
+extends = /AAAA/Antonin/activities/CodeEditorSurvey.pl
+
+title= Dummy Title CodeEditorResponse
+
+text==
+Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL [`CodeEditorResponse.pl` dont voici la documentation](filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl).
+
+Il contient l'affichage des deux graphes par défaut optionnels:
+
+- statistiques des scores 
+
+- statistiques de participation
+==
+
+editor.theme = dark
+editor.language = python
+editor.code ==
+# write your code here
+==
+
+evaluator==#|py|
+if "if" in editor.code:
+    score = 100
+elif "else" in editor.code:
+    score = 42
+else:
+    score = 0
+==
+
+include_stats_score = True
+include_stats_participation = True
