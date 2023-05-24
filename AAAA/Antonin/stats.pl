2a64bf376f9cfba372b8ac7c8d6cf56447dbcc9c
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-24 16:15:33 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -105,7 +105,7 @@ formstudent==#|html|
 before_stat==#|python|
 with open("database_utils.py", "r+") as f:
     f.seek(0, 0)
-    f.write(f"activity_id={activity__id}")
+    f.write(f"activity_id={42}")
 
 from database_utils import get_session, get_session, Base, Response
 from stats_utils import Stat, StatInput
