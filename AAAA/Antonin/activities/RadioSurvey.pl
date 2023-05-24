e005f9f2818ece9e381e5b247ae3caf4e93de8a7
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-24 15:01:41 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl

--- AAAA/Antonin/activities/RadioSurvey.pl
+++ AAAA/Antonin/activities/RadioSurvey.pl
@@ -68,8 +68,8 @@ if user__role == "teacher" and number_questions != 0:
         answers = session.query(RadioResponse).all()
     
     for answer in answers:
-        line_csv = [user__id,user__firstname,user__lastname,user__email]
-        for i, q in json.loads(str(answer[0])).items():
+        line_csv = [answer.username,answer.firstname,username.lastname,username.email]
+        for i, q in json.loads(str(answer.value)).items():
             data[questions[int(i)]][1].append(q)
             line_csv.append(q)
         answers_csv += ','.join(line_csv)
