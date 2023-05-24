bb4184768104bde42ae33ac305d93f10ecfc05ec
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-24 14:54:09 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CodeEditorSurvey.pl

--- AAAA/Antonin/activities/CodeEditorSurvey.pl
+++ AAAA/Antonin/activities/CodeEditorSurvey.pl
@@ -54,7 +54,7 @@ with get_session(table_class=CodeEditorResponse, base=Base) as session:
 
     answers = session.query(CodeEditorResponse).all()
     for answer in answers:
-        answers_csv += str(answer)
+        answers_csv += f"{answer.username},{answer.firstname},{answer.lastname},{answer.email},{answer.grade}"
 ==
 
 formstudent==#|html|
