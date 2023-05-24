c5cffbefc15e2774bf29b768f5fe18d185866ba9
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-24 11:36:34 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/docs/stats/imports.pl

--- AAAA/Antonin/docs/stats/imports.pl
+++ AAAA/Antonin/docs/stats/imports.pl
@@ -2,3 +2,29 @@ evaluation =@ evaluation.md
 options =@ options.md
 summary =@ summary.md
 user_manual =@ user_manual.md
+
+documentation==#|markdown|
+# Documentation:
+
+{{summary}}
+
+---
+
+## Manuel Utilisateur:
+
+{{user_manual}}
+
+---
+
+## Options:
+
+{{options}}
+
+---
+
+## Evaluation
+
+{{evaluation}}
+
+---
+==
