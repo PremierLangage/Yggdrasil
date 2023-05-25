7166757fc534b43228c0ec099bc81c3d1cb1521a
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-25 09:26:32 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl

---
+++ AAAA/Antonin/activities/CheckboxSurvey.pl
@@ -0,0 +1,36 @@
+extends = /AAAA/Antonin/stats.pl
+# ===============================================
+# Documentation Loading
+evaluation =@ ../docs/checkbox/evaluation.md
+options =@ ../docs/checkbox/options.md
+summary =@ ../docs/checkbox/summary.md
+user_manual =@ ../docs/checkbox/user_manual.md
+# ===============================================
+
+title= Dummy Title CheckboxSurvey
+
+text ==#|markdown|
+# Documentation:
+
+{{summary}}
+
+---
+
+## Manuel Utilisateur:
+
+{{user_manual}}
+
+---
+
+## Options:
+
+{{options}}
+
+---
+
+## Evaluation
+
+{{evaluation}}
+
+---
+==
