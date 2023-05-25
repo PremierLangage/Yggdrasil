7d7f65407830ae3433ec435023cbd06707054059
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-25 11:59:57 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl

--- AAAA/Antonin/activities/CheckboxSurvey.pl
+++ AAAA/Antonin/activities/CheckboxSurvey.pl
@@ -107,6 +107,16 @@ formstudent==#|html|
 
 # Evaluation de la réponse de l'étudiant
 evaluator_before == #|py|
+from database_utils import get_session, Base, CheckboxResponse
+import json
+
+answer = {}
+for i in range(len(radio)):
+    answer[ i ] = response[str(i)]['items'][ int(response[str(i)]['selection'])-1 ]['content']
+
+# Default Grade intilisation
+score = 100
+feedback = '<span class="success-state">Réponse enregistrée</span>'
 ==
 
 #default evaluator
