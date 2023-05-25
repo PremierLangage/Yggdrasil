aa407c4d5325df31a3425773a0cfdbf873022726
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-25 11:35:35 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl

--- AAAA/Antonin/activities/CheckboxSurvey.pl
+++ AAAA/Antonin/activities/CheckboxSurvey.pl
@@ -57,8 +57,6 @@ import json
 
 inputValues = {k[len("question_"):] : {"question": v, "items": []} for k, v in globals().items() if k.startswith("question_")} 
 for k, v in globals().items():
-    log(inputValues)
-    log_print()
     if k.startswith("items_"):
         k = k[len("items_"):]
         if k in inputValues:
