976258e5412da5aec896fe7e8fd37d2c08a8c2e2
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-25 10:30:39 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl

--- AAAA/Antonin/activities/CheckboxSurvey.pl
+++ AAAA/Antonin/activities/CheckboxSurvey.pl
@@ -54,9 +54,10 @@ number_questions = len(questions)
 data = {}
 answers_csv = f"username,firsname,lastname,email,{','.join([','.join([inputValues[key]['question']] + inputValues[key]['items'])])}\\n"
 
-checkBoxs
+checkBoxs = []
 for key in inputValues:
     tmp = CheckboxGroup(cid=str(i))
     tmp.items() = [{"id": f"item_{key}_{i}",  "content": i} for i in inputValues[key]["items"]]
+    checkBoxs.append(tmp)
 ==
 
