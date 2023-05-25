04fe4792ef2025f98876d46a05578f8c5b9dc76f
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-25 16:11:16 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/showcase/CheckboxExercise.pl

--- AAAA/Antonin/showcase/CheckboxExercise.pl
+++ AAAA/Antonin/showcase/CheckboxExercise.pl
@@ -1,1 +1,14 @@
 extends = /AAAA/Antonin/activities/CheckboxSurvey.pl
+
+title = Exercice d'exemple | Checkbox
+
+text==#|markdown|
+Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL [`Checkbox.pl` dont voici la documentation](filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl).
+
+Il contient l'affichage des réponses par question ainsi que l'affichage des deux graphes par défaut optionnels:
+
+- statistiques des scores 
+
+- statistiques de participation
+
+==
