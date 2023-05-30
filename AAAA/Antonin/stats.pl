a6e97eda636c01d9870ea47b578eb2d8e52e3805
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-30 15:29:15 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/stats.pl

--- AAAA/Antonin/stats.pl
+++ AAAA/Antonin/stats.pl
@@ -54,7 +54,7 @@ include_stats_participation = False
 # Values required for the program to work
 # ===============================================
 # Can (should) be redefined in implementations (see doc)
-data % {"test1": [[0,1,2,3], [1,2,2,3,3,3]], "test":  [["rien", "coucou", "test", "a"], ["coucou", "test", "a", "a"], '1']}
+data % {"test1": [[0,1,2,3], [1,2,2,3,3,3]], "test":  [["rien", "coucou", "test", "a"], ["coucou", "test", "a", "a"], "1"]}
 
 answers_csv="username,firsname,lastname,email,title,statement,grade\\n"
 formstudent==#|html|
