ce18da471af455595af5aae82a3c771e80e3ba0f
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-30 15:37:33 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/utils/graph_utils.py

--- AAAA/Antonin/utils/graph_utils.py
+++ AAAA/Antonin/utils/graph_utils.py
@@ -71,7 +71,7 @@ def generer_histogramme(
         'datasets': [{
             'label': 'Fréquence des valeurs',
             'data': frequences,
-            'backgroundColor': generate_color_palette(len(valeurs), 1.0, seed),
+            'backgroundColor': generate_color_palette(len(valeurs), 0.8, seed),
             'borderColor':  generate_color_palette(len(valeurs), 1.0, seed),
             'borderWidth': borderWidth
         }],
