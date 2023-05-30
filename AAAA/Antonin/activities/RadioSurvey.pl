1b83c3e20af8fd033c66cf0da6cbeb25f39ac372
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-05-30 16:31:34 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl

--- AAAA/Antonin/activities/RadioSurvey.pl
+++ AAAA/Antonin/activities/RadioSurvey.pl
@@ -133,7 +133,7 @@ def get_question(_id: str):
     v = inputValues.get(_id)
     if not v: return None
     return Question(v["question"], v["items"], answer[_id])
-
+log_print()
 ==
 
 #default evaluator
