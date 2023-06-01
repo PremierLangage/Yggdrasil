bb598efddc1edc47da6baec7020d6656b4f61cde
Orhan UYAR <orhan.uyar@univ-eiffel.fr>
2023-06-01 15:10:07 +0200

    ouyar1 modified Yggdrasil/AAAA/Orhan/diapo/demo_diapo_exo1.pl

--- AAAA/Orhan/diapo/demo_diapo_exo1.pl
+++ AAAA/Orhan/diapo/demo_diapo_exo1.pl
@@ -1,16 +1,15 @@
-title= Addition Aléatoire
 
-author = Orhan UYAR
+title= Addition statique
+
+author = Orhan Uyar
 
 text==
-Exercice 1
-Combien <i>font</i> ***{{ op1 }} + {{ op2 }}*** ?
-try count = {{ c }}
+Combien <i>font</i> ***3 + 4*** ?
 ==
 
 form==
 <div class="input-group">
-    <input id="form_answer" type="number" class="form-control" value="{{ answers__.answer }}" required/>
+    <input name="answer" id="form_answer" type="number" class="form-control" value="{{ answers__.answer }}" required/>
 </div>
 ==
 
@@ -23,8 +22,7 @@ import traceback
 import sys
 
 try: 
-    c += 1
-    if int(response['answer']) == op1 + op2:
+    if int(response['answer']) == 3 + 4:
         grade = (100, "Bonne réponse")
     else:
         grade = (0, "Mauvaise réponse")
@@ -33,15 +31,69 @@ except:
     grade = (-1, "Merci de rentrer un entier")
 ==
 
+@ /utils/sandboxio.py
+@ /grader/evaluator.py [grader.py]
 
-before==
-import random
-random.seed(seed)
-op1 = random.randint(1, 10)
-op2 = random.randint(1, 10)
-c = 0
+tests %=
+{
+    "correct" : {
+        "response" : {
+            "answer" : 7
+        },
+        "grade" : 100,
+        "feedback" : "Bonne réponse\n"
+    },
+    "incorrect" : {
+        "response" : {
+            "answer" : 8
+        },
+        "grade" : 0,
+        "feedback" : "Mauvaise réponse\n"
+    },
+    "not working" : {
+        "response" : {
+            "answer" : 8
+        },
+        "grade" : 50,
+        "feedback" : "Bonne réponse\n",
+        "seed" : 130
+    },
+    "only grade" : {
+        "response" : {
+            "answer" : 7
+        },
+        "grade" : 100
+    },
+    "only feedback" : {
+        "response" : {
+            "answer" : 7
+        },
+        "feedback" : "Bonne réponse\n"
+    },
+    "grader failure" : {
+        "response" : {
+            "answer" : "aa"
+        },
+        "feedback" : "Bonne réponse\n"
+    }
+}
 ==
 
-@ /utils/sandboxio.py
-@ /builder/before.py [builder.py]
-@ /grader/evaluator.py [grader.py]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
