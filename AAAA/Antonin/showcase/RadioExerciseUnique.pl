e8113ff80e7a2782768c3f9a10b02502c7a91216
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-01 16:28:33 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/showcase/RadioExerciseUnique.pl

--- AAAA/Antonin/showcase/RadioExerciseUnique.pl
+++ AAAA/Antonin/showcase/RadioExerciseUnique.pl
@@ -10,13 +10,37 @@ Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL
 unique_choice = True
 
 question_1 = Que pensez vous de cet exercice?
-question_2 = Et globalement de PL
+question_a = Et globalement de PL
 question_3 = Êtes vous satisfait de l'affichage des graphes
 
-items==
+items_1==
 Très satisfaisant
 Satisfaisant
 Moyennement satisfaisant
 Mediocre
 La couleur bleu
+==
+
+items_a==
+Très satisfaisant
+Satisfaisant
+Moyennement satisfaisant
+Mediocre
+La couleur bleu
+==
+
+graph_horizontal_a = 1
+
+items_3==
+Très satisfaisant
+Satisfaisant
+Moyennement satisfaisant
+Mediocre
+La couleur bleu
+==
+
+evaluator==#|py|
+score = 42
+if (unique_choice != "False") and (len(answer) != int(NUMBER_QUESTIONS)):
+    score = 0
 ==
