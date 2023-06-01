197bb47664b222f1919c54f762da9753abd9acf5
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-01 16:37:03 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/showcase/showcase.pl

--- AAAA/Antonin/showcase/showcase.pl
+++ AAAA/Antonin/showcase/showcase.pl
@@ -7,14 +7,20 @@ text==#|markdown|
 Si vous souhaitez prendre en main les exercices PL comprenant le module d'activité vous pouvez cliquer sur les liens plus bas:
 
 - CodeEditorExercise:
+
     - [Mode Enseignant](https://pl-preprod.u-pem.fr/filebrowser/demo/40760/)
+
     - [Mode Etudiant](https://pl-preprod.u-pem.fr/filebrowser/demo/40761/)
 
 - CheckBoxExercise:
+
     - [Mode Enseignant](https://pl-preprod.u-pem.fr/filebrowser/demo/40758/)
+
     - [Mode Etudiant](https://pl-preprod.u-pem.fr/filebrowser/demo/40759/)
 
 - RadioGroupExercise:
+
     - [Mode Enseignant](https://pl-preprod.u-pem.fr/filebrowser/demo/40763/)
+    
     - [Mode Etudiant](https://pl-preprod.u-pem.fr/filebrowser/demo/40762/)
 ==
