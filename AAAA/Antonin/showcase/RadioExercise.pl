9dd39e6c7d35a96470868d0a3b99b8a2a8b0f89a
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-01 16:25:03 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/showcase/RadioExercise.pl

--- AAAA/Antonin/showcase/RadioExercise.pl
+++ AAAA/Antonin/showcase/RadioExercise.pl
@@ -1,6 +1,6 @@
 extends = /AAAA/Antonin/activities/RadioSurvey.pl
 
-title = Exercice d'exemple | RadioGroup | Mode Enseignant
+title = Exercice d'exemple | RadioGroup
 
 text==#|markdown|
 Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL [`RadioSurvey.pl` dont voici la documentation](filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/activities/RadioSurvey.pl).
