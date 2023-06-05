d9d32ba04e0a08ceefd806962446b777836dad02
ANTHONIN BLEUSE <anthonin.bleuse@edu.univ-eiffel.fr>
2023-06-05 16:52:11 +0200

    ableuse modified Yggdrasil/ComputerScience/java/exo2.pl

---
+++ ComputerScience/java/exo2.pl
@@ -0,0 +1,37 @@
+extends = /template/java.pl
+
+title = méthode carré
+
+text = création d'une méthode qui prend un nombre n et qui retourne n au carré
+
+before==
+wrapper = (
+    "public class %s {\n"
+    "    public static void main(String[] args) {\n"
+    "        \n"
+    "    }\n"
+    "}"
+) % classname
+==
+
+build= wrapper% editor["code"]
+
+classname=Main
+
+junit==
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+
+@DisplayName("mkjhkjhlkjhkoijhkjh")  
+class MainTest {
+
+    @Test
+    @DisplayName("test")
+    void TestCarre() {
+        var p = 2
+        Assertions.assertEquals(p, 4);
+    }
+
+
+}
+==
