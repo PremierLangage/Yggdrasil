7d6fda57c57d33ff6431dce00d51fd935b9e9c72
Orhan UYAR <orhan.uyar@univ-eiffel.fr>
2023-06-05 14:52:25 +0200

    ouyar1 modified Yggdrasil/AAAA/Orhan/python/empty_exercice.pl

---
+++ AAAA/Orhan/python/empty_exercice.pl
@@ -0,0 +1,20 @@
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+@ /grader/evaluator.py [grader.py]
+
+author = Orhan Uyar
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = python
+editor.code ==
+# empty file
+==
+
+text==
+Entre du code python
+==
+
+form==
+{{editor}}
+==
