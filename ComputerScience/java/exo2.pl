d90ac44eb07404b31dfa88546b45a89e7f6008da
ANTHONIN BLEUSE <anthonin.bleuse@edu.univ-eiffel.fr>
2023-06-06 15:24:38 +0200

    ableuse modified Yggdrasil/ComputerScience/java/exo2.pl

--- ComputerScience/java/exo2.pl
+++ ComputerScience/java/exo2.pl
@@ -20,65 +20,39 @@ junit==
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions;
 import java.lang.reflect.*;
-import org.junit.rules.TestRule;
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
-
-public class StopOnFailureRule implements TestRule {
-
-    @Override
-    public Statement apply(Statement base, Description description) {
-        return new Statement() {
-            @Override
-            public void evaluate() throws Throwable {
-                try {
-                    base.evaluate();
-                } catch (Throwable t) {
-                    // Arrête l'exécution des autres tests si une erreur se produit
-                    return;
-                }
-            }
-        };
-    }
-}
 
 @DisplayName("SquareTest")  
 class SquareTest {
-
-    @Rule
-    public StopOnFailureRule stopOnFailureRule = new StopOnFailureRule();
-
 
     @Test
     @DisplayName("SimpleSquare")
     void simpleSquare() {
         var p = 2;
-        try{
-            Assertions.assertEquals(Square.square(p), 4,"la fonction ne renvoie pas le bon retour");
-        }catch(Exception e){
-        }
-
-        }
+        Assertions.assertEquals(Square.square(p), 4,"la fonction ne renvoie pas les bonnes valeurs");
+    }
 
     @Test
     @DisplayName("NegativeSquare")
     void negativeSquare() {
         var p = -2;
-        Assertions.assertEquals(Square.square(p), 4);
+        Assertions.assertEquals(Square.square(p), 4,"la fonction ne renvoie pas les bonnes valeurs");
     }
 
     @Test
     @DisplayName("ZeroSquare")
     void zeroSquare() {
         var p = 0;
-        Assertions.assertEquals(Square.square(p), 0);
+        Assertions.assertEquals(Square.square(p), 0,"la fonction ne renvoie pas les bonnes valeurs");
     }
     
     @Test
     @DisplayName("Visibility")
     void visibility() {
-        var p = 0;
-        Assertions.assertEquals(Square.square(p), 0);
+        var p = Square.class.getmethod("square");
+        String modifiers = p.Modifier.toString(method.getModifiers());
+        
+        Assertions.assertEquals(modifiers.contains("static") , true,"la fonction ne renvoie pas les bonnes valeurs");
+
     }
 
 }
