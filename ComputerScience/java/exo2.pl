a33ca6ee21bdeaeee21091a35edb3e47bb8ba767
ANTHONIN BLEUSE <anthonin.bleuse@edu.univ-eiffel.fr>
2023-06-06 11:34:34 +0200

    ableuse modified Yggdrasil/ComputerScience/java/exo2.pl

--- ComputerScience/java/exo2.pl
+++ ComputerScience/java/exo2.pl
@@ -20,30 +20,30 @@ junit==
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions;
 
-@DisplayName("MainTest")  
-class MainTest {
+@DisplayName("%sTest")  
+class %sTest {
 
     @Test
     @DisplayName("SimpleSquare")
     void SimpleSquare() {
         var p = 2;
-        Assertions.assertEquals(Main.square(p), 4);
+        Assertions.assertEquals(%s.square(p), 4);
     }
 
     @Test
     @DisplayName("NegativeSquare")
     void NegativeSquare() {
         var p = -2;
-        Assertions.assertEquals(Main.square(p), 4);
+        Assertions.assertEquals(%s.square(p), 4);
     }
 
     @Test
     @DisplayName("ZeroSquare")
     void ZeroSquare() {
         var p = 0;
-        Assertions.assertEquals(Main.square(p), 0);
+        Assertions.assertEquals(%s.square(p), 0);
     }
     
 
-}
+}%s classname
 ==
