5bae84e7380bfbcb2a54975b84c026bc2654ac31
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-06-06 15:02:00 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/fonctionBug/radioexemple.pl

---
+++ AAAA/Thomas/fonctionBug/radioexemple.pl
@@ -0,0 +1,50 @@
+
+@ /utils/sandboxio.py
+grader  =@ /AAAA/dominique/evaluatorwithlog.pl
+builder =@ /builder/before.py
+
+group =: RadioGroup
+
+
+propositions==
+c'est bien
+c'est cool
+c'est top
+==
+
+
+
+# GENERATE A RANDOM QUESTION
+before==
+group.items = []
+for prop in propositions.split("\n"):
+    group.items.append({
+        "id": str(prop), # generate a random id instead of an hardcoded to avoid cheat
+        "content": str(prop)
+    })
+==
+
+title = Votre avis
+
+text==
+Comment percevez vous la Plateforme PLaTon ? 
+
+==
+
+# PRESENT THE QUESTION TO THE STUDENT
+form==
+{{ group|component }}
+==
+
+# EVALUATE THE STUDENT ANSWER
+evaluator==
+
+feedback = str(globals())
+
+
+def lafonction():
+    pass
+
+
+grade = (score, feedback)
+==
