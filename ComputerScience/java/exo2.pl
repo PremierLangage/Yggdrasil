cd24fa6e7fa5d176328aba8988117edf6fda6e78
ANTHONIN BLEUSE <anthonin.bleuse@edu.univ-eiffel.fr>
2023-06-06 15:08:16 +0200

    ableuse modified Yggdrasil/ComputerScience/java/exo2.pl

--- ComputerScience/java/exo2.pl
+++ ComputerScience/java/exo2.pl
@@ -21,6 +21,27 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions;
 import java.lang.reflect.*;
 import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class StopOnFailureRule implements TestRule {
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                try {
+                    base.evaluate();
+                } catch (Throwable t) {
+                    // Arrête l'exécution des autres tests si une erreur se produit
+                    return;
+                }
+            }
+        };
+    }
+}
+
 @DisplayName("SquareTest")  
 class SquareTest {
 
