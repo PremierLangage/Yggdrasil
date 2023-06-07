49803543983d8cec90d08bc9ff4c0d0fd2d6f621
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-07 15:46:46 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/listeitems.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/listeitems.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/listeitems.pl
@@ -1,21 +1,8 @@
 
 extends = /AAAA/Antonin/activities/RadioSurvey.pl
 
-e:
 
-question_1 = Que pensez-vous de ce tutoriel ?
-
-question_plus_pertinente = Est-ce que vous aussi vous avez du mal à finir vos...
-
-Une liste de réponses possibles dans une variable items_X:
-
-Où une réponse équivaut à une ligne, et l'ensemble de réponse est définis pour une question ayant été définis dans l'exercice avec l'identifiant unique X
-
-Si une liste de réponse n'est rattachée à aucune question, elle sera perdue
-
-Si une question n'a aucune réponse attachée, le comportement sera incomplet.
-
-Exempl
+question_1 = Quel est le problème principale avec vos étudiants ?
 
 items_1==
  sont très hétérogènes en niveaux
