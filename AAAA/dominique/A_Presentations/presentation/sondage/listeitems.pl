0a748771a7090fc844bebc922a6668a1c8d18e1b
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-07 15:45:25 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/listeitems.pl

---
+++ AAAA/dominique/A_Presentations/presentation/sondage/listeitems.pl
@@ -0,0 +1,13 @@
+
+extends = /AAAA/Antonin/activities/RadioSurvey.pl
+
+
+
+ sont très hétérogènes en niveaux
+ ne sont pas engagés
+ ont besoin d’entraînement
+ n'apprennent pas le cours
+ s'ennuient
+ se perdent
+ apprennent mal 
+ ne sont pas critiques
