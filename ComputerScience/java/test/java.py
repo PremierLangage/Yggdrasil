325c401402d70f52132562f86d200d657e6b4a73
ANTHONIN BLEUSE <anthonin.bleuse@edu.univ-eiffel.fr>
2023-06-07 15:16:26 +0200

    ableuse modified Yggdrasil/ComputerScience/java/test/java.py

--- ComputerScience/java/test/java.py
+++ ComputerScience/java/test/java.py
@@ -211,10 +211,8 @@ class Grader:
     
     def modify_answers(self,answers):
         if 'code_before' in self.context:
-            answers="momo"
             answers = self.context['code_before'] + answers 
         if 'code_after' in self.context:
-            answer= "toto"
             answers = answers+self.context['code_after']
         return answers
     
