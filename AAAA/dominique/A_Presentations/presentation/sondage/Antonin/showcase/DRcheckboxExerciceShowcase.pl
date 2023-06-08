13ac23cac4a464bd04c952145b2acdb6574fb895
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-08 16:51:55 +0200

    ajean1 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/showcase/DRcheckboxExerciceShowcase.pl

---
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/showcase/DRcheckboxExerciceShowcase.pl
@@ -0,0 +1,42 @@
+extends = /AAAA/Antonin/activities/CheckboxSurvey.pl
+
+title = Exercice d'exemple | Checkbox
+
+text==#|markdown|
+Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL [`Checkbox.pl` dont voici la documentation](filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl).
+
+Il contient l'affichage des réponses par question ainsi que l'affichage des deux graphes par défaut optionnels:
+
+- statistiques des scores 
+
+- statistiques de participation
+
+==
+
+question_1 = Quel est le problème principale avec vos étudiants ?
+
+items_1==
+ sont très hétérogènes en niveaux
+ ne sont pas engagés
+ ont besoin d’entraînement
+ n'apprennent pas le cours
+ s'ennuient
+ se perdent
+ apprennent mal 
+ ne sont pas critiques
+==
+
+
+
+graph_horizontal_1 = 1
+
+question_2 = Et que pensez-vous des gens qui ne finissent pas leurs ...
+
+items_2==
+Pas grand
+Leurs quoi?
+J'aime bien surtout ceux
+==
+
+# Si vous voulez afficher toutes les combinaisons de réponses possibles:
+# all_possibilities = True
