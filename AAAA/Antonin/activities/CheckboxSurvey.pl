f7a3a9281307a76a6aa2e3da91ee49dc4c8b7141
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-08 14:12:47 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl

--- AAAA/Antonin/activities/CheckboxSurvey.pl
+++ AAAA/Antonin/activities/CheckboxSurvey.pl
@@ -123,6 +123,7 @@ globals()["answers_csv"] = answers_csv
 # Bloc html définissant le bloc utilisateur permettant de remplir des données pour les stats
 # C'est la partie jouable de l'exercice
 formstudent==#|html|
+{{data}}
 {% if False and HAS_ANSWERED != None %}
         <span class="success-state">Vous avez déjà répondu à ce sondage</span>
 {% else %}
