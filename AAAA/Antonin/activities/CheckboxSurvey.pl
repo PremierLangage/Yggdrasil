ccbda48bbef8a243f953af80c47fdac047a3239a
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-08 14:24:51 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl

--- AAAA/Antonin/activities/CheckboxSurvey.pl
+++ AAAA/Antonin/activities/CheckboxSurvey.pl
@@ -112,7 +112,7 @@ if user__role =="teacher" and number_questions != 0:
             if inputValues[i].get("combinations"): 
                 utils.log(f"{items} -> {getValuesAsString(items)}")
                 data["Représentation choix : " + question][1].append(getValuesAsString(items))
-                utils.log(f"data : {data}")
+                utils.log(f"data : {data['Représentation choix : ' + question][1]}")
             data[question][1] += list(map(str, items))
             line_csv += [question] + [1 if e in items else 0 for e in inputValues[q]["items"]]
         answers_csv += ','.join(map(str, line_csv)) + '\\n'
