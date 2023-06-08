6f39e07ef111410e09e53d0248e09e0fa6bcdd4f
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-08 12:33:47 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/activities/CheckboxSurvey.pl

--- AAAA/Antonin/activities/CheckboxSurvey.pl
+++ AAAA/Antonin/activities/CheckboxSurvey.pl
@@ -98,8 +98,8 @@ if user__role =="teacher" and number_questions != 0:
             data["Représentation choix : " + inputValues[i]["question"]] = [labels, []]
         data[inputValues[i]["question"]] = [inputValues[i]["items"], []]
         if len(inputValues[i]) > 2:
-            #if inputValues[i].get("combinations"): 
-            #    data["Représentation choix : " + inputValues[i]["question"]].append(inputValues[i]["horizontal"])
+            if inputValues[i].get("combinations"): 
+                data["Représentation choix : " + inputValues[i]["question"]].append(inputValues[i]["horizontal"])
             data[inputValues[i]["question"]].append(inputValues[i]["horizontal"])
 
     with get_session(table_class=CheckboxResponse, base=Base) as session:
