7c88acefccbeb61cc58862b030b39c04e693c025
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-08 14:29:12 +0200

    drevuz2 modified Yggdrasil/AAAA/Antonin/listeitem2.pl

---
+++ AAAA/Antonin/listeitem2.pl
@@ -0,0 +1,30 @@
+
+
+extends = /AAAA/Antonin/activities/CheckboxSurvey.pl
+
+author= DR
+title=
+text==
+# Sondage sur la problématique principale 
+
+**Choisisez la problématique principale** que vous avez constatés dans vos enseignement.
+
+==
+
+
+graph_horizontal_1= 1
+
+question_1 = Quel est le problème principale avec vos étudiants ?
+
+items_1==
+ sont très hétérogènes en niveaux
+ ne sont pas engagés
+ ont besoin d’entraînement
+ n'apprennent pas le cours
+ s'ennuient
+ se perdent
+ apprennent mal 
+ ne sont pas critiques
+==
+
+
