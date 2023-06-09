fa0b8e848b706dd0ca13be8212a6742938748d3d
CLEMENT GAUDET <clement.gaudet@edu.univ-eiffel.fr>
2023-06-09 17:26:50 +0200

    cgaudet1 modified Yggdrasil/AAAA/Gaudet/test_survey.pl

--- AAAA/Gaudet/test_survey.pl
+++ AAAA/Gaudet/test_survey.pl
@@ -20,37 +20,19 @@ Pistache
 
 ############################################
 
-group1 =: RadioGroup
-group2 =: RadioGroup
-group3 =: RadioGroup
+group =: RadioGroup
 
 before==#|python|
 vos_choix = ""
 
-group1.items = []
-group2.items = []
-group3.items = []
+group.items = []
 
 for i, item in enumerate(items.splitlines()):
-    group1.items.append({ "id": i, "content": item })
-    group2.items.append({ "id": i, "content": item })
-    group3.items.append({ "id": i, "content": item })
+    group.items.append({ "id": i, "content": item })
 ==
 
 form==
-
-{{ question1 }}
-
-{{ group1|component }}
-
-{{ question2 }}
-
-{{ group2|component }}
-
-{{ question3 }}
-
-{{ group3|component }}
-
+{{ group|component }}
 ==
 
 # EVALUATE THE STUDENT ANSWER
@@ -59,9 +41,7 @@ score = 100
 feedback = '<span class="success-state">Réponse enregistrée</span>'
 
 vos_choix = "<br /><br />"
-vos_choix += "<b>Premier choix :</b> " + str(group1.items[group1.selection]['content']) + "<br />"
-vos_choix += "<b>Second choix :</b> " + str(group2.items[group2.selection]['content']) + "<br />"
-vos_choix += "<b>Troisième choix :</b> " + str(group3.items[group3.selection]['content']) + "<br />"
+vos_choix += "<b>Choix :</b> " + str(group.items[group1.selection]['content']) + "<br />"
 
 feedback += vos_choix
 
