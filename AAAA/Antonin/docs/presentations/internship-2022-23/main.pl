7c8692cc545502e589b33770563e8cab6c16159e
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-12 13:48:43 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/docs/presentations/internship-2022-23/main.pl

---
+++ AAAA/Antonin/docs/presentations/internship-2022-23/main.pl
@@ -0,0 +1,27 @@
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+reveal =: RevealViewer
+slide =@ slide.md
+
+title = Présentation
+
+text= 
+
+before==#|python|
+
+reveal.content = f"""
+<section data-markdown>
+    <textarea data-template>
+        {slide}
+    </textarea>
+</section>
+"""
+
+==
+
+
+form==
+{{ reveal|component }}
+==
