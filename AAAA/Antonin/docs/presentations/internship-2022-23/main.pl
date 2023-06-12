691ba0936ba7821545dc7f6e3d073986e444030a
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-12 14:03:11 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/docs/presentations/internship-2022-23/main.pl

--- AAAA/Antonin/docs/presentations/internship-2022-23/main.pl
+++ AAAA/Antonin/docs/presentations/internship-2022-23/main.pl
@@ -3,7 +3,7 @@ grader  =@ /grader/evaluator.py
 builder =@ /builder/before.py
 
 # Chargement des ressources
-@/ressources/logo_igm.png
+@./ressources/logo_igm.png
 
 reveal =: RevealViewer
 slide =@ slides.md
