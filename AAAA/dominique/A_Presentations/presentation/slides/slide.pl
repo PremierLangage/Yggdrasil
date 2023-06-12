cb1099f69ba3d04582887542cb2713138fbdc988
Orhan UYAR <orhan.uyar@univ-eiffel.fr>
2023-06-12 11:56:51 +0200

    ouyar1 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/slides/slide.pl

--- AAAA/dominique/A_Presentations/presentation/slides/slide.pl
+++ AAAA/dominique/A_Presentations/presentation/slides/slide.pl
@@ -18,6 +18,16 @@ reveal.content = f"""
         {slide}
     </textarea>
 </section>
+<section data-markdown>
+  <textarea data-template>
+    ```js [1-2|3|4]
+    let a = 1;
+    let b = 2;
+    let c = x => 1 + 2 + x;
+    c(3);
+    ```
+  </textarea>
+</section>
 """
 
 ==
