aa8061e0abbd7c557ccdf29855815ac0b4fc9888
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-06-12 14:35:47 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/docs/presentations/internship-2022-23/main.pl

--- AAAA/Antonin/docs/presentations/internship-2022-23/main.pl
+++ AAAA/Antonin/docs/presentations/internship-2022-23/main.pl
@@ -9,13 +9,211 @@ igm_logo_platon =$ resources/logo_platon.png
 
 
 reveal =: RevealViewer
-slides =@ slides.md
 
 title =  Présentation de Soutenance Alternance 2022-2023 Antonin JEAN
 
 text= 
 
 before==#|python|
+slides = f"""
+<center>
+
+## Alternance PLaTon
+#### Présentation de Soutenance
+#### Antonin JEAN, Master 1  IGM 2022-23
+<img data-src="{igm_logo_platon}" />
+
+</center>
+
+---
+
+# Déroulé de la présentation
+- Présentation de l'entreprise et du cadre
+- Présentation de PL / PLaTon
+- Présentation des tâches réalisées:
+    - Composant Exercice sur le Front
+    - Module de Statistique
+    - Organisation de projet
+- Présentation Avenir de PLaTon
+
+- Conclusion
+---
+
+# Présentation de l'entreprise et du cadre
+
+## IGM : Institut Gaspard Monge
+
+
+<div class="pannel-around">
+<div> 
+
+
+- ### Institut d'électronique et d'informatique
+- ### Composante de l'Université Gustave Eiffel
+
+</div>
+<div style="width: 33%;">
+    <img data-src="{{igm_logo_igm}}" />
+</div>
+</div>
+
+## Equipe PLaTon
+
+<ul class="pannel-evenly">
+<li>Dominique Revuz</li>
+<li>Thomas Saillard</li>
+<li>Orhan Uyar</li>
+<li>Clément Gaudet</li>
+</ul>
+
+---
+
+<div class="pannel-between">
+<div>
+
+# Présentation de PL
+</div>
+<div style="width: 5%">
+<img data-src="./resources/logo_pl.png" />
+</div>
+</div>
+- PL est une plateforme d'exerciseur en ligne permettant à des enseignants de produire facilement des ressources pédagogiques jouables
+
+<br>
+
+- Développé par des étudiants sous la prise en charge de Dominique Revuz
+
+- Développé en Django / Angular
+
+- Code peu maintenable
+
+- Peu de possibilité d'édition
+
+- Expériences utilisateurs mitigées
+
+---
+
+
+<div class="pannel-between">
+<div>
+
+# Présentation de PL
+</div>
+<div style="width: 5%">
+<img data-src="./resources/logo_pl.png" />
+</div>
+</div>
+
+## Apport de PL
+<div class="pannel-evenly">
+<div>
+
+- ### Permets aux enseignants de:
+    - évaluer leurs étudiants
+    - analyser les données d'utilisation
+    - produire des exercices d'entrainement
+    - automatiser les corrections
+</div>
+<div>
+
+- ### Permets aux étudiants de:
+    - pratiquer régulièrement des exercices aux potentiels variés
+    - être évalué facilement avec des retours instantané
+    - avoir un suivis de leurs progressions
+    - aborder plus sereinement les examens
+    - se challenger sur des exercices
+</div>
+</div>
+
+---
+
+<div class="pannel-between">
+<div>
+
+# Présentation de PLaTon
+</div>
+<div style="width: 5%">
+<img data-src="./resources/logo_platon.png" />
+</div>
+</div>
+
+<div class="pannel-around">
+<div>
+
+- ## Projet ayant pour ambition de remplacer PL
+    - ### Basé sur le code de PL
+    - ### Systèmes retravaillés
+    - ### Technologies plus adaptées
+    - ### Pallie aux défauts de PL
+
+- ## En développement depuis plusieurs années
+    - ### Equipe tournante
+    - ### Projet aux allures de recherche
+
+</div>
+<div>
+<img data-src="./resources/logo_platon.png">
+</div>
+</div>
+
+---
+
+# Présentation des tâches réalisées
+
+- ## Front
+    - ### Composant Exercice
+- ## Back
+    - ### Module de statistique PL
+- ## Organisation
+    - ### Implémentation Poker Planning
+    - ### Introduction d'outils pour la travail d'équipe
+
+### Au cours de mon alternance j'ai pu réaliser des tâches sur PL et PLaTon, avec une ligne directrice : _"réaliser une activité de correction par les paires"_
+---
+<div class="pannel-between">
+<div>
+
+# Front | Composant Exercice
+</div>
+<div style="width: 5%">
+<img data-src="./resources/logo_platon.png" />
+</div>
+</div>
+
+- ### Réalisation des composants chargés d'afficher / rendre jouable une activité et ses exercices
+- ### Plusieurs versions au fils de l'année 
+- ### Tâche difficile à réaliser:
+    - ### Reprise de l'ancien code
+    - ### Mise en commun avec l'équipe
+    - ### Stabilité du cahier des charges
+
+
+- ### Démo de la dernière version réalisée sur un serveur local
+- ### Retrospective sur la tâche
+
+---
+<div class="pannel-between">
+<div>
+
+# Back | Module de Statitstique
+</div>
+<div style="width: 5%; display: flex;">
+<img data-src="./resources/logo_pl.png" />
+<img data-src="./resources/logo_platon.png" />
+</div>
+</div>
+
+- ### Réalisation d'une activité module PL permettant l'intégration de gestion de donnée dans un exercice PL.
+- ### Initié par une tâche demandé par M. Borie
+    - ### Cahier des charges 
+    - ### Répartition du travail
+- ### Tâche complexe
+- ### API, Préservation de la structure PL, Documentation
+- ### Intégration PLaTon
+
+- ### Démo PL module de statistique
+---
+"""
 
 reveal.content = f"""
 <section data-markdown>
