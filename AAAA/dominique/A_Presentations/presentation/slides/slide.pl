f50a12aa34b218baf416eb8ee659186455e18168
Orhan UYAR <orhan.uyar@univ-eiffel.fr>
2023-06-12 17:14:02 +0200

    ouyar1 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/slides/slide.pl

--- AAAA/dominique/A_Presentations/presentation/slides/slide.pl
+++ AAAA/dominique/A_Presentations/presentation/slides/slide.pl
@@ -6,7 +6,6 @@ builder =@ /builder/before.py
 reveal =: RevealViewer
 reveal.content =@ slide.md
 
-
 title = Présentation
 
 text= 
@@ -17,5 +16,12 @@ before==#|python|
 
 
 form==
+<style>
+  code {
+    background: #3c3f41 !important;
+    border-radius: 10px;
+    padding: 20px !important;
+  }
+</style>
 {{ reveal|component }}
 ==
