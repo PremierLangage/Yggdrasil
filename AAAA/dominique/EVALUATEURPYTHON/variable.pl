28fab23fe3b3409cbdcde7ba6e554a093f4ad156
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-12 15:14:09 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/EVALUATEURPYTHON/variable.pl

--- AAAA/dominique/EVALUATEURPYTHON/variable.pl
+++ AAAA/dominique/EVALUATEURPYTHON/variable.pl
@@ -4,3 +4,21 @@
 # un excercice de python avec un editeur 
 extends = /AAAA/dominique/python/template/pltest2023.pl
 
+title= Variable 
+
+text== 
+
+Déclarez une variable {{name}} contenant la constante {{constante}}. 
+
+
+==
+
+
+before==
+
+
+==
+
+evaluator== 
+
+==
