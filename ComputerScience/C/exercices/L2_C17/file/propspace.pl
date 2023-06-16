e6890fe38de82a8874187e55e84395b899eb80b5
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-16 16:59:49 +0200

    drevuz2 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/file/propspace.pl

--- ComputerScience/C/exercices/L2_C17/file/propspace.pl
+++ ComputerScience/C/exercices/L2_C17/file/propspace.pl
@@ -41,6 +41,7 @@ pour chaque ligne vous appelerez la fonction **void lireCoup(char *coup, Pos *de
 Pour afficher les coup on utilisera la fonction prédéfinie 
 **printCoup(Pos a, Pos b);**
 
+Ne touchez pas au code au dessus de vos fonctions.`
 ==
 
 code_before==#|c|
