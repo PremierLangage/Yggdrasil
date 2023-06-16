034da7f1e9545e01c12aeb65fd9dcb1d9c51c4b3
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-16 09:28:41 +0200

    drevuz2 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/linked_list/section_listes.pl

--- ComputerScience/C/exercices/L2_C17/linked_list/section_listes.pl
+++ ComputerScience/C/exercices/L2_C17/linked_list/section_listes.pl
@@ -13,6 +13,10 @@ text==
 
 La section suivante est consacrée aux listes Chainées. 
 
+**ATENTION les types de liste peuvent changer d'un exercice à l'autre**
+
+
+
 ==
 title = Section Listes
 form=
