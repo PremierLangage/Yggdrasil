81e6515ab256a076050dad3ecde2ebf045d81637
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-16 08:57:16 +0200

    drevuz2 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/file/letters_in_file.pl

--- ComputerScience/C/exercices/L2_C17/file/letters_in_file.pl
+++ ComputerScience/C/exercices/L2_C17/file/letters_in_file.pl
@@ -35,8 +35,8 @@ editor.height=300px
 
 text==#|markdown|
 Écrire une fonction qui prend en argument un fichier ouvert et qui fait un résumé 
-des pourcentages d'occurence de chacunes des lettres minuscules parmi toutes les 
-lettres minuscules. Ainsi, pour chaque lettre, il faudra calculer son pourcentage 
+des pourcentages d'occurence de chacunes des lettres **minuscules** parmi toutes les 
+lettres **minuscules**. Ainsi, pour chaque lettre, il faudra calculer son pourcentage 
 de présence parmi toutes les lettres.
 
 Pour un fichier de contenu :
