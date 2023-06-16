e74b9f0e6ce52c1bb50b8b2b02e62993d173c58e
Olivier BOUILLOT <olivier.bouillot@univ-eiffel.fr>
2023-06-16 16:57:57 +0200

    obouillot1 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/linked_list/section_echec.pl

--- ComputerScience/C/exercices/L2_C17/linked_list/section_echec.pl
+++ ComputerScience/C/exercices/L2_C17/linked_list/section_echec.pl
@@ -13,22 +13,26 @@ text==
 
 La section suivante est consacrée au jeu d'échec.
 
+
 Le jeu d’échecs est un jeu opposant deux joueurs (blanc et noir) qui se joue sur un plateau de 8 × 8
 cases. Il se joue avec différentes pièces, mais seules les tours, les fous et la reine nous intéresseront dans
-cet exercice.
+cet exercice.  
+
 
 Si une pièce se déplace sur une case occupée par une pièce adverse, celle-ci est prise et enlevée de
 l’échiquier. Une pièce ne peut pas se placer sur une case occupée par une pièce de son propre camp. La
-tour, le fou ou la reine ne peuvent pas non plus sauter au dessus d’une autre pièce.
+tour, le fou ou la reine ne peuvent pas non plus sauter au dessus d’une autre pièce.  
+
 
 La tour se déplace horizontalement ou verticalement d’autant de cases qu’elle veut. Le fou se déplace
 lui en diagonale d’autant de cases qu’il le souhaite. Enfin, la dame se déplace comme la tour et le fou : elle
-peut donc se déplacer verticalement, horizontalement et en diagonale d’autant de cases qu’elle veut.
+peut donc se déplacer verticalement, horizontalement et en diagonale d’autant de cases qu’elle veut.  
+
 
 Par exemple, dans la Figure 1, on a indiqué en couleur toutes les cases où la tour (respectivement le fou
 et la reine) située en e4 peut aller. Noter que dans le diagramme de gauche, la tour noire en b7 ne peut
 aller ni en g7, ni en h7. Elle peut néanmoins prendre le cavalier blanc en b2, mais ne peut pas se déplacer
-en b1.
+en b1.  
 
 
 ==
