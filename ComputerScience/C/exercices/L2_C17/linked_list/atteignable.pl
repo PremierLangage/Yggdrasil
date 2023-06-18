7fb8445e238c14ad2d3f4ff34624e71013d3fe0c
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-18 08:06:49 +0200

    drevuz2 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/linked_list/atteignable.pl

--- ComputerScience/C/exercices/L2_C17/linked_list/atteignable.pl
+++ ComputerScience/C/exercices/L2_C17/linked_list/atteignable.pl
@@ -137,45 +137,44 @@ code_after==#|c|
 
 int main(int argc, char const *argv[])
 {
-    if (argc == 1)
-    {
-        // test de attteignable
-        printf(" Test de attteignable Tour Noire en 0,0 \n");
-        Game thegame = {0};
-        place_piece(&thegame, TOUR, NOIR, (Pos){0, 0});
-        Dir a = atteignable(&thegame, (Pos){0, 0}, (Pos){0, 7});
-        printf("Dir de 0,7 : %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){0, 0}, (Pos){7, 7});
-        printf("Dir de 7,7: %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){0, 0}, (Pos){7, 0});
-        printf("Dir de 7,0 : %d %d\n", a.i, a.j);
-        printf(" Test de attteignable FOU  en 3,3 \n");
-        place_piece(&thegame, FOU, NOIR, (Pos){3, 3});
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){0, 0});
-        printf("Dir 0,0 : %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 7});
-        printf("Dir : %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 0});
-        printf("Dir : %d %d\n", a.i, a.j);
 
-        printf(" Test de attteignable REINE  en 3,3 \n");
-        place_piece(&thegame, REINE, NOIR, (Pos){3, 3});
-        printf(" Test depart == arrive \n");
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){3, 3});
-        printf((a.i == 0 && a.j == 0) ? "OK\n" : "KO\n");
-        printf(" Test de attteignable REINE  en 3,3 \n");
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){0, 0});
-        printf("Dir 0,0 : %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 7});
-        printf("Dir 7,7 : %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 0});
-        printf("Dir 7,0 : %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){4, 5});
-        printf("Dir 4,5 : %d %d\n", a.i, a.j);
-        a = atteignable(&thegame, (Pos){3, 3}, (Pos){3, 2});
-        printf("Dir 3,2 : %d %d\n", a.i, a.j);
-        return 0;
-    
+    // test de attteignable
+    printf(" Test de attteignable Tour Noire en 0,0 \n");
+    Game thegame = {0};
+    place_piece(&thegame, TOUR, NOIR, (Pos){0, 0});
+    Dir a = atteignable(&thegame, (Pos){0, 0}, (Pos){0, 7});
+    printf("Dir de 0,7 : %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){0, 0}, (Pos){7, 7});
+    printf("Dir de 7,7: %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){0, 0}, (Pos){7, 0});
+    printf("Dir de 7,0 : %d %d\n", a.i, a.j);
+    printf(" Test de attteignable FOU  en 3,3 \n");
+    place_piece(&thegame, FOU, NOIR, (Pos){3, 3});
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){0, 0});
+    printf("Dir 0,0 : %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 7});
+    printf("Dir : %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 0});
+    printf("Dir : %d %d\n", a.i, a.j);
+
+    printf(" Test de attteignable REINE  en 3,3 \n");
+    place_piece(&thegame, REINE, NOIR, (Pos){3, 3});
+    printf(" Test depart == arrive \n");
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){3, 3});
+    printf((a.i == 0 && a.j == 0) ? "OK\n" : "KO\n");
+    printf(" Test de attteignable REINE  en 3,3 \n");
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){0, 0});
+    printf("Dir 0,0 : %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 7});
+    printf("Dir 7,7 : %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){7, 0});
+    printf("Dir 7,0 : %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){4, 5});
+    printf("Dir 4,5 : %d %d\n", a.i, a.j);
+    a = atteignable(&thegame, (Pos){3, 3}, (Pos){3, 2});
+    printf("Dir 3,2 : %d %d\n", a.i, a.j);
+    return 0;
+
 }
 
 ==
