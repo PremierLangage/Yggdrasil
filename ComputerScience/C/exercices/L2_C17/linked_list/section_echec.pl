352124aacf0a1352986470cf38b8eefb2b7bac88
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-18 06:29:35 +0200

    drevuz2 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/linked_list/section_echec.pl

--- ComputerScience/C/exercices/L2_C17/linked_list/section_echec.pl
+++ ComputerScience/C/exercices/L2_C17/linked_list/section_echec.pl
@@ -37,7 +37,7 @@ en b1.
 Voic un code de base pour démarrer dans votre IDE de choix.
 
 <div style="font-size:1em">
-<details><summary>Fichier exemple <button onclick="docopy('ode')">Copy exemple</button></summary><blockquote id="ode" style="white-space: pre-line" 
+<details><summary>le code pour démarrer </summary><blockquote id="ode" style="white-space: pre-line" 
 >{{ode}}
 </blockquote></details>
 
