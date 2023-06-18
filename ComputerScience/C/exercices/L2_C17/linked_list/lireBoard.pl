fb523ec9d30acb75886713cc29bbcb26a8825e97
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-18 21:14:31 +0200

    drevuz2 modified Yggdrasil/ComputerScience/C/exercices/L2_C17/linked_list/lireBoard.pl

--- ComputerScience/C/exercices/L2_C17/linked_list/lireBoard.pl
+++ ComputerScience/C/exercices/L2_C17/linked_list/lireBoard.pl
@@ -220,9 +220,9 @@ int main(int argc, char const *argv[])
             printf("Error opening file!\n");
             return 0;
         }
-        printf(" File opened");
+        printf("Fichier ouvert");
         getGame(f, &thegame);
-        printf(" File red");
+        printf("Fichier lu");
         fclose(f);
         printGame(&thegame);
         return 0;
