3eed31eacf8379faa916243a33d0eb9e96522ab8
BIAYI BILINGI <biayi.bilingi@edu.univ-eiffel.fr>
2023-06-21 10:55:35 +0200

    bbilingi1 modified Yggdrasil/AAAA/Gaudet/template/cgbasic.pl

--- AAAA/Gaudet/template/cgbasic.pl
+++ AAAA/Gaudet/template/cgbasic.pl
@@ -1,7 +1,8 @@
 # Date : 16/06/2022
 # Auteur : Clément Gaudet
 
-title == test 
+title == "test"
+==
 doc==
 Ce template permet de faire des exercices pouvant être résolu avec différents langages, au choix
 de l'étudiant. 
