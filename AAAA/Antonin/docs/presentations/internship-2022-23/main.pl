7f45f35e2b54eb1f7f925235cec42fb785df922b
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-06-26 14:22:26 +0200

    drevuz2 modified Yggdrasil/AAAA/Antonin/docs/presentations/internship-2022-23/main.pl

--- AAAA/Antonin/docs/presentations/internship-2022-23/main.pl
+++ AAAA/Antonin/docs/presentations/internship-2022-23/main.pl
@@ -20,8 +20,8 @@ before==#|python|
 reveal.content = f"""
 <section data-markdown data-background-image="{img_logo_platon}" data-background-position="right" data-background-size="33%">
   <textarea data-template>
-## Alternance PLaTon
-### Présentation de Soutenance
+# PLaTon
+
 ### Dominique Revuz
   </textarea>
 </section>
