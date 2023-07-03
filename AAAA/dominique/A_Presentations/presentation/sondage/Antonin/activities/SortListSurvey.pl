b7bac9988a7bb59206967da819c92d632c286161
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-03 14:28:52 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/SortListSurvey.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/SortListSurvey.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/SortListSurvey.pl
@@ -4,3 +4,15 @@ extends = /AAAA/Antonin/modules/Statistics/activities/SortListSurvey.pl
 question_1 = Vos étudiants sont ils conforme a ces questions ? Classer les dans l'ordre d'importance décroissante.
 
 
+
+
+items_1==
+Vos étudiants ont besoin d’entraînement
+Vos étudiants n'apprennent pas le cours
+Vos étudiants s'ennuient
+Vos étudiants se perdent
+Vos étudiants sont très hétérogènes en niveaux
+Vos étudiants apprennent mal 
+Vos étudiants ne sont pas critiques
+Vos étudiants ne sont pas engagés
+==
