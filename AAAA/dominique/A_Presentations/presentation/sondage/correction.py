aa6cf1a75cce5935a0a1432f8a959e4f4e3b24aa
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-03 09:36:23 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/correction.py

---
+++ AAAA/dominique/A_Presentations/presentation/sondage/correction.py
@@ -0,0 +1,36 @@
+
+import jinja2
+
+
+class Copie:
+    def __init__(self, user, code, score):
+        self.user = user 
+        self.code = code 
+        self.grade = score
+
+    def setCode(self,code):
+        self.code = code 
+
+    def setScore(self, score):
+        if score > self.grade :
+            self.grade = secore 
+
+class FeedbackCor:
+    def __init_(self, filename="template.html"):
+
+        self.filename = filename # template jinja 
+        self.copies = {}
+
+    def addCopie(self, user , code , score):
+        if user in self.copies:
+            self.copies[user].setCode(code)
+            self.copies[user].setScore(score)
+        else:
+            self.copies[user]= Copie(user,code,score)
+
+    def render(self):
+        with open(self.filename,"r") as tempfile:
+            templatestring = tempfile.read()
+        template = jinja2.Template(templatestring)
+        x= template.render(feedback=self)
+        return  x 
