5b10aa0062b52c797278926732ac5aaba176ae79
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-03 09:56:27 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl
@@ -1,33 +1,9 @@
 @ /utils/sandboxio.py
 
-@ /builder/bbefore.py [builder.py]
 
 @ correction.py
 
 @ cortemplate.html [template.html]
 
 
-title=
-text= "ceci doit disparaitre" 
-form =
-
-
-before==
-
-
-import correction
-import random
-feedback = correction.FeedbackCor()
-
-for x in range(10):
-    feedback.addCopie(
-        random.randint(78,95),
-        random.choice(["if","\nelse\fifi\n\ntoto\nthen","else\fifi\n\ntoto","fifi\n\ntoto"]),
-        random.randint(1,10)*10
-        )
-
-text = feedback.render()
-==
-
-grader=
 
