44dcf926dcd24828f7dfd03424771caf90bde80b
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-07-03 11:31:34 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/modules/Statistics/activities/SortListSurvey.pl

--- AAAA/Antonin/modules/Statistics/activities/SortListSurvey.pl
+++ AAAA/Antonin/modules/Statistics/activities/SortListSurvey.pl
@@ -40,13 +40,6 @@ text ==#|markdown|
 # L'utilisateur doit-il choisir une réponse différente par question
 all_possibilities = False
 
-question_1 = test
-items_1 ==
-a
-b
-c
-==
-
 
 # Role : Traitement donnée, 
 # - doit remplir la variable globale : data
