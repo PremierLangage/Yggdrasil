f75ac64aed83a6916041667941dafbfc19140c26
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-07-03 13:29:41 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/modules/Statistics/activities/SortListSurvey.pl

--- AAAA/Antonin/modules/Statistics/activities/SortListSurvey.pl
+++ AAAA/Antonin/modules/Statistics/activities/SortListSurvey.pl
@@ -113,7 +113,7 @@ if user__role =="teacher" and number_questions != 0:
             question, items = i
             items_result = []
             for elem in range(len(items)):
-                items_result += [items[elem]] * ponderation[elem]
+                items_result += [items[elem]] * int(ponderation[elem])
             data[question][1] += list(map(str, items_result))
             line_csv += [question] + [inputValues[q]["items"].index(e) for e in items]
 
