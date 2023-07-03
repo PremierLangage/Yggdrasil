185fc2888689a4cdb37b72189e091b9ca4b9d764
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-03 14:37:16 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRRadioSurvey.pl

---
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRRadioSurvey.pl
@@ -0,0 +1,42 @@
+
+extends = /AAAA/Antonin/modules/Statistics/activities/RadioSurvey.pl
+
+title= Sondage Choix simples
+text==
+
+Donnez votre avis sur ce qui est le plus important !
+
+==
+
+
+
+question_1 = Vos étudiants sont ils conforme a ces questions ? Classer les dans l'ordre d'importance décroissante.
+
+
+graph_horizontal_1 = 1
+
+items_1==
+Vos étudiants ont besoin d’entraînement
+Vos étudiants n'apprennent pas le cours
+Vos étudiants s'ennuient
+Vos étudiants se perdent
+Vos étudiants sont très hétérogènes en niveaux
+Vos étudiants apprennent mal 
+Vos étudiants ne sont pas critiques
+Vos étudiants ne sont pas engagés
+==
+
+ponderation ==
+13
+8
+5
+3
+2
+1
+0
+0
+0
+0
+0
+0
+==
