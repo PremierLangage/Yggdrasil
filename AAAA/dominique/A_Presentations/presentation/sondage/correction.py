94c7c1d532d371e27e079abc2a9568272950666e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-03 09:42:10 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/correction.py

--- AAAA/dominique/A_Presentations/presentation/sondage/correction.py
+++ AAAA/dominique/A_Presentations/presentation/sondage/correction.py
@@ -1,36 +1,46 @@
-
 import jinja2
 
 
 class Copie:
     def __init__(self, user, code, score):
-        self.user = user 
-        self.code = code 
+        self.user = user
+        self.code = code
         self.grade = score
 
-    def setCode(self,code):
-        self.code = code 
+    def setCode(self, code):
+        self.code = code
 
     def setScore(self, score):
-        if score > self.grade :
-            self.grade = score 
+        if score > self.grade:
+            self.grade = score
+
 
 class FeedbackCor:
     def __init__(self, filename="template.html"):
-
-        self.filename = filename # template jinja 
+        self.filename = filename  # template jinja
         self.copies = {}
 
-    def addCopie(self, user , code , score):
+    def getCopies(self):
+        return [(x.user, x.code, x.grade) for x in self.copies.values()]
+
+    def addCopie(self, user, code, score):
         if user in self.copies:
             self.copies[user].setCode(code)
             self.copies[user].setScore(score)
         else:
-            self.copies[user]= Copie(user,code,score)
+            self.copies[user] = Copie(user, code, score)
 
     def render(self):
-        with open(self.filename,"r") as tempfile:
+        with open(self.filename, "r") as tempfile:
             templatestring = tempfile.read()
         template = jinja2.Template(templatestring)
-        x= template.render(feedback=self)
-        return  x 
+        x = template.render(feedback=self)
+        return x
+
+
+if __name__ == "__main__":
+    feedback = FeedbackCor("cortemplate.html")
+    feedback.addCopie("user1", "code1", 10)
+    feedback.addCopie("user2", "code2", 20)
+    feedback.addCopie("user3", "code3", 30)
+    print(feedback.render())
