ceb09fd67ddbbc28e5dbfb458f2f9e198a59cbc7
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-03 09:21:21 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl
@@ -3,3 +3,17 @@
 
 @ cortemplate.html [template.html]
 
+
+title=
+text=
+form=
+
+before==
+
+import correction
+import random
+feedback = correction.FeedbackCor()
+
+for x in range(10):
+    feedback.addCopie(random.randint(5),random.choice(["if","\nelse\fifi\n\ntoto\nthen","else\fifi\n\ntoto","fifi\n\ntoto"]
+
