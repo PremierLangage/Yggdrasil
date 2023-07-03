86827ba26604b4398870b318c7e8ff2428f9948f
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-03 10:55:13 +0200

    drevuz2 modified Yggdrasil/AAAA/Antonin/modules/Statistics/utils/database_utils.py

--- AAAA/Antonin/modules/Statistics/utils/database_utils.py
+++ AAAA/Antonin/modules/Statistics/utils/database_utils.py
@@ -58,7 +58,7 @@ class CodeEditorResponse(Response):
     
     score : Mapped[int] = mapped_column(Integer)
     code  : Mapped[Text] = mapped_column(Text) 
-    eval : Mapped[Bool] = mapped_column(Bool) 
+    eval : Mapped[Boolean] = mapped_column(Boolean, ,default=None) 
 
     def __repr__(self):
         return f"{self.student_id} a obtenu un score de : {self.grade}."
