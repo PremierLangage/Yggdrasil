6fc43b140a663b0fe798790baeefa96fd006c18b
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-07-04 15:22:16 +0200

    ajean1 modified Yggdrasil/AAAA/Thomas/Sondage-06-07-2023/codeCorrection/correction.py

--- AAAA/Thomas/Sondage-06-07-2023/codeCorrection/correction.py
+++ AAAA/Thomas/Sondage-06-07-2023/codeCorrection/correction.py
@@ -64,7 +64,7 @@ class FeedbackCor:
     def getCopiesAsHtml(self):
         result = []
         for i, copie in enumerate(self.getCopies()):
-            result.append(make_hide_block_on_click(str(i), "Contenu", f"""
+            result.append([make_hide_block_on_click(str(i), "Contenu", f"""
 <!-- Generated from script -->
 <div class="content">
         <div class="body">
@@ -81,7 +81,7 @@ class FeedbackCor:
         </div>
 </div>
 <!-- end of genered script -->
-"""))
+"""] + copie))
         return result
 
     def addCopie(self, user, code, score, checked):
