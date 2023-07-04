20cddb7ca5c8c3fa99d9bf0aca6681f4c3fa370b
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-07-04 11:37:07 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/Sondage-06-07-2023/utils/database_utils.py

--- AAAA/Thomas/Sondage-06-07-2023/utils/database_utils.py
+++ AAAA/Thomas/Sondage-06-07-2023/utils/database_utils.py
@@ -58,7 +58,7 @@ class CodeEditorResponse(Response):
     
     score : Mapped[int] = mapped_column(Integer)
     code  : Mapped[Text] = mapped_column(Text) 
-    checked : Mapped[Boolean] = mapped_column(Boolean)
+    checked : Mapped[int] = mapped_column(Integer)
 
     def __repr__(self):
         return f"{self.student_id} a obtenu un score de : {self.grade}."
