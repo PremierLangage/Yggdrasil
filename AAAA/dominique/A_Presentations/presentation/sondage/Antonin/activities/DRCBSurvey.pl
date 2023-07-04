ee64c79558aa67e76be3c887e7b1fcce3e6d2e30
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-04 10:55:18 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRCBSurvey.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRCBSurvey.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRCBSurvey.pl
@@ -13,9 +13,19 @@ text==
 
 question_1= Cochez toutes les cases qui sont vraies pour vos etudiants.
 
+
+
+
+graph_horizontal_1 = 1
+
 items_1==
-
-
-
-
+Vos étudiants ont besoin d’entraînement
+Vos étudiants n'apprennent pas le cours
+Vos étudiants s'ennuient
+Vos étudiants se perdent
+Vos étudiants sont très hétérogènes en niveaux
+Vos étudiants apprennent mal 
+Vos étudiants ne sont pas critiques
+Vos étudiants ne sont pas engagés
 ==
+
