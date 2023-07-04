2ec92012da247c234b1a396267200168e3008484
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-04 15:50:22 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/pypy.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/pypy.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/pypy.pl
@@ -4,3 +4,21 @@ extends = /ComputerScience/python/template/pltest.pl
 
 title = Un tout petit peu de python
 
+text==
+Ecrire une fonction Som(n) qui retourne la somme des n premiers nombre.
+
+Exemple:
+
+Som(3) retourne 6.
+
+==
+
+
+pltest==
+>>> Som(3) == 6
+True
+>>> Som(4) == 10
+True
+>>> Som(5) == 15
+True
+==
