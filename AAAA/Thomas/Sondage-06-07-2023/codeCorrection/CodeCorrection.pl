e006e383e7c79a951563714c363c2af0b6274cc6
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-07-04 17:03:08 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl

--- AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
+++ AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
@@ -54,8 +54,8 @@ evaluator_after==#|py|
 if user__firstname.lower() == "thomas" and user__lastname.lower() == "saillard" :
     # creer un affichage de correction 
     grade = (100,corhtml)
-    # with get_session(table_class = CodeEditorResponse, base=Base) as session:
-    #     users = map(lambda x : x[0], feedback.get_copies())
+    with get_session(table_class = CodeEditorResponse, base=Base) as session:
+         users = map(lambda x : x[0], feedback.get_copies())
     #     modified_copies = [(k, v) for k, v in response.items() if k in users]
     #     for user, checked in modified_copies:
     #         session.query(CodeEditorResponse).filter(CodeEditorResponse.username == user).update({'checked': checked})
