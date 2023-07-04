bb7acc134b986b8c69586bfd1ddb0a0d643f74ce
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-04 10:54:59 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRCBSurvey.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRCBSurvey.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/DRCBSurvey.pl
@@ -2,3 +2,20 @@
 extends= /AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/CheckboxSurvey.pl
 
 
+title = Vos Etudiants
+
+text==
+
+
+
+==
+
+
+question_1= Cochez toutes les cases qui sont vraies pour vos etudiants.
+
+items_1==
+
+
+
+
+==
