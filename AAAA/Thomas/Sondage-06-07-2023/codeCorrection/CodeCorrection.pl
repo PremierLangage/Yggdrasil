ae8b009700d355e7e656146fca2e05078a03e81b
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-07-05 12:47:59 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl

--- AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
+++ AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
@@ -74,10 +74,12 @@ if user__firstname.lower() == "thomas" and user__lastname.lower() == "saillard" 
         grade = (score,"")
         text = _feedback.render()
         text += """
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js"></script>
 <script>hljs.highlightAll();</script>
-        """
+        <script>$(document).ready(function() {
+   $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+});</script>"""
 
 else:
     with get_session(table_class = CodeEditorResponse, base=Base) as session:
