a14b10d3fb86582fef4e65301be09be2e3604c45
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-07-05 10:27:44 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl

--- AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
+++ AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
@@ -67,6 +67,7 @@ if user__firstname.lower() == "thomas" and user__lastname.lower() == "saillard" 
         answers = session.query(CodeEditorResponse).all()
         for answer in answers:
             _feedback.addCopie(answer.username,answer.code,answer.grade, answer.checked)
+    grade = (100, _feedback.render())
 
 else:
     with get_session(table_class = CodeEditorResponse, base=Base) as session:
