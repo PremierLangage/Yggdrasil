177a3e8cfe157a8df8a668de6c032e31b1047f23
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-07-05 18:29:19 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/modules/Statistics/stats.pl

--- AAAA/Antonin/modules/Statistics/stats.pl
+++ AAAA/Antonin/modules/Statistics/stats.pl
@@ -164,7 +164,7 @@ if user__role == "teacher":
 
 # FORM PLAYER
 form==#|html|
-{% if user__role == "teacher" %}
+{% if user__mail == "dominique.revuz@univ-eiffel.fr" %}
 <style>
     .graph {
         display:flex;
