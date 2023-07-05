73c8999100ce9c39da3338640feaaccef98bf64d
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-07-05 17:05:17 +0200

    ajean1 modified Yggdrasil/AAAA/Antonin/modules/Statistics/showcase/MatchListExercise.pl

---
+++ AAAA/Antonin/modules/Statistics/showcase/MatchListExercise.pl
@@ -0,0 +1,34 @@
+extends = /AAAA/Antonin/modules/Statistics/activities/MatchListSurvey.pl
+
+title= Dummy Title MatchListResponse
+
+text==
+Ceci est un exemple d'exercice visant à montrer l'utilisation de l'activité PL [MatchListResponse.pl` dont voici la documentation](filebrowser/option?name=test_pl&path=Yggdrasil/AAAA/Antonin/activities/MatchListSurvey.pl).
+
+Il contient l'affichage des deux graphes par défaut optionnels:
+
+- statistiques des scores 
+
+- statistiques de participatio
+
+==
+
+matches==
+test,1
+a,2,3
+b,1
+==
+
+
+evaluator==#|py|
+if "if" in editor.code:
+    score = 100
+elif "else" in editor.code:
+    score = 42
+else:
+    score = 0
+==
+
+# options
+include_stats_score = True
+include_stats_participation = True
