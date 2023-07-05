0cbfc428705e45ad3a6b22039c212464d3dbc710
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-05 13:52:58 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/match.pl

--- AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/match.pl
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/match.pl
@@ -13,10 +13,9 @@ n'apprennent pas le cours, Quizz pré-cours
 s'ennuient, Activité hors programme 
 se perdent, Ontologies support
 sont très hétérogènes en niveaux, Etayage
-apprennent mal, 
+apprennent mal, analyse de parcours
 ne sont pas critiques, Evaluation par les pairs
-ne sont pas engagés
-
+ne sont pas engagés, ludification
 ==
 
 targets ==
