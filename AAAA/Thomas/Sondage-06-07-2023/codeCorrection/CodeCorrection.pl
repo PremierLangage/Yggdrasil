86052a2d8066357f62f6717398e508092c480d2b
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-07-05 10:44:36 +0200

    tsaillard modified Yggdrasil/AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl

--- AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
+++ AAAA/Thomas/Sondage-06-07-2023/codeCorrection/CodeCorrection.pl
@@ -61,7 +61,7 @@ if user__firstname.lower() == "thomas" and user__lastname.lower() == "saillard" 
         # grade = (100, str(_feedback))
         for user, checked in modified_copies:
             #grade = (100, f"{user}, {checked}" + "ceci est le type de checked " + str(type(checked)) )
-            session.query(CodeEditorResponse).where(CodeEditorResponse.id.in_(session.query(CodeEditorResponse.id).join(Response).filter(Response.username == user).subquery())).update({'checked': int(checked)})
+            session.query(CodeEditorResponse).where(CodeEditorResponse.id.in_(select(CodeEditorResponse.id).join(Response).filter(Response.username == user).subquery())).update({'checked': int(checked)})
  
 
         session.commit()
