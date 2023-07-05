b830812c832d0419abd9305223531a39d1059e69
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-05 13:49:59 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/match.pl

---
+++ AAAA/dominique/A_Presentations/presentation/sondage/Antonin/activities/match.pl
@@ -0,0 +1,20 @@
+
+extends = /model/basic/matchlist.pl
+
+question ==
+Relier chaque problème la solution 
+==
+
+multiple = True
+
+matches ==
+Paris,France
+Marseille,France
+Milan,Italie
+Barcelone,Espagne
+==
+
+targets ==
+Allemagne
+Portugal
+==
