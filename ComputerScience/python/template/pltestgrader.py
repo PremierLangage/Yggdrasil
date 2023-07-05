bdf1d37d1480614fa9d96e77ccbbc83d1f38a98f
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-05 08:54:25 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/template/pltestgrader.py

--- ComputerScience/python/template/pltestgrader.py
+++ ComputerScience/python/template/pltestgrader.py
@@ -88,7 +88,6 @@ if __name__ == "__main__":
         testname = dic['testname0'] if 'testname0' in dic else "Groupe de test 0"
         a, b = tester.runpltest(testname)
     else:
-
         a,b= True, ""
     i=1
     while "pltest"+str(i) in dic and (a or stop ) :
