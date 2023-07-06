588a6410596f938be0d85452a9f421faa88f4afe
Isabelle NAVIZET <isabelle.navizet@univ-eiffel.fr>
2023-07-06 13:00:54 +0200

    inavizet1 modified Yggdrasil/AAAA/Isabelle/Kekule-exos-12avril/kekule-writter-modul.js

--- AAAA/Isabelle/Kekule-exos-12avril/kekule-writter-modul.js
+++ AAAA/Isabelle/Kekule-exos-12avril/kekule-writter-modul.js
@@ -11,8 +11,10 @@
 // fonction qui permet de lire dans la variable mol ce qui est dessiné
 // cela sauve dans la variable smiles le code smile
 // cela sauve en écriture dnas le variable textarea le code smile
+var a = new Kekule.Editor.Composer(document.getElementById('composer'));
+
 function getSmile() {
-    var a = new Kekule.Editor.Composer(document.getElementById('composer'));
+    
 
     var mol = a.exportObjs(Kekule.Molecule)[0];
     //portObjs(Kekule.Molecule)[0];
