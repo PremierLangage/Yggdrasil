7cafcae69d07e25b92ab9f1a9d1fafd3e27a5a92
Isabelle NAVIZET <isabelle.navizet@univ-eiffel.fr>
2023-07-06 12:53:06 +0200

    inavizet1 modified Yggdrasil/AAAA/Isabelle/Kekule-exos-12avril/kekule-writter-modul.js

--- AAAA/Isabelle/Kekule-exos-12avril/kekule-writter-modul.js
+++ AAAA/Isabelle/Kekule-exos-12avril/kekule-writter-modul.js
@@ -3,17 +3,17 @@
 // evite de rajouter une section extrajs
 // le composer utilise Kekule
 // il met les bon boutons autours de la boite
-/*
-var composer = new Kekule.Editor.Composer(document.getElementById('composer'));
-composer.setCommonToolButtons(["undo", "redo","zoomIn", "zoomOut"]);
-composer.setChemToolButtons(["manipulate", "erase", "bond", 'atomAndFormula', 'ring', 'charge']);
-composer.setAllowedObjModifierCategories(["general", "chemStruct", "glyph", "style", "misc"]);
-*/
+
+
 
 // fonction qui permet de lire dans la variable mol ce qui est dessiné
 // cela sauve dans la variable smiles le code smile
 // cela sauve en écriture dnas le variable textarea le code smile
 function getSmile() {
+    var composer = new Kekule.Editor.Composer(document.getElementById('composer'));
+    composer.setCommonToolButtons(["undo", "redo","zoomIn", "zoomOut"]);
+    composer.setChemToolButtons(["manipulate", "erase", "bond", 'atomAndFormula', 'ring', 'charge']);
+    composer.setAllowedObjModifierCategories(["general", "chemStruct", "glyph", "style", "misc"]);
     var mol = composer.exportObjs(Kekule.Molecule)[0];
     portObjs(Kekule.Molecule)[0];
     var smiles = Kekule.IO.saveFormatData(mol, 'smi');
