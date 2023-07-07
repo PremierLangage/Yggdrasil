e5f74e911b357de18a6a0935066b0f9a87778032
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-07-07 11:21:11 +0200

    ajean1 modified Yggdrasil/AAAA/dominique/pythoneditorsurvey/fixPyEditSurvey.pl

--- AAAA/dominique/pythoneditorsurvey/fixPyEditSurvey.pl
+++ AAAA/dominique/pythoneditorsurvey/fixPyEditSurvey.pl
@@ -3,7 +3,7 @@
 
 extends = /AAAA/Antonin/modules/Statistics/activities/CodeEditorSurvey.pl
 extends = /AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl
-@ /AAAA/dominique/pythoneditorsurvey/plteststatsgrader.py [grader.py]
+@ ComputerScience/python/template/pltestgrader.py [grader.py]
 
 
 stopfirsterror= True
