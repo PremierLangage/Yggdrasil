149687c886011e1ed3ee816bbedfe3d56d3967ca
ANTONIN JEAN <antonin.jean@edu.univ-eiffel.fr>
2023-07-07 11:10:21 +0200

    ajean1 modified Yggdrasil/AAAA/dominique/pythoneditorsurvey/fixPyEditSurvey.pl

--- AAAA/dominique/pythoneditorsurvey/fixPyEditSurvey.pl
+++ AAAA/dominique/pythoneditorsurvey/fixPyEditSurvey.pl
@@ -2,7 +2,7 @@ before==
 ==
 extends = /AAAA/Antonin/modules/Statistics/activities/CodeEditorSurvey.pl
 
-@ /ComputerScience/python/template/pltest_doc.py
+@ /ComputerScience/python/template/pltest_doc.py [pltest_doc.py]
 
 extends = /AAAA/dominique/A_Presentations/presentation/sondage/correctionTemplate.pl
 
