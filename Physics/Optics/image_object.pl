e90154bd8065ef28717713c3db0394e123f837a5
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-13 16:34:17 +0200

    drevuz2 modified Yggdrasil/Physics/Optics/image_object.pl

--- Physics/Optics/image_object.pl
+++ Physics/Optics/image_object.pl
@@ -5,7 +5,7 @@ title = Point objet et point image en optique géométrique
 lang = fr
 
 before ==
-from random import choice randint 
+from random import choice, randint 
 from sympy import Rational
 angle=random.choice([Rational(1,2),1,Rational(3,2),Rational(1,4),Rational(3,4),Rational(5,4)])*sp.pi
 valangle=float(angle.evalf())
