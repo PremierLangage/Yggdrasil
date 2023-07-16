6ac9953144b41dd6bfa29ace1119de0566b4e93a
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-16 22:27:31 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/javaerror/javeerrorexemple.pl

--- AAAA/dominique/javaerror/javeerrorexemple.pl
+++ AAAA/dominique/javaerror/javeerrorexemple.pl
@@ -6,6 +6,14 @@ extends= javaerror.pl
 
 title = this is an exemple 
 text=
+
+
+stdout_tests==
+"No argument" Bravo!
+==
+
+
+
 code==
 publique Class Main{
 System.out.println("Bravo!");
@@ -20,13 +28,9 @@ Il faut corriger le code dans l'editeur pour que le test puisse avoir lieux.
 Voici ce qu'a répondu la compilation.
 """
 
-
 editor['code']=code
 
 
 ==
 
 
-stdout_tests==
-"No argument" Bravo!
-==
