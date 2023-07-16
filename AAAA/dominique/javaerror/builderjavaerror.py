fad94384aa1a6cba92377e5e20c9f483a488c5e4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-16 22:23:19 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/javaerror/builderjavaerror.py

--- AAAA/dominique/javaerror/builderjavaerror.py
+++ AAAA/dominique/javaerror/builderjavaerror.py
@@ -12,16 +12,10 @@ import java
 if __name__ == "__main__":
     import sys
     if len(sys.argv) < 3:
-    msg = ("Sandbox did not call builder properly:\n"
-            +"Usage: python3 builder.py [input_json] [output_json]")
-    print(msg, file=sys.stderr)
-    sys.exit(1)
+        msg = ("Sandbox did not call builder properly:\n"
+                +"Usage: python3 builder.py [input_json] [output_json]")
+        print(msg, file=sys.stderr)
+        sys.exit(1)
     context = sandboxio.get_context()
-    if len(sys.argv) < 5 :
-        java.PreGrader.grade(context, context['code'])
-    else:
-        answers = sandboxio.get_answers()
-        # standar grader 
-        java.Grader.grade(context, answers)
-
+    java.PreGrader.grade(context, context['code'])
 
