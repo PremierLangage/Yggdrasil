d317bc8116d23a03458bc0466dbd1b62fac98286
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-16 17:32:57 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/javaerror/javeerrorexemple.pl

---
+++ AAAA/dominique/javaerror/javeerrorexemple.pl
@@ -0,0 +1,12 @@
+
+extends= javaerror.pl 
+
+title = this is an exemple 
+
+before==
+
+text = """
+Il faut corriger le code dans l'editeur pour que le test puisse avoir lieux.
+Voici ce qu'a répondu la compilation.
+"""
+
