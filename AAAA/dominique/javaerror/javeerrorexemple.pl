638408b05499912f3aa076caff23dd4986751b83
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-16 22:26:37 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/javaerror/javeerrorexemple.pl

--- AAAA/dominique/javaerror/javeerrorexemple.pl
+++ AAAA/dominique/javaerror/javeerrorexemple.pl
@@ -6,8 +6,13 @@ extends= javaerror.pl
 
 title = this is an exemple 
 text=
-pregrader=
-code=
+code==
+publique Class Main{
+System.out.println("Bravo!");
+}
+==
+
+
 before==
 
 text = """
@@ -15,11 +20,7 @@ Il faut corriger le code dans l'editeur pour que le test puisse avoir lieux.
 Voici ce qu'a répondu la compilation.
 """
 
-code="""
-publique Class Main{
-System.out.println("Bravo!");
-}
-"""
+
 editor['code']=code
 
 
