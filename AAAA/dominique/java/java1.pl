a903e8c642b9dd078852dacf69549d2a203ae5f7
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-16 09:02:41 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/java/java1.pl

---
+++ AAAA/dominique/java/java1.pl
@@ -0,0 +1,50 @@
+
+extends = ~/template/java.pl
+
+title = Première méthode
+text ==
+
+Ecrire dans la classe {{classname}} une méthode {{name}} qui prend un *int* en paramêtre et retourne sont carré.
+
+
+== 
+
+before==
+import random
+classname = random.choice(['Maclass','Main','Principale','Platon','PL'])
+name = random.choice(['maMethode','m','square','fois','prise'])
+
+
+before=
+editor["code"] = f"""
+public class {classname} {
+    public static void main(String[] args) {
+        
+    }
+}"""
+==
+
+junit==
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+
+@DisplayName("Point Test")
+class PointTest {
+    
+    @Test
+    @DisplayName("Get X")
+    void testGetX() {
+        var p = new Point(2, 3);
+        Assertions.assertEquals(p.getX(), 2);
+    }
+
+    @Test
+    @DisplayName("Get Y")
+    void testGetY() {
+        var p = new Point(2, 3);
+        Assertions.assertEquals(p.getY(), 3);
+    }
+}
+==
+
+
