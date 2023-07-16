27e1ad5fbd582ca43bcd0c357e4a016400c1499a
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-16 08:40:05 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/java/hellojava.pl

---
+++ AAAA/dominique/java/hellojava.pl
@@ -0,0 +1,18 @@
+
+extends= /template/java.pl
+
+title= Sorties
+
+text==
+
+On veut produire quelque chose avec notre code !
+
+Afficher un text sur l'ecran ! 
+
+Techniquement on dit "envoyer des caractères sur la sortie standard" !
+
+Pour cela on utilise la fonction **println** de la class **java.lang.System**.
+
+
+==
+
