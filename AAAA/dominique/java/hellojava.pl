93a4fa1161969c2f610f74fccca0ef751e040ff9
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-16 08:47:39 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/java/hellojava.pl

--- AAAA/dominique/java/hellojava.pl
+++ AAAA/dominique/java/hellojava.pl
@@ -15,8 +15,11 @@ Pour cela on utilise la fonction **println** de la class **java.lang.System**.
 
 Dans la fonction Main de la class Bienvenue ajoutez la ligne suivante :
 
-    System.println("Hello");
+    System.out.println("Hello");
 
+L'idée est que System.out est un canal de sortie de votre programme et que vous écrivez dedans en utilisant sa méthode println.
+
+La méthode println recopie ses paramètre dans le canal et ajoute un caractère de fin de ligne '\n'.
 ==
 
 classname = Bienvenue
