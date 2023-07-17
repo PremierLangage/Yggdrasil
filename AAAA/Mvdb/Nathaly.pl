7bb448beb9d3c43cceead4e7e9b7cf8879de0e17
Nadime FRANCIS <nadime.francis@univ-eiffel.fr>
2023-07-17 15:30:10 +0200

    nfrancis1 modified Yggdrasil/AAAA/Mvdb/Nathaly.pl

---
+++ AAAA/Mvdb/Nathaly.pl
@@ -0,0 +1,32 @@
+extends = /gift/templates/qmultichoice.pl
+
+title==
+Multiple Choice 
+==
+
+text==
+Parmi ces ingrédients, choisissez tout ceux qui permettent de réaliser des crêpes au chocolat.
+==
+
+choices==
+%-100.0%Jambon
+%25.0%Oeuf
+%-100.0%Levure
+%25.0%Farine
+%-100.0%Viande
+%25.0%Chocolat
+%25.0%Sucre
+==
+
+doc == 
+Exo à choix multiple où on peut choisir une ou plusieurs réponses parmi celles 
+proposées. 
+
+Code couleurs : Après validation de la réponse 
+-> Cases vertes = réponses justes
+-> Cases rouges = réponses fausses
+
+Si la case devrait être coché mais ne l'est pas alors il se mettra en valeur sous
+forme d'animation et sera de couleur verte. 
+==
+
