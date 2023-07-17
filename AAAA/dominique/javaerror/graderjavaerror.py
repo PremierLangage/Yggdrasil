149c022bcc1f0e4e6432edfeb55fac4c989f1d7e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-17 07:52:17 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/javaerror/graderjavaerror.py

--- AAAA/dominique/javaerror/graderjavaerror.py
+++ AAAA/dominique/javaerror/graderjavaerror.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     context = sandboxio.get_context()
     answers = sandboxio.get_answers()
     context['trys']=int(context['trys'])+1
+    context['text'] = context['basetext']
     java.Grader.grade(context, answers)
 
