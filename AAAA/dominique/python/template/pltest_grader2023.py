c9f9df6a2c6d5b086c17db2e7cedcb56c9ca8f9e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-07-19 11:48:33 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/python/template/pltest_grader2023.py

--- AAAA/dominique/python/template/pltest_grader2023.py
+++ AAAA/dominique/python/template/pltest_grader2023.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
         tester = PlRunner(student,pltest, fb=lfb)
         testname = dic['testname'] if 'testname' in dic else "Groupe de test un"
         a, b = tester.runpltest(testname, numgroup)
-        b += "<div>"+lfb.toJson()+"<div>"
+        b += "<div>"+str(lfb.toJson())+"<div>"
     elif "pltest0" in dic:
         pltest = dic['pltest0']
         tester = PlRunner(student,pltest)
