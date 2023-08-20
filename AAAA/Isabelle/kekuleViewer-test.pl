dd4f34f463d830be325183582669535aad162544
Isabelle NAVIZET <isabelle.navizet@univ-eiffel.fr>
2023-08-20 18:27:27 +0200

    inavizet1 modified Yggdrasil/AAAA/Isabelle/kekuleViewer-test.pl

---
+++ AAAA/Isabelle/kekuleViewer-test.pl
@@ -0,0 +1,38 @@
+@ lib:/utils/sandboxio.py
+@ lib:/builder/before.py [builder.py]
+@ lib:/grader/evaluator.py [grader.py]
+
+title = KekuleViewer test (Molecule Viewer)
+
+before ==
+
+==
+
+lang = fr
+
+text ==
+==
+
+# 4eme ligne: recupere kekule avec les moduldes chemWidget, algorithm, render, openbable et indigo. pas les autres modules
+form ==
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/109/three.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/109/three.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://unpkg.com/kekule/dist/kekule.js?modules=chemWidget,algorithm,render,openbabel,indigo"></script>
+<link rel="stylesheet" type="text/css" href="https://unpkg.com/kekule/dist/themes/default/kekule.css" />
+<script type="text/javascript">
+{{script|safe}}
+</script>
+<h2> SMILES </h2>
+<textarea rows="1" cols="50" id="smiles"  style="font-size: 12pt">
+exemple : CCN(c1cc(C)ccc1C)S(=O)(=O)c1cc(C(N)=O)n(C)c1
+</textarea>
+<div id="kekule" style="width:300px;height:300px"
+		 data-widget="Kekule.ChemWidget.Viewer2D" data-enable-toolbar="false"
+</div>
+==
+
+evaluator ==
+grade=(100,"Bonne réponse")
+==
+
