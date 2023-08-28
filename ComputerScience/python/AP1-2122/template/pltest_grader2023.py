8c0f20b7581942f1ec6f826292b2e8fa9fba64cb
Thomas SAILLARD <thomas.saillard2@univ-eiffel.fr>
2023-08-28 16:25:55 +0200

    tsaillard2 modified Yggdrasil/ComputerScience/python/AP1-2122/template/pltest_grader2023.py

--- ComputerScience/python/AP1-2122/template/pltest_grader2023.py
+++ ComputerScience/python/AP1-2122/template/pltest_grader2023.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
     outstr +=  b
     if "feedback" in dic: # FIXME feedback devrai être un dictionnaire.
         outstr += dic["feedback"]+" valeur de stop "+ str(stop)
-    output(a,outstr)
+    output(a,"score:"+str(a)+outstr)
 
 
 
