679e5952a66aded077500b31f51e047836c1ad0f
Thomas SAILLARD <thomas.saillard2@univ-eiffel.fr>
2023-08-28 16:25:30 +0200

    tsaillard2 modified Yggdrasil/ComputerScience/python/AP1-2122/chapitre_1_variables/variable001-2122.pl

--- ComputerScience/python/AP1-2122/chapitre_1_variables/variable001-2122.pl
+++ ComputerScience/python/AP1-2122/chapitre_1_variables/variable001-2122.pl
@@ -4,8 +4,7 @@
 title= Initialisation d'une variable (1).
 
 tag=variable|affectation 
-
-extends=/ComputerScience/python/template/pltest.pl
+extends=/ComputerScience/python/AP1-2122/template/pltest2023.pl
 @ /utils/sandboxio.py
 
 text==
