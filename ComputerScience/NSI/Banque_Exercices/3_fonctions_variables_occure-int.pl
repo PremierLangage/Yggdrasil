239ce092026bba3dbbf9c5be0e6b4404a8e64ee4
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-08-31 12:34:07 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl

--- ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl
+++ ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl
@@ -10,6 +10,8 @@ tag=function|variable|if|elif|else|conditionnelles
 
 before==
 inconnu = 6
+inconnugrand = 7
+inconnupetit = 5
 ==
 
 
@@ -17,14 +19,14 @@ text==
 Ecrivez une fonction `comparaison` ayant un unique paramètre `nombre` utilisant un **if elif else** retournant la chaîne de caractères:
 
 - "Trop petit !", si `nombre` est plus petit que {{inconnu}}.
-- "Tout juste !", si `nombre` est exactement 6.
-- "Trop grand !", si `nombre` est plus grand que 6.
+- "Tout juste !", si `nombre` est exactement {{inconnu}}.
+- "Trop grand !", si `nombre` est plus grand que {{inconnu}}.
 
 Exemples :
 
-	>>> comparaison(7)
+	>>> comparaison({{inconnugrand}})
     'Trop grand !'
-    >>> comparaison(-5)
+    >>> comparaison({{inconnupetit}})
     'Trop petit !'
 ==
 
