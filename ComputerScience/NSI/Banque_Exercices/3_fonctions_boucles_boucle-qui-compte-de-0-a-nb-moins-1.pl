1627c8b1b13206e61a5dde53b3cfa081f2c51989
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-08-31 15:39:58 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/3_fonctions_boucles_boucle-qui-compte-de-0-a-nb-moins-1.pl

---
+++ ComputerScience/NSI/Banque_Exercices/3_fonctions_boucles_boucle-qui-compte-de-0-a-nb-moins-1.pl
@@ -0,0 +1,41 @@
+extends = /ComputerScience/python/template/pltest.pl
+@ /ComputerScience/python/template/before.py [builder.py]
+tag=function|InstructionsRepetitives|ForInRange
+title = Boucle qui compte de 0 à `nb - 1`
+
+before==#|python|
+from random import randint
+n = randint(20, 40)
+
+def afficheboucle(n):
+    res = ''
+    for i in range(n):
+        res += str(i) + '\n'
+    return res
+
+pltest0 = f"""
+>>> afficheboucle({n})
+{afficheboucle(n)}
+==
+
+text==
+Écrire une fonction `afficheboucle` ayant pour paramètre `nb` un nombre entier positif qui retourne une chaine de caractère contenant les nombres de 
+`0` à `nb-1` (chacun suivi d'un retour à la ligne), à l'exclusion de tout autre caractère.
+
+Par exemple:
+    >>> print(afficheboucle(5))
+    0
+    1
+    2
+    3
+    4
+    
+    >>> afficheboucle(5)
+    '0\n1\n2\n3\n4\n'
+
+==
+
+pltest==
+>>> afficheboucle(5)
+'0\n1\n2\n3\n4\n'
+==
