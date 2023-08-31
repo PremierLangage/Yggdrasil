f8c4c193c6f5e92b56e23ce97b057826c97e3032
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-08-31 12:07:26 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl

--- ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl
+++ ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl
@@ -6,10 +6,14 @@ extends = /ComputerScience/python/template/pltest.pl
 title=Distinguer un nombre !
 tag=function|variable|if|elif|else|conditionnelles
 
+before==
+inconnu = 6
+==
+
 text==
 Ecrivez une fonction `comparaison` ayant un unique paramètre `nombre` utilisant un **if elif else** retournant la chaîne de caractères:
 
-- "Trop petit !", si `nombre` est plus petit que 6.
+- "Trop petit !", si `nombre` est plus petit que {inconnu}.
 - "Tout juste !", si `nombre` est exactement 6.
 - "Trop grand !", si `nombre` est plus grand que 6.
 
