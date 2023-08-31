700c2ff65006e0a37739d9e1f8bb2d0f6555a9b5
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-08-31 16:52:35 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_priority.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_priority.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_priority.pl
@@ -25,7 +25,7 @@ solved=[ False for n in range(N)]
 isfloat = [ False for n in range(N)]
 inputs = []
 for i in range(N):
-    inp = InputField()
+    inp = Input()
     globals()[f"input{i}"] = inp
     inputs.append(inp)
 ==
