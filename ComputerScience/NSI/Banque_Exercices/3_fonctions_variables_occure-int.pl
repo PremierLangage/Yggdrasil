1390f5d6c41a78268db8fa8df7447b1ee99b83a6
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-08-31 12:16:30 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl

--- ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl
+++ ComputerScience/NSI/Banque_Exercices/3_fonctions_variables_occure-int.pl
@@ -13,7 +13,7 @@ inconnu = 6
 ==
 
 
-text==
+textf==
 Ecrivez une fonction `comparaison` ayant un unique paramètre `nombre` utilisant un **if elif else** retournant la chaîne de caractères:
 
 - "Trop petit !", si `nombre` est plus petit que {inconnu}.
