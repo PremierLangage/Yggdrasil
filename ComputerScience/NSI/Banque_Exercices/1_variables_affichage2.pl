74afc3ce8560ad20a9f37da4432784e486033f6a
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-09-05 11:53:10 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_affichage2.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_affichage2.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_affichage2.pl
@@ -17,18 +17,11 @@ grader=f"""#|python|
 for nom in ['Jade', 'Louise', 'Emma', 'Alice', 'Ambre', 'Lina', 'Rose', 'Chloé']:
     for age in [14, 15, 16, 17, 18]:
         begin_test_group("Test " + nom)
-        # Dans cet exemple on veut faire un test pour chaque permutation de (1, 2, 3)
-        # On utilise ici un style "verbeux" avec plusieurs appels
-        # set_title(f"Test")
-        # Fixe les variables globales disponibles pour les prochaines exécution
         set_globals({variable} = nom, age = age)
-        # Lance l'exécution du programme dans les conditions spécifiées
         run()
-        # Vérifie la sortie standard obtenue
         assert_output("bonjour, je m'appelle " + nom + " et j'ai " + str(age) + ' ans\\n')
         # Vérifie que les variables globales n'ont pas changé
         assert_no_global_change()
-        # Clôt le précédent groupe de tests
         end_test_group()
 """
 
