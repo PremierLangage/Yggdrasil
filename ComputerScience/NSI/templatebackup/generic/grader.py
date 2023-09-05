c7066f39376da87825ec58d24e8d42be63ad3b80
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-09-05 11:38:47 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/templatebackup/generic/grader.py

--- ComputerScience/NSI/templatebackup/generic/grader.py
+++ ComputerScience/NSI/templatebackup/generic/grader.py
@@ -28,7 +28,6 @@ def _get_student_code(exercise_context: dict):
         raise test.GraderError(missing_editor)
     editor_id = exercise_context["editor"].cid
     answers = sandboxio.get_answers()
-    # print(answers['answer'])
     return answers['answer']
 
 
