ff923509834cb881039d36dcf3c1800d30dbc334
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-09-05 10:17:59 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_affichage.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_affichage.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_affichage.pl
@@ -2,7 +2,8 @@
 # author emmanuel brunache
 
 # Heritage d'un type d'exercice 
-extends=/ComputerScience/python/template/soluce.pl
+# extends=/ComputerScience/python/template/soluce.pl
+extends = /template/python.pl
 @ /builder/before.py [builder.py]
 title = Affichage
 
@@ -14,6 +15,7 @@ variable = random.choice(['prenom', 'name', 'surnom', 'blaze', 'sobriquet'])
 nom = random.choice(['Jade', 'Louise', 'Emma', 'Alice', 'Ambre', 'Lina', 'Rose', 'Chloé'])
 mplsoluce0="Test tiré aléatoirement \n{}\n".format(nom)
 soluce=f'{variable}="{nom}"\nprint("bonjour", {variable})'
+stdout_tests=f"""print("bonjour", {variable})"""
 # code=f'{variable}="{nom}" # NE PAS EFFACER CETTE LIGNE\n'
 with open("student.py","w") as sf:
         sf.write(f'{variable}="{nom}"')
