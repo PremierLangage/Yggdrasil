7b2fbf9e2010b05b8cb49c43f392cae3171ec408
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-09-05 10:09:15 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/1_variables_affichage.pl

--- ComputerScience/NSI/Banque_Exercices/1_variables_affichage.pl
+++ ComputerScience/NSI/Banque_Exercices/1_variables_affichage.pl
@@ -12,12 +12,12 @@ before==#|python|
 import random
 variable = random.choice(['prenom', 'name', 'surnom', 'blaze', 'sobriquet'])
 nom = random.choice(['Jade', 'Louise', 'Emma', 'Alice', 'Ambre', 'Lina', 'Rose', 'Chloé'])
-grader+=f"globals()[{variable}] = {nom}"
 mplsoluce0="Test tiré aléatoirement \n{}\n".format(nom)
 soluce=f'{variable}="{nom}"\nprint("bonjour", {variable})'
+# code=f'{variable}="{nom}" # NE PAS EFFACER CETTE LIGNE\n'
+with open("student.py","w") as sf:
+        sf.write(f'{variable}="{nom}"')
 ==
-
-
 
 text==  
 Supposons que la variable {{variable}} soit initrialisée avec une valeur de type chaine de caractère.
