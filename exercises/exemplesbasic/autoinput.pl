18beb3d25df0ea17e4abf0e1a1478b44189e8358
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-06 10:39:21 +0200

    drevuz2 modified Yggdrasil/exercises/exemplesbasic/autoinput.pl

--- exercises/exemplesbasic/autoinput.pl
+++ exercises/exemplesbasic/autoinput.pl
@@ -1,3 +1,73 @@
 
 extends = /model/basic/autoinput.pl
 
+# Specific keys
+
+
+valeurs==
+Albanie	Tirana
+Allemagne	Berlin
+Andorre	Andorre-la-Vieille
+Arménie	Erevan
+Autriche	Vienne
+Azerbaïdjan	Bakou
+Belgique	Bruxelles
+Biélorussie	Minsk
+Bosnie-Herzégovine	Sarajevo
+Bulgarie	Sofia
+Croatie	Zagreb
+Danemark	Copenhague
+Espagne	Madrid
+Estonie	Tallinn
+Finlande	Helsinki
+France	Paris
+Géorgie	Tbilissi
+Grèce	Athènes
+Hongrie	Budapest
+Irlande	Dublin
+Islande	Reykjavik
+Italie	Rome
+Lettonie	Riga
+Liechtenstein	Vaduz
+Lituanie	Vilnius
+Luxembourg	Luxembourg
+Macédoine	Skopje
+Malte	La Valette
+Moldavie	Chisinau
+Monaco	Communauté de Monaco
+Monténégro	Podgorica
+Norvège	Oslo
+Pays-Bas	Amsterdam
+Pologne	Varsovie
+Portugal	Lisbonne
+République Tchèque	Prague
+Roumanie	Bucarest
+Royaume-Uni	Londres
+Russie	Moscou
+Saint-Marin	Ville de Saint Marin
+Serbie	Belgrade
+Slovaquie	Bratislava
+Slovénie	Ljubljana
+Suède	Stockholm
+Suisse	Berne
+Turquie	Ankara
+Ukraine	Kiev
+Vatican	Vatican
+==
+
+
+sol ==
+
+==
+
+items ==
+
+
+==
+
+prefix = "Réponse :"
+
+# Question and input block
+
+question ==
+==
