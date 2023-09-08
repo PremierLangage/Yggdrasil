a415635a1c33255d6f15d4aecee96b3a04e41ad2
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-09-08 16:49:00 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/lesTests/test.pl

---
+++ AAAA/ThomasSaillard/lesTests/test.pl
@@ -0,0 +1,21 @@
+
+
+
+title = Les tests 
+
+statement==
+
+Ce test a pour objectif de tester les capacités du model pltest2022.
+
+En particulier la portabilité des exercices depuis Premier Langage.
+
+
+{{user__id}}
+{{activity__id}}
+
+
+
+==
+
+
+
