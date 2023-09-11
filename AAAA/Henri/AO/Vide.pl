14decbb8132cfc7ce8270b155611fe1bdede0f64
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2023-09-11 17:14:56 +0200

    hderycke1 modified Yggdrasil/AAAA/Henri/AO/Vide.pl

---
+++ AAAA/Henri/AO/Vide.pl
@@ -0,0 +1,47 @@
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+
+author=Henri Derycke
+
+title= Exemple d'assembleur
+
+text==#|markdown|
+Écrire un programme qui ne fait rien
+==
+
+editor =: CodeEditor
+editor.theme=dark
+editor.language=asm
+
+editor.code==#|asm|
+section .text
+global _start
+
+_start:
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+==
+
+
+# Correction
+sources.main==#|asm|
+section .text
+global _start
+
+_start:
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+==
+
+
+form ==
+{{ editor|component }}
+==
+
+evaluator==#|py|
+grade = 100
+==
