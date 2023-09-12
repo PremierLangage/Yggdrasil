44a9c5fea983599b5400b3375d23b89621d172d0
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-09-12 15:33:36 +0200

    tsaillard modified Yggdrasil/AAAA/Antonin/modules/Statistics/utils/database_utils.py

--- AAAA/Antonin/modules/Statistics/utils/database_utils.py
+++ AAAA/Antonin/modules/Statistics/utils/database_utils.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
 
 Base = declarative_base()
-def get_session(table_class, base, db_url = "activities-db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
+def get_session(table_class, base, db_url = "activity_db", db_name = "activity_db", db_user = "activity_user", db_password = "Dimz80k7X97!"):
     engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_url}/{db_name}")
     if not inspect(engine).has_table(table_class.__tablename__):
         base.metadata.create_all(engine)
