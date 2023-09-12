10a540aa72786048ebaf1deb30782a9b9d61b09b
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-12 11:21:55 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/RE/regexp1.pl

--- AAAA/dominique/RE/regexp1.pl
+++ AAAA/dominique/RE/regexp1.pl
@@ -6,18 +6,19 @@ extends=/ComputerScience/python/AP1-2122/template/pltest2023.pl
 title = la Méthode match du module re
 
 text==
+
 # Sachant que 
 
 
- Méthode     Objectif     
 
- match()     Détermine si la RE fait correspond dès le début de la chaîne. 
- search()    Analyse la chaîne à la recherche d'une position où la RE correspond. 
- findall()   Trouve toutes les sous-chaînes qui correspondent à la RE et les renvoie sous la forme d'une liste. 
- finditer()  Trouve toutes les sous-chaînes qui correspondent à la RE et les renvoie sous la forme d'un itérateur. 
+    Méthode     Objectif     
+    match()     Détermine si la RE fait correspond dès le début de la chaîne. 
+    search()    Analyse la chaîne à la recherche d'une position où la RE correspond. 
+    findall()   Trouve toutes les sous-chaînes qui correspondent à la RE et les renvoie sous la forme d'une liste. 
+    finditer()  Trouve toutes les sous-chaînes qui correspondent à la RE et les renvoie sous la forme d'un itérateur. 
 
 
-Ecrire une fonction *debut(motif,text)* qui retourne si oui ou non le motif est présent au debut du text.  
+Ecrire une fonction **debut(motif,text)** qui retourne si oui ou non le motif est présent au debut du text.  
 
 Ne pas oublier de faire l'import du module re !
 
