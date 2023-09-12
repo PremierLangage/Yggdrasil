c010fba387f9751f59e3d7e73d4a38ef751456dd
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-12 10:50:33 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/RE/regexp1.pl

---
+++ AAAA/dominique/RE/regexp1.pl
@@ -0,0 +1,31 @@
+
+
+extends=/ComputerScience/python/AP1-2122/template/pltest2023.pl
+
+
+titre = la Méthode match du module re
+
+text==
+# Sachant que 
+
+|Méthode/Attribut|Objectif|
+|---------------|-------------|
+|match()|Détermine si la RE fait correspond dès le début de la chaîne.|
+|search()| Analyse la chaîne à la recherche d'une position où la RE correspond.|
+|findall()|Trouve toutes les sous-chaînes qui correspondent à la RE et les renvoie sous la forme d'une liste.|
+|finditer()|Trouve toutes les sous-chaînes qui correspondent à la RE et les renvoie sous la forme d'un itérateur.|
+
+Ecrire une fonction *debut(motif,text)* qui retourne si oui ou non le motif est présent au debut du text.  
+
+Ne pas oublier de faire l'import du module re !
+
+==
+
+
+pltest==
+>>> debut("[Tt]oto","toto est la") #  [Tt]oto au début de "toto est la" 
+True
+>>> debut("[Tt]oto","Non toto n'est pas la") #  [Tt]oto au début de "Non toto n'est pas la"
+False
+==
+
