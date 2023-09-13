bfaf6f0a8a0d2c579fe3f0e376eed80a8297064d
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-13 10:27:13 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/RE/exemple.py

--- AAAA/dominique/RE/exemple.py
+++ AAAA/dominique/RE/exemple.py
@@ -1,10 +1,7 @@
-
-
-
 import re
 import pathlib
 
-doc = "Commande qui regarde dans le répertoire courrant si il existe un fichier .py qui contient le motif passé en parametre"
+doc = "Commande qui regarde dans le répertoire courrant et les sous dossiers si il existe un fichier .py qui contient le motif passé en parametre"
 
 def run(dir, exp, extension=".py"):
     for file in dir.iterdir():  # Pour chaque fichier dans le répertoire courrant
@@ -19,8 +16,6 @@ def run(dir, exp, extension=".py"):
                     print("found", file.name)  # Affiche le nom du fichier
                     print(result)
 
-
 if __name__ == "__main__":
     exp =  re.compile(input("Motif à rechercher : "))
     run(pathlib.Path.cwd(),exp)
-
