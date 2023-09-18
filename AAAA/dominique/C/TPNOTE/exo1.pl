e3949253d03be4b83eaec693bffb9af4f96aba9a
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-18 13:52:32 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/TPNOTE/exo1.pl

---
+++ AAAA/dominique/C/TPNOTE/exo1.pl
@@ -0,0 +1,110 @@
+
+
+
+
+author= DR
+
+# Heritage d'un type d'exercice 
+extends=/python/AP1-2122/template/pltest2023.pl
+
+tag=InstructionsRepetitives|ForInRange
+
+title = Analyse d'une suite Finie d'entiers
+
+
+text==  
+
+Votre programme doit lire une suite d'entiers positifs et s'arreter sur le premier entier négatif ou nul. 
+
+Puis doit afficher "croissante", "décroissante","constante", "indéterminée" en fonction de la proprété correspondante 
+de la suite des entiers possitifs lus.
+
+==
+
+code==
+
+CROISSANTE = "croissante"
+DECROISSANTE= "décroissante"
+CONSTANTE= "constante"
+INDETERMINEE = "indéterminée"
+
+==
+
+before==
+import random
+N=random.randint(4,10)
+mplsoluce4="Test aléatoire\n"
+v=40
+for i in range(N):
+    v += random.randint(1,6)
+    mplsoluce4 += str(v)+"\n"
+mplsoluce4 += str(v-10)+"\n"
+==
+
+# dr soluce qui marche avec des chaines ;)
+soluce==
+nb=1
+prev=input("Valeur :")
+next=input("Valeur :")
+while prev<next:
+    prev,next = next, input("Valeur :")
+    nb+=1
+
+print("Vous avez entré",nb,"valeur(s) en ordre croissant")
+==
+
+
+zipsoluce==
+nb=1
+prec=int(input())
+suiv=int(input())
+while suiv> prec:
+    nb+=1
+    prec=suiv
+    suiv=int(input())
+print("Vous avez entré",nb,"valeur(s) en ordre croissant")
+==
+mplsoluce0==
+5 Valeurs
+2 
+3
+5
+14
+77
+0
+==
+mplsoluce1==
+Une seul
+4
+1
+==
+mplsoluce2==
+Plateau
+4
+4
+1
+==
+
+mplsoluce3==
+Départ négatif
+-4
+4
+2
+2
+==
+
+
+mplsoluce5==
+Des chaines
+annanas
+bannana
+papel
+mango
+==
+
+
+
+
+
+
+
