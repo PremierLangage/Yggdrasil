cb462b79c7310e30adaf850e2172f83115f37609
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-19 11:23:49 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/TPNOTE/facile2.pl

---
+++ AAAA/dominique/C/TPNOTE/facile2.pl
@@ -0,0 +1,16 @@
+
+
+author= DR
+extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
+
+
+title = Evaluation d'une fonciton en un point
+
+text==
+
+Soit la fonction de $%\cal{r} \in \cal{r}%$ define par $%f(x) = ax^2+bx+c %$.
+
+Ecrire une fonction $%eval_f(x,a,b,c)%$ qui retourne la valeur de f en x.
+
+
+==
