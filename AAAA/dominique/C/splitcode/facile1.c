fa56ae053b9054b3fc70b207b9eb12778087d7dd
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-19 16:18:27 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/splitcode/facile1.c

---
+++ AAAA/dominique/C/splitcode/facile1.c
@@ -0,0 +1,71 @@
+
+
+
+/* PL:title= Facile  */
+/* PL:text==
+
+Ecrire une fonction **eval_f** qui  prend en paramètre un flottant et 3 entiers et qui retourne un flottant.
+ 
+
+Votre fonction **eval_f** calcule la valeur de la fonction $% f(x) = ax^2+b^2x+c^3%$ pour x = x0.
+
+PL:== */
+
+
+// PL:code_before==
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// PL:==
+// PL:solution==
+
+float exo(float x, int a , int b , int c){
+    return a*x*x+b*b*x+c*c*c;
+}
+
+// PL:==
+
+/* PL:code==
+void exo(){
+    
+printf("Plus longue partie constante %d\n",...);
+printf("Plus longue partie croissante %d\n",...);
+
+}
+
+PL:== */
+
+// PL:code_after==
+
+
+void dotest(){
+    int e,f,g;
+    float j;
+    e =rand() % 100;
+    f =rand() % 100;
+    g =rand() % 10;
+    j = 2.0 * (rand()% 100);
+    printf("exo(%.2f,%d,%d,%d) = %.2f\n",j,e,f,g,exo(j,e,f,g));
+}
+
+
+int main(int c, char **v){
+    srand(atoi(v[1]));
+    printf("exo(1.0,1,1,1) = %.2f\n",exo(1.0,1,1,1));
+    printf("exo(2.0,1,2,3) = %.2f\n",exo(2.0,1,2,3));
+    dotest();
+    dotest();
+    dotest();
+    dotest();
+    return 0;
+}
+
+
+// PL:==
+
+/* PL:checks_args_stdin==
+[["Test 1", [11], "3 4 5 6 7 7 67 88 263 -2"],
+["Test <<<<<Décroissant>>>>>", [], "263 134 121 111 98 87 65 43 21 -2"],
+]
+PL:== */
