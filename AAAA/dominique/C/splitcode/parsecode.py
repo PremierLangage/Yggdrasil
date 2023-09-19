01adb49bb2fe1c95a62316ab2e024065afc5caed
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-19 15:00:11 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/splitcode/parsecode.py

--- AAAA/dominique/C/splitcode/parsecode.py
+++ AAAA/dominique/C/splitcode/parsecode.py
@@ -1,4 +1,18 @@
-
+#*****************************************************************************
+#  Copyright (C) 2023 Dominique Revuz < dominique dot revuz at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
 
 # copyright dominique  Revuz 2023 
 
@@ -51,3 +65,4 @@ if __name__ == "__main__":
     else:
         print("Usage: python3 parseccode.py <file>")
         sys.exit(1)
+
