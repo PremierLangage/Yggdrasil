058b7ec290b7cf554b17b79662bf58d2a0e82de2
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-09-19 10:57:07 +0200

    drevuz2 modified Yggdrasil/AAAA/dominique/C/TPNOTE/facile1.pl

--- AAAA/dominique/C/TPNOTE/facile1.pl
+++ AAAA/dominique/C/TPNOTE/facile1.pl
@@ -5,79 +5,27 @@ extends=/ComputerScience/python/AP1-1920/templates/pltest.pl
 
 tag=tpnote
 
-title = Analyse d'une suite Finie d'entiers
+title = Enumerer des entiers
 
+text=
+ftext==
 
-@ truc.py 
+Votre programme lit deux entiers *a*,*b*.
+Si *a* est plus grand que *b* les deux entiers sont échangés.
 
-text==  
+Puis affiche pour chaque entier x de l'interval $%[a,b[%$ (fermé a gauche ouvert à droite, c-a-d, a dans l'ensemble et b ne l'est pas).  
 
-Votre programme doit lire une suite d'entiers positifs et s'arreter sur le premier entier négatif ou nul. 
-
-Puis doit afficher le nombre d'élèments de la plus grande section constante (la même valeur) par exemple 0 si il sont tous différents.
-Puis doit afficher le nombre d'élèments de la plus grande section croissante (la même valeur) par exemple 0 si la suite est purement décroissante ou constante.
-
-Exemple:
-3 3 12 -1
-
-Affiche "vide" si le premier élément est négatif ou nul.
-==
-
-code==
-
-lcz, ldc = 0,0
-
-print(f"Plus longue partie constante {lcz}")
-print(f"Plus longue partie croissante {ldc}")
-
-
-==
-
-soluce==
-
-lcz, ldc = 0, 0
-nlcz, nldc=0, 0
-
-
-l = int(input())
-if l <=0:
-    print("vide")
-else:
-    while True:
-        n = int(input())
-        if n <= 0:
-            break
-        if n >l:
-            nldc += 1
-            if nldc > ldc:
-                ldc = nldc 
-        else:
-            nldc = 0
-        if n == l:
-            nlcz+= 1
-            if nlcz > lcz :
-                lcz = nlcz
-        else:
-            nlcz=0
-        l = n
-    print(f"Plus longue partie constante {lcz}")
-    print(f"Plus longue partie croissante {ldc}")
-
-
+Si x est divisible par {div1} affiche {txt1}   
+si x est divisible par {div2} affiche {txt2}    
+si x est divisible par  {div1} **et** {div2} affiche "KOuak"   
+SINON affiche x !
 
 ==
 
 
-before==
-import truc
-for i,t in enumerate(truc.mpsolucesplateau()):
-    globals()[f"mplsoluce{i+1}"]= t
+before =
 
-mplsoluce0="""vide
--1
-"""
+text = ftext.format(div1=7, div2=13, txt1="Fizz",txt2="Buzz")
 
 ==
-
-
 
