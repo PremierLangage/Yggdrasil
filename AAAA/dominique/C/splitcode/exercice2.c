ab0f02c5bed32ed54c18469a968afba028a51a25
Olivier BOUILLOT <olivier.bouillot@univ-eiffel.fr>
2023-09-20 17:02:50 +0200

    obouillot1 modified Yggdrasil/AAAA/dominique/C/splitcode/exercice2.c

--- AAAA/dominique/C/splitcode/exercice2.c
+++ AAAA/dominique/C/splitcode/exercice2.c
@@ -10,22 +10,22 @@ Votre fonction **suite** doit lire une suite d'entiers positifs et s'arreter sur
 
 Elle affichera "Vide" si le premier élément saisie est négatif ou nul et s'arrêtera à ce stade.
 
-Sinon, elle affichera ensuite la longueur de la plus grande section constante (*i.e.* $0$ si il sont tous différents, 
-$1$ si elle contient au plus deux éléments consécutifs sont égaux, etc.)
+Sinon, elle affichera ensuite la longueur de la plus grande section constante (*i.e.* 0 si il sont tous différents, 
+1 si elle contient au plus deux éléments consécutifs sont égaux, etc.)
 
-Enfin, elle affichera la longueur de la plus longue section croissante (*i.e.* $0$ si la suite est purement
-décroissante ou constante, $1$ s'il y a exactement au plus deux éléments rangés par ordre croissants, etc.)
+Enfin, elle affichera la longueur de la plus longue section croissante (*i.e.* 0 si la suite est purement
+décroissante ou constante, 1 s'il y a exactement au plus deux éléments rangés par ordre croissants, etc.)
 Ici, les inégalités seront prises au sens large.
 <br />
 <br />
 
 **Exemple 1 :**   
-La saisie de $3$ $3$ $12$ $-1$ affichera :
+La saisie de 3 3 12 -1 affichera :
 Plus longue partie constante : 1
 Plus longue partie croissante : 2
 
 **Exemple 2 :**   
-La saisie de $1$ $3$ $12$ $-1$ affichera :
+La saisie de 1 3 12 -1 affichera :
 Plus longue partie constante : 0
 Plus longue partie croissante : 2
 PL:== */
