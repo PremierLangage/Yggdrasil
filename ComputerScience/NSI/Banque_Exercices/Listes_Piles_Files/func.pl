1cdf8a3e836a99a9184eb048d5d50695a8f3c248
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-09-25 09:20:28 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/func.pl

---
+++ ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/func.pl
@@ -0,0 +1,26 @@
+extends = /ComputerScience/NSI/templates/pltest/pltest2023.pl
+
+title= Distribution 
+
+text==
+
+
+Ecrire une fonction **{{funcname}}** qui vérifie le doctest suivant :
+
+{{doctest}}
+
+
+==
+
+befor ==
+
+==
+
+editor.code==
+==
+
+pltest==
+>>> deal([],3)
+>>> deal([(1,2),(3,4)],1)
+[(1,2)],[(3,4)]
+==
