dc519e4061b9fe8a5790e5e7a39a8f9ad73c95ea
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-09-25 09:38:34 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/pile_vide.pl

--- ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/pile_vide.pl
+++ ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/pile_vide.pl
@@ -5,7 +5,7 @@ title= Initiation d'une pile
 
 doctest==
 
-    la fonction pile_vide_a(n) retourne une pile avec n fois le caractere 'a'
+    la fonction pile_vide_a(n) retourne une pile de n fois le caractere 'a'
     exemple:
     >>> afficher(pile_vide_a(5))
     |a|a|a|a|a
