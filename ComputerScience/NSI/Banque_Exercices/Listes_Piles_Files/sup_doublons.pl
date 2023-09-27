f6d62a1858da57793b7663246d090d771d2bc7d4
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-09-27 15:37:14 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/sup_doublons.pl

--- ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/sup_doublons.pl
+++ ComputerScience/NSI/Banque_Exercices/Listes_Piles_Files/sup_doublons.pl
@@ -19,7 +19,7 @@ taboo = len|append|pop
 pltest==
 >>> ma_liste_chainee = [["c", ["c", ["c", ["c", ["o", ["o", ["c", ["a", None]]]]]]]]] #
 >>> sup_doublons(ma_liste_chainee) # Ne renvoie rien
->>> afficher(ma_liste_chainee) # liste modifiee
+>>> afficher(ma_liste_chainee) # liste chainee modifiee
 |c|*|-->|o|*|-->|c|*|-->|a|None|
 ==
 
