411c789c9a02fbcecf1d7bcd7daa30de0474bb2f
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-10-02 09:21:00 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/2_fonctions/oppose.pl

---
+++ ComputerScience/NSI/Banque_Exercices/2_fonctions/oppose.pl
@@ -0,0 +1,33 @@
+extends = /ComputerScience/NSI/templates/pltest/pltest2023.pl
+
+title= Opposé 
+
+text==
+Ecrire une fonction **oppose** qui vérifie le doctest suivant :
+
+    Cette fonction renvoie l'opposé de nombre (int ou float) passé en paramètre
+
+    >>> oppose(8)
+    -8
+    >>> oppose(-123)
+    123
+
+
+==
+
+befor ==
+==
+
+
+editor.code==
+==
+
+pltest==
+>>> oppose(8)
+-8
+>>> oppose(-123)
+123
+>>> oppose(0)
+0
+==
+
