ab5549f70928cee15caf3a45c8ac10a1f0b0e788
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-10-04 12:28:57 +0200

    drevuz2 modified Yggdrasil/AAAA/ThomasSaillard/EmbeddedDB/showcase/DRMatchList.pl

--- AAAA/ThomasSaillard/EmbeddedDB/showcase/DRMatchList.pl
+++ AAAA/ThomasSaillard/EmbeddedDB/showcase/DRMatchList.pl
@@ -23,7 +23,7 @@ matches = "\n".join(random.sample(matcl,8))
 ==
 
 
-Xmatches==
+matches==
 5/3,1.6666666666666667
 3+5,8
 5**3,125
@@ -33,7 +33,7 @@ Xmatches==
 3**5,243
 ==
 
-matches==
+Xmatches==
 ont besoin d’entraînement, exercice répétable aléatoires
 n'apprennent pas le cours, Quizz pré-cours
 s'ennuient, Activité hors programme 
