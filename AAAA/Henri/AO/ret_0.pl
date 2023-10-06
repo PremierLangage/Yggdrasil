cebb411548ff0a1d2c5e6be9fdeae07f5da787d3
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2023-10-06 11:02:46 +0200

    hderycke1 modified Yggdrasil/AAAA/Henri/AO/ret_0.pl

--- AAAA/Henri/AO/ret_0.pl
+++ AAAA/Henri/AO/ret_0.pl
@@ -1,2 +1,40 @@
 extends=./Vide.pl
 
+author=Henri Derycke
+
+title= Exemple d'assembleur
+
+text==#|markdown|
+Écrire un programme qui ne fait rien
+==
+
+editor.code==#|asm|
+section .text
+global _start
+
+_start:
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+==
+
+student_source = main
+
+# Correction
+sources.main==#|asm|
+section .text
+global _start
+
+_start:
+    mov eax, 1
+    mov ebx, 0
+    int 0x80
+
+==
+
+checks_args_stdin==#|python|
+[
+    ["Test basique", [], ""],
+]
+==
