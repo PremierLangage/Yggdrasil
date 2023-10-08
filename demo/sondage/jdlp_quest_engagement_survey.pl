bbb91ab376d2ab7606c45a7ae22d0abebfdc1b31
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-10-08 16:30:20 +0200

    nborie1 modified Yggdrasil/demo/sondage/jdlp_quest_engagement_survey.pl

--- demo/sondage/jdlp_quest_engagement_survey.pl
+++ demo/sondage/jdlp_quest_engagement_survey.pl
@@ -51,7 +51,7 @@ question2==
 
 items2==
 Les élèves ne sont pas toujours disponibles (transports, travail à coté de l'activité étudiante, contraintes fammilliales ou autres contraintes externes).
-Les jeunes ne trouvent pas ce qu'ils sont venus chercher. Ils prétendent que les situations professionelles des métiers finaux préparés sont décorrélées des activités pédagogiques de la formation.
+Les jeunes ne trouvent pas ce qu'ils sont venus chercher. Ils prétendent que les situations professionelles des <br />métiers finaux préparés sont décorrélées des activités pédagogiques de la formation.
 Les élèves ne sont pas à leur place, il y a une inadéquation entre leurs profils et la formation qu'ils suivent.
 Les élèves n'ont pas suffisement envie de réussir. Ils manquent de motivation et finissent par décrocher.
 Les contenus ne sont plus adaptés. Les jeunes ont évolués trop vite par rapport aux formations.
