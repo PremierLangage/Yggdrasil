804556d7888343b3ac74c817fbf4ed6089033782
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-10-09 17:43:40 +0200

    nborie1 modified Yggdrasil/ComputerScience/nasm/test_nasm.pl

--- ComputerScience/nasm/test_nasm.pl
+++ ComputerScience/nasm/test_nasm.pl
@@ -51,21 +51,21 @@ f.close()
 
 process = subprocess.run(['nasm', '-f', 'elf64', 'hello_world.asm', '-o', 'hello.o'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout = process.stdout
-print(stdout)
+text += stdout
 
 ans = "compilation avec nasm : " + stdout.decode()
 ans += "\n<br>"
 
 process = subprocess.run(['ld', 'hello.o', '-o', 'hello_w'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout = process.stdout
-print(stdout)
+text += stdout
 
 ans += "linkage avec ld : " + stdout.decode()
 ans += "\n<br>"
 
 process = subprocess.run(['./hello_w'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout = process.stdout
-print(stdout)
+text += stdout
 
 ans += "exécution : " + stdout.decode()
 
