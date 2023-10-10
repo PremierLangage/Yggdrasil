b8f6dd0d80fd346a2fad6a298aa0ebc6ed45a524
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-10-10 09:24:04 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/1_classe_cellule.pl

--- ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/1_classe_cellule.pl
+++ ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/1_classe_cellule.pl
@@ -5,12 +5,18 @@ title= Classe Cellule
 
 text==
 
-Ecrire la classe `Cellule` ayant deux attributs : 
+Ecrire la classe `Cellule` ayant 4 attributs : 
 
 - `vivant` de type booléen correspondant à l'état de la cellule
 - `futur` de type booléen correspondant à l'état suivant de la cellule après calcul.
+- `i` son numéro de ligne
+- `j` son numéro de ligne
 
-Son constructeur aura un seul paramètre l'état initial de la cellule. L'attribut `futur`sera mis à None initialement
+Son constructeur aura trois paramètres:
+- l'état initial de la cellule. 
+- son numéro de ligne et son numéro de colonne.
+
+L'attribut `futur` sera mis à None initialement
 
 On créera une cellule avec le code suivant:
 
@@ -28,18 +34,22 @@ editor.code==
 ==
 
 pltest==
->>> cell = Cellule(True)
+>>> cell = Cellule(True, 0, 0)
 >>> cell.vivant
 True
->>> cell2 = Cellule(False)
+>>> cell2 = Cellule(False, 1, 3)
 >>> cell2.vivant
 False
 >>> cell2.futur
+>>> cell2.i
+1
 ==
 
 soluce==
 class Cellule:
-    def __init__(self, etat):
+    def __init__(self, etat, i, j):
         self.vivant = etat
         self.futur = None
+        self.i = i
+        self.j = j
 ==
