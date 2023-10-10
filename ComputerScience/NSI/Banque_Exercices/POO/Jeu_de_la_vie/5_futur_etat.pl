f1bba36dec6b2e5c5957c35953f710d816979e69
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-10-10 08:59:08 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/5_futur_etat.pl

---
+++ ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/5_futur_etat.pl
@@ -0,0 +1,80 @@
+extends = /ComputerScience/NSI/templates/pltest/pltest2023.pl
+
+
+title= Méthode futur état
+
+text==
+
+Ecrire la méthode `futur_etat` de la classe `Cellule` ayant comme 
+paramètre le nombre de ses voisins et modifiant son attribut `futur`en suivant
+la règle suivante :
+
+- une cellule morte possédant exactement trois voisines vivantes devient vivante : elle naît ;
+- une cellule vivante possédant deux ou trois voisines vivantes le reste, sinon elle meurt.
+
+On pourra bien entendu utiliser les méthodes précédentes.
+
+    >>> cell.vivant
+    False
+    >>> cell.futur_etat(3)
+    >>> cell.futur
+    True
+
+==
+
+befor ==
+import os
+import time
+
+class Cellule:
+    def __init__(self, etat):
+        self.vivant = etat
+        self.futur = None
+
+class Jeu_de_la_vie:
+    def __init__(self, taille, liste_vivantes):
+        self.taille = taille
+        self.plateau = [[Cellule(False) for i in range(taille)] for i in range(taille)]
+        for i, j in liste_vivantes:
+            self.plateau[i][j].vivant = True
+            
+    def voisines(self, i, j):
+        lst = [(i - 1, j - 1), (i, j - 1), (i + 1, j - 1), (i - 1, j), (i + 1, j), (i - 1, j + 1), (i, j + 1), (i + 1, j + 1)]
+        res = []
+        for x, y in lst:
+            if x >= 0 and x < self.taille and y >= 0 and y < self.taille:
+                res.append(self.plateau[x][y])
+        return res
+
+    def nb_voisines(self, i, j):
+        cpt = 0
+        for cell in self.voisines(i, j):
+            if cell.vivant:
+                cpt += 1
+        return cpt
+==
+
+
+editor.code==
+
+==
+
+pltest==
+>>> Jeu_de_la_vie.nb_voisines = nb_voisines #
+>>> jeu = Jeu_de_la_vie(7, [(3, 4), (4, 4), (5, 4)]) #
+>>> jeu.nb_voisines(0,0)
+0
+>>> jeu.nb_voisines(4,3)
+3
+==
+
+soluce==
+def nb_voisines(self, i, j):
+    cpt = 0
+    for cell in self.voisines(i, j):
+        if cell.vivant:
+            cpt += 1
+    return cpt
+==
+
+
