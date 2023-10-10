d8113fee29cfce24131902ad3ce2fbe0189e7058
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-10-10 08:45:37 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/4_nb_voisines.pl

---
+++ ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/4_nb_voisines.pl
@@ -0,0 +1,64 @@
+extends = /ComputerScience/NSI/templates/pltest/pltest2023.pl
+
+
+title= Méthode nombre de voisines
+
+text==
+
+Ecrire la méthode `nb_voisines` de la classe `Jeu_de_la_vie` ayant comme 
+paramètres `i` le numéro de ligne et `j` le numéro de colonne et 
+renvoyant le nombre de cellules vivantes voisines.
+
+On pourra bien entendu utiliser les méthodes précédentes.
+
+    >>> jeu.nb_voisines(0,0)
+    0
+
+==
+
+befor ==
+import os
+import time
+
+class Cellule:
+    def __init__(self, etat):
+        self.vivant = etat
+
+class Jeu_de_la_vie:
+    def __init__(self, taille, liste_vivantes):
+        self.taille = taille
+        self.plateau = [[Cellule(False) for i in range(taille)] for i in range(taille)]
+        for i, j in liste_vivantes:
+            self.plateau[i][j].vivant = True
+    def voisines(self, i, j):
+        lst = [(i - 1, j - 1), (i, j - 1), (i + 1, j - 1), (i - 1, j), (i + 1, j), (i - 1, j + 1), (i, j + 1), (i + 1, j + 1)]
+        res = []
+        for x, y in lst:
+            if x >= 0 and x < self.taille and y >= 0 and y < self.taille:
+                res.append(self.plateau[x][y])
+        return res
+==
+
+
+editor.code==
+
+==
+
+pltest==
+>>> Jeu_de_la_vie.voisines = voisines #
+>>> jeu = Jeu_de_la_vie(7, [(3, 4), (4, 4), (5, 4)]) #
+>>> jeu.nb_voisines(0,0)
+0
+>>> jeu.nb_voisines(4,3)
+3
+==
+
+soluce==
+def nb_voisines(self, i, j):
+    cpt = 0
+    for cell in self.voisines(i, j):
+        if cell.vivant:
+            cpt += 1
+    return cpt
+==
+
