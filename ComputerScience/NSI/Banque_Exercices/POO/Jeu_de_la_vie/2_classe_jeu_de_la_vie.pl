8edfd9f19ec4c67c84184f69898b329490f66194
EMMANUEL BRUNACHE <47db9ead-8097-498a-8a8e-518dec217c11@moodle.net>
2023-10-10 09:24:56 +0200

    ebrunache2 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/2_classe_jeu_de_la_vie.pl

--- ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/2_classe_jeu_de_la_vie.pl
+++ ComputerScience/NSI/Banque_Exercices/POO/Jeu_de_la_vie/2_classe_jeu_de_la_vie.pl
@@ -68,7 +68,7 @@ soluce==
 class Jeu_de_la_vie:
     def __init__(self, taille, liste_vivantes):
         self.taille = taille
-        self.plateau = [[Cellule(False) for i in range(taille)] for i in range(taille)]
+        self.plateau = [[Cellule(False, i, j) for j in range(taille)] for i in range(taille)]
         for i, j in liste_vivantes:
             self.plateau[i][j].vivant = True
 ==
