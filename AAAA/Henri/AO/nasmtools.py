a22fd09886226e112d7ec766e7d46416fa599dda
Henri DERYCKE <henri.derycke@univ-eiffel.fr>
2023-10-12 08:54:23 +0200

    hderycke modified Yggdrasil/AAAA/Henri/AO/nasmtools.py

--- AAAA/Henri/AO/nasmtools.py
+++ AAAA/Henri/AO/nasmtools.py
@@ -130,15 +130,14 @@ class Program:
     def run(self, argv):
         command_args = ['./' + self.name] + argv
         sp = subprocess.run(command_args, stdin=open("stdin_content", "r"), stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=1)
-        output = ""
         try: 
-            ouput += sp.stdout.decode() + sp.stderr.decode()
+            output = sp.stdout.decode() + sp.stderr.decode()
         except:
-            ouput += "Impossible de décoder la sortie standard"
+            output = "Impossible de décoder la sortie standard"
         if -sp.returncode in signals:
-            ouput += "Process exited with UNIX signal ("+str(-sp.returncode)+") "+signals[-sp.returncode]
+            output += "Process exited with UNIX signal ("+str(-sp.returncode)+") "+signals[-sp.returncode]
         elif sp.returncode < 0:
-            ouput += "Process exited with UNIX signal ("+str(-sp.returncode)+")"
+            output += "Process exited with UNIX signal ("+str(-sp.returncode)+")"
         else:
             output += "Process exited with return code " + str(sp.returncode)
-        return ouput
+        return output
