0760708960ed271570850591231ff824ec583bbf
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-10-21 00:09:16 +0200

    nborie1 modified Yggdrasil/ComputerScience/logic/proof/implication.pl

--- ComputerScience/logic/proof/implication.pl
+++ ComputerScience/logic/proof/implication.pl
@@ -92,8 +92,8 @@ for item in group.items:
 
 if score == 0:
     feedback += '<br />'
-    for k in names:
-        feedback = feedback + '<br /> ' + names[k] + ' était ' + theo_dict[k] + '.<br />'
+    for k in list(names.keys()):
+        feedback += '<br /> ' + names[k] + ' était ' + theo_dict[k] + '.<br />'
 
 
 grade = (score, feedback)
