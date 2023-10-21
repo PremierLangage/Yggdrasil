7ae66af2cecee9d2a37fe74f0d760169855ee7cf
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-10-21 11:25:08 +0200

    nborie1 modified Yggdrasil/ComputerScience/logic/proof/reciproque.pl

---
+++ ComputerScience/logic/proof/reciproque.pl
@@ -0,0 +1,70 @@
+#*****************************************************************************
+#  Copyright (C) 2023 Nicolas Borie <nicolas dot borie at univ-eiffel . fr>
+#
+#  Distributed under the terms of Creative Commons Attribution-ShareAlike 3.0
+#  Creative Commons CC-by-SA 3.0
+#
+#    This code is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+#  The full text of the CC-By-SA 3.0 is available at:
+#
+#            https://creativecommons.org/licenses/by-sa/3.0/
+#            https://creativecommons.org/licenses/by-sa/3.0/fr/
+#*****************************************************************************
+
+@ /utils/sandboxio.py
+
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+group =: CheckboxGroup
+
+# GENERATE A RANDOM QUESTION
+before==
+import random
+group.items = []
+for i in range(4):
+    group.items.append({
+        "id": str(i),
+        "content": str(random.randint(0, 100))
+    })
+==
+
+author=Nicolas Borie
+
+title = Checkbox Group Component
+
+text==
+Select even numbers.
+==
+
+# PRESENT THE QUESTION TO THE STUDENT
+form==
+{{ group|component }}
+==
+
+# EVALUATE THE STUDENT ANSWER
+evaluator==
+right = 0
+total = 0
+for item in group.items:
+    checked = item['checked']
+    content = int(item['content'])
+    if content % 2 == 0:
+        total += 1
+        item['css'] = 'success-border animated pulse infinite'
+        if checked:
+            right += 1
+            item['css'] = 'success-border'
+    elif checked:
+        item['css'] = 'error-border'
+
+
+if total == 0:
+    grade = (100, 'Right')
+else:
+    grade = ((right / total) * 100, f"{right} / {total}")
+==
+
