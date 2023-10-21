bec5df87d3f83433d304ba8026287824ed57070e
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-10-21 17:41:18 +0200

    nborie1 modified Yggdrasil/ComputerScience/logic/proof/implication.pl

--- ComputerScience/logic/proof/implication.pl
+++ ComputerScience/logic/proof/implication.pl
@@ -27,31 +27,31 @@ import random
 all_theo =[{
     'contexte' : "Soit $%p%$ et $%q%$ deux propositions logiques.",
     'théorème' : "$%p \implies q%$",
-    'réciproque du théoème' : "$%q \implies p%$",
+    'réciproque du théorème' : "$%q \implies p%$",
     'contraposée du théorème' : "$% \lnot q \implies \lnot p %$",
     'contraposée de la réciproque du théorème' : "$% \lnot p \implies \lnot q %$"
 },{
     'contexte' : "Soit $%A%$ et $%B%$ deux propositions logiques.",
     'théorème' : "$%B \implies A%$",
-    'réciproque du théoème' : "$%A \implies B%$",
+    'réciproque du théorème' : "$%A \implies B%$",
     'contraposée du théorème' : "$% \lnot A \implies \lnot B %$",
     'contraposée de la réciproque du théorème' : "$% \lnot B \implies \lnot A %$"
 },{
     'contexte' : "Soit $%ABC%$ un triangle donc la mesure des cotés opposés aux trois sommets sont $%a, b%$ et $%c%$." ,
     'théorème' : "Si Le triangle est rectangle en C, alors $% a^2 + b^2 = c^2%$",
-    'réciproque du théoème' : "Si $% a^2 + b^2 = c^2%$, alors Le triangle est rectangle en C",
+    'réciproque du théorème' : "Si $% a^2 + b^2 = c^2%$, alors Le triangle est rectangle en C",
     'contraposée du théorème' : "Si $% a^2 + b^2 \\neq c^2%$, alors Le triangle n'est pas rectangle en C",
     'contraposée de la réciproque du théorème' : "Si Le triangle n'est pas rectangle en C, alors $% a^2 + b^2 \\neq c^2%$ "
 },{
     'contexte' : "Soit $%5%$ points du plan tel que les triplets $%O, A, B%$ et $%O, P, Q%$ soient alignés (c'est à dire une configuration de type Thalès).",
     'théorème' : "Si les droites $%(AP)%$ et $%(BQ)%$ sont parallèles, alors on a l'égalité des rapports $% \\frac{OA}{OP} = \\frac{OB}{OQ} = \\frac{AB}{PQ} %$",
-    'réciproque du théoème' : "Si on a l'égalité des rapports $% \\frac{OA}{OP} = \\frac{OB}{OQ} = \\frac{AB}{PQ}%$, alors les droites $%(AP)%$ et $%(BQ)%$ sont parallèles",
+    'réciproque du théorème' : "Si on a l'égalité des rapports $% \\frac{OA}{OP} = \\frac{OB}{OQ} = \\frac{AB}{PQ}%$, alors les droites $%(AP)%$ et $%(BQ)%$ sont parallèles",
     'contraposée du théorème' : "Si on n'a pas l'égalité des rapports $% \\frac{OA}{OP} = \\frac{OB}{OQ} = \\frac{AB}{PQ}%$, alors les droites $%(AP)%$ et $%(BQ)%$ ne peuvent pas être parallèles",
     'contraposée de la réciproque du théorème' : "Si les droites $%(AP)%$ et $%(BQ)%$ ne sont pas parallèles, alors on ne peut pas avoir l'égalité des rapports $% \\frac{OA}{OP} = \\frac{OB}{OQ} = \\frac{AB}{PQ} %$"
 },{
     'contexte' : "Soit $%n%$ un entier n'aturel, on s'intéresse à la parité de $%n%$.",
     'théorème' : "Si $%n%$ est pair, alors le reste de la division euclidienne de $%n%$ par $%2%$ est $%0%$",
-    'réciproque du théoème' : "Si le reste de la division euclidienne de $%n%$ par $%2%$ est $%0%$, alors $%n%$ est pair",
+    'réciproque du théorème' : "Si le reste de la division euclidienne de $%n%$ par $%2%$ est $%0%$, alors $%n%$ est pair",
     'contraposée du théorème' : "Si le reste de la division euclidienne de $%n%$ par $%2%$ n'est pas $%0%$, alors $%n%$ n'est pas pair",
     'contraposée de la réciproque du théorème' : "Si $%n%$ n'est pas pair, alors le reste de la division euclidienne de $%n%$ par $%2%$ ne peut pas être $%0%$"
 }
@@ -59,7 +59,7 @@ all_theo =[{
 
 names={
     'théorème' : "l'énoncé du théorème",
-    'réciproque du théoème' : "la réciproque du théorème",
+    'réciproque du théorème' : "la réciproque du théorème",
     'contraposée du théorème' : "la contraposée du théorème",
     'contraposée de la réciproque du théorème' : "la contraposée de la réciproque du théorème"
 }
@@ -67,7 +67,7 @@ names={
 theo_dict = random.choice(all_theo)
 prev = theo_dict['contexte']
 th = theo_dict['théorème']
-rt = theo_dict['réciproque du théoème']
+rt = theo_dict['réciproque du théorème']
 ct = theo_dict['contraposée du théorème']
 cr = theo_dict['contraposée de la réciproque du théorème']
 
