3f809581fb42554f3eff49436aa2a434052c5357
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-10-23 15:52:45 +0200

    drevuz2 modified Yggdrasil/ComputerScience/python/AP1-2021/Chapitre_4_fonctions/10_mais_pas_3.pl

--- ComputerScience/python/AP1-2021/Chapitre_4_fonctions/10_mais_pas_3.pl
+++ ComputerScience/python/AP1-2021/Chapitre_4_fonctions/10_mais_pas_3.pl
@@ -36,7 +36,7 @@ for i in range(4):
 True
 """
 
-code += f"""def oui_mais(n):\n return ((17 % n ==0) and (17 % n != 0))\n"""
+code += f"""def oui_mais(n):\n return ((n % {nb1} ==0) and (nombres % {nb2} != 0))\n"""
 
 
 
