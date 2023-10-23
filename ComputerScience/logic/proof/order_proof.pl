8db5e22711afad9b17781dafaeecaeda486dad86
Nicolas BORIE <nicolas.borie@univ-eiffel.fr>
2023-10-23 18:02:36 +0200

    nborie1 modified Yggdrasil/ComputerScience/logic/proof/order_proof.pl

--- ComputerScience/logic/proof/order_proof.pl
+++ ComputerScience/logic/proof/order_proof.pl
@@ -29,7 +29,7 @@ sortlist.items %= #|json|
     { "id": "1", "content": "Dans tout triangle, la somme de la mesure des angles est 180 degrés."},
     { "id": "2", "content": "Appliqué au triangle ABC, on en déduit l'équation $% 3x + 2x + x = 180%$."},
     { "id": "3", "content": "Résolvant l'équation, on obtient $%6x = 180%$ et donc $%x = 30%$."},
-    { "id": "4", "content": "Cette valeur de $%x%$ reporté au contexte fait que le triangle $%ABC%$ a un angle de mesure $%3x = 90%$ degrés."},
+    { "id": "4", "content": "Cette valeur de $%x%$ reportée au contexte fait que le triangle $%ABC%$ a un angle de mesure $%3x = 90%$ degrés en $%A%$."},
     { "id": "5", "content": "Conclusion : Le triangle $%ABC%$ est bien rectangle en $%A%$."}
 ]
 ==
