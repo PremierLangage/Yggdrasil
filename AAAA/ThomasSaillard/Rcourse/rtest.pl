bd8b9c29e319b246f0daffe61755410cc66ac89c
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-26 09:35:07 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/rtest.pl

---
+++ AAAA/ThomasSaillard/Rcourse/rtest.pl
@@ -0,0 +1,56 @@
+
+
+@ /utils/sandboxio.py
+grader  =@ /grader/evaluator.py
+builder =@ /builder/before.py
+
+editor =: CodeEditor
+editor.theme = dark
+editor.language = assembly
+editor.code ==
+# Simple Hello World in R
+print("Hello World")
+==
+
+before==
+==
+
+title = Test NASM 
+
+text==
+Mais quelle idée ce NASM sérieux ??? Il faut valider pour compiler avec NASM, puis linker avec LD puis exécuter ce Hello World !
+==
+
+form==
+{{ editor|component }}
+==
+
+evaluator==#|python|
+import subprocess
+
+f = open('hello_world.asm', 'w')
+f.write(editor.code)
+f.close()
+
+process = subprocess.run(['nasm', '-f', 'elf64', 'hello_world.asm', '-o', 'hello.o'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout = process.stdout
+ce = process.returncode
+
+ans = "compilation avec nasm : " + stdout.decode() + "(code Unix de retour : " + str(ce) + ")"
+ans += "\n<br>"
+
+process = subprocess.run(['ld', 'hello.o', '-o', 'hello_w'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout = process.stdout
+ce = process.returncode
+
+ans += "linkage avec ld : " + stdout.decode() + "(code Unix de retour : " + str(ce) + ")"
+ans += "\n<br>"
+
+process = subprocess.run(['./hello_w'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+stdout = process.stdout
+ce = process.returncode
+
+ans += "ex&eacute;cution : " + stdout.decode() + "(code Unix de retour : " + str(ce) + ")"
+
+grade = (100, ans)
+==
