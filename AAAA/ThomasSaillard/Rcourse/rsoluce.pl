cb600a4082e0a1af9466b62da89c9c94c78046b2
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-26 09:59:08 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/rsoluce.pl

---
+++ AAAA/ThomasSaillard/Rcourse/rsoluce.pl
@@ -0,0 +1,6 @@
+
+doc==
+Rsoluce.pl est un modèle permettant d'executer des programmes R.
+
+Le but de ce modèle est de prendre un code `bon` du professeur et de le comparer à celui de l'élève via 
+différents test
