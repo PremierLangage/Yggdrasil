dcd8fba131ee94e7c8d29b44c028a07926214652
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-26 10:09:59 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/rsoluce.pl

--- AAAA/ThomasSaillard/Rcourse/rsoluce.pl
+++ AAAA/ThomasSaillard/Rcourse/rsoluce.pl
@@ -3,4 +3,24 @@ doc==
 Rsoluce.pl est un modèle permettant d'executer des programmes R.
 
 Le but de ce modèle est de prendre un code `bon` du professeur et de le comparer à celui de l'élève via 
-différents test
+différents tests écrits par le professeur.
+
+La seule balise à définir obligatoirement : 
+
+    soluce==
+    Un script R répondant à l'éxercice.
+    ==
+
+Les balises optionnelles : 
+    testX==
+    Nom du test
+    entrée 1
+    entrée 2 
+    entrée 3
+    ==
+
+    Notez que les entrées sont passées au programme par l'entrée standart.
+    Il peut ne pas y avoir d'entrée du tout.
+    Dans le cas où aucun test n'a été défini les deux scripts seront quand même
+    éxécutés et comparés sans aucune entrée.
+
