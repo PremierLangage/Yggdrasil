0a1414da6961fa695ff863d3d7fcd2eb79643e5c
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-27 18:24:55 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/rsoluce.pl

--- AAAA/ThomasSaillard/Rcourse/rsoluce.pl
+++ AAAA/ThomasSaillard/Rcourse/rsoluce.pl
@@ -75,7 +75,7 @@ Les balises optionnelles :
 @ /ComputerScience/python/AP1-1920/templates/utils/template.html
 @ /utils/sandboxio.py
 builder =@ /builder/before.py
-
+grader  =@ /grader/evaluator.py
 
 settings.requirements=title,text,soluce
 
