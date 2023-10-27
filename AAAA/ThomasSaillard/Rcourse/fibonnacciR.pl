dc7415346cbbec71f679a3b5e778cad4baab7a8d
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-27 18:05:22 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/fibonnacciR.pl

--- AAAA/ThomasSaillard/Rcourse/fibonnacciR.pl
+++ AAAA/ThomasSaillard/Rcourse/fibonnacciR.pl
@@ -3,8 +3,15 @@ extends =/AAAA/ThomasSaillard/Rcourse/rsoluce.pl
 
 title= Exercice Fibonnaci en R
 
-test==
+text==
 Le but de cet exercice est d'écrire une fonction `Fibonnaci` en R qui prend en argument un entier
 et renvoie un vecteur ou une liste contenant la suite de Fibonnaci.
+==
+
+editor.code ==#|r|
+Fibonacci <- function(n) {
+
+  }
+}
 ==
 
