7dcb4a1329e4173fe1bb7ab33f1ec5693b99cf2b
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-27 18:29:14 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/rsoluce.pl

--- AAAA/ThomasSaillard/Rcourse/rsoluce.pl
+++ AAAA/ThomasSaillard/Rcourse/rsoluce.pl
@@ -169,6 +169,14 @@ def run_test(test : str, timeout : int = 4, feedback : FeedBack = FeedBack()):
                                 f"exit code: {teacher_exit_code}\n\nstdout:\n{teacher_stdout}\n\nstderr:\n{teacher_stderr}")
 
 
+tags = [i for i in globals() if i.startswith('test_')]
+for tag in tags:
+    if f"timeout_{tag.removeprefix('test_')}" in globals():
+        timeout = globals()[f"timeout_{tag.removeprefix('test_')}"]
+    else: timeout = 4
+    run_test(globals()[tag], timeout, feedback)
+    print(feedback.render())
+
 def default_grade(nb_success, nb_fail_timeout, nb_fail_stdout, nb_fail_stderr, nb_fail_exit_code):
     nb_tests = nb_success + nb_fail_timeout + nb_fail_stdout + nb_fail_stderr + nb_fail_exit_code
     return round(100 * nb_success / nb_tests)
