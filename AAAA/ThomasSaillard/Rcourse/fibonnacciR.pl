704f9965ad81918c6a8e55232d650c7475adef6c
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-27 18:06:03 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/fibonnacciR.pl

--- AAAA/ThomasSaillard/Rcourse/fibonnacciR.pl
+++ AAAA/ThomasSaillard/Rcourse/fibonnacciR.pl
@@ -11,7 +11,29 @@ et renvoie un vecteur ou une liste contenant la suite de Fibonnaci.
 editor.code ==#|r|
 Fibonacci <- function(n) {
 
-  }
 }
 ==
 
+
+soluce ==#|r|
+Fibonacci<- function(n) {
+  if (n <= 0) {
+    return(NULL)  # Return NULL for invalid input
+  } else if (n == 1) {
+    return(0)     # The first Fibonacci number is 0
+  } else if (n == 2) {
+    return(c(0, 1))  # The second Fibonacci number is 1
+  } else {
+    # Initialize the sequence with the first two numbers
+    sequence <- c(0, 1)
+    
+    # Generate the Fibonacci sequence
+    for (i in 3:n) {
+      next_term <- sequence[i - 1] + sequence[i - 2]
+      sequence <- c(sequence, next_term)
+    }
+    
+    return(sequence)
+  }
+}
+==
