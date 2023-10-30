d0bc51a8c621f9c21dad7ee28c20e1671a6c66a8
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-30 17:34:15 +0100

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/FibonacciR.pl

---
+++ AAAA/ThomasSaillard/Rcourse/FibonacciR.pl
@@ -0,0 +1,71 @@
+
+
+extends = rsoluce.pl
+
+title = Exercice Syracuse en R
+
+text==
+Le but de cet exercice est d'écrire une fonction `Syracuse` en R qui prend en argument un entier
+et renvoie un vecteur ou une liste contenant la suite de Syracuse.
+==
+
+editor.code ==#|r|
+Syracuse <- function(n) {
+
+}
+==
+
+code_before==#|r|
+starting_number <- as.integer(readLines(con = file("stdin"), n = 1))
+==
+
+soluce ==#|r|
+Syracuse <- function(n) {
+  # Create an empty vector to store the sequence
+  sequence <- c()
+  
+  # Continue generating the sequence until n becomes 1
+  while (n != 1) {
+    sequence <- c(sequence, n)  # Append the current value to the sequence
+    
+    # Apply the Collatz rules
+    if (n %% 2 == 0) {
+      n <- n / 2
+    } else {
+      n <- 3 * n + 1
+    }
+  }
+  
+  # Append the final value (1) to the sequence
+  sequence <- c(sequence, 1)
+  
+  return(sequence)
+}
+==
+
+code_after==#|r|
+result <- Syracuse(starting_number)
+print(paste("Syracuse sequence for", starting_number, "is:", paste(result, collapse = " -> ")))
+==
+
+test_1==
+Premier élément
+1
+==
+
+
+test_2==
+Entrée invalide
+-1
+==
+
+test_3==
+Suite des premiers 100 éléments
+100
+==
+
+test_4==
+Big test
+500
+==
+
