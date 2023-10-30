673174f063202bc083a7b8770a29d5ff71be06db
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-30 17:35:02 +0100

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/FibonacciR.pl

--- AAAA/ThomasSaillard/Rcourse/FibonacciR.pl
+++ AAAA/ThomasSaillard/Rcourse/FibonacciR.pl
@@ -2,50 +2,51 @@
 
 extends = rsoluce.pl
 
-title = Exercice Syracuse en R
+title = Exercice Fibonacci en R
 
 text==
-Le but de cet exercice est d'écrire une fonction `Syracuse` en R qui prend en argument un entier
-et renvoie un vecteur ou une liste contenant la suite de Syracuse.
+Le but de cet exercice est d'écrire une fonction `Fibonacci` en R qui prend en argument un entier
+et renvoie un vecteur ou une liste contenant la suite de Fibonacci.
 ==
 
 editor.code ==#|r|
-Syracuse <- function(n) {
+Fibonacci <- function(n) {
 
 }
 ==
 
 code_before==#|r|
-starting_number <- as.integer(readLines(con = file("stdin"), n = 1))
+num_terms <- as.integer(readLines(con = file("stdin"), n = 1))
 ==
 
 soluce ==#|r|
-Syracuse <- function(n) {
-  # Create an empty vector to store the sequence
-  sequence <- c()
-  
-  # Continue generating the sequence until n becomes 1
-  while (n != 1) {
-    sequence <- c(sequence, n)  # Append the current value to the sequence
+Fibonacci<- function(n) {
+  if (n <= 0) {
+    return(NULL)  # Return NULL for invalid input
+  } else if (n == 1) {
+    return(0)     # The first Fibonacci number is 0
+  } else if (n == 2) {
+    return(c(0, 1))  # The second Fibonacci number is 1
+  } else {
+    # Initialize the sequence with the first two numbers
+    sequence <- c(0, 1)
     
-    # Apply the Collatz rules
-    if (n %% 2 == 0) {
-      n <- n / 2
-    } else {
-      n <- 3 * n + 1
+    # Generate the Fibonacci sequence
+    for (i in 3:n) {
+      next_term <- sequence[i - 1] + sequence[i - 2]
+      sequence <- c(sequence, next_term)
     }
+    
+
+    return(sequence)
   }
-  
-  # Append the final value (1) to the sequence
-  sequence <- c(sequence, 1)
-  
-  return(sequence)
 }
 ==
 
 code_after==#|r|
-result <- Syracuse(starting_number)
-print(paste("Syracuse sequence for", starting_number, "is:", paste(result, collapse = " -> ")))
+fib_sequence <- Fibonacci(num_terms)
+cat("Fibonacci sequence with", num_terms, "terms: ")
+cat(fib_sequence, sep = ", ")
 ==
 
 test_1==
