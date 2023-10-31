2bd48cbc98538c587b30d178aff9d5c934bb4a4b
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-10-31 11:46:03 +0100

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/Rcourse/sortR.pl

---
+++ AAAA/ThomasSaillard/Rcourse/sortR.pl
@@ -0,0 +1,64 @@
+
+extends = rsoluce.pl
+
+title = Exercice Tri en R
+
+text==
+Le but de cet exercice est d'écrire une fonction `sort` en R qui prend en argument un vecteur d'entiers
+et renvoie un vecteur trié.
+Dans cette exercice la complexité de l'algorithme sera prise en compte
+==
+
+editor.code ==#|r|
+Syracuse <- function(n) {
+
+}
+==
+
+code_before==#|r|
+starting_number <- as.integer(readLines(con = file("stdin"), n = 1))
+==
+
+soluce ==#|r|
+Syracuse <- function(n) {
+  # Create an empty vector to store the sequence
+  sequence <- c()
+  
+  # Continue generating the sequence until n becomes 1
+  while (n != 1) {
+    sequence <- c(sequence, n)  # Append the current value to the sequence
+    
+    # Apply the Collatz rules
+    if (n %% 2 == 0) {
+      n <- n / 2
+    } else {
+      n <- 3 * n + 1
+    }
+  }
+  
+  # Append the final value (1) to the sequence
+  sequence <- c(sequence, 1)
+  
+  return(sequence)
+}
+==
+
+code_after==#|r|
+result <- Syracuse(starting_number)
+print(paste("Syracuse sequence for", starting_number, "is:", paste(result, collapse = " -> ")))
+==
+
+before_tests==#|py|
+test_2 = "Test al&eacute;atoire\n"
+
+from random import randint
+
+test_2 += str(randint(600, 3000))
+test_2 = test_2
+==
+
+test_1==
+Test suite 
+500
+==
+
