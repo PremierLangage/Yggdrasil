e4730b3b2ed9333204727c8f08eb251db5611a45
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-10 14:54:41 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/template/builderror.py

--- AAAA/dominique/python/template/builderror.py
+++ AAAA/dominique/python/template/builderror.py
@@ -5,13 +5,11 @@ def randomText():
     return random.choice(["Hello World","Hello World!","Bienvenu !","Salut","Welcome !","Vitaj !","Üdvözöljük","Καλώς ορίσατε"])
 
 def randomError():
-    return random.choice(["Hello World","Hello World!","Bienvenu !","Salut","Welcome !","Vitaj !","Üdvözöljük","Καλώς ορίσατε"])
+    return random.choice(["SyntaxeError","IndentationError",])
 
 def builderror(type=None):
-    if type:
-        return "pas de code",">>> a=1# Test de compilation\n","Pas d'aide"
     tic = randomText()
-
+    type = randomError()
     if type=="SyntaxeError":
         code = f"""def f():\n   print("{tic}"\nprint()\n"""
         letest= f""">>> f()\n{tic}\n"""
