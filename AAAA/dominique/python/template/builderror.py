16da7c698c0b23c68a55f76aed1c5295ec9e28db
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-10 14:53:12 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/template/builderror.py

--- AAAA/dominique/python/template/builderror.py
+++ AAAA/dominique/python/template/builderror.py
@@ -6,8 +6,14 @@ def randomText():
 
 def builderror(type=None):
     if type:
-        return "pas de code",">>> a=1# Test de compilation\n"
+        return "pas de code",">>> a=1# Test de compilation\n","Pas d'aide"
     tic = randomText()
-    code = f"""def f():\n   print("{tic}"\nprint()\n"""
-    letest= f""">>> f()\n{tic}\n"""
-    return code,letest
+    if type=="SyntaxeError":
+        code = f"""def f():\n   print("{tic}"\nprint()\n"""
+        letest= f""">>> f()\n{tic}\n"""
+        return code,letest, "Parenthèse sur la ligne au dessus."
+    if type=="IndentationError":
+        code = f"""def f():\nprint("{tic}"\nprint()\n"""
+        letest= f""">>> f()\n{tic}\n"""
+        return code,letest, "PROBLÈME D'INDENTATION IL MANQUE UNE TAB."
+    
