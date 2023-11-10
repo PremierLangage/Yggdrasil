c3e844bc91e19e76aee235a4e11a0cd05f545d06
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-10 15:04:54 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/template/builderror.py

--- AAAA/dominique/python/template/builderror.py
+++ AAAA/dominique/python/template/builderror.py
@@ -21,11 +21,10 @@ def builderror(type=None):
         return code,letest, "Parenthèse sur la ligne au dessus."
     if type=="IndentationError":
         if randint(1,10) <7:
-            code = f"""def f():\nprint("{tic}"\nprint()\n"""
+            code = f"""def f():\nprint("{tic}")\nprint()\n"""
             letest= f""">>> f()\n{tic}\n"""
             return code,letest, "PROBLÈME D'INDENTATION IL MANQUE UNE TAB."
         else:
-
             code = f"""
     def f(n):
     if n%2==0:
@@ -36,8 +35,9 @@ print()
             """
             letest= f""">>> f(0)\n{tic}\n"""
             return code,letest, "trop et pas asser de TABs."
-        
+
     if type=="NameError":
-        code = f"""def f(n):\n   if p: # p parametre de la fonction\n   print("{tic}"\nprint()\n"""
+        code = f"""def f(n):\n   if p: # p parametre de la fonction\n   print("{tic}")\nprint()\n"""
         letest= f""">>> f(True)\n{tic}\n"""
         return code,letest, "PROBLÈME D'INDENTATION IL MANQUE UNE TAB."
+
