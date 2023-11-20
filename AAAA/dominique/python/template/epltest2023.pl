a447fb9f5ea819363c5c4e6a3d27cf343b662616
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-20 13:59:01 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/template/epltest2023.pl

--- AAAA/dominique/python/template/epltest2023.pl
+++ AAAA/dominique/python/template/epltest2023.pl
@@ -6,7 +6,7 @@ text= Changer ce text
 
 @ feedback2023.py [feedback2.py] # utilisé par pltest_doc 
 @ plrunner2023.py [pltest_doc.py] # utilisé par grader 
-@ /grader/evaluator_v1.py [grader.py]
+@ /grader/evaluator.py [grader.py]
 @ template2023.html [template.html] # utilisé par feedback2
 # 
 
