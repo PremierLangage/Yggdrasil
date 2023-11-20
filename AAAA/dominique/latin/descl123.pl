74915746721a15393b9bd9f539e1dc08ff3221c7
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-20 16:22:13 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/latin/descl123.pl

--- AAAA/dominique/latin/descl123.pl
+++ AAAA/dominique/latin/descl123.pl
@@ -10,7 +10,7 @@ before==
 from gendec import question
 import random
 #random.seed(10)
-text, sol = question()
+text, sol, q = question()
 texti=text
 nbq=0
 nbbr=0
@@ -41,9 +41,11 @@ if ok:
     nbbr += 1 
     feedback += f'<span class="success-state">Good 👏👏👏 </span> <span > {nbbr}/{nbq} </span>'
     grade = (100," ")
+    # save to bdd (q,100) 
 else:
     feedback += f'<span class="error-state">Bad answer</span> <br/><span >{texti}<br> la bonne réponse  était : **{sol}** <br>  Nombre de réponses correctes {nbbr}/{nbq} </span>'
     grade = (0, " ")
+    # save to bdd (q,0) 
 #import random
 #random.seed(10)
 #from gendec import question
