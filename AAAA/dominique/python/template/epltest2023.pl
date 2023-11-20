1a99cc117498c83093aa212b40c1e48f1f01d10d
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-20 13:56:26 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/python/template/epltest2023.pl

---
+++ AAAA/dominique/python/template/epltest2023.pl
@@ -0,0 +1,37 @@
+
+
+
+
+
+@ feedback2023.py [feedback2.py] # utilisé par pltest_doc 
+@ plrunner2023.py [pltest_doc.py] # utilisé par grader 
+@ lib/grader/evaluator.py [grader.py]
+@ template2023.html [template.html] # utilisé par feedback2
+# 
+
+@ /utils/sandboxio.py
+@ /builder/before.py [builder.py]
+
+before ==
+
+text += "\n# vous n'avez pas défini de before"
+
+== 
+
+
+evaluator==
+
+grade=(1000,"Bravo")
+
+==
+
+
+
+editor.fontsize=14
+# une interface standard d'exercice avec un editeur pour la réponse
+form=@ /form/text_editor.html
+
+settings.allow_reroll=1
+
+
+
