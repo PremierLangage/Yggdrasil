e9f2dc5f884d8ab0387bd5a8656b747ceba69849
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-23 09:09:33 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/aleasujet.c

--- ComputerScience/C/TPNOTE/aleasujet.c
+++ ComputerScience/C/TPNOTE/aleasujet.c
@@ -8,7 +8,7 @@
 Ecrire une fonction **eval_f** qui prend en paramètre un flottant et 3 entiers (dans cet ordre) et qui retourne un flottant.
  
 
-Votre fonction **eval_f** calcule la valeur de la fonction $%f(x)={latexcode}%$.
+Votre fonction **eval_f** calcule la valeur de la fonction {latexcode}.
 
 PL:== */
 
