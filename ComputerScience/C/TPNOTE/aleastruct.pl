528811c244fa3dcdee19963accaa5f3a972286cc
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-23 14:41:07 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/aleastruct.pl

--- ComputerScience/C/TPNOTE/aleastruct.pl
+++ ComputerScience/C/TPNOTE/aleastruct.pl
@@ -17,17 +17,9 @@ editor.code==
 before_zb==
 
 text = text % globals()
-code_before = ff+code_before
+
 
 text += ff
-
-==
-
- 
-beurk==
-text = text.format(struct=struct,structname=structname,propriete=propriete)
-
-
 
 ==
 
