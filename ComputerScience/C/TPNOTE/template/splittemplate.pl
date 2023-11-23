359bfe950db71d8972591532df4d627754ae6dba
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-23 09:13:16 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/template/splittemplate.pl

--- ComputerScience/C/TPNOTE/template/splittemplate.pl
+++ ComputerScience/C/TPNOTE/template/splittemplate.pl
@@ -28,19 +28,12 @@ for k,v in splitwithre("sujet.c").items():
 
 
 
-if "aleacode" in globals():
-    cc=compile(aleacode, " Code alea du fichier c","exec")
-    exec(cc,globals())
-    if "latexcode" not in globals():
-        latexcode = "x^2+x^3+ax+bx+cx"
-    #text = text.format({"latexcode":latexcode})
-    #solution = solution % globals()
+
 
 # raise Exception(keys)
 
-code= "cac boudin" 
 
-passage = True
+
 
 ==
 
