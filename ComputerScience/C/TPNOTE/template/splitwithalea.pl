69eac4c396f8c2803aff98ba7f16a2b7a9b545c4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-23 09:17:14 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/template/splitwithalea.pl

--- ComputerScience/C/TPNOTE/template/splitwithalea.pl
+++ ComputerScience/C/TPNOTE/template/splitwithalea.pl
@@ -11,8 +11,6 @@ extends= splittemplate.pl
 
 before_zaftersplit==
 
-if "aleacode" in globals():
-    text += aleacode
 
 if "aleacode" in globals():
     cc=compile(aleacode, " Code alea du fichier c","exec")
