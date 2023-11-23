8a5750d4f47d14ecca451d52005a13c8eea8e317
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-23 09:53:23 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/exoaleacode.pl

--- ComputerScience/C/TPNOTE/exoaleacode.pl
+++ ComputerScience/C/TPNOTE/exoaleacode.pl
@@ -7,7 +7,9 @@ extends = template/splitwithalea.pl
 @ aleasujet.c [sujet.c]
 
 editor.code==
-float eval_f(float x, int a, int b, intc){
-// print("eval")
+float eval_f(float x, int a, int b, int c){
+// votre code 
 }
 ==
+
+
