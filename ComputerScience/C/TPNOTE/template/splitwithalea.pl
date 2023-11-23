a7971f4d1d250197a280dc4bd1f3028f4db93372
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-23 09:09:56 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/template/splitwithalea.pl

--- ComputerScience/C/TPNOTE/template/splitwithalea.pl
+++ ComputerScience/C/TPNOTE/template/splitwithalea.pl
@@ -18,7 +18,7 @@ if "aleacode" in globals():
     cc=compile(aleacode, " Code alea du fichier c","exec")
     exec(cc,globals())
     if "latexcode" not in globals():
-        latexcode = "x^2+x^3+ax+bx+cx"
+        latexcode = "$%f(x)=x^2+x^3+ax+bx+cx%$"
     #text = text.format({"latexcode":latexcode})
     #solution = solution % globals()
 
