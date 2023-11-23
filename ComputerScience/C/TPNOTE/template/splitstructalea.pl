81905f7700dbdac014312556476c501d0f860bb4
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-23 11:13:12 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/template/splitstructalea.pl

---
+++ ComputerScience/C/TPNOTE/template/splitstructalea.pl
@@ -0,0 +1,27 @@
+
+
+
+
+
+extends= ./splittemplate.pl
+
+
+
+
+
+
+before_zaftersplit==
+
+
+if "aleacode" in globals():
+    cc=compile(aleacode, " Code alea du fichier c","exec")
+    exec(cc,globals())
+    if "latexcode" not in globals():
+        latexcode = "$%f(x)=x^2+x^3+ax+bx+cx%$"
+
+
+    text = text.format(latex=latexcode)
+
+
+
+==
