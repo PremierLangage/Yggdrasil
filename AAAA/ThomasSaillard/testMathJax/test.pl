1411ef09db175bff7c84fe65ae7a95b8e60e4908
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-11-27 09:42:28 +0100

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/testMathJax/test.pl

--- AAAA/ThomasSaillard/testMathJax/test.pl
+++ AAAA/ThomasSaillard/testMathJax/test.pl
@@ -21,11 +21,5 @@ grade=(100," ")
 
 
 extracss==
-<style>
-.exercise__header {
-background: center no-repeat url("https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Cat_playing_with_a_lizard.jpg/2880px-Cat_playing_with_a_lizard.jpg");
-background-size: contain;
-}
-</style>
 ==
 
