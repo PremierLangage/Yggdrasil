0cfc7fc6f709ed6bd0adb302331a9d565d7c4583
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2023-11-27 17:38:31 +0100

    drevuz2 modified Yggdrasil/ComputerScience/C/TPNOTE/aleasujet.c

--- ComputerScience/C/TPNOTE/aleasujet.c
+++ ComputerScience/C/TPNOTE/aleasujet.c
@@ -8,7 +8,7 @@
 Ecrire une fonction **eval_f** qui prend en paramètre un flottant et 3 entiers (dans cet ordre) et qui retourne un flottant.
  
 
-Votre fonction **eval_f** calcule la valeur de la fonction $%{latexcode}%$, ou $%x%$ est un flottant et a,b,c des entiers.
+Votre fonction **eval_f** calcule la valeur de la fonction $%{latexcode}%$, où $%x%$ est un flottant et a,b,c des entiers.
 
 Rappel: $%z^y%$ est z à la puissance y. 
 
