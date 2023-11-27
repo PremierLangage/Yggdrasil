edae205eeca75dbdc7b455b08a60da67e4029284
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2023-11-27 09:41:55 +0100

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/testMathJax/test.pl

---
+++ AAAA/ThomasSaillard/testMathJax/test.pl
@@ -0,0 +1,31 @@
+
+
+ 
+ 
+ # The template default will no be neccessary in the futur 
+
+title= TestMathJax
+
+text== 
+
+Affichage de MathJax : $% \frac{a}{b} %$
+
+==
+
+form==
+==
+
+evaluator==
+grade=(100," ")
+==
+
+
+extracss==
+<style>
+.exercise__header {
+background: center no-repeat url("https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Cat_playing_with_a_lizard.jpg/2880px-Cat_playing_with_a_lizard.jpg");
+background-size: contain;
+}
+</style>
+==
+
