b758e6787e4abde5f6bdd6b743c0eaf25ca5dddb
EMMANUEL BRUNACHE <emmanuel.brunache@monlycee.net>
2023-12-30 19:06:18 +0100

    ebrunache5 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/2_classe_feuille.pl

---
+++ ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/2_classe_feuille.pl
@@ -0,0 +1,49 @@
+extends = /ComputerScience/NSI/templates/pltest/pltest2023.pl
+
+
+title= Classe Feuille 
+
+text==
+La classe `Feuille` servira à stocker les lettres et leurs occurences dans un arbre de huffman.
+
+Ecrire la classe `Feuille` ayant 2 attributs : 
+
+- `lettre` de type `str` correspondant à un caractère.
+- `valeur` de type entier correspondant à l'occuremce du caractère.
+
+
+Son constructeur aura deux paramètres:
+
+- la lettre de ce noeud feuille
+- l'occurence (valeur) de la lettre 
+
+On créera une Feuille avec le code suivant:
+
+    >>> Feuille('c', 2) # Feuille 'c' d'occurence 2
+
+==
+
+befor ==
+import os
+import time
+==
+
+
+editor.code==
+==
+
+pltest==
+>>> a = Feuille('c', 2)
+>>> a.lettre
+'c'
+>>> a.valeur
+2
+==
+
+soluce==
+class Feuille:
+    def __init__(self, valeur, lettre):
+        self.valeur = valeur
+        self.lettre = lettre
+==
+
