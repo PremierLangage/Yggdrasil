1349e831899c5ba3fc414c764aaf85e785c0c421
EMMANUEL BRUNACHE <emmanuel.brunache@monlycee.net>
2023-12-30 18:43:51 +0100

    ebrunache5 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/1_create_occurences.pl

---
+++ ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/1_create_occurences.pl
@@ -0,0 +1,42 @@
+extends = /ComputerScience/NSI/templates/pltest/pltest2023.pl
+
+
+title= Occurences caractères
+
+text==
+
+Écrire une fonction `create_occurences(texte)` prenant en paramètre une chaine de 
+caractères `texte` et retournant un dictionnaire sont les caractères de la chaine
+et les valeurs sont le nombre d'occurences de ceux-ci.
+
+Exemple:
+
+    >>> create_occurences("ceci est un essai")
+    {'c': 2, 'e': 3, 'i': 2, ' ': 3, 's': 3, 't': 1, 'u': 1, 'n': 1, 'a': 1}
+
+==
+
+befor ==
+import os
+import time
+==
+
+
+editor.code==
+==
+
+pltest==
+>>> occ_dict = create_occurences("ceci est un essai") #
+>>> occ_dict['c']
+2
+>>> occ_dict[' ']
+3
+==
+
+soluce==
+def create_occurences(texte):
+    d = {}
+    for caractere in texte:
+        d[caractere] = d.get(caractere, 0) + 1
+    return d
+==
