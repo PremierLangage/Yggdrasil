df0d8e73167354bfd2bf8ae55323751e7f7ff4a1
EMMANUEL BRUNACHE <emmanuel.brunache@monlycee.net>
2024-01-02 11:03:56 +0100

    ebrunache5 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/0_intro.pl

---
+++ ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/0_intro.pl
@@ -0,0 +1,15 @@
+form=@ /form/text_editor.html
+
+title = Codage de Huffman
+
+text ==
+
+L'objectif de cet exercice est de créer un [codage de huffman](https://fr.wikipedia.org/wiki/Codage_de_Huffman). Il faut donc commencer par bien lire l'article wikipédia en lien.
+
+Ressources disponibles:
+
+- https://fr.wikipedia.org/wiki/Codage_de_Huffman
+- http://lwh.free.fr/pages/algo/compression/huffman.html
+- https://www.youtube.com/watch?v=UAY-wpHZCs4
+
+==
