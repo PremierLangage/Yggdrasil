faa570e714c02256c35947ebdccace3e0725aad6
EMMANUEL BRUNACHE <emmanuel.brunache@monlycee.net>
2024-01-02 10:54:11 +0100

    ebrunache5 modified Yggdrasil/ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/0_Intro.pl

--- ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/0_Intro.pl
+++ ComputerScience/NSI/Banque_Exercices/Arbres/Huffman/0_Intro.pl
@@ -1,3 +1,4 @@
+title = Codage de Huffman
 text ==
 
 L'objectif de cet exercice est de créer un [codage de huffman](https://fr.wikipedia.org/wiki/Codage_de_Huffman). Il faut donc commencer par bien lire l'article wikipédia en lien.
