082e63b1ac20ebcd44d194a5d09f320484632e7e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2024-01-18 10:01:04 +0100

    drevuz2 modified Yggdrasil/ComputerScience/python/DIGITALU/exo1.pl

---
+++ ComputerScience/python/DIGITALU/exo1.pl
@@ -0,0 +1,34 @@
+
+extends = /AAAA/dominique/python/template/pltest2023.pl
+
+
+title = Des variables 
+
+remarque ==
+
+Le type int (entier)
+Le type float (flottant)
+Le type str (chaîne de caractères)
+Le type bool (booléen)
+Le type list (liste)
+Le type complex (complexe)
+
+==
+
+
+
+
+text==
+
+On vous demande de créer les variables A,B,C,D,E,F avec des contenus de type différents:
+
+A doit être un **int** par exemple: 1   
+B doit être un **float** 
+C doit être une **str** une chaine de caractère  
+D doit être une **list** une liste (vide par exemple)
+E doit être de type **NoneType** 
+F doit être une **fonction** , initialiser C avec le nom d'une fonction écrite avant.
+
+==
+
+
