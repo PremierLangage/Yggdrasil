609f7f22f8d01b7c580559decbc66cb3b3d34d4f
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2024-01-22 16:12:39 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/grid/euclide.pl

---
+++ AAAA/dominique/grid/euclide.pl
@@ -0,0 +1,8 @@
+
+euclide=
+    Un segment de droite peut être tracé en joignant deux points quelconques.
+    Un segment de droite peut être prolongé indéfiniment en une ligne droite.
+    Étant donné un segment de droite quelconque, un cercle peut être tracé en prenant ce segment comme rayon et l'une de ses extrémités comme centre.
+    Tous les angles droits sont congruents.
+    Si deux lignes droites sont sécantes avec une troisième de telle façon que la somme des angles intérieurs d'un côté est inférieure à deux angles droits, alors ces deux lignes sont forcément sécantes de ce côté.
+==
