c998b9711d30980ff41d6e87cde8b62b64547518
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2024-03-18 15:43:39 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/C/splitcode/pasfacile.c

--- AAAA/dominique/C/splitcode/pasfacile.c
+++ AAAA/dominique/C/splitcode/pasfacile.c
@@ -164,7 +164,7 @@ QUESTIONS=["Pas de question","Écrire la fonction **int hauteur(Arbre a)**.",
 "Écrire la fonction **int est_strictement_binaire(Arbre a)** qui vérifie que tout les noeuds on un nombre pair de fils."
 ]
 
-code_before += globals()["sol"+str(VERSION)]
+solution = globals()["sol"+str(VERSION)]
 
 text = QUESTIONS[VERSION]
 
