d636deb661f4cbf3945e72af3fde92d78ecaa151
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2024-03-18 15:54:36 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/C/splitcode/pasfacile.c

--- AAAA/dominique/C/splitcode/pasfacile.c
+++ AAAA/dominique/C/splitcode/pasfacile.c
@@ -125,7 +125,7 @@ int main(int argc, char const *argv[])
     srand(atoi(argv[1]));
     for(int i=0;i < 5 ; i++){
 
-    Arbre a = BuildTree();
+    Arbre a = BuildTree(10+rand() % (50*i+1));
     fprintf(stderr,"kiki");
     if (i< 2) { printf("arbre parcourt préfixe : "); printTree(a);}
 
