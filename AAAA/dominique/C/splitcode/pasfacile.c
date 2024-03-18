6d1f404be7262ce3ec5df4ae3edc388ecf2aff8c
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2024-03-18 15:50:41 +0100

    drevuz2 modified Yggdrasil/AAAA/dominique/C/splitcode/pasfacile.c

--- AAAA/dominique/C/splitcode/pasfacile.c
+++ AAAA/dominique/C/splitcode/pasfacile.c
@@ -121,13 +121,12 @@ int est_strictement_binaire(Arbre a){
 // PL:code_after==
 int main(int argc, char const *argv[])
 {
-    fprintf(stderr,"kiki");
-    srand(atoi(argv[1]));
-        fprintf(stderr,"kiki");
-    for(int i=0;i < 5 ; i++){
-            fprintf(stderr,"kiki");
-    Arbre a = BuildTree(50+rand() % (50*i));
 
+    srand(atoi(argv[1]));
+    for(int i=0;i < 5 ; i++){
+
+    Arbre a = BuildTree(50+rand() % (50*i));
+    fprintf(stderr,"kiki");
     if (i< 2) { printf("arbre parcourt préfixe : "); printTree(a);}
 
 
