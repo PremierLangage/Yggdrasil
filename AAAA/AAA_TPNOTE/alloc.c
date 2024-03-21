4b9c396a8be511ab49480bd8d0fe9e03fe7d6b7e
Dominique REVUZ <dominique.revuz@univ-eiffel.fr>
2024-03-21 16:44:46 +0100

    drevuz2 modified Yggdrasil/AAAA/AAA_TPNOTE/alloc.c

--- AAAA/AAA_TPNOTE/alloc.c
+++ AAAA/AAA_TPNOTE/alloc.c
@@ -47,7 +47,7 @@ On considère les structures suivantes pour représenter un arbre binaire:
 
 
 vous devez seulement ecrire une fonction **Arbre *createTree(int n)** qui alloue un arbre de taille N .
-et une fonction **Arbre *reallocTree(Arbre *a, int n)** qui réalloue à la taille N l'arbre passé en parametre.
+et une fonction **Arbre \*reallocTree(Arbre *a, int n)** qui réalloue à la taille N l'arbre passé en parametre.
 
 PL:== */ 
 
