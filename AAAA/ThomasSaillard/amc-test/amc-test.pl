b9e1b65006ae1605132984963c693b8e840af076
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2024-03-22 10:10:00 +0100

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/amc-test/amc-test.pl

---
+++ AAAA/ThomasSaillard/amc-test/amc-test.pl
@@ -0,0 +1,17 @@
+
+extends= /model/AMC2/QCM-AMCE.pl
+
+title= Fichier de questions
+
+onepergroup=true
+
+text==
+énoncé du QCM 
+==
+
+questions=
+@ exe.txt [question1.txt]
+@ exe.txt [question2.txt]
+
+
+
