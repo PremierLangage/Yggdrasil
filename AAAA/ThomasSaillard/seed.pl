5da2301f5d9cb0dfa42a71997e176202760bb615
THOMAS SAILLARD <thomas.saillard@edu.univ-eiffel.fr>
2024-04-10 09:33:07 +0200

    tsaillard1 modified Yggdrasil/AAAA/ThomasSaillard/seed.pl

--- AAAA/ThomasSaillard/seed.pl
+++ AAAA/ThomasSaillard/seed.pl
@@ -2,4 +2,4 @@
 title=Description
 text=test pour afficher la seeed
 type=description
-evaluator=def evaluator(reponse, dico): return True, 'Merci pour votre lecture'
+evaluator==def evaluator(reponse, dico): return True, 'Merci pour votre lecture'
