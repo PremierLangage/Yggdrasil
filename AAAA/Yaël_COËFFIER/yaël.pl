56cf70a72f4de4d3bd694d2c11eaad75a204a829
YAEL COEFFIER <yael.coeffier@edu.univ-eiffel.fr>
2024-05-14 11:15:51 +0200

    ycoeffier modified Yggdrasil/AAAA/Yaël_COËFFIER/yaël.pl

--- AAAA/Yaël_COËFFIER/yaël.pl
+++ AAAA/Yaël_COËFFIER/yaël.pl
@@ -3,10 +3,17 @@ extends = /ComputerScience/python/AP1-1920/templates/plsoluce.pl
 title = Bob
 
 text ==
+Lire une liste d'entiers et afficher le résultat de la fonction somme()
 Ecrire une fonction somme() qui prend une liste et qui retourne la somme des éléments de la liste.
 ==
 
 soluce ==
 def somme(l):
     return sum(l)
+
+l = []
+while a:=int(input()):
+    l.append(a)
 ==
+
+mplsoluce0==
