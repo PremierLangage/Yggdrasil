b8174629393a791c5c89f227ee8e86a8d786f04f
YAEL COEFFIER <yael.coeffier@edu.univ-eiffel.fr>
2024-05-14 11:11:43 +0200

    ycoeffier modified Yggdrasil/AAAA/Yaël_COËFFIER/yaël.pl

--- AAAA/Yaël_COËFFIER/yaël.pl
+++ AAAA/Yaël_COËFFIER/yaël.pl
@@ -1,3 +1,7 @@
 extends = /ComputerScience/python/AP1-1920/templates/plsoluce.pl
 
 title = Bob
+
+text ==
+Ecrire une fonction somme() qui prend une liste et qui retourne la somme des éléments de la liste.
+==
